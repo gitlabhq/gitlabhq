f8b5e5f78ebcd7f37a0dbfb299e96d918d9c40fa
gitlabhq <m@gitlabhq.com>
2011-10-18 17:57:01 +0300

    dashboard

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,7 +6,7 @@ class ProjectsController < ApplicationController
   before_filter :authorize_read_project!, :except => [:index, :new, :create] 
   before_filter :authorize_admin_project!, :only => [:edit, :update, :destroy] 
 
-  before_filter :require_non_empty_project, :only => [:blob, :tree, :show]
+  before_filter :require_non_empty_project, :only => [:blob, :tree]
 
   def index
     @projects = current_user.projects.all
@@ -60,21 +60,25 @@ class ProjectsController < ApplicationController
   end
 
   def show
-    @date = case params[:view]
-            when "week" then Date.today - 7.days
-            else Date.today
-            end
+    if @project.repo_exists?
+      @date = case params[:view]
+              when "week" then Date.today - 7.days
+              else Date.today
+              end
 
-    @heads = @project.repo.heads
-    @commits = @heads.map do |h| 
-      @project.repo.log(h.name, nil, :since => @date - 1.day)
-    end.flatten.uniq { |c| c.id }
-    
-    @commits.sort! do |x, y|
-      y.committed_date <=> x.committed_date
+      @heads = @project.repo.heads
+      @commits = @heads.map do |h| 
+        @project.repo.log(h.name, nil, :since => @date - 1.day)
+      end.flatten.uniq { |c| c.id }
+
+      @commits.sort! do |x, y|
+        y.committed_date <=> x.committed_date
+      end
+
+      @messages = project.notes.since(@date).limit(40).order("created_at DESC")
+    else 
+      return render "projects/empty"
     end
-
-    @messages = project.notes.since(@date).limit(40).order("created_at DESC")
   end
 
   #
