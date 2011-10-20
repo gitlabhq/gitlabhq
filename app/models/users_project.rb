0f0c562de09e325c5689b3db8e646b68322f5515
Adam Leonard <adamjamesleonard@gmail.com>
2011-10-20 09:48:09 -0400

    Add check to ensure that a team member has atleast one Access Role selected

--- app/models/users_project.rb
+++ app/models/users_project.rb
@@ -9,13 +9,18 @@ class UsersProject < ActiveRecord::Base
   validates_uniqueness_of :user_id, :scope => [:project_id]
   validates_presence_of :user_id
   validates_presence_of :project_id
- 
+  validate :user_has_a_role_selected
+
   delegate :name, :email, :to => :user, :prefix => true
 
   def update_gitosis_project
     Gitosis.new.configure do |c|
       c.update_project(project.path, project.gitosis_writers)
     end
+  end
+
+  def user_has_a_role_selected
+    errors.add(:base, "Please choose at least one Role in the Access list") unless read || write || admin
   end
 
 end
