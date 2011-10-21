1c2660729003932183ec6c7973e4ee15192f99d7
gitlabhq <m@gitlabhq.com>
2011-10-21 16:36:33 +0300

    corrected last migration

--- db/migrate/20111021101550_change_social_fields_in_users.rb
+++ db/migrate/20111021101550_change_social_fields_in_users.rb
@@ -1,8 +1,12 @@
 class ChangeSocialFieldsInUsers < ActiveRecord::Migration
   def up
-  	change_column(:users, :skype, :string, {:null => false, :default => ''})
-  	change_column(:users, :linkedin, :string, {:null => false, :default => ''})
-  	change_column(:users, :twitter, :string, {:null => false, :default => ''})
+    remove_column :users, :skype
+    remove_column :users, :linkedin
+    remove_column :users, :twitter
+
+    add_column :users, :skype, :string, {:null => false, :default => ''}
+    add_column :users, :linkedin, :string, {:null => false, :default => ''}
+    add_column :users, :twitter, :string, {:null => false, :default => ''}
   end
 
   def down
