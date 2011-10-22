1a817b58b88e226f42aaaa236486073afcfbee5a
VSizov <fk@sphere.com>
2011-10-22 13:50:02 +0300

    fix remove repo

--- lib/gitosis.rb
+++ lib/gitosis.rb
@@ -42,7 +42,7 @@ class Gitosis
   end
 
   def destroy_project(project)
-    `rm -Rf #{project.path_to_repo}`
+    `sudo -u git rm -Rf #{project.path_to_repo}`
     
     conf = IniFile.new(File.join(@local_dir,'gitosis','gitosis.conf'))
 
