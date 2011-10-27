5e584ee6264962cc60d9074c33141a28b4100607
Nihad Abbasov <narkoz.2008@gmail.com>
2011-10-27 12:11:42 +0500

    refactor lifetime_select helper

--- app/helpers/snippets_helper.rb
+++ app/helpers/snippets_helper.rb
@@ -1,10 +1,10 @@
 module SnippetsHelper
-  def snippet_lifetime_select_options
+  def lifetime_select_options
     options = [
         ['forever', nil],
-        ['1 day',   Date.strptime("#{Date.current.day}.#{Date.current.month}.#{Date.current.year}", "%d.%m.%Y") + 1.day],
-        ['1 week',  Date.strptime("#{Date.current.day}.#{Date.current.month}.#{Date.current.year}", "%d.%m.%Y") + 1.week],
-        ['1 month', Date.strptime("#{Date.current.day}.#{Date.current.month}.#{Date.current.year}", "%d.%m.%Y") + 1.month]
+        ['1 day',   "#{Date.current + 1.day}"],
+        ['1 week',  "#{Date.current + 1.week}"],
+        ['1 month', "#{Date.current + 1.month}"]
     ]
     options_for_select(options)
   end
