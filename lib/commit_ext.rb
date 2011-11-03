3abd977822247581465ff6d6df52d2c08e8da508
gitlabhq <m@gitlabhq.com>
2011-11-03 06:56:26 -0400

    fixed error with ascii error for dashboard

--- lib/commit_ext.rb
+++ lib/commit_ext.rb
@@ -12,4 +12,12 @@ module CommitExt
   def created_at
     committed_date
   end
+
+  def author_email
+    author.email.force_encoding(Encoding::UTF_8)
+  end
+
+  def author_name
+    author.name.force_encoding(Encoding::UTF_8)
+  end
 end
