3538ae85003ead7cd2d425b74426dac2accaa72a
gitlabhq <m@gitlabhq.com>
2011-11-15 08:51:43 -0500

    travis conf

--- db/fixtures/test/001_repo.rb
+++ db/fixtures/test/001_repo.rb
@@ -1,3 +1,7 @@
+# create tmp dir if not exist
+tmp_dir = File.join(Rails.root, "tmp")
+Dir.mkdir(tmp_dir) unless File.exists?(tmp_dir)
+
 # Create dir for test repo
 repo_dir = File.join(Rails.root, "tmp", "tests")
 Dir.mkdir(repo_dir) unless File.exists?(repo_dir)
