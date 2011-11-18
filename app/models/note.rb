91d5a906f9cd1f120312d1ca72c8e500263c4439
Drew <bowmande@gmail.com>
2011-11-18 01:52:13 -0500

    Resolved issue #235

--- app/models/note.rb
+++ app/models/note.rb
@@ -27,6 +27,7 @@ class Note < ActiveRecord::Base
 
   scope :common, where(:noteable_id => nil)
 
+  scope :today, where("created_at >= :date", :date => Date.today)
   scope :last_week, where("created_at  >= :date", :date => (Date.today - 7.days))
   scope :since, lambda { |day| where("created_at  >= :date", :date => (day)) }
   scope :fresh, order("created_at DESC")
