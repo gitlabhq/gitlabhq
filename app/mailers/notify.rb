d61268e4f15502d46a61ee34f27248194c2f3f53
Valery Sizov <vsv2711@gmail.com>
2011-11-22 23:36:14 +0200

    [#169] – Move email 'from' & 'host' settings to config file

--- app/mailers/notify.rb
+++ app/mailers/notify.rb
@@ -5,7 +5,7 @@ class Notify < ActionMailer::Base
   def new_user_email(user, password)
     @user = user
     @password = password
-    mail(:to => @user.email, :subject => "gitlab | Account was created for you")
+    mail(:to => @user.email, :subject => "gitlab | Account was created for you", :from => EMAIL_OPTS["from"])
   end
 
   def new_issue_email(issue)
@@ -13,14 +13,14 @@ class Notify < ActionMailer::Base
     @project = issue.project
     @issue = issue
 
-    mail(:to => @user.email, :subject => "gitlab | New Issue was created")
+    mail(:to => @user.email, :subject => "gitlab | New Issue was created", :from => EMAIL_OPTS["from"])
   end
 
   def note_wall_email(user, note)
     @user = user
     @note = note
     @project = note.project
-    mail(:to => @user.email, :subject => "gitlab | #{@note.project.name} ")
+    mail(:to => @user.email, :subject => "gitlab | #{@note.project.name} ", :from => EMAIL_OPTS["from"])
   end
 
   def note_commit_email(user, note)
@@ -28,7 +28,7 @@ class Notify < ActionMailer::Base
     @note = note
     @project = note.project
     @commit = @project.repo.commits(note.noteable_id).first
-    mail(:to => @user.email, :subject => "gitlab | #{@note.project.name} ")
+    mail(:to => @user.email, :subject => "gitlab | #{@note.project.name} ", :from => EMAIL_OPTS["from"])
   end
 
   def note_issue_email(user, note)
@@ -36,6 +36,6 @@ class Notify < ActionMailer::Base
     @note = note
     @project = note.project
     @issue = note.noteable
-    mail(:to => @user.email, :subject => "gitlab | #{@note.project.name} ")
+    mail(:to => @user.email, :subject => "gitlab | #{@note.project.name} ", :from => EMAIL_OPTS["from"])
   end
 end
--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,1 +1,2 @@
 GITOSIS = YAML.load_file("#{Rails.root}/config/gitosis.yml")
+EMAIL_OPTS = YAML.load_file("#{Rails.root}/config/email.yml")
