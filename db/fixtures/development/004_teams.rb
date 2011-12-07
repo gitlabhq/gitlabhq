2552a877df7fdb2c0a17a88479a5e49f315bcbad
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2011-12-07 22:55:09 +0200

    updated seeds for new permissions, issue seeds added

--- db/fixtures/development/004_teams.rb
+++ db/fixtures/development/004_teams.rb
@@ -1,21 +1,21 @@
 UsersProject.seed(:id, [
-  { :id => 1,  :project_id => 1, :user_id => 1, :read => true, :write => true,  :admin => true  },
-  { :id => 2,  :project_id => 1, :user_id => 2, :read => true, :write => false, :admin => false },
-  { :id => 3,  :project_id => 1, :user_id => 3, :read => true, :write => false, :admin => false },
-  { :id => 4,  :project_id => 1, :user_id => 4, :read => true, :write => false, :admin => false },
-  { :id => 5,  :project_id => 1, :user_id => 5, :read => true, :write => false, :admin => false },
+  { :id => 1,  :project_id => 1, :user_id => 1, :project_access => Project::PROJECT_RWA, :repo_access => Repository::REPO_N },
+  { :id => 2,  :project_id => 1, :user_id => 2, :project_access => Project::PROJECT_RW,  :repo_access => Repository::REPO_N },
+  { :id => 3,  :project_id => 1, :user_id => 3, :project_access => Project::PROJECT_RW,  :repo_access => Repository::REPO_N },
+  { :id => 4,  :project_id => 1, :user_id => 4, :project_access => Project::PROJECT_R,   :repo_access => Repository::REPO_N },
+  { :id => 5,  :project_id => 1, :user_id => 5, :project_access => Project::PROJECT_R,   :repo_access => Repository::REPO_N },
 
-  { :id => 6,  :project_id => 2, :user_id => 1, :read => true, :write => true,  :admin => true  },
-  { :id => 7,  :project_id => 2, :user_id => 2, :read => true, :write => false, :admin => false },
-  { :id => 8,  :project_id => 2, :user_id => 3, :read => true, :write => false, :admin => false },
-  { :id => 9,  :project_id => 2, :user_id => 4, :read => true, :write => false, :admin => false },
-  { :id => 11, :project_id => 2, :user_id => 5, :read => true, :write => false, :admin => false },
+  { :id => 6,  :project_id => 2, :user_id => 1, :project_access => Project::PROJECT_RWA, :repo_access => Repository::REPO_N },
+  { :id => 7,  :project_id => 2, :user_id => 2, :project_access => Project::PROJECT_R,   :repo_access => Repository::REPO_N },
+  { :id => 8,  :project_id => 2, :user_id => 3, :project_access => Project::PROJECT_R,   :repo_access => Repository::REPO_N },
+  { :id => 9,  :project_id => 2, :user_id => 4, :project_access => Project::PROJECT_RWA, :repo_access => Repository::REPO_N },
+  { :id => 11, :project_id => 2, :user_id => 5, :project_access => Project::PROJECT_RWA, :repo_access => Repository::REPO_N },
 
-  { :id => 12, :project_id => 3, :user_id => 1, :read => true, :write => true,  :admin => true  },
-  { :id => 13, :project_id => 3, :user_id => 2, :read => true, :write => false, :admin => false },
-  { :id => 14, :project_id => 3, :user_id => 3, :read => true, :write => false, :admin => false },
-  { :id => 15, :project_id => 3, :user_id => 4, :read => true, :write => false, :admin => false },
-  { :id => 16, :project_id => 3, :user_id => 5, :read => true, :write => false, :admin => false }
+  { :id => 12, :project_id => 3, :user_id => 1, :project_access => Project::PROJECT_RWA, :repo_access => Repository::REPO_N },
+  { :id => 13, :project_id => 3, :user_id => 2, :project_access => Project::PROJECT_R,   :repo_access => Repository::REPO_N },
+  { :id => 14, :project_id => 3, :user_id => 3, :project_access => Project::PROJECT_RWA, :repo_access => Repository::REPO_N },
+  { :id => 15, :project_id => 3, :user_id => 4, :project_access => Project::PROJECT_R,   :repo_access => Repository::REPO_N },
+  { :id => 16, :project_id => 3, :user_id => 5, :project_access => Project::PROJECT_RWA, :repo_access => Repository::REPO_N }
 ])
 
 
