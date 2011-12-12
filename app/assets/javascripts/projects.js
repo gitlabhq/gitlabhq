faf7c79eb2d0c380b692be413953d206f2984b0b
Dmitriy Zaporozhets <dzaporozhets@sphereconsultinginc.com>
2011-12-12 20:08:04 +0200

    lil js refactoring

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,17 +1,4 @@
 $(document).ready(function(){
-  $('#tree-slider td.tree-item-file-name a, #tree-breadcrumbs a').live("click", function() {
-    history.pushState({ path: this.path }, '', this.href)
-  })
-
-  $("#tree-slider tr.tree-item").live('click', function(e){
-    if(e.target.nodeName != "A") {
-      e.stopPropagation();
-      link = $(this).find("td.tree-item-file-name a")
-      link.click();
-      return false;
-    }
-  });
-
   $("#projects-list .project").live('click', function(e){
     if(e.target.nodeName != "A" && e.target.nodeName != "INPUT") {
       location.href = $(this).attr("url");
