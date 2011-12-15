0712e78597e5954ea61def4c0a6836a3d5b5d275
Dmitriy Zaporozhets <dzaporozhets@sphereconsultinginc.com>
2011-12-15 08:12:24 +0200

    Snippet restyle

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -28,6 +28,7 @@ class Snippet < ActiveRecord::Base
 
   scope :fresh, order("created_at DESC")
   scope :non_expired, where(["expires_at IS NULL OR expires_at > ?", Time.current])
+  scope :expired, where(["expires_at IS NOT NULL AND expires_at < ?", Time.current])
 
   def self.content_types
     [
