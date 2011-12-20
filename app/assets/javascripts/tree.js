3d4f524dcb7bc8c3fb10daaf49d1dd8504967706
Dmitriy Zaporozhets <dzaporozhets@sphereconsultinginc.com>
2011-12-20 08:59:14 +0200

    play with tree view

--- app/assets/javascripts/tree.js
+++ app/assets/javascripts/tree.js
@@ -5,17 +5,23 @@
 var Tree = { 
   init: 
     function() { 
+      (new Image).src = "/assets/ajax-loader-tree.gif";
+
       $('#tree-slider td.tree-item-file-name a, #tree-breadcrumbs a').live("click", function() {
         history.pushState({ path: this.path }, '', this.href)
+        $("#tree-content-holder").hide("slide", { direction: "left" }, 150)
       })
 
       $("#tree-slider tr.tree-item").live('click', function(e){
         if(e.target.nodeName != "A") {
-          e.stopPropagation();
           link = $(this).find("td.tree-item-file-name a");
-          link.click();
-          return false;
+          link.trigger("click");
         }
+      });
+
+      $('#tree-slider td.tree-item-file-name a, #tree-breadcrumbs a').live({ 
+        "ajax:beforeSend": function() { $('h2.icon').addClass("loading") },
+        "ajax:complete": function() { $('h2.icon').removeClass("loading")} 
       });
     }
 }
