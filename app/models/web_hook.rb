5ca836048d4f3319d24d2d4580912102eed54618
Ariejan de Vroom <ariejan@ariejan.net>
2011-12-26 10:12:09 +0100

    Use URI::regexp for validating WebHook urls

--- app/models/web_hook.rb
+++ app/models/web_hook.rb
@@ -9,7 +9,7 @@ class WebHook < ActiveRecord::Base
   validates :url,
             presence: true,
             format: {
-              with: /(^$)|(^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix,
+              with: URI::regexp(%w(http https)),
               message: "should be a valid url" }
 
   def execute(data)
