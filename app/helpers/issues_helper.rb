29819a73d0c27822c34be075ae2b97fb631786fe
Cedric Gatay <cedric@gatay.fr>
2011-12-29 19:52:33 +0100

    Added note when issue status changed (closed / reopened)

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -9,4 +9,11 @@ module IssuesHelper
     params[:f] ||= cookies['issue_filter']
     project_issues_path project, params
   end
+
+  def project_issues_change_status issue, is_being_closed
+    note = Note.new(:noteable => issue, :project => @project)
+    note.author = current_user
+    note.note = "_Status changed to #{is_being_closed ? 'closed' : 'reopened'}_"
+    note.save()
+  end
 end
