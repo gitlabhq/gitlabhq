dda6b0ab63eb8080e34b4273cfb6aadb7a29c028
miks <miks@cubesystems.lv>
2011-12-31 19:40:38 +0200

    Unused validator deleted

--- lib/unique_public_key_validator.rb
+++
@@ -1,7 +0,0 @@
-class UniquePublicKeyValidator < ActiveModel::Validator
-  def validate(record)
-    if (DeployKey.where('key = ? AND id !=?', record.key , record.id).count > 0 || Key.where('key = ? AND id !=?', record.key , record.id).count > 0)
-      record.errors.add :key, 'already exist.'
-    end
-  end
-end
