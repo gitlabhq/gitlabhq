97e9774336c1de54d2734304dcb94511682f6613
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2012-01-29 12:45:51 +0200

    fixed tree view

--- app/assets/javascripts/tree.js
+++ app/assets/javascripts/tree.js
@@ -5,21 +5,21 @@
 var Tree = { 
   init: 
     function() { 
-      (new Image).src = "ajax-loader-facebook.gif";
+      (new Image).src = "/assets/ajax-loader-facebook.gif";
 
-      $('#tree-slider td.tree-item-file-name a, #tree-breadcrumbs a').live("click", function() {
+      $('#tree-slider .tree-item-file-name a, .breadcrumb a').live("click", function() {
         history.pushState({ path: this.path }, '', this.href)
         $("#tree-content-holder").hide("slide", { direction: "left" }, 150)
       })
 
-      $("#tree-slider tr.tree-item").live('click', function(e){
+      $("#tree-slider .tree-item").live('click', function(e){
         if(e.target.nodeName != "A") {
-          link = $(this).find("td.tree-item-file-name a");
+          link = $(this).find(".tree-item-file-name a");
           link.trigger("click");
         }
       });
 
-      $('#tree-slider td.tree-item-file-name a, #tree-breadcrumbs a').live({ 
+      $('#tree-slider td.tree-item-file-name a, .breadcrumb a').live({ 
         "ajax:beforeSend": function() { $('.tree_progress').addClass("loading"); },
         "ajax:complete": function() { $('.tree_progress').removeClass("loading"); } 
       });
