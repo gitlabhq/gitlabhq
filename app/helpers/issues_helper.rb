c90f1d5c8bed09aeb51a11d223025b423b048143
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2012-02-05 12:58:02 +0200

    fixed issue, mr crash if assignee/author removed from team

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -9,4 +9,26 @@ module IssuesHelper
     params[:f] ||= cookies['issue_filter']
     project_issues_path project, params
   end
+
+  def link_to_issue_assignee(issue)
+    project = issue.project
+
+    tm = project.team_member_by_id(issue.assignee_id)
+    if tm
+      link_to issue.assignee_name, project_team_member_path(project, tm), :class => "author_link"
+    else
+      issue.assignee_name
+    end
+  end
+
+  def link_to_issue_author(issue)
+    project = issue.project
+
+    tm = project.team_member_by_id(issue.author_id)
+    if tm
+      link_to issue.author_name, project_team_member_path(project, tm), :class => "author_link"
+    else
+      issue.author_name
+    end
+  end
 end
