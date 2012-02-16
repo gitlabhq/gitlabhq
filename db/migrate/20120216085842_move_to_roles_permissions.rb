9a24ccdea1dd4dc82ac7f24c1fa1706a20949898
Dmitriy Zaporozhets <dzaporozhets@sphereconsultinginc.com>
2012-02-16 09:04:56 +0200

    Fixed mistype in permissions, Fixed migration

--- db/migrate/20120216085842_move_to_roles_permissions.rb
+++ db/migrate/20120216085842_move_to_roles_permissions.rb
@@ -5,8 +5,12 @@ class MoveToRolesPermissions < ActiveRecord::Migration
     repo_rw = 2
     project_rwa = 3
 
+
+    # Build masters and reset repo_access
+    UsersProject.update_all({:project_access => UsersProject::MASTER, :repo_access => 99 }, ["project_access = ?", project_rwa])
+
+    # Build other roles based on repo access
     UsersProject.update_all ["project_access = ?", UsersProject::DEVELOPER], ["repo_access = ?", repo_rw]
-    UsersProject.update_all ["project_access = ?", UsersProject::MASTER], ["project_access = ?", project_rwa]
     UsersProject.update_all ["project_access = ?", UsersProject::REPORTER], ["repo_access = ?", repo_r]
     UsersProject.update_all ["project_access = ?", UsersProject::GUEST], ["repo_access = ?", repo_n]
 
