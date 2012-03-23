255a9e0325471aa270a65201a0b43c77823faf06
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2012-03-23 23:00:05 +0200

    Improved resque.sh

--- resque.sh
+++ resque.sh
@@ -1,2 +1,2 @@
 mkdir tmp/pids
-nohup bundle exec rake environment resque:work QUEUE=* RAILS_ENV=production PIDFILE=tmp/pids/resque_worker.pid & >> log/resque_worker.log 2>&1
+bundle exec rake environment resque:work QUEUE=post_receive RAILS_ENV=production PIDFILE=tmp/pids/resque_worker.pid BACKGROUND=yes
