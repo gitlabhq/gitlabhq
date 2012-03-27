474a42cfbd939f644eb5d900e447dae48142508a
Ariejan de Vroom <ariejan@ariejan.net>
2012-03-27 16:59:25 +0300

    Only load MySQL2Adapter connection fix is MySQL2 is available. Useful for Sqlite or Postgres installations.

--- config/initializers/connection_fix.rb
+++ config/initializers/connection_fix.rb
@@ -1,16 +1,18 @@
-module ActiveRecord::ConnectionAdapters
-  class Mysql2Adapter
-    alias_method :execute_without_retry, :execute
+if defined?(ActiveRecord::ConnectionAdapters::Mysql2Adapter)
+  module ActiveRecord::ConnectionAdapters
+    class Mysql2Adapter
+      alias_method :execute_without_retry, :execute
 
-    def execute(*args)
-      execute_without_retry(*args)
-    rescue Mysql2::Error => e
-      if e.message =~ /server has gone away/i
-        warn "Server timed out, retrying"
-        reconnect!
-        retry
-      else
-        raise e
+      def execute(*args)
+        execute_without_retry(*args)
+      rescue Mysql2::Error => e
+        if e.message =~ /server has gone away/i
+          warn "Server timed out, retrying"
+          reconnect!
+          retry
+        else
+          raise e
+        end
       end
     end
   end
