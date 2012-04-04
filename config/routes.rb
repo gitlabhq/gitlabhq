80d333a121de4773ef99edbe852cd54a97dfb335
Nate Steffan <nsteffan@primetherapeutics.com>
2012-04-04 08:52:14 -0500

    Added a scope to enable the ability to use a context root on the application.

--- config/routes.rb
+++ config/routes.rb
@@ -1,134 +1,136 @@
 Gitlab::Application.routes.draw do
-  get 'search' => "search#show"
+  scope ENV['RAILS_RELATIVE_URL_ROOT'] || '/' do
+    get 'search' => "search#show"
 
-  # Optionally, enable Resque here
-  require 'resque/server'
-  mount Resque::Server.new, at: '/info/resque'
+    # Optionally, enable Resque here
+    require 'resque/server'
+    mount Resque::Server.new, at: '/info/resque'
 
-  get 'help' => 'help#index'
-  get 'help/permissions' => 'help#permissions'
-  get 'help/workflow' => 'help#workflow'
-  get 'help/web_hooks' => 'help#web_hooks'
+    get 'help' => 'help#index'
+    get 'help/permissions' => 'help#permissions'
+    get 'help/workflow' => 'help#workflow'
+    get 'help/web_hooks' => 'help#web_hooks'
 
-  namespace :admin do
-    resources :users do 
-      member do 
-        put :team_update
+    namespace :admin do
+      resources :users do 
+        member do 
+          put :team_update
+        end
       end
-    end
-    resources :projects, :constraints => { :id => /[^\/]+/ } do 
-      member do 
-        get :team
-        put :team_update
+      resources :projects, :constraints => { :id => /[^\/]+/ } do 
+        member do 
+          get :team
+          put :team_update
+        end
       end
-    end
-    resources :team_members, :only => [:edit, :update, :destroy]
-    get 'emails', :to => 'mailer#preview'
-    get 'mailer/preview_note'
-    get 'mailer/preview_user_new'
-    get 'mailer/preview_issue_new'
-    root :to => "users#index"
-  end
-
-  get "errors/githost"
-  get "profile/password", :to => "profile#password"
-  put "profile/password", :to => "profile#password_update"
-  put "profile/reset_private_token", :to => "profile#reset_private_token"
-  get "profile", :to => "profile#show"
-  get "profile/design", :to => "profile#design"
-  put "profile/update", :to => "profile#update"
-
-  get "dashboard", :to => "dashboard#index"
-  get "dashboard/issues", :to => "dashboard#issues"
-  get "dashboard/merge_requests", :to => "dashboard#merge_requests"
-  get "dashboard/activities", :to => "dashboard#activities"
-
-  #get "profile/:id", :to => "profile#show"
-
-  resources :projects, :constraints => { :id => /[^\/]+/ }, :only => [:new, :create, :index]
-  resources :keys
-
-  devise_for :users, :controllers => { :omniauth_callbacks => :omniauth_callbacks }
-
-  resources :projects, :constraints => { :id => /[^\/]+/ }, :except => [:new, :create, :index], :path => "/" do
-    member do
-      get "team"
-      get "wall"
-      get "graph"
-      get "files"
+      resources :team_members, :only => [:edit, :update, :destroy]
+      get 'emails', :to => 'mailer#preview'
+      get 'mailer/preview_note'
+      get 'mailer/preview_user_new'
+      get 'mailer/preview_issue_new'
+      root :to => "users#index"
     end
 
-    resources :wikis, :only => [:show, :edit, :destroy, :create] do
+    get "errors/githost"
+    get "profile/password", :to => "profile#password"
+    put "profile/password", :to => "profile#password_update"
+    put "profile/reset_private_token", :to => "profile#reset_private_token"
+    get "profile", :to => "profile#show"
+    get "profile/design", :to => "profile#design"
+    put "profile/update", :to => "profile#update"
+
+    get "dashboard", :to => "dashboard#index"
+    get "dashboard/issues", :to => "dashboard#issues"
+    get "dashboard/merge_requests", :to => "dashboard#merge_requests"
+    get "dashboard/activities", :to => "dashboard#activities"
+
+    #get "profile/:id", :to => "profile#show"
+
+    resources :projects, :constraints => { :id => /[^\/]+/ }, :only => [:new, :create, :index]
+    resources :keys
+
+    devise_for :users, :controllers => { :omniauth_callbacks => :omniauth_callbacks }
+
+    resources :projects, :constraints => { :id => /[^\/]+/ }, :except => [:new, :create, :index], :path => "/" do
       member do
-        get "history"        
-      end
-    end
-
-    resource :repository do 
-      member do 
-        get "branches"
-        get "tags"
-        get "archive"
-      end
-    end
-
-    resources :deploy_keys
-    resources :protected_branches, :only => [:index, :create, :destroy]
-
-    resources :refs, :only => [], :path => "/" do 
-      collection do 
-        get "switch"
+        get "team"
+        get "wall"
+        get "graph"
+        get "files"
       end
 
-      member do 
-        get "tree", :constraints => { :id => /[a-zA-Z.\/0-9_\-]+/ }
-        get "blob", 
-          :constraints => {
-            :id => /[a-zA-Z.0-9\/_\-]+/,
-            :path => /.*/
-          }
-
-
-        # tree viewer
-        get "tree/:path" => "refs#tree",
-          :as => :tree_file,
-          :constraints => {
-            :id => /[a-zA-Z.0-9\/_\-]+/,
-            :path => /.*/
-          }
-      end
-    end
-
-    resources :merge_requests do 
-      member do 
-        get :diffs
+      resources :wikis, :only => [:show, :edit, :destroy, :create] do
+        member do
+          get "history"        
+        end
       end
 
-      collection do 
-        get :branch_from
-        get :branch_to
+      resource :repository do 
+        member do 
+          get "branches"
+          get "tags"
+          get "archive"
+        end
       end
-    end
+
+      resources :deploy_keys
+      resources :protected_branches, :only => [:index, :create, :destroy]
+
+      resources :refs, :only => [], :path => "/" do 
+        collection do 
+          get "switch"
+        end
+ 
+        member do 
+          get "tree", :constraints => { :id => /[a-zA-Z.\/0-9_\-]+/ }
+          get "blob", 
+            :constraints => {
+              :id => /[a-zA-Z.0-9\/_\-]+/,
+              :path => /.*/
+            }
+
+
+          # tree viewer
+          get "tree/:path" => "refs#tree",
+            :as => :tree_file,
+            :constraints => {
+              :id => /[a-zA-Z.0-9\/_\-]+/,
+              :path => /.*/
+            }
+        end
+      end
+
+      resources :merge_requests do 
+        member do 
+          get :diffs
+        end
+
+        collection do 
+          get :branch_from
+          get :branch_to
+        end
+      end
     
-    resources :snippets
-    resources :hooks, :only => [:index, :new, :create, :destroy, :show] do 
-      member do 
-        get :test
+      resources :snippets
+      resources :hooks, :only => [:index, :new, :create, :destroy, :show] do 
+        member do 
+          get :test
+        end
       end
-    end
-    resources :commits do 
-      collection do 
-        get :compare
+      resources :commits do 
+        collection do 
+          get :compare
+        end
       end
-    end
-    resources :team_members
-    resources :issues do
-      collection do
-        post  :sort
-        get   :search
+      resources :team_members
+      resources :issues do
+        collection do
+          post  :sort
+          get   :search
+        end
       end
+      resources :notes, :only => [:index, :create, :destroy]
     end
-    resources :notes, :only => [:index, :create, :destroy]
+    root :to => "dashboard#index"
   end
-  root :to => "dashboard#index"
 end
