b6124979037b4fb86ea416b08ec79b01c848946e
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2012-04-06 01:28:37 +0300

    Fixed devise email_from, reordered initalizers

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "please-change-me-at-config-initializers-devise@example.com"
+  config.mailer_sender = EMAIL_OPTS["from"]
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
