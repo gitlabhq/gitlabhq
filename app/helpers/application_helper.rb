babb6a1a6b6cc08182d4053cf11266a687d79620
Wouter D'Haeseleer <wouter.dhaeseleer@vasco.com>
2012-05-03 14:19:06 +0200

    Define a default value as suggested by ariejan

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,8 @@
 require 'digest/md5'
 module ApplicationHelper
 
-  def gravatar_icon(user_email, size = 40)
+  def gravatar_icon(user_email = '', size = 40)
     gravatar_host = request.ssl? ? "https://secure.gravatar.com" :  "http://www.gravatar.com"
-    user_email ||= ''
     user_email.strip!
     "#{gravatar_host}/avatar/#{Digest::MD5.hexdigest(user_email.downcase)}?s=#{size}&d=identicon"
   end
