baa54629a9e393e339dc0f5f7c95ffe761206923
randx <dmitriy.zaporozhets@gmail.com>
2012-05-27 13:22:23 +0300

    [Tests] added spec for search page

---
+++ spec/requests/searc_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe "Search" do
+  before do
+    login_as :user
+    @project = Factory :project
+    @project.add_access(@user, :read)
+    visit search_path
+    fill_in "search", :with => @project.name[0..3]
+    click_button "Search"
+  end
+
+  it "should show project in search results" do 
+    page.should have_content @project.name
+  end
+end
+
