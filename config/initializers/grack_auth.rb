8db947bcfe2f3e988c995696f0fd24d9edfdc3b0
Saito <saitowu@gmail.com>
2012-06-29 18:11:37 +0800

    implements protected branches to smart http protocal

--- config/initializers/grack_auth.rb
+++ config/initializers/grack_auth.rb
@@ -18,14 +18,29 @@ module Grack
       elsif @env['REQUEST_METHOD'] == 'POST'
         if @env['REQUEST_URI'].end_with?('git-upload-pack')
           return project.dev_access_for?(user)
-        elsif @env['REQUEST_URI'].end_with?('git-upload-pack')
-          #TODO master branch protection
-          return project.dev_access_for?(user)
+        elsif @env['REQUEST_URI'].end_with?('git-receive-pack')
+          if project.protected_branches.map(&:name).include?(current_ref)
+            project.master_access_for?(user)
+          else
+            project.dev_access_for?(user)
+          end
         else
           false
         end
+      else
+        false
+      end
+    end# valid?
+
+    def current_ref
+      if @env["HTTP_CONTENT_ENCODING"] =~ /gzip/
+        input = Zlib::GzipReader.new(@request.body).string
+      else
+        input = @request.body.string
       end
 
-    end# valid?
+      oldrev, newrev, ref = input.split(' ')
+      /refs\/heads\/([\w-]+)/.match(ref).to_a.last
+    end
   end# Auth
 end# Grack
