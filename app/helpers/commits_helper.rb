a09558921bafcfae91b85af563ee10144c3b7ace
Valeriy Sizov <vsv2711@gmail.com>
2012-07-02 23:08:07 +0300

    InlineDiff: base implementation

--- app/helpers/commits_helper.rb
+++ app/helpers/commits_helper.rb
@@ -35,8 +35,9 @@ module CommitsHelper
     line_old = 1
     line_new = 1
     type = nil
-
-    lines_arr = diff_arr
+    
+    lines_arr = inline_diff diff_arr
+    #lines_arr = diff_arr
     lines_arr.each do |line|
       next if line.match(/^\-\-\- \/dev\/null/)
       next if line.match(/^\+\+\+ \/dev\/null/)
@@ -44,6 +45,9 @@ module CommitsHelper
       next if line.match(/^\+\+\+ b/)
 
       full_line = html_escape(line.gsub(/\n/, ''))
+
+      full_line.gsub!("#!idiff-start!#", "<span class='idiff'>")
+      full_line.gsub!("#!idiff-finish!#", "</span>")
 
       if line.match(/^@@ -/)
         type = "match"
@@ -80,5 +84,52 @@ module CommitsHelper
     else
       nil
     end
+  end
+
+  def inline_diff diff_arr
+    chain_of_first_symbols = ""
+    diff_arr.each_with_index do |line, i|
+      chain_of_first_symbols += line[0]
+    end
+    chain_of_first_symbols.gsub!(/[^\-\+]/, "#")
+    
+    offset = 0
+    indexes = []
+    while index = chain_of_first_symbols.index("#-+#", offset)
+      indexes << index
+      offset = index + 1
+    end
+
+    indexes.each do |index|
+      first_line = diff_arr[index+1]
+      second_line = diff_arr[index+2]
+      max_length = [first_line.size, second_line.size].max
+      
+      first_the_same_symbols = 0
+      (0..max_length + 1).each do |i|
+        first_the_same_symbols = i - 1
+        if first_line[i] != second_line[i] && i > 0
+          break
+        end
+      end
+      first_token = first_line[0..first_the_same_symbols][1..-1]
+      
+      diff_arr[index+1].sub!(first_token, first_token + "#!idiff-start!#")
+      diff_arr[index+2].sub!(first_token, first_token + "#!idiff-start!#")
+      
+      last_the_same_symbols = 0
+      (1..max_length + 1).each do |i|
+        last_the_same_symbols = -i
+        if first_line[-i] != second_line[-i]
+          break
+        end
+      end
+      last_the_same_symbols += 1
+      last_token = first_line[last_the_same_symbols..-1]
+
+      diff_arr[index+1].sub!(/#{last_token}$/, "#!idiff-finish!#" + last_token)
+      diff_arr[index+2].sub!(/#{last_token}$/, "#!idiff-finish!#" + last_token)
+    end
+    diff_arr
   end
 end
