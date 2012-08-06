5443021a0e16887d14189f4203aea7f9282a1853
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2012-08-06 05:08:22 +0200

    Update notifications mails and notify specs for GFM

--- app/mailers/notify.rb
+++ app/mailers/notify.rb
@@ -17,22 +17,23 @@ class Notify < ActionMailer::Base
   def new_issue_email(issue_id)
     @issue = Issue.find(issue_id)
     @project = @issue.project
-    mail(:to => @issue.assignee_email, :subject => "gitlab | New Issue was created")
+    mail(:to => @issue.assignee_email, :subject => "gitlab | new issue ##{@issue.id} | #{@issue.title} | #{@project.name}")
   end
 
   def note_wall_email(recipient_id, note_id)
     recipient = User.find(recipient_id)
     @note = Note.find(note_id)
     @project = @note.project
-    mail(:to => recipient.email, :subject => "gitlab | #{@note.project_name} ")
+    mail(:to => recipient.email, :subject => "gitlab | #{@project.name}")
   end
 
   def note_commit_email(recipient_id, note_id)
     recipient = User.find(recipient_id)
     @note = Note.find(note_id)
     @commit = @note.target
+    @commit = CommitDecorator.decorate(@commit)
     @project = @note.project
-    mail(:to => recipient.email, :subject => "gitlab | note for commit | #{@note.project_name} ")
+    mail(:to => recipient.email, :subject => "gitlab | note for commit #{@commit.short_id} | #{@commit.title} | #{@project.name}")
   end
 
   def note_merge_request_email(recipient_id, note_id)
@@ -40,7 +41,7 @@ class Notify < ActionMailer::Base
     @note = Note.find(note_id)
     @merge_request = @note.noteable
     @project = @note.project
-    mail(:to => recipient.email, :subject => "gitlab | note for merge request | #{@note.project_name} ")
+    mail(:to => recipient.email, :subject => "gitlab | note for merge request !#{@merge_request.id} | #{@project.name}")
   end
 
   def note_issue_email(recipient_id, note_id)
@@ -48,7 +49,7 @@ class Notify < ActionMailer::Base
     @note = Note.find(note_id)
     @issue = @note.noteable
     @project = @note.project
-    mail(:to => recipient.email, :subject => "gitlab | note for issue #{@issue.id} | #{@note.project_name} ")
+    mail(:to => recipient.email, :subject => "gitlab | note for issue ##{@issue.id} | #{@project.name}")
   end
 
   def note_wiki_email(recipient_id, note_id)
@@ -56,13 +57,13 @@ class Notify < ActionMailer::Base
     @note = Note.find(note_id)
     @wiki = @note.noteable
     @project = @note.project
-    mail(:to => recipient.email, :subject => "gitlab | note for wiki | #{@note.project_name}")
+    mail(:to => recipient.email, :subject => "gitlab | note for wiki | #{@project.name}")
   end
 
   def new_merge_request_email(merge_request_id)
     @merge_request = MergeRequest.find(merge_request_id)
     @project = @merge_request.project
-    mail(:to => @merge_request.assignee_email, :subject => "gitlab | new merge request | #{@merge_request.title} ")
+    mail(:to => @merge_request.assignee_email, :subject => "gitlab | new merge request !#{@merge_request.id} | #{@merge_request.title} | #{@project.name}")
   end
 
   def reassigned_merge_request_email(recipient_id, merge_request_id, previous_assignee_id)
@@ -70,7 +71,7 @@ class Notify < ActionMailer::Base
     @merge_request = MergeRequest.find(merge_request_id)
     @previous_assignee ||= User.find(previous_assignee_id)
     @project = @merge_request.project
-    mail(:to => recipient.email, :subject => "gitlab | merge request changed | #{@merge_request.title} ")
+    mail(:to => recipient.email, :subject => "gitlab | changed merge request !#{@merge_request.id} | #{@merge_request.title} | #{@project.name}")
   end
 
   def reassigned_issue_email(recipient_id, issue_id, previous_assignee_id)
@@ -78,6 +79,6 @@ class Notify < ActionMailer::Base
     @issue = Issue.find(issue_id)
     @previous_assignee ||= User.find(previous_assignee_id)
     @project = @issue.project
-    mail(:to => recipient.email, :subject => "gitlab | changed issue | #{@issue.title} ")
+    mail(:to => recipient.email, :subject => "gitlab | changed issue ##{@issue.id} | #{@issue.title} | #{@project.name}")
   end
 end
