b039a169462ba0f04402d4f1af127216b65f119d
Robert Speicher <rspeicher@gmail.com>
2012-08-14 04:32:19 -0400

    GFM refactor: Move the actual parsing to a class under the Gitlab module

--- app/helpers/gitlab_markdown_helper.rb
+++ app/helpers/gitlab_markdown_helper.rb
@@ -1,14 +1,4 @@
 module GitlabMarkdownHelper
-  REFERENCE_PATTERN = %r{
-    ([^\w&;])?         # Prefix (1)
-    (                  # Reference (2)
-      @([\w\._]+)    | # User name (3)
-      [#!$](\d+)     | # Issue/MR/Snippet ID (4)
-      [\h]{6,40}       # Commit ID (2)
-    )
-    ([^\w&;])?         # Suffix (5)
-  }x.freeze
-
   def gfm(text, html_options = {})
     return text if text.nil?
     return text if @project.nil?
@@ -22,21 +12,10 @@ module GitlabMarkdownHelper
       "{gfm-extraction-#{md5}}"
     end
 
-    text.gsub!(REFERENCE_PATTERN) do |match|
-      vals = {
-        prefix:       $1,
-        reference:    $2,
-        user_name:    $3,
-        reference_id: $4,
-        suffix:       $5
-      }
+    # TODO: add popups with additional information
 
-      if ref_link = reference_link(vals, html_options)
-        sprintf('%s%s%s', vals[:prefix], ref_link, vals[:suffix])
-      else
-        match
-      end
-    end
+    parser = Gitlab::Markdown.new(@project, html_options)
+    text = parser.parse(text)
 
     # Insert pre block extractions
     text.gsub!(/\{gfm-extraction-(\h{32})\}/) do
@@ -70,40 +49,5 @@ module GitlabMarkdownHelper
     })
 
     @__renderer.render(text).html_safe
-  end
-
-  private
-
-  def reference_link(vals, html_options)
-    # TODO: add popups with additional information
-    case vals[:reference]
-
-      # team member: @foo
-    when /^@/
-      user = @project.users.where(name: vals[:user_name]).first
-      member = @project.users_projects.where(user_id: user).first if user
-      link_to("@#{user.name}", project_team_member_path(@project, member), html_options.merge(class: "gfm gfm-team_member #{html_options[:class]}")) if member
-
-    # issue: #123
-    when /^#/
-      issue = @project.issues.where(id: vals[:reference_id]).first
-      link_to("##{issue.id}", project_issue_path(@project, issue), html_options.merge(title: "Issue: #{issue.title}", class: "gfm gfm-issue #{html_options[:class]}")) if issue
-
-    # merge request: !123
-    when /^!/
-      merge_request = @project.merge_requests.where(id: vals[:reference_id]).first
-      link_to("!#{merge_request.id}", project_merge_request_path(@project, merge_request), html_options.merge(title: "Merge Request: #{merge_request.title}", class: "gfm gfm-merge_request #{html_options[:class]}")) if merge_request
-
-    # snippet: $123
-    when /^\$/
-      snippet = @project.snippets.where(id: vals[:reference_id]).first
-      link_to("$#{snippet.id}", project_snippet_path(@project, snippet), html_options.merge(title: "Snippet: #{snippet.title}", class: "gfm gfm-snippet #{html_options[:class]}")) if snippet
-
-    # commit: 123456...
-    when /^\h/
-      commit = @project.commit(vals[:reference])
-      link_to(vals[:reference], project_commit_path(@project, id: commit.id), html_options.merge(title: "Commit: #{commit.author_name} - #{CommitDecorator.new(commit).title}", class: "gfm gfm-commit #{html_options[:class]}")) if commit
-
-    end
   end
 end
