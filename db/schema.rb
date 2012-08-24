6f62aeaa5e1669d7f928fb34f609aa4d6a96dfc2
shane tully <esc201@gmail.com>
2012-08-24 15:13:46 -0400

    projects internally public by default

--- db/schema.rb
+++ db/schema.rb
@@ -107,16 +107,16 @@ ActiveRecord::Schema.define(:version => 20121120113838) do
     t.string   "name"
     t.string   "path"
     t.text     "description"
-    t.datetime "created_at",                               :null => false
-    t.datetime "updated_at",                               :null => false
-    t.boolean  "private_flag",           :default => true, :null => false
+    t.datetime "created_at",                                   :null => false
+    t.datetime "updated_at",                                   :null => false
+    t.boolean  "private_flag",           :default => false,    :null => false
     t.string   "code"
     t.integer  "owner_id"
     t.string   "default_branch"
-    t.boolean  "issues_enabled",         :default => true, :null => false
-    t.boolean  "wall_enabled",           :default => true, :null => false
-    t.boolean  "merge_requests_enabled", :default => true, :null => false
-    t.boolean  "wiki_enabled",           :default => true, :null => false
+    t.boolean  "issues_enabled",         :default => true,     :null => false
+    t.boolean  "wall_enabled",           :default => true,     :null => false
+    t.boolean  "merge_requests_enabled", :default => true,     :null => false
+    t.boolean  "wiki_enabled",           :default => true,     :null => false
     t.integer  "group_id"
   end
 
