e738361314fb192f74f04d2f3e3a12bcb28190f7
Malte <malte.blaettermann@googlemail.com>
2012-09-02 12:04:15 +0200

    Added basic RestClient call to app/models/web_hook.rb

--- app/models/web_hook.rb
+++ app/models/web_hook.rb
@@ -13,6 +13,7 @@ class WebHook < ActiveRecord::Base
   def execute(data)
     parsed_url = URI.parse(url)
     if parsed_url.userinfo.blank?
+      RestClient.post(url, { "payload" => data.to_json }, :content_type => :json)
       WebHook.post(url, body: data.to_json, headers: { "Content-Type" => "application/json" })
     else
       post_url = url.gsub(parsed_url.userinfo+"@", "")
