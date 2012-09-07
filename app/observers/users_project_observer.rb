85db51f660c4d861c9adf97ed114d4bd4d14ff7d
Alex Denisov <1101.debian@gmail.com>
2012-09-07 10:23:45 +0300

    don't send notification when UsersProject destroyed

--- app/observers/users_project_observer.rb
+++ app/observers/users_project_observer.rb
@@ -1,5 +1,6 @@
 class UsersProjectObserver < ActiveRecord::Observer
   def after_commit(users_project)
+    return if users_project.destroyed?
     Notify.project_access_granted_email(users_project.id).deliver
   end
 end
