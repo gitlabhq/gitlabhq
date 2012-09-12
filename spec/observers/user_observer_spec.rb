d8f6d38d39868426902ffbad9c232b7748a4288f
randx <dmitriy.zaporozhets@gmail.com>
2012-09-12 09:05:12 +0300

    Fix observer test

--- spec/observers/user_observer_spec.rb
+++ spec/observers/user_observer_spec.rb
@@ -13,13 +13,18 @@ describe UserObserver do
   end
 
   context 'when a new user is created' do
-    let(:user) { double(:user, id: 42, password: 'P@ssword!') }
+    let(:user) { double(:user, id: 42, password: 'P@ssword!', name: 'John', email: 'u@mail.local') }
     let(:notification) { double :notification }
 
     it 'sends an email' do
       notification.should_receive(:deliver)
       Notify.should_receive(:new_user_email).with(user.id, user.password).and_return(notification)
 
+      subject.after_create(user)
+    end
+
+    it 'trigger logger' do
+      Gitlab::AppLogger.should_receive(:info)
       subject.after_create(user)
     end
   end
