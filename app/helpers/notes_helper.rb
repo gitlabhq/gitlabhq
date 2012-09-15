b8113334a86639b87d5f89cc78e2279a8ae4e38a
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2012-09-15 11:54:46 +0200

    Highlight voting notes for issues and merge requests

--- app/helpers/notes_helper.rb
+++ app/helpers/notes_helper.rb
@@ -6,4 +6,12 @@ module NotesHelper
   def loading_new_notes?
     params[:loading_new].present?
   end
+
+  def note_vote_class(note)
+    if note.upvote?
+      "vote upvote"
+    elsif note.downvote?
+      "vote downvote"
+    end
+  end
 end
