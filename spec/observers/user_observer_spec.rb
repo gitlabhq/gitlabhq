cb2be3ce0ab27ac9aa25f0c3eb59047ad5a0153a
Gabor Liptak <gliptak@gmail.com>
2012-09-19 13:36:00 -0500

    Don't email omniauth created users

--- spec/observers/user_observer_spec.rb
+++ spec/observers/user_observer_spec.rb
@@ -13,17 +13,25 @@ describe UserObserver do
   end
 
   context 'when a new user is created' do
-    let(:user) { double(:user, id: 42, password: 'P@ssword!', name: 'John', email: 'u@mail.local') }
     let(:notification) { double :notification }
 
-    it 'sends an email' do
+    it 'sends an email unless external' do
+      user = double(:user, id: 42, password: 'P@ssword!', name: 'John', email: 'u@mail.local', extern_uid?: false)
       notification.should_receive(:deliver)
       Notify.should_receive(:new_user_email).with(user.id, user.password).and_return(notification)
 
       subject.after_create(user)
     end
 
+    it 'no email for external' do
+      user = double(:user, id: 42, password: 'P@ssword!', name: 'John', email: 'u@mail.local', extern_uid?: true)
+      Notify.should_not_receive(:new_user_email)
+
+      subject.after_create(user)
+    end
+
     it 'trigger logger' do
+      user = double(:user, id: 42, password: 'P@ssword!', name: 'John', email: 'u@mail.local', extern_uid?: false)
       Gitlab::AppLogger.should_receive(:info)
       subject.after_create(user)
     end
