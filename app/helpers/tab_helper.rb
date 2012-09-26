f064c84019f7414cb9cfa9e49fb735dba7f495df
Robert Speicher <rspeicher@gmail.com>
2012-09-26 16:13:23 -0400

    Add nav_link helper to TabHelper

--- app/helpers/tab_helper.rb
+++ app/helpers/tab_helper.rb
@@ -1,4 +1,72 @@
 module TabHelper
+  # Navigation link helper
+  #
+  # Returns an `li` element with an 'active' class if the supplied
+  # controller(s) and/or action(s) currently active. The contents of the
+  # element is the value passed to the block.
+  #
+  # options - The options hash used to determine if the element is "active" (default: {})
+  #           :controller   - One or more controller names to check (optional).
+  #           :action       - One or more action names to check (optional).
+  #           :path         - A shorthand path, such as 'dashboard#index', to check (optional).
+  #           :html_options - Extra options to be passed to the list element (optional).
+  # block   - An optional block that will become the contents of the returned
+  #           `li` element.
+  #
+  # When both :controller and :action are specified, BOTH must match in order
+  # to be marked as active. When only one is given, either can match.
+  #
+  # Examples
+  #
+  #   # Assuming we're on TreeController#show
+  #
+  #   # Controller matches, but action doesn't
+  #   nav_link(controller: [:tree, :refs], action: :edit) { "Hello" }
+  #   # => '<li>Hello</li>'
+  #
+  #   # Controller matches
+  #   nav_link(controller: [:tree, :refs]) { "Hello" }
+  #   # => '<li class="active">Hello</li>'
+  #
+  #   # Shorthand path
+  #   nav_link(path: 'tree#show') { "Hello" }
+  #   # => '<li class="active">Hello</li>'
+  #
+  #   # Supplying custom options for the list element
+  #   nav_link(controller: :tree, html_options: {class: 'home'}) { "Hello" }
+  #   # => '<li class="home active">Hello</li>'
+  #
+  # Returns a list item element String
+  def nav_link(options = {}, &block)
+    if path = options.delete(:path)
+      c, a, _ = path.split('#')
+    else
+      c = options.delete(:controller)
+      a = options.delete(:action)
+    end
+
+    if c && a
+      # When given both options, make sure BOTH are active
+      klass = current_controller?(*c) && current_action?(*a) ? 'active' : ''
+    else
+      # Otherwise check EITHER option
+      klass = current_controller?(*c) || current_action?(*a) ? 'active' : ''
+    end
+
+    # Add our custom class into the html_options, which may or may not exist
+    # and which may or may not already have a :class key
+    o = options.delete(:html_options) || {}
+    o[:class] ||= ''
+    o[:class] += ' ' + klass
+    o[:class].strip!
+
+    if block_given?
+      content_tag(:li, capture(&block), o)
+    else
+      content_tag(:li, nil, o)
+    end
+  end
+
   def tab_class(tab_key)
     active = case tab_key
 
