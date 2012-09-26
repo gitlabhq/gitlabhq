adcc6a0b0e08158353627a8a900971aca07429bd
Robert Speicher <rspeicher@gmail.com>
2012-09-25 21:18:39 -0400

    Move tab_class helper to TabHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -99,45 +99,6 @@ module ApplicationHelper
       event.project.merge_requests_enabled
   end
 
-  def tab_class(tab_key)
-    active = case tab_key
-
-             # Project Area
-             when :wall; wall_tab?
-             when :wiki; controller.controller_name == "wikis"
-             when :issues; issues_tab?
-             when :network; current_page?(controller: "projects", action: "graph", id: @project)
-             when :merge_requests; controller.controller_name == "merge_requests"
-
-             # Dashboard Area
-             when :help; controller.controller_name == "help"
-             when :search; current_page?(search_path)
-             when :dash_issues; current_page?(dashboard_issues_path)
-             when :dash_mr; current_page?(dashboard_merge_requests_path)
-             when :root; current_page?(dashboard_path) || current_page?(root_path)
-
-             # Profile Area
-             when :profile;  current_page?(controller: "profile", action: :show)
-             when :history;  current_page?(controller: "profile", action: :history)
-             when :account;  current_page?(controller: "profile", action: :account)
-             when :token;    current_page?(controller: "profile", action: :token)
-             when :design;   current_page?(controller: "profile", action: :design)
-             when :ssh_keys; controller.controller_name == "keys"
-
-             # Admin Area
-             when :admin_root;     controller.controller_name == "dashboard"
-             when :admin_users;    controller.controller_name == 'users'
-             when :admin_projects; controller.controller_name == "projects"
-             when :admin_hooks;    controller.controller_name == 'hooks'
-             when :admin_resque;   controller.controller_name == 'resque'
-             when :admin_logs;   controller.controller_name == 'logs'
-
-             else
-               false
-             end
-    active ? "current" : nil
-  end
-
   def hexdigest(string)
     Digest::SHA1.hexdigest string
   end
