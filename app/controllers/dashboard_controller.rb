f9eda9b33a72cae91e51c5733fdf2c0d7b37d149
Dmitriy Zaporozhets <dzaporozhets@sphereconsultinginc.com>
2012-10-02 19:37:53 +0300

    Group filtering on dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,12 +2,22 @@ class DashboardController < ApplicationController
   respond_to :html
 
   def index
-    @projects = current_user.projects_with_events.page(params[:page]).per(40)
+    @groups = Group.where(id: current_user.projects.pluck(:group_id))
+
+    @projects = current_user.projects_with_events
+
+    if params[:group].present?
+      @group = Group.find_by_code(params[:group])
+      @projects = @projects.where(group_id: @group.id)
+    end
+
+    @projects = @projects.page(params[:page]).per(40)
+
     @events = Event.recent_for_user(current_user).limit(20).offset(params[:offset] || 0)
     @last_push = current_user.recent_push
 
     respond_to do |format|
-      format.html
+      format.html { render 'index', layout: determine_layout }
       format.js
       format.atom { render layout: false }
     end
@@ -30,5 +40,11 @@ class DashboardController < ApplicationController
       format.html
       format.atom { render layout: false }
     end
+  end
+
+  protected
+
+  def determine_layout
+    @group ? 'group' : 'application'
   end
 end
