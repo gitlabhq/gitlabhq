054c15e7b5f9967417a0ea03aad0c4d77afd3bca
Godric <godric@0x3f.fr>
2012-10-20 00:38:07 +0300

    enable Oauth login for existing regular users
    fix: https://github.com/gitlabhq/gitlabhq/issues/1620

--- lib/gitlab/auth.rb
+++ lib/gitlab/auth.rb
@@ -48,8 +48,12 @@ module Gitlab
 
     def find_or_new_for_omniauth(auth)
       provider, uid = auth.provider, auth.uid
+      email = auth.info.email.downcase unless auth.info.email.nil?
 
       if @user = User.find_by_provider_and_extern_uid(provider, uid)
+        @user
+      elsif @user = User.find_by_email(email)
+        @user.update_attributes(:extern_uid => uid, :provider => provider)
         @user
       else
         if Gitlab.config.omniauth['allow_single_sign_on']
