524b907042c36d832b82d980c9fee85e6d92b6da
Valeriy Sizov <vsv2711@gmail.com>
2012-10-21 15:33:02 +0300

    API: MergeRequest refactoring

--- lib/api/merge_requests.rb
+++ lib/api/merge_requests.rb
@@ -6,21 +6,18 @@ module Gitlab
     resource :projects do
       #list
       get ":id/merge_requests" do
-        project = current_user.projects.find(params[:id])
-        present project.merge_requests, with: Entities::MergeRequest
+        present user_project.merge_requests, with: Entities::MergeRequest
       end
       
       #show
       get ":id/merge_request/:merge_request_id" do
-        project = current_user.projects.find(params[:id])
-        present project.merge_requests.find(params[:merge_request_id]), with: Entities::MergeRequest
+        present user_project.merge_requests.find(params[:merge_request_id]), with: Entities::MergeRequest
       end
 
       #create merge_request
       post ":id/merge_requests" do
         attrs = attributes_for_keys [:source_branch, :target_branch, :assignee_id, :title]
-        project = current_user.projects.find(params[:id])
-        merge_request = project.merge_requests.new(attrs)
+        merge_request = user_project.merge_requests.new(attrs)
         merge_request.author = current_user
         
         if merge_request.save
@@ -34,8 +31,7 @@ module Gitlab
       #update merge_request
       put ":id/merge_request/:merge_request_id" do
         attrs = attributes_for_keys [:source_branch, :target_branch, :assignee_id, :title, :closed]
-        project = current_user.projects.find(params[:id])
-        merge_request = project.merge_requests.find(params[:merge_request_id])
+        merge_request = user_project.merge_requests.find(params[:merge_request_id])
         
         if merge_request.update_attributes attrs
           merge_request.reload_code
