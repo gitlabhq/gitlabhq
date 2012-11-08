f1ac2a616bedc2941bc7f67e2ac25e915da2ddb2
Saito <saitowu@gmail.com>
2012-11-09 01:41:07 +0800

    remove encode lib, clean all encoded area.

--- config/initializers/3_grit_ext.rb
+++ config/initializers/3_grit_ext.rb
@@ -6,23 +6,4 @@ Grit::Git.git_max_size = Gitlab.config.git_max_size
 
 Grit::Blob.class_eval do
   include Linguist::BlobHelper
-
-  def data
-    @data ||= @repo.git.cat_file({:p => true}, id)
-    Gitlab::Encode.utf8 @data
-  end
-end
-
-Grit::Diff.class_eval do
-  def old_path
-    Gitlab::Encode.utf8 @a_path
-  end
-
-  def new_path
-    Gitlab::Encode.utf8 @b_path
-  end
-
-  def diff
-    Gitlab::Encode.utf8 @diff
-  end
 end
