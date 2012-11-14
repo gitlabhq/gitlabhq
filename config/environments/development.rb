527d223680864a40887674768cf83a4a8626ead9
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2012-11-14 14:45:15 +0100

    Update Rails config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@ Gitlab::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
-  # every request.  This slows down response time but is perfect for development
+  # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
@@ -22,19 +22,21 @@ Gitlab::Application.configure do
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
 
-  # Do not compress assets
-  config.assets.compress = false
-
-  # Expands the lines which load the assets
-  config.assets.debug = true
-
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.action_mailer.delivery_method = :letter_opener
-
   # Raise exception on mass assignment protection for Active Record models
   config.active_record.mass_assignment_sanitizer = :strict
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # Do not compress assets
+  config.assets.compress = false
+
+  # Expands the lines which load the assets
+  config.assets.debug = true
+
+  # For having correct urls in mails
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # Open sent mails in browser
+  config.action_mailer.delivery_method = :letter_opener
 end
