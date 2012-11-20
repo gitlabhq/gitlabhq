93fdc4ca9d073ad73beacfd673ddc4e5652111ab
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2012-11-20 14:19:55 +0200

    Reannotated

--- app/models/web_hook.rb
+++ app/models/web_hook.rb
@@ -8,6 +8,7 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  type       :string(255)      default("ProjectHook")
+#  service_id :integer
 #
 
 class WebHook < ActiveRecord::Base
--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -2,13 +2,15 @@
 #
 # Table name: services
 #
-#  id         :integer          not null, primary key
-#  type       :string(255)
-#  title      :string(255)
-#  token      :string(255)
-#  project_id :integer          not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id          :integer          not null, primary key
+#  type        :string(255)
+#  title       :string(255)
+#  token       :string(255)
+#  project_id  :integer          not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  active      :boolean          default(FALSE), not null
+#  project_url :string(255)
 #
 
 require 'spec_helper'
