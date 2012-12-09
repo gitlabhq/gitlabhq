c8707a8774f19c90d25cf9dfeb4e8ed21af946e9
Cyril <jv.cyril@gmail.com>
2012-12-09 01:33:01 +0100

    fix post_receive_spec
    First parameter of PostReceive.perform should be Project#to_param instead of
    Project#path because path omits the namespace.
    
    Also fixed some spec that could never fail.

--- spec/workers/post_receive_spec.rb
+++ spec/workers/post_receive_spec.rb
@@ -14,27 +14,32 @@ describe PostReceive do
     let(:key_id) { key.identifier }
 
     it "fetches the correct project" do
-      Project.should_receive(:find_by_path).with(project.path).and_return(project)
-      PostReceive.perform(project.path, 'sha-old', 'sha-new', 'refs/heads/master', key_id)
+      Project.should_receive(:find_with_namespace).with(project.to_param).and_return(project)
+
+      PostReceive.perform(project.to_param, 'sha-old', 'sha-new', 'refs/heads/master', key_id)
+    end
+
+    it "does trigger hooks" do
+      Project.any_instance.should_receive(:trigger_post_receive)
+
+      PostReceive.perform(project.to_param, 'sha-old', 'sha-new', 'refs/heads/master', key_id)
     end
 
     it "does not run if the author is not in the project" do
       Key.stub(find_by_identifier: nil)
 
-      project.should_not_receive(:observe_push)
-      project.should_not_receive(:execute_hooks)
+      Project.any_instance.should_not_receive(:trigger_post_receive)
 
-      PostReceive.perform(project.path, 'sha-old', 'sha-new', 'refs/heads/master', key_id).should be_false
+      PostReceive.perform(project.to_param, 'sha-old', 'sha-new', 'refs/heads/master', key_id).should be_false
     end
 
     it "asks the project to trigger all hooks" do
-      Project.stub(find_by_path: project)
-      project.should_receive(:execute_hooks)
-      project.should_receive(:execute_services)
-      project.should_receive(:update_merge_requests)
-      project.should_receive(:observe_push)
+      Project.any_instance.should_receive(:observe_push)
+      Project.any_instance.should_receive(:update_merge_requests)
+      Project.any_instance.should_receive(:execute_hooks)
+      Project.any_instance.should_receive(:execute_services)
 
-      PostReceive.perform(project.path, 'sha-old', 'sha-new', 'refs/heads/master', key_id)
+      PostReceive.perform(project.to_param, 'sha-old', 'sha-new', 'refs/heads/master', key_id)
     end
   end
 end
