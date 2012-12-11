95af2baa3775144d9541ea3b6f2e4c87b65b309e
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2012-12-11 18:14:18 +0200

    Fix commit links in network graph

--- vendor/assets/javascripts/branch-graph.js
+++ vendor/assets/javascripts/branch-graph.js
@@ -90,7 +90,7 @@ function branchGraph(holder) {
         (function (c, x, y) {
             top.push(r.circle(x, y, 10).attr({fill: "#000", opacity: 0, cursor: "pointer"})
             .click(function(){
-              location.href = location.href.replace("graph", "commits/" + c.id);
+              location.href = location.href.replace("graph", "commit/" + c.id);
             })
             .hover(function () {
                 var s = r.text(100, 100,c.author + "\n \n" +c.id + "\n \n" + c.message).attr({fill: "#fff"});
