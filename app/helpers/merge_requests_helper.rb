03369a9165e2f333a3feb501004acf369e339d67
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2012-12-13 06:14:05 +0300

    Use one helper for linking to team member. More clear title for Issue and MR

--- app/helpers/merge_requests_helper.rb
+++ app/helpers/merge_requests_helper.rb
@@ -1,26 +1,4 @@
 module MergeRequestsHelper
-  def link_to_merge_request_assignee(merge_request)
-    project = merge_request.project
-
-    tm = project.team_member_by_id(merge_request.assignee_id)
-    if tm
-      link_to merge_request.assignee_name, project_team_member_path(project, tm), class: "author_link"
-    else
-      merge_request.assignee_name
-    end
-  end
-
-  def link_to_merge_request_author(merge_request)
-    project = merge_request.project
-
-    tm = project.team_member_by_id(merge_request.author_id)
-    if tm
-      link_to merge_request.author_name, project_team_member_path(project, tm), class: "author_link"
-    else
-      merge_request.author_name
-    end
-  end
-
   def new_mr_path_from_push_event(event)
     new_project_merge_request_path(
       event.project,
