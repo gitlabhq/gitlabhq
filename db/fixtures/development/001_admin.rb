e56a47ab19a6d161732a75e19f385b1df4e4821f
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2012-12-27 12:13:18 +0200

    Dev fixtures: Add sample ssh key to admin

--- db/fixtures/development/001_admin.rb
+++ db/fixtures/development/001_admin.rb
@@ -9,3 +9,13 @@ User.seed(:id, [
     admin: true,
   }
 ])
+
+
+Key.seed(:id, [
+  {
+    id: 1,
+    title: "Sample key",
+    key: "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAIEAiPWx6WM4lhHNedGfBpPJNPpZ7yKu+dnn1SJejgt4596k6YjzGGphH2TUxwKzxcKDKKezwkpfnxPkSMkuEspGRt/aZZ9wa++Oi7Qkr8prgHc4soW6NUlfDzpvZK2H5E7eQaSeP3SAwGmQKUFHCddNaP0L+hM7zhFNzjFvpaMgJw0=",
+    user_id: 1,
+  }
+])
