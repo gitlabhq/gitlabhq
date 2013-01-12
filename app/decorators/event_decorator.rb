576f4f167b474e7efd8b5fbe3f7d9f1dac017de1
Spike^ekipS <spikeekips@gmail.com>
2013-01-12 16:34:39 +0900

    fixed the atom bugs, it could not produce the note, including comments in the feed

--- app/decorators/event_decorator.rb
+++ app/decorators/event_decorator.rb
@@ -10,6 +10,8 @@ class EventDecorator < ApplicationDecorator
       "#{self.author_name} #{self.push_action_name} #{self.ref_type} #{self.ref_name} at #{self.project.name}"
     elsif self.membership_changed?
       "#{self.author_name} #{self.action_name} #{self.project.name}"
+    elsif self.note?
+      "#{self.author_name} #{self.action_name} '#{self.note_note}' on #{self.project.name}"
     else
       ""
     end
@@ -18,6 +20,14 @@ class EventDecorator < ApplicationDecorator
   def feed_url
     if self.issue?
       h.project_issue_url(self.project, self.issue)
+    elsif self.note?
+	if self.note_target_type == "issue"
+	    h.project_issue_url(self.project, self.note_target_id, anchor: "note_#{self.note_id}")
+	elsif self.note_target_type == "maprequest"
+	    h.project_merge_request_url(self.project, self.note_target_id, anchor: "note_#{self.note_id}")
+	else
+		h.wall_project_url(self.project, anchor: "note_#{self.note_id}")
+	end
     elsif self.merge_request?
       h.project_merge_request_url(self.project, self.merge_request)
 
