f5e73069b1f495ebdfa9a356a4b2edae31e0cd93
Oliver <oliver@zerolag.com>
2013-01-13 03:52:34 +0100

    Projects/Gitlab-shell: Make the default branch the real default.
    Gitlab will now tell gitlab-shell to update the HEAD pointer using the
    update-head command - this ensures that when performing a git clone, the
    branch you receive if none is specified will actually be the one you
    configured as default.
    
    This also resolves breakage when your repo lacks any branch named
    "master.
    
    Depends on commit 0cb0f8c in gitlab-shell

--- app/contexts/projects/update_context.rb
+++ app/contexts/projects/update_context.rb
@@ -3,6 +3,10 @@ module Projects
     def execute(role = :default)
       params[:project].delete(:namespace_id)
       params[:project].delete(:public) unless can?(current_user, :change_public_mode, project)
+
+      new_default = params[:project]['default_branch']
+      project.update_head(new_default) unless new_default == project.default_branch
+
       project.update_attributes(params[:project], as: role)
     end
   end
