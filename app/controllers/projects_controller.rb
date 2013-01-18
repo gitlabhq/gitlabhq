af7dcda1cb33fc8c828d41f4c2ac89f7d2ce8315
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2013-01-18 22:23:11 +0200

    Fix project creation

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -19,7 +19,7 @@ class ProjectsController < ProjectResourceController
   end
 
   def create
-    @project = Projects::CreateContext.new(current_user, params).execute
+    @project = Projects::CreateContext.new(current_user, params[:project]).execute
 
     respond_to do |format|
       flash[:notice] = 'Project was successfully created.' if @project.saved?
