a7667ffc14017732228058e34e1f724be08bf9a0
Andrey Kumanyaev <me@zzet.org>
2013-01-21 01:04:53 +0400

    Repair members management of teams

--- app/controllers/admin/teams/members_controller.rb
+++ app/controllers/admin/teams/members_controller.rb
@@ -1,4 +1,6 @@
 class Admin::Teams::MembersController < Admin::Teams::ApplicationController
+  before_filter :team_member, only: [:edit, :destroy, :update]
+
   def new
     @users = User.active
     @users = @users.not_in_team(@team) if @team.members.any?
@@ -17,11 +19,9 @@ class Admin::Teams::MembersController < Admin::Teams::ApplicationController
   end
 
   def edit
-    @member = @team.members.find(params[:id])
   end
 
   def update
-    @member = @team.members.find(params[:id])
     options = {default_projects_access: params[:default_project_access], group_admin: params[:group_admin]}
     if @team.update_membership(@member, options)
       redirect_to admin_team_path(@team), notice: 'Membership was successfully updated.'
@@ -31,5 +31,16 @@ class Admin::Teams::MembersController < Admin::Teams::ApplicationController
   end
 
   def destroy
+    if @team.remove_member(@member)
+      redirect_to admin_team_path(@team), notice: "Member was successfully removed from team."
+    else
+      redirect_to admin_team_members(@team), notice: "Something wrong."
+    end
+  end
+
+  private
+
+  def team_member
+    @member = @team.members.find(params[:id])
   end
 end
