cdaca0a17207831321c853159281d14f384f86c7
Andrey Kumanyaev <me@zzet.org>
2013-01-21 01:07:28 +0400

    save autogenerated files

---
+++ spec/controllers/admin/teams/projects_controller_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe Admin::Teams::ProjectsController do
+
+  describe "GET 'new'" do
+    it "returns http success" do
+      get 'new'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'create'" do
+    it "returns http success" do
+      get 'create'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'edit'" do
+    it "returns http success" do
+      get 'edit'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'update'" do
+    it "returns http success" do
+      get 'update'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'destroy'" do
+    it "returns http success" do
+      get 'destroy'
+      response.should be_success
+    end
+  end
+
+end
