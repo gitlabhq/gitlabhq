1e20459bcc248b52591ca4a7703c1f07b1ed9bfd
Andrey Kumanyaev <me@zzet.org>
2013-01-22 21:14:00 +0400

    Fix using context of Projects::UpdateContext (in admin section error, in public section - to next step if moving controlers)

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -29,7 +29,7 @@ class Admin::ProjectsController < Admin::ApplicationController
   end
 
   def update
-    status = Projects::UpdateContext.new(project, current_user, params).execute(:admin)
+    status = ::Projects::UpdateContext.new(project, current_user, params).execute(:admin)
 
     if status
       redirect_to [:admin, @project], notice: 'Project was successfully updated.'
