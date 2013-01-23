b695db4af4c9fe0dc27f78e549a6a785bf936731
Andrey Kumanyaev <me@zzet.org>
2013-01-24 01:40:45 +0400

    Remove save files with failed test %)

--- features/support/env.rb
+++ features/support/env.rb
@@ -21,7 +21,6 @@ Dir["#{Rails.root}/features/steps/shared/*.rb"].each {|file| require file}
 include GitoliteStub
 
 WebMock.allow_net_connect!
-Spinach.config.save_and_open_page_on_failure = true
 #
 # JS driver
 #
