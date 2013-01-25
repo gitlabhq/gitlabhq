3ce79e06580c0efb0abeba0263bdc3ea10670e2d
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2013-01-25 10:10:12 +0200

    fix spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -189,7 +189,7 @@ describe User do
 
     it { user.is_admin?.should be_false }
     it { user.require_ssh_key?.should be_true }
-    it { user.can_create_group?.should be_false }
+    it { user.can_create_group?.should be_true }
     it { user.can_create_project?.should be_true }
     it { user.first_name.should == 'John' }
   end
