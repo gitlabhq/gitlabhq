ac8247b46df8eb8c475436fc276a3648379ae7a0
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2013-01-25 10:07:21 +0200

    Improved search. added filters

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,18 @@
 class SearchController < ApplicationController
   def show
-    result = SearchContext.new(current_user.authorized_projects.map(&:id), params).execute
+    project_id = params[:project_id]
+    group_id = params[:group_id]
+
+    project_ids = current_user.authorized_projects.map(&:id)
+
+    if group_id.present?
+      group_project_ids = Group.find(group_id).projects.map(&:id)
+      project_ids.select! { |id| group_project_ids.include?(id)}
+    elsif project_id.present?
+      project_ids.select! { |id| id == project_id.to_i}
+    end
+
+    result = SearchContext.new(project_ids, params).execute
 
     @projects       = result[:projects]
     @merge_requests = result[:merge_requests]
