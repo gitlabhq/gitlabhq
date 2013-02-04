6b9a60904451fb192e445774768ccb53f7f8d2f8
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2013-02-04 14:28:10 +0200

    preparing for gitlab-shell

--- lib/gitlab/backend/gitolite.rb
+++ lib/gitlab/backend/gitolite.rb
@@ -1,5 +1,3 @@
-require_relative 'gitolite_config'
-
 module Gitlab
   class Gitolite
     class AccessDenied < StandardError; end
@@ -8,52 +6,25 @@ module Gitlab
       Gitlab::GitoliteConfig.new
     end
 
-    # Update gitolite config with new key
+    # Add new key to gitlab-shell
     #
     # Ex.
-    #   set_key("m_gitlab_com_12343", "sha-rsa ...", [2, 3, 6])
+    #   add_key("randx", "sha-rsa ...")
     #
-    def set_key(key_id, key_content, project_ids)
-      projects = Project.where(id: project_ids)
-
-      config.apply do |config|
-        config.write_key(key_id, key_content)
-        config.update_projects(projects)
-      end
+    def add_key(username, key_content)
+      # TODO: implement
     end
 
-    # Remove ssh key from gitolite config
+    # Remove ssh key from gitlab shell
     #
     # Ex.
-    #   remove_key("m_gitlab_com_12343", [2, 3, 6])
+    #   remove_key("sha-rsa")
     #
-    def remove_key(key_id, project_ids)
-      projects = Project.where(id: project_ids)
-
-      config.apply do |config|
-        config.rm_key(key_id)
-        config.update_projects(projects)
-      end
+    def remove_key(key_content)
+      # TODO: implement
     end
 
-    # Update project config in gitolite by project id
-    #
-    # Ex.
-    #   update_repository(23)
-    #
-    def update_repository(project_id)
-      project = Project.find(project_id)
-      config.update_project!(project)
-    end
-
-    def move_repository(old_repo, project)
-      config.apply do |config|
-        config.clean_repo(old_repo)
-        config.update_project(project)
-      end
-    end
-
-    # Remove repository from gitolite
+    # Remove repository from file system
     #
     # name - project path with namespace
     #
@@ -61,20 +32,18 @@ module Gitlab
     #   remove_repository("gitlab/gitlab-ci")
     #
     def remove_repository(name)
-      config.destroy_project!(name)
+      # TODO: implement
     end
 
-    # Update projects configs in gitolite by project ids
+    # Init new repository
+    #
+    # name - project path with namespace
     #
     # Ex.
-    #   update_repositories([1, 4, 6])
+    #   add_repository("gitlab/gitlab-ci")
     #
-    def update_repositories(project_ids)
-      projects = Project.where(id: project_ids)
-
-      config.apply do |config|
-        config.update_projects(projects)
-      end
+    def add_repository(name)
+      # TODO: implement
     end
 
     def url_to_repo path
@@ -84,7 +53,5 @@ module Gitlab
     def enable_automerge
       config.admin_all_repo!
     end
-
-    alias_method :create_repository, :update_repository
   end
 end
