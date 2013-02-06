1b4ba3eb99e30bded9bffea20c457af5e08a58f2
Marin Jankovski <maxlazio@gmail.com>
2013-02-06 12:44:09 +0100

    Add user delete option.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,17 @@
 class RegistrationsController < Devise::RegistrationsController
   before_filter :signup_enabled?
 
+  def destroy
+    if current_user.owned_projects.count > 0
+      redirect_to account_profile_path, alert: "Remove projects and groups before removing account." and return
+    end
+    current_user.destroy
+
+    respond_to do |format|
+      format.html { redirect_to new_user_session_path, notice: "Account successfully removed." }
+    end
+  end
+
   private
 
   def signup_enabled?
