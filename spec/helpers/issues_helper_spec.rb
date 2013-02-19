8caccae45457940b54174a3516a1a56915016c22
Andrew8xx8 <avk@8xx8.ru>
2013-02-19 17:21:59 +0400

    Issue helper tests improved

--- spec/helpers/issues_helper_spec.rb
+++ spec/helpers/issues_helper_spec.rb
@@ -1,9 +1,9 @@
 require "spec_helper"
 
 describe IssuesHelper do
-  let(:project) { create(:project) }
-  let(:issue) { create(:issue, project: project) }
-  let(:ext_project) { create(:redmine_project) }
+  let(:project) { create :project }
+  let(:issue) { create :issue, project: project }
+  let(:ext_project) { create :redmine_project }
 
   describe :title_for_issue do
     it "should return issue title if used internal tracker" do
@@ -20,6 +20,32 @@ describe IssuesHelper do
       @project = nil
 
       title_for_issue(rand(100)).should eq ""
+    end
+  end
+
+  describe :url_for_project_issues do
+    let(:project_url) { Gitlab.config.issues_tracker.redmine.project_url}
+    let(:ext_expected) do
+      project_url.gsub(':project_id', ext_project.id.to_s)
+                 .gsub(':issues_tracker_id', ext_project.issues_tracker_id.to_s)
+    end
+    let(:int_expected) { polymorphic_path([project]) }
+
+    it "should return internal path if used internal tracker" do
+      @project = project
+      url_for_project_issues.should match(int_expected)
+    end
+
+    it "should return path to external tracker" do
+      @project = ext_project
+
+      url_for_project_issues.should match(ext_expected)
+    end
+
+    it "should return empty string if project nil" do
+      @project = nil
+
+      url_for_project_issues.should eq ""
     end
   end
 
