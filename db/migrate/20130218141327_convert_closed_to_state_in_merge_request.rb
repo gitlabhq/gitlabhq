025d44ee18807cda2be31ddc992a21e8c1f41ea7
Anthony Moralez <anthony.moralez@gmail.com>
2013-02-21 10:49:03 -0800

    Fixed migration to work on postgresql

--- db/migrate/20130218141327_convert_closed_to_state_in_merge_request.rb
+++ db/migrate/20130218141327_convert_closed_to_state_in_merge_request.rb
@@ -1,16 +1,16 @@
 class ConvertClosedToStateInMergeRequest < ActiveRecord::Migration
   def up
     MergeRequest.transaction do
-      MergeRequest.where("closed = 1 AND merged = 1").update_all("state = 'merged'")
-      MergeRequest.where("closed = 1 AND merged = 0").update_all("state = 'closed'")
-      MergeRequest.where("closed = 0").update_all("state = 'opened'")
+      MergeRequest.where("closed = true AND merged = true").update_all("state = 'merged'")
+      MergeRequest.where("closed = true AND merged = false").update_all("state = 'closed'")
+      MergeRequest.where("closed = false").update_all("state = 'opened'")
     end
   end
 
   def down
     MergeRequest.transaction do
-      MergeRequest.where(state: :closed).update_all("closed = 1")
-      MergeRequest.where(state: :merged).update_all("closed = 1, merged = 1")
+      MergeRequest.where(state: :closed).update_all("closed = true")
+      MergeRequest.where(state: :merged).update_all("closed = true, merged = true")
     end
   end
 end
