b749dfb7e09d9ed4b124ac1e22a5dd46fe41ee3e
p-schneider <p-schneider@hotmail.de>
2013-02-26 16:00:05 +0100

    Update app/contexts/test_hook_context.rb
    Test Hook did not work (http://gitlabserver/someproject/hooks/1/test)
    related to the changes in 70e3bffd95eb5736dd108e0836abaa85a2f1c742
    
    It failed with this error before i fixed it:
    ```
    Started GET "/someproject/hooks/1/test" for 192.168.1.100 at 2013-02-26 15:24:26 +0100
    Processing by HooksController#test as HTML
      Parameters: {"project_id"=>"someproject", "id"=>"1"}
    Completed 500 Internal Server Error in 118ms
    
    NoMethodError (undefined method `post_receive_data' for #<Project:0x00000004e1ec28>):
      app/contexts/test_hook_context.rb:5:in `execute'
      app/controllers/hooks_controller.rb:26:in `test'
    ```

--- app/contexts/test_hook_context.rb
+++ app/contexts/test_hook_context.rb
@@ -2,7 +2,7 @@ class TestHookContext < BaseContext
   def execute
     hook = project.hooks.find(params[:id])
     commits = project.repository.commits(project.default_branch, nil, 3)
-    data = project.post_receive_data(commits.last.id, commits.first.id, "refs/heads/#{project.default_branch}", current_user)
+    data = GitPushService.new.execute(project, current_user, commits.last.id, commits.first.id, "refs/heads/#{project.default_branch}")
     hook.execute(data)
   end
 end
