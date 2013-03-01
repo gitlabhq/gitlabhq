87f555e1ef26205633ff6458623054c6a95b983b
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2013-03-01 16:07:55 +0200

    fixed db/schema

--- db/schema.rb
+++ db/schema.rb
@@ -106,11 +106,11 @@ ActiveRecord::Schema.define(:version => 20130220133245) do
   add_index "milestones", ["project_id"], :name => "index_milestones_on_project_id"
 
   create_table "namespaces", :force => true do |t|
-    t.string   "name",       :null => false
-    t.string   "path",       :null => false
-    t.integer  "owner_id",   :null => false
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string   "name",                        :null => false
+    t.string   "path",                        :null => false
+    t.integer  "owner_id",                    :null => false
+    t.datetime "created_at",                  :null => false
+    t.datetime "updated_at",                  :null => false
     t.string   "type"
     t.string   "description", :default => "", :null => false
   end
@@ -143,14 +143,14 @@ ActiveRecord::Schema.define(:version => 20130220133245) do
     t.string   "name"
     t.string   "path"
     t.text     "description"
-    t.datetime "created_at",                                :null => false
-    t.datetime "updated_at",                                :null => false
+    t.datetime "created_at",                                   :null => false
+    t.datetime "updated_at",                                   :null => false
     t.integer  "creator_id"
     t.string   "default_branch"
-    t.boolean  "issues_enabled",         :default => true,  :null => false
-    t.boolean  "wall_enabled",           :default => true,  :null => false
-    t.boolean  "merge_requests_enabled", :default => true,  :null => false
-    t.boolean  "wiki_enabled",           :default => true,  :null => false
+    t.boolean  "issues_enabled",         :default => true,     :null => false
+    t.boolean  "wall_enabled",           :default => true,     :null => false
+    t.boolean  "merge_requests_enabled", :default => true,     :null => false
+    t.boolean  "wiki_enabled",           :default => true,     :null => false
     t.integer  "namespace_id"
     t.boolean  "public",                 :default => false,    :null => false
     t.string   "issues_tracker",         :default => "gitlab", :null => false
@@ -233,14 +233,9 @@ ActiveRecord::Schema.define(:version => 20130220133245) do
     t.string   "name"
     t.string   "path"
     t.integer  "owner_id"
-<<<<<<< HEAD
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-=======
     t.datetime "created_at",                  :null => false
     t.datetime "updated_at",                  :null => false
     t.string   "description", :default => "", :null => false
->>>>>>> 2f1f05d431d1df062e46365930b98b358554a07d
   end
 
   create_table "users", :force => true do |t|
