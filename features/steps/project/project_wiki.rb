a3138efaaed5aa528002e3f8b0f54914db20a585
Florin STAN <tom@hangar88.net>
2013-03-03 22:17:46 -0700

    fix a typo

--- features/steps/project/project_wiki.rb
+++ features/steps/project/project_wiki.rb
@@ -7,7 +7,7 @@ class ProjectWiki < Spinach::FeatureSteps
   Given 'I create Wiki page' do
     fill_in "Title", :with => 'Test title'
     #fill_in "epiceditor-editor", :with => '[link test](test)...'
-    find("#epiceditor-editos").native.send_keys '[link test](test)'
+    find("#epiceditor-editor").native.send_keys '[link test](test)'
     click_on "Save"
   end
 
