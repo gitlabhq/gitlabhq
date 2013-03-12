804ae05c58984badf9041509fb01e950d02a71e1
Andrew8xx8 <avk@8xx8.ru>
2013-03-12 13:40:54 +0400

    Tests improved

---
+++ features/steps/dashboard/dashboard_projects.rb
@@ -0,0 +1,20 @@
+class Dashboard < Spinach::FeatureSteps
+  include SharedAuthentication
+  include SharedPaths
+  include SharedProject
+
+  Then 'I should see projects list' do
+    @user.authorized_projects.all.each do |project|
+      page.should have_link project.name_with_namespace
+    end
+  end
+
+  Given 'I search for "Sho"' do
+    fill_in "dashboard_projects_search", with: "Sho"
+    click_button "Search"
+  end
+
+  Then 'I should see "Shop" project link' do
+    page.should have_link "Shop"
+  end
+end
