1bba46d66117b4a96d279fd964a45fe673db658c
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2013-03-13 21:36:53 +0200

    md5 and utf_encode js libraries

---
+++ app/assets/javascripts/utf8_encode.js
@@ -0,0 +1,70 @@
+function utf8_encode (argString) {
+  // http://kevin.vanzonneveld.net
+  // +   original by: Webtoolkit.info (http://www.webtoolkit.info/)
+  // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // +   improved by: sowberry
+  // +    tweaked by: Jack
+  // +   bugfixed by: Onno Marsman
+  // +   improved by: Yves Sucaet
+  // +   bugfixed by: Onno Marsman
+  // +   bugfixed by: Ulrich
+  // +   bugfixed by: Rafal Kukawski
+  // +   improved by: kirilloid
+  // +   bugfixed by: kirilloid
+  // *     example 1: utf8_encode('Kevin van Zonneveld');
+  // *     returns 1: 'Kevin van Zonneveld'
+
+  if (argString === null || typeof argString === "undefined") {
+    return "";
+  }
+
+  var string = (argString + ''); // .replace(/\r\n/g, "\n").replace(/\r/g, "\n");
+  var utftext = '',
+    start, end, stringl = 0;
+
+  start = end = 0;
+  stringl = string.length;
+  for (var n = 0; n < stringl; n++) {
+    var c1 = string.charCodeAt(n);
+    var enc = null;
+
+    if (c1 < 128) {
+      end++;
+    } else if (c1 > 127 && c1 < 2048) {
+      enc = String.fromCharCode(
+         (c1 >> 6)        | 192,
+        ( c1        & 63) | 128
+      );
+    } else if (c1 & 0xF800 != 0xD800) {
+      enc = String.fromCharCode(
+         (c1 >> 12)       | 224,
+        ((c1 >> 6)  & 63) | 128,
+        ( c1        & 63) | 128
+      );
+    } else { // surrogate pairs
+      if (c1 & 0xFC00 != 0xD800) { throw new RangeError("Unmatched trail surrogate at " + n); }
+      var c2 = string.charCodeAt(++n);
+      if (c2 & 0xFC00 != 0xDC00) { throw new RangeError("Unmatched lead surrogate at " + (n-1)); }
+      c1 = ((c1 & 0x3FF) << 10) + (c2 & 0x3FF) + 0x10000;
+      enc = String.fromCharCode(
+         (c1 >> 18)       | 240,
+        ((c1 >> 12) & 63) | 128,
+        ((c1 >> 6)  & 63) | 128,
+        ( c1        & 63) | 128
+      );
+    }
+    if (enc !== null) {
+      if (end > start) {
+        utftext += string.slice(start, end);
+      }
+      utftext += enc;
+      start = end = n + 1;
+    }
+  }
+
+  if (end > start) {
+    utftext += string.slice(start, stringl);
+  }
+
+  return utftext;
+}
