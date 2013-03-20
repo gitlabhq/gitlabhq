a50d96a84149c34c85f9f02cacfd2d15582c8eff
Andrey Kumanyaev <me@zzet.org>
2013-03-21 02:00:52 +0400

    Repair failed test

--- spec/support/valid_commit.rb
+++ spec/support/valid_commit.rb
@@ -9,7 +9,7 @@ module ValidCommit
   C_FILE_PATH = "app/models"
   C_FILES = [".gitkeep", "ability.rb", "commit.rb", "issue.rb", "key.rb", "mailer_observer.rb", "merge_request.rb", "note.rb", "project.rb", "protected_branch.rb", "repository.rb", "snippet.rb", "tree.rb", "user.rb", "users_project.rb", "web_hook.rb", "wiki.rb"]
 
-  BLOB_FILE = %{%h3= @key.title\n%hr\n%pre= @key.key\n.actions\n  = link_to 'Remove', @key, confirm: 'Are you sure?', method: :delete, class: \"btn danger delete-key\"\n\n\n}
+  BLOB_FILE = %{%h3= @key.title\n%hr\n%pre= @key.key\n.actions\n  = link_to 'Remove', @key, :confirm => 'Are you sure?', :method => :delete, :class => \"btn danger delete-key\"\n\n\n}
   BLOB_FILE_PATH = "app/views/keys/show.html.haml"
 end
 
