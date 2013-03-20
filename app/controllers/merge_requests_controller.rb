a163135cb5bd9886b634d8026b7adf473b023782
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2013-03-20 20:22:29 +0200

    Intead of showing 404 give users ability to close MR with missing branches

--- app/controllers/merge_requests_controller.rb
+++ app/controllers/merge_requests_controller.rb
@@ -129,11 +129,11 @@ class MergeRequestsController < ProjectResourceController
 
   def validates_merge_request
     # Show git not found page if target branch doesn't exist
-    return git_not_found! unless @project.repo.heads.map(&:name).include?(@merge_request.target_branch)
+    return invalid_mr unless @project.repo.heads.map(&:name).include?(@merge_request.target_branch)
 
     # Show git not found page if source branch doesn't exist
     # and there is no saved commits between source & target branch
-    return git_not_found! if !@project.repo.heads.map(&:name).include?(@merge_request.source_branch) && @merge_request.commits.blank?
+    return invalid_mr if !@project.repo.heads.map(&:name).include?(@merge_request.source_branch) && @merge_request.commits.blank?
   end
 
   def define_show_vars
@@ -157,5 +157,10 @@ class MergeRequestsController < ProjectResourceController
              end
 
     can?(current_user, action, @project)
+  end
+
+  def invalid_mr
+    # Render special view for MR with removed source or target branch
+    render 'invalid'
   end
 end
