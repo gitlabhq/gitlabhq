06c5389780cf5625f68a0efa3d8ef21b496966a7
Sato Hiroyuki <sathiroyuki@gmail.com>
2013-03-21 15:24:06 +0000

    Fix travis failure randomly, because Capybara.default_wait_time is too short.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,7 @@ require 'email_spec'
 require 'sidekiq/testing/inline'
 require 'capybara/poltergeist'
 Capybara.javascript_driver = :poltergeist
+Capybara.default_wait_time = 10
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
