617239000762a01c57b35cbb699f155622ae921a
Andrew8xx8 <avk@8xx8.ru>
2013-03-24 11:57:15 +0400

    User Teams specific code moved to user_teams controller

--- app/controllers/projects/application_controller.rb
+++ app/controllers/projects/application_controller.rb
@@ -1,11 +1,3 @@
 class Projects::ApplicationController < ApplicationController
 
-  before_filter :authorize_admin_team_member!
-
-  protected
-
-  def user_team
-    @team ||= UserTeam.find_by_path(params[:id])
-  end
-
 end
