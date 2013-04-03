b1117dacbbdc7459e4ca495471ae9a24ebf69f7d
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2013-04-03 17:06:28 +0300

    Fixed before_filter for edit

--- app/controllers/edit_tree_controller.rb
+++ app/controllers/edit_tree_controller.rb
@@ -7,7 +7,7 @@ class EditTreeController < ProjectResourceController
   before_filter :authorize_code_access!
   before_filter :require_non_empty_project
 
-  before_filter :edit_requirements, only: [:edit, :update]
+  before_filter :edit_requirements, only: [:show, :update]
 
   def show
     @last_commit = @project.repository.last_commit_for(@ref, @path).sha
