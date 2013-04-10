d2c70b8920fbf8750c4f4e0b5d45d3846c32b9a9
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2013-04-10 14:38:04 +0300

    Use update_column to set :last_activity_at for project

--- app/observers/project_activity_cache_observer.rb
+++ app/observers/project_activity_cache_observer.rb
@@ -2,7 +2,7 @@ class ProjectActivityCacheObserver < BaseObserver
   observe :event
 
   def after_create(event)
-    event.project.update_attribute(:last_activity_at, event.created_at) if event.project
+    event.project.update_column(:last_activity_at, event.created_at) if event.project
   end
 end
 
