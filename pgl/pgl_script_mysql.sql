0e8cc2400fb36483a66a5e42e33ce0f79d0db2f2
ArthurHoaro <arthur@hoa.ro>
2013-04-11 20:55:52 +0200

    MySQL is now working

--- pgl/pgl_script_mysql.sql
+++ pgl/pgl_script_mysql.sql
@@ -1,28 +1,19 @@
--- NOT SUPPORTED YET
-
 INSERT INTO users (email, encrypted_password, name, username, projects_limit, can_create_team, can_create_group, sign_in_count, created_at, updated_at, admin )
 VALUES ('guest@local.host', '$2a$10$ivc.WwouK4tKT3ZtV8kiD.oVZRzJLV0df7K4nJRV73hhf9a92JeJ.', 'guest', 'guest', 0, 'f', 'f', 0, now(), now(), 'f');
 
 INSERT INTO user_teams (name, path, owner_id, created_at, updated_at, description)
 VALUES ('pgl_reporters', 'pgl_reporters', (SELECT id FROM users WHERE username = 'root'), now(), now(), 'Default new users team (reporter permission)');
 
- -- MySQL wants something like that
 DROP TRIGGER IF EXISTS pgl_new_user;
 delimiter //
 
 CREATE TRIGGER pgl_new_user
 AFTER INSERT ON users FOR EACH ROW
 BEGIN
-    DECLARE m_user_team_id integer;    
-    SELECT id INTO m_user_team_id FROM user_teams WHERE name = "pgl_reporters";
-
-    DECLARE m_projects_id integer;
-    DECLARE cur CURSOR FOR SELECT project_id FROM user_team_project_relationships WHERE user_team_id = m_user_team_id;
-    
     DECLARE done INT DEFAULT FALSE;
-    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
-
-    -- SET @m_user_team_id := (SELECT id FROM user_teams WHERE name = "pgl_reporters");
+    DECLARE m_projects_id integer;
+    DECLARE cur CURSOR FOR SELECT project_id FROM user_team_project_relationships WHERE user_team_id = (SELECT id FROM user_teams WHERE name = "pgl_reporters");
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE; 
 
     OPEN cur;
         ins_loop: LOOP
@@ -36,23 +27,22 @@ BEGIN
         END LOOP;
     CLOSE cur;
 
-    INSERT INTO user_team_user_relationships (user_id, user_team_id, permission, created_at, updated_at) VALUES (NEW.id, m_user_team_id, 20, now(), now());
+    INSERT INTO user_team_user_relationships (user_id, user_team_id, permission, created_at, updated_at) VALUES (NEW.id, (SELECT id FROM user_teams WHERE name = "pgl_reporters"), 20, now(), now());
 END//
+
 DELIMITER ;
 
-DROP TRIGGER IF EXISTS pgl_new_user;
+DROP TRIGGER IF EXISTS pgl_new_project;
+
 delimiter //
 
 CREATE TRIGGER pgl_new_project
-AFTER INSERT ON users FOR EACH ROW
+AFTER INSERT ON projects FOR EACH ROW
 BEGIN
-    DECLARE m_user_team_id integer;    
-    SELECT id INTO m_user_team_id FROM user_teams WHERE name = "pgl_reporters";
-
     DECLARE m_users_id integer;
-    DECLARE cur CURSOR FOR SELECT user_id FROM user_team_user_relationships WHERE user_team_id = m_user_team_id;
-    
     DECLARE done INT DEFAULT FALSE;
+
+    DECLARE cur CURSOR FOR SELECT user_id FROM user_team_user_relationships WHERE user_team_id = (SELECT id FROM user_teams WHERE name = "pgl_reporters");
     DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
 
     OPEN cur;
@@ -67,6 +57,6 @@ BEGIN
         END LOOP;
     CLOSE cur;
 
-    INSERT INTO user_team_project_relationships (project_id, user_team_id, greatest_access, created_at, updated_at) VALUES (NEW.id, m_user_team_id, 20, now(), now());            
+    INSERT INTO user_team_project_relationships (project_id, user_team_id, greatest_access, created_at, updated_at) VALUES (NEW.id, (SELECT id FROM user_teams WHERE name = "pgl_reporters"), 20, now(), now());            
 END//
 DELIMITER ;
