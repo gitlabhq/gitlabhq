228baa80b2063bc2692474e3bbc6eeef887f063e
Friedrich Beckmann <friedrich.beckmann@hs-augsburg.de>
2013-04-27 22:20:26 +0200

    LDAP Authentification with grack for https push - fixed password check

--- lib/gitlab/backend/grack_auth.rb
+++ lib/gitlab/backend/grack_auth.rb
@@ -1,4 +1,5 @@
 require_relative 'shell_env'
+require 'omniauth-ldap'
 
 module Grack
   class Auth < Rack::Auth::Basic
@@ -32,8 +33,14 @@ module Grack
         # Authentication with username and password
         login, password = @auth.credentials
         self.user = User.find_by_email(login) || User.find_by_username(login)
-        return false unless user.try(:valid_password?, password)
 
+        if user.nil?
+          ldap_auth(login,password)
+          return false unless !user.nil?
+        else
+          return false unless user.valid_password?(password);
+        end
+           
         Gitlab::ShellEnv.set_env(user)
       end
 
@@ -44,6 +51,23 @@ module Grack
         validate_post_request
       else
         false
+      end
+    end
+
+    def ldap_auth(login, password)
+      # Check user against LDAP backend if user is not authenticated
+      # Only check with valid login and password to prevent anonymous bind results
+      gl = Gitlab.config
+      if gl.ldap.enabled && !login.blank? && !password.blank?
+        ldap = OmniAuth::LDAP::Adaptor.new(gl.ldap)
+        ldap_user = ldap.bind_as(
+          filter: Net::LDAP::Filter.eq(ldap.uid, login),
+          size: 1,
+          password: password
+        )
+        if ldap_user
+          self.user = User.find_by_extern_uid_and_provider(ldap_user.dn, 'ldap')
+        end
       end
     end
 
