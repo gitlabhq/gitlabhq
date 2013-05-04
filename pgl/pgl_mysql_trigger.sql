695828feb16b72ceab5827be4b4c9b071ff8d6f1
Arthur <arthur@hoa.ro>
2013-05-04 17:08:24 +0200

    Fixes #6

--- pgl/pgl_mysql_trigger.sql
+++ pgl/pgl_mysql_trigger.sql
@@ -39,20 +39,22 @@ BEGIN
     DECLARE cur CURSOR FOR SELECT user_id FROM user_team_user_relationships WHERE user_team_id = (SELECT id FROM user_teams WHERE name = "pgl_reporters");
     DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
 
-    OPEN cur;
-        ins_loop: LOOP
-            FETCH cur INTO m_users_id;
-            IF done THEN
-                LEAVE ins_loop;
-            END IF;
+    IF NEW.public = 1 THEN
+        OPEN cur;
+            ins_loop: LOOP
+                FETCH cur INTO m_users_id;
+                IF done THEN
+                    LEAVE ins_loop;
+                END IF;
 
-            IF m_users_id <> NEW.creator_id THEN
-                INSERT INTO users_projects (user_id, project_id, created_at, updated_at, project_access)
-                VALUES (m_users_id, NEW.id, now(), now(), 20);
-            END IF;
-        END LOOP;
-    CLOSE cur;
+                IF m_users_id <> NEW.creator_id THEN
+                    INSERT INTO users_projects (user_id, project_id, created_at, updated_at, project_access)
+                    VALUES (m_users_id, NEW.id, now(), now(), 20);
+                END IF;
+            END LOOP;
+        CLOSE cur;
 
-    INSERT INTO user_team_project_relationships (project_id, user_team_id, greatest_access, created_at, updated_at) VALUES (NEW.id, (SELECT id FROM user_teams WHERE name = "pgl_reporters"), 20, now(), now());            
+        INSERT INTO user_team_project_relationships (project_id, user_team_id, greatest_access, created_at, updated_at) VALUES (NEW.id, (SELECT id FROM user_teams WHERE name = "pgl_reporters"), 20, now(), now());
+    END IF;
 END//
 DELIMITER ;
