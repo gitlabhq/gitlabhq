fb4a38f6077a27ea14709641f7fbe1547f37c8d4
Ben Bodenmiller <bbodenmiller@hotmail.com>
2013-05-15 00:41:33 -0600

    fixes #3115 and adds support for gif bmp & tiff images
    when showing image preview check via lowercased extension and adds support for gif bmp & tiff images

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -8,12 +8,12 @@ class AttachmentUploader < CarrierWave::Uploader::Base
   end
 
   def image?
-    img_ext = %w(png jpg jpeg)
+    img_ext = %w(png jpg jpeg gif bmp tiff)
     if file.respond_to?(:extension)
-      img_ext.include?(file.extension)
+      img_ext.include?(file.extension.downcase)
     else
       # Not all CarrierWave storages respond to :extension
-      ext = file.path.split('.').last
+      ext = file.path.split('.').last.downcase
       img_ext.include?(ext)
     end
   rescue
