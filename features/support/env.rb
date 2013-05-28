75ced766df1f38311507ed84e19855f9909f944d
Izaak Alpert <ialpert@blackberry.com>
2013-05-28 00:18:53 -0400

    Missed setting up initial test dir
    Change-Id: Iaa13c37f4cfd01083e2a5c297cc06896d84659d1

--- features/support/env.rb
+++ features/support/env.rb
@@ -44,7 +44,7 @@ Spinach.hooks.after_scenario do
 end
 
 Spinach.hooks.before_run do
-  TestEnv.init(mailer: false, init_repos: false, repos: false)
+  TestEnv.init(mailer: false, init_repos: true, repos: false)
   RSpec::Mocks::setup self
 
   include FactoryGirl::Syntax::Methods
