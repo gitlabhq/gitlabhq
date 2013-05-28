07fd95f3840908018cb3712162a441e63409bc31
Drew Blessing <drew.blessing@buckle.com>
2013-05-28 18:41:15 -0500

    Update api docs and remove old attribute from User entity.
    The API docs incorrectly referenced a few attributes associated with users. The 'blocked' attribute has been replaced with 'state'. Also, 'dark_scheme' seems to no longer be available in the User model so it should be removed from the exposed attributes in the API User entity.
    
    Add color scheme ID to API User entity

--- lib/api/entities.rb
+++ lib/api/entities.rb
@@ -2,7 +2,7 @@ module API
   module Entities
     class User < Grape::Entity
       expose :id, :username, :email, :name, :bio, :skype, :linkedin, :twitter,
-             :dark_scheme, :theme_id, :state, :created_at, :extern_uid, :provider
+             :theme_id, :color_scheme_id, :state, :created_at, :extern_uid, :provider
     end
 
     class UserSafe < Grape::Entity
