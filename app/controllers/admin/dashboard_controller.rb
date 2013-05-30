a52936f36a867dbdf40f0f45fd23db95e149480f
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2013-05-30 09:46:28 +0300

    Renames resque resource to background_jobs

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,8 +2,5 @@ class Admin::DashboardController < Admin::ApplicationController
   def index
     @projects = Project.order("created_at DESC").limit(10)
     @users = User.order("created_at DESC").limit(10)
-
-  rescue Redis::InheritedError
-    @resque_accessible = false
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -83,7 +83,7 @@ Gitlab::Application.routes.draw do
     end
 
     resource :logs, only: [:show]
-    resource :resque, controller: 'resque', only: [:show]
+    resource :background_jobs, controller: 'background_jobs', only: [:show]
 
     resources :projects, constraints: { id: /[a-zA-Z.\/0-9_\-]+/ }, only: [:index, :show] do
       scope module: :projects, constraints: { id: /[a-zA-Z.\/0-9_\-]+/ } do
