a3b42b6fa13991440aeb75264f865329cff719a3
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2013-06-19 18:24:18 +0300

    Remove dash projects search test

--- features/steps/dashboard/dashboard_projects.rb
+++ features/steps/dashboard/dashboard_projects.rb
@@ -8,16 +8,4 @@ class DashboardProjects < Spinach::FeatureSteps
       page.should have_link project.name_with_namespace
     end
   end
-
-  Given 'I search for "Sho"' do
-    fill_in "dashboard_projects_search", with: "Sho"
-
-    within ".dashboard-search-filter" do
-      find('button').click
-    end
-  end
-
-  Then 'I should see "Shop" project link' do
-    page.should have_link "Shop"
-  end
 end
