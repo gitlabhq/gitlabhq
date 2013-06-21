c3c849927d42c4ba5431b3970dab63fdb89b77f5
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2013-06-21 22:29:30 +0300

    Unbind note events to prevent firing twice with turbolinks

--- app/assets/javascripts/notes.js
+++ app/assets/javascripts/notes.js
@@ -1,5 +1,5 @@
 var NoteList = {
-
+  id: null,
   notes_path: null,
   target_params: null,
   target_id: 0,
@@ -15,6 +15,16 @@ var NoteList = {
 
     // get initial set of notes
     NoteList.getContent();
+
+    // Unbind events to prevent firing twice
+    $(document).off("click", ".js-add-diff-note-button");
+    $(document).off("click", ".js-discussion-reply-button");
+    $(document).off("click", ".js-note-preview-button");
+    $(document).off("click", ".js-note-attachment-input");
+    $(document).off("click", ".js-close-discussion-note-form");
+    $(document).off("click", ".js-note-delete");
+    $(document).off("ajax:complete", ".js-main-target-form");
+
 
     // add a new diff note
     $(document).on("click",
