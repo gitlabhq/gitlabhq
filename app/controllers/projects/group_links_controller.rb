0b012d966292224a3e1de4c29aef57d39243c37b
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2013-07-10 16:14:00 +0300

    GroupLinks: Empty Scaffold, routes + sample page

---
+++ app/controllers/projects/group_links_controller.rb
@@ -0,0 +1,16 @@
+class Projects::GroupLinksController < Projects::ApplicationController
+  layout 'project_settings'
+
+  def index
+    # TODO: Implement
+  end
+
+  def create
+    # TODO: Implement
+  end
+
+  def destroy
+    # TODO: Implement
+  end
+end
+
