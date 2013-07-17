60acc9c23806cc4a76f1f43ee9a1ba8b75244596
Lonre Wang <me@wanglong.me>
2013-07-18 01:33:34 +0800

    Custom gitlab-shell auth_file

--- config/initializers/1_settings.rb
+++ config/initializers/1_settings.rb
@@ -96,6 +96,7 @@ Settings.gitlab_shell['ssh_port']     ||= 22
 Settings.gitlab_shell['ssh_user']     ||= Settings.gitlab.user
 Settings.gitlab_shell['owner_group']  ||= Settings.gitlab.user
 Settings.gitlab_shell['ssh_path_prefix'] ||= Settings.send(:build_gitlab_shell_ssh_path_prefix)
+Settings.gitlab_shell['auth_file']    ||= Settings.gitlab['user_home'] + '/.ssh/authorized_keys'
 
 #
 # Backup
