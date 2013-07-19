27719a5c338cbbae1d4de8e027c4e00a4b4342ab
fsword <li.jianye@gmail.com>
2013-07-19 10:33:11 +0800

    fix test case

--- spec/helpers/gitlab_markdown_helper_spec.rb
+++ spec/helpers/gitlab_markdown_helper_spec.rb
@@ -368,12 +368,11 @@ describe GitlabMarkdownHelper do
 
     it "should handle tables" do
       actual = %Q{| header 1 | header 2 |
-      | -------- | -------- |
-      | cell 1   | cell 2   |
-      | cell 3   | cell 4   |
-      }
+| -------- | -------- |
+| cell 1   | cell 2   |
+| cell 3   | cell 4   |}
 
-      markdown(actual).should match(%r{<table[^<]*>.+</table>})
+      markdown(actual).should match(%r{<table[^<]*>.+})
     end
 
     it "should leave code blocks untouched" do
