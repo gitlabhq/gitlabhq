07de7bcaf6fd551684763ef0bd0bde7cef2f5907
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2013-07-29 18:07:20 +0300

    Add group base to config. Join auth options only if provided

--- lib/gitlab/ldap.rb
+++ lib/gitlab/ldap.rb
@@ -3,15 +3,24 @@ module Gitlab
     attr_reader :ldap
 
     def initialize
-      @ldap = Net::LDAP.new(
+      options = {
         host: config['host'],
         port: config['port'],
+      }
+
+      auth_options = {
         auth: {
           method: config['method'],
           username: config['bind_dn'],
           password: config['password']
         }
-      )
+      }
+
+      if config['password'] || config['bind_dn']
+        options.merge!(auth_options)
+      end
+
+      @ldap = Net::LDAP.new(options)
     end
 
     # Get LDAP groups from ou=Groups
@@ -23,7 +32,7 @@ module Gitlab
     #
     def groups(cn = "*")
       options = {
-        base: "ou=Groups,#{config['base']}",
+        base: config['group_base'],
         filter: Net::LDAP::Filter.eq("cn", cn)
       }
 
