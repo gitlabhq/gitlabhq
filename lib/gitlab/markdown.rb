868f83e5d2ca5c23e57b6fa75db714f854890a08
Bin <mongris@gmail.com>
2013-07-31 16:16:17 +0800

    Support table and toc in markdown

--- lib/gitlab/markdown.rb
+++ lib/gitlab/markdown.rb
@@ -63,7 +63,11 @@ module Gitlab
         insert_piece($1)
       end
 
-      sanitize text.html_safe, attributes: ActionView::Base.sanitized_allowed_attributes + %w(id class)
+      sanitize(
+        text.html_safe,
+        attributes: ActionView::Base.sanitized_allowed_attributes + %w(id class),
+        tags: ActionView::Base.sanitized_allowed_tags             + %w(table thead th tr td)
+      )
     end
 
     private
@@ -90,6 +94,7 @@ module Gitlab
     def parse(text)
       parse_references(text) if @project
       parse_emoji(text)
+      text = parse_toc(text)
 
       text
     end
@@ -145,6 +150,36 @@ module Gitlab
         end
       end
     end
+
+    # If the user wants TOC we run a Render::HTML_TOC
+    # See http://dev.af83.com/2012/02/27/howto-extend-the-redcarpet2-markdown-lib.html
+    # TODO: Allow TOC to be placed in the middle of the document, not only at top
+    def parse_toc(text)
+      if text.length > 0 && text.lines.first.match("~toc~") != nil
+        html_text = text.dup.to_str
+
+        # First we must convert HTML headers back to Markdown headers
+        html_text.gsub!(/<h1[^>]*>/, "# ")
+        html_text.gsub!(/<h2[^>]*>/, "## ")
+        html_text.gsub!(/<h3[^>]*>/, "### ")
+        html_text.gsub!(/<h4[^>]*>/, "#### ")
+        html_text.gsub!(/<h5[^>]*>/, "##### ")
+        html_text.gsub!(/<h6[^>]*>/, "###### ")
+
+        html_text.gsub!(/<\/?h\d[^>]*>/, "")
+
+
+        html_toc = Redcarpet::Markdown.new(Redcarpet::Render::HTML_TOC, space_after_headers: true)
+        toc = html_toc.render(html_text)
+
+        text["~toc~"]= ""
+
+        toc + text
+      else
+        text
+      end
+    end
+
 
     # Private: Checks if an emoji icon exists in the image asset directory
     #
