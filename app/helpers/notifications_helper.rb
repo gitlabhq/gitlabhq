9f38cf5093f5141717cfa1b1c956ed8ec9135ebe
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2013-07-31 17:05:22 +0300

    Improve Notifications page. Much easier to find muted projects now

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,2 +1,13 @@
 module NotificationsHelper
+  def notification_icon(notification)
+    if notification.disabled?
+      content_tag :i, nil, class: 'icon-circle cred'
+    elsif notification.participating?
+      content_tag :i, nil, class: 'icon-circle cblue'
+    elsif notification.watch?
+      content_tag :i, nil, class: 'icon-circle cgreen'
+    else
+      content_tag :i, nil, class: 'icon-circle-blank cblue'
+    end
+  end
 end
