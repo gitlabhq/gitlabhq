d81532f410fa6bfb8f520557c37961e8da67860d
Phlegx Systems <register@phlegx.com>
2013-08-23 20:20:47 +0200

    Add mail commit notifier with git-commit-notifier gem.

--- app/observers/project_activity_cache_observer.rb
+++ app/observers/project_activity_cache_observer.rb
@@ -3,6 +3,10 @@ class ProjectActivityCacheObserver < BaseObserver
 
   def after_create(event)
     event.project.update_column(:last_activity_at, event.created_at) if event.project
+    
+    if event.project && event.action == Event::PUSHED
+        notification.receive_commit(event)
+    end
   end
 end
 
