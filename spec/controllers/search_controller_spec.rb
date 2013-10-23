91c7b266cdffbd82ac70f35ca9bd5d08b989dbe3
Izaak Alpert <ialpert@blackberry.com>
2013-10-23 16:27:40 -0400

    Allow public repo searching
    GITLAB-1386
    
    Change-Id: I9a0bbe8db1a9bb00cef33b3e1854d78844b70a45

---
+++ spec/controllers/search_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe SearchController do
+  let(:project) { create(:project, public: true) }
+  let(:user) { create(:user) }
+
+  before do
+    sign_in(user)
+  end
+
+  describe '#find_project_ids' do
+    it 'should include public projects ids when searching within a single project' do
+      project_ids = controller.find_project_ids(nil, project.id)
+      project_ids.size.should == 1
+      project_ids[0].should == project.id
+    end
+  end
+end
