c46eaca91247ccf8e6fb3b691dad028e1b084ae3
Nigel Kukard <nkukard@lbsd.net>
2013-11-04 22:06:27 +0000

    More escaping
    - Database name may contain characters which are not shell friendly
    - Database password could contain the same
    - While we at it there is no harm in escaping generated paths too
    - Refactored 2-line system(command)
    
    Signed-off-by: Nigel Kukard <nkukard@lbsd.net>

--- spec/support/test_env.rb
+++ spec/support/test_env.rb
@@ -1,4 +1,5 @@
 require 'rspec/mocks'
+require 'shellwords'
 
 module TestEnv
   extend self
@@ -102,7 +103,7 @@ module TestEnv
     repo = repo(namespace, name)
 
     # Symlink tmp/repositories/gitlabhq to tmp/test-git-base-path/gitlabhq
-    system("ln -s -f #{seed_repo_path()} #{repo}")
+    system("ln -s -f #{Shellwords.shellescape(seed_repo_path())} #{Shellwords.shellescape(repo)}")
     create_satellite(repo, namespace, name)
   end
 
@@ -166,12 +167,11 @@ module TestEnv
     # Symlink tmp/satellite/gitlabhq to tmp/test-git-base-path/satellite/gitlabhq, create the directory if it doesn't exist already
     satellite_dir = File.dirname(satellite_repo)
     FileUtils.mkdir_p(satellite_dir) unless File.exists?(satellite_dir)
-    system("ln -s -f #{seed_satellite_path} #{satellite_repo}")
+    system("ln -s -f #{Shellwords.shellescape(seed_satellite_path)} #{Shellwords.shellescape(satellite_repo)}")
   end
 
   def create_temp_repo(path)
     FileUtils.mkdir_p path
-    command = "git init --quiet --bare #{path};"
-    system(command)
+    system("git init --quiet --bare #{Shellwords.shellescape(path)}")
   end
 end
