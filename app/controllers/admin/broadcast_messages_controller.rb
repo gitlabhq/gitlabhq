dd501aa7a79644919ba0c47d1ed764b6db768c8d
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2013-11-12 14:28:12 +0200

    Broadcast messages scaffold in admin area
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

---
+++ app/controllers/admin/broadcast_messages_controller.rb
@@ -0,0 +1,23 @@
+class Admin::BroadcastMessagesController < Admin::ApplicationController
+  before_filter :broadcast_messages
+
+  def index
+    @broadcast_message = BroadcastMessage.new
+  end
+
+  def create
+    @broadcast_message = BroadcastMessage.new(params[:broadcast_message])
+
+    if @broadcast_message.save
+      redirect_to admin_broadcast_messages_path, notice: 'Broadcast Message was successfully created.'
+    else
+      render :index
+    end
+  end
+
+  protected
+
+  def broadcast_messages
+    @broadcast_messages ||= BroadcastMessage.order("starts_at DESC").page(params[:page])
+  end
+end
--- app/models/broadcast_message.rb
+++ app/models/broadcast_message.rb
@@ -2,4 +2,6 @@ class BroadcastMessage < ActiveRecord::Base
   attr_accessible :alert_type, :ends_at, :message, :starts_at
 
   validates :message, presence: true
+  validates :starts_at, presence: true
+  validates :ends_at, presence: true
 end
