4091322dba7ee3b94de9ad0b8b061622ba1e468c
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2013-11-27 13:13:33 +0200

    Improve UX of project visiblity level. Improve "Public Access" help page
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- app/helpers/visibility_level_helper.rb
+++ app/helpers/visibility_level_helper.rb
@@ -18,17 +18,11 @@ module VisibilityLevelHelper
           haml_concat "Project access must be granted explicitly for each user."
         when Gitlab::VisibilityLevel::INTERNAL
           haml_concat "The project can be cloned by"
-          haml_tag :em, "any logged in user."
-          haml_concat "It will also be listed on the #{link_to "public access directory", public_root_path} for logged in users."
-          haml_tag :em, "Any logged in user"
-          haml_concat "will have #{link_to "Guest", help_permissions_path} permissions on the repository."
+          haml_concat "any logged in user."
         when Gitlab::VisibilityLevel::PUBLIC
           haml_concat "The project can be cloned"
-          haml_tag :em, "without any"
+          haml_concat "without any"
           haml_concat "authentication."
-          haml_concat "It will also be listed on the #{link_to "public access directory", public_root_path}."
-          haml_tag :em, "Any logged in user"
-          haml_concat "will have #{link_to "Guest", help_permissions_path} permissions on the repository."
         end
       end
     end
@@ -48,7 +42,7 @@ module VisibilityLevelHelper
   def visibility_level_label(level)
     Project.visibility_levels.key(level)
   end
-  
+
   def restricted_visibility_levels
     current_user.is_admin? ? [] : gitlab_config.restricted_visibility_levels
   end
