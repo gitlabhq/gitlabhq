cfcf24dce53d79c950de12eca0353b890ad95508
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2013-12-03 11:34:06 +0200

    Execute project hooks when issue or merge request created
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- app/observers/issue_observer.rb
+++ app/observers/issue_observer.rb
@@ -1,14 +1,14 @@
 class IssueObserver < BaseObserver
   def after_create(issue)
     notification.new_issue(issue, current_user)
-
     issue.create_cross_references!(issue.project, current_user)
+    execute_hooks(issue)
   end
 
   def after_close(issue, transition)
     notification.close_issue(issue, current_user)
-
     create_note(issue)
+    execute_hooks(issue)
   end
 
   def after_reopen(issue, transition)
@@ -28,5 +28,9 @@ class IssueObserver < BaseObserver
   # Create issue note with service comment like 'Status changed to closed'
   def create_note(issue)
     Note.create_status_change_note(issue, issue.project, current_user, issue.state, current_commit)
+  end
+
+  def execute_hooks(issue)
+    issue.project.execute_hooks(issue.to_hook_data, :issue_hooks)
   end
 end
