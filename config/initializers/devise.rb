ac5842d970ba305681bb1c0826db769243101123
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2013-12-05 16:26:34 +0200

    Migrate application to rails 4 step 1
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,6 +6,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
   config.mailer_sender = Gitlab.config.gitlab.email_from
 
+
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
 
