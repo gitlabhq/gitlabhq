19ee90e59ba40db14c488d1ab6c63a8c4e86083d
Paul Chechetin <paulche@yandex.ru>
2013-12-10 19:08:05 +0400

    Use :prod => :redis_store, :dev => :cache_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,10 +1,20 @@
 # Be sure to restart your server when you modify this file.
 
-Gitlab::Application.config.session_store(
-  :redis_store, # Using the cookie_store would enable session replay attacks.
-  servers: Gitlab::Application.config.cache_store.last, # re-use the Redis config from the Rails cache store
-  key: '_gitlab_session',
-  secure: Gitlab::Application.config.force_ssl,
-  httponly: true,
-  path: (Rails.application.config.relative_url_root.nil?) ? '/' : Rails.application.config.relative_url_root
-)
+session_store = if Rails.env.prod? 
+                  # Using the cookie_store would enable session replay attacks.
+                  :redis_store
+                else
+                  :cache_store 
+                end
+
+store_options = if Rails.env.prod?
+                  {
+                    servers: Gitlab::Application.config.cache_store.last, # re-use the Redis config from the Rails cache store
+                    key: '_gitlab_session',
+                    secure: Gitlab::Application.config.force_ssl,
+                    httponly: true,
+                    path: (Rails.application.config.relative_url_root.nil?) ? '/' : Rails.application.config.relative_url_root
+                  }
+                end
+
+Gitlab::Application.config.session_store(session_store, store_options)
