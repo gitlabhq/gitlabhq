d56831836f7a553e0dbceabbb691ba199cf3022e
Jason Hollingsworth <jhworth.developer@gmail.com>
2013-12-19 14:22:28 -0600

    OAuth nickname to username
    This uses the oauth nickname, when present, as the username.

--- spec/lib/oauth_spec.rb
+++ spec/lib/oauth_spec.rb
@@ -6,34 +6,62 @@ describe Gitlab::OAuth::User do
   before do
     Gitlab.config.stub(omniauth: {})
 
-    @info = double(
+    @info0 = double(
       uid: '12djsak321',
       name: 'John',
-      email: 'john@mail.com'
+      email: 'john@mail.com',
+      nickname: nil
+    )
+
+   @info1 = double(
+      uid: '12djsak321',
+      name: 'John',
+      email: 'john@mail.com',
+      nickname: 'johnny'
     )
   end
 
   describe :create do
     it "should create user from LDAP" do
-      @auth = double(info: @info, provider: 'ldap')
+      @auth = double(info: @info0, provider: 'ldap')
       user = gl_auth.create(@auth)
 
       user.should be_valid
-      user.extern_uid.should == @info.uid
+      user.extern_uid.should == @info0.uid
       user.provider.should == 'ldap'
     end
 
     it "should create user from Omniauth" do
-      @auth = double(info: @info, provider: 'twitter')
+      @auth = double(info: @info0, provider: 'twitter')
       user = gl_auth.create(@auth)
 
       user.should be_valid
-      user.extern_uid.should == @info.uid
+      user.extern_uid.should == @info0.uid
       user.provider.should == 'twitter'
     end
 
+    it "should create user from LDAP with nickname" do
+      @auth = double(info: @info1, provider: 'ldap')
+      user = gl_auth.create(@auth)
+
+      user.should be_valid
+      user.extern_uid.should == @info1.uid
+      user.provider.should == 'ldap'
+      user.username.should == 'johnny'
+    end
+
+    it "should create user from Omniauth with nickname" do
+      @auth = double(info: @info1, provider: 'twitter')
+      user = gl_auth.create(@auth)
+
+      user.should be_valid
+      user.extern_uid.should == @info1.uid
+      user.provider.should == 'twitter'
+      user.username.should == 'johnny'
+    end
+
     it "should apply defaults to user" do
-      @auth = double(info: @info, provider: 'ldap')
+      @auth = double(info: @info0, provider: 'ldap')
       user = gl_auth.create(@auth)
 
       user.should be_valid
