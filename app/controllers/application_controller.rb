94c96cd4458b89e3280fec8cc8ed42a69e063191
Marin Jankovski <marin@gitlab.com>
2013-12-30 09:41:05 +0100

    HTTP headers protect against MIME-sniffing, force https if enabled.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -161,6 +161,8 @@ class ApplicationController < ActionController::Base
     headers['X-Frame-Options'] = 'DENY'
     headers['X-XSS-Protection'] = '1; mode=block'
     headers['X-UA-Compatible'] = 'IE=edge'
+    headers['X-Content-Type-Options'] = 'nosniff'
+    headers['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains' if Gitlab.config.gitlab.https
   end
 
   def add_gon_variables
