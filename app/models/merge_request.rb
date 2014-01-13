db966364de38d896961abba74c20a69bd3d6361c
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2014-01-13 17:20:30 +0200

    MergeRequest#automerge accepts custom message now. MergeRequest#merge_commit_message generate default one
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- app/models/merge_request.rb
+++ app/models/merge_request.rb
@@ -214,8 +214,8 @@ class MergeRequest < ActiveRecord::Base
     self.merge
   end
 
-  def automerge!(current_user)
-    if Gitlab::Satellite::MergeAction.new(current_user, self).merge! && self.unmerged_commits.empty?
+  def automerge!(current_user, merge_commit_message = nil)
+    if Gitlab::Satellite::MergeAction.new(current_user, self).merge!(merge_commit_message) && self.unmerged_commits.empty?
       self.merge!(current_user.id)
       true
     end
@@ -317,6 +317,14 @@ class MergeRequest < ActiveRecord::Base
     Event.where(target_id: self.id, target_type: 'MergeRequest').
         order('id DESC').limit(100).
         update_all(updated_at: Time.now)
+  end
+
+  def merge_commit_message
+    message = "Merge branch '#{source_branch}' into '#{target_branch}'"
+    message << "\n\n"
+    message << title
+    message << "\n\n"
+    message << description
   end
 
   private
