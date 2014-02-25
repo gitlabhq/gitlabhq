a1b9b1d83fc1e81ab6ff9d6593c8a95697e7474a
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2014-02-25 17:42:22 +0200

    Use rails preload for Dashboard issues, mr
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -49,12 +49,13 @@ class GroupsController < ApplicationController
   def merge_requests
     @merge_requests = FilteringService.new.execute(MergeRequest, current_user, params)
     @merge_requests = @merge_requests.page(params[:page]).per(20)
+    @merge_requests = @merge_requests.preload(:author, :project)
   end
 
   def issues
     @issues = FilteringService.new.execute(Issue, current_user, params)
     @issues = @issues.page(params[:page]).per(20)
-    @issues = @issues.includes(:author, :project)
+    @issues = @issues.preload(:author, :project)
 
     respond_to do |format|
       format.html
