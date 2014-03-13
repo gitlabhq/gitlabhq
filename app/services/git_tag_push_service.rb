5be424565ebe71b6c4d93adfecea9d7b057ae086
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2014-03-13 11:37:13 +0200

    Create event and clear cache on new trag push
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- app/services/git_tag_push_service.rb
+++ app/services/git_tag_push_service.rb
@@ -1,8 +1,12 @@
 class GitTagPushService
   attr_accessor :project, :user, :push_data
+
   def execute(project, user, oldrev, newrev, ref)
     @project, @user = project, user
     @push_data = create_push_data(oldrev, newrev, ref)
+
+    create_push_event
+    project.repository.expire_cache
     project.execute_hooks(@push_data.dup, :tag_push_hooks)
   end
 
@@ -23,5 +27,14 @@ class GitTagPushService
         homepage: project.web_url
       }
     }
+  end
+
+  def create_push_event
+    Event.create!(
+      project: project,
+      action: Event::PUSHED,
+      data: push_data,
+      author_id: push_data[:user_id]
+    )
   end
 end
