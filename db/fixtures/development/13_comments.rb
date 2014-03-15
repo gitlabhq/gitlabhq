39f80884db394decdb4b4a21a30bc1ecb3203944
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2014-03-15 11:39:35 +0200

    Improve developer seeds
    Return execution of observers in seeds.
    Mute email sending to letter opening in you browser.
    Added `rake dev` task to reset db and add seeds.
    
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- db/fixtures/development/13_comments.rb
+++ db/fixtures/development/13_comments.rb
@@ -1,13 +1,19 @@
-ActiveRecord::Base.observers.disable :all
+Gitlab::Seeder.quiet do
+  Issue.all.limit(10).each_with_index do |issue, i|
+    5.times do
+      user = issue.project.team.users.sample
 
-Issue.all.limit(10).each_with_index do |issue, i|
-  5.times do
-    Note.seed(:id, [{
-      project_id: issue.project.id,
-      author_id: issue.project.team.users.sample.id,
-      note: Faker::Lorem.sentence,
-      noteable_id: issue.id,
-      noteable_type: 'Issue'
-    }])
+      Gitlab::Seeder.by_user(user) do
+        Note.seed(:id, [{
+          project_id: issue.project.id,
+          author_id: user.id,
+          note: Faker::Lorem.sentence,
+          noteable_id: issue.id,
+          noteable_type: 'Issue'
+        }])
+
+        print '.'
+      end
+    end
   end
 end
--- lib/tasks/dev.rake
+++ lib/tasks/dev.rake
@@ -1,10 +1,10 @@
+task dev: ["dev:setup"]
+
 namespace :dev do
   desc "GITLAB | Setup developer environment (db, fixtures)"
   task :setup => :environment do
     ENV['force'] = 'yes'
-    Rake::Task["db:setup"].invoke
-    Rake::Task["db:seed_fu"].invoke
+    Rake::Task["gitlab:setup"].invoke
     Rake::Task["gitlab:shell:setup"].invoke
   end
 end
-
