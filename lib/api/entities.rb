c02e3d441b6ef35bdbb4a45829f47369aa911690
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2014-03-17 15:29:45 +0200

    Fix tests
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- lib/api/entities.rb
+++ lib/api/entities.rb
@@ -132,7 +132,7 @@ module API
     end
 
     class MergeRequest < ProjectEntity
-      expose :target_branch, :source_branch, :title, :state, :upvotes, :downvotes
+      expose :target_branch, :source_branch, :title, :state, :upvotes, :downvotes, :description
       expose :author, :assignee, using: Entities::UserBasic
       expose :source_project_id, :target_project_id
     end
