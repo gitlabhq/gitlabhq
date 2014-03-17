f04d5bfaf6afcdbc4e91a2fa3069a5438b7fa569
Marin Jankovski <marin@gitlab.com>
2014-03-17 11:27:59 +0100

    Wiki will show relative links related to the wiki gollum repository.

--- app/helpers/gitlab_markdown_helper.rb
+++ app/helpers/gitlab_markdown_helper.rb
@@ -63,8 +63,7 @@ module GitlabMarkdownHelper
   # project_path_with_namespace - namespace/projectname, eg. gitlabhq/gitlabhq
   # ref - name of the branch or reference, eg. stable
   # requested_path - path of request, eg. doc/api/README.md, used in special case when path is pointing to the .md file were the original request is coming from
-  # wiki - whether the markdown is from wiki or not
-  def create_relative_links(text, project, ref, requested_path, wiki = false)
+  def create_relative_links(text, project, ref, requested_path)
     @path_to_satellite = project.satellite.path
     project_path_with_namespace = project.path_with_namespace
     paths = extract_paths(text)
