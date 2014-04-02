928fbeeec057692d923146994c4b8dff57024417
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2014-04-02 16:33:07 +0300

    More tests for Isses services
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- spec/services/issues/close_service_spec.rb
+++ spec/services/issues/close_service_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
-describe Issues::UpdateService do
+describe Issues::CloseService do
   let(:project) { create(:empty_project) }
   let(:user) { create(:user) }
   let(:user2) { create(:user) }
-  let(:issue) { create(:issue) }
+  let(:issue) { create(:issue, assignee: user2) }
 
   before do
     project.team << [user, :master]
@@ -14,19 +14,10 @@ describe Issues::UpdateService do
   describe :execute do
     context "valid params" do
       before do
-        opts = {
-          title: 'New title',
-          description: 'Also please fix',
-          assignee_id: user2.id,
-          state_event: 'close'
-        }
-
-        @issue = Issues::UpdateService.new(project, user, opts).execute(issue)
+        @issue = Issues::CloseService.new(project, user, {}).execute(issue)
       end
 
       it { @issue.should be_valid }
-      it { @issue.title.should == 'New title' }
-      it { @issue.assignee.should == user2 }
       it { @issue.should be_closed }
 
       it 'should send email to user2 about assign of new issue' do
@@ -37,7 +28,7 @@ describe Issues::UpdateService do
 
       it 'should create system note about issue reassign' do
         note = @issue.notes.last
-        note.note.should include "Reassigned to \@#{user2.username}"
+        note.note.should include "Status changed to closed"
       end
     end
   end
