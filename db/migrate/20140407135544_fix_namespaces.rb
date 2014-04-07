52cf11b9f2f22218abd79aa3936ddb7f0d6cee59
Job van der Voort <job@gitlab.com>
2014-04-07 14:26:52 +0000

    add a space

--- db/migrate/20140407135544_fix_namespaces.rb
+++ db/migrate/20140407135544_fix_namespaces.rb
@@ -1,7 +1,7 @@
 class FixNamespaces < ActiveRecord::Migration
   def up
     Namespace.where('name <> path and type is null').each do |namespace|
-     namespace.update_attribute(:name, namespace.path)
+      namespace.update_attribute(:name, namespace.path)
     end
   end
 
