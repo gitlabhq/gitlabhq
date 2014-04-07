6d196962670b35ea48588aad3cfdb6f2d11febab
Job van der Voort <job@gitlab.com>
2014-04-07 16:03:49 +0200

    Fix faulty namespaces

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140313092127) do
+ActiveRecord::Schema.define(version: 20140407135544) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
