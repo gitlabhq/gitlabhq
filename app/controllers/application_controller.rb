84d17f79655498f49d5c957c8f09aeef7cdc56c2
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2014-04-07 14:32:59 +0300

    Skip require email for devise controllers
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   before_filter :default_headers
   before_filter :add_gon_variables
   before_filter :configure_permitted_parameters, if: :devise_controller?
-  before_filter :require_email
+  before_filter :require_email, unless: :devise_controller?
 
   protect_from_forgery
 
