410790a355a6389bfa372e0834c31541b8ac8e4f
Jacob Vosmaer <contact@jacobvosmaer.nl>
2014-04-28 11:48:18 +0200

    Add NotesFinder spec

---
+++ spec/finders/notes_finder_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe NotesFinder do
+  let(:user) { create :user }
+  let(:project) { create :project }
+  let(:note1) { create :note_on_commit, project: project }
+  let(:note2) { create :note_on_commit, project: project }
+  let(:commit) { note1.commit }
+
+  before do
+    project.team << [user, :master]
+  end
+
+  describe :execute do
+    before do
+      note1
+      note2
+    end
+
+    it 'should find all notes' do
+      params = { target_id: commit.id, target_type: 'commit' }
+      notes = NotesFinder.new.execute(project, user, params)
+      notes.size.should eq(2)
+    end
+  end
+end
