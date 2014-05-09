d9c0ecdca812743a80cc464c9fea1b0ee77fe50c
Marin Jankovski <marin@gitlab.com>
2014-05-09 14:51:29 +0200

    Add scope to dn filter.

--- lib/gitlab/ldap/adapter.rb
+++ lib/gitlab/ldap/adapter.rb
@@ -108,7 +108,7 @@ module Gitlab
       end
 
       def dn_matches_filter?(dn, filter)
-        ldap_search(base: dn, filter: filter, attributes: %w{dn}).any?
+        ldap_search(base: dn, filter: filter, scope: Net::LDAP::SearchScope_BaseObject, attributes: %w{dn}).any?
       end
 
       def ldap_search(*args)
