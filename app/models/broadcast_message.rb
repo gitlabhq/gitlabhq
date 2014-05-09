12179f3ac1ce08686d906d69a1b5744822623aad
Max Mitchell <max.mitchell@gmail.com>
2014-05-09 10:15:31 +0100

    Fix bug allowing matching 4 and 5 character codes

--- app/models/broadcast_message.rb
+++ app/models/broadcast_message.rb
@@ -20,9 +20,9 @@ class BroadcastMessage < ActiveRecord::Base
   validates :starts_at, presence: true
   validates :ends_at, presence: true
 
-  validates :color, format: { with: /\A\#[0-9A-Fa-f]{3,6}+\Z/ },
+  validates :color, format: { with: /\A\#[0-9A-Fa-f]{3}{1,2}+\Z/ },
             allow_blank: true
-  validates :font,  format: { with: /\A\#[0-9A-Fa-f]{3,6}+\Z/ },
+  validates :font,  format: { with: /\A\#[0-9A-Fa-f]{3}{1,2}+\Z/ },
             allow_blank: true
 
   def self.current
