595c8ee67fecbb3879bd60b409d36b1421258012
Marvin Frick <marvin.frick@sinnerschrader.com>
2014-05-09 14:26:19 +0200

    fixes gitlab.com issue #229
    Changes .js.coffe files to not use a hardcoded path to
    `no_avatar.png` but instead stick with the asset pipeline.
    
    renames coffee.erb back to coffee

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -71,7 +71,7 @@ module ApplicationHelper
     size = 40 if size.nil? || size <= 0
 
     if !Gitlab.config.gravatar.enabled || user_email.blank?
-      '/assets/no_avatar.png'
+      image_path('no_avatar.png')
     else
       gravatar_url = request.ssl? || gitlab_config.https ? Gitlab.config.gravatar.ssl_url : Gitlab.config.gravatar.plain_url
       user_email.strip!
