34fd557055e027b6423241e73b7cf26c741c0b6b
Jacob Vosmaer <contact@jacobvosmaer.nl>
2014-05-15 10:17:13 +0200

    Move user access check to Gitlab::UserAccess

--- lib/gitlab/git_access.rb
+++ lib/gitlab/git_access.rb
@@ -61,18 +61,7 @@ module Gitlab
     private
 
     def user_allowed?(user)
-      return false if user.blocked?
-
-      if Gitlab.config.ldap.enabled
-        if user.ldap_user?
-          # Check if LDAP user exists and match LDAP user_filter
-          Gitlab::LDAP::Access.open do |adapter|
-            return false unless adapter.allowed?(user)
-          end
-        end
-      end
-
-      true
+      Gitlab::UserAccess.allowed?(user)
     end
   end
 end
