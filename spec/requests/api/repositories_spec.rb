242291850d7513731355c42e67c2f8c3cc727451
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2014-05-26 18:05:55 +0300

    Add some tests to compare api
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- spec/requests/api/repositories_spec.rb
+++ spec/requests/api/repositories_spec.rb
@@ -112,4 +112,24 @@ describe API::API, api: true  do
       response.status.should == 404
     end
   end
+
+  describe 'GET /GET /projects/:id/repository/compare' do
+    it "should compare 2 branches" do
+      get api("/projects/#{project.id}/repository/compare", user), from: 'master', to: 'simple_merge_request'
+      response.status.should == 200
+      json_response['commits'].size.should == 3
+    end
+
+    it "should compare 2 commits" do
+      get api("/projects/#{project.id}/repository/compare", user), from: 'b1e6a9dbf1c85', to: '1e689bfba395'
+      response.status.should == 200
+      json_response['commits'].size.should == 0
+    end
+
+    it "should compare 2 commits" do
+      get api("/projects/#{project.id}/repository/compare", user), from: '1e689bfba395', to: 'b1e6a9dbf1c85'
+      response.status.should == 200
+      json_response['commits'].size.should == 4
+    end
+  end
 end
