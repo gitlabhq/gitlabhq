f79a300417120bb2bdd5d5b22c8ced42e5c3dab7
Marin Jankovski <marin@gitlab.com>
2014-05-28 15:58:02 +0200

    Replace existing links to help pages with the new ones.

--- features/steps/help.rb
+++ features/steps/help.rb
@@ -8,7 +8,7 @@ class Spinach::Features::Help < Spinach::FeatureSteps
   end
 
   step 'I visit the "Rake Tasks" help page' do
-    visit help_raketasks_path
+    visit help_page_path("raketasks", "README")
   end
 
   step 'I should see "Rake Tasks" page markdown rendered' do
