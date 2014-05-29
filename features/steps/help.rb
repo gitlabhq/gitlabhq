0879babfd1224626d0523a66caca0cac44ee9ee8
Marin Jankovski <marin@gitlab.com>
2014-05-29 10:13:30 +0200

    Fix feature.

--- features/steps/help.rb
+++ features/steps/help.rb
@@ -8,14 +8,14 @@ class Spinach::Features::Help < Spinach::FeatureSteps
   end
 
   step 'I visit the "Rake Tasks" help page' do
-    visit help_page_path("raketasks", "README")
+    visit help_page_path("raketasks", "maintenance")
   end
 
   step 'I should see "Rake Tasks" page markdown rendered' do
-    page.should have_content "GitLab provides some specific rake tasks to enable special features or perform maintenance tasks"
+    page.should have_content "Gather information about GitLab and the system it runs on"
   end
 
   step 'Header "Rebuild project satellites" should have correct ids and links' do
-    header_should_have_correct_id_and_link(3, '(Re-)Create satellite repos', 're-create-satellite-repos')
+    header_should_have_correct_id_and_link(3, '(Re-)Create satellite repos', 're-create-satellite-repos', '.documentation')
   end
 end
