55ab7a2a958c99332f71d8b0e94ccae2b5b54c13
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2014-06-03 14:49:50 +0300

    Remove color-named css classes
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,13 +1,13 @@
 module NotificationsHelper
   def notification_icon(notification)
     if notification.disabled?
-      content_tag :i, nil, class: 'icon-volume-off cred'
+      content_tag :i, nil, class: 'icon-volume-off ns-mute'
     elsif notification.participating?
-      content_tag :i, nil, class: 'icon-volume-down cblue'
+      content_tag :i, nil, class: 'icon-volume-down ns-part'
     elsif notification.watch?
-      content_tag :i, nil, class: 'icon-volume-up cgreen'
+      content_tag :i, nil, class: 'icon-volume-up ns-watch'
     else
-      content_tag :i, nil, class: 'icon-circle-blank cblue'
+      content_tag :i, nil, class: 'icon-circle-blank ns-default'
     end
   end
 end
