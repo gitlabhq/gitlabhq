2838a993da78bf365d7a84dc51f57a605fc99c96
Jeroen van Baarsen <jeroenvanbaarsen@gmail.com>
2014-06-07 10:09:56 +0200

    DB cleaner reverted to a known working version
    Signed-off-by: Jeroen van Baarsen <jeroenvanbaarsen@gmail.com>

--- spec/support/db_cleaner.rb
+++ spec/support/db_cleaner.rb
@@ -1,18 +1,39 @@
-RSpec.configure do |config|
+# RSpec.configure do |config|
 
-  config.around(:each) do |example|
-    DatabaseCleaner.strategy = :transaction
+#   config.around(:each) do |example|
+#     DatabaseCleaner.strategy = :transaction
+#     DatabaseCleaner.clean_with(:truncation)
+#     DatabaseCleaner.cleaning do
+#       example.run
+#     end
+#   end
+
+#   config.around(:each, js: true) do |example|
+#     DatabaseCleaner.strategy = :truncation
+#     DatabaseCleaner.clean_with(:truncation)
+#     DatabaseCleaner.cleaning do
+#       example.run
+#     end
+#   end
+# end
+RSpec.configure do |config|
+  config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
-    DatabaseCleaner.cleaning do
-      example.run
-    end
   end
 
-  config.around(:each, js: true) do |example|
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, :js => true) do
     DatabaseCleaner.strategy = :truncation
-    DatabaseCleaner.clean_with(:truncation)
-    DatabaseCleaner.cleaning do
-      example.run
-    end
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
   end
 end
