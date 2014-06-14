6a7990c007f2349f3093aef3e70eaa8c092033c8
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2014-06-14 15:51:07 +0300

    Remove garbage and apply wiki style
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -2,17 +2,6 @@ class HelpController < ApplicationController
   def index
   end
 
-  def api
-    @category = params[:category]
-    @category = "README" if @category.blank?
-
-    if File.exists?(Rails.root.join('doc', 'api', @category + '.md'))
-      render 'api'
-    else
-      not_found!
-    end
-  end
-
   def show
     @category = params[:category]
     @file = params[:file]
