61a403ff22db19c8482bbd701b6222a1e4b4eb99
Paweł Świątkowski <pawel.swiatkowski@puzzleflow.com>
2014-06-17 14:13:01 +0200

    ensure that uid is a string

--- lib/gitlab/oauth/user.rb
+++ lib/gitlab/oauth/user.rb
@@ -61,7 +61,9 @@ module Gitlab
         end
 
         def uid
-          auth.info.uid || auth.uid
+          uid = auth.info.uid || auth.uid
+          uid = uid.to_s unless uid.nil?
+          uid
         end
 
         def email
