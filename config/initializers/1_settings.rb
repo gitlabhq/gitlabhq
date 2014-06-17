7be80fd89954a248527ca9be4bb9d9c320390811
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2014-06-17 09:27:50 +0300

    Enable identicon for gravatar by default
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- config/initializers/1_settings.rb
+++ config/initializers/1_settings.rb
@@ -104,8 +104,8 @@ Settings.gitlab['repository_downloads_path'] = File.absolute_path(Settings.gitla
 #
 Settings['gravatar'] ||= Settingslogic.new({})
 Settings.gravatar['enabled']      = true if Settings.gravatar['enabled'].nil?
-Settings.gravatar['plain_url']  ||= 'http://www.gravatar.com/avatar/%{hash}?s=%{size}&d=mm'
-Settings.gravatar['ssl_url']    ||= 'https://secure.gravatar.com/avatar/%{hash}?s=%{size}&d=mm'
+Settings.gravatar['plain_url']  ||= 'http://www.gravatar.com/avatar/%{hash}?s=%{size}&d=identicon'
+Settings.gravatar['ssl_url']    ||= 'https://secure.gravatar.com/avatar/%{hash}?s=%{size}&d=identicon'
 
 #
 # GitLab Shell
