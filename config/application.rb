a90a201d0ee3c9e33cc04075ac8a2509c1d2acc9
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2014-06-17 22:32:35 +0300

    Remove user observer

--- config/application.rb
+++ config/application.rb
@@ -19,8 +19,7 @@ module Gitlab
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
 
     # Activate observers that should always be running.
-    config.active_record.observers = :user_observer,
-                                     :users_project_observer
+    config.active_record.observers = :users_project_observer
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
