a8fb783a7074f4ce327c41d9dd33b51d21b1fd65
Boris HUISGEN <bhuisgen@hbis.fr>
2014-06-18 20:42:23 +0200

    Fix LDAP TLS authentication

--- lib/gitlab/ldap/adapter.rb
+++ lib/gitlab/ldap/adapter.rb
@@ -14,14 +14,14 @@ module Gitlab
       end
 
       def self.adapter_options
-	case config['method'].to_s
-		when 'ssl'
-			encryption = :simple_tls
-		when 'tls'
-			encryption = :start_tls
-		else
-			encryption = nil
-	end
+        case config['method'].to_s
+        when 'ssl'
+          encryption = :simple_tls
+        when 'tls'
+          encryption = :start_tls
+        else
+          encryption = nil
+        end
 
         options = {
           host: config['host'],
