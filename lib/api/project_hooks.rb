a0aa0bb6b5e1f95f51d5fb3c9073a5c0dd9a9414
Mathew Winstone <mathew.winstone@gmail.com>
2014-06-20 15:53:15 -0400

    Added tag_push_events to project hooks api

--- lib/api/project_hooks.rb
+++ lib/api/project_hooks.rb
@@ -38,7 +38,7 @@ module API
       #   POST /projects/:id/hooks
       post ":id/hooks" do
         required_attributes! [:url]
-        attrs = attributes_for_keys [:url, :push_events, :issues_events, :merge_requests_events]
+        attrs = attributes_for_keys [:url, :push_events, :issues_events, :merge_requests_events, :tag_push_events]
         @hook = user_project.hooks.new(attrs)
 
         if @hook.save
@@ -62,7 +62,7 @@ module API
       put ":id/hooks/:hook_id" do
         @hook = user_project.hooks.find(params[:hook_id])
         required_attributes! [:url]
-        attrs = attributes_for_keys [:url, :push_events, :issues_events, :merge_requests_events]
+        attrs = attributes_for_keys [:url, :push_events, :issues_events, :merge_requests_events, :tag_push_events]
 
         if @hook.update_attributes attrs
           present @hook, with: Entities::ProjectHook
