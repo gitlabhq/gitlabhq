f40e87a03bdaaafa72e5afede57581d6c54dc506
Marin Jankovski <marin@gitlab.com>
2014-06-24 12:57:41 +0200

    Return better error when account exists when attempting oauth account create.

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -45,14 +45,19 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
       # Create user if does not exist
       # and allow_single_sign_on is true
-      if Gitlab.config.omniauth['allow_single_sign_on']
-        @user ||= Gitlab::OAuth::User.create(oauth)
+      if Gitlab.config.omniauth['allow_single_sign_on'] && !@user
+        @user, errors = Gitlab::OAuth::User.create(oauth)
       end
 
-      if @user
+      if @user && !errors
         sign_in_and_redirect(@user)
       else
-        flash[:notice] = "There's no such user!"
+        if errors
+          error_message = errors.map{ |attribute, message| "#{attribute} #{message}" }.join(", ")
+          flash[:notice] = "There was a problem creating your account. #{error_message}"
+        else
+          flash[:notice] = "There's no such user!"
+        end
         redirect_to new_user_session_path
       end
     end
