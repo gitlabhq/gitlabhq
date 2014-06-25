e1afea777a6f0d2e28adc2765c9445cb10bd25f8
Marin Jankovski <marin@gitlab.com>
2014-06-25 13:30:23 +0200

    Show title of milestone.

--- app/controllers/groups/milestones_controller.rb
+++ app/controllers/groups/milestones_controller.rb
@@ -3,5 +3,8 @@ class Groups::MilestonesController < ApplicationController
 
   def index
     @group = Group.find_by(path: params[:group_id])
+    project_ids = @group.projects
+    project_milestones = Milestone.where(project_id: project_ids)
+    @milestones = project_milestones
   end
 end
