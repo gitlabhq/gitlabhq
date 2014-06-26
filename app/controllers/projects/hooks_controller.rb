2acde87e0d223bbc3ecd15777b9a1048d6bc5172
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2014-06-26 18:51:11 +0300

    Project hook, milestone, snippet strong params
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- app/controllers/projects/hooks_controller.rb
+++ app/controllers/projects/hooks_controller.rb
@@ -42,6 +42,6 @@ class Projects::HooksController < Projects::ApplicationController
   end
 
   def hook_params
-    params.require(:hook).permit(:url)
+    params.require(:hook).permit(:url, :push_events, :issues_events, :merge_requests_events, :tag_push_events)
   end
 end
