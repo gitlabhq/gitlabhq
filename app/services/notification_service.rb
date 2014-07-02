bc7656a306552c3308082ddf4695addac539ec6d
Marin Jankovski <marin@gitlab.com>
2014-07-02 16:38:03 +0200

    Do not notify person who merged the MR.

--- app/services/notification_service.rb
+++ app/services/notification_service.rb
@@ -88,7 +88,10 @@ class NotificationService
   #
   def merge_mr(merge_request, current_user)
     recipients = reject_muted_users([merge_request.author, merge_request.assignee], merge_request.target_project)
+
     recipients = recipients.concat(project_watchers(merge_request.target_project)).uniq
+
+    recipients.delete(merge_request.merge_event.author) if merge_request.merged?
 
     recipients.each do |recipient|
       mailer.merged_merge_request_email(recipient.id, merge_request.id, current_user.id)
