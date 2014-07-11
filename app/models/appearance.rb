4ff1fc35d4edf182b78707bb7dbbe06c8117071b
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2014-07-11 14:38:56 +0300

    Remove attr_accessible from EE only models
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -1,6 +1,4 @@
 class Appearance < ActiveRecord::Base
-  attr_accessible :title, :description, :logo
-
   validates :title, presence: true
   validates :description, presence: true
   validates :logo, file_size: { maximum: 1000.kilobytes.to_i }
--- app/models/git_hook.rb
+++ app/models/git_hook.rb
@@ -1,6 +1,4 @@
 class GitHook < ActiveRecord::Base
-  attr_accessible :deny_delete_tag, :delete_branch_regex, :commit_message_regex, :force_push_regex
-
   belongs_to :project
   validates :project, presence: true
 
