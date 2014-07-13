62d2aed036ce0699eb494fb629983a5809f746a5
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2014-07-13 20:56:29 +0300

    Improve login page behaviour
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- app/helpers/appearances_helper.rb
+++ app/helpers/appearances_helper.rb
@@ -1,23 +1,17 @@
 module AppearancesHelper
+  def brand_item
+    nil
+  end
+
   def brand_title
     'GitLab Community Edition'
   end
 
   def brand_image
-    image_tag 'brand_logo.png'
+    nil
   end
 
   def brand_text
-    default_text =<<eos
-### GitLab is open source software to collaborate on code.
-
-Manage git repositories with fine grained access controls that keep your code secure.
-Perform code reviews and enhance collaboration with merge requests.
-Each project can also have an issue tracker and a wiki.
-
-Used by more than 100,000 organizations, GitLab is the most popular solution to manage git repositories on-premises.
-Read more about GitLab at [about.gitlab.com](https://about.gitlab.com/).
-eos
-    markdown default_text
+    nil
   end
 end
