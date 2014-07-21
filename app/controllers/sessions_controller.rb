9714061bb0f099f3800ffdc75bc8807ee783e32a
Marin Jankovski <marin@gitlab.com>
2014-07-21 15:45:27 +0200

    If the referer is not set, use fullpath.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,14 @@ class SessionsController < Devise::SessionsController
 
   def new
     if request.referer.present?
-      store_location_for(:redirect, URI(request.referer).path)
+      referer_uri = URI(request.referer)
+      if referer_uri.host == Gitlab.config.gitlab.host
+        store_location_for(:redirect, referer_uri.path)
+      else
+        store_location_for(:redirect, request.fullpath)
+      end
+    else
+      store_location_for(:redirect, request.fullpath)
     end
 
     super
