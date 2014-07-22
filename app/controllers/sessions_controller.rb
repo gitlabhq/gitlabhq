b4217500352efcc463ee0517a97d37c41e66ddd5
Marin Jankovski <marin@gitlab.com>
2014-07-22 08:34:16 +0200

    Call store_location_for once.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,18 @@
 class SessionsController < Devise::SessionsController
 
   def new
-    if request.referer.present?
-      referer_uri = URI(request.referer)
-      if referer_uri.host == Gitlab.config.gitlab.host
-        store_location_for(:redirect, referer_uri.path)
-      else
-        store_location_for(:redirect, request.fullpath)
-      end
-    else
-      store_location_for(:redirect, request.fullpath)
-    end
+    redirect_url = if request.referer.present?
+                     referer_uri = URI(request.referer)
+                     if referer_uri.host == Gitlab.config.gitlab.host
+                       referer_uri.path
+                     else
+                       request.fullpath
+                     end
+                   else
+                     request.fullpath
+                   end
+
+    store_location_for(:redirect, redirect_url)
 
     super
   end
