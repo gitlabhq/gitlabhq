03654a6abf47c88b8b980a6707874ff78080d2fe
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2014-07-29 17:10:15 +0300

    Label and LabelLink models for implementing own GitLab labels
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

---
+++ app/models/label.rb
@@ -0,0 +1,7 @@
+class Label < ActiveRecord::Base
+  belongs_to :project
+  has_many :label_links, dependent: :destroy
+
+  validates :color, format: { with: /\A\#[0-9A-Fa-f]{3}{1,2}+\Z/ }, allow_blank: true
+  validates :project, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140625115202) do
+ActiveRecord::Schema.define(version: 20140729140420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -108,6 +108,22 @@ ActiveRecord::Schema.define(version: 20140625115202) do
   end
 
   add_index "keys", ["user_id"], name: "index_keys_on_user_id", using: :btree
+
+  create_table "label_links", force: true do |t|
+    t.integer  "label_id"
+    t.integer  "target_id"
+    t.string   "target_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "labels", force: true do |t|
+    t.string   "title"
+    t.string   "color"
+    t.integer  "project_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "merge_request_diffs", force: true do |t|
     t.string   "state"
