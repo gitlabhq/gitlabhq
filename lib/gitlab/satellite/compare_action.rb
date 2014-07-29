bf9ce1f4cf5592f7aa1f761a5b394e7d500dc034
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2014-07-29 12:11:16 +0300

    Refactor compare logic for MR. Use satellites only for forks for better performance
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- lib/gitlab/satellite/compare_action.rb
+++ lib/gitlab/satellite/compare_action.rb
@@ -10,28 +10,16 @@ module Gitlab
         @source_project, @source_branch = source_project, source_branch
       end
 
-      # Only show what is new in the source branch compared to the target branch, not the other way around.
-      # The line below with merge_base is equivalent to diff with three dots (git diff branch1...branch2)
-      # From the git documentation: "git diff A...B" is equivalent to "git diff $(git-merge-base A B) B"
-      def diffs
+      # Compare 2 repositories and return Gitlab::CompareResult object
+      def result
         in_locked_and_timed_satellite do |target_repo|
           prepare_satellite!(target_repo)
           update_satellite_source_and_target!(target_repo)
-          compare(target_repo).diffs
+
+          Gitlab::CompareResult.new(compare(target_repo))
         end
       rescue Grit::Git::CommandFailed => ex
         raise BranchesWithoutParent
-      end
-
-      # Retrieve an array of commits between the source and the target
-      def commits
-        in_locked_and_timed_satellite do |target_repo|
-          prepare_satellite!(target_repo)
-          update_satellite_source_and_target!(target_repo)
-          compare(target_repo).commits
-        end
-      rescue Grit::Git::CommandFailed => ex
-        handle_exception(ex)
       end
 
       private
