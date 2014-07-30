ecabefba1906526bf4038fa8c642688f93dbeb3e
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2014-07-30 13:26:54 +0300

    More strict validation for label title
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- app/models/label.rb
+++ app/models/label.rb
@@ -2,9 +2,11 @@ class Label < ActiveRecord::Base
   belongs_to :project
   has_many :label_links, dependent: :destroy
 
-  validates :color, format: { with: /\A\#[0-9A-Fa-f]{3}{1,2}+\Z/ }, allow_blank: true
+  validates :color, format: { with: /\A\#[0-9A-Fa-f]{6}+\Z/ }, allow_blank: true
   validates :project, presence: true
-  validates :title, presence: true
+
+  # Dont allow '?', '&', and ',' for label titles
+  validates :title, presence: true, format: { with: /\A[^&\?,&]*\z/ }
 
   def name
     title
