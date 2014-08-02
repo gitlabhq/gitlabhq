a54735613324cd0501e90c4744a698c46cfdc128
Walter Kreilinger <wk@linon.de>
2014-08-02 03:54:39 +0200

    history for files follows after rename

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -36,6 +36,7 @@ class Repository
       path: path,
       limit: limit,
       offset: offset,
+      follow: path.present?,
     )
     commits = Commit.decorate(commits) if commits.present?
     commits
