3b98830f7ec3c99aa16bde73e43670b9379843c0
Jan-Willem van der Meer <mail@jewilmeer.nl>
2014-08-08 17:13:04 +0200

    Give Admin email an interface

--- app/controllers/admin/emails_controller.rb
+++ app/controllers/admin/emails_controller.rb
@@ -1,5 +1,4 @@
 class Admin::EmailsController < Admin::ApplicationController
   def show
-    render text: 'hello world'
   end
 end
---
+++ app/helpers/admin_email_helper.rb
@@ -0,0 +1,19 @@
+module AdminEmailHelper
+  def admin_email_grouped_recipient_options
+    options_for_select([['Everyone', 'all']]) +
+    grouped_options_for_select(
+      'Groups' => Group.pluck(:name, :id).map{ |name, id| [name, "group_#{id}"] },
+      'Projects' => grouped_project_list
+    )
+  end
+
+  protected
+  def grouped_project_list
+    Group.includes(:projects).flat_map do |group|
+      group.human_name
+      group.projects.map do |project|
+        ["#{group.human_name} / #{project.name}", "project_#{project.id}"]
+      end
+    end
+  end
+end
