0f1351adbe44cdc3ffa62b94d9b6533a060cf14c
Jan-Willem van der Meer <mail@jewilmeer.nl>
2014-08-11 13:23:12 +0200

    Add unsubscribe link

--- app/mailers/emails/admin_notification.rb
+++ app/mailers/emails/admin_notification.rb
@@ -1,8 +1,10 @@
 module Emails
   module AdminNotification
     def send_admin_notification(user_id, subject, body)
+      email = recipient(user_id)
+      @unsubscribe_url = public_unsubscribe_url(email: email)
       @body = body
-      mail to: recipient(user_id), subject: subject
+      mail to: email, subject: subject
     end
   end
 end
