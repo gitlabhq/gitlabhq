6d785abaa9367173d62300b8000a68554e4e293c
Jacob Vosmaer <contact@jacobvosmaer.nl>
2014-08-11 18:26:20 +0200

    Rename resque_url to redis_url_string

--- config/initializers/7_cache_settings.rb
+++ config/initializers/7_cache_settings.rb
@@ -1,14 +1,14 @@
 redis_config_file = Rails.root.join('config', 'resque.yml')
 
-resque_url = if File.exists?(redis_config_file)
-               YAML.load_file(redis_config_file)[Rails.env]
-             else
-               "redis://localhost:6379"
-             end
+redis_url_string = if File.exists?(redis_config_file)
+                     YAML.load_file(redis_config_file)[Rails.env]
+                   else
+                     "redis://localhost:6379"
+                   end
 
 # Redis::Store does not handle Unix sockets well, so let's do it for them
-redis_config_hash = Redis::Store::Factory.extract_host_options_from_uri(resque_url)
-redis_uri = URI.parse(resque_url)
+redis_config_hash = Redis::Store::Factory.extract_host_options_from_uri(redis_url_string)
+redis_uri = URI.parse(redis_url_string)
 if redis_uri.scheme == 'unix'
   redis_config_hash[:path] = redis_uri.path
 end
