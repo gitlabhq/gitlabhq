2804902cfce28859fbed96e1d9de1d12964800ee
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2014-08-11 17:55:31 +0300

    Improve developer seeds
    * Improt projects inline so after seeds you have repos
    * Fix merge request seeds
    * Add comment seeds to both issues and merge requests
    * Remove some projects from seeds to increase speed
    
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- db/fixtures/development/09_issues.rb
+++ db/fixtures/development/09_issues.rb
@@ -1,32 +1,16 @@
 Gitlab::Seeder.quiet do
-  (1..300).each  do |i|
-    # Random Project
-    project = Project.all.sample
-
-    # Random user
-    user = project.team.users.sample
-
-    next unless user
-
-    user_id = user.id
-
-    Gitlab::Seeder.by_user(user) do
-      Issue.seed(:id, [{
-        id: i,
-        project_id: project.id,
-        author_id: user_id,
-        assignee_id: user_id,
+  Project.all.each do |project|
+    (1..10).each  do |i|
+      issue_params = {
+        title: Faker::Lorem.sentence(6),
+        description: Faker::Lorem.sentence,
         state: ['opened', 'closed'].sample,
         milestone: project.milestones.sample,
-        title: Faker::Lorem.sentence(6),
-        description: Faker::Lorem.sentence
-      }])
-    end
-    print('.')
-  end
+        assignee: project.team.users.sample
+      }
 
-  Issue.all.map do |issue|
-    issue.set_iid
-    issue.save
+      Issues::CreateService.new(project, project.team.users.sample, issue_params).execute
+      print '.'
+    end
   end
 end
