1dd995a763490cefdc411efeb6972eaf48e57089
Jan-Willem van der Meer <mail@jewilmeer.nl>
2014-08-12 17:56:29 +0200

    Move tests as well, remove public namespace from controller

--- app/controllers/unsubscribes_controller.rb
+++ app/controllers/unsubscribes_controller.rb
@@ -1,24 +1,22 @@
-module Public
-  class UnsubscribesController < ApplicationController
-    skip_before_filter :authenticate_user!,
-                       :reject_blocked, :set_current_user_for_observers,
-                       :add_abilities
-    layout 'public_users'
+class UnsubscribesController < ApplicationController
+  skip_before_filter :authenticate_user!,
+                     :reject_blocked, :set_current_user_for_observers,
+                     :add_abilities
+  layout 'public_users'
 
-    def show
-      @user = get_user
-    end
+  def show
+    @user = get_user
+  end
 
-    def create
-      @user = get_user
-      @user.admin_unsubscribe! if @user
-      redirect_to new_user_session_path, notice: 'You have been unsubscribed'
-    end
+  def create
+    @user = get_user
+    @user.admin_unsubscribe! if @user
+    redirect_to new_user_session_path, notice: 'You have been unsubscribed'
+  end
 
-    protected
-    def get_user
-      @email = "#{params[:email]}.#{params[:format]}"
-      User.where(email: @email).first
-    end
+  protected
+  def get_user
+    @email = "#{params[:email]}.#{params[:format]}"
+    User.where(email: @email).first
   end
 end
--- spec/controllers/unsubscribes_controller_spec.rb
+++ spec/controllers/unsubscribes_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe Public::UnsubscribesController do
+describe UnsubscribesController do
   let!(:user) { create :user, email: 'me@example.com' }
 
   describe "show" do
