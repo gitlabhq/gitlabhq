7efb958c3359be5e3c3dd0ee0f6f213b129c2664
Jan-Willem van der Meer <mail@jewilmeer.nl>
2014-08-14 12:54:40 +0200

    Ensure data is complete

--- app/models/ldap_group_link.rb
+++ app/models/ldap_group_link.rb
@@ -2,6 +2,7 @@ class LdapGroupLink < ActiveRecord::Base
   include Gitlab::Access
   belongs_to :group
 
+  validates :cn, :group_access, :group_id, presence: true
   validates :cn, uniqueness: { scope: :group_id }
 
   def access_field
