234ae2e9ef5af581a5d914af13cc5a0661604b0d
Jan-Willem van der Meer <mail@jewilmeer.nl>
2014-08-14 12:00:48 +0200

    Add Gitlab::Access properties to LdapGroupLink

--- app/models/ldap_group_link.rb
+++ app/models/ldap_group_link.rb
@@ -1,5 +1,10 @@
 class LdapGroupLink < ActiveRecord::Base
+  include Gitlab::Access
   belongs_to :group
 
   validates :cn, uniqueness: { scope: :group_id }
+
+  def access_field
+    group_access
+  end
 end
