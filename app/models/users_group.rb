6c424a2a6f4b98d5b39ad310e62561f4d9d3958c
Jan-Willem van der Meer <mail@jewilmeer.nl>
2014-08-15 19:48:34 +0200

    Refactor LdapResetService to reflect multiple LDAP groups

--- app/models/users_group.rb
+++ app/models/users_group.rb
@@ -27,6 +27,8 @@ class UsersGroup < ActiveRecord::Base
   scope :developers, -> { where(group_access: DEVELOPER) }
   scope :masters,  -> { where(group_access: MASTER) }
   scope :owners,  -> { where(group_access: OWNER) }
+  scope :with_ldap_dn, -> { references(:user).includes(:user).
+    where(users: { provider: 'ldap' }) }
 
   scope :with_group, ->(group) { where(group_id: group.id) }
   scope :with_user, ->(user) { where(user_id: user.id) }
