9ba563f357774c3f66a228f4497621e9e42bf6cc
Marin Jankovski <marin@gitlab.com>
2014-08-22 10:51:33 +0200

    Use base 64 encoding for email params.

--- app/mailers/emails/admin_notification.rb
+++ app/mailers/emails/admin_notification.rb
@@ -2,7 +2,7 @@ module Emails
   module AdminNotification
     def send_admin_notification(user_id, subject, body)
       email = recipient(user_id)
-      @unsubscribe_url = unsubscribe_url(email: CGI.escape(email))
+      @unsubscribe_url = unsubscribe_url(email: Base64.urlsafe_encode64(email))
       @body = body
       mail to: email, subject: subject
     end
