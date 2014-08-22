b5342de57b869a4e55d395a27f76ec393cab799b
Marin Jankovski <marin@gitlab.com>
2014-08-22 10:45:45 +0200

    Add admin notification unsubscribe feature

--- features/steps/user.rb
+++ features/steps/user.rb
@@ -7,4 +7,20 @@ class Spinach::Features::User < Spinach::FeatureSteps
   step 'I should see user "John Doe" page' do
     expect(page.title).to match(/^\s*John Doe/)
   end
+
+  step 'I visit unsubscribe link' do
+    visit "/unsubscribes/joh@doe.org"
+  end
+
+  step 'I should see unsubscribe text and button' do
+    page.should have_content "Unsubscribe from Admin notifications Yes, I want to unsubscribe joh@doe.org from any further admin emails."
+  end
+
+  step 'I press the unsubscribe button' do
+    click_button("Unsubscribe")
+  end
+
+  step 'I should be unsubscribed' do
+    current_path.should == root_path
+  end
 end
