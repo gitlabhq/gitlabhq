f019821dbff2cdb64670aee77716bbff7dacec6c
Jan-Willem van der Meer <mail@jewilmeer.nl>
2014-09-03 10:18:20 +0200

    In between patches fixup

--- spec/lib/gitlab/ldap/user_spec.rb
+++ spec/lib/gitlab/ldap/user_spec.rb
@@ -31,18 +31,6 @@ describe Gitlab::LDAP::User do
       gl_auth.find_or_create(@auth)
     end
 
-    it "should update credentials by username if missing uid and Gitlab.config.ldap.allow_username_or_email_login is true" do
-      user = double('User')
-      value = Gitlab.config.ldap.allow_username_or_email_login
-      Gitlab.config.ldap['allow_username_or_email_login'] = true
-      User.stub find_by_extern_uid_and_provider: nil
-      User.stub(:find_by).with(hash_including(email: anything())) { nil }
-      User.stub(:find_by).with(hash_including(username: anything())) { user }
-      user.should_receive :update_attributes
-      gl_auth.find_or_create(@auth)
-      Gitlab.config.ldap['allow_username_or_email_login'] = value
-    end
-
     it "should not update credentials by username if missing uid and Gitlab.config.ldap.allow_username_or_email_login is false" do
       user = double('User')
       value = Gitlab.config.ldap.allow_username_or_email_login
