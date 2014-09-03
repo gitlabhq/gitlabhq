0a7dea29aadb737f778830096515b10dc3dd1dd7
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2014-09-03 09:06:16 +0300

    /api/allowed use POST now
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- lib/api/internal.rb
+++ lib/api/internal.rb
@@ -12,7 +12,7 @@ module API
       #   ref - branch name
       #   forced_push - forced_push
       #
-      get "/allowed" do
+      post "/allowed" do
         # Check for *.wiki repositories.
         # Strip out the .wiki from the pathname before finding the
         # project. This applies the correct project permissions to
