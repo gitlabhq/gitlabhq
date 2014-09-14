e9b32005ebaa432f0fb40d3c87f9d0bf9b109db9
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2014-09-14 18:09:23 +0300

    Migrate old membership objects to new Member model
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140914113604) do
+ActiveRecord::Schema.define(version: 20140914145549) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
