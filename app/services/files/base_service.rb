0139be76bda551262f5c07687cb214bb4cf56ed3
Ciro Santilli <ciro.santilli@gmail.com>
2014-09-25 23:54:56 +0200

    Factor services with base service.

--- app/services/files/base_service.rb
+++ app/services/files/base_service.rb
@@ -10,6 +10,45 @@ module Files
 
     private
 
+    def permission_check
+      allowed = if project.protected_branch?(ref)
+                  can?(current_user, :push_code_to_protected_branches, project)
+                else
+                  can?(current_user, :push_code, project)
+                end
+
+      unless allowed
+        return error('You are not allowed to create file in this branch')
+      end
+
+      unless repository.branch_names.include?(ref)
+        return error('You can only create files if you are on top of a branch')
+      end
+
+      nil
+    end
+
+    def text_check
+      unless blob
+        return error('You can only edit text files')
+      end
+
+      nil
+    end
+
+    def get_execute_output(ok, msg = '')
+      if ok
+        success
+      else
+        error('Your changes could not be committed, '\
+              'maybe the file was changed by another process' + msg)
+      end
+    end
+
+    def blob
+      repository.blob_at_branch(ref, path)
+    end
+
     def repository
       project.repository
     end
--- app/services/files/delete_service.rb
+++ app/services/files/delete_service.rb
@@ -3,38 +3,19 @@ require_relative "base_service"
 module Files
   class DeleteService < BaseService
     def execute
-      allowed = if project.protected_branch?(ref)
-                  can?(current_user, :push_code_to_protected_branches, project)
-                else
-                  can?(current_user, :push_code, project)
-                end
+      error = permission_check
+      return error if error
 
-      unless allowed
-        return error("You are not allowed to push into this branch")
-      end
-
-      unless repository.branch_names.include?(ref)
-        return error("You can only create files if you are on top of a branch")
-      end
-
-      blob = repository.blob_at_branch(ref, path)
-
-      unless blob
-        return error("You can only edit text files")
-      end
+      error = text_check
+      return error if error
 
       delete_file_action = Gitlab::Satellite::DeleteFileAction.new(current_user, project, ref, path)
-
       deleted_successfully = delete_file_action.commit!(
         nil,
         params[:commit_message]
       )
 
-      if deleted_successfully
-        success
-      else
-        error("Your changes could not be committed, because the file has been changed")
-      end
+      get_execute_output(deleted_successfully)
     end
   end
 end
