3a8cd1fa59dc9d3ab0b00e0f01276e73b35a996c
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2014-09-29 12:05:17 +0300

    Refactor commits graph
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- app/controllers/projects/graphs_controller.rb
+++ app/controllers/projects/graphs_controller.rb
@@ -15,33 +15,10 @@ class Projects::GraphsController < Projects::ApplicationController
 
   def commits
     @commits = @project.repository.commits(nil, nil, 2000, 0, true)
-    @start_date = @commits.last.committed_date.to_date
-    @end_date = @commits.first.committed_date.to_date
-    @duration = (@end_date - @start_date).to_i
-    @authors = @commits.map(&:author_email).uniq.size
-    @commit_per_day = (@commits.size.to_f / @duration).round(1)
-
-    @commits_per_week_days = {}
-    Date::DAYNAMES.each { |day| @commits_per_week_days[day] = 0 }
-
-    @commits_per_time = {}
-    (0..23).to_a.each { |hour| @commits_per_time[hour] = 0 }
-
-    @commits_per_month = {}
-    (1..31).to_a.each { |day| @commits_per_month[day] = 0 }
-
-    @commits.each do |commit|
-      hour = commit.committed_date.strftime('%k').to_i
-      day_of_month = commit.committed_date.strftime('%e').to_i
-      weekday = commit.committed_date.strftime('%A')
-
-      @commits_per_week_days[weekday] ||= 0
-      @commits_per_week_days[weekday] += 1
-      @commits_per_time[hour] ||= 0
-      @commits_per_time[hour] += 1
-      @commits_per_month[day_of_month] ||= 0
-      @commits_per_month[day_of_month] += 1
-    end
+    @commits_graph = Gitlab::Graphs::Commits.new(@commits)
+    @commits_per_week_days = @commits_graph.commits_per_week_days
+    @commits_per_time = @commits_graph.commits_per_time
+    @commits_per_month = @commits_graph.commits_per_month
   end
 
   private
