b9985ed0fc4d64ce050524e341a276e96d0ebf50
Sullivan SENECHAL <soullivaneuh@gmail.com>
2014-10-04 17:48:00 +0200

    Backup compression option

--- lib/backup/manager.rb
+++ lib/backup/manager.rb
@@ -20,13 +20,42 @@ module Backup
       # create archive
       print "Creating backup archive: #{tar_file} ... "
       if Kernel.system('tar', '-cf', tar_file, *BACKUP_CONTENTS)
-        puts "done".green
       else
         puts "failed".red
         abort 'Backup failed'
       end
+      puts 'done'.green
 
+      tar_file = compress(tar_file)
       upload(tar_file)
+    end
+
+    def compress(tar_file)
+      print "Compressing backup archive: #{tar_file} ... "
+
+      compression_extensions = {
+        gzip:     '.gz',
+        bzip2:    '.bz2',
+        xz:       '.xz'
+      }
+
+      compression_cmd = Gitlab.config.backup.compression[:command]
+      compression_lvl = Gitlab.config.backup.compression[:level]
+      if compression_cmd.blank?
+        puts 'skipped'.yellow
+        return tar_file
+      end
+
+      if Kernel.system(compression_cmd, compression_lvl.blank? ? '--' : "-#{compression_lvl}", tar_file)
+      else
+        puts 'failed'.red
+        abort 'Compression failed'
+      end
+      puts 'done'.green
+
+      # Return the new tar file name
+      compression_ext = compression_extensions[compression_cmd.to_sym]
+      "#{tar_file}#{compression_ext}"
     end
 
     def upload(tar_file)
@@ -67,11 +96,11 @@ module Backup
 
       if keep_time > 0
         removed = 0
-        file_list = Dir.glob(Rails.root.join(path, "*_gitlab_backup.tar"))
-        file_list.map! { |f| $1.to_i if f =~ /(\d+)_gitlab_backup.tar/ }
-        file_list.sort.each do |timestamp|
+        file_list = Dir.glob(Rails.root.join(path, '*_gitlab_backup.tar*'))
+        file_list.sort.each do |tar_file|
+          timestamp = $1.to_i if tar_file =~ /(\d+)_gitlab_backup.tar*/
           if Time.at(timestamp) < (Time.now - keep_time)
-            if Kernel.system(*%W(rm #{timestamp}_gitlab_backup.tar))
+            if Kernel.system('rm', tar_file)
               removed += 1
             end
           end
@@ -86,7 +115,7 @@ module Backup
       Dir.chdir(Gitlab.config.backup.path)
 
       # check for existing backups in the backup dir
-      file_list = Dir.glob("*_gitlab_backup.tar").each.map { |f| f.split(/_/).first.to_i }
+      file_list = Dir.glob('*_gitlab_backup.tar*').each.map { |f| f.split(/_/).first.to_i }
       puts "no backups found" if file_list.count == 0
       if file_list.count > 1 && ENV["BACKUP"].nil?
         puts "Found more than one backup, please specify which one you want to restore:"
@@ -96,17 +125,37 @@ module Backup
 
       tar_file = ENV["BACKUP"].nil? ? File.join("#{file_list.first}_gitlab_backup.tar") : File.join(ENV["BACKUP"] + "_gitlab_backup.tar")
 
+      was_compressed = false
+      ['.gz', '.bz2', '.xz'].each do |compress_ext|
+        compressed_file = "#{tar_file}#{compress_ext}"
+        if File.exists?(compressed_file)
+          uncompress compressed_file, compress_ext
+          was_compressed = true
+        end
+      end
+
+      puts tar_file
       unless File.exists?(tar_file)
         puts "The specified backup doesn't exist!"
         exit 1
       end
 
-      print "Unpacking backup ... "
+      print "Unpacking backup: #{tar_file} ... "
       unless Kernel.system(*%W(tar -xf #{tar_file}))
         puts "failed".red
         exit 1
       else
         puts "done".green
+      end
+
+      if was_compressed
+        print "Deleting uncompressed backup: #{tar_file} ... "
+        if Kernel.system('rm', '-f', tar_file)
+          puts 'done'.green
+        else
+          puts 'failed'.red
+          exit 1
+        end
       end
 
       settings = YAML.load_file("backup_information.yml")
@@ -120,6 +169,24 @@ module Backup
         puts "  version: #{settings[:gitlab_version]}".red
         puts
         puts "Hint: git checkout v#{settings[:gitlab_version]}"
+        exit 1
+      end
+    end
+
+    def uncompress(tar_file, compress_ext)
+      print "Uncompressing backup: #{tar_file} ... "
+
+      uncompression_commands = {
+        '.gz'     => 'gzip',
+        '.bz2'    => 'bzip2',
+        '.xz'     => 'xz'
+      }
+      uncompression_cmd = uncompression_commands[compress_ext]
+
+      if Kernel.system(uncompression_cmd, '-kd', tar_file)
+        puts 'done'.green
+      else
+        puts 'failed'.red
         exit 1
       end
     end
