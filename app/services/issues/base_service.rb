23b4a2e72118afeb691fa92891ca79cbc85b9c4f
Ciro Santilli <ciro.santilli@gmail.com>
2014-10-07 16:09:28 +0200

    Factor issue and merge request services

--- app/services/issues/base_service.rb
+++ app/services/issues/base_service.rb
@@ -1,21 +1,13 @@
 module Issues
-  class BaseService < ::BaseService
+  class BaseService < ::IssuableBaseService
 
     private
-
-    def create_assignee_note(issue)
-      Note.create_assignee_change_note(issue, issue.project, current_user, issue.assignee)
-    end
 
     def execute_hooks(issue, action = 'open')
       issue_data = issue.to_hook_data
       issue_url = Gitlab::UrlBuilder.new(:issue).build(issue.id)
       issue_data[:object_attributes].merge!(url: issue_url, action: action)
       issue.project.execute_hooks(issue_data, :issue_hooks)
-    end
-
-    def create_milestone_note(issue)
-      Note.create_milestone_change_note(issue, issue.project, current_user, issue.milestone)
     end
   end
 end
--- app/services/merge_requests/base_service.rb
+++ app/services/merge_requests/base_service.rb
@@ -1,11 +1,5 @@
 module MergeRequests
-  class BaseService < ::BaseService
-
-    private
-
-    def create_assignee_note(merge_request)
-      Note.create_assignee_change_note(merge_request, merge_request.project, current_user, merge_request.assignee)
-    end
+  class BaseService < ::IssuableBaseService
 
     def create_note(merge_request)
       Note.create_status_change_note(merge_request, merge_request.target_project, current_user, merge_request.state, nil)
@@ -15,10 +9,6 @@ module MergeRequests
       if merge_request.project
         merge_request.project.execute_hooks(merge_request.to_hook_data, :merge_request_hooks)
       end
-    end
-
-    def create_milestone_note(merge_request)
-      Note.create_milestone_change_note(merge_request, merge_request.project, current_user, merge_request.milestone)
     end
   end
 end
