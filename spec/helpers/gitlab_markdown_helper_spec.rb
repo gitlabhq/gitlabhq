a912308340ec70f13de98ae5116a8d71929a995f
Marin Jankovski <maxlazio@gmail.com>
2014-10-09 16:12:17 +0200

    Add a test for apostrophe in code blocks.

--- spec/helpers/gitlab_markdown_helper_spec.rb
+++ spec/helpers/gitlab_markdown_helper_spec.rb
@@ -539,6 +539,14 @@ describe GitlabMarkdownHelper do
       markdown(actual).should match(expected)
     end
 
+    it "should not link the apostrophe to issue 39 in code blocks" do
+      project.team << [user, :master]
+      project.issues.stub(:where).with(iid: '39').and_return([issue])
+
+      actual   = "Yes, `it is @#{member.user.username}'s task.`"
+      expected = /Yes, <code>it is @gfm\'s task.<\/code>/
+      markdown(actual).should match(expected)
+    end
 
     it "should handle references in <em>" do
       actual = "Apply _!#{merge_request.iid}_ ASAP"
