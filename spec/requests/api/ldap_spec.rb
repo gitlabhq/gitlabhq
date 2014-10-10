3702eb5443fe7300f99c704f947d2d15d0d12f5a
Jan-Willem van der Meer <mail@jewilmeer.nl>
2014-10-10 12:57:59 +0200

    Add new endpoint for specific ldap group listing

--- spec/requests/api/ldap_spec.rb
+++ spec/requests/api/ldap_spec.rb
@@ -25,8 +25,26 @@ describe API::API do
 
     context "when authenticated as user" do
       it "should return an array of ldap groups" do
-        pending('Needs refactoring')
         get api("/ldap/groups", user)
+        response.status.should == 200
+        json_response.should be_an Array
+        json_response.length.should == 2
+        json_response.first['cn'].should == 'developers'
+      end
+    end
+  end
+
+  describe "GET /ldap/ldapmain/groups" do
+    context "when unauthenticated" do
+      it "should return authentication error" do
+        get api("/ldap/ldapmain/groups")
+        response.status.should == 401
+      end
+    end
+
+    context "when authenticated as user" do
+      it "should return an array of ldap groups" do
+        get api("/ldap/ldapmain/groups", user)
         response.status.should == 200
         json_response.should be_an Array
         json_response.length.should == 2
