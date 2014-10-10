492398a6c6c86d84644df86e5b6bd2edf6c693a5
Jan-Willem van der Meer <mail@jewilmeer.nl>
2014-10-10 11:42:49 +0200

    Clearly distance ourselfs from AR methods

--- lib/gitlab/ldap/user.rb
+++ lib/gitlab/ldap/user.rb
@@ -54,7 +54,7 @@ module Gitlab
 
       def initialize(auth_hash)
         super
-        update_attributes
+        update_user_attributes
       end
 
       # instance methods
@@ -73,7 +73,7 @@ module Gitlab
         model.find_by(email: auth_hash.email)
       end
 
-      def update_attributes
+      def update_user_attributes
         gl_user.attributes = {
           extern_uid: auth_hash.uid,
           provider: auth_hash.provider,
