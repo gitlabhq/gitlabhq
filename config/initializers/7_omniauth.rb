103419be96f7bc9d8314a789d117b9516f33f2af
Jan-Willem van der Meer <mail@jewilmeer.nl>
2014-10-14 17:26:42 +0200

    Add ldap callback definitions to initializer

--- config/initializers/7_omniauth.rb
+++ config/initializers/7_omniauth.rb
@@ -4,3 +4,9 @@ module OmniAuth::Strategies
     const_set(server.provider_class, Class.new(LDAP))
   end
 end
+
+OmniauthCallbacksController.class_eval do
+  Gitlab.config.ldap.servers.each do |server|
+    alias_method server.provider_name, :ldap
+  end
+end
