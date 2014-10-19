47f157a535b63a3ed732d6256b4ce6ae9bfce696
Ciro Santilli <ciro.santilli@gmail.com>
2014-10-19 22:34:31 +0200

    Remove unnecessary authorize_user! filter
    from Project controllers. The filter is already implied by
    ApplicationController#project which is used as a before_filter.

--- app/controllers/projects/application_controller.rb
+++ app/controllers/projects/application_controller.rb
@@ -1,20 +1,8 @@
 class Projects::ApplicationController < ApplicationController
+  skip_before_filter :authenticate_user!
   before_filter :project
   before_filter :repository
   layout :determine_layout
-
-  def authenticate_user!
-    # Restrict access to Projects area only
-    # for non-signed users
-    if !current_user
-      id = params[:project_id] || params[:id]
-      @project = Project.find_with_namespace(id)
-
-      return if @project && @project.public?
-    end
-
-    super
-  end
 
   def determine_layout
     if current_user
