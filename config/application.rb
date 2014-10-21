3dbe0810f503ae51fc5e9310d753278c5b645b11
Ben Bodenmiller <bbodenmiller@hotmail.com>
2014-10-21 13:36:09 -0700

    cleanup time zone settings
    time zone settings moved to gitlab.yml in https://github.com/gitlabhq/gitlabhq/pull/8015

--- config/application.rb
+++ config/application.rb
@@ -23,11 +23,6 @@ module Gitlab
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # NOTE: Please prefer set time zone on config/gitlab.yml configuration file.
-    # config.time_zone = 'Central Time (US & Canada)'
-
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
