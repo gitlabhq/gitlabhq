8beca98d7357d704e1871f6ace27718db86c712c
Jacob Vosmaer <contact@jacobvosmaer.nl>
2014-10-21 09:55:13 +0200

    Fix CE-EE LDAP merge conflicts

--- config/initializers/7_omniauth.rb
+++ config/initializers/7_omniauth.rb
@@ -1,16 +1,10 @@
 if Gitlab::LDAP::Config.enabled?
   module OmniAuth::Strategies
-<<<<<<< HEAD
     Gitlab::LDAP::Config.servers.each do |server|
       # do not redeclare LDAP
       next if server['provider_name'] == 'ldap'
       const_set(server['provider_class'], Class.new(LDAP))
     end
-=======
-    server = Gitlab.config.ldap.servers.values.first
-    klass = server['provider_class']
-    const_set(klass, Class.new(LDAP)) unless klass == 'LDAP'
->>>>>>> 7-4-stable
   end
 
   OmniauthCallbacksController.class_eval do
