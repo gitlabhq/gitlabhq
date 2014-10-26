261e7ea5d4bbac4fc9ae2c058c70846a92d7f6b0
Sacred Seven <sacred.seven@yahoo.com>
2014-10-26 18:24:46 +0330

    JalaliCalendar in broadcast messages

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module Gitlab
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Tehran'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
