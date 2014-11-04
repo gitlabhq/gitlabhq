bc403356bb16da28a8c9cb486decf308036fd8f5
Don Luchini <dluchini@enernoc.com>
2014-11-04 10:02:38 -0500

    Do not require immediate password reset if specifying one when seeding database.

--- db/fixtures/production/001_admin.rb
+++ db/fixtures/production/001_admin.rb
@@ -1,8 +1,12 @@
-password = if ENV['GITLAB_ROOT_PASSWORD'].blank?
-             "5iveL!fe"
-           else
-             ENV['GITLAB_ROOT_PASSWORD']
-           end
+password = nil
+expire_time = nil
+if ENV['GITLAB_ROOT_PASSWORD'].blank?
+  password = '5iveL!fe'
+  expire_time = Time.now
+else
+  password = ENV['GITLAB_ROOT_PASSWORD']
+  expire_time = nil
+end
 
 admin = User.create(
   email: "admin@example.com",
@@ -10,7 +14,7 @@ admin = User.create(
   username: 'root',
   password: password,
   password_confirmation: password,
-  password_expires_at: Time.now,
+  password_expires_at: expire_time,
   theme_id: Gitlab::Theme::MARS
 
 )
