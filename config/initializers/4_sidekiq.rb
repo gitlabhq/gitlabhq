2ad0ded19443032c8e38ae125f445ca49c618686
Jacob Vosmaer <contact@jacobvosmaer.nl>
2014-12-01 18:58:37 +0100

    Disable Sidekiq arguments logging by default
    Conflicts:
    	config/initializers/4_sidekiq.rb

--- config/initializers/4_sidekiq.rb
+++ config/initializers/4_sidekiq.rb
@@ -15,7 +15,8 @@ Sidekiq.configure_server do |config|
 
   config.server_middleware do |chain|
     chain.add Gitlab::SidekiqMiddleware::ArgumentsLogger if ENV['SIDEKIQ_LOG_ARGUMENTS']
-    chain.add Gitlab::SidekiqMiddleware::MemoryKiller if ENV['SIDEKIQ_MEMORY_KILLER_MAX_RSS']
+    chain.add Gitlab::SidekiqMiddleware::MemoryKiller if ENV['SIDEKIQ_MAX_RSS']
+    Disable Sidekiq arguments logging by default
   end
 end
 
--- doc/development/README.md
+++ doc/development/README.md
@@ -6,3 +6,4 @@
 - [CI setup](ci_setup.md) for testing GitLab
 - [Sidekiq debugging](sidekiq_debugging.md)
 - [UI guide](ui_guide.md) for building GitLab with existing css styles and elements
+Disable Sidekiq arguments logging by default
