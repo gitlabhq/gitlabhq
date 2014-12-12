8eb29cd5d6ed37476e1ea9f823add1ec5ff774af
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2014-12-12 13:32:15 +0200

    Fix db schema
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -226,6 +226,7 @@ ActiveRecord::Schema.define(version: 20141205134006) do
     t.integer  "iid"
     t.text     "description"
     t.integer  "position",          default: 0
+    t.datetime "locked_at"
   end
 
   add_index "merge_requests", ["assignee_id"], name: "index_merge_requests_on_assignee_id", using: :btree
