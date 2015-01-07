ccdf08d80a64590f6188a3e36d68625e506b331c
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2015-01-06 16:24:47 -0800

    Refactor merge request merge service
    * Add system note when user merges MR in same way as it closes it
    * Remove duplicating code

--- app/services/merge_requests/base_merge_service.rb
+++ app/services/merge_requests/base_merge_service.rb
@@ -1,21 +1,10 @@
 module MergeRequests
-  class BaseMergeService
+  class BaseMergeService < MergeRequests::BaseService
 
     private
 
-    def notification
-      NotificationService.new
-    end
-
     def create_merge_event(merge_request, current_user)
       EventCreateService.new.merge_mr(merge_request, current_user)
-    end
-
-    def execute_project_hooks(merge_request)
-      if merge_request.project
-        hook_data = merge_request.to_hook_data(current_user)
-        merge_request.project.execute_hooks(hook_data, :merge_request_hooks)
-      end
     end
   end
 end
