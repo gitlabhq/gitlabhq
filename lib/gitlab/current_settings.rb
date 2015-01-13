02d8575a610176bbb79eac91b5ab783872ef098a
DJ Mountney <dmountney@perforce.com>
2015-01-13 10:58:32 -0800

    Check for database connection before loading current application settings

--- lib/gitlab/current_settings.rb
+++ lib/gitlab/current_settings.rb
@@ -1,14 +1,10 @@
 module Gitlab
   module CurrentSettings
     def current_application_settings
-      begin
-        if ActiveRecord::Base.connection.table_exists?('application_settings')
-          ApplicationSetting.current ||
-            ApplicationSetting.create_from_defaults
-        else
-          fake_application_settings
-        end
-      rescue ActiveRecord::NoDatabaseError, database_adapter.constantize::Error
+      if ActiveRecord::Base.connected? && ActiveRecord::Base.connection.table_exists?('application_settings')
+        ApplicationSetting.current ||
+          ApplicationSetting.create_from_defaults
+      else
         fake_application_settings
       end
     end
@@ -21,17 +17,6 @@ module Gitlab
         gravatar_enabled: Settings.gravatar['enabled'],
         sign_in_text: Settings.extra['sign_in_text'],
       )
-    end
-
-    # We need to check which database is setup
-    # but we cannot assume that the database exists already.
-    # Not checking this will break "rake gitlab:setup".
-    def database_adapter
-      if Rails.configuration.database_configuration[Rails.env]['adapter'] == 'mysql2'
-        "Mysql2"
-      else
-        "PG"
-      end
     end
   end
 end
