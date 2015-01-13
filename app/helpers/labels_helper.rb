5b32fda4b8698deda5402b8c640d36bf5cd69222
phortx <benny@itws.de>
2015-01-13 14:09:19 +0100

    Add more label color suggestions

--- app/helpers/labels_helper.rb
+++ app/helpers/labels_helper.rb
@@ -14,14 +14,27 @@ module LabelsHelper
 
   def suggested_colors
     [
+      '#CC0033',
+      '#FF0000',
       '#D9534F',
+      '#D1D100',
       '#F0AD4E',
+      '#AD8D43',
+      '#0033CC',
       '#428BCA',
+      '#44AD8E',
+      '#A8D695',
       '#5CB85C',
+      '#69D100',
+      '#004E00',
       '#34495E',
       '#7F8C8D',
+      '#A295D6',
+      '#5843AD',
       '#8E44AD',
-      '#FFECDB'
+      '#AD4363',
+      '#FFECDB',
+      '#D10069'
     ]
   end
 
