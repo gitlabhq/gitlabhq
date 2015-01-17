b9d09a0c5ee43a58f2c19b37f590b2124bb10a25
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2015-01-16 16:09:20 -0800

    Add tests to home page url redirect

--- features/steps/admin/settings.rb
+++ features/steps/admin/settings.rb
@@ -4,13 +4,15 @@ class Spinach::Features::AdminSettings < Spinach::FeatureSteps
   include SharedAdmin
   include Gitlab::CurrentSettings
 
-  step 'I disable gravatars and save form' do
+  step 'I modify settings and save form' do
     uncheck 'Gravatar enabled'
+    fill_in 'Home page url', with: 'https://about.gitlab.com/'
     click_button 'Save'
   end
 
-  step 'I should be see gravatar disabled' do
+  step 'I should see application settings saved' do
     current_application_settings.gravatar_enabled.should be_false
+    current_application_settings.home_page_url.should == 'https://about.gitlab.com/'
     page.should have_content 'Application settings saved successfully'
   end
 end
