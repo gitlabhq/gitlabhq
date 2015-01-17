41d7be3ce1ae9a4bff93b62322f35989b6ad4cf6
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2015-01-16 16:01:15 -0800

    Allow to specify home page for non logged-in users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150108073740) do
+ActiveRecord::Schema.define(version: 20150116234544) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20150108073740) do
     t.text     "sign_in_text"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "home_page_url"
   end
 
   create_table "broadcast_messages", force: true do |t|
@@ -423,6 +424,7 @@ ActiveRecord::Schema.define(version: 20150108073740) do
     t.integer  "notification_level",       default: 1,     null: false
     t.datetime "password_expires_at"
     t.integer  "created_by_id"
+    t.datetime "last_credential_check_at"
     t.string   "avatar"
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
@@ -430,7 +432,6 @@ ActiveRecord::Schema.define(version: 20150108073740) do
     t.string   "unconfirmed_email"
     t.boolean  "hide_no_ssh_key",          default: false
     t.string   "website_url",              default: "",    null: false
-    t.datetime "last_credential_check_at"
     t.string   "github_access_token"
   end
 
