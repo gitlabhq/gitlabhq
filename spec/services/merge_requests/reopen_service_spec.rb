4ffdb83e713b1ffa9578c02d31cfd0d9afe56ca7
jubianchi <contact@jubianchi.fr>
2015-01-18 01:34:34 +0100

    Add action property to merge request hook

--- spec/services/merge_requests/reopen_service_spec.rb
+++ spec/services/merge_requests/reopen_service_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe MergeRequests::CloseService do
+describe MergeRequests::ReopenService do
   let(:user) { create(:user) }
   let(:user2) { create(:user) }
   let(:merge_request) { create(:merge_request, assignee: user2) }
@@ -13,31 +13,32 @@ describe MergeRequests::CloseService do
 
   describe :execute do
     context 'valid params' do
-      let(:service) { MergeRequests::CloseService.new(project, user, {}) }
+      let(:service) { MergeRequests::ReopenService.new(project, user, {}) }
 
       before do
         service.stub(:execute_hooks)
 
-        @merge_request = service.execute(merge_request)
+        merge_request.state = :closed
+        service.execute(merge_request)
       end
 
-      it { @merge_request.should be_valid }
-      it { @merge_request.should be_closed }
+      it { merge_request.should be_valid }
+      it { merge_request.should be_reopened }
 
-      it 'should execute hooks with close action' do
+      it 'should execute hooks with reopen action' do
         expect(service).to have_received(:execute_hooks).
-                               with(@merge_request, 'close')
+                               with(merge_request, 'reopen')
       end
 
-      it 'should send email to user2 about assign of new merge_request' do
+      it 'should send email to user2 about reopen of merge_request' do
         email = ActionMailer::Base.deliveries.last
         email.to.first.should == user2.email
         email.subject.should include(merge_request.title)
       end
 
-      it 'should create system note about merge_request reassign' do
-        note = @merge_request.notes.last
-        note.note.should include 'Status changed to closed'
+      it 'should create system note about merge_request reopen' do
+        note = merge_request.notes.last
+        note.note.should include 'Status changed to reopened'
       end
     end
   end
