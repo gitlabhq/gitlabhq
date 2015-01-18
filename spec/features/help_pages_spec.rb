d76d8974928c238eed133cced439cbde4dd3239c
Carlos Ribeiro <duduribeiro.gba@gmail.com>
2015-01-18 14:21:18 -0200

    Change to single-quoted strings in help_pages_spec

--- spec/features/help_pages_spec.rb
+++ spec/features/help_pages_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
-describe "Help Pages", feature: true do
-  describe "Show SSH page" do
+describe 'Help Pages', feature: true do
+  describe 'Show SSH page' do
     before do
       login_as :user
     end
-    it "replace the variable $your_email with the email of the user" do
-      visit help_page_path(category: "ssh", file: "ssh.md")
+    it 'replace the variable $your_email with the email of the user' do
+      visit help_page_path(category: 'ssh', file: 'ssh.md')
       page.should have_content("ssh-keygen -t rsa -C \"#{@user.email}\"")
     end
   end
