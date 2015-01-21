d79e9c62660d7a760eb00cdeb29647343096e145
Yakun Li <liyakun127@gmail.com>
2015-01-21 10:12:09 +0100

    Update new_tree_controller.rb

--- app/controllers/projects/new_tree_controller.rb
+++ app/controllers/projects/new_tree_controller.rb
@@ -1,8 +1,10 @@
 class Projects::NewTreeController < Projects::BaseTreeController
   before_filter :require_branch_head
   before_filter :authorize_push_code!
+  
   def show
   end
+  
   def update
     flag = 0
     # replace & upload 
@@ -10,21 +12,25 @@ class Projects::NewTreeController < Projects::BaseTreeController
     # get the name of the upload file
       file_na = params[:file_upload].original_filename
       file_path = nil
+      
       if file_na != nil
         file_list = tree.entries.select(&:file?)	 
         dir_list = tree.entries.select(&:dir?)
+        
         # check whether current path is sub-repository
 	dir_list.each do |dir|		
 	  if dir.name != nil
             flag = 1
 	  end
 	end
+	
 	# check whether current path is sub-repository		
 	file_list.each do |file|	
 	  if file.name != nil
 	    flag = 1
 	  end
 	end
+	
 	# replace existing file		
 	if @path.match(/(.*\/)*(.+)\z/) != nil && flag == 0	
 	  flag = 2
@@ -33,10 +39,12 @@ class Projects::NewTreeController < Projects::BaseTreeController
 	  file_path = @path
 	  result = Files::UpdateService.new(@project, current_user, params, @ref, @path).execute
 	end
+	
 	# upload new file			
 	if flag == 1 || flag == 0		
 	  flag = 3
-	  file_list.each do |file|				
+	  file_list.each do |file|
+	  	
 	    if file.name == file_na
 	      flash[:alert] = file.name + " already exists!"
 	      redirect_to project_blob_path(@project, File.join(@ref, @path))
@@ -50,10 +58,12 @@ class Projects::NewTreeController < Projects::BaseTreeController
 	end
       end
     end 
+    
     if flag == 0
       file_path = File.join(@path, File.basename(params[:file_name]))
       result = Files::CreateService.new(@project, current_user, params, @ref, file_path).execute
     end
+    
     if result[:status] == :success
       flash[:notice] = "Your changes have been successfully commited."		
       redirect_to project_blob_path(@project, File.join(@ref, file_path))
