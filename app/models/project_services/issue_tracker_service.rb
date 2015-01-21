9371c6b90136547e3622622510af9894fc27aeb0
Marin Jankovski <maxlazio@gmail.com>
2015-01-20 16:46:27 -0800

    Add issue tracker services.

---
+++ app/models/project_services/issue_tracker_service.rb
@@ -0,0 +1,14 @@
+class IssueTrackerService < Service
+
+  def project_url
+    # implement inside child
+  end
+
+  def issues_url
+    # implement inside child
+  end
+
+  def new_issue_url
+    # implement inside child
+  end
+end
---
+++ app/models/project_services/redmine_service.rb
@@ -0,0 +1,51 @@
+class RedmineService < IssueTrackerService
+
+  prop_accessor :title, :description, :project_url, :issues_url, :new_issue_url
+
+  def title
+    'Redmine'
+  end
+
+  def description
+    'Redmine issue tracker'
+  end
+
+  def to_param
+    'redmine'
+  end
+
+  def fields
+    [
+      { type: 'text', name: 'title', placeholder: title },
+      { type: 'text', name: 'description', placeholder: description },
+      { type: 'text', name: 'project_url', placeholder: 'Project url' },
+      { type: 'text', name: 'issues_url', placeholder: 'Issue url'},
+      { type: 'text', name: 'new_issue_url', placeholder: 'New Issue url'}
+    ]
+  end
+
+  def initialize_properties
+    if properties.nil?
+      if enabled_in_gitlab_config
+        self.properties = {
+          title: issues_tracker['title'],
+          project_url: issues_tracker['project_url'],
+          issues_url: issues_tracker['issues_url'],
+          new_issue_url: issues_tracker['new_issue_url']
+        }
+      end
+    end
+  end
+
+  private
+
+  def enabled_in_gitlab_config
+    Gitlab.config.issues_tracker &&
+    Gitlab.config.issues_tracker.values.any? &&
+    issues_tracker
+  end
+
+  def issues_tracker
+    Gitlab.config.issues_tracker['redmine']
+  end
+end
