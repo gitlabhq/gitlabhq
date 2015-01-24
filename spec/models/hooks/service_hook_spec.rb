acac7889023de96af61d293d855c33996bd780a1
bugagazavr <kirik910@gmail.com>
2015-01-24 03:10:43 +0300

    Added X-GitLab-Event header for web hooks

--- spec/models/hooks/service_hook_spec.rb
+++ spec/models/hooks/service_hook_spec.rb
@@ -21,4 +21,37 @@ describe ServiceHook do
   describe "Associations" do
     it { is_expected.to belong_to :service }
   end
+
+  describe "execute" do
+    before(:each) do
+      @service_hook = create(:service_hook)
+      @data = { project_id: 1, data: {}}
+
+      WebMock.stub_request(:post, @service_hook.url)
+    end
+
+    it "POSTs to the web hook URL" do
+      @service_hook.execute(@data)
+      expect(WebMock).to have_requested(:post, @service_hook.url).with(
+        headers: {'Content-Type'=>'application/json', 'X-Gitlab-Event'=>'Service Hook'}
+      ).once
+    end
+
+    it "POSTs the data as JSON" do
+      json = @data.to_json
+
+      @service_hook.execute(@data)
+      expect(WebMock).to have_requested(:post, @service_hook.url).with(
+        headers: {'Content-Type'=>'application/json', 'X-Gitlab-Event'=>'Service Hook'}
+      ).once
+    end
+
+    it "catches exceptions" do
+      expect(WebHook).to receive(:post).and_raise("Some HTTP Post error")
+
+      expect {
+        @service_hook.execute(@data)
+      }.to raise_error
+    end
+  end
 end
