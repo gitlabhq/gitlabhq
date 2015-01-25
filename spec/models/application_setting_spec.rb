2a4502111e03c233861b545ae3ff3afd95614c4a
Marco Wessel <marco@poop.nl>
2015-01-25 22:23:28 +0100

    Spelling error

--- spec/models/application_setting_spec.rb
+++ spec/models/application_setting_spec.rb
@@ -4,7 +4,7 @@
 #
 #  id                         :integer          not null, primary key
 #  default_projects_limit     :integer
-#  default_branch_protection  :interger
+#  default_branch_protection  :integer
 #  signup_enabled             :boolean
 #  signin_enabled             :boolean
 #  gravatar_enabled           :boolean
