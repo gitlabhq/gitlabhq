368e9a0862dd7d58b009956e8f1ac51d2a549cda
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2015-02-02 21:26:40 -0800

    Rubocop: Style/CaseIndentation enabled

--- lib/api/api_guard.rb
+++ lib/api/api_guard.rb
@@ -47,16 +47,12 @@ module APIGuard
         case validate_access_token(access_token, scopes)
         when Oauth2::AccessTokenValidationService::INSUFFICIENT_SCOPE
           raise InsufficientScopeError.new(scopes)
-
         when Oauth2::AccessTokenValidationService::EXPIRED
           raise ExpiredError
-
         when Oauth2::AccessTokenValidationService::REVOKED
           raise RevokedError
-
         when Oauth2::AccessTokenValidationService::VALID
           @current_user = User.find(access_token.resource_owner_id)
-
         end
       end
     end
@@ -121,7 +117,8 @@ module APIGuard
 
     def oauth2_bearer_token_error_handler
       Proc.new do |e|
-        response = case e
+        response =
+          case e
           when MissingTokenError
             Rack::OAuth2::Server::Resource::Bearer::Unauthorized.new
 
