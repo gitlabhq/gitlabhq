baeddaa9e056a9006307ba39498acd159aef06c1
Carlos Ribeiro <duduribeiro.gba@gmail.com>
2015-02-13 18:32:35 -0200

    Fix the regex used to mention the user to accept numbers in the beginning of the username

--- spec/models/concerns/mentionable_spec.rb
+++ spec/models/concerns/mentionable_spec.rb
@@ -2,13 +2,25 @@ require 'spec_helper'
 
 describe Issue, "Mentionable" do
   describe :mentioned_users do
-    let!(:user) { create(:user, username: 'stranger') }
-    let!(:user2) { create(:user, username: 'john') }
-    let!(:issue) { create(:issue, description: '@stranger mentioned') }
+    let!(:user) { create(:user, username: 'john') }
+    context 'mentioning an user with the username starting with a letter' do
+      let!(:user_mentioned) { create(:user, username: 'stranger') }
+      let!(:issue) { create(:issue, description: '@stranger mentioned') }
 
-    subject { issue.mentioned_users }
+      subject { issue.mentioned_users }
 
-    it { is_expected.to include(user) }
-    it { is_expected.not_to include(user2) }
+      it { is_expected.to include(user_mentioned) }
+      it { is_expected.not_to include(user) }
+    end
+
+    context 'mentioning an user with the username starting with a number' do
+      let!(:user_mentioned) { create(:user, username: '123stranger') }
+      let!(:issue) { create(:issue, description: '@123stranger mentioned') }
+
+      subject { issue.mentioned_users }
+
+      it { is_expected.to include(user_mentioned) }
+      it { is_expected.not_to include(user) }
+    end
   end
 end
