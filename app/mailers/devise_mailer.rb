b4be7aed7711e51becb3be29ed82d6753cc73cc4
Jakub Jirutka <jakub@jirutka.cz>
2015-02-15 01:28:50 +0100

    Allow to configure smtp and sendmail in gitlab.yml

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -1,4 +1,4 @@
 class DeviseMailer < Devise::Mailer
-  default from: "#{Gitlab.config.gitlab.email_display_name} <#{Gitlab.config.gitlab.email_from}>"
-  default reply_to: Gitlab.config.gitlab.email_reply_to
+  default from: "#{Gitlab.config.outgoing_emails.display_name} <#{Gitlab.config.outgoing_emails.from}>"
+  default reply_to: Gitlab.config.outgoing_emails.reply_to
 end
