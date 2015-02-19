7c3147e6e969a7ae97e2f8d05e536abeeb7d3936
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2015-02-19 08:57:33 -0800

    Revert "Nitpicking."
    This reverts commit ebd39fc082b09177e0777e5de5729c3f98495e87.

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -5,10 +5,9 @@ class FilesController < ApplicationController
 
     if uploader.file_storage?
       if can?(current_user, :read_project, note.project)
-        # Replace old notes location in /public with the new one in / and send the file
-        path = uploader.file.path.gsub("#{Rails.root}/public", Rails.root.to_s)
-
         disposition = uploader.image? ? 'inline' : 'attachment'
+        # Replace old notes location in /public with the new one in / and send the file
+        path = uploader.file.path.gsub("#{Rails.root}/public",Rails.root.to_s)
         send_file path, disposition: disposition
       else
         not_found!
