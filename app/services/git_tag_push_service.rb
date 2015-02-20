19f04cf989a75f425af09a8551957dae42b1ff31
Douwe Maan <douwe@gitlab.com>
2015-02-20 14:49:36 +0100

    Execute services for tag push.

--- app/services/git_tag_push_service.rb
+++ app/services/git_tag_push_service.rb
@@ -8,6 +8,7 @@ class GitTagPushService
     EventCreateService.new.push(project, user, @push_data)
     project.repository.expire_cache
     project.execute_hooks(@push_data.dup, :tag_push_hooks)
+    project.execute_services(@push_data.dup, :tag_push_hooks)
 
     if project.gitlab_ci?
       project.gitlab_ci_service.async_execute(@push_data)
