c801df81fb48272b670b7448e3898a98cdb8b742
Douwe Maan <douwe@gitlab.com>
2015-02-20 14:39:35 +0100

    Satisfy Rubocop.

--- config/routes.rb
+++ config/routes.rb
@@ -76,11 +76,11 @@ Gitlab::Application.routes.draw do
   scope path: :uploads do
     # Note attachments and User/Group/Project avatars
     get ":model/:mounted_as/:id/:filename", to: "uploads#show", 
-      constraints: { model: /note|user|group|project/, mounted_as: /avatar|attachment/, filename: /.+/ }
+        constraints: { model: /note|user|group|project/, mounted_as: /avatar|attachment/, filename: /.+/ }
 
     # Project markdown uploads
     get ":id/:secret/:filename", to: "projects/uploads#show", 
-      constraints: { id: /[a-zA-Z.0-9_\-]+\/[a-zA-Z.0-9_\-]+/, filename: /.+/ }
+        constraints: { id: /[a-zA-Z.0-9_\-]+\/[a-zA-Z.0-9_\-]+/, filename: /.+/ }
   end
 
   #
