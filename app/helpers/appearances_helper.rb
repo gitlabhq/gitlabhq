6e559be6c68b921e12518816a824724564e3e315
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2015-02-24 22:58:47 -0800

    Refactor header logo and setup expectation on it size

--- app/helpers/appearances_helper.rb
+++ app/helpers/appearances_helper.rb
@@ -14,4 +14,12 @@ module AppearancesHelper
   def brand_text
     nil
   end
+
+  def brand_header_logo
+    if theme_type == 'light_theme'
+      image_tag 'logo-black.png'
+    else
+      image_tag 'logo-white.png'
+    end
+  end
 end
