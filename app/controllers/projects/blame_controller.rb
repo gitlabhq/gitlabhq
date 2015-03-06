7f4cffd88b393a55f6462ce870807c7afd7ca98a
Douwe Maan <douwe@gitlab.com>
2015-03-06 12:25:42 +0100

    Reuse blob object fetched by Gitlab::Git::Blame.

--- app/controllers/projects/blame_controller.rb
+++ app/controllers/projects/blame_controller.rb
@@ -7,7 +7,7 @@ class Projects::BlameController < Projects::ApplicationController
   before_filter :authorize_download_code!
 
   def show
-    @blob = @repository.blob_at(@commit.id, @path)
-    @blame = Gitlab::Git::Blame.new(project.repository, @commit.id, @path)
+    @blame = Gitlab::Git::Blame.new(@repository, @commit.id, @path)
+    @blob = @blame.blob
   end
 end
