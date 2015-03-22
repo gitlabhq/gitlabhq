777c119fd2f61053937fa57625100168b7dccc8e
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2015-03-21 17:44:40 -0700

    Restyle oauth accounts at profile page and add ability to unlink account

--- app/controllers/profiles/accounts_controller.rb
+++ app/controllers/profiles/accounts_controller.rb
@@ -4,4 +4,10 @@ class Profiles::AccountsController < ApplicationController
   def show
     @user = current_user
   end
+
+  def unlink
+    provider = params[:provider]
+    current_user.identities.find_by(provider: provider).destroy
+    redirect_to profile_account_path
+  end
 end
