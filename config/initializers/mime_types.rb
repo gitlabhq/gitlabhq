1e27b68b364cfc0b504e7db429c2450d75b72cfd
Robert Speicher <rspeicher@gmail.com>
2015-04-11 16:46:36 -0400

    Add Markdown to Mime types

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -6,3 +6,5 @@
 
 Mime::Type.register_alias "text/plain", :diff
 Mime::Type.register_alias "text/plain", :patch
+Mime::Type.register_alias 'text/html',  :markdown
+Mime::Type.register_alias 'text/html',  :md
