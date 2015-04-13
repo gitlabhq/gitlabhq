a9306613f6502611d6f1eae268e82ed53943fa6c
Hannes Rosenögger <123haynes@gmail.com>
2015-04-13 22:09:44 +0200

    The help link has been removed. The commit removes the test for it.

--- features/steps/dashboard/active_tab.rb
+++
@@ -1,9 +0,0 @@
-class Spinach::Features::DashboardActiveTab < Spinach::FeatureSteps
-  include SharedAuthentication
-  include SharedPaths
-  include SharedActiveTab
-
-  step 'the active main tab should be Help' do
-    ensure_active_main_tab('Help')
-  end
-end
