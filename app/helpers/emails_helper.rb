8e9fbd847d8ad44edd7346428276b055c765e252
Hannes Rosenögger <123haynes@gmail.com>
2015-04-13 12:32:04 +0200

    Revert "Merge branch 'fix_email_images' into 'master'"
    This reverts commit d66148ef393f1748c669c934eec4e928d92ef36a, reversing
    changes made to cdb64a81a8ca96961033b8ab06d5191ef5449634.
    
    This change needed to be reverted,
    because not enough email clients support inline images.

--- app/helpers/emails_helper.rb
+++ app/helpers/emails_helper.rb
@@ -1,6 +1,3 @@
-require 'html/pipeline'
-require 'html/pipeline/gitlab'
-
 module EmailsHelper
 
   # Google Actions
@@ -37,27 +34,5 @@ module EmailsHelper
     formatter = Rugments::Formatters::HTML.new(cssclass: "highlight", inline_theme: :github)
     lexer = Rugments::Lexers::Diff.new
     raw formatter.format(lexer.lex(diffcontent))
-  end
-
-  def replace_image_links_with_base64(text, project)
-    # Used pipelines in GitLab:
-    # GitlabEmailImageFilter - replaces images that have been uploaded as attachments with inline images in emails.
-    #
-    # see https://gitlab.com/gitlab-org/html-pipeline-gitlab for more filters
-    filters = [
-      HTML::Pipeline::Gitlab::GitlabEmailImageFilter
-    ]
-
-    context = {
-      base_url: File.join(Gitlab.config.gitlab.url, project.path_with_namespace, 'uploads'),
-      upload_path: File.join(Rails.root, 'public', 'uploads', project.path_with_namespace),
-    }
-
-    pipeline = HTML::Pipeline::Gitlab.new(filters).pipeline
-
-    result = pipeline.call(text, context)
-    text = result[:output].to_html(save_with: 0)
-
-    text.html_safe
   end
 end
