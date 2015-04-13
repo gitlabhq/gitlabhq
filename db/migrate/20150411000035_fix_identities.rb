04f05ac1fb43904229bc084813dab92e82343f02
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2015-04-13 12:26:04 +0300

    Check for table instead of class

--- db/migrate/20150411000035_fix_identities.rb
+++ db/migrate/20150411000035_fix_identities.rb
@@ -11,13 +11,13 @@ class FixIdentities < ActiveRecord::Migration
     # the first LDAP server specified in gitlab.yml / gitlab.rb.
     new_provider = Gitlab.config.ldap.servers.first.last['provider_name']
 
-	  # Delete duplicate identities
+    # Delete duplicate identities
     execute "DELETE FROM identities WHERE provider = 'ldap' AND user_id IN (SELECT user_id FROM identities WHERE provider = '#{new_provider}')"
 
     # Update legacy identities
     execute "UPDATE identities SET provider = '#{new_provider}' WHERE provider = 'ldap';"
 
-    if defined?(LdapGroupLink)
+    if table_exists?('ldap_group_links')
       execute "UPDATE ldap_group_links SET provider = '#{new_provider}' WHERE provider IS NULL;"
     end
   end
