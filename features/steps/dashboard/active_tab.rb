5d1ab47bd2dc80e8c5237416405009786ef58629
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2015-04-15 12:13:49 +0300

    Revert help link tests removal
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- features/steps/dashboard/active_tab.rb
+++ features/steps/dashboard/active_tab.rb
@@ -3,4 +3,7 @@ class Spinach::Features::DashboardActiveTab < Spinach::FeatureSteps
   include SharedPaths
   include SharedActiveTab
 
+  step 'the active main tab should be Help' do
+    ensure_active_main_tab('Help')
+  end
 end
