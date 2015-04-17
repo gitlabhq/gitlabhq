8b4b687d445236766c168a844b856d8c09efe1a2
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2015-04-17 14:01:33 +0000

    Merge branch 'google-code-import-performance' into 'master'
    Decrease memory use and increase performance of Google Code importer.
    
    Addresses private issue https://dev.gitlab.org/gitlab/gitlabhq/issues/2241.
    
    See merge request !536

---
+++ app/models/project_import_data.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: project_import_datas
+#
+#  id           :integer          not null, primary key
+#  project_id   :integer
+#  data         :text
+#
+
+require 'carrierwave/orm/activerecord'
+require 'file_size_validator'
+
+class ProjectImportData < ActiveRecord::Base
+  belongs_to :project
+  
+  serialize :data, JSON
+
+  validates :project, presence: true
+end
