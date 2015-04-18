80657c818c3219ad15321bae975a6db7745b0f23
Douwe Maan <douwe@gitlab.com>
2015-04-18 12:06:50 +0200

    Redirect to sign in page after signing out.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -87,6 +87,10 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
+
   def abilities
     Ability.abilities
   end
