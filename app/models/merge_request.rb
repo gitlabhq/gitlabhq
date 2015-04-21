4f2e4c76561ba5b921b16d0ba4245f594c399e06
Douwe Maan <douwe@gitlab.com>
2015-04-21 15:38:14 +0200

    Select MR commit notes from source project.

--- app/models/merge_request.rb
+++ app/models/merge_request.rb
@@ -213,10 +213,13 @@ class MergeRequest < ActiveRecord::Base
     commits_for_notes_limit = 100
     commit_ids = commits.last(commits_for_notes_limit).map(&:id)
 
-    project.notes.where(
-      "(noteable_type = 'MergeRequest' AND noteable_id = :mr_id) OR (noteable_type = 'Commit' AND commit_id IN (:commit_ids))",
+    Note.where(
+      "(project_id = :target_project_id AND noteable_type = 'MergeRequest' AND noteable_id = :mr_id) OR" +
+      "(project_id = :source_project_id AND noteable_type = 'Commit' AND commit_id IN (:commit_ids))",
       mr_id: id,
-      commit_ids: commit_ids
+      commit_ids: commit_ids,
+      target_project_id: target_project_id,
+      source_project_id: source_project_id
     )
   end
 
