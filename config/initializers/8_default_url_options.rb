11a76a8b64673048b86ebad9a54709924d109600
Robert Speicher <rspeicher@gmail.com>
2015-04-23 12:58:11 -0400

    Add default_url_options initializer
    Fixes #2266

---
+++ config/initializers/8_default_url_options.rb
@@ -0,0 +1,13 @@
+unless Rails.env.test?
+  default_url_options = {
+    host:        Gitlab.config.gitlab.host,
+    protocol:    Gitlab.config.gitlab.protocol,
+    script_name: Gitlab.config.gitlab.relative_url_root
+  }
+
+  unless Gitlab.config.gitlab_on_standard_port?
+    default_url_options[:port] = Gitlab.config.gitlab.port
+  end
+
+  Rails.application.routes.default_url_options = default_url_options
+end
