04020d5e2038e9acd31147c16dc07fb5a2360a24
Douwe Maan <douwe@gitlab.com>
2015-04-23 17:27:45 +0200

    Explain purpose and usage.

--- app/models/concerns/participable.rb
+++ app/models/concerns/participable.rb
@@ -1,3 +1,27 @@
+# == Participable concern
+#
+# Contains functionality related to objects that can have participants, such as
+# an author, an assignee and people mentioned in its description or comments.
+#
+# Used by Issue, Note, MergeRequest, Snippet and Commit.
+#
+# Usage:
+#
+#     class Issue < ActiveRecord::Base
+#       include Participable
+#
+#       # ...
+#
+#       participant :author, :assignee, :mentioned_users, :notes
+#     end
+#     
+#     issue = Issue.last
+#     users = issue.participants
+#     # `users` will contain the issue's author, its assignee,
+#     # all users returned by its #mentioned_users method,
+#     # as well as all participants to all of the issue's notes,
+#     # since Note implements Participable as well.
+#
 module Participable
   extend ActiveSupport::Concern
 
