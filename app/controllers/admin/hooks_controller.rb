bc9ba5237cafd4b24405596f0c9e8af099635c29
Valery Sizov <vsv2711@gmail.com>
2015-04-27 16:29:31 +0300

    Revert "Added X-GitLab-Event header for web hooks"
    This reverts commit 548f182814acd0f7a110e6c165c186e345901b00.

--- app/controllers/admin/hooks_controller.rb
+++ app/controllers/admin/hooks_controller.rb
@@ -33,7 +33,7 @@ class Admin::HooksController < Admin::ApplicationController
       owner_name: "Someone",
       owner_email: "example@gitlabhq.com"
     }
-    @hook.execute(data, 'system_hooks')
+    @hook.execute(data)
 
     redirect_to :back
   end
