4d3a93af2db702197911d2e3ef58d3365f4cc7ed
Valery Sizov <vsv2711@gmail.com>
2015-04-29 13:46:57 +0300

    Remove abandoned GroupMembers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150425173433) do
+ActiveRecord::Schema.define(version: 20150429002313) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
