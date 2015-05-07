aaa4de2989e2663d209317135ef4adcdf3d52e5c
hxx <yellowstar520@gmail.com>
2015-05-07 20:48:59 +0800

    add mina/rvm and add shared_paths for gitlab.yml and unicorn.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,7 @@
 require 'mina/bundler'
 require 'mina/rails'
 require 'mina/git'
+require 'mina/rvm'
 # require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
 # require 'mina/rvm'    # for rvm support. (http://rvm.io)
 
@@ -36,7 +37,7 @@ end
 
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
-set :shared_paths, ['config/database.yml', 'log']
+set :shared_paths, ['config/database.yml', 'config/gitlab.yml', 'config/unicorn.rb', 'log']
 
 # Optional settings:
     set :user, 'deploy'    # Username in the server to SSH to.
@@ -51,7 +52,7 @@ task :environment do
   # invoke :'rbenv:load'
 
   # For those using RVM, use this to load an RVM version@gemset.
-  # invoke :'rvm:use[ruby-1.9.3-p125@default]'
+  invoke :'rvm:use[2.2.2@default]'
 end
 
 # Put any custom mkdir's in here for when `mina setup` is ran.
