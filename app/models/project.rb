dd6d43a2145b16892221571af5484145197ae9ff
Gleb Averchuk <altermn@gmail.com>
2015-05-08 18:42:32 +0300

    fix for PR comments

--- app/models/project.rb
+++ app/models/project.rb
@@ -50,18 +50,17 @@ class Project < ActiveRecord::Base
   default_value_for :wall_enabled, false
   default_value_for :snippets_enabled, gitlab_config_features.snippets
 
-  # set last_activity_at to the same as created_at
-  after_create :set_last_activity_at
-  def set_last_activity_at
-    update_column(:last_activity_at, self.created_at)
-  end
-
-  # create default hooks if they are presented
   after_create :create_default_web_hooks, if: -> { !!gitlab_config_features.auto_web_hooks }
   def create_default_web_hooks
     gitlab_config_features.auto_web_hooks.each do |web_hook_url|
       hooks.create(url: web_hook_url)
     end
+  end
+
+  # set last_activity_at to the same as created_at
+  after_create :set_last_activity_at
+  def set_last_activity_at
+    update_column(:last_activity_at, self.created_at)
   end
 
   ActsAsTaggableOn.strict_case_match = true
