ee1e1c31e3e00da0b6c2ad4b16064e679dcb9808
Jeroen van Baarsen <jeroenvanbaarsen@gmail.com>
2015-05-17 14:34:31 +0200

    Fixed features/dashboard spinach test
    Signed-off-by: Jeroen van Baarsen <jeroenvanbaarsen@gmail.com>

--- features/steps/dashboard/new_project.rb
+++ features/steps/dashboard/new_project.rb
@@ -4,13 +4,13 @@ class Spinach::Features::NewProject < Spinach::FeatureSteps
   include SharedProject
 
   step 'I click "New project" link' do
-    within('.content') do
+    page.within('.content') do
       click_link "New project"
     end
   end
 
   step 'I see "New project" page' do
-    page.should have_content("Project path")
+    expect(page).to have_content('Project path')
   end
 
   step 'I click on "Import project from GitHub"' do
