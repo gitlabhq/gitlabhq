jirutka/gitlab-foss

Handle AsciiDoc better, reuse HTML pipeline filters (fixes #9263)

app/helpers/gitlab_markdown_helper.rb
@@ -54,6 +54,16 @@ module GitlabMarkdownHelper
     @markdown.render(text).html_safe
   end
 
+  def asciidoc(text)
+    Gitlab::Asciidoc.render_asciidoc(text, {
+      commit: @commit,
+      project: @project,
+      project_wiki: @project_wiki,
+      requested_path: @path,
+      ref: @ref
+    })
+  end
+
   # Return the first line of +text+, up to +max_chars+, after parsing the line
   # as Markdown.  HTML tags in the parsed output are not counted toward the
   # +max_chars+ limit.  If the length limit falls within a tag's contents, then
@@ -65,8 +75,11 @@ module GitlabMarkdownHelper
   end
 
   def render_wiki_content(wiki_page)
-    if wiki_page.format == :markdown
+    case wiki_page.format
+    when :markdown
       markdown(wiki_page.content)
+    when :asciidoc
+      asciidoc(wiki_page.content)
     else
       wiki_page.formatted_content.html_safe
     end

lib/gitlab/asciidoc.rb
@@ -1,0 +1,60 @@
+require 'asciidoctor'
+require 'html/pipeline'
+
+module Gitlab
+  # Parser/renderer for the AsciiDoc format that uses Asciidoctor and filters
+  # the resulting HTML through HTML pipeline filters.
+  module Asciidoc
+
+    # Provide autoload paths for filters to prevent a circular dependency error
+    autoload :RelativeLinkFilter, 'gitlab/markdown/relative_link_filter'
+
+    DEFAULT_ADOC_ATTRS = [
+      'showtitle', 'idprefix=user-content-', 'idseparator=-', 'env=gitlab',
+      'env-gitlab', 'source-highlighter=html-pipeline'
+    ].freeze
+
+    # Public: Converts the provided Asciidoc markup into HTML.
+    #
+    # input         - the source text in Asciidoc format
+    # context       - a Hash with the template context:
+    #                 :commit
+    #                 :project
+    #                 :project_wiki
+    #                 :requested_path
+    #                 :ref
+    # asciidoc_opts - a Hash of options to pass to the Asciidoctor converter
+    # html_opts     - a Hash of options for HTML output:
+    #                 :xhtml - output XHTML instead of HTML
+    #
+    def self.render_asciidoc(input, context, asciidoc_opts = {}, html_opts = {})
+      asciidoc_opts = asciidoc_opts.reverse_merge(
+        safe: :secure,
+        backend: html_opts[:xhtml] ? :xhtml5 : :html5,
+        attributes: []
+      )
+      asciidoc_opts[:attributes].unshift(*DEFAULT_ADOC_ATTRS)
+
+      html = ::Asciidoctor.convert(input, asciidoc_opts)
+
+      if context[:project]
+        result = HTML::Pipeline.new(filters).call(html, context)
+
+        save_opts = html_opts[:xhtml] ?
+          Nokogiri::XML::Node::SaveOptions::AS_XHTML : 0
+
+        html = result[:output].to_html(save_with: save_opts)
+      end
+
+      html.html_safe
+    end
+
+    private
+
+    def self.filters
+      [
+        Gitlab::Markdown::RelativeLinkFilter
+      ]
+    end
+  end
+end

spec/lib/gitlab/asciidoc_spec.rb
@@ -1,0 +1,59 @@
+require 'spec_helper'
+require 'nokogiri'
+
+module Gitlab
+  describe Asciidoc do
+
+    let(:input) { '<b>ascii</b>' }
+    let(:context) { {} }
+    let(:html) { 'H<sub>2</sub>O' }
+
+    context "without project" do
+
+      it "should convert the input using Asciidoctor and default options" do
+        expected_asciidoc_opts = { safe: :secure, backend: :html5,
+          attributes: described_class::DEFAULT_ADOC_ATTRS }
+
+        expect(Asciidoctor).to receive(:convert)
+          .with(input, expected_asciidoc_opts).and_return(html)
+
+        expect( render_asciidoc(input, context) ).to eql html
+      end
+
+      context "with asciidoc_opts" do
+
+        let(:asciidoc_opts) { {safe: :safe, attributes: ['foo']} }
+
+        it "should merge the options with default ones" do
+          expected_asciidoc_opts = { safe: :safe, backend: :html5,
+            attributes: described_class::DEFAULT_ADOC_ATTRS + ['foo'] }
+
+          expect(Asciidoctor).to receive(:convert)
+            .with(input, expected_asciidoc_opts).and_return(html)
+
+          render_asciidoc(input, context, asciidoc_opts)
+        end
+      end
+    end
+
+    context "with project in context" do
+
+      let(:context) { {project: create(:project)} }
+
+      it "should filter converted input via HTML pipeline and return result" do
+        filtered_html = '<b>ASCII</b>'
+
+        allow(Asciidoctor).to receive(:convert).and_return(html)
+        expect_any_instance_of(HTML::Pipeline).to receive(:call)
+          .with(html, context)
+          .and_return(output: Nokogiri::HTML.fragment(filtered_html))
+
+        expect( render_asciidoc('foo', context) ).to eql filtered_html
+      end
+    end
+
+    def render_asciidoc(*args)
+      described_class.render_asciidoc(*args)
+    end
+  end
+end

gitlabhq/gitlabhq

app/helpers/gitlab_markdown_helper.rb
@@ -56,6 +56,16 @@ module GitlabMarkdownHelper
     @markdown.render(text).html_safe
   end
 
+  def asciidoc(text)
+    Gitlab::Asciidoc.render(text, {
+      commit: @commit,
+      project: @project,
+      project_wiki: @project_wiki,
+      requested_path: @path,
+      ref: @ref
+    })
+  end
+
   # Return the first line of +text+, up to +max_chars+, after parsing the line
   # as Markdown.  HTML tags in the parsed output are not counted toward the
   # +max_chars+ limit.  If the length limit falls within a tag's contents, then
@@ -67,8 +77,11 @@ module GitlabMarkdownHelper
   end
 
   def render_wiki_content(wiki_page)
-    if wiki_page.format == :markdown
+    case wiki_page.format
+    when :markdown
       markdown(wiki_page.content)
+    when :asciidoc
+      asciidoc(wiki_page.content)
     else
       wiki_page.formatted_content.html_safe
     end

lib/gitlab/asciidoc.rb
@@ -1,0 +1,60 @@
+require 'asciidoctor'
+require 'html/pipeline'
+
+module Gitlab
+  # Parser/renderer for the AsciiDoc format that uses Asciidoctor and filters
+  # the resulting HTML through HTML pipeline filters.
+  module Asciidoc
+
+    # Provide autoload paths for filters to prevent a circular dependency error
+    autoload :RelativeLinkFilter, 'gitlab/markdown/relative_link_filter'
+
+    DEFAULT_ADOC_ATTRS = [
+      'showtitle', 'idprefix=user-content-', 'idseparator=-', 'env=gitlab',
+      'env-gitlab', 'source-highlighter=html-pipeline'
+    ].freeze
+
+    # Public: Converts the provided Asciidoc markup into HTML.
+    #
+    # input         - the source text in Asciidoc format
+    # context       - a Hash with the template context:
+    #                 :commit
+    #                 :project
+    #                 :project_wiki
+    #                 :requested_path
+    #                 :ref
+    # asciidoc_opts - a Hash of options to pass to the Asciidoctor converter
+    # html_opts     - a Hash of options for HTML output:
+    #                 :xhtml - output XHTML instead of HTML
+    #
+    def self.render(input, context, asciidoc_opts = {}, html_opts = {})
+      asciidoc_opts = asciidoc_opts.reverse_merge(
+        safe: :secure,
+        backend: html_opts[:xhtml] ? :xhtml5 : :html5,
+        attributes: []
+      )
+      asciidoc_opts[:attributes].unshift(*DEFAULT_ADOC_ATTRS)
+
+      html = ::Asciidoctor.convert(input, asciidoc_opts)
+
+      if context[:project]
+        result = HTML::Pipeline.new(filters).call(html, context)
+
+        save_opts = html_opts[:xhtml] ?
+          Nokogiri::XML::Node::SaveOptions::AS_XHTML : 0
+
+        html = result[:output].to_html(save_with: save_opts)
+      end
+
+      html.html_safe
+    end
+
+    private
+
+    def self.filters
+      [
+        Gitlab::Markdown::RelativeLinkFilter
+      ]
+    end
+  end
+end

spec/lib/gitlab/asciidoc_spec.rb
@@ -1,0 +1,59 @@
+require 'spec_helper'
+require 'nokogiri'
+
+module Gitlab
+  describe Asciidoc do
+
+    let(:input) { '<b>ascii</b>' }
+    let(:context) { {} }
+    let(:html) { 'H<sub>2</sub>O' }
+
+    context "without project" do
+
+      it "should convert the input using Asciidoctor and default options" do
+        expected_asciidoc_opts = { safe: :secure, backend: :html5,
+          attributes: described_class::DEFAULT_ADOC_ATTRS }
+
+        expect(Asciidoctor).to receive(:convert)
+          .with(input, expected_asciidoc_opts).and_return(html)
+
+        expect( render(input, context) ).to eql html
+      end
+
+      context "with asciidoc_opts" do
+
+        let(:asciidoc_opts) { {safe: :safe, attributes: ['foo']} }
+
+        it "should merge the options with default ones" do
+          expected_asciidoc_opts = { safe: :safe, backend: :html5,
+            attributes: described_class::DEFAULT_ADOC_ATTRS + ['foo'] }
+
+          expect(Asciidoctor).to receive(:convert)
+            .with(input, expected_asciidoc_opts).and_return(html)
+
+          render(input, context, asciidoc_opts)
+        end
+      end
+    end
+
+    context "with project in context" do
+
+      let(:context) { {project: create(:project)} }
+
+      it "should filter converted input via HTML pipeline and return result" do
+        filtered_html = '<b>ASCII</b>'
+
+        allow(Asciidoctor).to receive(:convert).and_return(html)
+        expect_any_instance_of(HTML::Pipeline).to receive(:call)
+          .with(html, context)
+          .and_return(output: Nokogiri::HTML.fragment(filtered_html))
+
+        expect( render('foo', context) ).to eql filtered_html
+      end
+    end
+
+    def render(*args)
+      described_class.render(*args)
+    end
+  end
+end