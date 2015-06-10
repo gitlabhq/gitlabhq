878732b4264eeba0214964da096b6f8c139f6900
Robert Speicher <rspeicher@gmail.com>
2015-06-10 04:42:02 -0400

    Add a form field to customize the dashboard preference
    cherry-picked

---
+++ app/helpers/preferences_helper.rb
@@ -0,0 +1,21 @@
+# Helper methods for per-User preferences
+module PreferencesHelper
+  # Populates the dashboard preference select field with more user-friendly
+  # values.
+  def dashboard_choices
+    orig = User.dashboards.keys
+
+    choices = [
+      ['Projects (default)', orig[0]],
+      ['Starred Projects',   orig[1]]
+    ]
+
+    if orig.size != choices.size
+      # Assure that anyone adding new options updates this method too
+      raise RuntimeError, "`User` defines #{orig.size} dashboard choices," +
+        " but #{__method__} defined #{choices.size}"
+    else
+      choices
+    end
+  end
+end
