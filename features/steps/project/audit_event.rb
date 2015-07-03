7545290ddde29f7f91d6cce8ef349045d7fe4854
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2015-07-03 12:43:54 +0200

    Move audit tests for project in separate class
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- features/steps/project/audit_event.rb
+++ features/steps/project/audit_event.rb
@@ -24,4 +24,47 @@ class Spinach::Features::AuditEvent < Spinach::FeatureSteps
   step 'I see deploy key event' do
     expect(page).to have_content("Add deploy key")
   end
+
+  step 'I should see the audit event listed' do
+    page.within('table#audits') do
+      expect(page).to have_content "Change access level from developer to master"
+      expect(page).to have_content(project.owner.name)
+      expect(page).to have_content('Pete')
+    end
+  end
+
+  step 'gitlab user "Pete"' do
+    create(:user, name: "Pete")
+  end
+
+  step '"Pete" is "Shop" developer' do
+    user = User.find_by(name: "Pete")
+    project = Project.find_by(name: "Shop")
+    project.team << [user, :developer]
+  end
+
+  step 'I go to "Members"' do
+    find(:link, 'Members').trigger('click')
+  end
+
+  step 'I visit project "Shop" settings page' do
+    find(:link, 'Settings').trigger('click')
+  end
+
+  step 'I change "Pete" access level to master' do
+    user = User.find_by(name: "Pete")
+    project_member = @project.project_members.find_by(user_id: user)
+
+    page.within "#project_member_#{project_member.id}" do
+      click_button "Edit access level"
+      select "Master", from: "project_member_access_level"
+      click_button "Save"
+    end
+
+    sleep 0.05
+  end
+
+  step 'I go to "Audit Events"' do
+    find(:link, 'Audit Events').trigger('click')
+  end
 end
--- features/steps/project/project.rb
+++ features/steps/project/project.rb
@@ -112,47 +112,12 @@ class Spinach::Features::Project < Spinach::FeatureSteps
     expect(page).to have_content 'testme'
   end
 
-  step 'gitlab user "Pete"' do
-    create(:user, name: "Pete")
-  end
-
-  step '"Pete" is "Shop" developer' do
-    user = User.find_by(name: "Pete")
-    project = Project.find_by(name: "Shop")
-    project.team << [user, :developer]
-  end
-
   step 'I visit project "Shop" settings page' do
     click_link 'Settings'
   end
 
   step 'I go to "Members"' do
     click_link 'Members'
-  end
-
-  step 'I change "Pete" access level to master' do
-    user = User.find_by(name: "Pete")
-    project_member = @project.project_members.find_by(user_id: user)
-
-    page.within "#project_member_#{project_member.id}" do
-      click_button "Edit access level"
-      select "Master", from: "project_member_access_level"
-      click_button "Save"
-    end
-
-    sleep 0.05
-  end
-
-  step 'I go to "Audit Events"' do
-    find(:link, 'Audit Events').trigger('click')
-  end
-
-  step 'I should see the audit event listed' do
-    page.within('table#audits') do
-      expect(page).to have_content "Change access level from developer to master"
-      expect(page).to have_content(project.owner.name)
-      expect(page).to have_content('Pete')
-    end
   end
 
   step 'I add project tags' do
