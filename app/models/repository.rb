008d1381ce3a3cf322c6eb64ce07576cafc53826
Valery Sizov <vsv2711@gmail.com>
2015-07-21 13:07:39 +0300

    Revert "Revert "Merge branch 'cache-improvement' into '7-13-stable'
    This reverts commit ebb6393d0eda26db10da85f0a57c3d707b4de558.

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -94,18 +94,6 @@ class Repository
     gitlab_shell.rm_tag(path_with_namespace, tag_name)
   end
 
-  def round_commit_count
-    if commit_count > 10000
-      '10000+'
-    elsif commit_count > 5000
-      '5000+'
-    elsif commit_count > 1000
-      '1000+'
-    else
-      commit_count
-    end
-  end
-
   def branch_names
     cache.fetch(:branch_names) { raw_repository.branch_names }
   end
@@ -130,10 +118,29 @@ class Repository
     cache.fetch(:size) { raw_repository.size }
   end
 
-  def expire_cache
+  def cache_keys
     %i(size branch_names tag_names commit_count graph_log
-       readme version contribution_guide changelog license).each do |key|
+       readme version contribution_guide changelog license)
+  end
+
+  def build_cache
+    cache_keys.each do |key|
+      unless cache.exist?(key)
+        send(key)
+      end
+    end
+  end
+
+  def expire_cache
+    cache_keys.each do |key|
       cache.expire(key)
+    end
+  end
+
+  def rebuild_cache
+    cache_keys.each do |key|
+      cache.expire(key)
+      send(key)
     end
   end
 
