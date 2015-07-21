0a61f0b4e240bfcb559fe2a42625c25ce2ea7582
Kamil Trzcinski <ayufan@ayufan.eu>
2015-07-21 16:44:38 +0200

    PIN users to UIDs and monkey patch initctl

---
+++ docker/assets/gitlab-docker.rb
@@ -0,0 +1,16 @@
+# Docker options
+## Prevent Postgres from trying to allocate 25% of total memory
+postgresql['shared_buffers'] = '1MB'
+
+## PIN users to UIDs
+user['uid'] = 998
+user['gid'] = 998
+postgresql['uid'] = 996
+postgresql['gid'] = 996
+redis['uid'] = 997
+redis['gid'] = 997
+web_server['uid'] = 999
+web_server['gid'] = 999
+gitlab_ci['uid'] = 995
+gitlab_ci['gid'] = 995
+
