864e2bb082d588b0a9a938cf4b001768888cad71
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2015-08-06 16:57:04 +0200

    Add tests for abuse report feature
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

---
+++ features/steps/abuse_reports.rb
@@ -0,0 +1,28 @@
+class Spinach::Features::AbuseReports < Spinach::FeatureSteps
+  include SharedAuthentication
+
+  step 'I visit "Mike" user page' do
+    visit user_path(user_mike)
+  end
+
+  step 'I click "Report abuse" button' do
+    click_link 'Report abuse'
+  end
+
+  step 'I fill and submit abuse form' do
+    fill_in 'abuse_report_message', with: 'This user send spam'
+    click_button 'Send report'
+  end
+
+  step 'I should see success message' do
+    page.should have_content 'Thank you for your report'
+  end
+
+  step 'user "Mike" exists' do
+    user_mike
+  end
+
+  def user_mike
+    @user_mike ||= create(:user, name: 'Mike')
+  end
+end
