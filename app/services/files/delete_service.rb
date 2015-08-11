d4cfa0bf766bb99cb6b8e600faee1e90678816b4
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2015-08-11 11:49:09 +0200

    Revert "Refactor web editor"
    This reverts commit dfccb06dda344819989fa8d6a9a3c56c5ca0b65f.
    
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- app/services/files/delete_service.rb
+++ app/services/files/delete_service.rb
@@ -2,8 +2,36 @@ require_relative "base_service"
 
 module Files
   class DeleteService < Files::BaseService
-    def commit
-      repository.remove_file(current_user, @file_path, @commit_message, @target_branch)
+    def execute
+      allowed = ::Gitlab::GitAccess.new(current_user, project).can_push_to_branch?(ref)
+
+      unless allowed
+        return error("You are not allowed to push into this branch")
+      end
+
+      unless repository.branch_names.include?(ref)
+        return error("You can only create files if you are on top of a branch")
+      end
+
+      blob = repository.blob_at_branch(ref, path)
+
+      unless blob
+        return error("You can only edit text files")
+      end
+
+      sha = repository.remove_file(
+        current_user,
+        path,
+        params[:commit_message],
+        ref
+      )
+
+      if sha
+        after_commit(sha)
+        success
+      else
+        error("Your changes could not be committed, because the file has been changed")
+      end
     end
   end
 end
