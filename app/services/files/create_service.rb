fac581b170bebb88a7082a1c141b69bc8fed897f
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2015-08-11 11:49:29 +0200

    Revert "Make web editor work correctly after switch from satellites"
    This reverts commit 435f680b897b892103fa157d4699dbb6d9ecf758.

--- app/services/files/create_service.rb
+++ app/services/files/create_service.rb
@@ -1,7 +1,7 @@
 require_relative "base_service"
 
 module Files
-  class CreateService < Files::BaseService
+  class CreateService < BaseService
     def execute
       allowed = Gitlab::GitAccess.new(current_user, project).can_push_to_branch?(ref)
 
