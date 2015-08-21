gitlabhq/gitlabhq

commit baa157926d432f404a41c31ad6514ff8d5366269

Stricter mkdir's in 'rake gitlab:backup:create'

lib/backup/database.rb
@@ -7,7 +7,11 @@ module Backup
     def initialize
       @config = YAML.load_file(File.join(Rails.root,'config','database.yml'))[Rails.env]
       @db_dir = File.join(Gitlab.config.backup.path, 'db')
-      FileUtils.mkdir_p(@db_dir, mode: 0700)unless Dir.exists?(@db_dir)
+      FileUtils.rm_rf(@db_dir)
+      # Ensure the parent dir of @db_dir exists
+      FileUtils.mkdir_p(Gitlab.config.backup.path)
+      # Fail if somebody raced to create @db_dir before us
+      FileUtils.mkdir(@db_dir, mode: 0700)
     end
 
     def dump
@@ -25,7 +29,6 @@ module Backup
       abort 'Backup failed' unless success
 
       $progress.print 'Compressing database ... '
-      FileUtils.rm_f db_file_name_gz
       success = system('gzip', db_file_name)
       report_success(success)
       abort 'Backup failed: compress error' unless success

lib/backup/repository.rb
@@ -130,7 +130,10 @@ module Backup
 
     def prepare
       FileUtils.rm_rf(backup_repos_path)
-      FileUtils.mkdir_p(backup_repos_path, mode: 0700)
+      # Ensure the parent dir of backup_repos_path exists
+      FileUtils.mkdir_p(Gitlab.config.backup.path)
+      # Fail if somebody raced to create backup_repos_path before us
+      FileUtils.mkdir(backup_repos_path, mode: 0700)
     end
 
     def silent

lib/backup/uploads.rb
@@ -10,7 +10,11 @@ module Backup
 
     # Copy uploads from public/uploads to backup/uploads
     def dump
-      FileUtils.mkdir_p(backup_uploads_dir, mode: 0700)
+      FileUtils.rm_rf(backup_uploads_dir)
+      # Ensure the parent dir of backup_uploads_dir exists
+      FileUtils.mkdir_p(Gitlab.config.backup.path)
+      # Fail if somebody raced to create backup_uploads_dir before us
+      FileUtils.mkdir(backup_uploads_dir, mode: 0700)
       FileUtils.cp_r(app_uploads_dir, backup_dir)
     end
 

commit 4cd06867c76daca698bc4a9d23e1875f4b4866d5

Do not delete the SQL dump too early
The change in baa157926d432f404a41c31ad6514ff8d5366269 broke backup
restore fucnctionality. This would not lead to data loss, but it
prevented the restore script from working. This bug exists only in
7.14.0 release candidate versions, not in 7.13.

Reported in https://github.com/gitlabhq/gitlabhq/issues/9571 .

lib/backup/database.rb
@@ -7,14 +7,15 @@ module Backup
     def initialize
       @config = YAML.load_file(File.join(Rails.root,'config','database.yml'))[Rails.env]
       @db_dir = File.join(Gitlab.config.backup.path, 'db')
+    end
+
+    def dump
       FileUtils.rm_rf(@db_dir)
       # Ensure the parent dir of @db_dir exists
       FileUtils.mkdir_p(Gitlab.config.backup.path)
       # Fail if somebody raced to create @db_dir before us
       FileUtils.mkdir(@db_dir, mode: 0700)
-    end
 
-    def dump
       success = case config["adapter"]
       when /^mysql/ then
         $progress.print "Dumping MySQL database #{config['database']} ... "