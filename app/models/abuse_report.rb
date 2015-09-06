d7812a95cf518dfa1565a0514b87fb40f86949e5
Stan Hu <stanhu@gmail.com>
2015-09-06 07:48:48 -0700

    Re-annotate models

--- app/models/abuse_report.rb
+++ app/models/abuse_report.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: abuse_reports
+#
+#  id          :integer          not null, primary key
+#  reporter_id :integer
+#  user_id     :integer
+#  message     :text
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 class AbuseReport < ActiveRecord::Base
   belongs_to :reporter, class_name: "User"
   belongs_to :user
--- spec/models/abuse_report_spec.rb
+++ spec/models/abuse_report_spec.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: abuse_reports
+#
+#  id          :integer          not null, primary key
+#  reporter_id :integer
+#  user_id     :integer
+#  message     :text
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 require 'rails_helper'
 
 RSpec.describe AbuseReport, type: :model do
