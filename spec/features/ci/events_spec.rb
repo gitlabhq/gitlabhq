29b3279a954825541bb0cfb14b6fe0a92c867c4e
Kamil Trzcinski <ayufan@ayufan.eu>
2015-09-15 22:04:42 +0200

    Fix: features/ci/events_spec.rb once again

--- spec/features/ci/events_spec.rb
+++ spec/features/ci/events_spec.rb
@@ -1,11 +1,13 @@
 require 'spec_helper'
 
 describe "Events" do
+  let(:user)    { create(:user) }
   let(:project) { FactoryGirl.create :ci_project }
-  let(:event) { FactoryGirl.create :ci_admin_event, project: project }
-  
+  let(:event)   { FactoryGirl.create :ci_admin_event, project: project }
+
   before do
-    login_as :user
+    login_as(user)
+    project.gl_project.team << [user, :master]
   end
 
   describe "GET /ci/project/:id/events" do
