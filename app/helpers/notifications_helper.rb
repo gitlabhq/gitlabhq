8a6fb46dff970d599860b06a6c9105c503cbb89c
Patricio Cano <suprnova32@gmail.com>
2015-09-14 19:40:45 -0500

    Corrected `case` indentation to conform with Rubocop

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -15,32 +15,32 @@ module NotificationsHelper
 
   def notification_list_item(notification_level)
     case notification_level
-      when Notification::N_DISABLED
-        content_tag(:li) do
-          link_to '#', class: 'update-notification', data: { notification_level: Notification::N_DISABLED } do
-            icon('microphone-slash fw', text: 'Disabled')
-          end
+    when Notification::N_DISABLED
+      content_tag(:li) do
+        link_to '#', class: 'update-notification', data: { notification_level: Notification::N_DISABLED } do
+          icon('microphone-slash fw', text: 'Disabled')
         end
-      when Notification::N_PARTICIPATING
-        content_tag(:li) do
-          link_to '#', class: 'update-notification', data: { notification_level: Notification::N_PARTICIPATING } do
-            icon('volume-up fw', text: 'Participating')
-          end
+      end
+    when Notification::N_PARTICIPATING
+      content_tag(:li) do
+        link_to '#', class: 'update-notification', data: { notification_level: Notification::N_PARTICIPATING } do
+          icon('volume-up fw', text: 'Participating')
         end
-      when Notification::N_WATCH
-        content_tag(:li) do
-          link_to '#', class: 'update-notification', data: { notification_level: Notification::N_WATCH } do
-            icon('globe fw', text: 'Watch')
-          end
+      end
+    when Notification::N_WATCH
+      content_tag(:li) do
+        link_to '#', class: 'update-notification', data: { notification_level: Notification::N_WATCH } do
+          icon('globe fw', text: 'Watch')
         end
-      when Notification::N_MENTION
-        content_tag(:li) do
-          link_to '#', class: 'update-notification', data: { notification_level: Notification::N_MENTION }  do
-            icon('at fw', text: 'Mention')
-          end
+      end
+    when Notification::N_MENTION
+      content_tag(:li) do
+        link_to '#', class: 'update-notification', data: { notification_level: Notification::N_MENTION }  do
+          icon('at fw', text: 'Mention')
         end
-      else
-        # do nothing
+      end
+    else
+      # do nothing
     end
   end
 end
