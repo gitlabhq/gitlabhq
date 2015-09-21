2f88459297d9fe8a33f608bd924f0bd5ad40161b
Robert Speicher <rspeicher@gmail.com>
2015-09-21 15:35:29 -0400

    Ensure raise_error expectations provide arguments

--- spec/services/ci/create_project_service_spec.rb
+++ spec/services/ci/create_project_service_spec.rb
@@ -15,7 +15,8 @@ describe Ci::CreateProjectService do
 
     context 'without project dump' do
       it 'should raise exception' do
-        expect { service.execute(current_user, '', '') }.to raise_error
+        expect { service.execute(current_user, '', '') }.
+          to raise_error(NoMethodError)
       end
     end
 
