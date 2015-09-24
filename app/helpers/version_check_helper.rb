55d771104c2e5b61dbbca3eeb3f2eec9542d7394
Douwe Maan <douwe@gitlab.com>
2015-09-24 07:51:52 +0000

    Merge branch 'rs-help-page' into 'master'
    Help page improvements
    
    - Only signed-in users can see version information
    - Add "Community Edition" text so the header is
      less barren when version information is hidden
    - Minor copy edits
    - Make all "Quick help" link text the entire body
      of the link. Prior, it wasn't obvious which part
      of each item was an actual link.
    
    Closes #2721 
    
    See merge request !1415

--- app/helpers/version_check_helper.rb
+++ app/helpers/version_check_helper.rb
@@ -1,6 +1,6 @@
 module VersionCheckHelper
   def version_status_badge
-    if Rails.env.production?
+    if Rails.env.production? && current_application_settings.version_check_enabled
       image_tag VersionCheck.new.url
     end
   end
