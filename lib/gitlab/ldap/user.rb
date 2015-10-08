ada2c4552861455c6da09b74feaf0e8cf96c29bd
Drew Blessing <drew@gitlab.com>
2015-10-08 14:59:46 -0500

    Fix ldap email downcasing bug

--- lib/gitlab/ldap/user.rb
+++ lib/gitlab/ldap/user.rb
@@ -35,7 +35,7 @@ module Gitlab
       end
 
       def find_by_email
-        ::User.find_by(email: auth_hash.email)
+        ::User.find_by(email: auth_hash.email.downcase)
       end
 
       def update_user_attributes
