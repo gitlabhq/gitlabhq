1554786c6ac49b452697d2f7a3e8daf6e3ac36d3
Yorick Peterse <yorickpeterse@gmail.com>
2015-10-13 11:49:01 +0200

    Eager load various issue/note associations
    This ensures we don't end up running N+1 queries for the objects in the
    affected collections.

--- app/models/note.rb
+++ app/models/note.rb
@@ -59,6 +59,7 @@ class Note < ActiveRecord::Base
   scope :fresh, ->{ order(created_at: :asc, id: :asc) }
   scope :inc_author_project, ->{ includes(:project, :author) }
   scope :inc_author, ->{ includes(:author) }
+  scope :inc_associations, ->{ includes(:author, :noteable, :updated_by) }
 
   serialize :st_diff
   before_create :set_diff, if: ->(n) { n.line_code.present? }
