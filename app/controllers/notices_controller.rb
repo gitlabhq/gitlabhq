a697e53f0fa2d10b5b8be0f6d4c10aa2ddf630e5
Dean Perry <dean@deanperry.net>
2015-10-17 15:17:11 +0100

    skip_before_action

--- app/controllers/notices_controller.rb
+++ app/controllers/notices_controller.rb
@@ -3,7 +3,7 @@ class NoticesController < ApplicationController
   class ParamsError < StandardError; end
 
   skip_before_action :authenticate_user!, only: :create
-  skip_before_filter :verify_authenticity_token, only: :create
+  skip_before_action :verify_authenticity_token, only: :create
 
   rescue_from ParamsError, with: :bad_params
 
