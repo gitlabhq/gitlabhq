be3399868cbcfdce85daa6fdf53a079bc163056a
Zeger-Jan van de Weg <mail@zjvandeweg.nl>
2015-11-02 19:02:08 +0100

    Minor refactoring on Gitlab::Git

--- lib/gitlab/git.rb
+++ lib/gitlab/git.rb
@@ -1,8 +1,9 @@
 module Gitlab
   module Git
-    BLANK_SHA = '0' * 40
-    TAG_REF_PREFIX = "refs/tags/"
-    BRANCH_REF_PREFIX = "refs/heads/"
+    # '0' * 40 -- this was easyer to freeze
+    BLANK_SHA = "0000000000000000000000000000000000000000".freeze
+    TAG_REF_PREFIX = "refs/tags/".freeze
+    BRANCH_REF_PREFIX = "refs/heads/".freeze
 
     class << self
       def ref_name(ref)
