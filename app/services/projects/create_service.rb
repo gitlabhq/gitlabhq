a0519818cb4ff90d6a1d66b6360ec94f05bc79ec
Douwe Maan <douwe@gitlab.com>
2015-11-11 16:26:43 +0100

    Tweak code formatting.

--- app/services/projects/create_service.rb
+++ app/services/projects/create_service.rb
@@ -55,7 +55,9 @@ module Projects
         @project.save
 
         if @project.persisted? && !@project.import?
-          raise 'Failed to create repository' unless @project.create_repository
+          unless @project.create_repository
+            raise 'Failed to create repository'
+          end
         end
       end
 
@@ -94,9 +96,7 @@ module Projects
         @project.team << [current_user, :master, current_user]
       end
 
-      if @project.import?
-        @project.import_start
-      end
+      @project.import_start if @project.import?
     end
   end
 end
