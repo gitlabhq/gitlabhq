05335a3c8584c48a9317bd0919eccee6948de742
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2015-11-13 16:07:27 +0100

    Create milestones in the group
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- app/controllers/groups/milestones_controller.rb
+++ app/controllers/groups/milestones_controller.rb
@@ -1,14 +1,32 @@
 class Groups::MilestonesController < Groups::ApplicationController
   before_action :authorize_group_milestone!, only: :update
+  before_action :group
 
   def index
-    project_milestones = case params[:state]
-                         when 'all'; state
-                         when 'closed'; state('closed')
-                         else state('active')
-                         end
+    project_milestones =
+      case params[:state]
+      when 'all'; state
+      when 'closed'; state('closed')
+      else state('active')
+      end
+
     @group_milestones = Milestones::GroupService.new(project_milestones).execute
     @group_milestones = Kaminari.paginate_array(@group_milestones).page(params[:page]).per(PER_PAGE)
+  end
+
+  def new
+    @group_milestone = OpenStruct.new(title: nil, description: nil)
+  end
+
+  def create
+    project_ids = params[:milestone][:project_ids]
+    title = milestone_params[:title]
+
+    @group.projects.where(id: project_ids).each do |project|
+      Milestones::CreateService.new(project, current_user, milestone_params).execute
+    end
+
+    redirect_to group_milestone_path(@group, title.parameterize, title: title)
   end
 
   def show
@@ -50,5 +68,13 @@ class Groups::MilestonesController < Groups::ApplicationController
 
   def authorize_group_milestone!
     return render_404 unless can?(current_user, :admin_group, group)
+  end
+
+  def milestone_params
+    params.require(:milestone).permit(
+      :title,
+      :description,
+      :due_date
+    )
   end
 end
