84c71e5d827f1a0b225b7f6e9eeabe00a9c3eadd
Marin Jankovski <maxlazio@gmail.com>
2015-11-16 15:59:15 +0100

    Add unique to oid index for lfs object.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151114113410) do
+ActiveRecord::Schema.define(version: 20151116144118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -430,8 +430,7 @@ ActiveRecord::Schema.define(version: 20151114113410) do
     t.string   "file"
   end
 
-  add_index "lfs_objects", ["oid", "size"], name: "index_lfs_objects_on_oid_and_size", using: :btree
-  add_index "lfs_objects", ["oid"], name: "index_lfs_objects_on_oid", using: :btree
+  add_index "lfs_objects", ["oid"], name: "index_lfs_objects_on_oid", unique: true, using: :btree
 
   create_table "lfs_objects_projects", force: true do |t|
     t.integer  "lfs_object_id", null: false
