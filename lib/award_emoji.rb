2d1fcd802a291cc8e26fbbe5874e20316a5f93af
Valery Sizov <valery@gitlab.com>
2015-11-18 17:38:15 +0200

    Emoji: refactoring

---
+++ lib/award_emoji.rb
@@ -0,0 +1,6 @@
+class AwardEmoji
+  EMOJI_LIST = ["+1", "-1", "100", "blush", "heart", "smile", "rage",
+    "beers", "disappointed", "ok_hand",
+    "helicopter", "shit", "airplane", "alarm_clock",
+    "ambulance", "anguished", "two_hearts", "wink"]
+end
