7495af95c3c91df3bd1f58687b43f14c2cd41e7c
Marin Jankovski <maxlazio@gmail.com>
2015-11-24 15:57:28 +0100

    Also fallback to a default value if none is set.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,9 +3,9 @@
 require 'gitlab/current_settings'
 include Gitlab::CurrentSettings
 
-# allow it to fail: it may to do so when create_from_defaults is executed before migrations are actually done
+# allow it to fail: it may do so when create_from_defaults is executed before migrations are actually done
 begin
-  Settings.gitlab['session_expire_delay'] = current_application_settings.session_expire_delay
+  Settings.gitlab['session_expire_delay'] = current_application_settings.session_expire_delay || 10080
 rescue
   Settings.gitlab['session_expire_delay'] ||= 10080
 end
