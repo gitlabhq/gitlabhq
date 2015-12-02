74a81cbfe3e7b6fb6c3b7a620b07905f19971a86
Douwe Maan <douwe@gitlab.com>
2015-12-02 17:30:50 +0100

    Fix branch rendering

--- app/helpers/merge_requests_helper.rb
+++ app/helpers/merge_requests_helper.rb
@@ -56,12 +56,14 @@ module MergeRequestsHelper
   end
 
   def source_branch_with_namespace(merge_request)
+    branch = link_to(merge_request.source_branch, namespace_project_commits_path(merge_request.source_project.namespace, merge_request.source_project, merge_request.source_branch))
+
     if merge_request.for_fork?
       namespace = link_to(merge_request.source_project_namespace,
         project_path(merge_request.source_project))
-      namespace + ":#{merge_request.source_branch}"
+      namespace + ":" + branch
     else
-      merge_request.source_branch
+      branch
     end
   end
 
