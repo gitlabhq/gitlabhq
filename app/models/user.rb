e6dadea3891358ac7ed34dbb31eac403c193fcdc
Valery Sizov <valery@gitlab.com>
2015-12-02 08:50:00 +0200

    git rid of deprecated warnings

--- app/models/user.rb
+++ app/models/user.rb
@@ -794,4 +794,9 @@ class User < ActiveRecord::Base
     Gitlab::SQL::Union.new([personal_projects.select(:id), groups.select(:id),
                             other.select(:id)])
   end
+
+  # Added according to https://github.com/plataformatec/devise/blob/7df57d5081f9884849ca15e4fde179ef164a575f/README.md#activejob-integration
+  def send_devise_notification(notification, *args)
+    devise_mailer.send(notification, self, *args).deliver_later
+  end
 end
