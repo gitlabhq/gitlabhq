0a081e7eff9730beebd4bea1eb40873d907b6293
Marin Jankovski <maxlazio@gmail.com>
2015-12-03 14:59:10 +0100

    If a user clicks on the LFS object, it should be served if the user has access to the object.

--- app/models/lfs_object.rb
+++ app/models/lfs_object.rb
@@ -5,4 +5,14 @@ class LfsObject < ActiveRecord::Base
   validates :oid, presence: true, uniqueness: true
 
   mount_uploader :file, LfsObjectUploader
+
+  MATCH_FROM_POINTER_REGEX = "(?<=sha256:)([0-9a-f]{64})"
+
+  def storage_project(project)
+    if project && project.forked?
+      project.forked_from_project
+    else
+      project
+    end
+  end
 end
