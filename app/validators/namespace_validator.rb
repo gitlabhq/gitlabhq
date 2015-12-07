2379c8beeac600c3352e33fda0c2b4f4f39c8b84
Robert Speicher <rspeicher@gmail.com>
2015-12-07 16:29:39 -0500

    Inline Gitlab::Blacklist in NamespaceValidator

--- app/validators/namespace_validator.rb
+++ app/validators/namespace_validator.rb
@@ -2,21 +2,49 @@
 #
 # Custom validator for GitLab namespace values.
 #
-# Values are checked for formatting and exclusion from `Gitlab::Blacklist.path`.
+# Values are checked for formatting and exclusion from a list of reserved path
+# names.
 class NamespaceValidator < ActiveModel::EachValidator
+  RESERVED = %w(
+    admin
+    all
+    assets
+    ci
+    dashboard
+    files
+    groups
+    help
+    hooks
+    issues
+    merge_requests
+    notes
+    profile
+    projects
+    public
+    repository
+    s
+    search
+    services
+    snippets
+    teams
+    u
+    unsubscribes
+    users
+  ).freeze
+
   def validate_each(record, attribute, value)
     unless value =~ Gitlab::Regex.namespace_regex
       record.errors.add(attribute, Gitlab::Regex.namespace_regex_message)
     end
 
-    if blacklisted?(value)
+    if reserved?(value)
       record.errors.add(attribute, "#{value} is a reserved name")
     end
   end
 
   private
 
-  def blacklisted?(value)
-    Gitlab::Blacklist.path.include?(value)
+  def reserved?(value)
+    RESERVED.include?(value)
   end
 end
