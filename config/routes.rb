9957734481ee9537ab902b0079c66da5fb4fdfca
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2015-12-09 00:15:56 +0100

    Add group contribution statistics page
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -386,6 +386,7 @@ Rails.application.routes.draw do
     end
 
     scope module: :groups do
+      resource :stats, only: [:show]
       resource :ldap, only: [] do
         member do
           put :reset_access
