13f44822d97e643b55047cf619f7bb1f354f165b
Douwe Maan <douwe@gitlab.com>
2015-12-08 12:29:48 +0100

    Move CombinedPipeline methods around

--- lib/gitlab/markdown/combined_pipeline.rb
+++ lib/gitlab/markdown/combined_pipeline.rb
@@ -7,16 +7,18 @@ module Gitlab
         Class.new(Pipeline) do
           const_set :PIPELINES, pipelines
 
+          def self.pipelines
+            self::PIPELINES
+          end
+
           def self.filters
             pipelines.flat_map(&:filters)
           end
 
           def self.transform_context(context)
-            pipelines.reduce(context) { |context, pipeline| pipeline.transform_context(context) }
-          end
-
-          def self.pipelines
-            self::PIPELINES
+            pipelines.reduce(context) do |context, pipeline|
+              pipeline.transform_context(context)
+            end
           end
         end
       end
--- lib/gitlab/markdown/pipeline/full_pipeline.rb
+++ lib/gitlab/markdown/pipeline/full_pipeline.rb
@@ -3,7 +3,7 @@ require 'gitlab/markdown'
 module Gitlab
   module Markdown
     class FullPipeline < CombinedPipeline.new(PlainMarkdownPipeline, GfmPipeline)
-      
+
     end
   end
 end
