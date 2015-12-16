5f67bf74f460c9dd1e9061757a23a2bdf447fca3
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2015-12-16 19:07:27 +0100

    Improve CI builds fixtures

--- db/fixtures/development/14_builds.rb
+++ db/fixtures/development/14_builds.rb
@@ -1,12 +1,25 @@
 Gitlab::Seeder.quiet do
-  project = Project.first
-  commits = project.repository.commits('master', nil, 10)
-  commits_sha = commits.map { |commit| commit.raw.id }
-  ci_commits = commits_sha.map do |sha|
-    project.ensure_ci_commit(sha)
-  end
+  Project.all.sample(5).each do |project|
+    commits = project.repository.commits('master', nil, 10)
+    commits_sha = commits.map { |commit| commit.raw.id }
+    ci_commits = commits_sha.map do |sha|
+      project.ensure_ci_commit(sha)
+    end
 
-  ci_commits.each do |ci_commit|
-    ci_commit.create_builds('master', nil, User.first)
+    ci_commits.each do |ci_commit|
+      attributes = { type: 'Ci::Build', name: 'test build',
+                     commands: "$ build command", stage: 'test',
+                     stage_idx: 1, ref: 'master', user_id: User.first,
+                     gl_project_id: project.id, options: '---- opts',
+                     status: %w(running pending success failed canceled).sample,
+                     commit_id: ci_commit.id }
+
+      begin
+        Ci::Build.create!(attributes)
+        print '.'
+      rescue ActiveRecord::RecordInvalid
+        print 'F'
+      end
+    end
   end
 end
