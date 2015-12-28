de6b276edc3ffb881f67dd6207b279977da42e96
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2015-12-28 18:45:15 -0200

    Doesn't accept null value for max file size on Git hooks

--- app/models/git_hook.rb
+++ app/models/git_hook.rb
@@ -2,6 +2,7 @@ class GitHook < ActiveRecord::Base
   belongs_to :project
 
   validates :project, presence: true, unless: "is_sample?"
+  validates :max_file_size, numericality: { greater_than_or_equal_to: 0, only_integer: true }
 
   def commit_validation?
     commit_message_regex.present? ||
