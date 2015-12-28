d985d08a19d945ad4615b319c9346ec88abf2f97
Jacob Schatz <jacobschatz@Jacobs-MBP.fios-router.home>
2015-12-28 17:37:03 -0500

    adds proper `it` for multi line. adds `find` with correct id for button.

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -81,10 +81,10 @@ feature 'Project', feature: true do
     end
 
     it { expect(page).to have_content('You have Master access to this project.') }
-    it { 
-      find('#project-settings').click
+    it 'click project-settings and find leave project' do
+      find('#project-settings-button').click
       expect(page).to have_link('Leave Project') 
-    }
+    end
   end
 
   def remove_with_confirm(button_text, confirm_with)
