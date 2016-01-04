0e60282e36faab8b0f4faee0b71716987df28416
Robert Speicher <rspeicher@gmail.com>
2016-01-04 18:46:43 -0500

    Redirect back to user profile page after abuse report
    Now the reporter will see the fruits of their labor, namely, the red
    icon!

--- app/controllers/abuse_reports_controller.rb
+++ app/controllers/abuse_reports_controller.rb
@@ -14,7 +14,7 @@ class AbuseReportsController < ApplicationController
       end
 
       message = "Thank you for your report. A GitLab administrator will look into it shortly."
-      redirect_to root_path, notice: message
+      redirect_to @abuse_report.user, notice: message
     else
       render :new
     end
@@ -23,6 +23,9 @@ class AbuseReportsController < ApplicationController
   private
 
   def report_params
-    params.require(:abuse_report).permit(:user_id, :message)
+    params.require(:abuse_report).permit(%i(
+      message
+      user_id
+    ))
   end
 end
