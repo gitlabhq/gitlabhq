4b4fdf58c7f358fb06bed6dae166b758465850d0
Stan Hu <stanhu@gmail.com>
2016-01-04 23:18:23 -0800

    Fix Error 500 when visiting build page of project with nil runners_token
    Properly ensure that the token exists and add defensively check for a
    non-nil value.
    
    Closes #4294

--- app/models/project.rb
+++ app/models/project.rb
@@ -50,6 +50,7 @@ class Project < ActiveRecord::Base
   include Sortable
   include AfterCommitQueue
   include CaseSensitivity
+  include TokenAuthenticatable
 
   extend Gitlab::ConfigHelper
 
@@ -193,10 +194,7 @@ class Project < ActiveRecord::Base
     if: ->(project) { project.avatar.present? && project.avatar_changed? }
   validates :avatar, file_size: { maximum: 200.kilobytes.to_i }
 
-  before_validation :set_runners_token_token
-  def set_runners_token_token
-    self.runners_token = SecureRandom.hex(15) if self.runners_token.blank?
-  end
+  add_authentication_token_field :runners_token
 
   mount_uploader :avatar, AvatarUploader
 
@@ -899,5 +897,9 @@ class Project < ActiveRecord::Base
   def visibility_level_allowed?(level)
     return true unless forked?
     Gitlab::VisibilityLevel.allowed_fork_levels(forked_from_project.visibility_level).include?(level.to_i)
+  end
+
+  def runners_token
+    ensure_runners_token!
   end
 end
