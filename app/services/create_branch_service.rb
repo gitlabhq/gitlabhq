d295f74375cc41bbe2bd6010e12187baccd761b7
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2016-01-11 10:17:27 +0100

    Merge branch 'fix-duplicate-branch-activity' of https://gitlab.com/stanhu/gitlab-ce

--- app/services/create_branch_service.rb
+++ app/services/create_branch_service.rb
@@ -18,7 +18,6 @@ class CreateBranchService < BaseService
     if new_branch
       push_data = build_push_data(project, current_user, new_branch)
 
-      EventCreateService.new.push(project, current_user, push_data)
       project.execute_hooks(push_data.dup, :push_hooks)
       project.execute_services(push_data.dup, :push_hooks)
 
