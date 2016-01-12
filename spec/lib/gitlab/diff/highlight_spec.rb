f547e733d1f8acf2c8ae82835b91ae166cf95b16
Rubén Dávila <rdavila84@gmail.com>
2016-01-12 17:49:11 -0500

    Add more specs. #3945

--- spec/lib/gitlab/diff/highlight_spec.rb
+++ spec/lib/gitlab/diff/highlight_spec.rb
@@ -30,6 +30,24 @@ describe Gitlab::Diff::Highlight, lib: true do
         expect(diff_lines[0].text).to eq('@@ -6,12 +6,18 @@ module Popen')
         expect(diff_lines[22].text).to eq('@@ -19,6 +25,7 @@ module Popen')
       end
+
+      it 'should highlight unchanged lines' do
+        code = %Q{ <span id="LC7" class="line">  <span class="k">def</span> <span class="nf">popen</span><span class="p">(</span><span class="n">cmd</span><span class="p">,</span> <span class="n">path</span><span class="o">=</span><span class="kp">nil</span><span class="p">)</span></span>\n}
+
+        expect(diff_lines[2].text).to eq(code)
+      end
+
+      it 'should highlight added lines' do
+        code = %Q{+<span id="LC9" class="line">      <span class="k">raise</span> <span class="no">RuntimeError</span><span class="p">,</span> <span class="s2">&quot;System commands must be given as an array of strings&quot;</span></span>\n}
+
+        expect(diff_lines[5].text).to eq(code)
+      end
+
+      it 'should highlight removed lines' do
+        code = %Q{-<span id="LC9" class="line">      <span class="k">raise</span> <span class="s2">&quot;System commands must be given as an array of strings&quot;</span></span>\n}
+
+        expect(diff_lines[4].text).to eq(code)
+      end
     end
   end
 
