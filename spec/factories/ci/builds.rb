6a98fb03e708070641f9fce0eaad761e859a5099
Tomasz Maczukin <tomasz@maczukin.pl>
2016-01-14 12:04:44 +0100

    Remove hard-coded id from builds factory

--- spec/factories/ci/builds.rb
+++ spec/factories/ci/builds.rb
@@ -61,8 +61,7 @@ FactoryGirl.define do
     end
 
     factory :ci_build_with_trace do
-      id 999
-      after(:build) do  |build, evaluator|
+      after(:create) do  |build, evaluator|
         build.trace = 'BUILD TRACE'
       end
     end
