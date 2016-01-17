d9629a0cf03b80c8074b2ffa003d541193ca7097
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2016-01-17 16:57:42 +0100

    Add specs for CI Lint button exposed on a builds page

--- features/steps/project/builds/summary.rb
+++ features/steps/project/builds/summary.rb
@@ -4,11 +4,18 @@ class Spinach::Features::ProjectBuildsSummary < Spinach::FeatureSteps
   include SharedBuilds
   include RepoHelpers
 
-  step 'I see summary for build' do
+  step 'I see details of a build' do
     expect(page).to have_content "Build ##{@build.id}"
   end
 
   step 'I see build trace' do
     expect(page).to have_css '#build-trace'
+  end
+
+  step 'I see button to CI Lint Tool' do
+    page.within('.controls') do
+      ci_lint_tool_link = page.find_link('CI Lint Tool')
+      expect(ci_lint_tool_link[:href]).to eq ci_lint_path
+    end
   end
 end
