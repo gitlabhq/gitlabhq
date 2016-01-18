acd1c3f6bee1f641055cf06a101c4c801596070d
Valery Sizov <vsv2711@gmail.com>
2016-01-18 13:41:33 +0200

    ES: refactoring

--- lib/gitlab/elastic/snippet_search_results.rb
+++ lib/gitlab/elastic/snippet_search_results.rb
@@ -1,15 +1,6 @@
 module Gitlab
   module Elastic
-    class SnippetSearchResults < SearchResults
-      include SnippetsHelper
-
-      attr_reader :limit_snippet_ids
-
-      def initialize(limit_snippet_ids, query)
-        @limit_snippet_ids = limit_snippet_ids
-        @query = query
-      end
-
+    class SnippetSearchResults < ::Gitlab::SnippetSearchResults
       def objects(scope, page = nil)
         case scope
         when 'snippet_titles'
@@ -25,18 +16,6 @@ module Gitlab
         else
           super
         end
-      end
-
-      def total_count
-        @total_count ||= snippet_titles_count + snippet_blobs_count
-      end
-
-      def snippet_titles_count
-        @snippet_titles_count ||= snippet_titles.total_count
-      end
-
-      def snippet_blobs_count
-        @snippet_blobs_count ||= snippet_blobs.total_count
       end
 
       private
@@ -55,10 +34,6 @@ module Gitlab
         }
 
         Snippet.elastic_search_code(query, options: opt)
-      end
-
-      def default_scope
-        'snippet_blobs'
       end
     end
   end
