eddc6d9705e2686a06793c689d9fdbc5ed264aa2
Tomasz Maczukin <tomasz@maczukin.pl>
2016-01-19 23:10:00 +0100

    Remove unimplemented specs and factories

--- spec/factories/blocking_ips.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :blocking_ip do
-    ip "MyString"
-    description "MyText"
-    type ""
-  end
-end
