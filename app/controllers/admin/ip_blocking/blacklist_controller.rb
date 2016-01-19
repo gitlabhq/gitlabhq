efd5c95c36666af992195e0545dbebe321d0f304
Tomasz Maczukin <tomasz@maczukin.pl>
2016-01-19 06:04:39 +0100

    Add IP Whitelist/Blacklist management

--- app/controllers/admin/ip_blocking/blacklist_controller.rb
+++ app/controllers/admin/ip_blocking/blacklist_controller.rb
@@ -1,4 +1,63 @@
 class Admin::IpBlocking::BlacklistController < Admin::ApplicationController
+  before_action :set_ip, only: [:index]
+  before_action :get_ip, only: [:edit, :update, :destroy]
+
   def index
+    @ips = BlacklistedIp.all.order('id DESC')
+    if params[:search] && params[:search].empty? == false
+      @ips = @ips.where(ip: params[:search].to_s)
+    end
+
+    @ips = @ips.page(params[:page]).per(30)
+  end
+
+  def create
+    attrs = ip_attributes
+    attrs[:user] = current_user
+
+    @ip = BlacklistedIp.create(attrs)
+
+    if @ip.valid?
+      redirect_to admin_ip_blocking_blacklist_index_path,
+                  notice: 'Added new IP address to the blacklist'
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @ip.update_attributes(ip_attributes)
+    if @ip.valid?
+      redirect_to admin_ip_blocking_blacklist_index_path,
+                  notice: 'Updated IP address'
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @ip.destroy
+    redirect_to admin_ip_blocking_blacklist_index_path,
+                notice: 'Removed IP address from the blacklist'
+  end
+
+  private
+
+  def ip_attributes
+    params.require(:blacklisted_ip).permit(
+      :ip,
+      :description
+    )
+  end
+
+  def set_ip
+    @ip = BlacklistedIp.new
+  end
+
+  def get_ip
+    @ip = BlacklistedIp.find(params[:id].to_i)
   end
 end
--- app/models/blocking_ip.rb
+++ app/models/blocking_ip.rb
@@ -3,7 +3,9 @@ class BlockingIp < ActiveRecord::Base
 
   validates :ip,
             presence: true,
-            uniqueness: true
+            uniqueness: { message: 'has already been taken by IP Blacklist or IP Whitelist' },
+            format: { with: /\A(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\z/,
+                      message: 'allows only valid IP addresses' }
 
   scope :whitelisted, -> { where(type: 'WhitelistedIp') }
   scope :blacklisted, -> { where(type: 'BlacklistedIp') }
