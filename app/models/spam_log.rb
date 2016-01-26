64c9768bd6916edfc1e43907ca5e976524b8d51b
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2016-01-26 16:59:23 -0200

    Use truncate helper on spam logs list

--- app/models/spam_log.rb
+++ app/models/spam_log.rb
@@ -2,12 +2,4 @@ class SpamLog < ActiveRecord::Base
   belongs_to :user
 
   validates :user, presence: true
-
-  def truncated_description
-    if description.present? && description.length > 100
-      return description[0..100] + "..."
-    end
-
-    description
-  end
 end
--- features/steps/admin/spam_logs.rb
+++ features/steps/admin/spam_logs.rb
@@ -4,15 +4,25 @@ class Spinach::Features::AdminSpamLogs < Spinach::FeatureSteps
   include SharedAdmin
 
   step 'I should see list of spam logs' do
-    page.should have_content("Spam Logs")
-    spam_log = SpamLog.first
-    page.should have_content spam_log.title
-    page.should have_content spam_log.description
-    page.should have_link("Remove user")
-    page.should have_link("Block user")
+    expect(page).to have_content('Spam Logs')
+    expect(page).to have_content spam_log.source_ip
+    expect(page).to have_content spam_log.noteable_type
+    expect(page).to have_content 'N'
+    expect(page).to have_content spam_log.title
+    expect(page).to have_content truncate(spam_log.description)
+    expect(page).to have_link('Remove user')
+    expect(page).to have_link('Block user')
   end
 
   step 'spam logs exist' do
     create(:spam_log)
+  end
+
+  def spam_log
+    @spam_log ||= SpamLog.first
+  end
+
+  def truncate(description)
+    "#{spam_log.description[0...97]}..."
   end
 end
