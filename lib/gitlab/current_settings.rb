869b4d7c6ad5c025f310eeecce7293bf5bbc4926
Rémy Coutable <remy@rymai.me>
2016-01-28 10:14:21 +0100

    Only create the defaults ApplicationSetting when DB is fully migrated
    Return a fake application settings OpenStruct when this is not the case.
    Also, use ActiveRecord::Base.connection_pool.active_connection? instead
    of ActiveRecord::Base.connection.active? to avoid driver exception.

--- lib/gitlab/current_settings.rb
+++ lib/gitlab/current_settings.rb
@@ -4,11 +4,14 @@ module Gitlab
       key = :current_application_settings
 
       RequestStore.store[key] ||= begin
+        settings = nil
+
         if connect_to_db?
-          ApplicationSetting.current || ApplicationSetting.create_from_defaults
-        else
-          fake_application_settings
+          settings = ApplicationSetting.current
+          settings ||= ApplicationSetting.create_from_defaults unless ActiveRecord::Migrator.needs_migration?
         end
+
+        settings || fake_application_settings
       end
     end
 
@@ -18,35 +21,29 @@ module Gitlab
         default_branch_protection: Settings.gitlab['default_branch_protection'],
         signup_enabled: Settings.gitlab['signup_enabled'],
         signin_enabled: Settings.gitlab['signin_enabled'],
+        twitter_sharing_enabled: Settings.gitlab['twitter_sharing_enabled'],
         gravatar_enabled: Settings.gravatar['enabled'],
         sign_in_text: Settings.extra['sign_in_text'],
         restricted_visibility_levels: Settings.gitlab['restricted_visibility_levels'],
         max_attachment_size: Settings.gitlab['max_attachment_size'],
         session_expire_delay: Settings.gitlab['session_expire_delay'],
-        import_sources: Settings.gitlab['import_sources'],
+        default_project_visibility: Settings.gitlab.default_projects_features['visibility_level'],
+        default_snippet_visibility: Settings.gitlab.default_projects_features['visibility_level'],
+        restricted_signup_domains: Settings.gitlab['restricted_signup_domains'],
+        import_sources: ['github','bitbucket','gitlab','gitorious','google_code','fogbugz','git'],
         shared_runners_enabled: Settings.gitlab_ci['shared_runners_enabled'],
         max_artifacts_size: Settings.artifacts['max_size'],
+        require_two_factor_authentication: false,
+        two_factor_grace_period: 48
       )
     end
 
     private
 
     def connect_to_db?
-      use_db = if ENV['USE_DB'] == "false"
-                 false
-               else
-                 true
-               end
-
-      use_db && ActiveRecord::Base.connected? &&
-                # The following condition is important: if a migrations adds a
-                # column to the application_settings table and a validation in
-                # the ApplicationSetting uses this new column we might end-up in
-                # a vicious circle where migration crash before being done.
-                # See https://gitlab.com/gitlab-org/gitlab-ce/issues/12606 for
-                # a thorough explanation.
-                !ActiveRecord::Migrator.needs_migration? &&
-                ActiveRecord::Base.connection.table_exists?('application_settings')
+      ENV['USE_DB'] != 'false' &&
+      ActiveRecord::Base.connection_pool.active_connection? &&
+      ActiveRecord::Base.connection.table_exists?('application_settings')
 
     rescue ActiveRecord::NoDatabaseError
       false
