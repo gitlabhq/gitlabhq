b636f83e5910d10a954464370efe42cc14a097ba
Douwe Maan <douwe@selenight.nl>
2016-01-28 14:49:21 +0100

    Move blame group logic to dedicated class

--- app/controllers/projects/blame_controller.rb
+++ app/controllers/projects/blame_controller.rb
@@ -8,32 +8,6 @@ class Projects::BlameController < Projects::ApplicationController
 
   def show
     @blob = @repository.blob_at(@commit.id, @path)
-    @blame = group_blame_lines
-  end
-
-  def group_blame_lines
-    blame = Gitlab::Git::Blame.new(@repository, @commit.id, @path)
-
-    prev_sha = nil
-    groups = []
-    current_group = nil
-
-    highlighted_lines = Gitlab::Highlight.highlight(@blob.name, @blob.data).lines
-    i = 0
-    blame.each do |commit, line|
-      line = highlighted_lines[i].html_safe
-      if prev_sha && prev_sha == commit.sha
-        current_group[:lines] << line
-      else
-        groups << current_group if current_group.present?
-        current_group = { commit: commit, lines: [line] }
-      end
-
-      prev_sha = commit.sha
-      i += 1
-    end
-
-    groups << current_group if current_group.present?
-    groups
+    @blame_groups = Gitlab::Blame.new(@blob, @commit).groups
   end
 end
