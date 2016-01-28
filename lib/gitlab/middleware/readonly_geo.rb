d2716cde81ca819a4110fdbb19a7930da8345dcc
Gabriel Mazetto <brodock@gmail.com>
2016-01-28 18:05:20 -0200

    Geo: Redirect when using disallowed requests on readonly

--- lib/gitlab/middleware/readonly_geo.rb
+++ lib/gitlab/middleware/readonly_geo.rb
@@ -1,21 +1,22 @@
 module Gitlab
   module Middleware
     class ReadonlyGeo
-      READONLY_METHODS = %w(PATCH PUT DELETE)
+      DISALLOWED_METHODS = %w(PATCH PUT DELETE)
 
       def initialize(app)
         @app = app
       end
 
       def call(env)
-        if READONLY_METHODS.include?(env['REQUEST_METHOD']) && Gitlab::Geo.readonly?
+        @env = env
+
+        if disallowed_request? && Gitlab::Geo.readonly?
           Rails.logger.debug('Gitlab Geo: preventing possible non readonly operation')
 
-          rflash = rack_flash(env)
-          rflash.alert= 'You are using Gitlab Geo'
-          env['rack.session']['flash'] = rflash.to_session_value
+          rack_flash.alert = 'You cannot do writing operations on a readonly Gitlab Geo instance'
+          rack_session['flash'] = rack_flash.to_session_value
 
-          #TODO: should redirect to last visited page or root url
+          return [301, { 'Location' => last_visited_url}, [] ]
         end
 
         @app.call(env)
@@ -23,8 +24,25 @@ module Gitlab
 
       private
 
-      def rack_flash(env)
-        ActionDispatch::Flash::FlashHash.from_session_value(env['rack.session'])
+      def disallowed_request?
+        DISALLOWED_METHODS.include?(@env['REQUEST_METHOD'])
+      end
+
+      def rack_flash
+        @rack_flash ||= ActionDispatch::Flash::FlashHash.from_session_value(rack_session)
+      end
+
+      def rack_session
+        @env['rack.session']
+      end
+
+      def request
+        @request ||= Rack::Request.new(@env)
+      end
+
+      def last_visited_url
+        Rails.logger.debug("SESSION: #{rack_session.inspect}")
+        @env['HTTP_REFERER'] || rack_session['user_return_to'] || Rails.application.routes.url_helpers.root_url
       end
     end
   end
