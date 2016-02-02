4105f292e1c9d9c9416914fcd41f9c982b4a80fb
Robert Speicher <rspeicher@gmail.com>
2016-02-01 21:29:37 -0500

    Display database type and version in Administration dashboard
    Closes #12900

--- lib/gitlab/database.rb
+++ lib/gitlab/database.rb
@@ -1,16 +1,23 @@
 module Gitlab
   module Database
+    def self.adapter_name
+      connection.adapter_name
+    end
+
     def self.mysql?
-      ActiveRecord::Base.connection.adapter_name.downcase == 'mysql2'
+      adapter_name.downcase == 'mysql2'
     end
 
     def self.postgresql?
-      ActiveRecord::Base.connection.adapter_name.downcase == 'postgresql'
+      adapter_name.downcase == 'postgresql'
+    end
+
+    def self.version
+      database_version.match(/\A(?:PostgreSQL |)([^\s]+).*\z/)[1]
     end
 
     def true_value
-      case ActiveRecord::Base.connection.adapter_name.downcase
-      when 'postgresql'
+      if self.class.postgresql?
         "'t'"
       else
         1
@@ -18,12 +25,31 @@ module Gitlab
     end
 
     def false_value
-      case ActiveRecord::Base.connection.adapter_name.downcase
-      when 'postgresql'
+      if self.class.postgresql?
         "'f'"
       else
         0
       end
+    end
+
+    private
+
+    def self.connection
+      ActiveRecord::Base.connection
+    end
+
+    def self.database_version
+      row = connection.execute("SELECT VERSION()").first
+
+      if postgresql?
+        row['version']
+      else
+        row.first
+      end
+    end
+
+    def connection
+      self.class.connection
     end
   end
 end
