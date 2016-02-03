ccd99efe64b546a0f5e6bd774479cde2861287ef
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2016-02-03 22:48:21 +0100

    Update test after changes build page css
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- features/steps/project/builds/summary.rb
+++ features/steps/project/builds/summary.rb
@@ -13,7 +13,7 @@ class Spinach::Features::ProjectBuildsSummary < Spinach::FeatureSteps
   end
 
   step 'I see button to CI Lint' do
-    page.within('.controls') do
+    page.within('.nav-controls') do
       ci_lint_tool_link = page.find_link('CI Lint')
       expect(ci_lint_tool_link[:href]).to eq ci_lint_path
     end
