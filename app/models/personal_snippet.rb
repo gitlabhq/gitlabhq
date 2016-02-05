a4a4e260d9bccfa704b9c2931fac2ab6be4971ce
Valery Sizov <valery@gitlab.com>
2016-02-05 12:35:37 +0200

    ES: fixed bug in snippet indexing

--- app/models/personal_snippet.rb
+++ app/models/personal_snippet.rb
@@ -16,4 +16,7 @@
 #
 
 class PersonalSnippet < Snippet
+  # Elastic search configuration (it does not support STI)
+  document_type 'snippet'
+  index_name [Rails.application.class.parent_name.downcase, 'snippet', Rails.env].join('-')
 end
