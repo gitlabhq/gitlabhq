b123171d3da752c084ef7bfa82dbed962b3e5168
Kirill Zaitsev <kirik910@gmail.com>
2016-02-06 17:20:21 +0300

    Add new data to project in push, issue, merge-request and note webhooks data
    - Add `avatar_url`, `description`, `git_ssh_url`, `git_http_url`,
      `path_with_namespace` and `default_branch` in `project` in push, issue,
      merge-request and note webhooks data
    - Deprecate the `ssh_url` in favor of `git_ssh_url` and `http_url` in
      favor of `git_http_url` in `project` for push, issue, merge-request and
      note webhooks data
    - Deprecate the `repository` key in push, issue, merge-request and
      note webhooks data, use `project` instead

--- app/models/project.rb
+++ app/models/project.rb
@@ -738,11 +738,20 @@ class Project < ActiveRecord::Base
   def hook_attrs
     {
       name: name,
-      ssh_url: ssh_url_to_repo,
-      http_url: http_url_to_repo,
+      description: description,
       web_url: web_url,
+      avatar_url: avatar_url,
+      git_ssh_url: ssh_url_to_repo,
+      git_http_url: http_url_to_repo,
       namespace: namespace.name,
-      visibility_level: visibility_level
+      visibility_level: visibility_level,
+      path_with_namespace: path_with_namespace,
+      default_branch: default_branch,
+      # Backward compatibility
+      homepage: web_url,
+      url: url_to_repo,
+      ssh_url: ssh_url_to_repo,
+      http_url: http_url_to_repo
     }
   end
 
