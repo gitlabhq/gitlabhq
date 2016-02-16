917081fe9dd3da2995faf8f5555ed120ec4c4ea5
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2016-02-16 18:42:46 -0200

    Fix rubocop offenses

--- app/controllers/dashboard/tasks_controller.rb
+++ app/controllers/dashboard/tasks_controller.rb
@@ -1,11 +1,11 @@
 class Dashboard::TasksController < Dashboard::ApplicationController
   def index
     @tasks = case params[:state]
-      when 'done'
-        current_user.tasks.done
-      else
-        current_user.tasks.pending
-      end
+             when 'done'
+               current_user.tasks.done
+             else
+               current_user.tasks.pending
+             end
 
     @tasks = @tasks.page(params[:page]).per(PER_PAGE)
   end
