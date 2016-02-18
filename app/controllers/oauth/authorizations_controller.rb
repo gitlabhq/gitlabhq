9597920c6371db5555415705643ad26026d198fb
James Lopez <james@jameslopez.es>
2016-02-18 18:47:35 +0100

    attempting to fix omniauth problem

--- app/controllers/oauth/authorizations_controller.rb
+++ app/controllers/oauth/authorizations_controller.rb
@@ -7,6 +7,7 @@ class Oauth::AuthorizationsController < Doorkeeper::AuthorizationsController
     if pre_auth.authorizable?
       if skip_authorization? || matching_token?
         auth = authorization.authorize
+        reset_session
         redirect_to auth.redirect_uri
       else
         render "doorkeeper/authorizations/new"
