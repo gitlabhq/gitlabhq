12fcfde9c3b75f709f203769b05925347fc2d175
Gabriel Mazetto <brodock@gmail.com>
2016-02-22 08:14:08 -0300

    Fixed GeoNode url parsing to fix Gitlab::Geo.current_node again

--- app/models/geo_node.rb
+++ app/models/geo_node.rb
@@ -24,6 +24,8 @@ class GeoNode < ActiveRecord::Base
   validates :primary, uniqueness: { message: 'primary node already exists' }, if: :primary
   validates :schema, inclusion: %w(http https)
 
+  after_destroy :destroy_orphaned_geo_node_key
+
   def uri
     relative_url = relative_url_root[0] == '/' ? relative_url_root[1..-1] : relative_url_root
     URI.parse("#{schema}://#{host}:#{port}/#{relative_url}")
@@ -38,10 +40,18 @@ class GeoNode < ActiveRecord::Base
     self.schema = new_uri.scheme
     self.host = new_uri.host
     self.port = new_uri.port
-    self.relative_url_root = new_uri.path
+    self.relative_url_root = new_uri.path if new_uri.path != '/'
   end
 
   def notify_url
     URI::join(uri, "#{uri.path}/", 'api/geo/refresh_projects').to_s
+  end
+
+  private
+
+  def destroy_orphaned_geo_node_key
+    return unless self.geo_node_key.destroyed_when_orphaned? && self.geo_node_key.orphaned?
+
+    self.geo_node_key.destroy
   end
 end
