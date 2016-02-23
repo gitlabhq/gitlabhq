fe88ad87d4700179ca343ec17925b0ec254f93ad
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2016-02-23 19:28:37 -0300

    Add an option to user make an issue confidential

--- app/controllers/projects/issues_controller.rb
+++ app/controllers/projects/issues_controller.rb
@@ -163,7 +163,7 @@ class Projects::IssuesController < Projects::ApplicationController
 
   def issue_params
     params.require(:issue).permit(
-      :title, :assignee_id, :position, :description, :weight,
+      :title, :assignee_id, :position, :description, :confidential, :weight,
       :milestone_id, :state_event, :task_num, label_ids: []
     )
   end
