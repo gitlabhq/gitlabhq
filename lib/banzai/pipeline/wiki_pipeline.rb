bc43ad71ef2c1ab6cf56d94ac93df0bdc0e2b741
Robert Speicher <rspeicher@gmail.com>
2016-02-23 18:05:10 -0500

    Replace Gollum `[[_TOC_]]` tag with result of TableOfContentsFilter
    Closes #2494

--- lib/banzai/pipeline/wiki_pipeline.rb
+++ lib/banzai/pipeline/wiki_pipeline.rb
@@ -4,7 +4,11 @@ module Banzai
   module Pipeline
     class WikiPipeline < FullPipeline
       def self.filters
-        super.insert(1, Filter::GollumTagsFilter)
+        @filters ||= begin
+                       filters = super
+                       toc = filters.index(Filter::TableOfContentsFilter)
+                       filters.insert(toc + 1, Filter::GollumTagsFilter)
+                     end
       end
     end
   end
