d2e97b5d2c40c583adee23c38d31e0fc9142d1b5
Rémy Coutable <remy@rymai.me>
2016-03-02 15:48:05 +0100

    Update schema.rb according to latest migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160222153918) do
+ActiveRecord::Schema.define(version: 20160302141317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,10 +27,11 @@ ActiveRecord::Schema.define(version: 20160222153918) do
   create_table "appearances", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.string   "header_logo"
     t.string   "logo"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.integer  "updated_by"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "header_logo"
   end
 
   create_table "application_settings", force: :cascade do |t|
