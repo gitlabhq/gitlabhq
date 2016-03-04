f95f242cfad4fb0032caecea47964d78c1f652bc
Phil Hughes <me@iamphill.com>
2016-03-04 13:52:29 +0000

    Updated tests

--- features/steps/project/merge_requests.rb
+++ features/steps/project/merge_requests.rb
@@ -417,7 +417,7 @@ class Spinach::Features::ProjectMergeRequests < Spinach::FeatureSteps
 
     page.within(".js-discussion-note-form") do
       fill_in "note_note", with: "Line is wrong on here"
-      click_button "Add Comment"
+      click_button "Comment"
     end
   end
 
@@ -509,7 +509,7 @@ class Spinach::Features::ProjectMergeRequests < Spinach::FeatureSteps
   def leave_comment(message)
     page.within(".js-discussion-note-form", visible: true) do
       fill_in "note_note", with: message
-      click_button "Add Comment"
+      click_button "Comment"
     end
     page.within(".notes_holder", visible: true) do
       expect(page).to have_content message
