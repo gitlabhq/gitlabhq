9e00a237161679b0d6afdefd246e0a7bf209510c
Yorick Peterse <yorickpeterse@gmail.com>
2016-03-04 11:39:00 +0100

    Clean up ProjectsFinder for getting user projects
    We don't need the extra layer of nesting of UNION queries here (as
    User#authorized_projects already returns a UNION'd query).

--- app/models/user.rb
+++ app/models/user.rb
@@ -442,6 +442,11 @@ class User < ActiveRecord::Base
     Project.where("projects.id IN (#{projects_union.to_sql})")
   end
 
+  # Returns all the project relations
+  def project_relations
+    [personal_projects, groups_projects, projects]
+  end
+
   def owned_projects
     @owned_projects ||=
       Project.where('namespace_id IN (?) OR namespace_id = ?',
@@ -830,9 +835,7 @@ class User < ActiveRecord::Base
   private
 
   def projects_union
-    Gitlab::SQL::Union.new([personal_projects.select(:id),
-                            groups_projects.select(:id),
-                            projects.select(:id)])
+    Gitlab::SQL::Union.new(project_relations.map { |r| r.select(:id) })
   end
 
   def ci_projects_union
