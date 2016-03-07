5352ec2e21ba72d77a542b158ce1a98a1a3a9389
Zeger-Jan van de Weg <zegerjan@gitlab.com>
2016-03-07 11:45:14 +0100

    Fix denting and spec

--- app/controllers/concerns/continue_params.rb
+++ app/controllers/concerns/continue_params.rb
@@ -5,7 +5,7 @@ module ContinueParams
     continue_params = params[:continue]
     return nil unless continue_params
 
-    continue_params       = continue_params.permit(:to, :notice, :notice_now)
+    continue_params = continue_params.permit(:to, :notice, :notice_now)
     return unless continue_params[:to] && continue_params[:to].start_with?('/')
 
     continue_params
