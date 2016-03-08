99d87d8698437f87c21ced1d5a7fab4f9d253493
James Lopez <james@jameslopez.es>
2016-03-08 18:17:57 +0100

    WIP - added wiki repo bundler

--- app/services/projects/import_export/command_line_util.rb
+++ app/services/projects/import_export/command_line_util.rb
@@ -6,6 +6,12 @@ module Projects
         _output, status = Gitlab::Popen.popen(cmd)
         status.zero?
       end
+
+      def git_bundle(git_bin_path: Gitlab.config.git.bin_path, repo_path:, bundle_path:)
+        cmd = %W(#{git_bin_path} --git-dir=#{repo_path} bundle create #{bundle_path} --all)
+        _output, status = Gitlab::Popen.popen(cmd)
+        status.zero?
+      end
     end
   end
 end
---
+++ app/services/projects/import_export/wiki_repo_bundler.rb
@@ -0,0 +1,30 @@
+module Projects
+  module ImportExport
+    class WikiRepoBundler < RepoBundler
+      def bundle
+        @wiki = ProjectWiki.new(@project)
+        return false if !wiki?
+        @full_path = File.join(@export_path, project_filename)
+        bundle_to_disk
+      end
+
+      def bundle_to_disk
+        FileUtils.mkdir_p(@export_path)
+        git_bundle(repo_path: path_to_repo, bundle_path: @full_path)
+      rescue
+        #TODO: handle error
+        false
+      end
+
+      private
+
+      def path_to_repo
+        @wiki.repository.path_to_repo
+      end
+
+      def wiki?
+        File.exists?(@wiki.repository.path_to_repo) && !@wiki.repository.empty?
+      end
+    end
+  end
+end
