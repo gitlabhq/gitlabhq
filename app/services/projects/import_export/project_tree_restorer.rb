2cc6aaec4910be1c37b4eeab028ffcdb4c8b9b7f
James Lopez <james@jameslopez.es>
2016-03-09 18:42:04 +0100

    WIP (broken) - playing with import stuff

--- app/services/projects/import_export/project_tree_restorer.rb
+++ app/services/projects/import_export/project_tree_restorer.rb
@@ -1,30 +1,36 @@
 module Projects
   module ImportExport
     class ProjectTreeRestorer
-      attr_reader :full_path
+      attr_reader :project
 
-      def initialize(path: )
+      def initialize(path: , user: user)
         @path = path
+        @user = user
       end
 
+      #TODO deal with ID issues.
+      #TODO refactor this method
       def restore
         json = IO.read(@path)
         tree_hash = ActiveSupport::JSON.decode(json)
+        relation_hash = {}
         ImportExport.project_tree.each do |relation|
           next if tree_hash[relation.to_s].empty?
-          tree_hash[relation.to_s] = create_relation(relation, tree_hash[relation.to_s])
+          relation_hash[relation.to_s] = create_relation(relation, tree_hash[relation.to_s])
         end
-        project = Project.new(tree_hash)
-        project
+        project_params = tree_hash.delete_if { |_key, value | value.is_a?(Array)}
+        @project = ::Projects::CreateService.new(@user, project_params).execute
+        @project.saved?
       end
 
       private
 
-      def create_relation(relation, tree_hash)
-        Projects::ImportExport::RelationFactory.create(
-          relation_sym: relation, relation_hash: tree_hash[relation.to_s])
+      def create_relation(relation, relation_hash_list)
+        relation_hash_list.map do |relation_hash|
+          Projects::ImportExport::RelationFactory.create(
+            relation_sym: relation, relation_hash: relation_hash)
+        end
       end
-
     end
   end
 end
--- app/services/projects/import_export/relation_factory.rb
+++ app/services/projects/import_export/relation_factory.rb
@@ -3,8 +3,12 @@ module Projects
     module RelationFactory
       extend self
 
-      def create(relation_sym: , relation_hash:)
+      OVERRIDES = { snippets: :project_snippets }
+
+      def create(relation_sym:, relation_hash:)
+        relation_sym = parse_relation_sym(relation_sym)
         klass = relation_class(relation_sym)
+        relation_hash.delete('id') #screw IDs for now
         klass.new(relation_hash)
       end
 
@@ -12,6 +16,10 @@ module Projects
 
       def relation_class(relation_sym)
         relation_sym.to_s.classify.constantize
+      end
+
+      def parse_relation_sym(relation_sym)
+        OVERRIDES[relation_sym] || relation_sym
       end
     end
   end
