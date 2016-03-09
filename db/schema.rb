fe4d3abead763d627379bc348797dae81dfd5c17
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2016-03-09 19:22:43 +0000

    Merge branch 'fix-todos' into 'master'
    Fix error 500 in Todos
    
    Closes #14095
    
    Closes #14075
    
    Closes #14109
    
    Closes #14151
    
    See merge request !3141

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160222153918) do
+ActiveRecord::Schema.define(version: 20160309140734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
