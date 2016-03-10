f0ce83b1902e78b8adc819db49022ed98d12342e
James Lopez <james@jameslopez.es>
2016-03-10 13:04:11 +0100

    modify model creation to use services when they are available

--- app/services/projects/import_export/relation_factory.rb
+++ app/services/projects/import_export/relation_factory.rb
@@ -1,24 +1,43 @@
 module Projects
   module ImportExport
-    module RelationFactory
-      extend self
+    class RelationFactory
 
       OVERRIDES = { snippets: :project_snippets }
 
-      def create(relation_sym:, relation_hash:)
-        relation_sym = parse_relation_sym(relation_sym)
-        klass = relation_class(relation_sym)
-        relation_hash.delete('id') #screw IDs for now
-        klass.new(relation_hash)
+      def self.create(*args)
+        new(*args).create
+      end
+
+      def initialize(relation_sym:, relation_hash:, project:, user:)
+        @relation_sym = parsed_relation_sym(relation_sym)
+        @relation_hash = relation_hash
+        @project = project
+        @user = user
+      end
+
+      def create
+        @relation_hash.delete('id')
+        init_service_or_class
       end
 
       private
 
-      def relation_class(relation_sym)
-        relation_sym.to_s.classify.constantize
+      def init_service_or_class
+        # Attempt service first
+        relation_service.new(@project, @user, @relation_hash).execute
+      rescue NameError
+        relation_class.new(@relation_hash)
       end
 
-      def parse_relation_sym(relation_sym)
+      def relation_service
+        "#{@relation_sym.to_s.classify}::CreateService".constantize
+      end
+
+      def relation_class
+        @relation_sym.to_s.classify.constantize
+      end
+
+      def parsed_relation_sym(relation_sym)
         OVERRIDES[relation_sym] || relation_sym
       end
     end
