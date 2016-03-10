a28c84b4cab48d4ca7cc39985e1e67155a8a95d5
Jacob Vosmaer <contact@jacobvosmaer.nl>
2016-03-10 12:55:06 +0100

    Improve test descriptions

--- spec/lib/gitlab/exclusive_lease_spec.rb
+++ spec/lib/gitlab/exclusive_lease_spec.rb
@@ -1,17 +1,17 @@
 require 'spec_helper'
 
 describe Gitlab::ExclusiveLease do
-  it 'is exclusive' do
+  it 'cannot obtain twice before the lease has expired' do
     lease = Gitlab::ExclusiveLease.new(unique_key, timeout: 3600)
     expect(lease.try_obtain).to eq(true)
     expect(lease.try_obtain).to eq(false)
   end
 
-  it 'expires' do
+  it 'can obtain after the lease has expired' do
     timeout = 1
     lease = Gitlab::ExclusiveLease.new(unique_key, timeout: timeout)
-    lease.try_obtain
-    sleep(2 * timeout)
+    lease.try_obtain # start the lease
+    sleep(2 * timeout) # lease should have expired now
     expect(lease.try_obtain).to eq(true)
   end
 
