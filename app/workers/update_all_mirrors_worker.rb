944e71731ce64c307b5a6d33c8f8a98d0bf97a49
Robert Speicher <rspeicher@gmail.com>
2016-03-14 15:29:24 -0400

    Before updating all mirrors, clear out any that appear to be stuck
    Closes https://gitlab.com/gitlab-org/gitlab-ee/issues/247

--- app/workers/update_all_mirrors_worker.rb
+++ app/workers/update_all_mirrors_worker.rb
@@ -2,6 +2,19 @@ class UpdateAllMirrorsWorker
   include Sidekiq::Worker
 
   def perform
+    fail_stuck_mirrors!
+
     Project.mirror.each(&:update_mirror)
+  end
+
+  def fail_stuck_mirrors!
+    stuck = Project.mirror.
+      with_import_status(:started).
+      where('mirror_last_update_at < ?', 1.day.ago)
+
+    stuck.find_each(batch_size: 50) do |project|
+      project.import_fail
+      project.update_attribute(:import_error, 'The mirror update took too long to complete.')
+    end
   end
 end
