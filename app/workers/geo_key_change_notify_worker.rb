d48249b2005501c69529e7f48bf19c91ca062c99
Gabriel Mazetto <brodock@gmail.com>
2016-03-21 19:18:17 -0300

    Refactored Backoff strategy to a concern

--- app/workers/geo_key_change_notify_worker.rb
+++ app/workers/geo_key_change_notify_worker.rb
@@ -1,25 +1,10 @@
 class GeoKeyChangeNotifyWorker
   include Sidekiq::Worker
+  include GeoDynamicBackoff
 
-  sidekiq_options queue: :default, retry: 55
-
-  sidekiq_retry_in do |count|
-    count <= 30 ? linear_backoff_strategy(count) : geometric_backoff_strategy(count)
-  end
+  sidekiq_options queue: :default
 
   def perform(key_id, key, action)
     Geo::NotifyKeyChangeService.new(key_id, key, action).execute
-  end
-
-  private
-
-  def linear_backoff_strategy(count)
-    rand(1..20) + count
-  end
-
-  def geometric_backoff_strategy(count)
-    # This strategy is based on the original one from sidekiq
-    count = count-30 # we must start counting after 30
-    (count ** 4) + 15 + (rand(30)*(count+1))
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,8 @@ module Gitlab
                                    #{config.root}/app/models/hooks
                                    #{config.root}/app/models/concerns
                                    #{config.root}/app/models/project_services
-                                   #{config.root}/app/models/members))
+                                   #{config.root}/app/models/members
+                                   #{config.root}/app/workers/concerns))
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
