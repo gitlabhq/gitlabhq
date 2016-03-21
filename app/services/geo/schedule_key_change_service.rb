6983be2d5a0606d10a1941449a3c964154360c84
Gabriel Mazetto <brodock@gmail.com>
2016-03-21 14:44:22 -0300

    Fix API specs

--- app/services/geo/schedule_key_change_service.rb
+++ app/services/geo/schedule_key_change_service.rb
@@ -2,9 +2,9 @@ module Geo
   class ScheduleKeyChangeService
     attr_reader :id, :action
 
-    def initialize(key_id, change)
-      @id = key_id
-      @action = change
+    def initialize(key_change)
+      @id = key_change['id']
+      @action = key_change['change']
     end
 
     def execute
