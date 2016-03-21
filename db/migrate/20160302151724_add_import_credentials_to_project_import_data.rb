8d7d9c8daa61d58a17fea648771a1bb6c9341304
James Lopez <james@jameslopez.es>
2016-03-21 13:23:35 +0100

    added missing column for current att_encrypted version

--- db/migrate/20160302151724_add_import_credentials_to_project_import_data.rb
+++ db/migrate/20160302151724_add_import_credentials_to_project_import_data.rb
@@ -2,5 +2,6 @@ class AddImportCredentialsToProjectImportData < ActiveRecord::Migration
   def change
     add_column :project_import_data, :encrypted_credentials, :text
     add_column :project_import_data, :encrypted_credentials_iv, :text
+    add_column :project_import_data, :encrypted_credentials_salt, :text
   end
 end
