e989d12b0768f0caa7ea1a429a2182e26bbf225a
Rémy Coutable <remy@rymai.me>
2016-03-23 09:09:58 +0100

    Use respond_to instead of a conditional to paginate milestones

--- app/controllers/dashboard/milestones_controller.rb
+++ app/controllers/dashboard/milestones_controller.rb
@@ -2,14 +2,15 @@ class Dashboard::MilestonesController < Dashboard::ApplicationController
   include GlobalMilestones
 
   before_action :projects
-  before_action :milestones, only: [:index]
   before_action :milestone, only: [:show]
 
   def index
     respond_to do |format|
-      format.html
+      format.html do
+        @milestones = Kaminari.paginate_array(milestones).page(params[:page])
+      end
       format.json do
-        render json: @milestones
+        render json: milestones
       end
     end
   end
