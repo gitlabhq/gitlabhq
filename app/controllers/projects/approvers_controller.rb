1ec9ef597e0cf47fcb15d342c85b23c6adc08c84
Rémy Coutable <remy@rymai.me>
2016-03-23 12:11:26 +0100

    Ensure Projects::ApproversController inherits from Projects::ApplicationController
    Fix gitlab-org/gitlab-ce#14503.

--- app/controllers/projects/approvers_controller.rb
+++ app/controllers/projects/approvers_controller.rb
@@ -1,4 +1,4 @@
-class Projects::ApproversController < ApplicationController
+class Projects::ApproversController < Projects::ApplicationController
   def destroy
     if params[:merge_request_id]
       authorize_create_merge_request!
@@ -8,7 +8,7 @@ class Projects::ApproversController < ApplicationController
       authorize_admin_project!
       project.approvers.find(params[:id]).destroy
     end
-  
+
     redirect_back_or_default(default: { action: 'index' })
   end
 end
