aae577f92141f3ec973b4dd362452502274147f5
Jacob Vosmaer <contact@jacobvosmaer.nl>
2016-03-24 17:34:56 +0100

    Add test for gitlab_shell.upload_pack config setting

--- spec/requests/git_http_spec.rb
+++ spec/requests/git_http_spec.rb
@@ -52,10 +52,23 @@ describe 'Git HTTP requests', lib: true do
       let(:env) { {} }
 
       context "when the project is public" do
-        it "responds with status 200" do
+        before do
           project.update_attribute(:visibility_level, Project::PUBLIC)
+        end
+        
+        it "responds with status 200" do
           download(path, env) do |response|
             expect(response.status).to eq(200)
+          end
+        end
+        
+        context 'but git-upload-pack is disabled' do
+          it "responds with status 404" do
+            allow(Gitlab.config.gitlab_shell).to receive(:upload_pack).and_return(false)
+
+            download(path, env) do |response|
+              expect(response.status).to eq(404)
+            end
           end
         end
       end
