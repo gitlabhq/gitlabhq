ccc64676a97d251658190bfb62e97b166cee4db1
Phil Hughes <me@iamphill.com>
2016-03-30 17:13:18 +0100

    Commits comment test update

--- features/steps/shared/note.rb
+++ features/steps/shared/note.rb
@@ -2,7 +2,7 @@ module SharedNote
   include Spinach::DSL
 
   step 'I delete a comment' do
-    page.within('.notes') do
+    page.within('.main-notes-list') do
       find('.note').hover
       find(".js-note-delete").click
     end
