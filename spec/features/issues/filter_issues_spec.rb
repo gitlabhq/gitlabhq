1e87679702d57a0c01f6352851fd144fd213ea56
Arinde Eniola <eniolaarinde1@gmail.com>
2016-04-06 03:42:27 +0100

    complete the tests

--- spec/features/issues/filter_issues_spec.rb
+++ spec/features/issues/filter_issues_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 describe 'Filter issues', feature: true do
 
   let!(:project)   { create(:project) }
-  let!(:issue)     { create(:issue, project: project) }
   let!(:user)      { create(:user)}
+  let!(:milestone) { create(:milestone, project: project) }
+  let!(:label)     { create(:label, project: project) }
 
   before do
     project.team << [user, :master]
@@ -39,6 +40,79 @@ describe 'Filter issues', feature: true do
         find('.issues-state-filters a', text: "All").click
 
         expect(find('.js-assignee-search .dropdown-toggle-text')).to have_content(user.name)
+      end
+    end
+  end
+
+  describe 'Filter issues for milestone from issues#index' do
+
+    before do
+      visit namespace_project_issues_path(project.namespace, project)
+
+      find('.js-milestone-select').click
+
+      find('.milestone-filter .dropdown-content a', text: milestone.title).click
+
+      sleep 2
+    end
+
+    context 'milestone', js: true do
+      it 'should update to current milestone' do
+        expect(find('.js-milestone-select .dropdown-toggle-text')).to have_content(milestone.title)
+      end
+
+      it 'should not change when closed link is clicked' do
+        find('.issues-state-filters a', text: "Closed").click
+
+        expect(find('.js-milestone-select .dropdown-toggle-text')).to have_content(milestone.title)
+      end
+
+
+      it 'should not change when all link is clicked' do
+        find('.issues-state-filters a', text: "All").click
+
+        expect(find('.js-milestone-select .dropdown-toggle-text')).to have_content(milestone.title)
+      end
+    end
+  end
+
+  describe 'Filter issues for assignee and label from issues#index' do
+
+    before do
+      visit namespace_project_issues_path(project.namespace, project)
+
+      find('.js-assignee-search').click
+
+      find('.dropdown-menu-user-link', text: user.username).click
+
+      sleep 2
+
+      find('.js-label-select').click
+
+      find('.dropdown-menu-labels .dropdown-content a', text: label.title).click
+
+      sleep 2
+    end
+
+    context 'assignee and label', js: true do
+      it 'should update to current assignee and label' do
+        expect(find('.js-assignee-search .dropdown-toggle-text')).to have_content(user.name)
+        expect(find('.js-label-select .dropdown-toggle-text')).to have_content(label.title)
+      end
+
+      it 'should not change when closed link is clicked' do
+        find('.issues-state-filters a', text: "Closed").click
+
+        expect(find('.js-assignee-search .dropdown-toggle-text')).to have_content(user.name)
+        expect(find('.js-label-select .dropdown-toggle-text')).to have_content(label.title)
+      end
+
+
+      it 'should not change when all link is clicked' do
+        find('.issues-state-filters a', text: "All").click
+
+        expect(find('.js-assignee-search .dropdown-toggle-text')).to have_content(user.name)
+        expect(find('.js-label-select .dropdown-toggle-text')).to have_content(label.title)
       end
     end
   end
