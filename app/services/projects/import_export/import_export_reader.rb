acfa0b69d5370749cee9768bf79b58daf6d916a4
James Lopez <james@jameslopez.es>
2016-04-07 13:19:57 +0200

    continuing to refactor config, added spec and fixed a few problems

--- app/services/projects/import_export/import_export_reader.rb
+++ app/services/projects/import_export/import_export_reader.rb
@@ -7,6 +7,8 @@ module Projects
         { only: atts_only[:project], include: build_hash(tree) }
       end
 
+      private
+
       def config
         @config ||= YAML.load_file('app/services/projects/import_export/import_export.yml')
       end
@@ -27,20 +29,55 @@ module Projects
         array.map { |el| el.is_a?(Hash) ? process_include(el) : el }
       end
 
-      def process_include(hash)
-        included_classes_hash = {}
+      def process_include(hash, included_classes_hash = {})
         hash.values.flatten.each do |value|
-          value = value.is_a?(Hash) ? process_include(hash) : value
-          new_hash = { :include => value }
-          new_hash.merge!(check_only(value))
-          included_classes_hash[hash.keys.first] = new_hash
+          current_key, value = process_current_class(hash, included_classes_hash, value)
+          if included_classes_hash[current_key]
+            add_class(current_key, included_classes_hash, value)
+          else
+            add_new_class(current_key, included_classes_hash, value)
+          end
         end
         included_classes_hash
       end
 
+      def process_current_class(hash, included_classes_hash, value)
+        value = value.is_a?(Hash) ? process_include(hash, included_classes_hash) : value
+        current_key = hash.keys.first
+        current_key_only = check_only_and_except(current_key)
+        included_classes_hash[current_key] ||= current_key_only unless current_key_only.empty?
+        return current_key, value
+      end
+
+      def add_new_class(current_key, included_classes_hash, value)
+        new_hash = { :include => value }
+        new_hash.merge!(check_only_and_except(value))
+        included_classes_hash[current_key] = new_hash
+      end
+
+      def add_class(current_key, included_classes_hash, value)
+        check_only_hash = check_only_and_except(value)
+        value = { value => check_only_hash } unless check_only_hash.empty?
+        old_values = included_classes_hash[current_key][:include]
+        included_classes_hash[current_key][:include] = ([old_values] + [value]).compact.flatten
+      end
+
+      def check_only_and_except(value)
+        check_only(value).merge(check_except(value))
+      end
+
       def check_only(value)
-        key = value.is_a?(Hash) ? value.keys.first : value
+        key = key_from_hash(value)
         atts_only[key].nil? ? {} : { only: atts_only[key] }
+      end
+
+      def check_except(value)
+        key = key_from_hash(value)
+        atts_except[key].nil? ? {} : { except: atts_except[key] }
+      end
+
+      def key_from_hash(value)
+        value.is_a?(Hash) ? value.keys.first : value
       end
     end
   end
