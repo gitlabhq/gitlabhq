61a62e00e3b08e6ed962b029564e3a2446e169fd
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2016-04-12 12:57:39 -0300

    Fix specs for Projects::NotificationSettingsController

--- spec/controllers/projects/notification_settings_controller_spec.rb
+++ spec/controllers/projects/notification_settings_controller_spec.rb
@@ -8,34 +8,6 @@ describe Projects::NotificationSettingsController do
     project.team << [user, :developer]
   end
 
-  describe '#create' do
-    context 'when not authorized' do
-      it 'redirects to sign in page' do
-        post :create,
-             namespace_id: project.namespace.to_param,
-             project_id: project.to_param,
-             notification_setting: { level: :participating }
-
-        expect(response).to redirect_to(new_user_session_path)
-      end
-    end
-
-    context 'when authorized' do
-      before do
-        sign_in(user)
-      end
-
-      it 'returns success' do
-        post :create,
-            namespace_id: project.namespace.to_param,
-            project_id: project.to_param,
-            notification_setting: { level: :participating }
-
-        expect(response.status).to eq 200
-      end
-    end
-  end
-
   describe '#update' do
     context 'when not authorized' do
       it 'redirects to sign in page' do
