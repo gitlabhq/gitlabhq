251a78022d12c62ff738540d4104bbf0730ef234
Kamil Trzcinski <ayufan@ayufan.eu>
2016-04-13 15:58:22 +0200

    Cleanup changes

--- spec/helpers/ci_status_helper_spec.rb
+++ spec/helpers/ci_status_helper_spec.rb
@@ -6,7 +6,7 @@ describe CiStatusHelper do
   let(:success_commit) { double("Ci::Commit", status: 'success') }
   let(:failed_commit) { double("Ci::Commit", status: 'failed') }
 
-  describe 'ci_status_icon' do
+  describe 'ci_icon_for_status' do
     it { expect(helper.ci_icon_for_status(success_commit.status)).to include('fa-check') }
     it { expect(helper.ci_icon_for_status(failed_commit.status)).to include('fa-close') }
   end
