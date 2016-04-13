daa29729cc64c4c5ca150993bb2375f4c838b9f4
Kamil Trzcinski <ayufan@ayufan.eu>
2016-04-13 15:40:26 +0200

    Add indexes concurrently on PostgreSQL

--- db/migrate/20160412174954_add_ci_commit_indexes.rb
+++ db/migrate/20160412174954_add_ci_commit_indexes.rb
@@ -1,7 +1,13 @@
 class AddCiCommitIndexes < ActiveRecord::Migration
+  disable_ddl_transaction!
+
   def change
-    add_index :ci_commits, [:gl_project_id, :sha]
-    add_index :ci_commits, [:gl_project_id, :status]
-    add_index :ci_commits, [:status]
+    add_index :ci_commits, [:gl_project_id, :sha], index_options
+    add_index :ci_commits, [:gl_project_id, :status], index_options
+    add_index :ci_commits, [:status], index_options
+  end
+
+  def index_options
+    { algorithm: :concurrently } if Gitlab::Database.postgresql?
   end
 end
