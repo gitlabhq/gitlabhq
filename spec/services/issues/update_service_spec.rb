8dc82011f5bb17c0792e432c86d75e3bd294fc30
Grzegorz Bizon <grzegorz@gitlab.com>
2016-04-22 06:51:40 +0000

    Merge branch 'fix/private-labels-permissions' into 'master'
    Fix vulnerability that leaks private labels and milestones
    
    This fixes vulnerability that leaks information about private labels and milestones because of  insecure direct object reference in issueable create service.
    This affects merge requests and issues.
    
    See https://gitlab.com/gitlab-org/gitlab-ce/issues/15439
    
    This MR introduces additional check that rejects labels and milestone that does not belong to the same project issue/merg request does.
    
    `IssuableBaseService` may benefit from encapsulating filters in separate class/module, which then may improve coherency in this class.
    
    Closes https://gitlab.com/gitlab-org/gitlab-ce/issues/15439
    
    See merge request !1954

--- spec/services/issues/update_service_spec.rb
+++ spec/services/issues/update_service_spec.rb
@@ -4,9 +4,13 @@ describe Issues::UpdateService, services: true do
   let(:user) { create(:user) }
   let(:user2) { create(:user) }
   let(:user3) { create(:user) }
-  let(:issue) { create(:issue, title: 'Old title', assignee_id: user3.id) }
-  let(:label) { create(:label) }
-  let(:project) { issue.project }
+  let(:project) { create(:empty_project) }
+  let(:label) { create(:label, project: project) }
+  let(:issue) do
+    create(:issue, title: 'Old title',
+                   assignee_id: user3.id,
+                   project: project)
+  end
 
   before do
     project.team << [user, :master]
