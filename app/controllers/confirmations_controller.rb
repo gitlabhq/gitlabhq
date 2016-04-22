dd5a29e8da047f26aa288631b334cd30d5bccdee
Robert Speicher <robert@gitlab.com>
2016-04-22 08:34:59 +0000

    Merge branch 'confirmation-screen' into 'master'
    Improved email confirmation UX
    
    Closes #4228
    
    See merge request !3184

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,6 +1,15 @@
 class ConfirmationsController < Devise::ConfirmationsController
 
+  def almost_there
+    flash[:notice] = nil
+    render layout: "devise_empty"
+  end
+
   protected
+
+  def after_resending_confirmation_instructions_path_for(resource)
+    users_almost_there_path
+  end
 
   def after_confirmation_path_for(resource_name, resource)
     if signed_in?(resource_name)
