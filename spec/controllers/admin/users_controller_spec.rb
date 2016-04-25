55bbc7e3dfc8c1e33fd23da2277890e2546c2f45
Robert Speicher <robert@gitlab.com>
2016-04-25 20:50:38 +0000

    Merge branch 'fix-impersonation-issue' into 'master'
    Prevent privilege escalation via "impersonate" feature
    
    Closes https://gitlab.com/gitlab-org/gitlab-ce/issues/15548
    
    See merge request !1956

--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
 describe Admin::UsersController do
-  let(:admin)    { create(:admin) }
+  let(:user) { create(:user) }
+  let(:admin) { create(:admin) }
 
   before do
     sign_in(admin)
@@ -119,6 +120,52 @@ describe Admin::UsersController do
 
     def go
       patch :disable_two_factor, id: user.to_param
+    end
+  end
+
+  describe "POST impersonate" do
+    context "when the user is blocked" do
+      before do
+        user.block!
+      end
+
+      it "shows a notice" do
+        post :impersonate, id: user.username
+
+        expect(flash[:alert]).to eq("You cannot impersonate a blocked user")
+      end
+
+      it "doesn't sign us in as the user" do
+        post :impersonate, id: user.username
+
+        expect(warden.user).to eq(admin)
+      end
+    end
+
+    context "when the user is not blocked" do
+      it "stores the impersonator in the session" do
+        post :impersonate, id: user.username
+
+        expect(session[:impersonator_id]).to eq(admin.id)
+      end
+
+      it "signs us in as the user" do
+        post :impersonate, id: user.username
+
+        expect(warden.user).to eq(user)
+      end
+
+      it "redirects to root" do
+        post :impersonate, id: user.username
+
+        expect(response).to redirect_to(root_path)
+      end
+
+      it "shows a notice" do
+        post :impersonate, id: user.username
+
+        expect(flash[:alert]).to eq("You are now impersonating #{user.username}")
+      end
     end
   end
 end
