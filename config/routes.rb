806cc20803982dc1315a4b387c740e68b29f23bd
Robert Speicher <robert@gitlab.com>
2016-04-25 20:50:38 +0000

    Merge branch 'fix-impersonation-issue' into 'master'
    Prevent privilege escalation via "impersonate" feature
    
    Closes https://gitlab.com/gitlab-org/gitlab-ce/issues/15548
    
    See merge request !1956

--- config/routes.rb
+++ config/routes.rb
@@ -190,8 +190,6 @@ Rails.application.routes.draw do
       resources :keys, only: [:show, :destroy]
       resources :identities, except: [:show]
 
-      delete 'stop_impersonation' => 'impersonation#destroy', on: :collection
-
       member do
         get :projects
         get :keys
@@ -201,11 +199,13 @@ Rails.application.routes.draw do
         put :unblock
         put :unlock
         put :confirm
-        post 'impersonate' => 'impersonation#create'
+        post :impersonate
         patch :disable_two_factor
         delete 'remove/:email_id', action: 'remove_email', as: 'remove_email'
       end
     end
+
+    resource :impersonation, only: :destroy
 
     resources :abuse_reports, only: [:index, :destroy]
     resources :applications
