8c2b72b1c80e8bbd082ac8e1aedb21a1aad07235
Andrei Gliga <otzy_007@yahoo.com>
2016-05-04 17:04:54 +0300

    tests for enabled_button_based_providers helper method of AuthHelper

--- spec/helpers/auth_helper_spec.rb
+++ spec/helpers/auth_helper_spec.rb
@@ -2,7 +2,9 @@ require "spec_helper"
 
 describe AuthHelper do
   describe "button_based_providers" do
-    it 'returns all enabled providers' do
+    let(:settings) { ApplicationSetting.create_from_defaults }
+
+    it 'returns all enabled providers from devise' do
       allow(helper).to receive(:auth_providers) { [:twitter, :github] }
       expect(helper.button_based_providers).to include(*[:twitter, :github])
     end
@@ -15,6 +17,24 @@ describe AuthHelper do
     it 'returns empty array' do
       allow(helper).to receive(:auth_providers) { [] }
       expect(helper.button_based_providers).to eq([])
+    end
+
+    it 'returns all the enabled providers from settings' do
+      allow(helper).to receive(:auth_providers) { [:twitter, :github] }
+      expect(helper.enabled_button_based_providers).to include(*['twitter', 'github'])
+    end
+
+    it 'should not return github as provider because it\'s disabled from settings' do
+      settings.update_attribute(
+        :disabled_oauth_sign_in_sources,
+        ['github']
+      )
+
+      allow(helper).to receive(:auth_providers) { [:twitter, :github] }
+      allow(helper).to receive(:current_application_settings) {  settings }
+
+      expect(helper.enabled_button_based_providers).to include('twitter')
+      expect(helper.enabled_button_based_providers).to_not include('github')
     end
   end
 end
