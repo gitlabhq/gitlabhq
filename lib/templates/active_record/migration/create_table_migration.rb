d890807939a7b5ae9306cc347f87a1f53d46728e
Yorick Peterse <yorickpeterse@gmail.com>
2016-05-09 15:06:22 +0200

    Link to migration guide in migration templates

--- lib/templates/active_record/migration/create_table_migration.rb
+++ lib/templates/active_record/migration/create_table_migration.rb
@@ -1,3 +1,6 @@
+# See http://doc.gitlab.com/ce/development/migration_style_guide.html
+# for more information on how to write migrations for GitLab.
+
 class <%= migration_class_name %> < ActiveRecord::Migration
   include Gitlab::Database::MigrationHelpers
 
