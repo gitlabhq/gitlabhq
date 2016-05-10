5b51d13c5e432cb76dc911047cc8c5932ecfe421
James Lopez <james@jameslopez.es>
2016-05-10 12:01:10 +0200

    removed method no longer needed

--- lib/gitlab/import_export.rb
+++ lib/gitlab/import_export.rb
@@ -6,10 +6,6 @@ module Gitlab
       File.join(storage_path, relative_path)
     end
 
-    def project_tree_list
-      project_tree.map {|r| r.is_a?(Hash) ? r.keys.first : r }
-    end
-
     def project_tree
       Gitlab::ImportExport::ImportExportReader.new.project_tree
     end
