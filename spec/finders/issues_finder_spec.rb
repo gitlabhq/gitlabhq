750b2ff0eec67926e737a40c7975cce2b58e27f7
Sean McGivern <sean@gitlab.com>
2016-05-11 17:38:34 +0100

    Make upcoming milestone work across projects
    Before: we took the next milestone due across all projects in the
    search and found issues whose milestone title matched that
    one. Problems:
    
    1. The milestone could be closed.
    2. Different projects have milestones with different schedules.
    3. Different projects have milestones with different titles.
    4. Different projects can have milestones with different schedules, but
       the _same_ title. That means we could show issues from a past
       milestone, or one that's far in the future.
    
    After: gather the ID of the next milestone on each project we're looking
    at, and find issues with those milestone IDs. Problems:
    
    1. For a lot of projects, this can return a lot of IDs.
    2. The SQL query has to be different between Postgres and MySQL, because
       MySQL is much more lenient with HAVING: as well as the columns
       appearing in GROUP BY or in aggregate clauses, MySQL allows them to
       appear in the SELECT list (un-aggregated).

--- spec/finders/issues_finder_spec.rb
+++ spec/finders/issues_finder_spec.rb
@@ -66,6 +66,40 @@ describe IssuesFinder do
         end
       end
 
+      context 'filtering by upcoming milestone' do
+        let(:params) { { milestone_title: Milestone::Upcoming.name } }
+
+        let(:project_no_upcoming_milestones) { create(:empty_project, :public) }
+        let(:project_next_1_1) { create(:empty_project, :public) }
+        let(:project_next_8_8) { create(:empty_project, :public) }
+
+        let(:yesterday) { Date.today - 1.day }
+        let(:tomorrow) { Date.today + 1.day }
+        let(:two_days_from_now) { Date.today + 2.days }
+        let(:ten_days_from_now) { Date.today + 10.days }
+
+        let(:milestones) do
+          [
+            create(:milestone, :closed, project: project_no_upcoming_milestones),
+            create(:milestone, project: project_next_1_1, title: '1.1', due_date: two_days_from_now),
+            create(:milestone, project: project_next_1_1, title: '8.8', due_date: ten_days_from_now),
+            create(:milestone, project: project_next_8_8, title: '1.1', due_date: yesterday),
+            create(:milestone, project: project_next_8_8, title: '8.8', due_date: tomorrow)
+          ]
+        end
+
+        before do
+          milestones.each do |milestone|
+            create(:issue, project: milestone.project, milestone: milestone, author: user, assignee: user)
+          end
+        end
+
+        it 'returns issues in the upcoming milestone for each project' do
+          expect(issues.map { |issue| issue.milestone.title }).to contain_exactly('1.1', '8.8')
+          expect(issues.map { |issue| issue.milestone.due_date }).to contain_exactly(tomorrow, two_days_from_now)
+        end
+      end
+
       context 'filtering by label' do
         let(:params) { { label_name: label.title } }
 
