3e09dcd02b8016998a50e07a56f08cac7c78838b
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2016-05-12 16:32:57 -0500

    Fix group lock membership test
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- features/steps/groups_management.rb
+++ features/steps/groups_management.rb
@@ -43,7 +43,7 @@ class Spinach::Features::GroupsManagement < Spinach::FeatureSteps
   step 'I go to group settings page' do
     visit dashboard_groups_path
     click_link 'Sourcing'
-    click_link 'Settings'
+    click_link 'Edit Group'
   end
 
   step 'I enable membership lock' do
