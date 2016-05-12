ad77ab0376fabf3dfadea86c716358964b526956
DJ Mountney <david@twkie.net>
2016-05-12 09:04:04 -0700

    Add Changelog entry
    and drop exception handling around HealthCheck::Utils.process_checks, it wasn't needed

--- app/controllers/admin/health_check_controller.rb
+++ app/controllers/admin/health_check_controller.rb
@@ -1,9 +1,5 @@
 class Admin::HealthCheckController < Admin::ApplicationController
   def show
-    begin
-      @errors = HealthCheck::Utils.process_checks('standard')
-    rescue => e
-      @errors = e.message.blank? ? e.class.to_s : e.message.to_s
-    end
+    @errors = HealthCheck::Utils.process_checks('standard')
   end
 end
