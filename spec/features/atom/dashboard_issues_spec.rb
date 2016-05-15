af33338bbf752ab5f4dba5b981644b399ae3563b
Alexander Matyushentsev <amatyushentsev@gmail.com>
2016-05-15 00:22:01 -0700

    Add more information into RSS fead for issues

--- spec/features/atom/dashboard_issues_spec.rb
+++ spec/features/atom/dashboard_issues_spec.rb
@@ -2,15 +2,18 @@ require 'spec_helper'
 
 describe "Dashboard Issues Feed", feature: true  do
   describe "GET /issues" do
-    let!(:user)     { create(:user) }
-    let!(:project1) { create(:project) }
-    let!(:project2) { create(:project) }
-    let!(:issue1)   { create(:issue, author: user, assignee: user, project: project1) }
-    let!(:issue2)   { create(:issue, author: user, assignee: user, project: project2) }
+    let!(:user)         { create(:user) }
+    let!(:project1)     { create(:project) }
+    let!(:project2)     { create(:project) }
+    let!(:milestone1)   { create(:milestone, project: project1, title: 'v1') }
+    let!(:label1)       { create(:label, project: project1, title: 'label1') }
+    let!(:issue1)       { create(:issue, author: user, assignee: user, project: project1, milestone: milestone1) }
+    let!(:issue2)       { create(:issue, author: user, assignee: user, project: project2, description: 'test desc') }
 
     before do
       project1.team << [user, :master]
       project2.team << [user, :master]
+      issue1.labels << label1
     end
 
     describe "atom feed" do
@@ -20,10 +23,24 @@ describe "Dashboard Issues Feed", feature: true  do
         expect(response_headers['Content-Type']).
           to have_content('application/atom+xml')
         expect(body).to have_selector('title', text: "#{user.name} issues")
-        expect(body).to have_selector('author email', text: issue1.author_email)
-        expect(body).to have_selector('entry summary', text: issue1.title)
-        expect(body).to have_selector('author email', text: issue2.author_email)
-        expect(body).to have_selector('entry summary', text: issue2.title)
+
+        entry_1 = find(:xpath, "//feed/entry[contains(summary/text(),'#{issue1.title}')]")
+        expect(entry_1).to be_present
+
+        entry_2 = find(:xpath, "//feed/entry[contains(summary/text(),'#{issue2.title}')]")
+        expect(entry_2).to be_present
+
+        expect(entry_1).to have_selector('author email', text: issue1.author_email)
+        expect(entry_1).to have_selector('assignee email', text: issue1.author_email)
+        expect(entry_1).to have_selector('labels label', text: label1.title)
+        expect(entry_1).to have_selector('milestone', text: milestone1.title)
+        expect(entry_1).not_to have_selector('description')
+
+        expect(entry_2).to have_selector('author email', text: issue2.author_email)
+        expect(entry_2).to have_selector('assignee email', text: issue2.author_email)
+        expect(entry_2).not_to have_selector('labels')
+        expect(entry_2).not_to have_selector('milestone')
+        expect(entry_2).to have_selector('description', text: issue1.description)
       end
     end
   end
