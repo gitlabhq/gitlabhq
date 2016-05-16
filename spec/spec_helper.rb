30aa37c8127dfe10df44f7945a4722b95581ff24
James Lopez <james@jameslopez.es>
2016-05-16 11:24:20 +0200

    remove spec change

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -52,7 +52,3 @@ FactoryGirl::SyntaxRunner.class_eval do
 end
 
 ActiveRecord::Migration.maintain_test_schema!
-
-Capybara.register_driver :poltergeist do |app|
-  Capybara::Poltergeist::Driver.new(app, timeout: 1.minute)
-end
