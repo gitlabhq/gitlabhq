d47b2b92c9b5e80eb3430e2b4950e17646b8efd8
Connor Shea <connor.james.shea@gmail.com>
2016-05-19 13:52:08 -0500

    Upgrade devise and devise-two-factor, remove devise-async
    Devise (3.5.4 => 4.1.1) Changelog:
    https://github.com/plataformatec/devise/blob/master/CHANGELOG.md
    
    devise-two-factor (2.0.1 => 3.0.0) Changelog:
    https://github.com/tinfoil/devise-two-factor/blob/master/CHANGELOG.md
    
    These are reliant on each other, so they have to be upgraded together.
    
    devise-async is no longer necessary as Devise 4.1 fixes a bug with the
    ActiveJob integration.

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,7 @@ class User < ActiveRecord::Base
   devise :two_factor_backupable, otp_number_of_backup_codes: 10
   serialize :otp_backup_codes, JSON
 
-  devise :lockable, :async, :recoverable, :rememberable, :trackable,
+  devise :lockable, :recoverable, :rememberable, :trackable,
     :validatable, :omniauthable, :confirmable, :registerable
 
   attr_accessor :force_random_password
