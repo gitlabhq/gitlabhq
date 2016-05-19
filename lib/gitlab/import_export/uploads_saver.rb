bac27df16c35abb2f8115d9c94edcc4ecbace1a7
James Lopez <james@jameslopez.es>
2016-05-19 13:10:41 +0200

    Squashed commit of the following:
    commit 92de6309e1c918a4ae023641dc42b196b3fb25ea
    Merge: 6c082ed 30f4dcd
    Author: James Lopez <james@jameslopez.es>
    Date:   Thu May 19 13:06:34 2016 +0200
    
        Merge branches 'feature/project-export' and 'feature/project-import' of gitlab.com:gitlab-org/gitlab-ce into feature/project-import
    
    commit 30f4dcd4c906a71db98833075c76eb59922f5b98
    Author: James Lopez <james@jameslopez.es>
    Date:   Thu May 19 13:02:57 2016 +0200
    
        uploads export

---
+++ lib/gitlab/import_export/uploads_saver.rb
@@ -0,0 +1,35 @@
+module Gitlab
+  module ImportExport
+    class UploadsSaver
+
+      def self.save(*args)
+        new(*args).save
+      end
+
+      def initialize(project:, shared:)
+        @project = project
+        @shared = shared
+      end
+
+      def save
+        return true unless File.directory?(uploads_path)
+
+        FileUtils.copy_entry(uploads_path, uploads_export_path)
+        true
+      rescue => e
+        @shared.error(e.message)
+        false
+      end
+
+      private
+
+      def uploads_export_path
+        File.join(@shared.export_path, 'uploads')
+      end
+
+      def uploads_path
+        File.join(Rails.root.join('public/uploads'), project.path_with_namespace)
+      end
+    end
+  end
+end
