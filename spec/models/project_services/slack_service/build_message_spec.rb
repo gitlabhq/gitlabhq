fcd723eda6ab1c1f8839570140db54ae3f7ff837
Aran Koning <arankoning@gmail.com>
2016-05-23 16:35:19 +0200

    Seconds plural slack

--- spec/models/project_services/slack_service/build_message_spec.rb
+++ spec/models/project_services/slack_service/build_message_spec.rb
@@ -15,7 +15,7 @@ describe SlackService::BuildMessage do
       commit: {
         status: status,
         author_name: 'hacker',
-        duration: 10,
+        duration: duration,
       },
     }
   end
@@ -23,9 +23,10 @@ describe SlackService::BuildMessage do
   context 'succeeded' do
     let(:status) { 'success' }
     let(:color) { 'good' }
-
+    let(:duration) { 10 }
+    
     it 'returns a message with information about succeeded build' do
-      message = '<somewhere.com|project_name>: Commit <somewhere.com/commit/97de212e80737a608d939f648d959671fb0a0142/builds|97de212e> of <somewhere.com/commits/develop|develop> branch by hacker passed in 10 second(s)'
+      message = '<somewhere.com|project_name>: Commit <somewhere.com/commit/97de212e80737a608d939f648d959671fb0a0142/builds|97de212e> of <somewhere.com/commits/develop|develop> branch by hacker passed in 10 seconds'
       expect(subject.pretext).to be_empty
       expect(subject.fallback).to eq(message)
       expect(subject.attachments).to eq([text: message, color: color])
@@ -35,9 +36,23 @@ describe SlackService::BuildMessage do
   context 'failed' do
     let(:status) { 'failed' }
     let(:color) { 'danger' }
+    let(:duration) { 10 }
 
     it 'returns a message with information about failed build' do
-      message = '<somewhere.com|project_name>: Commit <somewhere.com/commit/97de212e80737a608d939f648d959671fb0a0142/builds|97de212e> of <somewhere.com/commits/develop|develop> branch by hacker failed in 10 second(s)'
+      message = '<somewhere.com|project_name>: Commit <somewhere.com/commit/97de212e80737a608d939f648d959671fb0a0142/builds|97de212e> of <somewhere.com/commits/develop|develop> branch by hacker failed in 10 seconds'
+      expect(subject.pretext).to be_empty
+      expect(subject.fallback).to eq(message)
+      expect(subject.attachments).to eq([text: message, color: color])
+    end
+  end 
+  
+  describe '#seconds_name' do
+    let(:status) { 'failed' }
+    let(:color) { 'danger' }
+    let(:duration) { 1 }
+
+    it 'returns seconds as singular when there is only one' do
+      message = '<somewhere.com|project_name>: Commit <somewhere.com/commit/97de212e80737a608d939f648d959671fb0a0142/builds|97de212e> of <somewhere.com/commits/develop|develop> branch by hacker failed in 1 second'
       expect(subject.pretext).to be_empty
       expect(subject.fallback).to eq(message)
       expect(subject.attachments).to eq([text: message, color: color])
