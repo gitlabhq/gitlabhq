81086bf91bbb4e12488230b93ae370c6bd6427cd
Robert Speicher <rspeicher@gmail.com>
2016-05-24 17:25:47 -0400

    Remove unused stubs from GitPushService spec

--- spec/services/git_push_service_spec.rb
+++ spec/services/git_push_service_spec.rb
@@ -264,7 +264,6 @@ describe GitPushService, services: true do
 
       allow(commit).to receive_messages(
         safe_message: "this commit \n mentions #{issue.to_reference}",
-        references: [issue],
         author_name: commit_author.name,
         author_email: commit_author.email
       )
@@ -314,7 +313,6 @@ describe GitPushService, services: true do
 
     before do
       allow(closing_commit).to receive_messages(
-        issue_closing_regex: /^([Cc]loses|[Ff]ixes) #\d+/,
         safe_message: "this is some work.\n\ncloses ##{issue.iid}",
         author_name: commit_author.name,
         author_email: commit_author.email
@@ -386,11 +384,10 @@ describe GitPushService, services: true do
         WebMock.stub_request(:get, jira_api_test_url)
 
         allow(closing_commit).to receive_messages({
-                                                    issue_closing_regex: Regexp.new(Gitlab.config.gitlab.issue_closing_pattern),
-                                                    safe_message: message,
-                                                    author_name: commit_author.name,
-                                                    author_email: commit_author.email
-                                                  })
+          safe_message: message,
+          author_name: commit_author.name,
+          author_email: commit_author.email
+        })
 
         allow(project.repository).to receive_messages(commits_between: [closing_commit])
       end
