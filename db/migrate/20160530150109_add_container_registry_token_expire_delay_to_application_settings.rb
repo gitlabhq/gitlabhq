000585be1b3cc77889ae7ea6b38aff9e12fb6541
Kamil Trzcinski <ayufan@ayufan.eu>
2016-05-31 15:51:45 +0200

    Remove update of container_registry_token_expire_delay
    This is not needed when we specify a default.

--- db/migrate/20160530150109_add_container_registry_token_expire_delay_to_application_settings.rb
+++ db/migrate/20160530150109_add_container_registry_token_expire_delay_to_application_settings.rb
@@ -5,8 +5,5 @@ class AddContainerRegistryTokenExpireDelayToApplicationSettings < ActiveRecord::
 
   def change
     add_column :application_settings, :container_registry_token_expire_delay, :integer, default: 5
-
-    # Set expire delay to 5 minutes for existing settings
-    execute("update application_settings set container_registry_token_expire_delay = 5")
   end
 end
