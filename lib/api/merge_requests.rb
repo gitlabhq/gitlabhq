04fdf4b9a926129d63d0483ad8cd7e748f3a4e07
Sean McGivern <sean@gitlab.com>
2016-06-02 17:00:16 +0100

    fixup! Add `sha` parameter to MR accept API

--- lib/api/merge_requests.rb
+++ lib/api/merge_requests.rb
@@ -235,7 +235,7 @@ module API
           render_api_error!('Branch cannot be merged', 406) unless merge_request.can_be_merged?
 
           if params[:sha] && merge_request.source_sha != params[:sha]
-            render_api_error!("SHA does not match HEAD of source branch: #{merge_request.target_sha}", 409)
+            render_api_error!("SHA does not match HEAD of source branch: #{merge_request.source_sha}", 409)
           end
 
           merge_params = {
