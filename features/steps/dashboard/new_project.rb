957143347c1b810476e86c2d09bbde8434b4f653
Luke "Jared" Bennett <lbennett@gitlab.com>
2016-06-08 02:10:55 +0100

    Wiped old page and started on new UI
    Completed new project page
    
    Updated CHANGELOG
    
    Corrected 'Create project' button
    
    Made responsive
    
    Added gitlab export button
    
    Changed Spinach test to match updated UI
    
    reverted test changes and fixed UI
    
    Corrected 'Repo by URL' text
    
    Fixed static namespace style
    
    Added errors partial
    
    Added padding to bottom of page-with-sidebar

--- features/steps/dashboard/new_project.rb
+++ features/steps/dashboard/new_project.rb
@@ -10,7 +10,7 @@ class Spinach::Features::NewProject < Spinach::FeatureSteps
   end
 
   step 'I see "New Project" page' do
-    expect(page).to have_content('Project owner')
+    expect(page).to have_content('Project path')
     expect(page).to have_content('Project name')
   end
 
