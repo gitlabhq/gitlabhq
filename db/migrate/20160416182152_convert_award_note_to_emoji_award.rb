216436c1d48c4bafd2124fc656a06a6d1b430631
Z.J. van de Weg <zegerjan@gitlab.com>
2016-06-09 12:30:21 +0200

    Fix migration when converting award notes

--- db/migrate/20160416182152_convert_award_note_to_emoji_award.rb
+++ db/migrate/20160416182152_convert_award_note_to_emoji_award.rb
@@ -1,9 +1,7 @@
 class ConvertAwardNoteToEmojiAward < ActiveRecord::Migration
-  def change
-    def up
-      execute "INSERT INTO award_emoji (awardable_type, awardable_id, user_id, name, created_at, updated_at) (SELECT noteable_type, noteable_id, author_id, note, created_at, updated_at FROM notes WHERE is_award = true)"
+  def up
+    execute "INSERT INTO award_emoji (awardable_type, awardable_id, user_id, name, created_at, updated_at) (SELECT noteable_type, noteable_id, author_id, note, created_at, updated_at FROM notes WHERE is_award = true)"
 
-      execute "DELETE FROM notes WHERE is_award = true"
-    end
+    execute "DELETE FROM notes WHERE is_award = true"
   end
 end
