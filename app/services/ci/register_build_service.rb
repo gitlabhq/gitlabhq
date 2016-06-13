ee2e583500360385c9b3f8d9231233223ab72b42
Kamil Trzcinski <ayufan@ayufan.eu>
2016-06-13 21:52:41 +0200

    Fair usage of Shared Runners

--- app/services/ci/register_build_service.rb
+++ app/services/ci/register_build_service.rb
@@ -7,14 +7,14 @@ module Ci
 
       builds =
         if current_runner.shared?
-          # don't run projects which have not enables shared runners
-          builds.joins(:project).where(projects: { builds_enabled: true, shared_runners_enabled: true })
+          # this returns builds that are ordered by number of running builds
+          # we prefer projects that don't use shared runners at all
+          builds.joins("JOIN (#{projects_with_builds_for_shared_runners.to_sql}) AS projects ON ci_builds.gl_project_id=projects.gl_project_id").
+            order('projects.running_builds ASC', 'ci_builds.id ASC')
         else
-          # do run projects which are only assigned to this runner
-          builds.where(project: current_runner.projects.where(builds_enabled: true))
+          # do run projects which are only assigned to this runner (FIFO)
+          builds.where(project: current_runner.projects.where(builds_enabled: true)).order('created_at ASC')
         end
-
-      builds = builds.order('created_at ASC')
 
       build = builds.find do |build|
         build.can_be_served?(current_runner)
@@ -34,6 +34,19 @@ module Ci
 
     rescue StateMachines::InvalidTransition
       nil
+    end
+
+    private
+
+    def projects_with_builds_for_shared_runners
+      Ci::Build.running_or_pending.
+        joins(:project).where(projects: { builds_enabled: true, shared_runners_enabled: true }).
+        group(:gl_project_id).
+        select(:gl_project_id, "count(case when status = 'running' AND runner_id = (#{shared_runners.to_sql}) then 1 end) as running_builds")
+    end
+
+    def shared_runners
+      Ci::Runner.shared.select(:id)
     end
   end
 end
