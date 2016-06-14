faa0e3f7580bc38d4d12916b4589c64d6c2678a7
Timothy Andrew <mail@timothyandrew.net>
2016-06-14 09:50:14 +0530

    Fix rubocop spec.

--- db/migrate/20160415062917_create_personal_access_tokens.rb
+++ db/migrate/20160415062917_create_personal_access_tokens.rb
@@ -2,7 +2,7 @@ class CreatePersonalAccessTokens < ActiveRecord::Migration
   def change
     create_table :personal_access_tokens do |t|
       t.references :user, index: true, foreign_key: true, null: false
-      t.string :token, index: {unique: true}, null: false
+      t.string :token, index: { unique: true }, null: false
       t.string :name, null: false
       t.boolean :revoked, default: false
       t.datetime :expires_at
