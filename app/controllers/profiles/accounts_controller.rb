34bf165147787125a601ad30a4a71ba7f966f724
Patricio Cano <suprnova32@gmail.com>
2016-06-14 18:28:17 -0500

    Disable the unlink feature for SAML connected accounts (social login).

--- app/controllers/profiles/accounts_controller.rb
+++ app/controllers/profiles/accounts_controller.rb
@@ -5,7 +5,7 @@ class Profiles::AccountsController < Profiles::ApplicationController
 
   def unlink
     provider = params[:provider]
-    current_user.identities.find_by(provider: provider).destroy
+    current_user.identities.find_by(provider: provider).destroy unless provider.to_s == 'saml'
     redirect_to profile_account_path
   end
 end
