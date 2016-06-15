96274c2765ad4daa23af667b27dc647a3406d7c0
http://jneen.net/ <jneen@jneen.net>
2016-06-15 12:04:25 -0700

    add the wrapping back in for the banzai filter

--- lib/banzai/filter/syntax_highlight_filter.rb
+++ lib/banzai/filter/syntax_highlight_filter.rb
@@ -20,7 +20,7 @@ module Banzai
         code     = node.text
 
         begin
-          highlighted = block_code(code, language)
+          highlighted = %<<pre class="#{css_classes}"><code>#{block_code(code, language)}</code></pre>>
         rescue
           # Gracefully handle syntax highlighter bugs/errors to ensure
           # users can still access an issue/comment/etc.
@@ -29,6 +29,10 @@ module Banzai
 
         # Extracted to a method to measure it
         replace_parent_pre_element(node, highlighted)
+      end
+
+      def css_classes
+        "code highlight js-syntax-highlight #{lexer.tag}"
       end
 
       private
