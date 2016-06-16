314befded7e30981bf858e8e4aebd3df7f8d7d54
Lin Jen-Shin <godfat@godfat.org>
2016-06-16 23:31:54 +0800

    Fix typo. It's ivar and the column was called locked
    Again! For admin.

--- app/controllers/admin/runner_projects_controller.rb
+++ app/controllers/admin/runner_projects_controller.rb
@@ -9,7 +9,7 @@ class Admin::RunnerProjectsController < Admin::ApplicationController
   def create
     @runner = Ci::Runner.find(params[:runner_project][:runner_id])
 
-    return head(403) if runner.is_shared? || runner.is_locked?
+    return head(403) if @runner.is_shared? || @runner.locked?
 
     runner_project = @runner.assign_to(@project, current_user)
 
