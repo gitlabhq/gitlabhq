a3d5343ee24096dac460789f0749b1ef63e51a97
Douwe Maan <douwe@gitlab.com>
2016-06-17 23:28:22 +0000

    Merge branch 'issue_12758' into 'master'
    Implement custom notification level options
    
    ![Screen_Shot_2016-06-17_at_15.31.43](/uploads/3fc47d2f461b3e8b67bb8acaa304cf99/Screen_Shot_2016-06-17_at_15.31.43.png)
    
    ![Screenshot_from_2016-06-15_10-52-27](/uploads/88dbdd21d97e80ee772fe08fa0c9b393/Screenshot_from_2016-06-15_10-52-27.png)
    
    part of #12758 
    
    See merge request !4389

--- app/models/notification_setting.rb
+++ app/models/notification_setting.rb
@@ -1,5 +1,5 @@
 class NotificationSetting < ActiveRecord::Base
-  enum level: { global: 3, watch: 2, mention: 4, participating: 1, disabled: 0 }
+  enum level: { global: 3, watch: 2, mention: 4, participating: 1, disabled: 0, custom: 5 }
 
   default_value_for :level, NotificationSetting.levels[:global]
 
@@ -15,6 +15,24 @@ class NotificationSetting < ActiveRecord::Base
   scope :for_groups, -> { where(source_type: 'Namespace') }
   scope :for_projects, -> { where(source_type: 'Project') }
 
+  EMAIL_EVENTS = [
+    :new_note,
+    :new_issue,
+    :reopen_issue,
+    :close_issue,
+    :reassign_issue,
+    :new_merge_request,
+    :reopen_merge_request,
+    :close_merge_request,
+    :reassign_merge_request,
+    :merge_merge_request
+  ]
+
+  store :events, accessors: EMAIL_EVENTS, coder: JSON
+
+  before_create :set_events
+  before_save :events_to_boolean
+
   def self.find_or_create_for(source)
     setting = find_or_initialize_by(source: source)
 
@@ -23,5 +41,22 @@ class NotificationSetting < ActiveRecord::Base
     end
 
     setting
+  end
+
+  # Set all event attributes to false when level is not custom or being initialized for UX reasons
+  def set_events
+    return if custom?
+
+    EMAIL_EVENTS.each do |event|
+      events[event] = false
+    end
+  end
+
+  # Validates store accessors values as boolean
+  # It is a text field so it does not cast correct boolean values in JSON
+  def events_to_boolean
+    EMAIL_EVENTS.each do |event|
+      events[event] = ActiveRecord::ConnectionAdapters::Column::TRUE_VALUES.include?(events[event])
+    end
   end
 end
