c702189bec3c1e5f21f2e74a50db7c8a64f76fcb
Valery Sizov <valery@gitlab.com>
2016-06-21 21:44:32 +0300

    Users with master permissions cannot edit the file locks

--- app/helpers/path_locks_helper.rb
+++ app/helpers/path_locks_helper.rb
@@ -1,6 +1,6 @@
 module PathLocksHelper
-  def can_unlock?(path_lock, current_user = @current_user)
-    can?(current_user, :admin_locks, path_lock) || path_lock.user == current_user
+  def can_unlock?(path_lock, current_user = @current_user, project = @project)
+    can?(current_user, :admin_path_locks, project) || path_lock.user == current_user
   end
 
   def license_allows_file_locks?
