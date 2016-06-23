242aa5bd66038e5c7393f17be51a73deaedd7d06
Josh Frye <joshfng@gmail.com>
2016-06-23 08:52:19 -0400

    Round figures for system info

--- app/controllers/admin/system_info_controller.rb
+++ app/controllers/admin/system_info_controller.rb
@@ -3,10 +3,10 @@ class Admin::SystemInfoController < Admin::ApplicationController
     system_info = Vmstat.snapshot
     @load = system_info.load_average.collect { |v| v.round(2) }.join(', ')
 
-    @mem_used = Filesize.from("#{system_info.memory.active_bytes} B").to_s('GB')
-    @mem_total = Filesize.from("#{system_info.memory.total_bytes} B").to_s('GB')
+    @mem_used = Filesize.from("#{system_info.memory.active_bytes} B").to_f('GB').round
+    @mem_total = Filesize.from("#{system_info.memory.total_bytes} B").to_f('GB').round
 
-    @disk_used = Filesize.from("#{system_info.disks[0].used_bytes} B").to_s('GB')
-    @disk_total = Filesize.from("#{system_info.disks[0].total_bytes} B").to_s('GB')
+    @disk_used = Filesize.from("#{system_info.disks[0].used_bytes} B").to_f('GB').round
+    @disk_total = Filesize.from("#{system_info.disks[0].total_bytes} B").to_f('GB').round
   end
 end
