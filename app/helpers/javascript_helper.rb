3e363a5a79dd0f03e7b0feb8c028f34e7ad45330
Connor Shea <connor.james.shea@gmail.com>
2016-06-23 11:58:20 -0600

    Appease Rubocop.

--- app/helpers/javascript_helper.rb
+++ app/helpers/javascript_helper.rb
@@ -1,5 +1,5 @@
 module JavascriptHelper
   def page_specific_javascript_tag(js)
-    javascript_include_tag asset_path(js), {integrity: true, "data-turbolinks-track" => true}
+    javascript_include_tag asset_path(js), { integrity: true, "data-turbolinks-track" => true }
   end
 end
