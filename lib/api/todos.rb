87ac9c9850d602fd18654498ab3fa005d2b85ac7
Robert Schilling <rschilling@student.tugraz.at>
2016-06-28 18:04:44 +0200

    Support creating a todo on issuables via API

--- lib/api/todos.rb
+++ lib/api/todos.rb
@@ -3,6 +3,36 @@ module API
   class Todos < Grape::API
     before { authenticate! }
 
+    ISSUABLE_TYPES = {
+      'merge_requests' => ->(id) { user_project.merge_requests.find(id) },
+      'issues' => ->(id) { find_project_issue(id) }
+    }
+
+    resource :projects do
+      ISSUABLE_TYPES.each do |type, finder|
+        type_id_str = "#{type.singularize}_id".to_sym
+
+        # Create a todo on an issuable
+        #
+        # Parameters:
+        #   id (required) - The ID of a project
+        #   issuable_id (required) - The ID of an issuable
+        # Example Request:
+        #   POST /projects/:id/issues/:issuable_id/todo
+        #   POST /projects/:id/merge_requests/:issuable_id/todo
+        post ":id/#{type}/:#{type_id_str}/todo" do
+          issuable = instance_exec(params[type_id_str], &finder)
+          todo = TodoService.new.mark_todo(issuable, current_user).first
+
+          if todo
+            present todo, with: Entities::Todo, current_user: current_user
+          else
+            not_modified!
+          end
+        end
+      end
+    end
+
     resource :todos do
       helpers do
         def find_todos
--- spec/requests/api/todos_spec.rb
+++ spec/requests/api/todos_spec.rb
@@ -11,7 +11,7 @@ describe API::Todos, api: true do
   let(:merge_request) { create(:merge_request, source_project: project_1) }
   let!(:pending_1) { create(:todo, :mentioned, project: project_1, author: author_1, user: john_doe) }
   let!(:pending_2) { create(:todo, project: project_2, author: author_2, user: john_doe) }
-  let!(:pending_3) { create(:todo, project: project_1, author: author_2, user: john_doe, target: merge_request) }
+  let!(:pending_3) { create(:todo, project: project_1, author: author_2, user: john_doe) }
   let!(:done) { create(:todo, :done, project: project_1, author: author_1, user: john_doe) }
 
   before do
@@ -59,6 +59,8 @@ describe API::Todos, api: true do
 
       context 'and using the type filter' do
         it 'filters based on type param' do
+          create(:todo, project: project_1, author: author_2, user: john_doe, target: merge_request)
+
           get api('/todos', john_doe), { type: 'MergeRequest' }
 
           expect(response.status).to eq(200)
@@ -137,6 +139,51 @@ describe API::Todos, api: true do
         expect(pending_1.reload).to be_done
         expect(pending_2.reload).to be_done
         expect(pending_3.reload).to be_done
+      end
+    end
+  end
+
+  shared_examples 'an issuable' do |issuable_type|
+    it 'creates a todo on an issuable' do
+      post api("/projects/#{project_1.id}/#{issuable_type}/#{issuable.id}/todo", john_doe)
+
+      expect(response.status).to eq(201)
+      expect(json_response['project']).to be_a Hash
+      expect(json_response['author']).to be_a Hash
+      expect(json_response['target_type']).to eq(issuable.class.name)
+      expect(json_response['target']).to be_a Hash
+      expect(json_response['target_url']).to be_present
+      expect(json_response['body']).to be_present
+      expect(json_response['state']).to eq('pending')
+      expect(json_response['action_name']).to eq('marked')
+      expect(json_response['created_at']).to be_present
+    end
+
+    it 'returns 304 there already exist a todo on that issuable' do
+      create(:todo, project: project_1, author: author_1, user: john_doe, target: issuable)
+
+      post api("/projects/#{project_1.id}/#{issuable_type}/#{issuable.id}/todo", john_doe)
+
+      expect(response.status).to eq(304)
+    end
+
+    it 'returns 404 if the issuable is not found' do
+      post api("/projects/#{project_1.id}/#{issuable_type}/123/todo", john_doe)
+
+      expect(response.status).to eq(404)
+    end
+  end
+
+  describe 'POST :id/issuable_type/:issueable_id/todo' do
+    context 'for an issue' do
+      it_behaves_like 'an issuable', 'issues' do
+        let(:issuable) { create(:issue, author: author_1, project: project_1) }
+      end
+    end
+
+    context 'for a merge request' do
+      it_behaves_like 'an issuable', 'merge_requests' do
+        let(:issuable) { merge_request }
       end
     end
   end
