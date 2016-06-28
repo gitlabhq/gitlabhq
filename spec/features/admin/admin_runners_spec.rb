gitlabhq/gitlabhq

commit 1b8f52d9206bdf19c0dde04505c4c0b1cf46cfbe

Avoid enabling locked runners. Give 403 in this case

app/controllers/admin/runner_projects_controller.rb
@@ -9,6 +9,8 @@ class Admin::RunnerProjectsController < Admin::ApplicationController
   def create
     @runner = Ci::Runner.find(params[:runner_project][:runner_id])
 
+    return head(403) if runner.is_shared? || runner.is_locked?
+
     if @runner.assign_to(@project, current_user)
       redirect_to admin_runner_path(@runner)
     else

app/controllers/projects/runner_projects_controller.rb
@@ -6,6 +6,7 @@ class Projects::RunnerProjectsController < Projects::ApplicationController
   def create
     @runner = Ci::Runner.find(params[:runner_project][:runner_id])
 
+    return head(403) if runner.is_shared? || runner.is_locked?
     return head(403) unless current_user.ci_authorized_runners.include?(@runner)
 
     path = runners_path(project)

lib/api/runners.rb
@@ -163,6 +163,7 @@ module API
 
       def authenticate_enable_runner!(runner)
         forbidden!("Runner is shared") if runner.is_shared?
+        forbidden!("Runner is locked") if runner.locked?
         return if current_user.is_admin?
         forbidden!("No access granted") unless user_can_access_runner?(runner)
       end

spec/requests/api/runners_spec.rb
@@ -362,11 +362,13 @@ describe API::Runners, api: true  do
 
   describe 'POST /projects/:id/runners' do
     context 'authorized user' do
-      it 'should enable specific runner' do
-        specific_runner2 = create(:ci_runner).tap do |runner|
+      let(:specific_runner2) do
+        create(:ci_runner).tap do |runner|
           create(:ci_runner_project, runner: runner, project: project2)
         end
+      end
 
+      it 'should enable specific runner' do
         expect do
           post api("/projects/#{project.id}/runners", user), runner_id: specific_runner2.id
         end.to change{ project.runners.count }.by(+1)
@@ -378,6 +380,16 @@ describe API::Runners, api: true  do
           post api("/projects/#{project.id}/runners", user), runner_id: specific_runner.id
         end.to change{ project.runners.count }.by(0)
         expect(response.status).to eq(201)
+      end
+
+      it 'should not enable locked runner' do
+        specific_runner2.update(locked: true)
+
+        expect do
+          post api("/projects/#{project.id}/runners", user), runner_id: specific_runner2.id
+        end.to change{ project.runners.count }.by(0)
+
+        expect(response.status).to eq(403)
       end
 
       it 'should not enable shared runner' do

commit 95c99cd4fd0f768394d582ac49dc83ac93d9c1e5

Admin should be able to turn shared runners into specific ones:
The regression was introduced by:

https://gitlab.com/gitlab-org/gitlab-ce/commit/1b8f52d9206bdf19c0dde04505c4c0b1cf46cfbe

I did that because there's a test specifying that a shared runner cannot
be enabled, in the API. So I assume that is the case for non-admin, but
admins should be able to do so anyway.

Also added a test to make sure this won't regress again.

Closes #19039

app/controllers/admin/runner_projects_controller.rb
@@ -4,7 +4,7 @@ class Admin::RunnerProjectsController < Admin::ApplicationController
   def create
     @runner = Ci::Runner.find(params[:runner_project][:runner_id])
 
-    return head(403) if @runner.is_shared? || @runner.locked?
+    return head(403) if @runner.locked?
 
     runner_project = @runner.assign_to(@project, current_user)
 

spec/features/admin/admin_runners_spec.rb
@@ -62,19 +62,35 @@ describe "Admin Runners" do
     end
 
     describe 'enable/create' do
-      before do
-        @project1.runners << runner
-        visit admin_runner_path(runner)
+      shared_examples 'enable runners' do
+        it 'enables a runner for a project' do
+          within '.unassigned-projects' do
+            click_on 'Enable'
+          end
+
+          assigned_project = page.find('.assigned-projects')
+
+          expect(assigned_project).to have_content(@project2.path)
+        end
       end
 
-      it 'enables specific runner for project' do
-        within '.unassigned-projects' do
-          click_on 'Enable'
+      context 'with specific runner' do
+        before do
+          @project1.runners << runner
+          visit admin_runner_path(runner)
         end
 
-        assigned_project = page.find('.assigned-projects')
+        it_behaves_like 'enable runners'
+      end
 
-        expect(assigned_project).to have_content(@project2.path)
+      context 'with shared runner' do
+        before do
+          @project1.destroy
+          runner.update(is_shared: true)
+          visit admin_runner_path(runner)
+        end
+
+        it_behaves_like 'enable runners'
       end
     end
 