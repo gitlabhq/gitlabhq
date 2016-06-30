63477fd27f3eeff5d5f612b27ed71c76926934d2
Lin Jen-Shin <godfat@godfat.org>
2016-06-30 21:38:29 +0800

    Disable Metrics/CyclomaticComplexity for Ability.allowed
    There's little point to cut that down.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,5 +1,6 @@
 class Ability
   class << self
+    # rubocop: disable Metrics/CyclomaticComplexity
     def allowed(user, subject)
       return anonymous_abilities(user, subject) if user.nil?
       return [] unless user.is_a?(User)
