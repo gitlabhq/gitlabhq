15e6d78dd78ff45db49b58a275fdb07f8e5b0077
Douwe Maan <douwe@gitlab.com>
2016-06-30 15:32:35 +0000

    Update spec

--- spec/helpers/path_locks_helper_spec.rb
+++ spec/helpers/path_locks_helper_spec.rb
@@ -11,7 +11,7 @@ describe PathLocksHelper do
     it "return correct string for nested locks" do
       user = create :user, name: 'John'
       path_lock = create :path_lock, path: 'app', user: user
-      expect(text_label_for_lock(path_lock, 'app/models')).to eq('John locked app')
+      expect(text_label_for_lock(path_lock, 'app/models')).to eq('John has a lock on "app"')
     end
   end
 end
