85d1a9708ea31084147911243be248a02e89c56a
tiagonbotelho <tiagonbotelho@hotmail.com>
2016-07-01 16:32:56 +0100

    implements the form for renaming the new filename on the file edit page

--- app/services/files/update_service.rb
+++ app/services/files/update_service.rb
@@ -3,6 +3,7 @@ require_relative "base_service"
 module Files
   class UpdateService < Files::BaseService
     def commit
+      # Need to update file_path with the new filename
       repository.commit_file(current_user, @file_path, @file_content, @commit_message, @target_branch, true)
     end
   end
