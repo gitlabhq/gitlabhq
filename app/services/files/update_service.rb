64306128a1786f25354cbf20ec5dc4e4d6f79621
tiagonbotelho <tiagonbotelho@hotmail.com>
2016-07-01 16:32:56 +0100

    implements the form for renaming the new filename on the file edit page

--- app/services/files/update_service.rb
+++ app/services/files/update_service.rb
@@ -6,6 +6,7 @@ module Files
       repository.update_file(current_user, @file_path, @file_content,
                              @target_branch, previous_path: @previous_path,
                              message: @commit_message, update: true)
+
     end
   end
 end
