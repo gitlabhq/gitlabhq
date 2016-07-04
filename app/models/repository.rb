e621fb57c4c40bc05cfc2c88a1a7b2493cafc2f4
tiagonbotelho <tiagonbotelho@hotmail.com>
2016-07-04 11:32:57 +0100

    creates the update_file method in repository.rb and applies changes accordingly

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -751,9 +751,6 @@ class Repository
       if previous_path
         options[:file].merge!(previous_path: previous_path)
 
-        puts "#" * 90
-        puts "Hello"
-        puts "#" * 90
         Gitlab::Git::Blob.rename(raw_repository, options)
       else
         puts "#" * 90
