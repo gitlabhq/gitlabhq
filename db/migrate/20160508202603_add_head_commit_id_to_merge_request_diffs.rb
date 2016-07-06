kuailemy123/gitlab-ce

Store diff head and start commit shas on MR diffs

db/migrate/20160508202603_add_head_commit_id_to_merge_request_diffs.rb
@@ -1,0 +1,5 @@
+class AddHeadCommitIdToMergeRequestDiffs < ActiveRecord::Migration
+  def change
+    add_column :merge_request_diffs, :head_commit_id, :string
+  end
+end

db/migrate/20160516224534_add_start_commit_id_to_merge_request_diffs.rb
@@ -1,0 +1,5 @@
+class AddStartCommitIdToMergeRequestDiffs < ActiveRecord::Migration
+  def change
+    add_column :merge_request_diffs, :start_commit_id, :string
+  end
+end

gitlabhq/gitlabhq

db/migrate/20160508202603_add_head_commit_id_to_merge_request_diffs.rb
@@ -1,0 +1,5 @@
+class AddHeadCommitIdToMergeRequestDiffs < ActiveRecord::Migration
+  def change
+    add_column :merge_request_diffs, :head_commit_sha, :string
+  end
+end

db/migrate/20160516224534_add_start_commit_id_to_merge_request_diffs.rb
@@ -1,0 +1,5 @@
+class AddStartCommitIdToMergeRequestDiffs < ActiveRecord::Migration
+  def change
+    add_column :merge_request_diffs, :start_commit_sha, :string
+  end
+end