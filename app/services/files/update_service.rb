c98369207917e83d445fe94a350a9fba7f17fd0c
tiagonbotelho <tiagonbotelho@hotmail.com>
2016-07-06 11:28:50 +0100

    removes redundant comment

--- app/services/files/update_service.rb
+++ app/services/files/update_service.rb
@@ -3,7 +3,6 @@ require_relative "base_service"
 module Files
   class UpdateService < Files::BaseService
     def commit
-      # Need to update file_path with the new filename
       repository.update_file(current_user, @file_path, @file_content,
                              @target_branch, previous_path: @previous_path,
                              message: @commit_message, update: true)
