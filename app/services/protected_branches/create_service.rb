9168f53116352da3fbefe12022d5b92edd7ffc34
Timothy Andrew <mail@timothyandrew.net>
2016-07-07 16:18:07 +0530

    Add "No One Can Push" to the protected branches UI.
    1. Move to dropdowns instead of checkboxes. One each for "Allowed to
       Push" and "Allowed to Merge"
    
    2. Refactor the `ProtectedBranches` coffeescript class into
       `ProtectedBranchesAccessSelect`.
    
    3. Modify the backend to accept the new parameters.

--- app/services/protected_branches/create_service.rb
+++ app/services/protected_branches/create_service.rb
@@ -1,19 +1,21 @@
-class ProtectedBranches::CreateService < BaseService
-  attr_reader :protected_branch
+module ProtectedBranches
+  class CreateService < BaseService
+    attr_reader :protected_branch
 
-  def execute
-    ProtectedBranch.transaction do
-      @protected_branch = project.protected_branches.new(name: params[:name])
-      @protected_branch.save!
+    def execute
+      ProtectedBranch.transaction do
+        @protected_branch = project.protected_branches.new(name: params[:name])
+        @protected_branch.save!
 
-      @protected_branch.create_push_access_level!
-      @protected_branch.create_merge_access_level!
+        @protected_branch.create_push_access_level!
+        @protected_branch.create_merge_access_level!
 
-      set_access_levels!
+        set_access_levels!
+      end
+
+      true
+    rescue ActiveRecord::RecordInvalid
+      false
     end
-
-    true
-  rescue ActiveRecord::RecordInvalid
-    false
   end
 end
