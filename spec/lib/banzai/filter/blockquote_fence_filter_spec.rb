6ba884530f6d1132621e1050175ab3384ebdcbb5
Douwe Maan <douwe@selenight.nl>
2016-07-10 14:59:36 -0500

    Fix typo in spec

--- spec/lib/banzai/filter/blockquote_fence_filter_spec.rb
+++ spec/lib/banzai/filter/blockquote_fence_filter_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe Banzai::Filter::BlockquoteFenceFilter, lib: true do
   include FilterSpecHelper
 
-  it 'convers blockquote fences to blockquote lines' do
+  it 'converts blockquote fences to blockquote lines' do
     content = File.read(Rails.root.join('spec/fixtures/blockquote_fence_before.md'))
     expected = File.read(Rails.root.join('spec/fixtures/blockquote_fence_after.md'))
 
