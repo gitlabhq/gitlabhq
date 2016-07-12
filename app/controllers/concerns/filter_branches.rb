9fa8cf6759c798eee8aeec9016a0abbea87ed275
tiagonbotelho <tiagonbotelho@hotmail.com>
2016-07-12 19:19:48 +0100

    removes redundant filter_branches file

--- app/controllers/concerns/filter_branches.rb
+++
@@ -1,8 +0,0 @@
-module FilterBranches
-  extend ActiveSupport::Concern
-
-  def filter_branches(branches)
-    if params[:search].present? && @sort
-      branches = @repository.find_similar_branches(params[:search], @sort)
-  end
-end
