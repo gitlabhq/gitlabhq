f493c9a8e9bf170585a9100b9d01ea3c08d90f10
Stan Hu <stanhu@gmail.com>
2016-07-13 15:19:08 -0700

    Add async task that reports EE usage every week
    Closes #380

---
+++ app/workers/gitlab_usage_ping_worker.rb
@@ -0,0 +1,46 @@
+class GitlabUsagePingWorker
+  LEASE_TIMEOUT = 86400
+
+  include LicenseHelper
+  include Sidekiq::Worker
+  include HTTParty
+
+  # This is not guaranteed to succeed, so don't retry on failure
+  sidekiq_options queue: :default, retry: false
+
+  def perform
+    # Multiple Sidekiq workers could run this. We should only do this at most once a day.
+    return unless try_obtain_lease
+
+    HTTParty.post(url,
+                  body: data.to_json,
+                  headers: { 'Content-type' => 'application/json' }
+                 )
+  end
+
+  def try_obtain_lease
+    Gitlab::ExclusiveLease.new('gitlab_usage_ping_worker:ping', timeout: LEASE_TIMEOUT).try_obtain
+  end
+
+  def data
+    usage_data = { version: Gitlab::VERSION }
+    usage_data[:active_users] = current_active_user_count
+    license = License.current
+
+    if license
+      usage_data[:historical_max_users] = max_historical_user_count
+      usage_data[:licensee] = license.licensee
+      usage_data[:license_user_count] = license.user_count
+      usage_data[:license_starts_at] = license.starts_at
+      usage_data[:license_expires_at] = license.expires_at
+      usage_data[:license_add_ons] = license.add_ons
+      usage_data[:recorded_at] = Time.now
+    end
+
+    usage_data
+  end
+
+  def url
+    'https://version.gitlab.com/usage_ping'
+  end
+end
---
+++ spec/workers/gitlab_usage_ping_worker_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe GitlabUsagePingWorker do
+  subject { GitlabUsagePingWorker.new }
+
+  it "gathers license data" do
+    data = subject.data
+    license = License.current
+
+    expect(data[:version]).to eq(Gitlab::VERSION)
+    expect(data[:licensee]).to eq(license.licensee)
+    expect(data[:active_users]).to eq(User.active.count)
+    expect(data[:licensee]).to eq(license.licensee)
+    expect(data[:license_user_count]).to eq(license.user_count)
+    expect(data[:license_starts_at]).to eq(license.starts_at)
+    expect(data[:license_expires_at]).to eq(license.expires_at)
+    expect(data[:license_add_ons]).to eq(license.add_ons)
+    expect(data[:recorded_at]).to be_a(Time)
+  end
+
+  it "sends POST request" do
+    stub_request(:post, "https://version.gitlab.com/usage_ping").
+        to_return(status: 200, body: '', headers: {})
+    expect(subject).to receive(:try_obtain_lease).and_return(true)
+
+    expect(subject.perform.response.code.to_i).to eq(200)
+  end
+end
