4fe8dc6bb15b0a2d6d60330b132448dde3b4e12b
Jacob Vosmaer <jacob@gitlab.com>
2016-07-13 19:00:54 +0200

    Add tests for Kerberos Spnego strategy

---
+++ spec/lib/omniauth/strategies/kerberos_spnego_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+require 'omniauth/strategies/kerberos_spnego'
+
+describe OmniAuth::Strategies::KerberosSpnego do
+  subject { described_class.new(:app) }
+  let(:session) { Hash.new }
+
+  before do
+    allow(subject).to receive(:session).and_return(session)
+  end
+
+  it 'extracts the username' do
+    session[:kerberos_spnego_principal_name] = 'Janedoe@FOOBAR.COM'
+    expect(subject.username).to eq('Janedoe')
+  end
+
+  it 'turns the principal name into an email address' do
+    session[:kerberos_spnego_principal_name] = 'Janedoe@FOOBAR.COM'
+    expect(subject.email).to eq('Janedoe@foobar.com')
+  end
+
+  it 'clears its special session key' do
+    session[:kerberos_spnego_principal_name] = 'Janedoe@FOOBAR.COM'
+    subject.username
+    expect(session).to eq({})
+  end
+end
