76771c294694bb7bae02778c30ad3c4aff27b782
James Lopez <james@jameslopez.es>
2016-07-15 16:03:31 +0200

    squashed - added avatar saver/restorer and specs
    added spec for avatar saver
    
    avatar saver!
    
    added avatar restorer spec
    
    fix spec
    
    added avatar restorer class
    
    fix export service
    
    fix warnings, added changelog
    
    fix spec
    
    some refactoring based on feedback
    
    fixed a few issues after testing i/e avatar

---
+++ lib/gitlab/import_export/avatar_restorer.rb
@@ -0,0 +1,31 @@
+module Gitlab
+  module ImportExport
+    class AvatarRestorer
+
+      def initialize(project:, shared:)
+        @project = project
+        @shared = shared
+      end
+
+      def restore
+        return true unless avatar_export_file
+
+        @project.avatar = File.open(avatar_export_file)
+        @project.save!
+      rescue => e
+        @shared.error(e)
+        false
+      end
+
+      private
+
+      def avatar_export_file
+        @avatar_export_file ||= Dir["#{avatar_export_path}/*"].first
+      end
+
+      def avatar_export_path
+        File.join(@shared.export_path, 'avatar')
+      end
+    end
+  end
+end
