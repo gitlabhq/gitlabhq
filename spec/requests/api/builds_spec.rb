6de48227badd879f7e12803592daa2fc73656f91
Lin Jen-Shin <godfat@godfat.org>
2016-07-18 13:58:48 +0800

    Use single line even if they're more than 80 chars, feedback:
    https://gitlab.com/gitlab-org/gitlab-ce/merge_requests/5142#note_13125628

--- spec/requests/api/builds_spec.rb
+++ spec/requests/api/builds_spec.rb
@@ -191,9 +191,7 @@ describe API::API, api: true do
     include_context 'artifacts from ref and build name'
 
     def path_from_ref(ref = pipeline.sha, job = build.name)
-      api(
-        "/projects/#{project.id}/builds/artifacts/#{ref}/download?job=#{job}",
-        user)
+      api("/projects/#{project.id}/builds/artifacts/#{ref}/download?job=#{job}", user)
     end
 
     context '401' do
