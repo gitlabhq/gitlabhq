64232c71a402b17f38f5425d9e600e2650078ce3
tiagonbotelho <tiagonbotelho@hotmail.com>
2016-07-20 12:02:39 +0100

    updates local schema

--- db/schema.rb
+++ db/schema.rb
@@ -84,7 +84,7 @@ ActiveRecord::Schema.define(version: 20160716115710) do
     t.string   "health_check_access_token"
     t.boolean  "send_user_confirmation_email",          default: false
     t.integer  "container_registry_token_expire_delay", default: 5
-    t.boolean  "user_default_external",                 default: false,        null: false
+    t.boolean  "user_default_external",                 default: false,       null: false
     t.text     "after_sign_up_text"
     t.string   "repository_storage",                    default: "default"
     t.string   "enabled_git_access_protocol"
@@ -605,9 +605,9 @@ ActiveRecord::Schema.define(version: 20160716115710) do
   add_index "merge_request_diffs", ["merge_request_id"], name: "index_merge_request_diffs_on_merge_request_id", unique: true, using: :btree
 
   create_table "merge_requests", force: :cascade do |t|
-    t.string   "target_branch",                             null: false
-    t.string   "source_branch",                             null: false
-    t.integer  "source_project_id",                         null: false
+    t.string   "target_branch",                                null: false
+    t.string   "source_branch",                                null: false
+    t.integer  "source_project_id",                            null: false
     t.integer  "author_id"
     t.integer  "assignee_id"
     t.string   "title"
@@ -616,20 +616,21 @@ ActiveRecord::Schema.define(version: 20160716115710) do
     t.integer  "milestone_id"
     t.string   "state"
     t.string   "merge_status"
-    t.integer  "target_project_id",                         null: false
+    t.integer  "target_project_id",                            null: false
     t.integer  "iid"
     t.text     "description"
-    t.integer  "position",                  default: 0
+    t.integer  "position",                     default: 0
     t.datetime "locked_at"
     t.integer  "updated_by_id"
     t.string   "merge_error"
     t.text     "merge_params"
-    t.boolean  "merge_when_build_succeeds", default: false, null: false
+    t.boolean  "merge_when_build_succeeds",    default: false, null: false
     t.integer  "merge_user_id"
     t.string   "merge_commit_sha"
     t.datetime "deleted_at"
     t.string   "in_progress_merge_commit_sha"
   end
+
   add_index "merge_requests", ["assignee_id"], name: "index_merge_requests_on_assignee_id", using: :btree
   add_index "merge_requests", ["author_id"], name: "index_merge_requests_on_author_id", using: :btree
   add_index "merge_requests", ["created_at", "id"], name: "index_merge_requests_on_created_at_and_id", using: :btree
