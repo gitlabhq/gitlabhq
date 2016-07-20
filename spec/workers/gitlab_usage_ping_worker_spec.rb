cf93bbad8f2b673baff42c441aa3be33d7a7eadc
Stan Hu <stanhu@gmail.com>
2016-07-20 10:04:33 -0700

    Display the JSON payload in settings page for EE usage ping

--- spec/workers/gitlab_usage_ping_worker_spec.rb
+++ spec/workers/gitlab_usage_ping_worker_spec.rb
@@ -3,22 +3,6 @@ require 'spec_helper'
 describe GitlabUsagePingWorker do
   subject { GitlabUsagePingWorker.new }
 
-  it "gathers license data" do
-    data = subject.data
-    license = License.current
-
-    expect(data[:license_md5]).to eq(Digest::MD5.hexdigest(license.data))
-    expect(data[:version]).to eq(Gitlab::VERSION)
-    expect(data[:licensee]).to eq(license.licensee)
-    expect(data[:active_user_count]).to eq(User.active.count)
-    expect(data[:licensee]).to eq(license.licensee)
-    expect(data[:license_user_count]).to eq(license.user_count)
-    expect(data[:license_starts_at]).to eq(license.starts_at)
-    expect(data[:license_expires_at]).to eq(license.expires_at)
-    expect(data[:license_add_ons]).to eq(license.add_ons)
-    expect(data[:recorded_at]).to be_a(Time)
-  end
-
   it "sends POST request" do
     stub_application_setting(usage_ping_enabled: true)
 
