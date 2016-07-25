b33c539eaefbf602d13eaebe532f98dff350ed05
Valery Sizov <valery@gitlab.com>
2016-07-25 21:28:20 +0300

    Dicouple ES index update from RepositoryUpdateMirrorWorker

--- app/workers/elastic_commit_indexer_worker.rb
+++ app/workers/elastic_commit_indexer_worker.rb
@@ -3,13 +3,16 @@ class ElasticCommitIndexerWorker
 
   sidekiq_options queue: :elasticsearch
 
-  def perform(project_id, oldrev, newrev)
+  def perform(project_id, oldrev = nil, newrev = nil)
     project = Project.find(project_id)
+    repository = project.repository
+
+    return true unless repository.exists? && !repository.empty?
 
     indexer = Gitlab::Elastic::Indexer.new
     indexer.run(
       project_id,
-      project.repository.path_to_repo,
+      repository.path_to_repo,
       oldrev,
       newrev
     )
--- spec/workers/elastic_commit_indexer_worker_spec.rb
+++ spec/workers/elastic_commit_indexer_worker_spec.rb
@@ -10,5 +10,13 @@ describe ElasticCommitIndexerWorker do
       expect_any_instance_of(Gitlab::Elastic::Indexer).to receive(:run)
       subject.perform(project.id, '0000', '0000')
     end
+
+    it 'does not run indexer when project is empty' do
+      empty_project = create :empty_project
+
+      expect_any_instance_of(Gitlab::Elastic::Indexer).not_to receive(:run)
+
+      subject.perform(empty_project.id, '0000', '0000')
+    end
   end
 end
