4be80eef3b36286c45578b96c8446d791eb42b7b
Gokmen Goksel <gokmen@goksel.me>
2016-07-25 20:59:39 -0700

    KodingController: initial commit

---
+++ app/controllers/koding_controller.rb
@@ -0,0 +1,10 @@
+class KodingController < ApplicationController
+  skip_before_action :authenticate_user, :reject_blocked
+
+  layout 'koding'
+
+  def index
+
+  end
+
+end
--- spec/features/security/dashboard_access_spec.rb
+++ spec/features/security/dashboard_access_spec.rb
@@ -43,6 +43,14 @@ describe "Dashboard access", feature: true  do
     it { is_expected.to be_allowed_for :visitor }
   end
 
+  describe "GET /koding" do
+    subject { koding_path }
+
+    it { is_expected.to be_allowed_for :admin }
+    it { is_expected.to be_allowed_for :user }
+    it { is_expected.to be_denied_for :visitor }
+  end
+
   describe "GET /projects/new" do
     it { expect(new_project_path).to be_allowed_for :admin }
     it { expect(new_project_path).to be_allowed_for :user }
