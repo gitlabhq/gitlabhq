49bb3e79b416407ef56a54b6c687775cb840b7f7
Drew Blessing <drew@gitlab.com>
2016-07-29 16:24:23 -0500

    Replace LDAP group sync exclusive lease with state machine

---
+++ spec/models/ee/group_spec.rb
@@ -0,0 +1,88 @@
+require 'spec_helper'
+
+describe Group, models: true do
+  let(:group) { create(:group) }
+
+  it { is_expected.to include_module(EE::Group) }
+
+  describe 'states' do
+    it { is_expected.to be_ldap_sync_ready }
+
+    context 'after the start transition' do
+      it 'sets the last sync timestamp' do
+        expect { group.start_ldap_sync }.to change { group.ldap_sync_last_sync_at }
+      end
+    end
+
+    context 'after the finish transition' do
+      it 'sets the state to started' do
+        group.start_ldap_sync
+
+        expect(group).to be_ldap_sync_started
+
+        group.finish_ldap_sync
+      end
+
+      it 'sets last update and last successful update to the same timestamp' do
+        group.start_ldap_sync
+
+        group.finish_ldap_sync
+
+        expect(group.ldap_sync_last_update_at)
+          .to eq(group.ldap_sync_last_successful_update_at)
+      end
+
+      it 'clears previous error message on success' do
+        group.start_ldap_sync
+        group.mark_ldap_sync_as_failed('Error')
+        group.start_ldap_sync
+
+        group.finish_ldap_sync
+
+        expect(group.ldap_sync_error).to be_nil
+      end
+    end
+
+    context 'after the fail transition' do
+      it 'sets the state to failed' do
+        group.start_ldap_sync
+
+        group.fail_ldap_sync
+
+        expect(group).to be_ldap_sync_failed
+      end
+
+      it 'sets last update timestamp but not last successful update timestamp' do
+        group.start_ldap_sync
+
+        group.fail_ldap_sync
+
+        expect(group.ldap_sync_last_update_at)
+          .not_to eq(group.ldap_sync_last_successful_update_at)
+      end
+    end
+  end
+
+  describe '#mark_ldap_sync_as_failed' do
+    it 'sets the state to failed' do
+      group.start_ldap_sync
+
+      group.mark_ldap_sync_as_failed('Error')
+
+      expect(group).to be_ldap_sync_failed
+    end
+
+    it 'sets the error message' do
+      group.start_ldap_sync
+
+      group.mark_ldap_sync_as_failed('Something went wrong')
+
+      expect(group.ldap_sync_error).to eq('Something went wrong')
+    end
+
+    it 'is graceful when current state is not valid for the fail transition' do
+      expect(group).to be_ldap_sync_ready
+      expect { group.mark_ldap_sync_as_failed('Error') }.not_to raise_error
+    end
+  end
+end
