a6a21f4ebd1d66a4e4c6db5bf63bf891bf0a48d4
Gokmen Goksel <gokmen@goksel.me>
2016-08-02 22:24:19 +0300

    KodingController: updates based on reviews

--- app/controllers/koding_controller.rb
+++ app/controllers/koding_controller.rb
@@ -1,10 +1,7 @@
 class KodingController < ApplicationController
-  skip_before_action :authenticate_user, :reject_blocked
-
+  skip_before_action :authenticate_user!, :reject_blocked!
   layout 'koding'
 
   def index
-
   end
-
 end
