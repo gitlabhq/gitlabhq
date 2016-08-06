0c3ac8276811ec17608acc56f02a03a0890f809a
Adam Niedzielski <adamsunday@gmail.com>
2016-08-06 22:53:48 +0200

    Remove inline JavaScript. Add datepicker to the edit form.

--- app/assets/javascripts/project_members.js
+++ app/assets/javascripts/project_members.js
@@ -4,6 +4,13 @@
       $('li.project_member').bind('ajax:success', function() {
         return $(this).fadeOut();
       });
+
+      $('.js-project-members-page').on('focus', '.js-access-expiration-date', function() {
+        $(this).datepicker({
+          dateFormat: 'yy-mm-dd',
+          minDate: 1
+        });
+      });
     }
 
     return ProjectMembers;
