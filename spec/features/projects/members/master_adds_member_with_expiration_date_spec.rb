245d4abbbf3d8a1a74b730f4b95a29218de73ee3
Adam Niedzielski <adamsunday@gmail.com>
2016-08-07 13:42:54 +0200

    Single quotes all the way.

--- spec/features/projects/members/master_adds_member_with_expiration_date_spec.rb
+++ spec/features/projects/members/master_adds_member_with_expiration_date_spec.rb
@@ -14,12 +14,12 @@ feature 'Projects > Members > Master adds member with expiration date', feature:
   end
 
   scenario 'expiration date is displayed in the members list' do
-    travel_to Time.zone.parse("2016-08-06 08:00") do
+    travel_to Time.zone.parse('2016-08-06 08:00') do
       visit namespace_project_project_members_path(project.namespace, project)
 
       page.within '.users-project-form' do
         select2(new_member.id, from: '#user_ids', multiple: true)
-        fill_in 'Access expiration date', with: "2016-08-10"
+        fill_in 'Access expiration date', with: '2016-08-10'
         click_on 'Add users to project'
       end
 
@@ -30,13 +30,13 @@ feature 'Projects > Members > Master adds member with expiration date', feature:
   end
 
   scenario 'change expiration date' do
-    travel_to Time.zone.parse("2016-08-06 08:00") do
-      project.team.add_users([new_member.id], :developer, expires_at: "2016-09-06")
+    travel_to Time.zone.parse('2016-08-06 08:00') do
+      project.team.add_users([new_member.id], :developer, expires_at: '2016-09-06')
       visit namespace_project_project_members_path(project.namespace, project)
 
       page.within '.project_member:first-child' do
         click_on 'Edit'
-        fill_in 'Access expiration date', with: "2016-08-09"
+        fill_in 'Access expiration date', with: '2016-08-09'
         click_on 'Save'
         expect(page).to have_content('Expires in 3 days')
       end
