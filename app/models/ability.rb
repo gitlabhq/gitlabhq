0719b8ed1242e8506ee0e5797ec2ba1d61211ba8
http://jneen.net/ <jneen@jneen.net>
2016-08-09 13:56:54 -0700

    re-enable the cyclomatic complexity checker

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,7 +10,6 @@ class Ability
       cached_allowed(user, subject).include?(action)
     end
 
-    # rubocop: disable Metrics/CyclomaticComplexity
     def allowed(user, subject)
       return anonymous_abilities(user, subject) if user.nil?
       return [] unless user.is_a?(User)
