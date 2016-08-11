f0ff1bfdcc43decd1888f7b8d4a9e8c4dd5540d9
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2016-08-11 12:38:03 +0200

    Implement the main class of test coverage badge

--- spec/lib/gitlab/badge/coverage/report_spec.rb
+++ spec/lib/gitlab/badge/coverage/report_spec.rb
@@ -2,20 +2,80 @@ require 'spec_helper'
 
 describe Gitlab::Badge::Coverage::Report do
   let(:project) { create(:project) }
-
-  let(:pipeline) do
-    create(:ci_pipeline, project: project,
-                         sha: project.commit.id,
-                         ref: 'master')
-  end
+  let(:job_name) { nil }
 
   let(:badge) do
-    described_class.new(project, 'master')
+    described_class.new(project, 'master', job_name)
   end
 
-  context 'builds exist' do
+  describe '#entity' do
+    it 'describes a coverage' do
+      expect(badge.entity).to eq 'coverage'
+    end
   end
 
-  context 'builds do not exist' do
+  shared_examples 'unknown coverage report' do
+    context 'particular job specified' do
+      let(:job_name) { '' }
+
+      it 'returns nil' do
+        expect(badge.status).to be_nil
+      end
+    end
+
+    context 'particular job not specified' do
+      let(:job_name) { nil }
+
+      it 'returns nil' do
+        expect(badge.status).to be_nil
+      end
+    end
+  end
+
+  context 'pipeline exists' do
+    let!(:pipeline) do
+      create(:ci_pipeline, project: project,
+                           sha: project.commit.id,
+                           ref: 'master')
+    end
+
+    context 'builds exist' do
+      before do
+        create(:ci_build, name: 'first', pipeline: pipeline, coverage: 40)
+        create(:ci_build, pipeline: pipeline, coverage: 60)
+      end
+
+      context 'particular job specified' do
+        let(:job_name) { 'first' }
+
+        it 'returns coverage for the particular job' do
+          expect(badge.status).to eq 40
+        end
+      end
+
+      context 'particular job not specified' do
+        let(:job_name) { '' }
+
+        it 'returns arithemetic mean for the pipeline' do
+          expect(badge.status).to eq 50
+        end
+      end
+    end
+
+    context 'builds do not exist' do
+      it_behaves_like 'unknown coverage report'
+
+      context 'particular job specified' do
+        let(:job_name) { 'nonexistent' }
+
+        it 'retruns nil' do
+          expect(badge.status).to be_nil
+        end
+      end
+    end
+  end
+
+  context 'pipeline does not exist' do
+    it_behaves_like 'unknown coverage report'
   end
 end
