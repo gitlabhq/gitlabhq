cdb0caaf320a67f193a7ab9faac9271a625d8b98
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2016-08-11 13:56:20 +0200

    Extract common badge metadata test examples

--- spec/lib/gitlab/badge/build/metadata_spec.rb
+++ spec/lib/gitlab/badge/build/metadata_spec.rb
@@ -1,28 +1,11 @@
 require 'spec_helper'
+require 'lib/gitlab/badge/shared/metadata'
 
 describe Gitlab::Badge::Build::Metadata do
   let(:badge) { double(project: create(:project), ref: 'feature') }
   let(:metadata) { described_class.new(badge) }
 
-  describe '#to_html' do
-    let(:html) { Nokogiri::HTML.parse(metadata.to_html) }
-    let(:a_href) { html.at('a') }
-
-    it 'points to link' do
-      expect(a_href[:href]).to eq metadata.link_url
-    end
-
-    it 'contains clickable image' do
-      expect(a_href.children.first.name).to eq 'img'
-    end
-  end
-
-  describe '#to_markdown' do
-    subject { metadata.to_markdown }
-
-    it { is_expected.to include metadata.image_url }
-    it { is_expected.to include metadata.link_url }
-  end
+  it_behaves_like 'badge metadata'
 
   describe '#image_url' do
     it 'returns valid url' do
