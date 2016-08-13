1301572a7a31f2fb344ec9bc8d5404f31c8e4882
Ruben Davila <rdavila84@gmail.com>
2016-08-13 16:17:59 -0500

    Fix broken 'Edit Merge Request' spec.

--- spec/features/merge_requests/edit_mr_spec.rb
+++ spec/features/merge_requests/edit_mr_spec.rb
@@ -9,13 +9,9 @@ feature 'Edit Merge Request', feature: true do
     before do
       project.team << [user, :master]
 
-      login_as user
-
-      visit edit_namespace_project_merge_request_path(project.namespace, project, merge_request)
+      visit_edit_mr_page
     end
-  end
 
-  context 'editing a MR' do
     it 'has class js-quick-submit in form' do
       expect(page).to have_selector('.js-quick-submit')
     end
@@ -26,9 +22,7 @@ feature 'Edit Merge Request', feature: true do
       project.team << [user, :master]
       project.update_attributes(approvals_before_merge: 2)
 
-      login_as user
-
-      visit edit_namespace_project_merge_request_path(project.namespace, project, merge_request)
+      visit_edit_mr_page
     end
 
     it 'shows the saved MR' do
@@ -36,5 +30,11 @@ feature 'Edit Merge Request', feature: true do
 
       expect(page).to have_link('Close merge request')
     end
+  end
+
+  def visit_edit_mr_page
+    login_as user
+
+    visit edit_namespace_project_merge_request_path(project.namespace, project, merge_request)
   end
 end
