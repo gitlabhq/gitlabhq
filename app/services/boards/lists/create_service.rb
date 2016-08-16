d8fc783bb9438434d157ca07b9dc033e7c4115df
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2016-08-15 23:20:16 -0300

    Remove useless increment when creating a new board list

--- app/services/boards/lists/create_service.rb
+++ app/services/boards/lists/create_service.rb
@@ -3,26 +3,19 @@ module Boards
     class CreateService < Boards::BaseService
       def execute
         List.transaction do
-          position = find_next_position
-          increment_higher_lists(position)
-          create_list_at(position)
+          create_list_at(next_position)
         end
       end
 
       private
 
-      def find_next_position
+      def next_position
         max_position = board.lists.label.maximum(:position)
         max_position.nil? ? 0 : max_position.succ
       end
 
       def create_list_at(position)
         board.lists.create(params.merge(list_type: :label, position: position))
-      end
-
-      def increment_higher_lists(position)
-        board.lists.label.where('position >= ?', position)
-                         .update_all('position = position + 1')
       end
     end
   end
