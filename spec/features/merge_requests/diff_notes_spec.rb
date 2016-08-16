48b5812a449e5d090d3529e3fbfb679cee7de5da
Stan Hu <stanhu@gmail.com>
2016-08-15 20:57:28 -0700

    Make Capybara finders less susceptible to timing issues

--- spec/features/merge_requests/diff_notes_spec.rb
+++ spec/features/merge_requests/diff_notes_spec.rb
@@ -129,11 +129,13 @@ feature 'Diff notes', js: true, feature: true do
     end
 
     def get_inline_line_components(line_holder)
-      { content: line_holder.first('.line_content'), num: line_holder.first('.diff-line-num') }
+      { content: line_holder.find('.line_content', match: :first), num: line_holder.find('.diff-line-num', match: :first) }
     end
 
     def get_parallel_line_components(line_holder, diff_side = nil)
       side_index = diff_side == 'left' ? 0 : 1
+      find('.line_content', match: :first)
+      find('.diff-line-num', match: :first)
       { content: line_holder.all('.line_content')[side_index], num: line_holder.all('.diff-line-num')[side_index] }
     end
 
