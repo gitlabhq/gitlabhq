76872372376e57cd7d55ba9b9c63b25fe53c82df
barthc <mackintosh02@gmail.com>
2016-08-17 12:21:06 +0100

    prevent authored awardable thumbs votes
    prevent authored awardable thumbs votes
    
    prevent authored awardable thumbs votes

--- app/controllers/concerns/toggle_award_emoji.rb
+++ app/controllers/concerns/toggle_award_emoji.rb
@@ -8,6 +8,8 @@ module ToggleAwardEmoji
   def toggle_award_emoji
     name = params.require(:name)
 
+    return render json: { ok: false } unless awardable.user_can_award?(current_user, name)
+
     awardable.toggle_award_emoji(name, current_user)
     TodoService.new.new_award_emoji(to_todoable(awardable), current_user)
 
--- app/models/concerns/awardable.rb
+++ app/models/concerns/awardable.rb
@@ -59,6 +59,15 @@ module Awardable
     true
   end
 
+  def awardable_votes?(name)
+    AwardEmoji::UPVOTE_NAME == name || AwardEmoji::DOWNVOTE_NAME == name
+  end
+
+  def user_can_award?(current_user, name)
+    name = normalize_name(name)
+    !(self.user_authored?(current_user) && awardable_votes?(name))
+  end
+
   def awarded_emoji?(emoji_name, current_user)
     award_emoji.where(name: emoji_name, user: current_user).exists?
   end
