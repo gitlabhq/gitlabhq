6ab44d17fd40cf4dcabb6035dc9fb233e66725cd
Rubén Dávila Santos <ruben@gitlab.com>
2016-08-18 13:12:17 +0000

    Merge branch 'tab-persistance' into 'master'
    Make new tabs persistent when changing view.
    
    ## What does this MR do?
    
    ## Are there points in the code the reviewer needs to double check?
    
    ## Why was this MR needed?
    
    ## What are the relevant issue numbers?
    
    ## Screenshots (if relevant)
    
    ## Does this MR meet the acceptance criteria?
    
    - [ ] [CHANGELOG](https://gitlab.com/gitlab-org/gitlab-ce/blob/master/CHANGELOG) entry added
    - [ ] [Documentation created/updated](https://gitlab.com/gitlab-org/gitlab-ce/blob/master/doc/development/doc_styleguide.md)
    - [ ] API support added
    - Tests
      - [ ] Added for this feature/bug
      - [ ] All builds are passing
    - [ ] Conform by the [style guides](https://gitlab.com/gitlab-org/gitlab-ce/blob/master/CONTRIBUTING.md#style-guides)
    - [ ] Branch has no merge conflicts with `master` (if you do - rebase it please)
    - [ ] [Squashed related commits together](https://git-scm.com/book/en/Git-Tools-Rewriting-History#Squashing-Commits)
    
    cc @stanhu @DouweM @rdavila 
    
    See merge request !5859

--- app/controllers/projects/merge_requests_controller.rb
+++ app/controllers/projects/merge_requests_controller.rb
@@ -216,7 +216,6 @@ class Projects::MergeRequestsController < Projects::ApplicationController
     @base_commit = @merge_request.diff_base_commit
     @diffs = @merge_request.diffs(diff_options) if @merge_request.compare
     @diff_notes_disabled = true
-
     @pipeline = @merge_request.pipeline
     @statuses = @pipeline.statuses.relevant if @pipeline
 
