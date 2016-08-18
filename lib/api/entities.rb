2038e035c73f80292fa1bf9757803d8ab5e6ecab
Z.J. van de Weg <zegerjan@gitlab.com>
2016-08-18 21:10:20 +0200

    Do not expose projects on deployments

--- lib/api/entities.rb
+++ lib/api/entities.rb
@@ -521,10 +521,9 @@ module API
 
     class Deployment < Grape::Entity
       expose :id, :iid, :ref, :sha, :created_at
-      expose :project,      using: Entities::Project
-      expose :user,         using: Entities::UserBasic
-      expose :environment,  using: Entities::EnvironmentBasic
-      expose :deployable,   using: Entities::Build
+      expose :user,        using: Entities::UserBasic
+      expose :environment, using: Entities::EnvironmentBasic
+      expose :deployable,  using: Entities::Build
     end
 
     class RepoLicense < Grape::Entity
