b07cd8ec641a933b819300e005e7124dd1d11c81
Douwe Maan <douwe@gitlab.com>
2016-08-18 18:18:25 +0000

    Merge branch 'zj-pipelines-api-endpoints' into 'master'
    Add endpoints for pipelines
    
    - [x] [CHANGELOG](https://gitlab.com/gitlab-org/gitlab-ce/blob/master/CHANGELOG) entry added
    - [x] [Documentation created/updated](https://gitlab.com/gitlab-org/gitlab-ce/blob/master/doc/development/doc_styleguide.md)
    - [x] API support added
    - Tests
      - [x] Added for this feature/bug
      - [x] All builds are passing
    - [x] Conform by the [style guides](https://gitlab.com/gitlab-org/gitlab-ce/blob/master/CONTRIBUTING.md#style-guides)
    - [ ] Branch has no merge conflicts with `master` (if you do - rebase it please)
    - [ ] [Squashed related commits together](https://git-scm.com/book/en/Git-Tools-Rewriting-History#Squashing-Commits)
    
    See merge request !5837

--- lib/api/entities.rb
+++ lib/api/entities.rb
@@ -500,6 +500,14 @@ module API
       expose :key, :value
     end
 
+    class Pipeline < Grape::Entity
+      expose :id, :status, :ref, :sha, :before_sha, :tag, :yaml_errors
+
+      expose :user, with: Entities::UserBasic
+      expose :created_at, :updated_at, :started_at, :finished_at, :committed_at
+      expose :duration
+    end
+
     class Environment < Grape::Entity
       expose :id, :name, :external_url
     end
