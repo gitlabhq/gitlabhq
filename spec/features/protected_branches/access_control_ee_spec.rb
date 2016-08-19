8d25553414b1fed67fadc63f15ac97e987a0f067
Timothy Andrew <mail@timothyandrew.net>
2016-08-20 00:13:14 +0530

    Improve protected branches feature spec.
    1. Previously, we had a separate test for each and every possible role,
       and another test for access by user, each multiplied by the access
       types (merge and push), and further multiplied by the type of CRUD
       operation (create or update).
    
    2. Since we allow selecting multiple access levels at once, it makes
       sense to collapse a lot of these tests. Now, in single test, we check
       access levels for all roles and 5 different users. Multiply that by
       4 (as mentioned above), and we only have 4 tests total (that still
       test everything the previous tests did).
    
    3. Use a shared example to reduce potential conflicts in the future,
       since CE access control specs are significantly different.

---
+++ spec/features/protected_branches/access_control_ee_spec.rb
@@ -0,0 +1,49 @@
+RSpec.shared_examples "protected branches > access control > EE" do
+  [['merge', ProtectedBranch::MergeAccessLevel], ['push', ProtectedBranch::PushAccessLevel]].each do |git_operation, access_level_class|
+    # Need to set a default for the `git_operation` access level that _isn't_ being tested
+    other_git_operation = git_operation == 'merge' ? 'push' : 'merge'
+
+    it "allows creating protected branches that roles and users can #{git_operation} to" do
+      users = create_list(:user, 5)
+      users.each { |user| project.team << [user, :developer] }
+      roles = access_level_class.human_access_levels
+
+      visit namespace_project_protected_branches_path(project.namespace, project)
+      set_protected_branch_name('master')
+
+      users.each { |user| set_allowed_to(git_operation, user.name) }
+      roles.each { |(_, access_type_name)| set_allowed_to(git_operation, access_type_name) }
+      set_allowed_to(other_git_operation)
+
+      click_on "Protect"
+
+      expect(ProtectedBranch.count).to eq(1)
+      roles.each { |(access_type_id, _)| expect(ProtectedBranch.last.send("#{git_operation}_access_levels".to_sym).map(&:access_level)).to include(access_type_id) }
+      users.each { |user| expect(ProtectedBranch.last.send("#{git_operation}_access_levels".to_sym).map(&:user_id)).to include(user.id) }
+    end
+
+    it "allows updating protected branches that roles and users can #{git_operation} to" do
+      users = create_list(:user, 5)
+      users.each { |user| project.team << [user, :developer] }
+      roles = access_level_class.human_access_levels
+
+      visit namespace_project_protected_branches_path(project.namespace, project)
+      set_protected_branch_name('master')
+      set_allowed_to('merge')
+      set_allowed_to('push')
+
+      click_on "Protect"
+
+      within(".js-protected-branch-edit-form") do
+        users.each { |user| set_allowed_to(git_operation, user.name) }
+        roles.each { |(_, access_type_name)| set_allowed_to(git_operation, access_type_name) }
+      end
+
+      wait_for_ajax
+
+      expect(ProtectedBranch.count).to eq(1)
+      roles.each { |(access_type_id, _)| expect(ProtectedBranch.last.send("#{git_operation}_access_levels".to_sym).map(&:access_level)).to include(access_type_id) }
+      users.each { |user| expect(ProtectedBranch.last.send("#{git_operation}_access_levels".to_sym).map(&:user_id)).to include(user.id) }
+    end
+  end
+end
