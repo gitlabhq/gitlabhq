464437139f91e8c77e7f238bda2504a6ed40ecaa
Ruben Davila <rdavila84@gmail.com>
2016-08-20 14:29:23 -0500

    Fix more specs for abilities in EE.

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -192,13 +192,13 @@ describe Ability, lib: true do
 
         results = described_class.project_abilities(admin, project)
 
-        expect(results.count).to eq(68)
+        expect(results.count).to eq(74)
       end
 
       it 'returns permissions for an owner' do
         results = described_class.project_abilities(project.owner, project)
 
-        expect(results.count).to eq(68)
+        expect(results.count).to eq(73)
       end
 
       it 'returns permissions for a master' do
