b4e76e64389ed86755bdb764e3f4c060a1c1b8d4
Valery Sizov <valery@gitlab.com>
2016-08-26 13:18:14 +0300

    [ES] Elastic workers should check settings each time when they are running

--- app/workers/elastic_commit_indexer_worker.rb
+++ app/workers/elastic_commit_indexer_worker.rb
@@ -1,9 +1,12 @@
 class ElasticCommitIndexerWorker
   include Sidekiq::Worker
+  include Gitlab::CurrentSettings
 
   sidekiq_options queue: :elasticsearch, retry: 2
 
   def perform(project_id, oldrev = nil, newrev = nil)
+    return true unless current_application_settings.elasticsearch_indexing?
+
     project = Project.find(project_id)
     repository = project.repository
 
--- spec/workers/elastic_commit_indexer_worker_spec.rb
+++ spec/workers/elastic_commit_indexer_worker_spec.rb
@@ -6,6 +6,10 @@ describe ElasticCommitIndexerWorker do
   subject { described_class.new }
 
   describe '#perform' do
+    before do
+      stub_application_setting(elasticsearch_indexing: true)
+    end
+
     it 'runs indexer' do
       expect_any_instance_of(Gitlab::Elastic::Indexer).to receive(:run)
       subject.perform(project.id, '0000', '0000')
@@ -26,6 +30,14 @@ describe ElasticCommitIndexerWorker do
       expect_any_instance_of(Repository).to receive(:rugged).and_return(rugged)
 
       expect(subject.perform(project.id)).to be_truthy
+    end
+
+    it 'returns true if ES disabled' do
+      stub_application_setting(elasticsearch_indexing: false)
+
+      expect_any_instance_of(Gitlab::Elastic::Indexer).not_to receive(:run)
+
+      expect(subject.perform(1)).to be_truthy
     end
   end
 end
