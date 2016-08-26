a165a4452eb7430715d672191411c76de7e7f659
Satish Perala <satish@cybrilla.com>
2016-08-26 09:15:09 +0530

    Checking for the presense of markdown text

--- lib/markdown_utils.rb
+++ lib/markdown_utils.rb
@@ -3,6 +3,10 @@ class MarkdownUtils
 
   # Convert image urls in the markdown text to absolute urls
   def self.absolute_image_urls(markdown_text)
-    markdown_text.gsub(/!\[(.*?)\]\((.*?)\)/, "![\\1](#{Settings.gitlab.url}\\2)")
+    if markdown_text.present?
+      markdown_text.gsub(/!\[(.*?)\]\((.*?)\)/, "![\\1](#{Settings.gitlab.url}\\2)")
+    else
+      markdown_text
+    end
   end
 end
