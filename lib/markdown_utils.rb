56b432bd998ad866618dc2a41bbd730371e47dde
Satish Perala <satish@cybrilla.com>
2016-08-26 09:36:03 +0530

    Removing the empty line at the class body beginning

--- lib/markdown_utils.rb
+++ lib/markdown_utils.rb
@@ -1,6 +1,5 @@
 # Class to have all utility functions related to markdown
 class MarkdownUtils
-
   # Convert image urls in the markdown text to absolute urls
   def self.absolute_image_urls(markdown_text)
     if markdown_text.present?
