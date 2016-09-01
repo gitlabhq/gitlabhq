8245c667b552b90e1f64cdadffea9b2649e769ab
Drew Blessing <drew@gitlab.com>
2016-09-01 16:12:29 -0500

    Add Sync now to group members page

--- app/workers/ldap_group_sync_worker.rb
+++ app/workers/ldap_group_sync_worker.rb
@@ -3,9 +3,21 @@ class LdapGroupSyncWorker
 
   sidekiq_options retry: false
 
-  def perform
-    logger.info 'Started LDAP group sync'
-    EE::Gitlab::LDAP::Sync::Groups.execute
-    logger.info 'Finished LDAP group sync'
+  def perform(group_id = nil)
+    if group_id
+      group = Group.find_by(id: group_id)
+      unless group
+        logger.warn "Could not find group #{group_id} for LDAP group sync"
+        return
+      end
+
+      logger.info "Started LDAP group sync for group #{group.name} (#{group.id})"
+      EE::Gitlab::LDAP::Sync::Group.execute_all_providers(group)
+      logger.info "Finished LDAP group sync for group #{group.name} (#{group.id})"
+    else
+      logger.info 'Started LDAP group sync'
+      EE::Gitlab::LDAP::Sync::Groups.execute
+      logger.info 'Finished LDAP group sync'
+    end
   end
 end
