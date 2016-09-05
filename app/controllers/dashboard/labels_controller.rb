825de2ec79567ec0cb57972ce5190ef15bef3ca8
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2016-09-05 18:46:16 -0300

    Fix Dashboard::LabelsController

--- app/controllers/dashboard/labels_controller.rb
+++ app/controllers/dashboard/labels_controller.rb
@@ -1,6 +1,6 @@
 class Dashboard::LabelsController < Dashboard::ApplicationController
   def index
-    labels = Label.where(project_id: projects).select(:id, :title, :color).uniq(:title)
+    labels = Label.where(subject: projects).select(:id, :title, :color).uniq(:title)
 
     respond_to do |format|
       format.json { render json: labels }
