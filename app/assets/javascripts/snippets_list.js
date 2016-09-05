7f0400398d7a09fd6eb35e8058c93ab693c0c527
Sean McGivern <sean@gitlab.com>
2016-09-05 17:38:20 +0100

    Convert SnippetsList to ES6

--- app/assets/javascripts/snippets_list.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  this.gl.SnippetsList = function() {
-    $('.snippets-list-holder .pagination').on('ajax:success', function(e, data) {
-      $('.snippets-list-holder').replaceWith(data.html);
-    });
-  };
-}).call(this);
