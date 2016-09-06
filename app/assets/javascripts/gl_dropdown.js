c67d2f46dd24d6f2bfc0d64ed62a4a6b47f1fcc0
Alfredo Sumaran <alfredo@gitlab.com>
2016-09-06 13:59:20 -0500

    fieldName can’t be a function
    Restore changes introduced in https://gitlab.com/gitlab-org/gitlab-ee/merge_requests/581

--- app/assets/javascripts/gl_dropdown.js
+++ app/assets/javascripts/gl_dropdown.js
@@ -488,7 +488,7 @@
       } else {
         if (!selected) {
           value = this.options.id ? this.options.id(data) : data.id;
-          fieldName = typeof this.options.fieldName === 'function' ? this.options.fieldName() : this.options.fieldName;
+          fieldName = this.options.fieldName;
 
           field = this.dropdown.parent().find("input[name='" + fieldName + "'][value='" + value + "']");
           if (field.length) {
@@ -546,6 +546,7 @@
 
     GitLabDropdown.prototype.rowClicked = function(el) {
       var field, fieldName, groupName, isInput, selectedIndex, selectedObject, value;
+      fieldName = this.options.fieldName;
       isInput = $(this.el).is('input');
       if (this.renderedData) {
         groupName = el.data('group');
@@ -557,7 +558,6 @@
           selectedObject = this.renderedData[selectedIndex];
         }
       }
-      fieldName = typeof this.options.fieldName === 'function' ? this.options.fieldName(selectedObject) : this.options.fieldName;
       value = this.options.id ? this.options.id(selectedObject, el) : selectedObject.id;
       if (isInput) {
         field = $(this.el);
@@ -608,9 +608,6 @@
       $input = $('<input>').attr('type', 'hidden').attr('name', fieldName).val(value);
       if (this.options.inputId != null) {
         $input.attr('id', this.options.inputId);
-      }
-      if (selectedObject && selectedObject.type) {
-        $input.attr('data-type', selectedObject.type);
       }
       return this.dropdown.before($input);
     };
