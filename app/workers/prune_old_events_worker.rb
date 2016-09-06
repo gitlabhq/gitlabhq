4b98e812b29e8f9bc11d2eaa4ad0ddd474caa9a6
Olaf Tomalka <olaf.tomalka@gmail.com>
2016-09-06 11:53:34 +0200

    Optimized event pruning query to avoid two queries.

--- app/workers/prune_old_events_worker.rb
+++ app/workers/prune_old_events_worker.rb
@@ -2,7 +2,16 @@ class PruneOldEventsWorker
   include Sidekiq::Worker
 
   def perform
-    # Contribution calendar shows maximum 12 months of events
-    Event.delete(Event.unscoped.where('created_at < ?', (12.months + 1.day).ago).limit(10_000).pluck(:id))
+    # Contribution calendar shows maximum 12 months of events.
+    # Double nested query is used because MySQL doesn't allow DELETE subqueries
+    # on the same table.
+    Event.unscoped.where(
+      '(id IN (SELECT id FROM (?) ids_to_remove))',
+      Event.unscoped.where(
+        'created_at < ?',
+        (12.months + 1.day).ago).
+      select(:id).
+      limit(10_000)).
+    delete_all
   end
 end
