035a38f18ea7bec1afd64218866a2fd986449b00
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2016-09-07 16:18:15 +0300

    Fix issuable_bulk_update_path and merge request spinach test
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- app/helpers/issuables_helper.rb
+++ app/helpers/issuables_helper.rb
@@ -113,10 +113,10 @@ module IssuablesHelper
   end
 
   def issuable_bulk_update_path(type)
-    if type == "issue"
-      bulk_update_namespace_project_issues_path(@project.namespace, @project)
-    else
+    if type == :merge_requests
       bulk_update_namespace_project_merge_requests_path(@project.namespace, @project)
+    else
+      bulk_update_namespace_project_issues_path(@project.namespace, @project)
     end
   end
 end
