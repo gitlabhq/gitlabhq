5067244cce2a89ea39a955f140f803e91a46e806
Stan Hu <stanhu@gmail.com>
2016-09-15 17:07:22 -0700

    Add more usage data to EE ping.
    Load it asynchronously to prevent blocking a page load.
    
    Closes #997
    
    Add a container around usage data help-block to prevent horizontal scroll in Firefox
    
    Closes #808

---
+++ app/assets/javascripts/application_settings.js.es6
@@ -0,0 +1,16 @@
+(global => {
+  global.gl = global.gl || {};
+
+  gl.ApplicationSettings = function() {
+    var usage_data_url = $('.usage-data').data('endpoint');
+
+    $.ajax({
+      type: "GET",
+      url: usage_data_url,
+      dataType: "html",
+      success: function (html) {
+        $(".usage-data").html(html);
+      }
+    });
+  }
+})(window);
---
+++ lib/gitlab/usage_data.rb
@@ -0,0 +1,73 @@
+module Gitlab
+  class UsageData
+    class << self
+      def data
+        Rails.cache.fetch('usage_data', expires_in: 1.hour) { uncached_data }
+      end
+
+      def uncached_data
+        license_usage_data.merge(system_usage_data)
+      end
+
+      def to_json
+        data.to_json
+      end
+
+      def system_usage_data
+        {
+          counts: {
+            boards: Board.count,
+            ci_builds: ::Ci::Build.count,
+            ci_pipelines: ::Ci::Pipeline.count,
+            ci_runners: ::Ci::Runner.count,
+            ci_triggers: ::Ci::Trigger.count,
+            deploy_keys: DeployKey.count,
+            deployments: Deployment.count,
+            environments: Environment.count,
+            geo_nodes: GeoNode.count,
+            groups: Group.count,
+            issues: Issue.count,
+            keys: Key.count,
+            labels: Label.count,
+            ldap_group_links: LdapGroupLink.count,
+            ldap_keys: LDAPKey.count,
+            ldap_users: User.ldap.count,
+            lfs_objects: LfsObject.count,
+            merge_requests: MergeRequest.count,
+            milestones: Milestone.count,
+            notes: Note.count,
+            pushes: Event.code_push.count,
+            pages_domains: PagesDomain.count,
+            projects: Project.count,
+            protected_branchess: ProtectedBranch.count,
+            releases: Release.count,
+            remote_mirrors: RemoteMirror.count,
+            services: Service.where(active: true).count,
+            snippets: Snippet.count,
+            todos: Todo.count,
+            web_hooks: WebHook.count
+          }
+        }
+      end
+
+      def license_usage_data
+        usage_data = { version: Gitlab::VERSION,
+                       active_user_count: User.active.count }
+        license = ::License.current
+
+        if license
+          usage_data[:license_md5] = Digest::MD5.hexdigest(license.data)
+          usage_data[:historical_max_users] = ::HistoricalData.max_historical_user_count
+          usage_data[:licensee] = license.licensee
+          usage_data[:license_user_count] = license.user_count
+          usage_data[:license_starts_at] = license.starts_at
+          usage_data[:license_expires_at] = license.expires_at
+          usage_data[:license_add_ons] = license.add_ons
+          usage_data[:recorded_at] = Time.now
+        end
+
+        usage_data
+      end
+    end
+  end
+end
