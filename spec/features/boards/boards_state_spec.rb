d46b4558272682ec071c8dc277ec09a8678d1805
Phil Hughes <me@iamphill.com>
2016-09-16 15:19:21 +0100

    Added tests for filtering & states

--- spec/features/boards/boards_state_spec.rb
+++ spec/features/boards/boards_state_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 describe 'Issue Boards', feature: true, js: true do
   include WaitForVueResource
+  include WaitForAjax
 
   let(:project) { create(:empty_project, :public) }
   let(:user)    { create(:user) }
-  let!(:user2)  { create(:user) }
 
   before do
     project.create_board
@@ -18,9 +18,10 @@ describe 'Issue Boards', feature: true, js: true do
   end
 
   context 'state' do
+    let!(:milestone)  { create(:milestone, project: project) }
     let(:planning)    { create(:label, project: project, name: 'Planning') }
     let!(:list1)      { create(:list, board: project.board, label: planning, position: 0) }
-    let!(:issue1)     { create(:labeled_issue, :closed, project: project, labels: [planning]) }
+    let!(:issue1)     { create(:labeled_issue, :closed, project: project, labels: [planning], milestone: milestone, assignee: user, author: user) }
     let!(:issue2)     { create(:labeled_issue, project: project, labels: [planning]) }
 
     before do
@@ -58,6 +59,76 @@ describe 'Issue Boards', feature: true, js: true do
 
       page.within('.board:nth-child(2)') do
         expect(page).to have_selector('.card', count: 2)
+      end
+    end
+
+    context 'filtering' do
+      it 'filters assignee and state' do
+        page.within '.issues-filters' do
+          click_button('Assignee')
+          wait_for_ajax
+
+          page.within '.dropdown-menu-assignee' do
+            click_link(user.name)
+          end
+          wait_for_vue_resource
+
+          expect(find('.js-assignee-search')).to have_content(user.name)
+        end
+
+        page.within('.issues-state-filters') do
+          click_link 'Closed'
+        end
+
+        page.within('.board:nth-child(2)') do
+          expect(page).to have_selector('.card', count: 1)
+          expect(first('.card')).to have_content(issue1.title)
+        end
+      end
+
+      it 'filters milestone and state' do
+        page.within '.issues-filters' do
+          click_button('Milestone')
+          wait_for_ajax
+
+          page.within '.milestone-filter' do
+            click_link(milestone.title)
+          end
+          wait_for_vue_resource
+
+          expect(find('.js-milestone-select')).to have_content(milestone.title)
+        end
+
+        page.within('.issues-state-filters') do
+          click_link 'Closed'
+        end
+
+        page.within('.board:nth-child(2)') do
+          expect(page).to have_selector('.card', count: 1)
+          expect(first('.card')).to have_content(issue1.title)
+        end
+      end
+
+      it 'filters label and state' do
+        page.within '.issues-filters' do
+          click_button('Label')
+          wait_for_ajax
+
+          page.within '.dropdown-menu-labels' do
+            click_link(planning.title)
+            wait_for_vue_resource
+            find('.dropdown-menu-close').click
+          end
+        end
+
+        page.within('.issues-state-filters') do
+          click_link 'Closed'
+        end
+
+        page.within('.board:nth-child(2)') do
+          expect(page).to have_selector('.card', count: 1)
+          expect(first('.card')).to have_content(issue1.title)
+        end
       end
     end
   end
