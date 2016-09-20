929ff01ac08db320402c31bb70b463007d1b379d
Rémy Coutable <remy@rymai.me>
2016-09-20 10:23:13 +0200

    Ensure we have a user before checking for their permission in Notes::SlashCommandsService
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- app/services/notes/slash_commands_service.rb
+++ app/services/notes/slash_commands_service.rb
@@ -11,6 +11,7 @@ module Notes
 
     def self.supported?(note, current_user)
       noteable_update_service(note) &&
+        current_user &&
         current_user.can?(:"update_#{note.noteable_type.underscore}", note.noteable)
     end
 
