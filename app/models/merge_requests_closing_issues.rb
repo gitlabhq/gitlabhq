b43214c533ae5942ec4b50c9f32e749b8ca5daae
Timothy Andrew <mail@timothyandrew.net>
2016-09-20 10:45:36 +0530

    Add a uniqueness constraint to `MergeRequestsClosingIssues`

--- app/models/merge_requests_closing_issues.rb
+++ app/models/merge_requests_closing_issues.rb
@@ -1,4 +1,9 @@
 class MergeRequestsClosingIssues < ActiveRecord::Base
   belongs_to :merge_request
   belongs_to :issue
+
+  validates_uniqueness_of :merge_request_id, scope: :issue_id
+
+  validates_presence_of :merge_request
+  validates_presence_of :issue
 end
