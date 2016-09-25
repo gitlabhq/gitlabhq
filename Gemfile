300b7763080765c6d62d2aaa63b99e8eb689f065
Gabriel Mazetto <gabriel@gitlab.com>
2016-09-25 19:14:05 -0300

    Use Database rewinder for specs with js: true

--- Gemfile
+++ Gemfile
@@ -271,6 +271,7 @@ group :development, :test do
   gem 'fuubar', '~> 2.0.0'
 
   gem 'database_cleaner',   '~> 1.5.0'
+  gem 'database_rewinder',  '~> 0.6.4'
   gem 'factory_girl_rails', '~> 4.6.0'
   gem 'rspec-rails',        '~> 3.5.0'
   gem 'rspec-retry',        '~> 0.4.5'
--- spec/support/db_cleaner.rb
+++ spec/support/db_cleaner.rb
@@ -1,21 +1,21 @@
 RSpec.configure do |config|
   config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
-  end
-
-  config.before(:each) do
     DatabaseCleaner.strategy = :transaction
+    DatabaseRewinder.clean_all
   end
 
-  config.before(:each, js: true) do
-    DatabaseCleaner.strategy = :truncation
+  config.before(:each) do |example|
+    unless example.metadata[:js]
+      DatabaseCleaner.start
+    end
   end
 
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-
-  config.append_after(:each) do
-    DatabaseCleaner.clean
+  config.append_after(:each) do |example|
+    if example.metadata[:js]
+      DatabaseRewinder.clean
+    else
+      DatabaseCleaner.clean
+      DatabaseRewinder.cleaners.each {|c| c.send(:reset) }
+    end
   end
 end
