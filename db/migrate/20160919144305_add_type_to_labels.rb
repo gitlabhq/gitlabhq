9629bb962cd3666ac58cfbaba9d79df011221f41
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2016-10-03 18:43:39 -0300

    Add column type to labels and do the batch update in the same migration

--- db/migrate/20160919144305_add_type_to_labels.rb
+++ db/migrate/20160919144305_add_type_to_labels.rb
@@ -1,9 +1,14 @@
 class AddTypeToLabels < ActiveRecord::Migration
   include Gitlab::Database::MigrationHelpers
 
-  DOWNTIME = false
+  DOWNTIME = true
+  DOWNTIME_REASON = 'Labels will not work as expected until this migration is complete.'
 
   def change
     add_column :labels, :type, :string
+
+    update_column_in_batches(:labels, :type, 'ProjectLabel') do |table, query|
+      query.where(table[:project_id].not_eq(nil))
+    end
   end
 end
