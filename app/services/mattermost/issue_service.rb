731176a7b3d08f25cf0abe14efc629446930a8a1
Z.J. van de Weg <zegerjan@gitlab.com>
2016-10-06 15:56:27 +0200

    Support for creation of issues

--- app/services/mattermost/issue_service.rb
+++ app/services/mattermost/issue_service.rb
@@ -1,6 +1,32 @@
 module Mattermost
   class IssueService < BaseService
+    def execute
+      if params[:text].start_with?('create')
+        create_issue
+      else
+        super
+      end
+    end
+
     private
+
+    def create_issue
+      issue = Issues::CreateService.new(project, current_user, issue_params).execute
+
+      byebug
+      if issue.valid?
+        generate_response(issue)
+      else
+        issue_create_error(issue.errors.full_messages)
+      end
+    end
+
+    def issue_create_error(errors)
+      {
+        response_type: :ephemeral,
+        text: "An error occured creating your issue: #{errors}"
+      }
+    end
 
     def collection
       project.issues
@@ -10,6 +36,17 @@ module Mattermost
       Gitlab::Routing.
         url_helpers.
         namespace_project_issue_url(project.namespace, project, issue)
+    end
+
+    def issue_params
+      match = params[:text].match(/\Acreate (?<title>.+$)/)
+
+      return issue_create_error("No title given") unless match
+
+      {
+        title: match[:title],
+        description: params[:text].gsub(/\Acreate .+$\s*/, ''),
+      }
     end
   end
 end
