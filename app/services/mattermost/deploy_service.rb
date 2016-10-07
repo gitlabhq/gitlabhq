c395dd63c64975303aa5f11f7f6c3542af152ef9
Kamil Trzcinski <ayufan@ayufan.eu>
2016-10-07 14:55:47 +0200

    Fix deployments

--- app/services/mattermost/deploy_service.rb
+++ app/services/mattermost/deploy_service.rb
@@ -2,6 +2,8 @@ module Mattermost
   class DeployService < BaseService
     def execute
       environment_name, action_name = parse_command
+      return respond_404 unless environment_name
+
       environment = project.environments.find_by(name: environment_name)
 
       return respond_404 unless can?(current_user, :read_environment, environment)
@@ -9,9 +11,18 @@ module Mattermost
       deployment = environment.last_deployment
       return respond_404 unless can?(current_user, :create_deployment, deployment) && deployment.deployable
 
-      build = environment.last_deployment.other_actions.find { |action| action.name = action_name }
+      build = environment.last_deployment.manual_actions.find { |action| action.name = action_name }
+      return {
+        response_type: :ephemeral,
+        text: "No action '#{action_name}' defined for #{environment_name}."
+      } unless build
 
-      generate_response(build.play(current_user))
+      new_build = build.play(current_user)
+
+      {
+        response_type: :in_channel,
+        text: "Action '#{action_name}' started on '#{environment_name}' you can [follow the progress](#{link(new_build)})."
+      }
     end
 
     private
@@ -19,13 +30,19 @@ module Mattermost
     def single_resource(build)
       {
         response_type: :in_channel,
-        text: "Deploy started: "
+        message: "Deploy started [#{build.name} ##{build.id}](#{link(build)})"
       }
     end
 
+    def link(build)
+      Gitlab::Routing.
+        url_helpers.
+        namespace_project_build_url(project.namespace, project, build)
+    end
+
     def parse_command
-      matches = params[:text].match(/\A\/deploy (?<name>\w+) to (?<action>\w+)/)
-      respond_404 unless matches
+      matches = params[:text].match(/\A(?<name>\w+) to (?<action>\w+)/)
+      return unless matches
 
       return matches[:name], matches[:action]
     end
