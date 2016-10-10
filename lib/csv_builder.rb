94e9d0498f7e8560773d0f33ae95845dcf7cb0c6
James Edwards-Jones <jedwardsjones@gitlab.com>
2016-10-10 11:29:35 +0100

    Added RDoc to CsvBuilder

--- lib/csv_builder.rb
+++ lib/csv_builder.rb
@@ -1,3 +1,22 @@
+# Generates CSV when given a mapping and a collection.
+#
+# Takes a hash of 'Column Heading' => 'value_method'.
+#
+# The value method will be called once for each object in the collection, to
+# determine the value for that row. It can either be the name of a method on
+# the object, or a lamda to call passing in the object.
+#
+# Example:
+#
+#     columns = {
+#       'Title' => 'title',
+#       'Comment' => 'comment',
+#       'Author' => -> (post) { post.author.full_name }
+#       'Created At (UTC)' => -> (post) { post.created_at&.strftime('%Y-%m-%d %H:%M:%S') }
+#     }
+#
+#     CsvBuilder.new(columns).render(@posts)
+#
 class CsvBuilder
   def initialize(header_to_value_hash)
     @header_to_value_hash = header_to_value_hash
