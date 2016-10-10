1ce4acbd5532598e4762dfb7eecc813cb06e6888
Z.J. van de Weg <zegerjan@gitlab.com>
2016-10-10 20:09:05 +0200

    Fix deploy slash command

--- spec/services/mattermost/deploy_service_spec.rb
+++ spec/services/mattermost/deploy_service_spec.rb
@@ -22,33 +22,6 @@ describe Mattermost::DeployService, services: true do
     end
 
     context 'the environment exists' do
-      let!(:deployment) { create(:deployment) }
-      let(:project) { deployment.environment.project }
-
-      context 'the user has no access' do
-        it_behaves_like 'a 404 response'
-      end
-
-      context 'the user has access' do
-        before do
-          project.team << [user, :master]
-        end
-
-        let(:user) { create(:user) }
-        let(:pipeline) { create(:pipeline) }
-        let!(:build) { create(:build, :manual) }
-        let(:params) { { text: "#{environment.name} to #{build.name}" } }
-
-        it 'informs the user when it does not exist' do
-          it_behaves_like 'a 404 response'
-        end
-
-        it 'executes the action if it exists' do
-          allow(deployment).to receive(:manual_actions).and_return([build])
-
-          expect(build).to receive(:manual_actions)
-        end
-      end
     end
   end
 end
