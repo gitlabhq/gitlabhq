60469abc8602a90b9ee67e1d998c99219f951b1c
Kamil Trzcinski <ayufan@ayufan.eu>
2016-10-12 13:53:37 +0200

    Fix deployments controller

--- app/controllers/projects/deployments_controller.rb
+++ app/controllers/projects/deployments_controller.rb
@@ -11,7 +11,9 @@ class Projects::DeploymentsController < Projects::ApplicationController
   def terminal_websocket_authorize
     Gitlab::Workhorse.verify_api_request!(request.headers)
 
-    render text: 'Not found', status: 404 unless deployment.deployable
+    render text: 'Missing deployable', status: 404 unless deployment.deployable
+
+    deployable_variables = deployment.deployable.variables
 
     variables = Hash[*%w[
       openshift_project CI_PROJECT_NAME
@@ -19,22 +21,22 @@ class Projects::DeploymentsController < Projects::ApplicationController
       openshift_server OPENSHIFT_SERVER
       openshift_token OPENSHIFT_TOKEN
     ]].map do |json_key, variable_key|
-      [json_key, deployment.deployable.variables.find { |v| v[:key] == variable_key }]
+      [json_key, deployable_variables.find { |v| v[:key].to_s == variable_key }]
     end.to_h
 
     # TODO: restrict access: this allows even 'guests' to have terminal access
     if variables.values.all?(&:present?)
       set_workhorse_internal_api_content_type
-      render json: variables.map { |k, v| [k, v[:value]] }.to_h
+      render json: variables.map { |k, v| [k, ExpandVariables.expand(v[:value], deployable_variables)] }.to_h
     else
-      render text: 'Not found', status: 404
+      render json: { message: 'Not found', variables: variables, deployable_variables: deployable_variables }, status: 404
     end
   end
 
   protected
 
   def deployment
-    @deployment ||= project.deployments.find(params[:id].to_i)
+    @deployment ||= project.deployments.find_by(iid: params[:id].to_i)
     @deployment || render_404
   end
 end
