17ae807ae3e03fcc67f557c4ad7d5c6e0502065e
Felipe Artur <felipefac@gmail.com>
2016-10-14 19:38:41 -0300

    Create project feature when project is created

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161018024550) do
+ActiveRecord::Schema.define(version: 20161019213545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
