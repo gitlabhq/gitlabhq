jakop345/gitlab-ee

Group Approvers
It allows to assign group as approvers
https://gitlab.com/gitlab-org/gitlab-ee/issues/180

app/assets/javascripts/approvals.js
@@ -1,6 +1,6 @@
 (function() {
   $(function() {
-    $(".approver-list").on("click", ".project-approvers .btn-remove", function() {
+    $(".approver-list").on("click", ".unsaved-approvers.approver .btn-remove", function() {
       var removeElement = $(this).closest("li");
       var approverId = parseInt(removeElement.attr("id").replace("user_",""));
       var approverIds = $("input#merge_request_approver_ids");
@@ -15,17 +15,45 @@
 
       return false;
     });
+
+    $(".approver-list").on("click", ".unsaved-approvers.approver-group .btn-remove", function() {
+      var removeElement = $(this).closest("li");
+      var approverGroupId = parseInt(removeElement.attr("id").replace("group_",""));
+      var approverGroupIds = $("input#merge_request_approver_group_ids");
+      var skipGroups = approverGroupIds.data("skip-group") || [];
+      var approverGroupIndex = skipGroups.indexOf(approverGroupId);
+
+      removeElement.remove();
+
+      if(approverGroupIndex > -1) {
+        approverGroupIds.data("skip-group", skipGroups.splice(approverGroupIndex, 1));
+      }
+
+      return false;
+    });
+
     $("form.merge-request-form").submit(function() {
-      var approver_ids, approvers_input;
+      var approverIds, approversInput, approverGroupIds, approverGroupsInput;
+
       if ($("input#merge_request_approver_ids").length) {
-        approver_ids = $.map($("li.project-approvers").not(".approver-template"), function(li, i) {
+        approverIds = $.map($("li.unsaved-approvers.approver").not(".approver-template"), function(li, i) {
           return li.id.replace("user_", "");
         });
-        approvers_input = $(this).find("input#merge_request_approver_ids");
-        approver_ids = approver_ids.concat(approvers_input.val().split(","));
-        return approvers_input.val(_.compact(approver_ids).join(","));
+        approversInput = $(this).find("input#merge_request_approver_ids");
+        approverIds = approverIds.concat(approversInput.val().split(","));
+        approversInput.val(_.compact(approverIds).join(","));
+      }
+
+      if ($("input#merge_request_approver_group_ids").length) {
+        approverGroupIds = $.map($("li.unsaved-approvers.approver-group"), function(li, i) {
+          return li.id.replace("group_", "");
+        });
+        approverGroupsInput = $(this).find("input#merge_request_approver_group_ids");
+        approverGroupIds = approverGroupIds.concat(approverGroupsInput.val().split(","));
+        approverGroupsInput.val(_.compact(approverGroupIds).join(","));
       }
     });
+
     return $(".suggested-approvers a").click(function() {
       var approver_item_html, user_id, user_name;
       user_id = this.id.replace("user_", "");
@@ -33,7 +61,7 @@
       if ($(".approver-list #user_" + user_id).length) {
         return false;
       }
-      approver_item_html = $(".project-approvers.approver-template").clone().removeClass("hide approver-template")[0].outerHTML.replace(/\{approver_name\}/g, user_name).replace(/\{user_id\}/g, user_id);
+      approver_item_html = $(".unsaved-approvers.approver-template").clone().removeClass("hide approver-template")[0].outerHTML.replace(/\{approver_name\}/g, user_name).replace(/\{user_id\}/g, user_id);
       $(".no-approvers").remove();
       $(".approver-list").append(approver_item_html);
       return false;

app/controllers/groups_controller.rb
@@ -93,7 +93,7 @@ class GroupsController < Groups::ApplicationController
   end
 
   def autocomplete
-    groups = Group.search(params[:search]).limit(params[:per_page])
+    groups = Group.search(params[:search]).where.not(path: params[:skip_group]).limit(params[:per_page])
 
     render json: groups.to_json
   end

db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160915201649) do
+ActiveRecord::Schema.define(version: 20160916101334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -110,6 +110,17 @@ ActiveRecord::Schema.define(version: 20160915201649) do
     t.datetime "updated_at"
   end
 
+  create_table "approver_groups", force: :cascade do |t|
+    t.integer  "target_id",   null: false
+    t.string   "target_type"
+    t.integer  "group_id",    null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "approver_groups", ["group_id"], name: "index_approver_groups_on_group_id", using: :btree
+  add_index "approver_groups", ["target_id", "target_type"], name: "index_approver_groups_on_target_id_and_target_type", using: :btree
+
   create_table "approvers", force: :cascade do |t|
     t.integer  "target_id",   null: false
     t.string   "target_type"
@@ -185,9 +196,9 @@ ActiveRecord::Schema.define(version: 20160915201649) do
     t.text     "commands"
     t.integer  "job_id"
     t.string   "name"
-    t.boolean  "deploy",              default: false
+    t.boolean  "deploy",                        default: false
     t.text     "options"
-    t.boolean  "allow_failure",       default: false, null: false
+    t.boolean  "allow_failure",                 default: false, null: false
     t.string   "stage"
     t.integer  "trigger_request_id"
     t.integer  "stage_idx"
@@ -620,11 +631,11 @@ ActiveRecord::Schema.define(version: 20160915201649) do
   add_index "lists", ["label_id"], name: "index_lists_on_label_id", using: :btree
 
   create_table "members", force: :cascade do |t|
-    t.integer  "access_level",       null: false
-    t.integer  "source_id",          null: false
-    t.string   "source_type",        null: false
+    t.integer  "access_level",                       null: false
+    t.integer  "source_id",                          null: false
+    t.string   "source_type",                        null: false
     t.integer  "user_id"
-    t.integer  "notification_level", null: false
+    t.integer  "notification_level",                 null: false
     t.string   "type"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -1297,18 +1308,18 @@ ActiveRecord::Schema.define(version: 20160915201649) do
     t.datetime "updated_at"
     t.string   "type",                                    default: "ProjectHook"
     t.integer  "service_id"
-    t.boolean  "push_events",                          default: true,          null: false
-    t.boolean  "issues_events",                        default: false,         null: false
-    t.boolean  "merge_requests_events",                default: false,         null: false
-    t.boolean  "tag_push_events",                      default: false
+    t.boolean  "push_events",                             default: true,          null: false
+    t.boolean  "issues_events",                           default: false,         null: false
+    t.boolean  "merge_requests_events",                   default: false,         null: false
+    t.boolean  "tag_push_events",                         default: false
     t.integer  "group_id"
-    t.boolean  "note_events",                          default: false,         null: false
-    t.boolean  "enable_ssl_verification",              default: true
-    t.boolean  "build_events",                         default: false,         null: false
+    t.boolean  "note_events",                             default: false,         null: false
+    t.boolean  "enable_ssl_verification",                 default: true
+    t.boolean  "build_events",                            default: false,         null: false
     t.string   "token"
-    t.boolean  "wiki_page_events",                     default: false,         null: false
-    t.boolean  "pipeline_events",                      default: false,         null: false
-    t.boolean  "confidential_issues_events",           default: false,         null: false
+    t.boolean  "wiki_page_events",                        default: false,         null: false
+    t.boolean  "pipeline_events",                         default: false,         null: false
+    t.boolean  "confidential_issues_events",              default: false,         null: false
   end
 
   add_index "web_hooks", ["project_id"], name: "index_web_hooks_on_project_id", using: :btree

gitlabhq/gitlabhq

app/assets/javascripts/approvals.js
@@ -1,6 +1,6 @@
 (function() {
   $(function() {
-    $(".approver-list").on("click", ".project-approvers .btn-remove", function() {
+    $(".approver-list").on("click", ".unsaved-approvers.approver .btn-remove", function() {
       var removeElement = $(this).closest("li");
       var approverId = parseInt(removeElement.attr("id").replace("user_",""));
       var approverIds = $("input#merge_request_approver_ids");
@@ -15,17 +15,45 @@
 
       return false;
     });
+
+    $(".approver-list").on("click", ".unsaved-approvers.approver-group .btn-remove", function() {
+      var removeElement = $(this).closest("li");
+      var approverGroupId = parseInt(removeElement.attr("id").replace("group_",""));
+      var approverGroupIds = $("input#merge_request_approver_group_ids");
+      var skipGroups = approverGroupIds.data("skip-groups") || [];
+      var approverGroupIndex = skipGroups.indexOf(approverGroupId);
+
+      removeElement.remove();
+
+      if(approverGroupIndex > -1) {
+        approverGroupIds.data("skip-groups", skipGroups.splice(approverGroupIndex, 1));
+      }
+
+      return false;
+    });
+
     $("form.merge-request-form").submit(function() {
-      var approver_ids, approvers_input;
+      var approverIds, approversInput, approverGroupIds, approverGroupsInput;
+
       if ($("input#merge_request_approver_ids").length) {
-        approver_ids = $.map($("li.project-approvers").not(".approver-template"), function(li, i) {
+        approverIds = $.map($("li.unsaved-approvers.approver").not(".approver-template"), function(li, i) {
           return li.id.replace("user_", "");
         });
-        approvers_input = $(this).find("input#merge_request_approver_ids");
-        approver_ids = approver_ids.concat(approvers_input.val().split(","));
-        return approvers_input.val(_.compact(approver_ids).join(","));
+        approversInput = $(this).find("input#merge_request_approver_ids");
+        approverIds = approverIds.concat(approversInput.val().split(","));
+        approversInput.val(_.compact(approverIds).join(","));
+      }
+
+      if ($("input#merge_request_approver_group_ids").length) {
+        approverGroupIds = $.map($("li.unsaved-approvers.approver-group"), function(li, i) {
+          return li.id.replace("group_", "");
+        });
+        approverGroupsInput = $(this).find("input#merge_request_approver_group_ids");
+        approverGroupIds = approverGroupIds.concat(approverGroupsInput.val().split(","));
+        approverGroupsInput.val(_.compact(approverGroupIds).join(","));
       }
     });
+
     return $(".suggested-approvers a").click(function() {
       var approver_item_html, user_id, user_name;
       user_id = this.id.replace("user_", "");
@@ -33,7 +61,7 @@
       if ($(".approver-list #user_" + user_id).length) {
         return false;
       }
-      approver_item_html = $(".project-approvers.approver-template").clone().removeClass("hide approver-template")[0].outerHTML.replace(/\{approver_name\}/g, user_name).replace(/\{user_id\}/g, user_id);
+      approver_item_html = $(".unsaved-approvers.approver-template").clone().removeClass("hide approver-template")[0].outerHTML.replace(/\{approver_name\}/g, user_name).replace(/\{user_id\}/g, user_id);
       $(".no-approvers").remove();
       $(".approver-list").append(approver_item_html);
       return false;

app/controllers/groups_controller.rb
@@ -93,7 +93,7 @@ class GroupsController < Groups::ApplicationController
   end
 
   def autocomplete
-    groups = Group.search(params[:search]).limit(params[:per_page])
+    groups = Group.search(params[:search]).where.not(path: params[:skip_groups]).limit(params[:per_page])
 
     render json: groups.to_json
   end

db/schema.rb
@@ -116,6 +116,17 @@ ActiveRecord::Schema.define(version: 20161007133303) do
     t.datetime "updated_at"
   end
 
+  create_table "approver_groups", force: :cascade do |t|
+    t.integer  "target_id",   null: false
+    t.string   "target_type", null: false
+    t.integer  "group_id",    null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "approver_groups", ["group_id"], name: "index_approver_groups_on_group_id", using: :btree
+  add_index "approver_groups", ["target_id", "target_type"], name: "index_approver_groups_on_target_id_and_target_type", using: :btree
+
   create_table "approvers", force: :cascade do |t|
     t.integer "target_id", null: false
     t.string "target_type"
@@ -1375,6 +1386,7 @@ ActiveRecord::Schema.define(version: 20161007133303) do
 
   add_index "web_hooks", ["project_id"], name: "index_web_hooks_on_project_id", using: :btree
 
+  add_foreign_key "approver_groups", "namespaces", column: "group_id", on_delete: :cascade
   add_foreign_key "boards", "projects"
   add_foreign_key "issue_metrics", "issues", on_delete: :cascade
   add_foreign_key "lists", "boards"