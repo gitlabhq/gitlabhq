afa5f5ed55a4f04755556f5d05500963f1c4ea66
zhufenggood <zhufenggood@gmail.com>
2016-10-18 01:08:53 +0800

    update to snippet_bundle.js
    Statement better end with semicolon.

--- app/assets/javascripts/snippet/snippet_bundle.js
+++ app/assets/javascripts/snippet/snippet_bundle.js
@@ -2,7 +2,7 @@
 
 (function() {
   $(function() {
-    var editor = ace.edit("editor")
+    var editor = ace.edit("editor");
 
     $(".snippet-form-holder form").on('submit', function() {
       $(".snippet-file-content").val(editor.getValue());
