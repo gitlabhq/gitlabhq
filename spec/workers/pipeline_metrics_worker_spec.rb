a497803072edb4c8edbf9f4daf3832c122ee50e2
Kamil Trzcinski <ayufan@ayufan.eu>
2016-10-17 09:55:47 +0200

    Improve spec for pipeline metrics worker

---
+++ spec/workers/pipeline_metrics_worker_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+describe PipelineMetricsWorker do
+  let(:project) { create(:project) }
+  let!(:merge_request) { create(:merge_request, source_project: project, source_branch: pipeline.ref) }
+
+  let(:pipeline) do
+    create(:ci_empty_pipeline,
+           status: status,
+           project: project,
+           ref: 'master',
+           sha: project.repository.commit('master').id,
+           started_at: 1.hour.ago,
+           finished_at: Time.now)
+  end
+
+  describe '#perform' do
+    subject { described_class.new.perform(pipeline.id) }
+
+    context 'when pipeline is running' do
+      let(:status) { 'running' }
+
+      it 'records the build start time' do
+        subject
+
+        expect(merge_request.reload.metrics.latest_build_started_at).to eq(pipeline.started_at)
+      end
+
+      it 'clears the build end time' do
+        subject
+
+        expect(merge_request.reload.metrics.latest_build_finished_at).to be_nil
+      end
+    end
+
+    context 'when pipeline succeeded' do
+      let(:status) { 'success' }
+
+      it 'records the build end time' do
+        subject
+
+        expect(merge_request.reload.metrics.latest_build_finished_at).to eq(pipeline.finished_at)
+      end
+    end
+  end
+end
