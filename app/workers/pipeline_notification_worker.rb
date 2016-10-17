0f21b1313b853d92b722b32ac25b8ad6a4860a63
Lin Jen-Shin <godfat@godfat.org>
2016-10-17 18:18:16 +0800

    Skip if cannot find pipeline, feedback:
    https://gitlab.com/gitlab-org/gitlab-ce/merge_requests/6342/diffs#note_17038807

--- app/workers/pipeline_notification_worker.rb
+++ app/workers/pipeline_notification_worker.rb
@@ -2,7 +2,9 @@ class PipelineNotificationWorker
   include Sidekiq::Worker
 
   def perform(pipeline_id, recipients = nil)
-    pipeline = Ci::Pipeline.find(pipeline_id)
+    pipeline = Ci::Pipeline.find_by(id: pipeline_id)
+
+    return unless pipeline
 
     NotificationService.new.pipeline_finished(pipeline, recipients)
   end
