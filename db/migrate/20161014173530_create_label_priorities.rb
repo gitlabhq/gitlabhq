771d3fc3cbaa2b473c87d23fc1c067fb9bad6206
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2016-10-18 00:56:05 -0200

    Split migration to create label priorities

--- db/migrate/20161014173530_create_label_priorities.rb
+++ db/migrate/20161014173530_create_label_priorities.rb
@@ -2,7 +2,7 @@ class CreateLabelPriorities < ActiveRecord::Migration
   include Gitlab::Database::MigrationHelpers
 
   DOWNTIME = true
-  DOWNTIME_REASON = 'Prioritezed labels will not work as expected until this migration is complete.'
+  DOWNTIME_REASON = 'This migration adds foreign keys'
 
   disable_ddl_transaction!
 
@@ -15,41 +15,11 @@ class CreateLabelPriorities < ActiveRecord::Migration
       t.timestamps null: false
     end
 
-    execute <<-EOF.strip_heredoc
-      INSERT INTO label_priorities (project_id, label_id, priority, created_at, updated_at)
-      SELECT labels.project_id, labels.id, labels.priority, NOW(), NOW()
-      FROM labels
-      WHERE labels.project_id IS NOT NULL
-        AND labels.priority IS NOT NULL;
-    EOF
-
     add_concurrent_index :label_priorities, [:project_id, :label_id], unique: true
     add_concurrent_index :label_priorities, :priority
-
-    remove_column :labels, :priority
   end
 
   def down
-    add_column :labels, :priority, :integer
-
-    if Gitlab::Database.mysql?
-      execute <<-EOF.strip_heredoc
-        UPDATE labels
-          INNER JOIN label_priorities ON labels.id = label_priorities.label_id AND labels.project_id = label_priorities.project_id
-        SET labels.priority = label_priorities.priority;
-      EOF
-    else
-      execute <<-EOF.strip_heredoc
-        UPDATE labels
-        SET priority = label_priorities.priority
-        FROM label_priorities
-        WHERE labels.id = label_priorities.label_id
-          AND labels.project_id = label_priorities.project_id;
-      EOF
-    end
-
-    add_concurrent_index :labels, :priority
-
     drop_table :label_priorities
   end
 end
