f6bf6d43c7f68316c6f24cdea255325e26bc7715
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2016-10-18 15:59:14 -0200

    Fix Projects::BoardsController#index to serialize board name

--- app/controllers/projects/boards_controller.rb
+++ app/controllers/projects/boards_controller.rb
@@ -83,6 +83,6 @@ class Projects::BoardsController < Projects::ApplicationController
   end
 
   def serialize_as_json(resource)
-    resource.as_json(only: [:id])
+    resource.as_json(only: [:id, :name])
   end
 end
