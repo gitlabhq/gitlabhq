8989baaae7091832855b976b0eeda32d8b545dcb
Stan Hu <stanhu@gmail.com>
2016-10-18 15:10:31 -0700

    Fix broken award emoji for comments
    Closes #23506

--- app/helpers/award_emoji_helper.rb
+++ app/helpers/award_emoji_helper.rb
@@ -4,7 +4,7 @@ module AwardEmojiHelper
 
     if awardable.is_a?(Note)
       # We render a list of notes very frequently and calling the specific method is a lot faster than the generic one (6.5x)
-      toggle_award_emoji_namespace_project_note_url(namespace_id: @project.namespace_id, project_id: @project.id, id: awardable.id)
+      toggle_award_emoji_namespace_project_note_url(namespace_id: @project.namespace, project_id: @project, id: awardable.id)
     else
       url_for([:toggle_award_emoji, @project.namespace.becomes(Namespace), @project, awardable])
     end
