d2aa46b71e7060524ed47d9a1b9fbb7c6ec0127b
Sean McGivern <sean@mcgivern.me.uk>
2016-10-18 20:49:51 +0000

    Merge branch '22782-external-link-filter-with-non-lowercase-scheme' into 'master'
    Add Nofollow for uppercased external url protocols
    
    Closes #22782
    
    See merge request !6820

--- lib/banzai/filter/external_link_filter.rb
+++ lib/banzai/filter/external_link_filter.rb
@@ -3,16 +3,42 @@ module Banzai
     # HTML Filter to modify the attributes of external links
     class ExternalLinkFilter < HTML::Pipeline::Filter
       def call
-        # Skip non-HTTP(S) links and internal links
-        doc.xpath("descendant-or-self::a[starts-with(@href, 'http') and not(starts-with(@href, '#{internal_url}'))]").each do |node|
-          node.set_attribute('rel', 'nofollow noreferrer')
-          node.set_attribute('target', '_blank')
+        links.each do |node|
+          href = href_to_lowercase_scheme(node["href"].to_s)
+
+          unless node["href"].to_s == href
+            node.set_attribute('href', href)
+          end
+
+          if href =~ /\Ahttp(s)?:\/\// && external_url?(href)
+            node.set_attribute('rel', 'nofollow noreferrer')
+            node.set_attribute('target', '_blank')
+          end
         end
 
         doc
       end
 
       private
+
+      def links
+        query = 'descendant-or-self::a[@href and not(@href = "")]'
+        doc.xpath(query)
+      end
+
+      def href_to_lowercase_scheme(href)
+        scheme_match = href.match(/\A(\w+):\/\//)
+
+        if scheme_match
+          scheme_match.to_s.downcase + scheme_match.post_match
+        else
+          href
+        end
+      end
+
+      def external_url?(url)
+        !url.start_with?(internal_url)
+      end
 
       def internal_url
         @internal_url ||= Gitlab.config.gitlab.url
