76690017e28c2e42e58130fe64d7feb92d085b01
Z.J. van de Weg <zegerjan@gitlab.com>
2016-10-18 20:07:56 +0200

    Expose more data on the SystemHooks API
     Now exposed:
     - push_events
     - tag_push_events
     - enable_ssl_verification
     - token
    
     Fixes gitlab-org/gitlab-ce#23307

--- lib/api/entities.rb
+++ lib/api/entities.rb
@@ -43,14 +43,13 @@ module API
     end
 
     class Hook < Grape::Entity
-      expose :id, :url, :created_at
+      expose :id, :url, :created_at, :push_events, :tag_push_events
+      expose :enable_ssl_verification, :token
     end
 
     class ProjectHook < Hook
-      expose :project_id, :push_events
-      expose :issues_events, :merge_requests_events, :tag_push_events
+      expose :project_id, :issues_events, :merge_requests_events
       expose :note_events, :build_events, :pipeline_events, :wiki_page_events
-      expose :enable_ssl_verification
     end
 
     class BasicProjectDetails < Grape::Entity
