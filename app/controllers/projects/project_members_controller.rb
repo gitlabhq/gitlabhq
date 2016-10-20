ae0a9336f106b14a7abc39e5a32fe8491dd9b544
Phil Hughes <me@iamphill.com>
2016-10-20 10:27:30 +0100

    Removed code from project members controller
    This code was meant to be added to another branch as an expirement, but instead was commited to wrong branch

--- app/controllers/projects/project_members_controller.rb
+++ app/controllers/projects/project_members_controller.rb
@@ -32,21 +32,6 @@ class Projects::ProjectMembersController < Projects::ApplicationController
       current_user: current_user
     )
 
-    if params[:group_ids].present?
-      group_ids = params[:group_ids].split(',')
-      groups = Group.where(id: group_ids)
-
-      groups.each do |group|
-        next unless can?(current_user, :read_group, group)
-
-        project.project_group_links.create(
-          group: group,
-          group_access: params[:access_level],
-          expires_at: params[:expires_at]
-        )
-      end
-    end
-
     redirect_to namespace_project_project_members_path(@project.namespace, @project)
   end
 
