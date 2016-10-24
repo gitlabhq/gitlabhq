9dbd5b3cfad10b214ae5ef27c39246bbb74a5077
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2016-10-24 10:19:36 +0300

    Revert "Change "Group#web_url" to return "/groups/twitter" rather than "/twitter"."
    This reverts commit c81ff152e08d58c13efbd50c40dd2e083ac65083.

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -265,10 +265,4 @@ describe Group, models: true do
 
     members
   end
-
-  describe '#web_url' do
-    it 'returns the canonical URL' do
-      expect(group.web_url).to include("groups/#{group.name}")
-    end
-  end
 end
