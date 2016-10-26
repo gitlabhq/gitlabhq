1bb70e127a7237d30863dc5c3b7491f7639c412f
Z.J. van de Weg <git@zjvandeweg.nl>
2016-10-25 21:11:34 -0700

    Delete Groups which failed to be deleted
    Also, this commit limits the amount of projects and groups we queue per
    call of the worker. At this time we have over 18k projects marked as
    pending_delete, so it wouldn't be wise to queue all these as removing a
    project for example is quite compute intensive for the disk and for PG.

--- app/workers/cleanup_pending_delete_worker.rb
+++ app/workers/cleanup_pending_delete_worker.rb
@@ -1,12 +1,19 @@
 class CleanupPendingDeleteWorker
   include Sidekiq::Worker
+  include CronjobQueue
+
+  MAX_NEW_JOBS = 256
 
   sidekiq_options retry: false # this job auto-repeats via sidekiq-cron
 
   def perform
     admin = User.find_by(admin: true)
 
-    Project.unscoped.where(pending_delete: true).find_each do |project|
+    Group.with_deleted.where.not(deleted_at: nil).limit(MAX_NEW_JOBS).order(:deleted_at).each do |group|
+      GroupDestroyWorker.perform_async(group.id, admin.id)
+    end
+
+    Project.unscoped.where(pending_delete: true).limit(MAX_NEW_JOBS).order(:updated_at).each do |project|
       ProjectDestroyWorker.perform_async(project.id, admin.id)
     end
   end
--- spec/workers/cleanup_pending_delete_worker_spec.rb
+++ spec/workers/cleanup_pending_delete_worker_spec.rb
@@ -1,14 +1,25 @@
 require 'spec_helper'
 
 describe CleanupPendingDeleteWorker do
+  let!(:admin)  { create(:admin) }
   let(:project) { create(:empty_project, pending_delete: true) }
-  let(:admin)   { create(:admin) }
+  let(:group)   { create(:group) }
 
-  subject { CleanupPendingDeleteWorker.new }
+  subject { described_class.new }
 
   describe "#perform" do
+    it "queues groups for deletion" do
+      # We can't set deleted_at on 1.week.ago because of acts_as_paranoid
+      group.destroy
+
+      expect(GroupDestroyWorker).to receive(:perform_async).with(group.id, admin.id)
+
+      subject.perform
+    end
+
     it "queues projects for deletion" do
       expect(ProjectDestroyWorker).to receive(:perform_async).with(project.id, admin.id)
+
       subject.perform
     end
   end
