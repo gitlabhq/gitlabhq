ceaafd3a6e253f6af827b9c9ede37150c3ed3b95
Annabel Dunstone Gray <annabel.dunstone@gmail.com>
2016-10-28 10:35:46 -0500

    Add commit_box spec

---
+++ spec/views/projects/commit/_commit_box.html.haml_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe 'projects/commit/_commit_box.html.haml' do
+  include Devise::Test::ControllerHelpers
+
+  let(:project) { create(:project) }
+
+  before do
+    assign(:project, project)
+    assign(:commit, project.commit)
+  end
+
+  it 'shows the commit SHA' do
+    render
+
+    expect(rendered).to have_text("Commit #{Commit.truncate_sha(project.commit.sha)}")
+  end
+
+  it 'shows the last pipeline that ran for the commit' do
+    first_pipeline = create(:ci_pipeline, project: project, sha: project.commit.id, status: 'success')
+    second_pipeline = create(:ci_pipeline, project: project, sha: project.commit.id, status: 'canceled')
+    third_pipeline = create(:ci_pipeline, project: project, sha: project.commit.id, status: 'failed')
+
+    render
+
+    expect(rendered).to have_text("Pipeline ##{third_pipeline.id} for #{Commit.truncate_sha(project.commit.sha)} failed")
+  end
+end
