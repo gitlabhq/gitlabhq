ce3e790a024ee6e8c8565292bb7a47f27b294985
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2016-11-07 15:33:12 +0100

    Extend tests for pipeline unlock worker

--- spec/workers/pipeline_unlock_worker_spec.rb
+++ spec/workers/pipeline_unlock_worker_spec.rb
@@ -5,30 +5,51 @@ describe PipelineUnlockWorker do
     let(:worker) { described_class.new }
 
     context 'when locked pipelines are present' do
-      let(:pipeline) do
-        create(:ci_pipeline, status: :running,
-                             updated_at: 10.hours.ago)
-      end
-
-      context 'when pipeline is locked in the last stage' do
-        before do
-          create_build(status: :success, stage: 'test')
+      context 'when multiple pipelines are locked in the last stage' do
+        let(:created_pipeline) do
+          create(:ci_pipeline, status: :created,
+                               updated_at: 10.hours.ago)
         end
 
-        it 'updates pipeline status to finished' do
-          expect(pipeline.reload).to be_running
+        let(:pending_pipeline) do
+          create(:ci_pipeline, status: :pending,
+                               updated_at: 10.hours.ago)
+        end
+
+        let(:running_pipeline) do
+          create(:ci_pipeline, status: :running,
+                               updated_at: 10.hours.ago)
+        end
+
+        before do
+          create_build(created_pipeline, :pending, stage: 'test')
+          create_build(pending_pipeline, :success, stage: 'test')
+          create_build(running_pipeline, :failed, stage: 'test')
+        end
+
+        it 'updates each pipeline status' do
+          expect(created_pipeline.reload).to be_created
+          expect(pending_pipeline.reload).to be_pending
+          expect(running_pipeline.reload).to be_running
 
           worker.perform
 
-          expect(pipeline.reload).to be_success
+          expect(created_pipeline.reload).to be_pending
+          expect(pending_pipeline.reload).to be_success
+          expect(running_pipeline.reload).to be_failed
         end
       end
 
       context 'when locked pipeline has multiple stages ahead' do
+        let(:pipeline) do
+          create(:ci_pipeline, status: :running,
+                               updated_at: 10.hours.ago)
+        end
+
         before do
-          create_build(status: :success, stage: 'build', stage_idx: 1)
-          create_build(status: :created, stage: 'test', stage_idx: 2)
-          create_build(status: :created, stage: 'deploy', stage_idx: 3)
+          create_build(pipeline, :success, stage: 'build', stage_idx: 1)
+          create_build(pipeline, :created, stage: 'test', stage_idx: 2)
+          create_build(pipeline, :created, stage: 'deploy', stage_idx: 3)
         end
 
         it 'retriggers pipeline processing' do
@@ -37,8 +58,8 @@ describe PipelineUnlockWorker do
           worker.perform
 
           expect(pipeline.reload).to be_running
-          expect(find_build(stage: 'test')).to be_pending
-          expect(find_build(stage: 'deploy')).to be_created
+          expect(find_build(pipeline, stage: 'test')).to be_pending
+          expect(find_build(pipeline, stage: 'deploy')).to be_created
         end
 
         it 'updates pipeline' do
@@ -67,11 +88,11 @@ describe PipelineUnlockWorker do
     end
   end
 
-  def create_build(opts)
-    create(:ci_build, opts.merge(pipeline: pipeline))
+  def create_build(pipeline, status, opts = {})
+    create(:ci_build, opts.merge(pipeline: pipeline, status: status))
   end
 
-  def find_build(opts)
+  def find_build(pipeline, opts)
     pipeline.builds.find_by(opts)
   end
 end
