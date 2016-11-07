aebb6294d0f6e8758bc52005c8e10940c285ddf8
Ruben Davila <rdavila84@gmail.com>
2016-11-07 12:24:00 -0500

    Add specs for Timelog model.

--- app/models/timelog.rb
+++ app/models/timelog.rb
@@ -1,3 +1,5 @@
 class Timelog < ActiveRecord::Base
+  validates :time_spent, :trackable, presence: true
+
   belongs_to :trackable, polymorphic: true
 end
---
+++ spec/models/timelog_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Timelog, type: :model do
+  subject { build(:timelog) }
+
+  it { is_expected.to be_valid }
+
+  it { is_expected.to validate_presence_of(:time_spent) }
+  it { is_expected.to validate_presence_of(:trackable) }
+end
