65ceed642ae6eeea2888bbf9f1b91589274d2abd
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2016-11-07 15:12:14 +0100

    Make PipelineUnlockWorker unlock created pipelines

--- app/workers/pipeline_unlock_worker.rb
+++ app/workers/pipeline_unlock_worker.rb
@@ -3,7 +3,7 @@ class PipelineUnlockWorker
   include CronjobQueue
 
   def perform
-    Ci::Pipeline.running_or_pending
+    Ci::Pipeline.unfinished
       .where('updated_at < ?', 6.hours.ago)
       .find_each do |pipeline|
         PipelineProcessWorker.new.perform(pipeline.id)
