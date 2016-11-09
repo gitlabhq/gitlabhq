4723f0cfe42f9f5b3ed5d1f272b228e217aa1024
Matt Lee <mattl@gitlab.com>
2016-11-09 15:40:29 -0500

    Do not send emails to users who are inactive from the EE admin panel

--- spec/workers/admin_emails_worker_spec.rb
+++ spec/workers/admin_emails_worker_spec.rb
@@ -2,23 +2,56 @@ require 'spec_helper'
 
 describe AdminEmailsWorker do
   context "recipients" do
-    let(:recipient_id) { "group-#{group.id}" }
     let(:group) { create :group }
+    let(:project) { create :project }
 
     before do
       2.times do
-        group.add_user(create(:user), Gitlab::Access::DEVELOPER)
+        user = create(:user)
+        group.add_user(user, Gitlab::Access::DEVELOPER)
+        project.add_user(user, Gitlab::Access::DEVELOPER)
       end
       unsubscribed_user = create(:user, admin_email_unsubscribed_at: 5.days.ago)
       group.add_user(unsubscribed_user, Gitlab::Access::DEVELOPER)
+      project.add_user(unsubscribed_user, Gitlab::Access::DEVELOPER)
+      
+      blocked_user = create(:user, state: :blocked)
+      group.add_user(blocked_user, Gitlab::Access::DEVELOPER)
+      project.add_user(blocked_user, Gitlab::Access::DEVELOPER)
       ActionMailer::Base.deliveries = []
     end
 
-    it "sends email to subscribed users" do
-      perform_enqueued_jobs do
-        AdminEmailsWorker.new.perform(recipient_id, 'subject', 'body')
-        expect(ActionMailer::Base.deliveries.count).to eql 2
+    context "sending emails to members of a group only" do
+      let(:recipient_id) { "group-#{group.id}" }
+
+      it "sends email to subscribed users" do
+        perform_enqueued_jobs do
+          AdminEmailsWorker.new.perform(recipient_id, 'subject', 'body')
+          expect(ActionMailer::Base.deliveries.count).to eql 2
+        end
       end
-    end
+    end   
+
+    context "sending emails to members of a project only" do
+      let(:recipient_id) { "project-#{project.id}" }
+
+      it "sends email to subscribed users" do
+        perform_enqueued_jobs do
+          AdminEmailsWorker.new.perform(recipient_id, 'subject', 'body')
+          expect(ActionMailer::Base.deliveries.count).to eql 2
+        end
+      end
+    end   
+
+    context "sending emails to users directly" do
+      let(:recipient_id) { "all" }
+
+      it "sends email to subscribed users" do
+        perform_enqueued_jobs do
+          AdminEmailsWorker.new.perform(recipient_id, 'subject', 'body')
+          expect(ActionMailer::Base.deliveries.count).to eql 4
+        end
+      end
+    end   
   end
 end
