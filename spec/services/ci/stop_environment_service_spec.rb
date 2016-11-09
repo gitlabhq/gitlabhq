45bd5391d7f20715ae4de7a4a5cd9233ab598c28
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2016-11-09 13:31:33 +0100

    Extend tests for service that stops environment

--- spec/services/ci/stop_environment_service_spec.rb
+++ spec/services/ci/stop_environment_service_spec.rb
@@ -7,7 +7,7 @@ describe Ci::StopEnvironmentService, services: true do
   let(:service) { described_class.new(project, user) }
 
   describe '#execute' do
-    context 'when environment exists' do
+    context 'when environment with review app exists' do
       before do
         create(:environment, :with_review_app, project: project)
       end
@@ -16,6 +16,54 @@ describe Ci::StopEnvironmentService, services: true do
         expect_any_instance_of(Environment).to receive(:stop!)
 
         service.execute('master')
+      end
+
+      context 'when specified branch does not exist' do
+        it 'does not stop environment' do
+          expect_any_instance_of(Environment).not_to receive(:stop!)
+
+          service.execute('non/existent/branch')
+        end
+      end
+
+      context 'when no branch not specified' do
+        it 'does not stop environment' do
+          expect_any_instance_of(Environment).not_to receive(:stop!)
+
+          service.execute(nil)
+        end
+      end
+
+      context 'when environment is not stoppable' do
+        before do
+          allow_any_instance_of(Environment)
+            .to receive(:stoppable?).and_return(false)
+        end
+
+        it 'does not stop environment' do
+          expect_any_instance_of(Environment).not_to receive(:stop!)
+
+          service.execute('master')
+        end
+      end
+    end
+
+    context 'when there is no environment associated with review app' do
+      before do
+        create(:environment, project: project)
+      end
+
+      it 'does not stop environment' do
+        expect_any_instance_of(Environment).not_to receive(:stop!)
+
+        service.execute('master')
+      end
+    end
+
+    context 'when environment does not exist' do
+      it 'does not raise error' do
+        expect { service.execute('master') }
+          .not_to raise_error
       end
     end
   end
