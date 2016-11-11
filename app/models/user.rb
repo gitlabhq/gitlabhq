ca625ab937aef77019b467ec8f82dc390cd94cf6
Timothy Andrew <mail@timothyandrew.net>
2016-11-11 11:57:43 +0530

    Deleting a user shouldn't delete associated issues.
    - "Associated" issues are issues the user has created + issues that the
      user is assigned to.
    
    - Issues that a user owns are transferred to a "Ghost User" (just a
      regular user with `state = 'ghost'` that is created when
      `User.ghost` is called).
    
    - Issues that a user is assigned to are moved to the "Unassigned" state.
    
    - Fix a spec failure in `profile_spec` — a spec was asserting that when a user
      is deleted, `User.count` decreases by 1. After this change, deleting a user
      creates (potentially) a ghost user, causing `User.count` not to change. The
      spec has been updated to look for the relevant user in the assertion.

--- app/models/user.rb
+++ app/models/user.rb
@@ -83,7 +83,6 @@ class User < ActiveRecord::Base
   has_many :authorized_projects, through: :project_authorizations, source: :project
 
   has_many :snippets,                 dependent: :destroy, foreign_key: :author_id
-  has_many :issues,                   dependent: :destroy, foreign_key: :author_id
   has_many :notes,                    dependent: :destroy, foreign_key: :author_id
   has_many :merge_requests,           dependent: :destroy, foreign_key: :author_id
   has_many :events,                   dependent: :destroy, foreign_key: :author_id
@@ -109,6 +108,11 @@ class User < ActiveRecord::Base
 
   has_many :assigned_issues,          dependent: :nullify, foreign_key: :assignee_id, class_name: "Issue"
   has_many :assigned_merge_requests,  dependent: :nullify, foreign_key: :assignee_id, class_name: "MergeRequest"
+
+  # Issues that a user owns are expected to be moved to the "ghost" user before
+  # the user is destroyed. If the user owns any issues during deletion, this
+  # should be treated as an exceptional condition.
+  has_many :issues,                   dependent: :restrict_with_exception, foreign_key: :author_id
 
   #
   # Validations
@@ -192,6 +196,8 @@ class User < ActiveRecord::Base
           "administrator if you think this is an error."
       end
     end
+
+    state :ghost
   end
 
   mount_uploader :avatar, AvatarUploader
@@ -361,6 +367,30 @@ class User < ActiveRecord::Base
         #{Regexp.escape(reference_prefix)}
         (?<user>#{Gitlab::Regex::NAMESPACE_REF_REGEX_STR})
       }x
+    end
+
+    # Return (create if necessary) the ghost user. The ghost user
+    # owns records previously belonging to deleted users.
+    def ghost
+      ghost_user = User.with_state(:ghost).first
+
+      ghost_user ||
+        begin
+          users = Enumerator.new do |y|
+            n = nil
+            loop do
+              user = User.new(
+                username: "ghost#{n}", password: Devise.friendly_token,
+                email: "ghost#{n}@example.com", name: "Ghost User", state: :ghost
+              )
+
+              y.yield(user)
+              n = n ? n.next : 0
+            end
+          end
+
+          users.lazy.select { |user| user.valid? }.first.tap(&:save!)
+        end
     end
   end
 
