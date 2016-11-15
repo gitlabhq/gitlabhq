3d13096f7198ed1ec4b7c568977877452f842394
Lucas Deschamps <lucasdchamps@gmail.com>
2016-11-15 09:05:19 +0100

    Refactor issuables_counter_spec.

--- spec/features/dashboard/issuables_counter_spec.rb
+++ spec/features/dashboard/issuables_counter_spec.rb
@@ -1,50 +1,44 @@
 require 'spec_helper'
 
-describe 'Navigation bar counter', feature: true, js: true do
+describe 'Navigation bar counter', feature: true, js: true, caching: true do
   let(:user) { create(:user) }
   let(:project) { create(:empty_project, namespace: user.namespace) }
+  let(:issue) { create(:issue, project: project) }
+  let(:merge_request) { create(:merge_request, source_project: project) }
 
   before do
+    issue.update(assignee: user)
+    merge_request.update(assignee: user)
     login_as(user)
   end
 
   it 'reflects dashboard issues count' do
-    issue = create(:issue, project: project, assignee: user)
     visit issues_dashboard_path
 
-    dashboard_count = find('li.active span.badge')
-    nav_count = find('.dashboard-shortcuts-issues span.count')
+    expect_counters('issues', '1')
 
-    expect(nav_count).to have_content('1')
-    expect(dashboard_count).to have_content('1')
-
-    issue.assignee = nil
+    issue.update(assignee: nil)
     visit issues_dashboard_path
 
-    dashboard_count = find('li.active span.badge')
-    nav_count = find('.dashboard-shortcuts-issues span.count')
-
-    expect(nav_count).to have_content('1')
-    expect(dashboard_count).to have_content('1')
+    expect_counters('issues', '1')
   end
 
   it 'reflects dashboard merge requests count' do
-    merge_request = create(:merge_request, source_project: project, assignee: user)
     visit merge_requests_dashboard_path
 
-    dashboard_count = find('li.active span.badge')
-    nav_count = find('.dashboard-shortcuts-merge_requests span.count')
+    expect_counters('merge_requests', '1')
 
-    expect(nav_count).to have_content('1')
-    expect(dashboard_count).to have_content('1')
-
-    merge_request.assignee = nil
+    merge_request.update(assignee: nil)
     visit merge_requests_dashboard_path
 
-    dashboard_count = find('li.active span.badge')
-    nav_count = find('.dashboard-shortcuts-merge_requests span.count')
+    expect_counters('merge_requests', '1')
+  end
 
-    expect(nav_count).to have_content('1')
-    expect(dashboard_count).to have_content('1')
+  def expect_counters(issuable_type, count)
+    dashboard_count = find('li.active span.badge')
+    nav_count = find(".dashboard-shortcuts-#{issuable_type} span.count")
+
+    expect(nav_count).to have_content(count)
+    expect(dashboard_count).to have_content(count)
   end
 end
