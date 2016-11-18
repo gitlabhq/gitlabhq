f0831056e5a90f9ef4ad8818e260121fa37d7527
Ruben Davila <rdavila84@gmail.com>
2016-11-18 16:06:18 -0500

    Alter ChronicDuration config only for Time Tracking stuff.

--- lib/gitlab/time_tracking_formatter.rb
+++ lib/gitlab/time_tracking_formatter.rb
@@ -3,13 +3,28 @@ module Gitlab
     extend self
 
     def parse(string)
-      ChronicDuration.parse(string, default_unit: 'hours')
-    rescue ChronicDuration::DurationParseError
-      nil
+      with_custom_config do
+        ChronicDuration.parse(string, default_unit: 'hours') rescue nil
+      end
     end
 
     def output(seconds)
-      ChronicDuration.output(seconds, format: :short, limit_to_hours: false, weeks: true)
+      with_custom_config do
+        ChronicDuration.output(seconds, format: :short, limit_to_hours: false, weeks: true) rescue nil
+      end
+    end
+
+    def with_custom_config
+      # We may want to configure it through project settings in a future version.
+      ChronicDuration.hours_per_day = 8
+      ChronicDuration.days_per_week = 5
+
+      result = yield
+
+      ChronicDuration.hours_per_day = 24
+      ChronicDuration.days_per_week = 7
+
+      result
     end
   end
 end
