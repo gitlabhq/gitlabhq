cd222357ade5f47f8d52f22dea66965c580a5843
James Lopez <james@jameslopez.es>
2016-11-18 10:13:42 +0100

    refactor events

--- lib/gitlab/cycle_analytics/test_event.rb
+++ lib/gitlab/cycle_analytics/test_event.rb
@@ -1,26 +1,12 @@
 module Gitlab
   module CycleAnalytics
-    class TestEvent < BaseEvent
+    class TestEvent < StagingEvent
       def initialize(*args)
+        super(*args)
+
         @stage = :test
         @start_time_attrs =  mr_metrics_table[:latest_build_started_at]
         @end_time_attrs = mr_metrics_table[:latest_build_finished_at]
-        @projections = [build_table[:id]]
-        @order = build_table[:created_at]
-
-        super(*args)
-      end
-
-      def custom_query(base_query)
-        base_query.join(build_table).on(mr_metrics_table[:pipeline_id].eq(build_table[:commit_id]))
-      end
-
-      private
-
-      def serialize(event)
-        build = ::Ci::Build.find(event['id'])
-
-        AnalyticsBuildSerializer.new.represent(build).as_json
       end
     end
   end
