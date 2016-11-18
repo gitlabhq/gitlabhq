1ba1a141c26735af0fc6606f864d0f9f1b5ced6e
Fatih Acet <acetfatih@gmail.com>
2016-11-18 18:11:02 +0000

    Merge branch 'less-intrusive-system-note' into 'master'
    Less intrusive system note
    
    ## What does this MR do?
    This MR is for making system notes less intrusive by implementing proposed design
     
    ## Are there points in the code the reviewer needs to double check?
    System notes are reusing in issues, commits and MR discussions. May need to double check to make sure that the design is not breaking anywhere
    
    ## Why was this MR needed?
    This merge request solve issue #19797 
    
    ## Screenshots (if relevant)
    **Before:**
    ![merge-request-before](/uploads/91953598087a613078d80333a43815a7/merge-request-before.png)
    **After**
    ![2016-11-15_17.41.46](/uploads/c809ba53cc34ee83bb439ed741dc39f3/2016-11-15_17.41.46.gif)
    
    ## Does this MR meet the acceptance criteria?
    
    - [x] [CHANGELOG](https://gitlab.com/gitlab-org/gitlab-ce/blob/master/CHANGELOG) entry added
    - [x] [Documentation created/updated](https://gitlab.com/gitlab-org/gitlab-ce/blob/master/doc/development/doc_styleguide.md)
    - [ ] API support added
    - Tests
      - [x] Added for this feature/bug
      - [x] All builds are passing
    - [x] Conform by the [merge request performance guides](http://docs.gitlab.com/ce/development/merge_request_performance_guidelines.html)
    - [x] Conform by the [style guides](https://gitlab.com/gitlab-org/gitlab-ce/blob/master/CONTRIBUTING.md#style-guides)
    - [x] Branch has no merge conflicts with `master` (if it does - rebase it please)
    - [x] [Squashed related commits together](https://git-scm.com/book/en/Git-Tools-Rewriting-History#Squashing-Commits)
    
    ## What are the relevant issue numbers?
    #19797, #23129
    
    See merge request !6755

--- spec/features/merge_requests/merge_when_build_succeeds_spec.rb
+++ spec/features/merge_requests/merge_when_build_succeeds_spec.rb
@@ -73,7 +73,7 @@ feature 'Merge When Build Succeeds', feature: true, js: true do
       expect(page).to have_button "Merge When Build Succeeds"
 
       visit_merge_request(merge_request) # refresh the page
-      expect(page).to have_content "Canceled the automatic merge"
+      expect(page).to have_content "canceled the automatic merge"
     end
 
     it "allows the user to remove the source branch" do
@@ -101,7 +101,7 @@ feature 'Merge When Build Succeeds', feature: true, js: true do
       expect(page).not_to have_link "Merge When Build Succeeds"
     end
   end
-  
+
   def visit_merge_request(merge_request)
     visit namespace_project_merge_request_path(merge_request.project.namespace, merge_request.project, merge_request)
   end
