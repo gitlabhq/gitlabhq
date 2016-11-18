0aa477884c6ac3298f79f62e08e63294d81735a3
James Lopez <james@jameslopez.es>
2016-11-18 12:09:36 +0100

    fix small typo

--- spec/lib/gitlab/cycle_analytics/build_updater_spec.rb
+++ spec/lib/gitlab/cycle_analytics/build_updater_spec.rb
@@ -4,7 +4,7 @@ describe Gitlab::CycleAnalytics::BuildUpdater do
   let(:build) { create(:ci_build) }
   let(:events) { [{ 'id' => build.id }] }
 
-  it 'maps the correct user' do
+  it 'maps the correct build' do
     described_class.update!(events)
 
     expect(events.first['build']).to eq(build)
