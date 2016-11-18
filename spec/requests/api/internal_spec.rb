0e41281e4c8a5e48c4f5d8f321212199265a8893
Rémy Coutable <remy@rymai.me>
2016-11-18 16:55:48 +0000

    Merge branch '24599-spec-lib-gitlab-backend-shell_spec-rb-causes-other-specs-to-fail' into 'master'
    Rely on Gitlab::Shell public API, not reading internal files it may use
    
    EE brother of gitlab-org/gitlab-ce!7557 so that specs can pass on EE until the CE->EE merge (no need to pick it into stable since the CE MR will be picked already).
    
    See merge request !887

--- spec/requests/api/internal_spec.rb
+++ spec/requests/api/internal_spec.rb
@@ -5,7 +5,7 @@ describe API::API, api: true  do
   let(:user) { create(:user) }
   let(:key) { create(:key, user: user) }
   let(:project) { create(:project) }
-  let(:secret_token) { File.read Gitlab.config.gitlab_shell.secret_file }
+  let(:secret_token) { Gitlab::Shell.secret_token }
 
   describe "GET /internal/check", no_db: true do
     it do
