ff2ee9c94770321bc769e7f284a06db21707a7fc
Bryce Johnson <bryce@gitlab.com>
2016-11-18 11:44:38 +0100

    Add integration tests for issue timetracking.

--- spec/features/issues/issue_sidebar_spec.rb
+++ spec/features/issues/issue_sidebar_spec.rb
@@ -102,6 +102,80 @@ feature 'Issue Sidebar', feature: true do
     end
   end
 
+  describe 'Time tracking slash commands', js: true do
+    include WaitForAjax
+
+    let(:issue) { create(:issue, author: @user, project: project) }
+
+    before do
+      project.team << [user, :developer]
+      visit_issue(project, issue)
+    end
+
+    it 'renders the sidebar component empty state' do
+      page.within '.issuable-sidebar' do
+        expect(page).to have_content 'No estimate or time spent'
+      end
+    end
+
+    it 'updates the sidebar component when estimate is added' do
+      submit_time('/estimate 3w 1d 1h')
+
+      page.within '.time-tracking-estimate-only' do
+        expect(page).to have_content '3w 1d 1h'
+      end
+    end
+
+    it 'updates the sidebar component when spent is added' do
+      submit_time('/spend 3w 1d 1h')
+
+      page.within '.time-tracking-spend-only' do
+        expect(page).to have_content '3w 1d 1h'
+      end
+    end
+
+    it 'shows the comparison when estimate and spent are added' do
+      submit_time('/estimate 3w 1d 1h')
+      submit_time('/spend 3w 1d 1h')
+
+      page.within '.time-tracking-pane-compare' do
+        expect(page).to have_content '3w 1d 1h'
+      end
+    end
+
+    it 'updates the sidebar component when estimate is removed' do
+      submit_time('/estimate 3w 1d 1h')
+      submit_time('/remove_estimate')
+
+      page.within '#issuable-time-tracker' do
+        expect(page).to have_content 'No estimate or time spent'
+      end
+    end
+
+    it 'updates the sidebar component when spent is removed' do
+      submit_time('/spend 3w 1d 1h')
+      submit_time('/remove_time_spent')
+
+      page.within '#issuable-time-tracker' do
+        expect(page).to have_content 'No estimate or time spent'
+      end
+    end
+
+    it 'shows the help state when icon is clicked' do
+      page.within '#issuable-time-tracker' do
+        find('.help-button').click
+        expect(page).to have_content 'Track time with slash commands'
+        expect(page).to have_content 'Learn more'
+      end
+    end
+  end
+
+  def submit_time(slash_command)
+    fill_in 'note[note]', with: slash_command
+    click_button 'Comment'
+    wait_for_ajax
+  end
+
   def visit_issue(project, issue)
     visit namespace_project_issue_path(project.namespace, project, issue)
   end
