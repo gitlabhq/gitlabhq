f2522c0c0ca6487deb1ee9081388287b81c24603
Kamil Trzciński <ayufan@ayufan.eu>
2016-11-21 13:49:09 +0000

    Merge branch 'fix/do-not-add-todo-when-build-allowed-to-fail' into 'master'
    Do not create TODO when build is allowed to fail
    
    ## What does this MR do?
    
    Do not create a TODO when build that failed is allowed to fail.
    
    ## Does this MR meet the acceptance criteria?
    
    - [x] [Changelog entry](https://docs.gitlab.com/ce/development/changelog.html) added
    - [x] [Documentation created/updated](https://gitlab.com/gitlab-org/gitlab-ce/blob/master/doc/development/doc_styleguide.md)
    - Tests
      - [x] Added for this feature/bug
      - [ ] All builds are passing
    
    ## What are the relevant issue numbers?
    
    Closes #22280
    
    See merge request !7618

--- spec/services/merge_requests/add_todo_when_build_fails_service_spec.rb
+++ spec/services/merge_requests/add_todo_when_build_fails_service_spec.rb
@@ -1,13 +1,22 @@
 require 'spec_helper'
 
-# Write specs in this file.
 describe MergeRequests::AddTodoWhenBuildFailsService do
   let(:user) { create(:user) }
   let(:merge_request) { create(:merge_request) }
   let(:project) { create(:project) }
   let(:sha) { '1234567890abcdef1234567890abcdef12345678' }
-  let(:pipeline) { create(:ci_pipeline_with_one_job, ref: merge_request.source_branch, project: project, sha: sha) }
-  let(:service) { MergeRequests::AddTodoWhenBuildFailsService.new(project, user, commit_message: 'Awesome message') }
+  let(:ref) { merge_request.source_branch }
+
+  let(:pipeline) do
+    create(:ci_pipeline_with_one_job, ref: ref,
+                                      project: project,
+                                      sha: sha)
+  end
+
+  let(:service) do
+    described_class.new(project, user, commit_message: 'Awesome message')
+  end
+
   let(:todo_service) { TodoService.new }
 
   let(:merge_request) do
@@ -23,7 +32,9 @@ describe MergeRequests::AddTodoWhenBuildFailsService do
 
   describe '#execute' do
     context 'commit status with ref' do
-      let(:commit_status) { create(:generic_commit_status, ref: merge_request.source_branch, pipeline: pipeline) }
+      let(:commit_status) do
+        create(:generic_commit_status, ref: ref, pipeline: pipeline)
+      end
 
       it 'notifies the todo service' do
         expect(todo_service).to receive(:merge_request_build_failed).with(merge_request)
@@ -32,7 +43,7 @@ describe MergeRequests::AddTodoWhenBuildFailsService do
     end
 
     context 'commit status with non-HEAD ref' do
-      let(:commit_status) { create(:generic_commit_status, ref: merge_request.source_branch) }
+      let(:commit_status) { create(:generic_commit_status, ref: ref) }
 
       it 'does not notify the todo service' do
         expect(todo_service).not_to receive(:merge_request_build_failed)
@@ -45,6 +56,18 @@ describe MergeRequests::AddTodoWhenBuildFailsService do
 
       it 'does not notify the todo service' do
         expect(todo_service).not_to receive(:merge_request_build_failed)
+        service.execute(commit_status)
+      end
+    end
+
+    context 'when commit status is a build allowed to fail' do
+      let(:commit_status) do
+        create(:ci_build, :allowed_to_fail, ref: ref, pipeline: pipeline)
+      end
+
+      it 'does not create todo' do
+        expect(todo_service).not_to receive(:merge_request_build_failed)
+
         service.execute(commit_status)
       end
     end
