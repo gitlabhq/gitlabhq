11e9f32d1be94d9216416c730c0b72effed1aa73
Mike Greiling <mike@pixelcog.com>
2016-11-21 15:40:58 -0600

    fix rubocop complaint about ambiguous splat operator

--- app/helpers/javascript_helper.rb
+++ app/helpers/javascript_helper.rb
@@ -3,6 +3,6 @@ module JavascriptHelper
     javascript_include_tag asset_path(js), { "data-turbolinks-track" => true }
   end
   def page_specific_javascript_bundle_tag(js)
-    javascript_include_tag *webpack_asset_paths(js), { "data-turbolinks-track" => true }
+    javascript_include_tag(*webpack_asset_paths(js), { "data-turbolinks-track" => true })
   end
 end
