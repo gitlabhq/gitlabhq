8639ea1b0315045c0e4a5ad8d6419903507850c3
James Lopez <james@jameslopez.es>
2016-11-22 12:01:21 +0100

    fix bad merge

--- app/models/cycle_analytics.rb
+++ app/models/cycle_analytics.rb
@@ -12,15 +12,17 @@ class CycleAnalytics
 
   def method_missing(method_sym, *arguments, &block)
     classify_stage(method_sym).new(project: @project, options: @options, stage: method_sym)
+  end
+
   def permissions(user:)
     Gitlab::CycleAnalytics::Permissions.get(user: user, project: @project)
   end
 
   def issue
     @fetcher.calculate_metric(:issue,
-                     Issue.arel_table[:created_at],
-                     [Issue::Metrics.arel_table[:first_associated_with_milestone_at],
-                      Issue::Metrics.arel_table[:first_added_to_board_at]])
+                              Issue.arel_table[:created_at],
+                              [Issue::Metrics.arel_table[:first_associated_with_milestone_at],
+                               Issue::Metrics.arel_table[:first_added_to_board_at]])
   end
 
   def classify_stage(method_sym)
