e4e313fccab6816fb2e52ebdf83807fba4a52051
James Lopez <james@jameslopez.es>
2016-11-23 13:04:06 +0100

    Fix other spec failures

--- spec/lib/gitlab/cycle_analytics/stage_summary_spec.rb
+++ spec/lib/gitlab/cycle_analytics/stage_summary_spec.rb
@@ -4,20 +4,20 @@ describe Gitlab::CycleAnalytics::StageSummary, models: true do
   let(:project) { create(:project) }
   let(:from) { Time.now }
   let(:user) { create(:user, :admin) }
-  subject { CycleAnalyticsTest.new(project, options: { from: from_date }) }
+  subject { described_class.new(project, from: Time.now).data }
 
   describe "#new_issues" do
     it "finds the number of issues created after the 'from date'" do
       Timecop.freeze(5.days.ago) { create(:issue, project: project) }
       Timecop.freeze(5.days.from_now) { create(:issue, project: project) }
 
-      expect(subject.new_issues).to eq(1)
+      expect(subject.first[:value]).to eq(1)
     end
 
     it "doesn't find issues from other projects" do
       Timecop.freeze(5.days.from_now) { create(:issue, project: create(:project)) }
 
-      expect(subject.new_issues).to eq(0)
+      expect(subject.first[:value]).to eq(0)
     end
   end
 
@@ -26,19 +26,19 @@ describe Gitlab::CycleAnalytics::StageSummary, models: true do
       Timecop.freeze(5.days.ago) { create_commit("Test message", project, user, 'master') }
       Timecop.freeze(5.days.from_now) { create_commit("Test message", project, user, 'master') }
 
-      expect(subject.commits).to eq(1)
+      expect(subject.second[:value]).to eq(1)
     end
 
     it "doesn't find commits from other projects" do
       Timecop.freeze(5.days.from_now) { create_commit("Test message", create(:project), user, 'master') }
 
-      expect(subject.commits).to eq(0)
+      expect(subject.second[:value]).to eq(0)
     end
 
     it "finds a large (> 100) snumber of commits if present" do
       Timecop.freeze(5.days.from_now) { create_commit("Test message", project, user, 'master', count: 100) }
 
-      expect(subject.commits).to eq(100)
+      expect(subject.second[:value]).to eq(100)
     end
   end
 
@@ -47,13 +47,13 @@ describe Gitlab::CycleAnalytics::StageSummary, models: true do
       Timecop.freeze(5.days.ago) { create(:deployment, project: project) }
       Timecop.freeze(5.days.from_now) { create(:deployment, project: project) }
 
-      expect(subject.deploys).to eq(1)
+      expect(subject.third[:value]).to eq(1)
     end
 
     it "doesn't find commits from other projects" do
       Timecop.freeze(5.days.from_now) { create(:deployment, project: create(:project)) }
 
-      expect(subject.deploys).to eq(0)
+      expect(subject.third[:value]).to eq(0)
     end
   end
 end
