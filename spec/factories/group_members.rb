525fb86b57c70cf8826054b571afd22623a5c903
Rémy Coutable <remy@rymai.me>
2016-11-25 10:20:53 +0100

    Add missing trait and remove spec that doesn't make sense in this branch
    The `repository_access_level` attribute doesn't exist in the 8-12-stable
    branch so repository access cannot be restricted using this feature.
    
    Fortunately we use `can?(user, :download_code, ref_project)` in
    lib/banzai/reference_parser/commit_parser.rb and
    lib/banzai/reference_parser/commit_range_parser.rb and this permission
    already exists in 8-12-stable (it only depends on the project
    `visibility_access`).
    
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- spec/factories/group_members.rb
+++ spec/factories/group_members.rb
@@ -3,5 +3,11 @@ FactoryGirl.define do
     access_level { GroupMember::OWNER }
     group
     user
+
+    trait(:guest)     { access_level GroupMember::GUEST }
+    trait(:reporter)  { access_level GroupMember::REPORTER }
+    trait(:developer) { access_level GroupMember::DEVELOPER }
+    trait(:master)    { access_level GroupMember::MASTER }
+    trait(:owner)     { access_level GroupMember::OWNER }
   end
 end
