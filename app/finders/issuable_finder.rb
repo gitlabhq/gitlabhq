4d26ab28a955885cfe8ae08917395cc7fc252ebf
Jacopo <beschi.jacopo@gmail.com>
2016-11-27 11:33:15 +0100

    Fix Archived project merge requests add to group's Merge Requests counter
    This is done by:
     - Extending the IssuableFinder adding the non_archived option to the params
     - Overriding the #filter_params in the MergeRequestsAction
     - Passing the non_archived param in the nav/_group.html.haml navbar
       partial from the groups/merge_requests.html.haml

--- app/finders/issuable_finder.rb
+++ app/finders/issuable_finder.rb
@@ -15,6 +15,7 @@
 #     search: string
 #     label_name: string
 #     sort: string
+#     non_archived: boolean
 #
 class IssuableFinder
   NONE = '0'
@@ -38,6 +39,7 @@ class IssuableFinder
     items = by_author(items)
     items = by_label(items)
     items = by_due_date(items)
+    items = by_non_archived(items)
     sort(items)
   end
 
@@ -351,6 +353,10 @@ class IssuableFinder
     else
       []
     end
+  end
+
+  def by_non_archived(items)
+    params[:non_archived].present? ? items.non_archived : items
   end
 
   def current_user_related?
