605715067767572ff96964370d78e7b31083ddde
Filipa Lacerda <filipa@gitlab.com>
2016-11-28 11:34:02 +0000

    Adds manual action icon and case to show it

--- app/helpers/ci_status_helper.rb
+++ app/helpers/ci_status_helper.rb
@@ -48,6 +48,8 @@ module CiStatusHelper
         'icon_status_created'
       when 'skipped'
         'icon_status_skipped'
+      when 'manual'
+        'icon_status_manual'
       else
         'icon_status_canceled'
       end
