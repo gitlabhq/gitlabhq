1905650adb8760fd8ce696059905d9e676c3a13f
Bryce Johnson <bryce@gitlab.com>
2016-11-29 20:09:05 +0100

    Revert approvable experiment.

--- app/models/concerns/approvable.rb
+++ app/models/concerns/approvable.rb
@@ -113,12 +113,7 @@ module Approvable
     def approvers_overwritten?
       approvers.any? || approver_groups.any?
     end
-    
-    def user_is_approver(user)
-      return false unless approvers.any?
-      approvers.include?(user)
-    end
-    
+
     def can_approve?(user)
       return false unless user
       return true if approvers_left.include?(user)
