940b44597ad69e1ac6764cf17fb4b65c119e0a1c
Douwe Maan <douwe@gitlab.com>
2016-11-29 13:47:43 +0000

    Merge branch 'jej-23867-use-mr-finder-instead-of-access-check' into 'security'
    Replace MR access checks with use of MergeRequestsFinder
    
    Split from !2024 to partially solve https://gitlab.com/gitlab-org/gitlab-ce/issues/23867
    
    ## Which fixes are in this MR?
    
    :warning: - Potentially untested  
    :bomb: - No test coverage  
    :traffic_light: - Test coverage of some sort exists (a test failed when error raised)  
    :vertical_traffic_light: - Test coverage of return value (a test failed when nil used)  
    :white_check_mark: - Permissions check tested
    
    ### MR lookup from project
    
    - [x] :bomb:  app/finders/notes_finder.rb:17
    - [x] :warning:  app/views/layouts/nav/_project.html.haml:80 [`.count`]
    - [x] :bomb:  app/controllers/concerns/creates_commit.rb:84
    - [x] :traffic_light:  app/controllers/projects/commits_controller.rb:24
    - [x] :traffic_light:  app/controllers/projects/compare_controller.rb:56
    - [x] :vertical_traffic_light:  app/controllers/projects/discussions_controller.rb:29
    - [x] :white_check_mark:  app/controllers/projects/todos_controller.rb:27
    - [x] :vertical_traffic_light:  app/models/commit.rb:268
    - [x] :white_check_mark: lib/gitlab/search_results.rb:71
    
    ### Previous discussions
    - [x] https://dev.gitlab.org/gitlab/gitlabhq/merge_requests/2024/diffs#d1c10892daedb4d4dd3d4b12b6d071091eea83df_267_266 Memoize ` merged_merge_request(current_user)`
    - [x] https://dev.gitlab.org/gitlab/gitlabhq/merge_requests/2024/diffs#d1c10892daedb4d4dd3d4b12b6d071091eea83df_248_247 Expected side effect for `merged_merge_request!`, consider `skip_authorization: true`.
    - [x] https://dev.gitlab.org/gitlab/gitlabhq/merge_requests/2024/diffs#d1c10892daedb4d4dd3d4b12b6d071091eea83df_269_269 Scary use  of unchecked `merged_merge_request?`
    
    See merge request !2033

--- app/models/concerns/milestoneish.rb
+++ app/models/concerns/milestoneish.rb
@@ -1,17 +1,17 @@
 module Milestoneish
-  def closed_items_count(user = nil)
+  def closed_items_count(user)
     issues_visible_to_user(user).closed.size + merge_requests.closed_and_merged.size
   end
 
-  def total_items_count(user = nil)
+  def total_items_count(user)
     issues_visible_to_user(user).size + merge_requests.size
   end
 
-  def complete?(user = nil)
+  def complete?(user)
     total_items_count(user) > 0 && total_items_count(user) == closed_items_count(user)
   end
 
-  def percent_complete(user = nil)
+  def percent_complete(user)
     ((closed_items_count(user) * 100) / total_items_count(user)).abs
   rescue ZeroDivisionError
     0
@@ -23,7 +23,7 @@ module Milestoneish
     (due_date - Date.today).to_i
   end
 
-  def issues_visible_to_user(user = nil)
+  def issues_visible_to_user(user)
     issues.visible_to_user(user)
   end
 end
