0fef17d2e77a1037414c8d73b63d89aeae8655d2
Sean McGivern <sean@mcgivern.me.uk>
2016-11-30 16:38:53 +0000

    Merge branch 'fix-migrations' into 'master'
    Make the `downtime_check` task happy
    
    See merge request !7845
    
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- db/migrate/20150313012111_create_subscriptions_table.rb
+++ db/migrate/20150313012111_create_subscriptions_table.rb
@@ -1,15 +1,17 @@
 # rubocop:disable all
 class CreateSubscriptionsTable < ActiveRecord::Migration
+  DOWNTIME = false
+
   def change
     create_table :subscriptions do |t|
       t.integer :user_id
       t.references :subscribable, polymorphic: true
       t.boolean :subscribed
-      
+
       t.timestamps
     end
 
-    add_index :subscriptions, 
+    add_index :subscriptions,
               [:subscribable_id, :subscribable_type, :user_id],
               unique: true,
               name: 'subscriptions_user_id_and_ref_fields'
