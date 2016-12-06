a7fa7bc23813efedabd994f55fdc709066b1e9ab
Alejandro Rodríguez <alejorro70@gmail.com>
2016-12-06 14:03:56 -0300

    Rename foreign keys migration to fit into the migration timeline

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161128161412) do
+ActiveRecord::Schema.define(version: 20161206201514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
