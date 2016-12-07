93c72e0f71919968972e874519e01370edcce022
Kamil Trzcinski <ayufan@ayufan.eu>
2016-12-07 13:14:45 +0100

    Add Ci::Status::Factory

--- lib/gitlab/ci/status/stage/factory.rb
+++ lib/gitlab/ci/status/stage/factory.rb
@@ -2,35 +2,11 @@ module Gitlab
   module Ci
     module Status
       module Stage
-        class Factory
-          EXTENDED_STATUSES = []
-
-          def initialize(stage)
-            @stage = stage
-            @status = stage.status || :created
-          end
-
-          def fabricate!
-            if extended_status
-              extended_status.new(core_status)
-            else
-              core_status
-            end
-          end
-
+        class Factory < Status::Factory
           private
 
           def core_status
-            Gitlab::Ci::Status
-              .const_get(@status.capitalize)
-              .new(@stage)
-              .extend(Status::Stage::Common)
-          end
-
-          def extended_status
-            @extended ||= EXTENDED_STATUSES.find do |status|
-              status.matches?(@stage)
-            end
+            super.extend(Status::Stage::Common)
           end
         end
       end
