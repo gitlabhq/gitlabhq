258ac7a6700f22b6e4051c7a64c234c0df59d965
Chris Wilson <chris@chrisjwilson.com>
2016-12-08 23:07:06 +0800

    Switch visibility level, remove gl-gray

--- app/assets/javascripts/groups_select.js
+++ app/assets/javascripts/groups_select.js
@@ -53,14 +53,27 @@
     }
 
     GroupsSelect.prototype.formatResult = function(group) {
-      var avatar;
+      var avatar,
+          visibility;
       if (group.avatar_url) {
         avatar = group.avatar_url;
       } else {
         avatar = gon.default_group_avatar;
       }
 
-      return "<li class='group-row " + (!group.description ? 'no-description' : void 0) + "'> <div class='stats'><span><i class='fa fa-bookmark'></i>" + group.project_count + "</span><span><i class='fa fa-users'></i>" + group.user_count + "</span><span class='visibility-icon has-tooltip' data-container='body' data-placement='left'><i class='fa fa-globe'></i></span></div><div class='avatar-container s40'><img class='avatar s40 hidden-xs' src='" + avatar + "'></div><div class='title'>" + group.name + "</div><div class='description'> <p dir='auto'>" + group.description + "</p></div></li>";
+      switch (group.visibility_level) {
+        case 0:
+          visibility = 'fa-lock';
+          break;
+        case 10:
+          visibility = 'fa-shield';
+          break;
+        case 20:
+          visibility = 'fa-globe';
+          break;
+      }
+
+      return "<li class='group-row " + (!group.description ? 'no-description' : void 0) + "'> <div class='stats'><span><i class='fa fa-bookmark'></i>" + group.project_count + "</span><span><i class='fa fa-users'></i>" + group.user_count + "</span><span class='visibility-icon has-tooltip' data-container='body' data-placement='left'><i class='fa " + visibility + "'></i></span></div><div class='avatar-container s40'><img class='avatar s40 hidden-xs' src='" + avatar + "'></div><div class='title'>" + group.name + "</div><div class='description'> <p dir='auto'>" + group.description + "</p></div></li>";
     };
 
     GroupsSelect.prototype.formatSelection = function(group) {
