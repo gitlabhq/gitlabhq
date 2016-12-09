jakop345/gitlab-ce

Refactor chat commands

app/models/project_services/mattermost_slash_commands_service.rb
@@ -31,7 +31,7 @@ class MattermostSlashCommandsService < ChatService
     user = find_chat_user(params)
     unless user
       url = authorize_chat_name_url(params)
-      return Mattermost::Presenter.authorize_chat_name(url)
+      return Gitlab::ChatCommands::Presenters::BasePresenter.new(url).authorize
     end
 
     Gitlab::ChatCommands::Command.new(project, user, params).execute

lib/gitlab/chat_commands/help.rb
@@ -1,10 +1,10 @@
 module Gitlab
   module ChatCommands
-    class Help < IssueCommand
+    class Help < BaseCommand
       # This class has to be used last, as it always matches. It has to match
       # because other commands were not triggered and we want to show the help
       # command
-      def self.match(_)
+      def self.match(_text)
         true
       end
 
@@ -16,10 +16,12 @@ module Gitlab
         true
       end
 
-      def execute(_)
-        commands = caller.first.available_commands
+      def execute(commands)
+        Gitlab::ChatCommands::Presenters::Help.new(commands).execute(trigger)
+      end
 
-        Gitlab::ChatCommands::Presenters::Help.new(commands).execute
+      def trigger
+        params[:command]
       end
     end
   end

spec/lib/gitlab/chat_commands/presenters/deploy_spec.rb
@@ -1,0 +1,47 @@
+require 'spec_helper'
+
+describe Gitlab::ChatCommands::Presenters::Deploy do
+  let(:build) { create(:ci_build) }
+
+  describe '#execute' do
+    subject { described_class.new(build).execute('staging', 'prod') }
+
+    it { is_expected.to have_key(:text) }
+    it { is_expected.to have_key(:response_type) }
+    it { is_expected.to have_key(:status) }
+    it { is_expected.not_to have_key(:attachments) }
+
+    it 'messages the channel of the deploy' do
+      expect(subject[:response_type]).to be(:in_channel)
+      expect(subject[:text]).to start_with("Deployment started from staging to prod")
+    end
+  end
+
+  describe '#no_actions' do
+    subject { described_class.new(nil).no_actions }
+
+    it { is_expected.to have_key(:text) }
+    it { is_expected.to have_key(:response_type) }
+    it { is_expected.to have_key(:status) }
+    it { is_expected.not_to have_key(:attachments) }
+
+    it 'tells the user there is no action' do
+      expect(subject[:response_type]).to be(:ephemeral)
+      expect(subject[:text]).to eq("No action found to be executed")
+    end
+  end
+
+  describe '#too_many_actions' do
+    subject { described_class.new(nil).too_many_actions }
+
+    it { is_expected.to have_key(:text) }
+    it { is_expected.to have_key(:response_type) }
+    it { is_expected.to have_key(:status) }
+    it { is_expected.not_to have_key(:attachments) }
+
+    it 'tells the user there is no action' do
+      expect(subject[:response_type]).to be(:ephemeral)
+      expect(subject[:text]).to eq("Too many actions defined")
+    end
+  end
+end

gitlabhq/gitlabhq

app/models/project_services/mattermost_slash_commands_service.rb
@@ -31,7 +31,7 @@ class MattermostSlashCommandsService < ChatService
     user = find_chat_user(params)
     unless user
       url = authorize_chat_name_url(params)
-      return Mattermost::Presenter.authorize_chat_name(url)
+      return Gitlab::ChatCommands::Presenters::Access.new(url).authorize
     end
 
     Gitlab::ChatCommands::Command.new(project, user, params).execute

lib/gitlab/chat_commands/help.rb
@@ -1,10 +1,10 @@
 module Gitlab
   module ChatCommands
-    class Help < IssueCommand
+    class Help < BaseCommand
       # This class has to be used last, as it always matches. It has to match
       # because other commands were not triggered and we want to show the help
       # command
-      def self.match(_)
+      def self.match(_text)
         true
       end
 
@@ -16,10 +16,12 @@ module Gitlab
         true
       end
 
-      def execute(_)
-        commands = caller.first.available_commands
+      def execute(commands)
+        Gitlab::ChatCommands::Presenters::Help.new(commands).present(trigger)
+      end
 
-        Gitlab::ChatCommands::Presenters::Help.new(commands).execute
+      def trigger
+        params[:command]
       end
     end
   end

spec/lib/gitlab/chat_commands/presenters/deploy_spec.rb
@@ -1,0 +1,47 @@
+require 'spec_helper'
+
+describe Gitlab::ChatCommands::Presenters::Deploy do
+  let(:build) { create(:ci_build) }
+
+  describe '#present' do
+    subject { described_class.new(build).present('staging', 'prod') }
+
+    it { is_expected.to have_key(:text) }
+    it { is_expected.to have_key(:response_type) }
+    it { is_expected.to have_key(:status) }
+    it { is_expected.not_to have_key(:attachments) }
+
+    it 'messages the channel of the deploy' do
+      expect(subject[:response_type]).to be(:in_channel)
+      expect(subject[:text]).to start_with("Deployment started from staging to prod")
+    end
+  end
+
+  describe '#no_actions' do
+    subject { described_class.new(nil).no_actions }
+
+    it { is_expected.to have_key(:text) }
+    it { is_expected.to have_key(:response_type) }
+    it { is_expected.to have_key(:status) }
+    it { is_expected.not_to have_key(:attachments) }
+
+    it 'tells the user there is no action' do
+      expect(subject[:response_type]).to be(:ephemeral)
+      expect(subject[:text]).to eq("No action found to be executed")
+    end
+  end
+
+  describe '#too_many_actions' do
+    subject { described_class.new(nil).too_many_actions }
+
+    it { is_expected.to have_key(:text) }
+    it { is_expected.to have_key(:response_type) }
+    it { is_expected.to have_key(:status) }
+    it { is_expected.not_to have_key(:attachments) }
+
+    it 'tells the user there is no action' do
+      expect(subject[:response_type]).to be(:ephemeral)
+      expect(subject[:text]).to eq("Too many actions defined")
+    end
+  end
+end