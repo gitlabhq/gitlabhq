097c283ac186bccb647143fd022bfe6a320379b0
Mike Greiling <mike@pixelcog.com>
2016-12-10 00:21:47 -0600

    remove left_align setting from notification setting dropdown in favor of a pure css solution

--- app/controllers/notification_settings_controller.rb
+++ app/controllers/notification_settings_controller.rb
@@ -37,11 +37,7 @@ class NotificationSettingsController < ApplicationController
 
   def render_response
     render json: {
-      html: view_to_html_string(
-        "shared/notifications/_button",
-        notification_setting: @notification_setting,
-        left_align: @notification_setting.source.nil?
-      ),
+      html: view_to_html_string("shared/notifications/_button", notification_setting: @notification_setting),
       saved: @saved
     }
   end
