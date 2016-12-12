1700ba59d422ae14ea68a12920b9a73d15572100
Munken <mm.munk@gmail.com>
2016-12-12 13:54:16 +0000

    Added line break to spec

--- spec/lib/banzai/filter/math_filter_spec.rb
+++ spec/lib/banzai/filter/math_filter_spec.rb
@@ -6,11 +6,13 @@ describe Banzai::Filter::MathFilter, lib: true do
   it 'leaves regular inline code unchanged' do
     input = "<code>2+2</code>"
     doc = filter(input)
+
     expect(doc.to_s).to eq input
   end
 
   it 'removes surrounding dollar signs and adds class code, math and js-render-math' do
     doc = filter("$<code>2+2</code>$")
+
     expect(doc.to_s).to eq '<code class="code math js-render-math" data-math-style="inline">2+2</code>'
   end
 
@@ -35,12 +37,14 @@ describe Banzai::Filter::MathFilter, lib: true do
   it 'adds data-math-style inline attribute to inline math' do
     doc = filter('$<code>2+2</code>$')
     code = doc.xpath('descendant-or-self::code').first
+
     expect(code['data-math-style']).to eq 'inline'
   end
 
   it 'adds class code and math to inline math' do
     doc = filter('$<code>2+2</code>$')
     code = doc.xpath('descendant-or-self::code').first
+
     expect(code[:class]).to include("code")
     expect(code[:class]).to include("math")
   end
@@ -48,6 +52,7 @@ describe Banzai::Filter::MathFilter, lib: true do
   it 'adds js-render-math class to inline math' do
     doc = filter('$<code>2+2</code>$')
     code = doc.xpath('descendant-or-self::code').first
+
     expect(code[:class]).to include("js-render-math")
   end
 
@@ -56,18 +61,21 @@ describe Banzai::Filter::MathFilter, lib: true do
   it 'ignores cases with missing dolar sign at the end' do
     input = "test $<code>2+2</code> test"
     doc = filter(input)
+
     expect(doc.to_s).to eq input
   end
 
   it 'ignores cases with missing dolar sign at the beginning' do
     input = "test <code>2+2</code>$ test"
     doc = filter(input)
+
     expect(doc.to_s).to eq input
   end
 
   it 'ignores dollar signs if it is not adjacent' do
     input = '<p>We check strictly $<code>2+2</code> and  <code>2+2</code>$ </p>'
     doc = filter(input)
+
     expect(doc.to_s).to eq input
   end
 
@@ -76,24 +84,28 @@ describe Banzai::Filter::MathFilter, lib: true do
   it 'adds data-math-style display attribute to display math' do
     doc = filter('<pre class="code highlight js-syntax-highlight math" v-pre="true"><code>2+2</code></pre>')
     pre = doc.xpath('descendant-or-self::pre').first
+
     expect(pre['data-math-style']).to eq 'display'
   end
 
   it 'adds js-render-math class to display math' do
     doc = filter('<pre class="code highlight js-syntax-highlight math" v-pre="true"><code>2+2</code></pre>')
     pre = doc.xpath('descendant-or-self::pre').first
+
     expect(pre[:class]).to include("js-render-math")
   end
 
   it 'ignores code blocks that are not math' do
     input = '<pre class="code highlight js-syntax-highlight plaintext" v-pre="true"><code>2+2</code></pre>'
     doc = filter(input)
+
     expect(doc.to_s).to eq input
   end
 
   it 'requires the pre to contain both code and math' do
     input = '<pre class="highlight js-syntax-highlight plaintext math" v-pre="true"><code>2+2</code></pre>'
     doc = filter(input)
+
     expect(doc.to_s).to eq input
   end
 end
