45220e310cd12da9bd7b0b07fe3509def063a60c
Kushal Pandya <kushal@gitlab.com>
2016-12-12 22:05:17 +0530

    Use `bind` instead of currying to access outer context

--- app/assets/javascripts/build.js
+++ app/assets/javascripts/build.js
@@ -86,15 +86,13 @@
       return $.ajax({
         url: this.buildUrl,
         dataType: 'json',
-        success: (function(_this) {
-          return function(buildData) {
-            $('.js-build-output').html(buildData.trace_html);
-            if (removeRefreshStatuses.indexOf(buildData.status) >= 0) {
-              _this.initScrollMonitor();
-              return $('.js-build-refresh').remove();
-            }
-          };
-        })(this)
+        success: function(buildData) {
+          $('.js-build-output').html(buildData.trace_html);
+          if (removeRefreshStatuses.indexOf(buildData.status) >= 0) {
+            this.initScrollMonitor();
+            return $('.js-build-refresh').remove();
+          }
+        }.bind(this)
       });
     };
 
