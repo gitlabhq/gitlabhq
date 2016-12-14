35a3e9183052bab847c30203f27fea9cf77901a4
Nick Thomas <nick@gitlab.com>
2016-12-14 22:58:44 +0000

    Make the index on environment name and project id unique, fixing up any duplicates

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161212142807) do
+ActiveRecord::Schema.define(version: 20161207231621) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -430,7 +430,7 @@ ActiveRecord::Schema.define(version: 20161212142807) do
     t.string "state", default: "available", null: false
   end
 
-  add_index "environments", ["project_id", "name"], name: "index_environments_on_project_id_and_name", using: :btree
+  add_index "environments", ["project_id", "name"], name: "index_environments_on_project_id_and_name", unique: true, using: :btree
 
   create_table "events", force: :cascade do |t|
     t.string "target_type"
