a20681c093d5444d24c54f41fd49a98ef919cd42
Kim "BKC" Carlbäcker <kim.carlbacker@gmail.com>
2016-12-15 22:37:52 +0100

    Update Runners in a Service

---
+++ app/services/ci/update_build_queue_service.rb
@@ -0,0 +1,12 @@
+module Ci
+  class UpdateBuildQueueService < BaseService
+
+    def execute(build)
+      build.project.runners.select do |runner|
+        if runner.can_pick?(build)
+          runner.tick_update
+        end
+      end
+    end
+  end
+end
--- app/workers/build_queue_worker.rb
+++ app/workers/build_queue_worker.rb
@@ -4,22 +4,8 @@ class BuildQueueWorker
 
   def perform(build_id)
     Ci::Build.find_by(id: build_id).try do |build|
-      build.project.runners.select do |runner|
-        if runner.can_pick?(build)
-          # Inject last_update into Redis
-          Gitlab::Redis.with do |redis|
-            new_update = Time.new.inspect
-            redis.set(runner_redis_key(runner), new_update, ex: 60.minutes)
-          end
-        end
-      end
+      UpdateBuildQueueService.new(build)
     end
-  end
-
-  private
-
-  def runner_redis_key(runner)
-    "#{runner.token}_#{runner.tag_list}"
   end
 
 end
