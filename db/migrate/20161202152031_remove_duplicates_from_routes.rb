ad4ec44673254b4bb91c6ddb391ff9553c9473a5
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2016-12-15 12:56:29 -0200

    Improve performance on RemoveDuplicatesFromRoutes migration

--- db/migrate/20161202152031_remove_duplicates_from_routes.rb
+++ db/migrate/20161202152031_remove_duplicates_from_routes.rb
@@ -12,20 +12,16 @@ class RemoveDuplicatesFromRoutes < ActiveRecord::Migration
     # to fill these values that avoid duplicate entries in the routes table.
     return unless Gitlab::Database.mysql?
 
-    select_all("SELECT path FROM #{quote_table_name(:routes)} GROUP BY path HAVING COUNT(*) > 1").each do |row|
-      path = connection.quote(row['path'])
-      execute(%Q{
-        DELETE FROM #{quote_table_name(:routes)}
-        WHERE path = #{path}
-        AND id != (
-          SELECT id FROM (
-            SELECT max(id) AS id
-            FROM #{quote_table_name(:routes)}
-            WHERE path = #{path}
-          ) max_ids
-        )
-      })
-    end
+    execute <<-EOF
+      DELETE duplicated_rows.*
+      FROM routes AS duplicated_rows
+        INNER JOIN (
+          SELECT path, MAX(id) as max_id
+          FROM routes
+          GROUP BY path
+          HAVING COUNT(*) > 1
+        ) AS good_rows ON good_rows.path = duplicated_rows.path AND good_rows.max_id <> duplicated_rows.id;
+    EOF
   end
 
   def down
