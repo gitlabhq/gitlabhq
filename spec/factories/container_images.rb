b408a192e0fbf630d4f9a4112f6835be50a681d8
Andre Guedes <andrebsguedes@gmail.com>
2016-12-16 12:07:20 -0200

    Adding mock for full_access_token

--- spec/factories/container_images.rb
+++ spec/factories/container_images.rb
@@ -11,6 +11,7 @@ FactoryGirl.define do
     after(:build) do |image, evaluator|
       if evaluator.stubbed
         allow(Gitlab.config.registry).to receive(:enabled).and_return(true)
+        allow(Auth::ContainerRegistryAuthenticationService).to receive(:full_access_token).and_return('token')
         allow(image.client).to receive(:repository_tags).and_return({
           name: image.name_with_namespace,
           tags: evaluator.tags
