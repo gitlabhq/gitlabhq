5fd1c6a3a0570ccd35f02dc91a89e81e740d920e
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2016-12-17 12:48:23 -0200

    Fix rubucop offense

--- spec/models/project_services/chat_notification_service_spec.rb
+++ spec/models/project_services/chat_notification_service_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 
 describe ChatNotificationService, models: true do
   describe "Associations" do
-
     before do
       allow(subject).to receive(:activated?).and_return(true)
     end
