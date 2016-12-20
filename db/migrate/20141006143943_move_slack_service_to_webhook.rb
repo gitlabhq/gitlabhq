7819e0fa78417f114ae4ec654473e802f0bb95f9
James Lopez <james@jameslopez.es>
2016-12-20 09:06:11 +0100

    fix migration downtime check

--- db/migrate/20141006143943_move_slack_service_to_webhook.rb
+++ db/migrate/20141006143943_move_slack_service_to_webhook.rb
@@ -1,5 +1,8 @@
 # rubocop:disable all
 class MoveSlackServiceToWebhook < ActiveRecord::Migration
+  DOWNTIME = true
+  DOWNTIME_REASON = 'Move old fields "token" and "subdomain" to one single field "webhook"'
+
   def change
     SlackService.all.each do |slack_service|
       if ["token", "subdomain"].all? { |property| slack_service.properties.key? property }
