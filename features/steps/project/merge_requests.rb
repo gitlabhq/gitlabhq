cd1b6acbf1716831fed2b328cefc1a1916db2c25
Bryce Johnson <bryce@gitlab.com>
2016-12-21 10:18:25 +0100

    Remove unused spec for merge button disabled.

--- features/steps/project/merge_requests.rb
+++ features/steps/project/merge_requests.rb
@@ -608,13 +608,6 @@ class Spinach::Features::ProjectMergeRequests < Spinach::FeatureSteps
     end
   end
 
-  step 'I should see the merge button disabled' do
-    page.within '.mr-state-widget' do
-      wait_for_ajax
-      expect(page).to have_button('Accept Merge Request', disabled: true)
-    end
-  end
-
   step 'I should not see merge button' do
     page.within '.mr-state-widget' do
       expect(page).not_to have_button('Accept Merge Request')
