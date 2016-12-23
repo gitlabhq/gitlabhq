f4a2d33a6795b84a3c7c00de0fb8dbfa6100c037
Jacob Vosmaer <jacob@gitlab.com>
2016-12-23 13:42:42 +0100

    Monkey-patch StrongParameters for ::UploadedFile

--- config/initializers/workhorse_multipart.rb
+++ config/initializers/workhorse_multipart.rb
@@ -1,3 +1,19 @@
 Rails.application.configure do |config|
   config.middleware.use(Gitlab::Middleware::Multipart)
 end
+
+module Gitlab
+  module StrongParameterScalars
+    GITLAB_PERMITTED_SCALAR_TYPES = [::UploadedFile]
+
+    def permitted_scalar?(value)
+      super || GITLAB_PERMITTED_SCALAR_TYPES.any? { |type| value.is_a?(type) }
+    end
+  end
+end
+
+module ActionController
+  class Parameters
+    prepend Gitlab::StrongParameterScalars
+  end
+end
