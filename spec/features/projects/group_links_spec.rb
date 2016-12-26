cfd8f635b391c9389aa122db4e0c9031adef6b5a
Jose Ivan Vargas <jvargas@gitlab.com>
2016-12-26 16:15:32 -0600

    Added tests for the MembersController and corrected some more tests

--- spec/features/projects/group_links_spec.rb
+++ spec/features/projects/group_links_spec.rb
@@ -17,7 +17,7 @@ feature 'Project group links', feature: true, js: true do
       visit namespace_project_settings_members_path(project.namespace, project)
 
       select2 group.id, from: '#link_group_id'
-      fill_in 'expires_at', with: (Time.current + 4.5.days).strftime('%Y-%m-%d')
+      fill_in 'expires_at_groups', with: (Time.current + 4.5.days).strftime('%Y-%m-%d')
       page.find('body').click
       click_on 'Share'
     end
