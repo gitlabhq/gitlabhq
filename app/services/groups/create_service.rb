9410f215eab0ba49051d2a00f0b4174f5dc13a6f
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2016-12-26 11:56:19 +0200

    Add nested groups support to the Groups::CreateService
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- app/services/groups/create_service.rb
+++ app/services/groups/create_service.rb
@@ -12,6 +12,19 @@ module Groups
         return @group
       end
 
+      parent_id = params[:parent_id]
+
+      if parent_id
+        parent = Group.find(parent_id)
+
+        unless can?(current_user, :admin_group, parent)
+          @group.parent_id = nil
+          @group.errors.add(:parent_id, 'manage access required to create subgroup')
+
+          return @group
+        end
+      end
+
       @group.name ||= @group.path.dup
       @group.save
       @group.add_owner(current_user)
