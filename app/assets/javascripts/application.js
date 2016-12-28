682b71eae692400b08ed73c76119eb0b6a462d17
Mike Greiling <mike@pixelcog.com>
2016-12-28 09:54:51 -0600

    move body click binding inside the onready callback

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -76,14 +76,6 @@
     gl.utils.handleLocationHash();
   }, false);
 
-  // `hashchange` is not triggered when link target is already in window.location
-  $('body').on('click', 'a', function() {
-    var href = this.getAttribute('href');
-    if (href.indexOf('#') === 0 && href.substr(1) === gl.utils.getLocationHash()) {
-      setTimeout(gl.utils.handleLocationHash, 1);
-    }
-  });
-
   $(function () {
     var $body = $('body');
     var $document = $(document);
@@ -96,6 +88,14 @@
 
     // Set the default path for all cookies to GitLab's root directory
     Cookies.defaults.path = gon.relative_url_root || '/';
+
+    // `hashchange` is not triggered when link target is already in window.location
+    $body.on('click', 'a', function() {
+      var href = this.getAttribute('href');
+      if (href.indexOf('#') === 0 && href.substr(1) === gl.utils.getLocationHash()) {
+        setTimeout(gl.utils.handleLocationHash, 1);
+      }
+    });
 
     // prevent default action for disabled buttons
     $('.btn').click(function(e) {
