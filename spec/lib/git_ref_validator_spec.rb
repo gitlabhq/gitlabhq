7a399b7061d4d374f01ddaa75ae7fba53ca4cb6b
Matthieu Tardy <matthieu.tardy@gmail.com>
2017-01-09 07:38:13 +0100

    Strip reference prefixes on branch creation
    Signed-off-by: Matthieu Tardy <matthieu.tardy@gmail.com>

--- spec/lib/git_ref_validator_spec.rb
+++ spec/lib/git_ref_validator_spec.rb
@@ -5,6 +5,7 @@ describe Gitlab::GitRefValidator, lib: true do
   it { expect(Gitlab::GitRefValidator.validate('implement_@all')).to be_truthy }
   it { expect(Gitlab::GitRefValidator.validate('my_new_feature')).to be_truthy }
   it { expect(Gitlab::GitRefValidator.validate('#1')).to be_truthy }
+  it { expect(Gitlab::GitRefValidator.validate('feature/refs/heads/foo')).to be_truthy }
   it { expect(Gitlab::GitRefValidator.validate('feature/~new/')).to be_falsey }
   it { expect(Gitlab::GitRefValidator.validate('feature/^new/')).to be_falsey }
   it { expect(Gitlab::GitRefValidator.validate('feature/:new/')).to be_falsey }
@@ -17,4 +18,8 @@ describe Gitlab::GitRefValidator, lib: true do
   it { expect(Gitlab::GitRefValidator.validate('feature\new')).to be_falsey }
   it { expect(Gitlab::GitRefValidator.validate('feature//new')).to be_falsey }
   it { expect(Gitlab::GitRefValidator.validate('feature new')).to be_falsey }
+  it { expect(Gitlab::GitRefValidator.validate('refs/heads/')).to be_falsey }
+  it { expect(Gitlab::GitRefValidator.validate('refs/remotes/')).to be_falsey }
+  it { expect(Gitlab::GitRefValidator.validate('refs/heads/feature')).to be_falsey }
+  it { expect(Gitlab::GitRefValidator.validate('refs/remotes/origin')).to be_falsey }
 end
