jakop345/gitlab-ce

migrate all javascript asset bundles and require syntax

app/assets/javascripts/users/users_bundle.js
@@ -1,7 +1,5 @@
 /* eslint-disable */
 
-/*= require_tree . */
-
-(function() {
-
-}).call(this);
+// require everything else in this directory
+function require_all(context) { return context.keys().map(context); }
+require_all(require.context('.', false, /^\.\/(?!users_bundle).*\.(js|es6)$/));

gitlabhq/gitlabhq

app/assets/javascripts/users/users_bundle.js
@@ -1,7 +1,3 @@
-/* eslint-disable func-names, space-before-function-paren */
-
-/*= require_tree . */
-
-(function() {
-
-}).call(this);
+// require everything else in this directory
+function requireAll(context) { return context.keys().map(context); }
+requireAll(require.context('.', false, /^\.\/(?!users_bundle).*\.(js|es6)$/));