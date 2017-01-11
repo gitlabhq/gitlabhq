53846da2c7fe3879b4f26383d6367b0bb69e5dc8
Z.J. van de Weg <git@zjvandeweg.nl>
2017-01-11 09:04:49 -0500

    Add help command

--- lib/gitlab/chat_commands/command.rb
+++ lib/gitlab/chat_commands/command.rb
@@ -18,24 +18,21 @@ module Gitlab
             Gitlab::ChatCommands::Presenters::Access.new.access_denied
           end
         else
-          help(help_messages)
+          Gitlab::ChatCommands::Help.new(project, current_user, params).execute(available_commands)
         end
       end
 
       def match_command
         match = nil
-        service = available_commands.find do |klass|
-          match = klass.match(params[:text])
-        end
+        service =
+          available_commands.find do |klass|
+            match = klass.match(params[:text])
+          end
 
         [service, match]
       end
 
       private
-
-      def help_messages
-        available_commands.map(&:help_message)
-      end
 
       def available_commands
         COMMANDS.select do |klass|
