8a643c8fcd15e23a3b97ddc03a161ce3c8451099
Mike Greiling <mike@pixelcog.com>
2017-01-13 11:05:26 -0500

    add phantomJS to karma config

--- config/karma.config.js
+++ config/karma.config.js
@@ -6,6 +6,7 @@ var ROOT_PATH = path.resolve(__dirname, '..');
 module.exports = function(config) {
   config.set({
     basePath: ROOT_PATH,
+    browsers: ['PhantomJS'],
     frameworks: ['jasmine'],
     files: [
       { pattern: 'spec/javascripts/test_bundle.js', watched: false },
