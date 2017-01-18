86e46688d794a58559ccefc0a0bc8a8d57401dcd
Sean McGivern <sean@mcgivern.me.uk>
2017-01-18 18:18:13 +0000

    Merge branch 'time-tracking-api' into 'master'
    Time tracking API
    
    Closes #25861
    
    See merge request !8483

--- spec/requests/api/issues_spec.rb
+++ spec/requests/api/issues_spec.rb
@@ -1193,4 +1193,10 @@ describe API::Issues, api: true  do
       expect(response).to have_http_status(404)
     end
   end
+
+  describe 'time tracking endpoints' do
+    let(:issuable) { issue }
+
+    include_examples 'time tracking endpoints', 'issue'
+  end
 end
