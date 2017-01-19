eb7553c484782900b8fce0a90307e3a59b2a0d18
Fatih Acet <acetfatih@gmail.com>
2017-01-19 20:52:03 +0000

    Merge branch '26066-wrong-messsage-for-unregistered-u2f-device-on-sign-in' into 'master'
    New U2F ineligible message
    
    Closes #26066
    
    See merge request !8392

--- app/assets/javascripts/u2f/error.js
+++ app/assets/javascripts/u2f/error.js
@@ -5,21 +5,21 @@
   var bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
   this.U2FError = (function() {
-    function U2FError(errorCode) {
+    function U2FError(errorCode, u2fFlowType) {
       this.errorCode = errorCode;
       this.message = bind(this.message, this);
       this.httpsDisabled = window.location.protocol !== 'https:';
+      this.u2fFlowType = u2fFlowType;
     }
 
     U2FError.prototype.message = function() {
-      switch (false) {
-        case !(this.errorCode === u2f.ErrorCodes.BAD_REQUEST && this.httpsDisabled):
-          return "U2F only works with HTTPS-enabled websites. Contact your administrator for more details.";
-        case this.errorCode !== u2f.ErrorCodes.DEVICE_INELIGIBLE:
-          return "This device has already been registered with us.";
-        default:
-          return "There was a problem communicating with your device.";
+      if (this.errorCode === u2f.ErrorCodes.BAD_REQUEST && this.httpsDisabled) {
+        return 'U2F only works with HTTPS-enabled websites. Contact your administrator for more details.';
+      } else if (this.errorCode === u2f.ErrorCodes.DEVICE_INELIGIBLE) {
+        if (this.u2fFlowType === 'authenticate') return 'This device has not been registered with us.';
+        if (this.u2fFlowType === 'register') return 'This device has already been registered with us.';
       }
+      return "There was a problem communicating with your device.";
     };
 
     return U2FError;
