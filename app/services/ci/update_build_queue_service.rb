a5ab418c370d79b3fe7e82ad00c47459ade678d8
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2017-01-20 14:32:32 +0100

    Add missing specs for update build queue service

--- app/services/ci/update_build_queue_service.rb
+++ app/services/ci/update_build_queue_service.rb
@@ -7,11 +7,13 @@ module Ci
         end
       end
 
+      return unless build.project.shared_runners_enabled?
+
       Ci::Runner.shared.each do |runner|
         if runner.can_pick?(build)
           runner.tick_runner_queue
         end
-      end if build.project.shared_runners_enabled?
+      end
     end
   end
 end
