d4bc570ee2707aee3dc4eb69f707cc45f849a814
Oswaldo Ferreira <oswaldo@gitlab.com>
2017-01-21 21:20:20 -0200

    Remove unnecessary attr_accessor

--- app/services/application_settings/update_service.rb
+++ app/services/application_settings/update_service.rb
@@ -2,9 +2,9 @@ module ApplicationSettings
   class UpdateService < ApplicationSettings::BaseService
     def execute
       # Repository size limit comes as MB from the view
-      assign_repository_size_limit_as_bytes(application_setting)
+      assign_repository_size_limit_as_bytes(@application_setting)
 
-      application_setting.update(params)
+      @application_setting.update(@params)
     end
   end
 end
