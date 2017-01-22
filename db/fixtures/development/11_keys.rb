88e627cf14b47ca5d63f2cb0ffe24124fb4b116a
Yorick Peterse <yorickpeterse@gmail.com>
2017-01-22 18:22:02 +0100

    Fix race conditions for AuthorizedProjectsWorker
    There were two cases that could be problematic:
    
    1. Because sometimes AuthorizedProjectsWorker would be scheduled in a
       transaction it was possible for a job to run/complete before a
       COMMIT; resulting in it either producing an error, or producing no
       new data.
    
    2. When scheduling jobs the code would not wait until completion. This
       could lead to a user creating a project and then immediately trying
       to push to it. Usually this will work fine, but given enough load it
       might take a few seconds before a user has access.
    
    The first one is problematic, the second one is mostly just annoying
    (but annoying enough to warrant a solution).
    
    This commit changes two things to deal with this:
    
    1. Sidekiq scheduling now takes places after a COMMIT, this is ensured
       by scheduling using Rails' after_commit hook instead of doing so in
       an arbitrary method.
    
    2. When scheduling jobs the calling thread now waits for all jobs to
       complete.
    
    Solution 2 requires tracking of job completions. Sidekiq provides a way
    to find a job by its ID, but this involves scanning over the entire
    queue; something that is very in-efficient for large queues. As such a
    more efficient solution is necessary. There are two main Gems that can
    do this in a more efficient manner:
    
    * sidekiq-status
    * sidekiq_status
    
    No, this is not a joke. Both Gems do a similar thing (but slightly
    different), and the only difference in their name is a dash vs an
    underscore. Both Gems however provide far more than just checking if a
    job has been completed, and both have their problems. sidekiq-status
    does not appear to be actively maintained, with the last release being
    in 2015. It also has some issues during testing as API calls are not
    stubbed in any way. sidekiq_status on the other hand does not appear to
    be very popular, and introduces a similar amount of code.
    
    Because of this I opted to write a simple home grown solution. After
    all, all we need is storing a job ID somewhere so we can efficiently
    look it up; we don't need extra web UIs (as provided by sidekiq-status)
    or complex APIs to update progress, etc.
    
    This is where Gitlab::SidekiqStatus comes in handy. This namespace
    contains some code used for tracking, removing, and looking up job IDs;
    all without having to scan over an entire queue. Data is removed
    explicitly, but also expires automatically just in case.
    
    Using this API we can now schedule jobs in a fork-join like manner: we
    schedule the jobs in Sidekiq, process them in parallel, then wait for
    completion. By using Sidekiq we can leverage all the benefits such as
    being able to scale across multiple cores and hosts, retrying failed
    jobs, etc.
    
    The one downside is that we need to make sure we can deal with
    unexpected increases in job processing timings. To deal with this the
    class Gitlab::JobWaiter (used for waiting for jobs to complete) will
    only wait a number of seconds (30 by default). Once this timeout is
    reached it will simply return.
    
    For GitLab.com almost all AuthorizedProjectWorker jobs complete in
    seconds, only very rarely do we spike to job timings of around a minute.
    These in turn seem to be the result of external factors (e.g. deploys),
    in which case a user is most likely not able to use the system anyway.
    
    In short, this new solution should ensure that jobs are processed
    properly and that in almost all cases a user has access to their
    resources whenever they need to have access.

--- db/fixtures/development/11_keys.rb
+++ db/fixtures/development/11_keys.rb
@@ -1,12 +1,18 @@
-Gitlab::Seeder.quiet do
-  User.first(10).each do |user|
-    key = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAIEAiPWx6WM4lhHNedGfBpPJNPpZ7yKu+dnn1SJejgt#{user.id + 100}6k6YjzGGphH2TUxwKzxcKDKKezwkpfnxPkSMkuEspGRt/aZZ9wa++Oi7Qkr8prgHc4soW6NUlfDzpvZK2H5E7eQaSeP3SAwGmQKUFHCddNaP0L+hM7zhFNzjFvpaMgJw0="
+require './spec/support/sidekiq'
 
-    user.keys.create(
-      title: "Sample key #{user.id}",
-      key: key
-    )
+# Creating keys runs a gitlab-shell worker. Since we may not have the right
+# gitlab-shell path set (yet) we need to disable this for these fixtures.
+Sidekiq::Testing.disable! do
+  Gitlab::Seeder.quiet do
+    User.first(10).each do |user|
+      key = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAIEAiPWx6WM4lhHNedGfBpPJNPpZ7yKu+dnn1SJejgt#{user.id + 100}6k6YjzGGphH2TUxwKzxcKDKKezwkpfnxPkSMkuEspGRt/aZZ9wa++Oi7Qkr8prgHc4soW6NUlfDzpvZK2H5E7eQaSeP3SAwGmQKUFHCddNaP0L+hM7zhFNzjFvpaMgJw0="
 
-    print '.'
+      user.keys.create(
+        title: "Sample key #{user.id}",
+        key: key
+      )
+
+      print '.'
+    end
   end
 end
