8ba5035c67326dfb4c8be0b16bd2317f7f35510a
Rémy Coutable <remy@rymai.me>
2017-01-23 11:37:44 +0100

    Revert "Merge branch 'revert-ddcc8e7c' into 'master'"
    This reverts commit 9eab22703c5ee86ac48c3a591b41ce4e24b85c90, reversing
    changes made to 46f2709da286d3757d7f4e8cd22d0963b0786e42.

--- spec/lib/gitlab/elastic/indexer_spec.rb
+++ spec/lib/gitlab/elastic/indexer_spec.rb
@@ -1,15 +1,21 @@
 require 'spec_helper'
 
 describe "Indexer" do
-  it "runs commands" do
+  include StubENV
+
+  before do
+    stub_env('IN_MEMORY_APPLICATION_SETTINGS', 'true')
     stub_application_setting(es_host: ['elastic-host1', 'elastic-host2'])
+  end
+
+  it "runs commands" do
     expect(Gitlab::Popen).to receive(:popen).with(
       [File.join(Rails.root, 'bin/elastic_repo_indexer'), '1', 'full_repo_path'],
       nil,
       hash_including(
         'ELASTIC_CONNECTION_INFO' => {
-                                       host: ApplicationSetting.current.elasticsearch_host,
-                                       port: ApplicationSetting.current.elasticsearch_port
+                                       host: current_application_settings.elasticsearch_host,
+                                       port: current_application_settings.elasticsearch_port
                                      }.to_json,
         'RAILS_ENV'               => Rails.env,
         'FROM_SHA' => '000000',
