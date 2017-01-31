5967c17e80ec0e17a2bdc615e18d1cb0a668f68f
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2017-01-31 15:12:02 +0100

    Introduced the deploy keys presenter

--- app/controllers/projects/settings/repository_controller.rb
+++ app/controllers/projects/settings/repository_controller.rb
@@ -7,7 +7,9 @@ module Projects
       before_action :load_protected_branches, only: [:show]
 
       def show
-        define_deploy_keys_variables
+        @deploy_keys = DeployKeysPresenter
+          .new(@project, current_user: @current_user)
+
         define_protected_branches_controller
       end
 
@@ -15,24 +17,7 @@ module Projects
         @protected_branches = @project.protected_branches.order(:name).page(params[:page])
       end
 
-      def set_index_vars
-        @enabled_keys           ||= @project.deploy_keys
-
-        @available_keys         ||= current_user.accessible_deploy_keys - @enabled_keys
-        @available_project_keys ||= current_user.project_deploy_keys - @enabled_keys
-        @available_public_keys  ||= DeployKey.are_public - @enabled_keys
-
-        # Public keys that are already used by another accessible project are already
-        # in @available_project_keys.
-        @available_public_keys -= @available_project_keys
-      end
-
       private
-
-      def define_deploy_keys_variables
-        @key = DeployKey.new
-        set_index_vars
-      end
 
       def define_protected_branches_controller
         @protected_branch = @project.protected_branches.new
