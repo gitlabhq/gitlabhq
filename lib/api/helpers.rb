4e9e29d295fe2f8cd258cde4b65e244eb74a1ae6
Toon Claes <toon@gitlab.com>
2017-02-01 11:23:57 +0100

    API: Consolidate /projects endpoint
    It consolidates these endpoints:
     - /projects
     - /projects/owned
     - /projects/visible
     - /projects/starred
     - /projects/all
    
    Into the /projects endpoint using query parameters.

--- lib/api/helpers.rb
+++ lib/api/helpers.rb
@@ -256,6 +256,14 @@ module API
     # project helpers
 
     def filter_projects(projects)
+      if params[:owned]
+        projects = projects.merge(current_user.owned_projects)
+      end
+
+      if params[:starred]
+        projects = projects.merge(current_user.starred_projects)
+      end
+
       if params[:search].present?
         projects = projects.search(params[:search])
       end
