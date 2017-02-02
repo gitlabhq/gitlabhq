d9d417d405c42721b300e3b4a0dc6656529c3411
Jose Ivan Vargas <jvargas@gitlab.com>
2017-02-02 11:25:12 -0600

    Added a variable_controller_spec test to test for flash messages on the #create action

---
+++ spec/controllers/projects/variables_controller_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe Projects::VariablesController do
+  let(:project) { create(:empty_project) }
+  let(:user) { create(:user) }
+
+  before do
+    sign_in(user)
+    project.team << [user, :master]
+  end
+
+  describe 'POST #create' do
+    context 'variable is valid' do
+      it 'shows a success flash message' do
+        post :create, namespace_id: project.namespace.to_param, project_id: project.to_param,
+                      variable: { key: "one", value: "two" }
+        expect(flash[:notice]).to include 'Variables were successfully updated.'
+        expect(response).to redirect_to(namespace_project_settings_ci_cd_path(project.namespace, project))
+      end
+    end
+
+    context 'variable is invalid' do
+      it 'shows a failure flash message' do
+        post :create, namespace_id: project.namespace.to_param, project_id: project.to_param,
+                      variable: { key: "..one", value: "two" }
+        expect(flash[:alert]).to include 'Key can contain only letters, digits and \'_\'.'
+        expect(response).to redirect_to(namespace_project_settings_ci_cd_path(project.namespace, project))
+      end
+    end
+  end
+end
