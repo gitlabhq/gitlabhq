bfd9bbba2b617200ecd65c9e9a9c35c2ff3cad2f
Phil Hughes <me@iamphill.com>
2017-02-02 16:57:41 +0000

    Added specs for merge request environment list
    Ported from https://gitlab.com/gitlab-org/gitlab-ee/merge_requests/1154

--- spec/features/merge_requests/widget_spec.rb
+++ spec/features/merge_requests/widget_spec.rb
@@ -5,30 +5,53 @@ describe 'Merge request', :feature, :js do
 
   let(:project) { create(:project) }
   let(:user) { create(:user) }
+  let(:merge_request) { create(:merge_request, source_project: project) }
 
   before do
     project.team << [user, :master]
     login_as(user)
-
-    visit new_namespace_project_merge_request_path(
-      project.namespace,
-      project,
-      merge_request: {
-        source_project_id: project.id,
-        target_project_id: project.id,
-        source_branch: 'feature',
-        target_branch: 'master'
-      }
-    )
   end
 
-  it 'shows widget status after creating new merge request' do
-    click_button 'Submit merge request'
+  context 'new merge request' do
+    before do
+      visit new_namespace_project_merge_request_path(
+        project.namespace,
+        project,
+        merge_request: {
+          source_project_id: project.id,
+          target_project_id: project.id,
+          source_branch: 'feature',
+          target_branch: 'master'
+        }
+      )
+    end
 
-    expect(find('.mr-state-widget')).to have_content('Checking ability to merge automatically')
+    it 'shows widget status after creating new merge request' do
+      click_button 'Submit merge request'
 
-    wait_for_ajax
+      expect(find('.mr-state-widget')).to have_content('Checking ability to merge automatically')
 
-    expect(page).to have_selector('.accept_merge_request')
+      wait_for_ajax
+
+      expect(page).to have_selector('.accept_merge_request')
+    end
+  end
+
+  context 'view merge request' do
+    let!(:environment) { create(:environment, project: project) }
+    let!(:deployment) { create(:deployment, environment: environment, ref: 'feature', sha: merge_request.diff_head_sha) }
+
+    before do
+      visit namespace_project_merge_request_path(project.namespace, project, merge_request)
+    end
+
+    it 'shows environments link' do
+      wait_for_ajax
+
+      page.within('.mr-widget-heading') do
+        expect(page).to have_content("Deployed to #{environment.name}")
+        expect(find('.js-environment-link')[:href]).to include(environment.formatted_external_url)
+      end
+    end
   end
 end
