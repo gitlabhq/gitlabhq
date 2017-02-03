aaf382d52b87ba13f0cf904e076d9e3eac8d77d9
Lin Jen-Shin <godfat@godfat.org>
2017-02-03 23:21:10 +0800

    Skip or retain project while deleting the project:
    * Skip Ci::Build#update_project_statistics whenever there's no
      project (i.e. we're deleting the project!)
    * Retain the unscoped_project before deleting the build,
      so that we could use the data to delete the artifacts.
      Note that carrierwave uses `after_commit` for this,
      so we need to retain it in the memory.
    
    Closes #15005

--- spec/workers/project_destroy_worker_spec.rb
+++ spec/workers/project_destroy_worker_spec.rb
@@ -1,21 +1,28 @@
 require 'spec_helper'
 
 describe ProjectDestroyWorker do
-  let(:project) { create(:project) }
+  let(:project) { create(:project, pending_delete: true) }
   let(:path) { project.repository.path_to_repo }
 
   subject { ProjectDestroyWorker.new }
 
-  describe "#perform" do
-    it "deletes the project" do
-      subject.perform(project.id, project.owner.id, {})
+  describe '#perform' do
+    context 'with pipelines and builds' do
+      let!(:pipeline) { create(:ci_pipeline, project: project) }
+      let!(:build) { create(:ci_build, :artifacts, pipeline: pipeline) }
 
-      expect(Project.all).not_to include(project)
-      expect(Dir.exist?(path)).to be_falsey
+      it 'deletes the project along with pipelines and builds' do
+        subject.perform(project.id, project.owner.id, {})
+
+        expect(Project.all).not_to include(project)
+        expect(Ci::Pipeline.all).not_to include(pipeline)
+        expect(Ci::Build.all).not_to include(build)
+        expect(Dir.exist?(path)).to be_falsey
+      end
     end
 
-    it "deletes the project but skips repo deletion" do
-      subject.perform(project.id, project.owner.id, { "skip_repo" => true })
+    it 'deletes the project but skips repo deletion' do
+      subject.perform(project.id, project.owner.id, { 'skip_repo' => true })
 
       expect(Project.all).not_to include(project)
       expect(Dir.exist?(path)).to be_truthy
