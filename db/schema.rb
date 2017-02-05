18a8ea564986f69dd7c9e1e8ec923cec107e0350
Yorick Peterse <yorickpeterse@gmail.com>
2017-02-05 19:52:06 +0000

    Merge branch 'sh-add-index-for-label-count' into 'master'
    Add index to labels for `type` and project_id`
    
    Closes #27676
    
    See merge request !8978

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170130204620) do
+ActiveRecord::Schema.define(version: 20170204181513) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -575,6 +575,7 @@ ActiveRecord::Schema.define(version: 20170130204620) do
   end
 
   add_index "labels", ["group_id", "project_id", "title"], name: "index_labels_on_group_id_and_project_id_and_title", unique: true, using: :btree
+  add_index "labels", ["type", "project_id"], name: "index_labels_on_type_and_project_id", using: :btree
 
   create_table "lfs_objects", force: :cascade do |t|
     t.string "oid", null: false
