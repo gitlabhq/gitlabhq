37bcdc913061959bc35d2dc6d0e4a22b14e90adf
Douwe Maan <douwe@selenight.nl>
2017-02-06 19:16:54 -0600

    Prevent licenses table from getting cleaned up

--- spec/support/db_cleaner.rb
+++ spec/support/db_cleaner.rb
@@ -12,7 +12,7 @@ RSpec.configure do |config|
   end
 
   config.before(:each, truncate: true) do
-    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.strategy = :truncation, { except: ['licenses'] }
   end
 
   config.before(:each) do
