2a9ac54b407be1d8633fb9663faec0a8b334acb1
Robert Speicher <rspeicher@gmail.com>
2017-02-08 17:09:16 -0500

    Add a custom RSpec/SingleLineHook cop
    This cop adds an offense when `before`, `after`, or `around` are used as
    single-line blocks.

---
+++ rubocop/cop/rspec/single_line_hook.rb
@@ -0,0 +1,36 @@
+module RuboCop
+  module Cop
+    module RSpec
+      # This cop checks for single-line hook blocks
+      #
+      # @example
+      #
+      #   # bad
+      #   before { do_something }
+      #   after(:each) { undo_something }
+      #
+      #   # good
+      #   before do
+      #     do_something
+      #   end
+      #
+      #   after(:each) do
+      #     undo_something
+      #   end
+      class SingleLineHook < RuboCop::Cop::RSpec::Cop
+        MESSAGE = "Don't use single-line hook blocks.".freeze
+
+        def_node_search :rspec_hook?, <<~PATTERN
+          (send nil {:after :around :before} ...)
+        PATTERN
+
+        def on_block(node)
+          return unless rspec_hook?(node)
+          return unless node.single_line?
+
+          add_offense(node, :expression, MESSAGE)
+        end
+      end
+    end
+  end
+end
