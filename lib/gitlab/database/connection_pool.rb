7cb21a2c43b4d932e6a609cc59ce9b5c3589316a
Lin Jen-Shin <godfat@godfat.org>
2017-02-11 02:00:01 +0800

    Introduce Gitlab::Database::ConnectionPool
    This is a class where we could use in the migration,
    creating a database connection pool with arbitrary
    pool size. In general, the only use case would be:
    
    ``` ruby
    Gitlab::Database::ConnectionPool.with_pool(2) do |pool|
      pool.execute_async('DELETE FROM table0;')
      pool.execute_async('DELETE FROM table1;')
      pool.execute_async('DELETE FROM table2;')
    end
    ```
    
    This would run the queries with ActiveRecord's `with_connection` and
    `connection.execute` in threads, and wait until all the queries are
    done, and finally gracefully close the connection pool.

---
+++ lib/gitlab/database/connection_pool.rb
@@ -0,0 +1,79 @@
+module Gitlab
+  module Database
+    class ConnectionPool
+      def self.with_pool(pool_size)
+        pool = new(pool_size)
+
+        yield(pool)
+
+      ensure
+        pool.join
+        pool.close
+      end
+
+      attr_reader :ar_pool, :workers
+
+      def initialize(pool_size)
+        @ar_pool = create_connection_pool(pool_size)
+        @workers = []
+        @mutex = Mutex.new
+      end
+
+      def pool_size
+        @ar_pool.spec.config[:pool]
+      end
+
+      def execute_async(sql)
+        @mutex.synchronize do
+          @workers << Thread.new do
+            @ar_pool.with_connection do |connection|
+              connection.execute(sql)
+            end
+          end
+        end
+      end
+
+      def join
+        threads = nil
+
+        @mutex.synchronize do
+          threads = @workers.dup
+          @workers.clear
+        end
+
+        threads.map(&:value)
+      end
+
+      def close
+        @ar_pool.disconnect!
+      end
+
+      def closed?
+        !@ar_pool.connected?
+      end
+
+      def inspect
+        "#<#{self.class.name}" \
+          " pool_size=#{pool_size}" \
+          " workers_size=#{workers.size}>"
+      end
+
+      private
+
+      def create_connection_pool(pool_size)
+        # See activerecord-4.2.7.1/lib/active_record/connection_adapters/connection_specification.rb
+        env = Rails.env
+        original_config = ActiveRecord::Base.configurations
+        env_config = original_config[env].merge('pool' => pool_size)
+        config = original_config.merge(env => env_config)
+
+        spec =
+          ActiveRecord::
+            ConnectionAdapters::
+            ConnectionSpecification::Resolver.new(config).spec(env.to_sym)
+
+        ActiveRecord::ConnectionAdapters::ConnectionPool.new(spec)
+      end
+    end
+  end
+end
---
+++ spec/lib/gitlab/database/connection_pool_spec.rb
@@ -0,0 +1,94 @@
+require 'spec_helper'
+
+describe Gitlab::Database::ConnectionPool, lib: true do
+  let(:pool_size) { 1 }
+  let(:subject) { described_class.new(pool_size) }
+
+  after do
+    subject.close
+  end
+
+  describe '.with_pool' do
+    let(:pool_size) { 123 }
+
+    it 'creates and passes a pool and join and close it after done' do
+      used_pool = described_class.with_pool(pool_size) do |pool|
+        expect(pool).to be_kind_of(described_class)
+        expect(pool).to receive(:join).and_call_original
+        expect(pool.pool_size).to eq(pool_size)
+
+        pool.execute_async('SELECT 1;')
+
+        pool
+      end
+
+      expect(used_pool).to be_closed
+    end
+
+    it 'tries to join and close the pool even if there is an error' do
+      error = Class.new(RuntimeError)
+
+      begin
+        described_class.with_pool(pool_size) do |pool|
+          expect(pool).to receive(:join).and_call_original
+          expect(pool).to receive(:close).and_call_original
+
+          pool.execute_async('SELECT 1;')
+
+          raise error.new('boom')
+        end
+      rescue error
+      end
+    end
+  end
+
+  describe '#pool_size' do
+    let(:pool_size) { 123 }
+
+    it 'returns correct pool size' do
+      expect(subject.pool_size).to eq(pool_size)
+    end
+  end
+
+  describe '#execute_async' do
+    it 'runs the right query' do
+      subject.execute_async('SELECT 1+2 AS value;')
+
+      expect(subject.join.map(&:to_a)).to eq([[{ 'value' => '3' }]])
+    end
+  end
+
+  describe '#join' do
+    before do
+      2.times.map do |n|
+        subject.execute_async("SELECT #{n} AS value;")
+      end
+    end
+
+    it 'joins the threads and give respective values, and clear workers' do
+      result = subject.join
+
+      expected = 2.times.map do |n|
+        [{ 'value' => n.to_s }]
+      end
+
+      expect(result.map(&:to_a)).to eq(expected)
+      expect(subject.workers).to be_empty
+    end
+  end
+
+  describe '#close and #closed?' do
+    before do
+      subject.execute_async('SELECT 1;')
+      subject.join
+    end
+
+    it 'disconnects the connection pool after closed' do
+      expect(subject).not_to be_closed
+
+      subject.close
+
+      expect(subject).to be_closed
+    end
+  end
+end
