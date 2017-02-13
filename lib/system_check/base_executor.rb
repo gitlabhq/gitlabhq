f182ea4ea5fe76349a584da12e5d4a9f681a8401
Gabriel Mazetto <brodock@gmail.com>
2017-02-13 09:45:39 +0100

    Some code-style fixes and documentation

--- lib/system_check/base_executor.rb
+++ lib/system_check/base_executor.rb
@@ -1,8 +1,11 @@
 module SystemCheck
+  # @attr_reader [Array<BaseCheck>] checks classes of corresponding checks to be executed in the same order
+  # @attr_reader [String] component name of the component relative to the checks being executed
   class BaseExecutor
     attr_reader :checks
     attr_reader :component
 
+    # @param [String] component name of the component relative to the checks being executed
     def initialize(component)
       raise ArgumentError unless component.is_a? String
 
@@ -10,6 +13,9 @@ module SystemCheck
       @checks = Set.new
     end
 
+    # Add a check to be executed
+    #
+    # @param [BaseCheck] check class
     def <<(check)
       raise ArgumentError unless check.is_a? BaseCheck
       @checks << check
