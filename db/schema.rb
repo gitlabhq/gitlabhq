178b6014f856dbca1653961a1f8341d7d1d38d2f
Z.J. van de Weg <git@zjvandeweg.nl>
2017-02-14 15:15:41 +0100

    Remove deprecated CI service

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170210075922) do
+ActiveRecord::Schema.define(version: 20170214111112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
