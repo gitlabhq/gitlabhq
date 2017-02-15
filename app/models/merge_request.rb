4d633c14c83999d373f098ad7f81bbce106dd2f7
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2017-02-15 14:49:07 +0200

    Make sure source_project is set when call MergeRequest#can_remove_source_branch? method
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- app/models/merge_request.rb
+++ app/models/merge_request.rb
@@ -442,7 +442,8 @@ class MergeRequest < ActiveRecord::Base
   end
 
   def can_remove_source_branch?(current_user)
-    !source_project.protected_branch?(source_branch) &&
+    source_project &&
+      !source_project.protected_branch?(source_branch) &&
       !source_project.root_ref?(source_branch) &&
       Ability.allowed?(current_user, :push_code, source_project) &&
       diff_head_commit == source_branch_head
