21aafcfc899d34f296ddd1e3380970dbe01397f6
James Lopez <james@jameslopez.es>
2017-02-16 11:34:08 +0100

    fix tmpdir

--- spec/lib/gitlab/import_export/repo_restorer_spec.rb
+++ spec/lib/gitlab/import_export/repo_restorer_spec.rb
@@ -5,7 +5,7 @@ describe Gitlab::ImportExport::RepoRestorer, services: true do
     let(:user) { create(:user) }
     let!(:project_with_repo) { create(:project, :test_repo, name: 'test-repo-restorer', path: 'test-repo-restorer') }
     let!(:project) { create(:empty_project) }
-    let(:export_path) { "#{Dir::tmpdir}/project_tree_saver_spec" }
+    let(:export_path) { "#{Dir.tmpdir}/project_tree_saver_spec" }
     let(:shared) { Gitlab::ImportExport::Shared.new(relative_path: project.path_with_namespace) }
     let(:bundler) { Gitlab::ImportExport::RepoSaver.new(project: project_with_repo, shared: shared) }
     let(:bundle_path) { File.join(shared.export_path, Gitlab::ImportExport.project_bundle_filename) }
