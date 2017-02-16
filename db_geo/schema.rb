8f05852ccada49ee2acd3d2d5003166bb13031eb
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2017-02-16 02:56:05 -0200

    Fix rubocop offenses

--- db_geo/schema.rb
+++ db_geo/schema.rb
@@ -12,7 +12,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20170206203234) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,5 +21,4 @@ ActiveRecord::Schema.define(version: 20170206203234) do
     t.datetime "last_repository_successful_sync_at"
     t.datetime "created_at", null: false
   end
-
 end
