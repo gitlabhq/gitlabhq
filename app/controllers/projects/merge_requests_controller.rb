f0f7af50696035946075daf47cf3ff04117af6bf
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2017-02-16 13:52:07 +0100

    Reinstate new merge request pipelines hash in API

--- app/controllers/projects/merge_requests_controller.rb
+++ app/controllers/projects/merge_requests_controller.rb
@@ -245,9 +245,10 @@ class Projects::MergeRequestsController < Projects::ApplicationController
       format.json do
         define_pipelines_vars
 
-        render json: PipelineSerializer
+        render json: {
+          pipelines: PipelineSerializer
           .new(project: @project, user: @current_user)
-          .represent(@pipelines)
+          .represent(@pipelines) }
       end
     end
   end
