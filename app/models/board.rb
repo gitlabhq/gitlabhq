2956f5f5a14b29aa86708460ddba4f01c220649c
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2017-02-17 16:20:53 -0200

    Returns Milestone::Upcoming for boards with this milestone

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,12 +1,20 @@
 class Board < ActiveRecord::Base
   belongs_to :project
+  belongs_to :milestone
 
   has_many :lists, -> { order(:list_type, :position) }, dependent: :delete_all
-  belongs_to :milestone
 
   validates :name, :project, presence: true
 
   def done_list
     lists.merge(List.done).take
+  end
+
+  def milestone
+    if milestone_id === Milestone::Upcoming.id
+      Milestone::Upcoming
+    else
+      super
+    end
   end
 end
