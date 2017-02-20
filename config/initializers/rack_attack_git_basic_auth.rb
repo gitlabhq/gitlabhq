3e56e06985af8e1a0bd2b0be2f935fd22b14f1e8
Brian Neel <brian@gitlab.com>
2017-02-20 11:50:31 -0500

    safelist/blocklist is for the Beta gem. Restore whitelist/blacklist

--- config/initializers/rack_attack_git_basic_auth.rb
+++ config/initializers/rack_attack_git_basic_auth.rb
@@ -3,8 +3,8 @@ git_basic_auth_enabled = Gitlab.config.rack_attack.git_basic_auth['enabled']
 
 unless Rails.env.test? || !rack_attack_enabled || !git_basic_auth_enabled
   # Tell the Rack::Attack Rack middleware to maintain an IP blocklist. We will
-  # update the blocklist from GitLab::Auth.rate_limit
-  Rack::Attack.blocklist('Git HTTP Basic Auth') do |req|
+  # update the blacklist from GitLab::Auth.rate_limit
+  Rack::Attack.blacklist('Git HTTP Basic Auth') do |req|
     Rack::Attack::Allow2Ban.filter(req.ip, Gitlab.config.rack_attack.git_basic_auth) do
       # This block only gets run if the IP was not already banned.
       # Return false, meaning that we do not see anything wrong with the
