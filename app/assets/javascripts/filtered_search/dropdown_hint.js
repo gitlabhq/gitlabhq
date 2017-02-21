6e7e9e80e0a89c2c295ccaa4b8469b5ed33acd27
Simon Knox <psimyn@gmail.com>
2017-02-21 16:32:08 +1100

    prevent filtering Issues by multiple milestones, authors, or assignees

--- app/assets/javascripts/filtered_search/dropdown_hint.js
+++ app/assets/javascripts/filtered_search/dropdown_hint.js
@@ -57,13 +57,15 @@ require('./filtered_search_dropdown');
       const dropdownData = [];
 
       [].forEach.call(this.input.closest('.filtered-search-input-container').querySelectorAll('.dropdown-menu'), (dropdownMenu) => {
-        const { icon, hint, tag } = dropdownMenu.dataset;
+        const { icon, hint, tag, type } = dropdownMenu.dataset;
         if (icon && hint && tag) {
-          dropdownData.push({
-            icon: `fa-${icon}`,
-            hint,
-            tag: `&lt;${tag}&gt;`,
-          });
+          dropdownData.push(
+            Object.assign({
+              icon: `fa-${icon}`,
+              hint,
+              tag: `&lt;${tag}&gt;`,
+            }, type && { type }),
+          );
         }
       });
 
