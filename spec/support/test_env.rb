c13d28c9af50e04989996a605060f15a3cf18e04
Sean McGivern <sean@gitlab.com>
2017-02-21 14:53:32 +0000

    Use sparse checkout for squashes
    For large repos, the checkout as part of creating a worktree can be very
    expensive. This can be so expensive that processes get 'stuck' on
    GitLab.com.
    
    To make this cheaper, configure sparse checkout for squash worktrees so
    that we only check out the files that were changed in the target
    branch (new files don't need to be included).
    
    Also add specs to cover the case where only new files are added, and the
    case where the diff isn't shown in the merge request because it's too
    large.

--- spec/support/test_env.rb
+++ spec/support/test_env.rb
@@ -38,7 +38,8 @@ module TestEnv
     'conflict-too-large'                 => '39fa04f',
     'deleted-image-test'                 => '6c17798',
     'wip'                                => 'b9238ee',
-    'csv'                                => '3dd0896'
+    'csv'                                => '3dd0896',
+    'squash-large-files'                 => '54cec52'
   }
 
   # gitlab-test-fork is a fork of gitlab-fork, but we don't necessarily
