4c622b71fd284058deee483bf0009f8179b792bc
Robert Speicher <rspeicher@gmail.com>
2017-02-22 14:25:06 -0500

    Add Upload model and UploadChecksumWorker worker

--- spec/support/carrierwave.rb
+++ spec/support/carrierwave.rb
@@ -1,7 +1,7 @@
-CarrierWave.root = 'tmp/tests/uploads'
+CarrierWave.root = File.expand_path('tmp/tests/uploads', Rails.root)
 
 RSpec.configure do |config|
   config.after(:each) do
-    FileUtils.rm_rf('tmp/tests/uploads')
+    FileUtils.rm_rf(CarrierWave.root)
   end
 end
