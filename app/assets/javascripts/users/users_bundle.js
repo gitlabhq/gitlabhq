08f08a34cfa4d0afe78f8c8afda16693bb96da45
Mike Greiling <mike@pixelcog.com>
2017-02-23 17:57:53 -0600

    remove require.context from users_bundle

--- app/assets/javascripts/users/users_bundle.js
+++ app/assets/javascripts/users/users_bundle.js
@@ -1,3 +1,1 @@
-// require everything else in this directory
-function requireAll(context) { return context.keys().map(context); }
-requireAll(require.context('.', false, /^\.\/(?!users_bundle).*\.(js|es6)$/));
+require('./calendar');
