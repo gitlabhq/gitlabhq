ed986806786d0c3fa5794409566e9e18a7916ed9
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2017-02-23 11:44:49 +0100

    Process skipped jobs in the pipeline when retrying it

--- app/services/ci/retry_pipeline_service.rb
+++ app/services/ci/retry_pipeline_service.rb
@@ -1,22 +1,21 @@
 module Ci
   class RetryPipelineService < ::BaseService
+    include Gitlab::OptimisticLocking
+
     def execute(pipeline)
       unless can?(current_user, :update_pipeline, pipeline)
         raise Gitlab::Access::AccessDeniedError
       end
 
-      pipeline.builds.failed_or_canceled.tap do |builds|
-        stage_idx = builds.order('stage_idx ASC')
-          .pluck('DISTINCT stage_idx').first
+      pipeline.builds.failed_or_canceled.find_each do |build|
+        next unless build.retryable?
 
-        pipeline.mark_as_processable_after_stage(stage_idx)
+        Ci::RetryBuildService.new(project, current_user)
+          .reprocess(build)
+      end
 
-        builds.find_each do |build|
-          next unless build.retryable?
-
-          Ci::RetryBuildService.new(project, current_user)
-            .reprocess(build)
-        end
+      pipeline.builds.skipped.find_each do |skipped|
+        retry_optimistic_lock(skipped) { |build| build.process }
       end
 
       MergeRequests::AddTodoWhenBuildFailsService
