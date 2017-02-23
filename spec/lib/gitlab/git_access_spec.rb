dacf222535060247a53ea220a26e6a1352ff4b0c
Sean McGivern <sean@gitlab.com>
2017-02-23 16:40:30 +0000

    Fit GitAccess specs
    These specs never ran due to incorrect indentation: the `context` blocks
    were inside the `before`.

--- spec/lib/gitlab/git_access_spec.rb
+++ spec/lib/gitlab/git_access_spec.rb
@@ -215,7 +215,9 @@ describe Gitlab::GitAccess, lib: true do
 
     def stub_git_hooks
       # Running the `pre-receive` hook is expensive, and not necessary for this test.
-      allow_any_instance_of(GitHooksService).to receive(:execute).and_yield(GitHooksService.new)
+      allow_any_instance_of(GitHooksService).to receive(:execute) do |service, &block|
+        block.call(service)
+      end
     end
 
     def merge_into_protected_branch
@@ -242,17 +244,17 @@ describe Gitlab::GitAccess, lib: true do
             else
               project.team << [user, role]
             end
+          end
 
-            permissions_matrix[role].each do |action, allowed|
-              context action do
-                subject { access.send(:check_push_access!, changes[action]) }
+          permissions_matrix[role].each do |action, allowed|
+            context action do
+              subject { access.send(:check_push_access!, changes[action]) }
 
-                it do
-                  if allowed
-                    expect { subject }.not_to raise_error
-                  else
-                    expect { subject }.to raise_error(Gitlab::GitAccess::UnauthorizedError)
-                  end
+              it do
+                if allowed
+                  expect { subject }.not_to raise_error
+                else
+                  expect { subject }.to raise_error(Gitlab::GitAccess::UnauthorizedError)
                 end
               end
             end
