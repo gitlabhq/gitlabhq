4269d6bccd44084267a4565f18be5e6499a62a13
Rémy Coutable <remy@rymai.me>
2017-02-23 22:39:48 +0000

    Merge branch '27989-disable-counting-tags' into 'master'
    Disable unused tags count cache for Projects, Builds and Runners
    
    Closes #27989
    
    See merge request !9467

---
+++ config/initializers/acts_as_taggable.rb
@@ -0,0 +1,5 @@
+ActsAsTaggableOn.strict_case_match = true
+
+# tags_counter enables caching count of tags which results in an update whenever a tag is added or removed
+# since the count is not used anywhere its better performance wise to disable this cache
+ActsAsTaggableOn.tags_counter = false
