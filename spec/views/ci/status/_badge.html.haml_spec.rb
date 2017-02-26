8d23b9647e0498d7b654e238fc93af7016218bcb
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2017-02-26 13:35:29 +0100

    Simplify view specs for commit status badge partial

--- spec/views/ci/status/_badge.html.haml_spec.rb
+++ spec/views/ci/status/_badge.html.haml_spec.rb
@@ -10,13 +10,13 @@ describe 'ci/status/_badge', :view do
       create(:ci_build, :success, pipeline: pipeline)
     end
 
-    context 'when status has details' do
+    context 'when user has ability to see details' do
       before do
         project.add_developer(user)
       end
 
       it 'has link to build details page' do
-        details_path =  namespace_project_build_path(
+        details_path = namespace_project_build_path(
           project.namespace, project, build)
 
         render_status(build)
@@ -25,7 +25,7 @@ describe 'ci/status/_badge', :view do
       end
     end
 
-    context 'when status does not have details' do
+    context 'when user do not have ability to see build details' do
       before do
         render_status(build)
       end
@@ -41,39 +41,44 @@ describe 'ci/status/_badge', :view do
   end
 
   context 'when rendering status for external job' do
-    before do
-      project.add_developer(use)
-
-      render_status(external_job)
-    end
-
-    context 'status has external target url' do
-      let(:external_job) do
-        create(:generic_commit_status, status: :running,
-                                       pipeline: pipeline,
-                                       target_url: 'http://gitlab.com')
+    context 'when user has ability to see commit status details' do
+      before do
+        project.add_developer(user)
       end
 
-      it 'contains valid commit status text' do
-        expect(rendered).to have_content 'running'
+      context 'status has external target url' do
+        before do
+          external_job = create(:generic_commit_status,
+                                status: :running,
+                                pipeline: pipeline,
+                                target_url: 'http://gitlab.com')
+
+          render_status(external_job)
+        end
+
+        it 'contains valid commit status text' do
+          expect(rendered).to have_content 'running'
+        end
+
+        it 'has link to external status page' do
+          expect(rendered).to have_link 'running', href: 'http://gitlab.com'
+        end
       end
 
-      it 'has link to external status page' do
-        expect(rendered).to have_link 'running', href: 'http://gitlab.com'
-      end
-    end
+      context 'status do not have external target url' do
+        before do
+          external_job = create(:generic_commit_status, status: :canceled)
 
-    context 'status do not have external target url' do
-      let(:external_job) do
-        create(:generic_commit_status, status: :canceled)
-      end
+          render_status(external_job)
+        end
 
-      it 'contains valid commit status text' do
-        expect(rendered).to have_content 'canceled'
-      end
+        it 'contains valid commit status text' do
+          expect(rendered).to have_content 'canceled'
+        end
 
-      it 'has link to external status page' do
-        expect(rendered).not_to have_link 'canceled'
+        it 'has link to external status page' do
+          expect(rendered).not_to have_link 'canceled'
+        end
       end
     end
   end
