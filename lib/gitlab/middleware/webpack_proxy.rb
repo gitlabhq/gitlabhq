879ef798eeca8252aa7c508c19437b2b8a142e11
Douwe Maan <douwe@gitlab.com>
2017-02-27 18:17:02 +0000

    Revert "Merge branch 'fix-webpack-dev-server-proxy' into 'master'"
    This reverts merge request !9543

--- lib/gitlab/middleware/webpack_proxy.rb
+++ lib/gitlab/middleware/webpack_proxy.rb
@@ -8,16 +8,16 @@ module Gitlab
         @proxy_host = opts.fetch(:proxy_host, 'localhost')
         @proxy_port = opts.fetch(:proxy_port, 3808)
         @proxy_path = opts[:proxy_path] if opts[:proxy_path]
-
-        super(app, backend: "http://#{proxy_host}:#{proxy_port}", **opts)
+        super(app, opts)
       end
 
       def perform_request(env)
-        if @proxy_path && env['PATH_INFO'].start_with?("/#{@proxy_path}")
-          super(env)
-        else
-          @app.call(env)
+        unless @proxy_path && env['PATH_INFO'].start_with?("/#{@proxy_path}")
+          return @app.call(env)
         end
+
+        env['HTTP_HOST'] = "#{@proxy_host}:#{@proxy_port}"
+        super(env)
       end
     end
   end
