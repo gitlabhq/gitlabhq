519bac658ae7e92e8454d7ed6445cf286c12fcb6
Timothy Andrew <mail@timothyandrew.net>
2017-02-28 14:23:58 +0530

    Fix time tracking endpoints for API v4
    - Use issue/merge_request IID instead of ID
    - Duplicate the original `TimeTrackingEndpoints` concern (+ specs) for V3, since
      this is a breaking change.

--- spec/requests/api/v3/issues_spec.rb
+++ spec/requests/api/v3/issues_spec.rb
@@ -1288,6 +1288,6 @@ describe API::V3::Issues, api: true  do
   describe 'time tracking endpoints' do
     let(:issuable) { issue }
 
-    include_examples 'time tracking endpoints', 'issue'
+    include_examples 'V3 time tracking endpoints', 'issue'
   end
 end
