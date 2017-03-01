e2b45367a966edc3621d87403d08e67a6a828014
Robert Speicher <robert@gitlab.com>
2017-03-01 18:19:22 +0000

    Merge branch '28609-fix-redirect-to-home-page-url' into 'master'
    Fix the redirect to custom home page URL and move it to RootController
    
    Closes #28609
    
    See merge request !9518

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -72,14 +72,6 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def authenticate_user!(*args)
-    if redirect_to_home_page_url?
-      redirect_to current_application_settings.home_page_url and return
-    end
-
-    super(*args)
-  end
-
   def log_exception(exception)
     application_trace = ActionDispatch::ExceptionWrapper.new(env, exception).application_trace
     application_trace.map!{ |t| "  #{t}\n" }
@@ -285,19 +277,6 @@ class ApplicationController < ActionController::Base
 
   def skip_two_factor?
     session[:skip_tfa] && session[:skip_tfa] > Time.current
-  end
-
-  def redirect_to_home_page_url?
-    # If user is not signed-in and tries to access root_path - redirect him to landing page
-    # Don't redirect to the default URL to prevent endless redirections
-    return false unless current_application_settings.home_page_url.present?
-
-    home_page_url = current_application_settings.home_page_url.chomp('/')
-    root_urls = [Gitlab.config.gitlab['url'].chomp('/'), root_url.chomp('/')]
-
-    return false if root_urls.include?(home_page_url)
-
-    current_user.nil? && root_path == request.path
   end
 
   # U2F (universal 2nd factor) devices need a unique identifier for the application
--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -8,7 +8,9 @@
 # `DashboardController#show`, which is the default.
 class RootController < Dashboard::ProjectsController
   skip_before_action :authenticate_user!, only: [:index]
-  before_action :redirect_to_custom_dashboard, only: [:index]
+
+  before_action :redirect_unlogged_user, if: -> { current_user.nil? }
+  before_action :redirect_logged_user, if: -> { current_user.present? }
 
   def index
     super
@@ -16,23 +18,38 @@ class RootController < Dashboard::ProjectsController
 
   private
 
-  def redirect_to_custom_dashboard
-    return redirect_to new_user_session_path unless current_user
+  def redirect_unlogged_user
+    if redirect_to_home_page_url?
+      redirect_to(current_application_settings.home_page_url)
+    else
+      redirect_to(new_user_session_path)
+    end
+  end
 
+  def redirect_logged_user
     case current_user.dashboard
     when 'stars'
       flash.keep
-      redirect_to starred_dashboard_projects_path
+      redirect_to(starred_dashboard_projects_path)
     when 'project_activity'
-      redirect_to activity_dashboard_path
+      redirect_to(activity_dashboard_path)
     when 'starred_project_activity'
-      redirect_to activity_dashboard_path(filter: 'starred')
+      redirect_to(activity_dashboard_path(filter: 'starred'))
     when 'groups'
-      redirect_to dashboard_groups_path
+      redirect_to(dashboard_groups_path)
     when 'todos'
-      redirect_to dashboard_todos_path
-    else
-      return
+      redirect_to(dashboard_todos_path)
     end
+  end
+
+  def redirect_to_home_page_url?
+    # If user is not signed-in and tries to access root_path - redirect him to landing page
+    # Don't redirect to the default URL to prevent endless redirections
+    return false unless current_application_settings.home_page_url.present?
+
+    home_page_url = current_application_settings.home_page_url.chomp('/')
+    root_urls = [Gitlab.config.gitlab['url'].chomp('/'), root_url.chomp('/')]
+
+    root_urls.exclude?(home_page_url)
   end
 end
