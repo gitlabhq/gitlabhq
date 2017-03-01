40be92e74c60262b5ed92064fe42a27eac88f1c3
Bob Van Landuyt <bob@vanlanduyt.co>
2017-03-01 10:15:58 +0100

    Make API v3 compatible with the changes in `Issues::CreateService`

--- lib/api/v3/issues.rb
+++ lib/api/v3/issues.rb
@@ -140,12 +140,6 @@ module API
 
           issue_params = declared_params(include_missing: false)
 
-          if merge_request_iid = params[:merge_request_for_resolving_discussions]
-            issue_params[:merge_request_for_resolving_discussions] = MergeRequestsFinder.new(current_user, project_id: user_project.id).
-              execute.
-              find_by(iid: merge_request_iid)
-          end
-
           issue = ::Issues::CreateService.new(user_project,
                                               current_user,
                                               issue_params.merge(request: request, api: true)).execute
