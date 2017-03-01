47004beeb44b9024eea3f0053d0763fa3e0a8d24
Alexis Reigel <mail@koffeinfrei.org>
2017-03-01 13:06:35 +0100

    add Ci::RunnerGroup join model

---
+++ db/migrate/20170301101006_add_ci_runner_groups.rb
@@ -0,0 +1,27 @@
+class AddCiRunnerGroups < ActiveRecord::Migration
+  include Gitlab::Database::MigrationHelpers
+
+  DOWNTIME = false
+
+  disable_ddl_transaction!
+
+  def up
+    create_table :ci_runner_groups do |t|
+      t.integer :runner_id
+      t.integer :group_id
+
+      t.timestamps_with_timezone null: false
+    end
+
+    add_concurrent_index :ci_runner_groups, :runner_id
+    add_concurrent_foreign_key :ci_runner_groups, :ci_runners, column: :runner_id, on_delete: :cascade
+
+    add_concurrent_index :ci_runner_groups, :group_id
+    add_concurrent_index :ci_runner_groups, [:runner_id, :group_id], unique: true
+    add_concurrent_foreign_key :ci_runner_groups, :namespaces, column: :group_id, on_delete: :cascade
+  end
+
+  def down
+    drop_table :ci_runner_groups
+  end
+end
