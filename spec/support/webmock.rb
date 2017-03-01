b76d51587f367b065ee5d9e166d5d155624a6349
Nick Thomas <nick@gitlab.com>
2017-03-01 21:55:44 +0000

    Allow custom ELASTIC_URL settings in tests

--- spec/support/webmock.rb
+++ spec/support/webmock.rb
@@ -1,7 +1,12 @@
 require 'webmock'
 require 'webmock/rspec'
 
-WebMock.disable_net_connect!(
-  allow_localhost: true,
-  allow: ['elasticsearch', 'registry.gitlab.com-gitlab-org-test-elastic-image']
-)
+allowed = %w[elasticsearch registry.gitlab.com-gitlab-org-test-elastic-image]
+
+if ENV.has_key?('ELASTIC_URL')
+  url = URI.parse(ENV['ELASTIC_URL'])
+  allowed << url.host
+  allowed.uniq!
+end
+
+WebMock.disable_net_connect!(allow_localhost: true, allow: allowed)
