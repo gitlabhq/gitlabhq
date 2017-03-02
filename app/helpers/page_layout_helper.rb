f3b5d68f7ec2e4842a9ee93e0ad66cd8492bfb7d
dimitrieh <dimitriehoekstra@gmail.com>
2017-03-02 21:26:20 +0100

    Adds a EE specific dev favicon

--- app/helpers/page_layout_helper.rb
+++ app/helpers/page_layout_helper.rb
@@ -35,7 +35,7 @@ module PageLayoutHelper
   end
 
   def favicon
-    Rails.env.development? ? 'favicon-blue.ico' : 'favicon.ico'
+    Rails.env.development? ? 'favicon-green.ico' : 'favicon.ico'
   end
 
   def page_image
