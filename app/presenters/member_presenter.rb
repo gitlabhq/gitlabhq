8fec0ece8d8e613d784e6ca9c8c0cde31a7567dd
TM Lee <tm89lee@gmail.com>
2017-03-02 14:01:02 +0800

    [EE] Refactor member view by using presenter
    - Create MemberPresenter alongside with GroupMemberPresenter and ProjectMemberPresenter
    - Make Member model Presentable
    - Move action_member_permission from MembersHelper into the MemberPresenter
    - Added rspec using double, separate specs for GroupMemberPresenter and ProjectMemberPresenter
    
    Fixes #28004.
    
    Signed-off-by: Rémy Coutable <remy@rymai.me>

---
+++ app/presenters/member_presenter.rb
@@ -0,0 +1,42 @@
+class MemberPresenter < Gitlab::View::Presenter::Delegated
+  include Gitlab::Allowable
+
+  prepend EE::MemberPresenter
+
+  presents :member
+
+  def can_resend_invite?
+    invite? &&
+      can?(current_user, admin_member_permission, source)
+  end
+
+  def can_update?
+    can?(current_user, update_member_permission, member)
+  end
+
+  def cannot_update?
+    !can_update?
+  end
+
+  def can_remove?
+    can?(current_user, destroy_member_permission, member)
+  end
+
+  def can_approve?
+    request? && can_update?
+  end
+
+  private
+
+  def admin_member_permission
+    raise NotImplementedError
+  end
+
+  def update_member_permission
+    raise NotImplementedError
+  end
+
+  def destroy_member_permission
+    raise NotImplementedError
+  end
+end
