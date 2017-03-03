545397c6cb88a5d76810c102c39c2e324079a049
Mike Greiling <mike@pixelcog.com>
2017-03-03 17:41:35 -0600

    remove manually vendored raphael library in favor of node module

--- config/application.rb
+++ config/application.rb
@@ -100,7 +100,6 @@ module Gitlab
     config.assets.precompile << "katex.js"
     config.assets.precompile << "xterm/xterm.css"
     config.assets.precompile << "lib/ace.js"
-    config.assets.precompile << "lib/raphael.js"
     config.assets.precompile << "u2f.js"
     config.assets.precompile << "vendor/assets/fonts/*"
 
