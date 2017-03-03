faeadf9903ce31266c32e35eec14e748981dcef2
Mike Greiling <mike@pixelcog.com>
2017-03-03 14:51:51 -0600

    Merge 'commons-chunk-plugin' into 'commons-chunk-plugin-ee'

--- app/assets/javascripts/commons/jquery.js
+++ app/assets/javascripts/commons/jquery.js
@@ -3,7 +3,6 @@ import 'jquery';
 // common jQuery plugins
 import 'jquery-ujs';
 import 'vendor/jquery.endless-scroll';
-import 'vendor/jquery.highlight';
 import 'vendor/jquery.caret';
 import 'vendor/jquery.atwho';
 import 'vendor/jquery.scrollTo';
