3b35d1f8c153fb31af66049ba5461f9b39c66397
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2017-03-06 18:24:55 +0100

    Migrate legacy actions in post deployment migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170306090835) do
+ActiveRecord::Schema.define(version: 20170306170512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
