43aca62028c04bee858628025bf2c1dac198ebe2
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2017-03-06 19:31:23 -0300

    Does not read cached value of Gitlab::Geo.current_node.enabled?

--- app/workers/geo_backfill_worker.rb
+++ app/workers/geo_backfill_worker.rb
@@ -77,6 +77,6 @@ class GeoBackfillWorker
     # No caching of the enabled! If we cache it and an admin disables
     # this node, an active GeoBackfillWorker would keep going for up
     # to max run time after the node was disabled.
-    Gitlab::Geo.current_node.enabled?
+    Gitlab::Geo.current_node.reload.enabled?
   end
 end
