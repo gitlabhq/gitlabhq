3788f5face7660c43374d85a4fdbeca04e7b8d68
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2017-03-06 10:50:35 +0100

    Add migration for manual actions with skipped status

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170217151947) do
+ActiveRecord::Schema.define(version: 20170306090835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
