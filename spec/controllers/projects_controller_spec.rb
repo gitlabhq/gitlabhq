1565d7f6e602258c587bfa0e5606f671b14b7e3c
Annabel Dunstone Gray <annabel.dunstone@gmail.com>
2017-03-06 14:49:31 -0600

    Remove readme-only project view preference

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -158,14 +158,6 @@ describe ProjectsController do
         expect(response).to render_template('_activity')
       end
 
-      it "renders the readme view" do
-        allow(controller).to receive(:current_user).and_return(user)
-        allow(user).to receive(:project_view).and_return('readme')
-
-        get :show, namespace_id: public_project.namespace, id: public_project
-        expect(response).to render_template('_readme')
-      end
-
       it "renders the files view" do
         allow(controller).to receive(:current_user).and_return(user)
         allow(user).to receive(:project_view).and_return('files')
