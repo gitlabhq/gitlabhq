d338e3f7870ffa8335394cbdeddf12d90c5fa6fe
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2017-03-07 20:48:49 -0300

    Add belongs_to :project to Geo::ProjectRegistry

--- app/models/geo/project_registry.rb
+++ app/models/geo/project_registry.rb
@@ -1,5 +1,7 @@
 class Geo::ProjectRegistry < Geo::BaseRegistry
-  validates :project_id, presence: true
+  belongs_to :project
+
+  validates :project, presence: true
 
   scope :failed, -> { where.not(last_repository_synced_at: nil).where(last_repository_successful_sync_at: nil) }
   scope :synced, -> { where.not(last_repository_synced_at: nil, last_repository_successful_sync_at: nil) }
