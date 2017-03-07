b900f3f0b1ca178a4e5fadf52464e0d6cf01b486
Jose Ivan Vargas <jvargas@gitlab.com>
2017-03-06 19:08:27 -0600

    Added test case for the available project keys

--- app/controllers/projects/protected_branches_controller.rb
+++ app/controllers/projects/protected_branches_controller.rb
@@ -58,27 +58,7 @@ class Projects::ProtectedBranchesController < Projects::ApplicationController
                                              push_access_levels_attributes: [:access_level, :id, :user_id, :_destroy, :group_id])
   end
 
-
   def load_protected_branches
     @protected_branches = @project.protected_branches.order(:name).page(params[:page])
-  end
-
-  def access_levels_options
-    {
-      push_access_levels: {
-        roles: ProtectedBranch::PushAccessLevel.human_access_levels.map { |id, text| { id: id, text: text, before_divider: true } },
-      },
-      merge_access_levels: {
-        roles: ProtectedBranch::MergeAccessLevel.human_access_levels.map { |id, text| { id: id, text: text, before_divider: true } },
-      },
-      selected_merge_access_levels: @protected_branch.merge_access_levels.map { |access_level| access_level.user_id || access_level.access_level },
-      selected_push_access_levels: @protected_branch.push_access_levels.map { |access_level| access_level.user_id || access_level.access_level }
-    }
-  end
-
-  def load_gon_index
-    params = { open_branches: @project.open_branches.map { |br| { text: br.name, id: br.name, title: br.name } } }
-    params.merge!(current_project_id: @project.id) if @project
-    gon.push(params.merge(access_levels_options))
   end
 end
