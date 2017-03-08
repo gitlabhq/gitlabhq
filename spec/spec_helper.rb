5f7592d53805b18fbbc2a117ab8b4d953b13dbb1
Rémy Coutable <remy@rymai.me>
2017-03-09 00:41:02 +0100

    Implement `json_response` as a `let` variable
    This is not a good idea to memoize `json_response` using an instance
    variable because `rspec-retry` doesn't clear instance variables on
    retries, only `let` variables.
    
    This will avoid issues where retries would fail on a different line that
    the original failure, blurrying what's the real failure.
    
    Also, automatically add api: true to specs under
    /spec/requests/(ci/)?api/, and include JsonHelpers in controller, request
    and API specs.
    
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -43,8 +43,14 @@ RSpec.configure do |config|
   config.include ActiveSupport::Testing::TimeHelpers
   config.include StubGitlabCalls
   config.include StubGitlabData
+  config.include ApiHelpers, :api
 
   config.infer_spec_type_from_file_location!
+
+  config.define_derived_metadata(file_path: %r{/spec/requests/(ci/)?api/}) do |metadata|
+    metadata[:api] = true
+  end
+
   config.raise_errors_for_deprecations!
 
   config.before(:suite) do
