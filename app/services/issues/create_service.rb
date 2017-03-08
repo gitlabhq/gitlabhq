843092b631ec667aff3cb3617c40a6eef811c097
Bob Van Landuyt <bob@gitlab.com>
2017-03-08 01:20:05 +0100

    Don't filter params and merge them back in later
    By building the issue before filtering

--- app/services/issues/create_service.rb
+++ app/services/issues/create_service.rb
@@ -4,15 +4,10 @@ module Issues
     include ResolveDiscussions
 
     def execute
+      @issue = BuildService.new(project, current_user, params).execute
+
       filter_spam_check_params
       filter_resolve_discussion_params
-
-      issue_attributes = params.merge(
-        merge_request_for_resolving_discussions: merge_request_for_resolving_discussions_iid,
-        discussion_to_resolve: discussion_to_resolve_id
-      )
-
-      @issue = BuildService.new(project, current_user, issue_attributes).execute
 
       create(@issue)
     end
