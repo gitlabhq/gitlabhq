94ab57742b76db0ca059d085462e28ab1d9b91e1
Fatih Acet <acetfatih@gmail.com>
2017-03-10 17:35:00 +0300

    MRWidget: Slightly refactor SVG icon imports to reuse.

--- app/assets/javascripts/vue_shared/components/pipeline_status_icon.js
+++ app/assets/javascripts/vue_shared/components/pipeline_status_icon.js
@@ -1,24 +1,4 @@
-import canceledSvg from 'icons/_icon_status_canceled.svg';
-import createdSvg from 'icons/_icon_status_created.svg';
-import failedSvg from 'icons/_icon_status_failed.svg';
-import manualSvg from 'icons/_icon_status_manual.svg';
-import pendingSvg from 'icons/_icon_status_pending.svg';
-import runningSvg from 'icons/_icon_status_running.svg';
-import skippedSvg from 'icons/_icon_status_skipped.svg';
-import successSvg from 'icons/_icon_status_success.svg';
-import warningSvg from 'icons/_icon_status_warning.svg';
-
-const svgsDictionary = {
-  icon_status_canceled: canceledSvg,
-  icon_status_created: createdSvg,
-  icon_status_failed: failedSvg,
-  icon_status_manual: manualSvg,
-  icon_status_pending: pendingSvg,
-  icon_status_running: runningSvg,
-  icon_status_skipped: skippedSvg,
-  icon_status_success: successSvg,
-  icon_status_warning: warningSvg,
-};
+import { statusClassToSvgMap } from '../pipeline_svg_icons';
 
 export default {
   name: 'PipelineStatusIcon',
@@ -27,7 +7,7 @@ export default {
   },
   computed: {
     svg() {
-      return svgsDictionary[this.pipelineStatus.icon];
+      return statusClassToSvgMap[this.pipelineStatus.icon];
     },
     statusClass() {
       return `ci-status-icon-${this.pipelineStatus.label}`;
@@ -41,4 +21,3 @@ export default {
     </div>
   `,
 };
-
