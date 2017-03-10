72d8bb15d33bb222dc417f8a97f9e20c50cb5e62
Regis <boudinot.regis@yahoo.com>
2017-03-09 17:40:27 -0700

    no need for document listeners, no need to call removeAll() prior to remove events

--- app/assets/javascripts/vue_realtime_listener/index.js
+++ app/assets/javascripts/vue_realtime_listener/index.js
@@ -1,16 +1,13 @@
 const VueRealtimeListener = (removeIntervals, startIntervals) => {
   const removeAll = () => {
-    removeIntervals();
     window.removeEventListener('beforeunload', removeIntervals);
     window.removeEventListener('focus', startIntervals);
     window.removeEventListener('blur', removeIntervals);
-    document.removeEventListener('beforeunload', removeAll);
   };
 
   window.addEventListener('beforeunload', removeIntervals);
   window.addEventListener('focus', startIntervals);
   window.addEventListener('blur', removeIntervals);
-  document.addEventListener('beforeunload', removeAll);
 
   // add removeAll methods to stack
   const stack = VueRealtimeListener.reset;
