027341f52ce3f0675e292d61a3cb2a6710ac5753
Mike Greiling <mike@pixelcog.com>
2017-03-11 16:28:11 -0600

    refactor blob_file_dropzone.js

--- app/assets/javascripts/blob/blob_file_dropzone.js
+++ app/assets/javascripts/blob/blob_file_dropzone.js
@@ -1,12 +1,12 @@
-/* eslint-disable func-names, space-before-function-paren, wrap-iife, one-var, no-var, one-var-declaration-per-line, camelcase, object-shorthand, quotes, comma-dangle, prefer-arrow-callback, no-unused-vars, prefer-template, no-useless-escape, no-alert, max-len */
+/* eslint-disable func-names, object-shorthand, prefer-arrow-callback */
 /* global Dropzone */
 
-window.BlobFileDropzone = (function() {
-  function BlobFileDropzone(form, method) {
-    var dropzone, form_dropzone, submitButton;
-    form_dropzone = form.find('.dropzone');
+export default class BlobFileDropzone {
+  constructor(form, method) {
+    const formDropzone = form.find('.dropzone');
     Dropzone.autoDiscover = false;
-    dropzone = form_dropzone.dropzone({
+
+    const dropzone = formDropzone.dropzone({
       autoDiscover: false,
       autoProcessQueue: false,
       url: form.attr('action'),
@@ -15,50 +15,51 @@ window.BlobFileDropzone = (function() {
       method: method,
       clickable: true,
       uploadMultiple: false,
-      paramName: "file",
+      paramName: 'file',
       maxFilesize: gon.max_file_size || 10,
       parallelUploads: 1,
       maxFiles: 1,
       addRemoveLinks: true,
       previewsContainer: '.dropzone-previews',
       headers: {
-        "X-CSRF-Token": $("meta[name=\"csrf-token\"]").attr("content")
+        'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content'),
       },
-      init: function() {
-        this.on('addedfile', function(file) {
+      init: function () {
+        this.on('addedfile', function () {
           $('.dropzone-alerts').html('').hide();
         });
-        this.on('success', function(header, response) {
+        this.on('success', function (header, response) {
           window.location.href = response.filePath;
         });
-        this.on('maxfilesexceeded', function(file) {
+        this.on('maxfilesexceeded', function (file) {
           this.removeFile(file);
         });
-        return this.on('sending', function(file, xhr, formData) {
+        this.on('sending', function (file, xhr, formData) {
           formData.append('target_branch', form.find('input[name="target_branch"]').val());
           formData.append('create_merge_request', form.find('.js-create-merge-request').val());
           formData.append('commit_message', form.find('.js-commit-message').val());
         });
       },
       // Override behavior of adding error underneath preview
-      error: function(file, errorMessage) {
-        var stripped;
-        stripped = $("<div/>").html(errorMessage).text();
-        $('.dropzone-alerts').html('Error uploading file: \"' + stripped + '\"').show();
+      error: function (file, errorMessage) {
+        const stripped = $('<div/>').html(errorMessage).text();
+        $('.dropzone-alerts').html(`Error uploading file: "${stripped}"`).show();
         this.removeFile(file);
-      }
+      },
     });
-    submitButton = form.find('#submit-all')[0];
-    submitButton.addEventListener('click', function(e) {
+
+    const submitButton = form.find('#submit-all')[0];
+    submitButton.addEventListener('click', function (e) {
       e.preventDefault();
       e.stopPropagation();
       if (dropzone[0].dropzone.getQueuedFiles().length === 0) {
-        alert("Please select a file");
+        // eslint-disable-next-line no-alert
+        alert('Please select a file');
       }
       dropzone[0].dropzone.processQueue();
       return false;
     });
   }
+}
 
-  return BlobFileDropzone;
-})();
+window.BlobFileDropzone = BlobFileDropzone;
