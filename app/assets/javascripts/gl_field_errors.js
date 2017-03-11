8796d17a2670888d8fb975e0b327ecaa362c6f52
Mike Greiling <mike@pixelcog.com>
2017-03-11 01:30:44 -0600

    remove IIFEs in preparation for ES module refactor

--- app/assets/javascripts/gl_field_errors.js
+++ app/assets/javascripts/gl_field_errors.js
@@ -2,47 +2,46 @@
 
 require('./gl_field_error');
 
-((global) => {
-  const customValidationFlag = 'gl-field-error-ignore';
+const customValidationFlag = 'gl-field-error-ignore';
 
-  class GlFieldErrors {
-    constructor(form) {
-      this.form = $(form);
-      this.state = {
-        inputs: [],
-        valid: false
-      };
-      this.initValidators();
-    }
+class GlFieldErrors {
+  constructor(form) {
+    this.form = $(form);
+    this.state = {
+      inputs: [],
+      valid: false
+    };
+    this.initValidators();
+  }
 
-    initValidators () {
-      // register selectors here as needed
-      const validateSelectors = [':text', ':password', '[type=email]']
-        .map((selector) => `input${selector}`).join(',');
+  initValidators () {
+    // register selectors here as needed
+    const validateSelectors = [':text', ':password', '[type=email]']
+      .map((selector) => `input${selector}`).join(',');
 
-      this.state.inputs = this.form.find(validateSelectors).toArray()
-        .filter((input) => !input.classList.contains(customValidationFlag))
-        .map((input) => new global.GlFieldError({ input, formErrors: this }));
+    this.state.inputs = this.form.find(validateSelectors).toArray()
+      .filter((input) => !input.classList.contains(customValidationFlag))
+      .map((input) => new window.gl.GlFieldError({ input, formErrors: this }));
 
-      this.form.on('submit', this.catchInvalidFormSubmit);
-    }
+    this.form.on('submit', this.catchInvalidFormSubmit);
+  }
 
-    /* Neccessary to prevent intercept and override invalid form submit
-     * because Safari & iOS quietly allow form submission when form is invalid
-     * and prevents disabling of invalid submit button by application.js */
+  /* Neccessary to prevent intercept and override invalid form submit
+   * because Safari & iOS quietly allow form submission when form is invalid
+   * and prevents disabling of invalid submit button by application.js */
 
-    catchInvalidFormSubmit (event) {
-      if (!event.currentTarget.checkValidity()) {
-        event.preventDefault();
-        event.stopPropagation();
-      }
-    }
-
-    focusOnFirstInvalid () {
-      const firstInvalid = this.state.inputs.filter((input) => !input.inputDomElement.validity.valid)[0];
-      firstInvalid.inputElement.focus();
+  catchInvalidFormSubmit (event) {
+    if (!event.currentTarget.checkValidity()) {
+      event.preventDefault();
+      event.stopPropagation();
     }
   }
 
-  global.GlFieldErrors = GlFieldErrors;
-})(window.gl || (window.gl = {}));
+  focusOnFirstInvalid () {
+    const firstInvalid = this.state.inputs.filter((input) => !input.inputDomElement.validity.valid)[0];
+    firstInvalid.inputElement.focus();
+  }
+}
+
+window.gl = window.gl || {};
+window.gl.GlFieldErrors = GlFieldErrors;
