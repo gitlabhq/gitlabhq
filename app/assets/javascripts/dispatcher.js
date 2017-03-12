d0beb755f9ee8a744be087b22d51e8fe9ea98586
Douglas Lovell <doug@wbreeze.com>
2017-03-12 12:22:00 -0300

    Add a name field to the group edit form
    Enables user specification of group name vs. name inferred from group path.
    
    Cause new group form to copy name from path
    
    Adds some new page-specific javascript that copies entry from the
    group path field to the group name field when the group name field
    is initially empty.
    
    Remove duplicate group name entry field on group edit form
    
    This corrects the duplicated name entry field and tests that the
    JavaScript does not update the group name field if the user
    edits the group path.  (Editing the group path is not recommended
    in any case, but it is possible.)
    
    Address eslint errors in group.js
    
    Enable group name copy with dispatch and explore group creation
    
    The dispatch and explore group creation forms require the group.js
    asset, and their tests now require testing against poltergeist
    
    Update workflow new group instruction
    
    Update the gitlab basics group creation document
    
    Add a change log entry
    
    Remove unused variable for eslint

--- app/assets/javascripts/dispatcher.js
+++ app/assets/javascripts/dispatcher.js
@@ -37,6 +37,7 @@
 import Issue from './issue';
 
 import BindInOut from './behaviors/bind_in_out';
+import Group from './group';
 import GroupName from './group_name';
 import GroupsList from './groups_list';
 import ProjectsList from './projects_list';
@@ -271,8 +272,9 @@ const ShortcutsBlob = require('./shortcuts_blob');
         case 'groups:create':
         case 'admin:groups:create':
           BindInOut.initAll();
-        case 'groups:new':
-        case 'admin:groups:new':
+          new Group();
+          new GroupAvatar();
+          break;
         case 'groups:edit':
         case 'admin:groups:edit':
           new GroupAvatar();
