c0873f946c27da9696d026860d7d2f1e768c8ebc
Jacob Schatz <jschatz@gitlab.com>
2017-03-15 21:21:48 +0000

    Merge branch 'fix-links-target-blank' into 'security'
    Adds rel="noopener noreferrer" to all links with target="_blank"
    
    See merge request !2071

--- app/assets/javascripts/environments/components/environment_external_url.js
+++ app/assets/javascripts/environments/components/environment_external_url.js
@@ -12,7 +12,7 @@ module.exports = Vue.component('external-url-component', {
   },
 
   template: `
-    <a class="btn external_url" :href="externalUrl" target="_blank">
+    <a class="btn external_url" :href="externalUrl" target="_blank" rel="noopener noreferrer">
       <i class="fa fa-external-link"></i>
     </a>
   `,
--- lib/banzai/filter/image_link_filter.rb
+++ lib/banzai/filter/image_link_filter.rb
@@ -2,7 +2,6 @@ module Banzai
   module Filter
     # HTML filter that wraps links around inline images.
     class ImageLinkFilter < HTML::Pipeline::Filter
-      
       # Find every image that isn't already wrapped in an `a` tag, create
       # a new node (a link to the image source), copy the image as a child
       # of the anchor, and then replace the img with the link-wrapped version.
@@ -12,7 +11,8 @@ module Banzai
             'a',
             class: 'no-attachment-icon',
             href: img['src'],
-            target: '_blank'
+            target: '_blank',
+            rel: 'noopener noreferrer'
           )
 
           link.children = img.clone
