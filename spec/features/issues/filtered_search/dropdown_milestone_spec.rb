43223c24858c21d0d0a42d9fc05c9e678da72139
Douwe Maan <douwe@gitlab.com>
2017-03-15 18:32:29 +0000

    Merge branch 'better-priority-sorting' into 'master'
    Better priority sorting
    
    Closes #28754
    
    See merge request !9938

--- spec/features/issues/filtered_search/dropdown_milestone_spec.rb
+++ spec/features/issues/filtered_search/dropdown_milestone_spec.rb
@@ -196,6 +196,14 @@ describe 'Dropdown milestone', js: true, feature: true do
       expect_tokens([{ name: 'milestone', value: 'upcoming' }])
       expect_filtered_search_input_empty
     end
+
+    it 'selects `started milestones`' do
+      click_static_milestone('Started')
+
+      expect(page).to have_css(js_dropdown_milestone, visible: false)
+      expect_tokens([{ name: 'milestone', value: 'started' }])
+      expect_filtered_search_input_empty
+    end
   end
 
   describe 'input has existing content' do
