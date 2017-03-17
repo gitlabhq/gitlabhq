33f26de56bca2c5c2d6a4da29ed2217b8abf1710
Phil Hughes <me@iamphill.com>
2017-03-17 17:00:55 +0000

    Loads raw JSON & displays in the component
    [ci skip]

--- app/assets/javascripts/blob/notebook_viewer.js
+++ app/assets/javascripts/blob/notebook_viewer.js
@@ -1,20 +1,49 @@
 import Vue from 'vue';
+import VueResource from 'vue-resource';
 import NotebookLab from 'vendor/notebooklab';
 
+Vue.use(VueResource);
 Vue.use(NotebookLab);
 
 $(() => {
+  const el = document.getElementById('js-notebook-viewer');
+
   new Vue({
-    el: '#js-notebook-viewer',
+    el,
     data() {
       return {
+        loading: true,
         json: {},
       };
     },
     template: `
-      <div>
-        <notebook-lab :notebook="json" />
+      <div class="container-fluid">
+        <i
+          class="fa fa-spinner fa-spin"
+          v-if="loading">
+        </i>
+        <notebook-lab
+          v-if="!loading"
+          :notebook="json" />
       </div>
     `,
+    mounted() {
+      $.get(gon.katex_css_url, () => {
+        const css = $('<link>', {
+          rel: 'stylesheet',
+          type: 'text/css',
+          href: gon.katex_css_url,
+        });
+        css.appendTo('head');
+      });
+
+      $.getScript(gon.katex_js_url, () => {
+        this.$http.get(el.dataset.endpoint)
+          .then((res) => {
+            this.json = res.json();
+            this.loading = false;
+          });
+      });
+    },
   });
 });
