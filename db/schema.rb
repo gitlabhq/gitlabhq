7cad597f6c8ba794c6852e23d718ed7827da35c6
Stan Hu <stanhu@gmail.com>
2017-03-16 20:18:57 -0700

    Revert "Merge branch '8836-mr-revert' into 'master'
    This reverts commit 68e40bd49fde7b790bb31b9ac85a249bedd817d2, reversing
    changes made to 2d1f823b4c8b60cee525384cb52e547d2be8925a.

--- db/schema.rb
+++ db/schema.rb
@@ -1318,6 +1318,7 @@ ActiveRecord::Schema.define(version: 20170315174634) do
     t.string "incoming_email_token"
     t.string "organization"
     t.boolean "authorized_projects_populated"
+    t.boolean "notified_of_own_activity", default: false, null: false
     t.boolean "ghost"
   end
 
