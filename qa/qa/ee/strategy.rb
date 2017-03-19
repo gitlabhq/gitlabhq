d631056c7cdd1cafb6cb1c0134369fe8bf2e8b82
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2017-03-19 09:44:34 +0100

    Do not add EE license if not set in GitLab QA tests

--- qa/qa/ee/strategy.rb
+++ qa/qa/ee/strategy.rb
@@ -2,12 +2,15 @@ module QA
   module EE
     module Strategy
       extend self
+
       def extend_autoloads!
         require 'qa/ee'
       end
 
       def perform_before_hooks
-        EE::Scenario::License::Add.perform
+        return unless ENV['EE_LICENSE']
+
+        EE::Scenario::License::Add.perform(ENV['EE_LICENSE'])
       rescue
         Capybara::Screenshot.screenshot_and_save_page
         raise
