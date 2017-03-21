93d1895b7baa7149ef3caaecad529459d076edcb
Clement Ho <ClemMakesApps@gmail.com>
2017-03-21 12:14:40 -0500

    Refactor v-if into computed

--- app/assets/javascripts/vue_sidebar_assignees/components/show_more_assignees.js
+++ app/assets/javascripts/vue_sidebar_assignees/components/show_more_assignees.js
@@ -4,10 +4,15 @@ export default {
     hiddenAssignees: { type: Number, required: true },
     toggle: { type: Function, required: true }
   },
+  computed: {
+    showMore() {
+      return this.hiddenAssignees > 0;
+    }
+  },
   template: `
     <div class="user-list-more">
       <button type="button" class="btn-link" @click="toggle">
-        <template v-if="hiddenAssignees > 0">
+        <template v-if="showMore">
           + {{hiddenAssignees}} more
         </template>
         <template v-else>
