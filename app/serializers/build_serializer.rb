c4441c623e7c33839e682819db269a7ee4cb8faa
Shinya Maeda <gitlab.shinyamaeda@gmail.com>
2017-03-21 21:07:01 +0900

    Use superclass represent

--- app/serializers/build_serializer.rb
+++ app/serializers/build_serializer.rb
@@ -1,10 +1,6 @@
 class BuildSerializer < BaseSerializer
   entity BuildEntity
 
-  def represent(resource, opts = {})
-    super(resource, opts)
-  end
-
   def represent_status(resource)
     data = represent(resource, { only: [:status] })
     data.fetch(:status, {})
