bfedbefdfea948c1e83e136f688ce64f2e421574
Sean McGivern <sean@gitlab.com>
2017-03-21 11:53:04 +0000

    Add logging for bulk update recipients
    This is only needed to debug
    https://gitlab.com/gitlab-org/gitlab-ce/issues/28836

--- app/services/issuable/bulk_update_service.rb
+++ app/services/issuable/bulk_update_service.rb
@@ -7,12 +7,18 @@ module Issuable
       ids = params.delete(:issuable_ids).split(",")
       items = model_class.where(id: ids)
 
+      # https://gitlab.com/gitlab-org/gitlab-ce/issues/28836
+      Rails.logger.info("BulkUpdateService received created for type: #{type} with IDs: #{ids.inspect} and params: #{params.inspect}")
+
       %i(state_event milestone_id assignee_id add_label_ids remove_label_ids subscription_event).each do |key|
         params.delete(key) unless params[key].present?
       end
 
       items.each do |issuable|
         next unless can?(current_user, :"update_#{type}", issuable)
+
+        # https://gitlab.com/gitlab-org/gitlab-ce/issues/28836
+        Rails.logger.info("BulkUpdateService created #{update_class} for #{issuable.to_reference(full: true)} with params: #{params.inspect}")
 
         update_class.new(issuable.project, current_user, params).execute(issuable)
       end
