f879e40da73b0c149c0928883ad8965224fab8d3
Rémy Coutable <remy@rymai.me>
2017-03-22 19:40:35 +0100

    Don't navigate to about:blank
    This turned out to prevent the browser from clearing the local storage.
    
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- spec/support/wait_for_requests.rb
+++ spec/support/wait_for_requests.rb
@@ -3,20 +3,12 @@ module WaitForRequests
 
   # This is inspired by http://www.salsify.com/blog/engineering/tearing-capybara-ajax-tests
   def wait_for_requests_complete
-    stop_client
     Gitlab::Testing::RequestBlockerMiddleware.block_requests!
     wait_for('pending AJAX requests complete') do
       Gitlab::Testing::RequestBlockerMiddleware.num_active_requests.zero?
     end
   ensure
     Gitlab::Testing::RequestBlockerMiddleware.allow_requests!
-  end
-
-  # Navigate away from the current page which will prevent any new requests from being started
-  def stop_client
-    page.execute_script %Q{
-      window.location = "about:blank";
-    }
   end
 
   # Waits until the passed block returns true
