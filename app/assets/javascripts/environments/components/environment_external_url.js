2e9388315ca434714afe8c62f44b36fabe442440
Jose Ivan Vargas <jvargas@gitlab.com>
2017-03-23 12:09:58 -0600

    Add tooltip to the external url component

--- app/assets/javascripts/environments/components/environment_external_url.js
+++ app/assets/javascripts/environments/components/environment_external_url.js
@@ -11,11 +11,11 @@ export default {
 
   template: `
     <a
-      class="btn external_url"
+      class="btn external_url has-tooltip"
       :href="externalUrl"
       target="_blank"
       rel="noopener noreferrer"
-      title="Environment external URL">
+      title="Open Environment">
       <i class="fa fa-external-link" aria-hidden="true"></i>
     </a>
   `,
