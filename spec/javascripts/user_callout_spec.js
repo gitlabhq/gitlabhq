b268771748e47be9269b9504034e079d92985f5b
Nick Thomas <nick@gitlab.com>
2017-03-23 18:10:56 +0000

    One more fix for the user_callout_spec.js

--- spec/javascripts/user_callout_spec.js
+++ spec/javascripts/user_callout_spec.js
@@ -3,11 +3,11 @@ import UserCallout from '~/user_callout';
 
 const USER_CALLOUT_COOKIE = 'user_callout_dismissed';
 
-describe('UserCallout', () => {
+describe('UserCallout', function () {
   const fixtureName = 'static/user_callout.html.raw';
   preloadFixtures(fixtureName);
 
-  beforeEach(function () {
+  beforeEach(() => {
     loadFixtures(fixtureName);
     Cookies.remove(USER_CALLOUT_COOKIE);
 
@@ -29,12 +29,12 @@ describe('UserCallout', () => {
     expect(this.userCalloutContainer.is(':visible')).toBe(true);
   });
 
-  it('hides when user clicks on the dismiss-icon', function () {
+  it('hides when user clicks on the dismiss-icon', () => {
     this.closeButton.click();
     expect(Cookies.get(USER_CALLOUT_COOKIE)).toBe('true');
   });
 
-  it('hides when user clicks on the "check it out" button', function () {
+  it('hides when user clicks on the "check it out" button', () => {
     this.userCalloutBtn.click();
     expect(Cookies.get(USER_CALLOUT_COOKIE)).toBe('true');
   });
