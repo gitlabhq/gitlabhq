0759db802f053e2757d52a284fd29098a9473df2
Toon Claes <toon@gitlab.com>
2017-03-23 09:43:34 +0100

    Move user_link to generic UsersHelper
    Make the user_link helper more generic to be used for objects other
    than pipelines.

---
+++ spec/helpers/avatars_helper_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe AvatarsHelper do
+  let(:user) { create(:user) }
+
+  describe '#user_avatar' do
+    subject { helper.user_avatar(user: user) }
+
+    it "links to the user's profile" do
+      is_expected.to include("href=\"#{user_path(user)}\"")
+    end
+
+    it "has the user's name as title" do
+      is_expected.to include("title=\"#{user.name}\"")
+    end
+
+    it "contains the user's avatar image" do
+      is_expected.to include(CGI.escapeHTML(user.avatar_url(16)))
+    end
+  end
+end
