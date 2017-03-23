70faa160915f183ed57d73a4495fe3d1dc2437c5
Fatih Acet <acetfatih@gmail.com>
2017-03-24 00:17:00 +0300

    MRWidget: Group merge conflict buttons.

--- app/assets/javascripts/vue_merge_request_widget/components/states/mr_widget_conflicts.js
+++ app/assets/javascripts/vue_merge_request_widget/components/states/mr_widget_conflicts.js
@@ -16,17 +16,19 @@ export default {
         There are merge conflicts.
         <span v-if="!mr.canMerge">Resolve these conflicts or ask someone with write access to this repository to merge it locally.</span>
       </span>
-      <a
-        :href="mr.conflictResolutionPath"
-        v-if="showResolveConflictsButton"
-        class="btn btn-default btn-xs how_to_merge_link vlink"
-      >Resolve conflicts</a>
-      <a
-        v-if="mr.canMerge"
-        class="btn btn-default btn-xs how_to_merge_link vlink"
-        data-toggle="modal"
-        href="#modal_merge_info"
-      >Merge locally</a>
+      <div class="btn-group">
+        <a
+          :href="mr.conflictResolutionPath"
+          v-if="showResolveConflictsButton"
+          class="btn btn-default btn-xs how_to_merge_link vlink"
+        >Resolve conflicts</a>
+        <a
+          v-if="mr.canMerge"
+          class="btn btn-default btn-xs how_to_merge_link vlink"
+          data-toggle="modal"
+          href="#modal_merge_info"
+        >Merge locally</a>
+      </div>
     </div>
   `,
 };
