86d8c2a78e3e9834bcb59a94041be6f9e40c7f98
Rydkin Maxim <maks.rydkin@gmail.com>
2017-03-27 21:14:17 +0300

    remove redundant `pipeline`

--- app/presenters/ci/pipeline_status_badge_presenter.rb
+++ app/presenters/ci/pipeline_status_badge_presenter.rb
@@ -7,7 +7,7 @@ module Ci
     end
 
     def status_title
-      "Pipeline is redundant and is auto-canceled by Pipeline ##{pipeline.auto_canceled_by_id}" if auto_canceled?
+      "Pipeline is redundant and is auto-canceled by Pipeline ##{auto_canceled_by_id}" if auto_canceled?
     end
   end
 end
