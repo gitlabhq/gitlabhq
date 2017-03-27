ca9a79f62026aa63ab44665aa51a8e88de941b01
Robert Speicher <rspeicher@gmail.com>
2017-03-27 17:14:01 -0400

    Use `:empty_project` where possible in service specs

--- spec/services/ci/update_build_queue_service_spec.rb
+++ spec/services/ci/update_build_queue_service_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Ci::UpdateBuildQueueService, :services do
-  let(:project) { create(:project) }
+  let(:project) { create(:project, :repository) }
   let(:build) { create(:ci_build, pipeline: pipeline) }
   let(:pipeline) { create(:ci_pipeline, project: project) }
 
