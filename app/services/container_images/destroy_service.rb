313e35e817445271b68d40ab06f192f1d3e0ccf0
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2017-03-29 13:14:06 +0200

    Remove container images destroy service class

--- app/services/container_images/destroy_service.rb
+++
@@ -1,9 +0,0 @@
-module ContainerImages
-  class DestroyService < BaseService
-    def execute(container_image)
-      return false unless can?(current_user, :update_container_image, project)
-
-      container_image.destroy!
-    end
-  end
-end
