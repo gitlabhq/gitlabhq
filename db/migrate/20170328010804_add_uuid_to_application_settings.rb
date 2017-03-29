237e48a77167af4682fcc54fd3e2e8cb4a939ab7
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2017-03-29 00:07:45 -0300

    Use SecureRandom.uuid over SecureRandom.urlsafe_base64

--- db/migrate/20170328010804_add_uuid_to_application_settings.rb
+++ db/migrate/20170328010804_add_uuid_to_application_settings.rb
@@ -8,18 +8,11 @@ class AddUuidToApplicationSettings < ActiveRecord::Migration
   def up
     add_column :application_settings, :uuid, :string
     add_concurrent_index :application_settings, :uuid
-    execute("UPDATE application_settings SET uuid = #{quote(generate_random_string(40))}")
+    execute("UPDATE application_settings SET uuid = #{quote(SecureRandom.uuid)}")
   end
 
   def down
     remove_index :application_settings, :uuid if index_exists?(:application_settings, :uuid)
     remove_column :application_settings, :uuid
-  end
-
-  private
-
-  def generate_random_string(size)
-    # urlsafe_base64 may return a string of size * 4/3
-    SecureRandom.urlsafe_base64(size)[0, size]
   end
 end
