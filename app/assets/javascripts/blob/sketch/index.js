14f220d39373d6eec6cb49ba2bcd33f39907e556
Regis <boudinot.regis@yahoo.com>
2017-03-28 23:40:34 -0600

    to closest working state with JSZip

--- app/assets/javascripts/blob/sketch/index.js
+++ app/assets/javascripts/blob/sketch/index.js
@@ -8,60 +8,51 @@ Vue.use(VueResource);
 export default () => {
   const el = document.getElementById('js-sketch-viewer');
 
-
-  new Vue({
+  return new Vue({
     el,
-    data(){
+    data() {
       return {
         previewURL: '',
-        error: false
-      }
+        error: false,
+      };
     },
-
     methods: {
-
       tryUnzip() {
-        return this.$http.get(el.dataset.endpoint)
-        // return new JSZip.external.Promise((resolve, reject) => {
-        //   JSZipUtils.getBinaryContent(el.dataset.endpoint, (err, data) => {
-        //     if (err) {
-        //       reject(err);
-        //     } else {
-        //       resolve(data);
-        //     }
-        //   });
-        // });
-
-      }
+        // return this.$http.get(el.dataset.endpoint)
+        return new JSZip.external.Promise((resolve, reject) => {
+          JSZipUtils.getBinaryContent(el.dataset.endpoint, (err, data) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(data);
+            }
+          });
+        });
+      },
     },
-
     mounted() {
       this.tryUnzip()
-      .then((res) => {
-        JSZip.loadAsync(res)
+        // .then((res) => {
+        //   JSZip.loadAsync(res)
+        //   .then((something) => {
+        //     console.log(something);
+        //   });
+        // });
+        .then(data => JSZip.loadAsync(data))
         .then((something) => {
-          console.log(something);
-        })
-      })
-      // .then((data) =>{
-      //   return JSZip.loadAsync(data);
-      // })
-      // .then((something) => {
-      //   something.files['previews/preview.png'].async('uint8array')
-      //   .then((content)=>{
-      //     var fileReader = new FileReader();
-      //     var blob = new Blob(content, {type: 'image/png'});
-      //     var url = URL.createObjectURL(blob);
+          something.files['previews/preview.png'].async('uint8array')
+            .then((content) => {
+              const blob = new Blob(content, { type: 'image/png' });
+              const url = URL.createObjectURL(blob);
 
-      //     this.previewURL = url;
-      //   });
-      // });
+              this.previewURL = url;
+            });
+        });
     },
-
     template: `
       <div>
         <img :src=previewURL />
       </div>
-    `
+    `,
   });
-}
+};
