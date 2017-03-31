62480461c943b4ca4c72830c04932cd5bba9f4e7
Shinya Maeda <gitlab.shinyamaeda@gmail.com>
2017-04-01 02:55:55 +0900

    Fixed failed tests

--- spec/models/ci/trigger_schedule_spec.rb
+++ spec/models/ci/trigger_schedule_spec.rb
@@ -21,7 +21,7 @@ describe Ci::TriggerSchedule, models: true do
       trigger_schedule.valid?
     end
 
-    context 'when every hour' do
+    context 'when cron frequency is too short' do
       let(:cron) { '0 * * * *' } # 00:00, 01:00, 02:00, ..., 23:00
 
       it 'gets an error' do
@@ -29,8 +29,8 @@ describe Ci::TriggerSchedule, models: true do
       end
     end
 
-    context 'when each six hours' do
-      let(:cron) { '0 */6 * * *' } # 00:00, 06:00, 12:00, 18:00
+    context 'when cron frequency is eligible' do
+      let(:cron) { '0 0 1 1 *' } # every 00:00, January 1st
 
       it 'gets no errors' do
         expect(trigger_schedule.errors[:cron]).to be_empty
@@ -39,17 +39,15 @@ describe Ci::TriggerSchedule, models: true do
   end
 
   describe '#schedule_next_run!' do
-    context 'when more_than_1_hour_from_now' do
-      let(:trigger_schedule) { create(:ci_trigger_schedule, :cron_nightly_build) }
+    let(:trigger_schedule) { create(:ci_trigger_schedule, :cron_nightly_build) }
 
-      before do
-        trigger_schedule.schedule_next_run!
-      end
+    before do
+      trigger_schedule.schedule_next_run!
+    end
 
-      it 'updates next_run_at' do
-        next_time = Ci::CronParser.new(trigger_schedule.cron, trigger_schedule.cron_time_zone).next_time_from(Time.now)
-        expect(Ci::TriggerSchedule.last.next_run_at).to eq(next_time)
-      end
+    it 'updates next_run_at' do
+      next_time = Ci::CronParser.new(trigger_schedule.cron, trigger_schedule.cron_time_zone).next_time_from(Time.now)
+      expect(Ci::TriggerSchedule.last.next_run_at).to eq(next_time)
     end
   end
 end
