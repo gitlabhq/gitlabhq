c3781da357ee53cdfdb59196232e2562d7c416c8
Phil Hughes <me@iamphill.com>
2017-03-31 14:35:36 +0100

    Fixed the mirror user dropdown not displaying
    This was caused by the JS class not being initialized in the dispatcher
    
    Closes https://gitlab.com/gitlab-org/gitlab-ce/issues/30262

--- spec/features/projects/settings/ee/push_rules_settings_spec.rb
+++ spec/features/projects/settings/ee/push_rules_settings_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
-describe 'Project settings > [EE] Push Rules', feature: true do
+describe 'Project settings > [EE] repository', feature: true do
+  include Select2Helper
+  
   let(:user) { create(:user) }
   let(:project) { create(:project_empty_repo) }
 
@@ -26,6 +28,26 @@ describe 'Project settings > [EE] Push Rules', feature: true do
 
     it 'saves the new value' do
       expect(project.push_rule.commit_message_regex).to eq commit_message
+    end
+  end
+
+  describe 'mirror settings', :js do
+    let(:user2) { create(:user) }
+
+    before do
+      project.team << [user2, :master]
+
+      visit namespace_project_settings_repository_path(project.namespace, project)
+    end
+
+    it 'sets mirror user' do
+      page.within('.edit_project') do
+        select2(user2.id, from: '#project_mirror_user_id')
+
+        click_button('Save changes')
+
+        expect(find('.select2-chosen')).to have_content(user2.name)
+      end
     end
   end
 end
