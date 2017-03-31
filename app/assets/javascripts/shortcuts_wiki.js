07442bf84141cf766d05f014385a2ada4d5b6442
geoandri <geoandri@gmail.com>
2017-03-31 12:32:55 +0300

    Add wikiEdit to constructor

--- app/assets/javascripts/shortcuts_wiki.js
+++ app/assets/javascripts/shortcuts_wiki.js
@@ -1,14 +1,14 @@
-/* eslint-disable class-methods-use-this*/
 /* global Mousetrap */
 /* global ShortcutsNavigation */
 
 export default class ShortcutsWiki extends ShortcutsNavigation {
   constructor() {
     super();
-    Mousetrap.bind('e', this.editWiki);
+    this.$wikiEdit = $('.wiki-edit');
+    Mousetrap.bind('e', this.editWiki.bind(this));
   }
 
   editWiki() {
-    gl.utils.visitUrl($('.wiki-edit').attr('href'));
+    gl.utils.visitUrl(this.$wikiEdit.attr('href'));
   }
 }
