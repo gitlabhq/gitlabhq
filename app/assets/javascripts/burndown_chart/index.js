d36d04f4be8cbd1554d8cdd08b2e603382e257b6
Mike Greiling <mike@pixelcog.com>
2017-03-31 15:50:26 -0500

    add toggle between issue count and issue weight

--- app/assets/javascripts/burndown_chart/index.js
+++ app/assets/javascripts/burndown_chart/index.js
@@ -17,10 +17,32 @@ $(() => {
     const startDate = chartElm.data('startDate');
     const dueDate = chartElm.data('endDate');
     const chartData = chartElm.data('chartData');
+    const openIssuesCount = chartData.map(d => [d[0], d[1]]);
+    const openIssuesWeight = chartData.map(d => [d[0], d[2]]);
 
     const chart = new BurndownChart({ container, startDate, dueDate });
 
-    chart.setData(chartData, { label: 'Open Issues', animate: true });
+    let currentView = 'count';
+    chart.setData(openIssuesCount, { label: 'Open Issues', animate: true });
+
+    $('.burndown-header').on('click', '.btn', function switchData() {
+      const $this = $(this);
+      const show = $this.data('show');
+      if (currentView !== show) {
+        currentView = show;
+        $this.addClass('active').siblings().removeClass('active');
+        switch (show) {
+          case 'count':
+            chart.setData(openIssuesCount, { label: 'Open Issues', animate: true });
+            break;
+          case 'weight':
+            chart.setData(openIssuesWeight, { label: 'Open Issue Weight', animate: true });
+            break;
+          default:
+            break;
+        }
+      }
+    });
 
     window.addEventListener('resize', () => chart.animateResize(1));
     $(document).on('click', '.js-sidebar-toggle', () => chart.animateResize(2));
