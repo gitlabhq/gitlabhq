c2d6dd321410f0f719338aac2f3580d9649e6765
Mike Greiling <mike@pixelcog.com>
2017-03-31 15:00:35 -0500

    remove test_data and rely on data passed from rails

--- app/assets/javascripts/burndown_chart/index.js
+++ app/assets/javascripts/burndown_chart/index.js
@@ -1,6 +1,5 @@
 import Cookies from 'js-cookie';
 import BurndownChart from './burndown_chart';
-import testData from './test_data.json';
 
 $(() => {
   // handle hint dismissal
@@ -10,14 +9,20 @@ $(() => {
     Cookies.set('hide_burndown_message', 'true');
   });
 
-  // render chart
-  const chart = new BurndownChart({
-    container: '.burndown-chart',
-    startDate: '2017-03-01',
-    dueDate: '2017-03-31',
-  });
+  // generate burndown chart (if data available)
+  const container = '.burndown-chart';
+  const chartElm = $(container);
 
-  chart.setData(testData, { label: 'Open Issues', animate: true });
-  window.addEventListener('resize', () => chart.animateResize(1));
-  $(document).on('click', '.js-sidebar-toggle', () => chart.animateResize(2));
+  if (chartElm.length) {
+    const startDate = chartElm.data('startDate');
+    const dueDate = chartElm.data('endDate');
+    const chartData = chartElm.data('chartData');
+
+    const chart = new BurndownChart({ container, startDate, dueDate });
+
+    chart.setData(chartData, { label: 'Open Issues', animate: true });
+
+    window.addEventListener('resize', () => chart.animateResize(1));
+    $(document).on('click', '.js-sidebar-toggle', () => chart.animateResize(2));
+  }
 });
