0a09925dcef1976970bc2674432f69d46786c38f
mhasbini <mohammad.hasbini@gmail.com>
2017-03-31 18:11:28 +0300

    Enable Style/Proc cop for rubocop

--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -30,7 +30,7 @@ class Milestone < ActiveRecord::Base
 
   validates :title, presence: true, uniqueness: { scope: :project_id }
   validates :project, presence: true
-  validate :start_date_should_be_less_than_due_date, if: Proc.new { |m| m.start_date.present? && m.due_date.present? }
+  validate :start_date_should_be_less_than_due_date, if: proc { |m| m.start_date.present? && m.due_date.present? }
 
   strip_attributes :title
 
