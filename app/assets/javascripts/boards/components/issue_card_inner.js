ffd5cf4ec6f67a6c0ed23714ca4e1520f5e3fb4f
Clement Ho <ClemMakesApps@gmail.com>
2017-03-30 23:47:04 -0500

    fix tests

--- app/assets/javascripts/boards/components/issue_card_inner.js
+++ app/assets/javascripts/boards/components/issue_card_inner.js
@@ -20,7 +20,7 @@ import eventHub from '../eventhub';
       list: {
         type: Object,
         required: false,
-        default: {},
+        default: () => ({}),
       },
       rootPath: {
         type: String,
