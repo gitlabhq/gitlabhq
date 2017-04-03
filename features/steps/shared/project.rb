d4377b8c12e106892098d66321059ca6bc1affdd
Annabel Dunstone Gray <annabel.dunstone@gmail.com>
2017-04-03 08:33:02 -0500

    Update commit icon; fix specs

--- features/steps/shared/project.rb
+++ features/steps/shared/project.rb
@@ -97,7 +97,7 @@ module SharedProject
 
   step 'I should see project "Shop" activity feed' do
     project = Project.find_by(name: "Shop")
-    expect(page).to have_content "#{@user.name} pushed new branch fix at #{project.name_with_namespace}"
+    expect(page).to have_content "pushed new branch fix at #{project.name_with_namespace}"
   end
 
   step 'I should see project settings' do
