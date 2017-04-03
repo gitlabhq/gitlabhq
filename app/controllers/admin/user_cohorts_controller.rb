af8a3e3583247001ed41a74c0ca5032ea6fe18fd
Sean McGivern <sean@gitlab.com>
2017-04-03 14:37:36 +0100

    Cache user cohorts results for a day

--- app/controllers/admin/user_cohorts_controller.rb
+++ app/controllers/admin/user_cohorts_controller.rb
@@ -1,7 +1,9 @@
 class Admin::UserCohortsController < Admin::ApplicationController
   def index
     if ApplicationSetting.current.usage_ping_enabled
-      @cohorts = UserCohortsService.new.execute(12)
+      @cohorts = Rails.cache.fetch('user_cohorts', expires_in: 1.day) do
+        UserCohortsService.new.execute
+      end
     end
   end
 end
