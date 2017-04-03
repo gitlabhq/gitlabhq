bbb09feaa1b535e10b20790d1857385bd34ae5f6
Kushal Pandya <kushal@gitlab.com>
2017-04-03 11:41:05 +0530

    Export Protected Tags Editing classes

--- app/assets/javascripts/protected_tags/protected_tags_bundle.js
+++ app/assets/javascripts/protected_tags/protected_tags_bundle.js
@@ -1,3 +1,5 @@
 require('./protected_tag_access_dropdown');
 require('./protected_tag_create');
 require('./protected_tag_dropdown');
+require('./protected_tag_edit');
+require('./protected_tag_edit_list');
