27f981b2901098f894e587bbd96b09e2a0f0c404
Shinya Maeda <gitlab.shinyamaeda@gmail.com>
2017-04-04 18:31:19 +0900

    Improve real_next_run. Improve triggers_helper_spec.

--- app/helpers/triggers_helper.rb
+++ app/helpers/triggers_helper.rb
@@ -11,16 +11,10 @@ module TriggersHelper
     "#{Settings.gitlab.url}/api/v3/projects/#{service.project_id}/services/#{service.to_param}/trigger"
   end
 
-  def real_next_run(trigger_schedule, worker_cron: nil, worker_time_zone: nil)
-    worker_cron = Settings.cron_jobs['trigger_schedule_worker']['cron'] unless worker_cron.present?
-    worker_time_zone = Time.zone.name unless worker_time_zone.present?
-
-    worker_next_time = Ci::CronParser.new(worker_cron, worker_time_zone).next_time_from(Time.now)
-
-    if trigger_schedule.next_run_at > worker_next_time
-      trigger_schedule.next_run_at
-    else
-      worker_next_time
-    end
+  def real_next_run(trigger_schedule,
+                    worker_cron: Settings.cron_jobs['trigger_schedule_worker']['cron'],
+                    worker_time_zone: Time.zone.name)
+    Ci::CronParser.new(worker_cron, worker_time_zone)
+                  .next_time_from(trigger_schedule.next_run_at)
   end
 end
