f1f7b71bd33b3f1d8404f6a7cbb1d782de1d0f23
Kushal Pandya <kushal@gitlab.com>
2017-04-05 00:33:41 +0530

    Update styling as per UI specs

--- app/assets/javascripts/vue_merge_request_widget/components/states/mr_widget_locked.js
+++ app/assets/javascripts/vue_merge_request_widget/components/states/mr_widget_locked.js
@@ -4,14 +4,15 @@ export default {
     mr: { type: Object, required: true },
   },
   template: `
-    <div class="mr-widget-body">
-      <span class="bold">Locked</span> This merge request is in the process of being merged, during which time it is locked and cannot be closed.
+    <div class="mr-widget-body mr-state-locked">
+      <span class="state-label">Locked</span> This merge request is in the process of being merged, during which time it is locked and cannot be closed.
       <i class="fa fa-spinner fa-spin" aria-hidden="true"></i>
-      <section>
+      <section class="mr-info-list mr-links">
+        <div class="legend"></div>
         <p>The changes will be merged into
-          <a :href="mr.targetBranchPath" class="label-branch">
-            {{mr.targetBranch}}
-          </a>
+          <span class="label-branch">
+            <a :href="mr.targetBranchPath">{{mr.targetBranch}}</a>
+          </span>
         </p>
       </section>
     </div>
