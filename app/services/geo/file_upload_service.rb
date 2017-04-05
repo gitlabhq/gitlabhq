393749210af2ac0165f636996af61d35cc480c8c
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2017-04-05 15:52:40 -0300

    Normalize default object types to file in the file upload service

--- app/services/geo/file_upload_service.rb
+++ app/services/geo/file_upload_service.rb
@@ -1,5 +1,6 @@
 module Geo
   class FileUploadService
+    DEFAULT_OBJECT_TYPES = [:attachment, :avatar, :file].freeze
     IAT_LEEWAY = 60.seconds.to_i
 
     attr_reader :object_type, :object_db_id, :auth_header
@@ -15,15 +16,31 @@ module Geo
       data = ::Gitlab::Geo::JwtRequestDecoder.new(auth_header).decode
       return unless data.present?
 
-      uploader_class.new(object_db_id, data).execute
+      uploader_klass.new(object_db_id, data).execute
     end
 
     private
 
-    def uploader_class
-      "Gitlab::Geo::#{object_type.to_s.camelize}Uploader".constantize
+    def uploader_klass
+      uploader_klass_name.constantize
     rescue NameError
-      Gitlab::Geo::FileUploader
+      log("Unknown file type: #{object_type}")
+      raise
+    end
+
+    def uploader_klass_name
+      klass_name =
+        if DEFAULT_OBJECT_TYPES.include?(object_type.to_sym)
+          :file
+        else
+          object_type
+        end
+
+      "Gitlab::Geo::#{klass_name.to_s.camelize}Uploader"
+    end
+
+    def log(message)
+      Rails.logger.info "#{self.class.name}: #{message}"
     end
   end
 end
--- spec/services/geo/file_upload_service_spec.rb
+++ spec/services/geo/file_upload_service_spec.rb
@@ -76,6 +76,59 @@ describe Geo::FileUploadService, services: true do
       end
     end
 
+    context 'attachment' do
+      let(:note) { create(:note, :with_attachment) }
+      let(:upload) { Upload.find_by(model: note, uploader: 'AttachmentUploader') }
+      let(:params) { { id: upload.id, type: 'attachment' } }
+      let(:file_transfer) { Gitlab::Geo::FileTransfer.new(:attachment, upload) }
+      let(:transfer_request) { Gitlab::Geo::TransferRequest.new(file_transfer.request_data) }
+      let(:req_header) { transfer_request.headers['Authorization'] }
+
+      it 'sends attachment file' do
+        service = described_class.new(params, req_header)
+
+        response = service.execute
+
+        expect(response[:code]).to eq(:ok)
+        expect(response[:file].path).to eq(note.attachment.path)
+      end
+
+      it 'returns nil if no authorization' do
+        service = described_class.new(params, nil)
+
+        expect(service.execute).to be_nil
+      end
+    end
+
+    context 'file upload' do
+      let(:project) { create(:empty_project) }
+      let(:upload) { Upload.find_by(model: project, uploader: 'FileUploader') }
+      let(:params) { { id: upload.id, type: 'file' } }
+      let(:file_transfer) { Gitlab::Geo::FileTransfer.new(:file, upload) }
+      let(:transfer_request) { Gitlab::Geo::TransferRequest.new(file_transfer.request_data) }
+      let(:req_header) { transfer_request.headers['Authorization'] }
+      let(:file) { fixture_file_upload(Rails.root + 'spec/fixtures/dk.png', 'image/png') }
+
+      before do
+        FileUploader.new(project).store!(file)
+      end
+
+      it 'sends the file' do
+        service = described_class.new(params, req_header)
+
+        response = service.execute
+
+        expect(response[:code]).to eq(:ok)
+        expect(response[:file].path).to end_with('dk.png')
+      end
+
+      it 'returns nil if no authorization' do
+        service = described_class.new(params, nil)
+
+        expect(service.execute).to be_nil
+      end
+    end
+
     context 'LFS Object' do
       let(:lfs_object) { create(:lfs_object, :with_file) }
       let(:params) { { id: lfs_object.id, type: 'lfs' } }
