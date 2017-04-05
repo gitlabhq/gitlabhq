7997a0df7106899f1e5352e4613a3e0c3e40db64
Jose Ivan Vargas Lopez <jvargas@gitlab.com>
2017-04-05 05:59:49 +0000

    Dashboard section capitalization fix

--- spec/features/dashboard/group_spec.rb
+++ spec/features/dashboard/group_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'Dashboard Group', feature: true do
 
   it 'creates new grpup' do
     visit dashboard_groups_path
-    click_link 'New Group'
+    click_link 'New group'
 
     fill_in 'group_path', with: 'Samurai'
     fill_in 'group_description', with: 'Tokugawa Shogunate'
