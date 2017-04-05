9c4a06a8f91be054e84b658d9dde1a5c556d1c7f
Luke "Jared" Bennett <lbennett@gitlab.com>
2017-04-05 20:50:26 +0100

    Fix close buttob bug when toggling dropdown

--- app/assets/javascripts/gl_form.js
+++ app/assets/javascripts/gl_form.js
@@ -30,6 +30,10 @@ GLForm.prototype.setupForm = function() {
     this.form.addClass('gfm-form');
     // remove notify commit author checkbox for non-commit notes
     gl.utils.disableButtonIfEmptyField(this.form.find('.js-note-text'), this.form.find('.js-comment-button'));
+
+    const newDiscussionToggle = this.form.find('.js-note-new-discussion');
+    if (newDiscussionToggle) gl.utils.disableButtonIfEmptyField(this.form.find('.js-note-text'), newDiscussionToggle);
+
     gl.GfmAutoComplete.setup(this.form.find('.js-gfm-input'));
     new DropzoneInput(this.form);
     autosize(this.textarea);
