25faa060604e4cab261f1d9845fa1072110e75fd
Annabel Dunstone Gray <annabel.dunstone@gmail.com>
2017-04-06 15:59:47 -0500

    Rewrite system note helper

--- app/helpers/system_note_helper.rb
+++ app/helpers/system_note_helper.rb
@@ -1,44 +1,26 @@
 module SystemNoteHelper
+  ICON_NAMES_BY_ACTION = {
+    'commit' => 'icon_commit',
+    'merge' => 'icon_merge',
+    'merged' => 'icon_merged',
+    'opened' => 'icon_status_open',
+    'closed' => 'icon_status_closed',
+    'time_tracking' => 'icon_stopwatch',
+    'assignee' => 'icon_user',
+    'title' => 'icon_pencil',
+    'task' => 'icon_check_square_o',
+    'label' => 'icon_tags',
+    'cross_reference' => 'icon_random',
+    'branch' => 'icon_code_fork',
+    'confidential' => 'icon_eye_slash',
+    'visible' => 'icon_eye',
+    'milestone' => 'icon_clock_o',
+    'discussion' => 'icon_comment_o',
+    'moved' => 'icon_arrow-circle-o-right'
+  }.freeze
+
   def icon_for_system_note(note)
-
-    icon_name =
-      case note.system_note_metadata.action
-      when 'commit'
-        'icon_commit'
-      when 'merge'
-        'icon_merge'
-      when 'merged'
-        'icon_merged'
-      when 'opened'
-        'icon_status_open'
-      when 'closed'
-        'icon_status_closed'
-      when 'time_tracking'
-        'icon_stopwatch'
-      when 'assignee'
-        'icon_user'
-      when 'title'
-        'icon_edit'
-      when 'task'
-        'icon_check_square_o'
-      when 'label'
-        'icon_tags'
-      when 'cross_reference'
-        'icon_random'
-      when 'branch'
-        'icon_code_fork'
-      when 'confidential'
-        'icon_eye_slash'
-      when 'visible'
-        'icon_eye'
-      when 'milestone'
-        'icon_clock_o'
-      when 'discussion'
-        'icon_comment_o'
-      when 'moved'
-        'icon_arrow-circle-o-right'
-      end
-
+    icon_name = ICON_NAMES_BY_ACTION[note.system_note_metadata&.action]
     custom_icon(icon_name) if icon_name
   end
 end
