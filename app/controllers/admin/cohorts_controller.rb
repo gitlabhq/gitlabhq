ac0146a08eb2ec6c23b46ea014376b7a70a21415
Sean McGivern <sean@gitlab.com>
2017-04-06 13:12:12 +0100

    Use serializer for formatting cohorts data

--- app/controllers/admin/cohorts_controller.rb
+++ app/controllers/admin/cohorts_controller.rb
@@ -1,9 +1,11 @@
 class Admin::CohortsController < Admin::ApplicationController
   def index
-    if ApplicationSetting.current.usage_ping_enabled
-      @cohorts = Rails.cache.fetch('cohorts', expires_in: 1.day) do
+    if current_application_settings.usage_ping_enabled
+      cohorts_results = Rails.cache.fetch('cohorts', expires_in: 1.day) do
         CohortsService.new.execute
       end
+
+      @cohorts = CohortsSerializer.new.represent(cohorts_results)
     end
   end
 end
