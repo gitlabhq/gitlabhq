909718414b4399497d67c86aa5f7f6ca7296ea0e
Mike Greiling <mike@pixelcog.com>
2017-04-06 16:36:54 -0500

    convert pipeline empty state to vue template

--- app/assets/javascripts/commit/pipelines/pipelines_table.js
+++ app/assets/javascripts/commit/pipelines/pipelines_table.js
@@ -4,7 +4,7 @@ import PipelinesTableComponent from '../../vue_shared/components/pipelines_table
 import PipelinesService from '../../vue_pipelines_index/services/pipelines_service';
 import PipelineStore from '../../vue_pipelines_index/stores/pipelines_store';
 import eventHub from '../../vue_pipelines_index/event_hub';
-import EmptyState from '../../vue_pipelines_index/components/empty_state';
+import EmptyState from '../../vue_pipelines_index/components/empty_state.vue';
 import ErrorState from '../../vue_pipelines_index/components/error_state';
 import '../../lib/utils/common_utils';
 import '../../vue_shared/vue_resource_interceptor';
