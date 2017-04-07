c0632f1463f9d9b9afd67df3d216511463a1cc8e
James Clark <james@jameshclrk.com>
2017-04-07 23:21:16 +0100

    Hide clone panel and file list when user is only a guest
    Fixes gitlab-org/gitlab-ce#17489
    
    Fix test finding two of the same element
    
    Capybara will raise an exception because it finds two elements that
    match .nav-links. This means this test would fail, even if the page
    meets the conditions for passing the test.
    
    Add more tests for guest access

--- spec/features/projects/guest_navigation_menu_spec.rb
+++ spec/features/projects/guest_navigation_menu_spec.rb
@@ -13,7 +13,7 @@ describe "Guest navigation menu" do
   it "shows allowed tabs only" do
     visit namespace_project_path(project.namespace, project)
 
-    within(".nav-links") do
+    within(".layout-nav") do
       expect(page).to have_content 'Project'
       expect(page).to have_content 'Issues'
       expect(page).to have_content 'Wiki'
@@ -21,6 +21,53 @@ describe "Guest navigation menu" do
       expect(page).not_to have_content 'Repository'
       expect(page).not_to have_content 'Pipelines'
       expect(page).not_to have_content 'Merge Requests'
+    end
+  end
+
+  it "does not show fork button" do
+    visit namespace_project_path(project.namespace, project)
+
+    within(".count-buttons") do
+      expect(page).not_to have_link 'Fork'
+    end
+  end
+
+  it "does not show clone path" do
+    visit namespace_project_path(project.namespace, project)
+
+    within(".project-repo-buttons") do
+      expect(page).not_to have_selector '.project-clone-holder'
+    end
+  end
+
+  describe 'project landing page' do
+    before do
+      project.project_feature.update_attribute("issues_access_level", ProjectFeature::DISABLED)
+      project.project_feature.update_attribute("wiki_access_level", ProjectFeature::DISABLED)
+    end
+
+    it "does not show the project file list landing page" do
+      visit namespace_project_path(project.namespace, project)
+      expect(page).not_to have_selector '.project-stats'
+      expect(page).not_to have_selector '.project-last-commit'
+      expect(page).not_to have_selector '.project-show-files'
+    end
+
+    it "shows the customize workflow when issues and wiki are disabled" do
+      visit namespace_project_path(project.namespace, project)
+      expect(page).to have_selector '.project-show-customize_workflow'
+    end
+
+    it "shows the wiki when enabled" do
+      project.project_feature.update_attribute("wiki_access_level", ProjectFeature::PRIVATE)
+      visit namespace_project_path(project.namespace, project)
+      expect(page).to have_selector '.project-show-wiki'
+    end
+
+    it "shows the issues when enabled" do
+      project.project_feature.update_attribute("issues_access_level", ProjectFeature::PRIVATE)
+      visit namespace_project_path(project.namespace, project)
+      expect(page).to have_selector '.issues-list'
     end
   end
 end
