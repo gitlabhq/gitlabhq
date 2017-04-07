bea1aba9caff0b19bc08004cfb80cb9e5f1b6b89
Douwe Maan <douwe@selenight.nl>
2017-04-07 10:31:49 -0500

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170405080720) do
+ActiveRecord::Schema.define(version: 20170406115029) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "pg_trgm"
