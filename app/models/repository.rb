6a07a2430b24fe8040124b103029b0129996c3e5
Jacob Vosmaer <jacob@gitlab.com>
2017-04-07 11:17:25 +0200

    Combine delegations in one line

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1156,8 +1156,7 @@ class Repository
     @project.repository_storage_path
   end
 
-  delegate :gitaly_repository, to: :raw_repository
-  delegate :gitaly_channel, to: :raw_repository
+  delegate :gitaly_channel, :gitaly_repository, to: :raw_repository
 
   def initialize_raw_repository
     Gitlab::Git::Repository.new(project.repository_storage, path_with_namespace + '.git')
