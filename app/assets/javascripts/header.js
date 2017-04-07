d6e39d8a4cafe275d4c4d46048bf6403e5068e82
Sam Rose <samrose3@gmail.com>
2017-04-07 13:26:39 +0000

    MR shortcuts counter for stable

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,8 +1,7 @@
-/* eslint-disable wrap-iife, func-names, space-before-function-paren, prefer-arrow-callback, no-var, max-len */
-(function() {
-  $(document).on('todo:toggle', function(e, count) {
-    var $todoPendingCount = $('.todos-pending-count');
-    $todoPendingCount.text(gl.text.highCountTrim(count));
-    $todoPendingCount.toggleClass('hidden', count === 0);
-  });
-})();
+/* eslint-disable func-names, space-before-function-paren, prefer-arrow-callback, no-var */
+
+$(document).on('todo:toggle', function(e, count) {
+  var $todoPendingCount = $('.todos-count');
+  $todoPendingCount.text(gl.text.highCountTrim(count));
+  $todoPendingCount.toggleClass('hidden', count === 0);
+});
