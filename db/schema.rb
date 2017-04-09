ad27f20426975cf515fd55e4b5b75e5d4703315c
blackst0ne <blackst0ne.ru@gmail.com>
2017-04-10 08:48:44 +1100

    Periodically clean up temporary upload files to recover storage space

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170407140450) do
+ActiveRecord::Schema.define(version: 20170408033905) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "pg_trgm"
