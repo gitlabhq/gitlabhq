4368a17061b67fa62ce8f8af9db0ab03da03236a
Oswaldo Ferreira <oswaldo@gitlab.com>
2017-04-10 16:14:13 -0300

    Separate CE from EE params on V4 API

--- lib/api/issues.rb
+++ lib/api/issues.rb
@@ -30,15 +30,22 @@ module API
         use :pagination
       end
 
-      params :issue_params do
+      params :issue_params_ce do
         optional :description, type: String, desc: 'The description of an issue'
         optional :assignee_id, type: Integer, desc: 'The ID of a user to assign issue'
         optional :milestone_id, type: Integer, desc: 'The ID of a milestone to assign issue'
         optional :labels, type: String, desc: 'Comma-separated list of label names'
         optional :due_date, type: String, desc: 'Date string in the format YEAR-MONTH-DAY'
         optional :confidential, type: Boolean, desc: 'Boolean parameter if the issue should be confidential'
-        # Gitlab-EE specific
+      end
+
+      params :issue_params_ee do
         optional :weight, type: Integer, values: 0..9, desc: 'The weight of the issue'
+      end
+
+      params :issue_params do
+        use :issue_params_ce
+        use :issue_params_ee
       end
     end
 
