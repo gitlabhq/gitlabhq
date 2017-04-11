71b7461f371c2257d85ab05c8463e440f569053d
Annabel Dunstone Gray <annabel.dunstone@gmail.com>
2017-04-11 12:32:42 -0500

    Reuse system notes helper for profile event icons

--- app/helpers/system_note_helper.rb
+++ app/helpers/system_note_helper.rb
@@ -1,9 +1,11 @@
 module SystemNoteHelper
   ICON_NAMES_BY_ACTION = {
     'commit' => 'icon_commit',
+    'push' => 'icon_commit',
     'merge' => 'icon_merge',
     'merged' => 'icon_merged',
     'opened' => 'icon_status_open',
+    'created' => 'icon_status_open',
     'closed' => 'icon_status_closed',
     'time_tracking' => 'icon_stopwatch',
     'assignee' => 'icon_user',
@@ -12,15 +14,18 @@ module SystemNoteHelper
     'label' => 'icon_tags',
     'cross_reference' => 'icon_random',
     'branch' => 'icon_code_fork',
+    'accepted' => 'icon_code_fork',
     'confidential' => 'icon_eye_slash',
     'visible' => 'icon_eye',
     'milestone' => 'icon_clock_o',
     'discussion' => 'icon_comment_o',
-    'moved' => 'icon_arrow_circle_o_right'
+    'commented on' => 'icon_comment_o',
+    'moved' => 'icon_arrow_circle_o_right',
+    'deleted' => 'icon_trash_o'
   }.freeze
 
   def icon_for_system_note(note)
-    icon_name = ICON_NAMES_BY_ACTION[note.system_note_metadata&.action]
+    icon_name = ICON_NAMES_BY_ACTION[note]
     custom_icon(icon_name) if icon_name
   end
 end
