0a6677c25ca4e35dbf908cc840dcb104817409b9
Alfredo Sumaran <alfredo@gitlab.com>
2017-04-10 20:02:55 -0500

    Remove IIFEs from files in cycle_analytics_bundle.js
    # Conflicts:
    #	spec/javascripts/lib/utils/text_utility_spec.js

--- app/assets/javascripts/cycle_analytics/components/stage_test_component.js
+++ app/assets/javascripts/cycle_analytics/components/stage_test_component.js
@@ -3,48 +3,47 @@ import Vue from 'vue';
 import iconBuildStatus from '../svg/icon_build_status.svg';
 import iconBranch from '../svg/icon_branch.svg';
 
-((global) => {
-  global.cycleAnalytics = global.cycleAnalytics || {};
+const global = window.gl || (window.gl = {});
+global.cycleAnalytics = global.cycleAnalytics || {};
 
-  global.cycleAnalytics.StageTestComponent = Vue.extend({
-    props: {
-      items: Array,
-      stage: Object,
-    },
-    data() {
-      return { iconBuildStatus, iconBranch };
-    },
-    template: `
-      <div>
-        <div class="events-description">
-          {{ stage.description }}
-          <limit-warning :count="items.length" />
-        </div>
-        <ul class="stage-event-list">
-          <li v-for="build in items" class="stage-event-item item-build-component">
-            <div class="item-details">
-              <h5 class="item-title">
-                <span class="icon-build-status">${iconBuildStatus}</span>
-                <a :href="build.url" class="item-build-name">{{ build.name }}</a>
-                &middot;
-                <a :href="build.url" class="pipeline-id">#{{ build.id }}</a>
-                <i class="fa fa-code-fork"></i>
-                <a :href="build.branch.url" class="branch-name monospace">{{ build.branch.name }}</a>
-                <span class="icon-branch">${iconBranch}</span>
-                <a :href="build.commitUrl" class="short-sha monospace">{{ build.shortSha }}</a>
-              </h5>
-              <span>
-                <a :href="build.url" class="issue-date">
-                  {{ build.date }}
-                </a>
-              </span>
-            </div>
-            <div class="item-time">
-              <total-time :time="build.totalTime"></total-time>
-            </div>
-          </li>
-        </ul>
+global.cycleAnalytics.StageTestComponent = Vue.extend({
+  props: {
+    items: Array,
+    stage: Object,
+  },
+  data() {
+    return { iconBuildStatus, iconBranch };
+  },
+  template: `
+    <div>
+      <div class="events-description">
+        {{ stage.description }}
+        <limit-warning :count="items.length" />
       </div>
-    `,
-  });
-})(window.gl || (window.gl = {}));
+      <ul class="stage-event-list">
+        <li v-for="build in items" class="stage-event-item item-build-component">
+          <div class="item-details">
+            <h5 class="item-title">
+              <span class="icon-build-status">${iconBuildStatus}</span>
+              <a :href="build.url" class="item-build-name">{{ build.name }}</a>
+              &middot;
+              <a :href="build.url" class="pipeline-id">#{{ build.id }}</a>
+              <i class="fa fa-code-fork"></i>
+              <a :href="build.branch.url" class="branch-name monospace">{{ build.branch.name }}</a>
+              <span class="icon-branch">${iconBranch}</span>
+              <a :href="build.commitUrl" class="short-sha monospace">{{ build.shortSha }}</a>
+            </h5>
+            <span>
+              <a :href="build.url" class="issue-date">
+                {{ build.date }}
+              </a>
+            </span>
+          </div>
+          <div class="item-time">
+            <total-time :time="build.totalTime"></total-time>
+          </div>
+        </li>
+      </ul>
+    </div>
+  `,
+});
--- app/assets/javascripts/cycle_analytics/cycle_analytics_service.js
+++ app/assets/javascripts/cycle_analytics/cycle_analytics_service.js
@@ -1,41 +1,41 @@
 /* eslint-disable no-param-reassign */
-((global) => {
-  global.cycleAnalytics = global.cycleAnalytics || {};
 
-  class CycleAnalyticsService {
-    constructor(options) {
-      this.requestPath = options.requestPath;
-    }
+const global = window.gl || (window.gl = {});
+global.cycleAnalytics = global.cycleAnalytics || {};
 
-    fetchCycleAnalyticsData(options) {
-      options = options || { startDate: 30 };
-
-      return $.ajax({
-        url: this.requestPath,
-        method: 'GET',
-        dataType: 'json',
-        contentType: 'application/json',
-        data: {
-          cycle_analytics: {
-            start_date: options.startDate,
-          },
-        },
-      });
-    }
-
-    fetchStageData(options) {
-      const {
-        stage,
-        startDate,
-      } = options;
-
-      return $.get(`${this.requestPath}/events/${stage.title.toLowerCase()}.json`, {
-        cycle_analytics: {
-          start_date: startDate,
-        },
-      });
-    }
+class CycleAnalyticsService {
+  constructor(options) {
+    this.requestPath = options.requestPath;
   }
 
-  global.cycleAnalytics.CycleAnalyticsService = CycleAnalyticsService;
-})(window.gl || (window.gl = {}));
+  fetchCycleAnalyticsData(options) {
+    options = options || { startDate: 30 };
+
+    return $.ajax({
+      url: this.requestPath,
+      method: 'GET',
+      dataType: 'json',
+      contentType: 'application/json',
+      data: {
+        cycle_analytics: {
+          start_date: options.startDate,
+        },
+      },
+    });
+  }
+
+  fetchStageData(options) {
+    const {
+      stage,
+      startDate,
+    } = options;
+
+    return $.get(`${this.requestPath}/events/${stage.title.toLowerCase()}.json`, {
+      cycle_analytics: {
+        start_date: startDate,
+      },
+    });
+  }
+}
+
+global.cycleAnalytics.CycleAnalyticsService = CycleAnalyticsService;
