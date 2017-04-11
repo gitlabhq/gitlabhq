cf678945044f657111997a92330420a0110d03fc
Luke "Jared" Bennett <lbennett@gitlab.com>
2017-04-11 13:06:45 +0100

    Finished feature specs

--- spec/features/projects/blobs/balsamiq_preview_spec.rb
+++ spec/features/projects/blobs/balsamiq_preview_spec.rb
@@ -7,18 +7,21 @@ feature 'Balsamiq preview', :feature, :js do
   let(:project) { create(:project) }
   let(:branch) { 'add-balsamiq-file' }
   let(:path) { 'files/images/balsamiq.bmpr' }
+  let(:file_content) { find('.file-content') }
 
   before do
     project.add_master(user)
 
     login_as user
 
-    p namespace_project_blob_path(project.namespace, project, tree_join(branch, path))
-
     visit namespace_project_blob_path(project.namespace, project, tree_join(branch, path))
   end
 
-  it 'should' do
-    screenshot_and_open_image
+  it 'should show a loading icon' do
+    expect(file_content).to have_selector('.loading')
+  end
+
+  it 'should show a viewer container' do
+    expect(page).to have_selector('.balsamiq-viewer')
   end
 end
