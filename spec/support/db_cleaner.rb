8ae423f7891ec3112276f253d97ed68c55234a28
James Edwards-Jones <jedwardsjones@gitlab.com>
2017-04-11 15:00:45 +0100

    Prefer DatabaseCleaner deletion over truncation in tests for performance

--- spec/support/db_cleaner.rb
+++ spec/support/db_cleaner.rb
@@ -1,10 +1,10 @@
 RSpec.configure do |config|
   config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.clean_with(:deletion)
   end
 
   config.append_after(:context) do
-    DatabaseCleaner.clean_with(:truncation, cache_tables: false)
+    DatabaseCleaner.clean_with(:deletion, cache_tables: false)
   end
 
   config.before(:each) do
@@ -12,7 +12,7 @@ RSpec.configure do |config|
   end
 
   config.before(:each, :js) do
-    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.strategy = :deletion
   end
 
   config.before(:each, :truncate) do
