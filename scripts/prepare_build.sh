11ef3154bbd7073a3083e05a79f4bf022ccd6859
Lin Jen-Shin <godfat@godfat.org>
2017-04-13 15:48:25 +0800

    Always use . and add more comments to prepare_build.sh

--- scripts/prepare_build.sh
+++ scripts/prepare_build.sh
@@ -3,9 +3,14 @@
 . scripts/utils.sh
 
 export SETUP_DB=${SETUP_DB:-true}
-export GITLAB_DATABASE=$(echo $CI_JOB_NAME | cut -f2 -d' ')
 export USE_BUNDLE_INSTALL=${USE_BUNDLE_INSTALL:-true}
 
+# Determine the database by looking at the job name.
+# For example, we'll get pg if the job is `rspec pg 19 20`
+export GITLAB_DATABASE=$(echo $CI_JOB_NAME | cut -f2 -d' ')
+
+# This would make the default database postgresql, and we could also use
+# pg to mean postgresql.
 if [ "$GITLAB_DATABASE" != 'mysql' ]; then
     export GITLAB_DATABASE='postgresql'
 fi
