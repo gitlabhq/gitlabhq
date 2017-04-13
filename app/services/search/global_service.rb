97286f4123cc49494952fc7ca51173d1dda085df
Nick Thomas <nick@gitlab.com>
2017-04-13 17:13:05 +0100

    Refactor group search out of global search

--- app/services/search/global_service.rb
+++ app/services/search/global_service.rb
@@ -9,36 +9,30 @@ module Search
     end
 
     def execute
-      group = Group.find_by(id: params[:group_id]) if params[:group_id].present?
-      projects = ProjectsFinder.new(current_user: current_user).execute
-
-      if group
-        projects = projects.inside_path(group.full_path)
-      end
-
       if current_application_settings.elasticsearch_search?
-        projects_spec =
-          if group
-            projects.pluck(:id)
-          else
-            if current_user && current_user.admin_or_auditor?
-              :any
-            elsif current_user
-              current_user.authorized_projects.pluck(:id)
-            else
-              []
-            end
-          end
-
-        Gitlab::Elastic::SearchResults.new(
-          current_user,
-          params[:search],
-          projects_spec,
-          !group # Ignore public projects outside of the group if provided
-        )
+        Gitlab::Elastic::SearchResults.new(current_user, params[:search], elastic_projects, elastic_global)
       else
         Gitlab::SearchResults.new(current_user, projects, params[:search])
       end
+    end
+
+    def projects
+      @projects ||= ProjectsFinder.new(current_user: current_user).execute
+    end
+
+    def elastic_projects
+      @elastic_projects ||=
+        if current_user.try(:admin_or_auditor?)
+          :any
+        elsif current_user
+          current_user.authorized_projects.pluck(:id)
+        else
+          []
+        end
+    end
+
+    def elastic_global
+      true
     end
 
     def scope
