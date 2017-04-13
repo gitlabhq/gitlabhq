a32adb82dc5b27069835219a5189f497686b8b04
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2017-04-13 19:00:10 -0300

    Remove sensitive information

--- lib/github/client.rb
+++ lib/github/client.rb
@@ -2,7 +2,7 @@ module Github
   class Client
     attr_reader :connection
 
-    def initialize(token = '881a01d03026458e51285a4c7038c9fe4daa5561')
+    def initialize(token)
       @connection = Faraday.new(url: 'https://api.github.com') do |faraday|
         faraday.authorization 'token', token
         faraday.adapter :net_http
