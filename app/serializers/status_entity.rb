fb20dcf8e67992cada346ff3718e078cdf5359e8
Luke "Jared" Bennett <lbennett@gitlab.com>
2017-04-14 11:34:50 +0100

    Remove unneeded format block

--- app/serializers/status_entity.rb
+++ app/serializers/status_entity.rb
@@ -1,10 +1,6 @@
 class StatusEntity < Grape::Entity
   include RequestAwareEntity
 
-  format_with(:status_favicon_path) do |favicon_name|
-    ci_status_favicon_path(favicon_name)
-  end
-
   expose :icon, :text, :label, :group
 
   expose :has_details?, as: :has_details
