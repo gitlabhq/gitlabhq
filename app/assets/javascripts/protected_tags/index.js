7a0c182839c74aa3be36c7378b28b308e7ceae87
kushalpandya <kushal@gitlab.com>
2017-04-17 19:04:38 +0530

    Add enums for `ACCESS_LEVELS` and `LEVEL_TYPES`

--- app/assets/javascripts/protected_tags/index.js
+++ app/assets/javascripts/protected_tags/index.js
@@ -1,2 +1,12 @@
 export { default as ProtectedTagCreate } from './protected_tag_create';
 export { default as ProtectedTagEditList } from './protected_tag_edit_list';
+
+export const ACCESS_LEVELS = {
+  CREATE: 'create_access_levels',
+};
+
+export const LEVEL_TYPES = {
+  ROLE: 'role',
+  USER: 'user',
+  GROUP: 'group',
+};
