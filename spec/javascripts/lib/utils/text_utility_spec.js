904c7242f8b6f67d8083d8cc5529dec4f64bfe33
Phil Hughes <me@iamphill.com>
2017-04-18 12:21:53 +0100

    Fixed conflicts

--- spec/javascripts/lib/utils/text_utility_spec.js
+++ spec/javascripts/lib/utils/text_utility_spec.js
@@ -4,8 +4,6 @@ describe('text_utility', () => {
   describe('gl.text.getTextWidth', () => {
     it('returns zero width when no text is passed', () => {
       expect(gl.text.getTextWidth('')).toBe(0);
-<<<<<<< HEAD
-=======
     });
 
     it('returns zero width when no text is passed and font is passed', () => {
@@ -16,50 +14,6 @@ describe('text_utility', () => {
       expect(gl.text.getTextWidth('foo') > 0).toBe(true);
     });
 
-    it('returns bigger width when font is larger', () => {
-      const largeFont = gl.text.getTextWidth('foo', '100px sans-serif');
-      const regular = gl.text.getTextWidth('foo', '10px sans-serif');
-      expect(largeFont > regular).toBe(true);
->>>>>>> upstream/master
-    });
-  });
-
-<<<<<<< HEAD
-    it('returns zero width when no text is passed and font is passed', () => {
-      expect(gl.text.getTextWidth('', '100px sans-serif')).toBe(0);
-=======
-  describe('gl.text.pluralize', () => {
-    it('returns pluralized', () => {
-      expect(gl.text.pluralize('test', 2)).toBe('tests');
-    });
-
-    it('returns pluralized when count is 0', () => {
-      expect(gl.text.pluralize('test', 0)).toBe('tests');
-    });
-
-    it('does not return pluralized', () => {
-      expect(gl.text.pluralize('test', 1)).toBe('test');
->>>>>>> upstream/master
-    });
-  });
-
-<<<<<<< HEAD
-    it('returns width when text is passed', () => {
-      expect(gl.text.getTextWidth('foo') > 0).toBe(true);
-=======
-  describe('gl.text.highCountTrim', () => {
-    it('returns 99+ for count >= 100', () => {
-      expect(gl.text.highCountTrim(105)).toBe('99+');
-      expect(gl.text.highCountTrim(100)).toBe('99+');
-    });
-
-    it('returns exact number for count < 100', () => {
-      expect(gl.text.highCountTrim(45)).toBe(45);
->>>>>>> upstream/master
-    });
-  });
-
-<<<<<<< HEAD
     it('returns bigger width when font is larger', () => {
       const largeFont = gl.text.getTextWidth('foo', '100px sans-serif');
       const regular = gl.text.getTextWidth('foo', '10px sans-serif');
@@ -89,7 +43,9 @@ describe('text_utility', () => {
 
     it('returns exact number for count < 100', () => {
       expect(gl.text.highCountTrim(45)).toBe(45);
-=======
+    });
+  });
+
   describe('gl.text.insertText', () => {
     let textArea;
 
@@ -147,7 +103,6 @@ describe('text_utility', () => {
 
         expect(textArea.value).toEqual(`${initialValue}* `);
       });
->>>>>>> upstream/master
     });
   });
 });
