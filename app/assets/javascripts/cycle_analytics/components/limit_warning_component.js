7d16537cac7eb808d8d18cd0b89475db4e4eeaaa
Phil Hughes <me@iamphill.com>
2017-04-19 16:53:06 +0100

    Created a plural filter Added tests for the filter
    [ci skip]

--- app/assets/javascripts/cycle_analytics/components/limit_warning_component.js
+++ app/assets/javascripts/cycle_analytics/components/limit_warning_component.js
@@ -11,7 +11,7 @@ export default {
           aria-hidden="true"
           title="Limited to showing 50 events at most"
           data-placement="top"></i>
-      {{ 'Showing 50 events' | translate }}
+      {{ 'Showing %d event' | translate-plural('Showing %d events', 50) }}
     </span>
   `,
 };
--- app/assets/javascripts/vue_shared/translate.js
+++ app/assets/javascripts/vue_shared/translate.js
@@ -3,6 +3,9 @@ import locale from '../locale';
 export default (Vue) => {
   Vue.filter('translate', text => locale.gettext(text));
 
+  Vue.filter('translate-plural', (text, pluralText, count) =>
+    locale.ngettext(text, pluralText, count).replace(/%d/g, count));
+
   Vue.directive('translate', {
     bind(el) {
       const $el = el;
