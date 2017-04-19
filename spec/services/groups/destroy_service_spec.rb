930e8c03a2da6c6dd55dd67ebbd2be3589e47b02
Yorick Peterse <yorickpeterse@gmail.com>
2017-04-19 11:53:04 +0000

    Merge branch 'fix/orphan-notification-settings' into 'master'
    Fix orphaned notification settings
    
    Closes #29688
    
    See merge request !10763

--- spec/services/groups/destroy_service_spec.rb
+++ spec/services/groups/destroy_service_spec.rb
@@ -6,7 +6,8 @@ describe Groups::DestroyService, services: true do
   let!(:user)         { create(:user) }
   let!(:group)        { create(:group) }
   let!(:nested_group) { create(:group, parent: group) }
-  let!(:project)      { create(:project, namespace: group) }
+  let!(:project)      { create(:empty_project, namespace: group) }
+  let!(:notification_setting) { create(:notification_setting, source: group)}
   let!(:gitlab_shell) { Gitlab::Shell.new }
   let!(:remove_path)  { group.path + "+#{group.id}+deleted" }
 
@@ -23,6 +24,7 @@ describe Groups::DestroyService, services: true do
       it { expect(Group.unscoped.all).not_to include(group) }
       it { expect(Group.unscoped.all).not_to include(nested_group) }
       it { expect(Project.unscoped.all).not_to include(project) }
+      it { expect(NotificationSetting.unscoped.all).not_to include(notification_setting) }
     end
 
     context 'file system' do
