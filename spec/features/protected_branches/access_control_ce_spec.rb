ceb7b902be49fde97b31cc3a22bdc9ca4f66146d
kushalpandya <kushal@gitlab.com>
2017-04-19 17:54:06 +0530

    Update selector class

--- spec/features/protected_branches/access_control_ce_spec.rb
+++ spec/features/protected_branches/access_control_ce_spec.rb
@@ -5,7 +5,7 @@ RSpec.shared_examples "protected branches > access control > CE" do
 
       set_protected_branch_name('master')
 
-      within('.new_protected_branch') do
+      within('.js-new-protected-branch') do
         allowed_to_push_button = find(".js-allowed-to-push")
 
         unless allowed_to_push_button.text == access_type_name
@@ -31,7 +31,7 @@ RSpec.shared_examples "protected branches > access control > CE" do
 
       within(".protected-branches-list") do
         find(".js-allowed-to-push").click
-        
+
         within('.js-allowed-to-push-container') do
           expect(first("li")).to have_content("Roles")
           click_on access_type_name
@@ -50,7 +50,7 @@ RSpec.shared_examples "protected branches > access control > CE" do
 
       set_protected_branch_name('master')
 
-      within('.new_protected_branch') do
+      within('.js-new-protected-branch') do
         allowed_to_merge_button = find(".js-allowed-to-merge")
 
         unless allowed_to_merge_button.text == access_type_name
