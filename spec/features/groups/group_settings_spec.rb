f4a2dfb46f168d3fd7309aca8631cf680456fa82
Michael Kozono <mkozono@gmail.com>
2017-04-21 14:05:19 -0700

    Add happy path feature tests for redirect behavior

---
+++ spec/features/groups/group_settings_spec.rb
@@ -0,0 +1,77 @@
+require 'spec_helper'
+
+feature 'Edit group settings', feature: true do
+  given(:user)  { create(:user) }
+  given(:group) { create(:group, path: 'foo') }
+
+  background do
+    group.add_owner(user)
+    login_as(user)
+  end
+
+  describe 'when the group path is changed' do
+    let(:new_group_path) { 'bar' }
+    let(:old_group_full_path) { "/#{group.path}" }
+    let(:new_group_full_path) { "/#{new_group_path}" }
+
+    scenario 'the group is accessible via the new path' do
+      update_path(new_group_path)
+      visit new_group_full_path
+      expect(current_path).to eq(new_group_full_path)
+      expect(find('h1.group-title')).to have_content(new_group_path)
+    end
+
+    scenario 'the old group path redirects to the new path' do
+      update_path(new_group_path)
+      visit old_group_full_path
+      expect(current_path).to eq(new_group_full_path)
+      expect(find('h1.group-title')).to have_content(new_group_path)
+    end
+
+    context 'with a subgroup' do
+      given!(:subgroup) { create(:group, parent: group, path: 'subgroup') }
+      given(:old_subgroup_full_path) { "/#{group.path}/#{subgroup.path}" }
+      given(:new_subgroup_full_path) { "/#{new_group_path}/#{subgroup.path}" }
+
+      scenario 'the subgroup is accessible via the new path' do
+        update_path(new_group_path)
+        visit new_subgroup_full_path
+        expect(current_path).to eq(new_subgroup_full_path)
+        expect(find('h1.group-title')).to have_content(subgroup.path)
+      end
+
+      scenario 'the old subgroup path redirects to the new path' do
+        update_path(new_group_path)
+        visit old_subgroup_full_path
+        expect(current_path).to eq(new_subgroup_full_path)
+        expect(find('h1.group-title')).to have_content(subgroup.path)
+      end
+    end
+
+    context 'with a project' do
+      given!(:project) { create(:project, group: group, path: 'project') }
+      given(:old_project_full_path) { "/#{group.path}/#{project.path}" }
+      given(:new_project_full_path) { "/#{new_group_path}/#{project.path}" }
+
+      scenario 'the project is accessible via the new path' do
+        update_path(new_group_path)
+        visit new_project_full_path
+        expect(current_path).to eq(new_project_full_path)
+        expect(find('h1.project-title')).to have_content(project.name)
+      end
+
+      scenario 'the old project path redirects to the new path' do
+        update_path(new_group_path)
+        visit old_project_full_path
+        expect(current_path).to eq(new_project_full_path)
+        expect(find('h1.project-title')).to have_content(project.name)
+      end
+    end
+  end
+end
+
+def update_path(new_group_path)
+  visit edit_group_path(group)
+  fill_in 'group_path', with: new_group_path
+  click_button 'Save group'
+end
--- spec/features/projects/project_settings_spec.rb
+++ spec/features/projects/project_settings_spec.rb
@@ -1,64 +1,154 @@
 require 'spec_helper'
 
 describe 'Edit Project Settings', feature: true do
+  include Select2Helper
+
   let(:user) { create(:user) }
-  let(:project) { create(:empty_project, path: 'gitlab', name: 'sample') }
+  let(:project) { create(:empty_project, namespace: user.namespace, path: 'gitlab', name: 'sample') }
 
   before do
     login_as(user)
-    project.team << [user, :master]
   end
 
-  describe 'Project settings', js: true do
+  describe 'Project settings section', js: true do
     it 'shows errors for invalid project name' do
       visit edit_namespace_project_path(project.namespace, project)
-
       fill_in 'project_name_edit', with: 'foo&bar'
-
       click_button 'Save changes'
-
       expect(page).to have_field 'project_name_edit', with: 'foo&bar'
       expect(page).to have_content "Name can contain only letters, digits, emojis, '_', '.', dash, space. It must start with letter, digit, emoji or '_'."
       expect(page).to have_button 'Save changes'
     end
 
-    scenario 'shows a successful notice when the project is updated' do
+    it 'shows a successful notice when the project is updated' do
       visit edit_namespace_project_path(project.namespace, project)
-
       fill_in 'project_name_edit', with: 'hello world'
-
       click_button 'Save changes'
-
       expect(page).to have_content "Project 'hello world' was successfully updated."
     end
   end
 
-  describe 'Rename repository' do
-    it 'shows errors for invalid project path/name' do
-      visit edit_namespace_project_path(project.namespace, project)
+  describe 'Rename repository section' do
+    context 'with invalid characters' do
+      it 'shows errors for invalid project path/name' do
+        rename_project(project, name: 'foo&bar', path: 'foo&bar')
+        expect(page).to have_field 'Project name', with: 'foo&bar'
+        expect(page).to have_field 'Path', with: 'foo&bar'
+        expect(page).to have_content "Name can contain only letters, digits, emojis, '_', '.', dash, space. It must start with letter, digit, emoji or '_'."
+        expect(page).to have_content "Path can contain only letters, digits, '_', '-' and '.'. Cannot start with '-', end in '.git' or end in '.atom'"
+      end
+    end
 
-      fill_in 'project_name', with: 'foo&bar'
-      fill_in 'Path', with: 'foo&bar'
+    context 'when changing project name' do
+      it 'renames the repository' do
+        rename_project(project, name: 'bar')
+        expect(find('h1.title')).to have_content(project.name)
+      end
 
-      click_button 'Rename project'
+      context 'with emojis' do
+        it 'shows error for invalid project name' do
+          rename_project(project, name: '🚀 foo bar ☁️')
+          expect(page).to have_field 'Project name', with: '🚀 foo bar ☁️'
+          expect(page).not_to have_content "Name can contain only letters, digits, emojis '_', '.', dash and space. It must start with letter, digit, emoji or '_'."
+        end
+      end
+    end
 
-      expect(page).to have_field 'Project name', with: 'foo&bar'
-      expect(page).to have_field 'Path', with: 'foo&bar'
-      expect(page).to have_content "Name can contain only letters, digits, emojis, '_', '.', dash, space. It must start with letter, digit, emoji or '_'."
-      expect(page).to have_content "Path can contain only letters, digits, '_', '-' and '.'. Cannot start with '-', end in '.git' or end in '.atom'"
+    context 'when changing project path' do
+      # Not using empty project because we need a repo to exist
+      let(:project) { create(:project, namespace: user.namespace, name: 'gitlabhq') }
+
+      specify 'the project is accessible via the new path' do
+        rename_project(project, path: 'bar')
+        new_path = namespace_project_path(project.namespace, 'bar')
+        visit new_path
+        expect(current_path).to eq(new_path)
+        expect(find('h1.title')).to have_content(project.name)
+      end
+
+      specify 'the project is accessible via a redirect from the old path' do
+        old_path = namespace_project_path(project.namespace, project)
+        rename_project(project, path: 'bar')
+        new_path = namespace_project_path(project.namespace, 'bar')
+        visit old_path
+        expect(current_path).to eq(new_path)
+        expect(find('h1.title')).to have_content(project.name)
+      end
+
+      context 'and a new project is added with the same path' do
+        it 'overrides the redirect' do
+          old_path = namespace_project_path(project.namespace, project)
+          rename_project(project, path: 'bar')
+          new_project = create(:empty_project, namespace: user.namespace, path: 'gitlabhq', name: 'quz')
+          visit old_path
+          expect(current_path).to eq(old_path)
+          expect(find('h1.title')).to have_content(new_project.name)
+        end
+      end
     end
   end
 
-  describe 'Rename repository name with emojis' do
-    it 'shows error for invalid project name' do
-      visit edit_namespace_project_path(project.namespace, project)
+  describe 'Transfer project section', js: true do
+    # Not using empty project because we need a repo to exist
+    let(:project) { create(:project, namespace: user.namespace, name: 'gitlabhq') }
+    let(:group) { create(:group) }
+    before do
+      group.add_owner(user)
+    end
 
-      fill_in 'project_name', with: '🚀 foo bar ☁️'
+    specify 'the project is accessible via the new path' do
+      transfer_project(project, group)
+      new_path = namespace_project_path(group, project)
+      visit new_path
+      expect(current_path).to eq(new_path)
+      expect(find('h1.title')).to have_content(project.name)
+    end
 
-      click_button 'Rename project'
+    specify 'the project is accessible via a redirect from the old path' do
+      old_path = namespace_project_path(project.namespace, project)
+      transfer_project(project, group)
+      new_path = namespace_project_path(group, project)
+      visit old_path
+      expect(current_path).to eq(new_path)
+      expect(find('h1.title')).to have_content(project.name)
+    end
 
-      expect(page).to have_field 'Project name', with: '🚀 foo bar ☁️'
-      expect(page).not_to have_content "Name can contain only letters, digits, emojis '_', '.', dash and space. It must start with letter, digit, emoji or '_'."
+    context 'and a new project is added with the same path' do
+      it 'overrides the redirect' do
+        old_path = namespace_project_path(project.namespace, project)
+        transfer_project(project, group)
+        new_project = create(:empty_project, namespace: user.namespace, path: 'gitlabhq', name: 'quz')
+        visit old_path
+        expect(current_path).to eq(old_path)
+        expect(find('h1.title')).to have_content(new_project.name)
+      end
     end
   end
+end
+
+def rename_project(project, name: nil, path: nil)
+  visit edit_namespace_project_path(project.namespace, project)
+  fill_in('project_name', with: name) if name
+  fill_in('Path', with: path) if path
+  click_button('Rename project')
+  wait_for_edit_project_page_reload
+  project.reload
+end
+
+def transfer_project(project, namespace)
+  visit edit_namespace_project_path(project.namespace, project)
+  select2(namespace.id, from: '#new_namespace_id')
+  click_button('Transfer project')
+  confirm_transfer_modal
+  wait_for_edit_project_page_reload
+  project.reload
+end
+
+def confirm_transfer_modal
+  fill_in('confirm_name_input', with: project.path)
+  click_button 'Confirm'
+end
+
+def wait_for_edit_project_page_reload
+  expect(find('.project-edit-container')).to have_content('Rename repository')
 end
