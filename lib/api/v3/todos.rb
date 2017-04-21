a204d14c672e08a825479511473ba3999ed08434
Toon Claes <toon@gitlab.com>
2017-04-21 11:36:34 +0200

    Avoid plucking Todo ids and use sub-queries instead
    TodoService should not call `.select(&:id)` on todos, because this is
    bad performance. So instead use sub-queries, which will result in a
    single SQL query to the database.
    
    https://docs.gitlab.com/ee/development/sql.html#plucking-ids

--- lib/api/v3/todos.rb
+++ lib/api/v3/todos.rb
@@ -11,10 +11,10 @@ module API
           requires :id, type: Integer, desc: 'The ID of the todo being marked as done'
         end
         delete ':id' do
+          TodoService.new.mark_todos_as_done_by_ids(params[:id], current_user)
           todo = current_user.todos.find(params[:id])
-          TodoService.new.mark_todos_as_done([todo], current_user)
 
-          present todo.reload, with: ::API::Entities::Todo, current_user: current_user
+          present todo, with: ::API::Entities::Todo, current_user: current_user
         end
 
         desc 'Mark all todos as done'
