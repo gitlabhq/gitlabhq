f5f97359456bff5226f55f5338e01120ea90cbb8
Phil Hughes <me@iamphill.com>
2017-04-21 16:32:10 +0100

    Improved the spec Now correctly tests against different forms

--- spec/javascripts/shortcuts_spec.js
+++ spec/javascripts/shortcuts_spec.js
@@ -1,29 +1,45 @@
 /* global Shortcuts */
 describe('Shortcuts', () => {
-  const fixtureName = 'issues/open-issue.html.raw';
+  const fixtureName = 'issues/issue_with_comment.html.raw';
+  const createEvent = (type, target) => $.Event(type, {
+    target,
+  });
 
   preloadFixtures(fixtureName);
 
   describe('toggleMarkdownPreview', () => {
     let sc;
-    let event;
 
     beforeEach(() => {
       loadFixtures(fixtureName);
 
-      spyOnEvent('.js-md-preview-button', 'focus');
-
-      event = $.Event('', {
-        target: document.querySelector('.js-note-text'),
-      });
+      spyOnEvent('.js-new-note-form .js-md-preview-button', 'focus');
+      spyOnEvent('.edit-note .js-md-preview-button', 'focus');
 
       sc = new Shortcuts();
     });
 
     it('focuses preview button in form', () => {
-      sc.toggleMarkdownPreview(event);
+      sc.toggleMarkdownPreview(
+        createEvent('KeyboardEvent', document.querySelector('.js-new-note-form .js-note-text'),
+      ));
 
-      expect('focus').toHaveBeenTriggeredOn('.js-md-preview-button');
+      expect('focus').toHaveBeenTriggeredOn('.js-new-note-form .js-md-preview-button');
+    });
+
+    it('focues preview button inside edit comment form', (done) => {
+      document.querySelector('.js-note-edit').click();
+
+      setTimeout(() => {
+        sc.toggleMarkdownPreview(
+          createEvent('KeyboardEvent', document.querySelector('.edit-note .js-note-text'),
+        ));
+
+        expect('focus').not.toHaveBeenTriggeredOn('.js-new-note-form .js-md-preview-button');
+        expect('focus').toHaveBeenTriggeredOn('.edit-note .js-md-preview-button');
+
+        done();
+      });
     });
   });
 });
