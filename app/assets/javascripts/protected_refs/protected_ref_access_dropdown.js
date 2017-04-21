46dd6f3d9eca8a42d7d3ed9f159bcc7274571685
kushalpandya <kushal@gitlab.com>
2017-04-21 18:13:36 +0530

    Shared ProtectedRefAccessDropdown class

---
+++ app/assets/javascripts/protected_refs/protected_ref_access_dropdown.js
@@ -0,0 +1,30 @@
+export default class ProtectedRefAccessDropdown {
+  constructor(options, { inputId, fieldName, activeCls }) {
+    this.options = options;
+    this.inputId = inputId;
+    this.fieldName = fieldName;
+    this.activeCls = activeCls;
+    this.initDropdown();
+  }
+
+  initDropdown() {
+    const { onSelect } = this.options;
+    const activeCls = this.activeCls;
+    this.options.$dropdown.glDropdown({
+      data: this.options.data,
+      selectable: true,
+      inputId: this.options.$dropdown.data(this.inputId),
+      fieldName: this.options.$dropdown.data(this.fieldName),
+      toggleLabel(item, $el) {
+        if ($el.is(activeCls)) {
+          return item.text;
+        }
+        return 'Select';
+      },
+      clicked(item, $el, e) {
+        e.preventDefault();
+        onSelect();
+      },
+    });
+  }
+}
