8a5b4e2f739bc0f43a568621d9f32d71a1de0256
Oswaldo Ferreira <oswaldo@gitlab.com>
2017-04-24 10:44:55 -0300

    Add RelatedIssue model

---
+++ app/models/related_issue.rb
@@ -0,0 +1,9 @@
+class RelatedIssue < ActiveRecord::Base
+  belongs_to :issue
+  belongs_to :related_issue, class_name: 'Issue'
+
+  validates :issue, presence: true, uniqueness: { scope: :related_issue_id }
+  validates :related_issue, presence: true
+
+  validates :issue_id, uniqueness: { scope: :related_issue_id }
+end
---
+++ spec/models/related_issue_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe RelatedIssue do
+  describe "Associations" do
+    it { is_expected.to belong_to(:issue) }
+    it { is_expected.to belong_to(:related_issue).class_name('Issue') }
+  end
+
+  describe "Validation" do
+    subject { build :related_issue }
+
+    it { is_expected.to validate_presence_of(:issue) }
+    it { is_expected.to validate_presence_of(:related_issue) }
+    it { is_expected.to validate_uniqueness_of(:issue_id).scoped_to(:related_issue_id) }
+  end
+end
