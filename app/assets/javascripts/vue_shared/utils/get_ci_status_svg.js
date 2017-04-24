0566a898045e675038e437db89875578a248b9c0
Bryce Johnson <bryce@gitlab.com>
2017-04-24 19:20:16 -0400

    Prevent status svg methods from blowing up when icon is undefined.

--- app/assets/javascripts/vue_shared/utils/get_ci_status_svg.js
+++ app/assets/javascripts/vue_shared/utils/get_ci_status_svg.js
@@ -1,19 +1,13 @@
 import { borderlessIcons, baseIcons } from './ci_status_svg_index';
 
-export function normalizeStatus(status) {
+export function normalizeStatus(status = '') {
   // Supports the same syntax as the existing ruby helper and plain status strings.
   const legacyPrefix = /icon_status_/gi;
   return status.replace(legacyPrefix, '');
 }
 
-export function getCiStatusSvg({ status, borderless }) {
+export function getCiStatusSvg({ status = 'canceled', borderless }) {
   const normalizedStatus = normalizeStatus(status);
 
-  const fallbackStatus = 'canceled';
-
-  if (borderless) {
-    return borderlessIcons[normalizedStatus] || borderlessIcons[fallbackStatus];
-  }
-
-  return baseIcons[normalizedStatus] || baseIcons[fallbackStatus];
+  return borderless ? borderlessIcons[normalizedStatus] : baseIcons[normalizedStatus];
 }
