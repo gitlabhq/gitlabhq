8ebeb3e709e75f67fb82a1d06c395343554e5207
Bryce Johnson <bryce@gitlab.com>
2017-04-24 14:22:35 -0400

    Remove status list for get_ci_status_spec.

--- spec/javascripts/vue_shared/utils/ci_status_svg_index_spec.js
+++ spec/javascripts/vue_shared/utils/ci_status_svg_index_spec.js
@@ -1,29 +1,14 @@
 import { borderlessIcons, baseIcons } from '~/vue_shared/utils/ci_status_svg_index';
 
 describe('CI status icons index', () => {
-  const statuses = [
-    'canceled',
-    'created',
-    'failed',
-    'manual',
-    'pending',
-    'running',
-    'skipped',
-    'success',
-    'passed',
-    'warning',
-    'success_with_warnings',
-    'blocked',
-  ];
-
-  statuses.forEach((status) => {
+  _.each(borderlessIcons, (svg, status) => {
     it(`should export a borderless ${status} svg`, () => {
       expect(Object.hasOwnProperty.call(borderlessIcons, status)).toBe(true);
       expect(borderlessIcons[status]).toMatch(/^<svg/);
     });
   });
 
-  statuses.forEach((status) => {
+  _.each(baseIcons, (svg, status) => {
     it(`should export a base ${status} svg`, () => {
       expect(Object.hasOwnProperty.call(baseIcons, status)).toBe(true);
       expect(baseIcons[status]).toMatch(/^<svg/);
