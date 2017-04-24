8be840f42caa6632d74bdbd1cbf386847b32b6e8
James Lopez <james@jameslopez.es>
2017-04-24 15:13:43 +0200

    Fix missing project attributes used by the Import/Export feature
    Improve AR replace_records debugging
    Fix weird description_html export issue  - ignored by to_json
    Fix extra attributes

--- app/models/project.rb
+++ app/models/project.rb
@@ -1268,6 +1268,9 @@ class Project < ActiveRecord::Base
     else
       update_attribute(name, value)
     end
+
+  rescue ActiveRecord::RecordNotSaved => e
+    handle_update_attribute_error(e, value)
   end
 
   def pushes_since_gc
@@ -1380,5 +1383,17 @@ class Project < ActiveRecord::Base
     return false unless Gitlab.config.registry.enabled
 
     ContainerRepository.build_root_repository(self).has_tags?
+  end
+
+  def handle_update_attribute_error(e, value)
+    if e.message.start_with?('Failed to replace')
+      if value.respond_to?(:each)
+        invalid = value.detect(&:invalid?)
+
+        raise $!, ([$!] + invalid.errors.full_messages).join(' ') if invalid
+      end
+    end
+
+    raise e
   end
 end
