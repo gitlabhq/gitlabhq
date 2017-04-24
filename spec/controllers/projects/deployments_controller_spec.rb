5bf0441fd9cda2dcc3c3ef1ce3084df5ed87602d
Pawel Chojnacki <pawel@chojnacki.ws>
2017-04-24 19:55:07 +0200

    Cleanup deploymentsd controller spec and use schema validation to test the output

--- spec/controllers/projects/deployments_controller_spec.rb
+++ spec/controllers/projects/deployments_controller_spec.rb
@@ -6,16 +6,15 @@ describe Projects::DeploymentsController do
   let(:user) { create(:user) }
   let(:project) { create(:empty_project) }
   let(:environment) { create(:environment, name: 'production', project: project) }
-  let(:deployment) { create(:deployment, project: project, environment: environment) }
 
   before do
-    project.team << [user, :master]
+    project.add_master(user)
 
     sign_in(user)
   end
 
   describe 'GET #index' do
-    it 'returns list of deployments withing last 8 hours' do
+    it 'returns list of deployments from last 8 hours' do
       create(:deployment, environment: environment, created_at: 9.hours.ago)
       create(:deployment, environment: environment, created_at: 7.hours.ago)
       create(:deployment, environment: environment)
@@ -31,13 +30,9 @@ describe Projects::DeploymentsController do
       create(:deployment, environment: environment)
 
       get :index, environment_params
+
       expect(response).to be_ok
-
-      deployments = json_response['deployments']
-      deployment_info = deployments.first.with_indifferent_access
-
-      expect(deployments.count).to eq(1)
-      expect(deployment_info).to include(:id, :iid, :sha, :ref, :tag, :created_at)
+      expect(response).to match_response_schema('deployments')
     end
   end
 
