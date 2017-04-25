ab7e59122437cbf64d731b6a27304d0f96dd2b2c
Regis <boudinot.regis@yahoo.com>
2017-04-25 12:48:00 -0600

    formatting in test

--- spec/javascripts/issue_show/issue_title_description_spec.js
+++ spec/javascripts/issue_show/issue_title_description_spec.js
@@ -41,12 +41,19 @@ describe('Issue Title', () => {
     setTimeout(() => {
       expect(document.querySelector('title').innerText)
         .toContain('this is a title');
+
       expect(issueShowComponent.$el.querySelector('.title').innerHTML)
         .toContain('<p>this is a title</p>');
+
       expect(issueShowComponent.$el.querySelector('.wiki').innerHTML)
         .toContain('<p>this is a description!</p>');
-      expect(issueShowComponent.$el.querySelector('.js-task-list-field').innerText)
+
+      const hiddenText = issueShowComponent.$el
+        .querySelector('.js-task-list-field').innerText;
+
+      expect(hiddenText)
         .toContain('this is a description');
+
       done();
     }, 10);
     // 10ms is just long enough for the update hook to fire
