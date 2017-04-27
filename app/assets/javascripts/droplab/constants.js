abde62b53ed993b4ceec778d4fb839fa0132c165
Luke "Jared" Bennett <lbennett@gitlab.com>
2017-04-27 17:06:55 +0100

    [ci skip] Add comment to regex

--- app/assets/javascripts/droplab/constants.js
+++ app/assets/javascripts/droplab/constants.js
@@ -2,8 +2,9 @@ const DATA_TRIGGER = 'data-dropdown-trigger';
 const DATA_DROPDOWN = 'data-dropdown';
 const SELECTED_CLASS = 'droplab-item-selected';
 const ACTIVE_CLASS = 'droplab-item-active';
-const TEMPLATE_REGEX = /\{\{(.+?)\}\}/g;
 const IGNORE_CLASS = 'droplab-item-ignore';
+// Matches `{{anything}}` and `{{ everything }}`.
+const TEMPLATE_REGEX = /\{\{(.+?)\}\}/g;
 
 export {
   DATA_TRIGGER,
