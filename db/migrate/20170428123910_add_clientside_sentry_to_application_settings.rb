637ed8a21e9f9457d1b194f9c591a0813c20cc3e
Luke "Jared" Bennett <lbennett@gitlab.com>
2017-04-28 18:09:55 +0100

    Added migration downtime tag

--- db/migrate/20170428123910_add_clientside_sentry_to_application_settings.rb
+++ db/migrate/20170428123910_add_clientside_sentry_to_application_settings.rb
@@ -1,5 +1,8 @@
 # rubocop:disable all
 class AddClientsideSentryToApplicationSettings < ActiveRecord::Migration
+  DOWNTIME = true
+  DOWNTIME_REASON = 'This migration requires downtime because we must add 2 new columns, 1 of which has a default value.'
+
   def change
     change_table :application_settings do |t|
       t.boolean :clientside_sentry_enabled, default: false
