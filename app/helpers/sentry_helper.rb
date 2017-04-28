8e8be5d1710f246bbb43c6c0bae82dead1139fde
Luke "Jared" Bennett <lbennett@gitlab.com>
2017-04-28 13:51:45 +0100

    Fixed view to correct property

--- app/helpers/sentry_helper.rb
+++ app/helpers/sentry_helper.rb
@@ -6,10 +6,4 @@ module SentryHelper
   def sentry_context
     Gitlab::Sentry.context(current_user)
   end
-
-  def clientside_sentry_enabled?
-    current_application_settings.clientside_sentry_enabled
-  end
-
-  delegate :clientside_sentry_dsn, to: :current_application_settings
 end
