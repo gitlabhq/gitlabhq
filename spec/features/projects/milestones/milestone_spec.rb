77d924057bcaaf92b641596a98bfe89bcd1243f1
Luke "Jared" Bennett <lbennett@gitlab.com>
2017-04-28 13:29:47 +0100

    add issues and merge-requests classes and use to find in rspec

--- spec/features/projects/milestones/milestone_spec.rb
+++ spec/features/projects/milestones/milestone_spec.rb
@@ -78,11 +78,10 @@ feature 'Project milestone', :feature do
 
       it 'shows the total MR and issue counts' do
         find('.milestone-sidebar .block', match: :first)
-        blocks = all('.milestone-sidebar .block')
 
         aggregate_failures 'MR and issue blocks' do
-          expect(blocks[3]).to have_content 1
-          expect(blocks[4]).to have_content 0
+          expect(find('.milestone-sidebar .block.issues')).to have_content 1
+          expect(find('.milestone-sidebar .block.merge-requests')).to have_content 0
         end
       end
     end
