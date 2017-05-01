82941cf8f25bf596cf302c824beba0fa7ba69a90
Annabel Dunstone Gray <annabel.dunstone@gmail.com>
2017-05-01 15:39:12 -0500

    Improve UX of geo page

--- app/helpers/ee/geo_helper.rb
+++ app/helpers/ee/geo_helper.rb
@@ -1,29 +1,35 @@
 module EE
   module GeoHelper
     def node_status_icon(node)
-      if node.primary?
-        icon 'star fw', class: 'has-tooltip', title: 'Primary node'
-      else
+      unless node.primary?
         status = node.enabled? ? 'healthy' : 'disabled'
 
-        icon 'globe fw',
-             class: "js-geo-node-icon geo-node-icon-#{status} has-tooltip",
+        icon 'check fw',
+             class: "js-geo-node-icon geo-node-#{status} has-tooltip",
              title: status.capitalize
+      end
+    end
+
+    def node_status(node)
+      status = node.enabled? ? 'healthy' : 'disabled'
+
+      content_tag :span, class: "geo-node-#{status}" do
+        status.capitalize
       end
     end
 
     def toggle_node_button(node)
       btn_class, title, data =
         if node.enabled?
-          ['warning', 'Disable node', { confirm: 'Disabling a node stops the sync process. Are you sure?' }]
+          ['warning', 'Disable', { confirm: 'Disabling a node stops the sync process. Are you sure?' }]
         else
-          ['success', 'Enable node']
+          ['success', 'Enable']
         end
 
-      link_to icon('power-off fw', text: title),
+      link_to title,
               toggle_admin_geo_node_path(node),
               method: :post,
-              class: "btn btn-sm btn-#{btn_class} prepend-left-10 has-tooltip",
+              class: "btn btn-sm btn-#{btn_class}",
               title: title,
               data: data
     end
