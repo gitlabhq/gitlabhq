1df50a113619b5e5c0c13db30fb0f5b4a2b2b34d
Toon Claes <toon@gitlab.com>
2017-05-02 13:59:18 +0200

    Add post-deploy migrate to cleanup projects in pending delete state
    There are many projects in `pending_delete` state, this post-deploy migration
    cleans them up.
    
    The script is based on https://gitlab.com/gitlab-org/gitlab-ce/snippets/1648654
    and https://gitlab.com/gitlab-org/gitlab-ce/snippets/1611429. The use of these
    scripts were described in
    https://gitlab.com/gitlab-com/infrastructure/issues/888.

---
+++ db/post_migrate/20170502101023_clean_up_pending_delete_projects.rb
@@ -0,0 +1,47 @@
+# See http://doc.gitlab.com/ce/development/migration_style_guide.html
+# for more information on how to write migrations for GitLab.
+
+class CleanUpPendingDeleteProjects < ActiveRecord::Migration
+  include Gitlab::Database::MigrationHelpers
+
+  DOWNTIME = false
+
+  disable_ddl_transaction!
+
+  def up
+    admin = User.find_by(admin: true)
+    return unless admin
+
+    Project.unscoped.where(pending_delete: true).each { |project| delete_project(project, admin) }
+  end
+
+  def down
+    # noop
+  end
+
+  private
+
+  def delete_project(project, user)
+    project.team.truncate
+
+    unlink_fork(project) if project.forked?
+
+    [:events, :issues, :merge_requests, :labels, :milestones, :notes, :snippets].each do |thing|
+      project.send(thing).delete_all
+    end
+
+    # Override Project#remove_pages for this instance so it doesn't do anything
+    def project.remove_pages
+    end
+
+    project.destroy!
+  end
+
+  def unlink_fork(project)
+    merge_requests = project.forked_from_project.merge_requests.opened.from_project(project)
+
+    merge_requests.update_all(state: 'closed')
+
+    project.forked_project_link.destroy
+  end
+end
