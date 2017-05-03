5ffc8ffc345268e31300129f4af169cf46ac9bd6
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2017-05-02 21:56:03 -0300

    Add index to last_repository_updated_at on projects

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170503004125) do
+ActiveRecord::Schema.define(version: 20170503004425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1134,6 +1134,7 @@ ActiveRecord::Schema.define(version: 20170503004125) do
   add_index "projects", ["description"], name: "index_projects_on_description_trigram", using: :gin, opclasses: {"description"=>"gin_trgm_ops"}
   add_index "projects", ["last_activity_at"], name: "index_projects_on_last_activity_at", using: :btree
   add_index "projects", ["last_repository_check_failed"], name: "index_projects_on_last_repository_check_failed", using: :btree
+  add_index "projects", ["last_repository_updated_at"], name: "index_projects_on_last_repository_updated_at", using: :btree
   add_index "projects", ["mirror_last_successful_update_at"], name: "index_projects_on_mirror_last_successful_update_at", using: :btree
   add_index "projects", ["name"], name: "index_projects_on_name_trigram", using: :gin, opclasses: {"name"=>"gin_trgm_ops"}
   add_index "projects", ["namespace_id"], name: "index_projects_on_namespace_id", using: :btree
