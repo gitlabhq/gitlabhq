0670055fba3d4102d13033b998bfefced62c6574
Nick Thomas <nick@gitlab.com>
2017-05-03 12:40:14 +0100

    Import the gitlab-elasticsearch-git gem unmodified

---
+++ lib/elasticsearch/git.rb
@@ -0,0 +1,9 @@
+require "elasticsearch/git/version"
+require "elasticsearch/git/model"
+require "elasticsearch/git/repository"
+
+module Elasticsearch
+  module Git
+  end
+end
+
---
+++ lib/elasticsearch/git/lite_blob.rb
@@ -0,0 +1,28 @@
+require 'linguist'
+require 'elasticsearch/git/encoder_helper'
+
+module Elasticsearch
+  module Git
+    class LiteBlob
+      include Linguist::BlobHelper
+      include Elasticsearch::Git::EncoderHelper
+
+      attr_accessor :id, :name, :path, :size, :mode, :commit_id
+      attr_writer :data
+
+      def initialize(repo, raw_blob_hash)
+        @id   = raw_blob_hash[:oid]
+        @blob  = repo.lookup(@id)
+
+        @mode = raw_blob_hash[:mode].to_s(8)
+        @size = @blob.size
+        @path = encode!(raw_blob_hash[:path])
+        @name = @path.split('/').last
+      end
+
+      def data
+        @data ||= encode!(@blob.content)
+      end
+    end
+  end
+end
