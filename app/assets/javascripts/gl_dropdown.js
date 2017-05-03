b74683eef78c654f5bbce3f50a06232e5b97c94b
Robert Speicher <robert@gitlab.com>
2017-05-03 14:28:46 +0000

    Merge branch 'branch-name-escape' into 'security'
    Fix XSS in branches dropdown
    
    See merge request !2093

--- app/assets/javascripts/gl_dropdown.js
+++ app/assets/javascripts/gl_dropdown.js
@@ -581,7 +581,7 @@
         var link = document.createElement('a');
 
         link.href = url;
-        link.innerHTML = text;
+        link.textContent = text;
 
         if (selected) {
           link.className = 'is-active';
