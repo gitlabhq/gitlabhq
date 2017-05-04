1cdd33ca1e65a8e39223c10dee70389fbef3d1b8
James Edwards-Jones <jedwardsjones@gitlab.com>
2017-05-04 14:03:39 +0100

    Use Concern#class_methods in ProtectedRef instead of “def self.”

--- app/models/concerns/protected_ref.rb
+++ app/models/concerns/protected_ref.rb
@@ -8,8 +8,14 @@ module ProtectedRef
     validates :project, presence: true
 
     delegate :matching, :matches?, :wildcard?, to: :ref_matcher
+  end
 
-    def self.protected_ref_access_levels(*types)
+  def commit
+    project.commit(self.name)
+  end
+
+  class_methods do
+    def protected_ref_access_levels(*types)
       types.each do |type|
         has_many :"#{type}_access_levels", dependent: :destroy
 
@@ -25,29 +31,25 @@ module ProtectedRef
       end
     end
 
-    def self.protected_ref_accessible_to?(ref, user, action:)
+    def protected_ref_accessible_to?(ref, user, action:)
       access_levels_for_ref(ref, action: action).any? do |access_level|
         access_level.check_access(user)
       end
     end
 
-    def self.developers_can?(action, ref)
+    def developers_can?(action, ref)
       access_levels_for_ref(ref, action: action).any? do |access_level|
         access_level.access_level == Gitlab::Access::DEVELOPER
       end
     end
 
-    def self.access_levels_for_ref(ref, action:)
+    def access_levels_for_ref(ref, action:)
       self.matching(ref).map(&:"#{action}_access_levels").flatten
     end
 
-    def self.matching(ref_name, protected_refs: nil)
+    def matching(ref_name, protected_refs: nil)
       ProtectedRefMatcher.matching(self, ref_name, protected_refs: protected_refs)
     end
-  end
-
-  def commit
-    project.commit(self.name)
   end
 
   private
