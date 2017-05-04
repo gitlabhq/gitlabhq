b64a37c4ed5561d423ee607f9821b75fd0337168
Alfredo Sumaran <alfredo@gitlab.com>
2017-05-04 08:09:21 +0000

    Allow to create new branch and empty WIP merge request from issue page

--- config/routes/project.rb
+++ config/routes/project.rb
@@ -234,6 +234,7 @@ constraints(ProjectUrlConstrainer.new) do
           get :related_branches
           get :can_create_branch
           get :rendered_title
+          post :create_merge_request
         end
         collection do
           post  :bulk_update
