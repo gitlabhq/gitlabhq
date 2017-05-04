cdcbe99aa749d4516f47ca9d2e7d510dc29d1c91
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2017-05-03 22:36:02 -0300

    Add last_repository_updated_at to projects

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170502091007) do
+ActiveRecord::Schema.define(version: 20170503004125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -971,6 +971,7 @@ ActiveRecord::Schema.define(version: 20170502091007) do
     t.boolean "printing_merge_request_link_enabled", default: true, null: false
     t.string "import_jid"
     t.integer "cached_markdown_version"
+    t.datetime "last_repository_updated_at"
   end
 
   add_index "projects", ["ci_id"], name: "index_projects_on_ci_id", using: :btree
