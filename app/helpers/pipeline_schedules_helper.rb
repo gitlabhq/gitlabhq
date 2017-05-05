9f40d609a7096c9655cd3f33d49a4768a2173fe2
Bryce Johnson <bryce@gitlab.com>
2017-05-05 10:53:23 -0400

    Move timezone data construction to a helper.

---
+++ app/helpers/pipeline_schedules_helper.rb
@@ -0,0 +1,9 @@
+module PipelineSchedulesHelper
+  def timezone_data
+    ActiveSupport::TimeZone.all.map do |timezone| 
+      { name: timezone.name, 
+        offset: timezone.utc_offset, 
+        identifier: timezone.tzinfo.identifier }.to_json
+    end
+  end
+end
