106848748/gitlab-ce2

Build failures summary page for pipelines

app/helpers/builds_helper.rb
@@ -1,4 +1,12 @@
 module BuildsHelper
+  def build_summary(build)
+    if build.has_trace?
+      build.trace.html(last_lines: 10).html_safe
+    else
+      "No build trace"
+    end
+  end
+
   def sidebar_build_class(build, current_build)
     build_class = ''
     build_class += ' active' if build.id === current_build.id

spec/features/projects/pipelines/pipeline_spec.rb
@@ -254,4 +254,41 @@ describe 'Pipeline', :feature, :js do
       it { expect(build_manual.reload).to be_pending }
     end
   end
+
+  describe 'GET /:project/pipelines/:id/failures' do
+    let(:project) { create(:project) }
+    let(:pipeline) { create(:ci_pipeline, project: project, ref: 'master', sha: project.commit.id) }
+    let(:pipeline_failues_page) { failures_namespace_project_pipeline_path(project.namespace, project, pipeline) }
+    let!(:failed_build) { create(:ci_build, :failed, pipeline: pipeline) }
+
+    context 'with failed build' do
+      before do
+        failed_build.trace.set('4 examples, 1 failure')
+        visit pipeline_failues_page
+      end
+
+      it 'shows jobs tab pane as active' do
+        expect(page).to have_css('#js-tab-failures.active')
+      end
+
+      it 'lists failed builds' do
+        expect(page).to have_content(failed_build.name)
+        expect(page).to have_content(failed_build.stage)
+      end
+
+      it 'shows build failure logs' do
+        expect(page).to have_content('4 examples, 1 failure')
+      end
+    end
+
+    context 'when missing build logs' do
+      before do
+        visit pipeline_failues_page
+      end
+
+      it 'includes failed jobs' do
+        expect(page).to have_content('No build trace')
+      end
+    end
+  end
 end

gitlabhq/gitlabhq

app/helpers/builds_helper.rb
@@ -1,4 +1,12 @@
 module BuildsHelper
+  def build_summary(build)
+    if build.has_trace?
+      build.trace.html(last_lines: 10).html_safe
+    else
+      "No job trace"
+    end
+  end
+
   def sidebar_build_class(build, current_build)
     build_class = ''
     build_class += ' active' if build.id === current_build.id

spec/features/projects/pipelines/pipeline_spec.rb
@@ -254,4 +254,42 @@ describe 'Pipeline', :feature, :js do
       it { expect(build_manual.reload).to be_pending }
     end
   end
+
+  describe 'GET /:project/pipelines/:id/failures' do
+    let(:project) { create(:project) }
+    let(:pipeline) { create(:ci_pipeline, project: project, ref: 'master', sha: project.commit.id) }
+    let(:pipeline_failures_page) { failures_namespace_project_pipeline_path(project.namespace, project, pipeline) }
+    let!(:failed_build) { create(:ci_build, :failed, pipeline: pipeline) }
+
+    context 'with failed build' do
+      before do
+        failed_build.trace.set('4 examples, 1 failure')
+
+        visit pipeline_failures_page
+      end
+
+      it 'shows jobs tab pane as active' do
+        expect(page).to have_css('#js-tab-failures.active')
+      end
+
+      it 'lists failed builds' do
+        expect(page).to have_content(failed_build.name)
+        expect(page).to have_content(failed_build.stage)
+      end
+
+      it 'shows build failure logs' do
+        expect(page).to have_content('4 examples, 1 failure')
+      end
+    end
+
+    context 'when missing build logs' do
+      before do
+        visit pipeline_failures_page
+      end
+
+      it 'includes failed jobs' do
+        expect(page).to have_content('No job trace')
+      end
+    end
+  end
 end