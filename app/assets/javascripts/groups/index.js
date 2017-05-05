d67ab685350005b83a12988845b7fb87c613b472
Alfredo Sumaran <alfredo@gitlab.com>
2017-05-04 20:49:07 -0500

    Set Groups Vue app for Dashboard page

--- app/assets/javascripts/groups/index.js
+++ app/assets/javascripts/groups/index.js
@@ -1,3 +1,11 @@
+import Vue from 'vue';
+import GroupsStore from './stores/groups_store';
+
 $(() => {
-  // Groups bundle
+  const groupsStore = new GroupsStore();
+
+  const GroupsApp = new Vue({
+    el: document.querySelector('.js-groups-list-holder'),
+    data: groupsStore,
+  });
 });
---
+++ app/assets/javascripts/groups/stores/groups_store.js
@@ -0,0 +1,5 @@
+export default class GroupsStore {
+  constructor() {
+    this.groups = [];
+  }
+}
