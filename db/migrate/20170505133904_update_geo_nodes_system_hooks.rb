d4fcd5b62aa4c0bc8123879be67b5fd1b64ab866
Gabriel Mazetto <brodock@gmail.com>
2017-05-05 15:53:12 +0200

    Updates existing SystemHooks associated with GeoNodes to use new hook.

---
+++ db/migrate/20170505133904_update_geo_nodes_system_hooks.rb
@@ -0,0 +1,25 @@
+# See http://doc.gitlab.com/ce/development/migration_style_guide.html
+# for more information on how to write migrations for GitLab.
+
+class UpdateGeoNodesSystemHooks < ActiveRecord::Migration
+  # Set this constant to true if this migration requires downtime.
+  DOWNTIME = false
+
+  def up
+    attrs = {
+      push_events: false,
+      tag_push_events: false,
+      repository_update_events: true
+    }
+    SystemHook.joins('INNER JOIN "geo_nodes" ON "geo_nodes".system_hook_id = "web_hooks".id').update_all(attrs)
+  end
+
+  def down
+    attrs = {
+      push_events: true,
+      tag_push_events: true,
+      repository_update_events: false
+    }
+    SystemHook.joins('INNER JOIN "geo_nodes" ON "geo_nodes".system_hook_id = "web_hooks".id').update_all(attrs)
+  end
+end
