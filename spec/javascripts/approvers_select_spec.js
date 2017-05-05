65d20f6e8268d71334ecf83e2d536e2539d9ff8c
Luke "Jared" Bennett <lbennett@gitlab.com>
2017-05-05 22:46:25 +0000

    Resolve "Can't activate approvals: The form contains the following error: Approver groups is invalid"

---
+++ spec/javascripts/approvers_select_spec.js
@@ -0,0 +1,32 @@
+import ApproversSelect from '~/approvers_select';
+import ClassSpecHelper from './helpers/class_spec_helper';
+
+describe('ApproversSelect', () => {
+  describe('saveApprovers', () => {
+    let complete;
+    const $input = jasmine.createSpyObj('$input', ['val']);
+
+    beforeEach(() => {
+      spyOn(window, '$').and.returnValue($input);
+      spyOn(window.$, 'ajax').and.callFake((options) => {
+        complete = options.complete;
+      });
+
+      $input.val.and.returnValue('newValue');
+
+      ApproversSelect.saveApprovers('fieldName');
+    });
+
+    ClassSpecHelper.itShouldBeAStaticMethod(ApproversSelect, 'saveApprovers');
+
+    describe('when request completes', () => {
+      it('should empty the $input value', () => {
+        $input.val.calls.reset();
+
+        complete();
+
+        expect($input.val).toHaveBeenCalledWith('');
+      });
+    });
+  });
+});
