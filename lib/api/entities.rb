671b20bc920efdc69072af1eae0b3dae9373b033
Gabriel Mazetto <brodock@gmail.com>
2017-05-05 18:00:32 +0200

    Update SystemHooks API to expose and handle new repository_update_events

--- lib/api/entities.rb
+++ lib/api/entities.rb
@@ -50,7 +50,7 @@ module API
     end
 
     class Hook < Grape::Entity
-      expose :id, :url, :created_at, :push_events, :tag_push_events
+      expose :id, :url, :created_at, :push_events, :tag_push_events, :repository_update_events
       expose :enable_ssl_verification
     end
 
