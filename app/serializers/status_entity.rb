c17e6a6c68b0412b3433632802b852db474a7b30
Zeger-Jan van de Weg <zegerjan@gitlab.com>
2017-05-06 16:45:46 +0000

    Real time pipeline show action

--- app/serializers/status_entity.rb
+++ app/serializers/status_entity.rb
@@ -12,4 +12,11 @@ class StatusEntity < Grape::Entity
 
     ActionController::Base.helpers.image_path(File.join(dir, "#{status.favicon}.ico"))
   end
+
+  expose :action, if: -> (status, _) { status.has_action? } do
+    expose :action_icon, as: :icon
+    expose :action_title, as: :title
+    expose :action_path, as: :path
+    expose :action_method, as: :method
+  end
 end
