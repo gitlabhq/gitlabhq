96595b2794368a6cc61648cbdb088261f39ddc58
Oswaldo Ferreira <oswaldo@gitlab.com>
2017-05-07 16:31:37 -0300

    Return 0 if no window.activeVueResources

--- spec/support/wait_for_vue_resource.rb
+++ spec/support/wait_for_vue_resource.rb
@@ -8,7 +8,7 @@ module WaitForVueResource
   def finished_all_vue_resource_requests?
     return true unless javascript_test?
 
-    page.evaluate_script('window.activeVueResources').zero?
+    page.evaluate_script('window.activeVueResources || 0').zero?
   end
 
   def javascript_test?
