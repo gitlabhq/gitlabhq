8df337205ad64c637467b1309705f850c9172558
Phil Hughes <me@iamphill.com>
2017-05-08 14:14:00 +0100

    Fixed JS tests

--- app/assets/javascripts/boards/models/assignee.js
+++ app/assets/javascripts/boards/models/assignee.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-unused-vars */
 
 class ListAssignee {
-  constructor(user) {
+  constructor(user, defaultAvatar) {
     this.id = user.id;
     this.name = user.name;
     this.username = user.username;
-    this.avatar = user.avatar_url;
+    this.avatar = user.avatar_url || defaultAvatar;
   }
 }
 
