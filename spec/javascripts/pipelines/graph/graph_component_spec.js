d55048a50a1b6de0a3eab8d719971e22c6bcc940
Fatih Acet <acetfatih@gmail.com>
2017-05-09 22:29:33 +0000

    Merge branch '32021-job-component-test-failure' into 'master'
    Fix test failure in job vue componenFix test failure in job vue componentt
    
    Closes #32021
    
    See merge request !11208

--- spec/javascripts/pipelines/graph/graph_component_spec.js
+++ spec/javascripts/pipelines/graph/graph_component_spec.js
@@ -19,38 +19,52 @@ describe('graph component', () => {
   });
 
   describe('with a successfull response', () => {
-    const interceptor = (request, next) => {
-      next(request.respondWith(JSON.stringify({
-        details: {
-          stages: [{
-            name: 'test',
-            title: 'test: passed',
+    const graphJSON = {
+      details: {
+        stages: [{
+          name: 'review',
+          title: 'review: passed',
+          groups: [{
+            name: 'review_1',
+            size: 1,
             status: {
               icon: 'icon_status_success',
               text: 'passed',
               label: 'passed',
-              details_path: '/root/ci-mock/pipelines/123#test',
+              group: 'success',
+              has_details: true,
+              details_path: '/root/review-app/builds/4374',
+              favicon: '/assets/ci_favicons/dev/favicon_status_success-308b4fc054cdd1b68d0865e6cfb7b02e92e3472f201507418f8eddb74ac11a59.ico',
+              action: {
+                icon: 'icon_action_retry',
+                title: 'Retry',
+                path: '/root/review-app/builds/4374/retry',
+                method: 'post',
+              },
             },
-            path: '/root/ci-mock/pipelines/123#test',
-            groups: [{
-              name: 'test',
-              size: 1,
-              jobs: [{
-                id: 4153,
-                name: 'test',
-                status: {
-                  icon: 'icon_status_success',
-                  text: 'passed',
-                  label: 'passed',
-                  details_path: '/root/ci-mock/builds/4153',
-                  action: {
-                    icon: 'icon_action_retry',
-                    title: 'Retry',
-                    path: '/root/ci-mock/builds/4153/retry',
-                    method: 'post',
-                  },
+            jobs: [{
+              id: 4374,
+              name: 'review_1',
+              build_path: '/root/review-app/builds/4374',
+              retry_path: '/root/review-app/builds/4374/retry',
+              playable: false,
+              created_at: '2017-05-08T14:57:39.880Z',
+              updated_at: '2017-05-08T14:57:52.639Z',
+              status: {
+                icon: 'icon_status_success',
+                text: 'passed',
+                label: 'passed',
+                group: 'success',
+                has_details: true,
+                details_path: '/root/review-app/builds/4374',
+                favicon: '/assets/ci_favicons/dev/favicon_status_success-308b4fc054cdd1b68d0865e6cfb7b02e92e3472f201507418f8eddb74ac11a59.ico',
+                action: {
+                  icon: 'icon_action_retry',
+                  title: 'Retry',
+                  path: '/root/review-app/builds/4374/retry',
+                  method: 'post',
                 },
-              }],
+              },
             }],
           },
           {
@@ -103,8 +117,12 @@ describe('graph component', () => {
               }],
             }],
           }],
-        },
-      }), {
+        }],
+      },
+    };
+
+    const interceptor = (request, next) => {
+      next(request.respondWith(JSON.stringify(graphJSON), {
         status: 200,
       }));
     };
