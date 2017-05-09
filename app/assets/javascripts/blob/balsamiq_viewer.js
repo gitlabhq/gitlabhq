fe2eb5ab11b375487b13a0dcd1fdd369cd4f4410
Luke "Jared" Bennett <lbennett@gitlab.com>
2017-05-09 11:48:40 +0100

    Return if no viewer element on ready

--- app/assets/javascripts/blob/balsamiq_viewer.js
+++ app/assets/javascripts/blob/balsamiq_viewer.js
@@ -2,6 +2,9 @@ import BalsamiqViewer from './balsamiq/balsamiq_viewer';
 
 function loadBalsamiqViewer() {
   const viewer = document.getElementById('js-balsamiq-viewer');
+
+  if (!(viewer instanceof Element) || !viewer.dataset.endpoint) return;
+
   const endpoint = viewer.dataset.endpoint;
 
   const balsamiqViewer = new BalsamiqViewer(viewer, endpoint);
