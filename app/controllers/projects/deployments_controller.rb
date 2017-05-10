9d08dddfdb0ef66de9ec3b7c7cbfd96e2f949678
Oswaldo Ferreira <oswaldo@gitlab.com>
2017-05-10 13:30:54 -0300

    Ensure all Serializers receive `current_user` instead `user`

--- app/controllers/projects/deployments_controller.rb
+++ app/controllers/projects/deployments_controller.rb
@@ -6,7 +6,7 @@ class Projects::DeploymentsController < Projects::ApplicationController
     deployments = environment.deployments.reorder(created_at: :desc)
     deployments = deployments.where('created_at > ?', params[:after].to_time) if params[:after]&.to_time
 
-    render json: { deployments: DeploymentSerializer.new(user: @current_user, project: project)
+    render json: { deployments: DeploymentSerializer.new(current_user: @current_user, project: project)
                                   .represent_concise(deployments) }
   end
 
