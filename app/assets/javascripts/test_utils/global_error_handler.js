fcbd4dbe5bfe435bc378d8396975f22f7a8ecc57
Luke "Jared" Bennett <lbennett@gitlab.com>
2017-05-10 18:27:53 +0000

    use console.log

--- app/assets/javascripts/test_utils/global_error_handler.js
+++ app/assets/javascripts/test_utils/global_error_handler.js
@@ -1,6 +1,6 @@
 function globalErrorHandler() {
   // eslint-disable-next-line no-console
-  window.addEventListener('error', console.error);
+  window.addEventListener('error', console.log);
 }
 
 export default globalErrorHandler;
