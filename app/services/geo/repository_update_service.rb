ea4858ccaf61004bdd573953036bf92c3d5f592c
Gabriel Mazetto <brodock@gmail.com>
2017-05-10 02:53:39 +0100

    Codestyle and changelog changes

--- app/services/geo/repository_update_service.rb
+++ app/services/geo/repository_update_service.rb
@@ -1,11 +1,11 @@
 module Geo
   class RepositoryUpdateService
-    attr_reader :project, :clone_url
+    attr_reader :project, :clone_url, :logger
 
     LEASE_TIMEOUT = 1.hour.freeze
     LEASE_KEY_PREFIX = 'geo_repository_fetch'.freeze
 
-    def initialize(project, clone_url, logger = nil)
+    def initialize(project, clone_url, logger = Rails.logger)
       @project = project
       @clone_url = clone_url
       @logger = logger
@@ -39,10 +39,8 @@ module Geo
       begin
         yield
       ensure
-        if repository_lease.present?
-          log('Releasing leases to sync repository')
-          Gitlab::ExclusiveLease.cancel(lease_key, repository_lease)
-        end
+        log('Releasing leases to sync repository')
+        Gitlab::ExclusiveLease.cancel(lease_key, repository_lease)
       end
     end
 
@@ -52,10 +50,6 @@ module Geo
 
     def log(message)
       logger.info("#{self.class.name}: #{message} for project #{project.path_with_namespace} (#{project.id})")
-    end
-
-    def logger
-      @logger || Rails.logger
     end
   end
 end
