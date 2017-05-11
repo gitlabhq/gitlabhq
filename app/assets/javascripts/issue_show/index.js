0c55c8891b891eadef41026420f955bb504f305f
Phil Hughes <me@iamphill.com>
2017-05-11 12:25:22 +0100

    Remove some weird code to add/remove the task status Moved the data into the data method Renamed edited ago class name

--- app/assets/javascripts/issue_show/index.js
+++ app/assets/javascripts/issue_show/index.js
@@ -2,31 +2,41 @@ import Vue from 'vue';
 import issuableApp from './components/app.vue';
 import '../vue_shared/vue_resource_interceptor';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const issuableElement = document.getElementById('js-issuable-app');
-  const issuableTitleElement = issuableElement.querySelector('.title');
-  const issuableDescriptionElement = issuableElement.querySelector('.wiki');
-  const issuableDescriptionTextarea = issuableElement.querySelector('.js-task-list-field');
-  const {
-    canUpdate,
-    endpoint,
-    issuableRef,
-  } = issuableElement.dataset;
+document.addEventListener('DOMContentLoaded', () => new Vue({
+  el: document.getElementById('js-issuable-app'),
+  components: {
+    issuableApp,
+  },
+  data() {
+    const issuableElement = this.$options.el;
+    const issuableTitleElement = issuableElement.querySelector('.title');
+    const issuableDescriptionElement = issuableElement.querySelector('.wiki');
+    const issuableDescriptionTextarea = issuableElement.querySelector('.js-task-list-field');
+    const {
+      canUpdate,
+      endpoint,
+      issuableRef,
+    } = issuableElement.dataset;
 
-  return new Vue({
-    el: issuableElement,
-    components: {
-      issuableApp,
-    },
-    render: createElement => createElement('issuable-app', {
+    return {
+      canUpdate: gl.utils.convertPermissionToBoolean(canUpdate),
+      endpoint,
+      issuableRef,
+      initialTitle: issuableTitleElement.innerHTML,
+      initialDescriptionHtml: issuableDescriptionElement ? issuableDescriptionElement.innerHTML : '',
+      initialDescriptionText: issuableDescriptionTextarea ? issuableDescriptionTextarea.textContent : '',
+    };
+  },
+  render(createElement) {
+    return createElement('issuable-app', {
       props: {
-        canUpdate: gl.utils.convertPermissionToBoolean(canUpdate),
-        endpoint,
-        issuableRef,
-        initialTitle: issuableTitleElement.innerHTML,
-        initialDescriptionHtml: issuableDescriptionElement ? issuableDescriptionElement.innerHTML : '',
-        initialDescriptionText: issuableDescriptionTextarea ? issuableDescriptionTextarea.textContent : '',
+        canUpdate: this.canUpdate,
+        endpoint: this.endpoint,
+        issuableRef: this.issuableRef,
+        initialTitle: this.initialTitle,
+        initialDescriptionHtml: this.initialDescriptionHtml,
+        initialDescriptionText: this.initialDescriptionText,
       },
-    }),
-  });
-});
+    });
+  },
+}));
