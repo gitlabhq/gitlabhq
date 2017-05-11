a45cfd1e9fe65237f0a8a20fb8f0507f60110fd7
Phil Hughes <me@iamphill.com>
2017-05-11 16:26:07 +0100

    Fixed rubocop

--- lib/gitlab/visibility_level.rb
+++ lib/gitlab/visibility_level.rb
@@ -43,7 +43,7 @@ module Gitlab
         {
           N_('Private')  => PRIVATE,
           N_('Internal') => INTERNAL,
-          N_('Public')    => PUBLIC
+          N_('Public')   => PUBLIC
         }
       end
 
