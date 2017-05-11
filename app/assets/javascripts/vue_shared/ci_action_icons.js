d32b7f93b96b64100c09f3c54db3d3027493a9ea
Filipa Lacerda <filipa@gitlab.com>
2017-05-11 12:33:05 +0100

    Improve ci action icon function

--- app/assets/javascripts/vue_shared/ci_action_icons.js
+++ app/assets/javascripts/vue_shared/ci_action_icons.js
@@ -3,24 +3,19 @@ import retrySVG from 'icons/_icon_action_retry.svg';
 import playSVG from 'icons/_icon_action_play.svg';
 import stopSVG from 'icons/_icon_action_stop.svg';
 
+/**
+ * For the provided action returns the respective SVG
+ *
+ * @param  {String} action
+ * @return {SVG|String}
+ */
 export default function getActionIcon(action) {
-  let icon;
-  switch (action) {
-    case 'icon_action_cancel':
-      icon = cancelSVG;
-      break;
-    case 'icon_action_retry':
-      icon = retrySVG;
-      break;
-    case 'icon_action_play':
-      icon = playSVG;
-      break;
-    case 'icon_action_stop':
-      icon = stopSVG;
-      break;
-    default:
-      icon = '';
-  }
+  const icons = {
+    icon_action_cancel: cancelSVG,
+    icon_action_play: playSVG,
+    icon_action_retry: retrySVG,
+    icon_action_stop: stopSVG,
+  };
 
-  return icon;
+  return icons[action] || '';
 }
