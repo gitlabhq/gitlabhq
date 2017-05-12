c47dfc9e56e783ad95690d050d88ec6bf742bd9d
Luke "Jared" Bennett <lbennett@gitlab.com>
2017-05-12 20:46:16 +0100

    Check for filtered-search before constructing filteredsearchmanager

--- app/assets/javascripts/dispatcher.js
+++ app/assets/javascripts/dispatcher.js
@@ -120,7 +120,7 @@ const ShortcutsBlob = require('./shortcuts_blob');
           break;
         case 'projects:merge_requests:index':
         case 'projects:issues:index':
-          if (gl.FilteredSearchManager) {
+          if (gl.FilteredSearchManager && document.querySelector('.filtered-search')) {
             new gl.FilteredSearchManager(page === 'projects:issues:index' ? 'issues' : 'merge_requests');
           }
           Issuable.init();
