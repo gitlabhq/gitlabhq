66539563c890a8207b2ec28c4a0fc8577149f8a0
Phil Hughes <me@iamphill.com>
2017-05-12 10:11:15 +0100

    Added eventHub events to change the showForm value
    [ci skip]

--- app/assets/javascripts/issue_show/index.js
+++ app/assets/javascripts/issue_show/index.js
@@ -1,42 +1,64 @@
 import Vue from 'vue';
+import eventHub from './event_hub';
 import issuableApp from './components/app.vue';
 import '../vue_shared/vue_resource_interceptor';
 
-document.addEventListener('DOMContentLoaded', () => new Vue({
-  el: document.getElementById('js-issuable-app'),
-  components: {
-    issuableApp,
-  },
-  data() {
-    const issuableElement = this.$options.el;
-    const issuableTitleElement = issuableElement.querySelector('.title');
-    const issuableDescriptionElement = issuableElement.querySelector('.wiki');
-    const issuableDescriptionTextarea = issuableElement.querySelector('.js-task-list-field');
-    const {
-      canUpdate,
-      endpoint,
-      issuableRef,
-    } = issuableElement.dataset;
+document.addEventListener('DOMContentLoaded', () => {
+  $('.issuable-edit').on('click', (e) => {
+    e.preventDefault();
 
-    return {
-      canUpdate: gl.utils.convertPermissionToBoolean(canUpdate),
-      endpoint,
-      issuableRef,
-      initialTitle: issuableTitleElement.innerHTML,
-      initialDescriptionHtml: issuableDescriptionElement ? issuableDescriptionElement.innerHTML : '',
-      initialDescriptionText: issuableDescriptionTextarea ? issuableDescriptionTextarea.textContent : '',
-    };
-  },
-  render(createElement) {
-    return createElement('issuable-app', {
-      props: {
-        canUpdate: this.canUpdate,
-        endpoint: this.endpoint,
-        issuableRef: this.issuableRef,
-        initialTitle: this.initialTitle,
-        initialDescriptionHtml: this.initialDescriptionHtml,
-        initialDescriptionText: this.initialDescriptionText,
+    eventHub.$emit('open.form');
+  });
+
+  return new Vue({
+    el: document.getElementById('js-issuable-app'),
+    components: {
+      issuableApp,
+    },
+    data() {
+      const issuableElement = this.$options.el;
+      const issuableTitleElement = issuableElement.querySelector('.title');
+      const issuableDescriptionElement = issuableElement.querySelector('.wiki');
+      const issuableDescriptionTextarea = issuableElement.querySelector('.js-task-list-field');
+      const {
+        canUpdate,
+        endpoint,
+        issuableRef,
+      } = issuableElement.dataset;
+
+      return {
+        canUpdate: gl.utils.convertPermissionToBoolean(canUpdate),
+        endpoint,
+        issuableRef,
+        initialTitle: issuableTitleElement.innerHTML,
+        initialDescriptionHtml: issuableDescriptionElement ? issuableDescriptionElement.innerHTML : '',
+        initialDescriptionText: issuableDescriptionTextarea ? issuableDescriptionTextarea.textContent : '',
+        showForm: false,
+      };
+    },
+    methods: {
+      openForm() {
+        this.showForm = true;
+        console.log(this.showForm);
       },
-    });
-  },
-}));
+    },
+    created() {
+      eventHub.$on('open.form', this.openForm);
+    },
+    beforeDestroy() {
+      eventHub.$off('open.form', this.openForm);
+    },
+    render(createElement) {
+      return createElement('issuable-app', {
+        props: {
+          canUpdate: this.canUpdate,
+          endpoint: this.endpoint,
+          issuableRef: this.issuableRef,
+          initialTitle: this.initialTitle,
+          initialDescriptionHtml: this.initialDescriptionHtml,
+          initialDescriptionText: this.initialDescriptionText,
+        },
+      });
+    },
+  });
+});
