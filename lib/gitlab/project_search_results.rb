bdc1ece85e5b787ac59970d2f55903f41e05fe71
Valery Sizov <valery@gitlab.com>
2017-05-12 10:16:33 +0300

    Backport FileFinder from EE

--- lib/gitlab/project_search_results.rb
+++ lib/gitlab/project_search_results.rb
@@ -84,23 +84,7 @@ module Gitlab
     def blobs
       return [] unless Ability.allowed?(@current_user, :download_code, @project)
 
-      @blobs ||= begin
-        blobs = project.repository.search_files_by_content(query, repository_ref).first(100)
-        found_file_names = Set.new
-
-        results = blobs.map do |blob|
-          blob = self.class.parse_search_result(blob)
-          found_file_names << blob.filename
-
-          [blob.filename, blob]
-        end
-
-        project.repository.search_files_by_name(query, repository_ref).first(100).each do |filename|
-          results << [filename, nil] unless found_file_names.include?(filename)
-        end
-
-        results.sort_by(&:first)
-      end
+      @blobs ||= Gitlab::FileFinder.new(project, repository_ref).find(query)
     end
 
     def wiki_blobs
