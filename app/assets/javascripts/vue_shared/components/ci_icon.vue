40b78c536f1c03b668da2211f14d82e8ba9d512a
Phil Hughes <me@iamphill.com>
2017-05-12 11:28:27 +0000

    Merge branch '30286-ci-badge-component' into 'master'
    Uniform CI status components in vue
    
    Closes #30286
    
    See merge request !11235

--- app/assets/javascripts/vue_shared/components/ci_icon.vue
+++ app/assets/javascripts/vue_shared/components/ci_icon.vue
@@ -1,6 +1,27 @@
 <script>
-  import { statusIconEntityMap, statusCssClasses } from '../../vue_shared/ci_status_icons';
+  import { statusIconEntityMap } from '../ci_status_icons';
 
+  /**
+   * Renders CI icon based on API response shared between all places where it is used.
+   *
+   * Receives status object containing:
+   * status: {
+   *   details_path: "/gitlab-org/gitlab-ce/pipelines/8150156" // url
+   *   group:"running" // used for CSS class
+   *   icon: "icon_status_running" // used to render the icon
+   *   label:"running" // used for potential tooltip
+   *   text:"running" // text rendered
+   * }
+   *
+   * Used in:
+   * - Pipelines table Badge
+   * - Pipelines table mini graph
+   * - Pipeline graph
+   * - Pipeline show view badge
+   * - Jobs table
+   * - Jobs show view header
+   * - Jobs show view sidebar
+   */
   export default {
     props: {
       status: {
@@ -15,7 +36,7 @@
       },
 
       cssClass() {
-        const status = statusCssClasses[this.status.icon];
+        const status = this.status.group;
         return `ci-status-icon ci-status-icon-${status} js-ci-status-icon-${status}`;
       },
     },
