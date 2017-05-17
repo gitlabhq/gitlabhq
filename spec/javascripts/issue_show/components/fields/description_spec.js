292780272e41740102eca2630a681887d137e67e
Phil Hughes <me@iamphill.com>
2017-05-17 14:35:02 +0100

    Focus the description field in the inline form when mounted
    [ci skip]

---
+++ spec/javascripts/issue_show/components/fields/description_spec.js
@@ -0,0 +1,34 @@
+import Vue from 'vue';
+import descriptionField from '~/issue_show/components/fields/description.vue';
+
+describe('Description field component', () => {
+  let vm;
+
+  beforeEach((done) => {
+    const Component = Vue.extend(descriptionField);
+
+    // Needs an el in the DOM to be able to text the element is focused
+    const el = document.createElement('div');
+
+    document.body.appendChild(el);
+
+    vm = new Component({
+      el,
+      propsData: {
+        formState: {
+          description: '',
+        },
+        markdownDocs: '/',
+        markdownPreviewUrl: '/',
+      },
+    }).$mount();
+
+    Vue.nextTick(done);
+  });
+
+  it('focuses field when mounted', () => {
+    expect(
+      document.activeElement,
+    ).toBe(vm.$refs.textarea);
+  });
+});
