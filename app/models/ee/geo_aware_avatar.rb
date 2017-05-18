ad2f0fe9e59d8475b76766a941e0b114930ea684
Rémy Coutable <remy@rymai.me>
2017-05-18 17:15:46 +0200

    Fix GeoAwareAvatar#avatar_url signature
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- app/models/ee/geo_aware_avatar.rb
+++ app/models/ee/geo_aware_avatar.rb
@@ -4,7 +4,7 @@ module EE
   # This module is intended to encapsulate Geo-specific logic
   # and be **prepended** in the `Group`, `User`, `Project` models
   module GeoAwareAvatar
-    def avatar_url(size = nil, scale = 2)
+    def avatar_url(**args)
       if ::Gitlab::Geo.secondary? && self[:avatar].present?
         File.join(::Gitlab::Geo.primary_node.url, avatar.url)
       else
