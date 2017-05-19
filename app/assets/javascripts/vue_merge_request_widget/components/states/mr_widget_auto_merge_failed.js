0aa5e98a59346feb4ec7debfc015174236a3ccf4
Fatih Acet <acetfatih@gmail.com>
2017-05-19 04:06:08 +0300

    MRWidget: Add refresh button into MR widget failed state.

--- app/assets/javascripts/vue_merge_request_widget/components/states/mr_widget_auto_merge_failed.js
+++ app/assets/javascripts/vue_merge_request_widget/components/states/mr_widget_auto_merge_failed.js
@@ -1,7 +1,22 @@
+import eventHub from '../../event_hub';
+
 export default {
   name: 'MRWidgetAutoMergeFailed',
   props: {
     mr: { type: Object, required: true },
+  },
+  data() {
+    return {
+      isRefreshing: false,
+    };
+  },
+  methods: {
+    refreshWidget() {
+      this.isRefreshing = true;
+      eventHub.$emit('MRWidgetUpdateRequested', () => {
+        this.isRefreshing = false;
+      });
+    },
   },
   template: `
     <div class="mr-widget-body">
@@ -16,6 +31,17 @@ export default {
       </span>
       <div class="merge-error-text">
         {{mr.mergeError}}
+        <button
+          @click="refreshWidget"
+          :class="{ disabled: isRefreshing }"
+          type="button"
+          class="btn btn-xs btn-default">
+          <i
+            v-if="isRefreshing"
+            class="fa fa-spinner fa-spin"
+            aria-hidden="true" />
+          Refresh
+        </button>
       </div>
     </div>
   `,
