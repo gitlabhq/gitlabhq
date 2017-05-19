3eda0c4ff936321c1ac797af92c69e46e70be69a
Phil Hughes <me@iamphill.com>
2017-05-19 06:04:32 +0000

    Merge branch 'fix-js-translations' into 'master'
    Fix JavaScript translations that are using a namespace
    
    See merge request !11522

--- app/assets/javascripts/cycle_analytics/components/stage_plan_component.js
+++ app/assets/javascripts/cycle_analytics/components/stage_plan_component.js
@@ -31,10 +31,10 @@ global.cycleAnalytics.StagePlanComponent = Vue.extend({
               </a>
             </h5>
             <span>
-              {{ __('FirstPushedBy|First') }}
+              {{ s__('FirstPushedBy|First') }}
               <span class="commit-icon">${iconCommit}</span>
-              <a :href="commit.commitUrl" class="commit-hash-link monospace">{{ commit.shortSha }}</a>
-              {{ __('FirstPushedBy|pushed by') }}
+              <a :href="commit.commitUrl" class="commit-hash-link commit-sha">{{ commit.shortSha }}</a>
+              {{ s__('FirstPushedBy|pushed by') }}
               <a :href="commit.author.webUrl" class="commit-author-link">
                 {{ commit.author.name }}
               </a>
