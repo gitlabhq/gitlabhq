e6bede04aae0c6ecf86c77054c8f42fae2c396ec
Oswaldo Ferreira <oswaldo@gitlab.com>
2017-05-19 22:52:36 -0300

    Handle transient MR query count spec

--- spec/controllers/projects/merge_requests_controller_spec.rb
+++ spec/controllers/projects/merge_requests_controller_spec.rb
@@ -290,7 +290,7 @@ describe Projects::MergeRequestsController do
 
           recorded = ActiveRecord::QueryRecorder.new { go(format: :json) }
 
-          expect(recorded.count).to be_within(1).of(100)
+          expect(recorded.count).to be_within(5).of(95)
           expect(recorded.cached_count).to eq(0)
         end
       end
