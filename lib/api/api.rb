88d0ccd551f0334304de665ece94ba9810c60de7
Robert Speicher <robert@gitlab.com>
2017-05-23 18:49:44 +0000

    Merge branch 'dz-api-x-frame' into 'security-9-2'
    Restrict API X-Frame-Options to same origin
    
    See merge request !2103

--- lib/api/api.rb
+++ lib/api/api.rb
@@ -45,6 +45,7 @@ module API
     end
 
     before { allow_access_with_scope :api }
+    before { header['X-Frame-Options'] = 'SAMEORIGIN' }
     before { Gitlab::I18n.set_locale(current_user) }
 
     after { Gitlab::I18n.reset_locale }
