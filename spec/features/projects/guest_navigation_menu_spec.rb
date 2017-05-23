719e30c53d1d0690fa9e96fa488047e0a68d00a2
Valery Sizov <valery@gitlab.com>
2017-05-23 10:04:47 +0300

    Fix review comment

--- spec/features/projects/guest_navigation_menu_spec.rb
+++ spec/features/projects/guest_navigation_menu_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "Guest navigation menu" do
+describe 'Guest navigation menu' do
   let(:project) { create(:empty_project, :private, public_builds: false) }
   let(:guest) { create(:user) }
 
@@ -10,10 +10,10 @@ describe "Guest navigation menu" do
     login_as(guest)
   end
 
-  it "shows allowed tabs only" do
+  it 'shows allowed tabs only' do
     visit namespace_project_path(project.namespace, project)
 
-    within(".layout-nav") do
+    within('.layout-nav') do
       expect(page).to have_content 'Project'
       expect(page).to have_content 'Issues'
       expect(page).to have_content 'Wiki'
@@ -24,49 +24,58 @@ describe "Guest navigation menu" do
     end
   end
 
-  it "does not show fork button" do
+  it 'does not show fork button' do
     visit namespace_project_path(project.namespace, project)
 
-    within(".count-buttons") do
+    within('.count-buttons') do
       expect(page).not_to have_link 'Fork'
     end
   end
 
-  it "does not show clone path" do
+  it 'does not show clone path' do
     visit namespace_project_path(project.namespace, project)
 
-    within(".project-repo-buttons") do
+    within('.project-repo-buttons') do
       expect(page).not_to have_selector '.project-clone-holder'
     end
   end
 
   describe 'project landing page' do
     before do
-      project.project_feature.update_attribute("issues_access_level", ProjectFeature::DISABLED)
-      project.project_feature.update_attribute("wiki_access_level", ProjectFeature::DISABLED)
+      project.project_feature.update!(
+        issues_access_level: ProjectFeature::DISABLED,
+        wiki_access_level: ProjectFeature::DISABLED
+      )
     end
 
-    it "does not show the project file list landing page" do
+    it 'does not show the project file list landing page' do
       visit namespace_project_path(project.namespace, project)
+
       expect(page).not_to have_selector '.project-stats'
       expect(page).not_to have_selector '.project-last-commit'
       expect(page).not_to have_selector '.project-show-files'
-    end
-
-    it "shows the customize workflow when issues and wiki are disabled" do
-      visit namespace_project_path(project.namespace, project)
       expect(page).to have_selector '.project-show-customize_workflow'
     end
 
-    it "shows the wiki when enabled" do
-      project.project_feature.update_attribute("wiki_access_level", ProjectFeature::PRIVATE)
+    it 'shows the customize workflow when issues and wiki are disabled' do
       visit namespace_project_path(project.namespace, project)
+
+      expect(page).to have_selector '.project-show-customize_workflow'
+    end
+
+    it 'shows the wiki when enabled' do
+      project.project_feature.update!(wiki_access_level: ProjectFeature::PRIVATE)
+
+      visit namespace_project_path(project.namespace, project)
+
       expect(page).to have_selector '.project-show-wiki'
     end
 
-    it "shows the issues when enabled" do
-      project.project_feature.update_attribute("issues_access_level", ProjectFeature::PRIVATE)
+    it 'shows the issues when enabled' do
+      project.project_feature.update!(issues_access_level: ProjectFeature::PRIVATE)
+
       visit namespace_project_path(project.namespace, project)
+
       expect(page).to have_selector '.issues-list'
     end
   end
