33d66a47f6da19831889d8717ec9f11b8b5c6b6c
Luke "Jared" Bennett <lbennett@gitlab.com>
2017-05-24 11:53:28 +0100

    Correct editable_helper spec and format issuable_app_data updated_at to iso8061

--- app/helpers/editable_helper.rb
+++ app/helpers/editable_helper.rb
@@ -3,7 +3,7 @@ module EditableHelper
     return {} unless editable.is_edited?
 
     {
-      updated_at: editable.updated_at,
+      updated_at: editable.updated_at.to_time.iso8601,
       updated_by: {
         name: editable.last_edited_by.name,
         path: user_path(editable.last_edited_by)
--- spec/helpers/editable_helper_spec.rb
+++ spec/helpers/editable_helper_spec.rb
@@ -7,7 +7,7 @@ describe EditableHelper do
     let(:edited_editable) { create(:issue, last_edited_by: user, created_at: 3.days.ago, updated_at: 2.days.ago, last_edited_at: 2.days.ago) }
     let(:edited_updated_at_by) do
       {
-        updated_at: edited_editable.updated_at,
+        updated_at: edited_editable.updated_at.to_time.iso8601,
         updated_by: {
           name: user.name,
           path: user_path(user)
@@ -15,7 +15,7 @@ describe EditableHelper do
       }
     end
 
-    it { expect(helper.updated_at_by(unedited_editable)).to be_nil }
+    it { expect(helper.updated_at_by(unedited_editable)).to eq({}) }
     it { expect(helper.updated_at_by(edited_editable)).to eq(edited_updated_at_by) }
   end
 end
