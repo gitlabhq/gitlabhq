4d8f3978a254f33d959de65dbf7b20a1d41a058d
Pawel Chojnacki <pawel@chojnacki.ws>
2017-05-25 16:19:54 +0200

    Use before action to respond with 404 if prometheus metrics are missing

--- app/controllers/projects/prometheus_controller.rb
+++ app/controllers/projects/prometheus_controller.rb
@@ -1,8 +1,8 @@
 class Projects::PrometheusController < Projects::ApplicationController
   before_action :authorize_read_project!
+  before_action :require_prometheus_metrics!
 
   def active_metrics
-    return render_404 unless has_prometheus_metrics?
     matched_metrics = prometheus_service.reactive_query(Gitlab::Prometheus::Queries::MatchedMetricsQuery.name, &:itself)
 
     if matched_metrics
@@ -12,11 +12,17 @@ class Projects::PrometheusController < Projects::ApplicationController
     end
   end
 
+  private
+
   def prometheus_service
     project.monitoring_service
   end
 
   def has_prometheus_metrics?
     prometheus_service&.respond_to?(:reactive_query)
+  end
+
+  def require_prometheus_metrics!
+    render_404 unless has_prometheus_metrics?
   end
 end
