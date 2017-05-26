730f3f5c08ca39bd004a5a2227afa2ed976f57e4
Nick Thomas <nick@gitlab.com>
2017-05-26 10:28:40 +0100

    Fix a bad merge in spec/features/boards/sidebar_spec.rb

--- spec/features/boards/sidebar_spec.rb
+++ spec/features/boards/sidebar_spec.rb
@@ -134,6 +134,8 @@ describe 'Issue Boards', feature: true, js: true do
           click_link 'Unassigned'
         end
 
+        find('.dropdown-menu-toggle').click
+
         wait_for_requests
 
         expect(page).to have_content('No assignee')
