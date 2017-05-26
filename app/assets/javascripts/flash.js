f5bd18c690b2aa1f56c4d7ae94dd34e3925446d6
Mike Greiling <mike@pixelcog.com>
2017-05-26 03:31:21 -0500

    refactor Flash() function

--- app/assets/javascripts/flash.js
+++ app/assets/javascripts/flash.js
@@ -1,41 +1,20 @@
-/* eslint-disable func-names, space-before-function-paren, wrap-iife, no-var, one-var, one-var-declaration-per-line, no-param-reassign, quotes, quote-props, prefer-template, comma-dangle, max-len */
+export default function flash(message, type = 'alert', parent = null) {
+  const $flashContainer = parent ?
+    parent.find('.flash-container') : $('.flash-container-page');
 
-window.Flash = (function() {
-  var hideFlash;
+  const containerClasses = $flashContainer.parent().hasClass('content-wrapper') ?
+    'container-fluid container-limited' : '';
 
-  hideFlash = function() {
-    return $(this).fadeOut();
-  };
+  const $flash = $(`
+    <div class="flash-${type}">
+      <div class="flash-text ${containerClasses}">
+        ${message}
+      </div>
+    </div>
+  `).on('click', () => $flash.fadeOut());
 
-  function Flash(message, type, parent) {
-    var flash, textDiv;
-    if (type == null) {
-      type = 'alert';
-    }
-    if (parent == null) {
-      parent = null;
-    }
-    if (parent) {
-      this.flashContainer = parent.find('.flash-container');
-    } else {
-      this.flashContainer = $('.flash-container-page');
-    }
-    this.flashContainer.html('');
-    flash = $('<div/>', {
-      "class": "flash-" + type
-    });
-    flash.on('click', hideFlash);
-    textDiv = $('<div/>', {
-      "class": 'flash-text',
-      text: message
-    });
-    textDiv.appendTo(flash);
-    if (this.flashContainer.parent().hasClass('content-wrapper')) {
-      textDiv.addClass('container-fluid container-limited');
-    }
-    flash.appendTo(this.flashContainer);
-    this.flashContainer.show();
-  }
+  $flashContainer.empty().append($flash).show();
+}
 
-  return Flash;
-})();
+// global name capitalized for legacy reasons
+window.Flash = flash;
