806dbf1091e2fbc43e5eb99b85f1b03138ab1b02
Filipa Lacerda <filipa@gitlab.com>
2017-05-26 11:56:54 +0100

    Creates a mediator for pipeline details vue in order to mount several vue apps with the same data

---
+++ app/assets/javascripts/pipelines/pipeline_details_bundle.js
@@ -0,0 +1,31 @@
+import Vue from 'vue';
+import PipelinesMediator from './pipeline_details_mediatior';
+import pipelineGraph from './components/graph/graph_component.vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const dataset = document.querySelector('.js-pipeline-details-vue').dataset;
+
+  const mediator = new PipelinesMediator({ endpoint: dataset.endpoint });
+
+  mediator.fetchPipeline();
+
+  const pipelineGraphApp = new Vue({
+    el: '#js-pipeline-graph-vue',
+    data: {
+      mediator,
+    },
+    components: {
+      pipelineGraph,
+    },
+    render(createElement) {
+      return createElement('pipeline-graph', {
+        props: {
+          isLoading: this.mediator.state.isLoading,
+          pipeline: this.mediator.store.state.pipeline,
+        },
+      });
+    },
+  });
+
+  return pipelineGraphApp;
+});
