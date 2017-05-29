2ba6ea60f8c7565b8699fa355db5450d56b7212c
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2017-05-29 20:25:39 -0300

    Move foreign_key creation to create_table statement on CreateGeoEventLog

--- db/migrate/20170522201800_create_geo_event_log.rb
+++ db/migrate/20170522201800_create_geo_event_log.rb
@@ -5,8 +5,8 @@ class CreateGeoEventLog < ActiveRecord::Migration
     create_table :geo_event_log do |t|
       t.datetime :created_at, index: true, null: false
       t.integer :push_event_id, index: true
-    end
 
-    add_foreign_key :geo_event_log, :geo_push_events, column: :push_event_id, on_delete: :cascade # rubocop: disable Migration/AddConcurrentForeignKey
+      t.foreign_key :geo_push_events, column: :push_event_id, on_delete: :cascade
+    end
   end
 end
