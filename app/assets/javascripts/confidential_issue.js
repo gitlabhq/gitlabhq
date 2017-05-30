67b253a8d2d570cdb960252efa236abb4e9351a1
Regis <boudinot.regis@yahoo.com>
2017-05-30 17:59:53 -0600

    fix z-index and make it work with all browsers - wow

--- app/assets/javascripts/confidential_issue.js
+++ app/assets/javascripts/confidential_issue.js
@@ -1,10 +1,17 @@
 export default () => {
-  const body = document.body;
+  /**
+  * for IE11/Firefox/Edge :(
+  * document.documentElement.scrollTop || document.body.scrollTop
+  * if documentElement.scrollTop is 0 then we are on a non WebKit browser
+  * then `body.scrollTop` will be used
+  * otherwise it will return a `truthy` value :)
+  **/
+
   const banner = document.querySelector('.confidential-issue-warning');
   const bannerClassList = banner.classList;
 
   const confidentialScroll = () => {
-    if (body.scrollTop > 5) {
+    if ((document.documentElement.scrollTop || document.body.scrollTop) > 5) {
       bannerClassList.add('confidential-issue-scroll');
     } else {
       bannerClassList.remove('confidential-issue-scroll');
