854e9de935c2bbb2a7143c73bb5e9edf98d3fc65
Simon Knox <psimyn@gmail.com>
2017-05-30 18:32:49 +1000

    animate adding issue to boards
    fix some false positive tests for board_new_issue

--- app/assets/javascripts/boards/models/list.js
+++ app/assets/javascripts/boards/models/list.js
@@ -103,13 +103,19 @@ class List {
   }
 
   newIssue (issue) {
-    this.addIssue(issue);
+    this.addIssue(issue, null, 0);
     this.issuesSize += 1;
 
     return gl.boardService.newIssue(this.id, issue)
       .then((resp) => {
         const data = resp.json();
         issue.id = data.iid;
+      })
+      .then(() => {
+        if (this.issuesSize > 1) {
+          const moveBeforeIid = this.issues[1].id;
+          gl.boardService.moveIssue(issue.id, null, null, null, moveBeforeIid);
+        }
       });
   }
 
