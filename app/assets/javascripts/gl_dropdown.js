b484ebf5109bf9af82b2ee901a3a9322571ad0dd
Phil Hughes <me@iamphill.com>
2017-05-30 07:56:36 +0000

    Merge branch '2472-assignee-dropdown-does-not-display-selected-assignee-ce' into 'master'
    Assignee dropdown does not display selected assignee
    
    Closes #2472
    
    See merge request !11744
     Conflicts:
    	app/assets/javascripts/users_select.js

--- app/assets/javascripts/gl_dropdown.js
+++ app/assets/javascripts/gl_dropdown.js
@@ -469,8 +469,8 @@ GitLabDropdown = (function() {
 
     // Process the data to make sure rendered data
     // matches the correct layout
-    if (this.fullData && hasMultiSelect && this.options.processData) {
-      const inputValue = this.filterInput.val();
+    const inputValue = this.filterInput.val();
+    if (this.fullData && hasMultiSelect && this.options.processData && inputValue.length === 0) {
       this.options.processData.call(this.options, inputValue, this.filteredFullData(), this.parseData.bind(this));
     }
 
@@ -739,6 +739,12 @@ GitLabDropdown = (function() {
     $input = $('<input>').attr('type', 'hidden').attr('name', fieldName).val(value);
     if (this.options.inputId != null) {
       $input.attr('id', this.options.inputId);
+    }
+
+    if (this.options.multiSelect) {
+      Object.keys(selectedObject).forEach((attribute) => {
+        $input.attr(`data-${attribute}`, selectedObject[attribute]);
+      });
     }
 
     if (this.options.inputMeta) {
