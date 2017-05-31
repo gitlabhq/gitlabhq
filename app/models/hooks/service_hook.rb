be8b43eaccd74054f3f3782a0e3d43a33489e912
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2017-05-31 20:45:39 +0300

    Fix service hook after merge from CE
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- app/models/hooks/service_hook.rb
+++ app/models/hooks/service_hook.rb
@@ -1,7 +1,7 @@
 class ServiceHook < WebHook
   belongs_to :service
 
-  def execute(data)
-    WebHookService.new(self, data, 'service_hook').execute
+  def execute(data, hook_name = 'service_hook')
+    WebHookService.new(self, data, hook_name).execute
   end
 end
