4e46b1c854db32f2b2f4378d8b6da885fa794424
Phil Hughes <me@iamphill.com>
2017-05-31 16:35:47 +0000

    Port of issue-edit-inline to EE

--- app/serializers/issue_entity.rb
+++ app/serializers/issue_entity.rb
@@ -1,4 +1,6 @@
 class IssueEntity < IssuableEntity
+  include RequestAwareEntity
+
   expose :branch_name
   expose :confidential
   expose :assignees, using: API::Entities::UserBasic
@@ -8,4 +10,8 @@ class IssueEntity < IssuableEntity
   expose :weight
   expose :milestone, using: API::Entities::Milestone
   expose :labels, using: LabelEntity
+
+  expose :web_url do |issue|
+    namespace_project_issue_path(issue.project.namespace, issue.project, issue)
+  end
 end
