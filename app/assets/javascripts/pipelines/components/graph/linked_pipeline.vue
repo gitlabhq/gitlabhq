0fc1a03853a630b513dda7e71d6ac949ed164657
Bryce Johnson <bryce@gitlab.com>
2017-06-02 14:29:22 -0400

    Style linked pipeline separator. [ci-skip]

--- app/assets/javascripts/pipelines/components/graph/linked_pipeline.vue
+++ app/assets/javascripts/pipelines/components/graph/linked_pipeline.vue
@@ -50,7 +50,8 @@ export default {
           <ci-status :status="pipelineStatus"/>
         </span>
         <span class="linked-pipeline-project-name"> {{ projectName }}</span>
-        <span class="linked-pipeline-id"> &#8226; #{{ pipelineId }}</span>
+        <span class="project-name-pipeline-id-separator">&#8226;</span>
+        <span class="linked-pipeline-id"> #{{ pipelineId }}</span>
       </a>
     </div>
   </li>
