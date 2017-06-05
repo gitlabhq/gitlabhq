9a9bb02421ce373a73569ebcdcaf2683f3c03359
kushalpandya <kushal@gitlab.com>
2017-06-05 19:21:10 +0530

    Optimize jQuery lookup

--- app/assets/javascripts/protected_tags/protected_tag_access_dropdown.js
+++ app/assets/javascripts/protected_tags/protected_tag_access_dropdown.js
@@ -17,8 +17,9 @@ export default class ProtectedTagAccessDropdown {
     this.accessLevelsData = accessLevelsData.roles;
     this.$dropdown = $dropdown;
     this.$wrap = this.$dropdown.closest(`.${this.accessLevel}-container`);
-    this.usersPath = $('.js-protected-tags-container').data('users-autocomplete');
-    this.groupsPath = $('.js-protected-tags-container').data('groups-autocomplete');
+    this.$protectedTagsContainer = $('.js-protected-tags-container');
+    this.usersPath = this.$protectedTagsContainer.data('users-autocomplete');
+    this.groupsPath = this.$protectedTagsContainer.data('groups-autocomplete');
     this.defaultLabel = this.$dropdown.data('defaultLabel');
 
     this.setSelectedItems([]);
