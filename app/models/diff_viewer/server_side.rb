794425456322864f37dbd862aca9bc6b6447591a
Douwe Maan <douwe@selenight.nl>
2017-06-06 16:28:06 -0500

    Implement diff viewers

--- app/models/diff_viewer/server_side.rb
+++ app/models/diff_viewer/server_side.rb
@@ -1,15 +1,15 @@
-module BlobViewer
+module DiffViewer
   module ServerSide
     extend ActiveSupport::Concern
 
     included do
-      self.load_async = true
-      self.collapse_limit = 2.megabytes
+      self.collapse_limit = 1.megabyte
       self.size_limit = 5.megabytes
     end
 
     def prepare!
-      blob.load_all_data!
+      diff_file.old_blob&.load_all_data!
+      diff_file.new_blob&.load_all_data!
     end
 
     def render_error
@@ -17,8 +17,8 @@ module BlobViewer
       # build artifacts, can only be rendered using a client-side viewer,
       # since we do not want to read large amounts of data into memory on the
       # server side. Client-side viewers use JS and can fetch the file from
-      # `blob_raw_url` using AJAX.
-      return :server_side_but_stored_externally if blob.stored_externally?
+      # `diff_file_blob_raw_path` and `diff_file_old_blob_raw_path` using AJAX.
+      return :server_side_but_stored_externally if diff_file.stored_externally?
 
       super
     end
