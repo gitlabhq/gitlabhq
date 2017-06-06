5a31bbe86ca41b1461ed8bf5a663aa51c123f149
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2017-06-06 13:26:19 +0200

    Make pipeline stages ref migration more readable

--- db/post_migrate/20170526185921_migrate_build_stage_reference.rb
+++ db/post_migrate/20170526185921_migrate_build_stage_reference.rb
@@ -6,9 +6,11 @@ class MigrateBuildStageReference < ActiveRecord::Migration
   def up
     disable_statement_timeout
 
-    stage_id = Arel.sql('(SELECT id FROM ci_stages ' \
-                         'WHERE ci_stages.pipeline_id = ci_builds.commit_id ' \
-                         'AND ci_stages.name = ci_builds.stage)')
+    stage_id = Arel.sql(<<-SQL.strip_heredoc
+      (SELECT id FROM ci_stages
+         WHERE ci_stages.pipeline_id = ci_builds.commit_id
+           AND ci_stages.name = ci_builds.stage)
+    SQL
 
     update_column_in_batches(:ci_builds, :stage_id, stage_id) do |table, query|
       query.where(table[:stage_id].eq(nil))
