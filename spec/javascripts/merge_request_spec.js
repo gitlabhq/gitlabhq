421e41c9ce3abdd843079c1eb022cad8974d36ff
Luke "Jared" Bennett <lbennett@gitlab.com>
2017-06-07 20:34:19 +0100

    Added specs

--- spec/javascripts/merge_request_spec.js
+++ spec/javascripts/merge_request_spec.js
@@ -2,10 +2,11 @@
 /* global MergeRequest */
 
 import '~/merge_request';
+import * as CloseReopenReportToggle from '~/close_reopen_report_toggle';
 
 (function() {
   describe('MergeRequest', function() {
-    return describe('task lists', function() {
+    describe('task lists', function() {
       preloadFixtures('merge_requests/merge_request_with_task_list.html.raw');
       beforeEach(function() {
         loadFixtures('merge_requests/merge_request_with_task_list.html.raw');
@@ -25,6 +26,43 @@ import '~/merge_request';
           return expect(req.data.merge_request.description).not.toBe(null);
         });
         return $('.js-task-list-field').trigger('tasklist:changed');
+      });
+    });
+
+    describe('class constructor', () => {
+      it('calls .initCloseReopenReport', () => {
+        spyOn(MergeRequest, 'initCloseReopenReport');
+
+        new MergeRequest(); // eslint-disable-line no-new
+
+        expect(MergeRequest.initCloseReopenReport).toHaveBeenCalled();
+      });
+    });
+
+    describe('initCloseReopenReport', () => {
+      it('inits a new CloseReopenReportToggle instance and calls .initDroplab', () => {
+        const container = jasmine.createSpyObj('container', ['querySelector']);
+        const closeReopenReportToggle = jasmine.createSpyObj('closeReopenReportToggle', ['initDroplab']);
+        const dropdownTrigger = {};
+        const dropdownList = {};
+        const button = {};
+
+        spyOn(document, 'querySelector').and.returnValue(container);
+        spyOn(CloseReopenReportToggle, 'default').and.returnValue(closeReopenReportToggle);
+        container.querySelector.and.returnValues(dropdownTrigger, dropdownList, button);
+
+        MergeRequest.initCloseReopenReport();
+
+        expect(document.querySelector).toHaveBeenCalledWith('.js-issuable-close-dropdown');
+        expect(container.querySelector).toHaveBeenCalledWith('.js-issuable-close-toggle');
+        expect(container.querySelector).toHaveBeenCalledWith('.js-issuable-close-menu');
+        expect(container.querySelector).toHaveBeenCalledWith('.js-issuable-close-button');
+        expect(CloseReopenReportToggle.default).toHaveBeenCalledWith({
+          dropdownTrigger,
+          dropdownList,
+          button,
+        });
+        expect(closeReopenReportToggle.initDroplab).toHaveBeenCalled();
       });
     });
   });
