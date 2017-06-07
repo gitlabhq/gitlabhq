a05acce8b697fa1b07325fb958846b6c54ccb80f
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2017-06-07 11:22:01 -0300

    Use spec_helper rather than rails_helper on Geo model specs

--- spec/models/geo/event_log_spec.rb
+++ spec/models/geo/event_log_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe Geo::EventLog, type: :model do
   describe 'relationships' do
--- spec/models/geo/repository_updated_event_spec.rb
+++ spec/models/geo/repository_updated_event_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe Geo::RepositoryUpdatedEvent, type: :model do
   describe 'relationships' do
