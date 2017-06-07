1b76c7196b3b50074e6f2eba64d988ead90e7264
James Lopez <james@jameslopez.es>
2017-06-07 14:57:26 +0200

    add more spec examples

--- spec/lib/gitlab/backup/repository_spec.rb
+++ spec/lib/gitlab/backup/repository_spec.rb
@@ -22,14 +22,49 @@ describe Backup::Repository, lib: true do
     $progress = @old_progress # rubocop:disable Style/GlobalVars
   end
 
-  describe 'repo failure' do
-    before do
-      allow_any_instance_of(Project).to receive(:empty_repo?).and_raise(Rugged::OdbError)
-      allow(Gitlab::Popen).to receive(:popen).and_return(['normal output', 0])
+  describe '.dump' do
+    describe 'repo failure' do
+      before do
+        allow_any_instance_of(Project).to receive(:empty_repo?).and_raise(Rugged::OdbError)
+        allow(Gitlab::Popen).to receive(:popen).and_return(['normal output', 0])
+      end
+
+      it 'does not raise error' do
+        expect { described_class.new.dump }.not_to raise_error
+      end
+
+      it 'shows the appropriate error' do
+        described_class.new.dump
+
+        expect(progress).to have_received(:puts).with("Ignoring error on #{project.full_path} repository - Rugged::OdbError")
+      end
     end
 
-    it 'does not raise error' do
-      expect { described_class.new.dump }.not_to raise_error
+    describe 'command failure' do
+      before do
+        allow_any_instance_of(Project).to receive(:empty_repo?).and_return(false)
+        allow(Gitlab::Popen).to receive(:popen).and_return(['error', 1])
+      end
+
+      it 'shows the appropriate error' do
+        described_class.new.dump
+
+        expect(progress).to have_received(:puts).with("Ignoring error on #{project.full_path} - error")
+      end
+    end
+  end
+
+  describe '.restore' do
+    describe 'command failure' do
+      before do
+        allow(Gitlab::Popen).to receive(:popen).and_return(['error', 1])
+      end
+
+      it 'shows the appropriate error' do
+        described_class.new.restore
+
+        expect(progress).to have_received(:puts).with("Ignoring error on #{project.full_path} - error")
+      end
     end
   end
 end
