64623be20e9219606019f540a45bce2ee086c23d
Douwe Maan <douwe@gitlab.com>
2017-06-07 16:21:49 +0000

    Merge branch 'bvl-fix-typo-renaming-appearance' into 'security-9-2'
    Fix typo in moving the`appearance` folder in migrations
    
    See merge request !2116

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170426181740) do
-
+ActiveRecord::Schema.define(version: 20170606202615) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "pg_trgm"
