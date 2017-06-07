20afb35d68ff6a0cbe29ca6f2976ae442b78b2ab
黄涛 <htve@outlook.com>
2017-06-07 10:10:49 +0800

    add bulgarian translation of cycle analytics page to I18N
    Fix #33383

--- lib/gitlab/i18n.rb
+++ lib/gitlab/i18n.rb
@@ -8,7 +8,8 @@ module Gitlab
       'de' => 'Deutsch',
       'zh_CN' => '简体中文',
       'zh_HK' => '繁體中文(香港)',
-      'zh_TW' => '繁體中文(臺灣)'
+      'zh_TW' => '繁體中文(臺灣)',
+      'bg' => 'български'
     }.freeze
 
     def available_locales
