722585a90fcc48c7730086f1858a818bf3f09af3
Nick Thomas <nick@gitlab.com>
2017-06-08 10:05:21 +0100

    Fix a spec failure caused by extra factories in EE

--- spec/factories_spec.rb
+++ spec/factories_spec.rb
@@ -8,12 +8,16 @@ describe 'factories' do
       end
 
       it 'does not raise error when created' do
+        skip("#{factory.name} cannot be saved") unless build(factory.name).respond_to?(:save!)
+
         expect { create(factory.name) }.not_to raise_error
       end
 
       factory.definition.defined_traits.map(&:name).each do |trait_name|
         describe "linting #{trait_name} trait" do
           skip 'does not raise error when created' do
+            skip("#{factory.name} cannot be saved") unless build(factory.name).respond_to?(:save!)
+
             expect { create(factory.name, trait_name) }.not_to raise_error
           end
         end
