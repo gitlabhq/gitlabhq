3275a767994aef36e8cbc7b908b4a5069f6434f6
Nick Thomas <nick@gitlab.com>
2017-06-08 01:47:15 +0100

    Fix merge conflicts in WikiPages::{Create,Destroy,Update}Service specs

--- spec/services/wiki_pages/create_service_spec.rb
+++ spec/services/wiki_pages/create_service_spec.rb
@@ -2,11 +2,7 @@ require 'spec_helper'
 
 describe WikiPages::CreateService, services: true do
   let(:project) { create(:empty_project) }
-<<<<<<< HEAD
-  let(:user) { create(:user) }
-=======
   let(:user)    { create(:user) }
->>>>>>> master
 
   let(:opts) do
     {
@@ -19,11 +15,7 @@ describe WikiPages::CreateService, services: true do
   subject(:service) { described_class.new(project, user, opts) }
 
   before do
-<<<<<<< HEAD
-    project.add_developer(user)
-=======
     project.add_master(user)
->>>>>>> master
   end
 
   describe '#execute' do
@@ -31,22 +23,11 @@ describe WikiPages::CreateService, services: true do
       page = service.execute
 
       expect(page).to be_valid
-<<<<<<< HEAD
-      expect(page.title).to eq(opts[:title])
-      expect(page.content).to eq(opts[:content])
-      expect(page.format).to eq(opts[:format].to_sym)
-    end
-
-    it 'executes webhooks' do
-      expect(service).to receive(:execute_hooks).once
-        .with(instance_of(WikiPage), 'create')
-=======
       expect(page).to have_attributes(title: opts[:title], content: opts[:content], format: opts[:format].to_sym)
     end
 
     it 'executes webhooks' do
       expect(service).to receive(:execute_hooks).once.with(instance_of(WikiPage), 'create')
->>>>>>> master
 
       service.execute
     end
--- spec/services/wiki_pages/update_service_spec.rb
+++ spec/services/wiki_pages/update_service_spec.rb
@@ -2,13 +2,8 @@ require 'spec_helper'
 
 describe WikiPages::UpdateService, services: true do
   let(:project) { create(:empty_project) }
-<<<<<<< HEAD
-  let(:user) { create(:user) }
-  let(:page) { create(:wiki_page) }
-=======
   let(:user)    { create(:user) }
   let(:page)    { create(:wiki_page) }
->>>>>>> master
 
   let(:opts) do
     {
@@ -21,11 +16,7 @@ describe WikiPages::UpdateService, services: true do
   subject(:service) { described_class.new(project, user, opts) }
 
   before do
-<<<<<<< HEAD
-    project.add_developer(user)
-=======
     project.add_master(user)
->>>>>>> master
   end
 
   describe '#execute' do
@@ -33,22 +24,11 @@ describe WikiPages::UpdateService, services: true do
       updated_page = service.execute(page)
 
       expect(updated_page).to be_valid
-<<<<<<< HEAD
-      expect(updated_page.message).to eq(opts[:message])
-      expect(updated_page.content).to eq(opts[:content])
-      expect(updated_page.format).to eq(opts[:format].to_sym)
-    end
-
-    it 'executes webhooks' do
-      expect(service).to receive(:execute_hooks).once
-        .with(instance_of(WikiPage), 'update')
-=======
       expect(updated_page).to have_attributes(message: opts[:message], content: opts[:content], format: opts[:format].to_sym)
     end
 
     it 'executes webhooks' do
       expect(service).to receive(:execute_hooks).once.with(instance_of(WikiPage), 'update')
->>>>>>> master
 
       service.execute(page)
     end
