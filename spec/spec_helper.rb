4e17724b7058a5cbf53fa470d93acbf7e05f87a8
Robert Speicher <rspeicher@gmail.com>
2017-06-08 13:04:38 -0400

    Include Devise::Test::IntegrationHelpers for all feature specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -44,6 +44,7 @@ RSpec.configure do |config|
 
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
+  config.include Devise::Test::IntegrationHelpers, type: :feature
   config.include Warden::Test::Helpers, type: :request
   config.include LoginHelpers, type: :feature
   config.include SearchHelpers, type: :feature
