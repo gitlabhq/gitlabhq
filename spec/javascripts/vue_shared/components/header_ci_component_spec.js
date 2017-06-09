0ff932cdce69f1d3bdd22cda4ff73f92569b3317
Mike Greiling <mike@pixelcog.com>
2017-06-09 00:23:10 -0500

    fix incorrect equals assertion

--- spec/javascripts/vue_shared/components/header_ci_component_spec.js
+++ spec/javascripts/vue_shared/components/header_ci_component_spec.js
@@ -87,7 +87,7 @@ describe('Header CI Component', () => {
     vm.actions[0].isLoading = true;
 
     Vue.nextTick(() => {
-      expect(vm.$el.querySelector('.btn .fa-spinner').getAttribute('style')).toEqual('');
+      expect(vm.$el.querySelector('.btn .fa-spinner').getAttribute('style')).toBeFalsy();
       done();
     });
   });
