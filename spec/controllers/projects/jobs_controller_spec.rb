8e1b6041a8382b2a67d971ce9fb41eb6d372a8b7
Oswaldo Ferreira <oswaldo@gitlab.com>
2017-06-09 12:20:53 -0300

    Use :request_store hooks on specs

--- spec/controllers/projects/jobs_controller_spec.rb
+++ spec/controllers/projects/jobs_controller_spec.rb
@@ -69,16 +69,9 @@ describe Projects::JobsController do
         Ci::Build::AVAILABLE_STATUSES.each do |status|
           create_build(status, status)
         end
-
-        RequestStore.begin!
       end
 
-      after do
-        RequestStore.end!
-        RequestStore.clear!
-      end
-
-      it "verifies number of queries" do
+      it 'verifies number of queries', :request_store do
         recorded = ActiveRecord::QueryRecorder.new { get_index }
         expect(recorded.count).to be_within(5).of(8)
       end
