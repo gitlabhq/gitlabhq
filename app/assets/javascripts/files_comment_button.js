2556e84c272aeab96fca06049b98430b0dfa44ac
Bryce Johnson <bryce@gitlab.com>
2017-06-12 19:12:49 -0400

    Refactor relationship between diff.js and files_comment_button.js.

--- app/assets/javascripts/files_comment_button.js
+++ app/assets/javascripts/files_comment_button.js
@@ -1,57 +1,49 @@
 /* eslint-disable func-names, space-before-function-paren, no-var, prefer-rest-params, wrap-iife, max-len, one-var, one-var-declaration-per-line, quotes, prefer-template, newline-per-chained-call, comma-dangle, new-cap, no-else-return, consistent-return */
-/* global FilesCommentButton */
 /* global notes */
 
-window.FilesCommentButton = (function() {
-  var COMMENT_BUTTON_CLASS, EMPTY_CELL_CLASS, LINE_COLUMN_CLASSES, LINE_CONTENT_CLASS, LINE_HOLDER_CLASS, LINE_NUMBER_CLASS, OLD_LINE_CLASS, TEXT_FILE_SELECTOR, UNFOLDABLE_LINE_CLASS;
+/* Developer beware! Do not add logic to showButton or hideButton
+ * that will force a reflow. Doing so will create a signficant performance
+ * bottleneck for pages with large diffs. For a comprehensive list of what
+ * causes reflows, visit https://gist.github.com/paulirish/5d52fb081b3570c81e3a
+ */
 
-  COMMENT_BUTTON_CLASS = '.add-diff-note';
+const LINE_NUMBER_CLASS = 'diff-line-num';
+const LINE_CONTENT_CLASS = 'line_content';
+const UNFOLDABLE_LINE_CLASS = 'js-unfold';
+const EMPTY_CELL_CLASS = 'empty-cell';
+const OLD_LINE_CLASS = 'old_line';
+const LINE_COLUMN_CLASSES = "." + LINE_NUMBER_CLASS + ", .line_content";
 
-  LINE_HOLDER_CLASS = '.line_holder';
+export default {
+  init($diffFile) {
+    const userCanCreateNote = $diffFile && $diffFile.parent().data('can-create-note') !== null;
 
-  LINE_NUMBER_CLASS = 'diff-line-num';
+    if (userCanCreateNote) {
+      $diffFile.on('mouseover', LINE_COLUMN_CLASSES, e => this.showButton(e))
+        .on('mouseleave', LINE_COLUMN_CLASSES, e => this.hideButton(e));
+    }
+  },
 
-  LINE_CONTENT_CLASS = 'line_content';
-
-  UNFOLDABLE_LINE_CLASS = 'js-unfold';
-
-  EMPTY_CELL_CLASS = 'empty-cell';
-
-  OLD_LINE_CLASS = 'old_line';
-
-  LINE_COLUMN_CLASSES = "." + LINE_NUMBER_CLASS + ", .line_content";
-
-  TEXT_FILE_SELECTOR = '.text-file';
-
-  function FilesCommentButton(filesContainerElement) {
-    this.render = this.render.bind(this);
-    this.hideButton = this.hideButton.bind(this);
-    this.isParallelView = notes.isParallelView();
-    filesContainerElement.on('mouseover', LINE_COLUMN_CLASSES, this.render)
-      .on('mouseleave', LINE_COLUMN_CLASSES, this.hideButton);
-  }
-
-  FilesCommentButton.prototype.render = function(e) {
+  showButton(e) {
     const $currentTarget = $(e.currentTarget);
     const buttonParentElement = this.getButtonParent($currentTarget);
 
     if (!this.validateButtonParent(buttonParentElement)) return;
 
-    $button = $(COMMENT_BUTTON_CLASS, buttonParentElement);
     buttonParentElement.addClass('is-over')
       .nextUntil(`.${LINE_CONTENT_CLASS}`).addClass('is-over');
-  };
+  },
 
-  FilesCommentButton.prototype.hideButton = function(e) {
-    var $currentTarget = $(e.currentTarget);
-    var buttonParentElement = this.getButtonParent($currentTarget);
+  hideButton(e) {
+    const $currentTarget = $(e.currentTarget);
+    const buttonParentElement = this.getButtonParent($currentTarget);
 
     buttonParentElement.removeClass('is-over')
       .nextUntil(`.${LINE_CONTENT_CLASS}`).removeClass('is-over');
-  };
+  },
 
-  FilesCommentButton.prototype.getButtonParent = function(hoveredElement) {
-    if (!this.isParallelView) {
+  getButtonParent(hoveredElement) {
+    if (!notes.isParallelView()) {
       if (hoveredElement.hasClass(OLD_LINE_CLASS)) {
         return hoveredElement;
       }
@@ -62,22 +54,9 @@ window.FilesCommentButton = (function() {
       }
       return $(hoveredElement).prev("." + LINE_NUMBER_CLASS);
     }
-  };
+  },
 
-  FilesCommentButton.prototype.validateButtonParent = function(buttonParentElement) {
+  validateButtonParent(buttonParentElement) {
     return !buttonParentElement.hasClass(EMPTY_CELL_CLASS) && !buttonParentElement.hasClass(UNFOLDABLE_LINE_CLASS);
-  };
-
-  return FilesCommentButton;
-})();
-
-$.fn.filesCommentButton = function() {
-  if (!(this && (this.parent().data('can-create-note') != null))) {
-    return;
-  }
-  return this.each(function() {
-    if (!$.data(this, 'filesCommentButton')) {
-      return $.data(this, 'filesCommentButton', new FilesCommentButton($(this)));
-    }
-  });
+  },
 };
