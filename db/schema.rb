886dd5aeb21750ac377cd56fca5d2991c1fb0cd8
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2017-06-12 10:19:27 +0300

    Resolve conflicts in Gemfile.lock and db/schema.rb
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20170603200744) do
-
-=======
 ActiveRecord::Schema.define(version: 20170606202615) do
->>>>>>> ce-com/master
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "pg_trgm"
