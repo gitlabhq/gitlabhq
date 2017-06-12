e51078ecfc4aeae7bdfa0ced45fe0de21c9afd8c
Eric Eastwood <contact@ericeastwood.com>
2017-06-12 14:42:12 -0500

    Disable autocomplete on snippets comments
    Fix https://gitlab.com/gitlab-org/gitlab-ce/issues/33594
    
    Was supposedly fixed in MR for
    https://gitlab.com/gitlab-org/gitlab-ce/issues/32086 but seeing this pop
    up again

--- spec/features/projects/snippets_spec.rb
+++ spec/features/projects/snippets_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'Project snippets', feature: true do
+describe 'Project snippets', :js, feature: true do
   context 'when the project has snippets' do
     let(:project) { create(:empty_project, :public) }
     let!(:snippets) { create_list(:project_snippet, 2, :public, author: project.owner, project: project) }
@@ -24,6 +24,29 @@ describe 'Project snippets', feature: true do
 
         expect(page).to have_content(snippets[0].title)
         expect(page).to have_content(snippets[1].title)
+      end
+    end
+
+    context 'when submitting a note' do
+      before do
+        login_as :admin
+        visit namespace_project_snippet_path(project.namespace, project, snippets[0])
+      end
+
+      it 'should not have autocomplete' do
+        wait_for_requests
+        request_count_before = page.driver.network_traffic.count
+
+        find('#note_note').native.send_keys('')
+        fill_in 'note[note]', with: '@'
+
+        wait_for_requests
+        request_count_after = page.driver.network_traffic.count
+
+        # This selector probably won't be in place even if autocomplete was enabled
+        # but we want to make sure
+        expect(page).not_to have_selector('.atwho-view')
+        expect(request_count_before).to eq(request_count_after)
       end
     end
   end
