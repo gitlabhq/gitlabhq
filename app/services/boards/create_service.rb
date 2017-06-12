c620ca037e865947cbdc49f2b629b8654282bd1f
Phil Hughes <me@iamphill.com>
2017-06-12 09:06:52 +0100

    resolve app/services/boards/create_service.rb
    [ci skip]

--- app/services/boards/create_service.rb
+++ app/services/boards/create_service.rb
@@ -3,18 +3,10 @@ module Boards
     def execute
       board = project.boards.create(params)
 
-<<<<<<< HEAD
       if board.persisted?
+        board.lists.create(list_type: :backlog)
         board.lists.create(list_type: :closed)
       end
-=======
-    private
-
-    def create_board!
-      board = project.boards.create
-      board.lists.create(list_type: :backlog)
-      board.lists.create(list_type: :closed)
->>>>>>> ce-com/master
 
       board
     end
