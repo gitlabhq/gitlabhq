a69be90ae717ae8a4494c33e13e6d8c16069affc
Nick Thomas <nick@gitlab.com>
2017-06-12 10:02:07 +0100

    Add explicit licensing for Elasticsearch

--- app/models/license.rb
+++ app/models/license.rb
@@ -7,12 +7,14 @@ class License < ActiveRecord::Base
   AUDITOR_USER_FEATURE = 'GitLab_Auditor_User'.freeze
   SERVICE_DESK_FEATURE = 'GitLab_ServiceDesk'.freeze
   OBJECT_STORAGE_FEATURE = 'GitLab_ObjectStorage'.freeze
+  ELASTIC_SEARCH_FEATURE = 'GitLab_ElasticSearch'.freeze
 
   FEATURE_CODES = {
     geo: GEO_FEATURE,
     auditor_user: AUDITOR_USER_FEATURE,
     service_desk: SERVICE_DESK_FEATURE,
     object_storage: OBJECT_STORAGE_FEATURE,
+    elastic_search: ELASTIC_SEARCH_FEATURE,
     # Features that make sense to Namespace:
     deploy_board: DEPLOY_BOARD_FEATURE,
     file_lock: FILE_LOCK_FEATURE
@@ -24,7 +26,7 @@ class License < ActiveRecord::Base
   EARLY_ADOPTER_PLAN = 'early_adopter'.freeze
 
   EES_FEATURES = [
-    # ..
+    { ELASTIC_SEARCH_FEATURE => 1 }
   ].freeze
 
   EEP_FEATURES = [
@@ -90,6 +92,8 @@ class License < ActiveRecord::Base
         load_license
       end
     end
+
+    delegate :feature_available?, to: :current, allow_nil: true
 
     def reset_current
       RequestStore.delete(:current_license)
