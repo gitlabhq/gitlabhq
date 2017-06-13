fe4107b732533bef01728e74137a99cf60dfab8e
Phil Hughes <me@iamphill.com>
2017-06-13 16:18:51 +0100

    fixed board create_service_spec

--- spec/services/boards/create_service_spec.rb
+++ spec/services/boards/create_service_spec.rb
@@ -44,8 +44,8 @@ describe Boards::CreateService, services: true do
       it "creates board's default lists" do
         board = service.execute
 
-        expect(board.lists.size).to eq 1
-        expect(board.lists.first).to be_closed
+        expect(board.lists.size).to eq 2
+        expect(board.lists.last).to be_closed
       end
     end
   end
