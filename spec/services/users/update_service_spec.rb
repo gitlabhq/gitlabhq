bfab2f5f2ac3e9b9a021f3f4791f53f5438c278b
James Lopez <james@jameslopez.es>
2017-06-13 13:52:20 +0200

    fix spec

--- spec/services/users/update_service_spec.rb
+++ spec/services/users/update_service_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 describe Users::UpdateService, services: true do
   let(:user) { create(:user) }
   let(:admin) { create(:admin) }
-  let(:user) { create(:empty_user, creator_id: user.id, namespace: user.namespace) }
 
   describe '#execute' do
     it 'updates the name' do
@@ -14,20 +13,20 @@ describe Users::UpdateService, services: true do
 
     context 'when updated by an admin' do
       it 'updates the name' do
-        result = update_user(user, admin, name: 'New Name')
+        result = update_user(admin, user, name: 'New Name')
         expect(result).to eq({ status: :success })
         expect(user.name).to eq('New Name')
       end
     end
 
     it 'returns an error result when record cannot be updated' do
-      result = update_user(user, create(:user), { name: 'New Name' })
-
-      expect(result).to eq({ status: :error, message: 'User could not be updated' })
+      expect do
+        update_user(user, create(:user), { name: 'New Name' })
+      end.to raise_error Gitlab::Access::AccessDeniedError
     end
 
     def update_user(current_user, user, opts)
-      described_class.new(user, user, opts).execute
+      described_class.new(current_user, user, opts).execute
     end
   end
 end
