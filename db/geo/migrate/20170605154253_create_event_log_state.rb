c57f9dfb57f8f2b97118607f8d0d9fd635902b7c
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2017-06-13 17:00:02 -0300

    Use 8 bytes value for event_id column on the geo_event_log_state table

--- db/geo/migrate/20170605154253_create_event_log_state.rb
+++ db/geo/migrate/20170605154253_create_event_log_state.rb
@@ -1,7 +1,7 @@
 class CreateEventLogState < ActiveRecord::Migration
   def change
     create_table :event_log_states, id: false do |t|
-      t.primary_key :event_id, :bigserial
+      t.integer :event_id, limit: 8, primary_key: true
     end
   end
 end
