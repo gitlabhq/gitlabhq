5da5ed42b5e342171617fc82ce26f8ef74cd461f
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2017-06-13 17:03:56 -0300

    Validate presence of Geo::EventLogState#event_id

--- app/models/geo/event_log_state.rb
+++ app/models/geo/event_log_state.rb
@@ -1,6 +1,8 @@
 class Geo::EventLogState < Geo::BaseRegistry
   self.primary_key = :event_id
 
+  validates :event_id, presence: true
+
   def self.last_processed
     order(event_id: :desc).first
   end
