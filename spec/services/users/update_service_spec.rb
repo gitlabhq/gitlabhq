50dc44a467fed6a915937b9cc4b09c9e79575c7e
James Lopez <james@jameslopez.es>
2017-06-13 16:24:36 +0200

    added bang option to spec

--- spec/services/users/update_service_spec.rb
+++ spec/services/users/update_service_spec.rb
@@ -7,6 +7,7 @@ describe Users::UpdateService, services: true do
   describe '#execute' do
     it 'updates the name' do
       result = update_user(user, user, name: 'New Name')
+
       expect(result).to eq({ status: :success })
       expect(user.name).to eq('New Name')
     end
@@ -14,6 +15,7 @@ describe Users::UpdateService, services: true do
     context 'when updated by an admin' do
       it 'updates the name' do
         result = update_user(admin, user, name: 'New Name')
+
         expect(result).to eq({ status: :success })
         expect(user.name).to eq('New Name')
       end
@@ -27,6 +29,34 @@ describe Users::UpdateService, services: true do
 
     def update_user(current_user, user, opts)
       described_class.new(current_user, user, opts).execute
+    end
+  end
+
+  describe '#execute!' do
+    it 'updates the name' do
+      result = update_user(user, user, name: 'New Name')
+
+      expect(result).to be true
+      expect(user.name).to eq('New Name')
+    end
+
+    context 'when updated by an admin' do
+      it 'updates the name' do
+        result = update_user(admin, user, name: 'New Name')
+
+        expect(result).to be true
+        expect(user.name).to eq('New Name')
+      end
+    end
+
+    it 'returns an error result when record cannot be updated' do
+      expect do
+        update_user(user, create(:user), { name: 'New Name' })
+      end.to raise_error Gitlab::Access::AccessDeniedError
+    end
+
+    def update_user(current_user, user, opts)
+      described_class.new(current_user, user, opts).execute!
     end
   end
 end
