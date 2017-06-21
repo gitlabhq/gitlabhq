d61450a36372aaf0b3ce242a88a972f69809091d
Phil Hughes <me@iamphill.com>
2017-06-21 07:33:40 +0000

    Merge branch 'bulk-edit-scroll-fix' into 'master'
    Bulk edit scroll fix
    
    Closes #33870
    
    See merge request !12219

--- spec/features/issues/bulk_assignment_labels_spec.rb
+++ spec/features/issues/bulk_assignment_labels_spec.rb
@@ -16,6 +16,21 @@ feature 'Issues > Labels bulk assignment', feature: true do
       login_as user
     end
 
+    context 'sidebar' do
+      before do
+        enable_bulk_update
+      end
+
+      it 'is present when bulk edit is enabled' do
+        expect(page).to have_css('.issuable-sidebar')
+      end
+
+      it 'is not present when bulk edit is disabled' do
+        disable_bulk_update
+        expect(page).not_to have_css('.issuable-sidebar')
+      end
+    end
+
     context 'can bulk assign' do
       before do
         enable_bulk_update
@@ -397,5 +412,9 @@ feature 'Issues > Labels bulk assignment', feature: true do
   def enable_bulk_update
     visit namespace_project_issues_path(project.namespace, project)
     click_button 'Edit Issues'
+  end
+
+  def disable_bulk_update
+    click_button 'Cancel'
   end
 end
