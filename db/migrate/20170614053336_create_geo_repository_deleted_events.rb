146e146386c49a554b14625f8c7557ffe1fe6532
Stan Hu <stanhu@gmail.com>
2017-06-21 07:23:58 -0700

    Use add_timestamps_with_timezone

--- db/migrate/20170614053336_create_geo_repository_deleted_events.rb
+++ db/migrate/20170614053336_create_geo_repository_deleted_events.rb
@@ -7,13 +7,13 @@ class CreateGeoRepositoryDeletedEvents < ActiveRecord::Migration
     create_table :geo_repository_deleted_events, id: :bigserial do |t|
       # If a project is deleted, we need to retain this entry
       t.references :project, index: true, foreign_key: false, null: false
-      t.datetime :created_at, null: false
       t.text :repository_storage_path, null: false
       t.text :deleted_path, null: false
       t.text :deleted_wiki_path
       t.text :deleted_project_name, null: false
     end
 
+    add_timestamps_with_timezone :geo_repository_deleted_events
     add_column :geo_event_log, :repository_deleted_event_id, :integer, limit: 8
   end
 end
