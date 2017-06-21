9f586b8181cfab3a3b8e5ff5d47cf29d01851a6a
Toon Claes <toon@gitlab.com>
2017-06-21 16:28:27 +0200

    Refactor long `if` condition to helper method for readability

--- app/helpers/ee/search_helper.rb
+++ app/helpers/ee/search_helper.rb
@@ -2,9 +2,16 @@ module EE
   module SearchHelper
     def search_filter_input_options(type)
       options = super
-      options[:data][:'multiple-assignees'] = 'true' if (type == :issues) && @project.feature_available?(:multiple_issue_assignees)
+      options[:data][:'multiple-assignees'] = 'true' if search_multiple_assignees?(type)
 
       options
+    end
+
+    private
+
+    def search_multiple_assignees?(type)
+      type == :issues &&
+        @project.feature_available?(:multiple_issue_assignees)
     end
   end
 end
