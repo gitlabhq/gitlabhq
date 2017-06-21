5590f349581e2237103b6920b0f24ed68a5c68dd
Shinya Maeda <shinya@gitlab.com>
2017-06-21 18:25:01 +0900

    pipeline_schedule_variables model/db

---
+++ app/models/concerns/has_variable.rb
@@ -0,0 +1,24 @@
+module HasVariable
+  extend ActiveSupport::Concern
+
+  included do
+    validates :key,
+      presence: true,
+      length: { maximum: 255 },
+      format: { with: /\A[a-zA-Z0-9_]+\z/,
+                message: "can contain only letters, digits and '_'." }
+
+    scope :order_key_asc, -> { reorder(key: :asc) }
+    scope :unprotected, -> { where(protected: false) }
+
+    attr_encrypted :value,
+       mode: :per_attribute_iv_and_salt,
+       insecure_mode: true,
+       key: Gitlab::Application.secrets.db_key_base,
+       algorithm: 'aes-256-cbc'
+
+    def to_runner_variable
+      { key: key, value: value, public: false }
+    end
+  end
+end
