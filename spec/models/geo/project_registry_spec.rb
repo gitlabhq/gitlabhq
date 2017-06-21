e191eb67e35e2d3438e4c176eb1a38b967cb3d62
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2017-06-20 22:24:19 -0300

    Does not return dirty projects on Geo::ProjectRegistry.synced

--- spec/models/geo/project_registry_spec.rb
+++ spec/models/geo/project_registry_spec.rb
@@ -8,4 +8,51 @@ describe Geo::ProjectRegistry, models: true do
   describe 'validations' do
     it { is_expected.to validate_presence_of(:project) }
   end
+
+  describe '.synced' do
+    let(:project) { create(:empty_project) }
+    let(:synced_at) { Time.now }
+
+    it 'does not return dirty projects' do
+      Geo::ProjectRegistry.create(
+        project: project,
+        last_repository_synced_at: synced_at,
+        last_repository_successful_sync_at: synced_at,
+        last_wiki_synced_at: synced_at,
+        last_wiki_successful_sync_at: synced_at,
+        resync_repository: true,
+        resync_wiki: true
+      )
+
+      expect(described_class.synced).to be_empty
+    end
+
+    it 'does not return projects where last attempt to sync failed' do
+      Geo::ProjectRegistry.create(
+        project: project,
+        last_repository_synced_at: synced_at,
+        last_repository_successful_sync_at: nil,
+        last_wiki_synced_at: synced_at,
+        last_wiki_successful_sync_at: nil,
+        resync_repository: true,
+        resync_wiki: true
+      )
+
+      expect(described_class.synced).to be_empty
+    end
+
+    it 'returns synced projects' do
+      registry = Geo::ProjectRegistry.create(
+        project: project,
+        last_repository_synced_at: synced_at,
+        last_repository_successful_sync_at: synced_at,
+        last_wiki_synced_at: synced_at,
+        last_wiki_successful_sync_at: synced_at,
+        resync_repository: false,
+        resync_wiki: false
+      )
+
+      expect(described_class.synced).to match_array([registry])
+    end
+  end
 end
