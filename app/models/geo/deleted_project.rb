3c0a94a0e25c75d0bbbd8ebb55295d9ebe69fb6e
Gabriel Mazetto <brodock@gmail.com>
2017-06-22 01:31:12 +0200

    When repository_storage is nil, pick default from local configuration

--- app/models/geo/deleted_project.rb
+++ app/models/geo/deleted_project.rb
@@ -2,7 +2,9 @@ class Geo::DeletedProject < ::Project
   after_initialize :readonly!
   attr_reader :path_with_namespace
 
-  def initialize(id:, name:, path_with_namespace:, repository_storage: 'default')
+  def initialize(id:, name:, path_with_namespace:, repository_storage:)
+    repository_storage = current_application_settings.pick_repository_storage unless repository_storage
+
     super(id: id, name: name, repository_storage: repository_storage)
     @path_with_namespace = path_with_namespace
   end
