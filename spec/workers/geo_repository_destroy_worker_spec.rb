4d3eac1a6c336ea5f2f099f30189870ff992fcbe
Gabriel Mazetto <brodock@gmail.com>
2017-06-22 00:54:36 +0200

    Added specs for probe_repository_storage

--- spec/workers/geo_repository_destroy_worker_spec.rb
+++ spec/workers/geo_repository_destroy_worker_spec.rb
@@ -6,10 +6,6 @@ describe GeoRepositoryDestroyWorker do
   let!(:remove_path) { path.sub(/\.git\Z/, "+#{project.id}+deleted.git") }
   let(:perform!) { subject.perform(project.id, project.name, path) }
 
-  before do
-    project.delete
-  end
-
   it 'delegates project removal to Projects::DestroyService' do
     expect_any_instance_of(::Projects::DestroyService).to receive(:geo_replicate)
 
@@ -17,6 +13,10 @@ describe GeoRepositoryDestroyWorker do
   end
 
   context 'sidekiq execution' do
+    before do
+      project.delete
+    end
+
     it 'removes the repository from disk' do
       expect(project.gitlab_shell.exists?(project.repository_storage_path, path + '.git')).to be_truthy
 
@@ -24,6 +24,16 @@ describe GeoRepositoryDestroyWorker do
 
       expect(project.gitlab_shell.exists?(project.repository_storage_path, path + '.git')).to be_falsey
       expect(project.gitlab_shell.exists?(project.repository_storage_path, remove_path + '.git')).to be_falsey
+    end
+  end
+
+  describe '#probe_repository_storage' do
+    it 'returns a repository_storage when repository can be found' do
+      expect(subject.send(:probe_repository_storage, project.path_with_namespace)).to eq('default')
+    end
+
+    it 'returns nil when repository cannot be found in any existing repository_storage' do
+      expect(subject.send(:probe_repository_storage, 'nonexistent/project')).to eq(nil)
     end
   end
 end
