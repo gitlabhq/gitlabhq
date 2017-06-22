27c999e1381a72dddaa64ac4923df6cac96fb091
Douwe Maan <douwe@gitlab.com>
2017-06-22 01:03:22 +0000

    Merge branch 'bvl-security-9-0-remove-appearance-symlink' into 'security-9-0'
    (security-9-0) Remove the `appearance` symlink that was previously missed
    
    See merge request !2127

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170606202615) do
+ActiveRecord::Schema.define(version: 20170613111224) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "pg_trgm"
