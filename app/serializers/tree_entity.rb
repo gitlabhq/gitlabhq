f516b8d0d7c8284ed948a26d1476856c28ad9bc7
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2017-06-22 10:50:55 +0200

    Expose last_commit information for tree and blob when serialized
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- app/serializers/tree_entity.rb
+++ app/serializers/tree_entity.rb
@@ -3,6 +3,10 @@ class TreeEntity < Grape::Entity
 
   expose :id, :path, :name, :mode
 
+  expose :last_commit do |tree|
+    request.project.repository.last_commit_for_path(tree.commit_id, tree.path)
+  end
+
   expose :icon do |tree|
     IconsHelper.file_type_icon_class('folder', tree.mode, tree.name)
   end
