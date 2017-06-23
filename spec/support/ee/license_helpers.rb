2b90727c1078f656c84e9323aa756e58d7ffd3a9
Bob Van Landuyt <bob@gitlab.com>
2017-06-23 08:52:09 +0200

    Change method signature to allow stubbing multiple features

--- spec/support/ee/license_helpers.rb
+++ spec/support/ee/license_helpers.rb
@@ -1,9 +1,19 @@
 module EE
   module LicenseHelpers
-    def stub_feature(feature, enabled = true)
+    # Enable/Disable a feature on the License for a spec.
+    #
+    # Example:
+    #
+    #   stub_licensed_features(geo: true, deploy_board: false)
+    #
+    # This enables `geo` and disables `deploy_board` features for a spec.
+    # Other features are still enabled/disabled as defined in the licence.
+    def stub_licensed_features(features)
       allow(License).to receive(:feature_available?).and_call_original
 
-      allow(License).to receive(:feature_available?).with(feature) { enabled }
+      features.each do |feature, enabled|
+        allow(License).to receive(:feature_available?).with(feature) { enabled }
+      end
     end
   end
 end
