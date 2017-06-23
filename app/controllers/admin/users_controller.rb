efb3d5e70f163bfd186fa3a02c967154103d27f4
James Lopez <james@jameslopez.es>
2017-06-23 19:00:22 +0200

    fix spec failures

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -152,7 +152,7 @@ class Admin::UsersController < Admin::ApplicationController
 
   def remove_email
     email = user.emails.find(params[:email_id])
-    success = Emails::DestroyService.new(current_user, user, email: email.email).execute
+    success = Emails::DestroyService.new(user, email: email.email).execute
 
     respond_to do |format|
       if success
--- spec/services/emails/destroy_service_spec.rb
+++ spec/services/emails/destroy_service_spec.rb
@@ -4,7 +4,7 @@ describe Emails::DestroyService, services: true do
   let!(:user) { create(:user) }
   let!(:email) { create(:email, user: user) }
 
-  subject(:service) { described_class.new(user, user, email: email.email) }
+  subject(:service) { described_class.new(user, email: email.email) }
 
   describe '#execute' do
     it 'removes an email' do
