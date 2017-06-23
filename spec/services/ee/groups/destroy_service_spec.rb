088a401f26304f8b0085f972a2297bd1e30856eb
Stan Hu <stanhu@gmail.com>
2017-06-23 11:17:00 -0700

    Add spec to ensure that deletion of a namespace also generates an event log

---
+++ spec/services/ee/groups/destroy_service_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe Groups::DestroyService, services: true do
+  let!(:user)         { create(:user) }
+  let!(:group)        { create(:group) }
+  let!(:project)      { create(:empty_project, namespace: group) }
+  let!(:geo_node) { create(:geo_node, :current, :primary) }
+
+  before do
+    group.add_user(user, Gitlab::Access::OWNER)
+  end
+
+  it 'creates a Geo event log for each project deleted' do
+    Groups::DestroyService.new(group, user).execute
+
+    expect(Geo::EventLog.count).to eq(1)
+    expect(Geo::RepositoryDeletedEvent.count).to eq(1)
+  end
+end
