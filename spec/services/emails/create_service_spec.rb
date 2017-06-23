bf82b5886c4c106d312c50ec8491d9fce5eb66d6
James Lopez <james@jameslopez.es>
2017-06-23 19:00:22 +0200

    fix spec failures

--- spec/services/emails/create_service_spec.rb
+++ spec/services/emails/create_service_spec.rb
@@ -4,7 +4,7 @@ describe Emails::CreateService, services: true do
   let(:user) { create(:user) }
   let(:opts) { { email: 'new@email.com' } }
 
-  subject(:service) { described_class.new(user, user, opts) }
+  subject(:service) { described_class.new(user, opts) }
 
   describe '#execute' do
     it 'creates an email with valid attributes' do
