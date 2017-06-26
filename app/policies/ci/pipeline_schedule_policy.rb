9c369648094fdfa41b3b4f648eab7e839d82b8bb
Shinya Maeda <shinya@gitlab.com>
2017-06-26 18:25:08 +0900

    Add functionality and security.

--- app/policies/ci/pipeline_schedule_policy.rb
+++ app/policies/ci/pipeline_schedule_policy.rb
@@ -1,4 +1,15 @@
 module Ci
   class PipelineSchedulePolicy < PipelinePolicy
+    alias_method :pipeline_schedule, :subject
+
+    def rules
+      super
+
+      access = pipeline_schedule.project.team.max_member_access(user.id)
+
+      if access == Gitlab::Access::DEVELOPER && pipeline_schedule.owner != user
+        cannot! :update_pipeline_schedule
+      end
+    end
   end
 end
