4d7b6a6d42cce3578a6c977c830ca7c36d232c4c
Ruben Davila <rdavila84@gmail.com>
2017-06-26 23:32:29 -0500

    Add basic form for creating trial licenses [ci skip]

---
+++ app/controllers/trials_controller.rb
@@ -0,0 +1,4 @@
+class TrialsController < ApplicationController
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -82,6 +82,9 @@ Rails.application.routes.draw do
   # Notification settings
   resources :notification_settings, only: [:create, :update]
 
+  # Trial licenses
+  resources :trials, only: [:new, :create]
+
   draw :import
   draw :uploads
   draw :explore
