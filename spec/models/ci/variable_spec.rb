de893b19c30acf83ce43dd42376783505d704763
Shinya Maeda <shinya@gitlab.com>
2017-06-28 15:25:36 +0900

    Improve small things

--- spec/models/ci/variable_spec.rb
+++ spec/models/ci/variable_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Ci::Variable, models: true do
   subject { build(:ci_variable) }
 
-  it { is_expected.to be_kind_of(HasVariable) }
+  it { is_expected.to include_module(HasVariable) }
   it { is_expected.to validate_uniqueness_of(:key).scoped_to(:project_id) }
 
   describe '.unprotected' do
