98992c4e4b3231a99eb5ff17c44e96fe79a6cff2
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2017-06-28 12:01:52 +0200

    Add initial build stage_id ref background migration

--- lib/gitlab/background_migration/migrate_build_stage_id_reference.rb
+++ lib/gitlab/background_migration/migrate_build_stage_id_reference.rb
@@ -1,15 +1,19 @@
 module Gitlab
   module BackgroundMigration
     class MigrateBuildStageIdReference
-      class Build < ActiveRecord::Base
-        self.table_name = 'ci_builds'
-      end
-
-      class Stage < ActiveRecord::Base
-        self.table_name = 'ci_stages'
-      end
-
       def perform(id)
+        raise ArgumentError unless id.is_a?(Integer)
+
+        sql = <<-SQL.strip_heredoc
+          UPDATE "ci_builds" SET "stage_id" = (
+            SELECT id FROM ci_stages
+              WHERE ci_stages.pipeline_id = ci_builds.commit_id
+              AND ci_stages.name = ci_builds.stage
+            )
+          WHERE "ci_builds"."id" = #{id} AND "ci_builds"."stage_id" IS NULL
+        SQL
+
+        ActiveRecord::Base.connection.execute(sql)
       end
     end
   end
