3a23766a3c354fb226f9826603dcb6cd529775b2
Shinya Maeda <shinya@gitlab.com>
2017-06-28 22:40:14 +0900

    Rename route2

---
+++ app/controllers/projects/project_variables_controller.rb
@@ -0,0 +1,50 @@
+class Projects::ProjectVariablesController < Projects::ApplicationController
+  before_action :authorize_admin_build!
+
+  layout 'project_settings'
+
+  def index
+    redirect_to namespace_project_settings_ci_cd_path(@project.namespace, @project)
+  end
+
+  def show
+    @variable = @project.variables.find(params[:id])
+  end
+
+  def update
+    @variable = @project.variables.find(params[:id])
+
+    if @variable.update_attributes(project_params)
+      redirect_to namespace_project_project_variables_path(project.namespace, project), notice: 'Variable was successfully updated.'
+    else
+      render action: "show"
+    end
+  end
+
+  def create
+    @variable = Ci::ProjectVariable.new(project_params)
+
+    if @variable.valid? && @project.variables << @variable
+      flash[:notice] = 'Variables were successfully updated.'
+      redirect_to namespace_project_settings_ci_cd_path(project.namespace, project)
+    else
+      render "show"
+    end
+  end
+
+  def destroy
+    @key = @project.variables.find(params[:id])
+    @key.destroy
+
+    redirect_to namespace_project_settings_ci_cd_path(project.namespace, project),
+                status: 302,
+                notice: 'Variable was successfully removed.'
+  end
+
+  private
+
+  def project_params
+    params.require(:project_variable)
+      .permit([:id, :key, :value, :protected, :_destroy])
+  end
+end
