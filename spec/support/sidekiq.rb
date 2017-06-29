945cdf326edcafdcd7a1d2aeaef611d888a4828e
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2017-06-29 11:41:19 +0200

    Make it possible to schedule bg migrations in bulk

--- spec/support/sidekiq.rb
+++ spec/support/sidekiq.rb
@@ -1,5 +1,11 @@
-require 'sidekiq/testing/inline'
+require 'sidekiq/testing'
 
 Sidekiq::Testing.server_middleware do |chain|
   chain.add Gitlab::SidekiqStatus::ServerMiddleware
+end
+
+RSpec.configure do |config|
+  config.after(:each, :sidekiq) do
+    Sidekiq::Worker.clear_all
+  end
 end
