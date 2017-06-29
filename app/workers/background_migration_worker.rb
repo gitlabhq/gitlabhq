af2f2dc5ed588d33919d5db3f684c165d7427ab7
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2017-06-29 11:41:19 +0200

    Make it possible to schedule bg migrations in bulk

--- app/workers/background_migration_worker.rb
+++ app/workers/background_migration_worker.rb
@@ -2,16 +2,30 @@ class BackgroundMigrationWorker
   include Sidekiq::Worker
   include DedicatedSidekiqQueue
 
-  # Schedules a number of jobs in bulk
+  # Enqueues a number of jobs in bulk.
   #
   # The `jobs` argument should be an Array of Arrays, each sub-array must be in
   # the form:
   #
   #     [migration-class, [arg1, arg2, ...]]
-  def self.perform_bulk(*jobs)
+  def self.perform_bulk(jobs)
     Sidekiq::Client.push_bulk('class' => self,
                               'queue' => sidekiq_options['queue'],
                               'args' => jobs)
+  end
+
+  # Schedules a number of jobs in bulk, with a delay.
+  #
+  def self.perform_bulk_in(delay, jobs)
+    now = Time.now.to_f
+    schedule = now + delay.to_f
+
+    raise ArgumentError if schedule <= now
+
+    Sidekiq::Client.push_bulk('class' => self,
+                              'queue' => sidekiq_options['queue'],
+                              'args' => jobs,
+                              'at' => schedule)
   end
 
   # Performs the background migration.
