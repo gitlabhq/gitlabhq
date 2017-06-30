6997dfa3426b26f7eb8f294b261827ef0b6d823b
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2017-06-30 12:07:31 +0200

    Sanitize id value passed to async background migration

--- lib/gitlab/background_migration/migrate_build_stage_id_reference.rb
+++ lib/gitlab/background_migration/migrate_build_stage_id_reference.rb
@@ -2,15 +2,13 @@ module Gitlab
   module BackgroundMigration
     class MigrateBuildStageIdReference
       def perform(id)
-        raise ArgumentError unless id.present?
-
         sql = <<-SQL.strip_heredoc
           UPDATE "ci_builds"
             SET "stage_id" =
               (SELECT id FROM ci_stages
                 WHERE ci_stages.pipeline_id = ci_builds.commit_id
                 AND ci_stages.name = ci_builds.stage)
-          WHERE "ci_builds"."id" = #{id}
+          WHERE "ci_builds"."id" = #{id.to_i}
             AND "ci_builds"."stage_id" IS NULL
         SQL
 
