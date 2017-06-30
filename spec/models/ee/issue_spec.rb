4c1af6cc1458985f348610e6b8237ca9d5225cec
Toon Claes <toon@gitlab.com>
2017-06-30 11:22:51 +0200

    Use polymorphism to implement allows_multiple_assignees?
    Only Issue might allow multiple assignees, but only when the
    multiple_issue_assignees feature is available. Do this check in Issue
    itself, instead of a type-based check in Issuable.

--- spec/models/ee/issue_spec.rb
+++ spec/models/ee/issue_spec.rb
@@ -1,6 +1,24 @@
 require 'spec_helper'
 
 describe Issue do
+  describe '#allows_multiple_assignees?' do
+    it 'does not allow multiple assignees without license' do
+      stub_licensed_features(multiple_issue_assignees: false)
+
+      issue = build(:issue)
+
+      expect(issue.allows_multiple_assignees?).to be_falsey
+    end
+
+    it 'does not allow multiple assignees without license' do
+      stub_licensed_features(multiple_issue_assignees: true)
+
+      issue = build(:issue)
+
+      expect(issue.allows_multiple_assignees?).to be_truthy
+    end
+  end
+
   describe '#weight' do
     [
       { license: true,  database: 5,    expected: 5 },
