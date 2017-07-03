cb1f3423876f84861facc4c3631eeb4b453ceee6
Nick Thomas <nick@gitlab.com>
2017-07-03 11:31:37 +0100

    Don't resolve fork relationships for projects pending delete

--- spec/models/forked_project_link_spec.rb
+++ spec/models/forked_project_link_spec.rb
@@ -24,6 +24,22 @@ describe ForkedProjectLink, "add link on fork" do
     expect(project_to.forked_from_project).to eq(project_from)
   end
 
+  context 'project_to is pending_delete' do
+    before do
+      project_to.update!(pending_delete: true)
+    end
+
+    it { expect(project_from.forks.count).to eq(0) }
+  end
+
+  context 'project_from is pending_delete' do
+    before do
+      project_from.update!(pending_delete: true)
+    end
+
+    it { expect(project_to.forked_from_project).to be_nil }
+  end
+
   describe '#forked?' do
     let(:project_to) { create(:project, forked_project_link: forked_project_link) }
     let(:forked_project_link) { build(:forked_project_link) }
