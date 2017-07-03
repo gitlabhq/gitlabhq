76bcb1d996d666adf5ce5337eb7fe2c09eb184d1
James Lopez <james@jameslopez.es>
2017-07-03 11:13:38 +0200

    add new changes tests

---
+++ spec/lib/ee/audit/changes_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe EE::Audit::Changes do
+  describe '.audit_changes' do
+    let(:user) { create(:user) }
+    let(:foo_class) { Class.new { include EE::Audit::Changes } }
+
+    before do
+      allow_any_instance_of(foo_class).to receive(:model).and_return(user)
+    end
+
+    describe 'non audit changes' do
+      it 'does not call the audit event service' do
+        expect_any_instance_of(AuditEventService).not_to receive(:security_event)
+
+        user.name = 'new name'
+        foo_class.new.audit_changes(:email)
+      end
+    end
+
+    describe 'audit changes' do
+      it 'calls the audit event service' do
+        expect_any_instance_of(AuditEventService).to receive(:security_event)
+
+        user.name = 'new name'
+        foo_class.new.audit_changes(:name)
+      end
+    end
+  end
+end
