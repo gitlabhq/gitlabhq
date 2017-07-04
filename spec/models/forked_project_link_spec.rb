1501a4f578fa947af66a981fd56885be8467d5b5
Nick Thomas <nick@gitlab.com>
2017-07-04 11:48:25 +0100

    Clean up the ForkedProjectLink specs

--- spec/models/forked_project_link_spec.rb
+++ spec/models/forked_project_link_spec.rb
@@ -2,53 +2,59 @@ require 'spec_helper'
 
 describe ForkedProjectLink, "add link on fork" do
   let(:project_from) { create(:project, :repository) }
+  let(:project_to) { fork_project(project_from, user) }
   let(:user) { create(:user) }
   let(:namespace) { user.namespace }
 
   before do
-    create(:project_member, :reporter, user: user, project: project_from)
-    @project_to = fork_project(project_from, user)
+    project_from.add_reporter(user)
   end
 
-  it "project_to knows it is forked" do
-    expect(@project_to.forked?).to be_truthy
-  end
+  it 'project_from knows its forks' do
+    _ = project_to
 
-  it "project knows who it is forked from" do
-    expect(@project_to.forked_from_project).to eq(project_from)
-  end
-end
-
-describe '#forked?' do
-  let(:forked_project_link) { build(:forked_project_link) }
-  let(:project_from) { create(:project, :repository) }
-  let(:project_to) { create(:project, forked_project_link: forked_project_link) }
-
-  before :each do
-    forked_project_link.forked_from_project = project_from
-    forked_project_link.forked_to_project = project_to
-    forked_project_link.save!
+    expect(project_from.forks.count).to eq(1)
   end
 
   it "project_to knows it is forked" do
     expect(project_to.forked?).to be_truthy
   end
 
-  it "project_from is not forked" do
-    expect(project_from.forked?).to be_falsey
+  it "project knows who it is forked from" do
+    expect(project_to.forked_from_project).to eq(project_from)
   end
 
-  it "project_to.destroy destroys fork_link" do
-    expect(forked_project_link).to receive(:destroy)
-    project_to.destroy
+  describe '#forked?' do
+    let(:project_to) { create(:project, forked_project_link: forked_project_link) }
+    let(:forked_project_link) { build(:forked_project_link) }
+
+    before do
+      forked_project_link.forked_from_project = project_from
+      forked_project_link.forked_to_project = project_to
+      forked_project_link.save!
+    end
+
+    it "project_to knows it is forked" do
+      expect(project_to.forked?).to be_truthy
+    end
+
+    it "project_from is not forked" do
+      expect(project_from.forked?).to be_falsey
+    end
+
+    it "project_to.destroy destroys fork_link" do
+      expect(forked_project_link).to receive(:destroy)
+
+      project_to.destroy
+    end
   end
-end
 
-def fork_project(from_project, user)
-  shell = double('gitlab_shell', fork_repository: true)
+  def fork_project(from_project, user)
+    service = Projects::ForkService.new(from_project, user)
+    shell = double('gitlab_shell', fork_repository: true)
 
-  service = Projects::ForkService.new(from_project, user)
-  allow(service).to receive(:gitlab_shell).and_return(shell)
+    allow(service).to receive(:gitlab_shell).and_return(shell)
 
-  service.execute
+    service.execute
+  end
 end
