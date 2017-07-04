5af3eb5f9f4d467ad70a2c5107d7c837bff36afa
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2017-07-04 12:40:45 +0200

    Check foreign keys in migration in separate conditional

--- db/migrate/20170703102400_add_stage_id_foreign_key_to_builds.rb
+++ db/migrate/20170703102400_add_stage_id_foreign_key_to_builds.rb
@@ -8,14 +8,28 @@ class AddStageIdForeignKeyToBuilds < ActiveRecord::Migration
   def up
     unless index_exists?(:ci_builds, :stage_id)
       add_concurrent_index(:ci_builds, :stage_id)
+    end
+
+    unless foreign_key_exists?(:ci_builds, :stage_id)
       add_concurrent_foreign_key(:ci_builds, :ci_stages, column: :stage_id, on_delete: :cascade)
     end
   end
 
   def down
-    if index_exists?(:ci_builds, :stage_id)
+    if foreign_key_exists?(:ci_builds, :stage_id)
       remove_foreign_key(:ci_builds, column: :stage_id)
+    end
+
+    if index_exists?(:ci_builds, :stage_id)
       remove_concurrent_index(:ci_builds, :stage_id)
+    end
+  end
+
+  private
+
+  def foreign_key_exists?(table, column)
+    foreign_keys(:ci_builds).any? do |key|
+      key.options[:column] == column.to_s
     end
   end
 end
