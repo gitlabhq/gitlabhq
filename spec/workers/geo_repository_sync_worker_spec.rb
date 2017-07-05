27011f7729d28c72262a05930307f79718d51c5b
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2017-07-05 17:35:40 -0300

    Use the Geo::ProjectRegistry factory on geo_repository_worker_spec.rb

--- spec/workers/geo_repository_sync_worker_spec.rb
+++ spec/workers/geo_repository_sync_worker_spec.rb
@@ -20,19 +20,8 @@ describe GeoRepositorySyncWorker do
     end
 
     it 'performs Geo::ProjectSyncWorker for projects where last attempt to sync failed' do
-      Geo::ProjectRegistry.create(
-        project: project_1,
-        last_repository_synced_at: DateTime.now,
-        last_repository_successful_sync_at: nil
-      )
-
-      Geo::ProjectRegistry.create(
-        project: project_2,
-        last_repository_synced_at: DateTime.now,
-        last_repository_successful_sync_at: DateTime.now,
-        resync_repository: false,
-        resync_wiki: false
-      )
+      create(:geo_project_registry, :sync_failed, project: project_1)
+      create(:geo_project_registry, :synced, project: project_2)
 
       expect(Geo::ProjectSyncWorker).to receive(:perform_in).once.and_return(spy)
 
@@ -40,29 +29,9 @@ describe GeoRepositorySyncWorker do
     end
 
     it 'performs Geo::ProjectSyncWorker for synced projects updated recently' do
-      Geo::ProjectRegistry.create(
-        project: project_1,
-        last_repository_synced_at: 2.days.ago,
-        last_repository_successful_sync_at: 2.days.ago,
-        resync_repository: true,
-        resync_wiki: false
-      )
-
-      Geo::ProjectRegistry.create(
-        project: project_2,
-        last_repository_synced_at: 10.minutes.ago,
-        last_repository_successful_sync_at: 10.minutes.ago,
-        resync_repository: false,
-        resync_wiki: false
-      )
-
-      Geo::ProjectRegistry.create(
-        project: create(:empty_project),
-        last_repository_synced_at: 5.minutes.ago,
-        last_repository_successful_sync_at: 5.minutes.ago,
-        resync_repository: false,
-        resync_wiki: true
-      )
+      create(:geo_project_registry, :synced, :repository_dirty, project: project_1)
+      create(:geo_project_registry, :synced, project: project_2)
+      create(:geo_project_registry, :synced, :wiki_dirty)
 
       expect(Geo::ProjectSyncWorker).to receive(:perform_in).twice.and_return(spy)
 
