bbeeaad332a1ad11d38a982c10bf182a000e2403
Gabriel Mazetto <brodock@gmail.com>
2017-07-06 06:16:46 +0200

    Raise exception when Geo secondary database is not configured
    This is used by ActiveRecord before establishing a connection.
    Otherwise the user would receive a crypt `relation "xxx" does not exist"
    exception.

--- app/models/geo/base_registry.rb
+++ app/models/geo/base_registry.rb
@@ -4,4 +4,10 @@ class Geo::BaseRegistry < ActiveRecord::Base
   if Gitlab::Geo.secondary_role_enabled? && Gitlab::Geo.geo_database_configured?
     establish_connection Rails.configuration.geo_database
   end
+
+  def self.connection
+    raise 'Geo secondary database is not configured' unless Gitlab::Geo.geo_database_configured?
+
+    super
+  end
 end
