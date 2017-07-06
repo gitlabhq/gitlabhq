7ed7ca8cbeffeb1310e894ef9aa9f7e466824108
Tiago Botelho <tiagonbotelho@hotmail.com>
2017-07-06 14:43:07 +0100

    Refactors Project Destroy service and worker code

--- app/workers/project_destroy_worker.rb
+++ app/workers/project_destroy_worker.rb
@@ -7,10 +7,7 @@ class ProjectDestroyWorker
     user = User.find(user_id)
 
     ::Projects::DestroyService.new(project, user, params.symbolize_keys).execute
-  rescue Exception => error # rubocop:disable Lint/RescueException
-    project&.update_attributes(delete_error: error.message, pending_delete: false)
-    Rails.logger.error("Deletion failed on #{project&.full_path} with the following message: #{error.message}")
-
-    raise
+  rescue ActiveRecord::RecordNotFound => error
+    logger.error("Failed to delete project #{project.path_with_namespace} (#{project.id}): #{error.message}")
   end
 end
