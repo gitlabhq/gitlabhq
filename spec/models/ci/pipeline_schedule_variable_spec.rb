1ee9f7db83fb0e33b9cffbf437d20db343afd8a3
Shinya Maeda <shinya@gitlab.com>
2017-07-07 16:27:42 +0900

    Remove a spec: validate_uniqueness_of for key

--- spec/models/ci/pipeline_schedule_variable_spec.rb
+++ spec/models/ci/pipeline_schedule_variable_spec.rb
@@ -4,5 +4,4 @@ describe Ci::PipelineScheduleVariable, models: true do
   subject { build(:ci_pipeline_schedule_variable) }
 
   it { is_expected.to include_module(HasVariable) }
-  it { is_expected.to validate_uniqueness_of(:key).scoped_to(:pipeline_schedule_id) }
 end
