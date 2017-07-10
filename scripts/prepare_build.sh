3f52ea5bd17cc829edc5615b5e84f62f2f130053
Takuya Noguchi <tak.noguchi.iridge@gmail.com>
2017-07-10 23:48:27 +0900

    Bump knapsack to 1.14.0

--- scripts/prepare_build.sh
+++ scripts/prepare_build.sh
@@ -8,9 +8,10 @@ if [ "$USE_BUNDLE_INSTALL" != "false" ]; then
     bundle install --clean $BUNDLE_INSTALL_FLAGS && bundle check
 fi
 
-# Only install knapsack after bundle install! Otherwise oddly some native
+# Only install fog-aws/mime-types after bundle install! Otherwise oddly some native
 # gems could not be found under some circumstance. No idea why, hours wasted.
-retry gem install knapsack fog-aws mime-types
+# TODO: remove workaround on !10156
+retry gem install fog-aws mime-types
 
 cp config/resque.yml.example config/resque.yml
 sed -i 's/localhost/redis/g' config/resque.yml
