a0e468b551537d876c2d8cdc1b98b60a1ecd351e
Clement Ho <ClemMakesApps@gmail.com>
2017-07-10 13:42:16 -0500

    Fix conflict

--- lib/api/users.rb
+++ lib/api/users.rb
@@ -3,14 +3,11 @@ module API
     include PaginationParams
     include APIGuard
 
-<<<<<<< HEAD
+    allow_access_with_scope :read_user, if: -> (request) { request.get? }
+
     before do
-      allow_access_with_scope :read_user if request.get?
       authenticate!
     end
-=======
-    allow_access_with_scope :read_user, if: -> (request) { request.get? }
->>>>>>> 9876895... Merge branch '33580-fix-api-scoping' into 'master'
 
     resource :users, requirements: { uid: /[0-9]*/, id: /[0-9]*/ } do
       helpers do
