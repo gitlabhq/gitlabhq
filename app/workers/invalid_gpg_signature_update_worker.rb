71d884ad2d6eb4a877d2bfe163baeae0b7ddbac6
Alexis Reigel <mail@koffeinfrei.org>
2017-07-11 13:35:57 +0200

    don't use assignment in if condition

--- app/workers/invalid_gpg_signature_update_worker.rb
+++ app/workers/invalid_gpg_signature_update_worker.rb
@@ -3,10 +3,12 @@ class InvalidGpgSignatureUpdateWorker
   include DedicatedSidekiqQueue
 
   def perform(gpg_key_id)
-    if gpg_key = GpgKey.find_by(id: gpg_key_id)
-      Gitlab::Gpg::InvalidGpgSignatureUpdater.new(gpg_key).run
-    else
-      Rails.logger.error("InvalidGpgSignatureUpdateWorker: couldn't find gpg_key with ID=#{gpg_key_id}, skipping job")
+    gpg_key = GpgKey.find_by(id: gpg_key_id)
+
+    unless gpg_key
+      return Rails.logger.error("InvalidGpgSignatureUpdateWorker: couldn't find gpg_key with ID=#{gpg_key_id}, skipping job")
     end
+
+    Gitlab::Gpg::InvalidGpgSignatureUpdater.new(gpg_key).run
   end
 end
