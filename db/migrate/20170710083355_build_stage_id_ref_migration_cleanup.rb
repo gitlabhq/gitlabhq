f4eb2ff725ae4f05679b2d1d41dbc3e033e7d3ac
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2017-07-11 14:40:18 +0200

    Implement build stage_id reference migration clean up

---
+++ db/migrate/20170710083355_build_stage_id_ref_migration_cleanup.rb
@@ -0,0 +1,21 @@
+require Rails.root.join('db', 'post_migrate', '20170628080858_migrate_stage_id_reference_in_background')
+
+class BuildStageIdRefMigrationCleanup < ActiveRecord::Migration
+  include Gitlab::Database::MigrationHelpers
+
+  DOWNTIME = false
+
+  disable_ddl_transaction!
+
+  ##
+  # `MigrateStageIdReferenceInBackground` background migration cleanup.
+  #
+  def up
+    Gitlab::BackgroundMigration
+      .steal(MigrateStageIdReferenceInBackground::MIGRATION)
+  end
+
+  def down
+    # noop
+  end
+end
