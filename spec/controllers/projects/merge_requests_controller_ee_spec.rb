gitlabhq/gitlabhq

commit 34457b307bb4d60a8abe8be3e63b44f986b802bd

Implement rebase feature
Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

app/controllers/projects/merge_requests_controller.rb
@@ -2,7 +2,7 @@ class Projects::MergeRequestsController < Projects::ApplicationController
   before_action :module_enabled
   before_action :merge_request, only: [
     :edit, :update, :show, :diffs, :commits, :merge, :merge_check,
-    :ci_status, :toggle_subscription, :approve, :ff_merge
+    :ci_status, :toggle_subscription, :approve, :ff_merge, :rebase
   ]
   before_action :closes_issues, only: [:edit, :update, :show, :diffs, :commits]
   before_action :validates_merge_request, only: [:show, :diffs, :commits]
@@ -229,6 +229,14 @@ class Projects::MergeRequestsController < Projects::ApplicationController
     else
       @status = false
     end
+  end
+
+  def rebase
+    return access_denied! unless @merge_request.can_be_merged_by?(current_user)
+    return render_404 unless @merge_request.approved?
+
+    MergeRequests::RebaseService.new(merge_request.target_project, current_user).
+      execute(merge_request)
   end
 
   protected

app/services/merge_requests/rebase_service.rb
@@ -1,0 +1,54 @@
+module MergeRequests
+  # MergeService class
+  #
+  # Do git merge and in case of success
+  # mark merge request as merged and execute all hooks and notifications
+  # Executed when you do merge via GitLab UI
+  #
+  class RebaseService < MergeRequests::BaseService
+    include Gitlab::Popen
+
+    attr_reader :merge_request
+
+    def execute(merge_request)
+      @merge_request = merge_request
+
+      if rebase
+        success
+      else
+        error('Failed to rebase. Should be done manually')
+      end
+    end
+
+    def rebase
+      Gitlab::ShellEnv.set_env(current_user)
+
+      # Clone
+      output, status = popen(%W(git clone -b #{merge_request.source_branch} -- #{source_project.repository.path_to_repo} #{tree_path}))
+      raise 'Failed to clone repo' unless status.zero?
+
+      # Rebase
+      output, status = popen(%W(git pull --rebase #{target_project.repository.path_to_repo} #{merge_request.target_branch}), tree_path)
+      raise 'Failed to rebase' unless status.zero?
+
+      output, status = popen(%W(git push -f origin #{merge_request.source_branch}), tree_path)
+      raise 'Failed to push' unless status.zero?
+
+      true
+    ensure
+      Gitlab::ShellEnv.reset_env
+    end
+
+    def source_project
+      @source_project ||= merge_request.source_project
+    end
+
+    def target_project
+      @target_project ||= merge_request.target_project
+    end
+
+    def tree_path
+      @tree_path ||= Rails.root.join('tmp', 'rebase', source_project.id.to_s, SecureRandom.hex).to_s
+    end
+  end
+end

app/views/projects/merge_requests/widget/open/_ff_accept.html.haml
@@ -10,7 +10,7 @@
         Fast-forward merge without creating merge commit
 - else
   = form_for [:rebase, @project.namespace.becomes(Namespace), @project, @merge_request],
-    remote: true, method: :post, html: { class: 'accept-mr-form js-requires-input' } do |f|
+    remote: false, method: :post, html: { class: 'accept-mr-form js-requires-input' } do |f|
     = hidden_field_tag :authenticity_token, form_authenticity_token
     .accept-merge-holder.clearfix.js-toggle-container
       - if @merge_request.target_project.merge_requests_rebase_enabled && can_rebase?(@merge_request.target_project, @merge_request.target_branch)

commit 334d4b1c01d16c01c2a8e616a4c4af0d73c5bc3e

Fix access checks for rebasing an MR
This has been broken since 34457b30, when the feature was implemented: a user
who can merge the MR may not have access to push to the source branch, and vice
versa.

This makes the backend check the correct thing (which the UI already did):
whether the user can push to the source branch.

app/controllers/ee/projects/merge_requests_controller.rb
@@ -5,11 +5,10 @@ module EE
 
       prepended do
         before_action :check_merge_request_rebase_available!, only: [:rebase]
+        before_action :check_user_can_push_to_source_branch!, only: [:rebase]
       end
 
       def rebase
-        return access_denied! unless @merge_request.can_be_merged_by?(current_user)
-
         RebaseWorker.perform_async(@merge_request.id, current_user.id)
 
         render nothing: true, status: 200
@@ -63,6 +62,16 @@ module EE
         attrs << :squash if project.feature_available?(:merge_request_squash)
 
         attrs
+      end
+
+      def check_user_can_push_to_source_branch!
+        return access_denied! unless @merge_request.source_branch_exists?
+
+        access_check = ::Gitlab::UserAccess
+          .new(current_user, project: @merge_request.source_project)
+          .can_push_to_branch?(@merge_request.source_branch)
+
+        access_denied! unless access_check
       end
     end
   end

spec/controllers/projects/merge_requests_controller_ee_spec.rb
@@ -311,13 +311,13 @@ describe Projects::MergeRequestsController do
       post :rebase, namespace_id: project.namespace, project_id: project, id: merge_request
     end
 
-    def expect_rebase_worker
-      expect(RebaseWorker).to receive(:perform_async).with(merge_request.id, viewer.id)
+    def expect_rebase_worker_for(user)
+      expect(RebaseWorker).to receive(:perform_async).with(merge_request.id, user.id)
     end
 
     context 'successfully' do
       it 'enqeues a RebaseWorker' do
-        expect_rebase_worker
+        expect_rebase_worker_for(viewer)
 
         post_rebase
 
@@ -329,7 +329,7 @@ describe Projects::MergeRequestsController do
       let(:project) { create(:project, approvals_before_merge: 1) }
 
       it 'returns 200' do
-        expect_rebase_worker
+        expect_rebase_worker_for(viewer)
 
         post_rebase
 
@@ -337,26 +337,46 @@ describe Projects::MergeRequestsController do
       end
     end
 
-    context 'user cannot merge' do
-      let(:viewer) { create(:user) }
+    context 'with a forked project' do
+      let(:fork_project) { create(:project, forked_from_project: project) }
+      let(:fork_owner) { fork_project.owner }
 
       before do
-        project.add_reporter(viewer)
+        merge_request.update!(source_project: fork_project)
+        fork_project.add_reporter(user)
       end
 
-      it 'returns 404' do
-        expect_rebase_worker.never
+      context 'user cannot push to source branch' do
+        it 'returns 404' do
+          expect_rebase_worker_for(viewer).never
 
-        post_rebase
+          post_rebase
 
-        expect(response.status).to eq(404)
+          expect(response.status).to eq(404)
+        end
+      end
+
+      context 'user can push to source branch' do
+        before do
+          project.add_reporter(fork_owner)
+
+          sign_in(fork_owner)
+        end
+
+        it 'returns 200' do
+          expect_rebase_worker_for(fork_owner)
+
+          post_rebase
+
+          expect(response.status).to eq(200)
+        end
       end
     end
 
     context 'rebase unavailable in license' do
       it 'returns 404' do
         stub_licensed_features(merge_request_rebase: false)
-        expect_rebase_worker.never
+        expect_rebase_worker_for(viewer).never
 
         post_rebase
 