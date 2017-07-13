2b2c1c2c50586a074a64a1abbdffd35c5e53c2da
Valery Sizov <valery@gitlab.com>
2017-07-13 17:48:41 +0300

    [rails 5] Update configuration system[ci skip]

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,10 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
 require 'rails/all'
 
-Bundler.require(:default, Rails.env)
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
 
 module Gitlab
   class Application < Rails::Application
--- config/boot.rb
+++ config/boot.rb
@@ -1,9 +1,6 @@
-require 'rubygems'
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
-# Set up gems listed in the Gemfile.
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
-
-require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
+require 'bundler/setup' # Set up gems listed in the Gemfile.
 
 # set default directory for multiproces metrics gathering
 if ENV['RAILS_ENV'] == 'development' || ENV['RAILS_ENV'] == 'test'
