5cd6f8c76ec74c85e6ff6dae9736b27baeb55988
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2017-07-13 13:25:11 +0200

    Add a foreign key to `merge_requests.head_pipeline_id`

---
+++ db/migrate/20170713104829_add_foreign_key_to_merge_requests.rb
@@ -0,0 +1,28 @@
+class AddForeignKeyToMergeRequests < ActiveRecord::Migration
+  include Gitlab::Database::MigrationHelpers
+
+  DOWNTIME = false
+
+  disable_ddl_transaction!
+
+  def up
+    unless foreign_key_exists?(:merge_requests, :head_pipeline_id)
+      add_concurrent_foreign_key(:merge_requests, :ci_pipelines,
+                                 column: :head_pipeline_id, on_delete: :nullify)
+    end
+  end
+
+  def down
+    if foreign_key_exists?(:merge_requests, :head_pipeline_id)
+      remove_foreign_key(:merge_requests, column: :head_pipeline_id)
+    end
+  end
+
+  private
+
+  def foreign_key_exists?(table, column)
+    foreign_keys(table).any? do |key|
+      key.options[:column] == column.to_s
+    end
+  end
+end
