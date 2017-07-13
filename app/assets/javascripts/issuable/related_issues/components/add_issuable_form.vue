91b17b066363a3c0c9d1e4176c50042c6b275e3b
Eric Eastwood <contact@ericeastwood.com>
2017-07-12 19:10:49 -0500

    Fix duplicate related issue reference inserted when using autocomplete
    Fix https://gitlab.com/gitlab-org/gitlab-ee/issues/2914

--- app/assets/javascripts/issuable/related_issues/components/add_issuable_form.vue
+++ app/assets/javascripts/issuable/related_issues/components/add_issuable_form.vue
@@ -85,12 +85,12 @@ export default {
   mounted() {
     const $input = $(this.$refs.input);
 
-    new GfmAutoComplete(this.autoCompleteSources).setup($input, {
+    this.gfmAutoComplete = new GfmAutoComplete(this.autoCompleteSources);
+    this.gfmAutoComplete.setup($input, {
       issues: true,
     });
     $input.on('shown-issues.atwho', this.onAutoCompleteToggled.bind(this, true));
     $input.on('hidden-issues.atwho', this.onAutoCompleteToggled.bind(this, false));
-    $input.on('inserted-issues.atwho', this.onInput);
 
     this.$refs.input.focus();
   },
