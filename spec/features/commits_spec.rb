c52718332cb723cc4b3035c17eec9eeb9926c8cf
Alexis Reigel <mail@koffeinfrei.org>
2017-07-13 16:04:19 +0200

    show gpg key's user info when no profile exists

--- spec/features/commits_spec.rb
+++ spec/features/commits_spec.rb
@@ -263,26 +263,43 @@ describe 'Commits' do
     end
 
     it 'shows popover badges', :js do
-      user = create :user, email: GpgHelpers::User1.emails.first, username: 'nannie.bernhard', name: 'Nannie Bernhard'
-      project.team << [user, :master]
+      gpg_user = create :user, email: GpgHelpers::User1.emails.first, username: 'nannie.bernhard', name: 'Nannie Bernhard'
       Sidekiq::Testing.inline! do
-        create :gpg_key, key: GpgHelpers::User1.public_key, user: user
+        create :gpg_key, key: GpgHelpers::User1.public_key, user: gpg_user
       end
+
+      user = create :user
+      project.team << [user, :master]
 
       sign_in(user)
       visit namespace_project_commits_path(project.namespace, project, :'signed-commits')
 
+      # unverified signature
       click_on 'Unverified', match: :first
       within '.popover' do
         expect(page).to have_content 'This commit was signed with an unverified signature.'
         expect(page).to have_content "GPG Key ID: #{GpgHelpers::User2.primary_keyid}"
       end
 
+      # verified and the gpg user has a gitlab profile
       click_on 'Verified'
       within '.popover' do
         expect(page).to have_content 'This commit was signed with a verified signature.'
         expect(page).to have_content 'nannie.bernhard'
         expect(page).to have_content 'Nannie Bernhard'
+        expect(page).to have_content "GPG Key ID: #{GpgHelpers::User1.primary_keyid}"
+      end
+
+      # verified and the gpg user's profile doesn't exist anymore
+      gpg_user.destroy!
+
+      visit namespace_project_commits_path(project.namespace, project, :'signed-commits')
+
+      click_on 'Verified'
+      within '.popover' do
+        expect(page).to have_content 'This commit was signed with a verified signature.'
+        expect(page).to have_content 'Nannie Bernhard'
+        expect(page).to have_content 'nannie.bernhard@example.com'
         expect(page).to have_content "GPG Key ID: #{GpgHelpers::User1.primary_keyid}"
       end
     end
