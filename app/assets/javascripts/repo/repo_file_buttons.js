a95552212889b7787370b730ed8a608d0a617e64
Jacob Schatz <jschatz1@gmail.com>
2017-07-14 09:12:50 -0400

    Fixes bug and gets repo running again.

--- app/assets/javascripts/repo/repo_file_buttons.js
+++ app/assets/javascripts/repo/repo_file_buttons.js
@@ -14,7 +14,7 @@ export default class RepoSidebar {
       el: '#repo-file-buttons',
       data: () => Store,
       template: `
-      <div id='repo-file-buttons'>
+      <div id='repo-file-buttons' v-if='!isTree'>
         <a :href='rawFileURL' target='_blank' class='btn btn-default'>Download file</a>
         <div class="btn-group" role="group" aria-label="File actions">
           <a :href='blameFileUrl' class='btn btn-default'>Blame</a>
