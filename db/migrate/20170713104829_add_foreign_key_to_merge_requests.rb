4aab52b20f9af44f03f2452875eb43bb40f2a6c6
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2017-07-14 16:01:05 +0200

    Nullify orphaned head_pipeline_ids in merge_requests

--- db/migrate/20170713104829_add_foreign_key_to_merge_requests.rb
+++ db/migrate/20170713104829_add_foreign_key_to_merge_requests.rb
@@ -6,6 +6,14 @@ class AddForeignKeyToMergeRequests < ActiveRecord::Migration
   disable_ddl_transaction!
 
   def up
+    execute <<-SQL.strip_heredoc
+      UPDATE merge_requests SET head_pipeline_id = null
+        WHERE NOT EXISTS (
+          SELECT 1 FROM ci_pipelines
+            WHERE ci_pipelines.id = merge_requests.head_pipeline_id
+        )
+    SQL
+
     unless foreign_key_exists?(:merge_requests, :head_pipeline_id)
       add_concurrent_foreign_key(:merge_requests, :ci_pipelines,
                                  column: :head_pipeline_id, on_delete: :nullify)
