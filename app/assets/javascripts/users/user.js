5f615276bf268a72c08fa9aab697ad0523c6e6ad
Mike Greiling <mgreiling@gitlab.com>
2017-07-14 15:06:49 +0000

    Refactor user bundle

--- app/assets/javascripts/users/user.js
+++ app/assets/javascripts/users/user.js
@@ -1,9 +1,9 @@
-/* eslint-disable class-methods-use-this, comma-dangle, arrow-parens, no-param-reassign */
+/* eslint-disable class-methods-use-this */
 
 import Cookies from 'js-cookie';
 import UserTabs from './user_tabs';
 
-class User {
+export default class User {
   constructor({ action }) {
     this.action = action;
     this.placeProfileAvatarsToTop();
@@ -13,25 +13,22 @@ class User {
 
   placeProfileAvatarsToTop() {
     $('.profile-groups-avatars').tooltip({
-      placement: 'top'
+      placement: 'top',
     });
   }
 
   initTabs() {
     return new UserTabs({
       parentEl: '.user-profile',
-      action: this.action
+      action: this.action,
     });
   }
 
   hideProjectLimitMessage() {
-    $('.hide-project-limit-message').on('click', e => {
+    $('.hide-project-limit-message').on('click', (e) => {
       e.preventDefault();
       Cookies.set('hide_project_limit_message', 'false');
       $(this).parents('.project-limit-message').remove();
     });
   }
 }
-
-window.gl = window.gl || {};
-window.gl.User = User;
