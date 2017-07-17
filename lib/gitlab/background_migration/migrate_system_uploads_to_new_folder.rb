6f26f6f79f33fdb7e4e556b7f6b37999b78cd323
Bob Van Landuyt <bob@vanlanduyt.co>
2017-07-17 18:11:07 +0200

    Allow groups with the name system

--- lib/gitlab/background_migration/migrate_system_uploads_to_new_folder.rb
+++ lib/gitlab/background_migration/migrate_system_uploads_to_new_folder.rb
@@ -4,43 +4,22 @@ module Gitlab
       include Gitlab::Database::MigrationHelpers
       attr_reader :old_folder, :new_folder
 
+      class Upload < ActiveRecord::Base
+        self.table_name = 'uploads'
+        include EachBatch
+      end
+
       def perform(old_folder, new_folder)
-        @old_folder = old_folder
-        @new_folder = new_folder
-
         replace_sql = replace_sql(uploads[:path], old_folder, new_folder)
+        affected_uploads = Upload.where(uploads[:path].matches("#{old_folder}%"))
 
-        while remaining_rows > 0
-          sql = "UPDATE uploads "\
-                "SET path = #{replace_sql} "\
-                "WHERE uploads.id IN "\
-                "  (SELECT uploads.id FROM uploads "\
-                "  WHERE #{affected_uploads.to_sql} LIMIT 1000)"
-          connection.execute(sql)
+        affected_uploads.each_batch do |batch|
+          batch.update_all("path = #{replace_sql}")
         end
       end
 
       def uploads
         Arel::Table.new('uploads')
-      end
-
-      def remaining_rows
-        remaining_result = connection.exec_query("SELECT count(id) FROM uploads WHERE #{affected_uploads.to_sql}")
-        remaining = remaining_result.first['count'].to_i
-        logger.info "#{remaining} uploads remaining"
-        remaining
-      end
-
-      def affected_uploads
-        uploads[:path].matches("#{old_folder}%")
-      end
-
-      def connection
-        ActiveRecord::Base.connection
-      end
-
-      def logger
-        Sidekiq.logger || Rails.logger || Logger.new(STDOUT)
       end
     end
   end
