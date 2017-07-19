0925edb4ff4ae8c12648be7a59866340c81b9eff
Phil Hughes <me@iamphill.com>
2017-07-19 16:57:00 +0100

    updated styles

--- spec/javascripts/fly_out_nav_spec.js
+++ spec/javascripts/fly_out_nav_spec.js
@@ -1,4 +1,4 @@
-import { calculateTop, createArrowStyles } from '~/fly_out_nav';
+import { calculateTop } from '~/fly_out_nav';
 
 describe('Fly out sidebar navigation', () => {
   describe('calculateTop', () => {
@@ -20,18 +20,6 @@ describe('Fly out sidebar navigation', () => {
       expect(
         calculateTop(boundingRect, 100),
       ).toBe(window.innerHeight - 100);
-    });
-  });
-
-  describe('createArrowStyles', () => {
-    it('returns translate3d styles', () => {
-      const boundingRect = {
-        top: 100,
-      };
-
-      expect(
-        createArrowStyles(boundingRect, 50),
-      ).toContain('translate3d(0, 50px, 0)');
     });
   });
 });
