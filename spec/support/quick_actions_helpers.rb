c00ff16ffe7559c46e53f8788836ac4e98b8f589
Fatih Acet <acetfatih@gmail.com>
2017-07-21 19:54:25 +0300

    IssueNotesRefactor: Revert note selector.

--- spec/support/quick_actions_helpers.rb
+++ spec/support/quick_actions_helpers.rb
@@ -2,7 +2,7 @@ module QuickActionsHelpers
   def write_note(text)
     Sidekiq::Testing.fake! do
       page.within('.js-main-target-form') do
-        fill_in 'note-body', with: text
+        fill_in 'note[note]', with: text
         find('.js-comment-submit-button').trigger('click')
         wait_for_requests
       end
