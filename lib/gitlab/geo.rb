d045cbefffb4589402e6c59ea311afd07729717a
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2017-07-24 17:50:09 -0300

    Move RepositorySyncWorker under the Geo namespace

--- lib/gitlab/geo.rb
+++ lib/gitlab/geo.rb
@@ -41,7 +41,7 @@ module Gitlab
 
     def self.current_node_enabled?
       # No caching of the enabled! If we cache it and an admin disables
-      # this node, an active GeoRepositorySyncWorker would keep going for up
+      # this node, an active Geo::RepositorySyncWorker would keep going for up
       # to max run time after the node was disabled.
       Gitlab::Geo.current_node.reload.enabled?
     end
--- spec/workers/geo/repository_sync_worker_spec.rb
+++ spec/workers/geo/repository_sync_worker_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe GeoRepositorySyncWorker do
+describe Geo::RepositorySyncWorker do
   let!(:primary)   { create(:geo_node, :primary, host: 'primary-geo-node') }
   let!(:secondary) { create(:geo_node, :current) }
   let!(:project_1) { create(:empty_project) }
