7ce408c3917615114925bf446d009dbd26e8d4da
Oswaldo Ferreira <oswaldo@gitlab.com>
2017-07-25 15:37:08 -0300

    Remove unnecessary stub for testing authorization being skipped

--- spec/controllers/oauth/authorizations_controller_spec.rb
+++ spec/controllers/oauth/authorizations_controller_spec.rb
@@ -34,22 +34,6 @@ describe Oauth::AuthorizationsController do
     end
 
     context 'with valid params' do
-      context 'when trusted application' do
-        before do
-          doorkeeper.update(trusted: true)
-        end
-
-        it 'deletes session.user_return_to and redirects' do
-          request.session['user_return_to'] = 'http://example.com'
-          allow(controller).to receive(:skip_authorization?).and_return(true)
-
-          get :new, params
-
-          expect(request.session['user_return_to']).to be_nil
-          expect(response).to have_http_status(302)
-        end
-      end
-
       it 'returns 200 code and renders view' do
         get :new, params
 
@@ -58,8 +42,8 @@ describe Oauth::AuthorizationsController do
       end
 
       it 'deletes session.user_return_to and redirects when skip authorization' do
+        doorkeeper.update(trusted: true)
         request.session['user_return_to'] = 'http://example.com'
-        allow(controller).to receive(:skip_authorization?).and_return(true)
 
         get :new, params
 
