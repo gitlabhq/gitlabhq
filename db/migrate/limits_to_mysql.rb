f86580c075f50b78517283febca012afcc8b6211
Alexis Reigel <mail@koffeinfrei.org>
2017-07-25 16:02:30 +0200

    no more more :length due to mysql set_index hack

--- db/migrate/limits_to_mysql.rb
+++ db/migrate/limits_to_mysql.rb
@@ -1,9 +1,5 @@
 # rubocop:disable all
-require Rails.root.join('lib/gitlab/database/migration_helpers.rb')
-
 class LimitsToMysql < ActiveRecord::Migration
-  include Gitlab::Database::MigrationHelpers
-
   def up
     return unless ActiveRecord::Base.configurations[Rails.env]['adapter'] =~ /^mysql/
 
@@ -12,14 +8,5 @@ class LimitsToMysql < ActiveRecord::Migration
     change_column :snippets, :content, :text, limit: 2147483647
     change_column :notes, :st_diff, :text, limit: 2147483647
     change_column :events, :data, :text, limit: 2147483647
-
-    [
-      [:gpg_keys, :primary_keyid],
-      [:gpg_signatures, :commit_sha],
-      [:gpg_signatures, :gpg_key_primary_keyid]
-    ].each do |table_name, column_name|
-      remove_index table_name, column_name if index_exists?(table_name, column_name)
-      add_concurrent_index table_name, column_name, length: 20
-    end
   end
 end
