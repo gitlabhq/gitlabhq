775d91041f98ea8e95f2bca458a6b7e7660a4e41
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2017-07-26 13:07:32 +0300

    Fix deprecated usage of constant name as string in test env
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Make sure the middleware is inserted first in middleware chain
-  config.middleware.insert_before('ActionDispatch::Static', 'Gitlab::Testing::RequestBlockerMiddleware')
+  config.middleware.insert_before(ActionDispatch::Static, Gitlab::Testing::RequestBlockerMiddleware)
 
   # Settings specified here will take precedence over those in config/application.rb
 
