f9817808eb5948bf8701eb27ce34eefedb9a2720
Yorick Peterse <yorickpeterse@gmail.com>
2017-07-26 11:19:41 +0000

    Merge branch 'mk-add-lower-path-index-to-redirect-routes' into 'master'
    Add lower path index to redirect_routes
    
    Closes #34638
    
    See merge request !13062

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170717150329) do
+ActiveRecord::Schema.define(version: 20170724214302) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "pg_trgm"
