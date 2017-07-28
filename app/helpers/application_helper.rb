e259680f425cb489eb50b6cdcc1c5350bd20fa77
Valery Sizov <valery@gitlab.com>
2017-07-28 20:31:22 +0300

    [rails 5] Fix a bunch of specs by relocating "safe_params" method to application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -301,4 +301,10 @@ module ApplicationHelper
   def show_new_nav?
     cookies["new_nav"] == "true"
   end
+
+  # Use this in place of params when generating links from params
+  # See https://github.com/rails/rails/issues/26289
+  def safe_params
+    params.except(:host, :port, :protocol).permit!
+  end
 end
