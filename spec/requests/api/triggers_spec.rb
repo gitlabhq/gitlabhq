97c63eb793204a1b77ee59f4dddd2108ca4faa0e
Shinya Maeda <shinya@gitlab.com>
2017-07-31 20:20:07 +0900

    Add proper test for api/triggers_spec.rb

--- spec/requests/api/triggers_spec.rb
+++ spec/requests/api/triggers_spec.rb
@@ -184,6 +184,23 @@ describe API::Triggers do
               expect(json_response['message']).to eq('400 Job has to be running')
             end
           end
+
+          context 'when variables are defined' do
+            let(:params) do
+              { ref: 'refs/heads/other-branch',
+                variables: { 'KEY' => 'VALUE' } }
+            end
+
+            it 'creates a new pipeline with a variable' do
+              expect { subject }.to change(Ci::Pipeline, :count)
+                                .and change(Ci::PipelineVariable, :count)
+
+              expect(response).to have_http_status(201)
+              expect(Ci::Pipeline.last.source).to eq('pipeline')
+              expect(Ci::Pipeline.last.triggered_by_pipeline).not_to be_nil
+              expect(Ci::Pipeline.last.variables.map { |v| { v.key => v.value } }.last).to eq(params[:variables])
+            end
+          end
         end
       end
     end
