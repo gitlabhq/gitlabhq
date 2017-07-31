a7a931019cc4395219670c72688a0cfa3d3acb38
Shinya Maeda <shinya@gitlab.com>
2017-07-31 23:55:16 +0900

    Split spec for ee

--- spec/services/ee/ci/pipeline_trigger_service_spec.rb
+++ spec/services/ee/ci/pipeline_trigger_service_spec.rb
@@ -9,31 +9,42 @@ describe Ci::PipelineTriggerService, services: true do
 
   describe '#execute' do
     let(:user) { create(:user) }
-    let(:trigger) { create(:ci_trigger, project: project, owner: user) }
+    let!(:pipeline) { create(:ci_empty_pipeline, project: project) }
+    let(:job) { create(:ci_build, :running, pipeline: pipeline, user: user) }
     let(:result) { described_class.new(project, user, params).execute }
 
     before do
       project.add_developer(user)
     end
 
-    context 'when trigger belongs to a different project' do
-      let(:params) { { token: trigger.token, ref: 'master', variables: nil } }
-      let(:trigger) { create(:ci_trigger, project: create(:empty_project), owner: user) }
+    context 'when job user does not have a permission to read a project' do
+      let(:params) { { token: job.token, ref: 'master', variables: nil } }
+      let(:job) { create(:ci_build, pipeline: pipeline, user: create(:user)) }
 
       it 'does nothing' do
         expect { result }.not_to change { Ci::Pipeline.count }
       end
     end
 
-    context 'when params have an existsed trigger token' do
+    context 'when job is not running' do
+      let(:params) { { token: job.token, ref: 'master', variables: nil } }
+      let(:job) { create(:ci_build, :success, pipeline: pipeline, user: user) }
+
+      it 'does nothing' do
+        expect { result }.not_to change { Ci::Pipeline.count }
+        expect(result[:message]).to eq('400 Job has to be running')
+      end
+    end
+
+    context 'when params have an existsed job token' do
       context 'when params have an existsed ref' do
-        let(:params) { { token: trigger.token, ref: 'master', variables: nil } }
+        let(:params) { { token: job.token, ref: 'master', variables: nil } }
 
         it 'triggers a pipeline' do
           expect { result }.to change { Ci::Pipeline.count }.by(1)
           expect(result[:pipeline].ref).to eq('master')
           expect(result[:pipeline].project).to eq(project)
-          expect(result[:pipeline].user).to eq(trigger.owner)
+          expect(result[:pipeline].user).to eq(job.user)
           expect(result[:status]).to eq(:success)
         end
 
@@ -49,22 +60,22 @@ describe Ci::PipelineTriggerService, services: true do
         end
 
         context 'when params have a variable' do
-          let(:params) { { token: trigger.token, ref: 'master', variables: variables } }
+          let(:params) { { token: job.token, ref: 'master', variables: variables } }
           let(:variables) { { 'AAA' => 'AAA123' } }
 
           it 'has a variable' do
             expect { result }.to change { Ci::PipelineVariable.count }.by(1)
-                             .and change { Ci::TriggerRequest.count }.by(1)
+                             .and change { Ci::Sources::Pipeline.count }.by(1)
             expect(result[:pipeline].variables.map { |v| { v.key => v.value } }.first).to eq(variables)
-            expect(result[:pipeline].trigger_requests.last.variables).to be_nil
+            expect(job.sourced_pipelines.last.pipeline_id).to eq(result[:pipeline].id)
           end
         end
       end
 
       context 'when params have a non-existsed ref' do
-        let(:params) { { token: trigger.token, ref: 'invalid-ref', variables: nil } }
+        let(:params) { { token: job.token, ref: 'invalid-ref', variables: nil } }
 
-        it 'does not trigger a pipeline' do
+        it 'does not job a pipeline' do
           expect { result }.not_to change { Ci::Pipeline.count }
           expect(result[:http_status]).to eq(400)
         end
