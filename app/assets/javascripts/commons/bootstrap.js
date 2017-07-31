cb127785e08c37fd1f5e0dd11ca2327930fa443a
Filipa Lacerda <filipa@gitlab.com>
2017-07-31 17:33:10 +0100

    Adds CSS for desktop and mobile

--- app/assets/javascripts/commons/bootstrap.js
+++ app/assets/javascripts/commons/bootstrap.js
@@ -3,6 +3,7 @@ import $ from 'jquery';
 // bootstrap jQuery plugins
 import 'bootstrap-sass/assets/javascripts/bootstrap/affix';
 import 'bootstrap-sass/assets/javascripts/bootstrap/alert';
+import 'bootstrap-sass/assets/javascripts/bootstrap/button';
 import 'bootstrap-sass/assets/javascripts/bootstrap/dropdown';
 import 'bootstrap-sass/assets/javascripts/bootstrap/modal';
 import 'bootstrap-sass/assets/javascripts/bootstrap/tab';
