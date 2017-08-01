5622c090dc144f2e30694a228fb1aac8b75be12e
Luke "Jared" Bennett <lbennett@gitlab.com>
2017-08-01 12:59:33 +0100

    Fix monaco_loader_spec

--- spec/javascripts/repo/monaco_loader_spec.js
+++ spec/javascripts/repo/monaco_loader_spec.js
@@ -1,8 +1,17 @@
-describe('MonacoLoader', () => {
-  it('sets __monaco_context__', () => {
-    const monacoContext = require('monaco-editor/dev/vs/loader'); // eslint-disable-line global-require
+/* global __webpack_public_path__ */
+import monacoContext from 'monaco-editor/dev/vs/loader';
 
-    expect(window.__monaco_context__) // eslint-disable-line no-underscore-dangle
-      .toBe(monacoContext);
+describe('MonacoLoader', () => {
+  it('calls require.config and exports require', () => {
+    spyOn(monacoContext.require, 'config');
+
+    const monacoLoader = require('~/repo/monaco_loader'); // eslint-disable-line global-require
+
+    expect(monacoContext.require.config).toHaveBeenCalledWith({
+      paths: {
+        vs: `${__webpack_public_path__}monaco-editor/vs`, // eslint-disable-line camelcase
+      },
+    });
+    expect(monacoLoader.default).toBe(monacoContext.require);
   });
 });
