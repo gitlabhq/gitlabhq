28ee4ef713cb85a7d4061d253e212cb2d45f08ad
Simon Knox <psimyn@gmail.com>
2017-08-02 06:26:06 +1000

    enable dragging and creating empty boards
    NB: this changes all can? checks in board.html admin_list, this should be project dependent clientside

--- app/helpers/boards_helper.rb
+++ app/helpers/boards_helper.rb
@@ -9,7 +9,7 @@ module BoardsHelper
       lists_endpoint: board_lists_path(board),
       board_id: board.id,
       board_milestone_title: board&.milestone&.title,
-      disabled: "#{!can?(current_user, :admin_list, @project)}",
+      disabled: "#{!can?(current_user, :admin_list, current_board_parent)}",
       issue_link_base: @issues_path,
       root_path: root_path,
       bulk_update_path: @bulk_issues_path,
