60f21ff967f23a7689d80ff746263b9476b2f19a
Oswaldo Ferreira <oswaldo@gitlab.com>
2017-08-02 12:21:50 -0300

    Eagerload queues yaml file

--- lib/gitlab/sidekiq_config.rb
+++ lib/gitlab/sidekiq_config.rb
@@ -5,7 +5,10 @@ module Gitlab
     def self.queues(rails_path = Rails.root.to_s, except: [])
       queues_file_path = File.join(rails_path, 'config', 'sidekiq_queues.yml')
 
-      YAML.load_file(queues_file_path).fetch(:queues).map { |queue, _| queue } - except
+      @queues_file = {}
+      @queues_file[queues_file_path] ||= YAML.load_file(queues_file_path)
+
+      @queues_file[queues_file_path].fetch(:queues).map { |queue, _| queue } - except
     end
   end
 end
