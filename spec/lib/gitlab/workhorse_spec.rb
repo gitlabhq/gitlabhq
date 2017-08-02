dbcf48af8b21c0f1e54b73ea421911028081e1c1
David Turner <dturner@twosigma.com>
2017-08-02 14:14:50 -0400

    Add username as GL_USERNAME in hooks (http)
    When calling pre-receive, post-receive, and update hooks, add the GitLab
    username as the GL_USERNAME environment variable.
    
    This patch only handles cases where pushes are over http, or via
    the web interface.  Later, we will address the ssh case.

--- spec/lib/gitlab/workhorse_spec.rb
+++ spec/lib/gitlab/workhorse_spec.rb
@@ -182,7 +182,12 @@ describe Gitlab::Workhorse do
     let(:repo_path) { repository.path_to_repo }
     let(:action) { 'info_refs' }
     let(:params) do
-      { GL_ID: "user-#{user.id}", GL_REPOSITORY: "project-#{project.id}", RepoPath: repo_path }
+      {
+        GL_ID: "user-#{user.id}",
+        GL_USERNAME: user.username,
+        GL_REPOSITORY: "project-#{project.id}",
+        RepoPath: repo_path
+      }
     end
 
     subject { described_class.git_http_ok(repository, false, user, action) }
@@ -191,7 +196,12 @@ describe Gitlab::Workhorse do
 
     context 'when is_wiki' do
       let(:params) do
-        { GL_ID: "user-#{user.id}", GL_REPOSITORY: "wiki-#{project.id}", RepoPath: repo_path }
+        {
+          GL_ID: "user-#{user.id}",
+          GL_USERNAME: user.username,
+          GL_REPOSITORY: "wiki-#{project.id}",
+          RepoPath: repo_path
+        }
       end
 
       subject { described_class.git_http_ok(repository, true, user, action) }
