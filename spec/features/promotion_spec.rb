52ec57dd7bcf6c7de36d22d02290faff871f0555
Tim Zallmann <tzallmann@gitlab.com>
2017-08-03 20:58:49 +0200

    Trying with context

--- spec/features/promotion_spec.rb
+++ spec/features/promotion_spec.rb
@@ -19,41 +19,45 @@ describe 'Promotions', js: true do
   end
 
   describe 'for project features in general on premise' do
-    before do
-      allow(License).to receive(:current).and_return(nil)
+    context 'no license installed' do
+      before do
+        allow(License).to receive(:current).and_return(nil)
 
-      sign_in(user)
-      project.team << [user, :master]
-    end
+        sign_in(user)
+        project.team << [user, :master]
+      end
 
-    it 'should have the contact admin line' do
-      expect(find('#promote_service_desk')).to have_content 'Contact your Administrator to upgrade your license.'
-    end
-    
-    it 'should have the start trial button' do
-      sign_in(admin)
-      visit edit_project_path(project)
-      expect(find('#promote_service_desk')).to have_content 'Start GitLab Enterprise Edition trial'
+      it 'should have the contact admin line' do
+        expect(find('#promote_service_desk')).to have_content 'Contact your Administrator to upgrade your license.'
+      end
+      
+      it 'should have the start trial button' do
+        sign_in(admin)
+        visit edit_project_path(project)
+        expect(find('#promote_service_desk')).to have_content 'Start GitLab Enterprise Edition trial'
+      end
     end
   end
 
   describe 'for project features in general for .com', js: true do
-    before do
-      allow(Gitlab).to receive(:com?).and_return(true)
-    end
+    context 'for .com' do
+      before do
+        allow(Gitlab).to receive(:com?).and_return(true)
+      end
 
-    it 'should have the Upgrade your plan button' do
-      sign_in(user)
-      project.team << [user, :master]
+      it 'should have the Upgrade your plan button' do
+        sign_in(user)
+        project.team << [user, :master]
 
-      expect(find('#promote_service_desk')).to have_content 'Upgrade your plan'
-    end
+        expect(find('#promote_service_desk')).to have_content 'Upgrade your plan'
+      end
 
-    it 'should have the contact owner line' do
-      sign_in(developer)
-      project.team << [developer, :developer]
+      it 'should have the contact owner line' do
+        sign_in(developer)
+        project.team << [developer, :developer]
 
-      expect(find('#promote_service_desk')).to have_content 'Upgrade your plan'
+        expect(find('#promote_service_desk')).to have_content 'Upgrade your plan'
+      end
     end
   end  
 
