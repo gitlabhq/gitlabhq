21b820c4ec4db61a20586af6aa7fb69f50d7e3ac
Clement Ho <ClemMakesApps@gmail.com>
2017-08-03 16:37:27 +0200

    Remove the new file and new folder buttons

--- app/assets/javascripts/repo/components/repo_file_options.vue
+++ app/assets/javascripts/repo/components/repo_file_options.vue
@@ -20,20 +20,6 @@ export default RepoFileOptions;
 <tr v-if="isMini" class="repo-file-options">
     <td>
       <span class="title">{{projectName}}</span>
-
-      <ul>
-        <li>
-          <a href="#" title="New File">
-            <i class="fa fa-file-o"></i>
-          </a>
-        </li>
-
-        <li>
-          <a href="#" title="New Folder">
-            <i class="fa fa-folder-o"></i>
-          </a>
-        </li>
-      </ul>
     </td>
   </tr>
 </template>
