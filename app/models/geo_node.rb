808ab1c88b5ac8ff53d904318a1c80504483b5d1
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2017-08-02 22:28:07 -0300

    Remove unused method GeoNode#namespaces_changed?

--- app/models/geo_node.rb
+++ app/models/geo_node.rb
@@ -108,10 +108,6 @@ class GeoNode < ActiveRecord::Base
     end
   end
 
-  def namespaces_changed?
-    namespaces.any? { |n| n.new_record? || n.marked_for_destruction? }
-  end
-
   def project_ids
     return unless namespaces.presence
 
