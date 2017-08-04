bea56452435d057c32f2b37ac13582b0638dbf03
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2017-08-03 21:50:21 -0300

    Combine validators for the namespace_id on GeoNodeNamespaceLink model

--- app/models/geo_node_namespace_link.rb
+++ app/models/geo_node_namespace_link.rb
@@ -2,6 +2,5 @@ class GeoNodeNamespaceLink < ActiveRecord::Base
   belongs_to :geo_node, inverse_of: :namespaces
   belongs_to :namespace
 
-  validates :namespace_id, presence: true
-  validates :namespace_id, uniqueness: { scope: [:geo_node_id] }
+  validates :namespace_id, presence: true, uniqueness: { scope: [:geo_node_id] }
 end
