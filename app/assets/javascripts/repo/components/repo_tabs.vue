dee68253da2a57c918d9f19294a133f1a5139534
Jacob Schatz <jschatz1@gmail.com>
2017-08-06 12:28:23 -0400

    Fix xclicked not existing.

--- app/assets/javascripts/repo/components/repo_tabs.vue
+++ app/assets/javascripts/repo/components/repo_tabs.vue
@@ -37,7 +37,7 @@ export default RepoTabs;
 
 <template>
 <ul id="tabs" v-if="isMini" v-cloak :class="{'overflown': tabsOverflow}">
-  <repo-tab v-for="tab in openedFiles" :key="tab.id" :tab="tab" :class="{'active' : tab.active}" @xclicked="xclicked"/>
+  <repo-tab v-for="tab in openedFiles" :key="tab.id" :tab="tab" :class="{'active' : tab.active}" @xclicked="xClicked"/>
   <li class="tabs-divider" />
 </ul>
 </template>
