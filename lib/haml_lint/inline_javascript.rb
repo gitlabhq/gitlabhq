b3537000aaf3bf0de98adec938bd19e97bf8b3d6
winh <winnie@gitlab.com>
2017-08-08 09:19:44 +0200

    Restrict InlineJavaScript for haml_lint to dev and test environment

--- lib/haml_lint/inline_javascript.rb
+++ lib/haml_lint/inline_javascript.rb
@@ -1,14 +1,16 @@
-require 'haml_lint/haml_visitor'
-require 'haml_lint/linter'
-require 'haml_lint/linter_registry'
+unless Rails.env.production?
+  require 'haml_lint/haml_visitor'
+  require 'haml_lint/linter'
+  require 'haml_lint/linter_registry'
 
-module HamlLint
-  class Linter::InlineJavaScript < Linter
-    include LinterRegistry
+  module HamlLint
+    class Linter::InlineJavaScript < Linter
+      include LinterRegistry
 
-    def visit_filter(node)
-      return unless node.filter_type == 'javascript'
-      record_lint(node, 'Inline JavaScript is discouraged (https://docs.gitlab.com/ee/development/gotchas.html#do-not-use-inline-javascript-in-views)')
+      def visit_filter(node)
+        return unless node.filter_type == 'javascript'
+        record_lint(node, 'Inline JavaScript is discouraged (https://docs.gitlab.com/ee/development/gotchas.html#do-not-use-inline-javascript-in-views)')
+      end
     end
   end
 end
