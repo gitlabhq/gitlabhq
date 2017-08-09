02c66dadaec447d9e031d4370f95a14b51186c42
Filipa Lacerda <filipa@gitlab.com>
2017-08-09 14:38:21 +0100

    [ci skip] Adds unit tests for issue_note_signed_out_widget component

--- spec/javascripts/notes/components/issue_note_signed_out_widget_spec.js
+++ spec/javascripts/notes/components/issue_note_signed_out_widget_spec.js
@@ -1,9 +1,37 @@
-describe('issue_note_signed_out_widget component', () => {
-  it('should render sign in link provided in the store', () => {
+import Vue from 'vue';
+import issueNoteSignedOut from '~/notes/components/issue_note_signed_out_widget.vue';
+import store from '~/notes/stores';
+import { notesDataMock } from '../mock_data';
 
+describe('issue_note_signed_out_widget component', () => {
+  let vm;
+
+  beforeEach(() => {
+    const Component = Vue.extend(issueNoteSignedOut);
+    store.dispatch('setNotesData', notesDataMock);
+
+    vm = new Component({
+      store,
+    }).$mount();
+  });
+
+  afterEach(() => {
+    vm.$destroy();
+  });
+
+  it('should render sign in link provided in the store', () => {
+    expect(
+      vm.$el.querySelector(`a[href="${notesDataMock.newSessionPath}"]`).textContent,
+    ).toEqual('sign in');
   });
 
   it('should render register link provided in the store', () => {
+    expect(
+      vm.$el.querySelector(`a[href="${notesDataMock.registerPath}"]`).textContent,
+    ).toEqual('register');
+  });
 
+  it('should render information text', () => {
+    expect(vm.$el.textContent.replace(/\s+/g, ' ').trim()).toEqual('Please register or sign in to reply');
   });
 });
