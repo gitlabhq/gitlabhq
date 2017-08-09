bc4635d23263ba5971fdab7b39a2f74eb23efbac
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2017-08-09 16:00:52 -0300

    Check if a wiki can be updated on a node with project restrictions

--- app/services/geo/schedule_wiki_repo_update_service.rb
+++ app/services/geo/schedule_wiki_repo_update_service.rb
@@ -8,8 +8,18 @@ module Geo
 
     def execute
       @projects.each do |project|
+        next unless can_update?(project['id'])
+
         GeoWikiRepositoryUpdateWorker.perform_async(project['id'], project['clone_url'])
       end
+    end
+
+    private
+
+    def can_update?(project_id)
+      return true if Gitlab::Geo.current_node&.restricted_project_ids.nil?
+
+      Gitlab::Geo.current_node.restricted_project_ids.include?(project_id)
     end
   end
 end
