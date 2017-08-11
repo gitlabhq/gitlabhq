e320b933d1e37bdb9c3c10fb8822a210a3bd5f70
Michael Kozono <mkozono@gmail.com>
2017-08-11 14:17:14 -0700

    Disable datetime_with_timezone cop

--- db/migrate/20170811203342_fix_email_opted_in_at_on_users.rb
+++ db/migrate/20170811203342_fix_email_opted_in_at_on_users.rb
@@ -8,8 +8,15 @@ class FixEmailOptedInAtOnUsers < ActiveRecord::Migration
   DOWNTIME = false
 
   def up
+    # MySQL makes the first TIMESTAMP column in a table default to
+    # CURRENT_TIMESTAMP and gives it a NOT NULL constraint
+    # (https://bugs.mysql.com/bug.php?id=75098). This prevents this value from
+    # ever being set to NULL. While it's possible to override MySQL's behavior
+    # in the the migration by adding null: true to add_column, this does not do
+    # the right thing when the database is initialized from scratch. Using the
+    # DATETIME type avoids these pitfalls.
     remove_column :users, :email_opted_in_at
-    add_column :users, :email_opted_in_at, :datetime, null: true
+    add_column :users, :email_opted_in_at, :datetime, null: true # rubocop:disable Migration/Datetime
   end
 
   def down
