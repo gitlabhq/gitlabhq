a27f780a1b901a55709b4a1751c812f28aed0f80
Jose Ivan Vargas <jvargas@gitlab.com>
2017-08-15 11:12:14 -0500

    Fixed schema date

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170809142252) do
+ActiveRecord::Schema.define(version: 20170809134534) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
