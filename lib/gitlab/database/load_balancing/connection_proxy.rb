ef72b7d087675c3f4b2ffa59aae3afb9f87f5393
Douwe Maan <douwe@selenight.nl>
2017-08-15 11:25:10 +0200

    Fix ConnectionProxy#method_missing for trailing hash arguments

--- lib/gitlab/database/load_balancing/connection_proxy.rb
+++ lib/gitlab/database/load_balancing/connection_proxy.rb
@@ -25,34 +25,34 @@ module Gitlab
         end
 
         def select(*args)
-          read_using_load_balancer(:select, *args)
+          read_using_load_balancer(:select, args)
         end
 
         def select_all(arel, name = nil, binds = [])
           if arel.respond_to?(:locked) && arel.locked
             # SELECT ... FOR UPDATE queries should be sent to the primary.
-            write_using_load_balancer(:select_all, arel, name, binds,
+            write_using_load_balancer(:select_all, [arel, name, binds],
                                       sticky: true)
           else
-            read_using_load_balancer(:select_all, arel, name, binds)
+            read_using_load_balancer(:select_all, [arel, name, binds])
           end
         end
 
         STICKY_WRITES.each do |name|
           define_method(name) do |*args, &block|
-            write_using_load_balancer(name, *args, sticky: true, &block)
+            write_using_load_balancer(name, args, sticky: true, &block)
           end
         end
 
         # Delegates all unknown messages to a read-write connection.
         def method_missing(name, *args, &block)
-          write_using_load_balancer(name, *args, &block)
+          write_using_load_balancer(name, args, &block)
         end
 
         # Performs a read using the load balancer.
         #
         # name - The name of the method to call on a connection object.
-        def read_using_load_balancer(name, *args, &block)
+        def read_using_load_balancer(name, args, &block)
           method = Session.current.use_primary? ? :read_write : :read
 
           @load_balancer.send(method) do |connection|
@@ -65,7 +65,7 @@ module Gitlab
         # name - The name of the method to call on a connection object.
         # sticky - If set to true the session will stick to the master after
         #          the write.
-        def write_using_load_balancer(name, *args, sticky: false, &block)
+        def write_using_load_balancer(name, args, sticky: false, &block)
           result = @load_balancer.read_write do |connection|
             # Sticking has to be enabled before calling the method. Not doing so
             # could lead to methods called in a block still being performed on a
