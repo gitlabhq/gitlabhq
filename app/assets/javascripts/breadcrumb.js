bec6ea20f6933f07cb616c29ce72efc076a97dc0
Phil Hughes <me@iamphill.com>
2017-08-17 14:00:50 +0100

    fixed breadcrumbs being undefined

--- app/assets/javascripts/breadcrumb.js
+++ app/assets/javascripts/breadcrumb.js
@@ -8,14 +8,17 @@ export const addTooltipToEl = (el) => {
 
 export default () => {
   const breadcrumbs = document.querySelector('.breadcrumbs-list');
-  const topLevelLinks = breadcrumbs.querySelectorAll('.breadcrumbs-list > li > a');
-  const $expander = $('.js-breadcrumbs-collapsed-expander');
 
-  topLevelLinks.forEach(el => addTooltipToEl(el));
+  if (breadcrumbs) {
+    const topLevelLinks = breadcrumbs.querySelectorAll('.breadcrumbs-list > li > a');
+    const $expander = $('.js-breadcrumbs-collapsed-expander');
 
-  $expander.closest('.dropdown')
-    .on('show.bs.dropdown hide.bs.dropdown', (e) => {
-      $('.js-breadcrumbs-collapsed-expander', e.currentTarget).toggleClass('open')
-        .tooltip('hide');
-    });
+    topLevelLinks.forEach(el => addTooltipToEl(el));
+
+    $expander.closest('.dropdown')
+      .on('show.bs.dropdown hide.bs.dropdown', (e) => {
+        $('.js-breadcrumbs-collapsed-expander', e.currentTarget).toggleClass('open')
+          .tooltip('hide');
+      });
+  }
 };
