998833dfdcf15a1e63f342efa6cba24bb370c610
Felipe Artur <felipefac@gmail.com>
2017-08-18 13:02:05 -0300

    Remove ignore columns

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -9,10 +9,7 @@ class Issue < ActiveRecord::Base
   include Spammable
   include FasterCacheKeys
   include RelativePositioning
-  include IgnorableColumn
   include CreatedAtFilterable
-
-  ignore_column :position
 
   DueDateStruct = Struct.new(:title, :name).freeze
   NoDueDate     = DueDateStruct.new('No Due Date', '0').freeze
