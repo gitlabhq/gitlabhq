8f6f609e8a18be7cc7d4a147bc3e3f019fd18ef8
Luke "Jared" Bennett <lbennett@gitlab.com>
2017-08-18 19:35:44 +0100

    Finished repo_store_spec

--- spec/javascripts/repo/stores/repo_store_spec.js
+++ spec/javascripts/repo/stores/repo_store_spec.js
@@ -1,15 +1,58 @@
+import RepoStore from '~/repo/stores/repo_store';
+
 describe('RepoStore', () => {
   describe('setFileActivity', () => {
-    it('calls setActiveFile', () => {
+    const index = 10;
 
+    it('calls setActiveFile', () => {
+      const file = {
+        url: '//url',
+      };
+      const openedFile = file;
+      const activeFile = openedFile;
+      activeFile.active = true;
+
+      spyOn(RepoStore, 'setActiveFile');
+
+      const returnValue = RepoStore.setFileActivity(file, openedFile, index);
+
+      expect(RepoStore.setActiveFile).toHaveBeenCalledWith(activeFile, index);
+      expect(returnValue).toEqual(activeFile);
     });
 
     it('does not call setActiveFile if file is not active', () => {
+      const file = {
+        url: '//url',
+      };
+      const openedFile = {
+        url: '//other-url',
+      };
+      const activeFile = openedFile;
+      activeFile.active = false;
 
+      spyOn(RepoStore, 'setActiveFile');
+
+      const returnValue = RepoStore.setFileActivity(file, openedFile, index);
+
+      expect(RepoStore.setActiveFile).not.toHaveBeenCalled();
+      expect(returnValue).toEqual(activeFile);
     });
 
     it('sets currentLine and reset hashToSet if hasToSet is set', () => {
+      const file = {
+        url: '//url',
+      };
+      const openedFile = file;
+      const activeFile = openedFile;
+      const hash = 'L10';
+      activeFile.active = true;
+      activeFile.currentLint = hash;
+      RepoStore.hashToSet = hash;
 
+      const returnValue = RepoStore.setFileActivity(file, openedFile, index);
+
+      expect(returnValue).toEqual(activeFile);
+      expect(RepoStore.hashToSet).toBeFalsy();
     });
   });
 });
