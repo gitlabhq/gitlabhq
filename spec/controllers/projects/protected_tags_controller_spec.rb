2ec7cfc91d81c843e82060c7c17da81aac68cb48
Stan Hu <stanhu@gmail.com>
2017-08-20 04:35:06 -0700

    Add spec for deleting protected tags

--- spec/controllers/projects/protected_tags_controller_spec.rb
+++ spec/controllers/projects/protected_tags_controller_spec.rb
@@ -8,4 +8,21 @@ describe Projects::ProtectedTagsController do
       get(:index, namespace_id: project.namespace.to_param, project_id: project)
     end
   end
+
+  describe "DELETE #destroy" do
+    let(:project) { create(:project, :repository) }
+    let(:protected_tag) { create(:protected_tag, :developers_can_create, project: project) }
+    let(:user) { create(:user) }
+
+    before do
+      project.add_master(user)
+      sign_in(user)
+    end
+
+    it "deletes the protected tag" do
+      delete(:destroy, namespace_id: project.namespace.to_param, project_id: project, id: protected_tag.id)
+
+      expect { ProtectedTag.find(protected_tag.id) }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
 end
