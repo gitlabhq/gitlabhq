c87f86ef0b055ed2852c1f3dddc051929e5db3db
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2017-08-21 17:46:41 -0300

    Geo - Remove unnecessary param from Geo::BaseSyncService#update_registry

--- app/services/geo/repository_sync_service.rb
+++ app/services/geo/repository_sync_service.rb
@@ -11,17 +11,17 @@ module Geo
 
     def fetch_project_repository
       log_info('Fetching project repository')
-      update_registry(:repository, started_at: DateTime.now)
+      update_registry(started_at: DateTime.now)
 
       begin
         project.ensure_repository
         project.repository.fetch_geo_mirror(ssh_url_to_repo)
 
-        update_registry(:repository, finished_at: DateTime.now)
+        update_registry(finished_at: DateTime.now)
       rescue Gitlab::Shell::Error, Geo::EmptyCloneUrlPrefixError => e
-        log_error("Error syncing repository", e)
+        log_error('Error syncing repository', e)
       rescue Gitlab::Git::Repository::NoRepository => e
-        log_error("Invalid repository", e)
+        log_error('Invalid repository', e)
         log_info('Expiring caches')
         project.repository.after_create
       end
