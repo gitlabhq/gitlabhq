57fb2d4f8f0c9f5dc563bdb0eb6935748674a299
http://jneen.net/ <jneen@jneen.net>
2017-08-21 13:47:15 -0700

    rm a comment that is a lie

--- spec/services/notification_service_spec.rb
+++ spec/services/notification_service_spec.rb
@@ -151,7 +151,6 @@ describe NotificationService, :mailer do
           add_users_with_subscription(note.project, issue)
           reset_delivered_emails!
 
-          # Ensure create SentNotification by noteable = issue 6 times, not noteable = note
           expect(SentNotification).to receive(:record).with(issue, any_args).exactly(9).times
 
           notification.new_note(note)
