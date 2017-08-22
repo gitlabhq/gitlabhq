e8525e107da9234d743caf8a0c7db1f46af60e89
Sean McGivern <sean@gitlab.com>
2017-08-22 16:27:09 +0100

    Show un-highlighted diffs when blobs are the same
    For some old merge requests, we don't have enough information to figure out the
    old blob and the new blob for the file. This means that we can't highlight the
    diff correctly, but we can still display it without highlighting.

--- lib/gitlab/diff/file.rb
+++ lib/gitlab/diff/file.rb
@@ -186,7 +186,10 @@ module Gitlab
       end
 
       def content_changed?
-        old_blob && new_blob && old_blob.id != new_blob.id
+        return blobs_changed? if diff_refs
+        return false if new_file? || deleted_file? || renamed_file?
+
+        text? && diff_lines.any?
       end
 
       def different_type?
@@ -224,6 +227,10 @@ module Gitlab
       end
 
       private
+
+      def blobs_changed?
+        old_blob && new_blob && old_blob.id != new_blob.id
+      end
 
       def simple_viewer_class
         return DiffViewer::NotDiffable unless diffable?
