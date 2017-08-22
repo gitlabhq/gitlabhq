a61f8c44bef6d3b8ad19b7791740960b4fd9a938
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2017-08-22 14:59:16 +0200

    Enqueue a new stage worker in CI/CD processing queue

--- app/workers/stage_update_worker.rb
+++ app/workers/stage_update_worker.rb
@@ -2,6 +2,8 @@ class StageUpdateWorker
   include Sidekiq::Worker
   include PipelineQueue
 
+  enqueue_in group: :processing
+
   def perform(stage_id)
     Ci::Stage.find_by(id: stage_id).try do |stage|
       stage.update_status
