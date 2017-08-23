6d8e102c740b75ac9e1d168a84f532f6d9ebaa65
Tiago Botelho <tiagonbotelho@hotmail.com>
2017-08-23 17:53:29 +0100

    Adds cacheless render to Banzai object render

--- app/controllers/concerns/renders_commit_description.rb
+++
@@ -1,7 +0,0 @@
-module RendersCommitDescription
-  def prepare_commit_descriptions_for_rendering(commit_descriptions)
-    Banzai::CommitDescriptionRenderer.render(commit_descriptions, @project, current_user)
-
-    commit_descriptions
-  end
-end
