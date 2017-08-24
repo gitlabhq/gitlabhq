545d7837dcc25f3675071bfc4b462959df8b4f7c
Tim Zallmann <tzallmann@gitlab.com>
2017-08-24 15:36:23 +0200

    Adding more access rights

--- spec/features/promotion_spec.rb
+++ spec/features/promotion_spec.rb
@@ -5,7 +5,7 @@ describe 'Promotions', js: true do
   let(:user) { create(:user) }
   let(:otherdeveloper) { create(:user, name: 'TheOtherDeveloper') }
   let(:group) { create(:group) }
-  let(:project) { create(:project, :repository) }
+  let(:project) { create(:project, :repository, namespace: group.namespace) }
   let(:milestone) { create(:milestone, project: project, start_date: Date.today, due_date: 7.days.from_now) }
   let!(:issue)  { create(:issue, project: project, author: user) }
   let(:otherproject) { create(:project, :repository, namespace: otherdeveloper.namespace) }
@@ -183,6 +183,7 @@ describe 'Promotions', js: true do
       allow(Gitlab).to receive(:com?) { true }
 
       project.team << [user, :master]
+      otherproject.team << [user, :master]
       sign_in(user)
     end
 
