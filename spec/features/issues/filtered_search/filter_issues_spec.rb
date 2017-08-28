656d2ca96422e9c4c44bbc5412f7a628cb9a7e57
Douwe Maan <douwe@gitlab.com>
2017-08-28 12:15:48 +0000

    Merge branch 'rs-remove-filtered-issues-skips' into 'master'
    Remove skipped examples in filtered issues feature spec
    
    See merge request !13845

--- spec/features/issues/filtered_search/filter_issues_spec.rb
+++ spec/features/issues/filtered_search/filter_issues_spec.rb
@@ -101,14 +101,6 @@ describe 'Filter issues', js: true do
         expect_issues_list_count(5)
         expect_filtered_search_input_empty
       end
-
-      it 'filters issues by invalid author' do
-        skip('to be tested, issue #26546')
-      end
-
-      it 'filters issues by multiple authors' do
-        skip('to be tested, issue #26546')
-      end
     end
 
     context 'author with other filters' do
@@ -158,10 +150,6 @@ describe 'Filter issues', js: true do
         expect_filtered_search_input(search_term)
       end
     end
-
-    it 'sorting' do
-      skip('to be tested, issue #26546')
-    end
   end
 
   describe 'filter issues by assignee' do
@@ -180,14 +168,6 @@ describe 'Filter issues', js: true do
         expect_tokens([{ name: 'assignee', value: 'none' }])
         expect_issues_list_count(8, 1)
         expect_filtered_search_input_empty
-      end
-
-      it 'filters issues by invalid assignee' do
-        skip('to be tested, issue #26546')
-      end
-
-      it 'filters issues by multiple assignees' do
-        skip('to be tested, issue #26546')
       end
     end
 
@@ -238,12 +218,6 @@ describe 'Filter issues', js: true do
         expect_filtered_search_input(search_term)
       end
     end
-
-    context 'sorting' do
-      it 'sorts' do
-        skip('to be tested, issue #26546')
-      end
-    end
   end
 
   describe 'filter issues by label' do
@@ -264,10 +238,6 @@ describe 'Filter issues', js: true do
         expect_tokens([{ name: 'label', value: 'none' }])
         expect_issues_list_count(9, 1)
         expect_filtered_search_input_empty
-      end
-
-      it 'filters issues by invalid label' do
-        skip('to be tested, issue #26546')
       end
 
       it 'filters issues by multiple labels' do
@@ -471,12 +441,6 @@ describe 'Filter issues', js: true do
         expect_filtered_search_input_empty
       end
     end
-
-    context 'sorting' do
-      it 'sorts' do
-        skip('to be tested, issue #26546')
-      end
-    end
   end
 
   describe 'filter issues by milestone' do
@@ -511,14 +475,6 @@ describe 'Filter issues', js: true do
         expect_tokens([{ name: 'milestone', value: 'started' }])
         expect_issues_list_count(5)
         expect_filtered_search_input_empty
-      end
-
-      it 'filters issues by invalid milestones' do
-        skip('to be tested, issue #26546')
-      end
-
-      it 'filters issues by multiple milestones' do
-        skip('to be tested, issue #26546')
       end
 
       it 'filters issues by milestone containing special characters' do
@@ -588,12 +544,6 @@ describe 'Filter issues', js: true do
         ])
         expect_issues_list_count(2)
         expect_filtered_search_input(search_term)
-      end
-    end
-
-    context 'sorting' do
-      it 'sorts' do
-        skip('to be tested, issue #26546')
       end
     end
   end
