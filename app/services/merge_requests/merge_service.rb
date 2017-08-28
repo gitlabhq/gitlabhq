86f43912fcb8dbf8604b0054d020acd12071d5b2
Oswaldo Ferreira <oswaldo@gitlab.com>
2017-08-28 16:41:05 -0300

    Cleans merge_jid when possible on MergeService

--- app/services/merge_requests/merge_service.rb
+++ app/services/merge_requests/merge_service.rb
@@ -26,14 +26,13 @@ module MergeRequests
       merge_request.in_locked_state do
         if commit
           after_merge
+          clean_merge_jid
           success
         end
       end
     rescue MergeError => e
+      clean_merge_jid
       log_merge_error(e.message, save_message_on_model: true)
-    ensure
-      # Make sure to clean up merge_jid in the end of the merge process.
-      merge_request.update_column(:merge_jid, nil)
     end
 
     private
@@ -71,6 +70,10 @@ module MergeRequests
             .execute(merge_request.source_branch)
         end
       end
+    end
+
+    def clean_merge_jid
+      merge_request.update_column(:merge_jid, nil)
     end
 
     def branch_deletion_user
