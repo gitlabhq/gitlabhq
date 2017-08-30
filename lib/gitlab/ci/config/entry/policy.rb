1bf87d25241eb5e1a0f147d350814a3a1cfa1343
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2017-08-30 14:47:54 +0200

    Remove unused expressions policy from ci/cd config

--- lib/gitlab/ci/config/entry/policy.rb
+++ lib/gitlab/ci/config/entry/policy.rb
@@ -7,31 +7,12 @@ module Gitlab
         #
         class Policy < Simplifiable
           strategy :RefsPolicy, if: -> (config) { config.is_a?(Array) }
-          strategy :ExpressionsPolicy, if: -> (config) { config.is_a?(Hash) }
 
           class RefsPolicy < Entry::Node
             include Entry::Validatable
 
             validations do
               validates :config, array_of_strings_or_regexps: true
-            end
-          end
-
-          class ExpressionsPolicy < Entry::Node
-            include Entry::Validatable
-            include Entry::Attributable
-
-            attributes :refs, :expressions
-
-            validations do
-              validates :config, presence: true
-              validates :config, allowed_keys: %i[refs expressions]
-
-              with_options allow_nil: true do
-                validates :refs, array_of_strings_or_regexps: true
-                validates :expressions, type: Array
-                validates :expressions, presence: true
-              end
             end
           end
 
