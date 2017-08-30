871fc8946ac94e4cd6dc0631f8c6a6895ef9ce75
Michael Kozono <mkozono@gmail.com>
2017-08-30 10:10:48 -0700

    Prevent more sync jobs if already pending

--- app/controllers/groups/ldaps_controller.rb
+++ app/controllers/groups/ldaps_controller.rb
@@ -3,9 +3,13 @@ class Groups::LdapsController < Groups::ApplicationController
   before_action :authorize_admin_group!
 
   def sync
-    @group.pending_ldap_sync
-    LdapGroupSyncWorker.perform_async(@group.id)
+    if @group.pending_ldap_sync
+      LdapGroupSyncWorker.perform_async(@group.id)
+      message = 'The group sync has been scheduled'
+    else
+      message = 'The group sync is already scheduled'
+    end
 
-    redirect_to group_group_members_path(@group), notice: 'The group sync has been scheduled'
+    redirect_to group_group_members_path(@group), notice: message
   end
 end
