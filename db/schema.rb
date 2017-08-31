f3d3cecf5a7691c5df48fc6b0f3d206cd57203db
Shinya Maeda <shinya@gitlab.com>
2017-08-31 20:25:25 +0900

    Fix schema. Add safe_model_attributes for pipelines

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170830125940) do
+ActiveRecord::Schema.define(version: 20170824162758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
