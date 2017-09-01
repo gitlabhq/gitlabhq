411d047d4bea85c711ed7eff88e7364f98d3d613
Douwe Maan <douwe@gitlab.com>
2017-09-01 11:49:22 +0000

    Merge branch '37202-revert-changes-to-signing-enabled' into 'master'
    Rollback changes made to signing_enabled.
    
    Closes #37202
    
    See merge request !13956

--- app/models/user.rb
+++ app/models/user.rb
@@ -598,7 +598,7 @@ class User < ActiveRecord::Base
   end
 
   def require_personal_access_token_creation_for_git_auth?
-    return false if allow_password_authentication? || ldap_user?
+    return false if current_application_settings.password_authentication_enabled? || ldap_user?
 
     PersonalAccessTokensFinder.new(user: self, impersonation: false, state: 'active').execute.none?
   end
