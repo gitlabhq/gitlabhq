a1a4754b0ac464cbe0cbbb3c3fab178d19e8c187
Michael Kozono <mkozono@gmail.com>
2017-08-31 17:31:03 -0700

    Add pre-nested group share lock tests

--- spec/features/projects/members/share_with_group_spec.rb
+++ spec/features/projects/members/share_with_group_spec.rb
@@ -2,76 +2,138 @@ require 'spec_helper'
 
 feature 'Project > Members > Share with Group', :js do
   include Select2Helper
+  include ActionView::Helpers::DateHelper
 
-  let(:master) { create(:user) }
-  let(:project) { create(:project) }
-  let!(:group) { create(:group) }
+  describe 'Share Lock' do
+    let(:master) { create(:user) }
+    let(:group) { create(:group) }
+    let!(:other_group) { create(:group) }
+    let(:project) { create(:project, namespace: group) }
 
-  background do
-    project.add_master(master)
-    sign_in(master)
+    background do
+      project.add_master(master)
+      sign_in(master)
+    end
+
+    context 'when the group does not have "Share lock" enabled' do
+      before do
+        visit project_settings_members_path(project)
+
+        click_on 'share-with-group-tab'
+
+        select2 other_group.id, from: '#link_group_id'
+        page.find('body').click
+        find('.btn-create').trigger('click')
+      end
+
+      scenario 'the group link appears in the existing groups list' do
+        page.within('.project-members-groups') do
+          expect(page).to have_content(other_group.name)
+        end
+      end
+    end
+
+    context 'when the group has "Share lock" enabled' do
+      before do
+        group.update_column(:share_with_group_lock, true)
+        visit project_settings_members_path(project)
+      end
+
+      scenario 'the "Share with group" tab does not exist' do
+        expect(page).to have_selector('#add-member-tab')
+        expect(page).not_to have_selector('#share-with-group-tab')
+      end
+    end
   end
 
-  context 'after setting an expiration date for a group link' do
+  describe 'setting an expiration date for a group link' do
+    let(:master) { create(:user) }
+    let(:project) { create(:project) }
+    let!(:group) { create(:group) }
+
+    around do |example|
+      Timecop.freeze { example.run }
+    end
+
     before do
+      project.add_master(master)
+      sign_in(master)
+
       visit project_settings_members_path(project)
 
       click_on 'share-with-group-tab'
 
       select2 group.id, from: '#link_group_id'
-      fill_in 'expires_at_groups', with: (Time.current + 4.5.days).strftime('%Y-%m-%d')
+
+      fill_in 'expires_at_groups', with: (Time.now + 4.5.days).strftime('%Y-%m-%d')
       page.find('body').click
       find('.btn-create').trigger('click')
     end
 
     scenario 'the group link shows the expiration time with a warning class' do
       page.within('.project-members-groups') do
-        expect(page).to have_content('Expires in 4 days')
+        # Using distance_of_time_in_words_to_now because it is not the same as
+        # subtraction, and this way avoids time zone issues as well
+        expires_in_text = distance_of_time_in_words_to_now(project.project_group_links.first.expires_at)
+        expect(page).to have_content(expires_in_text)
         expect(page).to have_selector('.text-warning')
       end
     end
   end
 
-  context 'for a project in a nested group' do
-    let!(:nested_group) { create(:group, parent: group) }
-    let!(:another_group) { create(:group) }
-    let!(:project) { create(:project, namespace: nested_group) }
+  describe 'the groups dropdown' do
+    context 'with multiple groups to choose from' do
+      let(:master) { create(:user) }
+      let(:project) { create(:project) }
+      let(:group) { create(:group) }
 
-    background do
-      group.add_master(master)
-      another_group.add_master(master)
-    end
+      background do
+        project.add_master(master)
+        sign_in(master)
 
-    scenario 'the groups dropdown does not show ancestors', :nested_groups do
-      visit project_settings_members_path(project)
+        group_two = create(:group)
+        group.add_owner(master)
+        group_two.add_owner(master)
 
-      click_on 'share-with-group-tab'
-      click_link 'Search for a group'
+        visit project_settings_members_path(project)
+        execute_script 'GroupsSelect.PER_PAGE = 1;'
+        open_select2 '#link_group_id'
+      end
 
-      page.within '.select2-drop' do
-        expect(page).to have_content(another_group.name)
-        expect(page).not_to have_content(group.name)
+      it 'should infinitely scroll' do
+        expect(find('.select2-drop .select2-results')).to have_selector('.select2-result', count: 1)
+
+        scroll_select2_to_bottom('.select2-drop .select2-results:visible')
+
+        expect(find('.select2-drop .select2-results')).to have_selector('.select2-result', count: 2)
       end
     end
-  end
 
-  describe 'the groups dropdown' do
-    before do
-      group_two = create(:group)
-      group.add_owner(master)
-      group_two.add_owner(master)
+    context 'for a project in a nested group' do
+      let(:master) { create(:user) }
+      let(:group) { create(:group) }
+      let!(:nested_group) { create(:group, parent: group) }
+      let!(:another_group) { create(:group) }
+      let!(:project) { create(:project, namespace: nested_group) }
 
-      visit project_settings_members_path(project)
-      execute_script 'GroupsSelect.PER_PAGE = 1;'
-      open_select2 '#link_group_id'
-    end
+      background do
+        project.add_master(master)
+        sign_in(master)
+        group.add_master(master)
+        another_group.add_master(master)
+      end
 
-    it 'should infinitely scroll' do
-      expect(find('.select2-drop .select2-results')).to have_selector('.select2-result', count: 1)
+      scenario 'the groups dropdown does not show ancestors', :nested_groups do
+        visit project_settings_members_path(project)
 
-      scroll_select2_to_bottom('.select2-drop .select2-results:visible')
+        click_on 'share-with-group-tab'
+        click_link 'Search for a group'
 
-      expect(find('.select2-drop .select2-results')).to have_selector('.select2-result', count: 2)
+        page.within '.select2-drop' do
+          expect(page).to have_content(another_group.name)
+          expect(page).not_to have_content(group.name)
+        end
+      end
     end
   end
 end
