703b39d4405af553b142071634db006941272db6
Alexis Reigel <mail@koffeinfrei.org>
2017-09-04 11:45:17 +0200

    truncate gpg signature instead of destroy

--- db/migrate/20170817123339_add_verification_status_to_gpg_signatures.rb
+++ db/migrate/20170817123339_add_verification_status_to_gpg_signatures.rb
@@ -4,20 +4,12 @@ class AddVerificationStatusToGpgSignatures < ActiveRecord::Migration
   include Gitlab::Database::MigrationHelpers
   disable_ddl_transaction!
 
-  class GpgSignature < ActiveRecord::Base
-    self.table_name = 'gpg_signatures'
-
-    include EachBatch
-  end
-
   def up
     # First we remove all signatures because we need to re-verify them all
     # again anyway (because of the updated verification logic).
     #
     # This makes adding the column with default values faster
-    GpgSignature.each_batch do |relation|
-      relation.delete_all
-    end
+    truncate(:gpg_signatures)
 
     add_column_with_default(:gpg_signatures, :verification_status, :smallint, default: 0)
   end
--- db/post_migrate/20170830084744_destroy_gpg_signatures.rb
+++ db/post_migrate/20170830084744_destroy_gpg_signatures.rb
@@ -1,19 +1,8 @@
 class DestroyGpgSignatures < ActiveRecord::Migration
   DOWNTIME = false
 
-  include Gitlab::Database::MigrationHelpers
-  disable_ddl_transaction!
-
-  class GpgSignature < ActiveRecord::Base
-    self.table_name = 'gpg_signatures'
-
-    include EachBatch
-  end
-
   def up
-    GpgSignature.each_batch do |relation|
-      relation.delete_all
-    end
+    truncate(:gpg_signatures)
   end
 
   def down
