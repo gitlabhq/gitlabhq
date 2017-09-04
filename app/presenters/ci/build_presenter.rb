fa6b9acaf9759c58353f8407ff20a7d02b8edf92
Shinya Maeda <shinya@gitlab.com>
2017-09-04 17:17:36 +0900

    trigger_variables should consider trigger_request existstance always

--- app/presenters/ci/build_presenter.rb
+++ app/presenters/ci/build_presenter.rb
@@ -17,16 +17,5 @@ module Ci
         "Job is redundant and is auto-canceled by Pipeline ##{auto_canceled_by_id}"
       end
     end
-
-    def old_or_new_trigger_variables
-      return @old_or_new_trigger_variables if defined?(@old_or_new_trigger_variables)
-
-      if build.pipeline.variables.any?
-        @old_or_new_trigger_variables =
-          build.pipeline.variables&.map { |v| { v.key => v.value } } &.reduce({}, :merge)
-      else
-        @old_or_new_trigger_variables = build.trigger_request.variables
-      end
-    end
   end
 end
