440e93114ea4e183817664a55070f28e0901c9ce
Kamil Trzcinski <ayufan@ayufan.eu>
2017-09-05 15:39:19 +0200

    Fix spelling of AutoDevopsHelper

--- app/helpers/auto_devops_helper.rb
+++ app/helpers/auto_devops_helper.rb
@@ -1,4 +1,4 @@
-module AutoDevOpsHelper
+module AutoDevopsHelper
   def show_auto_devops_callout?(project)
     show_callout?('auto_devops_settings_dismissed') &&
         can?(current_user, :admin_pipeline, project) &&
