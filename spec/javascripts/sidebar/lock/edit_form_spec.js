1e9afd38e2be0bb2ccce96d4f5751c12e36283e1
Luke "Jared" Bennett <lbennett@gitlab.com>
2017-09-06 12:23:32 +0100

    [ci skip] Added EditForm

--- spec/javascripts/sidebar/lock/edit_form_spec.js
+++ spec/javascripts/sidebar/lock/edit_form_spec.js
@@ -2,4 +2,38 @@ import Vue from 'vue';
 import editForm from '~/sidebar/components/lock/edit_form.vue';
 
 describe('EditForm', () => {
+  let vm1;
+  let vm2;
+
+  beforeEach(() => {
+    const Component = Vue.extend(editForm);
+    const toggleForm = () => { };
+    const updateLockedAttribute = () => { };
+
+    vm1 = new Component({
+      propsData: {
+        isLocked: true,
+        toggleForm,
+        updateLockedAttribute,
+      },
+    }).$mount();
+
+    vm2 = new Component({
+      propsData: {
+        isLocked: false,
+        toggleForm,
+        updateLockedAttribute,
+      },
+    }).$mount();
+  });
+
+  it('renders on the appropriate warning text', () => {
+    expect(
+      vm1.$el.innerHTML.includes('Unlock this issue?'),
+    ).toBe(true);
+
+    expect(
+      vm2.$el.innerHTML.includes('Lock this issue?'),
+    ).toBe(true);
+  });
 });
