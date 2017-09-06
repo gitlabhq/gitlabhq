8dbb228be9afb7f5e9e71b3e19706b90eff79620
Pawel Chojnacki <pawel@chojnacki.ws>
2017-09-06 18:55:21 +0200

    Rails cache spec

--- spec/lib/gitlab/metrics/subscribers/rails_cache_spec.rb
+++ spec/lib/gitlab/metrics/subscribers/rails_cache_spec.rb
@@ -1,15 +1,16 @@
 require 'spec_helper'
 
 describe Gitlab::Metrics::Subscribers::RailsCache do
-  let(:transaction) { Gitlab::Metrics::Transaction.new }
+  let(:env) { {} }
+  let(:transaction) { Gitlab::Metrics::Transaction.new(env) }
   let(:subscriber) { described_class.new }
 
   let(:event) { double(:event, duration: 15.2) }
 
   describe '#cache_read' do
     it 'increments the cache_read duration' do
-      expect(subscriber).to receive(:increment)
-        .with(:cache_read, event.duration)
+      expect(subscriber).to receive(:observe)
+                              .with(:read, event.duration)
 
       subscriber.cache_read(event)
     end
@@ -17,7 +18,7 @@ describe Gitlab::Metrics::Subscribers::RailsCache do
     context 'with a transaction' do
       before do
         allow(subscriber).to receive(:current_transaction)
-          .and_return(transaction)
+                               .and_return(transaction)
       end
 
       context 'with hit event' do
@@ -25,9 +26,9 @@ describe Gitlab::Metrics::Subscribers::RailsCache do
 
         it 'increments the cache_read_hit count' do
           expect(transaction).to receive(:increment)
-            .with(:cache_read_hit_count, 1)
+                                   .with(:cache_read_hit_count, 1, false)
           expect(transaction).to receive(:increment)
-            .with(any_args).at_least(1) # Other calls
+                                   .with(any_args).at_least(1) # Other calls
 
           subscriber.cache_read(event)
         end
@@ -37,7 +38,7 @@ describe Gitlab::Metrics::Subscribers::RailsCache do
 
           it 'does not increment cache read miss' do
             expect(transaction).not_to receive(:increment)
-              .with(:cache_read_hit_count, 1)
+                                         .with(:cache_read_hit_count, 1)
 
             subscriber.cache_read(event)
           end
@@ -49,9 +50,9 @@ describe Gitlab::Metrics::Subscribers::RailsCache do
 
         it 'increments the cache_read_miss count' do
           expect(transaction).to receive(:increment)
-            .with(:cache_read_miss_count, 1)
+                                   .with(:cache_read_miss_count, 1, false)
           expect(transaction).to receive(:increment)
-            .with(any_args).at_least(1) # Other calls
+                                   .with(any_args).at_least(1) # Other calls
 
           subscriber.cache_read(event)
         end
@@ -61,7 +62,7 @@ describe Gitlab::Metrics::Subscribers::RailsCache do
 
           it 'does not increment cache read miss' do
             expect(transaction).not_to receive(:increment)
-              .with(:cache_read_miss_count, 1)
+                                         .with(:cache_read_miss_count, 1)
 
             subscriber.cache_read(event)
           end
@@ -72,8 +73,8 @@ describe Gitlab::Metrics::Subscribers::RailsCache do
 
   describe '#cache_write' do
     it 'increments the cache_write duration' do
-      expect(subscriber).to receive(:increment)
-        .with(:cache_write, event.duration)
+      expect(subscriber).to receive(:observe)
+                              .with(:write, event.duration)
 
       subscriber.cache_write(event)
     end
@@ -81,8 +82,8 @@ describe Gitlab::Metrics::Subscribers::RailsCache do
 
   describe '#cache_delete' do
     it 'increments the cache_delete duration' do
-      expect(subscriber).to receive(:increment)
-        .with(:cache_delete, event.duration)
+      expect(subscriber).to receive(:observe)
+                              .with(:delete, event.duration)
 
       subscriber.cache_delete(event)
     end
@@ -90,8 +91,8 @@ describe Gitlab::Metrics::Subscribers::RailsCache do
 
   describe '#cache_exist?' do
     it 'increments the cache_exists duration' do
-      expect(subscriber).to receive(:increment)
-        .with(:cache_exists, event.duration)
+      expect(subscriber).to receive(:observe)
+                              .with(:exists, event.duration)
 
       subscriber.cache_exist?(event)
     end
@@ -109,12 +110,12 @@ describe Gitlab::Metrics::Subscribers::RailsCache do
     context 'with a transaction' do
       before do
         allow(subscriber).to receive(:current_transaction)
-          .and_return(transaction)
+                               .and_return(transaction)
       end
 
       it 'increments the cache_read_hit count' do
         expect(transaction).to receive(:increment)
-          .with(:cache_read_hit_count, 1)
+                                 .with(:cache_read_hit_count, 1)
 
         subscriber.cache_fetch_hit(event)
       end
@@ -133,47 +134,47 @@ describe Gitlab::Metrics::Subscribers::RailsCache do
     context 'with a transaction' do
       before do
         allow(subscriber).to receive(:current_transaction)
-          .and_return(transaction)
+                               .and_return(transaction)
       end
 
       it 'increments the cache_fetch_miss count' do
         expect(transaction).to receive(:increment)
-          .with(:cache_read_miss_count, 1)
+                                 .with(:cache_read_miss_count, 1)
 
         subscriber.cache_generate(event)
       end
     end
   end
 
-  describe '#increment' do
+  describe '#observe' do
     context 'without a transaction' do
       it 'returns' do
         expect(transaction).not_to receive(:increment)
 
-        subscriber.increment(:foo, 15.2)
+        subscriber.observe(:foo, 15.2)
       end
     end
 
     context 'with a transaction' do
       before do
         allow(subscriber).to receive(:current_transaction)
-          .and_return(transaction)
+                               .and_return(transaction)
       end
 
       it 'increments the total and specific cache duration' do
         expect(transaction).to receive(:increment)
-          .with(:cache_duration, event.duration)
+                                 .with(:cache_duration, event.duration, false)
 
         expect(transaction).to receive(:increment)
-          .with(:cache_count, 1)
+                                 .with(:cache_count, 1, false)
 
         expect(transaction).to receive(:increment)
-          .with(:cache_delete_duration, event.duration)
+                                 .with(:cache_delete_duration, event.duration, false)
 
         expect(transaction).to receive(:increment)
-          .with(:cache_delete_count, 1)
+                                 .with(:cache_delete_count, 1, false)
 
-        subscriber.increment(:cache_delete, event.duration)
+        subscriber.observe(:delete, event.duration)
       end
     end
   end
