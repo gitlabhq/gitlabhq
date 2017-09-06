ec7a12da818898b278a3e47a9b96ccebafbe5b4c
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2017-09-06 14:45:28 +0200

    Revert moving authorization hook in jobs API

--- lib/api/jobs.rb
+++ lib/api/jobs.rb
@@ -2,12 +2,12 @@ module API
   class Jobs < Grape::API
     include PaginationParams
 
+    before { authenticate! }
+
     params do
       requires :id, type: String, desc: 'The ID of a project'
     end
     resource :projects, requirements: API::PROJECT_ENDPOINT_REQUIREMENTS do
-      before { authenticate! }
-
       helpers do
         params :optional_scope do
           optional :scope, types: [String, Array[String]], desc: 'The scope of builds to show',
