cdc74459269831f7e0c5d80d7bc95d3dbd810835
Luke "Jared" Bennett <lbennett@gitlab.com>
2017-09-06 12:26:37 +0100

    Add edito_form_buttons_spec

--- spec/javascripts/sidebar/lock/edit_form_buttons_spec.js
+++ spec/javascripts/sidebar/lock/edit_form_buttons_spec.js
@@ -2,4 +2,38 @@ import Vue from 'vue';
 import editFormButtons from '~/sidebar/components/lock/edit_form_buttons.vue';
 
 describe('EditFormButtons', () => {
+  let vm1;
+  let vm2;
+
+  beforeEach(() => {
+    const Component = Vue.extend(editFormButtons);
+    const toggleForm = () => { };
+    const updateLockedAttribute = () => { };
+
+    vm1 = new Component({
+      propsData: {
+        isLocked: true,
+        toggleForm,
+        updateLockedAttribute,
+      },
+    }).$mount();
+
+    vm2 = new Component({
+      propsData: {
+        isLocked: false,
+        toggleForm,
+        updateLockedAttribute,
+      },
+    }).$mount();
+  });
+
+  it('renders unlock or lock text based on locked state', () => {
+    expect(
+      vm1.$el.innerHTML.includes('Unlock'),
+    ).toBe(true);
+
+    expect(
+      vm2.$el.innerHTML.includes('Lock'),
+    ).toBe(true);
+  });
 });
