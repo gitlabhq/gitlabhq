c1f66cc7f0658621f2bfcb09f0269da675f57e84
micael.bergeron <micael.bergeron@solutionstlm.com>
2017-09-06 09:15:27 -0400

    remove useless comment

--- lib/gitlab/encoding_helper.rb
+++ lib/gitlab/encoding_helper.rb
@@ -13,8 +13,6 @@ module Gitlab
     # https://gitlab.com/gitlab-org/gitlab_git/merge_requests/77#note_4754193
     ENCODING_CONFIDENCE_THRESHOLD = 50
 
-    #
-    #
     def encode!(message)
       return nil unless message.respond_to? :force_encoding
 
