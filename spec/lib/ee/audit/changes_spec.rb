9cc7764adcea4e812c610f0abb19492663edcf8b
James Lopez <james@jameslopez.es>
2017-09-12 09:28:17 +0200

    refactor and fix spec

--- spec/lib/ee/audit/changes_spec.rb
+++ spec/lib/ee/audit/changes_spec.rb
@@ -7,24 +7,23 @@ describe EE::Audit::Changes do
 
     before do
       foo_instance.instance_variable_set(:@current_user, user)
+      foo_instance.instance_variable_set(:@user, user)
       allow(foo_instance).to receive(:model).and_return(user)
     end
 
     describe 'non audit changes' do
       it 'does not call the audit event service' do
-        expect_any_instance_of(AuditEventService).not_to receive(:security_event)
-
         user.update!(name: 'new name')
-        foo_instance.audit_changes(:email)
+
+        expect{ foo_instance.audit_changes(:email) }.not_to change { SecurityEvent.count }
       end
     end
 
     describe 'audit changes' do
       it 'calls the audit event service' do
-        expect_any_instance_of(AuditEventService).to receive(:security_event)
-
         user.update!(name: 'new name')
-        foo_instance.audit_changes(:name)
+
+        expect{ foo_instance.audit_changes(:name) }.to change { SecurityEvent.count }.by(1)
       end
     end
   end
