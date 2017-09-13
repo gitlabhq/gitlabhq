39df53ff0aeac24d390eea52a1df6dfe103a4c14
Bob Van Landuyt <bob@vanlanduyt.co>
2017-09-13 11:29:32 +0200

    Use the default sort set by the `Sortable` concern

--- app/controllers/dashboard/groups_controller.rb
+++ app/controllers/dashboard/groups_controller.rb
@@ -1,13 +1,11 @@
 class Dashboard::GroupsController < Dashboard::ApplicationController
   def index
-    @sort = params[:sort] || 'id_desc'
-
     @groups = GroupsFinder.new(current_user, all_available: false).execute
     # Only show root groups if no parent-id is given
     @groups = @groups.where(parent_id: params[:parent_id])
     @groups = @groups.search(params[:filter]) if params[:filter].present?
     @groups = @groups.includes(:route)
-    @groups = @groups.sort(@sort)
+    @groups = @groups.sort(@sort = params[:sort])
     @groups = @groups.page(params[:page])
 
     respond_to do |format|
