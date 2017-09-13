374442b101b27e251c0ae5adc0f42422b51b8ba4
Rémy Coutable <remy@rymai.me>
2017-09-13 17:46:49 +0200

    Resolve conflict with lib/ci/model.rb | lib/gitlab/ci/model.rb
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- lib/gitlab/ci/model.rb
+++ lib/gitlab/ci/model.rb
@@ -6,7 +6,7 @@ module Gitlab
       end
 
       def model_name
-        @model_name ||= ActiveModel::Name.new(self, nil, self.name.split("::").last)
+        @model_name ||= ActiveModel::Name.new(self, nil, self.name.demodulize)
       end
     end
   end
