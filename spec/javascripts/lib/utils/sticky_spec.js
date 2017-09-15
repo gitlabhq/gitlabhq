734bb736258293e213fb0580f1f0b15618a78bc2
Phil Hughes <me@iamphill.com>
2017-09-15 09:01:58 +0100

    fixed and added specs for removing placeholder element

--- spec/javascripts/lib/utils/sticky_spec.js
+++ spec/javascripts/lib/utils/sticky_spec.js
@@ -4,12 +4,25 @@ describe('sticky', () => {
   const el = {
     offsetTop: 0,
     classList: {},
+    parentNode: {},
+    nextElementSibling: {},
   };
+  let isStuck = false;
 
   beforeEach(() => {
     el.offsetTop = 0;
-    el.classList.add = jasmine.createSpy('spy');
-    el.classList.remove = jasmine.createSpy('spy');
+    el.classList.add = jasmine.createSpy('classListAdd');
+    el.classList.remove = jasmine.createSpy('classListRemove');
+    el.classList.contains = jasmine.createSpy('classListContains').and.callFake(() => isStuck);
+    el.parentNode.insertBefore = jasmine.createSpy('insertBefore');
+    el.nextElementSibling.remove = jasmine.createSpy('nextElementSibling');
+    el.nextElementSibling.classList = {
+      contains: jasmine.createSpy('classListContains').and.returnValue(true),
+    };
+  });
+
+  afterEach(() => {
+    isStuck = false;
   });
 
   describe('classList.remove', () => {
@@ -21,13 +34,26 @@ describe('sticky', () => {
       ).not.toHaveBeenCalled();
     });
 
-    it('calls classList.remove when not stuck', () => {
+    it('calls classList.remove when no longer stuck', () => {
+      isStuck = true;
+
       el.offsetTop = 10;
       isSticky(el, 0, 0);
 
       expect(
         el.classList.remove,
       ).toHaveBeenCalledWith('is-stuck');
+    });
+
+    it('removes placeholder when no longer stuck', () => {
+      isStuck = true;
+
+      el.offsetTop = 10;
+      isSticky(el, 0, 0, true);
+
+      expect(
+        el.nextElementSibling.remove,
+      ).toHaveBeenCalled();
     });
   });
 
@@ -47,6 +73,14 @@ describe('sticky', () => {
       expect(
         el.classList.add,
       ).not.toHaveBeenCalled();
+    });
+
+    it('inserts placeholder element when stuck', () => {
+      isSticky(el, 0, 0, true);
+
+      expect(
+        el.parentNode.insertBefore,
+      ).toHaveBeenCalled();
     });
   });
 });
