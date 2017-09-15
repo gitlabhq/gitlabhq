97caed20ed271861fa217d2a6164b8ac26473d2f
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2017-09-15 12:20:53 +0200

    Add specs for CI/CD job policy factory method

---
+++ spec/lib/gitlab/ci/build/policy_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe Gitlab::Ci::Build::Policy do
+  let(:policy) { spy('policy specification') }
+
+  before do
+    stub_const("#{described_class}::Something", policy)
+  end
+
+  describe '.fabricate' do
+    context 'when policy exists' do
+      it 'fabricates and initializes relevant policy' do
+        specs = described_class.fabricate(something: 'some value')
+
+        expect(specs).to be_an Array
+        expect(specs).to be_one
+        expect(policy).to have_received(:new).with('some value')
+      end
+    end
+
+    context 'when some policies are not defined' do
+      it 'gracefully skips unknown policies' do
+        specs = described_class.fabricate(something: 'first', else: 'unknown')
+
+        expect(specs).to be_an Array
+        expect(specs).to be_one
+        expect(policy).to have_received(:new).with('first')
+      end
+    end
+
+    context 'when passing a nil value as specs' do
+      it 'returns an empty array' do
+        specs = described_class.fabricate(nil)
+
+        expect(specs).to be_an Array
+        expect(specs).to be_empty
+      end
+    end
+  end
+end
