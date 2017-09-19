1c30f13b194401fe11eaee3db0a789eb02c60aa7
Jacob Vosmaer <jacob@gitlab.com>
2017-09-19 14:57:35 +0200

    Hide Gollum inside Gitlab::Git::Wiki

---
+++ lib/gitlab/git/wiki_page_version.rb
@@ -0,0 +1,20 @@
+module Gitlab
+  module Git
+    class WikiPageVersion
+      attr_reader :commit, :format
+
+      # This class is meant to be serializable so that it can be constructed
+      # by Gitaly and sent over the network to GitLab.
+      #
+      # Both 'commit' (a Gitlab::Git::Commit) and 'format' (a string) are
+      # serializable.
+      def initialize(commit, format)
+        @commit = commit
+        @format = format
+      end
+
+      # 'delegate' comes from active_support/core_ext/delegation
+      delegate :message, :sha, :id, :author_name, :authored_date, to: :commit
+    end
+  end
+end
