c207122fd2b4439ff8303a1860c35de658d6bdfb
Matt Coleman <matt@datto.com>
2017-09-21 16:05:44 -0400

    Add Packagist project service

--- app/models/project.rb
+++ app/models/project.rb
@@ -117,6 +117,7 @@ class Project < ActiveRecord::Base
   has_one :mock_deployment_service
   has_one :mock_monitoring_service
   has_one :microsoft_teams_service
+  has_one :packagist_service
 
   # TODO: replace these relations with the fork network versions
   has_one  :forked_project_link,  foreign_key: "forked_to_project_id"
