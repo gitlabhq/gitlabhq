bd6a9133817b77c9f6791926f3dd52ef7b2ab580
James Lopez <james@jameslopez.es>
2017-09-24 11:15:50 +0200

    fix spec

--- spec/ee/spec/lib/ee/gitlab/ldap/user_filter_spec.rb
+++ spec/ee/spec/lib/ee/gitlab/ldap/user_filter_spec.rb
@@ -7,6 +7,7 @@ describe EE::Gitlab::LDAP::UserFilter do
     OmniAuth::AuthHash.new(uid: 'uid=john,ou=people,dc=example,dc=com', provider: 'ldapmain')
   end
   let!(:fake_proxy) { fake_ldap_sync_proxy(auth_hash.provider) }
+  let(:fake_entry) { ldap_user_entry('user1') }
 
   before do
     stub_ldap_config(
@@ -19,10 +20,15 @@ describe EE::Gitlab::LDAP::UserFilter do
     it 'returns dns from an LDAP search' do
       filter = '(ou=people)'
 
-      entry = ldap_group_entry(%w(john mary))
-      allow(fake_proxy.adapter).to receive(:ldap_search).and_return([entry])
+      allow(fake_proxy.adapter).to receive(:ldap_search).and_return([fake_entry])
 
-      expect(described_class.filter(fake_proxy, filter)).to eq('')
+      expect(described_class.filter(fake_proxy, filter)).to eq(['uid=user1,ou=users,dc=example,dc=com'])
+    end
+
+    it 'errors out with an invalid filter' do
+      filter = ')('
+
+      expect{ described_class.filter(fake_proxy, filter) }.to raise_error(Net::LDAP::FilterSyntaxInvalidError, 'Invalid filter syntax.')
     end
   end
 end
