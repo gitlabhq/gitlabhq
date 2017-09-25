2212dad64632f09a7399b8a32801a16bfd218f1d
Toon Claes <toon@iotcl.com>
2017-09-25 22:22:38 +0200

    Do not check for secondary? but use readonly? instead
    See gitlab-org/gitlab-ee!2983.

--- spec/ee/spec/services/ee/keys/last_used_service_spec.rb
+++ spec/ee/spec/services/ee/keys/last_used_service_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
 describe Keys::LastUsedService do
-  it 'does not run on Geo secondaries', :clean_gitlab_redis_shared_state do
+  it 'does not run on read-only GitLab instances', :clean_gitlab_redis_shared_state do
     key = create(:key, last_used_at: 1.year.ago)
     original_time = key.last_used_at
 
-    allow(::Gitlab::Geo).to receive(:secondary?).and_return(true)
+    allow(::Gitlab::Database).to receive(:readonly?).and_return(true)
     described_class.new(key).execute
 
     expect(key.reload.last_used_at).to be_like_time(original_time)
