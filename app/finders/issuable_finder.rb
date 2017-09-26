a4168719abefe9cfa6c417d73e0833823a92fa08
Rémy Coutable <remy@rymai.me>
2017-09-26 16:18:45 +0200

    Remove `weight` from IssuableFinder::SCALAR_PARAMS and improve the array formatting
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- app/finders/issuable_finder.rb
+++ app/finders/issuable_finder.rb
@@ -25,8 +25,26 @@ class IssuableFinder
 
   NONE = '0'.freeze
 
-  SCALAR_PARAMS = %i(scope state group_id project_id milestone_title assignee_id search label_name sort assignee_username author_id author_username authorized_only due_date iids non_archived weight).freeze
+  SCALAR_PARAMS = %i[
+    assignee_id
+    assignee_username
+    author_id
+    author_username
+    authorized_only
+    due_date
+    group_id
+    iids
+    label_name
+    milestone_title
+    non_archived
+    project_id
+    scope
+    search
+    sort
+    state
+  ].freeze
   ARRAY_PARAMS = { label_name: [], iids: [], assignee_username: [] }.freeze
+
   VALID_PARAMS = (SCALAR_PARAMS + [ARRAY_PARAMS]).freeze
 
   attr_accessor :current_user, :params
