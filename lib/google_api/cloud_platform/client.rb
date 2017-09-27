9900933432fc05a07b4c3e742c0ec3f295cf773c
Shinya Maeda <shinya@gitlab.com>
2017-09-27 17:04:25 +0900

    Remove sensitive literal data

--- lib/google_api/cloud_platform/client.rb
+++ lib/google_api/cloud_platform/client.rb
@@ -23,7 +23,6 @@ module GoogleApi
       end
 
       # Responce exmaple 
-      # {"name":"operation-1506424047439-0293f57c","operationType":"CREATE_CLUSTER","selfLink":"https://container.googleapis.com/v1/projects/696404988091/zones/us-central1-a/operations/operation-1506424047439-0293f57c","startTime":"2017-09-26T11:07:27.439033158Z","status":"RUNNING","targetLink":"https://container.googleapis.com/v1/projects/696404988091/zones/us-central1-a/clusters/gke-test-creation","zone":"us-central1-a"}
       # TODO: machine_type : Defailt 3.75 GB
       def projects_zones_clusters_create(project_id, zone, cluster_name, cluster_size:, machine_type:)
         service = Google::Apis::ContainerV1::ContainerService.new
