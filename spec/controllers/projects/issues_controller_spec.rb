4baea2262bfa7c35df6adf897fed789f69b1e554
Robert Speicher <robert@gitlab.com>
2017-09-27 19:39:45 +0000

    Merge branch 'port-ee-3435' into 'security-10-0'
    [10.0 CE] Prevent "Related Issues" from leaking confidential issues
    
    See merge request gitlab/gitlabhq!2193

--- spec/controllers/projects/issues_controller_spec.rb
+++ spec/controllers/projects/issues_controller_spec.rb
@@ -226,7 +226,7 @@ describe Projects::IssuesController do
           id: issue.iid,
           issue: { assignee_ids: [assignee.id] },
           format: :json
-        body = JSON.parse(response.body)
+        body = json_response
 
         expect(body['assignees'].first.keys)
           .to match_array(%w(id name username avatar_url state web_url))
@@ -889,16 +889,17 @@ describe Projects::IssuesController do
 
   describe 'GET #discussions' do
     let!(:discussion) { create(:discussion_note_on_issue, noteable: issue, project: issue.project) }
+    context 'when authenticated' do
+      before do
+        project.add_developer(user)
+        sign_in(user)
+      end
 
-    before do
-      project.add_developer(user)
-      sign_in(user)
-    end
+      it 'returns discussion json' do
+        get :discussions, namespace_id: project.namespace, project_id: project, id: issue.iid
 
-    it 'returns discussion json' do
-      get :discussions, namespace_id: project.namespace, project_id: project, id: issue.iid
-
-      expect(JSON.parse(response.body).first.keys).to match_array(%w[id reply_id expanded notes individual_note])
+        expect(json_response.first.keys).to match_array(%w[id reply_id expanded notes individual_note])
+      end
     end
 
     context 'with cross-reference system note', :request_store do
