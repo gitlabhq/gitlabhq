3b0aedda4ff411eba0e90488f44f5809b7c41b2d
Nick Thomas <nick@gitlab.com>
2017-09-27 15:45:27 +0100

    Send a Geo repository renamed event when migrating to hashed storage

--- app/services/projects/hashed_storage_migration_service.rb
+++ app/services/projects/hashed_storage_migration_service.rb
@@ -2,6 +2,8 @@ module Projects
   class HashedStorageMigrationService < BaseService
     include Gitlab::ShellAdapter
 
+    prepend ::EE::Projects::HashedStorageMigrationService
+
     attr_reader :old_disk_path, :new_disk_path
 
     def initialize(project, logger = nil)
