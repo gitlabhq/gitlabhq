d361c0d3fdf514cfdd31fc511758f3286c7f8e5a
Valery Sizov <valery@gitlab.com>
2017-09-27 16:10:23 +0300

    [Rails 5][ci skip] Comment the special route configuration option so other developer can understand it

--- config/routes/wiki.rb
+++ config/routes/wiki.rb
@@ -6,6 +6,9 @@ scope(controller: :wikis) do
     post '/', to: 'wikis#create'
   end
 
+  # Note, we use "format: false" and "defaults: { format: :html }" parameters together
+  # While it can be confusing it's absolutelly needed to make it behave like Rails 4.
+  # See https://github.com/rails/rails/issues/28901
   scope(path: 'wikis/*id', as: :wiki, format: false, defaults: { format: :html }) do
     get :edit
     get :history
