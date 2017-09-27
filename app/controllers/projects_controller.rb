1123942fce9590bfa1a0f21f078020e342ec1223
Robert Speicher <robert@gitlab.com>
2017-09-27 20:15:39 +0000

    Merge branch 'fix-arbitrary-redirect-vulnerability' into 'security-10-0'
    Fix arbitrary redirect location vulnerability
    
    See merge request gitlab/gitlabhq!2192

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,6 +3,7 @@ class ProjectsController < Projects::ApplicationController
   include ExtractsPath
 
   before_action :authenticate_user!, except: [:index, :show, :activity, :refs]
+  before_action :redirect_git_extension, only: [:show]
   before_action :project, except: [:index, :new, :create]
   before_action :repository, except: [:index, :new, :create]
   before_action :assign_ref_vars, only: [:show], if: :repo_exists?
@@ -398,5 +399,14 @@ class ProjectsController < Projects::ApplicationController
 
   def project_export_enabled
     render_404 unless current_application_settings.project_export_enabled?
+  end
+
+  def redirect_git_extension
+    # Redirect from
+    #   localhost/group/project.git
+    # to
+    #   localhost/group/project
+    #
+    redirect_to request.original_url.sub(/\.git\/?\Z/, '') if params[:format] == 'git'
   end
 end
