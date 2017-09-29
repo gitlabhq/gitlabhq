33985a9c4b41a2176e5e89b7d5501372624a3f8b
Valery Sizov <valery@gitlab.com>
2017-09-29 19:58:55 +0300

    Remove Project#merge_method accessors from EE side as we backported this to CE

--- ee/app/models/ee/project.rb
+++ ee/app/models/ee/project.rb
@@ -337,34 +337,6 @@ module EE
       @path_lock_finder.find(path, exact_match: exact_match, downstream: downstream)
     end
 
-    def merge_method
-      if self.merge_requests_ff_only_enabled
-        :ff
-      elsif self.merge_requests_rebase_enabled
-        :rebase_merge
-      else
-        :merge
-      end
-    end
-
-    def merge_method=(method)
-      case method.to_s
-      when "ff"
-        self.merge_requests_ff_only_enabled = true
-        self.merge_requests_rebase_enabled = true
-      when "rebase_merge"
-        self.merge_requests_ff_only_enabled = false
-        self.merge_requests_rebase_enabled = true
-      when "merge"
-        self.merge_requests_ff_only_enabled = false
-        self.merge_requests_rebase_enabled = false
-      end
-    end
-
-    def ff_merge_must_be_possible?
-      self.merge_requests_ff_only_enabled || self.merge_requests_rebase_enabled
-    end
-
     def import_url_updated?
       # check if import_url has been updated and it's not just the first assignment
       import_url_changed? && changes['import_url'].first
