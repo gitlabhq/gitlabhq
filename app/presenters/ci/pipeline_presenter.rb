ac312d732dd6c0bd7b4bc5c6f8fc27a38ed96452
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2017-09-29 11:14:00 +0200

    Move part of pipeline presenter to main CE file

--- app/presenters/ci/pipeline_presenter.rb
+++ app/presenters/ci/pipeline_presenter.rb
@@ -2,7 +2,16 @@ module Ci
   class PipelinePresenter < Gitlab::View::Presenter::Delegated
     prepend ::EE::Ci::PipelinePresenter
 
+    FAILURE_REASONS = EE_FAILURE_REASONS
+
     presents :pipeline
+
+    def failure_reason
+      return unless pipeline.failure_reason?
+
+      FAILURE_REASONS[pipeline.failure_reason.to_sym] ||
+        pipeline.failure_reason
+    end
 
     def status_title
       if auto_canceled?
--- ee/app/presenters/ee/ci/pipeline_presenter.rb
+++ ee/app/presenters/ee/ci/pipeline_presenter.rb
@@ -1,17 +1,10 @@
 module EE
   module Ci
     module PipelinePresenter
-      FAILURE_REASONS = {
+      EE_FAILURE_REASONS = {
         activity_limit_exceeded: 'Pipeline activity limit exceeded!',
         size_limit_exceeded: 'Pipeline size limit exceeded!'
       }
-
-      def failure_reason
-        return unless pipeline.failure_reason?
-
-        FAILURE_REASONS[pipeline.failure_reason.to_sym] ||
-          pipeline.failure_reason
-      end
     end
   end
 end
