577f2ceb22187b794e0abd4b8e3c863dd35cfe4b
James Lopez <james@jameslopez.es>
2017-10-02 15:46:54 +0200

    add EE keys service

---
+++ ee/app/services/ee/keys/create_service.rb
@@ -0,0 +1,26 @@
+module EE
+  module Keys
+    module CreateService
+      def initialize(user, params)
+        super(user, params)
+
+        @ip_address = params.delete(:ip_address)
+      end
+
+      def execute
+        super.tap do |key|
+          log_audit_event(key)
+        end
+      end
+
+      def log_audit_event(key)
+        ::AuditEventService.new(@user,
+                                @user,
+                                action: :custom,
+                                custom_message: 'Added SSH key',
+                                ip_address: @ip_address)
+            .for_user(key.title).security_event
+      end
+    end
+  end
+end
