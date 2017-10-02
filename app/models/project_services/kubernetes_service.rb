a1b3cd40647e8f7768b6db0bc64179e60f5d5937
Mircea Danila Dumitrescu <mircea@statustoday.com>
2017-10-02 20:32:36 +0000

    namespace should be lowercased in kubernetes. This is also true for the scenario where the namespace is generated from the project group-name.

--- app/models/project_services/kubernetes_service.rb
+++ app/models/project_services/kubernetes_service.rb
@@ -153,7 +153,17 @@ class KubernetesService < DeploymentService
   end
 
   def default_namespace
-    "#{project.path}-#{project.id}" if project.present?
+    return unless project
+
+    # 1. lowercase
+    # 2. replace non kubernetes characters with dash
+    # 3. trim dash from the beginning and end
+
+    slugified = "#{project.path}-#{project.id}"
+    slugified.downcase!
+    slugified.gsub!(/[^a-z0-9]/, '-')
+    slugified.gsub!(/^-+|-+$/, '')
+    slugified
   end
 
   def build_kubeclient!(api_path: 'api', api_version: 'v1')
