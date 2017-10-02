cddab09bc69d291924164b1b83fdf7cdbf422dfc
Filipa Lacerda <filipa@gitlab.com>
2017-10-02 12:41:46 +0100

    Starts edit form

---
+++ app/assets/javascripts/clusters.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  const toggleButton = document.querySelector('.js-toggle-cluster');
+  // disable in case of no permissions
+  toggleButton.addEventListener(() => {
+
+    // update text
+  });
+
+  // poll the status
+});
