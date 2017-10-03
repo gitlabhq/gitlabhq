be6ecb784234932edaa662ccc4e862840ba4e34c
Valery Sizov <valery@gitlab.com>
2017-10-03 12:36:34 +0300

    [Rails 5][ci skip] Fix spec/lib/gitlab/database/rename_reserved_paths_migration/v1/rename_namespaces_spec.rb

--- lib/gitlab/database/rename_reserved_paths_migration/v1/migration_classes.rb
+++ lib/gitlab/database/rename_reserved_paths_migration/v1/migration_classes.rb
@@ -37,6 +37,7 @@ module Gitlab
           class Namespace < ActiveRecord::Base
             include MigrationClasses::Routable
             self.table_name = 'namespaces'
+            self.inheritance_column = nil
             belongs_to :parent,
                        class_name: "#{MigrationClasses.name}::Namespace"
             has_one :route, as: :source
