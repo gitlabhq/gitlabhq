b638029978541758e53449a5138539d1340a445a
Bryce Johnson <bryce@gitlab.com>
2017-10-04 10:32:32 -0400

    Check in most recent feature spec.

--- spec/features/merge_requests/image_diff_notes.rb
+++ spec/features/merge_requests/image_diff_notes.rb
@@ -5,22 +5,6 @@ feature 'Diff note avatars', js: true do
 
   let(:user)          { create(:user) }
   let(:project)       { create(:project, :public, :repository) }
-  let(:merge_request) { create(:merge_request_with_diffs, :with_image_diffs, source_project: project, author: user, title: "Added images and changes") }
-  let(:path)          { "files/images/ee_repo_logo.png" }
-  let(:position) do
-    Gitlab::Diff::Position.new(
-      old_path: path,
-      new_path: path,
-      width: 100,
-      height: 100,
-      x_axis: 1,
-      y_axis: 1,
-      position_type: "image",
-      diff_refs: merge_request.diff_refs
-    )
-  end
-
-  let!(:note) { create(:diff_note_on_merge_request, project: project, noteable: merge_request, position: position) }
 
   before do
     project.team << [user, :master]
@@ -29,10 +13,12 @@ feature 'Diff note avatars', js: true do
     page.driver.set_cookie('sidebar_collapsed', 'true')
   end
 
-  context 'commit view' do
-    describe 'creating a new diff note' do
+  context 'commit view', focus: true do
+    commit_id = '2f63565e7aac07bcdadb654e253078b727143ec4'
+
+    describe 'create a new diff note' do
       before do
-        visit project_commit_path(project, '2f63565e7aac07bcdadb654e253078b727143ec4')
+        visit project_commit_path(project, commit_id)
         create_image_diff_note
       end
 
@@ -63,10 +49,24 @@ feature 'Diff note avatars', js: true do
     end
 
     describe 'render diff notes' do
-      commit_id = '2f63565e7aac07bcdadb654e253078b727143ec4'
+      let(:path)          { "files/images/6049019_460s.jpg" }
 
-      let!(:note2) { create(:note_on_commit, commit_id: commit_id, project: project, note: 'my note 2') }
-      let!(:note3) { create(:note_on_commit, commit_id: commit_id, project: project, note: 'my note 3') }
+      # move into MR context
+      let(:note2_position) do
+        Gitlab::Diff::Position.new(
+          old_path: path,
+          new_path: path,
+          width: 100,
+          height: 100,
+          x_axis: 1,
+          y_axis: 1,
+          position_type: "image",
+          diff_refs: commit_id
+        )
+      end
+
+      # Need to scope these notes to the diff
+      let!(:note2) { create(:note_on_commit, commit_id: commit_id, project: project, position: :note2_position, note: 'my note 2') }
 
       before do
         visit project_commit_path(project, commit_id)
@@ -82,17 +82,33 @@ feature 'Diff note avatars', js: true do
 
       it 'shows the diff notes with correct avatar badge numbers' do
         first_note_avatar = find('.image-diff-avatar-link', match: :first)
-        second_note_avatar = find('.image-diff-avatar-link', match: :second)
 
         expect(first_note_avatar).to have_content("1")
-        expect(second_note_avatar).to have_content("2")
       end
     end
   end
 
   %w(inline parallel).each do |view|
     context "#{view} view" do
-      describe 'creating a new diff note', focus: true do
+      let(:merge_request) { create(:merge_request_with_diffs, :with_image_diffs, source_project: project, author: user, title: "Added images and changes") }
+      let(:path)          { "files/images/ee_repo_logo.png" }
+
+      let(:position) do
+        Gitlab::Diff::Position.new(
+          old_path: path,
+          new_path: path,
+          width: 100,
+          height: 100,
+          x_axis: 1,
+          y_axis: 1,
+          position_type: "image",
+          diff_refs: merge_request.diff_refs
+        )
+      end
+
+      let!(:note) { create(:diff_note_on_merge_request, project: project, noteable: merge_request, position: position) }
+
+      describe 'creating a new diff note' do
         before do
           visit diffs_project_merge_request_path(project, merge_request, view: view)
           create_image_diff_note
@@ -138,25 +154,24 @@ feature 'Diff note avatars', js: true do
         it 'shows the diff notes with correct avatar badge numbers' do
         end
       end
+
+      context 'discussion tab' do
+        before do
+          visit project_merge_request_path(project, merge_request)
+        end
+
+        it 'shows the image diff frame' do
+          frame = find('.js-image-frame')
+        end
+
+        it 'shows the indicator on the frame' do
+        end
+
+        it 'shows the note with a generic comment icon' do
+        end
+      end
     end
   end
-
-  context 'discussion tab' do
-    before do
-      visit project_merge_request_path(project, merge_request)
-    end
-
-    it 'shows the image diff frame' do
-      frame = find('.js-image-frame')
-    end
-
-    it 'shows the indicator on the frame' do
-    end
-
-    it 'shows the note with a generic comment icon' do
-    end
-  end
-
 end
 
 def create_image_diff_note
