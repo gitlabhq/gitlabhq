e710e766b042ad0c74536416044ad0af2fdca012
Shinya Maeda <shinya@gitlab.com>
2017-10-05 01:55:33 +0900

    fetch_gcp_operation_service_spec. finalize_cluster_creation_service_spec. wait_for_cluster_creation_worker_spec.

--- spec/services/ci/fetch_gcp_operation_service_spec.rb
+++ spec/services/ci/fetch_gcp_operation_service_spec.rb
@@ -1,16 +1,35 @@
 require 'spec_helper'
+require 'google/apis'
 
 describe Ci::FetchGcpOperationService do
   describe '#execute' do
-    context 'when correct params' do
+    let(:cluster) { create(:gcp_cluster) }
+    let(:operation) { double }
+
+    context 'when suceeded' do
+      before do
+        allow_any_instance_of(GoogleApi::CloudPlatform::Client)
+          .to receive(:projects_zones_operations).and_return(operation)
+      end
+
       it 'fetch the gcp operaion' do
-        
+        expect { |b| described_class.new.execute(cluster, &b) }
+          .to yield_with_args(operation)
       end
     end
 
-    context 'when invalid params' do
-      it 'sets an error to cluster object' do
+    context 'when raises an error' do
+      let(:error) { Google::Apis::ServerError.new('a') }
 
+      before do
+        allow_any_instance_of(GoogleApi::CloudPlatform::Client)
+          .to receive(:projects_zones_operations).and_raise(error)
+      end
+
+      it 'sets an error to cluster object' do
+        expect { |b| described_class.new.execute(cluster, &b) }
+          .not_to yield_with_args
+        expect(cluster.reload).to be_errored
       end
     end
   end
--- spec/workers/wait_for_cluster_creation_worker_spec.rb
+++ spec/workers/wait_for_cluster_creation_worker_spec.rb
@@ -4,14 +4,54 @@ describe WaitForClusterCreationWorker do
   describe '#perform' do
     context 'when cluster exists' do
       let(:cluster) { create(:gcp_cluster) }
+      let(:operation) { double }
 
-      it 'fetches gcp operation status' do
-        expect_any_instance_of(Ci::FetchGcpOperationService).to receive(:execute)
-
-        described_class.new.perform(cluster.id)
+      before do
+        allow(operation).to receive(:status).and_return(status)
+        allow(operation).to receive(:start_time).and_return(1.minutes.ago)
+        allow(operation).to receive(:status_message).and_return('error')
+        allow_any_instance_of(Ci::FetchGcpOperationService).to receive(:execute).and_yield(operation)
       end
 
-      # TODO: context 'when operation.status is runnning'
+      context 'when operation status is RUNNING' do
+        let(:status) { 'RUNNING' }
+
+        it 'reschedules worker' do
+          expect(WaitForClusterCreationWorker).to receive(:perform_in)
+
+          described_class.new.perform(cluster.id)
+        end
+
+        context 'when operation timeout' do
+          before do
+            allow(operation).to receive(:start_time).and_return(30.minutes.ago)
+          end
+
+          it 'sets an error message on cluster' do
+            described_class.new.perform(cluster.id)
+            expect(cluster.reload).to be_errored
+          end
+        end
+      end
+
+      context 'when operation status is DONE' do
+        let(:status) { 'DONE' }
+
+        it 'finalizes cluster creation' do
+          expect_any_instance_of(Ci::FinalizeClusterCreationService).to receive(:execute)
+
+          described_class.new.perform(cluster.id)
+        end
+      end
+
+      context 'when operation status is others' do
+        let(:status) { 'others' }
+
+        it 'sets an error message on cluster' do
+          described_class.new.perform(cluster.id)
+          expect(cluster.reload).to be_errored
+        end
+      end
     end
 
     context 'when cluster does not exist' do
