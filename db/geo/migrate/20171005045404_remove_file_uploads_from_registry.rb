7379ac01489830e7f4845c8bb6923795e3c253ed
Stan Hu <stanhu@gmail.com>
2017-10-05 08:25:52 -0700

    Only prune non-LFS files

--- db/geo/migrate/20171005045404_remove_file_uploads_from_registry.rb
+++ db/geo/migrate/20171005045404_remove_file_uploads_from_registry.rb
@@ -1,7 +1,8 @@
 class RemoveFileUploadsFromRegistry < ActiveRecord::Migration
-  # Previous to GitLab 10.0, GitLab would save attachments/avatars to the wrong
-  # directory. Destroy these entries so they will be downloaded again.
-  def change
-    Geo::FileRegistry.destroy_all
+  # Previous to GitLab 10.1, GitLab would save attachments/avatars to the
+  # wrong directory (/var/opt/gitlab/gitlab-rails/working). Destroy these
+  # entries so they will be downloaded again.
+  def up
+    Geo::BaseRegistry.connection.execute("DELETE FROM file_registry WHERE file_type != 'lfs'")
   end
 end
