28f8f8fcb37a93fa87e2a896aacc2873cd7790f0
Felipe Artur <felipefac@gmail.com>
2017-10-05 21:21:27 -0300

    Add feature specs

--- spec/features/merge_requests/image_diff_notes.rb
+++ spec/features/merge_requests/image_diff_notes.rb
@@ -1,19 +1,23 @@
 require 'spec_helper'
 
-feature 'Diff note avatars', js: true do
+feature 'image diff notes', js: true do
   include NoteInteractionHelpers
 
-  let(:user)          { create(:user) }
-  let(:project)       { create(:project, :public, :repository) }
+  let(:user) { create(:user) }
+  let(:project) { create(:project, :public, :repository) }
 
   before do
     project.team << [user, :master]
     sign_in user
 
     page.driver.set_cookie('sidebar_collapsed', 'true')
+
+    # Stub helper to return any blob file as image from public app folder.
+    # This is necessary to run this specs since we don't display repo images in capybara.
+    allow_any_instance_of(DiffHelper).to receive(:diff_file_blob_raw_path).and_return('/apple-touch-icon.png')
   end
 
-  context 'commit view', focus: true do
+  context 'create commit diff notes', focus: true do
     commit_id = '2f63565e7aac07bcdadb654e253078b727143ec4'
 
     describe 'create a new diff note' do
@@ -34,42 +38,53 @@ feature 'Diff note avatars', js: true do
         expect(badge).to have_content('1')
       end
 
-      it 'allows collapsing the discussion notes' do
-        find('.js-diff-notes-toggle').click
+      it 'allows collapsing/expanding the discussion notes' do
+        find('.js-diff-notes-toggle', :first).click
 
         expect(page).not_to have_content('image diff test comment')
-      end
 
-      it 'allows expanding discussion notes' do
-        find('.js-diff-notes-toggle').click
         find('.js-diff-notes-toggle').click
 
         expect(page).to have_content('image diff test comment')
       end
     end
 
-    describe 'render diff notes' do
-      let(:path)          { "files/images/6049019_460s.jpg" }
+    describe 'render commit diff notes' do
+      let(:path) { "files/images/6049019_460s.jpg" }
+      let(:commit) { project.commit('2f63565e7aac07bcdadb654e253078b727143ec4') }
 
-      # move into MR context
+      let(:note1_position) do
+        Gitlab::Diff::Position.new(
+          old_path: path,
+          new_path: path,
+          width: 100,
+          height: 100,
+          x_axis: 10,
+          y_axis: 10,
+          position_type: "image",
+          diff_refs: commit.diff_refs
+        )
+      end
+
       let(:note2_position) do
         Gitlab::Diff::Position.new(
           old_path: path,
           new_path: path,
           width: 100,
           height: 100,
-          x_axis: 1,
-          y_axis: 1,
+          x_axis: 20,
+          y_axis: 20,
           position_type: "image",
-          diff_refs: commit_id
+          diff_refs: commit.diff_refs
         )
       end
 
-      # Need to scope these notes to the diff
-      let!(:note2) { create(:note_on_commit, commit_id: commit_id, project: project, position: :note2_position, note: 'my note 2') }
+      let!(:note1) { create(:diff_note_on_commit, commit_id: commit.id, project: project, position: note1_position, note: 'my note 1') }
+      let!(:note2) { create(:diff_note_on_commit, commit_id: commit.id, project: project, position: note2_position, note: 'my note 2') }
 
       before do
-        visit project_commit_path(project, commit_id)
+        visit project_commit_path(project, commit.id)
+        wait_for_requests
       end
 
       it 'render diff indicators within the image diff frame' do
@@ -81,16 +96,15 @@ feature 'Diff note avatars', js: true do
       end
 
       it 'shows the diff notes with correct avatar badge numbers' do
-        first_note_avatar = find('.image-diff-avatar-link', match: :first)
-
-        expect(first_note_avatar).to have_content("1")
+        expect(page).to have_css('.image-diff-avatar-link', text: 1)
+        expect(page).to have_css('.image-diff-avatar-link', text: 2)
       end
     end
   end
 
   %w(inline parallel).each do |view|
     context "#{view} view" do
-      let(:merge_request) { create(:merge_request_with_diffs, :with_image_diffs, source_project: project, author: user, title: "Added images and changes") }
+      let(:merge_request) { create(:merge_request_with_diffs, :with_image_diffs, source_project: project, author: user) }
       let(:path)          { "files/images/ee_repo_logo.png" }
 
       let(:position) do
@@ -126,56 +140,56 @@ feature 'Diff note avatars', js: true do
           expect(badge).to have_content('1')
         end
 
-        it 'allows collapsing the discussion notes' do
-          find('.js-diff-notes-toggle', match: :first).click
+        it 'allows expanding/collapsing the discussion notes' do
+          page.all('.js-diff-notes-toggle')[0].trigger('click')
+          page.all('.js-diff-notes-toggle')[1].trigger('click')
 
           expect(page).not_to have_content('image diff test comment')
-        end
 
-        it 'allows expanding discussion notes' do
-          find('.js-diff-notes-toggle', match: :first).click
-          find('.js-diff-notes-toggle', match: :first).click
+          page.all('.js-diff-notes-toggle')[0].trigger('click')
+          page.all('.js-diff-notes-toggle')[1].trigger('click')
 
           expect(page).to have_content('image diff test comment')
         end
       end
+    end
+  end
 
-      describe 'render diff notes' do
-        before do
-          # mock a couple separate comments on the image diff
-        end
+  describe 'discussion tab polling', :js do
+    let(:merge_request) { create(:merge_request_with_diffs, :with_image_diffs, source_project: project, author: user) }
+    let(:path)          { "files/images/ee_repo_logo.png" }
 
-        it 'render diff indicators within the image frame' do
-        end
+    let(:position) do
+      Gitlab::Diff::Position.new(
+        old_path: path,
+        new_path: path,
+        width: 100,
+        height: 100,
+        x_axis: 50,
+        y_axis: 50,
+        position_type: "image",
+        diff_refs: merge_request.diff_refs
+      )
+    end
 
-        it 'shows the diff notes' do
-        end
+    before do
+      visit project_merge_request_path(project, merge_request)
+    end
 
-        it 'shows the diff notes with correct avatar badge numbers' do
-        end
-      end
+    it 'render diff indicators within the image frame' do
+      diff_note = create(:diff_note_on_merge_request, project: project, noteable: merge_request, position: position)
 
-      context 'discussion tab' do
-        before do
-          visit project_merge_request_path(project, merge_request)
-        end
+      wait_for_requests
 
-        it 'shows the image diff frame' do
-          frame = find('.js-image-frame')
-        end
-
-        it 'shows the indicator on the frame' do
-        end
-
-        it 'shows the note with a generic comment icon' do
-        end
-      end
+      expect(page).to have_selector('.image-comment-badge')
+      expect(page).to have_content(diff_note.note)
     end
   end
 end
 
 def create_image_diff_note
-  find('.js-add-image-diff-note-button').click
+  find('.js-add-image-diff-note-button', match: :first).click
+  page.all('.js-add-image-diff-note-button')[0].trigger('click')
   find('.diff-content .note-textarea').native.send_keys('image diff test comment')
   click_button 'Comment'
   wait_for_requests
