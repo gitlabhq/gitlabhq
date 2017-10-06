ff5886c34cc1a14b5aeed22f13133456a418da64
Fatih Acet <acetfatih@gmail.com>
2017-10-06 22:56:09 +0300

    Fix karma specs and scss linter.

--- spec/javascripts/repo/services/repo_service_spec.js
+++ spec/javascripts/repo/services/repo_service_spec.js
@@ -8,78 +8,18 @@ describe('RepoService', () => {
     expect(RepoService.options.params.format).toBe('json');
   });
 
-  describe('buildParams', () => {
-    let newParams;
-    const url = 'url';
-
-    beforeEach(() => {
-      newParams = {};
-
-      spyOn(Object, 'assign').and.returnValue(newParams);
-    });
-
-    it('clones params', () => {
-      const params = RepoService.buildParams(url);
-
-      expect(Object.assign).toHaveBeenCalledWith({}, RepoService.options.params);
-
-      expect(params).toBe(newParams);
-    });
-
-    it('sets and returns viewer params to richif urlIsRichBlob is true', () => {
-      spyOn(RepoService, 'urlIsRichBlob').and.returnValue(true);
-
-      const params = RepoService.buildParams(url);
-
-      expect(params.viewer).toEqual('rich');
-    });
-
-    it('returns params urlIsRichBlob is false', () => {
-      spyOn(RepoService, 'urlIsRichBlob').and.returnValue(false);
-
-      const params = RepoService.buildParams(url);
-
-      expect(params.viewer).toBeUndefined();
-    });
-
-    it('calls urlIsRichBlob with the objects url prop if no url arg is provided', () => {
-      spyOn(RepoService, 'urlIsRichBlob');
-      RepoService.url = url;
-
-      RepoService.buildParams();
-
-      expect(RepoService.urlIsRichBlob).toHaveBeenCalledWith(url);
-    });
-  });
-
-  describe('urlIsRichBlob', () => {
-    it('returns true for md extension', () => {
-      const isRichBlob = RepoService.urlIsRichBlob('url.md');
-
-      expect(isRichBlob).toBeTruthy();
-    });
-
-    it('returns false for js extension', () => {
-      const isRichBlob = RepoService.urlIsRichBlob('url.js');
-
-      expect(isRichBlob).toBeFalsy();
-    });
-  });
-
   describe('getContent', () => {
-    const params = {};
+    const params = { format: 'json' };
     const url = 'url';
     const requestPromise = Promise.resolve();
 
     beforeEach(() => {
-      spyOn(RepoService, 'buildParams').and.returnValue(params);
       spyOn(axios, 'get').and.returnValue(requestPromise);
     });
 
-    it('calls buildParams and axios.get', () => {
+    it('axios.get', () => {
       const request = RepoService.getContent(url);
 
-      expect(RepoService.buildParams).toHaveBeenCalledWith(url);
       expect(axios.get).toHaveBeenCalledWith(url, {
         params,
       });
