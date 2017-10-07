4aa2deb4782f1f3b516bfaecdb57203765bbb123
Rubén Dávila <ruben@gitlab.com>
2017-10-07 10:47:53 -0500

    Fix error with GPG signature updater when commit was deleted

--- lib/gitlab/gpg/invalid_gpg_signature_updater.rb
+++ lib/gitlab/gpg/invalid_gpg_signature_updater.rb
@@ -10,7 +10,7 @@ module Gitlab
           .select(:id, :commit_sha, :project_id)
           .where('gpg_key_id IS NULL OR verification_status <> ?', GpgSignature.verification_statuses[:verified])
           .where(gpg_key_primary_keyid: @gpg_key.keyids)
-          .find_each { |sig| sig.gpg_commit.update_signature!(sig) }
+          .find_each { |sig| sig.gpg_commit&.update_signature!(sig) }
       end
     end
   end
