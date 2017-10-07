b57f75ad7649d9cf7dfbc56f0f6151709c292faa
Nick Thomas <nick@gitlab.com>
2017-10-07 17:19:31 +0100

    CE->EE: Fix a failing spec

--- spec/helpers/groups_helper_spec.rb
+++ spec/helpers/groups_helper_spec.rb
@@ -36,7 +36,7 @@ describe GroupsHelper do
 
       it 'gives default avatar_icon when no avatar is present' do
         group.save!
-        expect(group_icon(group.path)).to match('group_avatar.png')
+        expect(group_icon(group.path)).to match_asset_path('group_avatar.png')
       end
     end
   end
