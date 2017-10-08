dfbbc9a15e1c1f6723baec87e8c2eb75bf623e79
Vitaliy @blackst0ne Klachkov <blackst0ne.ru@gmail.com>
2017-10-08 13:39:35 +1100

    [Rails 5] Fix db/migrate/20170913131410_environments_project_id_not_null.rb

--- db/migrate/20170913131410_environments_project_id_not_null.rb
+++ db/migrate/20170913131410_environments_project_id_not_null.rb
@@ -7,10 +7,35 @@ class EnvironmentsProjectIdNotNull < ActiveRecord::Migration
   DOWNTIME = false
 
   def up
+    remove_foreign_key_for_mysql
+
     change_column_null :environments, :project_id, false
+
+    add_foreign_key_for_mysql
   end
 
   def down
+    remove_foreign_key_for_mysql
+
     change_column_null :environments, :project_id, true
+
+    add_foreign_key_for_mysql
+  end
+
+  private
+
+  # Create the foreign key explicitly for MySQL.
+  def add_foreign_key_for_mysql
+    if Gitlab::Database.mysql? && !foreign_key_exists?(:environments, :projects)
+      add_foreign_key :environments, :projects, on_delete: :cascade
+    end
+  end
+
+  # Drop the foreign key explicitly for MySQL.
+  def remove_foreign_key_for_mysql
+    if Gitlab::Database.mysql? && foreign_key_exists?(:environments, :projects)
+      remove_foreign_key :environments, :projects
+    end
   end
 end
+
