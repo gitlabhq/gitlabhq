b245f67c421087d8b2d0aaab2aa2ef1ec8cd0d70
Alessio Caiazza <acaiazza@gitlab.com>
2017-10-09 16:16:45 +0200

    Resolve CI/CD build related merge conflicts

--- spec/models/ci/build_spec.rb
+++ spec/models/ci/build_spec.rb
@@ -18,11 +18,8 @@ describe Ci::Build do
   it { is_expected.to belong_to(:trigger_request) }
   it { is_expected.to belong_to(:erased_by) }
   it { is_expected.to have_many(:deployments) }
-<<<<<<< HEAD
   it { is_expected.to have_many(:sourced_pipelines) }
-=======
   it { is_expected.to have_many(:trace_sections)}
->>>>>>> ce-com/master
   it { is_expected.to validate_presence_of(:ref) }
   it { is_expected.to respond_to(:has_trace?) }
   it { is_expected.to respond_to(:trace) }
