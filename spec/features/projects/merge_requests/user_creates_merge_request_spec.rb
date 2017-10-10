cfe12a0df32ac63fde710d558b29bdd8de9e9177
blackst0ne <blackst0ne.ru@gmail.com>
2017-10-10 13:11:54 +1100

    Resolve conflicts in spec/features/projects/merge_requests/user_creates_merge_request_spec.rb

--- spec/features/projects/merge_requests/user_creates_merge_request_spec.rb
+++ spec/features/projects/merge_requests/user_creates_merge_request_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe 'User creates a merge request', :js do
-<<<<<<< HEAD
   let(:project) do
     create(:project,
       :repository,
@@ -10,29 +9,19 @@ describe 'User creates a merge request', :js do
   end
   let(:user) { create(:user) }
   let(:user2) { create(:user) }
-=======
-  let(:project) { create(:project, :repository) }
-  let(:user) { create(:user) }
->>>>>>> ce-com/master
 
   before do
     project.add_master(user)
     sign_in(user)
 
-<<<<<<< HEAD
     project.approvers.create(user_id: user.id)
 
-=======
->>>>>>> ce-com/master
     visit(project_new_merge_request_path(project))
   end
 
   it 'creates a merge request' do
-<<<<<<< HEAD
     allow_any_instance_of(Gitlab::AuthorityAnalyzer).to receive(:calculate).and_return([user2])
 
-=======
->>>>>>> ce-com/master
     find('.js-source-branch').click
     click_link('fix')
 
@@ -41,7 +30,6 @@ describe 'User creates a merge request', :js do
 
     click_button('Compare branches')
 
-<<<<<<< HEAD
     expect(find_field('merge_request_description').value).to eq('This merge request should contain the following.')
 
     # Approvers
@@ -60,17 +48,12 @@ describe 'User creates a merge request', :js do
     end
     # End of approvers
 
-=======
->>>>>>> ce-com/master
     fill_in('merge_request_title', with: 'Wiki Feature')
     click_button('Submit merge request')
 
     page.within('.merge-request') do
       expect(page).to have_content('Wiki Feature')
     end
-
-<<<<<<< HEAD
-    # wait_for_requests
 
     page.within('.issuable-actions') do
       click_link('Edit', match: :first)
@@ -79,8 +62,5 @@ describe 'User creates a merge request', :js do
     page.within('ul.approver-list') do
       expect(page).to have_content(user2.name)
     end
-=======
-    wait_for_requests
->>>>>>> ce-com/master
   end
 end
