85b915c39b8327afdfec5444c732f1b119081e0e
Mike Greiling <mike@pixelcog.com>
2017-10-10 15:43:05 -0500

    fix click trigger method

--- spec/support/features/discussion_comments_shared_example.rb
+++ spec/support/features/discussion_comments_shared_example.rb
@@ -71,7 +71,7 @@ shared_examples 'discussion comments' do |resource_name|
       expect(page).not_to have_selector menu_selector
 
       find(toggle_selector).click
-      find('body').trigger 'click'
+      find('body').click
 
       expect(page).not_to have_selector menu_selector
     end
