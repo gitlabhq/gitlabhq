b8adcff34512da0488a05339ad36b701c04de472
Nick Thomas <nick@gitlab.com>
2017-10-11 15:12:04 +0100

    CE->EE: fix conflicts in spec/helpers/groups_helper_spec.rb

--- spec/helpers/groups_helper_spec.rb
+++ spec/helpers/groups_helper_spec.rb
@@ -3,16 +3,13 @@ require 'spec_helper'
 describe GroupsHelper do
   include ApplicationHelper
 
-<<<<<<< HEAD
-  let(:group) { create(:group) }
-=======
   let(:asset_host) { 'http://assets' }
->>>>>>> upstream/master
 
   describe 'group_icon' do
     avatar_file_path = File.join(Rails.root, 'spec', 'fixtures', 'banana_sample.gif')
 
     it 'returns an url for the avatar' do
+      group = create(:group)
       group.avatar = fixture_file_upload(avatar_file_path)
       group.save!
 
@@ -59,11 +56,13 @@ describe GroupsHelper do
     end
 
     it 'gives default avatar_icon when no avatar is present' do
+      group = create(:group)
       group.save!
       expect(group_icon_url(group.path)).to match_asset_path('group_avatar.png')
     end
 
     context 'in a geo secondary node' do
+      let(:group) { create(:group) }
       let(:geo_url) { 'http://geo.example.com' }
 
       before do
@@ -79,7 +78,7 @@ describe GroupsHelper do
 
       it 'gives default avatar_icon when no avatar is present' do
         group.save!
-        expect(group_icon(group.path)).to match_asset_path('group_avatar.png')
+        expect(group_icon(group.path)).to match_asset_path('no_group_avatar.png')
       end
     end
   end
