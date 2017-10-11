bd0248307dae9c62f62177ef7d5f87ec0a7acf22
Annabel Dunstone Gray <annabel.dunstone@gmail.com>
2017-10-11 09:07:19 +0000

    Simplify project page

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -12,8 +12,9 @@ feature 'Project' do
       visit new_project_path
     end
 
-    it "allows creation from templates" do
-      page.choose(template.name)
+    it "allows creation from templates", :js do
+      find('#create-from-template-tab').trigger('click')
+      find("##{template.name}").trigger('click')
       fill_in("project_path", with: template.name)
 
       page.within '#content-body' do
