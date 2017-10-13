d6044a10f1d173e04fd2f4d69f6ebc1df34630be
Richard Clamp <richardc@unixbeard.net>
2017-10-13 15:03:13 +0100

    Fixup company name

--- qa/qa/scenario/entrypoint.rb
+++ qa/qa/scenario/entrypoint.rb
@@ -1,7 +1,7 @@
 module QA
   module Scenario
     ##
-    # Run test suite against any Gitlab instance,
+    # Run test suite against any GitLab instance,
     # including staging and on-premises installation.
     #
     class Entrypoint < Template
