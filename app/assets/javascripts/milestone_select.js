ff5519561b12c50922b4e90eddc6b5e3f8fc23af
Lin Jen-Shin <godfat@godfat.org>
2017-10-13 19:29:40 +0800

    Resolve app/assets/javascripts/milestone_select.js

--- app/assets/javascripts/milestone_select.js
+++ app/assets/javascripts/milestone_select.js
@@ -169,10 +169,11 @@ import _ from 'underscore';
             let selected = options.selectedObj;
 
             var data, isIssueIndex, isMRIndex, isSelecting, page, boardsStore;
-<<<<<<< HEAD
+
+            // EE begins
             if (!selected) return;
-=======
->>>>>>> upstream/master
+            // EE ends
+
             page = $('body').attr('data-page');
             isIssueIndex = page === 'projects:issues:index';
             isMRIndex = (page === page && page === 'projects:merge_requests:index');
