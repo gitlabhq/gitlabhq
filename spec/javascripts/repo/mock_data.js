a11410a82d2323cb4834ac5ed405a92245788dac
Phil Hughes <me@iamphill.com>
2017-10-16 15:13:43 +0100

    fixed karma build because of a removed method

--- spec/javascripts/repo/mock_data.js
+++ spec/javascripts/repo/mock_data.js
@@ -1,7 +1,7 @@
 import RepoHelper from '~/repo/helpers/repo_helper';
 
 // eslint-disable-next-line import/prefer-default-export
-export const file = () => RepoHelper.serializeBlob({
+export const file = () => RepoHelper.serializeRepoEntity('blob', {
   icon: 'icon',
   url: 'url',
   name: 'name',
