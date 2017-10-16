99ed544e8be057b644bb739a81a098192a6cf886
Douwe Maan <douwe@gitlab.com>
2017-10-16 15:24:51 +0000

    Merge branch 'fix_global_board_routes_39073' into 'master'
    Remove "boards" from TOP_LEVEL_ROUTES
    
    Closes #39073
    
    See merge request gitlab-org/gitlab-ce!14861
    # Conflicts:
    #	config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -44,6 +44,19 @@ Rails.application.routes.draw do
     get 'readiness' => 'health#readiness'
     resources :metrics, only: [:index]
     mount Peek::Railtie => '/peek'
+
+    # Boards resources shared between group and projects
+    resources :boards, only: [] do
+      resources :lists, module: :boards, only: [:index, :create, :update, :destroy] do
+        collection do
+          post :generate
+        end
+
+        resources :issues, only: [:index, :create, :update]
+      end
+
+      resources :issues, module: :boards, only: [:index, :update]
+    end
   end
 
   # Koding route
@@ -87,6 +100,7 @@ Rails.application.routes.draw do
     resources :issues, module: :boards, only: [:index, :update]
   end
 
+  draw :google_api
   draw :import
   draw :uploads
   draw :explore
