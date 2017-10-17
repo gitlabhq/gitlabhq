35ec9ab0cd802812975c6f4bca7e129065117720
Jacob Vosmaer <jacob@gitlab.com>
2017-10-17 14:37:20 +0200

    Mess with bundler, what could go wrong

--- scripts/prepare_build.sh
+++ scripts/prepare_build.sh
@@ -2,7 +2,8 @@
 
 export SETUP_DB=${SETUP_DB:-true}
 export USE_BUNDLE_INSTALL=${USE_BUNDLE_INSTALL:-true}
-export BUNDLE_INSTALL_FLAGS="--without production --jobs $(nproc) --path vendor --retry 3 --quiet"
+export BUNDLE_PATH=vendor
+export BUNDLE_INSTALL_FLAGS="--without production --jobs $(nproc) --retry 3 --quiet"
 
 if [ "$USE_BUNDLE_INSTALL" != "false" ]; then
     bundle install --clean $BUNDLE_INSTALL_FLAGS && bundle check
