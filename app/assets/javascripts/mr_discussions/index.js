8093aad129c8edc97f1aadc18a613194fc63ae97
Fatih Acet <acetfatih@gmail.com>
2017-10-19 01:16:21 +0300

    Initially render MR discussions with using issue notes app. [ci skip]

--- app/assets/javascripts/mr_discussions/index.js
+++ app/assets/javascripts/mr_discussions/index.js
@@ -1,1 +1,34 @@
-console.log('HELLO WORLD');
+import Vue from 'vue';
+import issueNotesApp from '../notes/components/issue_notes_app.vue';
+
+document.addEventListener('DOMContentLoaded', () => new Vue({
+  el: '#js-vue-mr-discussions',
+  components: {
+    issueNotesApp,
+  },
+  data() {
+    const notesDataset = document.getElementById('js-vue-mr-discussions').dataset;
+    return {
+      issueData: JSON.parse(notesDataset.issueData),
+      currentUserData: JSON.parse(notesDataset.currentUserData),
+      notesData: {
+        lastFetchedAt: notesDataset.lastFetchedAt,
+        discussionsPath: notesDataset.discussionsPath,
+        newSessionPath: notesDataset.newSessionPath,
+        registerPath: notesDataset.registerPath,
+        notesPath: notesDataset.notesPath,
+        markdownDocsPath: notesDataset.markdownDocsPath,
+        quickActionsDocsPath: notesDataset.quickActionsDocsPath,
+      },
+    };
+  },
+  render(createElement) {
+    return createElement('issue-notes-app', {
+      props: {
+        issueData: this.issueData,
+        notesData: this.notesData,
+        userData: this.currentUserData,
+      },
+    });
+  },
+}));
