02a42f6d5303c4e94bfe44d5775b36e90d41e83a
Oswaldo Ferreira <oswaldo@gitlab.com>
2017-10-19 22:30:29 +0300

    Remove unnecessary &. board checks

--- ee/app/helpers/ee/boards_helper.rb
+++ ee/app/helpers/ee/boards_helper.rb
@@ -21,11 +21,11 @@ module EE
 
       data = {
         board_milestone_title: milestone_title,
-        board_milestone_id: board&.milestone_id,
-        board_assignee_username: board&.assignee&.username,
-        label_ids: board&.label_ids,
-        labels: board&.labels.to_json(only: [:id, :title, :color, :text_color] ),
-        board_weight: board&.weight,
+        board_milestone_id: board.milestone_id,
+        board_assignee_username: board.assignee&.username,
+        label_ids: board.label_ids,
+        labels: board.labels.to_json(only: [:id, :title, :color, :text_color] ),
+        board_weight: board.weight,
         focus_mode_available: parent.feature_available?(:issue_board_focus_mode).to_s,
         show_promotion: show_feature_promotion
       }
