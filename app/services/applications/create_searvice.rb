11e5218ceb7b4179ac11f941e138ff8081edfe45
James Lopez <james@jameslopez.es>
2017-10-24 10:34:04 +0300

    fix spec

--- app/services/applications/create_searvice.rb
+++ app/services/applications/create_searvice.rb
@@ -1,7 +1,5 @@
 module Applications
   class CreateService
-    prepend ::EE::Applications::CreateService
-
     def initialize(current_user, params)
       @current_user = current_user
       @params = params
