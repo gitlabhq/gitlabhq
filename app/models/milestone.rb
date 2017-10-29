f39fe4d8db2d7fc6443c68aefff60f3eafcdf602
George Andrinopoulos <geoandri@gmail.com>
2017-10-29 22:58:49 +0200

    Add total time spent to milestones

--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -213,6 +213,14 @@ class Milestone < ActiveRecord::Base
     project_id.present?
   end
 
+  def total_time_spent
+    issues.joins(:timelogs).sum(:time_spent) + merge_requests.joins(:timelogs).sum(:time_spent)
+  end
+
+  def human_total_time_spent
+    Gitlab::TimeTrackingFormatter.output(total_time_spent) || 0
+  end
+
   private
 
   # Milestone titles must be unique across project milestones and group milestones
