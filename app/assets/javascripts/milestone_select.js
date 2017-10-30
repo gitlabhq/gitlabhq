60f8ee0670131214a24bbeed42f770a15334c88e
Simon Knox <psimyn@gmail.com>
2017-10-30 15:32:02 +0200

    resolve milestone_select conflict

--- app/assets/javascripts/milestone_select.js
+++ app/assets/javascripts/milestone_select.js
@@ -148,7 +148,6 @@ import _ from 'underscore';
             $(`[data-milestone-id="${selectedMilestone}"] > a`, $el).addClass('is-active');
           },
           vue: $dropdown.hasClass('js-issue-board-sidebar'),
-<<<<<<< HEAD
           hideRow: function(milestone) {
             if ($('html').hasClass('issue-boards-page') && !$dropdown.hasClass('js-issue-board-sidebar') &&
               !$dropdown.closest('.add-issues-modal').length && gl.issueBoards.BoardsStore.state.currentBoard.milestone) {
@@ -165,14 +164,9 @@ import _ from 'underscore';
 
             return true;
           },
-          clicked: function(options) {
-            const { $el, e } = options;
-            let selected = options.selectedObj;
-=======
           clicked: function(clickEvent) {
             const { $el, e } = clickEvent;
             let selected = clickEvent.selectedObj;
->>>>>>> ce-com/master
 
             var data, isIssueIndex, isMRIndex, isSelecting, page, boardsStore;
             if (!selected) return;
