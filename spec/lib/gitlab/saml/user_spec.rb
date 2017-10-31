00b8bd761c6de207f27be58df6e3e3c19573b4cc
Sean McGivern <sean@mcgivern.me.uk>
2017-10-31 17:36:51 +0000

    Merge branch 'dm-ldap-identity-normalize-dn' into 'master'
    Normalize LDAP DN when looking up identity
    
    Closes #39559
    
    See merge request gitlab-org/gitlab-ce!15103

--- spec/lib/gitlab/saml/user_spec.rb
+++ spec/lib/gitlab/saml/user_spec.rb
@@ -6,7 +6,7 @@ describe Gitlab::Saml::User do
   let(:saml_user) { described_class.new(auth_hash) }
   let(:gl_user) { saml_user.gl_user }
   let(:uid) { 'my-uid' }
-  let(:dn) { 'uid=user1,ou=People,dc=example' }
+  let(:dn) { 'uid=user1,ou=people,dc=example' }
   let(:provider) { 'saml' }
   let(:auth_hash) { OmniAuth::AuthHash.new(uid: uid, provider: provider, info: info_hash, extra: { raw_info: OneLogin::RubySaml::Attributes.new({ 'groups' => %w(Developers Freelancers Designers) }) }) }
   let(:info_hash) do
