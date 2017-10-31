95c296713388214e5bea0ec608688d7fbc4c242b
James Lopez <james@jameslopez.es>
2017-10-31 09:39:16 +0100

    fix spec failure

--- spec/services/applications/create_service_spec.rb
+++ spec/services/applications/create_service_spec.rb
@@ -3,16 +3,17 @@ require 'spec_helper'
 describe ::Applications::CreateService do
   let(:user) { create(:user) }
   let(:params) { attributes_for(:application) }
+  let(:request) { ActionController::TestRequest.new(remote_ip: '127.0.0.1') }
 
   subject { described_class.new(user, params) }
 
   it 'creates an application' do
-    expect { subject.execute }.to change { Doorkeeper::Application.count }.by(1)
+    expect { subject.execute(request) }.to change { Doorkeeper::Application.count }.by(1)
   end
 
   it 'creates an audit log' do
     stub_licensed_features(extended_audit_events: true)
 
-    expect { subject.execute }.to change { SecurityEvent.count }.by(1)
+    expect { subject.execute(request) }.to change { SecurityEvent.count }.by(1)
   end
 end
