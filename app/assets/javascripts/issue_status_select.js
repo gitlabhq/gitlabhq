9afa9b8d76434d6bb21b7ab87b4d22fe55ced6e5
Filipa Lacerda <filipa@gitlab.com>
2017-10-31 11:27:50 +0000

    Enable eslint

--- app/assets/javascripts/issue_status_select.js
+++ app/assets/javascripts/issue_status_select.js
@@ -1,25 +1,22 @@
-/* eslint-disable func-names,wrap-iife, no-shadow, no-unused-vars, one-var */
 export default function issueStatusSelect() {
-  $('.js-issue-status').each(function (i, el) {
+  $('.js-issue-status').each((i, el) => {
     const fieldName = $(el).data('field-name');
     return $(el).glDropdown({
       selectable: true,
       fieldName,
-      toggleLabel: (function (_this) {
-        return function (selected, el, instance) {
-          let label = 'Author';
-          const $item = instance.dropdown.find('.is-active');
-          if ($item.length) {
-            label = $item.text();
-          }
-          return label;
-        };
-      })(this),
+      toggleLabel(selected, element, instance) {
+        let label = 'Author';
+        const $item = instance.dropdown.find('.is-active');
+        if ($item.length) {
+          label = $item.text();
+        }
+        return label;
+      },
       clicked(options) {
         return options.e.preventDefault();
       },
-      id(obj, el) {
-        return $(el).data('id');
+      id(obj, element) {
+        return $(element).data('id');
       },
     });
   });
