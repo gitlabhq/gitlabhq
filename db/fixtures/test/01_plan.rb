addb45c378769b827b79076b60bed87220de2015
Douwe Maan <douwe@selenight.nl>
2017-11-01 15:23:10 +0100

    Refactor Namespace#feature_available_in_plan?

--- db/fixtures/test/01_plan.rb
+++ db/fixtures/test/01_plan.rb
@@ -3,6 +3,6 @@ require './spec/support/sidekiq'
 Plan.create!(name: EE::Namespace::FREE_PLAN,
              title: EE::Namespace::FREE_PLAN.titleize)
 
-EE::Namespace::EE_PLANS.each_key do |plan|
+EE::Namespace::NAMESPACE_PLANS_TO_LICENSE_PLANS.each_key do |plan|
   Plan.create!(name: plan, title: plan.titleize)
 end
