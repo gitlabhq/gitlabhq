8b4eaf601d139a0266bda3492820513381e218c5
Mike Greiling <mike@pixelcog.com>
2017-11-02 17:31:49 -0500

    fix reference to hidden element

--- spec/features/issues/filtered_search/filter_issues_spec.rb
+++ spec/features/issues/filtered_search/filter_issues_spec.rb
@@ -520,7 +520,7 @@ describe 'Filter issues', :js do
 
     it 'updates atom feed link for group issues' do
       visit issues_group_path(group, milestone_title: milestone.title, assignee_id: user.id)
-      link = find('.nav-controls a', text: 'Subscribe')
+      link = find('.nav-controls a', text: 'Subscribe', visible: false)
       params = CGI.parse(URI.parse(link[:href]).query)
       auto_discovery_link = find('link[type="application/atom+xml"]', visible: false)
       auto_discovery_params = CGI.parse(URI.parse(auto_discovery_link[:href]).query)
