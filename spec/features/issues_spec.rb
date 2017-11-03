442248104c979afaa9b23512141602a9f77da871
Mike Greiling <mike@pixelcog.com>
2017-11-03 11:03:22 -0500

    prevent race condition when attempting to cancel a file upload

--- spec/features/issues_spec.rb
+++ spec/features/issues_spec.rb
@@ -585,9 +585,11 @@ describe 'Issues' do
       end
 
       it "cancels a file upload correctly" do
-        dropzone_file([Rails.root.join('spec', 'fixtures', 'dk.png')], 0, false)
+        slow_requests do
+          dropzone_file([Rails.root.join('spec', 'fixtures', 'dk.png')], 0, false)
 
-        click_button 'Cancel'
+          click_button 'Cancel'
+        end
 
         expect(page).to have_button('Attach a file')
         expect(page).not_to have_button('Cancel')
