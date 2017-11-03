a10925e1c37e7dab19de346c553311adfaccb86c
Douwe Maan <douwe@selenight.nl>
2017-11-03 19:48:15 +0100

    Reallow project paths ending in periods

--- lib/constraints/group_url_constrainer.rb
+++ lib/constraints/group_url_constrainer.rb
@@ -2,7 +2,7 @@ class GroupUrlConstrainer
   def matches?(request)
     full_path = request.params[:group_id] || request.params[:id]
 
-    return false unless DynamicPathValidator.valid_group_path?(full_path)
+    return false unless NamespacePathValidator.valid_path?(full_path)
 
     Group.find_by_full_path(full_path, follow_redirects: request.get?).present?
   end
