ca049902dc7dad6e6177b05c8e3dc74c00487d27
James Edwards-Jones <jedwardsjones@gitlab.com>
2017-11-03 14:33:06 +0000

    Gitlab::Git::RevList and LfsChanges use lazy popen

--- spec/lib/gitlab/git/rev_list_spec.rb
+++ spec/lib/gitlab/git/rev_list_spec.rb
@@ -3,26 +3,44 @@ require 'spec_helper'
 describe Gitlab::Git::RevList do
   let(:project) { create(:project, :repository) }
   let(:rev_list) { described_class.new(newrev: 'newrev', path_to_repo: project.repository.path_to_repo) }
-
-  before do
-    allow(Gitlab::Git::Env).to receive(:all).and_return({
-      GIT_OBJECT_DIRECTORY: 'foo',
-      GIT_ALTERNATE_OBJECT_DIRECTORIES: 'bar'
-    })
-  end
-
-  def stub_popen_rev_list(*additional_args, output:)
-    expect(rev_list).to receive(:popen).with([
-      Gitlab.config.git.bin_path,
-      "--git-dir=#{project.repository.path_to_repo}",
-      'rev-list',
-      *additional_args
-    ],
-    nil,
+  let(:env_hash) do
     {
       'GIT_OBJECT_DIRECTORY' => 'foo',
       'GIT_ALTERNATE_OBJECT_DIRECTORIES' => 'bar'
-    }).and_return([output, 0])
+    }
+  end
+
+  before do
+    allow(Gitlab::Git::Env).to receive(:all).and_return(env_hash.symbolize_keys)
+  end
+
+  def args_for_popen(args_list)
+    [
+      Gitlab.config.git.bin_path,
+      "--git-dir=#{project.repository.path_to_repo}",
+      'rev-list',
+      *args_list
+    ]
+  end
+
+  def stub_popen_rev_list(*additional_args, output:)
+    args = args_for_popen(additional_args)
+
+    expect(rev_list).to receive(:popen).with(args, nil, env_hash)
+                                       .and_return([output, 0])
+  end
+
+  def stub_lazy_popen_rev_list(*additional_args, output:)
+    params = [
+      args_for_popen(additional_args),
+      nil,
+      env_hash,
+      hash_including(lazy_block: anything)
+    ]
+
+    expect(rev_list).to receive(:popen).with(*params) do |*_, lazy_block:|
+      lazy_block.call(output.split("\n").lazy)
+    end
   end
 
   context "#new_refs" do
@@ -46,10 +64,22 @@ describe Gitlab::Git::RevList do
       expect(rev_list.new_objects(require_path: true)).to eq(%w[sha2])
     end
 
-    it 'can return a lazy enumerator' do
-      stub_popen_rev_list('newrev', '--not', '--all', '--objects', output: "sha1\nsha2")
+    it 'can yield a lazy enumerator' do
+      stub_lazy_popen_rev_list('newrev', '--not', '--all', '--objects', output: "sha1\nsha2")
 
-      expect(rev_list.new_objects(lazy: true)).to be_a Enumerator::Lazy
+      rev_list.new_objects do |object_ids|
+        expect(object_ids).to be_a Enumerator::Lazy
+      end
+    end
+
+    it 'returns the result of the block when given' do
+      stub_lazy_popen_rev_list('newrev', '--not', '--all', '--objects', output: "sha1\nsha2")
+
+      objects = rev_list.new_objects do |object_ids|
+        object_ids.first
+      end
+
+      expect(objects).to eq 'sha1'
     end
 
     it 'can accept list of references to exclude' do
@@ -69,7 +99,7 @@ describe Gitlab::Git::RevList do
     it 'fetches list of all pushed objects using rev-list' do
       stub_popen_rev_list('--all', '--objects', output: "sha1\nsha2")
 
-      expect(rev_list.all_objects.force).to eq(%w[sha1 sha2])
+      expect(rev_list.all_objects).to eq(%w[sha1 sha2])
     end
   end
 
