70b8f421ae6587d2a57db40e7487bf2dd788cbde
Mayra Cabrera <mcabrera@gitlab.com>
2017-11-04 14:51:26 -0400

    Modifies commit branches section
    - Display the default branch (if the limit is not exceeded)
    - Requires '...' to be clicked before showing the rest of the branches and/or tags
    - Fixes haml lints

--- spec/views/projects/commit/branches.html.haml_spec.rb
+++ spec/views/projects/commit/branches.html.haml_spec.rb
@@ -7,7 +7,7 @@ describe 'projects/commit/branches.html.haml' do
     assign(:project, project)
   end
 
-  context 'branches and tags' do
+  context 'when branches and tags are available' do
     before do
       assign(:branches, ['master', 'test-branch'])
       assign(:branches_limit_exceeded, false)
@@ -17,21 +17,88 @@ describe 'projects/commit/branches.html.haml' do
       render
     end
 
-    it 'shows branch and tag links' do
+    it 'shows default branch' do
       expect(rendered).to have_link('master')
+    end
+
+    it 'shows js expand link' do
+      expect(rendered).to have_selector('.js-details-expand')
+    end
+
+    it 'shows branch and tag links' do
       expect(rendered).to have_link('test-branch')
       expect(rendered).to have_link('tag1')
     end
   end
 
-  context 'throttled branches and tags' do
+  context 'when branches are available but no tags' do
     before do
-      assign(:branches, [])
+      assign(:branches, ['master', 'test-branch'])
+      assign(:branches_limit_exceeded, false)
+      assign(:tags, [])
+      assign(:tags_limit_exceeded, true)
+
+      render
+    end
+
+    it 'shows branches' do
+      expect(rendered).to have_link('master')
+      expect(rendered).to have_link('test-branch')
+    end
+
+    it 'shows js expand link' do
+      expect(rendered).to have_selector('.js-details-expand')
+    end
+
+    it 'shows limit exceeded message for tags' do
+      expect(rendered).to have_text('Tags unavailable')
+    end
+  end
+
+  context 'when tags are available but no branches (just default)' do
+    before do
+      assign(:branches, ['master'])
+      assign(:branches_limit_exceeded, true)
+      assign(:tags, ['tag1', 'tag2'])
+      assign(:tags_limit_exceeded, false)
+
+      render
+    end
+
+    it 'shows default branch' do
+      expect(rendered).to have_text('master')
+    end
+
+    it 'shows js expand link' do
+      expect(rendered).to have_selector('.js-details-expand')
+    end
+
+    it 'shows tags' do
+      expect(rendered).to have_link('tag1')
+      expect(rendered).to have_link('tag2')
+    end
+
+    it 'shows limit exceeded for branches' do
+      expect(rendered).to have_text('Branches unavailable')
+    end
+  end
+
+  context 'when branches and tags are not available' do
+    before do
+      assign(:branches, ['master'])
       assign(:branches_limit_exceeded, true)
       assign(:tags, [])
       assign(:tags_limit_exceeded, true)
 
       render
+    end
+
+    it 'shows default branch' do
+      expect(rendered).to have_text('master')
+    end
+
+    it 'shows js expand link' do
+      expect(rendered).to have_selector('.js-details-expand')
     end
 
     it 'shows too many to search' do
