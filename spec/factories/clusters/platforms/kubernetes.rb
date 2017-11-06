a99ad59e655d66fda8af7f2b89aced79b8bc1060
Shinya Maeda <shinya@gitlab.com>
2017-11-06 23:06:10 +0900

    Remove 10.3 comments (Tracked by a tech debts issue). Refactor spec factory name. Use ArgumentError

--- spec/factories/clusters/platforms/kubernetes.rb
+++ spec/factories/clusters/platforms/kubernetes.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do
-  factory :platform_kubernetes, class: Clusters::Platforms::Kubernetes do
+  factory :cluster_platform_kubernetes, class: Clusters::Platforms::Kubernetes do
     cluster
     namespace nil
     api_url 'https://kubernetes.example.com'
