1756604f90588a746ce6df7e4386830db9b3a485
Zeger-Jan van de Weg <git@zjvandeweg.nl>
2017-11-07 10:18:32 +0100

    JobArtifactsUploader does not inherrit from ArtifactsUploader

--- app/uploaders/job_artifact_uploader.rb
+++ app/uploaders/job_artifact_uploader.rb
@@ -1,4 +1,14 @@
-class JobArtifactUploader < ArtifactUploader
+class JobArtifactUploader < GitlabUploader
+  storage :file
+
+  def self.local_artifacts_store
+    Gitlab.config.artifacts.path
+  end
+
+  def self.artifacts_upload_path
+    File.join(self.local_artifacts_store, 'tmp/uploads/')
+  end
+
   def initialize(artifact, _field)
     @artifact = artifact
   end
@@ -9,16 +19,20 @@ class JobArtifactUploader < ArtifactUploader
     @artifact.size
   end
 
-  private
-
-  def disk_hash
-    @disk_hash ||= Digest::SHA2.hexdigest(@artifact.project_id.to_s)
+  def store_dir
+    File.join(self.class.local_artifacts_store, default_path)
   end
+
+  private
 
   def default_path
     creation_date = @artifact.created_at.utc.strftime('%Y_%m_%d')
 
     File.join(disk_hash[0..1], disk_hash[2..3], disk_hash,
-              creation_date, @artifact.ci_job_id.to_s, @artifact.id.to_s)
+              creation_date, @artifact.job_id.to_s, @artifact.id.to_s)
+  end
+
+  def disk_hash
+    @disk_hash ||= Digest::SHA2.hexdigest(@artifact.project_id.to_s)
   end
 end
