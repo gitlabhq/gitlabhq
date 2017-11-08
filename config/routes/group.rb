2882c93478c9df38a6dd837dfa6c3abbf2d6fca3
Rémy Coutable <remy@rymai.me>
2017-11-08 19:01:15 +0100

    Move EE-specific code to a better position
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- config/routes/group.rb
+++ config/routes/group.rb
@@ -44,14 +44,6 @@ constraints(GroupUrlConstrainer.new) do
       end
     end
 
-    ## EE-specific
-    legacy_ee_group_boards_redirect = redirect do |params, request|
-      path = "/groups/#{params[:group_id]}/-/boards"
-      path << "/#{params[:extra_params]}" if params[:extra_params].present?
-      path << "?#{request.query_string}" if request.query_string.present?
-      path
-    end
-
     resource :avatar, only: [:destroy]
 
     resources :group_members, only: [:index, :create, :update, :destroy], concerns: :access_requestable do
@@ -88,6 +80,12 @@ constraints(GroupUrlConstrainer.new) do
       end
     end
 
+    legacy_ee_group_boards_redirect = redirect do |params, request|
+      path = "/groups/#{params[:group_id]}/-/boards"
+      path << "/#{params[:extra_params]}" if params[:extra_params].present?
+      path << "?#{request.query_string}" if request.query_string.present?
+      path
+    end
     get 'boards(/*extra_params)', as: :legacy_ee_group_boards_redirect, to: legacy_ee_group_boards_redirect
     ## EE-specific
   end
