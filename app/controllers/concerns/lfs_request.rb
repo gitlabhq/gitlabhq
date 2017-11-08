64fb6f7a2d90d43e0c1f732b52d4ca21568cfec2
Rémy Coutable <remy@rymai.me>
2017-11-08 16:21:41 +0000

    Merge branch 'sh-fix-lfs-write-deploy-keys' into 'master'
    Fix Error 500 when pushing LFS objects with a write deploy key
    
    Closes #39467
    
    See merge request gitlab-org/gitlab-ce!15039
    (cherry picked from commit 07ab4ad60a894d60ac561f9fdbd641fac90b4acc)

--- app/controllers/concerns/lfs_request.rb
+++ app/controllers/concerns/lfs_request.rb
@@ -74,8 +74,9 @@ module LfsRequest
 
   def lfs_upload_access?
     return false unless project.lfs_enabled?
+    return false unless has_authentication_ability?(:push_code)
 
-    has_authentication_ability?(:push_code) && can?(user, :push_code, project)
+    lfs_deploy_token? || can?(user, :push_code, project)
   end
 
   def lfs_deploy_token?
