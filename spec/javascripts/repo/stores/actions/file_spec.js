59f666da6057e9822dc66d48135220eef795ec3b
Phil Hughes <me@iamphill.com>
2017-11-08 17:20:46 +0000

    file action specs

--- spec/javascripts/repo/stores/actions/file_spec.js
+++ spec/javascripts/repo/stores/actions/file_spec.js
@@ -1,3 +1,7 @@
+import store from '~/repo/stores';
+import service from '~/repo/services';
+import { file } from '../../helpers';
+
 describe('Multi-file store file actions', () => {
   describe('closeFile', () => {
 
@@ -12,11 +16,51 @@ describe('Multi-file store file actions', () => {
   });
 
   describe('getRawFileData', () => {
+    let tmpFile;
 
+    beforeEach(() => {
+      spyOn(service, 'getRawFileData').and.returnValue(Promise.resolve('raw'));
+
+      tmpFile = file();
+    });
+
+    it('calls getRawFileData service method', (done) => {
+      store.dispatch('getRawFileData', tmpFile)
+        .then(() => {
+          expect(service.getRawFileData).toHaveBeenCalledWith(tmpFile);
+
+          done();
+        }).catch(done.fail);
+    });
+
+    it('updates file raw data', (done) => {
+      store.dispatch('getRawFileData', tmpFile)
+        .then(() => {
+          expect(tmpFile.raw).toBe('raw');
+
+          done();
+        }).catch(done.fail);
+    });
   });
 
   describe('changeFileContent', () => {
+    let tmpFile;
 
+    beforeEach(() => {
+      tmpFile = file();
+    });
+
+    it('updates file content', (done) => {
+      store.dispatch('changeFileContent', {
+        file: tmpFile,
+        content: 'content',
+      })
+      .then(() => {
+        expect(tmpFile.content).toBe('content');
+
+        done();
+      }).catch(done.fail);
+    });
   });
 
   describe('createTempFile', () => {
