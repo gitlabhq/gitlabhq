9424e05240341da38d3c84a79b3cf520fc06e337
Phil Hughes <me@iamphill.com>
2017-11-08 13:02:52 +0000

    resolved repo components [ci skip]

--- app/assets/javascripts/repo/components/repo_loading_file.vue
+++ app/assets/javascripts/repo/components/repo_loading_file.vue
@@ -19,11 +19,7 @@
     class="loading-file"
     aria-label="Loading files"
   >
-<<<<<<< HEAD
-    <td>
-=======
     <td class="multi-file-table-col-name">
->>>>>>> upstream/master
       <skeleton-loading-container
         :small="true"
       />
