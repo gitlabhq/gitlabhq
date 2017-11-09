687e7dea5df7d0d53d48b49cc593b2bcb3c4ef93
Tim Zallmann <tzallmann@gitlab.com>
2017-11-09 15:02:19 +0100

    Put Icons Path in Helper

--- spec/helpers/icons_helper_spec.rb
+++ spec/helpers/icons_helper_spec.rb
@@ -19,7 +19,7 @@ describe IconsHelper do
   describe 'sprite_icon_path' do
     it 'returns relative path' do
       expect(sprite_icon_path())
-        .to eq image_path("icons.svg")
+        .to eq icons_path()
     end
 
     context 'when an asset_host is set in the config it will return an absolute local URL' do
@@ -30,9 +30,8 @@ describe IconsHelper do
       end
 
       it 'returns an absolute URL on that asset host' do
-        gitlab_url = Gitlab.config.gitlab.url
         expect(sprite_icon_path())
-          .to eq image_path("icons.svg", host: Gitlab.config.gitlab.url)
+          .to eq icons_path()
       end
     end
   end
@@ -42,17 +41,17 @@ describe IconsHelper do
 
     it 'returns svg icon html' do
       expect(sprite_icon(icon_name).to_s)
-        .to eq "<svg><use xlink:href=\"#{image_path("icons.svg")}##{icon_name}\"></use></svg>"
+        .to eq "<svg><use xlink:href=\"#{icons_path()}##{icon_name}\"></use></svg>"
     end
 
     it 'returns svg icon html + size classes' do
       expect(sprite_icon(icon_name, size: 72).to_s)
-        .to eq "<svg class=\"s72\"><use xlink:href=\"#{image_path("icons.svg")}##{icon_name}\"></use></svg>"
+        .to eq "<svg class=\"s72\"><use xlink:href=\"#{icons_path()}##{icon_name}\"></use></svg>"
     end
 
     it 'returns svg icon html + size classes + additional class' do
       expect(sprite_icon(icon_name, size: 72, css_class: 'icon-danger').to_s)
-        .to eq "<svg class=\"s72 icon-danger\"><use xlink:href=\"#{image_path("icons.svg")}#{icon_name}\"></use></svg>"
+        .to eq "<svg class=\"s72 icon-danger\"><use xlink:href=\"#{icons_path()}#{icon_name}\"></use></svg>"
     end
   end
 
@@ -160,5 +159,9 @@ describe IconsHelper do
     it 'returns file-text-o class with no extension' do
       expect(file_type_icon_class('file', 0, 'CHANGELOG')).to eq 'file-text-o'
     end
+  end
+
+  def icons_path
+    ActionController::Base.helpers.image_path("icons.svg", host: Gitlab.config.gitlab.url)
   end
 end
