c3d9d3ca6000f83a23dde1aab4b6a40527ea39ea
Jarka Kadlecova <jarka@gitlab.com>
2017-11-10 16:47:41 +0100

    Refactor spec code

--- spec/ee/spec/services/epic_issues/create_service_spec.rb
+++ spec/ee/spec/services/epic_issues/create_service_spec.rb
@@ -7,84 +7,29 @@ describe EpicIssues::CreateService do
     let(:project) { create(:project, group: group) }
     let(:issue) { create :issue, project: project }
     let(:user) { create :user }
-    let(:reference) { issue.to_reference(full: true) }
-    let(:params) do
-      {}
+    let(:valid_reference) { issue.to_reference(full: true) }
+
+    def assign_issue(references)
+      params = { issue_references: references }
+
+      described_class.new(epic, user, params).execute
     end
 
-    subject { described_class.new(epic, user, params).execute }
+    shared_examples 'returns success' do
+      it 'creates relationships' do
+        expect { subject }.to change(EpicIssue, :count).from(0).to(1)
 
-    context 'when user has permissions to link the issue' do
-      before do
-        group.add_developer(user)
+        expect(EpicIssue.find_by!(issue_id: issue.id)).to have_attributes(epic: epic)
       end
 
-      context 'when the reference list is empty' do
-        let(:params) do
-          { issue_references: [] }
-        end
-
-        it 'returns error' do
-          is_expected.to eq(message: 'No Issue found for given params', status: :error, http_status: 404)
-        end
-      end
-
-      context 'when there is an issue to relate' do
-        context 'when shortcut for Issue is given' do
-          let(:params) do
-            { issue_references: [issue.to_reference] }
-          end
-
-          it 'returns error' do
-            is_expected.to eq(message: 'No Issue found for given params', status: :error, http_status: 404)
-          end
-
-          it 'no relationship is created' do
-            expect { subject }.not_to change { EpicIssue.count }
-          end
-        end
-
-        context 'when a full reference is given' do
-          let(:params) do
-            { issue_references: [reference] }
-          end
-
-          it 'creates relationships' do
-            expect { subject }.to change(EpicIssue, :count).from(0).to(1)
-
-            expect(EpicIssue.find_by!(issue_id: issue.id)).to have_attributes(epic: epic)
-          end
-
-          it 'returns success status' do
-            is_expected.to eq(status: :success)
-          end
-        end
-
-        context 'when an issue links is given' do
-          let(:params) do
-            { issue_references: [IssuesHelper.url_for_issue(issue.iid, issue.project)] }
-          end
-
-          it 'creates relationships' do
-            expect { subject }.to change(EpicIssue, :count).from(0).to(1)
-
-            expect(EpicIssue.find_by!(issue_id: issue.id)).to have_attributes(epic: epic)
-          end
-
-          it 'returns success status' do
-            is_expected.to eq(status: :success)
-          end
-        end
+      it 'returns success status' do
+        expect(subject).to eq(status: :success)
       end
     end
 
-    context 'when user does not have permissions to link the issue' do
-      let(:params) do
-        { issue_references: [reference] }
-      end
-
-      it 'returns error' do
-        is_expected.to eq(message: 'No Issue found for given params', status: :error, http_status: 404)
+    shared_examples 'returns an error' do
+      it 'returns an error' do
+        expect(subject).to eq(message: 'No Issue found for given params', status: :error, http_status: 404)
       end
 
       it 'no relationship is created' do
@@ -92,11 +37,45 @@ describe EpicIssues::CreateService do
       end
     end
 
-    context 'when an issue is already assigned to another epic' do
-      let(:params) do
-        { issue_references: [reference] }
+    context 'when user has permissions to link the issue' do
+      before do
+        group.add_developer(user)
       end
 
+      context 'when the reference list is empty' do
+        it 'returns an error' do
+          expect(assign_issue([])).to eq(message: 'No Issue found for given params', status: :error, http_status: 404)
+        end
+      end
+
+      context 'when there is an issue to relate' do
+        context 'when shortcut for Issue is given' do
+          subject { assign_issue([issue.to_reference]) }
+
+          include_examples 'returns an error'
+        end
+
+        context 'when a full reference is given' do
+          subject { assign_issue([valid_reference]) }
+
+          include_examples 'returns success'
+        end
+
+        context 'when an issue links is given' do
+          subject { assign_issue([IssuesHelper.url_for_issue(issue.iid, issue.project)]) }
+
+          include_examples 'returns success'
+        end
+      end
+    end
+
+    context 'when user does not have permissions to link the issue' do
+      subject { assign_issue([valid_reference]) }
+
+      include_examples 'returns an error'
+    end
+
+    context 'when an issue is already assigned to another epic' do
       before do
         group.add_developer(user)
         create(:epic_issue, epic: epic, issue: issue)
@@ -104,14 +83,18 @@ describe EpicIssues::CreateService do
 
       let(:another_epic) { create(:epic, group: group) }
 
-      subject { described_class.new(another_epic, user, params).execute }
+      subject do
+        params = { issue_references: [valid_reference] }
+
+        described_class.new(another_epic, user, params).execute
+      end
 
       it 'does not create a new association' do
         expect { subject }.not_to change(EpicIssue, :count).from(1)
       end
 
       it 'updates the existing association' do
-        expect { subject }.to change { EpicIssue.find_by!(issue_id: issue.id).epic }.from(epic).to(another_epic)
+        expect { subject }.to change { EpicIssue.last.epic }.from(epic).to(another_epic)
       end
 
       it 'returns success status' do
@@ -120,24 +103,16 @@ describe EpicIssues::CreateService do
     end
 
     context 'when issue from non group project is given' do
-      let(:another_issue) { create :issue }
+      subject { assign_issue([another_issue.to_reference(full: true)]) }
 
-      let(:params) do
-        { issue_references: [another_issue.to_reference(full: true)] }
-      end
+      let(:another_issue) { create :issue }
 
       before do
         group.add_developer(user)
         another_issue.project.add_developer(user)
       end
 
-      it 'returns error' do
-        is_expected.to eq(message: 'No Issue found for given params', status: :error, http_status: 404)
-      end
-
-      it 'no relationship is created' do
-        expect { subject }.not_to change { EpicIssue.count }
-      end
+      include_examples 'returns an error'
     end
   end
 end
