7d2a20354d759470ef1c4ee1e6ad91a1fa01bba2
Kim "BKC" Carlbäcker <kim.carlbacker@gmail.com>
2017-11-14 19:47:25 +0100

    skip_gitaly_mock disables Gitaly in specs

--- spec/support/gitaly.rb
+++ spec/support/gitaly.rb
@@ -1,6 +1,9 @@
 RSpec.configure do |config|
   config.before(:each) do |example|
-    next if example.metadata[:skip_gitaly_mock]
-    allow(Gitlab::GitalyClient).to receive(:feature_enabled?).and_return(true)
+    if example.metadata[:skip_gitaly_mock]
+      allow(Gitlab::GitalyClient).to receive(:feature_enabled?).and_return(false)
+    else
+      allow(Gitlab::GitalyClient).to receive(:feature_enabled?).and_return(true)
+    end
   end
 end
