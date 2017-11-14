456ef34e00f47fec6b636f16b74703a99055857e
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2017-11-14 17:13:08 +0200

    Move codequalit and sast code from merge_request_entity.rb to ee module
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- app/serializers/merge_request_entity.rb
+++ app/serializers/merge_request_entity.rb
@@ -1,5 +1,6 @@
 class MergeRequestEntity < IssuableEntity
   include TimeTrackableEntity
+  prepend ::EE::MergeRequestEntity
 
   expose :state
   expose :deleted_at
@@ -189,41 +190,6 @@ class MergeRequestEntity < IssuableEntity
     commit_change_content_project_merge_request_path(merge_request.project, merge_request)
   end
 
-  # EE-specific
-  expose :codeclimate, if: -> (mr, _) { mr.has_codeclimate_data? } do
-    expose :head_path, if: -> (mr, _) { can?(current_user, :read_build, mr.head_codeclimate_artifact) } do |merge_request|
-      raw_project_build_artifacts_url(merge_request.source_project,
-                                      merge_request.head_codeclimate_artifact,
-                                      path: 'codeclimate.json')
-    end
-
-    expose :head_blob_path, if: -> (mr, _) { mr.head_pipeline_sha } do |merge_request|
-      project_blob_path(merge_request.project, merge_request.head_pipeline_sha)
-    end
-
-    expose :base_path, if: -> (mr, _) { can?(current_user, :read_build, mr.base_codeclimate_artifact) } do |merge_request|
-      raw_project_build_artifacts_url(merge_request.target_project,
-                                      merge_request.base_codeclimate_artifact,
-                                      path: 'codeclimate.json')
-    end
-
-    expose :base_blob_path, if: -> (mr, _) { mr.base_pipeline_sha } do |merge_request|
-      project_blob_path(merge_request.project, merge_request.base_pipeline_sha)
-    end
-  end
-
-  expose :sast, if: -> (mr, _) { expose_sast_data?(mr, current_user) } do
-    expose :path do |merge_request|
-      raw_project_build_artifacts_url(merge_request.source_project,
-                                      merge_request.sast_artifact,
-                                      path: 'gl-sast-report.json')
-    end
-
-    expose :blob_path, if: -> (mr, _) { mr.head_pipeline_sha } do |merge_request|
-      project_blob_path(merge_request.project, merge_request.head_pipeline_sha)
-    end
-  end
-
   private
 
   delegate :current_user, to: :request
@@ -231,11 +197,5 @@ class MergeRequestEntity < IssuableEntity
   def presenter(merge_request)
     @presenters ||= {}
     @presenters[merge_request] ||= MergeRequestPresenter.new(merge_request, current_user: current_user)
-  end
-
-  def expose_sast_data?(mr, current_user)
-    mr.project.feature_available?(:sast) &&
-      mr.has_sast_data? &&
-      can?(current_user, :read_build, mr.sast_artifact)
   end
 end
