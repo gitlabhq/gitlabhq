516b6e8d7fa2c5964d6bab274b4d137026c72af7
Gabriel Mazetto <brodock@gmail.com>
2017-11-15 14:42:38 +0100

    WIP Attachments migration

--- app/services/projects/hashed_storage_migration_service.rb
+++ app/services/projects/hashed_storage_migration_service.rb
@@ -1,7 +1,7 @@
 module Projects
   class HashedStorageMigrationService < BaseService
     attr_reader :logger
-    
+
     def initialize(project, logger = nil)
       @project = project
       @logger = logger || Rails.logger
@@ -11,6 +11,11 @@ module Projects
       # Migrate repository from Legacy to Hashed Storage
       unless project.hashed_storage?(:repository)
         return unless HashedStorage::MigrateRepositoryService.new(project, logger).execute
+      end
+
+      # Migrate attachments from Legacy to Hashed Storage
+      unless project.hashed_storage?(:attachments)
+        HashedStorage::MigrateAttachmentsService.new(project, logger).execute
       end
     end
   end
