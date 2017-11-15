64e6fa6fee029cbdcace8f91673b52594e7f7acb
Shinya Maeda <shinya@gitlab.com>
2017-11-16 02:55:41 +0900

    Avoid deactivation when pipeline schedules execute a commit includes `[ci skip]`

--- app/workers/pipeline_schedule_worker.rb
+++ app/workers/pipeline_schedule_worker.rb
@@ -9,7 +9,7 @@ class PipelineScheduleWorker
         pipeline = Ci::CreatePipelineService.new(schedule.project,
                                                  schedule.owner,
                                                  ref: schedule.ref)
-          .execute(:schedule, save_on_errors: false, schedule: schedule)
+          .execute(:schedule, ignore_skip_ci: true, save_on_errors: false, schedule: schedule)
 
         schedule.deactivate! unless pipeline.persisted?
       rescue => e
