0715034805a8f68c66118cf78777bca92ad7cef1
Michael Kozono <mkozono@gmail.com>
2017-11-14 22:20:15 -0800

    Remove irrelevant copy-pasted code

--- lib/gitlab/background_migration/populate_untracked_uploads.rb
+++ lib/gitlab/background_migration/populate_untracked_uploads.rb
@@ -153,28 +153,8 @@ module Gitlab
 
         belongs_to :model, polymorphic: true # rubocop:disable Cop/PolymorphicAssociations
 
-        validates :size, presence: true
-        validates :path, presence: true
-        validates :model, presence: true
-        validates :uploader, presence: true
-
         before_save  :calculate_checksum, if:     :foreground_checksum?
         after_commit :schedule_checksum,  unless: :foreground_checksum?
-
-        def self.remove_path(path)
-          where(path: path).destroy_all
-        end
-
-        def self.record(uploader)
-          remove_path(uploader.relative_path)
-
-          create(
-            size: uploader.file.size,
-            path: uploader.relative_path,
-            model: uploader.model,
-            uploader: uploader.class.to_s
-          )
-        end
 
         def absolute_path
           return path unless relative_path?
