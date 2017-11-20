b94bf73f58dfd74467d0931cd77f670eb23d0a9f
Sean McGivern <sean@mcgivern.me.uk>
2017-11-20 15:08:50 +0000

    Merge branch 'osw-merge-process-logs' into 'master'
    Add logs for monitoring the merge process
    
    See merge request gitlab-org/gitlab-ce!15425
    
    (cherry picked from commit 190ea021cd1fd89d20c9548a72034a7b941413ca)
    
    011558ea Add logs for monitoring the merge process
    fad11dec Add MR reference to logs

--- app/services/merge_requests/merge_service.rb
+++ app/services/merge_requests/merge_service.rb
@@ -10,6 +10,8 @@ module MergeRequests
 
     attr_reader :merge_request, :source
 
+    delegate :merge_jid, :state, to: :@merge_request
+
     def execute(merge_request)
       if project.merge_requests_ff_only_enabled && !self.is_a?(FfMergeService)
         FfMergeService.new(project, current_user, params).execute(merge_request)
@@ -27,6 +29,7 @@ module MergeRequests
           success
         end
       end
+      log_info("Merge process finished on JID #{merge_jid} with state #{state}")
     rescue MergeError => e
       handle_merge_error(log_message: e.message, save_message_on_model: true)
     end
@@ -49,7 +52,9 @@ module MergeRequests
     def commit
       message = params[:commit_message] || merge_request.merge_commit_message
 
+      log_info("Git merge started on JID #{merge_jid}")
       commit_id = repository.merge(current_user, source, merge_request, message)
+      log_info("Git merge finished on JID #{merge_jid} commit #{commit_id}")
 
       raise MergeError, 'Conflicts detected during merge' unless commit_id
 
@@ -63,7 +68,9 @@ module MergeRequests
     end
 
     def after_merge
+      log_info("Post merge started on JID #{merge_jid} with state #{state}")
       MergeRequests::PostMergeService.new(project, current_user).execute(merge_request)
+      log_info("Post merge finished on JID #{merge_jid} with state #{state}")
 
       if delete_source_branch?
         DeleteBranchService.new(@merge_request.source_project, branch_deletion_user)
@@ -90,6 +97,11 @@ module MergeRequests
     def handle_merge_error(log_message:, save_message_on_model: false)
       Rails.logger.error("MergeService ERROR: #{merge_request_info} - #{log_message}")
       @merge_request.update(merge_error: log_message) if save_message_on_model
+    end
+
+    def log_info(message)
+      @logger ||= Rails.logger
+      @logger.info("#{merge_request_info} - #{message}")
     end
 
     def merge_request_info
