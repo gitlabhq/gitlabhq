21316718346f2618a54eddfbfbf23e1b710adc6a
Eric Eastwood <contact@ericeastwood.com>
2017-11-21 10:11:45 -0600

    Fix reply quote keyboard shortcut on MRs
    Fix https://gitlab.com/gitlab-org/gitlab-ce/issues/40295

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -50,6 +50,7 @@ RSpec.configure do |config|
   config.include SearchHelpers, type: :feature
   config.include CookieHelper, :js
   config.include InputHelper, :js
+  config.include SelectionHelper, :js
   config.include InspectRequests, :js
   config.include WaitForRequests, :js
   config.include LiveDebugger, :js
