c6f5f8f678b9b2e6ba802dbdc1fea635e22bae8d
Annabel Dunstone Gray <annabel.dunstone@gmail.com>
2017-11-21 17:07:57 +0000

    Merge branch '40377-blank-states' into 'master'
    Fix blank states using old css
    
    Closes #40377
    
    See merge request gitlab-org/gitlab-ce!15521
    
    (cherry picked from commit 4ebe69600fd7ae9da6fb419d76dd5267d9c3b8ca)
    
    0ae8dc9d Fix blank states using old css

--- app/assets/javascripts/pipelines/components/pipelines.vue
+++ app/assets/javascripts/pipelines/components/pipelines.vue
@@ -202,9 +202,11 @@
         />
 
       <div
-        class="blank-state blank-state-no-icon"
+        class="blank-state-row"
         v-if="shouldRenderNoPipelinesMessage">
-        <h2 class="blank-state-title js-blank-state-title">No pipelines to show.</h2>
+        <div class="blank-state-center">
+          <h2 class="blank-state-title js-blank-state-title">No pipelines to show.</h2>
+        </div>
       </div>
 
       <div
