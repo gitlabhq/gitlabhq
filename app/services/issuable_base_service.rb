7b52400a08f9d5021537a65ee9908ab7e8c450fd
Rémy Coutable <remy@rymai.me>
2017-11-22 10:00:52 +0100

    Disable temporarily the Metrics/AbcSize cop for IssuableBaseService#update
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- app/services/issuable_base_service.rb
+++ app/services/issuable_base_service.rb
@@ -163,7 +163,7 @@ class IssuableBaseService < BaseService
     # To be overridden by subclasses
   end
 
-  def update(issuable)
+  def update(issuable) # rubocop:disable Metrics/AbcSize
     change_state(issuable)
     change_subscription(issuable)
     change_todo(issuable)
