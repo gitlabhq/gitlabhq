60a4fe8e881bfd3159bd0d9f795e9ddf1b61ca48
Filipa Lacerda <filipa@gitlab.com>
2017-11-22 19:02:33 +0000

    Fix conflicts for app/assets/javascripts/init_legacy_filters.js

--- app/assets/javascripts/init_legacy_filters.js
+++ app/assets/javascripts/init_legacy_filters.js
@@ -1,10 +1,7 @@
 /* eslint-disable no-new */
 import LabelsSelect from './labels_select';
 /* global MilestoneSelect */
-<<<<<<< HEAD
 /* global WeightSelect */
-=======
->>>>>>> upstream/master
 import subscriptionSelect from './subscription_select';
 import UsersSelect from './users_select';
 import issueStatusSelect from './issue_status_select';
@@ -15,8 +12,5 @@ export default () => {
   new MilestoneSelect();
   issueStatusSelect();
   subscriptionSelect();
-<<<<<<< HEAD
   new WeightSelect();
-=======
->>>>>>> upstream/master
 };
