65bd6868d014e23c21e4d5ecff468124b2c72f4c
Gabriel Mazetto <brodock@gmail.com>
2017-11-22 06:35:53 +0100

    Codestyle changes and Added Exclusive Lease to hashed storage migration

--- app/workers/project_migrate_hashed_storage_worker.rb
+++ app/workers/project_migrate_hashed_storage_worker.rb
@@ -2,10 +2,34 @@ class ProjectMigrateHashedStorageWorker
   include Sidekiq::Worker
   include DedicatedSidekiqQueue
 
+  LEASE_TIMEOUT = 30.seconds.to_i
+
   def perform(project_id)
     project = Project.find_by(id: project_id)
     return if project.nil? || project.pending_delete?
 
-    ::Projects::HashedStorageMigrationService.new(project, logger).execute
+    uuid = try_obtain_lease_for(project_id)
+    if uuid
+      ::Projects::HashedStorageMigrationService.new(project, logger).execute
+    else
+      false
+    end
+  rescue => ex
+    cancel_lease_for(project_id, uuid)
+    raise ex
+  end
+
+  private
+
+  def try_obtain_lease_for(project_id)
+    Gitlab::ExclusiveLease.new(lease_key(project_id), timeout: LEASE_TIMEOUT).try_obtain
+  end
+
+  def lease_key(project_id)
+    "project_migrate_hashed_storage_worker:#{project_id}"
+  end
+
+  def cancel_lease_for(project_id, uuid)
+    Gitlab::ExclusiveLease.cancel(lease_key(project_id), uuid)
   end
 end
--- spec/workers/project_migrate_hashed_storage_worker_spec.rb
+++ spec/workers/project_migrate_hashed_storage_worker_spec.rb
@@ -5,25 +5,43 @@ describe ProjectMigrateHashedStorageWorker do
     let(:project) { create(:project, :empty_repo) }
     let(:pending_delete_project) { create(:project, :empty_repo, pending_delete: true) }
 
-    it 'skips when project no longer exists' do
-      nonexistent_id = 999999999999
+    context 'when have exclusive lease' do
+      before do
+        allow_any_instance_of(Gitlab::ExclusiveLease).to receive(:try_obtain).and_return(true)
+      end
 
-      expect(::Projects::HashedStorageMigrationService).not_to receive(:new)
-      subject.perform(nonexistent_id)
+      it 'skips when project no longer exists' do
+        nonexistent_id = 999999999999
+
+        expect(::Projects::HashedStorageMigrationService).not_to receive(:new)
+        subject.perform(nonexistent_id)
+      end
+
+      it 'skips when project is pending delete' do
+        expect(::Projects::HashedStorageMigrationService).not_to receive(:new)
+
+        subject.perform(pending_delete_project.id)
+      end
+
+      it 'delegates removal to service class' do
+        service = double('service')
+        expect(::Projects::HashedStorageMigrationService).to receive(:new).with(project, subject.logger).and_return(service)
+        expect(service).to receive(:execute)
+
+        subject.perform(project.id)
+      end
     end
 
-    it 'skips when project is pending delete' do
-      expect(::Projects::HashedStorageMigrationService).not_to receive(:new)
+    context 'when dont have exclusive lease' do
+      before do
+        allow_any_instance_of(Gitlab::ExclusiveLease).to receive(:try_obtain).and_return(false)
+      end
 
-      subject.perform(pending_delete_project.id)
-    end
+      it 'skips when dont have lease' do
+        expect(::Projects::HashedStorageMigrationService).not_to receive(:new)
 
-    it 'delegates removal to service class' do
-      service = double('service')
-      expect(::Projects::HashedStorageMigrationService).to receive(:new).with(project, subject.logger).and_return(service)
-      expect(service).to receive(:execute)
-
-      subject.perform(project.id)
+        subject.perform(project.id)
+      end
     end
   end
 end
