9613f1d7d9d69d24798d73c5400cb6305d148774
Rémy Coutable <remy@rymai.me>
2017-11-23 18:14:20 +0100

    Resolve conflict in app/assets/javascripts/environments/mixins/environments_mixin.js
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- app/assets/javascripts/environments/mixins/environments_mixin.js
+++ app/assets/javascripts/environments/mixins/environments_mixin.js
@@ -21,10 +21,7 @@ import tabs from '../../vue_shared/components/navigation_tabs.vue';
 import container from '../components/container.vue';
 
 export default {
-<<<<<<< HEAD
 
-=======
->>>>>>> origin/master
   components: {
     environmentTable,
     container,
@@ -61,10 +58,7 @@ export default {
         }
       });
     },
-<<<<<<< HEAD
 
-=======
->>>>>>> origin/master
     /**
      * Handles URL and query parameter changes.
      * When the user uses the pagination or the tabs,
