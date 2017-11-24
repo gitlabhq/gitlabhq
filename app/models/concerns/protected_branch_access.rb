96106287db2b6403809b238689afb592da5e2abf
James Edwards-Jones <jedwardsjones@gitlab.com>
2017-11-24 12:43:02 +0000

    Deduplicate protected ref human_access_levels
    Previously these were duplicated so they could be different for push/merge,
    but this was no longer necessary after
    https://gitlab.com/gitlab-org/gitlab-ce/merge_requests/11232

--- app/models/concerns/protected_branch_access.rb
+++ app/models/concerns/protected_branch_access.rb
@@ -8,14 +8,6 @@ module ProtectedBranchAccess
 
     delegate :project, to: :protected_branch
 
-    def self.human_access_levels
-      {
-        Gitlab::Access::MASTER => "Masters",
-        Gitlab::Access::DEVELOPER => "Developers + Masters",
-        Gitlab::Access::NO_ACCESS => "No one"
-      }.with_indifferent_access
-    end
-
     def check_access(user)
       return false if access_level == Gitlab::Access::NO_ACCESS
 
--- app/models/protected_tag/create_access_level.rb
+++ app/models/protected_tag/create_access_level.rb
@@ -1,14 +1,6 @@
 class ProtectedTag::CreateAccessLevel < ActiveRecord::Base
   include ProtectedTagAccess
 
-  def self.human_access_levels
-    {
-      Gitlab::Access::MASTER => "Masters",
-      Gitlab::Access::DEVELOPER => "Developers + Masters",
-      Gitlab::Access::NO_ACCESS => "No one"
-    }.with_indifferent_access
-  end
-
   def check_access(user)
     return false if access_level == Gitlab::Access::NO_ACCESS
 
