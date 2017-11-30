a23ad6a4bc7a9b0eb082e981c6422d56b14cb30d
Mike Greiling <mike@pixelcog.com>
2017-11-30 14:26:06 -0600

    resolve conflicts in main.js

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -44,11 +44,6 @@ import './commits';
 import './compare';
 import './compare_autocomplete';
 import './confirm_danger_modal';
-<<<<<<< HEAD
-import './diff';
-import './files_comment_button';
-=======
->>>>>>> upstream/master
 import Flash, { removeFlashClickListener } from './flash';
 import './gl_dropdown';
 import './gl_field_error';
