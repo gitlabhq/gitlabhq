2ecdfe47491bf76c6582981c2859fdc008253c4d
Rémy Coutable <remy@rymai.me>
2017-11-30 13:09:48 +0100

    Resolve conflict in db/schema.rb
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20171124150326) do
-=======
 ActiveRecord::Schema.define(version: 20171124165823) do
->>>>>>> origin/master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
