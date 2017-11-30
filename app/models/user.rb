8362247c4b73fe78173c5117ea1daa81c5f93d77
Francisco Lopez <fjlopez@gitlab.com>
2017-11-30 18:07:30 +0100

    Refactored max_member_access_* and moving the call to the controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,7 @@ class User < ActiveRecord::Base
   include FeatureGate
   include CreatedAtFilterable
   include IgnorableColumn
+  include BulkMemberAccessLoad
 
   DEFAULT_NOTIFICATION_LEVEL = :participating
 
@@ -1304,38 +1305,5 @@ class User < ActiveRecord::Base
     user
   ensure
     Gitlab::ExclusiveLease.cancel(lease_key, uuid)
-  end
-
-  # Determine the maximum access level for a group of resources in bulk.
-  #
-  # Returns a Hash mapping resource ID -> maximum access level.
-  def max_member_access_for_resource_ids(resource_klass, resource_ids, &block)
-    raise 'Block is mandatory' unless block_given?
-
-    resource_ids = resource_ids.uniq
-    key = "max_member_access_for_#{resource_klass.name.underscore.pluralize}:#{self.id}"
-    access = {}
-
-    if RequestStore.active?
-      RequestStore.store[key] ||= {}
-      access = RequestStore.store[key]
-    end
-
-    # Look up only the IDs we need
-    resource_ids = resource_ids - access.keys
-
-    return access if resource_ids.empty?
-
-    resource_access = yield(resource_ids)
-
-    access.merge!(resource_access)
-
-    missing_resource_ids = resource_ids - resource_access.keys
-
-    missing_resource_ids.each do |resource_id|
-      access[resource_id] = Gitlab::Access::NO_ACCESS
-    end
-
-    access
   end
 end
