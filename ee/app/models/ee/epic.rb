761bdd32141b221a7dddaa716b4f100636b6ae48
Jarka Kadlecova <jarka@gitlab.com>
2017-12-01 10:21:05 +0100

    Support mentioning epics

--- ee/app/models/ee/epic.rb
+++ ee/app/models/ee/epic.rb
@@ -6,6 +6,7 @@ module EE
       include InternalId
       include Issuable
       include Noteable
+      include Referable
 
       belongs_to :assignee, class_name: "User"
       belongs_to :group
@@ -13,6 +14,46 @@ module EE
       has_many :epic_issues
 
       validates :group, presence: true
+    end
+
+    module ClassMethods
+      # We support internal references (&epic_id) and cross-references (group.full_path&epic_id)
+      #
+      # Escaped versions with `&amp;` will be extracted too
+      #
+      # The parent of epic is group instead of project and therefore we have to define new patterns
+      def reference_pattern
+        @reference_pattern ||= begin
+          combined_prefix = Regexp.union(Regexp.escape(reference_prefix), Regexp.escape(reference_prefix_escaped))
+          group_regexp = %r{
+            (?<!\w)
+            (?<group>#{::Gitlab::PathRegex::FULL_NAMESPACE_FORMAT_REGEX})
+          }x
+          %r{
+            (#{group_regexp})?
+            (?:#{combined_prefix})(?<epic>\d+)
+          }x
+        end
+      end
+
+      def link_reference_pattern
+        %r{
+          (?<url>
+            #{Regexp.escape(::Gitlab.config.gitlab.url)}
+            \/groups\/(?<group>#{::Gitlab::PathRegex::FULL_NAMESPACE_FORMAT_REGEX})
+            \/-\/epics
+            \/(?<epic>\d+)
+            (?<path>
+              (\/[a-z0-9_=-]+)*
+            )?
+            (?<query>
+              \?[a-z0-9_=-]+
+              (&[a-z0-9_=-]+)*
+            )?
+            (?<anchor>\#[a-z0-9_-]+)?
+          )
+        }x
+      end
     end
 
     def assignees
@@ -27,6 +68,18 @@ module EE
       false
     end
 
+    def to_reference(from = nil, full: false)
+      reference = "#{self.class.reference_prefix}#{iid}"
+
+      return reference unless cross_reference?(from) || full
+
+      "#{group.full_path}#{reference}"
+    end
+
+    def cross_reference?(from)
+      from && from != group
+    end
+
     # we don't support project epics for epics yet, planned in the future #4019
     def update_project_counter_caches
     end
@@ -37,6 +90,10 @@ module EE
         .where("epic_issues.epic_id = #{id}")
 
       Ability.issues_readable_by_user(related_issues, current_user)
+    end
+
+    def mentionable_params
+      { group: group }
     end
   end
 end
