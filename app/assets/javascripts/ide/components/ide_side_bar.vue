4624d46f3d4befb74cf3b9e091f147fe841c87dd
Tim Zallmann <tzallmann@gitlab.com>
2017-12-01 16:41:00 +0100

    Side bar Buttons fixed

--- app/assets/javascripts/ide/components/ide_side_bar.vue
+++ app/assets/javascripts/ide/components/ide_side_bar.vue
@@ -1,34 +1,61 @@
 <script>
 import { mapState } from 'vuex';
 import ProjectTree from './ide_project_tree.vue';
+import icon from '../../vue_shared/components/icon.vue';
 
 export default {
   components: {
     ProjectTree,
+    icon,
+  },
+  data() {
+    return {
+      collapsed: false,
+    };
   },
   computed: {
     ...mapState([
       'loading',
       'projects',
     ]),
+    currentIcon() {
+      return this.collapsed ? 'angle-double-right' : 'angle-double-left';
+    }
+  },
+  methods: {
+    toggleCollapsed() {
+      this.collapsed = !this.collapsed;
+    },
   },
 };
 </script>
 
 <template>
-<div class="nav-sidebar">
-  <div class="nav-sidebar-inner-scroll">
-    {{ projects.length }}
+<div
+    class="multi-file-commit-panel"
+    :class="{
+      'is-collapsed': collapsed,
+    }"
+  >
+  <div class="multi-file-commit-panel-inner-scroll">
     <project-tree
       v-for="(project, index) in projects"
       :key="project.id"
       :project="project"/>
-
-    <a class="toggle-sidebar-button js-toggle-sidebar" role="button" title="Toggle sidebar" type="button">
-      <svg class=" icon-angle-double-left"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/assets/icons-f1e1e3187fbe3fe3aa42a17cb2558778f1a2ddc2a1914f0ea7ea59d27b4e425c.svg#angle-double-left"></use></svg>
-      <svg class=" icon-angle-double-right"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/assets/icons-f1e1e3187fbe3fe3aa42a17cb2558778f1a2ddc2a1914f0ea7ea59d27b4e425c.svg#angle-double-right"></use></svg>
-      <span class="collapse-text">Collapse sidebar</span>
-    </a>
   </div>
+  <button
+    type="button"
+    class="btn btn-transparent left-collapse-btn"
+    @click="toggleCollapsed"
+  >
+    <icon
+      :name="currentIcon"
+      :size="18"
+    />
+    <span
+      v-if="!collapsed"
+      class="collapse-text"
+    >Collapse sidebar</span>
+  </button>
 </div>
 </template>
