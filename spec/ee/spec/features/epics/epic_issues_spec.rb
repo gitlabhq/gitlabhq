6cf8378db25f635da5fb897cd470146ed805840b
Jarka Kadlecova <jarka@gitlab.com>
2017-12-01 12:15:44 +0100

    Add additional spec and assertions for epis_issues

--- spec/ee/spec/features/epics/epic_issues_spec.rb
+++ spec/ee/spec/features/epics/epic_issues_spec.rb
@@ -43,6 +43,17 @@ describe 'Epic Issues', :js do
   end
 
   context 'when user is a group member' do
+    let(:issue_to_add) { create(:issue, project: private_project) }
+    let(:issue_invalid) { create(:issue) }
+
+    def add_issues(references)
+      find('.related-issues-block h3.panel-title button').click
+      find('.js-add-issuable-form-input').set(references)
+      find('.js-add-issuable-form-add-button').click
+
+      wait_for_requests
+    end
+
     before do
       group.add_developer(user)
       visit_epic
@@ -64,16 +75,20 @@ describe 'Epic Issues', :js do
       end
     end
 
+    it 'user cannot add new issues to the epic from another group' do
+      add_issues("#{issue_invalid.to_reference(full: true)}")
+
+      expect(page).to have_selector('.content-wrapper .alert-wrapper .flash-text')
+      expect(find('.flash-alert')).to have_text('No Issue found for given params')
+    end
+
     it 'user can add new issues to the epic' do
-      issue_to_add = create(:issue, project: private_project)
-      issue_invalid = create(:issue)
       references = "#{issue_to_add.to_reference(full: true)} #{issue_invalid.to_reference(full: true)}"
 
-      find('.related-issues-block h3.panel-title button').click
-      find('.js-add-issuable-form-input').set references
-      find('.js-add-issuable-form-add-button').click
+      add_issues(references)
 
-      wait_for_requests
+      expect(page).not_to have_selector('.content-wrapper .alert-wrapper .flash-text')
+      expect(page).not_to have_content('No Issue found for given params')
 
       within('.related-issues-block ul.issuable-list') do
         expect(page).to have_selector('li', count: 3)
