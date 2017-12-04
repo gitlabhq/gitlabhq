151569e5727f65a0004ade7839d4bdfa1c3eb736
Mike Greiling <mike@pixelcog.com>
2017-12-04 16:55:08 -0600

    allow path without url within collapsable merge request section

--- ee/app/assets/javascripts/vue_merge_request_widget/components/mr_widget_report_issues.vue
+++ ee/app/assets/javascripts/vue_merge_request_widget/components/mr_widget_report_issues.vue
@@ -61,11 +61,15 @@
         in
 
         <a
+          v-if="issue.urlPath"
           :href="issue.urlPath"
           target="_blank"
           rel="noopener noreferrer nofollow">
           {{issue.path}}<template v-if="issue.line">:{{issue.line}}</template>
         </a>
+        <template v-else>
+          {{issue.path}}<template v-if="issue.line">:{{issue.line}}</template>
+        </template>
       </template>
 
     </li>
