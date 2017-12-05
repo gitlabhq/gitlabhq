31dcfcc585d8f184b4afe2c7f14f41342f8b4284
Kamil Trzcinski <ayufan@ayufan.eu>
2017-12-05 12:44:22 +0100

    Fix EE schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171124150326) do
+ActiveRecord::Schema.define(version: 20171124165823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
