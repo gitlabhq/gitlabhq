fc31da4cf721083785ee2d484cf85fe17584720c
Rémy Coutable <remy@rymai.me>
2017-12-06 18:23:32 +0100

    Resolve conflict in spec/factories/appearances.rb
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- spec/factories/appearances.rb
+++ spec/factories/appearances.rb
@@ -2,13 +2,8 @@
 
 FactoryGirl.define do
   factory :appearance do
-<<<<<<< HEAD
     title "GitLab Enterprise Edition"
     description "Open source software to collaborate on code"
-=======
-    title       "MepMep"
-    description "This is my Community Edition instance"
->>>>>>> upstream/master
     new_project_guidelines "Custom project guidelines"
   end
 end
