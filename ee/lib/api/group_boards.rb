a92f699ce398cd3a153e7e8b1230e7f257dd182c
Felipe Artur <felipefac@gmail.com>
2017-12-06 17:07:47 -0200

    Add group boards API endpoint

--- ee/lib/api/group_boards.rb
+++ ee/lib/api/group_boards.rb
@@ -1,39 +1,71 @@
 module API
-  class Boards < Grape::API
+  class GroupBoards < Grape::API
     include BoardsResponses
+    include EE::API::BoardsResponses
     include PaginationParams
 
-    before { authenticate! }
+    before do
+      authenticate!
+      check_group_issue_boards!
+    end
 
     helpers do
       def board_parent
-        user_project
+        user_group
+      end
+
+      def check_group_issue_boards!
+        forbidden! unless ::License.feature_available?(:group_issue_boards)
       end
     end
 
     params do
-      requires :id, type: String, desc: 'The ID of a project'
+      requires :id, type: String, desc: 'The ID of a group'
     end
-    resource :projects, requirements: API::PROJECT_ENDPOINT_REQUIREMENTS do
+
+    resource :groups, requirements: API::PROJECT_ENDPOINT_REQUIREMENTS do
       segment ':id/boards' do
-        desc 'Get all project boards' do
-          detail 'This feature was introduced in 8.13'
+        desc 'Get all group boards' do
+          detail 'This feature was introduced in 10.4'
           success Entities::Board
         end
         params do
           use :pagination
         end
         get '/' do
-          authorize!(:read_board, user_project)
           present paginate(board_parent.boards), with: Entities::Board
         end
 
-        desc 'Find a project board' do
+        desc 'Find a group board' do
           detail 'This feature was introduced in 10.4'
           success Entities::Board
         end
         get '/:board_id' do
           present board, with: Entities::Board
+        end
+
+        desc 'Create a group board' do
+          detail 'This feature was introduced in 10.4'
+          success Entities::Board
+        end
+        params do
+          requires :name, type: String, desc: 'The board name'
+        end
+        post '/' do
+          authorize!(:admin_board, board_parent)
+
+          create_board
+        end
+
+        desc 'Delete a group board' do
+          detail 'This feature was introduced in 10.4'
+          success Entities::Board
+        end
+
+        delete '/:board_id' do
+          authorize!(:admin_board, board_parent)
+
+          delete_board
         end
       end
 
@@ -41,49 +73,47 @@ module API
         requires :board_id, type: Integer, desc: 'The ID of a board'
       end
       segment ':id/boards/:board_id' do
-        desc 'Get the lists of a project board' do
-          detail 'Does not include `done` list. This feature was introduced in 8.13'
+        desc 'Get the lists of a group board' do
+          detail 'Does not include backlog and closed lists. This feature was introduced in 10.4'
           success Entities::List
         end
         params do
           use :pagination
         end
         get '/lists' do
-          authorize!(:read_board, user_project)
           present paginate(board_lists), with: Entities::List
         end
 
-        desc 'Get a list of a project board' do
-          detail 'This feature was introduced in 8.13'
+        desc 'Get a list of a group board' do
+          detail 'This feature was introduced in 10.4'
           success Entities::List
         end
         params do
           requires :list_id, type: Integer, desc: 'The ID of a list'
         end
         get '/lists/:list_id' do
-          authorize!(:read_board, user_project)
           present board_lists.find(params[:list_id]), with: Entities::List
         end
 
         desc 'Create a new board list' do
-          detail 'This feature was introduced in 8.13'
+          detail 'This feature was introduced in 10.4'
           success Entities::List
         end
         params do
           requires :label_id, type: Integer, desc: 'The ID of an existing label'
         end
         post '/lists' do
-          unless available_labels_for(user_project).exists?(params[:label_id])
+          unless available_labels_for(board_parent).exists?(params[:label_id])
             render_api_error!({ error: 'Label not found!' }, 400)
           end
 
-          authorize!(:admin_list, user_project)
+          authorize!(:admin_list, user_group)
 
           create_list
         end
 
         desc 'Moves a board list to a new position' do
-          detail 'This feature was introduced in 8.13'
+          detail 'This feature was introduced in 10.4'
           success Entities::List
         end
         params do
@@ -93,20 +123,20 @@ module API
         put '/lists/:list_id' do
           list = board_lists.find(params[:list_id])
 
-          authorize!(:admin_list, user_project)
+          authorize!(:admin_list, user_group)
 
           move_list(list)
         end
 
         desc 'Delete a board list' do
-          detail 'This feature was introduced in 8.13'
+          detail 'This feature was introduced in 10.4'
           success Entities::List
         end
         params do
           requires :list_id, type: Integer, desc: 'The ID of a board list'
         end
         delete "/lists/:list_id" do
-          authorize!(:admin_list, user_project)
+          authorize!(:admin_list, user_group)
           list = board_lists.find(params[:list_id])
 
           destroy_list(list)
---
+++ spec/ee/support/shared_examples/multiple_and_scoped_issue_boards_.rb
@@ -0,0 +1,48 @@
+shared_examples_for 'multiple and scoped issue boards' do |route_definition|
+  let(:root_url) { route_definition.gsub(":id", board_parent.id.to_s) }
+
+  context 'multiple issue boards' do
+    before do
+      board_parent.add_reporter(user)
+      stub_licensed_features(multiple_issue_boards: true, group_issue_boards: true)
+    end
+
+    describe "POST #{route_definition}" do
+      it 'creates a board' do
+        post api(root_url, user), name: "new board"
+
+        expect(response).to have_gitlab_http_status(201)
+
+        expect(response).to match_response_schema('public_api/v4/board', dir: "ee")
+      end
+    end
+
+    describe "DELETE #{route_definition}" do
+      let(:url) { "#{root_url}/#{board.id}" }
+
+      it 'deletes a board' do
+        delete api(url, user)
+
+        expect(response).to have_gitlab_http_status(204)
+      end
+    end
+  end
+
+  context 'with the scoped_issue_board-feature available' do
+    it 'returns the milestone when the `scoped_issue_board` feature is enabled' do
+      stub_licensed_features(scoped_issue_board: true, group_issue_boards: true)
+
+      get api(root_url, user)
+
+      expect(json_response.first["milestone"]).not_to be_nil
+    end
+
+    it 'hides the milestone when the `scoped_issue_board` feature is disabled' do
+      stub_licensed_features(scoped_issue_board: false, group_issue_boards: true)
+
+      get api(root_url, user)
+
+      expect(json_response.first["milestone"]).to be_nil
+    end
+  end
+end
