f8e93cbbd7c1a129d931ee946ecbb7bd5c0664f7
Rémy Coutable <remy@rymai.me>
2017-12-06 18:49:51 +0100

    Resolve conflict in app/uploaders/legacy_artifact_uploader.rb
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- app/uploaders/legacy_artifact_uploader.rb
+++ app/uploaders/legacy_artifact_uploader.rb
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 class LegacyArtifactUploader < ObjectStoreUploader
   storage_options Gitlab.config.artifacts
-=======
-class LegacyArtifactUploader < GitlabUploader
-  storage :file
->>>>>>> upstream/master
 
   def self.local_store_path
     Gitlab.config.artifacts.path
@@ -14,29 +9,8 @@ class LegacyArtifactUploader < GitlabUploader
     File.join(self.local_store_path, 'tmp/uploads/')
   end
 
-<<<<<<< HEAD
   private
 
-=======
-  def store_dir
-    default_local_path
-  end
-
-  def cache_dir
-    File.join(self.class.local_store_path, 'tmp/cache')
-  end
-
-  def work_dir
-    File.join(self.class.local_store_path, 'tmp/work')
-  end
-
-  private
-
-  def default_local_path
-    File.join(self.class.local_store_path, default_path)
-  end
-
->>>>>>> upstream/master
   def default_path
     File.join(model.created_at.utc.strftime('%Y_%m'), model.project_id.to_s, model.id.to_s)
   end
