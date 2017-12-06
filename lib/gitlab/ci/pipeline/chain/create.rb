5ccced63122fe2d21fff7d7298db1081776193b4
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2017-12-06 15:30:20 +0100

    Move invalid builds counter out of the transaction

--- lib/gitlab/ci/pipeline/chain/create.rb
+++ lib/gitlab/ci/pipeline/chain/create.rb
@@ -17,10 +17,27 @@ module Gitlab
             end
           rescue ActiveRecord::RecordInvalid => e
             error("Failed to persist the pipeline: #{e}")
+          ensure
+            pipeline.builds.find_each do |build|
+              next if build.stage_id.present?
+
+              invalid_builds_counter.increment(node: hostname)
+            end
           end
 
           def break?
             !pipeline.persisted?
+          end
+
+          private
+
+          def invalid_builds_counter
+            @counter ||= Gitlab::Metrics
+              .counter(:invalid_builds_counter, 'Invalid builds counter')
+          end
+
+          def hostname
+            @hostname ||= Socket.gethostname
           end
         end
       end
--- lib/gitlab/ci/stage/seed.rb
+++ lib/gitlab/ci/stage/seed.rb
@@ -39,10 +39,6 @@ module Gitlab
           pipeline.stages.create!(stage).tap do |stage|
             builds_attributes = builds.map do |attributes|
               attributes.merge(stage_id: stage.id)
-
-              if attributes.fetch(:stage_id).nil?
-                invalid_builds_counter.increment(node: hostname)
-              end
             end
 
             pipeline.builds.create!(builds_attributes).each do |build|
@@ -55,15 +51,6 @@ module Gitlab
 
         def protected_ref?
           @protected_ref ||= project.protected_for?(pipeline.ref)
-        end
-
-        def invalid_builds_counter
-          @counter ||= Gitlab::Metrics.counter(:invalid_builds_counter,
-                                               'Builds without stage assigned counter')
-        end
-
-        def hostname
-          @hostname ||= Socket.gethostname
         end
       end
     end
