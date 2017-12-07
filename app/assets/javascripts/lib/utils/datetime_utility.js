a79f7ca3afc9e9e73bf1d3e8ad723dd427c32ac2
Bob Van Landuyt <bob@vanlanduyt.co>
2017-12-07 10:19:46 +0100

    Don't set timeago title to what was already there.
    This didn't actually do anything.

--- app/assets/javascripts/lib/utils/datetime_utility.js
+++ app/assets/javascripts/lib/utils/datetime_utility.js
@@ -35,8 +35,6 @@ window.dateFormat = dateFormat;
 
     w.gl.utils.localTimeAgo = function($timeagoEls, setTimeago = true) {
       $timeagoEls.each((i, el) => {
-        el.setAttribute('title', el.getAttribute('title'));
-
         if (setTimeago) {
           // Recreate with custom template
           $(el).tooltip({
