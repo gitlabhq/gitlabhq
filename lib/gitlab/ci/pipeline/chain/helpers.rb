ad3be46b9fe7d6d42b30d257a923b0070f79da94
Kamil Trzcinski <ayufan@ayufan.eu>
2017-12-08 11:35:36 +0100

    Implement and use Gitlab::Ci::Pipeline::Chain::Command

--- lib/gitlab/ci/pipeline/chain/helpers.rb
+++ lib/gitlab/ci/pipeline/chain/helpers.rb
@@ -3,24 +3,6 @@ module Gitlab
     module Pipeline
       module Chain
         module Helpers
-          include Gitlab::Utils::StrongMemoize
-
-          def branch_exists?
-            strong_memoize(:is_branch) do
-              raise ArgumentError unless pipeline.ref
-
-              project.repository.branch_exists?(pipeline.ref)
-            end
-          end
-
-          def tag_exists?
-            strong_memoize(:is_tag) do
-              raise ArgumentError unless pipeline.ref
-
-              project.repository.tag_exists?(pipeline.ref)
-            end
-          end
-
           def error(message)
             pipeline.errors.add(:base, message)
           end
