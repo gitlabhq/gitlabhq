e05b24bd35821bdba04da256d937a8ffdf67b9a2
Kamil Trzcinski <ayufan@ayufan.eu>
2017-12-08 13:00:09 +0100

    Fix Rubocop

--- spec/lib/gitlab/ci/pipeline/chain/validate/abilities_spec.rb
+++ spec/lib/gitlab/ci/pipeline/chain/validate/abilities_spec.rb
@@ -5,7 +5,7 @@ describe Gitlab::Ci::Pipeline::Chain::Validate::Abilities do
   set(:user) { create(:user) }
 
   let(:pipeline) do
-    build_stubbed(:ci_pipeline,  project: project)
+    build_stubbed(:ci_pipeline, project: project)
   end
 
   let(:command) do
