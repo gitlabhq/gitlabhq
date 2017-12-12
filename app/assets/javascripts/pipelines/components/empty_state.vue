40d6edac49404290f773e83952e61fde0912245f
Filipa Lacerda <filipa@gitlab.com>
2017-12-12 16:55:16 +0000

    Adds i18n

--- app/assets/javascripts/pipelines/components/empty_state.vue
+++ app/assets/javascripts/pipelines/components/empty_state.vue
@@ -23,18 +23,17 @@
     <div class="col-xs-12">
       <div class="text-content">
         <h4 class="text-center">
-          Build with confidence
+          {{ s__("Pipelines|Build with confidence") }}
         </h4>
         <p>
-          Continous Integration can help catch bugs by running your tests automatically,
-          while Continuous Deployment can help you deliver code to your product environment.
+          {{ s__("Pipelines|Continous Integration can help catch bugs by running your tests automatically, while Continuous Deployment can help you deliver code to your product environment.") }}
         </p>
         <div class="text-center">
           <a
             :href="helpPagePath"
             class="btn btn-info"
             >
-            Get started with Pipelines
+            {{ s__("Pipelines|Get started with Pipelines") }}
           </a>
         </div>
       </div>
