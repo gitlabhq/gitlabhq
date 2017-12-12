81038fbaf4b6267c87450fbaf04df2b4e4bbbe90
Sean McGivern <sean@mcgivern.me.uk>
2017-12-12 08:38:04 +0000

    Merge branch 'fix-remove-branch-name-migration' into 'master'
    Fix migration that removes issues.branch_name
    
    See merge request gitlab-org/gitlab-ce!15858

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -9,6 +9,10 @@ class Issue < ActiveRecord::Base
   include FasterCacheKeys
   include RelativePositioning
   include TimeTrackable
+  include ThrottledTouch
+  include IgnorableColumn
+
+  ignore_column :assignee_id, :branch_name
 
   DueDateStruct = Struct.new(:title, :name).freeze
   NoDueDate     = DueDateStruct.new('No Due Date', '0').freeze
