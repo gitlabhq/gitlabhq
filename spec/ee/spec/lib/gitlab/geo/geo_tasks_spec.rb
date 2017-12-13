4a7368530b08634ec119b04c0452843c750546a0
Toon Claes <toon@iotcl.com>
2017-12-13 20:31:33 +0100

    Show clear message when set-geo-primary-node was successful
    Closes gitlab-org/gitlab-ee#2177.

---
+++ spec/ee/spec/lib/gitlab/geo/geo_tasks_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe Gitlab::Geo::GeoTasks do
+  describe '.set_primary_geo_node' do
+    before do
+      allow(GeoNode).to receive(:current_node_url).and_return('https://primary.geo.example.com')
+    end
+
+    it 'sets the primary node' do
+      expect { subject.set_primary_geo_node }.to output(/https:\/\/primary.geo.example.com\/ is now the primary Geo node/).to_stdout
+    end
+
+    it 'returns error when there is already a Primary node' do
+      create(:geo_node, :primary)
+
+      expect { subject.set_primary_geo_node }.to output(/Error saving Geo node:/).to_stdout
+    end
+  end
+end
