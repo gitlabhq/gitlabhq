16b03b5fa91a840a2c8d9a1c14dc517a5bc5ecba
Pawel Chojnacki <pawel@chojnacki.ws>
2017-12-13 17:19:27 +0100

    Initial model

---
+++ app/models/prometheus_query.rb
@@ -0,0 +1,3 @@
+class PrometheusQuery < ActiveRecord::Base
+  belongs_to :project
+end
---
+++ db/migrate/20171213160629_create_prometheus_queries.rb
@@ -0,0 +1,14 @@
+class CreatePrometheusQueries < ActiveRecord::Migration
+  def change
+    create_table :prometheus_queries do |t|
+      t.references :project, index: true, foreign_key: true
+      t.string :title
+      t.string :query
+      t.string :y_label
+      t.string :unit
+      t.string :legend
+
+      t.timestamps null: false
+    end
+  end
+end
