a69e7affbca9bbfe8fb9a51ab1646f81d8ade339
Phil Hughes <me@iamphill.com>
2017-12-16 00:24:18 +0000

    changed webpack config

--- app/assets/javascripts/repo/lib/diff/diff_worker.js
+++ app/assets/javascripts/repo/lib/diff/diff_worker.js
@@ -1,7 +1,6 @@
 import { computeDiff } from './diff';
 
-// eslint-disable-next-line prefer-arrow-callback
-self.addEventListener('message', function diffWorker(e) {
+self.addEventListener('message', (e) => {
   const data = e.data;
 
   self.postMessage({
