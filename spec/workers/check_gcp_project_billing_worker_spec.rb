99043d244c4d579f27382f003df9e3243287df2a
Matija Čupić <matteeyah@gmail.com>
2017-12-16 04:21:13 +0100

    Add lease to CheckGcpProjectBillingWorker

--- spec/workers/check_gcp_project_billing_worker_spec.rb
+++ spec/workers/check_gcp_project_billing_worker_spec.rb
@@ -5,20 +5,38 @@ describe CheckGcpProjectBillingWorker do
     let(:token) { 'bogustoken' }
     subject { described_class.new.perform(token) }
 
-    it 'calls the service' do
-      expect(CheckGcpProjectBillingService).to receive_message_chain(:new, :execute)
+    context 'when there is no lease' do
+      before do
+        allow_any_instance_of(CheckGcpProjectBillingWorker).to receive(:try_obtain_lease_for).and_return('randomuuid')
+      end
 
-      subject
+      it 'calls the service' do
+        expect(CheckGcpProjectBillingService).to receive_message_chain(:new, :execute)
+
+        subject
+      end
+
+      it 'stores billing status in redis' do
+        redis_double = double
+
+        expect(CheckGcpProjectBillingService).to receive_message_chain(:new, :execute).and_return(true)
+        expect(Gitlab::Redis::SharedState).to receive(:with).and_yield(redis_double)
+        expect(redis_double).to receive(:set).with(CheckGcpProjectBillingWorker.redis_shared_state_key_for(token), anything)
+
+        subject
+      end
     end
 
-    it 'stores billing status in redis' do
-      redis_double = double
+    context 'when there is a lease' do
+      before do
+        allow_any_instance_of(CheckGcpProjectBillingWorker).to receive(:try_obtain_lease_for).and_return(false)
+      end
 
-      expect(CheckGcpProjectBillingService).to receive_message_chain(:new, :execute).and_return(true)
-      expect(Gitlab::Redis::SharedState).to receive(:with).and_yield(redis_double)
-      expect(redis_double).to receive(:set).with(CheckGcpProjectBillingWorker.redis_shared_state_key_for(token), anything)
+      it 'does not call the service' do
+        expect(CheckGcpProjectBillingService).not_to receive(:new)
 
-      subject
+        subject
+      end
     end
   end
 end
