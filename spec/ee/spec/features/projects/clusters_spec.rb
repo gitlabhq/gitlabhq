cff66a0a2dc70a487a5143ec55e3c065852aa7b8
Matija Čupić <matteeyah@gmail.com>
2017-12-17 23:49:48 +0100

    Refactor clusters feature spec

--- spec/ee/spec/features/projects/clusters_spec.rb
+++ spec/ee/spec/features/projects/clusters_spec.rb
@@ -1,8 +1,6 @@
 require 'spec_helper'
 
-feature 'Clusters', :js do
-  include GoogleApi::CloudPlatformHelpers
-
+feature 'EE Clusters' do
   let(:project) { create(:project) }
   let(:user) { create(:user) }
 
@@ -11,31 +9,41 @@ feature 'Clusters', :js do
     gitlab_sign_in(user)
   end
 
-  context 'when user has a cluster and visits cluster index page' do
-    let!(:cluster) { create(:cluster, :project, :provided_by_gcp) }
-    let(:project) { cluster.project }
-
-    before do
-      visit project_clusters_path(project)
-    end
+  context 'when user has a cluster' do
+    let!(:cluster) { create(:cluster, :provided_by_gcp, projects: [project]) }
 
     context 'when license has multiple clusters feature' do
       before do
-        allow_any_instance_of(EE::Project).to receive(:feature_available?).with(:multiple_clusters).and_return(true)
+        allow(License).to receive(:feature_available?).and_call_original
+        allow(License).to receive(:feature_available?).with(:multiple_clusters).and_return(true)
       end
 
-      it 'user sees a add cluster button ' do
-        expect(page).to have_selector('.js-add-cluster')
+      context 'when user visits clusters page' do
+        before do
+          visit project_clusters_path(project)
+        end
+
+        it 'user sees a add cluster button ' do
+          expect(page).not_to have_selector('.js-add-cluster.disabled')
+          expect(page).to have_selector('.js-add-cluster')
+        end
       end
     end
 
     context 'when license does not have multiple clusters feature' do
       before do
-        allow_any_instance_of(EE::Project).to receive(:feature_available?).with(:multiple_clusters).and_return(false)
+        allow(License).to receive(:feature_available?).and_call_original
+        allow(License).to receive(:feature_available?).with(:multiple_clusters).and_return(false)
       end
 
-      it 'user sees a disabled add cluster button ' do
-        expect(page).to have_selector('.js-add-cluster.disabled')
+      context 'when user visits cluster index page' do
+        before do
+          visit project_clusters_path(project)
+        end
+
+        it 'user sees a disabled add cluster button ' do
+          expect(page).to have_selector('.js-add-cluster.disabled')
+        end
       end
     end
   end
