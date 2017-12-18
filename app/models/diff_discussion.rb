c77083a4938b42e1901a4cb69106a39a04f5c30d
Micaël Bergeron <mbergeron@gitlab.com>
2017-12-18 10:21:33 -0500

    fix the commit diff discussion sending the wrong url
    it should now send you to the merge request diff path scoped to the commit.

--- app/models/diff_discussion.rb
+++ app/models/diff_discussion.rb
@@ -22,12 +22,10 @@ class DiffDiscussion < Discussion
 
   def merge_request_version_params
     return unless for_merge_request?
-    return {} if active?
 
-    if on_merge_request_commit?
-      { commit_id: commit_id }
-    else
-      noteable.version_params_for(position.diff_refs)
+    version_params do |params|
+      params[:commit_id] = commit_id if on_merge_request_commit?
+      params
     end
   end
 
@@ -36,5 +34,16 @@ class DiffDiscussion < Discussion
       original_position: original_position.to_json,
       position: position.to_json
     )
+  end
+
+  private
+
+  def version_params
+    params = if active?
+               {}
+             else
+               noteable.version_params_for(position.diff_refs)
+             end
+    yield params
   end
 end
