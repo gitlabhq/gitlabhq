3cc47403955724941c8dc6be50f994476821c8b4
Zeger-Jan van de Weg <zegerjan@gitlab.com>
2017-12-20 10:30:42 +0000

    Update clusters_helper.rb
    Resolves a merge conflict, feature not available in CE, so EE is
    maintained

--- app/helpers/clusters_helper.rb
+++ app/helpers/clusters_helper.rb
@@ -1,9 +1,5 @@
 module ClustersHelper
   def has_multiple_clusters?(project)
-<<<<<<< HEAD
     project.feature_available?(:multiple_clusters)
-=======
-    false
->>>>>>> upstream/master
   end
 end
