6b502e78e8c780523bfa202e47005bd8c9ccecab
Fatih Acet <acetfatih@gmail.com>
2017-12-20 23:46:01 +0100

    MRNotesRefactor: Expose resolve with issue path.

--- app/serializers/discussion_entity.rb
+++ app/serializers/discussion_entity.rb
@@ -12,4 +12,7 @@ class DiscussionEntity < Grape::Entity
   expose :resolve_path, if: -> (d, _) { d.resolvable? } do |discussion|
     resolve_project_merge_request_discussion_path(discussion.project, discussion.noteable, discussion.id)
   end
+  expose :resolve_with_issue_path do |discussion|
+    new_project_issue_path(discussion.project, merge_request_to_resolve_discussions_of: discussion.noteable.iid, discussion_to_resolve: discussion.id)
+  end
 end
