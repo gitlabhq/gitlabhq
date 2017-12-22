9ce69872a45304583d0745dcd12b7642cd8cb2b3
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2017-12-22 13:13:09 +0100

    Move QA elements DSL as it belongs to different class

--- qa/qa/page/element.rb
+++ qa/qa/page/element.rb
@@ -7,26 +7,6 @@ module QA
         @name = name
         @pattern = pattern
       end
-
-      def self.evaluate(&block)
-        Page::Element::DSL.new.tap do |evaluator|
-          evaluator.instance_exec(&block)
-
-          return evaluator.elements
-        end
-      end
-
-      class DSL
-        attr_reader :elements
-
-        def initialize
-          @elements = []
-        end
-
-        def element(name, pattern)
-          @elements.push(Page::Element.new(name, pattern))
-        end
-      end
     end
   end
 end
