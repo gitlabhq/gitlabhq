481f461380d4919077c543c51f58e37337167706
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2017-12-22 15:22:54 +0100

    Add implementation for matching view elements in QA

--- qa/qa/page/view.rb
+++ qa/qa/page/view.rb
@@ -8,6 +8,27 @@ module QA
         @elements = elements
       end
 
+      def pathname
+        Pathname.new(File.join( __dir__, '../../../', @path))
+          .cleanpath.expand_path
+      end
+
+      def errors
+        ##
+        # Reduce required elements by streaming views and making assertions on
+        # elements' patterns.
+        #
+        @missing ||= @elements.dup.tap do |elements|
+          File.new(pathname.to_s).foreach do |line|
+            elements.reject! { |element| element.matches?(line) }
+          end
+        end
+
+        @missing.map do |missing|
+          "Missing element `#{missing}` in `#{pathname}` view partial!"
+        end
+      end
+
       def self.evaluate(&block)
         Page::View::DSL.new.tap do |evaluator|
           evaluator.instance_exec(&block)
