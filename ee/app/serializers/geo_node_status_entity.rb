08cc49e0f6e05f1fd1789ad8b40a32153e036798
Stan Hu <stanhu@gmail.com>
2017-12-30 12:57:09 -0800

    Simplify storage_shards_match conditions

--- ee/app/serializers/geo_node_status_entity.rb
+++ ee/app/serializers/geo_node_status_entity.rb
@@ -70,7 +70,7 @@ class GeoNodeStatusEntity < Grape::Entity
   end
 
   expose :storage_shards_match?, as: :storage_shards_match, if: -> (status, options) do
-    Gitlab::Geo.primary? && status.storage_shards.present?
+    status.storage_shards_match.present?
   end
 
   private
