c0df26e08a739e00b557ead26bef231c8b90550e
LUKE BENNETT <lbennett@gitlab.com>
2018-01-04 20:06:12 +0000

    Resolve init_legacy_filters.js conflicts

--- app/assets/javascripts/init_legacy_filters.js
+++ app/assets/javascripts/init_legacy_filters.js
@@ -4,11 +4,7 @@ import subscriptionSelect from './subscription_select';
 import UsersSelect from './users_select';
 import issueStatusSelect from './issue_status_select';
 import MilestoneSelect from './milestone_select';
-<<<<<<< HEAD
-
 import WeightSelect from 'ee/weight_select'; // eslint-disable-line import/first
-=======
->>>>>>> upstream/master
 
 export default () => {
   new UsersSelect();
