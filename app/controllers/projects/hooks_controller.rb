30785eb1535c4cada50cc3eb0c0694dced9b8be4
Robert Speicher <rspeicher@gmail.com>
2018-01-04 15:45:35 -0600

    Add system hooks for merge requests
    See https://gitlab.com/gitlab-org/gitlab-ce/merge_requests/14387
    
    This commit was created manually via patch as recommended by the EE
    compat check.

--- app/controllers/projects/hooks_controller.rb
+++ app/controllers/projects/hooks_controller.rb
@@ -64,18 +64,10 @@ class Projects::HooksController < Projects::ApplicationController
 
   def hook_params
     params.require(:hook).permit(
-      :job_events,
-      :pipeline_events,
       :enable_ssl_verification,
-      :issues_events,
-      :confidential_issues_events,
-      :merge_requests_events,
-      :note_events,
-      :push_events,
-      :tag_push_events,
       :token,
       :url,
-      :wiki_page_events
+      *ProjectHook.triggers.values
     )
   end
 end
