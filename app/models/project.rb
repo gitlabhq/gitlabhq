f9b048e2d893905beaab08c6e397cca41ddd7020
LUKE BENNETT <lbennett@gitlab.com>
2018-01-05 12:46:49 +0000

    Resolve project.rb conflicts

--- app/models/project.rb
+++ app/models/project.rb
@@ -915,14 +915,11 @@ class Project < ActiveRecord::Base
     @ci_service ||= ci_services.reorder(nil).find_by(active: true)
   end
 
-<<<<<<< HEAD
   def deployment_platform(environment: nil)
     @deployment_platform ||= clusters.find_by(enabled: true)&.platform_kubernetes
     @deployment_platform ||= services.where(category: :deployment).reorder(nil).find_by(active: true)
   end
 
-=======
->>>>>>> upstream/master
   def monitoring_services
     services.where(category: :monitoring)
   end
