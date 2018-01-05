e757c447a4eaa7bba15bf53b7599ed458c60ecc4
Matija Čupić <matteeyah@gmail.com>
2018-01-05 01:31:33 +0100

    Add jobs_cache_index to project table

--- db/schema.rb
+++ db/schema.rb
@@ -1872,6 +1872,7 @@ ActiveRecord::Schema.define(version: 20171229225929) do
     t.boolean "remote_mirror_available_overridden"
     t.boolean "only_mirror_protected_branches"
     t.boolean "pull_mirror_available_overridden"
+    t.integer "jobs_cache_index"
   end
 
   add_index "projects", ["ci_id"], name: "index_projects_on_ci_id", using: :btree
