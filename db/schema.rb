65c5cdb6b050151e5acb205684c9ebbca4eb7358
Douwe Maan <douwe@gitlab.com>
2018-01-05 15:23:44 +0000

    Merge branch 'sh-migrate-can-push-to-deploy-keys-projects-10-3' into 'security-10-3'
    [10.3] Migrate `can_push` column from `keys` to `deploy_keys_project`
    
    See merge request gitlab/gitlabhq!2276
    
    (cherry picked from commit f6ca52d31bac350a23938e0aebf717c767b4710c)
    
    1f2bd3c0 Backport to 10.3

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171206221519) do
+ActiveRecord::Schema.define(version: 20171215121259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -618,6 +618,7 @@ ActiveRecord::Schema.define(version: 20171206221519) do
     t.integer "project_id", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean "can_push", default: false, null: false
   end
 
   add_index "deploy_keys_projects", ["project_id"], name: "index_deploy_keys_projects_on_project_id", using: :btree
@@ -892,7 +893,6 @@ ActiveRecord::Schema.define(version: 20171206221519) do
     t.string "type"
     t.string "fingerprint"
     t.boolean "public", default: false, null: false
-    t.boolean "can_push", default: false, null: false
     t.datetime "last_used_at"
   end
 
