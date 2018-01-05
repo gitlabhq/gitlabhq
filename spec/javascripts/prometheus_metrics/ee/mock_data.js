02b3c01724812b1ac51ed4d0cb5142291dfc99b4
Jose Ivan Vargas <jvargas@gitlab.com>
2018-01-05 11:05:22 -0600

    Added frontend specs

---
+++ spec/javascripts/prometheus_metrics/ee/mock_data.js
@@ -0,0 +1,18 @@
+export const metrics = [
+  {
+    edit_path: '/root/prometheus-test/prometheus/metrics/3/edit',
+    id: 3,
+    title: 'Requests',
+  },
+  {
+    edit_path: '/root/prometheus-test/prometheus/metrics/2/edit',
+    id: 2,
+    title: 'Sales by the hour',
+  },
+  {
+    edit_path: '/root/prometheus-test/prometheus/metrics/1/edit',
+    id: 1,
+    title: 'Requests',
+  },
+];
+export const environments = [];
