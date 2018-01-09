fce8f526b19a4b7d5b68cf0ff109b77bceaeeaf3
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2018-01-09 12:45:17 +0100

    Copy edit QA selectors sanity test failure message

--- qa/qa/scenario/test/sanity/selectors.rb
+++ qa/qa/scenario/test/sanity/selectors.rb
@@ -19,21 +19,21 @@ module QA
                 GitLab QA sanity selectors validation test detected problems
                 your merge request!
 
-                The purpose of this tes is to make sure that GitLab QA tests,
-                that are entirely black-box and click-driven scenario, do match
-                pages structure / layout in the GitLab CE / EE repositories.
+                The purpose of this test is to make sure that GitLab QA tests,
+                that are entirely black-box, click-driven scenarios, do match
+                pages structure / layout in GitLab CE / EE repositories.
 
                 It looks like you have changed views / pages / selectors, and
                 these are now out of sync with what we have defined in `qa/`
                 directory.
 
-                Please update code in `qa/` directory to match currect changes
-                in this merge request.
+                Please update the code in `qa/` directory to make it match
+                current changes in this merge request.
 
                 For more help see documentation in `qa/page/README.md` file or
                 ask for help on #qa channel on Slack (GitLab Team only).
 
-                If you are not a team member, and you still need help to
+                If you are not a Team Member, and you still need help to
                 contribute, please open an issue in GitLab QA issue tracker.
 
                 Please see errors described below.
