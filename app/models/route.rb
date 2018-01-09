3576d59ae95a61dd20e997a619dbc6c8e8a70276
Michael Kozono <mkozono@gmail.com>
2018-01-09 12:09:24 -0800

    Revert "Fix Route validation for unchanged path"
    This reverts commit 8040edcce8b4e736b4f4857e6709f94aeb5e274c.

--- app/models/route.rb
+++ app/models/route.rb
@@ -8,7 +8,7 @@ class Route < ActiveRecord::Base
     presence: true,
     uniqueness: { case_sensitive: false }
 
-  validate :ensure_permanent_paths, if: :path_changed?
+  validate :ensure_permanent_paths
 
   after_create :delete_conflicting_redirects
   after_update :delete_conflicting_redirects, if: :path_changed?
