54636e1d4293a8465a772020a54b6193d7df9878
Phil Hughes <me@iamphill.com>
2018-01-09 08:39:22 +0000

    Merge branch 'fl-ipythin-10-3' into 'security-10-3'
    Port of [10.2] Sanitizes IPython notebook output
    
    See merge request gitlab/gitlabhq!2285
    
    (cherry picked from commit 1c46e031c70706450a8e0ae730f4c323b72f9e4c)
    
    aac035fe Port of [10.2] Sanitizes IPython notebook output

--- app/assets/javascripts/notebook/cells/output/html.vue
+++ app/assets/javascripts/notebook/cells/output/html.vue
@@ -1,10 +1,16 @@
 <script>
+<<<<<<< HEAD
   import Prompt from '../prompt.vue';
+=======
+import sanitize from 'sanitize-html';
+import Prompt from '../prompt.vue';
+>>>>>>> Merge branch 'fl-ipythin-10-3' into 'security-10-3'
 
   export default {
     components: {
       prompt: Prompt,
     },
+<<<<<<< HEAD
     props: {
       rawCode: {
         type: String,
@@ -12,11 +18,30 @@
       },
     },
   };
+=======
+  },
+  components: {
+    prompt: Prompt,
+  },
+  computed: {
+    sanitizedOutput() {
+      return sanitize(this.rawCode, {
+        allowedTags: sanitize.defaults.allowedTags.concat([
+          'img', 'svg',
+        ]),
+        allowedAttributes: {
+          img: ['src'],
+        },
+      });
+    },
+  },
+};
+>>>>>>> Merge branch 'fl-ipythin-10-3' into 'security-10-3'
 </script>
 
 <template>
   <div class="output">
     <prompt />
-    <div v-html="rawCode"></div>
+    <div v-html="sanitizedOutput"></div>
   </div>
 </template>
