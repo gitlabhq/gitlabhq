7f3ff2414b8f308f46599a620c46197debfb0a68
Robert Speicher <rspeicher@gmail.com>
2018-01-10 10:04:27 -0600

    Add TriggerableHooks to GroupHook

--- ee/app/models/hooks/group_hook.rb
+++ ee/app/models/hooks/group_hook.rb
@@ -1,7 +1,20 @@
 class GroupHook < ProjectHook
   include CustomModelNaming
+  include TriggerableHooks
 
   self.singular_route_key = :hook
+
+  triggerable_hooks [
+    :push_hooks,
+    :tag_push_hooks,
+    :issue_hooks,
+    :confidential_issue_hooks,
+    :note_hooks,
+    :merge_request_hooks,
+    :job_hooks,
+    :pipeline_hooks,
+    :wiki_page_hooks
+  ]
 
   belongs_to :group
 
