15be32b9e068d4da5081ad3a3c0f9dac1f5b9792
Pawel Chojnacki <pawel@chojnacki.ws>
2018-01-10 22:58:17 +0100

    Cleanup migration

--- db/migrate/20180101160629_create_prometheus_metrics.rb
+++ db/migrate/20180101160629_create_prometheus_metrics.rb
@@ -3,14 +3,13 @@ class CreatePrometheusMetrics < ActiveRecord::Migration
 
   def change
     create_table :prometheus_metrics do |t|
-      t.references :project, index: true, foreign_key: true
+      t.references :project, index: true, foreign_key: { on_delete: :cascade }, null: false
       t.string :title, null: false
       t.string :query, null: false
       t.string :y_label
       t.string :unit
       t.string :legend
-      t.integer :group, null: false
-      t.index :group
+      t.integer :group, null: false, index: true
       t.timestamps_with_timezone null: false
     end
   end
