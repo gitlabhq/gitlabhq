c9505b648b9b67817c7660cf278a7c033968d9ab
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2018-01-10 11:48:52 +0100

    Add selectors coupling between QA and Geo nodes views

--- qa/qa/ee/page/admin/geo/nodes/new.rb
+++ qa/qa/ee/page/admin/geo/nodes/new.rb
@@ -5,6 +5,15 @@ module QA
         module Geo
           module Nodes
             class New < QA::Page::Base
+              view 'ee/app/views/admin/geo_nodes/_form.html.haml' do
+                element :node_url_field, 'text_field :url'
+                element :node_url_placeholder, "label :url, 'URL'"
+              end
+
+              view 'ee/app/views/admin/geo_nodes/new.html.haml' do
+                element :add_node_button, "submit 'Add Node'"
+              end
+
               def set_node_address(address)
                 fill_in 'URL', with: address
               end
