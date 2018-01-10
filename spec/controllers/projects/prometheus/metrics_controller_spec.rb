90502cad55ac91ad739066e3c5a32030f31dd6c1
Pawel Chojnacki <pawel@chojnacki.ws>
2018-01-10 22:56:42 +0100

    Split tests to CE and EE version

--- spec/controllers/projects/prometheus/metrics_controller_spec.rb
+++ spec/controllers/projects/prometheus/metrics_controller_spec.rb
@@ -8,40 +8,10 @@ describe Projects::Prometheus::MetricsController do
 
   before do
     allow(controller).to receive(:project).and_return(project)
-    allow(project).to receive(:prometheus_service).and_return(prometheus_service)
+    allow(controller).to receive(:prometheus_service).and_return(prometheus_service)
 
     project.add_master(user)
     sign_in(user)
-  end
-
-  describe 'POST #validate_query' do
-    before do
-      allow(prometheus_service).to receive(:validate_query)
-    end
-
-    let(:query) { 'avg(metric)' }
-
-    context 'validation information is ready' do
-      let(:validation_result) { { valid: true } }
-
-      it 'validation data is returned' do
-        expect(prometheus_service).to receive(:validate_query).with(query).and_return(validation_result)
-        post :validate_query, project_params(format: :json, query: query)
-
-        expect(json_response).to eq("valid" => true)
-      end
-    end
-
-    context 'validation information is not ready' do
-      let(:validation_result) { {} }
-
-      it 'validation data is returned' do
-        expect(prometheus_service).to receive(:validate_query).with(query).and_return(validation_result)
-        post :validate_query, project_params(format: :json, query: query)
-
-        expect(response).to have_gitlab_http_status(204)
-      end
-    end
   end
 
   describe 'GET #active_common' do
@@ -79,53 +49,6 @@ describe Projects::Prometheus::MetricsController do
 
           expect(response).to have_gitlab_http_status(404)
         end
-      end
-    end
-  end
-
-  describe 'POST #create' do
-    context 'metric is valid' do
-      let(:valid_metric) { { prometheus_metric: { title: 'title', query: 'query', group: 'business' } } }
-
-      it 'shows a success flash message' do
-        post :create, project_params(valid_metric)
-
-        expect(flash[:notice]).to include('Metric was successfully added.')
-        expect(response).to redirect_to(edit_project_service_path(project, project.prometheus_service))
-      end
-    end
-
-    context 'metric is invalid' do
-      let(:invalid_metric) { { prometheus_metric: { title: 'title' } } }
-
-      it 'returns an error' do
-        post :create, project_params(invalid_metric)
-
-        expect(response).to have_gitlab_http_status(422)
-      end
-    end
-  end
-
-  describe 'DELETE #destroy' do
-    context 'format html' do
-      let!(:metric) { create(:prometheus_metric, project: project) }
-
-      it 'destroys the metric' do
-        delete :destroy, project_params(id: metric.id)
-
-        expect(response).to redirect_to(edit_project_service_path(project, project.prometheus_service))
-        expect(PrometheusMetric.find_by(id: metric.id)).to be_nil
-      end
-    end
-
-    context 'format json' do
-      let!(:metric) { create(:prometheus_metric, project: project) }
-
-      it 'remove the metric' do
-        delete :destroy, project_params(id: metric.id, format: :json)
-
-        expect(response).to have_gitlab_http_status(200)
-        expect(PrometheusMetric.find_by(id: metric.id)).to be_nil
       end
     end
   end
