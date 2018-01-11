584149fa711fccc994eeefe96e76c2549b45bbaf
Kushal Pandya <kushal@gitlab.com>
2018-01-11 20:46:46 +0530

    Move modal dialog to app root component

--- app/assets/javascripts/groups/components/item_actions.vue
+++ app/assets/javascripts/groups/components/item_actions.vue
@@ -1,17 +1,4 @@
 <script>
-<<<<<<< HEAD
-  import { s__ } from '~/locale';
-  import tooltip from '~/vue_shared/directives/tooltip';
-  import icon from '~/vue_shared/components/icon.vue';
-  import modal from '~/vue_shared/components/modal.vue';
-  import eventHub from '../event_hub';
-  import { COMMON_STR } from '../constants';
-
-  export default {
-    components: {
-      icon,
-      modal,
-=======
 import tooltip from '~/vue_shared/directives/tooltip';
 import icon from '~/vue_shared/components/icon.vue';
 import eventHub from '../event_hub';
@@ -29,57 +16,19 @@ export default {
       type: Object,
       required: false,
       default: () => ({}),
->>>>>>> upstream/master
     },
-    directives: {
-      tooltip,
+    group: {
+      type: Object,
+      required: true,
     },
-<<<<<<< HEAD
-    props: {
-      parentGroup: {
-        type: Object,
-        required: false,
-        default: () => ({}),
-      },
-      group: {
-        type: Object,
-        required: true,
-      },
-=======
   },
   computed: {
     leaveBtnTitle() {
       return COMMON_STR.LEAVE_BTN_TITLE;
->>>>>>> upstream/master
     },
-    data() {
-      return {
-        modalStatus: false,
-      };
+    editBtnTitle() {
+      return COMMON_STR.EDIT_BTN_TITLE;
     },
-<<<<<<< HEAD
-    computed: {
-      leaveBtnTitle() {
-        return COMMON_STR.LEAVE_BTN_TITLE;
-      },
-      editBtnTitle() {
-        return COMMON_STR.EDIT_BTN_TITLE;
-      },
-      leaveConfirmationMessage() {
-        return s__(`GroupsTree|Are you sure you want to leave the "${this.group.fullName}" group?`);
-      },
-    },
-    methods: {
-      onLeaveGroup() {
-        this.modalStatus = true;
-      },
-      leaveGroup() {
-        this.modalStatus = false;
-        eventHub.$emit('leaveGroup', this.group, this.parentGroup);
-      },
-    },
-  };
-=======
   },
   methods: {
     onLeaveGroup() {
@@ -87,7 +36,6 @@ export default {
     },
   },
 };
->>>>>>> upstream/master
 </script>
 
 <template>
