67c5debf152e32644f4e7d7b9aef8e0908940a89
Toon Claes <toon@iotcl.com>
2018-01-11 15:15:25 +0100

    Make the specs tagged :geo run with FDW enabled

--- scripts/prepare_postgres_fdw.sh
+++ scripts/prepare_postgres_fdw.sh
@@ -8,3 +8,8 @@ CREATE SCHEMA gitlab_secondary;
 IMPORT FOREIGN SCHEMA public FROM SERVER gitlab_secondary INTO gitlab_secondary;
 GRANT USAGE ON FOREIGN SERVER gitlab_secondary TO current_user;
 EOF
+
+# Ensure the FDW setting is enabled
+sed -i '/fdw:/d' config/database_geo.yml
+sed -i '/gitlabhq_geo_test/a\
+\ \ fdw: true' config/database_geo.yml
