5963b74a345a483265a0f773ac7ed57ed13fc036
Phil Hughes <me@iamphill.com>
2018-01-11 13:18:26 +0000

    correctly init GFM

--- app/assets/javascripts/shared/milestones/form.js
+++ app/assets/javascripts/shared/milestones/form.js
@@ -2,8 +2,8 @@ import ZenMode from '../../zen_mode';
 import DueDateSelectors from '../../due_date_select';
 import GLForm from '../../gl_form';
 
-export default () => {
+export default (initGFM = true) => {
   new ZenMode(); // eslint-disable-line no-new
   new DueDateSelectors(); // eslint-disable-line no-new
-  new GLForm($('.milestone-form'), true); // eslint-disable-line no-new
+  new GLForm($('.milestone-form'), initGFM); // eslint-disable-line no-new
 };
