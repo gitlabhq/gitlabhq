e52bae3b9e6104d605e44d9167efc8d8478f6838
Matija Čupić <matteeyah@gmail.com>
2018-01-11 01:29:35 +0100

    Fix CheckGcpProjectBillingService spec

--- spec/services/check_gcp_project_billing_service_spec.rb
+++ spec/services/check_gcp_project_billing_service_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe CheckGcpProjectBillingService do
   let(:service) { described_class.new }
-  let(:projects) { [double(name: 'first_project'), double(name: 'second_project')] }
+  let(:projects) { [double(name: 'first_project', project_id: 'first_project-1234'), double(name: 'second_project', project_id: 'second_project-1234')] }
 
   describe '#execute' do
     before do
@@ -10,7 +10,7 @@ describe CheckGcpProjectBillingService do
         .to receive(:projects_list).and_return(projects)
 
       allow_any_instance_of(GoogleApi::CloudPlatform::Client)
-        .to receive_message_chain(:projects_get_billing_info, :billingEnabled)
+        .to receive_message_chain(:projects_get_billing_info, :billing_enabled)
         .and_return(project_billing_enabled)
     end
 
