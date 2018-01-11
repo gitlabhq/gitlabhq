75cacd701f854197a7605149f3ec81124be48251
Jose Ivan Vargas <jvargas@gitlab.com>
2018-01-11 10:03:37 -0600

    Solved features/support/env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -21,11 +21,8 @@ end
   require Rails.root.join('spec', 'support', f)
 end
 
-<<<<<<< HEAD
 Dir["#{Rails.root}/features/steps/shared/*.rb"].each { |file| require file }
 
-=======
->>>>>>> upstream/master
 Spinach.hooks.before_run do
   include RSpec::Mocks::ExampleMethods
   include ActiveJob::TestHelper
