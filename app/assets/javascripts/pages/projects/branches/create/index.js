8298fb73ed846ccc2ae4ca5e8285ee837e55b7fa
Clement Ho <ClemMakesApps@gmail.com>
2018-01-12 12:28:56 -0600

    Remove unused create index.js

--- app/assets/javascripts/pages/projects/branches/create/index.js
+++
@@ -1,3 +0,0 @@
-import NewBranchForm from '~/new_branch_form';
-
-export default () => new NewBranchForm($('.js-create-branch-form'), JSON.parse(document.getElementById('availableRefs').innerHTML));
