669c34db6c75d826b6b692232f235f9dcc492082
Filipe Freire <livrofubia@gmail.com>
2018-01-12 10:22:35 +0000

    Add relevant element for push event button

--- qa/qa/page/activity/activity.rb
+++ qa/qa/page/activity/activity.rb
@@ -2,15 +2,12 @@ module QA
   module Page
     module Activity
       class Activity < Page::Base
-        ##
-        # TODO, define all selectors required by this page object
-        #
-        # See gitlab-org/gitlab-qa#155
-        #
-        view 'app/views/projects/activity.html.haml'
+        view 'app/views/projects/activity.html.haml' do
+          element :push_events_button, 'Push events'
+        end
 
         def go_to_push_events
-          click_button 'Push events'
+          click_button :push_events_button
         end
       end
     end
