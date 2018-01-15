56e57355dcd187150004e2be6f025d9bf0941df4
Simon Knox <psimyn@gmail.com>
2018-01-15 21:18:52 +1100

    dedupe form

--- app/assets/javascripts/pages/projects/issues/new/index.js
+++ app/assets/javascripts/pages/projects/issues/new/index.js
@@ -1,18 +1,5 @@
-/* eslint-disable no-new */
-import GLForm from '~/gl_form';
-import IssuableForm from '~/issuable_form';
-import LabelsSelect from '~/labels_select';
-import MilestoneSelect from '~/milestone_select';
-import ShortcutsNavigation from '~/shortcuts_navigation';
-import IssuableTemplateSelectors from '~/templates/issuable_template_selectors';
-import WeightSelect from 'ee/weight_select';
+import initForm from '../form';
 
 export default () => {
-  new ShortcutsNavigation();
-  new GLForm($('.issue-form'), true);
-  new IssuableForm($('.issue-form'));
-  new LabelsSelect();
-  new MilestoneSelect();
-  new IssuableTemplateSelectors();
-  new WeightSelect();
+  initForm();
 };
