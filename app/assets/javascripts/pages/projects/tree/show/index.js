24fa3461e1acbd3f0b1cd6cc11566a2571b16de0
Constance Okoghenun <cokoghenun@gitlab.com>
2018-01-15 07:33:20 +0100

    Refactored import of projects:tags:* and projects:tree:* of dispatcher.js

---
+++ app/assets/javascripts/pages/projects/tree/show/index.js
@@ -0,0 +1,16 @@
+import TreeView from '../../../../tree';
+import ShortcutsNavigation from '../../../../shortcuts_navigation';
+import BlobViewer from '../../../../blob/viewer';
+import NewCommitForm from '../../../../new_commit_form';
+import { ajaxGet } from '../../../../lib/utils/common_utils';
+
+export default () => {
+  new TreeView(); // eslint-disable-line no-new
+  new BlobViewer(); // eslint-disable-line no-new
+  new NewCommitForm($('.js-create-dir-form')); // eslint-disable-line no-new
+  $('#tree-slider').waitForImages(() =>
+    ajaxGet(document.querySelector('.js-tree-content').dataset.logsPath));
+
+  return new ShortcutsNavigation(); // eslint-disable-line no-new
+};
+
