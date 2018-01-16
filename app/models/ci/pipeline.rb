926d8457c5e8ec8bcdb3640e42c82db781ac8bde
Rémy Coutable <remy@rymai.me>
2018-01-16 16:18:44 +0100

    Resolve conflict in app/models/ci/pipeline.rb
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- app/models/ci/pipeline.rb
+++ app/models/ci/pipeline.rb
@@ -7,11 +7,8 @@ module Ci
     include Presentable
     include Gitlab::OptimisticLocking
 
-<<<<<<< HEAD
     prepend ::EE::Ci::Pipeline
 
-=======
->>>>>>> upstream/master
     belongs_to :project, inverse_of: :pipelines
     belongs_to :user
     belongs_to :auto_canceled_by, class_name: 'Ci::Pipeline'
