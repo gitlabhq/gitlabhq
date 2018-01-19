44bd85d30b30325dfefc2ee4398eae1b8c19edc8
Simon Knox <simon@gitlab.com>
2018-01-19 00:05:17 +0000

    dynamic imports for groups: pages

---
+++ app/assets/javascripts/pages/groups/show/index.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-new */
+
+import NewGroupChild from '~/groups/new_group_child';
+import notificationsDropdown from '~/notifications_dropdown';
+import NotificationsForm from '~/notifications_form';
+import ProjectsList from '~/projects_list';
+import ShortcutsNavigation from '~/shortcuts_navigation';
+
+export default () => {
+  const newGroupChildWrapper = document.querySelector('.js-new-project-subgroup');
+  new ShortcutsNavigation();
+  new NotificationsForm();
+  notificationsDropdown();
+  new ProjectsList();
+
+  if (newGroupChildWrapper) {
+    new NewGroupChild(newGroupChildWrapper);
+  }
+};
