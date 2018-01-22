bbe00038da5b10f8c152332da7d72f38b5552262
Nick Thomas <nick@gitlab.com>
2018-01-22 15:22:03 +0000

    Work around a bug in DatabaseCleaner when using the deletion strategy on MySQL

--- spec/support/db_cleaner.rb
+++ spec/support/db_cleaner.rb
@@ -1,3 +1,18 @@
+require 'database_cleaner/active_record/deletion'
+
+module FakeInformationSchema
+  # Work around a bug in DatabaseCleaner when using the deletion strategy:
+  # https://github.com/DatabaseCleaner/database_cleaner/issues/347
+  #
+  # On MySQL, if the information schema is said to exist, we use an inaccurate
+  # row count leading to some tables not being cleaned when they should
+  def information_schema_exists?(_connection)
+    false
+  end
+end
+
+DatabaseCleaner::ActiveRecord::Deletion.prepend(FakeInformationSchema)
+
 RSpec.configure do |config|
   # Ensure all sequences are reset at the start of the suite run
   config.before(:suite) do
