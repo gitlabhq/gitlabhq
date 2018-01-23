d327277c39df1537b41073912d9b4e9f765d8da5
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2018-01-23 13:02:35 +0100

    Add views / selectors for pipeline show page object

--- qa/qa/page/project/pipeline/show.rb
+++ qa/qa/page/project/pipeline/show.rb
@@ -5,6 +5,18 @@ module QA::Page
         element :pipeline_header, /header class.*ci-header-container.*/
       end
 
+      view 'app/assets/javascripts/pipelines/components/graph/graph_component.vue' do
+        element :pipeline_graph, /class.*pipeline-graph.*/
+      end
+
+      view 'app/assets/javascripts/pipelines/components/graph/job_component.vue' do
+        element :job_component, /class.*ci-job-component.*/
+      end
+
+      view 'app/assets/javascripts/vue_shared/components/ci_icon.vue' do
+        element :status_icon, 'ci-status-icon-${status}'
+      end
+
       def running?
         within('.ci-header-container') do
           return page.has_content?('running')
