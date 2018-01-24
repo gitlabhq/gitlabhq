5e911c5d2c18b5f909f2072cc97bb75704496ea1
Lin Jen-Shin <godfat@godfat.org>
2018-01-24 23:35:17 +0800

    Prefer local variables instead

--- qa/qa/specs/features/project/add_deploy_key_spec.rb
+++ qa/qa/specs/features/project/add_deploy_key_spec.rb
@@ -1,12 +1,12 @@
 module QA
   feature 'deploy keys support', :core do
-    given(:key) { Runtime::RSAKey.new }
-    given(:deploy_key_title) { 'deploy key title' }
-    given(:deploy_key_value) { key.public_key }
-
     scenario 'user adds a deploy key' do
       Runtime::Browser.visit(:gitlab, Page::Main::Login)
       Page::Main::Login.act { sign_in_using_credentials }
+
+      key = Runtime::RSAKey.new
+      deploy_key_title = 'deploy key title'
+      deploy_key_value = key.public_key
 
       deploy_key = Factory::Resource::DeployKey.fabricate! do |resource|
         resource.title = deploy_key_title
