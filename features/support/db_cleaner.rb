59aaec296308bc614e050f552c20ffcc34113d80
Stan Hu <stanhu@gmail.com>
2018-01-24 15:34:26 -0800

    Resolve conflict in features/support/db_cleaner.rb

--- features/support/db_cleaner.rb
+++ features/support/db_cleaner.rb
@@ -1,10 +1,6 @@
 require 'database_cleaner'
 
-<<<<<<< HEAD
 DatabaseCleaner[:active_record].strategy = :deletion, { except: ['licenses'] }
-=======
-DatabaseCleaner[:active_record].strategy = :deletion
->>>>>>> upstream/master
 
 Spinach.hooks.before_scenario do
   DatabaseCleaner.start
