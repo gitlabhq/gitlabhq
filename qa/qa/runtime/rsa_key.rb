cc02eeff6eca6ad318c5147848dcbc4ddf87680d
Lin Jen-Shin <godfat@godfat.org>
2018-01-24 23:21:59 +0800

    Move initialize method later.

--- qa/qa/runtime/rsa_key.rb
+++ qa/qa/runtime/rsa_key.rb
@@ -6,12 +6,12 @@ module QA
     class RSAKey
       extend Forwardable
 
+      attr_reader :key
+      def_delegators :@key, :fingerprint
+
       def initialize(bits = 4096)
         @key = OpenSSL::PKey::RSA.new(bits)
       end
-
-      attr_reader :key
-      def_delegators :@key, :fingerprint
 
       def public_key
         @public_key ||= "#{key.ssh_type} #{[key.to_blob].pack('m0')}"
