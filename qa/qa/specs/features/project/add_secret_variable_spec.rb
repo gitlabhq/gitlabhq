3d4a7f63cbf197465fa556ece387b57f57176d10
Lin Jen-Shin <godfat@godfat.org>
2018-01-25 21:40:27 +0800

    Prefer local variables over given/let

--- qa/qa/specs/features/project/add_secret_variable_spec.rb
+++ qa/qa/specs/features/project/add_secret_variable_spec.rb
@@ -1,11 +1,11 @@
 module QA
   feature 'secret variables support', :core do
-    given(:variable_key) { 'VARIABLE_KEY' }
-    given(:variable_value) { 'variable value' }
-
     scenario 'user adds a secret variable' do
       Runtime::Browser.visit(:gitlab, Page::Main::Login)
       Page::Main::Login.act { sign_in_using_credentials }
+
+      variable_key = 'VARIABLE_KEY'
+      variable_value = 'variable value'
 
       variable = Factory::Resource::SecretVariable.fabricate! do |resource|
         resource.key = variable_key
