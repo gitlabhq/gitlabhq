e853495f80de79213a15f15a72171955d04f13e0
James Lopez <james@gitlab.com>
2018-01-26 15:39:10 +0000

    Merge branch 'fix/gh-namespace-issue' into 'security-10-4'
    [10.4] Fix GH namespace security issue
    
    See merge request gitlab/gitlabhq!2295

--- app/controllers/import/base_controller.rb
+++ app/controllers/import/base_controller.rb
@@ -2,26 +2,16 @@ class Import::BaseController < ApplicationController
   private
 
   def find_or_create_namespace(names, owner)
-    return current_user.namespace if names == owner
-    return current_user.namespace unless current_user.can_create_group?
-
     names = params[:target_namespace].presence || names
-    full_path_namespace = Namespace.find_by_full_path(names)
 
-    return full_path_namespace if full_path_namespace
+    return current_user.namespace if names == owner
 
-    names.split('/').inject(nil) do |parent, name|
-      begin
-        namespace = Group.create!(name: name,
-                                  path: name,
-                                  owner: current_user,
-                                  parent: parent)
-        namespace.add_owner(current_user)
+    group = Groups::NestedCreateService.new(current_user, group_path: names).execute
 
-        namespace
-      rescue ActiveRecord::RecordNotUnique, ActiveRecord::RecordInvalid
-        Namespace.where(parent: parent).find_by_path_or_name(name)
-      end
-    end
+    group.errors.any? ? current_user.namespace : group
+  rescue => e
+    Gitlab::AppLogger.error(e)
+
+    current_user.namespace
   end
 end
