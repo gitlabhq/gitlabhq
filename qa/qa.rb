468072d07ddc1bbae40e70b6a082c908ad517399
Michael Kozono <mkozono@gmail.com>
2018-01-26 11:56:26 -0800

    Add Geo attachment replication QA spec
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- qa/qa.rb
+++ qa/qa.rb
@@ -27,6 +27,7 @@ module QA
     module Resource
       autoload :Sandbox, 'qa/factory/resource/sandbox'
       autoload :Group, 'qa/factory/resource/group'
+      autoload :Issue, 'qa/factory/resource/issue'
       autoload :Project, 'qa/factory/resource/project'
       autoload :MergeRequest, 'qa/factory/resource/merge_request'
       autoload :DeployKey, 'qa/factory/resource/deploy_key'
@@ -129,6 +130,12 @@ module QA
         autoload :DeployKeys, 'qa/page/project/settings/deploy_keys'
         autoload :SecretVariables, 'qa/page/project/settings/secret_variables'
         autoload :Runners, 'qa/page/project/settings/runners'
+      end
+
+      module Issue
+        autoload :New, 'qa/page/project/issue/new'
+        autoload :Show, 'qa/page/project/issue/show'
+        autoload :Index, 'qa/page/project/issue/index'
       end
     end
 
