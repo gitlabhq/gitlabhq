1abb37f530ef18e6d69c99375be427a996234ca0
Rubén Dávila <ruben@gitlab.com>
2018-01-28 08:23:05 -0500

    Update migration in order to avoid error with MySql
    Build was failing with: Mysql2::Error: Cannot drop index 'index_lfs_file_locks_on_project_id_and_path':
    needed in a foreign key constraint: DROP INDEX `index_lfs_file_locks_on_project_id_and_path` ON `lfs_file_locks`
    
    More details on https://gitlab.com/gitlab-org/gitlab-ee/-/jobs/49848260

--- db/migrate/20180116193854_create_lfs_file_locks.rb
+++ db/migrate/20180116193854_create_lfs_file_locks.rb
@@ -1,7 +1,11 @@
 class CreateLfsFileLocks < ActiveRecord::Migration
+  include Gitlab::Database::MigrationHelpers
+
   DOWNTIME = false
 
-  def change
+  disable_ddl_transaction!
+
+  def up
     create_table :lfs_file_locks do |t|
       t.references :project, null: false, foreign_key: { on_delete: :cascade }
       t.references :user, null: false, index: true, foreign_key: { on_delete: :cascade }
@@ -10,5 +14,17 @@ class CreateLfsFileLocks < ActiveRecord::Migration
     end
 
     add_index :lfs_file_locks, [:project_id, :path], unique: true
+  end
+
+  def down
+    if foreign_keys_for(:lfs_file_locks, :project_id).any?
+      remove_foreign_key :lfs_file_locks, column: :project_id
+    end
+
+    if index_exists?(:lfs_file_locks, [:project_id, :path])
+      remove_concurrent_index :lfs_file_locks, [:project_id, :path]
+    end
+
+    drop_table :lfs_file_locks
   end
 end
