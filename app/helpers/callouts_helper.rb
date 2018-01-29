977996d2b396c1892a0bcdb386d63e4f78675239
Matija Čupić <matteeyah@gmail.com>
2018-01-30 00:05:48 +0100

    Add check for guest user

--- app/helpers/callouts_helper.rb
+++ app/helpers/callouts_helper.rb
@@ -1,6 +1,6 @@
 module CalloutsHelper
   def show_gke_cluster_integration_callout?(kube_feature_name, project)
-    !user_dismissed?(kube_feature_name) && project.team.master?(current_user)
+    current_user && !user_dismissed?(kube_feature_name) && project.team.master?(current_user)
   end
 
   private
