a3244cacb0bbf9fccda2c24fe9fb17674ed38e63
James Lopez <james@jameslopez.es>
2018-01-30 10:06:19 +0100

    add metrics spec

---
+++ spec/ee/spec/lib/gitlab/metrics/multi_file_editor_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe Gitlab::Metrics::MultiFileEditor do
+  let(:project) { create(:project, :repository) }
+  let(:user) { create(:user) }
+  let(:params) do
+    [
+      :multi_file_editor_usage,
+      'Total number of commits using the multi-file web editor',
+      {
+        project: 'abcd',
+        user: 'abcd',
+        line_changes: 1,
+        files_count: 1
+      }
+    ]
+  end
+
+  subject { described_class.new(project, user, project.repository.commit('HEAD')) }
+
+  before do
+    allow(Digest::SHA256).to receive(:hexdigest).and_return('abcd')
+  end
+
+  describe '.record' do
+    it 'records the right number of commit files' do
+      expect(::Gitlab::Metrics).to receive(:counter).with(*params)
+
+      subject.record
+    end
+  end
+end
