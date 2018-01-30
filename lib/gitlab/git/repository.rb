0cf34d4d6d350ae6987da2260e0824bf91e752ed
Jacob Vosmaer <jacob@gitlab.com>
2018-01-30 15:19:58 +0100

    Remove Rugged::Walk implementation of #log

--- lib/gitlab/git/repository.rb
+++ lib/gitlab/git/repository.rb
@@ -462,7 +462,6 @@ module Gitlab
           path: nil,
           follow: false,
           skip_merges: false,
-          disable_walk: false,
           after: nil,
           before: nil
         }
@@ -494,11 +493,7 @@ module Gitlab
           return []
         end
 
-        if log_using_shell?(options)
-          log_by_shell(sha, options)
-        else
-          log_by_walk(sha, options)
-        end
+        log_by_shell(sha, options)
       end
 
       def count_commits(options)
@@ -1605,24 +1600,6 @@ module Gitlab
         else
           options
         end
-      end
-
-      def log_using_shell?(options)
-        options[:path].present? ||
-          options[:disable_walk] ||
-          options[:skip_merges] ||
-          options[:after] ||
-          options[:before]
-      end
-
-      def log_by_walk(sha, options)
-        walk_options = {
-          show: sha,
-          sort: Rugged::SORT_NONE,
-          limit: options[:limit],
-          offset: options[:offset]
-        }
-        Rugged::Walker.walk(rugged, walk_options).to_a
       end
 
       # Gitaly note: JV: although #log_by_shell shells out to Git I think the
