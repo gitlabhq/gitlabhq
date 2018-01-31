b291749415c6815d757a0c4e493d66caad934b4c
James Lopez <james@jameslopez.es>
2018-01-31 12:14:51 +0100

    add metric table

---
+++ db/migrate/20180131083552_create_web_ide_metrics.rb
@@ -0,0 +1,12 @@
+class CreateWebIdeMetrics < ActiveRecord::Migration
+  def change
+    create_table :web_ide_metrics do |t|
+      t.string :project, null: false, limit: 64
+      t.string :user, null: false, limit: 64
+      t.integer :line_count, null: false
+      t.integer :file_count, null: false
+
+      t.datetime_with_timezone :created_at
+    end
+  end
+end
