0ef543085027a18101a65a777bdc675e8b9b4a18
Matija Čupić <matteeyah@gmail.com>
2018-02-01 21:02:07 +0100

    Add callout priority enum

--- app/helpers/callouts_helper.rb
+++ app/helpers/callouts_helper.rb
@@ -1,6 +1,11 @@
 module CalloutsHelper
   GKE_CLUSTER_INTEGRATION = 'gke_cluster_integration'.freeze
 
+  # Higher value = higher priority
+  PRIORITY = {
+    GKE_CLUSTER_INTEGRATION: 0
+  }.freeze
+
   def show_gke_cluster_integration_callout?(project)
     current_user && !user_dismissed?(GKE_CLUSTER_INTEGRATION) &&
       (project.team.master?(current_user) ||
