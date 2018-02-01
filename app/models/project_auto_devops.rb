e6487168ea1e0016a76e3e62f156990dd4412679
Matija Čupić <matteeyah@gmail.com>
2018-02-01 23:59:14 +0100

    Read the AutoDevOps instance domain in ProjectAutoDevOps

--- app/models/project_auto_devops.rb
+++ app/models/project_auto_devops.rb
@@ -1,4 +1,6 @@
 class ProjectAutoDevops < ActiveRecord::Base
+  include Gitlab::CurrentSettings
+
   belongs_to :project
 
   scope :enabled, -> { where(enabled: true) }
@@ -6,13 +8,17 @@ class ProjectAutoDevops < ActiveRecord::Base
 
   validates :domain, allow_blank: true, hostname: { allow_numeric_hostname: true }
 
+  def instance_domain
+    current_application_settings.auto_devops_domain
+  end
+
   def has_domain?
-    domain.present?
+    domain.present? || instance_domain.present?
   end
 
   def variables
     variables = []
-    variables << { key: 'AUTO_DEVOPS_DOMAIN', value: domain, public: true } if domain.present?
+    variables << { key: 'AUTO_DEVOPS_DOMAIN', value: domain || instance_domain, public: true } if has_domain?
     variables
   end
 end
