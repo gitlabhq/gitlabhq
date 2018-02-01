f94d3f212eadd50469f6215ce996a002d5fb4488
Matija Čupić <matteeyah@gmail.com>
2018-02-01 23:29:11 +0100

    Add specs for .auto_devops_warning_message

--- app/helpers/auto_devops_helper.rb
+++ app/helpers/auto_devops_helper.rb
@@ -9,21 +9,28 @@ module AutoDevopsHelper
   end
 
   def auto_devops_warning_message(project)
-    missing_domain = !project.auto_devops&.has_domain?
-    missing_service = !project.deployment_platform&.active?
-
-    if missing_service
+    if missing_service?
       params = {
         kubernetes: link_to('Kubernetes cluster', project_clusters_path(project))
       }
 
-      if missing_domain
+      if missing_domain?
         _('Auto Review Apps and Auto Deploy need a domain name and a %{kubernetes} to work correctly.') % params
       else
         _('Auto Review Apps and Auto Deploy need a %{kubernetes} to work correctly.') % params
       end
-    elsif missing_domain
+    elsif missing_domain?
       _('Auto Review Apps and Auto Deploy need a domain name to work correctly.')
     end
+  end
+
+  private
+
+  def missing_domain?
+    !(project.auto_devops&.has_domain? || current_application_settings.auto_devops_domain.present?)
+  end
+
+  def missing_service?
+    !project.deployment_platform&.active?
   end
 end
