32ffb39414bf96c12720ba61515c37e652c4b896
Lin Jen-Shin <godfat@godfat.org>
2018-02-02 16:37:41 +0800

    Make sure we're using the same bundler version on
    omnibus for our tests.

--- scripts/prepare_build.sh
+++ scripts/prepare_build.sh
@@ -6,6 +6,12 @@ export USE_BUNDLE_INSTALL=${USE_BUNDLE_INSTALL:-true}
 export BUNDLE_INSTALL_FLAGS="--without=production --jobs=$(nproc) --path=vendor --retry=3 --quiet"
 
 if [ "$USE_BUNDLE_INSTALL" != "false" ]; then
+    # if there's a comma (,), then we have multiple bundlers
+    if gem list -qe bundler | grep -q ,; then
+        # Uninstall non-default bundler to make sure we're using the one
+        gem uninstall -i /usr/local/lib/ruby/gems/2.3.0 bundler
+    fi
+    retry gem install bundler -v 1.13.7
     bundle install --clean $BUNDLE_INSTALL_FLAGS && bundle check
 fi
 
