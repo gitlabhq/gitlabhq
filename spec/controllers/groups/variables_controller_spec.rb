f7ed096455c932a5ead8bafb8c937ff9cdb3070c
Matija Čupić <matteeyah@gmail.com>
2018-02-02 19:35:00 +0100

    Extract Variables controllers specs to shared_examples

--- spec/controllers/groups/variables_controller_spec.rb
+++ spec/controllers/groups/variables_controller_spec.rb
@@ -16,15 +16,12 @@ describe Groups::VariablesController do
       get :show, group_id: group, format: :json
     end
 
-    it 'renders the ci_group_variable as json' do
-      subject
-
-      expect(response).to match_response_schema('variables')
-    end
+    include_examples 'GET #show lists all variables'
   end
 
-  describe 'POST #update' do
+  describe 'PATCH #update' do
     let!(:variable) { create(:ci_group_variable, group: group) }
+    let(:owner) { group }
 
     subject do
       patch :update,
@@ -33,89 +30,6 @@ describe Groups::VariablesController do
         format: :json
     end
 
-    let(:variable_attributes) do
-      { id: variable.id,
-        key: variable.key,
-        value: variable.value,
-        protected: variable.protected?.to_s }
-    end
-    let(:new_variable_attributes) do
-      { key: 'new_key',
-        value: 'dummy_value',
-        protected: 'false' }
-    end
-
-    context 'with invalid new variable parameters' do
-      let(:variables_attributes) do
-        [
-          variable_attributes.merge(value: 'other_value'),
-          new_variable_attributes.merge(key: '...?')
-        ]
-      end
-
-      it 'does not update the existing variable' do
-        expect { subject }.not_to change { variable.reload.value }
-      end
-
-      it 'does not create the new variable' do
-        expect { subject }.not_to change { group.variables.count }
-      end
-
-      it 'returns a bad request response' do
-        subject
-
-        expect(response).to have_gitlab_http_status(:bad_request)
-      end
-    end
-
-    context 'with valid new variable parameters' do
-      let(:variables_attributes) do
-        [
-          variable_attributes.merge(value: 'other_value'),
-          new_variable_attributes
-        ]
-      end
-
-      it 'updates the existing variable' do
-        expect { subject }.to change { variable.reload.value }.to('other_value')
-      end
-
-      it 'creates the new variable' do
-        expect { subject }.to change { group.variables.count }.by(1)
-      end
-
-      it 'returns a successful response' do
-        subject
-
-        expect(response).to have_gitlab_http_status(:ok)
-      end
-
-      it 'has all variables in response' do
-        subject
-
-        expect(response).to match_response_schema('variables')
-      end
-    end
-
-    context 'with a deleted variable' do
-      let(:variables_attributes) { [variable_attributes.merge(_destroy: 'true')] }
-
-      it 'destroys the variable' do
-        expect { subject }.to change { group.variables.count }.by(-1)
-        expect { variable.reload }.to raise_error ActiveRecord::RecordNotFound
-      end
-
-      it 'returns a successful response' do
-        subject
-
-        expect(response).to have_gitlab_http_status(:ok)
-      end
-
-      it 'has all variables in response' do
-        subject
-
-        expect(response).to match_response_schema('variables')
-      end
-    end
+    include_examples 'PATCH #update updates variables'
   end
 end
