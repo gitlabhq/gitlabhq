bb4ebd0b708aa299775377eaf5b554fe0333ebc3
Shinya Maeda <shinya@gitlab.com>
2018-02-05 17:06:53 +0900

    Make all workers in BuildFinishedWorker to run async and reorder

--- app/workers/build_finished_worker.rb
+++ app/workers/build_finished_worker.rb
@@ -7,9 +7,9 @@ class BuildFinishedWorker
   def perform(build_id)
     Ci::Build.find_by(id: build_id).try do |build|
       BuildTraceSectionsWorker.perform_async(build.id)
+      BuildCoverageWorker.perform_async(build.id)
+      BuildHooksWorker.perform_async(build.id)
       CreateTraceArtifactWorker.perform_async(build.id)
-      BuildCoverageWorker.new.perform(build.id)
-      BuildHooksWorker.new.perform(build.id)
     end
   end
 end
