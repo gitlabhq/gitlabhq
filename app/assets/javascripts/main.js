fbc87366f8bacd3de1a9ae4c4969f05d366a9472
Mario de la Ossa <mariodelaossa@gmail.com>
2018-02-05 11:03:46 -0600

    resolve conflicts on app/assets/javascripts/main.js

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -33,12 +33,9 @@ import './projects_dropdown';
 import './render_gfm';
 import initBreadcrumbs from './breadcrumb';
 
-<<<<<<< HEAD
 // EE-only scripts
 import 'ee/main';
 
-=======
->>>>>>> upstream/master
 import initDispatcher from './dispatcher';
 
 // eslint-disable-next-line global-require, import/no-commonjs
