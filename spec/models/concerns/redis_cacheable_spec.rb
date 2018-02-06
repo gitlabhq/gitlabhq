b75fa80eaae13b4474bac5453420988e14903c4f
Matija Čupić <matteeyah@gmail.com>
2018-02-06 19:01:23 +0100

    Check return value in RedisCacheable#cached_attribute spec

--- spec/models/concerns/redis_cacheable_spec.rb
+++ spec/models/concerns/redis_cacheable_spec.rb
@@ -8,14 +8,19 @@ describe RedisCacheable do
   end
 
   describe '#cached_attribute' do
-    subject { model.cached_attribute(:anything) }
+    let(:payload) { { attribute: 'value' } }
+
+    subject { model.cached_attribute(payload.keys.first) }
 
     it 'gets the cache attribute' do
+      expect(model).to receive(:cache_attribute_key).and_return('key')
+
       Gitlab::Redis::SharedState.with do |redis|
-        expect(redis).to receive(:get).with(model.send(:cache_attribute_key))
+        expect(redis).to receive(:get).with('key')
+          .and_return(payload.to_json)
       end
 
-      subject
+      expect(subject).to eq(payload.values.first)
     end
   end
 
