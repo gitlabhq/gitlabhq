eae053a059c4e16ce9cdfcfcd87dc2b7b25f6ed2
Matija Čupić <matteeyah@gmail.com>
2018-02-06 18:42:28 +0100

    Use double instead of runner in RedisCacheable spec

--- spec/models/concerns/redis_cacheable_spec.rb
+++ spec/models/concerns/redis_cacheable_spec.rb
@@ -1,14 +1,18 @@
 require 'spec_helper'
 
 describe RedisCacheable do
-  let(:runner) { build(:ci_runner) }
+  let(:model) { double }
+
+  before do
+    model.extend(described_class)
+  end
 
   describe '#cached_attribute' do
-    subject { runner.cached_attribute(:anything) }
+    subject { model.cached_attribute(:anything) }
 
     it 'gets the cache attribute' do
       Gitlab::Redis::SharedState.with do |redis|
-        expect(redis).to receive(:get).with(runner.send(:cache_attribute_key))
+        expect(redis).to receive(:get).with(model.send(:cache_attribute_key))
       end
 
       subject
@@ -18,12 +22,12 @@ describe RedisCacheable do
   describe '#cache_attributes' do
     let(:values) { { name: 'new_name' } }
 
-    subject { runner.cache_attributes(values) }
+    subject { model.cache_attributes(values) }
 
     it 'sets the cache attributes' do
       Gitlab::Redis::SharedState.with do |redis|
         values.each do |key, value|
-          redis_key = runner.send(:cache_attribute_key)
+          redis_key = model.send(:cache_attribute_key)
           expect(redis).to receive(:set).with(redis_key, values.to_json, anything)
         end
       end
