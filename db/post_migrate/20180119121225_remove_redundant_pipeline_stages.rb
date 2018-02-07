394c1c65883883d5ac8bcbeecd9fe05b1d3fd87b
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2018-02-07 13:09:53 +0100

    Disable statement timeout when removing redundant stages

--- db/post_migrate/20180119121225_remove_redundant_pipeline_stages.rb
+++ db/post_migrate/20180119121225_remove_redundant_pipeline_stages.rb
@@ -38,6 +38,8 @@ class RemoveRedundantPipelineStages < ActiveRecord::Migration
   end
 
   def remove_redundant_pipeline_stages!
+    disable_statement_timeout
+
     redundant_stages_ids = <<~SQL
       SELECT id FROM ci_stages WHERE (pipeline_id, name) IN (
         SELECT pipeline_id, name FROM ci_stages
