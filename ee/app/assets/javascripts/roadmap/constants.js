7c85a5d1c7797247dbd7d49a2a3cb59ead7a276d
Kushal Pandya <kushal@gitlab.com>
2018-02-08 00:09:56 +0530

    Roadmap app global constants

---
+++ ee/app/assets/javascripts/roadmap/constants.js
@@ -0,0 +1,11 @@
+export const TIMEFRAME_LENGTH = 6;
+
+export const EPIC_DETAILS_CELL_WIDTH = 320;
+
+export const TIMELINE_CELL_MIN_WIDTH = 180;
+
+export const SCROLL_BAR_SIZE = 15;
+
+export const TIMELINE_END_OFFSET_HALF = 17;
+
+export const TIMELINE_END_OFFSET_FULL = 26;
