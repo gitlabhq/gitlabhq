8ae5c4bf6400b4c65e9bae32363c78cd354a3d75
Pawel Chojnacki <pawel@chojnacki.ws>
2018-02-07 18:37:40 +0100

    fix new rubocop failures

--- config/routes/project.rb
+++ config/routes/project.rb
@@ -74,7 +74,7 @@ constraints(ProjectUrlConstrainer.new) do
       resource :mattermost, only: [:new, :create]
 
       namespace :prometheus do
-        resources :metrics, constraints: { id: /[^\/]+/ }, only: [:index, :new, :create, :edit, :update, :destroy] do
+        resources :metrics, constraints: { id: %r{/[^\/]+} }, only: [:index, :new, :create, :edit, :update, :destroy] do
           post :validate_query, on: :collection
           get :active_common, on: :collection
         end
