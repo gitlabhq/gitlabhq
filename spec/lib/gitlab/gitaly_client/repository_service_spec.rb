acc0e25de739aa515dbf8ef151921f366fe322b1
Alejandro Rodríguez <alejorro70@gmail.com>
2018-02-07 18:26:35 -0300

    Incorporate Gitaly's RepositoryService.IsSquashInProgress RPC

--- spec/lib/gitlab/gitaly_client/repository_service_spec.rb
+++ spec/lib/gitlab/gitaly_client/repository_service_spec.rb
@@ -84,4 +84,30 @@ describe Gitlab::GitalyClient::RepositoryService do
       expect(client.has_local_branches?).to be(true)
     end
   end
+
+  describe '#rebase_in_progress?' do
+    let(:rebase_id) { 1 }
+
+    it 'sends a repository_rebase_in_progress message' do
+      expect_any_instance_of(Gitaly::RepositoryService::Stub)
+        .to receive(:is_rebase_in_progress)
+        .with(gitaly_request_with_path(storage_name, relative_path), kind_of(Hash))
+        .and_return(double(in_progress: true))
+
+      client.rebase_in_progress?(rebase_id)
+    end
+  end
+
+  describe '#squash_in_progress?' do
+    let(:squash_id) { 1 }
+
+    it 'sends a repository_squash_in_progress message' do
+      expect_any_instance_of(Gitaly::RepositoryService::Stub)
+        .to receive(:is_squash_in_progress)
+        .with(gitaly_request_with_path(storage_name, relative_path), kind_of(Hash))
+        .and_return(double(in_progress: true))
+
+      client.squash_in_progress?(squash_id)
+    end
+  end
 end
