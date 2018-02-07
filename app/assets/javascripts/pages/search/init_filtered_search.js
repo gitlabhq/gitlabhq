5a5b7ecf9adffd6bc03ec28e0c28f2ab29e94c7f
Jacob Schatz <jschatz1@gmail.com>
2018-02-07 15:39:54 -0500

    Refactor issue index filtered search

--- app/assets/javascripts/pages/search/init_filtered_search.js
+++ app/assets/javascripts/pages/search/init_filtered_search.js
@@ -1,3 +1,14 @@
+import '~/filtered_search/dropdown_emoji';
+import '~/filtered_search/dropdown_hint';
+import '~/filtered_search/dropdown_non_user';
+import '~/filtered_search/dropdown_user';
+import '~/filtered_search/dropdown_utils';
+import '~/filtered_search/filtered_search_dropdown_manager';
+import '~/filtered_search/filtered_search_dropdown';
+import '~/filtered_search/filtered_search_manager';
+import '~/filtered_search/filtered_search_tokenizer';
+import '~/filtered_search/filtered_search_visual_tokens';
+
 export default ({ page, filteredSearchTokenKeys, stateFiltersSelector }) => {
   const filteredSearchEnabled = gl.FilteredSearchManager && document.querySelector('.filtered-search');
   if (filteredSearchEnabled) {
