260282b0ac292f1ec7c6f2ed0eb23142175f898a
Stuart Nelson <stuartnelson3@gmail.com>
2018-02-10 14:54:45 +0100

    Create cron issue due spec skeleton

---
+++ spec/workers/issue_due_worker_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe IssueDueWorker do
+  describe 'perform' do
+    let(:worker) { described_class.new }
+
+    it 'finds issues due on the day run' do
+    end
+  end
+end
