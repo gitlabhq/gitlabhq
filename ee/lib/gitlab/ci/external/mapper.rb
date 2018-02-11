c73d0ff7bdcd2df3d34b01b2da58d2a45dc2ab1b
Mayra Cabrera <mcabrera@gitlab.com>
2018-02-11 13:32:53 -0600

    Fixes invalid external locations
    - Ensures every location, whether local or remote is a valid yml file
    - Dry up files by creating a base class
    - Adjust specs
    
    Closes #4874
    Closes #4873

--- ee/lib/gitlab/ci/external/mapper.rb
+++ ee/lib/gitlab/ci/external/mapper.rb
@@ -17,10 +17,8 @@ module Gitlab
         attr_reader :locations, :project, :sha
 
         def build_external_file(location)
-          remote_file = Gitlab::Ci::External::File::Remote.new(location)
-
-          if remote_file.valid?
-            remote_file
+          if ::Gitlab::UrlSanitizer.valid?(location)
+            Gitlab::Ci::External::File::Remote.new(location)
           else
             options = { project: project, sha: sha }
             Gitlab::Ci::External::File::Local.new(location, options)
