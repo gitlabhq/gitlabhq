7bd912783ae20a841acb8f62f6218b46e8703ce8
Stuart Nelson <stuartnelson3@gmail.com>
2018-02-13 10:12:36 +0100

    Add spec for issue_due_worker

--- spec/workers/issue_due_worker_spec.rb
+++ spec/workers/issue_due_worker_spec.rb
@@ -5,6 +5,13 @@ describe IssueDueWorker do
     let(:worker) { described_class.new }
 
     it 'finds issues due on the day run' do
+      issue1 = create(:issue, :opened, due_date: Date.today)
+      issue3 = create(:issue, :opened, due_date: 3.days.from_now)
+      issue4 = create(:issue, :opened, due_date: 4.days.from_now)
+
+      expect_any_instance_of(NotificationService).to receive(:issue_due_email).with(issue1)
+
+      worker.perform
     end
   end
 end
