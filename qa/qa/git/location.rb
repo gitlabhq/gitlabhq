de484c6ba035a4b0ed0c0a42212aaedf9788c45c
Lin Jen-Shin <godfat@godfat.org>
2018-02-13 23:37:36 +0800

    Just use initialize and remove scheme we're not using

--- qa/qa/git/location.rb
+++ qa/qa/git/location.rb
@@ -11,24 +11,16 @@ module QA
 
       # See: config/initializers/1_settings.rb
       # Settings#build_gitlab_shell_ssh_path_prefix
-      def self.parse(git_uri)
-        if git_uri.start_with?('ssh://')
-          new(git_uri, URI.parse(git_uri))
+      def initialize(git_uri)
+        @git_uri = git_uri
+        @uri = if git_uri.start_with?('ssh://')
+          URI.parse(git_uri)
         else
           *rest, path = git_uri.split(':')
           # Host cannot have : so we'll need to escape it
           user_host = rest.join('%3A').sub(/\A\[(.+)\]\z/, '\1')
-          new(git_uri, URI.parse("ssh://#{user_host}/#{path}"))
+          URI.parse("ssh://#{user_host}/#{path}")
         end
-      end
-
-      def initialize(git_uri, uri)
-        @git_uri = git_uri
-        @uri = uri
-      end
-
-      def scheme
-        uri.scheme || 'ssh'
       end
 
       def port
