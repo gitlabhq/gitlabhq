ca6a6d31a7ab5c3df1d7b811400600040f9eec71
Matija Čupić <matteeyah@gmail.com>
2018-02-13 14:51:44 +0100

    Extend variables_duplicates validator with scope handling

--- app/validators/variable_duplicates_validator.rb
+++ app/validators/variable_duplicates_validator.rb
@@ -1,11 +1,22 @@
 # VariableDuplicatesValidator
 #
-# This validtor is designed for especially the following condition
+# This validator is designed for especially the following condition
 # - Use `accepts_nested_attributes_for :xxx` in a parent model
 # - Use `validates :xxx, uniqueness: { scope: :xxx_id }` in a child model
 class VariableDuplicatesValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    duplicates = value.reject(&:marked_for_destruction?).group_by(&:key).select { |_, v| v.many? }.map(&:first)
+    if options[:scope]
+      scoped = value.group_by { |variable| Array(options[:scope]).map { |attr| variable.send(attr) } }
+      scoped.each_value { |scope| validate_duplicates(record, attribute, scope) }
+    else
+      validate_duplicates(record, attribute, value)
+    end
+  end
+
+  private
+
+  def validate_duplicates(record, attribute, values)
+    duplicates = values.reject(&:marked_for_destruction?).group_by(&:key).select { |_, v| v.many? }.map(&:first)
     if duplicates.any?
       record.errors.add(attribute, "Duplicate variables: #{duplicates.join(", ")}")
     end
