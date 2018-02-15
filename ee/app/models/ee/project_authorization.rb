e5d3d06a7a652fdc3e81d3caa294ec7cf3f1b7fe
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2018-02-15 16:56:04 +0200

    Improve sql query to remove duplicates for roles stats
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- ee/app/models/ee/project_authorization.rb
+++ ee/app/models/ee/project_authorization.rb
@@ -5,18 +5,22 @@ module EE
     module ClassMethods
       def roles_stats
         connection.execute <<-EOF.strip_heredoc
-          SELECT CASE max(access_level)
-          WHEN 10 THEN 'guest'
-          WHEN 20 THEN 'reporter'
-          WHEN 30 THEN 'developer'
-          WHEN 40 THEN 'master'
-          WHEN 50 THEN 'owner'
-          ELSE 'unknown' END
-          AS kind,
-            count(DISTINCT user_id) AS amount
-          FROM #{table_name}
+          SELECT CASE access_level
+            WHEN 10 THEN 'guest'
+            WHEN 20 THEN 'reporter'
+            WHEN 30 THEN 'developer'
+            WHEN 40 THEN 'master'
+            WHEN 50 THEN 'owner'
+            ELSE 'unknown' END
+            AS kind,
+            count(*) AS amount
+          FROM (
+              SELECT user_id, max(access_level) AS access_level
+              FROM #{table_name}
+              GROUP BY user_id
+          ) access_per_user
           GROUP BY access_level
-          ORDER BY amount DESC;
+          ORDER BY access_level ASC;
         EOF
       end
     end
