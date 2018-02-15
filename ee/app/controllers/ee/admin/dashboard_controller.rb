9351997f4cd0e9d6770d1880ef4fd07829958f47
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2018-02-15 17:19:29 +0200

    Move roles statistic to separate page
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- ee/app/controllers/ee/admin/dashboard_controller.rb
+++ ee/app/controllers/ee/admin/dashboard_controller.rb
@@ -4,13 +4,7 @@ module EE
     module DashboardController
       extend ActiveSupport::Concern
 
-      prepended do
-        before_action :set_roles_count, only: [:index]
-      end
-
-      private
-
-      def set_roles_count
+      def stats
         @admin_count = ::User.admins.count
         @roles_count = ::ProjectAuthorization.roles_stats
       end
