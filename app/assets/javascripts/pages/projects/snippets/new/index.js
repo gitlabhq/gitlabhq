704ec01955e98c9098e5e57c2d0cb0c2493af5ad
Phil Hughes <me@iamphill.com>
2018-02-19 10:23:38 +0000

    fixed ace editor not loading in snippets

--- app/assets/javascripts/pages/projects/snippets/new/index.js
+++ app/assets/javascripts/pages/projects/snippets/new/index.js
@@ -1,3 +1,12 @@
+/* global ace */
 import initForm from '~/pages/projects/init_form';
 
-document.addEventListener('DOMContentLoaded', () => initForm($('.snippet-form')));
+document.addEventListener('DOMContentLoaded', () => {
+  const editor = ace.edit('editor');
+
+  initForm($('.snippet-form'));
+
+  $('.snippet-form-holder form').on('submit', () => {
+    $('.snippet-file-content').val(editor.getValue());
+  });
+});
