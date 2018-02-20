9879487e4d83f61d3ae31c842599724f1dfc6052
Mike Greiling <mike@pixelcog.com>
2018-02-20 01:59:16 -0600

    migrate projects:tags:new to static bundle

--- app/assets/javascripts/dispatcher.js
+++ app/assets/javascripts/dispatcher.js
@@ -53,11 +53,6 @@ var Dispatcher;
         case 'projects:merge_requests:edit':
           shortcut_handler = true;
           break;
-        case 'projects:tags:new':
-          import('./pages/projects/tags/new')
-            .then(callDefault)
-            .catch(fail);
-          break;
         case 'projects:services:edit':
           import('./pages/projects/services/edit')
             .then(callDefault)
