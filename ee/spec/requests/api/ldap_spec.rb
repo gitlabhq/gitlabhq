254e029bfe91dbc82aff1327266bd2870ff7e0c2
Douwe Maan <douwe@gitlab.com>
2018-02-20 10:41:52 +0000

    Merge branch 'cherry-pick-b01d0422' into 'security-10-5-ee'
    Fix LDAP group sync no longer configurable for regular users (10.5 port)
    
    See merge request gitlab/gitlab-ee!594

--- ee/spec/requests/api/ldap_spec.rb
+++ ee/spec/requests/api/ldap_spec.rb
@@ -17,6 +17,7 @@ describe API::Ldap do
     allow(Gitlab::Auth::LDAP::Config).to receive(:enabled?).and_return(true)
     allow(Gitlab::Auth::LDAP::Adapter).to receive(:new).and_return(adapter)
     allow(adapter).to receive_messages(groups: groups)
+    stub_application_setting(allow_group_owners_to_manage_ldap: false)
   end
 
   describe "GET /ldap/groups" do
@@ -31,6 +32,20 @@ describe API::Ldap do
       it "returns authentication error" do
         get api("/ldap/groups", user)
         expect(response.status).to eq 403
+      end
+    end
+
+    context 'when group owners are allowed to manage LDAP' do
+      before do
+        stub_application_setting(allow_group_owners_to_manage_ldap: true)
+      end
+
+      it "returns an array of ldap groups" do
+        get api("/ldap/groups", user)
+        expect(response.status).to eq 200
+        expect(json_response).to be_an Array
+        expect(json_response.length).to eq 2
+        expect(json_response.first['cn']).to eq 'developers'
       end
     end
 
@@ -57,6 +72,20 @@ describe API::Ldap do
       it "returns authentication error" do
         get api("/ldap/ldapmain/groups", user)
         expect(response.status).to eq 403
+      end
+    end
+
+    context 'when group owners are allowed to manage LDAP' do
+      before do
+        stub_application_setting(allow_group_owners_to_manage_ldap: true)
+      end
+
+      it "returns an array of ldap groups" do
+        get api("/ldap/ldapmain/groups", admin)
+        expect(response.status).to eq 200
+        expect(json_response).to be_an Array
+        expect(json_response.length).to eq 2
+        expect(json_response.first['cn']).to eq 'developers'
       end
     end
 
