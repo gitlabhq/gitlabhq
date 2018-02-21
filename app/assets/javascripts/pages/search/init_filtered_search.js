bd8290d93e4a0669a78c23cecdd1f5d2164d19cf
Jacob Schatz <jschatz@gitlab.com>
2018-02-21 20:22:56 +0000

    Rest of Dispatcher Refactor

--- app/assets/javascripts/pages/search/init_filtered_search.js
+++ app/assets/javascripts/pages/search/init_filtered_search.js
@@ -1,18 +1,9 @@
-import '~/filtered_search/dropdown_emoji';
-import '~/filtered_search/dropdown_hint';
-import '~/filtered_search/dropdown_non_user';
-import '~/filtered_search/dropdown_user';
-import '~/filtered_search/dropdown_utils';
-import '~/filtered_search/filtered_search_dropdown_manager';
-import '~/filtered_search/filtered_search_dropdown';
-import '~/filtered_search/filtered_search_manager';
-import '~/filtered_search/filtered_search_tokenizer';
-import '~/filtered_search/filtered_search_visual_tokens';
+import FilteredSearchManager from '~/filtered_search/filtered_search_manager';
 
 export default ({ page }) => {
-  const filteredSearchEnabled = gl.FilteredSearchManager && document.querySelector('.filtered-search');
+  const filteredSearchEnabled = FilteredSearchManager && document.querySelector('.filtered-search');
   if (filteredSearchEnabled) {
-    const filteredSearchManager = new gl.FilteredSearchManager({ page });
+    const filteredSearchManager = new FilteredSearchManager({ page });
     filteredSearchManager.setup();
   }
 };
