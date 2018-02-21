e73506832036caf216980070fce8de926a845e0f
Clement Ho <ClemMakesApps@gmail.com>
2018-02-21 15:14:58 -0600

    Add export to ee module

--- ee/app/assets/javascripts/pages/projects/tree/show/index.js
+++ ee/app/assets/javascripts/pages/projects/tree/show/index.js
@@ -1,11 +1,11 @@
 import '~/pages/projects/tree/show/index';
 import initPathLocks from 'ee/path_locks';
 
-document.addEventListener('DOMContentLoaded', () => {
+export default () => {
   if (document.querySelector('.js-tree-content').dataset.pathLocksAvailable === 'true') {
     initPathLocks(
       document.querySelector('.js-tree-content').dataset.pathLocksToggle,
       document.querySelector('.js-tree-content').dataset.pathLocksPath,
     );
   }
-});
+};
