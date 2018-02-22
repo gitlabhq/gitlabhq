3bed273d60554be06a9c464ed6f04f71d91c7f28
haseeb <haseebeqx@yahoo.com>
2018-02-22 19:34:35 +0530

    dropdown style fixes

--- app/assets/javascripts/snippet/snippet_embed.js
+++ app/assets/javascripts/snippet/snippet_embed.js
@@ -4,12 +4,16 @@
 
     $('#share-btn').click((event) => {
       event.preventDefault();
+      $('#share-btn').addClass('is-active');
+      $('#embed-btn').removeClass('is-active');
       $('#snippet-url-area').val(`${protocol}//${host + pathname}`);
       $('#embed-action').html('Share');
     });
 
     $('#embed-btn').click((event) => {
       event.preventDefault();
+      $('#embed-btn').addClass('is-active');
+      $('#share-btn').removeClass('is-active');
       const scriptTag = `<script src="${protocol}//${host + pathname}.js"></script>`;
       $('#snippet-url-area').val(scriptTag);
       $('#embed-action').html('Embed');
