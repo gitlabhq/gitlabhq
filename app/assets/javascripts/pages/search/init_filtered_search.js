1d3fc67bc2354b93dfc8b8c8716b61f3838dc61e
Constance Okoghenun <constanceokoghenun@gmail.com>
2018-02-22 11:18:12 +0100

    resolved conflicts in app/assets/javascripts/pages/search/init_filtered_search.js

--- app/assets/javascripts/pages/search/init_filtered_search.js
+++ app/assets/javascripts/pages/search/init_filtered_search.js
@@ -1,6 +1,5 @@
 import FilteredSearchManager from '~/filtered_search/filtered_search_manager';
 
-<<<<<<< HEAD
 export default ({ page, filteredSearchTokenKeys, stateFiltersSelector }) => {
   const filteredSearchEnabled = FilteredSearchManager && document.querySelector('.filtered-search');
   if (filteredSearchEnabled) {
@@ -9,12 +8,6 @@ export default ({ page, filteredSearchTokenKeys, stateFiltersSelector }) => {
       filteredSearchTokenKeys,
       stateFiltersSelector,
     });
-=======
-export default ({ page }) => {
-  const filteredSearchEnabled = FilteredSearchManager && document.querySelector('.filtered-search');
-  if (filteredSearchEnabled) {
-    const filteredSearchManager = new FilteredSearchManager({ page });
->>>>>>> upstream/master
     filteredSearchManager.setup();
   }
 };
