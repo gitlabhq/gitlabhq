3bee921dfd91421d31e864395ab127930394de68
Constance Okoghenun <cokoghenun@gitlab.com>
2018-02-23 21:23:27 +0100

    Removed protected_tags webpack bundle

--- app/assets/javascripts/pages/projects/settings/repository/show/index.js
+++ app/assets/javascripts/pages/projects/settings/repository/show/index.js
@@ -1,3 +1,11 @@
+/* eslint-disable no-new */
+
+import ProtectedTagCreate from '~/protected_tags/protected_tag_create';
+import ProtectedTagEditList from '~/protected_tags/protected_tag_edit_list';
 import initSettingsPanels from '~/settings_panels';
 
-document.addEventListener('DOMContentLoaded', initSettingsPanels);
+document.addEventListener('DOMContentLoaded', () => {
+  new ProtectedTagCreate();
+  new ProtectedTagEditList();
+  initSettingsPanels();
+});
