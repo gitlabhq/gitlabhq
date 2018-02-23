a8c12e952e2679e13c1dce12ecc5f9492546a5d6
James Edwards-Jones <jedwardsjones@gitlab.com>
2018-02-23 06:41:40 +0000

    CI/CD GitHub import sets up service integration
    Sets up service to notify GitHub of pipeline status

--- ee/app/services/ci_cd/github_setup_service.rb
+++ ee/app/services/ci_cd/github_setup_service.rb
@@ -8,12 +8,17 @@ module CiCd
 
     def execute
       create_webhook
+      setup_project_integration
     end
 
     private
 
     def create_webhook
       ::CreateGithubWebhookWorker.perform_async(project.id)
+    end
+
+    def setup_project_integration
+      ::CiCd::GithubIntegrationSetupService.new(project).execute
     end
   end
 end
