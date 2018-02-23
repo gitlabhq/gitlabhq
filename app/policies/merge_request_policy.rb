e74d8994718a547c0a52f498cc5ee53a028c8da2
Bob Van Landuyt <bob@vanlanduyt.co>
2018-02-23 13:28:47 +0100

    Conflicts: app/policies/merge_request_policy.rb

--- app/policies/merge_request_policy.rb
+++ app/policies/merge_request_policy.rb
@@ -1,8 +1,5 @@
 class MergeRequestPolicy < IssuablePolicy
-<<<<<<< HEAD
   prepend EE::MergeRequestPolicy
 
-=======
->>>>>>> upstream/master
   rule { can?(:read_merge_request) | visible_to_user }.enable :read_merge_request_iid
 end
