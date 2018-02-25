b1e3237677f9cdc4e0e792f26dbc3fc642c7ef28
Travis Miller <travis@travismiller.com>
2018-02-24 21:18:46 -0600

    review: add feature introduction detail

--- lib/api/project_export.rb
+++ lib/api/project_export.rb
@@ -10,13 +10,16 @@ module API
     end
     resource :projects, requirements: { id: %r{[^/]+} } do
       desc 'Get export status' do
+        detail 'This feature was introduced in GitLab 10.6.'
         success Entities::ProjectExportStatus
       end
       get ':id/export' do
         present user_project, with: Entities::ProjectExportStatus
       end
 
-      desc 'Download export'
+      desc 'Download export' do
+        detail 'This feature was introduced in GitLab 10.6.'
+      end
       get ':id/export/download' do
         path = user_project.export_project_path
 
@@ -25,7 +28,9 @@ module API
         present_file!(path, File.basename(path), 'application/gzip')
       end
 
-      desc 'Start export'
+      desc 'Start export' do
+        detail 'This feature was introduced in GitLab 10.6.'
+      end
       post ':id/export' do
         user_project.add_export_job(current_user: current_user)
 
