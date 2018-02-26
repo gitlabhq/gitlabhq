3073f1aa6e8658825af739440f7288deeef9169d
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2018-02-26 16:57:10 +0200

    Handle excpetion in PluginWorker
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

---
+++ spec/workers/plugin_worker_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe PluginWorker do
+  include RepoHelpers
+
+  subject { described_class.new }
+
+  let(:filename) { 'my_plugin.rb' }
+
+  describe '#perform' do
+    it 'executes Gitlab::Plugin with expected values' do
+      data = { 'event_name' => 'project_create' }
+
+      allow(Gitlab::Plugin).to receive(:execute).with(filename, data).and_return(true)
+
+      expect(subject.perform(filename, data)).to be_truthy
+    end
+
+    it 'handles exception well' do
+      data = { 'event_name' => 'project_create' }
+
+      allow(Gitlab::Plugin).to receive(:execute).with(filename, data).and_raise('Permission denied')
+
+      expect { subject.perform(filename, data) }.to_not raise_error
+    end
+  end
+end
