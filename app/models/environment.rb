637c9558627655aaf08066371f0a5d50461578be
Pawel Chojnacki <pawel@chojnacki.ws>
2018-02-26 12:40:40 +0100

    Fix failing test, when deployment platform is not bound to a cluster.

--- app/models/environment.rb
+++ app/models/environment.rb
@@ -233,8 +233,10 @@ class Environment < ActiveRecord::Base
   end
 
   def cluster_prometheus_adapter
-    cluster = project.deployment_platform&.cluster
-    return unless cluster&.application_prometheus&.installed?
+    return unless project.deployment_platform.respond_to?(:cluster)
+
+    cluster = project.deployment_platform.cluster
+    return unless cluster.application_prometheus&.installed?
 
     cluster.application_prometheus
   end
