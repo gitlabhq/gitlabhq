0a091b207a49dcb6fa752f79a102541895ffd0c4
Clement Ho <ClemMakesApps@gmail.com>
2018-02-26 12:25:28 -0600

    Remove protected branches webpack bundle tag (EE)

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -72,7 +72,6 @@ var config = {
     pipelines_details:    './pipelines/pipeline_details_bundle.js',
     profile:              './profile/profile_bundle.js',
     project_import_gl:    './projects/project_import_gitlab_project.js',
-    protected_branches:   './protected_branches',
     protected_tags:       './protected_tags',
     registry_list:        './registry/index.js',
     sidebar:              './sidebar/sidebar_bundle.js',
