847ca9d5798c6adcb61d28e6d28cae3886be0b5f
Stan Hu <stanhu@gmail.com>
2018-02-27 14:28:13 -0800

    Resolve conflicts in app/services/members/destroy_service.rb

--- app/services/members/destroy_service.rb
+++ app/services/members/destroy_service.rb
@@ -1,6 +1,5 @@
 module Members
   class DestroyService < Members::BaseService
-<<<<<<< HEAD
     prepend EE::Members::DestroyService
 
     def execute(member, skip_authorization: false)
@@ -12,17 +11,6 @@ module Members
         unassign_issues_and_merge_requests(member) unless member.invite?
         member.notification_setting&.destroy
 
-=======
-    def execute(member, skip_authorization: false)
-      raise Gitlab::Access::AccessDeniedError unless skip_authorization || can_destroy_member?(member)
-
-      return member if member.is_a?(GroupMember) && member.source.last_owner?(member.user)
-
-      Member.transaction do
-        unassign_issues_and_merge_requests(member) unless member.invite?
-        member.notification_setting&.destroy
-
->>>>>>> upstream/master
         member.destroy
       end
 
@@ -49,43 +37,7 @@ module Members
         :destroy_project_member
       else
         raise "Unknown member type: #{member}!"
-<<<<<<< HEAD
       end
-    end
-
-    def unassign_issues_and_merge_requests(member)
-      if member.is_a?(GroupMember)
-        issues = Issue.unscoped.select(1)
-                 .joins(:project)
-                 .where('issues.id = issue_assignees.issue_id AND projects.namespace_id = ?', member.source_id)
-
-        # DELETE FROM issue_assignees WHERE user_id = X AND EXISTS (...)
-        IssueAssignee.unscoped
-          .where('user_id = :user_id AND EXISTS (:sub)', user_id: member.user_id, sub: issues)
-          .delete_all
-
-        MergeRequestsFinder.new(current_user, group_id: member.source_id, assignee_id: member.user_id)
-          .execute
-          .update_all(assignee_id: nil)
-      else
-        project = member.source
-
-        # SELECT 1 FROM issues WHERE issues.id = issue_assignees.issue_id AND issues.project_id = X
-        issues = Issue.unscoped.select(1)
-                 .where('issues.id = issue_assignees.issue_id')
-                 .where(project_id: project.id)
-
-        # DELETE FROM issue_assignees WHERE user_id = X AND EXISTS (...)
-        IssueAssignee.unscoped
-          .where('user_id = :user_id AND EXISTS (:sub)', user_id: member.user_id, sub: issues)
-          .delete_all
-
-        project.merge_requests.opened.assigned_to(member.user).update_all(assignee_id: nil)
-=======
->>>>>>> upstream/master
-      end
-
-      member.user.invalidate_cache_counts
     end
 
     def unassign_issues_and_merge_requests(member)
