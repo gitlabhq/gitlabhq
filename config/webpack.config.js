fef151337781d3f9f21eeb644f43342bb509df30
Mike Greiling <mike@pixelcog.com>
2018-02-27 00:03:52 -0600

    configure webpack to watch for new auto-entries instead of restarting webpack with nodemon

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -21,10 +21,16 @@ var DEV_SERVER_LIVERELOAD = process.env.DEV_SERVER_LIVERELOAD !== 'false';
 var WEBPACK_REPORT = process.env.WEBPACK_REPORT;
 var NO_COMPRESSION = process.env.NO_COMPRESSION;
 
+var autoEntriesCount = 0;
+var watchAutoEntries = [];
+
 function generateEntries() {
   // generate automatic entry points
   var autoEntries = {};
   var pageEntries = glob.sync('pages/**/index.js', { cwd: path.join(ROOT_PATH, 'app/assets/javascripts') });
+  watchAutoEntries = [
+    path.join(ROOT_PATH, 'app/assets/javascripts/pages/'),
+  ];
 
   function generateAutoEntries(path, prefix = '.') {
     const chunkPath = path.replace(/\/index\.js$/, '');
@@ -34,14 +40,12 @@ function generateEntries() {
 
   pageEntries.forEach(( path ) => generateAutoEntries(path));
 
+  // EE-specific auto entries
   const eePageEntries = glob.sync('pages/**/index.js', { cwd: path.join(ROOT_PATH, 'ee/app/assets/javascripts') });
   eePageEntries.forEach(( path ) => generateAutoEntries(path, 'ee'));
+  watchAutoEntries.concat(path.join(ROOT_PATH, 'ee/app/assets/javascripts/pages/'));
 
-  // report our auto-generated bundle count
-  if (IS_DEV_SERVER) {
-    var autoEntriesCount = Object.keys(autoEntries).length;
-    console.log(`${autoEntriesCount} entries from '/pages' automatically added to webpack output.`);
-  }
+  autoEntriesCount = Object.keys(autoEntries).length;
 
   const manualEntries = {
     balsamiq_viewer:      './blob/balsamiq_viewer.js',
@@ -378,7 +382,24 @@ if (IS_DEV_SERVER) {
   };
   config.plugins.push(
     // watch node_modules for changes if we encounter a missing module compile error
-    new WatchMissingNodeModulesPlugin(path.join(ROOT_PATH, 'node_modules'))
+    new WatchMissingNodeModulesPlugin(path.join(ROOT_PATH, 'node_modules')),
+
+    // watch for changes to our automatic entry point modules
+    {
+      apply(compiler) {
+        compiler.plugin('emit', (compilation, callback) => {
+          compilation.contextDependencies = [
+            ...compilation.contextDependencies,
+            ...watchAutoEntries,
+          ];
+
+          // report our auto-generated bundle count
+          console.log(`${autoEntriesCount} entries from '/pages' automatically added to webpack output.`);
+
+          callback();
+        })
+      },
+    },
   );
   if (DEV_SERVER_LIVERELOAD) {
     config.plugins.push(new webpack.HotModuleReplacementPlugin());
