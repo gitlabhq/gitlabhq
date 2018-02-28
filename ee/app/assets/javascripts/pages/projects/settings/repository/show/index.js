2e63c8b6eaf1fc1ee1916e0971702970c982596d
Constance Okoghenun <constanceokoghenun@gmail.com>
2018-02-28 23:30:45 +0100

    Added conditional loading of protected tag bundles

--- ee/app/assets/javascripts/pages/projects/settings/repository/show/index.js
+++ ee/app/assets/javascripts/pages/projects/settings/repository/show/index.js
@@ -3,14 +3,21 @@ import UsersSelect from '~/users_select';
 import UserCallout from '~/user_callout';
 import initSettingsPanels from '~/settings_panels';
 import initDeployKeys from '~/deploy_keys';
-import ProtectedTagCreate from 'ee/protected_tags/protected_tag_create';
-import ProtectedTagEditList from 'ee/protected_tags/protected_tag_edit_list';
+import ProtectedTagCreateEE from 'ee/protected_tags/protected_tag_create';
+import ProtectedTagEditListEE from 'ee/protected_tags/protected_tag_edit_list';
+import ProtectedTagCreateCE from '~/protected_tags/protected_tag_create';
+import ProtectedTagEditListCE from '~/protected_tags/protected_tag_edit_list';
 
 document.addEventListener('DOMContentLoaded', () => {
   new UsersSelect();
   new UserCallout();
-  new ProtectedTagCreate();
-  new ProtectedTagEditList();
+  if (document.querySelector('.js-protected-refs-for-users')) {
+    new ProtectedTagCreateEE();
+    new ProtectedTagEditListEE();
+  } else {
+    new ProtectedTagCreateCE();
+    new ProtectedTagEditListCE();
+  }
   initDeployKeys();
   initSettingsPanels();
 });
