83b3e9fa40571c7475bc803b15e9b57ad2dc36b8
Winnie Hellmann <winnie@gitlab.com>
2018-02-28 21:42:34 +0000

    Update inline_javascript.rb

--- lib/haml_lint/inline_javascript.rb
+++ lib/haml_lint/inline_javascript.rb
@@ -6,8 +6,8 @@ unless Rails.env.production?
   module HamlLint
     class Linter::InlineJavaScript < Linter
       include LinterRegistry
-
-      def visit_filter(node)
+      
+      def visit_tag(node)
         return unless node.tag_name == 'script'
 
         record_lint(node, 'Inline JavaScript is discouraged (https://docs.gitlab.com/ee/development/gotchas.html#do-not-use-inline-javascript-in-views)')
