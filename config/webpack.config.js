eefd17437c201418b7336493eb320a129a5b8abe
Mike Greiling <mike@pixelcog.com>
2018-02-28 15:22:15 -0600

    remove the "test" bundle and apply it conditionally within main.js

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -56,7 +56,6 @@ function generateEntries() {
     main:                 './main.js',
     ide:                  './ide/index.js',
     raven:                './raven/index.js',
-    test:                 './test.js',
     webpack_runtime:      './webpack.js',
   };
 
