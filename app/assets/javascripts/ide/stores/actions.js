edf2268c225b07a15f88a2e050e8cd39b98f08fb
Rémy Coutable <remy@rymai.me>
2018-02-28 19:29:56 +0100

    Resolve conflict in app/assets/javascripts/ide/stores/actions.js
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- app/assets/javascripts/ide/stores/actions.js
+++ app/assets/javascripts/ide/stores/actions.js
@@ -1,10 +1,5 @@
 import Vue from 'vue';
 import { visitUrl } from '~/lib/utils/url_utility';
-<<<<<<< HEAD
-=======
-import flash from '~/flash';
-import service from '../services';
->>>>>>> upstream/master
 import * as types from './mutation_types';
 
 export const redirectToUrl = (_, url) => visitUrl(url);
