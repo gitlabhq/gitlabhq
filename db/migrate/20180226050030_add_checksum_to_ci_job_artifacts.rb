c2954f38150fa5654fe63dd37f36de550e5f3679
Shinya Maeda <shinya@gitlab.com>
2018-02-28 20:49:50 +0900

    Change column type to binary from string

--- db/migrate/20180226050030_add_checksum_to_ci_job_artifacts.rb
+++ db/migrate/20180226050030_add_checksum_to_ci_job_artifacts.rb
@@ -2,7 +2,6 @@ class AddChecksumToCiJobArtifacts < ActiveRecord::Migration
   DOWNTIME = false
 
   def change
-    add_column :ci_job_artifacts, :checksum, :string, limit: 64
+    add_column :ci_job_artifacts, :checksum, :binary
   end
 end
-
