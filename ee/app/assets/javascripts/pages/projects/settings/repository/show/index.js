e8844652938acd5b19ef13c31f2f403bebff5775
Constance Okoghenun <constanceokoghenun@gmail.com>
2018-02-28 23:56:36 +0100

    Switched naming convention for improted CE files

--- ee/app/assets/javascripts/pages/projects/settings/repository/show/index.js
+++ ee/app/assets/javascripts/pages/projects/settings/repository/show/index.js
@@ -3,20 +3,20 @@ import UsersSelect from '~/users_select';
 import UserCallout from '~/user_callout';
 import initSettingsPanels from '~/settings_panels';
 import initDeployKeys from '~/deploy_keys';
-import ProtectedTagCreateEE from 'ee/protected_tags/protected_tag_create';
-import ProtectedTagEditListEE from 'ee/protected_tags/protected_tag_edit_list';
-import ProtectedTagCreateCE from '~/protected_tags/protected_tag_create';
-import ProtectedTagEditListCE from '~/protected_tags/protected_tag_edit_list';
+import ProtectedTagCreate from 'ee/protected_tags/protected_tag_create';
+import ProtectedTagEditList from 'ee/protected_tags/protected_tag_edit_list';
+import CEProtectedTagCreate from '~/protected_tags/protected_tag_create';
+import CEProtectedTagEditList from '~/protected_tags/protected_tag_edit_list';
 
 document.addEventListener('DOMContentLoaded', () => {
   new UsersSelect();
   new UserCallout();
   if (document.querySelector('.js-protected-refs-for-users')) {
-    new ProtectedTagCreateEE();
-    new ProtectedTagEditListEE();
+    new ProtectedTagCreate();
+    new ProtectedTagEditList();
   } else {
-    new ProtectedTagCreateCE();
-    new ProtectedTagEditListCE();
+    new CEProtectedTagCreate();
+    new CEProtectedTagEditList();
   }
   initDeployKeys();
   initSettingsPanels();
