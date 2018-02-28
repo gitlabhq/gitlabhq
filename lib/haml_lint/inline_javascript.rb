25283e71011490c674c54e64384957a692235e38
Connor Shea <connor.james.shea@gmail.com>
2018-02-28 15:58:12 -0700

    Update inline_javascript.rb to lint uses of the javascript filter as well as script tags.

--- lib/haml_lint/inline_javascript.rb
+++ lib/haml_lint/inline_javascript.rb
@@ -6,7 +6,13 @@ unless Rails.env.production?
   module HamlLint
     class Linter::InlineJavaScript < Linter
       include LinterRegistry
-      
+
+      def visit_filter(node)
+        return unless node.filter_type == 'javascript'
+
+        record_lint(node, 'Inline JavaScript is discouraged (https://docs.gitlab.com/ee/development/gotchas.html#do-not-use-inline-javascript-in-views)')
+      end
+
       def visit_tag(node)
         return unless node.tag_name == 'script'
 
