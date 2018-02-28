6417d8c9d0d9ce82066f673300897a66559540e3
Clement Ho <clemmakesapps@gmail.com>
2018-02-28 22:46:04 +0000

    Fix casing

--- ee/app/assets/javascripts/pages/projects/settings/repository/show/index.js
+++ ee/app/assets/javascripts/pages/projects/settings/repository/show/index.js
@@ -2,8 +2,8 @@ import initSettingsPanels from '~/settings_panels';
 import initDeployKeys from '~/deploy_keys';
 import UsersSelect from '~/users_select';
 import UserCallout from '~/user_callout';
-import CEprotectedBranchCreate from '~/protected_branches/protected_branch_create';
-import CEprotectedBranchEditList from '~/protected_branches/protected_branch_edit_list';
+import CEProtectedBranchCreate from '~/protected_branches/protected_branch_create';
+import CEProtectedBranchEditList from '~/protected_branches/protected_branch_edit_list';
 
 import ProtectedBranchCreate from 'ee/protected_branches/protected_branch_create';
 import ProtectedBranchEditList from 'ee/protected_branches/protected_branch_edit_list';
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
     new ProtectedBranchCreate(); // eslint-disable-line no-new
     new ProtectedBranchEditList(); // eslint-disable-line no-new
   } else {
-    new CEprotectedBranchCreate(); // eslint-disable-line no-new
-    new CEprotectedBranchEditList(); // eslint-disable-line no-new
+    new CEProtectedBranchCreate(); // eslint-disable-line no-new
+    new CEProtectedBranchEditList(); // eslint-disable-line no-new
   }
 });
