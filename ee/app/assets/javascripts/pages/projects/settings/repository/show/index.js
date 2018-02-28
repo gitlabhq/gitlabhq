756e5844d1ca06242d4e6c168945827539c5d0d8
Clement Ho <ClemMakesApps@gmail.com>
2018-02-28 16:04:24 -0600

    Add element check for when to initialize EE feature

--- ee/app/assets/javascripts/pages/projects/settings/repository/show/index.js
+++ ee/app/assets/javascripts/pages/projects/settings/repository/show/index.js
@@ -2,6 +2,8 @@ import initSettingsPanels from '~/settings_panels';
 import initDeployKeys from '~/deploy_keys';
 import UsersSelect from '~/users_select';
 import UserCallout from '~/user_callout';
+import CEprotectedBranchCreate from '~/protected_branches/protected_branch_create';
+import CEprotectedBranchEditList from '~/protected_branches/protected_branch_edit_list';
 
 import ProtectedBranchCreate from 'ee/protected_branches/protected_branch_create';
 import ProtectedBranchEditList from 'ee/protected_branches/protected_branch_edit_list';
@@ -9,8 +11,14 @@ import ProtectedBranchEditList from 'ee/protected_branches/protected_branch_edit
 document.addEventListener('DOMContentLoaded', () => {
   initDeployKeys();
   initSettingsPanels();
-  new ProtectedBranchCreate(); // eslint-disable-line no-new
-  new ProtectedBranchEditList(); // eslint-disable-line no-new
   new UsersSelect(); // eslint-disable-line no-new
   new UserCallout(); // eslint-disable-line no-new
+
+  if (document.querySelector('.js-protected-refs-for-users')) {
+    new ProtectedBranchCreate(); // eslint-disable-line no-new
+    new ProtectedBranchEditList(); // eslint-disable-line no-new
+  } else {
+    new CEprotectedBranchCreate(); // eslint-disable-line no-new
+    new CEprotectedBranchEditList(); // eslint-disable-line no-new
+  }
 });
