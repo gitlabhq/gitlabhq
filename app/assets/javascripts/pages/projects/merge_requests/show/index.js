f7351ec0a420051d85f6968ca5bc935a4dc8162d
Simon Knox <psimyn@gmail.com>
2018-02-28 21:43:36 +1100

    resolve conflict
    content is the same as init_merge_request_show.js

--- app/assets/javascripts/pages/projects/merge_requests/show/index.js
+++ app/assets/javascripts/pages/projects/merge_requests/show/index.js
@@ -2,26 +2,6 @@ import initSidebarBundle from '~/sidebar/sidebar_bundle';
 import initShow from '../init_merge_request_show';
 
 document.addEventListener('DOMContentLoaded', () => {
-<<<<<<< HEAD
   initShow();
   initSidebarBundle();
-=======
-  new Diff(); // eslint-disable-line no-new
-  new ZenMode(); // eslint-disable-line no-new
-
-  initIssuableSidebar();
-  initNotes();
-  initDiffNotes();
-  initPipelines();
-
-  const mrShowNode = document.querySelector('.merge-request');
-  window.mergeRequest = new MergeRequest({
-    action: mrShowNode.dataset.mrAction,
-  });
-
-  new ShortcutsIssuable(true); // eslint-disable-line no-new
-  handleLocationHash();
-  howToMerge();
-  initWidget();
->>>>>>> origin/master
 });
