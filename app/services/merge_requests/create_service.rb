6297446d1773c95d86ecd31f591e1829b431f378
Oswaldo Ferreira <oswaldo@gitlab.com>
2018-03-01 11:32:39 -0300

    Move wip handling to MergeRequest::BaseService

--- app/services/merge_requests/create_service.rb
+++ app/services/merge_requests/create_service.rb
@@ -34,6 +34,12 @@ module MergeRequests
       super
     end
 
+    # Override from IssuableBaseService
+    def handle_quick_actions(merge_request)
+      super
+      handle_wip_event(merge_request)
+    end
+
     private
 
     def update_merge_requests_head_pipeline(merge_request)
