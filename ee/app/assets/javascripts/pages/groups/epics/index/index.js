40b5d034e606d5baa6b1d422ff3893cbf855a8b4
Mike Greiling <mike@pixelcog.com>
2018-03-01 09:35:39 -0600

    remove filtered_search bundle from epics page

--- ee/app/assets/javascripts/pages/groups/epics/index/index.js
+++ ee/app/assets/javascripts/pages/groups/epics/index/index.js
@@ -1,14 +1,10 @@
-import FilteredSearchManager from '~/filtered_search/filtered_search_manager';
+import initFilteredSearch from '~/pages/search/init_filtered_search';
 import FilteredSearchTokenKeysEpics from 'ee/filtered_search/filtered_search_token_keys_epics';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const filteredSearchEnabled = FilteredSearchManager && document.querySelector('.filtered-search');
-  if (filteredSearchEnabled) {
-    const filteredSearchManager = new FilteredSearchManager({
-      page: 'epics',
-      filteredSearchTokenKeys: FilteredSearchTokenKeysEpics,
-      stateFiltersSelector: '.epics-state-filters',
-    });
-    filteredSearchManager.setup();
-  }
+  initFilteredSearch({
+    page: 'epics',
+    filteredSearchTokenKeys: FilteredSearchTokenKeysEpics,
+    stateFiltersSelector: '.epics-state-filters',
+  });
 });
