0e732fa466c4d1c48163472b268dded18cc39407
Micaël Bergeron <mbergeron@gitlab.com>
2018-03-01 11:18:37 -0500

    removed EE specific code from the port

--- ee/app/models/geo/fdw/lfs_object.rb
+++
@@ -1,9 +0,0 @@
-module Geo
-  module Fdw
-    class LfsObject < ::Geo::BaseFdw
-      self.table_name = Gitlab::Geo.fdw_table('lfs_objects')
-
-      scope :with_files_stored_locally, -> { where(file_store: [nil, LfsObjectUploader::Store::LOCAL]) }
-    end
-  end
-end
