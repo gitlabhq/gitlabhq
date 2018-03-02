738d03fd830cc63da5c64f4f1299202e1b94014d
Constance Okoghenun <constanceokoghenun@gmail.com>
2018-03-02 15:35:19 +0100

    Removed burndown_chart webpack bundle

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -64,7 +64,6 @@ function generateEntries() {
 
     // EE-only
     add_gitlab_slack_application: 'ee/add_gitlab_slack_application/index.js',
-    burndown_chart:       'ee/burndown_chart/index.js',
     geo_nodes:            'ee/geo_nodes',
     ldap_group_links:     'ee/groups/ldap_group_links.js',
     mirrors:              'ee/mirrors',
