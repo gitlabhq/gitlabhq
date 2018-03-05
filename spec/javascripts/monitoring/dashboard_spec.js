75b98d051f7c34bd91568c466b3c2c66ea17d5e3
Mike Greiling <mike@pixelcog.com>
2018-03-05 17:39:56 -0600

    fix karma tests

--- spec/javascripts/monitoring/dashboard_spec.js
+++ spec/javascripts/monitoring/dashboard_spec.js
@@ -8,6 +8,20 @@ describe('Dashboard', () => {
   const fixtureName = 'environments/metrics/metrics.html.raw';
   let DashboardComponent;
   let component;
+  const propsData = {
+    hasMetrics: 'false',
+    documentationPath: '/path/to/docs',
+    settingsPath: '/path/to/settings',
+    clustersPath: '/path/to/clusters',
+    tagsPath: '/path/to/tags',
+    projectPath: '/path/to/project',
+    metricsEndpoint: mockApiEndpoint,
+    deploymentEndpoint: '/endpoint/deployments',
+    emptyGettingStartedSvgPath: '/path/to/getting-started.svg',
+    emptyLoadingSvgPath: '/path/to/loading.svg',
+    emptyUnableToConnectSvgPath: '/path/to/unable-to-connect.svg',
+  };
+
   preloadFixtures(fixtureName);
 
   beforeEach(() => {
@@ -19,6 +33,7 @@ describe('Dashboard', () => {
     it('shows a getting started empty state when no metrics are present', () => {
       component = new DashboardComponent({
         el: document.querySelector('#prometheus-graphs'),
+        propsData,
       });
 
       component.$mount();
@@ -30,7 +45,6 @@ describe('Dashboard', () => {
   describe('requests information to the server', () => {
     let mock;
     beforeEach(() => {
-      document.querySelector('#prometheus-graphs').setAttribute('data-has-metrics', 'true');
       mock = new MockAdapter(axios);
       mock.onGet(mockApiEndpoint).reply(200, {
         metricsGroupsAPIResponse,
@@ -44,6 +58,7 @@ describe('Dashboard', () => {
     it('shows up a loading state', (done) => {
       component = new DashboardComponent({
         el: document.querySelector('#prometheus-graphs'),
+        propsData: { ...propsData, hasMetrics: 'true' },
       });
       component.$mount();
       Vue.nextTick(() => {
