0e57f7ac03b0af6c51db53a0d7cb95bbfaf75de8
Shinya Maeda <shinya@gitlab.com>
2018-03-07 00:49:40 +0900

    Remove unnecessary changes

--- app/workers/concerns/object_storage_queue.rb
+++
@@ -1,8 +0,0 @@
-# Concern for setting Sidekiq settings for the various GitLab ObjectStorage workers.
-module ObjectStorageQueue
-  extend ActiveSupport::Concern
-
-  included do
-    queue_namespace :object_storage
-  end
-end
