68bcd4feb141c402fb58385837541c2521bf6570
Mike Greiling <mike@pixelcog.com>
2018-03-06 01:43:12 -0600

    move ee-specific files into the ee namespace

--- app/assets/javascripts/pages/projects/clusters/show/index.js
+++ app/assets/javascripts/pages/projects/clusters/show/index.js
@@ -1,7 +1,5 @@
 import ClustersBundle from '~/clusters/clusters_bundle';
-import initClusterHealth from './cluster_health';
 
 document.addEventListener('DOMContentLoaded', () => {
   new ClustersBundle(); // eslint-disable-line no-new
-  initClusterHealth();
 });
