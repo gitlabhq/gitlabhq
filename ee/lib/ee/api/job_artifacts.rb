a538305427a07c682694971f0fb00cd5eea651f8
Lin Jen-Shin <godfat@godfat.org>
2018-03-06 23:00:13 +0800

    Use ActiveSupport::Concern

--- ee/lib/ee/api/job_artifacts.rb
+++ ee/lib/ee/api/job_artifacts.rb
@@ -1,18 +1,18 @@
 module EE
   module API
     module JobArtifacts
-      def self.prepended(api)
-        api.module_eval do
-          helpers do
-            def authorize_download_artifacts!
-              super
-              check_cross_project_pipelines_feature!
-            end
+      extend ActiveSupport::Concern
 
-            def check_cross_project_pipelines_feature!
-              if job_token_authentication? && !@project.feature_available?(:cross_project_pipelines)
-                not_found!('Project')
-              end
+      prepended do
+        helpers do
+          def authorize_download_artifacts!
+            super
+            check_cross_project_pipelines_feature!
+          end
+
+          def check_cross_project_pipelines_feature!
+            if job_token_authentication? && !@project.feature_available?(:cross_project_pipelines)
+              not_found!('Project')
             end
           end
         end
