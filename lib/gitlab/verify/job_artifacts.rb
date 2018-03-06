4419d7ea1fa780066016c9527b96dba4f15d3f61
Nick Thomas <nick@gitlab.com>
2018-03-06 18:46:36 +0000

    Implement foreground verification of CI artifacts

---
+++ lib/gitlab/verify/job_artifacts.rb
@@ -0,0 +1,27 @@
+module Gitlab
+  module Verify
+    class JobArtifacts < BatchVerifier
+      def name
+        'Job artifacts'
+      end
+
+      def describe(object)
+        "Job artifact: #{object.id}"
+      end
+
+      private
+
+      def relation
+        ::Ci::JobArtifact.all
+      end
+
+      def expected_checksum(artifact)
+        artifact.file_sha256
+      end
+
+      def actual_checksum(artifact)
+        Digest::SHA256.file(artifact.file.path).hexdigest
+      end
+    end
+  end
+end
