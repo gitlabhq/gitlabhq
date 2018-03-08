bc55aeb40df5a4845a36ef496317cedda59939b9
Yorick Peterse <yorickpeterse@gmail.com>
2018-03-08 12:54:57 +0000

    Merge branch 'sh-add-missing-acts-as-taggable-indices' into 'master'
    Adding missing indexes migrations from taggings table
    
    Closes #43927
    
    See merge request gitlab-org/gitlab-ce!17563

--- db/schema.rb
+++ db/schema.rb
@@ -1732,7 +1732,9 @@ ActiveRecord::Schema.define(version: 20180307012445) do
   end
 
   add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true, using: :btree
+  add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id", using: :btree
   add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context", using: :btree
+  add_index "taggings", ["taggable_id", "taggable_type"], name: "index_taggings_on_taggable_id_and_taggable_type", using: :btree
 
   create_table "tags", force: :cascade do |t|
     t.string "name"
