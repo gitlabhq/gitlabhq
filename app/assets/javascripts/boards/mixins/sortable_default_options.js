1bf0261ca8e3de45f0284b193d8a68c12f79423d
Lin Jen-Shin <godfat@godfat.org>
2018-03-12 21:31:49 +0800

    Resolve app/assets/javascripts/boards/mixins/sortable_default_options.js

--- app/assets/javascripts/boards/mixins/sortable_default_options.js
+++ app/assets/javascripts/boards/mixins/sortable_default_options.js
@@ -2,11 +2,7 @@
 /* global DocumentTouch */
 
 import $ from 'jquery';
-<<<<<<< HEAD
 import sortableConfig from 'ee/sortable/sortable_config';
-=======
-import sortableConfig from '../../sortable/sortable_config';
->>>>>>> upstream/master
 
 window.gl = window.gl || {};
 window.gl.issueBoards = window.gl.issueBoards || {};
