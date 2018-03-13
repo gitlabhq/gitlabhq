fa271f86d4459f0cd4eeea55aeccfd05c04f64da
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2018-03-13 17:28:27 +0100

    Fix pipeline build specs in EE

--- spec/models/ci/build_spec.rb
+++ spec/models/ci/build_spec.rb
@@ -1916,7 +1916,7 @@ describe Ci::Build do
           allow_any_instance_of(Project)
             .to receive(:predefined_variables) { [project_pre_var] }
 
-          allow_any_instance_of(Project)
+          allow_any_instance_of(EE::Project)
             .to receive(:secret_variables_for)
             .with(ref: 'master', environment: nil) do
             [create(:ci_variable, key: 'secret', value: 'value')]
