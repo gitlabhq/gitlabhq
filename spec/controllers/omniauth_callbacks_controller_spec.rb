7dd97985d11369c62056a4b91b7a88c88443c641
James Lopez <james@gitlab.com>
2018-03-15 15:58:18 +0000

    Merge branch 'fix/auth0-unsafe-login-10-4' into 'security-10-4'
    [10.4] Fix GitLab Auth0 integration signs in the wrong user
    
    See merge request gitlab/gitlabhq!2352

--- spec/controllers/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/omniauth_callbacks_controller_spec.rb
@@ -3,73 +3,90 @@ require 'spec_helper'
 describe OmniauthCallbacksController do
   include LoginHelpers
 
-  let(:user) { create(:omniauth_user, extern_uid: 'my-uid', provider: provider) }
-  let(:provider) { :github }
+  let(:user) { create(:omniauth_user, extern_uid: extern_uid, provider: provider) }
 
   before do
-    mock_auth_hash(provider.to_s, 'my-uid', user.email)
+    mock_auth_hash(provider.to_s, extern_uid, user.email)
     stub_omniauth_provider(provider, context: request)
   end
 
-  it 'allows sign in' do
-    post provider
+  context 'github' do
+    let(:extern_uid) { 'my-uid' }
+    let(:provider) { :github }
 
-    expect(request.env['warden']).to be_authenticated
-  end
-
-  shared_context 'sign_up' do
-    let(:user) { double(email: 'new@example.com') }
-
-    before do
-      stub_omniauth_setting(block_auto_created_users: false)
-    end
-  end
-
-  context 'sign up' do
-    include_context 'sign_up'
-
-    it 'is allowed' do
+    it 'allows sign in' do
       post provider
 
       expect(request.env['warden']).to be_authenticated
     end
-  end
 
-  context 'when OAuth is disabled' do
-    before do
-      stub_env('IN_MEMORY_APPLICATION_SETTINGS', 'false')
-      settings = Gitlab::CurrentSettings.current_application_settings
-      settings.update(disabled_oauth_sign_in_sources: [provider.to_s])
-    end
+    shared_context 'sign_up' do
+      let(:user) { double(email: 'new@example.com') }
 
-    it 'prevents login via POST' do
-      post provider
-
-      expect(request.env['warden']).not_to be_authenticated
-    end
-
-    it 'shows warning when attempting login' do
-      post provider
-
-      expect(response).to redirect_to new_user_session_path
-      expect(flash[:alert]).to eq('Signing in using GitHub has been disabled')
-    end
-
-    it 'allows linking the disabled provider' do
-      user.identities.destroy_all
-      sign_in(user)
-
-      expect { post provider }.to change { user.reload.identities.count }.by(1)
+      before do
+        stub_omniauth_setting(block_auto_created_users: false)
+      end
     end
 
     context 'sign up' do
       include_context 'sign_up'
 
-      it 'is prevented' do
+      it 'is allowed' do
+        post provider
+
+        expect(request.env['warden']).to be_authenticated
+      end
+    end
+
+    context 'when OAuth is disabled' do
+      before do
+        stub_env('IN_MEMORY_APPLICATION_SETTINGS', 'false')
+        settings = Gitlab::CurrentSettings.current_application_settings
+        settings.update(disabled_oauth_sign_in_sources: [provider.to_s])
+      end
+
+      it 'prevents login via POST' do
         post provider
 
         expect(request.env['warden']).not_to be_authenticated
       end
+
+      it 'shows warning when attempting login' do
+        post provider
+
+        expect(response).to redirect_to new_user_session_path
+        expect(flash[:alert]).to eq('Signing in using GitHub has been disabled')
+      end
+
+      it 'allows linking the disabled provider' do
+        user.identities.destroy_all
+        sign_in(user)
+
+        expect { post provider }.to change { user.reload.identities.count }.by(1)
+      end
+
+      context 'sign up' do
+        include_context 'sign_up'
+
+        it 'is prevented' do
+          post provider
+
+          expect(request.env['warden']).not_to be_authenticated
+        end
+      end
+    end
+  end
+
+  context 'auth0' do
+    let(:extern_uid) { '' }
+    let(:provider) { :auth0 }
+
+    it 'does not allow sign in without extern_uid' do
+      post 'auth0'
+
+      expect(request.env['warden']).not_to be_authenticated
+      expect(response.status).to eq(302)
+      expect(controller).to set_flash[:alert].to('Wrong extern UID provided. Make sure Auth0 is configured correctly.')
     end
   end
 end
