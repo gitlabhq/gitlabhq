511cf9345f2b95dd6b8c0aa26450ae8458637d2f
Sean McGivern <sean@gitlab.com>
2018-03-15 12:44:59 +0000

    Fix pipeline serializer spec
    This is 36 in CE, and 40 in EE.

--- spec/serializers/pipeline_serializer_spec.rb
+++ spec/serializers/pipeline_serializer_spec.rb
@@ -118,7 +118,7 @@ describe PipelineSerializer do
         it 'verifies number of queries', :request_store do
           recorded = ActiveRecord::QueryRecorder.new { subject }
 
-          expect(recorded.count).to be_within(1).of(40)
+          expect(recorded.count).to be_within(1).of(36)
           expect(recorded.cached_count).to eq(0)
         end
       end
