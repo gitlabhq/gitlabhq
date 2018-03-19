2b7acb655ee1dbec4aece241d93852fa788783bd
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2018-03-19 12:09:40 +0200

    Merorize report artifacts into instance variables
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- ee/app/models/ee/ci/pipeline.rb
+++ ee/app/models/ee/ci/pipeline.rb
@@ -13,23 +13,23 @@ module EE
       end
 
       def codeclimate_artifact
-        artifacts.codequality.find(&:has_codeclimate_json?)
+        @codeclimate_artifact ||= artifacts.codequality.find(&:has_codeclimate_json?)
       end
 
       def performance_artifact
-        artifacts.performance.find(&:has_performance_json?)
+        @performance_artifact ||= artifacts.performance.find(&:has_performance_json?)
       end
 
       def sast_artifact
-        artifacts.sast.find(&:has_sast_json?)
+        @sast_artifact ||= artifacts.sast.find(&:has_sast_json?)
       end
 
       def sast_container_artifact
-        artifacts.sast_container.find(&:has_sast_container_json?)
+        @sast_container_artifact ||= artifacts.sast_container.find(&:has_sast_container_json?)
       end
 
       def dast_artifact
-        artifacts.dast.find(&:has_dast_json?)
+        @dast_artifact ||= artifacts.dast.find(&:has_dast_json?)
       end
 
       def initialize_yaml_processor
