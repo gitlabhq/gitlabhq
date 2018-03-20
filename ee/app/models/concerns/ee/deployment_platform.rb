3613fed3b81e3c5fa585ac40f34fcea58d2f8ec4
Mayra Cabrera <mcabrera@gitlab.com>
2018-03-20 11:39:33 -0600

    Fix deployment_platform assignation when multiple clusters & environments
    Closes #5347

--- ee/app/models/concerns/ee/deployment_platform.rb
+++ ee/app/models/concerns/ee/deployment_platform.rb
@@ -6,7 +6,7 @@ module EE
     def deployment_platform(environment: nil)
       return super unless environment && feature_available?(:multiple_clusters)
 
-      @deployment_platform ||= # rubocop:disable Gitlab/ModuleWithInstanceVariables
+      @deployment_platform = # rubocop:disable Gitlab/ModuleWithInstanceVariables
         clusters.enabled.on_environment(environment.name)
           .last&.platform_kubernetes
 
