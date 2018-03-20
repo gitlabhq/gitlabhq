1b3ae7fd3e164ba77009d17eeef3a2623ec1b660
Dylan Griffith <dyl.griffith@gmail.com>
2018-03-21 10:03:50 +1100

    Update ProjectStatistics#build_artifacts_size synchronously without summing (#41059)
    Previously we scheduled a worker to just some this but we were running
    into performance issues when the build table was getting too large. So
    now we've updated the code such that this column is updated immediately
    and incremented/decremented by the correct amount whenever artifacts are
    created or deleted.
    
    We've also added the performance optimization that we do not update this
    statistic if a project is deleted because it could result in many
    updates for a project with many builds.

--- app/models/ci/job_artifact.rb
+++ app/models/ci/job_artifact.rb
@@ -1,6 +1,7 @@
 module Ci
   class JobArtifact < ActiveRecord::Base
     prepend EE::Ci::JobArtifact
+
     include AfterCommitQueue
     include ObjectStorage::BackgroundMove
     extend Gitlab::Ci::Model
@@ -10,6 +11,8 @@ module Ci
 
     before_save :update_file_store
     before_save :set_size, if: :file_changed?
+    after_save :update_project_statistics_after_save, if: :size_changed?
+    after_destroy :update_project_statistics_after_destroy, unless: :project_destroyed?
 
     scope :with_files_stored_locally, -> { where(file_store: [nil, ::JobArtifactUploader::Store::LOCAL]) }
     scope :with_files_stored_remotely, -> { where(file_store: ::JobArtifactUploader::Store::REMOTE) }
@@ -36,10 +39,6 @@ module Ci
       [nil, ::JobArtifactUploader::Store::LOCAL].include?(self.file_store)
     end
 
-    def set_size
-      self.size = file.size
-    end
-
     def expire_in
       expire_at - Time.now if expire_at
     end
@@ -49,6 +48,29 @@ module Ci
         if value
           ChronicDuration.parse(value)&.seconds&.from_now
         end
+    end
+
+    private
+
+    def set_size
+      self.size = file.size
+    end
+
+    def update_project_statistics_after_save
+      update_project_statistics(size.to_i - size_was.to_i)
+    end
+
+    def update_project_statistics_after_destroy
+      update_project_statistics(-self.size)
+    end
+
+    def update_project_statistics(difference)
+      ProjectStatistics.increment_statistic(project_id, :build_artifacts_size, difference)
+    end
+
+    def project_destroyed?
+      # Use job.project to avoid extra DB query for project
+      job.project.pending_delete?
     end
   end
 end
