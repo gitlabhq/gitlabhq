3b1abfb94e4ed6192215bc332f69565217ba419b
Robert Speicher <rspeicher@gmail.com>
2018-03-19 20:38:02 -0500

    Resolve conflict in vue_merge_request_widget/dependencies.js

--- app/assets/javascripts/vue_merge_request_widget/dependencies.js
+++ app/assets/javascripts/vue_merge_request_widget/dependencies.js
@@ -27,11 +27,7 @@ export { default as ConflictsState } from './components/states/mr_widget_conflic
 export { default as NothingToMergeState } from './components/states/nothing_to_merge.vue';
 export { default as MissingBranchState } from './components/states/mr_widget_missing_branch.vue';
 export { default as NotAllowedState } from './components/states/mr_widget_not_allowed.vue';
-<<<<<<< HEAD
 export { default as ReadyToMergeState } from 'ee/vue_merge_request_widget/components/states/mr_widget_ready_to_merge';
-=======
-export { default as ReadyToMergeState } from './components/states/mr_widget_ready_to_merge';
->>>>>>> upstream/master
 export { default as ShaMismatchState } from './components/states/sha_mismatch.vue';
 export { default as UnresolvedDiscussionsState } from './components/states/unresolved_discussions.vue';
 export { default as PipelineBlockedState } from './components/states/mr_widget_pipeline_blocked.vue';
