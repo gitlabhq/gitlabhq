43ff14e0c5074311fd544d654108550253e0fd93
Mike Greiling <mike@pixelcog.com>
2018-03-20 00:37:16 -0500

    refactor ConfirmDangerModal into ES module

--- app/assets/javascripts/confirm_danger_modal.js
+++ app/assets/javascripts/confirm_danger_modal.js
@@ -1,33 +1,22 @@
-/* eslint-disable func-names, space-before-function-paren, wrap-iife, one-var, no-var, camelcase, one-var-declaration-per-line, no-else-return, max-len */
-
 import $ from 'jquery';
 import { rstrip } from './lib/utils/common_utils';
 
-window.ConfirmDangerModal = (function() {
-  function ConfirmDangerModal(form, text) {
-    var project_path, submit;
-    this.form = form;
-    $('.js-confirm-text').text(text || '');
-    $('.js-confirm-danger-input').val('');
-    $('#modal-confirm-danger').modal('show');
-    project_path = $('.js-confirm-danger-match').text();
-    submit = $('.js-confirm-danger-submit');
-    submit.disable();
-    $('.js-confirm-danger-input').off('input');
-    $('.js-confirm-danger-input').on('input', function() {
-      if (rstrip($(this).val()) === project_path) {
-        return submit.enable();
-      } else {
-        return submit.disable();
-      }
-    });
-    $('.js-confirm-danger-submit').off('click');
-    $('.js-confirm-danger-submit').on('click', (function(_this) {
-      return function() {
-        return _this.form.submit();
-      };
-    })(this));
-  }
+export default function initConfirmDangerModal($form, text) {
+  $('.js-confirm-text').text(text || '');
+  $('.js-confirm-danger-input').val('');
+  $('#modal-confirm-danger').modal('show');
 
-  return ConfirmDangerModal;
-})();
+  const confirmTextMatch = $('.js-confirm-danger-match').text();
+  const $submit = $('.js-confirm-danger-submit');
+  $submit.disable();
+
+  $('.js-confirm-danger-input').off('input').on('input', function handleInput() {
+    const confirmText = rstrip($(this).val());
+    if (confirmText === confirmTextMatch) {
+      $submit.enable();
+    } else {
+      $submit.disable();
+    }
+  });
+  $('.js-confirm-danger-submit').off('click').on('click', () => $form.submit());
+}
