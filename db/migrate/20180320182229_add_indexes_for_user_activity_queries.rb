5f8abe4783a252c5979d87572ceaeb37b11d59dc
Andreas Brandl <abrandl@gitlab.com>
2018-03-20 21:39:17 +0100

    Flip column order in new index.

--- db/migrate/20180320182229_add_indexes_for_user_activity_queries.rb
+++ db/migrate/20180320182229_add_indexes_for_user_activity_queries.rb
@@ -6,12 +6,12 @@ class AddIndexesForUserActivityQueries < ActiveRecord::Migration
   disable_ddl_transaction!
 
   def up
-    add_concurrent_index :events, [:project_id, :author_id]
+    add_concurrent_index :events, [:author_id, :project_id]
     add_concurrent_index :user_interacted_projects, :user_id
   end
 
   def down
-    remove_concurrent_index :events, [:project_id, :author_id]
+    remove_concurrent_index :events, [:author_id, :project_id]
 
     patch_foreign_keys do
       remove_concurrent_index :user_interacted_projects, :user_id
--- db/schema.rb
+++ db/schema.rb
@@ -727,8 +727,8 @@ ActiveRecord::Schema.define(version: 20180320182229) do
   end
 
   add_index "events", ["action"], name: "index_events_on_action", using: :btree
+  add_index "events", ["author_id", "project_id"], name: "index_events_on_author_id_and_project_id", using: :btree
   add_index "events", ["author_id"], name: "index_events_on_author_id", using: :btree
-  add_index "events", ["project_id", "author_id"], name: "index_events_on_project_id_and_author_id", using: :btree
   add_index "events", ["project_id", "id"], name: "index_events_on_project_id_and_id", using: :btree
   add_index "events", ["target_type", "target_id"], name: "index_events_on_target_type_and_target_id", using: :btree
 
