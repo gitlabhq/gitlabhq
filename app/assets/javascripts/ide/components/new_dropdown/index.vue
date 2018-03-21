481dabd976de24bb501d7fbc366cb0bcc26deb39
Phil Hughes <me@iamphill.com>
2018-03-21 17:28:33 +0000

    reverted blur listener

--- app/assets/javascripts/ide/components/new_dropdown/index.vue
+++ app/assets/javascripts/ide/components/new_dropdown/index.vue
@@ -1,47 +1,49 @@
 <script>
-import { mapActions } from 'vuex';
-import icon from '~/vue_shared/components/icon.vue';
-import newModal from './modal.vue';
-import upload from './upload.vue';
+  import { mapActions } from 'vuex';
+  import icon from '~/vue_shared/components/icon.vue';
+  import newModal from './modal.vue';
+  import upload from './upload.vue';
 
-export default {
-  components: {
-    icon,
-    newModal,
-    upload,
-  },
-  props: {
-    branch: {
-      type: String,
-      required: true,
+  export default {
+    components: {
+      icon,
+      newModal,
+      upload,
     },
-    path: {
-      type: String,
-      required: true,
+    props: {
+      branch: {
+        type: String,
+        required: true,
+      },
+      path: {
+        type: String,
+        required: true,
+      },
     },
-  },
-  data() {
-    return {
-      openModal: false,
-      modalType: '',
-      dropdownOpen: false,
-    };
-  },
-  methods: {
-    ...mapActions(['createTempEntry']),
-    createNewItem(type) {
-      this.modalType = type;
-      this.openModal = true;
-      this.dropdownOpen = false;
+    data() {
+      return {
+        openModal: false,
+        modalType: '',
+        dropdownOpen: false,
+      };
     },
-    hideModal() {
-      this.openModal = false;
+    methods: {
+      ...mapActions([
+        'createTempEntry',
+      ]),
+      createNewItem(type) {
+        this.modalType = type;
+        this.openModal = true;
+        this.dropdownOpen = false;
+      },
+      hideModal() {
+        this.openModal = false;
+      },
+      openDropdown() {
+        this.dropdownOpen = !this.dropdownOpen;
+      },
     },
-    openDropdown() {
-      this.dropdownOpen = !this.dropdownOpen;
-    },
-  },
-};
+  };
 </script>
 
 <template>
@@ -57,7 +59,6 @@ export default {
         class="btn btn-sm btn-default dropdown-toggle add-to-tree"
         aria-label="Create new file or directory"
         @click.stop="openDropdown()"
-        @blur="openDropdown"
       >
         <icon
           name="plus"
