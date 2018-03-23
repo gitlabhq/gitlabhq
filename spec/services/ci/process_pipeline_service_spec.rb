d4ff35c39399c6cf36b087b668dce5ae42c89963
Dylan Griffith <dyl.griffith@gmail.com>
2018-03-23 12:37:30 +1100

    Fix test failure in spec/services/ci/process_pipeline_service_spec.rb

--- spec/services/ci/process_pipeline_service_spec.rb
+++ spec/services/ci/process_pipeline_service_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Ci::ProcessPipelineService, '#execute' do
   let(:user) { create(:user) }
-  let(:project) { create(:project) }
+  let(:project) { create(:project, :repository) }
 
   let(:pipeline) do
     create(:ci_empty_pipeline, ref: 'master', project: project)
