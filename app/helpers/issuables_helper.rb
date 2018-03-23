39cceaa17047d4ad9f320130f5a6362406434eb9
Jan Provaznik <jprovaznik@gitlab.com>
2018-03-23 15:27:15 +0100

    Check if at least one filter is set on dashboard
    When listing issues and merge requests on dasboard page,
    make sure that at least one filter is enabled.
    
    User's id is used in search autocomplete widget instead
    of username, which allows presetting user in filter dropdowns.
    
    Related to #43246
    
    (cherry picked from commit 37193ac0da0e5e33080d6af6e41d8896854ee37e)

--- app/helpers/issuables_helper.rb
+++ app/helpers/issuables_helper.rb
@@ -159,16 +159,22 @@ module IssuablesHelper
     label_names.join(', ')
   end
 
+<<<<<<< HEAD
   def issuables_state_counter_text(issuable_type, state = :all)
+=======
+  def issuables_state_counter_text(issuable_type, state, display_count)
+>>>>>>> 37193ac0da0... Check if at least one filter is set on dashboard
     titles = {
       opened: "Open"
     }
 
     state_title = titles[state] || state.to_s.humanize
-    count = issuables_count_for_state(issuable_type, state)
-
     html = content_tag(:span, state_title)
-    html << " " << content_tag(:span, number_with_delimiter(count), class: 'badge')
+
+    if display_count
+      count = issuables_count_for_state(issuable_type, state)
+      html << " " << content_tag(:span, number_with_delimiter(count), class: 'badge')
+    end
 
     html.html_safe
   end
@@ -191,6 +197,7 @@ module IssuablesHelper
     end
   end
 
+<<<<<<< HEAD
   def issuable_filter_params
     [
       :search,
@@ -202,12 +209,10 @@ module IssuablesHelper
     ]
   end
 
+=======
+>>>>>>> 37193ac0da0... Check if at least one filter is set on dashboard
   def issuable_reference(issuable)
     @show_full_reference ? issuable.to_reference(full: true) : issuable.to_reference(@group || @project)
-  end
-
-  def issuable_filter_present?
-    issuable_filter_params.any? { |k| params.key?(k) }
   end
 
   def issuable_initial_data(issuable)
