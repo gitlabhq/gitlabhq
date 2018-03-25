557c662821a47a41e48af70310514cdd6874c473
James Edwards-Jones <jedwardsjones@gitlab.com>
2018-03-25 12:04:49 +0100

    Branch UnprotectAccessLevels can be set via API

--- app/services/protected_branches/api_service.rb
+++ app/services/protected_branches/api_service.rb
@@ -5,13 +5,15 @@ module ProtectedBranches
     def create
       @push_params = AccessLevelParams.new(:push, params)
       @merge_params = AccessLevelParams.new(:merge, params)
+      @unprotect_params = AccessLevelParams.new(:unprotect, params)
 
       verify_params!
 
       protected_branch_params = {
         name: params[:name],
         push_access_levels_attributes: @push_params.access_levels,
-        merge_access_levels_attributes: @merge_params.access_levels
+        merge_access_levels_attributes: @merge_params.access_levels,
+        unprotect_access_levels_attributes: @unprotect_params.access_levels
       }
 
       ::ProtectedBranches::CreateService.new(@project, @current_user, protected_branch_params).execute
