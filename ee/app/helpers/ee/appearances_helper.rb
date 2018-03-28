d1a54a41f2684b4d194948a70d1bbfde5eed2655
Tiago Botelho <tiagonbotelho@hotmail.com>
2018-03-28 16:19:04 +0100

    Makes use of the current_appearance helper method

--- ee/app/helpers/ee/appearances_helper.rb
+++ ee/app/helpers/ee/appearances_helper.rb
@@ -1,34 +1,34 @@
 module EE
   module AppearancesHelper
-    def header_message(appearance)
-      return unless appearance&.show_header?
+    def header_message
+      return unless current_appearance&.show_header?
 
       class_names = []
       class_names << 'with-performance-bar' if performance_bar_enabled?
 
-      render_message(appearance, :header_message, class_names)
+      render_message(:header_message, class_names)
     end
 
-    def footer_message(appearance)
-      return unless appearance&.show_footer?
+    def footer_message
+      return unless current_appearance&.show_footer?
 
-      render_message(appearance, :footer_message)
+      render_message(:footer_message)
     end
 
     private
 
-    def render_message(appearance, field_sym, class_names = [])
+    def render_message(field_sym, class_names = [])
       class_names << field_sym.to_s.dasherize
 
-      content_tag :div, class: class_names, style: message_style(appearance) do
-        ::Banzai.render_field(appearance, field_sym).html_safe
+      content_tag :div, class: class_names, style: message_style do
+        ::Banzai.render_field(current_appearance, field_sym).html_safe
       end
     end
 
-    def message_style(appearance)
+    def message_style
       style = ''
-      style << "background-color: #{appearance.background_color};"
-      style << "color: #{appearance.font_color}"
+      style << "background-color: #{current_appearance.background_color};"
+      style << "color: #{current_appearance.font_color}"
       style
     end
   end
