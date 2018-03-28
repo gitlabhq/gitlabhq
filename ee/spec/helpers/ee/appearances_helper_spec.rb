61ba8a28c4865e6a1f2ffa3b1869836a9dcdd3db
Tiago Botelho <tiagonbotelho@hotmail.com>
2018-03-28 16:24:36 +0100

    Adds documentation about the system header and footer

--- ee/spec/helpers/ee/appearances_helper_spec.rb
+++ ee/spec/helpers/ee/appearances_helper_spec.rb
@@ -3,38 +3,34 @@ require 'spec_helper'
 describe AppearancesHelper do
   describe '#header_message' do
     it 'returns nil when header message field is not set' do
-      appearance = build(:appearance)
+      appearance = create(:appearance)
 
-      expect(helper.header_message(appearance)).to be_nil
+      expect(helper.header_message).to be_nil
     end
 
     context 'when header message is set' do
-      let(:appearance) { build(:appearance, header_message: message) }
-
       it 'includes current message' do
         message = "Foo bar"
-        appearance = build(:appearance, header_message: message)
+        appearance = create(:appearance, header_message: message)
 
-        expect(helper.header_message(appearance)).to include(message)
+        expect(helper.header_message).to include(message)
       end
     end
   end
 
   describe '#footer_message' do
     it 'returns nil when footer message field is not set' do
-      appearance = build(:appearance)
+      appearance = create(:appearance)
 
-      expect(helper.footer_message(appearance)).to be_nil
+      expect(helper.footer_message).to be_nil
     end
 
     context 'when footer message is set' do
-      let(:appearance) { build(:appearance, header_message: message) }
-
       it 'includes current message' do
         message = "Foo bar"
-        appearance = build(:appearance, footer_message: message)
+        appearance = create(:appearance, footer_message: message)
 
-        expect(helper.footer_message(appearance)).to include(message)
+        expect(helper.footer_message).to include(message)
       end
     end
   end
