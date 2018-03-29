1e017224e05880d8c80b191047990bf805405fe3
Tomasz Maczukin <tomasz@maczukin.pl>
2018-03-29 15:31:12 +0200

    Resolve conflicts in app/models/ci/runner.rb and spec/services/ci/retry_build_service_spec.rb

--- spec/services/ci/retry_build_service_spec.rb
+++ spec/services/ci/retry_build_service_spec.rb
@@ -29,13 +29,8 @@ describe Ci::RetryBuildService do
        commit_id deployments erased_by_id last_deployment project_id
        runner_id tag_taggings taggings tags trigger_request_id
        user_id auto_canceled_by_id retried failure_reason
-<<<<<<< HEAD
        sourced_pipelines artifacts_file_store artifacts_metadata_store
        metadata].freeze # EE
-=======
-       artifacts_file_store artifacts_metadata_store
-       metadata].freeze
->>>>>>> upstream/master
 
   shared_examples 'build duplication' do
     let(:another_pipeline) { create(:ci_empty_pipeline, project: project) }
