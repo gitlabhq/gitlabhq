2c73a8e7207251a72b24fc067f5ce39e8705c232
Luke Bennett <lukeeeebennettplus@gmail.com>
2018-03-29 05:43:11 +0100

    Clean binding and use MirrorPull

--- ee/app/assets/javascripts/pages/projects/settings/repository/show/push_pull.js
+++ ee/app/assets/javascripts/pages/projects/settings/repository/show/push_pull.js
@@ -1,3 +1,5 @@
+import MirrorPull from 'ee/mirrors/mirror_pull';
+
 export default {
   init() {
     this.mirrorDirectionSelect = document.querySelector('.js-mirror-direction');
@@ -10,24 +12,45 @@ export default {
       pull: document.querySelector('.js-pull-mirrors-form').innerHTML,
     };
 
-    this.boundUpdatedForm = this.updateForm.bind(this);
+    this.boundUpdateForm = this.updateForm.bind(this);
+    this.boundUpdateUrl = this.updateUrl.bind(this);
+    this.boundUpdateProtectedBranches = this.updateProtectedBranches.bind(this);
 
-    this.boundUpdatedForm();
+    this.boundUpdateForm();
     this.registerUpdateListeners();
   },
 
   updateForm() {
-    this.insertionPoint.innerHTML = this.directionFormMap[this.mirrorDirectionSelect.value];
+    const direction = this.mirrorDirectionSelect.value;
+    this.insertionPoint.innerHTML = this.directionFormMap[direction];
 
     setTimeout(() => {
-      document.querySelector('.js-mirror-url-hidden').value = this.urlInput.value;
-      document.querySelector('.js-mirror-protected-hidden').value = this.protectedBranchesInput.checked ? this.protectedBranchesInput.value : '0';
+      this.updateUrl();
+      this.updateProtectedBranches();
+
+      if (direction !== 'pull') return;
+
+      this.initMirrorPull();
     }, 0);
   },
 
+  updateUrl() {
+    document.querySelector('.js-mirror-url-hidden').value = this.urlInput.value;
+  },
+
+  updateProtectedBranches() {
+    document.querySelector('.js-mirror-protected-hidden').value = this.protectedBranchesInput.checked ? this.protectedBranchesInput.value : '0';
+  },
+
+  initMirrorPull() {
+    const mirrorPull = new MirrorPull('.js-mirror-form');
+
+    if (mirrorPull) mirrorPull.init();
+  },
+
   registerUpdateListeners() {
-    this.mirrorDirectionSelect.addEventListener('change', this.boundUpdatedForm);
-    this.urlInput.addEventListener('change', this.boundUpdatedForm);
-    this.protectedBranchesInput.addEventListener('change', this.boundUpdatedForm);
+    this.mirrorDirectionSelect.addEventListener('change', this.boundUpdateForm);
+    this.urlInput.addEventListener('change', this.boundUpdateUrl);
+    this.protectedBranchesInput.addEventListener('change', this.boundUpdateProtectedBranches);
   },
 };
