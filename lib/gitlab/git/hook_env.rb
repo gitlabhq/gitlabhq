b94246278843477f9a9e05097e162300df76bd8c
Jacob Vosmaer (GitLab) <jacob@gitlab.com>
2018-03-30 09:19:46 +0000

    Remove support for absolute dirs from Git::Env

--- lib/gitlab/git/hook_env.rb
+++ lib/gitlab/git/hook_env.rb
@@ -3,46 +3,44 @@
 module Gitlab
   module Git
     # Ephemeral (per request) storage for environment variables that some Git
-    # commands may need.
+    # commands need during internal API calls made from Git push hooks.
     #
     # For example, in pre-receive hooks, new objects are put in a temporary
     # $GIT_OBJECT_DIRECTORY. Without it set, the new objects cannot be retrieved
     # (this would break push rules for instance).
     #
     # This class is thread-safe via RequestStore.
-    class Env
+    class HookEnv
       WHITELISTED_VARIABLES = %w[
-        GIT_OBJECT_DIRECTORY
         GIT_OBJECT_DIRECTORY_RELATIVE
-        GIT_ALTERNATE_OBJECT_DIRECTORIES
         GIT_ALTERNATE_OBJECT_DIRECTORIES_RELATIVE
       ].freeze
 
-      def self.set(env)
+      def self.set(gl_repository, env)
         return unless RequestStore.active?
 
-        RequestStore.store[:gitlab_git_env] = whitelist_git_env(env)
+        raise "missing gl_repository" if gl_repository.blank?
+
+        RequestStore.store[:gitlab_git_env] ||= {}
+        RequestStore.store[:gitlab_git_env][gl_repository] = whitelist_git_env(env)
       end
 
-      def self.all
+      def self.all(gl_repository)
         return {} unless RequestStore.active?
 
-        RequestStore.fetch(:gitlab_git_env) { {} }
+        h = RequestStore.fetch(:gitlab_git_env) { {} }
+        h.fetch(gl_repository, {})
       end
 
-      def self.to_env_hash
+      def self.to_env_hash(gl_repository)
         env = {}
 
-        all.compact.each do |key, value|
+        all(gl_repository).compact.each do |key, value|
           value = value.join(File::PATH_SEPARATOR) if value.is_a?(Array)
           env[key.to_s] = value
         end
 
         env
-      end
-
-      def self.[](key)
-        all[key]
       end
 
       def self.whitelist_git_env(env)
