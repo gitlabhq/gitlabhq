9d27fb29ac5f775f8fc172740f17b46099120cc2
Balasankar "Balu" C <balasankar@gitlab.com>
2018-03-30 16:41:51 +0530

    Add more tests

--- spec/lib/backup/files_spec.rb
+++ spec/lib/backup/files_spec.rb
@@ -9,6 +9,9 @@ describe Backup::Files do
     allow(progress).to receive(:print)
     allow(FileUtils).to receive(:mkdir_p).and_return(true)
     allow(FileUtils).to receive(:mv).and_return(true)
+    allow(File).to receive(:exist?).and_return(true)
+    allow(File).to receive(:realpath).with("/var/gitlab-registry").and_return("/var/gitlab-registry")
+    allow(File).to receive(:realpath).with("/var/gitlab-registry/..").and_return("/var")
 
     allow_any_instance_of(String).to receive(:color) do |string, _color|
       string
@@ -25,11 +28,31 @@ describe Backup::Files do
       Timecop.freeze(timestamp) { example.run }
     end
 
+    describe 'folders with permission' do
+      before do
+        allow(subject).to receive(:run_pipeline!).and_return(true)
+        allow(subject).to receive(:backup_existing_files).and_return(true)
+        allow(Dir).to receive(:glob).with("/var/gitlab-registry/*", File::FNM_DOTMATCH).and_return(["/var/gitlab-registry/.", "/var/gitlab-registry/..", "/var/gitlab-registry/sample1"])
+      end
+
+      it 'moves all necessary files' do
+        allow(subject).to receive(:backup_existing_files).and_call_original
+        expect(FileUtils).to receive(:mv).with(["/var/gitlab-registry/sample1"], File.join(Gitlab.config.backup.path, "tmp", "registry.#{Time.now.to_i}"))
+        subject.restore
+      end
+
+      it 'raises no errors' do
+        expect { subject.restore }.not_to raise_error
+      end
+
+      it 'calls tar command with unlink' do
+        expect(subject).to receive(:run_pipeline!).with([%w(gzip -cd), %w(tar --unlink-first --recursive-unlink -C /var/gitlab-registry -xf -)], any_args)
+        subject.restore
+      end
+    end
+
     describe 'folders without permissions' do
       before do
-        allow(File).to receive(:realpath).with("/var/gitlab-registry").and_return("/var/gitlab-registry")
-        allow(File).to receive(:realpath).with("/var/gitlab-registry/..").and_return("/var")
-        allow(File).to receive(:exist?).and_return(true)
         allow(FileUtils).to receive(:mv).and_raise(Errno::EACCES)
         allow(subject).to receive(:run_pipeline!).and_return(true)
       end
