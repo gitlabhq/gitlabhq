5e8d48d803aa50d29e6fe8e329de42894c9f9a76
Kushal Pandya <kushal@gitlab.com>
2018-04-02 12:29:33 +0530

    Update template, add scroll handler

--- ee/app/assets/javascripts/roadmap/components/roadmap_shell.vue
+++ ee/app/assets/javascripts/roadmap/components/roadmap_shell.vue
@@ -1,5 +1,7 @@
 <script>
-  import { SCROLL_BAR_SIZE } from '../constants';
+  import bp from '~/breakpoints';
+  import { SCROLL_BAR_SIZE, EPIC_ITEM_HEIGHT, SHELL_MIN_WIDTH } from '../constants';
+  import eventHub from '../event_hub';
 
   import epicsListSection from './epics_list_section.vue';
   import roadmapTimelineSection from './roadmap_timeline_section.vue';
@@ -26,13 +28,20 @@
     data() {
       return {
         shellWidth: 0,
+        shellHeight: 0,
+        noScroll: false,
       };
     },
     computed: {
-      tableStyles() {
-        // return width after deducting size of vertical scrollbar
-        // to hide the scrollbar while preserving ability to scroll
-        return `width: ${this.shellWidth - SCROLL_BAR_SIZE}px;`;
+      containerStyles() {
+        const width = bp.windowWidth() > SHELL_MIN_WIDTH ?
+          this.shellWidth + this.getWidthOffset() :
+          this.shellWidth;
+
+        return {
+          width: `${width}px`,
+          height: `${this.shellHeight}px`,
+        };
       },
     },
     mounted() {
@@ -44,28 +53,45 @@
         // before setting shellWidth
         // see https://vuejs.org/v2/api/#Vue-nextTick
         if (this.$el.parentElement) {
-          this.shellWidth = this.$el.parentElement.clientWidth;
+          this.shellHeight = window.innerHeight - this.$el.offsetTop;
+          this.noScroll = this.shellHeight > (EPIC_ITEM_HEIGHT * (this.epics.length + 1));
+          this.shellWidth = this.$el.parentElement.clientWidth + this.getWidthOffset();
         }
       });
+    },
+    methods: {
+      getWidthOffset() {
+        return this.noScroll ? 0 : SCROLL_BAR_SIZE;
+      },
+      handleScroll() {
+        const { scrollTop, scrollLeft, clientHeight, scrollHeight } = this.$el;
+        if (!this.noScroll) {
+          eventHub.$emit('epicsListScrolled', { scrollTop, scrollLeft, clientHeight, scrollHeight });
+        }
+      },
     },
   };
 </script>
 
 <template>
-  <table
+  <div
     class="roadmap-shell"
-    :style="tableStyles"
+    :class="{ 'prevent-vertical-scroll': noScroll }"
+    :style="containerStyles"
+    @scroll="handleScroll"
   >
     <roadmap-timeline-section
       :epics="epics"
       :timeframe="timeframe"
       :shell-width="shellWidth"
+      :list-scrollable="!noScroll"
     />
     <epics-list-section
       :epics="epics"
       :timeframe="timeframe"
       :shell-width="shellWidth"
       :current-group-id="currentGroupId"
+      :list-scrollable="!noScroll"
     />
-  </table>
+  </div>
 </template>
