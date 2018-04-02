32ee2a4963b9128bd2cdb5041f59f5ca63a6a6c8
Jacopo <beschi.jacopo@gmail.com>
2018-04-02 12:55:11 +0200

    Minor review changes

--- lib/gitlab/ci/trace/stream.rb
+++ lib/gitlab/ci/trace/stream.rb
@@ -77,7 +77,7 @@ module Gitlab
 
           match = ""
 
-          matched = reverse_line do |line|
+          reverse_line do |line|
             line.chomp!
             matches = regex.scan(line)
             next unless matches.is_a?(Array)
@@ -85,10 +85,10 @@ module Gitlab
 
             match = matches.flatten.last
             coverage = match.gsub(/\d+(\.\d+)?/).first
-            break coverage if coverage.present?
+            return coverage if coverage.present? # rubocop:disable Cop/AvoidReturnFromBlocks
           end
 
-          matched.presence
+          nil
         rescue
           # if bad regex or something goes wrong we dont want to interrupt transition
           # so we just silently ignore error for now
--- lib/gitlab/optimistic_locking.rb
+++ lib/gitlab/optimistic_locking.rb
@@ -3,18 +3,15 @@ module Gitlab
     module_function
 
     def retry_lock(subject, retries = 100, &block)
-      loop do
-        begin
-          ActiveRecord::Base.transaction do
-            return yield(subject) # rubocop:disable Cop/AvoidReturnFromBlocks
-          end
-        rescue ActiveRecord::StaleObjectError
-          retries -= 1
-          raise unless retries >= 0
-
-          subject.reload
-        end
+      ActiveRecord::Base.transaction do
+        yield(subject)
       end
+    rescue ActiveRecord::StaleObjectError
+      retries -= 1
+      raise unless retries >= 0
+
+      subject.reload
+      retry
     end
 
     alias_method :retry_optimistic_lock, :retry_lock
