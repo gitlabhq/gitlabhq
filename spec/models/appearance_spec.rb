28e1739a3fd8c93e4805fe0053faf820cec4e1bc
Douwe Maan <douwe@gitlab.com>
2018-04-02 14:26:59 +0000

    Port EE System Header And Footer feature to CE
    remove EE specific code
    remove EE licence checks
    move migration from EE to CE folder structure
    move specs from EE to CE folder structure
    remove EE specific flag specs

--- spec/models/appearance_spec.rb
+++ spec/models/appearance_spec.rb
@@ -63,4 +63,19 @@ describe Appearance do
   %i(logo header_logo favicon).each do |logo_type|
     it_behaves_like 'logo paths', logo_type
   end
+
+  describe 'validations' do
+    let(:triplet) { '#000' }
+    let(:hex)     { '#AABBCC' }
+
+    it { is_expected.to allow_value(nil).for(:message_background_color) }
+    it { is_expected.to allow_value(triplet).for(:message_background_color) }
+    it { is_expected.to allow_value(hex).for(:message_background_color) }
+    it { is_expected.not_to allow_value('000').for(:message_background_color) }
+
+    it { is_expected.to allow_value(nil).for(:message_font_color) }
+    it { is_expected.to allow_value(triplet).for(:message_font_color) }
+    it { is_expected.to allow_value(hex).for(:message_font_color) }
+    it { is_expected.not_to allow_value('000').for(:message_font_color) }
+  end
 end
