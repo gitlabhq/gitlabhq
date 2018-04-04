52b19c35f05bed4ed85d6000db3630ab003cd79d
Nick Thomas <nick@gitlab.com>
2018-04-04 13:57:54 +0100

    Add Gitlab::Geo::LogHelpers to Geo::Scheduler::PerShardSchedulerWorker

---
+++ ee/spec/workers/geo/scheduler/per_shard_scheduler_worker_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe Geo::Scheduler::PerShardSchedulerWorker do
+  it 'includes ::Gitlab::Geo::LogHelpers' do
+    expect(described_class).to include_module(::Gitlab::Geo::LogHelpers)
+  end
+
+  it 'needs many other specs'
+end
