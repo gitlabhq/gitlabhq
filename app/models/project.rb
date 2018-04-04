3d3b46f344748420a638f8a66745d974fa2c2748
blackst0ne <blackst0ne.ru@gmail.com>
2018-04-04 09:19:47 +0000

    [Rails5] Rename `sort` methods to `sort_by_attribute`

--- app/models/project.rb
+++ app/models/project.rb
@@ -436,7 +436,7 @@ class Project < ActiveRecord::Base
       Gitlab::VisibilityLevel.options
     end
 
-    def sort(method)
+    def sort_by_attribute(method)
       case method.to_s
       when 'storage_size_desc'
         # storage_size is a joined column so we need to
