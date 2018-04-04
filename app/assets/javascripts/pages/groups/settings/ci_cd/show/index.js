3f91c9164462cc2e294d4dd4033ff4396a5f6e27
Dylan Griffith <dyl.griffith@gmail.com>
2018-04-04 10:47:20 +1000

    Make group settings page have expandable menus (#10244)

--- app/assets/javascripts/pages/groups/settings/ci_cd/show/index.js
+++ app/assets/javascripts/pages/groups/settings/ci_cd/show/index.js
@@ -1,6 +1,10 @@
+import initSettingsPanels from '~/settings_panels';
 import AjaxVariableList from '~/ci_variable_list/ajax_variable_list';
 
 document.addEventListener('DOMContentLoaded', () => {
+  // Initialize expandable settings panels
+  initSettingsPanels();
+
   const variableListEl = document.querySelector('.js-ci-variable-list-section');
   // eslint-disable-next-line no-new
   new AjaxVariableList({
