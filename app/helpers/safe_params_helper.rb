c36d70748273c4c54db249b4f5f735699bc61438
blackst0ne <blackst0ne.ru@gmail.com>
2018-04-08 16:27:27 +1100

    Fix typos in comments

--- app/helpers/safe_params_helper.rb
+++ app/helpers/safe_params_helper.rb
@@ -1,6 +1,6 @@
 module SafeParamsHelper
-  # Rails 5.0 requires to permit parameters if used in url helpers.
-  # User this helper when generating links with `params.merge(...)`
+  # Rails 5.0 requires to permit `params` if they're used in url helpers.
+  # Use this helper when generating links with `params.merge(...)`
   def safe_params
     if params.respond_to?(:permit!)
       params.except(:host, :port, :protocol).permit!
