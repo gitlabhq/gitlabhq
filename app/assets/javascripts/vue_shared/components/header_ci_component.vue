c84ed8263da6abe0fb9f2e76fca1d5b3dd71d4f7
Clement Ho <ClemMakesApps@gmail.com>
2018-04-09 14:38:44 -0500

    [skip ci] Replace .visible-xs-* with .d-block.d-sm-none

--- app/assets/javascripts/vue_shared/components/header_ci_component.vue
+++ app/assets/javascripts/vue_shared/components/header_ci_component.vue
@@ -163,7 +163,7 @@
       <button
         v-if="hasSidebarButton"
         type="button"
-        class="btn btn-secondary visible-xs-block
+        class="btn btn-secondary d-block d-sm-none
 visible-sm-block sidebar-toggle-btn js-sidebar-build-toggle js-sidebar-build-toggle-header"
         aria-label="Toggle Sidebar"
         id="toggleSidebar"
