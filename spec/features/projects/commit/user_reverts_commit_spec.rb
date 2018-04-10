c39e3f22ce12a302e294deb3523414e4a684b6fb
Bob Van Landuyt <bob@vanlanduyt.co>
2018-04-10 19:49:26 +0200

    Show `New Issue` link for projects
    When a user is not logged in, we want to show the `New Issue` link so
    he gets directed to the login flow first.
    
    When a project is archived, we never want to show the link.

--- spec/features/projects/commit/user_reverts_commit_spec.rb
+++ spec/features/projects/commit/user_reverts_commit_spec.rb
@@ -10,13 +10,16 @@ describe 'User reverts a commit', :js do
     sign_in(user)
 
     visit(project_commit_path(project, sample_commit.id))
+  end
 
+  def click_revert
     find('.header-action-buttons .dropdown').click
     find('a[href="#modal-revert-commit"]').click
   end
 
   context 'without creating a new merge request' do
     before do
+      click_revert
       page.within('#modal-revert-commit') do
         uncheck('create_merge_request')
         click_button('Revert')
@@ -44,6 +47,10 @@ describe 'User reverts a commit', :js do
   end
 
   context 'with creating a new merge request' do
+    before do
+      click_revert
+    end
+
     it 'reverts a commit' do
       page.within('#modal-revert-commit') do
         click_button('Revert')
@@ -51,6 +58,16 @@ describe 'User reverts a commit', :js do
 
       expect(page).to have_content('The commit has been successfully reverted. You can now submit a merge request to get this change into the original branch.')
       expect(page).to have_content("From revert-#{Commit.truncate_sha(sample_commit.id)} into master")
+    end
+  end
+
+  context 'when the project is archived' do
+    let(:project) { create(:project, :repository, namespace: user.namespace, archived: true) }
+
+    it 'does not show the revert link' do
+      find('.header-action-buttons .dropdown').click
+
+      expect(page).not_to have_link('Revert')
     end
   end
 end
--- spec/features/projects/issues/user_views_issue_spec.rb
+++ spec/features/projects/issues/user_views_issue_spec.rb
@@ -6,11 +6,27 @@ describe "User views issue" do
   set(:issue) { create(:issue, project: project, description: "# Description header", author: user) }
 
   before do
-    project.add_guest(user)
+    project.add_developer(user)
     sign_in(user)
 
     visit(project_issue_path(project, issue))
   end
 
   it { expect(page).to have_header_with_correct_id_and_link(1, "Description header", "description-header") }
+
+  it { expect(page).to have_link('New issue') }
+
+  it { expect(page).to have_button('Create merge request') }
+
+  it { expect(page).to have_link('Close issue') }
+
+  context 'when the project is archived' do
+    let(:project) { create(:project, :public, archived: true) }
+
+    it { expect(page).not_to have_link('New issue') }
+
+    it { expect(page).not_to have_button('Create merge request') }
+
+    it { expect(page).not_to have_link('Close issue') }
+  end
 end
