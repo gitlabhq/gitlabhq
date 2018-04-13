203185feca9d1ba8063d817d18b535c673794ef2
Mike Greiling <mike@pixelcog.com>
2018-04-13 10:45:26 -0500

    add test for shortcuts_dashboard_navigation.js

--- app/assets/javascripts/shortcuts_dashboard_navigation.js
+++ app/assets/javascripts/shortcuts_dashboard_navigation.js
@@ -1,13 +1,15 @@
+import { visitUrl } from './lib/utils/url_utility';
+
 /**
  * Helper function that finds the href of the fiven selector and updates the location.
  *
  * @param  {String} selector
  */
-export default selector => {
+export default function findAndFollowLink(selector) {
   const element = document.querySelector(selector);
   const link = element && element.getAttribute('href');
 
   if (link) {
-    window.location = link;
+    visitUrl(link);
   }
-};
+}
