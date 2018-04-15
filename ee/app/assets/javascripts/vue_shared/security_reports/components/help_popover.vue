72552cc81e1aa56ac7bd0d156ce8f5c600483f21
Luke Bennett <lukeeeebennettplus@gmail.com>
2018-04-15 16:43:34 +0100

    pass jquery obj to togglePopover in help_popover

--- ee/app/assets/javascripts/vue_shared/security_reports/components/help_popover.vue
+++ ee/app/assets/javascripts/vue_shared/security_reports/components/help_popover.vue
@@ -20,22 +20,23 @@ export default {
     },
   },
   mounted() {
-    $(this.$el)
-      .popover({
-        html: true,
-        trigger: 'focus',
-        container: 'body',
-        placement: 'top',
-        template:
-          '<div class="popover" role="tooltip"><div class="arrow"></div><p class="popover-title"></p><div class="popover-content"></div></div>',
-        ...this.options,
-      })
-      .on('mouseenter', mouseenter)
-      .on('mouseleave', debouncedMouseleave(300))
-      .on('inserted.bs.popover', inserted)
-      .on('show.bs.popover', () => {
-        window.addEventListener('scroll', togglePopover.bind(this.$el, false), { once: true });
-      });
+    const $el = $(this.$el);
+
+    $el.popover({
+      html: true,
+      trigger: 'focus',
+      container: 'body',
+      placement: 'top',
+      template:
+        '<div class="popover" role="tooltip"><div class="arrow"></div><p class="popover-title"></p><div class="popover-content"></div></div>',
+      ...this.options,
+    })
+    .on('mouseenter', mouseenter)
+    .on('mouseleave', debouncedMouseleave(300))
+    .on('inserted.bs.popover', inserted)
+    .on('show.bs.popover', () => {
+      window.addEventListener('scroll', togglePopover.bind($el, false), { once: true });
+    });
   },
 };
 </script>
