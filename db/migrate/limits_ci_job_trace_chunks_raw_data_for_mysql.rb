d5740846f6622995302036ac822b758da60f2ba3
Shinya Maeda <shinya@gitlab.com>
2018-04-17 14:31:53 +0900

    Sanitize migration file. Added a comment on LimitsCiJobTraceChunksRawDataForMysql

--- db/migrate/limits_ci_job_trace_chunks_raw_data_for_mysql.rb
+++ db/migrate/limits_ci_job_trace_chunks_raw_data_for_mysql.rb
@@ -2,6 +2,8 @@ class LimitsCiJobTraceChunksRawDataForMysql < ActiveRecord::Migration
   def up
     return unless Gitlab::Database.mysql?
 
+    # Mysql needs MEDIUMTEXT type (up to 16MB) rather than TEXT (up to 64KB)
+    # Because 'raw_data' is always capped by Ci::JobTraceChunk::CHUNK_SIZE, which is 128KB
     change_column :ci_job_trace_chunks, :raw_data, :text, limit: 16.megabytes - 1 #MEDIUMTEXT
   end
 end
