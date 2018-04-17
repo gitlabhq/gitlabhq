b48bf85495aeb6dbf1aabc5e59197b8c0ebaf875
Rémy Coutable <remy@rymai.me>
2018-04-17 15:26:24 +0200

    Resolve conflict in qa/qa/page/merge_request/show.rb
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- qa/qa/page/merge_request/show.rb
+++ qa/qa/page/merge_request/show.rb
@@ -2,11 +2,8 @@ module QA
   module Page
     module MergeRequest
       class Show < Page::Base
-<<<<<<< HEAD
         prepend QA::EE::Page::MergeRequest::Show
 
-=======
->>>>>>> upstream/master
         view 'app/assets/javascripts/vue_merge_request_widget/components/states/ready_to_merge.vue' do
           element :merge_button
           element :fast_forward_message, 'Fast-forward merge without a merge commit'
