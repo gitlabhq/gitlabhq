949160e382765bab5569c2d1de0f398d0ce72eb5
Phil Hughes <me@iamphill.com>
2018-04-19 14:45:50 +0100

    removed collapsed & toggle collapsed specs

--- spec/javascripts/ide/components/commit_sidebar/list_spec.js
+++ spec/javascripts/ide/components/commit_sidebar/list_spec.js
@@ -50,35 +50,6 @@ describe('Multi-file editor commit sidebar list', () => {
     });
   });
 
-  describe('collapsed', () => {
-    beforeEach(done => {
-      vm.$store.state.rightPanelCollapsed = true;
-
-      Vue.nextTick(done);
-    });
-
-    it('hides list', () => {
-      expect(vm.$el.querySelector('.list-unstyled')).toBeNull();
-      expect(vm.$el.querySelector('.help-block')).toBeNull();
-    });
-  });
-
-  describe('with toggle', () => {
-    beforeEach(done => {
-      spyOn(vm, 'toggleRightPanelCollapsed');
-
-      vm.showToggle = true;
-
-      Vue.nextTick(done);
-    });
-
-    it('calls setPanelCollapsedStatus when clickin toggle', () => {
-      vm.$el.querySelector('.multi-file-commit-panel-collapse-btn').click();
-
-      expect(vm.toggleRightPanelCollapsed).toHaveBeenCalled();
-    });
-  });
-
   describe('action button', () => {
     beforeEach(() => {
       spyOn(vm, 'stageAllChanges');
