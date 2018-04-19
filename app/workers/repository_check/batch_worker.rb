624a2abb24d7ee6aa6f418edfdf4364083a68029
Toon Claes <toon@gitlab.com>
2018-04-19 16:03:56 +0200

    Pick Various improvements to repository checks into EE
    Pick the changes from gitlab-org/gitlab-ce!18484 into EE.

--- app/workers/repository_check/batch_worker.rb
+++ app/workers/repository_check/batch_worker.rb
@@ -6,6 +6,8 @@ module RepositoryCheck
     RUN_TIME = 3600
 
     def perform
+      break unless Gitlab::CurrentSettings.repository_checks_enabled
+
       start = Time.now
 
       # This loop will break after a little more than one hour ('a little
@@ -15,7 +17,6 @@ module RepositoryCheck
       # check, only one (or two) will be checked at a time.
       project_ids.each do |project_id|
         break if Time.now - start >= RUN_TIME
-        break unless current_settings.repository_checks_enabled
 
         next unless try_obtain_lease(project_id)
 
@@ -46,17 +47,6 @@ module RepositoryCheck
         "project_repository_check:#{id}",
         timeout: 24.hours
       ).try_obtain
-    end
-
-    def current_settings
-      # No caching of the settings! If we cache them and an admin disables
-      # this feature, an active RepositoryCheckWorker would keep going for up
-      # to 1 hour after the feature was disabled.
-      if Rails.env.test?
-        Gitlab::CurrentSettings.fake_application_settings
-      else
-        ApplicationSetting.current
-      end
     end
   end
 end
--- app/workers/repository_check/single_repository_worker.rb
+++ app/workers/repository_check/single_repository_worker.rb
@@ -5,27 +5,38 @@ module RepositoryCheck
 
     def perform(project_id)
       project = Project.find(project_id)
-      project.update_columns(
-        last_repository_check_failed: !check(project),
-        last_repository_check_at: Time.now
-      )
+
+      save_result(project, !check)
     end
 
     private
 
-    def check(project)
-      if has_pushes?(project) && !git_fsck(project.repository)
-        false
-      elsif project.wiki_enabled?
-        # Historically some projects never had their wiki repos initialized;
-        # this happens on project creation now. Let's initialize an empty repo
-        # if it is not already there.
-        project.create_wiki
+    def save_result(project, failure)
+      project.update_columns(
+        last_repository_check_failed: failure,
+        last_repository_check_at: Time.now
+      )
+    end
 
-        git_fsck(project.wiki.repository)
-      else
-        true
-      end
+    def check(project)
+      check_repo(project) && check_wiki_repo(project)
+    end
+
+    def check_repo(project)
+      return true if project.empty_repo?
+
+      git_fsck(project.repository)
+    end
+
+    def check_wiki_repo(project)
+      return true unless project.wiki_enabled?
+
+      # Historically some projects never had their wiki repos initialized;
+      # this happens on project creation now. Let's initialize an empty repo
+      # if it is not already there.
+      project.create_wiki
+
+      git_fsck(project.wiki.repository)
     end
 
     def git_fsck(repository)
