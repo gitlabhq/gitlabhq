02999234d4b7ebcf60332816b68addf8f4667ce7
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2018-04-20 12:44:20 +0200

    Add background migration that migrates stages indices

---
+++ lib/gitlab/background_migration/migrate_stage_index.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+# rubocop:disable Style/Documentation
+
+module Gitlab
+  module BackgroundMigration
+    class MigrateStageIndex
+      module Migratable
+        class Stage < ActiveRecord::Base
+          self.table_name = 'ci_stages'
+        end
+      end
+
+      def perform(start_id, stop_id)
+        sql = <<~SQL
+          UPDATE ci_stages
+            SET index =
+              (SELECT stage_idx FROM ci_builds
+                WHERE ci_builds.stage_id = ci_stages.id
+                GROUP BY ci_builds.stage_idx ORDER BY COUNT(*) DESC LIMIT 1)
+          WHERE ci_stages.id BETWEEN #{start_id.to_i} AND #{stop_id.to_i}
+        SQL
+
+        ActiveRecord::Base.connection.execute(sql)
+      end
+    end
+  end
+end
