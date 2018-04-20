5051ca971c9196441725bf2b758f341294fc2381
Sean McGivern <sean@gitlab.com>
2018-04-20 09:41:10 +0200

    Ignore ordering in IssueDueSchedulerWorker spec

--- spec/workers/issue_due_scheduler_worker_spec.rb
+++ spec/workers/issue_due_scheduler_worker_spec.rb
@@ -14,7 +14,9 @@ describe IssueDueSchedulerWorker do
       create(:issue, :closed, project: project_closed_issue, due_date: Date.tomorrow)
       create(:issue, :opened, project: project_issue_due_another_day, due_date: Date.today)
 
-      expect(MailScheduler::IssueDueWorker).to receive(:bulk_perform_async).with([[project1.id], [project2.id]])
+      expect(MailScheduler::IssueDueWorker).to receive(:bulk_perform_async) do |args|
+        expect(args).to match_array([[project1.id], [project2.id]])
+      end
 
       described_class.new.perform
     end
