1e9b57a242a122fae642f88c995a2c411c4213f8
Phil Hughes <me@iamphill.com>
2018-04-20 13:05:17 +0100

    updated code to use getter to get changedFile

--- app/assets/javascripts/ide/stores/getters.js
+++ app/assets/javascripts/ide/stores/getters.js
@@ -42,15 +42,15 @@ export const collapseButtonTooltip = state =>
 
 export const hasMergeRequest = state => !!state.currentMergeRequestId;
 
-export const getChangesInFolder = state => path => {
-  const filePatchMatches = f => f.path.replace(new RegExp(`/${f.name}$`), '').indexOf(path) === 0;
-  const changedFilesCount = state.changedFiles.filter(f => filePatchMatches(f)).length;
+export const getChangesInFolder = (state, getters) => path => {
+  const filePathMatches = f => f.path.replace(new RegExp(`/${f.name}$`), '').indexOf(path) === 0;
+  const changedFilesCount = state.changedFiles.filter(f => filePathMatches(f)).length;
   const stagedFilesCount = state.stagedFiles.filter(
-    f =>
-      filePatchMatches(f) && !state.changedFiles.find(changedFile => changedFile.path === f.path),
+    f => filePathMatches(f) && !getters.getChangedFile(f.path),
   ).length;
 
   return changedFilesCount + stagedFilesCount;
 };
 
+export const getChangedFile = state => path => state.stagedFiles.find(f => f.path === path);
 export const getStagedFile = state => path => state.stagedFiles.find(f => f.path === path);
