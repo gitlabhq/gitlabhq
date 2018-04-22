d14851f9a36ce4e41e94e21d251589a3d197c423
James Edwards-Jones <jedwardsjones@gitlab.com>
2018-04-22 22:04:56 +0100

    Backport Identity.uniqueness_scope from EE
    Originates in 'SamlProvider has many linked Identities' from EE

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -1,12 +1,16 @@
 class Identity < ActiveRecord::Base
+  def self.uniqueness_scope
+    :provider
+  end
+
   include Sortable
   include CaseSensitivity
 
   belongs_to :user
 
   validates :provider, presence: true
-  validates :extern_uid, allow_blank: true, uniqueness: { scope: :provider, case_sensitive: false }
-  validates :user_id, uniqueness: { scope: :provider }
+  validates :extern_uid, allow_blank: true, uniqueness: { scope: uniqueness_scope, case_sensitive: false }
+  validates :user_id, uniqueness: { scope: uniqueness_scope }
 
   before_save :ensure_normalized_extern_uid, if: :extern_uid_changed?
   after_destroy :clear_user_synced_attributes, if: :user_synced_attributes_metadata_from_provider?
