a2235739761e7ea241558e01d049e260424431eb
James Edwards-Jones <jedwardsjones@gitlab.com>
2018-04-23 11:56:44 +0100

    Unify Saml::IdentityLinker and OAuth::IdentityLinker

--- lib/gitlab/auth/o_auth/identity_linker.rb
+++ lib/gitlab/auth/o_auth/identity_linker.rb
@@ -2,25 +2,6 @@ module Gitlab
   module Auth
     module OAuth
       class IdentityLinker < OmniauthIdentityLinkerBase
-        def create_or_update
-          if identity.new_record?
-            @created = identity.save
-          end
-        end
-
-        def error_message
-          identity.validate
-
-          identity.errors.full_messages.join(', ')
-        end
-
-        private
-
-        def identity
-          @identity ||= current_user.identities
-                        .with_extern_uid(oauth['provider'], oauth['uid'])
-                        .first_or_initialize(extern_uid: oauth['uid'])
-        end
       end
     end
   end
--- lib/gitlab/auth/omniauth_identity_linker_base.rb
+++ lib/gitlab/auth/omniauth_identity_linker_base.rb
@@ -6,19 +6,41 @@ module Gitlab
       def initialize(current_user, oauth)
         @current_user = current_user
         @oauth = oauth
-        @created = false
+        @changed = false
       end
 
-      def created?
-        @created
+      def link
+        save if identity.new_record?
+      end
+
+      def changed?
+        @changed
       end
 
       def error_message
-        ''
+        identity.validate
+
+        identity.errors.full_messages.join(', ')
       end
 
-      def create_or_update
-        raise NotImplementedError
+      private
+
+      def save
+        @changed = identity.save
+      end
+
+      def identity
+        @identity ||= current_user.identities
+                                  .with_extern_uid(provider, uid)
+                                  .first_or_initialize(extern_uid: uid)
+      end
+
+      def provider
+        oauth['provider']
+      end
+
+      def uid
+        oauth['uid']
       end
     end
   end
