3d6d0a09b65f032bbe1bd5ad4736dd764195bbe1
Bob Van Landuyt <bob@vanlanduyt.co>
2018-04-24 18:28:04 +0200

    Store application wide terms
    This allows admins to define terms in the application settings.
    
    Every time the terms are adjusted, a new version is stored and becomes
    the 'active' version. This allows tracking which specific version was
    accepted by a user.

--- app/services/application_settings/update_service.rb
+++ app/services/application_settings/update_service.rb
@@ -1,7 +1,22 @@
 module ApplicationSettings
   class UpdateService < ApplicationSettings::BaseService
     def execute
+      update_terms(@params.delete(:terms))
+
       @application_setting.update(@params)
+    end
+
+    private
+
+    def update_terms(terms)
+      return unless terms.present?
+
+      # Avoid creating a new terms record if the text is exactly the same.
+      terms = terms.strip
+      return if terms == @application_setting.terms
+
+      ApplicationSetting::Term.create(terms: terms)
+      @application_setting.reset_memoized_terms
     end
   end
 end
