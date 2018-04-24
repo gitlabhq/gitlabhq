564050ec0ff4bd642e7d1963b185403042af5696
James Ramsay <james@jramsay.com.au>
2018-04-24 12:51:39 +0200

    Enable LFS setting UI for Masters and Owners
    LFS can be enabled and disabled per project using the API by Masters
    and Owners, but the UI is only available to Admins. The interface and
    API should be consistent.
    
    LFS can also be enabled and disabled per group using the API by Owners,
    but the UI is only available to Admins. This interface should also be
    consistent.
    
    Additionally removes an unneeded check if 2FA interface, since the
    interface is only available to Owners.
    
    Closes #33320

--- spec/features/projects/settings/lfs_settings_spec.rb
+++ spec/features/projects/settings/lfs_settings_spec.rb
@@ -1,21 +1,27 @@
 require 'rails_helper'
 
 describe 'Projects > Settings > LFS settings' do
-  let(:admin) { create(:admin) }
   let(:project) { create(:project) }
+  let(:user) { create(:user) }
+  let(:role) { :master }
 
   context 'LFS enabled setting' do
     before do
       allow(Gitlab.config.lfs).to receive(:enabled).and_return(true)
 
-      sign_in(admin)
+      sign_in(user)
+      project.add_role(user, role)
     end
 
-    it 'displays the correct elements', :js do
-      visit edit_project_path(project)
+    context 'for master' do
+      let(:role) { :master }
 
-      expect(page).to have_content('Git Large File Storage')
-      expect(page).to have_selector('input[name="project[lfs_enabled]"] + button', visible: true)
+      it 'displays the correct elements', :js do
+        visit edit_project_path(project)
+
+        expect(page).to have_content('Git Large File Storage')
+        expect(page).to have_selector('input[name="project[lfs_enabled]"] + button', visible: true)
+      end
     end
   end
 end
