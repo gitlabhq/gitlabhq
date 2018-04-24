dc67ab29784b0895a76d3de705826a8749e8bf9b
Luke Bennett <lukeeeebennettplus@gmail.com>
2018-04-24 13:26:39 +0100

    Clone pri label

--- app/assets/javascripts/label_manager.js
+++ app/assets/javascripts/label_manager.js
@@ -97,7 +97,7 @@ export default class LabelManager {
     if (action === 'remove') {
       $('.js-priority-badge', $label).remove();
     } else {
-      $('.label-links', $label).append(this.$badgeItemTemplate);
+      $('.label-links', $label).append(this.$badgeItemTemplate.clone());
     }
   }
 
