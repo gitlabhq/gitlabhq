54caf961641348e8983a31fa7fe855fd5bf94e18
schwedenmut <schwedenmut@googlemail.com>
2018-04-26 11:18:32 +0000

    Resolve "User deletion modal with same info for delete user / delete user and contributions"

--- app/assets/javascripts/pages/admin/users/components/delete_user_modal.vue
+++ app/assets/javascripts/pages/admin/users/components/delete_user_modal.vue
@@ -52,16 +52,15 @@
       text() {
         const keepContributionsText = s__(`AdminArea|
           You are about to permanently delete the user %{username}.
-          This will delete all of the issues, merge requests, and groups linked to them.
+          Issues, merge requests, and groups linked to them will be transferred to a system-wide "Ghost-user".
           To avoid data loss, consider using the %{strong_start}block user%{strong_end} feature instead.
           Once you %{strong_start}Delete user%{strong_end}, it cannot be undone or recovered.`);
 
         const deleteContributionsText = s__(`AdminArea|
           You are about to permanently delete the user %{username}.
-          Issues, merge requests, and groups linked to them will be transferred to a system-wide "Ghost-user".
+          This will delete all of the issues, merge requests, and groups linked to them.
           To avoid data loss, consider using the %{strong_start}block user%{strong_end} feature instead.
           Once you %{strong_start}Delete user%{strong_end}, it cannot be undone or recovered.`);
-
         return sprintf(this.deleteContributions ? deleteContributionsText : keepContributionsText,
           {
             username: `<strong>${_.escape(this.username)}</strong>`,
