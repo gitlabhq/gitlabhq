77c72c688d70104c776bbb76d3490ec53e10a56d
Dylan Griffith <dyl.griffith@gmail.com>
2018-04-26 13:23:35 +0800

    Increase PipelineSerializer query limit count to support new group runner queries

--- spec/serializers/pipeline_serializer_spec.rb
+++ spec/serializers/pipeline_serializer_spec.rb
@@ -118,7 +118,7 @@ describe PipelineSerializer do
         it 'verifies number of queries', :request_store do
           recorded = ActiveRecord::QueryRecorder.new { subject }
 
-          expect(recorded.count).to be_within(1).of(36)
+          expect(recorded.count).to be_within(1).of(44)
           expect(recorded.cached_count).to eq(0)
         end
       end
