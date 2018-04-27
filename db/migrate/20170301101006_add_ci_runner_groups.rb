3c3e14430ba14ae45ec758d263ba882ddf32f76f
Dylan Griffith <dyl.griffith@gmail.com>
2018-04-27 09:04:24 +0800

    Remove unecessary index on ci_runner_groups.runner_id

--- db/migrate/20170301101006_add_ci_runner_groups.rb
+++ db/migrate/20170301101006_add_ci_runner_groups.rb
@@ -11,11 +11,9 @@ class AddCiRunnerGroups < ActiveRecord::Migration
       t.integer :group_id
     end
 
-    add_concurrent_index :ci_runner_groups, :runner_id
-    add_concurrent_foreign_key :ci_runner_groups, :ci_runners, column: :runner_id, on_delete: :cascade
-
     add_concurrent_index :ci_runner_groups, :group_id
     add_concurrent_index :ci_runner_groups, [:runner_id, :group_id], unique: true
+    add_concurrent_foreign_key :ci_runner_groups, :ci_runners, column: :runner_id, on_delete: :cascade
     add_concurrent_foreign_key :ci_runner_groups, :namespaces, column: :group_id, on_delete: :cascade
   end
 
