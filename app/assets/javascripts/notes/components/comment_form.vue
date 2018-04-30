cfa92112d189dce1028bce7145a5cbe609c389a8
Kushal Pandya <kushal@gitlab.com>
2018-04-30 17:09:30 +0530

    Enable quick support actions default

--- app/assets/javascripts/notes/components/comment_form.vue
+++ app/assets/javascripts/notes/components/comment_form.vue
@@ -99,11 +99,6 @@ export default {
         'js-note-target-reopen': !this.isOpen,
       };
     },
-    supportQuickActions() {
-      return true;
-      // Disable quick actions support for Epics
-      //return this.noteableType !== constants.EPIC_NOTEABLE_TYPE;
-    },
     markdownDocsPath() {
       return this.getNotesData.markdownDocsPath;
     },
@@ -360,7 +355,7 @@ Please check your network connection and try again.`;
                   name="note[note]"
                   class="note-textarea js-vue-comment-form
 js-gfm-input js-autosize markdown-area js-vue-textarea"
-                  :data-supports-quick-actions="supportQuickActions"
+                  data-supports-quick-actions="true"
                   aria-label="Description"
                   v-model="note"
                   ref="textarea"
