0316ea13cae6f9eac63ca884e1683dd5a7def4fa
Jacob Schatz <jschatz@gitlab.com>
2018-05-02 16:27:37 -0400

    Add yml file instead of config.

--- lib/pseudonymity/table.rb
+++ lib/pseudonymity/table.rb
@@ -1,5 +1,6 @@
 require 'digest'
 require 'csv'
+require 'yaml'
 
 module Pseudonymity
   class Anon
@@ -8,8 +9,12 @@ module Pseudonymity
     end
 
     def anonymize(results)
-
-      results.collect! do | r |
+      count = 0
+      results.each do | r |
+        break if count > 0
+        count += 1
+        puts r.inspect
+        puts @anon_fields
         new_hash = r.each_with_object({}) do | (k, v), h |
           if @anon_fields.include? k and !v.nil?
             h[k] = Digest::SHA2.new(256).hexdigest v
@@ -23,33 +28,50 @@ module Pseudonymity
   end
 
   class Table
-    class << self
-      def table_to_csv(table, whitelist_columns, pseudonymity_columns)
-        sql = "SELECT #{whitelist_columns.join(",")} from #{table}"
-        results = ActiveRecord::Base.connection.exec_query(sql)
-        anon = Anon.new(pseudonymity_columns)
-        results = anon.anonymize results
-        write_to_csv_file table, results
-      end
 
-      def write_to_csv_file(title, contents)
-        file_path = "/tmp/#{title}.csv"
-        if contents.empty?
-          File.open(file_path, "w") {}
-          return file_path
-        end
-        column_names = contents.first.keys
-        contents = CSV.generate do | csv |
-          csv << column_names
-          contents.each do |x|
-            csv << x.values
-          end
-        end
-        File.open(file_path, 'w') { |file| file.write(contents) }
+    config = {}
+
+    def initialize
+      parse_config
+    end
+
+    def tables_to_csv
+      tables = @config["tables"]
+
+      tables.map do | k, v |
+        table_to_csv(k, v["whitelist"], v["anon"])
+      end
+    end
+
+    def table_to_csv(table, whitelist_columns, pseudonymity_columns)
+      sql = "SELECT #{whitelist_columns.join(",")} from #{table}"
+      results = ActiveRecord::Base.connection.exec_query(sql)
+      anon = Anon.new(pseudonymity_columns)
+      results = anon.anonymize results
+      write_to_csv_file table, results
+    end
+
+    def parse_config
+      @config = YAML.load_file('./lib/assets/pseudonymity_dump.yml')
+    end
+
+    def write_to_csv_file(title, contents)
+      file_path = "/tmp/#{title}.csv"
+      if contents.empty?
+        File.open(file_path, "w") {}
         return file_path
       end
-
-      private :write_to_csv_file
+      column_names = contents.first.keys
+      contents = CSV.generate do | csv |
+        csv << column_names
+        contents.each do |x|
+          csv << x.values
+        end
+      end
+      File.open(file_path, 'w') { |file| file.write(contents) }
+      return file_path
     end
+
+    private :write_to_csv_file
   end
 end
