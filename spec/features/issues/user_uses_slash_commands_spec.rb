fb681f687992e9e4570336f23e9c9b8e663debd5
Lukas Eipert <git@leipert.io>
2018-05-03 14:31:32 +0200

    ensure users are signed out properly in user_uses_slash_commands_spec.rb
    See https://gitlab.com/gitlab-org/gitlab-ce/issues/45970

--- spec/features/issues/user_uses_slash_commands_spec.rb
+++ spec/features/issues/user_uses_slash_commands_spec.rb
@@ -235,6 +235,7 @@ feature 'Issues > User uses quick actions', :js do
 
         before do
           target_project.add_master(user)
+          gitlab_sign_out
           sign_in(user)
           visit project_issue_path(project, issue)
         end
@@ -294,6 +295,7 @@ feature 'Issues > User uses quick actions', :js do
 
         before do
           target_project.add_master(user)
+          gitlab_sign_out
           sign_in(user)
           visit project_issue_path(project, issue)
         end
