7bc24ec2e524b55402fa5caeb64e75e02f97ddc5
Dylan Griffith <dyl.griffith@gmail.com>
2018-05-03 11:03:19 +0200

    Extract constants in 20180430143705_backfill_runner_type_for_ci_runners_post_migrate

--- db/post_migrate/20180430143705_backfill_runner_type_for_ci_runners_post_migrate.rb
+++ db/post_migrate/20180430143705_backfill_runner_type_for_ci_runners_post_migrate.rb
@@ -3,14 +3,17 @@ class BackfillRunnerTypeForCiRunnersPostMigrate < ActiveRecord::Migration
 
   DOWNTIME = false
 
+  PROJECT_RUNNER_TYPE = 1
+  INSTANCE_RUNNER_TYPE = 3
+
   disable_ddl_transaction!
 
   def up
-    update_column_in_batches(:ci_runners, :runner_type, 1) do |table, query|
+    update_column_in_batches(:ci_runners, :runner_type, PROJECT_RUNNER_TYPE) do |table, query|
       query.where(table[:is_shared].eq(true)).where(table[:runner_type].eq(nil))
     end
 
-    update_column_in_batches(:ci_runners, :runner_type, 3) do |table, query|
+    update_column_in_batches(:ci_runners, :runner_type, INSTANCE_RUNNER_TYPE) do |table, query|
       query.where(table[:is_shared].eq(false)).where(table[:runner_type].eq(nil))
     end
   end
