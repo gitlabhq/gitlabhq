fc1df0d3d08f85c083a06c4052ced994313359ae
Ash McKenzie <amckenzie@gitlab.com>
2018-05-04 14:03:28 +0000

    Shard name vs. Shard updates, general tech debt cleanup

--- ee/app/workers/geo/project_sync_worker.rb
+++ ee/app/workers/geo/project_sync_worker.rb
@@ -39,8 +39,9 @@ module Geo
       registry.force_to_redownload_wiki = false
 
       if registry.changed? || registry.last_wiki_synced_at.nil? || registry.last_wiki_successful_sync_at.nil?
-        registry.last_wiki_synced_at = DateTime.now
-        registry.last_wiki_successful_sync_at = DateTime.now
+        now = DateTime.now
+        registry.last_wiki_synced_at = now
+        registry.last_wiki_successful_sync_at = now
 
         success = registry.save
         log_info("#{success ? 'Successfully marked' : 'Failed to mark'} disabled wiki as synced", registry_id: registry.id, project_id: registry.project_id)
--- ee/spec/workers/geo/scheduler/per_shard_scheduler_worker_spec.rb
+++ ee/spec/workers/geo/scheduler/per_shard_scheduler_worker_spec.rb
@@ -1,9 +1,73 @@
 require 'spec_helper'
 
 describe Geo::Scheduler::PerShardSchedulerWorker do
-  it 'includes ::Gitlab::Geo::LogHelpers' do
+  it 'includes ApplicationWorker' do
+    expect(described_class).to include_module(ApplicationWorker)
+  end
+
+  it 'includes CronjobQueue' do
+    expect(described_class).to include_module(CronjobQueue)
+  end
+
+  it 'includes Gitlab::Utils::StrongMemoize' do
+    expect(described_class).to include_module(::Gitlab::Utils::StrongMemoize)
+  end
+
+  it 'includes Gitlab::Geo::LogHelpers' do
     expect(described_class).to include_module(::Gitlab::Geo::LogHelpers)
   end
 
-  it 'needs many other specs'
+  it 'includes FsShards and Gitaly health checks' do
+    expect(described_class::HEALTHY_SHARD_CHECKS).to include(
+      Gitlab::HealthChecks::FsShardsCheck,
+      Gitlab::HealthChecks::GitalyCheck
+    )
+  end
+
+  describe 'instance methods' do
+    subject(:per_shard_scheduler_worker) { described_class.new }
+
+    let(:default_shard_name) { 'default' }
+    let(:other_shard_name) { 'other' }
+    let(:unhealthy_shard_name) { 'unhealthy' }
+
+    let(:default_shard) { Gitlab::HealthChecks::Result.new(true, nil, shard: default_shard_name) }
+    let(:other_shard) { Gitlab::HealthChecks::Result.new(true, nil, shard: other_shard_name) }
+    let(:unhealthy_shard) { Gitlab::HealthChecks::Result.new(false, '14:Connect Failed', shard: unhealthy_shard_name) }
+
+    before do
+      allow(Gitlab::HealthChecks::FsShardsCheck).to receive(:readiness).and_return([default_shard, other_shard, unhealthy_shard])
+      allow(Gitlab::HealthChecks::GitalyCheck).to receive(:readiness).and_return([default_shard, other_shard, unhealthy_shard])
+    end
+
+    describe '#schedule_job' do
+      it "raises a NotImplementedError exception" do
+        expect do
+          per_shard_scheduler_worker.schedule_job(default_shard_name)
+        end.to raise_exception(NotImplementedError)
+      end
+    end
+
+    describe '#ready_shards' do
+      let(:ready_shards) { [[default_shard, other_shard, unhealthy_shard], [default_shard, other_shard, unhealthy_shard]] }
+
+      it "returns an array of ready shards" do
+        expect(per_shard_scheduler_worker.ready_shards).to eq(ready_shards)
+      end
+    end
+
+    describe '#healthy_ready_shards' do
+      let(:healthy_ready_shards) { [default_shard, other_shard] }
+
+      it "returns an array of healthy shard names" do
+        expect(per_shard_scheduler_worker.healthy_ready_shards).to eq(healthy_ready_shards)
+      end
+    end
+
+    describe '#healthy_shard_names' do
+      it "returns an array of healthy shard names" do
+        expect(per_shard_scheduler_worker.healthy_shard_names).to eq([default_shard_name, other_shard_name])
+      end
+    end
+  end
 end
