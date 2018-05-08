e6142470b08b7b8cd128be1213fb7a0b7aadb2b0
Ash McKenzie <amckenzie@gitlab.com>
2018-05-08 13:48:46 +1000

    Resolve conflicts in config/routes/project.rb

--- config/routes/project.rb
+++ config/routes/project.rb
@@ -202,21 +202,15 @@ constraints(::Constraints::ProjectUrlConstrainer.new) do
 
       resource :mirror, only: [:show, :update] do
         member do
-<<<<<<< HEAD
           get :ssh_host_keys, constraints: { format: :json }
-=======
->>>>>>> upstream/master
           post :update_now
         end
       end
 
-<<<<<<< HEAD
       ## EE-specific
       resources :push_rules, constraints: { id: /\d+/ }, only: [:update]
       ## EE-specific
 
-=======
->>>>>>> upstream/master
       resources :pipelines, only: [:index, :new, :create, :show] do
         collection do
           resource :pipelines_settings, path: 'settings', only: [:show, :update]
