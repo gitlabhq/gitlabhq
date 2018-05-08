1d5f30790e75f2ff778a0d28def2c8b2d3e7ad4a
Brett Walker <brett@digitalmoksha.com>
2018-05-08 00:13:41 +0000

    Keep ShaAttribute from halting startup when we can’t connect to Geo tracking database

--- app/models/concerns/sha_attribute.rb
+++ app/models/concerns/sha_attribute.rb
@@ -2,20 +2,35 @@ module ShaAttribute
   extend ActiveSupport::Concern
 
   module ClassMethods
-    def sha_attribute(name, database_available = true)
+    def sha_attribute(name)
       return if ENV['STATIC_VERIFICATION']
-      return unless database_available && table_exists?
+
+      validate_binary_column_exists!(name) unless Rails.env.production?
+
+      attribute(name, Gitlab::Database::ShaAttribute.new)
+    end
+
+    # This only gets executed in non-production environments as an additional check to ensure
+    # the column is the correct type.  In production it should behave like any other attribute.
+    # See https://gitlab.com/gitlab-org/gitlab-ee/merge_requests/5502 for more discussion
+    def validate_binary_column_exists!(name)
+      unless table_exists?
+        warn "WARNING: sha_attribute #{name.inspect} is invalid since the table doesn't exist - you may need to run database migrations"
+        return
+      end
 
       column = columns.find { |c| c.name == name.to_s }
 
-      # In case the table doesn't exist we won't be able to find the column,
-      # thus we will only check the type if the column is present.
-      if column && column.type != :binary
-        raise ArgumentError,
-          "sha_attribute #{name.inspect} is invalid since the column type is not :binary"
+      unless column
+        raise ArgumentError.new("sha_attribute #{name.inspect} is invalid since the column doesn't exist")
       end
 
-      attribute(name, Gitlab::Database::ShaAttribute.new)
+      unless column.type == :binary
+        raise ArgumentError.new("sha_attribute #{name.inspect} is invalid since the column type is not :binary")
+      end
+    rescue => error
+      Gitlab::AppLogger.error "ShaAttribute initialization: #{error.message}"
+      raise
     end
   end
 end
