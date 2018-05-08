4d41663a85df99e80a846a93c529e6819017d06c
Mike Greiling <mike@pixelcog.com>
2018-05-08 03:11:34 -0500

    add vue translate mixin everywhere to avoid module order side-effects

--- app/assets/javascripts/commons/vue.js
+++ app/assets/javascripts/commons/vue.js
@@ -1,5 +1,8 @@
 import Vue from 'vue';
+import Translate from '../vue_shared/translate';
 import '../vue_shared/vue_resource_interceptor';
+
+Vue.use(Translate);
 
 if (process.env.NODE_ENV !== 'production') {
   Vue.config.productionTip = false;
