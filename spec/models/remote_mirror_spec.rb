f64aa8c893fa2183bd39f444704148479288cffa
Ash McKenzie <amckenzie@gitlab.com>
2018-05-08 14:25:48 +1000

    Resolve conflicts in spec/models/remote_mirror_spec.rb

--- spec/models/remote_mirror_spec.rb
+++ spec/models/remote_mirror_spec.rb
@@ -164,7 +164,6 @@ describe RemoteMirror do
       end
     end
 
-<<<<<<< HEAD
     context 'as a Geo secondary' do
       it 'returns nil' do
         allow(Gitlab::Geo).to receive(:secondary?).and_return(true)
@@ -173,8 +172,6 @@ describe RemoteMirror do
       end
     end
 
-=======
->>>>>>> upstream/master
     context 'with remote mirroring enabled' do
       context 'with only protected branches enabled' do
         context 'when it did not update in the last minute' do
