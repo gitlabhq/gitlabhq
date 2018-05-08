ab24e1142080dcb2c2dd565c5be0d41da7a3ddec
Stan Hu <stanhu@gmail.com>
2018-05-07 20:08:37 -0700

    Resolve conflicts in app/controllers/projects/mirrors_controller.rb

--- app/controllers/projects/mirrors_controller.rb
+++ app/controllers/projects/mirrors_controller.rb
@@ -1,11 +1,8 @@
 class Projects::MirrorsController < Projects::ApplicationController
   include RepositorySettingsRedirect
 
-<<<<<<< HEAD
   prepend EE::Projects::MirrorsController
 
-=======
->>>>>>> upstream/master
   # Authorize
   before_action :remote_mirror, only: [:update]
   before_action :check_mirror_available!
