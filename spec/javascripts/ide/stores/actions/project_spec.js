af2aa63827f708bc0a56b6dfed5879a88fe80c03
André Luís <aluis@gitlab.com>
2018-05-08 01:22:10 +0100

    Add tests for pipelines in WebIDE status bar

--- spec/javascripts/ide/stores/actions/project_spec.js
+++ spec/javascripts/ide/stores/actions/project_spec.js
@@ -1,5 +1,6 @@
 import {
   refreshLastCommitData,
+  getLastCommitPipeline,
 } from '~/ide/stores/actions';
 import store from '~/ide/stores';
 import service from '~/ide/services';
@@ -20,15 +21,13 @@ describe('IDE store project actions', () => {
       store.state.currentProjectId = 'abcproject';
       store.state.currentBranchId = 'master';
       store.state.projects.abcproject = {
+        id: 4,
         branches: {
           master: {
             commit: null,
           },
         },
       };
-    });
-
-    it('calls the service', done => {
       spyOn(service, 'getBranchData').and.returnValue(
         Promise.resolve({
           data: {
@@ -36,7 +35,9 @@ describe('IDE store project actions', () => {
           },
         }),
       );
+    });
 
+    it('calls the service', done => {
       store
         .dispatch('refreshLastCommitData', {
           projectId: store.state.currentProjectId,
@@ -53,8 +54,11 @@ describe('IDE store project actions', () => {
     it('commits getBranchData', done => {
       testAction(
         refreshLastCommitData,
-        {},
-        {},
+        {
+          projectId: store.state.currentProjectId,
+          branchId: store.state.currentBranchId,
+        },
+        store.state,
         [{
           type: 'SET_BRANCH_COMMIT',
           payload: {
@@ -63,6 +67,100 @@ describe('IDE store project actions', () => {
             commit: { id: '123' },
           },
         }], // mutations
+        [
+          {
+            type: 'getLastCommitPipeline',
+            payload: {
+              projectId: 'abcproject',
+              projectIdNumber: store.state.projects.abcproject.id,
+              branchId: 'master',
+            },
+          },
+        ], // action
+        done,
+      );
+    });
+  });
+
+  describe('getLastCommitPipeline', () => {
+    beforeEach(() => {
+      store.state.currentProjectId = 'abcproject';
+      store.state.currentBranchId = 'master';
+      store.state.projects.abcproject = {
+        id: 4,
+        branches: {
+          master: {
+            commit: {
+              id: 'abc123def456ghi789jkl',
+              title: 'example',
+            },
+          },
+        },
+      };
+      spyOn(service, 'getPipelinesForProject').and.returnValue(
+        Promise.resolve({
+          data: [
+            { id: '1', ref: 'master', sha: 'lkj987ihg654fed321cba', status: 'failed' },
+            { id: '2', ref: 'master', sha: 'abc123def456ghi789jkl', status: 'passed' },
+          ],
+        }),
+      );
+    });
+
+    it('calls the service', done => {
+      store
+        .dispatch('getLastCommitPipeline', {
+          projectId: store.state.currentProjectId,
+          projectIdNumber: store.state.projects[store.state.currentProjectId].id,
+          branchId: store.state.currentBranchId,
+        })
+        .then(() => {
+          expect(service.getPipelinesForProject).toHaveBeenCalledWith(4);
+
+          done();
+        })
+        .catch(done.fail);
+    });
+
+    it('commits correct pipeline', done => {
+      testAction(
+        getLastCommitPipeline,
+        {
+          projectId: 'abcproject',
+          projectIdNumber: store.state.projects.abcproject.id,
+          branchId: 'master',
+        },
+        store.state,
+        [{
+          type: 'SET_LAST_COMMIT_PIPELINE',
+          payload: {
+            projectId: 'abcproject',
+            branchId: 'master',
+            pipeline: {
+              id: '2',
+              ref: 'master',
+              sha: 'abc123def456ghi789jkl',
+              status: 'passed',
+            },
+          },
+        }], // mutations
+        [], // action
+        done,
+      );
+    });
+
+    it('doesn\'t commit anything if no pipeline exists for last commit', done => {
+      store.state.projects.abcproject.branches.master.commit.id = 'nonexistanthash';
+
+      testAction(
+        getLastCommitPipeline,
+        {
+          projectId: 'abcproject',
+          projectIdNumber: store.state.projects.abcproject.id,
+          branchId: 'master',
+        },
+        store.state,
+        [], // mutations
         [], // action
         done,
       );
--- spec/javascripts/ide/stores/mutations/branch_spec.js
+++ spec/javascripts/ide/stores/mutations/branch_spec.js
@@ -37,4 +37,32 @@ describe('Multi-file store branch mutations', () => {
       expect(localState.projects.Example.branches.master.commit.title).toBe('Example commit');
     });
   });
+
+  describe('SET_LAST_COMMIT_PIPELINE', () => {
+    it('sets the pipeline for the last commit on current project', () => {
+      localState.projects = {
+        Example: {
+          branches: {
+            master: {
+              commit: {},
+            },
+          },
+        },
+      };
+
+      mutations.SET_LAST_COMMIT_PIPELINE(localState, {
+        projectId: 'Example',
+        branchId: 'master',
+        pipeline: {
+          id: 42,
+          ref: 'master',
+          status: 'failed',
+        },
+      });
+
+      expect(localState.projects.Example.branches.master.commit.pipeline.id).toBe(42);
+      expect(localState.projects.Example.branches.master.commit.pipeline.ref).toBe('master');
+      expect(localState.projects.Example.branches.master.commit.pipeline.status).toBe('failed');
+    });
+  });
 });
