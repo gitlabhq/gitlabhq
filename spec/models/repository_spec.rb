1b4e575f5efee292af8bf89d30c42000b0e5a741
Ash McKenzie <amckenzie@gitlab.com>
2018-05-08 14:27:03 +1000

    Resolve conflicts in spec/models/repository_spec.rb

--- spec/models/repository_spec.rb
+++ spec/models/repository_spec.rb
@@ -2403,7 +2403,6 @@ describe Repository do
     end
   end
 
-<<<<<<< HEAD
   describe '#after_sync' do
     it 'expires repository cache' do
       expect(repository).to receive(:expire_all_method_caches)
@@ -2414,8 +2413,6 @@ describe Repository do
     end
   end
 
-=======
->>>>>>> upstream/master
   def create_remote_branch(remote_name, branch_name, target)
     rugged = repository.rugged
     rugged.references.create("refs/remotes/#{remote_name}/#{branch_name}", target.id)
