002b02fe0bde852ef9d5b534533c8debca992436
Mike Greiling <mike@pixelcog.com>
2018-05-08 03:32:17 +0000

    Resolve conflicts in config/webpack.config.js
    [ci-skip]

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -37,7 +37,6 @@ function generateEntries() {
 
   pageEntries.forEach(path => generateAutoEntries(path));
 
-<<<<<<< HEAD
   // EE-specific auto entries
   const eePageEntries = glob.sync('pages/**/index.js', {
     cwd: path.join(ROOT_PATH, 'ee/app/assets/javascripts'),
@@ -45,8 +44,6 @@ function generateEntries() {
   eePageEntries.forEach(path => generateAutoEntries(path, 'ee'));
   watchAutoEntries.push(path.join(ROOT_PATH, 'ee/app/assets/javascripts/pages/'));
 
-=======
->>>>>>> upstream/master
   const autoEntryKeys = Object.keys(autoEntriesMap);
   autoEntriesCount = autoEntryKeys.length;
 
