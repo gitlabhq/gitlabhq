0f4d9f7c621ed95da8000b27d47e2388d98d9f49
Felipe Artur <felipefac@gmail.com>
2018-05-09 11:19:33 -0300

    Revert "Revert "Code improvements and add some properties to user entity""
    This reverts commit c152e046d499676508b6d346dffcfad6c7da09b5.

--- app/serializers/user_entity.rb
+++ app/serializers/user_entity.rb
@@ -1,7 +1,21 @@
 class UserEntity < API::Entities::UserBasic
   include RequestAwareEntity
 
+  expose :can_fork do |user|
+    can?(user, :fork_project, request.project) if project
+  end
+
+  expose :can_create_merge_request do |user|
+    can?(user, :create_merge_request_in, project) if project
+  end
+
   expose :path do |user|
     user_path(user)
+  end
+
+  def project
+    return false unless request.respond_to?(:project) && request.project
+
+    request.project
   end
 end
