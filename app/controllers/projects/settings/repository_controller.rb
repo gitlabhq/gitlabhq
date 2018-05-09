c1b0e55e7956ae9bc91cefadc4ab623b6f346129
Lin Jen-Shin <godfat@godfat.org>
2018-05-09 18:32:36 +0800

    Move include SafeMirrorParams to EE module

--- app/controllers/projects/settings/repository_controller.rb
+++ app/controllers/projects/settings/repository_controller.rb
@@ -1,8 +1,6 @@
 module Projects
   module Settings
     class RepositoryController < Projects::ApplicationController
-      include SafeMirrorParams
-
       before_action :authorize_admin_project!
       before_action :remote_mirror, only: [:show]
 
