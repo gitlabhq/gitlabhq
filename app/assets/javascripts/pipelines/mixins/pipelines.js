feb9caab9033262ccd93ae881f095bbfd73904ad
Filipa Lacerda <filipa@gitlab.com>
2018-05-11 14:03:27 +0000

    Resolve "Remove modal box confirmation when retrying a pipeline"

--- app/assets/javascripts/pipelines/mixins/pipelines.js
+++ app/assets/javascripts/pipelines/mixins/pipelines.js
@@ -53,10 +53,12 @@ export default {
     });
 
     eventHub.$on('postAction', this.postAction);
+    eventHub.$on('retryPipeline', this.postAction);
     eventHub.$on('clickedDropdown', this.updateTable);
   },
   beforeDestroy() {
     eventHub.$off('postAction', this.postAction);
+    eventHub.$off('retryPipeline', this.postAction);
     eventHub.$off('clickedDropdown', this.updateTable);
   },
   destroyed() {
