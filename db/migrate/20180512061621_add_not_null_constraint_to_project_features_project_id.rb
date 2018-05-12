dc09828d09c6b818a10eaa678b427d812fbd85b2
Stan Hu <stanhu@gmail.com>
2018-05-12 01:18:49 -0700

    Fix #down implementation

--- db/migrate/20180512061621_add_not_null_constraint_to_project_features_project_id.rb
+++ db/migrate/20180512061621_add_not_null_constraint_to_project_features_project_id.rb
@@ -16,6 +16,6 @@ class AddNotNullConstraintToProjectFeaturesProjectId < ActiveRecord::Migration
   end
 
   def down
-    change_column_null :project_features, :project_id, false
+    change_column_null :project_features, :project_id, true
   end
 end
