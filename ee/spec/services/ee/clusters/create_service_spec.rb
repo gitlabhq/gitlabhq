298f023e029d302bb41352517ddc411ba5dcb611
Matija Čupić <matteeyah@gmail.com>
2018-05-13 14:14:39 +0200

    Extract shared components from Clusters::CreateService spec

--- ee/spec/services/ee/clusters/create_service_spec.rb
+++ ee/spec/services/ee/clusters/create_service_spec.rb
@@ -8,64 +8,6 @@ describe Clusters::CreateService do
 
   subject { described_class.new(project, user, params).execute(access_token) }
 
-  shared_context 'valid params' do
-    let(:params) do
-      {
-        name: 'test-cluster',
-        provider_type: :gcp,
-        provider_gcp_attributes: {
-          gcp_project_id: 'gcp-project',
-          zone: 'us-central1-a',
-          num_nodes: 1,
-          machine_type: 'machine_type-a'
-        }
-      }
-    end
-  end
-
-  shared_context 'invalid params' do
-    let(:params) do
-      {
-        name: 'test-cluster',
-        provider_type: :gcp,
-        provider_gcp_attributes: {
-          gcp_project_id: '!!!!!!!',
-          zone: 'us-central1-a',
-          num_nodes: 1,
-          machine_type: 'machine_type-a'
-        }
-      }
-    end
-  end
-
-  shared_examples 'create cluster' do
-    it 'creates a cluster object and performs a worker' do
-      expect(ClusterProvisionWorker).to receive(:perform_async)
-
-      expect { subject }
-        .to change { Clusters::Cluster.count }.by(1)
-        .and change { Clusters::Providers::Gcp.count }.by(1)
-
-      expect(subject.name).to eq('test-cluster')
-      expect(subject.user).to eq(user)
-      expect(subject.project).to eq(project)
-      expect(subject.provider.gcp_project_id).to eq('gcp-project')
-      expect(subject.provider.zone).to eq('us-central1-a')
-      expect(subject.provider.num_nodes).to eq(1)
-      expect(subject.provider.machine_type).to eq('machine_type-a')
-      expect(subject.provider.access_token).to eq(access_token)
-      expect(subject.platform).to be_nil
-    end
-  end
-
-  shared_examples 'error' do
-    it 'returns an error' do
-      expect(ClusterProvisionWorker).not_to receive(:perform_async)
-      expect { subject }.to change { Clusters::Cluster.count }.by(0)
-      expect(subject.errors[:"provider_gcp.gcp_project_id"]).to be_present
-    end
-  end
-
   before do
     allow(project).to receive(:feature_available?).and_call_original
   end
@@ -76,20 +18,20 @@ describe Clusters::CreateService do
     end
 
     context 'when correct params' do
-      include_context 'valid params'
+      include_context 'valid cluster create params'
 
-      include_examples 'create cluster'
+      include_examples 'create cluster service success'
     end
 
     context 'when invalid params' do
-      include_context 'invalid params'
+      include_context 'invalid cluster create params'
 
-      include_examples 'error'
+      include_examples 'create cluster service error'
     end
   end
 
   context 'when license does not have multiple clusters feature' do
-    include_context 'valid params'
+    include_context 'valid cluster create params'
 
     before do
       allow(project).to receive(:feature_available?).with(:multiple_clusters).and_return(false)
