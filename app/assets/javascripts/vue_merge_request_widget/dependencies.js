bd822ca32ba568a6f65e78f2c18abe9227f1e4d1
Filipa Lacerda <filipa@gitlab.com>
2018-05-14 17:45:16 +0100

    Fix conflicts for app/assets/javascripts/vue_merge_request_widget/dependencies.js

--- app/assets/javascripts/vue_merge_request_widget/dependencies.js
+++ app/assets/javascripts/vue_merge_request_widget/dependencies.js
@@ -38,15 +38,9 @@ export { default as CheckingState } from './components/states/mr_widget_checking
 export { default as MRWidgetStore } from 'ee/vue_merge_request_widget/stores/mr_widget_store';
 export { default as MRWidgetService } from 'ee/vue_merge_request_widget/services/mr_widget_service';
 export { default as eventHub } from './event_hub';
-<<<<<<< HEAD
 export { default as getStateKey } from 'ee/vue_merge_request_widget/stores/get_state_key';
 export { default as stateMaps } from 'ee/vue_merge_request_widget/stores/state_maps';
 export { default as SquashBeforeMerge } from 'ee/vue_merge_request_widget/components/states/mr_widget_squash_before_merge.vue';
-=======
-export { default as getStateKey } from './stores/get_state_key';
-export { default as stateMaps } from './stores/state_maps';
-export { default as SquashBeforeMerge } from './components/states/mr_widget_squash_before_merge.vue';
->>>>>>> upstream/master
 export { default as notify } from '../lib/utils/notify';
 export { default as SourceBranchRemovalStatus } from './components/source_branch_removal_status.vue';
 
