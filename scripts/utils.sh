0ab6469187285368d9f64f9ec67dbbcfa3e5a901
Lin Jen-Shin <godfat@godfat.org>
2018-05-15 01:49:46 +0800

    Grant privileges after database is created
    Never drop the database when granting privileges

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -13,7 +13,17 @@ retry() {
     return 1
 }
 
+setup_db_user_only() {
+    if [ "$GITLAB_DATABASE" = "postgresql" ]; then
+        . scripts/create_postgres_user.sh
+    else
+        . scripts/create_mysql_user.sh
+    fi
+}
+
 setup_db() {
+    setup_db_user_only
+
     bundle exec rake db:drop db:create db:schema:load db:migrate
 
     if [ "$GITLAB_DATABASE" = "mysql" ]; then
