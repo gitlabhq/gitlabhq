1cfa5ed07065f04531900fe0931deaaaef3e69d2
Dylan Griffith <dyl.griffith@gmail.com>
2018-05-16 09:56:28 +0200

    Refactor out duplication in runner_policy.rb

--- app/policies/ci/runner_policy.rb
+++ app/policies/ci/runner_policy.rb
@@ -6,10 +6,14 @@ module Ci
     condition(:owned_runner) { @user.ci_owned_runners.exists?(@subject.id) }
 
     rule { anonymous }.prevent_all
-    rule { admin | owned_runner }.enable :assign_runner
-    rule { admin | owned_runner }.enable :read_runner
-    rule { admin | owned_runner }.enable :update_runner
-    rule { admin | owned_runner }.enable :delete_runner
+
+    rule { admin | owned_runner }.policy do
+      enable :assign_runner
+      enable :read_runner
+      enable :update_runner
+      enable :delete_runner
+    end
+
     rule { ~admin & locked }.prevent :assign_runner
   end
 end
