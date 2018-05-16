6d81905fafb23f20520eb5ffa46c94b18ccc8686
Rémy Coutable <remy@rymai.me>
2018-05-16 20:26:22 +0200

    Introduce a new Keys::DestroyService service
    Signed-off-by: Rémy Coutable <remy@rymai.me>

---
+++ app/services/keys/destroy_service.rb
@@ -0,0 +1,12 @@
+module Keys
+  class DestroyService < ::Keys::BaseService
+    def execute(key)
+      key.destroy if destroy_possible?(key)
+    end
+
+    # overriden in EE::Keys::DestroyService
+    def destroy_possible?(key)
+      true
+    end
+  end
+end
