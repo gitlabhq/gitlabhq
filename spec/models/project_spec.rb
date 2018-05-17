99caa7ba61dba3cbfb8b247603b2c41b19833b60
Ash McKenzie <amckenzie@gitlab.com>
2018-05-17 10:42:29 +1000

    Resolve conflicts in spec/models/project_spec.rb

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -77,12 +77,8 @@ describe Project do
     it { is_expected.to have_many(:project_group_links) }
     it { is_expected.to have_many(:notification_settings).dependent(:delete_all) }
     it { is_expected.to have_many(:forks).through(:forked_project_links) }
-<<<<<<< HEAD
     it { is_expected.to have_many(:approver_groups).dependent(:destroy) }
     it { is_expected.to have_many(:uploads).dependent(:destroy) }
-=======
-    it { is_expected.to have_many(:uploads) }
->>>>>>> upstream/master
     it { is_expected.to have_many(:pipeline_schedules) }
     it { is_expected.to have_many(:members_and_requesters) }
     it { is_expected.to have_many(:clusters) }
