a660f548966bc8232cb56c4e344b0da3c91b3701
Mark Chao <mchao@gitlab.com>
2018-05-18 09:50:01 +0800

    Plural reason(s)

--- spec/mailers/notify_spec.rb
+++ spec/mailers/notify_spec.rb
@@ -422,6 +422,7 @@ describe Notify do
           aggregate_failures do
             is_expected.to have_referable_subject(merge_request, reply: true)
             is_expected.to have_body_text(project_merge_request_path(project, merge_request))
+            is_expected.to have_body_text('reasons:')
             reasons.each do |reason|
               is_expected.to have_body_text(reason)
             end
