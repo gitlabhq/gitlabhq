f9fea6ae90c0db3d2bd32334161e673d5b0ff4ae
Stan Hu <stanhu@gmail.com>
2018-05-20 21:01:11 -0700

    Improve and fix spec in ee/spec/lib/gitlab/geo/logger_spec.rb

--- ee/spec/lib/gitlab/geo/logger_spec.rb
+++ ee/spec/lib/gitlab/geo/logger_spec.rb
@@ -3,8 +3,9 @@ require 'spec_helper'
 describe Gitlab::Geo::Logger do
   it 'uses the same log_level defined in Rails' do
     allow(Rails.logger).to receive(:level) { 99 }
-    expect_any_instance_of(::Gitlab::Geo::Logger).to receive(:level=).with(99)
 
-    described_class.build
+    logger = described_class.build
+
+    expect(logger.level).to eq(99)
   end
 end
