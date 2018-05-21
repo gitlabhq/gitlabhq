d8d30f8cb88d5ce2431d84e3acb015bbe33548a7
Andreas Brandl <abrandl@gitlab.com>
2018-05-21 20:16:46 +0200

    Increase text limit for GPG keys (mysql only).
    Closes #46530.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180517082340) do
+ActiveRecord::Schema.define(version: 20180521171529) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
