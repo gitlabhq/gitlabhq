1c0b943fabab31ba9af9d2874b131447dd07406f
Clement Ho <ClemMakesApps@gmail.com>
2018-05-22 17:27:53 -0500

    Scope to .dropdown

--- app/assets/javascripts/ci_variable_list/ci_variable_list.js
+++ app/assets/javascripts/ci_variable_list/ci_variable_list.js
@@ -142,7 +142,7 @@ export default class VariableList {
     });
 
     // Close any dropdowns
-    $rowClone.find('.show').each((index, $dropdown) => {
+    $rowClone.find('.dropdown.show').each((index, $dropdown) => {
       $dropdown.classList.remove('show');
     });
 
