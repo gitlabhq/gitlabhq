6b9d496638588400b44991cec0b50b30bc7124bd
Mario de la Ossa <mariodelaossa@gmail.com>
2018-05-21 19:43:16 -0600

    Extract EE-specific code from app/services/application_settings/update_service.rb

---
+++ ee/app/services/ee/application_settings/update_service.rb
@@ -0,0 +1,17 @@
+module EE
+  module ApplicationSettings
+    module UpdateService
+      extend ::Gitlab::Utils::Override
+      extend ActiveSupport::Concern
+
+      override :execute
+      def execute
+        # Repository size limit comes as MB from the view
+        limit = params.delete(:repository_size_limit)
+        application_setting.repository_size_limit = ::Gitlab::Utils.try_megabytes_to_bytes(limit) if limit
+
+        super
+      end
+    end
+  end
+end
