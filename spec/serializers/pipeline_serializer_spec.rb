01792f5000fe0ac6ad5b2362b8e7b8c704794c1e
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2018-05-24 16:55:24 +0200

    Reduce pipeline serialization queries when preloaded

--- spec/serializers/pipeline_serializer_spec.rb
+++ spec/serializers/pipeline_serializer_spec.rb
@@ -99,7 +99,8 @@ describe PipelineSerializer do
       end
     end
 
-    context 'number of queries' do
+    describe 'number of queries when preloaded' do
+      subject { serializer.represent(resource, preload: true) }
       let(:resource) { Ci::Pipeline.all }
 
       before do
@@ -120,7 +121,7 @@ describe PipelineSerializer do
         it 'verifies number of queries', :request_store do
           recorded = ActiveRecord::QueryRecorder.new { subject }
 
-          expect(recorded.count).to be_within(1).of(38)
+          expect(recorded.count).to be_within(1).of(31)
           expect(recorded.cached_count).to eq(0)
         end
       end
@@ -139,7 +140,7 @@ describe PipelineSerializer do
           # pipeline. With the same ref this check is cached but if refs are
           # different then there is an extra query per ref
           # https://gitlab.com/gitlab-org/gitlab-ce/issues/46368
-          expect(recorded.count).to be_within(1).of(45)
+          expect(recorded.count).to be_within(1).of(38)
           expect(recorded.cached_count).to eq(0)
         end
       end
