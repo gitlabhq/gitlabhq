2b8eb7273efee84180b322a0aac33e1f4ee30d2c
Rémy Coutable <remy@rymai.me>
2018-05-25 18:44:15 +0200

    Ensure ApplicationSetting#performance_bar_allowed_group_id is properly set when retrieved from cache
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- app/services/application_settings/update_service.rb
+++ app/services/application_settings/update_service.rb
@@ -3,6 +3,10 @@ module ApplicationSettings
     def execute
       update_terms(@params.delete(:terms))
 
+      if params.key?(:performance_bar_allowed_group_path)
+        params[:performance_bar_allowed_group_id] = performance_bar_allowed_group_id
+      end
+
       @application_setting.update(@params)
     end
 
@@ -17,6 +21,14 @@ module ApplicationSettings
 
       ApplicationSetting::Term.create(terms: terms)
       @application_setting.reset_memoized_terms
+    end
+
+    def performance_bar_allowed_group_id
+      performance_bar_enabled = !params.key?(:performance_bar_enabled) || params.delete(:performance_bar_enabled)
+      group_full_path = params.delete(:performance_bar_allowed_group_path)
+      return nil unless Gitlab::Utils.to_boolean(performance_bar_enabled)
+
+      Group.find_by_full_path(group_full_path)&.id if group_full_path.present?
     end
   end
 end
