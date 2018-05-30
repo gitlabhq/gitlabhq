de761bea1b1d5ea75fa518f6273d5866e287f1f3
Imre Farkas <ifarkas@gitlab.com>
2018-05-30 12:12:42 +0200

    Enforce UTF-8 encoding on user input in LogrageWithTimestamp formatter and filter out file content from logs

--- config/application.rb
+++ config/application.rb
@@ -84,6 +84,7 @@ module Gitlab
     # - Webhook URLs (:hook)
     # - Sentry DSN (:sentry_dsn)
     # - Deploy keys (:key)
+    # - File content from Web Editor (:content)
     config.filter_parameters += [/token$/, /password/, /secret/]
     config.filter_parameters += %i(
       certificate
@@ -95,6 +96,7 @@ module Gitlab
       sentry_dsn
       trace
       variables
+      content
     )
 
     # Enable escaping HTML in JSON.
