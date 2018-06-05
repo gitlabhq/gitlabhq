7af70a4e9fe1ea14f5b84553dedd5beec7da75f7
Sean McGivern <sean@gitlab.com>
2018-06-05 18:31:32 +0100

    Force Postgres to avoid trigram indexes when in a group
    When filtering issues with a search string in a group, we observed on GitLab.com
    that Postgres was using an inefficient query plan, preferring the (global)
    trigram indexes on description and title, rather than using a filter on the
    restricted set of issues within the group.
    
    Change the callers of the IssuableFinder to use a CTE in this case to fence the
    rest of the query from the LIKE filters, so that the optimiser is forced to
    perform the filter in the order we prefer.
    
    This will only force the use of a CTE when:
    
    1. The use_cte_for_search params is truthy.
    2. We are using Postgres.
    3. We have passed the `search` param.
    
    The third item is important - searching issues using the search box does not use
    the finder in this way, but contructs a query and appends `full_search` to
    that. For some reason, this query does not suffer from the same issue.
    
    Currenly, we only pass this param when filtering issuables (issues or MRs) in a
    group context.

--- app/controllers/concerns/issuable_collections.rb
+++ app/controllers/concerns/issuable_collections.rb
@@ -96,6 +96,7 @@ module IssuableCollections
     elsif @group
       @filter_params[:group_id] = @group.id
       @filter_params[:include_subgroups] = true
+      @filter_params[:use_cte_for_search] = true
     end
 
     @filter_params.permit(finder_type.valid_params)
