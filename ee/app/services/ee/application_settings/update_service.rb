2eb84ce92a4d5644ec8b938a3a2334eb09dc0b60
Bob Van Landuyt <bob@vanlanduyt.co>
2018-06-05 15:56:29 +0200

    Validate classification label on create & update
    This makes sure that the current user is allowed to access the project
    once the classification label has changed.

--- ee/app/services/ee/application_settings/update_service.rb
+++ ee/app/services/ee/application_settings/update_service.rb
@@ -1,6 +1,7 @@
 module EE
   module ApplicationSettings
     module UpdateService
+      include ValidatesClassificationLabel
       extend ::Gitlab::Utils::Override
       extend ActiveSupport::Concern
 
@@ -9,6 +10,12 @@ module EE
         # Repository size limit comes as MB from the view
         limit = params.delete(:repository_size_limit)
         application_setting.repository_size_limit = ::Gitlab::Utils.try_megabytes_to_bytes(limit) if limit
+
+        validate_classification_label(application_setting, :external_authorization_service_default_label)
+
+        if application_setting.errors.any?
+          return false
+        end
 
         super
       end
