2bd5dadbd1b742d15bfb0914ad1be7e2182c1755
Shinya Maeda <shinya@gitlab.com>
2018-06-05 19:41:08 +0900

    Add a gurad caluse to block adding concurrent index for mysql

--- db/migrate/20180428200509_add_index_to_ci_builds_artifacts_file.rb
+++ db/migrate/20180428200509_add_index_to_ci_builds_artifacts_file.rb
@@ -6,10 +6,12 @@ class AddIndexToCiBuildsArtifactsFile < ActiveRecord::Migration
   disable_ddl_transaction!
 
   def up
+    return unless Gitlab::Database.postgresql?
+
     # We add an temporary index to `ci_builds.artifacts_file` column to avoid statements timeout in legacy artifacts migrations
     # This index is to be removed after we have cleaned up background migrations
     # https://gitlab.com/gitlab-org/gitlab-ce/issues/46866
-    add_concurrent_index :ci_builds, :artifacts_file, where: "artifacts_file <> ''", length: 20
+    add_concurrent_index :ci_builds, :artifacts_file, where: "artifacts_file <> ''"
   end
 
   def down
