c0ec68af02706d3c01b20b9f589113a900ff2071
Shinya Maeda <shinya@gitlab.com>
2018-06-05 17:51:40 +0900

    Separate add_concurrent_index by database type

--- db/migrate/20180428200509_add_index_to_ci_builds_artifacts_file.rb
+++ db/migrate/20180428200509_add_index_to_ci_builds_artifacts_file.rb
@@ -9,10 +9,14 @@ class AddIndexToCiBuildsArtifactsFile < ActiveRecord::Migration
     # We add an temporary index to `ci_builds.artifacts_file` column to avoid statements timeout in legacy artifacts migrations
     # This index is to be removed after we have cleaned up background migrations
     # https://gitlab.com/gitlab-org/gitlab-ce/issues/46866
-    add_concurrent_index :ci_builds, :artifacts_file, where: "artifacts_file <> ''", length: 65535 - 1
+    if Gitlab::Database.mysql?
+      add_concurrent_index :ci_builds, :artifacts_file, length: 65535 - 1
+    else
+      add_concurrent_index :ci_builds, :artifacts_file, where: "artifacts_file <> ''"
+    end
   end
 
   def down
-    remove_concurrent_index :ci_builds, :artifacts_file, where: "artifacts_file <> ''", length: 65535 - 1
+    remove_concurrent_index :ci_builds, :artifacts_file
   end
 end
