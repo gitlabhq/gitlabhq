94b9485e88580895702398befbc5d0f2971cabf9
Francisco Javier López <fjlopez@gitlab.com>
2018-06-06 19:23:42 +0200

    Using singleton module

--- lib/api/helpers/full_route_builder.rb
+++ lib/api/helpers/full_route_builder.rb
@@ -1,16 +1,13 @@
 module API
   module Helpers
     class FullRouteBuilder
+      include Singleton
       include ::API::Helpers::RelatedResourcesHelpers
 
       def self.full_url(api_route_name, params)
-        return unless builder.respond_to? api_route_name
+        return unless instance.respond_to? api_route_name
 
-        builder.expose_url(builder.send(api_route_name, params))
-      end
-
-      def self.builder
-        @builder ||= new
+        instance.expose_url(instance.send(api_route_name, params))
       end
     end
   end
