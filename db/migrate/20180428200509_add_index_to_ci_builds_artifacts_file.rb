7728ab3f9dba0af21033e1ca3b0213cacaef85d8
Shinya Maeda <shinya@gitlab.com>
2018-06-06 15:28:50 +0900

    Try to explicitly separate with postgresql and mysql for addin index

--- db/migrate/20180428200509_add_index_to_ci_builds_artifacts_file.rb
+++ db/migrate/20180428200509_add_index_to_ci_builds_artifacts_file.rb
@@ -6,17 +6,21 @@ class AddIndexToCiBuildsArtifactsFile < ActiveRecord::Migration
   disable_ddl_transaction!
 
   def up
-    return unless Gitlab::Database.postgresql?
-
     # We add an temporary index to `ci_builds.artifacts_file` column to avoid statements timeout in legacy artifacts migrations
     # This index is to be removed after we have cleaned up background migrations
     # https://gitlab.com/gitlab-org/gitlab-ce/issues/46866
-    add_concurrent_index :ci_builds, :artifacts_file, where: "artifacts_file <> ''", length: 20
+    if Gitlab::Database.postgresql?
+      add_concurrent_index :ci_builds, :artifacts_file, where: "artifacts_file <> ''"
+    elsif Gitlab::Database.mysql?
+      add_concurrent_index :ci_builds, :artifacts_file, where: "artifacts_file <> ''", length: 20
+    end
   end
 
   def down
-    return unless Gitlab::Database.postgresql?
-
-    remove_concurrent_index :ci_builds, :artifacts_file
+    if Gitlab::Database.postgresql?
+      remove_concurrent_index :ci_builds, :artifacts_file, where: "artifacts_file <> ''"
+    elsif Gitlab::Database.mysql?
+      remove_concurrent_index :ci_builds, :artifacts_file, where: "artifacts_file <> ''", length: 20
+    end
   end
 end
