08d513a59e3cc2aa09b91217773267f3d3fd5653
Dennis Tang <dtang@gitlab.com>
2018-06-05 21:47:09 -0700

    redirect back to form if token expires

--- app/controllers/projects/clusters/gcp_controller.rb
+++ app/controllers/projects/clusters/gcp_controller.rb
@@ -7,17 +7,21 @@ class Projects::Clusters::GcpController < Projects::ApplicationController
   helper_method :valid_gcp_token
 
   def create
-    @cluster = ::Clusters::CreateService
-      .new(project, current_user, create_params)
-      .execute(token_in_session)
+    if valid_gcp_token
+      @cluster = ::Clusters::CreateService
+        .new(project, current_user, create_params)
+        .execute(token_in_session)
 
-    if @cluster.persisted?
-      redirect_to project_cluster_path(project, @cluster)
+      if @cluster.persisted?
+        redirect_to project_cluster_path(project, @cluster)
+      else
+        @gcp_cluster = @cluster
+        user_cluster
+
+        render 'projects/clusters/new', locals: { active_tab: 'gcp' }
+      end
     else
-      @gcp_cluster = @cluster
-      user_cluster
-
-      render 'projects/clusters/new', locals: { active_tab: 'gcp' }
+      redirect_to new_project_cluster_path(@project)
     end
   end
 
