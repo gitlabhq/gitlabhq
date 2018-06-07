132db99a5b98daf7629f416ebb4ff817010cc0a9
Phil Hughes <me@iamphill.com>
2018-06-07 15:11:10 +0100

    fixed karma

--- spec/javascripts/lib/utils/url_utility_spec.js
+++ spec/javascripts/lib/utils/url_utility_spec.js
@@ -2,6 +2,10 @@ import { webIDEUrl } from '~/lib/utils/url_utility';
 
 describe('URL utility', () => {
   describe('webIDEUrl', () => {
+    afterEach(() => {
+      gon.relative_url_root = '';
+    });
+
     describe('without relative_url_root', () => {
       it('returns IDE path with route', () => {
         expect(webIDEUrl('/gitlab-org/gitlab-ce/merge_requests/1')).toBe(
