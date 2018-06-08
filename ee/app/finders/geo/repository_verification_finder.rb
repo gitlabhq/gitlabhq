9c654efc3543942795394e5207bc1280fe469031
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2018-06-08 16:19:19 -0300

    Optimize query to return outdated projects

--- ee/app/finders/geo/repository_verification_finder.rb
+++ ee/app/finders/geo/repository_verification_finder.rb
@@ -1,20 +1,45 @@
 module Geo
   class RepositoryVerificationFinder
+    attr_reader :shard_name
+
+    def initialize(shard_name: nil)
+      @shard_name = shard_name
+    end
+
     def find_outdated_projects(batch_size:)
-      Project.select(:id)
-       .with_route
-       .joins(:repository_state)
-       .where(repository_outdated.or(wiki_outdated))
-       .order(last_repository_updated_at_asc)
-       .limit(batch_size)
+      cte_definition =
+        projects_table
+          .join(repository_state_table).on(project_id_matcher)
+          .project(projects_table[:id], projects_table[:last_repository_updated_at])
+          .where(repository_outdated.or(wiki_outdated))
+          .take(batch_size)
+
+      if shard_name.present?
+        cte_definition = shard_restriction(cte_definition)
+      end
+
+      cte_table    = Arel::Table.new(:outdated_projects)
+      composed_cte = Arel::Nodes::As.new(cte_table, cte_definition)
+      alias_to     = Arel::Nodes::As.new(cte_table, projects_table)
+
+      Project.with(composed_cte)
+             .from(alias_to)
+             .order(last_repository_updated_at_asc)
     end
 
     def find_unverified_projects(batch_size:)
-      Project.select(:id)
-       .with_route
-       .joins(left_join_repository_state)
-       .where(repository_never_verified)
-       .limit(batch_size)
+      relation =
+        Project.select(:id)
+         .with_route
+         .joins(left_join_repository_state)
+         .where(repository_never_verified)
+         .limit(batch_size)
+
+      if shard_name.present?
+        relation = shard_restriction(relation)
+      end
+
+      relation
     end
 
     def count_verified_repositories
@@ -43,10 +68,14 @@ module Geo
       ProjectRepositoryState.arel_table
     end
 
+    def project_id_matcher
+      projects_table[:id].eq(repository_state_table[:project_id])
+    end
+
     def left_join_repository_state
       projects_table
         .join(repository_state_table, Arel::Nodes::OuterJoin)
-        .on(projects_table[:id].eq(repository_state_table[:project_id]))
+        .on(project_id_matcher)
         .join_sources
     end
 
@@ -66,6 +95,10 @@ module Geo
 
     def last_repository_updated_at_asc
       Gitlab::Database.nulls_last_order('projects.last_repository_updated_at', 'ASC')
+    end
+
+    def shard_restriction(relation)
+      relation.where(projects_table[:repository_storage].eq(shard_name))
     end
   end
 end
