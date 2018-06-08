81e785349c30bd84495faf633ec1adc1c3913baf
Kushal Pandya <kushal@gitlab.com>
2018-06-08 18:31:41 +0530

    Resolve conflicts for Issue boards assignees list

--- app/assets/javascripts/boards/models/list.js
+++ app/assets/javascripts/boards/models/list.js
@@ -55,12 +55,8 @@ class List {
       entityType = 'assignee_id';
     }
 
-<<<<<<< HEAD
     return gl.boardService
       .createList(entity.id, entityType)
-=======
-    return gl.boardService.createList(this.label.id)
->>>>>>> upstream/master
       .then(res => res.data)
       .then(data => {
         this.id = data.id;
