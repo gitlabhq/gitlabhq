23864e196db99ae3f82ce5529f284489aefd0118
Oswaldo Ferreira <oswaldo@gitlab.com>
2018-06-08 20:07:21 -0300

    Use OpenStruct instead Hashie::Mash

--- app/models/cached_blob.rb
+++ app/models/cached_blob.rb
@@ -2,7 +2,7 @@ class CachedBlob
   delegate *BlobsService::CACHED_METHODS, to: :@blob_cache
 
   def initialize(blob_cache)
-    @blob_cache = Hashie::Mash.new(blob_cache)
+    @blob_cache = OpenStruct.new(blob_cache)
   end
 
   def load_all_data!
