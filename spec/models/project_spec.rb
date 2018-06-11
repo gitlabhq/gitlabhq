072f9224afded08cb1c93fa0e6c077928f60cca0
Stan Hu <stanhu@gmail.com>
2018-06-11 12:06:25 -0700

    Relax expectation in spec/models/project_spec.rb
    Multiple Sidekiq workers can run in the spec, causing `Project.find`
    to be run in different places. Instead of setting a fixed number of
    calls, just use `allow`.

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1734,7 +1734,7 @@ describe Project do
         .and_return(true)
 
       # Works around https://github.com/rspec/rspec-mocks/issues/910
-      expect(described_class).to receive(:find).with(project.id).twice.and_return(project)
+      allow(described_class).to receive(:find).with(project.id).and_return(project)
       expect(project.repository).to receive(:after_import)
         .and_call_original
       expect(project.wiki.repository).to receive(:after_import)
