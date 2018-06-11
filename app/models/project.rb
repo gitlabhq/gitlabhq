pasteof/gitlab-ce

Fixes rejected pushes from maintainers
Before the push git would make a call to
`/:namespace/:project/git-receive-pack`. This would perform an access
check without a ref. So the `Project#branch_allows_maintainer_push?`
would return false.

This adjusts `Project#branch_allows_maintainer_push?` to return true
when passing no branch name if there are merge requests open that
would allow the user to push.

The actual check then happens when a call to
`/api/v4/internal/allowed` is made from a git hook.

app/models/project.rb
@@ -2122,10 +2122,14 @@ class Project < ActiveRecord::Base
     check_access = -> do
       next false if empty_repo?
 
-      merge_request = source_of_merge_requests.opened
-                        .where(allow_maintainer_to_push: true)
-                        .find_by(source_branch: branch_name)
-      merge_request&.can_be_merged_by?(user)
+      merge_requests = source_of_merge_requests.opened
+                         .where(allow_maintainer_to_push: true)
+
+      if branch_name
+        merge_requests.find_by(source_branch: branch_name)&.can_be_merged_by?(user)
+      else
+        merge_requests.any? { |merge_request| merge_request.can_be_merged_by?(user) }
+      end
     end
 
     if RequestStore.active?

spec/models/project_spec.rb
@@ -3598,6 +3598,11 @@ describe Project do
           .to be_truthy
       end
 
+      it 'allows access when there are merge requests open but no branch name is given' do
+        expect(project.branch_allows_maintainer_push?(user, nil))
+          .to be_truthy
+      end
+
       it 'does not allow guest users access' do
         guest = create(:user)
         target_project.add_guest(guest)

gitlabhq/gitlabhq

app/models/project.rb
@@ -2139,10 +2139,14 @@ class Project < ActiveRecord::Base
     check_access = -> do
       next false if empty_repo?
 
-      merge_request = source_of_merge_requests.opened
-                        .where(allow_collaboration: true)
-                        .find_by(source_branch: branch_name)
-      merge_request&.can_be_merged_by?(user)
+      merge_requests = source_of_merge_requests.opened
+                         .where(allow_collaboration: true)
+
+      if branch_name
+        merge_requests.find_by(source_branch: branch_name)&.can_be_merged_by?(user)
+      else
+        merge_requests.any? { |merge_request| merge_request.can_be_merged_by?(user) }
+      end
     end
 
     if RequestStore.active?

spec/models/project_spec.rb
@@ -3658,6 +3658,11 @@ describe Project do
           .to be_truthy
       end
 
+      it 'allows access when there are merge requests open but no branch name is given' do
+        expect(project.branch_allows_collaboration?(user, nil))
+          .to be_truthy
+      end
+
       it 'does not allow guest users access' do
         guest = create(:user)
         target_project.add_guest(guest)