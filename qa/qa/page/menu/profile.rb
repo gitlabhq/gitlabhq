40e95746247a70112b4676e2ed44e11bb0f995fa
Mark Lapierre <mlapierre@gmail.com>
2018-06-11 18:58:06 -0400

    Add an SSH key and use it to clone and push

--- qa/qa/page/menu/profile.rb
+++ qa/qa/page/menu/profile.rb
@@ -6,11 +6,18 @@ module QA
           element :access_token_link, 'link_to profile_personal_access_tokens_path'
           element :access_token_title, 'Access Tokens'
           element :top_level_items, '.sidebar-top-level-items'
+          element :ssh_keys, 'SSH Keys'
         end
 
         def click_access_tokens
           within_sidebar do
             click_link('Access Tokens')
+          end
+        end
+
+        def click_ssh_keys
+          within_sidebar do
+            click_link('SSH Keys')
           end
         end
 
