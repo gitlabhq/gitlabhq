9d6a41f0066dca85bea797320cb33cd3c099f5ad
Toon Claes <toon@gitlab.com>
2018-06-12 11:52:23 +0200

    Test #down migration for all the tables

--- ee/spec/migrations/drop_repository_storage_events_for_geo_events_spec.rb
+++ ee/spec/migrations/drop_repository_storage_events_for_geo_events_spec.rb
@@ -4,15 +4,14 @@ require 'spec_helper'
 require Rails.root.join('ee', 'db', 'post_migrate', '20180417102933_drop_repository_storage_events_for_geo_events.rb')
 
 describe DropRepositoryStorageEventsForGeoEvents, :migration do
-  let(:migration) { described_class.new }
-
   describe '#up' do
     before do
-      migration.up
+      schema_migrate_up!
     end
 
-    it 'dropped the repository_storage_path column' do
-      described_class::TABLES.each do |table_name|
+    where(table_name: described_class::TABLES)
+    with_them do
+      it 'dropped the repository_storage_path column' do
         columns = table(table_name).columns.map(&:name)
 
         expect(columns).not_to include("repository_storage_path")
@@ -21,35 +20,71 @@ describe DropRepositoryStorageEventsForGeoEvents, :migration do
   end
 
   describe '#down' do
-    let(:event_name) { :geo_repository_created_event }
+    let(:namespace) { table(:namespaces).create!(name: 'foo', path: 'foo_namespace') }
+    let(:project) { table(:projects).create!(name: 'bar', path: 'path/to/bar', namespace_id: namespace.id) }
 
-    before do
-      described_class.const_set(:BATCH_SIZE, 1)
+    shared_examples 'recreates the repository_storage_path column' do
+      before do
+        schema_migrate_up!
 
-      Gitlab.config.repositories.storages.each do |name, _|
-        described_class.execute(<<~SQL
-                                INSERT INTO #{event_name}s (project_id, project_name, repository_storage_name, repo_path)
-                                VALUES (1, 'mepmep', '#{name}', 'path/to/gitlab-org')
-                                SQL
-                               )
+        Gitlab.config.repositories.storages.each do |name, _|
+          table(table_name).create!({ project_id: project.id, repository_storage_name: name }.merge(extra_cols))
+        end
+
+        schema_migrate_down!
       end
 
-      migration.down
-      reset_column_in_all_models
-    end
-
-    it 'created the repository_storage_path column' do
-      described_class::TABLES.each do |table_name|
+      it 'should fill in repository_storage_path' do
         columns = table(table_name).columns.map(&:name)
 
         expect(columns).to include("repository_storage_path")
-      end
 
-      null_columns = described_class
-        .execute("SELECT COUNT(*) FROM #{event_name}s WHERE repository_storage_path IS NULL;")
+        null_columns = described_class
+        .execute("SELECT COUNT(*) FROM #{table_name} WHERE repository_storage_path IS NULL;")
         .first['count']
 
-      expect(null_columns.to_i).to be(0)
+        expect(null_columns.to_i).to eq(0)
+      end
+    end
+
+    context 'geo_hashed_storage_migrated_events' do
+      let(:table_name) { :geo_hashed_storage_migrated_events }
+      let(:extra_cols) do
+        {
+          old_disk_path: '/ye/olde/path', new_disk_path: 'da39a3ee5e6b4b0d3255bfef95601890afd80709',
+          old_wiki_disk_path: '/ye/olde/path.wiki', new_wiki_disk_path: 'da39a3ee5e6b4b0d3255bfef95601890afd80709.wiki',
+          new_storage_version: 2
+        }
+      end
+
+      it_behaves_like 'recreates the repository_storage_path column'
+    end
+
+    context 'geo_repository_created_events' do
+      let(:table_name) { :geo_repository_created_events }
+      let(:extra_cols) { { project_name: project.name, repo_path: project.path } }
+
+      it_behaves_like 'recreates the repository_storage_path column'
+    end
+
+    context 'geo_repository_deleted_events' do
+      let(:table_name) { :geo_repository_deleted_events }
+      let(:extra_cols) { { deleted_path: '/null/path', deleted_project_name: 'im_gone' } }
+
+      it_behaves_like 'recreates the repository_storage_path column'
+    end
+
+    context 'geo_repository_renamed_events' do
+      let(:table_name) { :geo_repository_renamed_events }
+      let(:extra_cols) do
+        {
+          old_path: '/ye/olde/path', new_path: '/ze/n3w/p4th',
+          old_path_with_namespace: '/ye/olde/namespace/path', new_path_with_namespace: '/ze/n3w/n4mesp4ce/p4th',
+          old_wiki_path_with_namespace: '/ye/olde/namespace/path.wiki', new_wiki_path_with_namespace: '/ze/n3w/n4mesp4ce/p4th.wiki'
+        }
+      end
+
+      it_behaves_like 'recreates the repository_storage_path column'
     end
   end
 end
