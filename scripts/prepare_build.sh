1fb0c5da45c6619a4a85304c7eb031122afd2618
Shinya Maeda <shinya@gitlab.com>
2018-06-12 17:56:56 +0900

    Add db:migrate before setup_db

--- scripts/prepare_build.sh
+++ scripts/prepare_build.sh
@@ -49,6 +49,8 @@ sed -i 's/localhost/redis/g' config/redis.queues.yml
 cp config/redis.shared_state.yml.example config/redis.shared_state.yml
 sed -i 's/localhost/redis/g' config/redis.shared_state.yml
 
+bundle exec rake db:migrate
+
 if [ "$SETUP_DB" != "false" ]; then
     setup_db
 elif getent hosts postgres || getent hosts mysql; then
