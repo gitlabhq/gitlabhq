fc99ed95b3497b154f49f98d49e193b94c79ccb5
Paul Slaughter <pslaughter@gitlab.com>
2018-06-13 14:53:17 -0500

    Add spec for edit board name

--- ee/spec/features/boards/boards_spec.rb
+++ ee/spec/features/boards/boards_spec.rb
@@ -5,12 +5,12 @@ describe 'issue boards', :js do
   let(:project) { create(:project, :public) }
   let!(:board) { create(:board, project: project) }
 
-  before do
-    project.add_developer(user)
-    login_as(user)
-  end
-
   context 'issue board focus mode' do
+    before do
+      project.add_developer(user)
+      login_as(user)
+    end
+
     it 'shows the button when the feature is enabled' do
       stub_licensed_features(issue_board_focus_mode: true)
 
@@ -25,6 +25,30 @@ describe 'issue boards', :js do
       visit_board_page
 
       expect(page).not_to have_link('Toggle focus mode')
+    end
+  end
+
+  context 'with group and reporter' do
+    let(:group) { create(:group) }
+    let(:project) { create(:project, :public, namespace: group) }
+
+    before do
+      project.add_master(user)
+      group.add_reporter(user)
+      login_as(user)
+    end
+
+    it 'can edit board name' do
+      visit_board_page
+
+      board_name = board.name
+      new_board_name = board_name + '-Test'
+
+      click_button 'Edit board'
+      fill_in 'board-new-name', with: new_board_name
+      click_button 'Save changes'
+
+      expect(page).to have_content new_board_name
     end
   end
 
