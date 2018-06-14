d9c61790d0e9e93d076c0943ae1b33be63da1ca8
Filipa Lacerda <filipa@gitlab.com>
2018-06-14 09:32:13 +0100

    Fix conflicts for app/assets/javascripts/boards/components/issue_card_inner.js

--- app/assets/javascripts/boards/components/issue_card_inner.js
+++ app/assets/javascripts/boards/components/issue_card_inner.js
@@ -12,10 +12,7 @@ window.gl.issueBoards = window.gl.issueBoards || {};
 gl.issueBoards.IssueCardInner = Vue.extend({
   components: {
     UserAvatarLink,
-<<<<<<< HEAD
     IssueCardWeight,
-=======
->>>>>>> upstream/master
   },
   props: {
     issue: {
