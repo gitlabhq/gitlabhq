5ea82efb77fcbf70c7f11afa439cd7906a6f5232
Annabel Gray <annabel.m.gray@gmail.com>
2018-06-14 17:35:15 +0000

    Merge branch '47850-placeholder-note-renders-the-user-name-in-a-new-line' into 'master'
    Resolve "Placeholder note renders the user name in a new line"
    
    Closes #47850
    
    See merge request gitlab-org/gitlab-ce!19791

--- app/assets/javascripts/notes.js
+++ app/assets/javascripts/notes.js
@@ -1675,7 +1675,7 @@ export default class Notes {
                <div class="note-header">
                   <div class="note-header-info">
                      <a href="/${_.escape(currentUsername)}">
-                       <span class="d-none d-sm-block">${_.escape(
+                       <span class="d-none d-sm-inline-block">${_.escape(
                          currentUsername,
                        )}</span>
                        <span class="note-headline-light">${_.escape(
@@ -1694,7 +1694,7 @@ export default class Notes {
       </li>`,
     );
 
-    $tempNote.find('.d-none.d-sm-block').text(_.escape(currentUserFullname));
+    $tempNote.find('.d-none.d-sm-inline-block').text(_.escape(currentUserFullname));
     $tempNote
       .find('.note-headline-light')
       .text(`@${_.escape(currentUsername)}`);
