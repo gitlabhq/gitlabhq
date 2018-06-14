14f83fdb48e597c6d4f6519a2bd4b1dc59d525f0
Kushal Pandya <kushal@gitlab.com>
2018-06-14 16:51:27 +0530

    Update read-only message

--- ee/app/helpers/ee/application_helper.rb
+++ ee/app/helpers/ee/application_helper.rb
@@ -6,7 +6,7 @@ module EE
     def read_only_message
       return super unless ::Gitlab::Geo.secondary_with_primary?
 
-      (_('You are on a secondary (read-only) Geo node. If you want to make any changes, you must visit the %{primary_node}.') %
+      (_('You are on a secondary, <b>read-only</b> Geo node. If you want to make changes, you must visit this page on the %{primary_node}.') %
         { primary_node: link_to('primary node', ::Gitlab::Geo.primary_node.url) }).html_safe
     end
 
