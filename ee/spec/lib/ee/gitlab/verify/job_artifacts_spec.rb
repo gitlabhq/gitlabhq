78ebc68ce020fb7cd65bd303dd7f2ed76b607e43
Rémy Coutable <remy@rymai.me>
2018-06-14 12:37:00 +0200

    Fix 3 tests that were failing due to a CE change
    Following gitlab-org/gitlab-ce!19501, we now check remote objects too.
    
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- ee/spec/lib/ee/gitlab/verify/job_artifacts_spec.rb
+++ ee/spec/lib/ee/gitlab/verify/job_artifacts_spec.rb
@@ -5,13 +5,13 @@ describe Gitlab::Verify::JobArtifacts do
     stub_artifacts_object_storage
   end
 
-  it 'skips LFS objects in object storage' do
+  it 'includes CI job artifacts in object storage' do
     local_failure = create(:ci_job_artifact)
-    create(:ci_job_artifact, :remote_store)
+    remote_failure = create(:ci_job_artifact, :remote_store)
 
     failures = {}
     described_class.new(batch_size: 10).run_batches { |_, failed| failures.merge!(failed) }
 
-    expect(failures.keys).to contain_exactly(local_failure)
+    expect(failures.keys).to contain_exactly(local_failure, remote_failure)
   end
 end
--- ee/spec/lib/ee/gitlab/verify/lfs_objects_spec.rb
+++ ee/spec/lib/ee/gitlab/verify/lfs_objects_spec.rb
@@ -5,13 +5,13 @@ describe Gitlab::Verify::LfsObjects do
     stub_lfs_object_storage
   end
 
-  it 'skips LFS objects in object storage' do
+  it 'includes LFS objects in object storage' do
     local_failure = create(:lfs_object)
-    create(:lfs_object, :object_storage)
+    remote_failure = create(:lfs_object, :object_storage)
 
     failures = {}
     described_class.new(batch_size: 10).run_batches { |_, failed| failures.merge!(failed) }
 
-    expect(failures.keys).to contain_exactly(local_failure)
+    expect(failures.keys).to contain_exactly(local_failure, remote_failure)
   end
 end
