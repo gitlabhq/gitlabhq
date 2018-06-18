4bf06686a78da89003591fae4204330751f37969
Winnie Hellmann <winnie@gitlab.com>
2018-06-18 11:07:52 +0200

    Resolve conflict in app/assets/javascripts/notes/components/noteable_note.vue

--- app/assets/javascripts/notes/components/noteable_note.vue
+++ app/assets/javascripts/notes/components/noteable_note.vue
@@ -133,12 +133,7 @@ export default {
     formCancelHandler(shouldConfirm, isDirty) {
       if (shouldConfirm && isDirty) {
         // eslint-disable-next-line no-alert
-<<<<<<< HEAD
-        if (!confirm('Are you sure you want to cancel editing this comment?')) return;
-=======
-        if (!window.confirm('Are you sure you want to cancel editing this comment?'))
-          return;
->>>>>>> master
+        if (!window.confirm('Are you sure you want to cancel editing this comment?')) return;
       }
       this.$refs.noteBody.resetAutoSave();
       if (this.oldContent) {
