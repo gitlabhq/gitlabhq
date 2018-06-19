3e5899ad9839d3ef8a920da8b6948ba4d0fa2c06
Dennis Tang <dennis@dennistang.net>
2018-06-19 15:10:40 +0000

    Update index.js

--- app/assets/javascripts/pages/projects/index.js
+++ app/assets/javascripts/pages/projects/index.js
@@ -5,7 +5,11 @@ import ShortcutsNavigation from '../../shortcuts_navigation';
 
 document.addEventListener('DOMContentLoaded', () => {
   const page = document.body.dataset.page;
-  const newClusterViews = ['projects:clusters:new', 'projects:clusters:create'];
+  const newClusterViews = [
+    'projects:clusters:new',
+    'projects:clusters:create_gcp',
+    'projects:clusters:create_user',
+  ];
 
   if (newClusterViews.indexOf(page) > -1) {
     gcpSignupOffer();
