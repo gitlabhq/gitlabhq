f93fd02d695ca11ec7d39f5d8fdb8d27bdb3a9b9
Douwe Maan <douwe@gitlab.com>
2018-06-20 14:00:13 +0000

    Merge branch 'fix/favicon-cross-origin' into 'master'
    Fix: Serve favicon image always from the main GitLab domain to avoid issues with CORS
    
    Closes #47802
    
    See merge request gitlab-org/gitlab-ce!19810

--- spec/lib/gitlab/favicon_spec.rb
+++ spec/lib/gitlab/favicon_spec.rb
@@ -21,6 +21,21 @@ RSpec.describe Gitlab::Favicon, :request_store do
       create :appearance, favicon: fixture_file_upload(Rails.root.join('spec/fixtures/dk.png'))
       expect(described_class.main).to match %r{/uploads/-/system/appearance/favicon/\d+/dk.png}
     end
+
+    context 'asset host' do
+      before do
+        allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new('production'))
+      end
+
+      it 'returns a relative url when the asset host is not configured' do
+        expect(described_class.main).to match %r{^/assets/favicon-(?:\h+).png$}
+      end
+
+      it 'returns a full url when the asset host is configured' do
+        allow(Gitlab::Application.config).to receive(:asset_host).and_return('http://assets.local')
+        expect(described_class.main).to match %r{^http://localhost/assets/favicon-(?:\h+).png$}
+      end
+    end
   end
 
   describe '.status_overlay' do
