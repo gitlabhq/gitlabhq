133f4613aef885e6f4b313c31ba798617d470d83
Lin Jen-Shin <godfat@godfat.org>
2018-06-25 21:16:33 +0800

    Override index to setup `@license`

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -11,6 +11,5 @@ class Admin::DashboardController < Admin::ApplicationController
     @projects = Project.order_id_desc.without_deleted.with_route.limit(10)
     @users = User.order_id_desc.limit(10)
     @groups = Group.order_id_desc.with_route.limit(10)
-    @license = License.current
   end
 end
