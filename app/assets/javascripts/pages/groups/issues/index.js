d4387d88767d76666e32feaab9deb48d43b6249a
Alexis Reigel <alexis.reigel.ext@siemens.com>
2018-06-26 16:21:38 +0200

    make FilteredSearchTokenKeys generic
    for the current usage for the issues search bar a specific instance of
    FilteredSearchTokenKeys is created, which contains the config for that
    case.

--- app/assets/javascripts/pages/groups/issues/index.js
+++ app/assets/javascripts/pages/groups/issues/index.js
@@ -1,11 +1,13 @@
 import projectSelect from '~/project_select';
 import initFilteredSearch from '~/pages/search/init_filtered_search';
 import { FILTERED_SEARCH } from '~/pages/constants';
+import IssuesFilteredSearchTokenKeys from '~/filtered_search/issues_filtered_search_token_keys';
 
 document.addEventListener('DOMContentLoaded', () => {
   initFilteredSearch({
     page: FILTERED_SEARCH.ISSUES,
     isGroupDecendent: true,
+    filteredSearchTokenKeys: IssuesFilteredSearchTokenKeys,
   });
   projectSelect();
 });
--- app/assets/javascripts/pages/projects/issues/index/index.js
+++ app/assets/javascripts/pages/projects/issues/index/index.js
@@ -4,12 +4,14 @@ import IssuableIndex from '~/issuable_index';
 import ShortcutsNavigation from '~/shortcuts_navigation';
 import UsersSelect from '~/users_select';
 import initFilteredSearch from '~/pages/search/init_filtered_search';
+import IssuesFilteredSearchTokenKeys from '~/filtered_search/issues_filtered_search_token_keys';
 import { FILTERED_SEARCH } from '~/pages/constants';
 import { ISSUABLE_INDEX } from '~/pages/projects/constants';
 
 document.addEventListener('DOMContentLoaded', () => {
   initFilteredSearch({
     page: FILTERED_SEARCH.ISSUES,
+    filteredSearchTokenKeys: IssuesFilteredSearchTokenKeys,
   });
   new IssuableIndex(ISSUABLE_INDEX.ISSUE);
 
