64e6e5397f43008a71b085ac61157840b78d40d1
Shinya Maeda <shinya@gitlab.com>
2018-06-27 20:05:19 +0900

    Add cleanup scripts for fill file store background migratiosn

---
+++ db/post_migrate/20180627191935_cleanup_fill_file_store_background_migrations.rb
@@ -0,0 +1,53 @@
+class CleanupFillFileStoreBackgroundMigrations < ActiveRecord::Migration
+  include Gitlab::Database::MigrationHelpers
+
+  DOWNTIME = false
+
+  disable_ddl_transaction!
+
+  class JobArtifact < ActiveRecord::Base
+    include EachBatch
+    self.table_name = 'ci_job_artifacts'
+  end
+
+  class LfsObject < ActiveRecord::Base
+    include EachBatch
+    self.table_name = 'lfs_objects'
+  end
+
+  class Upload < ActiveRecord::Base
+    include EachBatch
+    self.table_name = 'uploads'
+    self.inheritance_column = :_type_disabled # Disable STI
+  end
+
+  def up
+    Gitlab::BackgroundMigration.steal('FillFileStoreJobArtifact')
+    Gitlab::BackgroundMigration.steal('FillFileStoreLfsObject')
+    Gitlab::BackgroundMigration.steal('FillStoreUpload')
+
+    CleanupFillFileStoreBackgroundMigrations::JobArtifact.where(file_store: nil).each_batch(batch_size: 10_000) do |relation|
+      start_id, end_id = relation.pluck('MIN(id), MAX(id)').first
+
+      Gitlab::BackgroundMigration::FillFileStoreJobArtifact.new.perform(start_id, end_id)
+    end
+
+    CleanupFillFileStoreBackgroundMigrations::LfsObject.where(file_store: nil).each_batch(batch_size: 10_000) do |relation|
+      start_id, end_id = relation.pluck('MIN(id), MAX(id)').first
+
+      Gitlab::BackgroundMigration::FillFileStoreLfsObject.new.perform(start_id, end_id)
+    end
+
+    CleanupFillFileStoreBackgroundMigrations::Upload.where(store: nil).each_batch(batch_size: 10_000) do |relation|
+      start_id, end_id = relation.pluck('MIN(id), MAX(id)').first
+
+      Gitlab::BackgroundMigration::FillStoreUpload.new.perform(start_id, end_id)
+    end
+
+    ## TODO: Do we add NOT NULL constraint here?
+  end
+
+  def down
+    # no-op
+  end
+end
