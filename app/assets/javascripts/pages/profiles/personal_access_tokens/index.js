d4d1e5c4f66088bc6e8d617700484e4652e2c15c
Paul Slaughter <pslaughter@gitlab.com>
2018-06-28 10:13:18 -0500

    Add project-multi-select to personal access token form

--- app/assets/javascripts/pages/profiles/personal_access_tokens/index.js
+++ app/assets/javascripts/pages/profiles/personal_access_tokens/index.js
@@ -1,3 +1,7 @@
 import DueDateSelectors from '~/due_date_select';
+import multiProjectSelect from '~/project_select_multi';
 
-document.addEventListener('DOMContentLoaded', () => new DueDateSelectors());
+document.addEventListener('DOMContentLoaded', () => {
+  new DueDateSelectors(); // eslint-disable-line no-new
+  multiProjectSelect();
+});
