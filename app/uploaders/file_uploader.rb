cebdd267e672c75696cd534bb89d10fda8de129f
Micaël Bergeron <mbergeron@gitlab.com>
2018-06-28 10:57:28 -0400

    add support for file copy on object storage

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -81,6 +81,13 @@ class FileUploader < GitlabUploader
     apply_context!(uploader_context)
   end
 
+  def initialize_copy(from)
+    super
+
+    @secret = self.class.generate_secret
+    @upload = nil # calling record_upload would delete the old upload if set
+  end
+
   # enforce the usage of Hashed storage when storing to
   # remote store as the FileMover doesn't support OS
   def base_dir(store = nil)
@@ -142,6 +149,25 @@ class FileUploader < GitlabUploader
 
   def secret
     @secret ||= self.class.generate_secret
+  end
+
+  # return a new uploader with a file copy on another project
+  def self.copy_to(uploader, to_project)
+    moved = uploader.dup.tap do |u|
+      u.model = to_project
+    end
+
+    moved.copy_file(uploader.file)
+    moved
+  end
+
+  def copy_file(file)
+    if file_storage?
+      store!(file)
+    else
+      self.file = file.copy_to(store_path)
+      record_upload # after_store is not triggered
+    end
   end
 
   private
