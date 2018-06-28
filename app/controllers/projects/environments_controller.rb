b80f819b36da2547a54d9ec7fa6e83e20a41642c
Tiago Botelho <tiagonbotelho@hotmail.com>
2018-06-28 17:14:47 +0200

    Moves the request of the environments list to the index action

--- app/controllers/projects/environments_controller.rb
+++ app/controllers/projects/environments_controller.rb
@@ -6,11 +6,13 @@ class Projects::EnvironmentsController < Projects::ApplicationController
   before_action :authorize_update_environment!, only: [:edit, :update]
   before_action :authorize_admin_environment!, only: [:terminal, :terminal_websocket_authorize]
   before_action :environment, only: [:show, :edit, :update, :stop, :terminal, :terminal_websocket_authorize, :metrics]
-  before_action :environments, only: [:index]
   before_action :verify_api_request!, only: :terminal_websocket_authorize
   before_action :expire_etag_cache, only: [:index]
 
   def index
+    @environments = project.environments
+      .with_state(params[:scope] || :available)
+
     respond_to do |format|
       format.html
       format.json do
@@ -126,12 +128,7 @@ class Projects::EnvironmentsController < Projects::ApplicationController
     respond_to do |format|
       format.html
       format.json do
-        render json: {
-          metrics: @metrics,
-          environments: EnvironmentSerializer
-            .new(project: project, current_user: current_user)
-            .represent(environments)
-        }, status: @metrics.any? ? :ok : :no_content
+        render json: @metrics, status: @metrics.any? ? :ok : :no_content
       end
     end
   end
@@ -141,12 +138,7 @@ class Projects::EnvironmentsController < Projects::ApplicationController
       format.json do
         additional_metrics = environment.additional_metrics || {}
 
-        render json: {
-          additional_metrics: additional_metrics,
-          environments: EnvironmentSerializer
-            .new(project: project, current_user: current_user)
-            .represent(environments)
-        }, status: additional_metrics.empty? ? :ok : :no_content
+        render json: additional_metrics, status: additional_metrics.any? ? :ok : :no_content
       end
     end
   end
@@ -172,9 +164,5 @@ class Projects::EnvironmentsController < Projects::ApplicationController
 
   def environment
     @environment ||= project.environments.find(params[:id])
-  end
-
-  def environments
-    @environments ||= project.environments.with_state(params[:scope] || :available)
   end
 end
