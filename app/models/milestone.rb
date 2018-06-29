9bc07524516a1091386c3ae118934018efbbd645
blackst0ne <blackst0ne.ru@gmail.com>
2018-06-29 15:40:13 +1100

    [Rails5] Apply patch

--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -131,7 +131,7 @@ class Milestone < ActiveRecord::Base
       rel.order(:project_id, :due_date).select('DISTINCT ON (project_id) id')
     else
       rel
-        .group(:project_id)
+        .group(:project_id, :id)
         .having('due_date = MIN(due_date)')
         .pluck(:id, :project_id, :due_date)
         .map(&:first)
