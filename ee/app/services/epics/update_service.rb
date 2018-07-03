9436579bfc56abef816af5991c0ecb2f1b83b72e
Jarka Kadlecová <jarka@gitlab.com>
2018-07-03 20:44:22 +0200

    Mark todos as done when epic was relabeled

--- ee/app/services/epics/update_service.rb
+++ ee/app/services/epics/update_service.rb
@@ -9,6 +9,11 @@ module Epics
     def handle_changes(epic, options)
       old_associations = options.fetch(:old_associations, {})
       old_mentioned_users = old_associations.fetch(:mentioned_users, [])
+      old_labels = old_associations.fetch(:labels, [])
+
+      if has_changes?(epic, old_labels: old_labels)
+        todo_service.mark_pending_todos_as_done(epic, current_user)
+      end
 
       todo_service.update_epic(epic, current_user, old_mentioned_users)
     end
--- ee/spec/services/epics/update_service_spec.rb
+++ ee/spec/services/epics/update_service_spec.rb
@@ -61,18 +61,58 @@ describe Epics::UpdateService do
     end
 
     context 'todos' do
-      let(:mentioned1) { create(:user) }
-      let(:mentioned2) { create(:user) }
-
       before do
         group.update(visibility: Gitlab::VisibilityLevel::PUBLIC)
-        epic.update(description: "FYI: #{mentioned1.to_reference}")
       end
 
-      it 'creates todos for only newly mentioned users' do
-        expect do
-          update_epic(description: "FYI: #{mentioned1.to_reference} #{mentioned2.to_reference}")
-        end.to change { Todo.count }.by(1)
+      context 'creating todos' do
+        let(:mentioned1) { create(:user) }
+        let(:mentioned2) { create(:user) }
+
+        before do
+          epic.update(description: "FYI: #{mentioned1.to_reference}")
+        end
+
+        it 'creates todos for only newly mentioned users' do
+          expect do
+            update_epic(description: "FYI: #{mentioned1.to_reference} #{mentioned2.to_reference}")
+          end.to change { Todo.count }.by(1)
+        end
+      end
+
+      context 'adding a label' do
+        let(:label) {  create(:group_label, group: group) }
+        let(:user2) { create(:user) }
+        let!(:todo1) do
+          create(:todo, :mentioned, :pending,
+            target: epic,
+            group: group,
+            project: nil,
+            author: user,
+            user: user)
+        end
+        let!(:todo2) do
+          create(:todo, :mentioned, :pending,
+            target: epic,
+            group: group,
+            project: nil,
+            author: user2,
+            user: user2)
+        end
+
+        before do
+          group.add_developer(user)
+
+          update_epic(label_ids: [label.id])
+        end
+
+        it 'marks todo as done for a user who added a label' do
+          expect(todo1.reload.state).to eq('done')
+        end
+
+        it 'does not mark todos as done for other users' do
+          expect(todo2.reload.state).to eq('pending')
+        end
       end
     end
   end
