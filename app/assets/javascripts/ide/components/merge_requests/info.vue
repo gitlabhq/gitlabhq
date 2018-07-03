b3b4272cb756519f8921a1c64865db623d430c83
Phil Hughes <me@iamphill.com>
2018-07-03 14:18:27 +0100

    tidy up component added api spec

--- app/assets/javascripts/ide/components/merge_requests/info.vue
+++ app/assets/javascripts/ide/components/merge_requests/info.vue
@@ -1,47 +1,24 @@
 <script>
 import { mapGetters } from 'vuex';
 import Icon from '../../../vue_shared/components/icon.vue';
-import timeago from '../../../vue_shared/mixins/timeago';
-import tooltip from '../../../vue_shared/directives/tooltip';
 import TitleComponent from '../../../issue_show/components/title.vue';
 import DescriptionComponent from '../../../issue_show/components/description.vue';
 
-const states = {
-  open: 'open',
-  closed: 'closed',
-};
-
 export default {
-  directives: {
-    tooltip,
-  },
   components: {
     Icon,
     TitleComponent,
     DescriptionComponent,
   },
-  mixins: [timeago],
   computed: {
     ...mapGetters(['currentMergeRequest']),
-    isOpen() {
-      return this.currentMergeRequest.state === states.open;
-    },
-    isClosed() {
-      return this.currentMergeRequest.state === states.closed;
-    },
-    authorUsername() {
-      return `@${this.currentMergeRequest.author.username}`;
-    },
-    iconName() {
-      return this.isOpen ? 'issue-open-m' : 'close';
-    },
   },
 };
 </script>
 
 <template>
   <div class="ide-merge-request-info">
-    <div class="detail-page-header">
+    <div class="detail-page-header sticky-top bg-white">
       <icon
         name="git-merge"
         class="align-self-center append-right-8"
@@ -64,13 +41,3 @@ export default {
     </div>
   </div>
 </template>
-
-<style scoped>
-.ide-merge-request-info {
-  overflow: auto;
-}
-
-.detail-page-header {
-  line-height: initial;
-}
-</style>
