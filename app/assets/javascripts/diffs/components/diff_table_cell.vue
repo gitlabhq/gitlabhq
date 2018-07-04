2e6c64f2b2eea7a9933467a6018a819081a2f12a
Fatih Acet <acetfatih@gmail.com>
2018-07-04 17:19:48 +0200

    Fix conflicts on diff_table_cell.vue.

--- app/assets/javascripts/diffs/components/diff_table_cell.vue
+++ app/assets/javascripts/diffs/components/diff_table_cell.vue
@@ -11,11 +11,8 @@ import {
   LINE_HOVER_CLASS_NAME,
   LINE_UNFOLD_CLASS_NAME,
   INLINE_DIFF_VIEW_TYPE,
-<<<<<<< HEAD
   LINE_POSITION_LEFT,
   LINE_POSITION_RIGHT,
-=======
->>>>>>> eba05eb8d48... Merge branch '_acet-mr-diff-performance' into 'master'
 } from '../constants';
 
 export default {
@@ -70,7 +67,6 @@ export default {
   computed: {
     ...mapGetters(['isLoggedIn']),
     normalizedLine() {
-<<<<<<< HEAD
       let normalizedLine;
 
       if (this.diffViewType === INLINE_DIFF_VIEW_TYPE) {
@@ -79,10 +75,6 @@ export default {
         normalizedLine = this.line.left;
       } else if (this.linePosition === LINE_POSITION_RIGHT) {
         normalizedLine = this.line.right;
-=======
-      if (this.diffViewType === INLINE_DIFF_VIEW_TYPE) {
-        return this.line;
->>>>>>> eba05eb8d48... Merge branch '_acet-mr-diff-performance' into 'master'
       }
 
       return normalizedLine;
