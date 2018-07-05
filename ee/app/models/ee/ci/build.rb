8d5382e83030a6c67ef653af6b57a4ec23c8b2b4
Rémy Coutable <remy@rymai.me>
2018-07-05 08:52:58 +0200

    Replace .shared? with .instance_type? in ee/app/models/ee/ci/build.rb
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- ee/app/models/ee/ci/build.rb
+++ ee/app/models/ee/ci/build.rb
@@ -38,7 +38,7 @@ module EE
       end
 
       def shared_runners_minutes_limit_enabled?
-        runner && runner.shared? && project.shared_runners_minutes_limit_enabled?
+        runner && runner.instance_type? && project.shared_runners_minutes_limit_enabled?
       end
 
       def stick_build_if_status_changed
