0f24c92248b962a1834513ca64e658bf1cf40cf7
Sean McGivern <sean@gitlab.com>
2018-07-05 12:55:00 +0100

    Extract EE-specific lines from TodoService

--- ee/app/services/ee/todo_service.rb
+++ ee/app/services/ee/todo_service.rb
@@ -2,6 +2,23 @@ module EE
   module TodoService
     extend ::Gitlab::Utils::Override
 
+    # When new approvers are added for a merge request:
+    #
+    #  * create a todo for those users to approve the MR
+    #
+    def add_merge_request_approvers(merge_request, approvers)
+      create_approval_required_todos(merge_request, approvers, merge_request.author)
+    end
+
+    override :new_issuable
+    def new_issuable(issuable, author)
+      if issuable.is_a?(MergeRequest)
+        create_approval_required_todos(issuable, issuable.overall_approvers, author)
+      end
+
+      super
+    end
+
     def new_epic(epic, current_user)
       create_mention_todos(nil, epic, current_user)
     end
@@ -9,6 +26,8 @@ module EE
     def update_epic(epic, current_user, skip_users = [])
       create_mention_todos(nil, epic, current_user, nil, skip_users)
     end
+
+    private
 
     override :attributes_for_target
     def attributes_for_target(target)
@@ -19,6 +38,11 @@ module EE
       end
 
       attributes
+    end
+
+    def create_approval_required_todos(merge_request, approvers, author)
+      attributes = attributes_for_todo(merge_request.project, merge_request, author, ::Todo::APPROVAL_REQUIRED)
+      create_todos(approvers.map(&:user), attributes)
     end
   end
 end
