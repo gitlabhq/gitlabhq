c084e87ad7be45f39a79347b306f03f618705049
Gabriel Mazetto <brodock@gmail.com>
2018-07-05 17:46:10 +0200

    Added SiteStatistics as counter cache for Projects and Wikis

---
+++ app/models/site_statistic.rb
@@ -0,0 +1,46 @@
+class SiteStatistic < ActiveRecord::Base
+  # prevents the creation of multiple rows
+  default_value_for :id, 1
+
+  COUNTER_ATTRIBUTES = %w(repositories_count wikis_count).freeze
+
+  # https://dev.mysql.com/doc/refman/5.5/en/error-messages-server.html
+  MYSQL_NO_SUCH_TABLE = 1146
+
+  def self.track(raw_attribute)
+    with_statistics_available(raw_attribute) do |attribute|
+      SiteStatistic.update_all(["#{attribute} = #{attribute}+1"])
+    end
+  end
+
+  def self.untrack(raw_attribute)
+    with_statistics_available(raw_attribute) do |attribute|
+      SiteStatistic.update_all(["#{attribute} = #{attribute}-1 WHERE #{attribute} > 0"])
+    end
+  end
+
+  def self.with_statistics_available(raw_attribute)
+    unless raw_attribute.in?(COUNTER_ATTRIBUTES)
+      raise ArgumentError, "Invalid attribute: '#{raw_attribute}' to '#{caller_locations(1, 1)[0].label}' method. " \
+                           "Valid attributes are: #{COUNTER_ATTRIBUTES.join(', ')}"
+    end
+
+    # we have quite a lot of specs testing migrations, we need this and the rescue to not break them
+    SiteStatistic.transaction(requires_new: true) do
+      SiteStatistic.first_or_create if Rails.env.test? # make sure SiteStatistic exists during tests
+      attribute = self.connection.quote_column_name(raw_attribute)
+
+      yield(attribute)
+    end
+  rescue ActiveRecord::StatementInvalid => ex
+    # we want to ignore this so we don't break the migration specs
+    unless ex.original_exception.is_a?(PG::UndefinedTable) ||
+        (ex.original_exception.is_a?(Mysql2::Error) && ex.original_exception.error_number == MYSQL_NO_SUCH_TABLE)
+      raise ex
+    end
+  end
+
+  def self.fetch
+    SiteStatistic.first_or_create!
+  end
+end
