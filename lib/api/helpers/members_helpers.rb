29d84b612c1d18b56b946a909f931773574456f7
Jacopo <beschi.jacopo@gmail.com>
2018-07-06 08:56:36 +0200

    Fixes linter errors

--- lib/api/helpers/members_helpers.rb
+++ lib/api/helpers/members_helpers.rb
@@ -12,25 +12,25 @@ module API
       end
 
       def find_all_members(source_type, source)
-        members = (source_type == 'project') ? find_all_members_for_project(source) : find_all_members_for_group(source)
-        members.non_invite.
-          non_request
+        members = source_type == 'project' ? find_all_members_for_project(source) : find_all_members_for_group(source)
+        members.non_invite
+          .non_request
       end
 
       def find_all_members_for_project(project)
         shared_group_ids = project.project_group_links.pluck(:group_id)
         source_ids = [project.id, project.group&.id].concat(shared_group_ids).compact
-        Member.includes(:user).
-          joins(user: :project_authorizations).
-          where(project_authorizations: { project_id: project.id }).
-          where(source_id: source_ids)
+        Member.includes(:user)
+          .joins(user: :project_authorizations)
+          .where(project_authorizations: { project_id: project.id })
+          .where(source_id: source_ids)
       end
 
       def find_all_members_for_group(group)
         source_ids = group.self_and_ancestors.pluck(:id)
-        Member.includes(:user).
-          where(source_id: source_ids).
-          where(source_type: 'Namespace')
+        Member.includes(:user)
+          .where(source_id: source_ids)
+          .where(source_type: 'Namespace')
       end
     end
   end
