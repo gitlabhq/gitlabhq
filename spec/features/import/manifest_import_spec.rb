eeff4d2c0874f3d9971b2b84214c023a57b60a94
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2018-07-09 10:55:10 +0300

    Rename button at manifest import form
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- spec/features/import/manifest_import_spec.rb
+++ spec/features/import/manifest_import_spec.rb
@@ -16,7 +16,7 @@ feature 'Import multiple repositories by uploading a manifest file', :js, :postg
     visit new_import_manifest_path
 
     attach_file('manifest', Rails.root.join('spec/fixtures/aosp_manifest.xml'))
-    click_on 'Continue to the next step'
+    click_on 'List available repositories'
 
     expect(page).to have_button('Import all repositories')
     expect(page).to have_content('https://android-review.googlesource.com/platform/build/blueprint')
@@ -26,7 +26,7 @@ feature 'Import multiple repositories by uploading a manifest file', :js, :postg
     visit new_import_manifest_path
 
     attach_file('manifest', Rails.root.join('spec/fixtures/aosp_manifest.xml'))
-    click_on 'Continue to the next step'
+    click_on 'List available repositories'
 
     page.within(first_row) do
       click_on 'Import'
