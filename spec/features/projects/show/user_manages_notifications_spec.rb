ef129737235832908e81f58da9133bbd8e9e7963
Felipe Artur <felipefac@gmail.com>
2018-07-10 11:29:27 -0300

    Allow to toggle notifications for issues due soon

--- spec/features/projects/show/user_manages_notifications_spec.rb
+++ spec/features/projects/show/user_manages_notifications_spec.rb
@@ -16,4 +16,36 @@ describe 'Projects > Show > User manages notifications', :js do
       expect(page).to have_content 'On mention'
     end
   end
+
+  context 'custom notification settings' do
+    let(:email_events) do
+      [
+        :new_note,
+        :new_issue,
+        :reopen_issue,
+        :close_issue,
+        :reassign_issue,
+        :issue_due,
+        :new_merge_request,
+        :push_to_merge_request,
+        :reopen_merge_request,
+        :close_merge_request,
+        :reassign_merge_request,
+        :merge_merge_request,
+        :failed_pipeline,
+        :success_pipeline
+      ]
+    end
+
+    it 'shows notification settings checkbox' do
+      first('.notifications-btn').click
+      page.find('a[data-notification-level="custom"]').click
+
+      page.within('.custom-notifications-form') do
+        email_events.each do |event_name|
+          expect(page).to have_selector("input[name='notification_setting[#{event_name}]']")
+        end
+      end
+    end
+  end
 end
