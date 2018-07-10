f4a7ca8bd1908b68ba960f538e4a2d999a1dc3d9
Jacob Vosmaer (GitLab) <jacob@gitlab.com>
2018-07-10 09:08:17 +0000

    Revert "Merge branch 'gitaly-with-config' into 'master'"
    This reverts merge request !6428

--- ee/spec/services/geo/repository_sync_service_spec.rb
+++ ee/spec/services/geo/repository_sync_service_spec.rb
@@ -286,11 +286,7 @@ describe Geo::RepositorySyncService do
 
         subject.execute
 
-        repo_path = Gitlab::GitalyClient::StorageSettings.allow_disk_access do
-          project.repository.path
-        end
-
-        expect(File.directory?(repo_path)).to be true
+        expect(File.directory?(project.repository.path)).to be true
       end
 
       it 'tries to redownload repo when force_redownload flag is set' do
