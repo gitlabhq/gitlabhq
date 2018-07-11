e868d09dbf9672100759bed9908de8f52b2603dc
Fatih Acet <acetfatih@gmail.com>
2018-07-12 00:34:00 +0200

    Use built-in autosave mixin in diff line note form.

--- app/assets/javascripts/diffs/components/diff_line_note_form.vue
+++ app/assets/javascripts/diffs/components/diff_line_note_form.vue
@@ -5,13 +5,14 @@ import createFlash from '~/flash';
 import { s__ } from '~/locale';
 import noteForm from '../../notes/components/note_form.vue';
 import { getNoteFormData } from '../store/utils';
-import Autosave from '../../autosave';
+import autosave from '../../notes/mixins/autosave';
 import { DIFF_NOTE_TYPE, NOTE_TYPE } from '../constants';
 
 export default {
   components: {
     noteForm,
   },
+  mixins: [autosave],
   props: {
     diffFileHash: {
       type: String,
@@ -41,28 +42,25 @@ export default {
   },
   mounted() {
     if (this.isLoggedIn) {
-      const noteableData = this.getNoteableData;
+      const noteableData = this.noteableData;
       const keys = [
-        NOTE_TYPE,
-        this.noteableType,
-        noteableData.id,
         noteableData.diff_head_sha,
         DIFF_NOTE_TYPE,
         noteableData.source_project_id,
         this.line.lineCode,
       ];
 
-      this.autosave = new Autosave($(this.$refs.noteForm.$refs.textarea), keys);
+      this.initAutoSave(noteableData, keys);
     }
   },
   methods: {
     ...mapActions('diffs', ['cancelCommentForm']),
     ...mapActions(['saveNote', 'refetchDiscussionById']),
     handleCancelCommentForm() {
-      this.autosave.reset();
       this.cancelCommentForm({
         lineCode: this.line.lineCode,
       });
+      this.resetAutoSave();
     },
     handleSaveNote(note) {
       const selectedDiffFile = this.getDiffFileByHash(this.diffFileHash);
