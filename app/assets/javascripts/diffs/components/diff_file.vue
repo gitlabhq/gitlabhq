ef168c42ee93caf1b17f81991f2fdad14b7ee836
Phil Hughes <me@iamphill.com>
2018-07-12 12:42:56 +0000

    Merge branch '48789-remove-event-listeners-scroll' into 'master'
    Resolve "Improve performance of MR Changes tab: reduce event listeners on scroll event"
    
    Closes #48639 and #48789
    
    See merge request gitlab-org/gitlab-ce!20558

--- app/assets/javascripts/diffs/components/diff_file.vue
+++ app/assets/javascripts/diffs/components/diff_file.vue
@@ -25,7 +25,6 @@ export default {
   },
   data() {
     return {
-      isActive: false,
       isLoadingCollapsedDiff: false,
       forkMessageVisible: false,
     };
@@ -48,12 +47,6 @@ export default {
       return this.isCollapsed && !this.isLoadingCollapsedDiff && !this.file.tooLarge;
     },
   },
-  mounted() {
-    document.addEventListener('scroll', this.handleScroll);
-  },
-  beforeDestroy() {
-    document.removeEventListener('scroll', this.handleScroll);
-  },
   methods: {
     ...mapActions('diffs', ['loadCollapsedDiff']),
     handleToggle() {
@@ -64,36 +57,6 @@ export default {
       } else {
         this.file.collapsed = !this.file.collapsed;
       }
-    },
-    handleScroll() {
-      if (!this.updating) {
-        requestAnimationFrame(this.scrollUpdate.bind(this));
-        this.updating = true;
-      }
-    },
-    scrollUpdate() {
-      const header = document.querySelector('.js-diff-files-changed');
-      if (!header) {
-        this.updating = false;
-        return;
-      }
-
-      const { top, bottom } = this.$el.getBoundingClientRect();
-      const { top: topOfFixedHeader, bottom: bottomOfFixedHeader } = header.getBoundingClientRect();
-
-      const headerOverlapsContent = top < topOfFixedHeader && bottom > bottomOfFixedHeader;
-      const fullyAboveHeader = bottom < bottomOfFixedHeader;
-      const fullyBelowHeader = top > topOfFixedHeader;
-
-      if (headerOverlapsContent && !this.isActive) {
-        this.$emit('setActive');
-        this.isActive = true;
-      } else if (this.isActive && (fullyAboveHeader || fullyBelowHeader)) {
-        this.$emit('unsetActive');
-        this.isActive = false;
-      }
-
-      this.updating = false;
     },
     handleLoadCollapsedDiff() {
       this.isLoadingCollapsedDiff = true;
