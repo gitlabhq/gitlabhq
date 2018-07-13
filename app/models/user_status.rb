812bfb158b70b09cfd438379a4b9446aa85b52ec
Bob Van Landuyt <bob@vanlanduyt.co>
2018-07-13 12:22:02 +0200

    Add the `UserStatus` model
    This model will hold the status of a user, including these fields:
    
    - emoji: always present, with a default value
    - user: always present, foreign key to user
    - message: optional, maximum length of 100
    
    The table also stores
    
    - cached_markdown_version
    - message_html
    
    For rendering markdown in the `message` field.

---
+++ app/models/user_status.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class UserStatus < ActiveRecord::Base
+  include CacheMarkdownField
+
+  belongs_to :user
+
+  validates :user, presence: true
+  validates :emoji, inclusion: { in: Gitlab::Emoji.emojis_names }
+  validates :message, length: { maximum: 100 }, allow_blank: true
+
+  cache_markdown_field :message, pipeline: :single_line
+end
