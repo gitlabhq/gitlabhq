cd1b20ae93738b61f0aa074460e2b62b03c1e938
Filipa Lacerda <filipa@gitlab.com>
2018-07-13 15:52:08 +0100

    Backports security reports reusable components into CE code base

---
+++ spec/javascripts/vue_shared/components/reports/report_issues_spec.js
@@ -0,0 +1,17 @@
+// import Vue from 'vue';
+// import reportIssues from '~/vue_shared/reports/components/report_issues.vue';
+
+// describe('Report issues', () => {
+//   let vm;
+//   let ReportIssues;
+
+//   beforeEach(() => {
+//     ReportIssues = Vue.extend(reportIssues);
+//   });
+
+//   afterEach(() => {
+//     vm.$destroy();
+//   });
+
+//   // TODO
+// });
