8cc646fa0ec05820c83a163df74605f04c8877ea
Reuben Pereira <reuben453@gmail.com>
2018-07-15 17:25:04 +0530

    Remove nodes_visible_to_user from ProjectParser and memoize readable_project_ids

--- lib/banzai/reference_parser/project_parser.rb
+++ lib/banzai/reference_parser/project_parser.rb
@@ -1,25 +1,12 @@
 module Banzai
   module ReferenceParser
     class ProjectParser < BaseParser
+      include Gitlab::Utils::StrongMemoize
+
       self.reference_type = :project
 
       def references_relation
         Project
-      end
-
-      def nodes_visible_to_user(user, nodes)
-        nodes_projects_hash = lazy { projects_for_nodes(nodes) }
-        project_attr = 'data-project'
-
-        readable_project_ids = projects_readable_by_user(nodes_projects_hash.values, user)
-
-        nodes.select do |node|
-          if node.has_attribute?(project_attr)
-            readable_project_ids.include?(nodes_projects_hash[node].try(:id))
-          else
-            true
-          end
-        end
       end
 
       private
@@ -28,8 +15,14 @@ module Banzai
       #
       # projects - The projects to reduce down to those readable by the user.
       # user - The User for which to check the projects
-      def projects_readable_by_user(projects, user)
-        Project.public_or_visible_to_user(user).where("projects.id IN (?)", projects.map(&:id)).pluck(:id)
+      def readable_project_ids_for(projects, user)
+        strong_memoize(:readable_project_ids_for) do
+          Project.public_or_visible_to_user(user).where("projects.id IN (?)", projects.map(&:id)).pluck(:id)
+        end
+      end
+
+      def can_read_reference?(user, ref_project, node)
+        readable_project_ids_for(@projects_for_nodes.values, user).include?(ref_project.try(:id))
       end
     end
   end
