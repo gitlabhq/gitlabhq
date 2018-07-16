dce9599ef53dcdeb035b339510b759d71ad10f1e
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2018-07-16 13:40:55 +0200

    Implement action mailer delivery metrics observer

--- config/initializers/action_mailer_hooks.rb
+++ config/initializers/action_mailer_hooks.rb
@@ -3,5 +3,10 @@ unless Gitlab.config.gitlab.email_enabled
   ActionMailer::Base.logger = nil
 end
 
-ActionMailer::Base.register_interceptor(::Gitlab::Email::Hook::AdditionalHeadersInterceptor)
-ActionMailer::Base.register_interceptor(::Gitlab::Email::Hook::EmailTemplateInterceptor)
+ActionMailer::Base.register_interceptors(
+  ::Gitlab::Email::Hook::AdditionalHeadersInterceptor,
+  ::Gitlab::Email::Hook::EmailTemplateInterceptor,
+  ::Gitlab::Email::Hook::DeliveryMetricsObserver
+)
+
+ActionMailer::Base.register_observer(::Gitlab::Email::Hook::DeliveryMetricsObserver)
