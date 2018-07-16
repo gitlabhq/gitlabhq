0f36cbdfc071a5fbe52ac8793762fda1f4969841
Grzegorz Bizon <grzegorz@gitlab.com>
2018-07-16 12:35:36 +0000

    Resolve conflicts in mock_data.js

--- spec/javascripts/environments/mock_data.js
+++ spec/javascripts/environments/mock_data.js
@@ -64,7 +64,6 @@ export const serverData = [
       environment_type: 'build',
       last_deployment: null,
       has_stop_action: false,
-<<<<<<< HEAD
       environment_path: '/root/review-app/environments/12',
       stop_path: '/root/review-app/environments/12/stop',
       created_at: '2017-02-01T19:42:18.400Z',
@@ -82,8 +81,6 @@ export const serverData = [
       environment_type: 'build',
       last_deployment: null,
       has_stop_action: false,
-=======
->>>>>>> upstream/master
       environment_path: '/root/review-app/environments/12',
       stop_path: '/root/review-app/environments/12/stop',
       created_at: '2017-02-01T19:42:18.400Z',
