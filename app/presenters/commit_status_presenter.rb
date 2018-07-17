6b0bfda8ac0c2eebfa0e89dd0d37c5fc58160c54
Kamil Trzciński <ayufan@ayufan.eu>
2018-07-17 12:58:57 +0200

    Add `runner_unsupported` CI failure

--- app/presenters/commit_status_presenter.rb
+++ app/presenters/commit_status_presenter.rb
@@ -6,7 +6,8 @@ class CommitStatusPresenter < Gitlab::View::Presenter::Delegated
       api_failure: 'There has been an API failure, please try again',
       stuck_or_timeout_failure: 'There has been a timeout failure or the job got stuck. Check your timeout limits or try again',
       runner_system_failure: 'There has been a runner system failure, please try again',
-      missing_dependency_failure: 'There has been a missing dependency failure'
+      missing_dependency_failure: 'There has been a missing dependency failure',
+      # COMMENTED to check if tests gonna fail: runner_unsupported: 'Your runner is unsupported. Upgrade runner to use new features of your Pipeline',
   }.freeze
 
   presents :build
@@ -20,6 +21,6 @@ class CommitStatusPresenter < Gitlab::View::Presenter::Delegated
   end
 
   def unrecoverable?
-    script_failure? || missing_dependency_failure?
+    script_failure? || missing_dependency_failure? || runner_unsupported?
   end
 end
