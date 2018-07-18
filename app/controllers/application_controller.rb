ccebbc97751168c96c12f2d26a354f9a3157df1f
Stan Hu <stanhu@gmail.com>
2018-07-18 11:18:14 -0700

    Limit the TTL for anonymous sessions to 1 hour
    By default, all sessions are given the same expiration time configured in the
    session store (e.g. 1 week). However, unauthenticated users can generate a lot
    of sessions, primarily for CSRF verification. It makes sense to reduce the TTL
    for unauthenticated to something much lower than the default (e.g. 1 hour) to
    limit Redis memory. In addition, Rails creates a new session after login,
    so the short TTL doesn't even need to be extended.
    
    Closes #48101

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,7 @@ class ApplicationController < ActionController::Base
   include EnforcesTwoFactorAuthentication
   include WithPerformanceBar
 
+  before_action :limit_unauthenticated_session_times
   before_action :authenticate_sessionless_user!
   before_action :authenticate_user!
   before_action :enforce_terms!, if: :should_enforce_terms?
@@ -83,6 +84,24 @@ class ApplicationController < ActionController::Base
     else
       authenticate_user!
     end
+  end
+
+  # By default, all sessions are given the same expiration time configured in
+  # the session store (e.g. 1 week). However, unauthenticated users can
+  # generate a lot of sessions, primarily for CSRF verification. It makes
+  # sense to reduce the TTL for unauthenticated to something much lower than
+  # the default (e.g. 1 hour) to limit Redis memory. In addition, Rails
+  # creates a new session after login, so the short TTL doesn't even need to
+  # be extended.
+  def limit_unauthenticated_session_times
+    return if current_user
+
+    # Rack sets this header, but not all tests may have it: https://github.com/rack/rack/blob/fdcd03a3c5a1c51d1f96fc97f9dfa1a9deac0c77/lib/rack/session/abstract/id.rb#L251-L259
+    return unless request.env['rack.session.options']
+
+    # This works because Rack uses these options every time a request is handled:
+    # https://github.com/rack/rack/blob/fdcd03a3c5a1c51d1f96fc97f9dfa1a9deac0c77/lib/rack/session/abstract/id.rb#L342
+    request.env['rack.session.options'][:expire_after] = Settings.gitlab['unauthenticated_session_expire_delay']
   end
 
   protected
