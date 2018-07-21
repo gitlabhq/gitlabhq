2642967818210c4796ccbd99f5bf9dc1be80f729
Peter Leitzen <pl@neopoly.de>
2018-07-21 12:24:07 +0200

    Tag a commit via `/tag 1.2.3 message`

--- app/services/commits/update_service.rb
+++ app/services/commits/update_service.rb
@@ -3,7 +3,26 @@
 module Commits
   class UpdateService < BaseService
     def execute(commit)
-      # TODO authorize user
+      tag_commit(commit)
+    end
+
+    private
+
+    def tag_commit(commit)
+      # TODO authorize
+      return unless params[:tag_name]
+
+      tag_name = params[:tag_name]
+      message = params[:tag_message]
+      release_description = nil
+
+      result = Tags::CreateService
+        .new(commit.project, current_user)
+        .execute(tag_name, commit.sha, message, release_description)
+
+      if result[:status]
+        commit
+      end
     end
   end
 end
