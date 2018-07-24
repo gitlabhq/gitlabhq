2d0fe45163f0c64163a361d08f0d07e7a3fc2d7d
Rémy Coutable <remy@rymai.me>
2018-07-24 13:15:25 +0000

    Merge branch 'zj-backup-timeout' into 'master'
    Remove timeouts for long running requests
    
    See merge request gitlab-org/gitlab-ce!20810

--- lib/gitlab/gitaly_client.rb
+++ lib/gitlab/gitaly_client.rb
@@ -407,7 +407,7 @@ module Gitlab
 
     # The default timeout on all Gitaly calls
     def self.default_timeout
-      return 0 if Sidekiq.server?
+      return no_timeout if Sidekiq.server?
 
       timeout(:gitaly_timeout_default)
     end
@@ -418,6 +418,10 @@ module Gitlab
 
     def self.medium_timeout
       timeout(:gitaly_timeout_medium)
+    end
+
+    def self.no_timeout
+      0
     end
 
     def self.timeout(timeout_name)
