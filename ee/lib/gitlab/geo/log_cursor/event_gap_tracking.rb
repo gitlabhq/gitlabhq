3abe09f4e838573bdf754e956514bb6f2e3ce14e
Toon Claes <toon@gitlab.com>
2018-07-24 13:53:46 +0200

    Track the Geo event log gaps in redis and handle them later
    Due to the way PostgreSQL replication works can it happen events are
    replicated after Geo log cursor tries to replay them. Therefore, track
    these gaps in Redis, and try 10 minutes later to replay them.
    
    Closes gitlab-org/gitlab-ee#6956

---
+++ ee/lib/gitlab/geo/log_cursor/event_gap_tracking.rb
@@ -0,0 +1,84 @@
+module Gitlab
+  module Geo
+    module LogCursor
+      class EventGapTracking
+        include Utils::StrongMemoize
+
+        attr_reader :log_level
+        attr_accessor :previous_id
+
+        GEO_LOG_CURSOR_GAPS = "Gitlab::Geo::LogCursor::Gaps".freeze
+        GAP_GRACE_PERIOD = 10.minutes
+        GAP_OUTDATED_PERIOD = 1.hour
+
+        def initialize(log_level = Rails.logger.level)
+          @log_level = log_level
+          @previous_id = 0
+        end
+
+        def check!(current_id)
+          return unless previous_id > 0
+
+          return unless gap?(current_id)
+
+          track_gap(current_id)
+        ensure
+          self.previous_id = current_id
+        end
+
+        # accepts a block that should return whether the event was handled
+        def fill_gaps
+          with_redis do |redis|
+            redis.zremrangebyscore(GEO_LOG_CURSOR_GAPS, '-inf', outdated_timestamp)
+
+            gap_ids = redis.zrangebyscore(GEO_LOG_CURSOR_GAPS, '-inf', grace_timestamp, with_scores: true)
+
+            gap_ids.each do |event_id, score|
+              handled = yield event_id.to_i
+
+              redis.zrem(GEO_LOG_CURSOR_GAPS, event_id) if handled
+            end
+          end
+        end
+
+        def track_gap(current_id)
+          logger.info("Event log gap detected", previous_event_id: previous_id, current_event_id: current_id)
+
+          with_redis do |redis|
+            expire_time = Time.now.to_i
+
+            (previous_id+1 .. current_id-1).each do |gap_id|
+              redis.zadd(GEO_LOG_CURSOR_GAPS, expire_time, gap_id)
+            end
+          end
+        end
+
+        def gap?(current_id)
+          return false if previous_id <= 0
+
+          current_id > (previous_id + 1)
+        end
+
+        private
+
+        def logger
+          strong_memoize(:logger) do
+            Gitlab::Geo::LogCursor::Logger.new(self.class, log_level)
+          end
+        end
+
+        def with_redis
+          ::Gitlab::Redis::Cache.with { |redis| yield redis }
+        end
+
+        def grace_timestamp
+          (Time.now - GAP_GRACE_PERIOD).to_i
+        end
+
+        def outdated_timestamp
+          (Time.now - GAP_OUTDATED_PERIOD).to_i
+        end
+      end
+    end
+  end
+end
---
+++ ee/spec/lib/gitlab/geo/log_cursor/event_gap_tracking_spec.rb
@@ -0,0 +1,144 @@
+require 'spec_helper'
+
+describe Gitlab::Geo::LogCursor::EventGapTracking, :clean_gitlab_redis_cache do
+  let(:previous_event_id) { 7 }
+  let(:gap_id) { previous_event_id + 1 }
+  let(:event_id_with_gap) { previous_event_id + 2 }
+  subject(:gap_tracking) { described_class.new }
+
+  before do
+    gap_tracking.previous_id = previous_event_id
+  end
+
+  describe '#check!' do
+    it 'does nothing when previous id not valid' do
+      gap_tracking.previous_id = 0
+
+      expect(gap_tracking).to_not receive(:gap?)
+
+      gap_tracking.check!(event_id_with_gap)
+
+      expect(gap_tracking.previous_id).to eq(event_id_with_gap)
+    end
+
+    it 'does nothing when there is no gap' do
+      expect(gap_tracking).to_not receive(:track_gap)
+
+      gap_tracking.check!(previous_event_id + 1)
+
+      expect(gap_tracking.previous_id).to eq(previous_event_id + 1)
+    end
+
+    it 'tracks the gap if there is one' do
+      expect(gap_tracking).to receive(:track_gap)
+
+      gap_tracking.check!(event_id_with_gap)
+
+      expect(gap_tracking.previous_id).to eq(event_id_with_gap)
+    end
+  end
+
+  describe '#fill_gaps' do
+    it 'ignore gaps that are less than 10 minutes old' do
+      Timecop.freeze do
+        gap_tracking.check!(event_id_with_gap)
+
+        expect { |blk| gap_tracking.fill_gaps(&blk) }.not_to yield_with_args(anything)
+      end
+    end
+
+    it 'handles gaps that are more than 10 minutes old' do
+      gap_tracking.check!(event_id_with_gap)
+
+      Timecop.travel(12.minutes) do
+        expect { |blk| gap_tracking.fill_gaps(&blk) }.to yield_with_args(gap_id)
+      end
+    end
+
+    it 'drops gaps older than 1 hour' do
+      gap_tracking.check!(event_id_with_gap)
+
+      Timecop.travel(62.minutes) do
+        expect { |blk| gap_tracking.fill_gaps(&blk) }.not_to yield_with_args(anything)
+      end
+
+      expect(read_gaps).to be_empty
+    end
+  end
+
+  describe '#track_gap' do
+    it 'logs a message' do
+      logger = spy(:logger)
+      allow(gap_tracking).to receive(:logger).and_return(logger)
+
+      expect(logger).to receive(:info).with(/gap detected/, hash_including(previous_event_id: previous_event_id, current_event_id: event_id_with_gap))
+
+      gap_tracking.track_gap(event_id_with_gap)
+    end
+
+    it 'saves the gap id in redis' do
+      Timecop.freeze do
+        gap_tracking.track_gap(event_id_with_gap)
+
+        expect(read_gaps).to contain_exactly([gap_id.to_s, Time.now.to_i])
+      end
+    end
+
+    it 'saves a range of gaps id in redis' do
+      Timecop.freeze do
+        gap_tracking.track_gap(event_id_with_gap + 3)
+
+        expected_gaps = (previous_event_id+1..event_id_with_gap+2).collect { |id| [id.to_s, Time.now.to_i] }
+
+        expect(read_gaps).to match_array(expected_gaps)
+      end
+    end
+
+    it 'saves the gaps in order' do
+      expected_gaps = []
+
+      Timecop.freeze do
+        gap_tracking.track_gap(event_id_with_gap)
+        expected_gaps << [gap_id.to_s, Time.now.to_i]
+      end
+
+      Timecop.travel(2.minutes) do
+        gap_tracking.previous_id = 17
+        gap_tracking.track_gap(19)
+        expected_gaps << [18.to_s, Time.now.to_i]
+      end
+
+      expect(read_gaps).to eq(expected_gaps)
+    end
+  end
+
+  describe '#gap?' do
+    it 'returns false when current_id is the previous +1' do
+      expect(gap_tracking.gap?(previous_event_id + 1)).to be_falsy
+    end
+
+    it 'returns true when current_id is the previous +2' do
+      expect(gap_tracking.gap?(previous_event_id + 2)).to be_truthy
+    end
+
+    it 'returns false when current_id is equal to the previous' do
+      expect(gap_tracking.gap?(previous_event_id)).to be_falsy
+    end
+
+    it 'returns false when current_id less than the previous' do
+      expect(gap_tracking.gap?(previous_event_id - 1)).to be_falsy
+    end
+
+    it 'returns false when previous id is 0' do
+      gap_tracking.previous_id = 0
+
+      expect(gap_tracking.gap?(100)).to be_falsy
+    end
+  end
+
+  def read_gaps
+    ::Gitlab::Redis::Cache.with do |redis|
+      redis.zrangebyscore(described_class::GEO_LOG_CURSOR_GAPS, '-inf', '+inf', with_scores: true)
+    end
+  end
+end
