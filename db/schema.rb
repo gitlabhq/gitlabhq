a68f24164ef07188ba2abcb81c1debdb9df5fc08
Luke Bennett <lukeeeebennettplus@gmail.com>
2018-07-25 12:20:46 +0100

    Revert all schema.rb changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180718005113) do
+ActiveRecord::Schema.define(version: 20180704204006) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -168,7 +168,6 @@ ActiveRecord::Schema.define(version: 20180718005113) do
     t.boolean "enforce_terms", default: false
     t.boolean "mirror_available", default: true, null: false
     t.boolean "hide_third_party_offers", default: false, null: false
-    t.boolean "instance_statistics_visibility_private", default: false, null: false
   end
 
   create_table "audit_events", force: :cascade do |t|
