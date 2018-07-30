f128cdb8962e3a92ec9cb956c3aa674d7dc10ba5
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2018-07-30 20:36:37 +0300

    Add more specs to labels search feature
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- spec/features/projects/labels/search_labels_spec.rb
+++ spec/features/projects/labels/search_labels_spec.rb
@@ -9,11 +9,11 @@ describe 'Search for labels', :js do
   before do
     project.add_maintainer(user)
     sign_in(user)
+
+    visit project_labels_path(project)
   end
 
   it 'searches for label by title' do
-    visit project_labels_path(project)
-
     fill_in 'label-search', with: 'Bar'
     find('#label-search').native.send_keys(:enter)
 
@@ -24,8 +24,6 @@ describe 'Search for labels', :js do
   end
 
   it 'searches for label by title' do
-    visit project_labels_path(project)
-
     fill_in 'label-search', with: 'Lorem'
     find('#label-search').native.send_keys(:enter)
 
@@ -33,5 +31,48 @@ describe 'Search for labels', :js do
     expect(page).to have_content(label1.description)
     expect(page).not_to have_content(label2.title)
     expect(page).not_to have_content(label2.description)
+  end
+
+  it 'shows nothing found message' do
+    fill_in 'label-search', with: 'nonexistent'
+    find('#label-search').native.send_keys(:enter)
+
+    expect(page).to have_content('No labels with such name or description')
+    expect(page).not_to have_content(label1.title)
+    expect(page).not_to have_content(label1.description)
+    expect(page).not_to have_content(label2.title)
+    expect(page).not_to have_content(label2.description)
+  end
+
+  context 'priority labels' do
+    let!(:label_priority) { create(:label_priority, label: label1, project: project) }
+
+    it 'searches for priority label' do
+      fill_in 'label-search', with: 'Foo'
+      find('#label-search').native.send_keys(:enter)
+
+      page.within('.prioritized-labels') do
+        expect(page).to have_content(label1.title)
+        expect(page).to have_content(label1.description)
+      end
+
+      page.within('.other-labels') do
+        expect(page).to have_content('No labels with such name or description')
+      end
+    end
+
+    it 'searches for other label' do
+      fill_in 'label-search', with: 'Bar'
+      find('#label-search').native.send_keys(:enter)
+
+      page.within('.prioritized-labels') do
+        expect(page).to have_content('No labels with such name or description')
+      end
+
+      page.within('.other-labels') do
+        expect(page).to have_content(label2.title)
+        expect(page).to have_content(label2.description)
+      end
+    end
   end
 end
