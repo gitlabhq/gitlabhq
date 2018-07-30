9fab947119f310fc59f58ec575bbe222b8ec3bdf
Peter Leitzen <pl@neopoly.de>
2018-07-30 13:25:51 +0200

    Let Commits::TagService return a result hash

--- spec/services/commits/tag_service_spec.rb
+++ spec/services/commits/tag_service_spec.rb
@@ -13,11 +13,12 @@ describe Commits::TagService do
   describe '#execute' do
     let(:service) { described_class.new(project, user, opts) }
 
-    shared_examples 'tagging fails' do
-      it 'returns nil' do
-        tagged_commit = service.execute(commit)
+    shared_examples 'tag failure' do
+      it 'returns a hash with the :error status' do
+        result = service.execute(commit)
 
-        expect(tagged_commit).to be_nil
+        expect(result[:status]).to eq(:error)
+        expect(result[:message]).to eq(error_message)
       end
 
       it 'does not add a system note' do
@@ -51,10 +52,14 @@ describe Commits::TagService do
       end
 
       context 'when tagging succeeds' do
-        it 'returns the commit' do
-          tagged_commit = service.execute(commit)
+        it 'returns a hash with the :success status and created tag' do
+          result = service.execute(commit)
 
-          expect(tagged_commit).to eq(commit)
+          expect(result[:status]).to eq(:success)
+
+          tag = result[:tag]
+          expect(tag.name).to eq(opts[:tag_name])
+          expect(tag.message).to eq(opts[:tag_message])
         end
 
         it 'adds a system note' do
@@ -66,13 +71,19 @@ describe Commits::TagService do
       end
 
       context 'when tagging fails' do
+        let(:tag_error) { 'GitLab: You are not allowed to push code to this project.' }
+
         before do
           tag_stub = instance_double(Tags::CreateService)
           allow(Tags::CreateService).to receive(:new).and_return(tag_stub)
-          allow(tag_stub).to receive(:execute).and_return({ status: :error })
+          allow(tag_stub).to receive(:execute).and_return({
+            status: :error, message: tag_error
+          })
         end
 
-        include_examples 'tagging fails'
+        it_behaves_like 'tag failure' do
+          let(:error_message) { tag_error }
+        end
       end
     end
 
@@ -81,7 +92,9 @@ describe Commits::TagService do
         {}
       end
 
-      include_examples 'tagging fails'
+      it_behaves_like 'tag failure' do
+        let(:error_message) { 'Missing parameter tag_name' }
+      end
     end
   end
 end
