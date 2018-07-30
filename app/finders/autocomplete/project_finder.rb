6f3c490107f5fa7dd00bce0bbd89e4a0fa4d6389
Yorick Peterse <yorickpeterse@gmail.com>
2018-07-30 17:45:49 +0200

    Refactor AutocompleteController
    This refactors the AutocompleteController according to the guidelines
    and boundaries discussed in
    https://gitlab.com/gitlab-org/gitlab-ce/issues/49653. Specifically,
    ActiveRecord logic is moved to different finders, which are then used in
    the controller. View logic in turn is moved to presenters, instead of
    directly using ActiveRecord's "to_json" method.
    
    The finder MoveToProjectFinder is also adjusted according to the
    abstraction guidelines and boundaries, resulting in a much more simple
    finder.
    
    By using finders (and other abstractions) more actively, we can push a
    lot of logic out of the controller. We also remove the need for various
    "before_action" hooks, though this could be achieved without using
    finders as well.
    
    The various finders related to AutcompleteController have also been
    moved into a namespace. This removes the need for calling everything
    "AutocompleteSmurfFinder", instead you can use
    "Autocomplete::SmurfFinder".

---
+++ app/finders/autocomplete/project_finder.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module Autocomplete
+  # Finder for retrieving a project to use for autocomplete data sources.
+  class ProjectFinder
+    attr_reader :current_user, :project_id
+
+    # current_user - The currently logged in user, if any.
+    # params - A Hash containing parameters to use for finding the project.
+    #
+    # The following parameters are supported:
+    #
+    # * project_id: The ID of the project to find.
+    def initialize(current_user = nil, params = {})
+      @current_user = current_user
+      @project_id = params[:project_id]
+    end
+
+    # Attempts to find a Project based on the current project ID.
+    def execute
+      return if project_id.blank?
+
+      project = Project.find(project_id)
+
+      # This removes the need for using `return render_404` and similar patterns
+      # in controllers that use this finder.
+      unless Ability.allowed?(current_user, :read_project, project)
+        raise ActiveRecord::RecordNotFound
+          .new("Could not find a Project with ID #{project_id}")
+      end
+
+      project
+    end
+  end
+end
