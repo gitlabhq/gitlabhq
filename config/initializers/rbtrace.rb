37748e44f7d1eec0b280d1f6aa15b2282a12dec4
Stan Hu <stanhu@gmail.com>
2018-07-30 08:10:49 -0700

    Move rbtrace initialization into Sidekiq

--- config/initializers/rbtrace.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-# rbtrace needs to be included after the unicorn worker forks.
-# See the after_fork block in config/unicorn.rb.example.
-require 'rbtrace' if ENV['ENABLE_RBTRACE'] && Sidekiq.server?
