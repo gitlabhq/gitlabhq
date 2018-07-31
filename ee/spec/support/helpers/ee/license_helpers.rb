c6afc977be5f7ba4a3ef7b4327ea52d793066476
Oswaldo Ferreira <oswaldo@gitlab.com>
2018-07-30 21:31:34 -0300

    Raise error when stubbing a feature that is not defined on license.rb

--- ee/spec/support/helpers/ee/license_helpers.rb
+++ ee/spec/support/helpers/ee/license_helpers.rb
@@ -9,6 +9,15 @@ module EE
     # This enables `geo` and disables `deploy_board` features for a spec.
     # Other features are still enabled/disabled as defined in the license.
     def stub_licensed_features(features)
+      existing_features = License::FEATURES_BY_PLAN.values.flatten.map(&:to_sym)
+      missing_features = features.keys.map(&:to_sym) - existing_features
+
+      if missing_features.any?
+        subject = missing_features.join(', ')
+        noun = 'feature'.pluralize(missing_features.size)
+        raise ArgumentError, "#{subject} should be defined as licensed #{noun}"
+      end
+
       allow(License).to receive(:feature_available?).and_call_original
 
       features.each do |feature, enabled|
