e16e82b583e9a837095d573962900af84d46a03e
Luke Bennett <lukeeeebennettplus@gmail.com>
2018-08-01 14:11:04 +0100

    Port CE

--- spec/features/instance_statistics/cohorts_spec.rb
+++ spec/features/instance_statistics/cohorts_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'Admin cohorts page' do
+describe 'Cohorts page' do
   before do
     sign_in(create(:admin))
   end
@@ -8,7 +8,7 @@ describe 'Admin cohorts page' do
   it 'See users count per month' do
     2.times { create(:user) }
 
-    visit admin_cohorts_path
+    visit instance_statistics_cohorts_path
 
     expect(page).to have_content("#{Time.now.strftime('%b %Y')} 3 0")
   end
