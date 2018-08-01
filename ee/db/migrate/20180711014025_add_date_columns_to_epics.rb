dfe844e9e694d47328087a9eb1ce3cd58330de93
Mark Chao <mchao@gitlab.com>
2018-08-01 10:52:18 +0800

    Fix migration ordering

--- ee/db/migrate/20180711014025_add_date_columns_to_epics.rb
+++ ee/db/migrate/20180711014025_add_date_columns_to_epics.rb
@@ -7,12 +7,12 @@ class AddDateColumnsToEpics < ActiveRecord::Migration
 
   def change
     change_table :epics do |t|
-      t.boolean :start_date_is_fixed
-      t.date :start_date_fixed
       t.references :start_date_sourcing_milestone
-      t.boolean :due_date_is_fixed
-      t.date :due_date_fixed
       t.references :due_date_sourcing_milestone
+      t.date :start_date_fixed
+      t.date :due_date_fixed
+      t.boolean :start_date_is_fixed
+      t.boolean :due_date_is_fixed
     end
   end
 end
