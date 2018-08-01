05c7c7e0ef0639fde2d98ad3fe9f09b2297ffa4b
Thong Kuah <tkuah@gitlab.com>
2018-08-01 22:43:27 +1200

    Update Helm Tiller used by gitlab-managed-apps to 2.7.2

--- spec/support/shared_examples/helm_generated_script.rb
+++ spec/support/shared_examples/helm_generated_script.rb
@@ -7,7 +7,7 @@ shared_examples 'helm commands' do
          echo http://mirror.clarkson.edu/alpine/v$ALPINE_VERSION/main >> /etc/apk/repositories
          echo http://mirror1.hs-esslingen.de/pub/Mirrors/alpine/v$ALPINE_VERSION/main >> /etc/apk/repositories
          apk add -U wget ca-certificates openssl >/dev/null
-         wget -q -O - https://kubernetes-helm.storage.googleapis.com/helm-v2.7.0-linux-amd64.tar.gz | tar zxC /tmp >/dev/null
+         wget -q -O - https://kubernetes-helm.storage.googleapis.com/helm-v2.7.2-linux-amd64.tar.gz | tar zxC /tmp >/dev/null
          mv /tmp/linux-amd64/helm /usr/bin/
       EOS
     end
