45deab01c8574ae38d0fabbe663eebf50be1a92a
Luke Bennett <lukeeeebennettplus@gmail.com>
2018-08-01 20:03:55 +0100

    Fix rubocop lint

--- ee/app/helpers/mirror_helper.rb
+++ ee/app/helpers/mirror_helper.rb
@@ -36,6 +36,6 @@ module MirrorHelper
 
   def mirrors_form_data_attributes
     { project_mirror_ssh_endpoint: ssh_host_keys_project_mirror_path(@project, :json),
-    project_mirror_endpoint: project_mirror_path(@project) }
+      project_mirror_endpoint: project_mirror_path(@project) }
   end
 end
