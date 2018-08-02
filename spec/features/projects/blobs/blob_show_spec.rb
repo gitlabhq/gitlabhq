fed97a68b941659949e7b8b40977d54850bfbf8f
Winnie Hellmann <winnie@gitlab.com>
2018-08-02 18:17:20 +0200

    Fix GPG status badge loading regressions

--- spec/features/projects/blobs/blob_show_spec.rb
+++ spec/features/projects/blobs/blob_show_spec.rb
@@ -552,4 +552,33 @@ describe 'File blob', :js do
       end
     end
   end
+
+  context 'for subgroups' do
+    let(:group) { create(:group) }
+    let(:subgroup) { create(:group, parent: group) }
+    let(:project) { create(:project, :public, :repository, group: subgroup) }
+
+    it 'renders tree table without errors' do
+      visit_blob('README.md')
+
+      expect(page).to have_selector('.file-content')
+      expect(page).not_to have_selector('.flash-alert')
+    end
+
+    it 'displays a GPG badge' do
+      visit_blob('CONTRIBUTING.md', ref: '33f3729a45c02fc67d00adb1b8bca394b0e761d9')
+
+      expect(page).not_to have_selector '.gpg-status-box.js-loading-gpg-badge'
+      expect(page).to have_selector '.gpg-status-box.invalid'
+    end
+  end
+
+  context 'on signed merge commit' do
+    it 'displays a GPG badge' do
+      visit_blob('conflicting-file.md', ref: '6101e87e575de14b38b4e1ce180519a813671e10')
+
+      expect(page).not_to have_selector '.gpg-status-box.js-loading-gpg-badge'
+      expect(page).to have_selector '.gpg-status-box.invalid'
+    end
+  end
 end
