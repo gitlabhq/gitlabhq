02bf0639b4ba0dd73079dd129aebfd01180f6faf
Shinya Maeda <shinya@gitlab.com>
2018-08-03 17:34:31 +0900

    Fix static analysys

--- app/services/ci/compare_test_reports_service.rb
+++ app/services/ci/compare_test_reports_service.rb
@@ -6,10 +6,10 @@ module Ci
       base_pipeline = project.pipelines.find_by_iid(base_pipeline_iid) if base_pipeline_iid
       head_pipeline = project.pipelines.find_by_iid(head_pipeline_iid)
 
-      begin  
+      begin
         comparer = Gitlab::Ci::Reports::TestReportsComparer
           .new(base_pipeline&.test_reports, head_pipeline.test_reports)
-  
+
         {
           status: :parsed,
           data: TestReportsComparerSerializer
