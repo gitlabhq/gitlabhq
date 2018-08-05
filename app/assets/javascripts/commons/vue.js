4c803ff7cf0a6b1d144d5b4b2a078a2d7f28e6be
Tim Zallmann <tzallmann@gitlab.com>
2018-08-05 19:27:56 +0200

    Set Performance Mode + Added Gutter Content back in

--- app/assets/javascripts/commons/vue.js
+++ app/assets/javascripts/commons/vue.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import '../vue_shared/vue_resource_interceptor';
 
+Vue.config.performance = true;
+
 if (process.env.NODE_ENV !== 'production') {
   Vue.config.productionTip = false;
 }
