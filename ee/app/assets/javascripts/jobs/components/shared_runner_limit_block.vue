12be1559c6a0d2f270b8a98d3debf41a99c4732b
Filipa Lacerda <filipa@gitlab.com>
2018-08-09 17:14:54 +0100

    Creates a Vue component for shared runner limit block

---
+++ ee/app/assets/javascripts/jobs/components/shared_runner_limit_block.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  props: {
+    quotaUsed: {
+      type: Number,
+      required: true,
+    },
+    quotaLimit: {
+      type: Number,
+      required: true,
+    },
+    runnersPath: {
+      type: String,
+      required: false,
+      default: null,
+    },
+  },
+};
+</script>
+<template>
+  <div class="bs-callout bs-callout-warning">
+    <p>
+      {{ s__("Runners|You have used all your shared Runners pipeline minutes.") }}
+      {{ quotaUsed }} of {{ quotaLimit }}
+
+      <template v-if="runnersPath">
+        {{ __("For more information, go to the ") }}
+        <a :href="runnersPath">
+          {{ __("Runners page.") }}
+        </a>
+      </template>
+    </p>
+  </div>
+</template>
