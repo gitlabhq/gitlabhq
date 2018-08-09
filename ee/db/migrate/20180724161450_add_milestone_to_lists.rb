d8dad0ec4a3917fa845e0b3d895c8b87974976cd
Jasper Maes <jaspermaes.jm@gmail.com>
2018-08-10 00:50:47 +0200

    Rails5 fix AddMilestoneToLists migration rollback deleting wrong foreign key

--- ee/db/migrate/20180724161450_add_milestone_to_lists.rb
+++ ee/db/migrate/20180724161450_add_milestone_to_lists.rb
@@ -1,7 +1,11 @@
 class AddMilestoneToLists < ActiveRecord::Migration
   DOWNTIME = false
 
-  def change
+  def up
     add_reference :lists, :milestone, index: true, foreign_key: { on_delete: :cascade }
+  end
+
+  def down
+    remove_reference :lists, :milestone, foreign_key: true
   end
 end
