d1a7206ffd7b3986441b6d8e13f9cabb59268878
Peter Leitzen <pl@neopoly.de>
2018-08-10 18:15:25 +0200

    Freeze string literals
    See Danger's suggestions:
    https://gitlab.com/gitlab-org/gitlab-ee/merge_requests/6869#note_93730253

--- app/policies/commit_policy.rb
+++ app/policies/commit_policy.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CommitPolicy < BasePolicy
   delegate { @subject.project }
 end
--- spec/features/commits/user_uses_slash_commands_spec.rb
+++ spec/features/commits/user_uses_slash_commands_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 describe 'Commit > User uses quick actions', :js do
