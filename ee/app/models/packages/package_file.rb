ffea496b137a0c6c4503b2fb071251df6575cec1
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2018-08-10 19:32:15 +0300

    Fix PackageFile for object storage [ci skip]
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- ee/app/models/packages/package_file.rb
+++ ee/app/models/packages/package_file.rb
@@ -7,4 +7,12 @@ class Packages::PackageFile < ActiveRecord::Base
   validates :file_name, presence: true
 
   mount_uploader :file, Packages::PackageFileUploader
+
+  after_save :update_file_store, if: :file_changed?
+
+  def update_file_store
+    # The file.object_store is set during `uploader.store!`
+    # which happens after object is inserted/updated
+    self.update_column(:file_store, file.object_store)
+  end
 end
