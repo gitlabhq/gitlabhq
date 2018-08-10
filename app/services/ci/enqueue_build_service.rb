f8324c83dc1050b385542f855cfd9c3f5d382f64
Mayra Cabrera <mcabrera@gitlab.com>
2018-08-10 11:11:23 -0500

    Fixes resolution for enqueue build service

--- app/services/ci/enqueue_build_service.rb
+++ app/services/ci/enqueue_build_service.rb
@@ -1,11 +1,8 @@
 # frozen_string_literal: true
 module Ci
   class EnqueueBuildService < BaseService
-<<<<<<< HEAD
     prepend EE::Ci::EnqueueBuildService
 
-=======
->>>>>>> upstream/master
     def execute(build)
       build.enqueue
     end
