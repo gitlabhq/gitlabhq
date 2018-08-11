2ad1688be9bfed122e61d4e10ed64faf8395c26e
gfyoung <gfyoung17@gmail.com>
2018-08-11 00:00:39 -0700

    Enable frozen string in vestigial app files
    Partially addresses #47424.

--- app/serializers/test_reports_comparer_entity.rb
+++ app/serializers/test_reports_comparer_entity.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class TestReportsComparerEntity < Grape::Entity
   expose :total_status, as: :status
 
--- app/services/todos/destroy/project_private_service.rb
+++ app/services/todos/destroy/project_private_service.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Todos
   module Destroy
     class ProjectPrivateService < ::Todos::Destroy::BaseService
