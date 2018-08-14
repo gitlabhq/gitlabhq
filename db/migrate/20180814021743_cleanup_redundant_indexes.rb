ad5fa6078725a226614dbbf08c83ec92e0bfc39c
NikolayS <nsamokhvalov@gitlab.com>
2018-08-13 22:32:37 -0700

    Enable Frozen String Literal

--- db/migrate/20180814021743_cleanup_redundant_indexes.rb
+++ db/migrate/20180814021743_cleanup_redundant_indexes.rb
@@ -1,5 +1,4 @@
-# See http://doc.gitlab.com/ce/development/migration_style_guide.html
-# for more information on how to write migrations for GitLab.
+# frozen_string_literal: true
 
 class CleanupRedundantIndexes < ActiveRecord::Migration
   include Gitlab::Database::MigrationHelpers
