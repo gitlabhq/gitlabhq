2bb221f0efe9068a4c08efa1a8ccd69fd22ab745
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2018-08-14 14:03:16 +0300

    Rebuild db/schema with changes in pockages migrations
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -1922,9 +1922,9 @@ ActiveRecord::Schema.define(version: 20180810155213) do
   end
 
   create_table "packages_maven_metadata", id: :bigserial, force: :cascade do |t|
+    t.integer "package_id", limit: 8, null: false
     t.datetime_with_timezone "created_at", null: false
     t.datetime_with_timezone "updated_at", null: false
-    t.integer "package_id", null: false
     t.string "app_group", null: false
     t.string "app_name", null: false
     t.string "app_version"
@@ -1935,10 +1935,10 @@ ActiveRecord::Schema.define(version: 20180810155213) do
   add_index "packages_maven_metadata", ["path"], name: "index_packages_maven_metadata_on_path", using: :btree
 
   create_table "packages_package_files", id: :bigserial, force: :cascade do |t|
+    t.integer "package_id", limit: 8, null: false
     t.datetime_with_timezone "created_at", null: false
     t.datetime_with_timezone "updated_at", null: false
     t.integer "size", limit: 8
-    t.integer "package_id", null: false
     t.integer "file_type"
     t.integer "file_store"
     t.binary "file_md5"
@@ -1951,9 +1951,9 @@ ActiveRecord::Schema.define(version: 20180810155213) do
   add_index "packages_package_files", ["package_id"], name: "index_packages_package_files_on_package_id", using: :btree
 
   create_table "packages_packages", id: :bigserial, force: :cascade do |t|
+    t.integer "project_id", null: false
     t.datetime_with_timezone "created_at", null: false
     t.datetime_with_timezone "updated_at", null: false
-    t.integer "project_id", null: false
     t.string "name", null: false
     t.string "version"
   end
