f0c3dad06b9fe33d460de4c669f62ca7bd01d78a
Tim Zallmann <tzallmann@gitlab.com>
2018-08-15 07:05:02 +0000

    Merge branch '50281-js-pages-do-not-load-on-windows-8-ie-11' into 'master'
    Resolve "JS Pages do not load on windows 8, IE 11"
    
    Closes #50281
    
    See merge request gitlab-org/gitlab-ce!21214

--- app/assets/javascripts/commons/polyfills.js
+++ app/assets/javascripts/commons/polyfills.js
@@ -8,6 +8,7 @@ import 'core-js/fn/object/assign';
 import 'core-js/fn/promise';
 import 'core-js/fn/string/code-point-at';
 import 'core-js/fn/string/from-code-point';
+import 'core-js/fn/string/includes';
 import 'core-js/fn/symbol';
 import 'core-js/es6/map';
 import 'core-js/es6/weak-map';
