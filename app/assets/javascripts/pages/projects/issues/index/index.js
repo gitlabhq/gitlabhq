777e90d51652df9b722f136518f7aeffafaf2657
Winnie Hellmann <winnie@gitlab.com>
2018-08-20 20:53:46 +0200

    Resolve conflict in app/assets/javascripts/pages/projects/issues/index/index.js

--- app/assets/javascripts/pages/projects/issues/index/index.js
+++ app/assets/javascripts/pages/projects/issues/index/index.js
@@ -4,19 +4,14 @@ import IssuableIndex from '~/issuable_index';
 import ShortcutsNavigation from '~/shortcuts_navigation';
 import UsersSelect from '~/users_select';
 import initFilteredSearch from '~/pages/search/init_filtered_search';
-import IssuesFilteredSearchTokenKeys from '~/filtered_search/issues_filtered_search_token_keys';
 import { FILTERED_SEARCH } from '~/pages/constants';
 import { ISSUABLE_INDEX } from '~/pages/projects/constants';
-import FilteredSearchTokenKeysIssues from 'ee/filtered_search/filtered_search_token_keys_issues';
+import IssuesFilteredSearchTokenKeysEE from 'ee/filtered_search/issues_filtered_search_token_keys';
 
 document.addEventListener('DOMContentLoaded', () => {
   initFilteredSearch({
     page: FILTERED_SEARCH.ISSUES,
-<<<<<<< HEAD
-    filteredSearchTokenKeys: FilteredSearchTokenKeysIssues,
-=======
-    filteredSearchTokenKeys: IssuesFilteredSearchTokenKeys,
->>>>>>> d4387d88767... make FilteredSearchTokenKeys generic
+    filteredSearchTokenKeys: IssuesFilteredSearchTokenKeysEE,
   });
   new IssuableIndex(ISSUABLE_INDEX.ISSUE);
 
