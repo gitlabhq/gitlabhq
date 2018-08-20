e3f798496e0ce32c49a034a13fe7695c71436260
Andrew Newdigate <andrew@gitlab.com>
2018-08-20 22:28:22 +0100

    Switched to OpenTracing.start_span

--- lib/gitlab/tracing_interceptor.rb
+++ lib/gitlab/tracing_interceptor.rb
@@ -5,54 +5,82 @@ class TracingGRPCClientInterceptor < GRPC::ClientInterceptor
   def request_response(request:, call:, method:, metadata:)
     return yield if OpenTracing.active_span == nil
 
-    metadata['request_id'] = generate_request_id
-    scope = OpenTracing.start_active_span(method)
+    span = OpenTracing.start_span(method)
+    if span
+      span.set_tag('grpc.type','unary')
+      span.set_tag('component','gRPC')
+      span.set_tag('span.kind','client')
+    end
+
+    OpenTracing.scope_manager.activate(span)
+
     begin
+      OpenTracing.inject(span.context, OpenTracing::FORMAT_TEXT_MAP, metadata)
       yield
     ensure
-      scope.close
+      span.finish
     end
   end
 
   def client_streamer(requests:, call:, method:, metadata:)
     return yield if OpenTracing.active_span == nil
 
-    metadata['request_id'] = generate_request_id
-    scope = OpenTracing.start_active_span(method)
+    span = OpenTracing.start_span(method)
+    if span
+      span.set_tag('grpc.type','client_stream')
+      span.set_tag('component','gRPC')
+      span.set_tag('span.kind','client')
+    end
+
+    OpenTracing.scope_manager.activate(span)
+
     begin
+      OpenTracing.inject(span.context, OpenTracing::FORMAT_TEXT_MAP, metadata)
       yield
     ensure
-      scope.close
+      span.finish
     end
   end
 
   def server_streamer(request:, call:, method:, metadata:)
     return yield if OpenTracing.active_span == nil
 
-    metadata['request_id'] = generate_request_id
-    scope = OpenTracing.start_active_span(method)
+    span = OpenTracing.start_span(method)
+    if span
+      span.set_tag('grpc.type','server_stream')
+      span.set_tag('component','gRPC')
+      span.set_tag('span.kind','client')
+    end
+
+
+    OpenTracing.scope_manager.activate(span)
+
     begin
+      OpenTracing.inject(span.context, OpenTracing::FORMAT_TEXT_MAP, metadata)
       yield
     ensure
-      scope.close
+      span.finish
     end
   end
 
   def bidi_streamer(requests:, call:, method:, metadata:)
     return yield if OpenTracing.active_span == nil
 
-    metadata['request_id'] = generate_request_id
-    scope = OpenTracing.start_active_span(method)
+    span = OpenTracing.start_span(method)
+    if span
+      span.set_tag('grpc.type','bidi_stream')
+      span.set_tag('component','gRPC')
+      span.set_tag('span.kind','client')
+    end
+
+    OpenTracing.scope_manager.activate(span)
+
     begin
+      OpenTracing.inject(span.context, OpenTracing::FORMAT_TEXT_MAP, metadata)
       yield
     ensure
-      scope.close
+      span.finish
     end
   end
 
-  private
-
-  def generate_request_id
-    SecureRandom.uuid
-  end
 end
