c0923cbd4f8a543deaeba81ebf70e051f9ad9404
Phil Hughes <me@iamphill.com>
2018-08-28 10:41:11 +0100

    added quick create new file buttons to modal

--- app/assets/javascripts/ide/components/new_dropdown/modal.vue
+++ app/assets/javascripts/ide/components/new_dropdown/modal.vue
@@ -1,6 +1,7 @@
 <script>
+import $ from 'jquery';
 import { __ } from '~/locale';
-import { mapActions, mapState } from 'vuex';
+import { mapActions, mapState, mapGetters } from 'vuex';
 import GlModal from '~/vue_shared/components/gl_modal.vue';
 import { modalTypes } from '../../constants';
 
@@ -15,6 +16,7 @@ export default {
   },
   computed: {
     ...mapState(['entryModal']),
+    ...mapGetters('fileTemplates', ['templateTypes']),
     entryName: {
       get() {
         if (this.entryModal.type === modalTypes.rename) {
@@ -31,7 +33,9 @@ export default {
       if (this.entryModal.type === modalTypes.tree) {
         return __('Create new directory');
       } else if (this.entryModal.type === modalTypes.rename) {
-        return this.entryModal.entry.type === modalTypes.tree ? __('Rename folder') : __('Rename file');
+        return this.entryModal.entry.type === modalTypes.tree
+          ? __('Rename folder')
+          : __('Rename file');
       }
 
       return __('Create new file');
@@ -40,7 +44,9 @@ export default {
       if (this.entryModal.type === modalTypes.tree) {
         return __('Create directory');
       } else if (this.entryModal.type === modalTypes.rename) {
-        return this.entryModal.entry.type === modalTypes.tree ? __('Rename folder') : __('Rename file');
+        return this.entryModal.entry.type === modalTypes.tree
+          ? __('Rename folder')
+          : __('Rename file');
       }
 
       return __('Create file');
@@ -60,6 +66,14 @@ export default {
           type: this.entryModal.type,
         });
       }
+    },
+    createFromTemplate(template) {
+      this.createTempEntry({
+        name: template.name,
+        type: this.entryModal.type,
+      });
+
+      $('#ide-new-entry').modal('toggle');
     },
     focusInput() {
       this.$refs.fieldName.focus();
@@ -84,16 +98,32 @@ export default {
     <div
       class="form-group row"
     >
-      <label class="label-bold col-form-label col-sm-3">
+      <label class="label-bold col-form-label col-sm-2">
         {{ __('Name') }}
       </label>
-      <div class="col-sm-9">
+      <div class="col-sm-10">
         <input
           ref="fieldName"
           v-model="entryName"
           type="text"
           class="form-control"
+          placeholder="/dir/file_name"
         />
+        <ul class="prepend-top-default list-inline">
+          <li
+            v-for="(template, index) in templateTypes"
+            :key="index"
+            class="list-inline-item"
+          >
+            <button
+              type="button"
+              class="btn btn-missing p-1 pr-2 pl-2"
+              @click="createFromTemplate(template)"
+            >
+              {{ template.name }}
+            </button>
+          </li>
+        </ul>
       </div>
     </div>
   </gl-modal>
