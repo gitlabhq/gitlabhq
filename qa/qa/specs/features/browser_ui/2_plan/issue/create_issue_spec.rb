jdbean/gitlab-ce

Improve QA scenarios contexts & descriptions consistency
Signed-off-by: Rémy Coutable <remy@rymai.me>

qa/qa/specs/features/browser/2_plan/issue/create_issue_spec.rb
@@ -1,18 +1,22 @@
+# frozen_string_literal: true
+
 module QA
-  describe 'creates issue', :core do
-    let(:issue_title) { 'issue title' }
+  context :plan, :core do
+    describe 'Issue creation' do
+      let(:issue_title) { 'issue title' }
 
-    it 'user creates issue' do
-      Runtime::Browser.visit(:gitlab, Page::Main::Login)
-      Page::Main::Login.act { sign_in_using_credentials }
+      it 'user creates an issue' do
+        Runtime::Browser.visit(:gitlab, Page::Main::Login)
+        Page::Main::Login.act { sign_in_using_credentials }
 
-      Factory::Resource::Issue.fabricate! do |issue|
-        issue.title = issue_title
+        Factory::Resource::Issue.fabricate! do |issue|
+          issue.title = issue_title
+        end
+
+        Page::Menu::Side.act { click_issues }
+
+        expect(page).to have_content(issue_title)
       end
-
-      Page::Menu::Side.act { click_issues }
-
-      expect(page).to have_content(issue_title)
     end
   end
 end

gitlabhq/gitlabhq

qa/qa/specs/features/browser_ui/2_plan/issue/create_issue_spec.rb
@@ -1,18 +1,22 @@
+# frozen_string_literal: true
+
 module QA
-  describe 'creates issue', :smoke do
-    let(:issue_title) { 'issue title' }
+  context :plan, :smoke do
+    describe 'Issue creation' do
+      let(:issue_title) { 'issue title' }
 
-    it 'user creates issue' do
-      Runtime::Browser.visit(:gitlab, Page::Main::Login)
-      Page::Main::Login.act { sign_in_using_credentials }
+      it 'user creates an issue' do
+        Runtime::Browser.visit(:gitlab, Page::Main::Login)
+        Page::Main::Login.act { sign_in_using_credentials }
 
-      Factory::Resource::Issue.fabricate! do |issue|
-        issue.title = issue_title
+        Factory::Resource::Issue.fabricate! do |issue|
+          issue.title = issue_title
+        end
+
+        Page::Menu::Side.act { click_issues }
+
+        expect(page).to have_content(issue_title)
       end
-
-      Page::Menu::Side.act { click_issues }
-
-      expect(page).to have_content(issue_title)
     end
   end
 end