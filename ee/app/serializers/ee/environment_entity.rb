ccc847db1e166bd17593b802bf5ee6ddd926e68c
Gilbert Roulot <groulot@gitlab.com>
2018-09-03 09:41:44 +0000

    Show secure results on environments page, backend

--- ee/app/serializers/ee/environment_entity.rb
+++ ee/app/serializers/ee/environment_entity.rb
@@ -7,6 +7,32 @@ module EE
       expose :logs_path, if: -> (*) { can_read_pod_logs? } do |environment|
         logs_project_environment_path(environment.project, environment)
       end
+
+      expose :secure_artifacts do
+        expose :sast_path, if: -> (*) { environment.last_pipeline&.expose_sast_data? } do |environment|
+          raw_project_build_artifacts_url(environment.project,
+                                          environment.last_pipeline.sast_artifact,
+                                          path: Ci::Build::SAST_FILE)
+        end
+
+        expose :dependency_scanning_path, if: -> (*) { environment.last_pipeline&.expose_dependency_scanning_data? } do |environment|
+          raw_project_build_artifacts_url(environment.project,
+                                          environment.last_pipeline.dependency_scanning_artifact,
+                                          path: Ci::Build::DEPENDENCY_SCANNING_FILE)
+        end
+
+        expose :dast_path, if: -> (*) { environment.last_pipeline&.expose_dast_data? } do |environment|
+          raw_project_build_artifacts_url(environment.project,
+                                          environment.last_pipeline.dast_artifact,
+                                          path: Ci::Build::DAST_FILE)
+        end
+
+        expose :container_scanning_path, if: -> (*) { environment.last_pipeline&.expose_container_scanning_data? } do |environment|
+          raw_project_build_artifacts_url(environment.project,
+                                          environment.last_pipeline.container_scanning_artifact,
+                                          path: Ci::Build::CONTAINER_SCANNING_FILE)
+        end
+      end
     end
 
     private
