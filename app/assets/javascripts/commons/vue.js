f8214f956883b44e9cc4835646b678ef1bc7a25c
Tim Zallmann <tzallmann@gitlab.com>
2018-09-03 18:05:36 +0200

    Fixed Left side comments

--- app/assets/javascripts/commons/vue.js
+++ app/assets/javascripts/commons/vue.js
@@ -1,8 +1,6 @@
 import Vue from 'vue';
 import '../vue_shared/vue_resource_interceptor';
 
-Vue.config.performance = true;
-
 if (process.env.NODE_ENV !== 'production') {
   Vue.config.productionTip = false;
 }
