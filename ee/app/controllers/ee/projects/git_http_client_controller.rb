a979aeacdf9eaaa7f3d677181b02413039ad9935
Ash McKenzie <amckenzie@gitlab.com>
2018-09-03 22:20:46 +1000

    Geo: LFS batch downloads handled by secondary

--- ee/app/controllers/ee/projects/git_http_client_controller.rb
+++ ee/app/controllers/ee/projects/git_http_client_controller.rb
@@ -3,12 +3,6 @@ module EE
     module GitHttpClientController
       extend ActiveSupport::Concern
 
-      ALLOWED_CONTROLLER_AND_ACTIONS = {
-        'git_http' => %w{git_receive_pack},
-        'lfs_api' => %w{batch},
-        'lfs_locks_api' => %w{create unlock verify}
-      }.freeze
-
       prepended do
         before_action do
           redirect_to(primary_full_url) if redirect?
@@ -20,35 +14,64 @@ module EE
       class RouteHelper
         attr_reader :controller_name, :action_name
 
-        def initialize(controller_name, action_name, params, allowed)
+        ALLOWED_CONTROLLER_AND_ACTIONS = {
+          'git_http' => %w{git_receive_pack},
+          'lfs_locks_api' => %w{create unlock verify}
+        }.freeze
+
+        def initialize(controller_name, action_name, service)
           @controller_name = controller_name
           @action_name = action_name
-          @params = params
-          @allowed = allowed
+          @service = service
         end
 
         def match?(c_name, a_name)
           controller_name == c_name && action_name == a_name
         end
 
-        def allowed_match?
-          !!allowed[controller_name]&.include?(action_name)
-        end
-
-        def service_or_action_name
-          action_name == 'info_refs' ? params[:service] : action_name.dasherize
+        def redirect?
+          git_receive_pack? || controller_and_action_match?
         end
 
         private
 
-        attr_reader :params, :allowed
+        attr_reader :service
+
+        def git_receive_pack?
+          service_or_action_name == 'git-receive-pack'
+        end
+
+        def controller_and_action_match?
+          !!ALLOWED_CONTROLLER_AND_ACTIONS[controller_name]&.include?(action_name)
+        end
+
+        def service_or_action_name
+          action_name == 'info_refs' ? service : action_name.dasherize
+        end
       end
 
       class GitLFSHelper
         MINIMUM_GIT_LFS_VERSION = '2.4.2'.freeze
 
-        def initialize(current_version)
+        def initialize(route_helper, operation, current_version)
+          @route_helper = route_helper
+          @operation = operation
           @current_version = current_version
+        end
+
+        def incorrect_version_response
+          {
+            json: { message: incorrect_version_message },
+            content_type: ::LfsRequest::CONTENT_TYPE,
+            status: 403
+          }
+        end
+
+        def redirect?
+          return false unless route_helper.match?('lfs_api', 'batch')
+          return true if upload?
+
+          false
         end
 
         def version_ok?
@@ -57,35 +80,31 @@ module EE
           ::Gitlab::VersionInfo.parse(current_version) >= wanted_version
         end
 
-        def incorrect_version_opts
-          {
-            json: { message: incorrect_version_message },
-            content_type: ::LfsRequest::CONTENT_TYPE,
-            status: 403
-          }
-        end
-
         private
 
-        attr_reader :current_version
-
-        def wanted_version
-          ::Gitlab::VersionInfo.parse(MINIMUM_GIT_LFS_VERSION)
-        end
+        attr_reader :route_helper, :operation, :current_version
 
         def incorrect_version_message
           translation = _("You need git-lfs version %{min_git_lfs_version} (or greater) to continue. Please visit https://git-lfs.github.com")
           translation % { min_git_lfs_version: MINIMUM_GIT_LFS_VERSION }
         end
+
+        def upload?
+          operation == 'upload'
+        end
+
+        def wanted_version
+          ::Gitlab::VersionInfo.parse(MINIMUM_GIT_LFS_VERSION)
+        end
       end
 
       def route_helper
-        @route_helper ||= RouteHelper.new(controller_name, action_name, params,
-                    ALLOWED_CONTROLLER_AND_ACTIONS)
+        @route_helper ||= RouteHelper.new(controller_name, action_name, params[:service])
       end
 
       def git_lfs_helper
-        @git_lfs_helper ||= GitLFSHelper.new(current_git_lfs_version)
+        # params[:operation] explained: https://github.com/git-lfs/git-lfs/blob/master/docs/api/batch.md#requests
+        @git_lfs_helper ||= GitLFSHelper.new(route_helper, params[:operation], request.headers['User-Agent'])
       end
 
       def request_fullpath_for_primary
@@ -97,23 +116,18 @@ module EE
         File.join(::Gitlab::Geo.primary_node.url, request_fullpath_for_primary)
       end
 
-      def current_git_lfs_version
-        request.headers['User-Agent']
-      end
-
       def redirect?
-        ::Gitlab::Geo.secondary_with_primary? && match? && !filtered?
-      end
+        return false unless ::Gitlab::Geo.secondary_with_primary?
+        return true if route_helper.redirect?
 
-      def match?
-        route_helper.service_or_action_name == 'git-receive-pack' ||
-          route_helper.allowed_match?
-      end
+        if git_lfs_helper.redirect?
+          return true if git_lfs_helper.version_ok?
 
-      def filtered?
-        if route_helper.match?('lfs_api', 'batch') && !git_lfs_helper.version_ok?
-          render(git_lfs_helper.incorrect_version_opts)
-          return true
+          # git-lfs 2.4.2 is really only required for requests that involve
+          # redirection, so we only render if it's an LFS upload operation
+          #
+          render(git_lfs_helper.incorrect_version_response)
+          return false
         end
 
         false
