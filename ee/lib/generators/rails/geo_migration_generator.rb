0088548f002ef8ca2de36263e3eeb0ad187fa6ad
Ash McKenzie <amckenzie@gitlab.com>
2018-09-04 13:46:47 +1000

    Use local generator templates for Geo DB migrations

--- ee/lib/generators/rails/geo_migration_generator.rb
+++ ee/lib/generators/rails/geo_migration_generator.rb
@@ -2,12 +2,8 @@ require 'rails/generators'
 require 'rails/generators/active_record'
 require 'rails/generators/active_record/migration/migration_generator'
 
-# TODO: this can be removed once we verify that this works as intended if we're
-# on Rails 5 since we use private Rails APIs.
-raise "Vendored ActiveRecord 5 code! Delete #{__FILE__}!" if ActiveRecord::VERSION::MAJOR >= 5
-
 class GeoMigrationGenerator < ActiveRecord::Generators::MigrationGenerator
-  source_root File.join(File.dirname(ActiveRecord::Generators::MigrationGenerator.instance_method(:create_migration_file).source_location.first), 'templates')
+  source_root File.join(Rails.root, 'generator_templates/active_record/migration')
 
   def create_migration_file
     set_local_assigns!
