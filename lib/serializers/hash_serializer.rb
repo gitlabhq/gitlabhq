c8d147780fa07fc51e11290070c0870f5cedcf62
Kamil Trzciński <ayufan@ayufan.eu>
2018-09-04 15:50:07 +0200

    Improve serializer

--- lib/serializers/hash_serializer.rb
+++ lib/serializers/hash_serializer.rb
@@ -1,17 +1,13 @@
 module Serializers
   # This serializer exports data as JSON,
   # but when loaded allows to access them with symbols
-  module HashSerializer
-    extend self
-
-    def dump(hash)
-      hash.to_json
+  class HashSerializer < ActiveRecord::Coders::JSON
+    def self.dump(obj)
+      ActiveSupport::JSON.encode(obj)
     end
 
-    def load(hash)
-      hash = JSON.load(hash) if hash.is_a?(String)
-
-      (hash || {}).with_indifferent_access
+    def self.load(json)
+      ActiveSupport::JSON.decode(json).with_indifferent_access unless json.nil?
     end
   end
 end
