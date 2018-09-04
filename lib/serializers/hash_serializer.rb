8e4be40d7080bd2be43bfc54d045fda623e6169b
Kamil Trzciński <ayufan@ayufan.eu>
2018-09-04 16:32:34 +0200

    Fix serializer

--- lib/serializers/hash_serializer.rb
+++ lib/serializers/hash_serializer.rb
@@ -1,13 +1,23 @@
 module Serializers
   # This serializer exports data as JSON,
   # but when loaded allows to access them with symbols
-  class HashSerializer < ActiveRecord::Coders::JSON
+  class HashSerializer
     def self.dump(obj)
       ActiveSupport::JSON.encode(obj)
     end
 
     def self.load(json)
-      ActiveSupport::JSON.decode(json).with_indifferent_access unless json.nil?
+      self.deep_indifferent_access(ActiveSupport::JSON.decode(json))
+    end
+
+    def self.deep_indifferent_access(data)
+      if data.is_a?(Array)
+        data.map { |item| self.deep_indifferent_access(item) }
+      elsif data.is_a?(Hash)
+        data.with_indifferent_access
+      else
+        data
+      end
     end
   end
 end
