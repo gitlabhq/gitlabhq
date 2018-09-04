b138b0c94437b51fc156b120dff019c99fef9fe1
Kamil Trzciński <ayufan@ayufan.eu>
2018-09-04 14:56:21 +0200

    Fix mysql2 query size

---
+++ db/migrate/20180831164904_fix_prometheus_metric_query_limits.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# See http://doc.gitlab.com/ce/development/migration_style_guide.html
+# for more information on how to write migrations for GitLab.
+require Rails.root.join('db/migrate/prometheus_metrics_limits_to_mysql')
+
+class FixPrometheusMetricQueryLimits < ActiveRecord::Migration
+  include Gitlab::Database::MigrationHelpers
+
+  def up
+    PrometheusMetricsLimitsToMysql.new.up
+  end
+
+  def down
+    # no-op
+  end
+end
