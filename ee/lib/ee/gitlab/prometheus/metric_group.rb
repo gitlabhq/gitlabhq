40a675ab8fd952f056d975cb4369bfbed18afdc3
Kamil Trzciński <ayufan@ayufan.eu>
2018-09-04 23:09:19 +0200

    Fix MetricGroup overwrites

---
+++ ee/lib/ee/gitlab/prometheus/metric_group.rb
@@ -0,0 +1,23 @@
+module EE
+  module Gitlab
+    module Prometheus
+      module MetricGroup
+        module ClassMethods
+          def custom_metrics(project)
+            project.prometheus_metrics.all.group_by(&:group_title).map do |name, metrics|
+              ::Gitlab::Prometheus::MetricGroup.new(name: name, priority: 0, metrics: metrics.map(&:to_query_metric))
+            end
+          end
+
+          def for_project(project)
+            super + custom_metrics(project)
+          end
+        end
+
+        def self.prepended(base)
+          base.singleton_class.prepend ClassMethods
+        end
+      end
+    end
+  end
+end
