40181bfcab73b37e6b32fbaf8758aaeaa5157e2b
Kamil Trzciński <ayufan@ayufan.eu>
2018-09-05 21:05:08 +0200

    Fix concurrent index problem

---
+++ db/migrate/20180831164909_add_index_for_identifier_to_prometheus_metric.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class AddIdentifierToPrometheusMetric < ActiveRecord::Migration
+  include Gitlab::Database::MigrationHelpers
+
+  DOWNTIME = false
+
+  disable_ddl_transaction!
+
+  def up
+    add_concurrent_index :prometheus_metrics, :identifier, unique: true
+  end
+
+  def down
+    remove_concurrent_index :prometheus_metrics, :identifier, unique: true
+  end
+end
