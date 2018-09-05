5951331a2dde2b2cc9655c05480dffe9503962f1
Kamil Trzciński <ayufan@ayufan.eu>
2018-09-05 17:21:34 +0200

    Code-style long line

--- ee/lib/ee/gitlab/prometheus/metric_group.rb
+++ ee/lib/ee/gitlab/prometheus/metric_group.rb
@@ -5,7 +5,8 @@ module EE
         module ClassMethods
           def custom_metrics(project)
             project.prometheus_metrics.all.group_by(&:group_title).map do |name, metrics|
-              ::Gitlab::Prometheus::MetricGroup.new(name: name, priority: 0, metrics: metrics.map(&:to_query_metric))
+              ::Gitlab::Prometheus::MetricGroup.new(
+                name: name, priority: 0, metrics: metrics.map(&:to_query_metric))
             end
           end
 
