f4f84b1249fd7a819e3b596ccead618016c3aeca
André Luís <aluis@gitlab.com>
2018-09-05 16:40:01 +0100

    Display Ready to Merge widget when MWPS fails

--- app/assets/javascripts/vue_merge_request_widget/components/states/mr_widget_auto_merge_failed.vue
+++ app/assets/javascripts/vue_merge_request_widget/components/states/mr_widget_auto_merge_failed.vue
@@ -1,18 +1,28 @@
 <script>
   import loadingIcon from '~/vue_shared/components/loading_icon.vue';
+  import callout from '~/vue_shared/components/callout.vue';
+  import { s__ } from '~/locale';
   import eventHub from '../../event_hub';
   import statusIcon from '../mr_widget_status_icon.vue';
+  import { ReadyToMergeState } from '../../dependencies';
 
   export default {
     name: 'MRWidgetAutoMergeFailed',
     components: {
       statusIcon,
       loadingIcon,
+      callout,
+      'mr-widget-ready-to-merge': ReadyToMergeState,
     },
     props: {
       mr: {
         type: Object,
         required: true,
+      },
+      service: {
+        type: Object,
+        required: false,
+        default: () => {},
       },
     },
     data() {
@@ -27,29 +37,23 @@
           this.isRefreshing = false;
         });
       },
+      calloutMessage() {
+        return this.mr.mergeError ? `${this.mr.mergeError}.` : s__('mrWidget|This merge request failed to be merged automatically');
+      },
     },
   };
 </script>
 <template>
-  <div class="mr-widget-body media">
-    <status-icon status="warning" />
-    <div class="media-body space-children">
-      <span class="bold">
-        <template v-if="mr.mergeError">{{ mr.mergeError }}.</template>
-        {{ s__("mrWidget|This merge request failed to be merged automatically") }}
-      </span>
-      <button
-        :disabled="isRefreshing"
-        type="button"
-        class="btn btn-sm btn-default"
-        @click="refreshWidget"
-      >
-        <loading-icon
-          v-if="isRefreshing"
-          :inline="true"
-        />
-        {{ s__("mrWidget|Refresh") }}
-      </button>
+  <div class="mr-widget-body">
+    <mr-widget-ready-to-merge
+      :mr="mr"
+      :service="service"
+    />
+    <div class="mr-widget-callout media mt-2">
+      <callout
+        :message="calloutMessage()"
+        category="danger"
+      />
     </div>
   </div>
 </template>
