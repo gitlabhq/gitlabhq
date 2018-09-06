35491ba76ca556c75f34fe877f3d5f4b6e1a5bbb
Dennis Tang <dennis@dennistang.net>
2018-09-06 09:37:19 +0000

    Update app/assets/javascripts/pages/projects/project.js

--- app/assets/javascripts/pages/projects/project.js
+++ app/assets/javascripts/pages/projects/project.js
@@ -37,14 +37,11 @@ export default class Project {
         $label.text(activeText);
       });
 
-<<<<<<< HEAD
       $('#modal-geo-info').data({
         cloneUrlSecondary: $this.attr('href'),
         cloneUrlPrimary: $this.data('primaryUrl') || '',
       });
 
-=======
->>>>>>> upstream/master
       $projectCloneField.val(url);
       $('.js-git-empty .js-clone').text(url);
     });
