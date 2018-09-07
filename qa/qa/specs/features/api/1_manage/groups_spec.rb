98a1efbbaf380ac8dafaf79fa687db3f64c8e205
sliaquat <sliaquat@gitlab.com>
2018-09-07 16:13:42 +0500

    Complete scenario

--- qa/qa/specs/features/api/1_manage/groups_spec.rb
+++ qa/qa/specs/features/api/1_manage/groups_spec.rb
@@ -12,54 +12,45 @@ module QA
     let(:random_string) { SecureRandom.hex(8) }
     let(:group_name) { "api-group-name-#{random_string}" }
     let(:group_path) { "api-group-path-#{random_string}" }
+    let(:description) { 'This is a test group' }
 
-    it 'user creates, updates and deletes a group' do
+    let(:updated_group_name) { "updated-#{group_name}" }
+    let(:updated_group_path) { "updated-#{group_path}" }
+    let(:updated_description) { "updated-#{description}" }
+
+
+    it 'Creates, updates and deletes a group' do
       @api_client = Runtime::API::Client.new(:gitlab, personal_access_token: @personal_access_token)
 
-      groups_request = Runtime::API::Request.new(@api_client, "/groups")
+      create_fetch_groups_request = Runtime::API::Request.new(@api_client, "/groups")
 
-      description = 'This is a test group'
+      expected_response = {
+          name: group_name,
+          path: group_path,
+          description: description,
+          full_name: group_name,
+          full_path: group_path
+      }
 
       # Create
-      post groups_request.url, name: group_name, path: group_path, description: description
+      post create_fetch_groups_request.url, name: group_name, path: group_path, description: description
       expect_status(201)
 
-      expect(json_body).to match(
-        a_hash_including(
-          name: group_name,
-          path: group_path,
-          description: description,
-          visibility: 'private',
-          lfs_enabled: true,
-          request_access_enabled: false,
-          full_name: group_name,
-          full_path: group_path)
-      )
+      expect(json_body).to match a_hash_including(expected_response)
 
-      created_group_id = json_body[:id]
+      expected_response[:id] = created_group_id = json_body[:id]
 
-      get groups_request.url
+      get create_fetch_groups_request.url
       expect_status(200)
-      expect(json_body).to include(
-        a_hash_including(
-          id: created_group_id,
-          name: group_name,
-          path: group_path,
-          description: description,
-          visibility: 'private',
-          lfs_enabled: true,
-          request_access_enabled: false,
-          full_name: group_name,
-          full_path: group_path)
-      )
+      expect(json_body).to include a_hash_including(expected_response)
 
       # Update
-      update_groups_request = Runtime::API::Request.new(@api_client, "/groups/#{created_group_id}")
+      update_delete_groups_request = Runtime::API::Request.new(@api_client, "/groups/#{created_group_id}")
 
-      put update_groups_request.url,
+      put update_delete_groups_request.url,
           name: updated_group_name,
           path: updated_group_path,
-          description: updated_group_description
+          description: updated_description
 
       expect_status(200)
 
@@ -68,13 +59,18 @@ module QA
           id: created_group_id,
           name: updated_group_name,
           path: updated_group_path,
-          description: updated_group_description,
-          visibility: 'private',
-          lfs_enabled: true,
-          request_access_enabled: false,
+          description: updated_description,
           full_name: updated_group_name,
           full_path: updated_group_path)
       )
+
+      # Delete
+      delete update_delete_groups_request.url
+      expect_status(202)
+
+      get create_fetch_groups_request.url
+      expect_status(200)
+      expect(json_body).to_not include a_hash_including(id: created_group_id)
     end
   end
 end
