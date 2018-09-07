fe487fe2f2e6f07b95cf92f8b0fa786425e62dae
Lukas Eipert <leipert@gitlab.com>
2018-09-07 13:20:06 +0200

    check yarn.lock for duplicate entries

--- scripts/frontend/postinstall.js
+++ scripts/frontend/postinstall.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const chalk = require('chalk');
 
 // check that fsevents is available if we're on macOS
@@ -15,6 +17,56 @@ if (process.platform === 'darwin') {
         You can try installing again with \`${chalk.cyan('yarn install --force')}\`
       `)
     );
+    process.exit(1);
+  }
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  const lockfileParser = require('@yarnpkg/lockfile');
+
+  // check that certain packages are only listed once in the yarn.lock
+  const packagesToCheck = ['@gitlab-org/gitlab-svgs', 'bootstrap'];
+  const file = fs.readFileSync(path.join(__dirname, '../../yarn.lock'), 'utf8');
+  const packages = lockfileParser.parse(file);
+
+  if (packages.type !== 'success') {
+    console.error(`${chalk.red('error')} Could not parse 'yarn.lock'. Please make sure it exists`);
+    process.exit(1);
+  }
+
+  function checkUniqueYarnLockEntry(hasError, packageName) {
+    const resolved = Object.entries(packages.object).reduce((result, [name, value]) => {
+      if (name.replace(/"/g, '').startsWith(`${packageName}@`)) {
+        const resolvedUrl = value.resolved;
+
+        if (!result.includes(resolvedUrl)) {
+          result.push(resolvedUrl);
+        }
+      }
+      return result;
+    }, []);
+
+    if (resolved.length !== 1) {
+      console.error(
+        chalk.red(`
+        The dependency ${packageName} has ${resolved.length} entries in 'yarn.lock'.
+        It is supposed to have exactly one entry, please fix that by:
+
+           1. Delete all lines starting with '${packageName}@' from the yarn.lock
+           2. Re-run \`${chalk.cyan('yarn install')}\`
+           3. Commit changes to yarn.lock
+      `)
+      );
+      return true;
+    }
+
+    return hasError;
+  }
+
+  const hasError = packagesToCheck.reduce(checkUniqueYarnLockEntry, false);
+
+  if (hasError) {
+    console.error(`${chalk.red('error')} Dependency postinstall check failed.`);
     process.exit(1);
   }
 }
