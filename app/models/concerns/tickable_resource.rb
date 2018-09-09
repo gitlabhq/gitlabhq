3202e05ffb6ebda7cb15981c06bb9194d2735b60
Kamil Trzciński <ayufan@ayufan.eu>
2018-09-09 11:22:55 +0200

    Fix TickableResource

--- app/models/concerns/tickable_resource.rb
+++ app/models/concerns/tickable_resource.rb
@@ -10,37 +10,37 @@ module TickableResource
 
     private # rubocop:disable Lint/UselessAccessModifier
 
-    def add_tickable_resource(name, expire:, notification_channel: Gitlab::Workhorse::NOTIFICATION_CHANNEL, &blk)
+    def add_tickable_resource(field, expire:, notification_channel: Gitlab::Workhorse::NOTIFICATION_CHANNEL, &blk)
       @tickable_resource_fields = [] unless @tickable_resource_fields
-      @tickable_resource_fields << name
+      @tickable_resource_fields << field
 
-      define_singleton_method("#{token_field}_key") do
+      define_singleton_method("#{field}_key") do
         blk.call
       end
 
-      define_singleton_method("tick_#{token_field}") do
+      define_singleton_method("tick_#{field}") do
         SecureRandom.hex.tap do |new_update|
           ::Gitlab::Workhorse.set_key_and_notify(
-            public_send("#{token_field}_key"), new_update,
+            public_send("#{field}_key"), new_update,
             expire: expire, overwrite: true)
         end
       end
 
-      define_singleton_method("ensure_#{token_field}_value") do
+      define_singleton_method("ensure_#{field}_value") do
         new_value = SecureRandom.hex
         ::Gitlab::Workhorse.set_key_and_notify(
-          public_send("#{token_field}_key"), new_value,
+          public_send("#{field}_key"), new_value,
           expire: expire, overwrite: false)
       end
 
-      define_singleton_method("clear_#{token_field}") do
+      define_singleton_method("clear_#{field}") do
         Gitlab::Redis::Queues.with do |redis|
-          redis.del(public_send("#{token_field}_key"))
+          redis.del(public_send("#{field}_key"))
         end
       end
 
-      define_singleton_method("#{token_field}_value_latest?") do |value|
-        public_send("ensure_#{token_field}_value") == value if value.present?
+      define_singleton_method("#{field}_value_latest?") do |value|
+        public_send("ensure_#{field}_value") == value if value.present?
       end
     end
   end
