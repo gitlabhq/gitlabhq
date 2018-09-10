28e80dbcc5882bb4635941b18b69cba696061ade
Mark Lapierre <mlapierre@gitlab.com>
2018-09-10 15:01:55 -0400

    Enable signing in as admin when adding a license
    Adds the ability to use admin credentials to add a license so that when
    testing an LDAP user on EE the LDAP user does not have to be an admin.
    
    Admin credentials default to GDK's root user. Can be overriden via
    ADMIN_USERNAME and ADMIN_PASSWORD environment variables.

--- qa/qa/runtime/user.rb
+++ qa/qa/runtime/user.rb
@@ -7,24 +7,36 @@ module QA
         'root'
       end
 
+      def default_password
+        '5iveL!fe'
+      end
+
       def username
         Runtime::Env.user_username || default_username
       end
 
       def password
-        Runtime::Env.user_password || '5iveL!fe'
+        Runtime::Env.user_password || default_password
       end
 
       def ldap_user?
-        Runtime::Env.user_type == 'ldap'
+        Runtime::Env.ldap_username && Runtime::Env.ldap_password
       end
 
       def ldap_username
-        Runtime::Env.ldap_username || name
+        Runtime::Env.ldap_username || username
       end
 
       def ldap_password
         Runtime::Env.ldap_password || password
+      end
+
+      def admin_username
+        Runtime::Env.admin_username || default_username
+      end
+
+      def admin_password
+        Runtime::Env.admin_password || default_password
       end
     end
   end
