81122e7ac797103f70cd07fd3594c80e6eea68e4
Lukas Eipert <leipert@gitlab.com>
2018-09-10 12:26:42 +0200

    remove unnecessary help text from container scanning results

--- app/assets/javascripts/reports/components/issues_list.vue
+++ app/assets/javascripts/reports/components/issues_list.vue
@@ -6,9 +6,6 @@ import {
   STATUS_NEUTRAL,
 } from '~/reports/constants';
 
-import { componentNames } from 'ee/vue_shared/components/reports/issue_body';
-import SastContainerInfo from 'ee/vue_shared/security_reports/components/sast_container_info.vue';
-
 /**
  * Renders block of issues
  */
@@ -16,9 +13,7 @@ import SastContainerInfo from 'ee/vue_shared/security_reports/components/sast_co
 export default {
   components: {
     IssuesBlock,
-    SastContainerInfo,
   },
-  componentNames,
   success: STATUS_SUCCESS,
   failed: STATUS_FAILED,
   neutral: STATUS_NEUTRAL,
@@ -53,7 +48,7 @@ export default {
 </script>
 <template>
   <div class="report-block-container">
-    <sast-container-info v-if="component === $options.componentNames.SastContainerIssueBody" />
+
     <issues-block
       v-if="newIssues.length"
       :component="component"
