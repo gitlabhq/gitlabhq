106640e031c92813803c98674e0304a91ce07db4
Adriel Santiago <adriel@gitlab.com>
2018-09-10 15:36:09 -0400

    Use GitLab ui pagination component for groups

--- app/assets/javascripts/commons/gitlab_ui.js
+++ app/assets/javascripts/commons/gitlab_ui.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import Pagination from '@gitlab-org/gitlab-ui/dist/components/base/pagination';
 import progressBar from '@gitlab-org/gitlab-ui/dist/components/base/progress_bar';
 import modal from '@gitlab-org/gitlab-ui/dist/components/base/modal';
 import loadingIcon from '@gitlab-org/gitlab-ui/dist/components/base/loading_icon';
@@ -6,6 +7,7 @@ import loadingIcon from '@gitlab-org/gitlab-ui/dist/components/base/loading_icon
 import dModal from '@gitlab-org/gitlab-ui/dist/directives/modal';
 import dTooltip from '@gitlab-org/gitlab-ui/dist/directives/tooltip';
 
+Vue.component('gl-pagination', Pagination);
 Vue.component('gl-progress-bar', progressBar);
 Vue.component('gl-ui-modal', modal);
 Vue.component('gl-loading-icon', loadingIcon);
