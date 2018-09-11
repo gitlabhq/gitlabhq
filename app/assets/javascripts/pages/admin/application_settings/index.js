b4c7e7225a4c48a8a78344a2de2dd585cfd62027
Martin Wortschack <mwortschack@gitlab.com>
2018-09-11 08:50:35 +0200

    load usage ping script on admin settings subpage only

--- app/assets/javascripts/pages/admin/application_settings/index.js
+++ app/assets/javascripts/pages/admin/application_settings/index.js
@@ -1,13 +1,8 @@
 import initSettingsPanels from '~/settings_panels';
 import projectSelect from '~/project_select';
-import UsagePingPayload from './usage_ping_payload';
 
 document.addEventListener('DOMContentLoaded', () => {
   // Initialize expandable settings panels
   initSettingsPanels();
   projectSelect();
-  new UsagePingPayload(
-    document.querySelector('.js-usage-ping-payload-trigger'),
-    document.querySelector('.js-usage-ping-payload'),
-  ).init();
 });
