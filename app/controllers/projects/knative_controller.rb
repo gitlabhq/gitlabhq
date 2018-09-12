88225ea3085d01f083a0ce693c4b87a7f9514f8d
Kamil Trzciński <ayufan@ayufan.eu>
2018-09-12 14:43:06 +0200

    Provide simple interface for Knative

--- app/controllers/projects/knative_controller.rb
+++ app/controllers/projects/knative_controller.rb
@@ -1,8 +1,6 @@
-class Projects::FeatureFlagsController < Projects::ApplicationController
+class Projects::KnativeController < Projects::ApplicationController
   respond_to :html
 
   def index
-    @feature_flags = project.project_feature_flags
-    @unleash_instanceid = project.project_feature_flags_access_tokens.first&.token || project.project_feature_flags_access_tokens.create!.token
   end
 end
