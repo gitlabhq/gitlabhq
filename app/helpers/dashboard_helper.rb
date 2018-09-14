c58a4ea81e6969594050bc7c0d6b548fd7112e16
Luke Bennett <lbennett@gitlab.com>
2018-09-14 03:09:32 +0100

    Lint

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -22,9 +22,9 @@ module DashboardHelper
   def controller_action_to_child_dashboards(controller = controller_name, action = action_name)
     case "#{controller}##{action}"
     when 'projects#index', 'root#index', 'projects#starred', 'projects#trending'
-      %w(projects, 'stars)
+      %w(projects stars)
     when 'dashboard#activity'
-      %w(starred_project_activity, 'project_activity)
+      %w(starred_project_activity project_activity)
     when 'groups#index'
       %w(groups)
     when 'todos#index'
@@ -38,7 +38,7 @@ module DashboardHelper
     end
   end
 
-  def is_default_dashboard?(user = current_user)
+  def user_default_dashboard?(user = current_user)
     controller_action_to_child_dashboards.any? {|dashboard| dashboard == user.dashboard }
   end
 
