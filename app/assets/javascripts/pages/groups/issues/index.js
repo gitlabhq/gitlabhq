737a62f6d228bf0679aa9f69b5b0ad7cd27c61d8
Grzegorz Bizon <grzegorz@gitlab.com>
2018-09-14 10:29:58 +0000

    Resolve conflicts in index.js

--- app/assets/javascripts/pages/groups/issues/index.js
+++ app/assets/javascripts/pages/groups/issues/index.js
@@ -1,21 +1,13 @@
 import projectSelect from '~/project_select';
 import initFilteredSearch from '~/pages/search/init_filtered_search';
 import { FILTERED_SEARCH } from '~/pages/constants';
-<<<<<<< HEAD
-import IssuesFilteredSearchTokenKeysEE from 'ee/filtered_search/issues_filtered_search_token_keys';
-=======
-import IssuableFilteredSearchTokenKeys from '~/filtered_search/issuable_filtered_search_token_keys';
->>>>>>> upstream/master
+import IssuableFilteredSearchTokenKeysEE from 'ee/filtered_search/issuable_filtered_search_token_keys';
 
 document.addEventListener('DOMContentLoaded', () => {
   initFilteredSearch({
     page: FILTERED_SEARCH.ISSUES,
     isGroupDecendent: true,
-<<<<<<< HEAD
-    filteredSearchTokenKeys: IssuesFilteredSearchTokenKeysEE,
-=======
-    filteredSearchTokenKeys: IssuableFilteredSearchTokenKeys,
->>>>>>> upstream/master
+    filteredSearchTokenKeys: IssuableFilteredSearchTokenKeysEE,
   });
   projectSelect();
 });
