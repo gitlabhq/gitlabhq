71555ac56724af918572b439db1f7f6c0757a231
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2018-09-14 12:40:54 +0200

    Resolve conflicts in groups/merge_requests/index.js and remove outdated file

--- app/assets/javascripts/pages/groups/merge_requests/index.js
+++ app/assets/javascripts/pages/groups/merge_requests/index.js
@@ -1,21 +1,13 @@
 import projectSelect from '~/project_select';
 import initFilteredSearch from '~/pages/search/init_filtered_search';
-<<<<<<< HEAD
-import IssuesFilteredSearchTokenKeys from '~/filtered_search/issues_filtered_search_token_keys';
-=======
 import IssuableFilteredSearchTokenKeys from '~/filtered_search/issuable_filtered_search_token_keys';
->>>>>>> upstream/master
 import { FILTERED_SEARCH } from '~/pages/constants';
 
 document.addEventListener('DOMContentLoaded', () => {
   initFilteredSearch({
     page: FILTERED_SEARCH.MERGE_REQUESTS,
     isGroupDecendent: true,
-<<<<<<< HEAD
-    filteredSearchTokenKeys: IssuesFilteredSearchTokenKeys,
-=======
     filteredSearchTokenKeys: IssuableFilteredSearchTokenKeys,
->>>>>>> upstream/master
   });
   projectSelect();
 });
