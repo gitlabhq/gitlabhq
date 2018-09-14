50a62b479e354efce5d85dd46bc25033cfd066f3
Brett Walker <bwalker@gitlab.com>
2018-09-14 14:56:56 -0500

    Filter out archived projects from issue/mr count
    Pass the 'non_archived' flag to finder methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1176,13 +1176,13 @@ class User < ActiveRecord::Base
 
   def assigned_open_merge_requests_count(force: false)
     Rails.cache.fetch(['users', id, 'assigned_open_merge_requests_count'], force: force, expires_in: 20.minutes) do
-      MergeRequestsFinder.new(self, assignee_id: self.id, state: 'opened').execute.count
+      MergeRequestsFinder.new(self, assignee_id: self.id, state: 'opened', non_archived: true).execute.count
     end
   end
 
   def assigned_open_issues_count(force: false)
     Rails.cache.fetch(['users', id, 'assigned_open_issues_count'], force: force, expires_in: 20.minutes) do
-      IssuesFinder.new(self, assignee_id: self.id, state: 'opened').execute.count
+      IssuesFinder.new(self, assignee_id: self.id, state: 'opened', non_archived: true).execute.count
     end
   end
 
