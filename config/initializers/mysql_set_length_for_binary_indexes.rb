e00b815efe08a91b006128c0506d9756ae0907fa
Jan Provaznik <jprovaznik@gitlab.com>
2018-09-19 15:00:19 +0200

    Fix index for mysql adapter
    * similar to rails 5 it assures that index length is set for blob
      columns also in rails 4
    * it also ignores multiple definitions of indexes for mysql, for some
      tables we define multiple indexes on the same set of columns, but with
      different parameters (opclasses, where), these are not supported by
      mysql adapter so the second definition of index is skipped

--- config/initializers/mysql_set_length_for_binary_indexes.rb
+++ config/initializers/mysql_set_length_for_binary_indexes.rb
@@ -24,28 +24,46 @@ if defined?(ActiveRecord::ConnectionAdapters::Mysql2Adapter)
   ActiveRecord::ConnectionAdapters::Mysql2Adapter.send(:prepend, MysqlSetLengthForBinaryIndex)
 end
 
-if Gitlab.rails5?
-  module MysqlSetLengthForBinaryIndexAndIgnorePostgresOptionsForSchema
-    # This method is used in Rails 5 schema loading as t.index
-    def index(column_names, options = {})
-      options[:length] ||= {}
-      Array(column_names).each do |column_name|
-        column = columns.find { |c| c.name == column_name }
+module MysqlSetLengthForBinaryIndexAndIgnorePostgresOptionsForSchema
+  # This method is used in Rails 5 schema loading as t.index
+  def index(column_names, options = {})
+    # Ignore indexes that use opclasses,
+    # also see config/initializers/mysql_ignore_postgresql_options.rb
+    if options[:opclasses]
+      warn "WARNING: index on columns #{column_names} uses unsupported option, skipping."
+      return
+    end
 
-        if column&.type == :binary
-          options[:length][column_name] = 20
-        end
-      end
+    # when running rails 4 with rails 5 schema, rails 4 doesn't support multiple
+    # indexes on the same set of columns. Mysql doesn't support partial indexes, so if
+    # an index already exists and we add another index, skip it if it's partial:
+    # see https://gitlab.com/gitlab-org/gitlab-ce/merge_requests/21492#note_102821326
+    if !Gitlab.rails5? && indexes[column_names] && options[:where]
+      warn "WARNING: index on columns #{column_names} already exists and partial index is not supported, skipping."
+      return
+    end
 
-      # Ignore indexes that use opclasses,
-      # also see config/initializers/mysql_ignore_postgresql_options.rb
-      unless options[:opclasses]
-        super(column_names, options)
+    options[:length] ||= {}
+    Array(column_names).each do |column_name|
+      column = columns.find { |c| c.name == column_name }
+
+      if column&.type == :binary
+        options[:length][column_name] = 20
       end
     end
-  end
 
+    super(column_names, options)
+  end
+end
+
+def mysql_adapter?
+  defined?(ActiveRecord::ConnectionAdapters::Mysql2Adapter) && ActiveRecord::Base.connection.is_a?(ActiveRecord::ConnectionAdapters::Mysql2Adapter)
+end
+
+if Gitlab.rails5?
   if defined?(ActiveRecord::ConnectionAdapters::MySQL::TableDefinition)
     ActiveRecord::ConnectionAdapters::MySQL::TableDefinition.send(:prepend, MysqlSetLengthForBinaryIndexAndIgnorePostgresOptionsForSchema)
   end
+elsif mysql_adapter? && defined?(ActiveRecord::ConnectionAdapters::TableDefinition)
+  ActiveRecord::ConnectionAdapters::TableDefinition.send(:prepend, MysqlSetLengthForBinaryIndexAndIgnorePostgresOptionsForSchema)
 end
