6abb5030d817acfab23bd3b02184e24f1ae0647c
Felipe Artur <felipefac@gmail.com>
2018-09-19 14:59:25 -0300

    Fix rubocop

--- db/migrate/20180917200829_create_user_preferences.rb
+++ db/migrate/20180917200829_create_user_preferences.rb
@@ -11,15 +11,19 @@ class CreateUserPreferences < ActiveRecord::Migration
     create_table :user_preferences do |t|
       t.references :user,
                    null: false,
-                   index: { unique: true }, foreign_key: { on_delete: :cascade }
+                   index: { unique: true },
+                   foreign_key: { on_delete: :cascade }
 
-      t.integer :issue_notes_filter, index: true,
-        default: UserPreference::NOTES_FILTERS[:all_activity],
-        null: false, limit: 2
+      t.integer :issue_notes_filter,
+                index: true,
+                default: UserPreference::NOTES_FILTERS[:all_activity],
+                null: false, limit: 2
 
-      t.integer :merge_request_notes_filter, index: true,
-        default: UserPreference::NOTES_FILTERS[:all_activity],
-        null: false, limit: 2
+      t.integer :merge_request_notes_filter,
+                index: true,
+                default: UserPreference::NOTES_FILTERS[:all_activity],
+                null: false,
+                limit: 2
 
       t.timestamps_with_timezone null: false
     end
