4423f26270acf71aeac019ee763f67dad228da9b
Brett Walker <bwalker@gitlab.com>
2018-09-20 16:20:37 -0500

    Filter system notes with public and private cross references

--- ee/app/models/ee/system_note_metadata.rb
+++ ee/app/models/ee/system_note_metadata.rb
@@ -8,9 +8,20 @@ module EE
       epic_issue_moved issue_changed_epic epic_date_changed
     ].freeze
 
+    EE_TYPES_WITH_CROSS_REFERENCES = %w[
+      relate unrelate
+      epic_issue_added issue_added_to_epic epic_issue_removed issue_removed_from_epic
+      epic_issue_moved issue_changed_epic
+    ].freeze
+
     override :icon_types
     def icon_types
       @icon_types ||= (super + EE_ICON_TYPES).freeze
+    end
+
+    override :cross_reference_types
+    def cross_reference_types
+      @cross_reference_types ||= (super + EE_TYPES_WITH_CROSS_REFERENCES).freeze
     end
   end
 end
