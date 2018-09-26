f3901842493c58faba71ad0812bf3102790a1b9f
Paul Slaughter <pslaughter@gitlab.com>
2018-09-26 08:28:50 +0000

    Resolve "Commit details are not displayed when reviewing an MR commit by commit"

--- spec/serializers/commit_entity_spec.rb
+++ spec/serializers/commit_entity_spec.rb
@@ -51,4 +51,37 @@ describe CommitEntity do
   it 'exposes gravatar url that belongs to author' do
     expect(subject.fetch(:author_gravatar_url)).to match /gravatar/
   end
+
+  context 'when type is not set' do
+    it 'does not expose extra properties' do
+      expect(subject).not_to include(:description_html)
+      expect(subject).not_to include(:title_html)
+    end
+  end
+
+  context 'when type is "full"' do
+    let(:entity) do
+      described_class.new(commit, request: request, type: :full)
+    end
+
+    it 'exposes extra properties' do
+      expect(subject).to include(:description_html)
+      expect(subject).to include(:title_html)
+      expect(subject.fetch(:description_html)).not_to be_nil
+      expect(subject.fetch(:title_html)).not_to be_nil
+    end
+  end
+
+  context 'when commit_url_params is set' do
+    let(:entity) do
+      params = { merge_request_iid: 3 }
+
+      described_class.new(commit, request: request, commit_url_params: params)
+    end
+
+    it 'adds commit_url_params to url and path' do
+      expect(subject[:commit_path]).to include "?merge_request_iid=3"
+      expect(subject[:commit_url]).to include "?merge_request_iid=3"
+    end
+  end
 end
