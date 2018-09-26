e90435f1f56025e288e3d05faa8e488f56a5b64c
Jose Vargas <jvargas@gitlab.com>
2018-09-26 16:52:30 -0500

    Add gl-alert UI component

--- app/assets/javascripts/issue_show/components/locked_warning.vue
+++ app/assets/javascripts/issue_show/components/locked_warning.vue
@@ -1,5 +1,10 @@
 <script>
+  import { Alert } from '@gitlab-org/gitlab-ui';
+
   export default {
+    components: {
+      'gl-alert': Alert,
+    },
     computed: {
       currentPath() {
         return window.location.pathname;
@@ -9,12 +14,14 @@
 </script>
 
 <template>
-  <div class="alert alert-danger">
+  <gl-alert
+    variant="danger"
+  >
     Someone edited the issue at the same time you did. Please check out
     <a
       :href="currentPath"
       target="_blank"
       rel="nofollow">the issue</a>
     and make sure your changes will not unintentionally remove theirs.
-  </div>
+  </gl-alert>
 </template>
