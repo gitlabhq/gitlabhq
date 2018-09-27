8969874a30d64434942187d0cff5896c7adab160
Paul Slaughter <pslaughter@gitlab.com>
2018-09-26 22:59:02 -0500

    Refactor IDE to allow RightPane extension
    **Why?**
    - This is needed by the Web Terminal EE feature.
      https://gitlab.com/gitlab-org/gitlab-ee/issues/5426
    
    **Notes:**
    - RightPane component tabs is now data driven.

---
+++ ee/app/assets/javascripts/ide/components/panes/right.vue
@@ -0,0 +1,20 @@
+<script>
+import RightPane from '~/ide/components/panes/right.vue';
+
+export default {
+  name: 'EERightPane',
+  components: {
+    RightPane,
+  },
+  computed: {
+    extensionTabs() {
+      // This is empty for now, but it will be used in: https://gitlab.com/gitlab-org/gitlab-ee/issues/5426
+      return [];
+    },
+  },
+};
+</script>
+
+<template>
+  <right-pane :extension-tabs="extensionTabs"/>
+</template>
