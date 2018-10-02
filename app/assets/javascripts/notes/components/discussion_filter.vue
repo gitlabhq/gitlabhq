cef109354ff383ff59373753070393dab30dc13f
Constance Okoghenun <constanceokoghenun@gmail.com>
2018-10-03 00:36:12 +0100

    Updated spacing around discussion filter component

--- app/assets/javascripts/notes/components/discussion_filter.vue
+++ app/assets/javascripts/notes/components/discussion_filter.vue
@@ -52,7 +52,7 @@ export default {
 <template>
   <div
     v-if="discussionTabCounter > 0"
-    class="prepend-top-10 append-right-4 d-inline-block">
+    class="prepend-top-10 d-inline-block">
     <button
       id="discussion-filter-dropdown"
       class="dropdown-toggle btn btn-default"
