b2fd857a047d45f9389d2b6df75f55910c49266b
Oswaldo Ferreira <oswaldo@gitlab.com>
2018-10-02 15:28:05 -0300

    Adjust db/schema.rb conflict

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20180924070647) do
-=======
 ActiveRecord::Schema.define(version: 20180924141949) do
->>>>>>> upstream/master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
