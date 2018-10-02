819ecd5fb01ab64cacc31253c51984a7564949d7
Sean McGivern <sean@gitlab.com>
2018-10-02 13:39:46 +0100

    Fix N+1 for notification recipients in subscribers

--- spec/services/notification_recipient_service_spec.rb
+++ spec/services/notification_recipient_service_spec.rb
@@ -10,27 +10,50 @@ describe NotificationRecipientService do
     let(:issue) { create(:issue, project: project, assignees: [assignee]) }
     let(:note) { create(:note_on_issue, noteable: issue, project_id: issue.project_id) }
 
-    def create_watcher
-      watcher = create(:user)
-      create(:notification_setting, source: project, user: watcher, level: :watch)
+    context 'when there are multiple watchers' do
+      def create_watcher
+        watcher = create(:user)
+        create(:notification_setting, source: project, user: watcher, level: :watch)
 
-      other_projects.each do |other_project|
-        create(:notification_setting, source: other_project, user: watcher, level: :watch)
+        other_projects.each do |other_project|
+          create(:notification_setting, source: other_project, user: watcher, level: :watch)
+        end
+      end
+
+      it 'avoids N+1 queries', :request_store do
+        create_watcher
+
+        service.build_new_note_recipients(note)
+
+        control_count = ActiveRecord::QueryRecorder.new do
+          service.build_new_note_recipients(note)
+        end
+
+        create_watcher
+
+        expect { service.build_new_note_recipients(note) }.not_to exceed_query_limit(control_count)
       end
     end
 
-    it 'avoids N+1 queries', :request_store do
-      create_watcher
-
-      service.build_new_note_recipients(note)
-
-      control_count = ActiveRecord::QueryRecorder.new do
-        service.build_new_note_recipients(note)
+    context 'when there are multiple subscribers' do
+      def create_subscriber
+        subscriber = create(:user)
+        issue.subscriptions.create(user: subscriber, project: project, subscribed: true)
       end
 
-      create_watcher
+      it 'avoids N+1 queries' do
+        create_subscriber
 
-      expect { service.build_new_note_recipients(note) }.not_to exceed_query_limit(control_count)
+        service.build_new_note_recipients(note)
+
+        control_count = ActiveRecord::QueryRecorder.new do
+          service.build_new_note_recipients(note)
+        end
+
+        create_subscriber
+
+        expect { service.build_new_note_recipients(note) }.not_to exceed_query_limit(control_count)
+      end
     end
   end
 end
