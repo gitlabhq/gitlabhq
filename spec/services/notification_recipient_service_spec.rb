28e6af88aa16a33717dbdbe870a8423300aef042
Sean McGivern <sean@gitlab.com>
2018-10-02 15:29:45 +0100

    Fix N+1 for notification recipients on private projects
    If we don't call #to_a, we're relying on the members already being loaded from
    elsewhere. Otherwise we'll do a separate query for each user:
    
        [1] pry(main)> Project.first.team.members.include?(User.first)
          Project Load (0.7ms)  SELECT  "projects".* FROM "projects"  ORDER BY "projects"."id" ASC LIMIT 1
          ↳ (pry):3
          User Load (1.8ms)  SELECT  "users".* FROM "users"  ORDER BY "users"."id" ASC LIMIT 1
          ↳ (pry):3
          User Exists (0.6ms)  SELECT  1 AS one FROM "users" INNER JOIN "project_authorizations" ON "users"."id" = "project_authorizations"."user_id" WHERE "project_authorizations"."project_id" = $1 AND "users"."id" = $2 LIMIT 1  [["project_id", 1], ["id", 1]]
          ↳ (pry):3
        => true
        [2] pry(main)> Project.first.team.members.to_a.include?(User.first)
          Project Load (12.8ms)  SELECT  "projects".* FROM "projects"  ORDER BY "projects"."id" ASC LIMIT 1
          ↳ (pry):1
          User Load (9.6ms)  SELECT "users".* FROM "users" INNER JOIN "project_authorizations" ON "users"."id" = "project_authorizations"."user_id" WHERE "project_authorizations"."project_id" = $1  [["project_id", 1]]
          ↳ (pry):1
          User Load (0.6ms)  SELECT  "users".* FROM "users"  ORDER BY "users"."id" ASC LIMIT 1
          ↳ (pry):1
        => true

--- spec/services/notification_recipient_service_spec.rb
+++ spec/services/notification_recipient_service_spec.rb
@@ -10,8 +10,24 @@ describe NotificationRecipientService do
     let(:issue) { create(:issue, project: project, assignees: [assignee]) }
     let(:note) { create(:note_on_issue, noteable: issue, project_id: issue.project_id) }
 
+    shared_examples 'no N+1 queries' do
+      it 'avoids N+1 queries', :request_store do
+        create_user
+
+        service.build_new_note_recipients(note)
+
+        control_count = ActiveRecord::QueryRecorder.new do
+          service.build_new_note_recipients(note)
+        end
+
+        create_user
+
+        expect { service.build_new_note_recipients(note) }.not_to exceed_query_limit(control_count)
+      end
+    end
+
     context 'when there are multiple watchers' do
-      def create_watcher
+      def create_user
         watcher = create(:user)
         create(:notification_setting, source: project, user: watcher, level: :watch)
 
@@ -20,39 +36,23 @@ describe NotificationRecipientService do
         end
       end
 
-      it 'avoids N+1 queries', :request_store do
-        create_watcher
-
-        service.build_new_note_recipients(note)
-
-        control_count = ActiveRecord::QueryRecorder.new do
-          service.build_new_note_recipients(note)
-        end
-
-        create_watcher
-
-        expect { service.build_new_note_recipients(note) }.not_to exceed_query_limit(control_count)
-      end
+      include_examples 'no N+1 queries'
     end
 
     context 'when there are multiple subscribers' do
-      def create_subscriber
+      def create_user
         subscriber = create(:user)
         issue.subscriptions.create(user: subscriber, project: project, subscribed: true)
       end
 
-      it 'avoids N+1 queries' do
-        create_subscriber
+      include_examples 'no N+1 queries'
 
-        service.build_new_note_recipients(note)
-
-        control_count = ActiveRecord::QueryRecorder.new do
-          service.build_new_note_recipients(note)
+      context 'when the project is private' do
+        before do
+          project.update!(visibility_level: Gitlab::VisibilityLevel::PRIVATE)
         end
 
-        create_subscriber
-
-        expect { service.build_new_note_recipients(note) }.not_to exceed_query_limit(control_count)
+        include_examples 'no N+1 queries'
       end
     end
   end
