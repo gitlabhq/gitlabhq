c5caa70f509e31e346b04905d177f006b7e9e684
Felipe Artur <felipefac@gmail.com>
2018-10-03 12:48:20 -0300

    Add notes filters to user preference entity

--- app/serializers/user_preference_entity.rb
+++ app/serializers/user_preference_entity.rb
@@ -3,4 +3,11 @@
 class UserPreferenceEntity < Grape::Entity
   expose :issue_notes_filter
   expose :merge_request_notes_filter
+
+  expose :notes_filters do |user_preference|
+    {
+      _('Notes|Show all activity') => UserPreference::NOTES_FILTERS[:all_notes],
+      _('Notes|Show comments only') => UserPreference::NOTES_FILTERS[:only_comments]
+    }
+  end
 end
