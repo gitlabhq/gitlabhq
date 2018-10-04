ab6c87df47a8c2671fdea97858a2b934ec253014
Constance Okoghenun <constanceokoghenun@gmail.com>
2018-10-04 22:22:24 +0100

    Integated BE for discussion filters

--- app/assets/javascripts/notes/discussion_filters.js
+++ app/assets/javascripts/notes/discussion_filters.js
@@ -1,25 +1,15 @@
 import Vue from 'vue';
-import { __ } from '~/locale';
 import DiscussionFilter from './components/discussion_filter.vue';
 
 export default (store) => {
   const discussionFilterEl = document.getElementById('js-vue-discussion-filter');
 
   if (discussionFilterEl) {
-    const { defaultFilter } = discussionFilterEl.dataset;
+    const { defaultFilter, notesFilters } = discussionFilterEl.dataset;
     const defaultValue = defaultFilter ? parseInt(defaultFilter, 10) : null;
-
-    // TODO fetch these values from backend
-    const filterValues = [
-      {
-        title: __('Show all activity'),
-        value: 0,
-      },
-      {
-        title: __('Show comments only'),
-        value: 1,
-      },
-    ];
+    const filterValues = notesFilters ? JSON.parse(notesFilters) : {};
+    const filters = Object.entries(filterValues).map(entry =>
+      ({ title: entry[0], value: entry[1] }));
 
     return new Vue({
       el: discussionFilterEl,
@@ -31,7 +21,7 @@ export default (store) => {
       render(createElement) {
         return createElement('discussion-filter', {
           props: {
-            filters: filterValues,
+            filters,
             defaultValue,
           },
         });
