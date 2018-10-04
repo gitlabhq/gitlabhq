be24f5d51eb97cd4d6368308ae410ad962349453
Filipa Lacerda <filipa@gitlab.com>
2018-10-04 23:29:42 +0100

    Adds `:js` flag in used quota specs

--- ee/spec/features/projects/jobs_spec.rb
+++ ee/spec/features/projects/jobs_spec.rb
@@ -13,13 +13,14 @@ describe 'Jobs', :clean_gitlab_redis_shared_state do
     sign_in(user)
   end
 
-  describe "GET /:project/jobs/:id" do
+  describe "GET /:project/jobs/:id", :js do
     context 'job project is over shared runners limit' do
       let(:group) { create(:group, :with_used_build_minutes_limit) }
       let(:project) { create(:project, :repository, namespace: group, shared_runners_enabled: true) }
 
       it 'displays a warning message' do
         visit project_job_path(project, job)
+        wait_for_requests
 
         expect(page).to have_content('You have used all your shared Runners pipeline minutes.')
       end
