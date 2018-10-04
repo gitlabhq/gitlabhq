11db1f4170c78fe496b2bec323862df308475310
Mark Chao <mchao@gitlab.com>
2018-10-04 12:04:13 +0800

    Centralize loading blob data before highlighting

--- app/presenters/blob_presenter.rb
+++ app/presenters/blob_presenter.rb
@@ -4,6 +4,8 @@ class BlobPresenter < Gitlab::View::Presenter::Simple
   presents :blob
 
   def highlight(plain: nil)
+    blob.load_all_data! if blob.respond_to?(:load_all_data!)
+
     Gitlab::Highlight.highlight(
       blob.path,
       blob.data,
