df833931b65499ea71937b7d20bddd85d5f27c54
Jeremy Watson <jwatson@gitlab.com>
2018-10-05 17:10:20 +0000

    Renamed test events

--- spec/workers/prune_old_events_worker_spec.rb
+++ spec/workers/prune_old_events_worker_spec.rb
@@ -5,8 +5,9 @@ describe PruneOldEventsWorker do
     let(:user) { create(:user) }
 
     let!(:expired_event) { create(:event, :closed, author: user, created_at: 25.months.ago) }
-    let!(:not_expired_event) { create(:event, :closed, author: user,  created_at: 1.day.ago) }
-    let!(:exactly_2_years_event) { create(:event, :closed, author: user, created_at: 2.years.ago) }
+    let!(:not_expired_1_day_event) { create(:event, :closed, author: user,  created_at: 1.day.ago) }
+    let!(:not_expired_13_month_event) { create(:event, :closed, author: user,  created_at: 13.months.ago) }
+    let!(:not_expired_2_years_event) { create(:event, :closed, author: user, created_at: 2.years.ago) }
 
     it 'prunes events older than 2 years' do
       expect { subject.perform }.to change { Event.count }.by(-1)
