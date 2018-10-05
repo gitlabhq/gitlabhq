8deb9c010fb4ab24b56a68470ecbf4b875467248
Shinya Maeda <shinya@gitlab.com>
2018-10-05 20:55:25 +0900

    Remove unnecessary change in schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -2290,7 +2290,6 @@ ActiveRecord::Schema.define(version: 20180924201039) do
   add_foreign_key "boards", "namespaces", column: "group_id", on_delete: :cascade
   add_foreign_key "boards", "projects", name: "fk_f15266b5f9", on_delete: :cascade
   add_foreign_key "chat_teams", "namespaces", on_delete: :cascade
-  add_foreign_key "ci_build_schedules", "ci_builds", column: "build_id", on_delete: :cascade
   add_foreign_key "ci_build_trace_chunks", "ci_builds", column: "build_id", on_delete: :cascade
   add_foreign_key "ci_build_trace_section_names", "projects", on_delete: :cascade
   add_foreign_key "ci_build_trace_sections", "ci_build_trace_section_names", column: "section_name_id", name: "fk_264e112c66", on_delete: :cascade
