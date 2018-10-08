5960353f4578df02ac7a472ce6a39f7458b617c1
Oswaldo Ferreira <oswaldo@gitlab.com>
2018-10-08 09:57:04 -0300

    Move Issue#related_issues method to EE scope
    This is an EE only method. So ideally we should
    move to EE scope to avoid conflicts on CE -> EE.

--- spec/models/issue_spec.rb
+++ spec/models/issue_spec.rb
@@ -268,43 +268,6 @@ describe Issue do
     end
   end
 
-  describe '#related_issues' do
-    let(:user) { create(:user) }
-    let(:authorized_project) { create(:project) }
-    let(:authorized_project2) { create(:project) }
-    let(:unauthorized_project) { create(:project) }
-
-    let(:authorized_issue_a) { create(:issue, project: authorized_project) }
-    let(:authorized_issue_b) { create(:issue, project: authorized_project) }
-    let(:authorized_issue_c) { create(:issue, project: authorized_project2) }
-
-    let(:unauthorized_issue) { create(:issue, project: unauthorized_project) }
-
-    let!(:issue_link_a) { create(:issue_link, source: authorized_issue_a, target: authorized_issue_b) }
-    let!(:issue_link_b) { create(:issue_link, source: authorized_issue_a, target: unauthorized_issue) }
-    let!(:issue_link_c) { create(:issue_link, source: authorized_issue_a, target: authorized_issue_c) }
-
-    before do
-      authorized_project.add_developer(user)
-      authorized_project2.add_developer(user)
-    end
-
-    it 'returns only authorized related issues for given user' do
-      expect(authorized_issue_a.related_issues(user))
-        .to contain_exactly(authorized_issue_b, authorized_issue_c)
-    end
-
-    describe 'when a user cannot read cross project' do
-      it 'only returns issues within the same project' do
-        expect(Ability).to receive(:allowed?).with(user, :read_cross_project)
-                             .and_return(false)
-
-        expect(authorized_issue_a.related_issues(user))
-          .to contain_exactly(authorized_issue_b)
-      end
-    end
-  end
-
   describe '#suggested_branch_name' do
     let(:repository) { double }
 
