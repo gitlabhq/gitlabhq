9685c82045397d2c4b267f8440a238500a64cc7c
Robert Speicher <robert@gitlab.com>
2018-10-08 23:14:54 +0000

    Merge branch 'batch-comments-per-project-license-check' into 'master'
    Use project-level license check for Batch Comments
    
    See merge request gitlab-org/gitlab-ee!7838

--- ee/app/helpers/batch_comments_helper.rb
+++ ee/app/helpers/batch_comments_helper.rb
@@ -1,5 +1,5 @@
 module BatchCommentsHelper
   def batch_comments_enabled?
-    current_user.present? && License.feature_available?(:batch_comments) && Feature.enabled?(:batch_comments, current_user, default_enabled: false)
+    current_user.present? && @project.feature_available?(:batch_comments, current_user)
   end
 end
