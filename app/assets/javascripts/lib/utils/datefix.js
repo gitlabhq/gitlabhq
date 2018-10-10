f7a3e8ff53449bc4ad4a19b3df5a6d19f391b180
Mike Greiling <mike@pixelcog.com>
2018-10-10 01:25:43 -0500

    Prettify lib/utils modules

--- app/assets/javascripts/lib/utils/datefix.js
+++ app/assets/javascripts/lib/utils/datefix.js
@@ -1,12 +1,11 @@
-
-export const pad = (val, len = 2) => (`0${val}`).slice(-len);
+export const pad = (val, len = 2) => `0${val}`.slice(-len);
 
 /**
  * Formats dates in Pickaday
  * @param {String} dateString Date in yyyy-mm-dd format
  * @return {Date} UTC format
  */
-export const parsePikadayDate = (dateString) => {
+export const parsePikadayDate = dateString => {
   const parts = dateString.split('-');
   const year = parseInt(parts[0], 10);
   const month = parseInt(parts[1] - 1, 10);
@@ -20,7 +19,7 @@ export const parsePikadayDate = (dateString) => {
  * @param {Date} date UTC format
  * @return {String} Date formated in yyyy-mm-dd
  */
-export const pikadayToString = (date) => {
+export const pikadayToString = date => {
   const day = pad(date.getDate());
   const month = pad(date.getMonth() + 1);
   const year = date.getFullYear();
--- app/assets/javascripts/lib/utils/simple_poll.js
+++ app/assets/javascripts/lib/utils/simple_poll.js
@@ -2,7 +2,7 @@ export default (fn, interval = 2000, timeout = 60000) => {
   const startTime = Date.now();
 
   return new Promise((resolve, reject) => {
-    const stop = arg => ((arg instanceof Error) ? reject(arg) : resolve(arg));
+    const stop = arg => (arg instanceof Error ? reject(arg) : resolve(arg));
     const next = () => {
       if (Date.now() - startTime < timeout) {
         setTimeout(fn.bind(null, next, stop), interval);
