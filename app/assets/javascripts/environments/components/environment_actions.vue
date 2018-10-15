c8c01bbe0229128f898df167aa7fcc3ef927c4d0
Winnie Hellmann <winnie@gitlab.com>
2018-10-15 12:27:59 +0200

    Make frontend files prettier

--- app/assets/javascripts/environments/components/environment_actions.vue
+++ app/assets/javascripts/environments/components/environment_actions.vue
@@ -32,7 +32,12 @@ export default {
   methods: {
     onClickAction(action) {
       if (action.scheduledAt) {
-        const confirmationMessage = sprintf(s__("DelayedJobs|Are you sure you want to run %{jobName} immediately? This job will run automatically after it's timer finishes."), { jobName: action.name });
+        const confirmationMessage = sprintf(
+          s__(
+            "DelayedJobs|Are you sure you want to run %{jobName} immediately? This job will run automatically after it's timer finishes.",
+          ),
+          { jobName: action.name },
+        );
         // https://gitlab.com/gitlab-org/gitlab-ce/issues/52156
         // eslint-disable-next-line no-alert
         if (!window.confirm(confirmationMessage)) {
