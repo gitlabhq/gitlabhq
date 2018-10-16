jdbean/gitlab-ce

First iteration to allow creating QA resources using the API
With these changes the 'user creates issue' scenario went from ~30
seconds to ~15 seconds to run.

Signed-off-by: Rémy Coutable <remy@rymai.me>

qa/qa/factory/product.rb
@@ -7,16 +7,16 @@ module QA
 
       Attribute = Struct.new(:name, :block)
 
-      def initialize
-        @location = current_url
+      def initialize(product_url)
+        @location = product_url.is_a?(String) ? product_url : current_url
       end
 
       def visit!
         visit @location
       end
 
-      def self.populate!(factory)
-        new.tap do |product|
+      def self.populate!(factory, product_url)
+        new(product_url).tap do |product|
           factory.class.attributes.each_value do |attribute|
             product.instance_exec(factory, attribute.block) do |factory, block|
               value = block.call(factory)

gitlabhq/gitlabhq

qa/qa/factory/product.rb
@@ -5,18 +5,20 @@ module QA
     class Product
       include Capybara::DSL
 
+      attr_reader :location
+
       Attribute = Struct.new(:name, :block)
 
-      def initialize
-        @location = current_url
+      def initialize(location)
+        @location = location
       end
 
       def visit!
         visit @location
       end
 
-      def self.populate!(factory)
-        new.tap do |product|
+      def self.populate!(factory, location)
+        new(location).tap do |product|
           factory.class.attributes.each_value do |attribute|
             product.instance_exec(factory, attribute.block) do |factory, block|
               value = block.call(factory)