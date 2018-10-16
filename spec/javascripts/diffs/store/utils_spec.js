297f1906e57acec8ca6c6de6162bdd1e23e4c7eb
Phil Hughes <me@iamphill.com>
2018-10-16 12:05:19 +0100

    Fixed failing specs
    Added tests to file row truncated text computed prop

--- spec/javascripts/diffs/store/utils_spec.js
+++ spec/javascripts/diffs/store/utils_spec.js
@@ -461,7 +461,6 @@ describe('DiffsStoreUtils', () => {
         {
           key: 'app',
           path: 'app',
-          truncatedPath: 'app',
           name: 'app',
           type: 'tree',
           tree: [
@@ -473,7 +472,6 @@ describe('DiffsStoreUtils', () => {
               key: 'app/index.js',
               name: 'index.js',
               path: 'app/index.js',
-              truncatedPath: 'app/index.js',
               removedLines: 10,
               tempFile: false,
               type: 'blob',
@@ -482,7 +480,6 @@ describe('DiffsStoreUtils', () => {
             {
               key: 'app/test',
               path: 'app/test',
-              truncatedPath: 'app/test',
               name: 'test',
               type: 'tree',
               opened: true,
@@ -495,7 +492,6 @@ describe('DiffsStoreUtils', () => {
                   key: 'app/test/index.js',
                   name: 'index.js',
                   path: 'app/test/index.js',
-                  truncatedPath: 'app/test/index.js',
                   removedLines: 0,
                   tempFile: true,
                   type: 'blob',
@@ -509,7 +505,6 @@ describe('DiffsStoreUtils', () => {
                   key: 'app/test/filepathneedstruncating.js',
                   name: 'filepathneedstruncating.js',
                   path: 'app/test/filepathneedstruncating.js',
-                  truncatedPath: '...est/filepathneedstruncating.js',
                   removedLines: 0,
                   tempFile: true,
                   type: 'blob',
@@ -523,7 +518,6 @@ describe('DiffsStoreUtils', () => {
         {
           key: 'package.json',
           path: 'package.json',
-          truncatedPath: 'package.json',
           name: 'package.json',
           type: 'blob',
           changed: true,
