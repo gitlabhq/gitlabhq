08cdf65b9e1f5f6fc1f4063a8422c25005868e58
Zeger-Jan van de Weg <git@zjvandeweg.nl>
2018-10-17 09:31:35 +0200

    Remove broken git storage for testing
    Broken storage used to be used to test situations where the Git storage
    wasn't being reached. These days we can just mock the Gitaly response.
    
    But given the broken storage is removed now, Gitaly can take over
    control of the storage being reachable. If it's not, Gitaly won't boot.
    That's nice for situations where a disk wasn't mounted for instance.
    
    Gitaly MR: https://gitlab.com/gitlab-org/gitaly/merge_requests/675

--- spec/support/stored_repositories.rb
+++ spec/support/stored_repositories.rb
@@ -1,8 +1,4 @@
 RSpec.configure do |config|
-  config.before(:all, :broken_storage) do
-    FileUtils.rm_rf Gitlab.config.repositories.storages.broken.legacy_disk_path
-  end
-
   config.before(:each, :broken_storage) do
     allow(Gitlab::GitalyClient).to receive(:call) do
       raise GRPC::Unavailable.new('Gitaly broken in this spec')
