135ef0465d284a136513593d53940406087ac903
Kushal Pandya <kushal@gitlab.com>
2018-10-24 16:20:55 +0530

    Add pikaday parsing methods
    Adds methods from `app/assets/javascripts/lib/utils/datefix.js`
    and deletes the files

--- app/assets/javascripts/lib/utils/datetime_utility.js
+++ app/assets/javascripts/lib/utils/datetime_utility.js
@@ -46,6 +46,8 @@ const getMonthNames = abbreviated => {
   ];
 };
 
+export const pad = (val, len = 2) => `0${val}`.slice(-len);
+
 /**
  * Given a date object returns the day of the week in English
  * @param {date} date
@@ -387,4 +389,31 @@ export const formatTime = milliseconds => {
   if (remainingSeconds < 10) formattedTime += '0';
   formattedTime += remainingSeconds;
   return formattedTime;
+};
+
+/**
+ * Formats dates in Pickaday
+ * @param {String} dateString Date in yyyy-mm-dd format
+ * @return {Date} UTC format
+ */
+export const parsePikadayDate = dateString => {
+  const parts = dateString.split('-');
+  const year = parseInt(parts[0], 10);
+  const month = parseInt(parts[1] - 1, 10);
+  const day = parseInt(parts[2], 10);
+
+  return new Date(year, month, day);
+};
+
+/**
+ * Used `onSelect` method in pickaday
+ * @param {Date} date UTC format
+ * @return {String} Date formated in yyyy-mm-dd
+ */
+export const pikadayToString = date => {
+  const day = pad(date.getDate());
+  const month = pad(date.getMonth() + 1);
+  const year = date.getFullYear();
+
+  return `${year}-${month}-${day}`;
 };
--- spec/javascripts/datetime_utility_spec.js
+++ spec/javascripts/datetime_utility_spec.js
@@ -192,3 +192,29 @@ describe('formatTime', () => {
     });
   });
 });
+
+describe('datefix', () => {
+  describe('pad', () => {
+    it('should add a 0 when length is smaller than 2', () => {
+      expect(datetimeUtility.pad(2)).toEqual('02');
+    });
+
+    it('should not add a zero when lenght matches the default', () => {
+      expect(datetimeUtility.pad(12)).toEqual('12');
+    });
+
+    it('should add a 0 when lenght is smaller than the provided', () => {
+      expect(datetimeUtility.pad(12, 3)).toEqual('012');
+    });
+  });
+
+  describe('parsePikadayDate', () => {
+    // removed because of https://gitlab.com/gitlab-org/gitlab-ce/issues/39834
+  });
+
+  describe('pikadayToString', () => {
+    it('should format a UTC date into yyyy-mm-dd format', () => {
+      expect(datetimeUtility.pikadayToString(new Date('2020-01-29:00:00'))).toEqual('2020-01-29');
+    });
+  });
+});
