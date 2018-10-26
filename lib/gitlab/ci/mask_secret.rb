06892e88f56eff13747f58e269c7190fc662a9ae
gfyoung <gfyoung17@gmail.com>
2018-10-25 21:12:43 -0700

    Enable frozen string for lib/gitlab/ci
    Enables frozen string for the following:
    
    * lib/gitlab/ci/*.rb
    * lib/gitlab/ci/build/**/*.rb
    * lib/gitlab/ci/config/**/*.rb
    * lib/gitlab/ci/pipeline/**/*.rb
    * lib/gitlab/ci/reports/**/*.rb
    
    Partially addresses #47424.

--- lib/gitlab/ci/mask_secret.rb
+++ lib/gitlab/ci/mask_secret.rb
@@ -1,9 +1,13 @@
+# frozen_string_literal: true
+
 module Gitlab
   module Ci::MaskSecret
     class << self
       def mask!(value, token)
         return value unless value.present? && token.present?
 
+        # We assume 'value' must be mutable, given
+        # that frozen string is enabled.
         value.gsub!(token, 'x' * token.length)
         value
       end
