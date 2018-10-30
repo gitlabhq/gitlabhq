81f5955eb6677849c15d35b60223312f6a9d77a3
Bob Van Landuyt <bob@vanlanduyt.co>
2018-10-30 14:37:40 +0100

    Move Repository#wrapped_gitaly_errors into concern
    Having this in a concern allows us to reuse it for different single
    purpose classes that call out to git without going through the
    repository every time.

--- lib/gitlab/git/commit_stats.rb
+++ lib/gitlab/git/commit_stats.rb
@@ -3,6 +3,8 @@
 module Gitlab
   module Git
     class CommitStats
+      include Gitlab::Git::WrapsGitalyErrors
+
       attr_reader :id, :additions, :deletions, :total
 
       # Instantiate a CommitStats object
@@ -14,7 +16,7 @@ module Gitlab
         @deletions = 0
         @total = 0
 
-        repo.wrapped_gitaly_errors do
+        wrapped_gitaly_errors do
           gitaly_stats(repo, commit)
         end
       end
---
+++ lib/gitlab/git/wraps_gitaly_errors.rb
@@ -0,0 +1,15 @@
+module Gitlab
+  module Git
+    module WrapsGitalyErrors
+      def wrapped_gitaly_errors(&block)
+        yield block
+      rescue GRPC::NotFound => e
+        raise Gitlab::Git::Repository::NoRepository.new(e)
+      rescue GRPC::InvalidArgument => e
+        raise ArgumentError.new(e)
+      rescue GRPC::BadStatus => e
+        raise Gitlab::Git::CommandError.new(e)
+      end
+    end
+  end
+end
