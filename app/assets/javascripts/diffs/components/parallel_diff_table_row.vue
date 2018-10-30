5722f63f2d8c69a20c874d32dd14dcd6c669e38b
at.ramya <rauthappan@gitlab.com>
2018-10-30 12:13:43 +0530

    Updating selectors for diff line

--- app/assets/javascripts/diffs/components/parallel_diff_table_row.vue
+++ app/assets/javascripts/diffs/components/parallel_diff_table_row.vue
@@ -143,7 +143,7 @@ export default {
         :show-comment-button="true"
         :diff-view-type="parallelDiffViewType"
         line-position="right"
-        class="diff-line-num new_line qa-new-diff-line"
+        class="diff-line-num new_line"
       />
       <td
         :id="line.right.lineCode"
