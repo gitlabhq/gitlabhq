8c126525faba40032244328187ba73a53b6eaf4c
Brett Walker <bwalker@gitlab.com>
2018-11-01 18:07:37 -0500

    Extract code into IssueBoardEntity

--- app/serializers/issue_serializer.rb
+++ app/serializers/issue_serializer.rb
@@ -4,15 +4,17 @@ class IssueSerializer < BaseSerializer
   # This overrided method takes care of which entity should be used
   # to serialize the `issue` based on `basic` key in `opts` param.
   # Hence, `entity` doesn't need to be declared on the class scope.
-  def represent(merge_request, opts = {})
+  def represent(issue, opts = {})
     entity =
       case opts[:serializer]
       when 'sidebar'
         IssueSidebarEntity
+      when 'board'
+        IssueBoardEntity
       else
         IssueEntity
       end
 
-    super(merge_request, opts, entity)
+    super(issue, opts, entity)
   end
 end
