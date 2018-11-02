be0af5e69fc60bf629130290b49dee07510359a3
Achilleas Pipinellis <axil@gitlab.com>
2018-11-02 11:15:04 +0100

    Test type of README file without extension

--- spec/lib/gitlab/file_detector_spec.rb
+++ spec/lib/gitlab/file_detector_spec.rb
@@ -15,8 +15,9 @@ describe Gitlab::FileDetector do
 
   describe '.type_of' do
     it 'returns the type of a README file' do
-      %w(README readme INDEX index).each do |filename|
-        %w(.md .adoc).each do |extname|
+      %w[README readme INDEX index].each do |filename|
+        expect(described_class.type_of(filename)).to eq(:readme)
+        %w[.md .adoc .rst].each do |extname|
           expect(described_class.type_of(filename + extname)).to eq(:readme)
         end
       end
