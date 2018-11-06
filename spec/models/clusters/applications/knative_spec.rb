e5c408e44549b2e8a32492ba6ab10b45727c44dd
Chris Baumbauer <cab@cabnetworks.net>
2018-11-06 11:52:15 -0800

    Refactor cluster application tests to centralize helm CA cert verification

--- spec/models/clusters/applications/knative_spec.rb
+++ spec/models/clusters/applications/knative_spec.rb
@@ -5,6 +5,7 @@ describe Clusters::Applications::Knative do
 
   include_examples 'cluster application core specs', :clusters_applications_knative
   include_examples 'cluster application status specs', :clusters_applications_knative
+  include_examples 'cluster application helm specs', :clusters_applications_knative
 
   describe '.installed' do
     subject { described_class.installed }
@@ -67,24 +68,6 @@ describe Clusters::Applications::Knative do
 
     it 'should include knative specific keys in the values.yaml file' do
       expect(values).to include('domain')
-    end
-
-    context 'when the helm application does not have a ca_cert' do
-      before do
-        application.cluster.application_helm.ca_cert = nil
-      end
-
-      it 'should not include cert files when there is no ca_cert entry' do
-        expect(subject).not_to include(:'ca.pem', :'cert.pem', :'key.pem')
-      end
-    end
-
-    it 'should include cert files when there is a ca_cert entry' do
-      expect(subject).to include(:'ca.pem', :'cert.pem', :'key.pem')
-      expect(subject[:'ca.pem']).to eq(application.cluster.application_helm.ca_cert)
-
-      cert = OpenSSL::X509::Certificate.new(subject[:'cert.pem'])
-      expect(cert.not_after).to be < 60.minutes.from_now
     end
   end
 end
