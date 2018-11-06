4eeb51dc71062fa2eac828c6030388d354179efc
Shinya Maeda <shinya@gitlab.com>
2018-11-06 23:27:04 +0900

    Align index name to others

--- db/migrate/20181106135939_add_index_to_deployments.rb
+++ db/migrate/20181106135939_add_index_to_deployments.rb
@@ -6,14 +6,14 @@ class AddIndexToDeployments < ActiveRecord::Migration
   DOWNTIME = false
   SHORT_INDEX_NAME_FOR_ENVIRONMENT_ID = 'index_deployments_on_env_deployed_for_id'
   SHORT_INDEX_NAME_FOR_ENVIRONMENT_IID = 'index_deployments_on_env_deployed_for_iid'
-  SHORT_INDEX_NAME_FOR_FINISHED_AT = 'index_deployments_on_deployed_for_finished_at'
+  SHORT_INDEX_NAME_FOR_PROJECT_FINISHED_AT = 'index_deployments_on_prj_deployed_for_finished_at'
 
   disable_ddl_transaction!
 
   def up
     add_concurrent_index :deployments, [:project_id, :action, :status, :id]
     add_concurrent_index :deployments, [:project_id, :action, :status, :iid]
-    add_concurrent_index :deployments, [:project_id, :action, :status, :finished_at], name: SHORT_INDEX_NAME_FOR_FINISHED_AT
+    add_concurrent_index :deployments, [:project_id, :action, :status, :finished_at], name: SHORT_INDEX_NAME_FOR_PROJECT_FINISHED_AT
     add_concurrent_index :deployments, [:environment_id, :action, :status, :id], name: SHORT_INDEX_NAME_FOR_ENVIRONMENT_ID
     add_concurrent_index :deployments, [:environment_id, :action, :status, :iid], name: SHORT_INDEX_NAME_FOR_ENVIRONMENT_IID
     add_concurrent_index :deployments, [:environment_id, :action, :sha]
@@ -22,7 +22,7 @@ class AddIndexToDeployments < ActiveRecord::Migration
   def down
     remove_concurrent_index :deployments, [:project_id, :action, :status, :id]
     remove_concurrent_index :deployments, [:project_id, :action, :status, :iid]
-    remove_concurrent_index_by_name(:deployments, SHORT_INDEX_NAME_FOR_FINISHED_AT)
+    remove_concurrent_index_by_name(:deployments, SHORT_INDEX_NAME_FOR_PROJECT_FINISHED_AT)
     remove_concurrent_index_by_name(:deployments, SHORT_INDEX_NAME_FOR_ENVIRONMENT_ID)
     remove_concurrent_index_by_name(:deployments, SHORT_INDEX_NAME_FOR_ENVIRONMENT_IID)
     remove_concurrent_index :deployments, [:environment_id, :action, :sha]
--- db/schema.rb
+++ db/schema.rb
@@ -839,7 +839,7 @@ ActiveRecord::Schema.define(version: 20181106135939) do
   add_index "deployments", ["environment_id", "iid", "project_id"], name: "index_deployments_on_environment_id_and_iid_and_project_id", using: :btree
   add_index "deployments", ["environment_id", "status"], name: "index_deployments_on_environment_id_and_status", using: :btree
   add_index "deployments", ["id"], name: "partial_index_deployments_for_legacy_successful_deployments", where: "((finished_at IS NULL) AND (status = 2))", using: :btree
-  add_index "deployments", ["project_id", "action", "status", "finished_at"], name: "index_deployments_on_deployed_for_finished_at", using: :btree
+  add_index "deployments", ["project_id", "action", "status", "finished_at"], name: "index_deployments_on_prj_deployed_for_finished_at", using: :btree
   add_index "deployments", ["project_id", "action", "status", "id"], name: "index_deployments_on_project_id_and_action_and_status_and_id", using: :btree
   add_index "deployments", ["project_id", "action", "status", "iid"], name: "index_deployments_on_project_id_and_action_and_status_and_iid", using: :btree
   add_index "deployments", ["project_id", "iid"], name: "index_deployments_on_project_id_and_iid", unique: true, using: :btree
