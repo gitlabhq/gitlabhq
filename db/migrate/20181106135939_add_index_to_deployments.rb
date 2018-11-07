90df732144263d058039a3846d9fe4420816efe1
Shinya Maeda <shinya@gitlab.com>
2018-11-07 21:59:52 +0900

    Remove low selective indexes

--- db/migrate/20181106135939_add_index_to_deployments.rb
+++ db/migrate/20181106135939_add_index_to_deployments.rb
@@ -8,20 +8,10 @@ class AddIndexToDeployments < ActiveRecord::Migration
   disable_ddl_transaction!
 
   def up
-    add_concurrent_index :deployments, [:project_id, :status, :id]
-    add_concurrent_index :deployments, [:project_id, :status, :iid]
     add_concurrent_index :deployments, [:project_id, :status, :created_at]
-    add_concurrent_index :deployments, [:environment_id, :status, :id]
-    add_concurrent_index :deployments, [:environment_id, :status, :iid]
-    add_concurrent_index :deployments, [:environment_id, :sha]
   end
 
   def down
-    remove_concurrent_index :deployments, [:project_id, :status, :id]
-    remove_concurrent_index :deployments, [:project_id, :status, :iid]
     remove_concurrent_index :deployments, [:project_id, :status, :created_at]
-    remove_concurrent_index :deployments, [:environment_id, :status, :id]
-    remove_concurrent_index :deployments, [:environment_id, :status, :iid]
-    remove_concurrent_index :deployments, [:environment_id, :sha]
   end
 end
