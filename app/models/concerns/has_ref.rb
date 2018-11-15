837ab8c5d83243db8efe1cec1c6e001d1cbeb43f
Matija Čupić <matteeyah@gmail.com>
2018-11-15 21:28:17 +0100

    Make HasRef#git_ref public

--- app/models/concerns/has_ref.rb
+++ app/models/concerns/has_ref.rb
@@ -7,15 +7,11 @@ module HasRef
     !tag?
   end
 
-  private
-
   def git_ref
     if branch?
       Gitlab::Git::BRANCH_REF_PREFIX + ref.to_s
     elsif tag?
       Gitlab::Git::TAG_REF_PREFIX + ref.to_s
-    else
-      raise ArgumentError, 'Invalid pipeline type!'
     end
   end
 end
--- spec/models/concerns/has_ref_spec.rb
+++ spec/models/concerns/has_ref_spec.rb
@@ -28,4 +28,32 @@ describe HasRef do
       end
     end
   end
+
+  describe '#git_ref' do
+    subject { pipeline.git_ref }
+
+    context 'when tag is true' do
+      let(:pipeline) { create(:ci_pipeline, tag: true) }
+
+      it 'returns a tag ref' do
+        is_expected.to start_with(Gitlab::Git::TAG_REF_PREFIX)
+      end
+    end
+
+    context 'when tag is false' do
+      let(:pipeline) { create(:ci_pipeline, tag: false) }
+
+      it 'returns a branch ref' do
+        is_expected.to start_with(Gitlab::Git::BRANCH_REF_PREFIX)
+      end
+    end
+
+    context 'when tag is nil' do
+      let(:pipeline) { create(:ci_pipeline, tag: nil) }
+
+      it 'returns a branch ref' do
+        is_expected.to start_with(Gitlab::Git::BRANCH_REF_PREFIX)
+      end
+    end
+  end
 end
