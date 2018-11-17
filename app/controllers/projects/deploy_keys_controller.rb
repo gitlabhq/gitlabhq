7be7f570dc9d8bf482d8a1e548b3bd21cccdc39d
Stan Hu <stanhu@gmail.com>
2018-11-16 21:59:17 -0800

    Fix enabling project deploy key for admins
    Admins would be prevented from adding a project deploy key since the
    accessible keys would be restricted to the user's keys.
    
    Also backports a spec for DeployKeysController from
    https://gitlab.com/gitlab-org/gitlab-ee/merge_requests/8432.

--- app/controllers/projects/deploy_keys_controller.rb
+++ app/controllers/projects/deploy_keys_controller.rb
@@ -46,7 +46,9 @@ class Projects::DeployKeysController < Projects::ApplicationController
   end
 
   def enable
-    Projects::EnableDeployKeyService.new(@project, current_user, params).execute
+    key = Projects::EnableDeployKeyService.new(@project, current_user, params).execute
+
+    return render_404 unless key
 
     respond_to do |format|
       format.html { redirect_to_repository_settings(@project, anchor: 'js-deploy-keys-settings') }
@@ -54,19 +56,16 @@ class Projects::DeployKeysController < Projects::ApplicationController
     end
   end
 
-  # rubocop: disable CodeReuse/ActiveRecord
   def disable
-    deploy_key_project = @project.deploy_keys_projects.find_by(deploy_key_id: params[:id])
+    deploy_key_project = Projects::DisableDeployKeyService.new(@project, current_user, params).execute
+
     return render_404 unless deploy_key_project
 
-    deploy_key_project.destroy!
-
     respond_to do |format|
       format.html { redirect_to_repository_settings(@project, anchor: 'js-deploy-keys-settings') }
       format.json { head :ok }
     end
   end
-  # rubocop: enable CodeReuse/ActiveRecord
 
   protected
 
