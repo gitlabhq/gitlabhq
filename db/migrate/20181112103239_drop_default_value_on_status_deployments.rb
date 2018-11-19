0f793c6f2c47daa55df4568d992cd1a2522719c6
Shinya Maeda <shinya@gitlab.com>
2018-11-19 17:47:52 +0900

    Remove unnecessary code

--- db/migrate/20181112103239_drop_default_value_on_status_deployments.rb
+++ db/migrate/20181112103239_drop_default_value_on_status_deployments.rb
@@ -1,12 +1,8 @@
 # frozen_string_literal: true
 
 class DropDefaultValueOnStatusDeployments < ActiveRecord::Migration
-  include Gitlab::Database::MigrationHelpers
-
   DOWNTIME = false
   DEPLOYMENT_STATUS_SUCCESS = 2 # Equivalent to Deployment.state_machine.states['success'].value
-
-  disable_ddl_transaction!
 
   def up
     change_column_default :deployments, :status, nil
