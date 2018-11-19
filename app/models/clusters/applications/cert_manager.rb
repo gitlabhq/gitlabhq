6eced5ff74e0b829492dde7456379d215290b8ec
Amit Rathi <amit@hypertrack.io>
2018-11-20 01:54:49 +0530

    Remove linter complaints from cert_manager.rb

--- app/models/clusters/applications/cert_manager.rb
+++ app/models/clusters/applications/cert_manager.rb
@@ -1,55 +1,55 @@
 # frozen_string_literal: true
 
 module Clusters
-    module Applications
-      class CertManager < ActiveRecord::Base
-        VERSION = 'v0.5.0'.freeze
-  
-        self.table_name = 'clusters_applications_cert_managers'
+  module Applications
+    class CertManager < ActiveRecord::Base
+      VERSION = 'v0.5.0'.freeze
 
-        include ::Clusters::Concerns::ApplicationCore
-        include ::Clusters::Concerns::ApplicationStatus
-        include ::Clusters::Concerns::ApplicationVersion
-        include ::Clusters::Concerns::ApplicationData
-  
-        default_value_for :version, VERSION
+      self.table_name = 'clusters_applications_cert_managers'
 
-        def chart
-          'stable/cert-manager'
-        end
+      include ::Clusters::Concerns::ApplicationCore
+      include ::Clusters::Concerns::ApplicationStatus
+      include ::Clusters::Concerns::ApplicationVersion
+      include ::Clusters::Concerns::ApplicationData
 
-        def install_command
-          Gitlab::Kubernetes::Helm::InstallCommand.new(
-            name: 'certmanager',
-            version: VERSION,
-            rbac: cluster.platform_kubernetes_rbac?,
-            chart: chart,
-            files: files.merge(cluster_issuer_file),
-            postinstall: post_install_script
-          )
-        end
+      default_value_for :version, VERSION
 
-        private
+      def chart
+        'stable/cert-manager'
+      end
 
-        def post_install_script
-          ["/usr/bin/kubectl create -f /data/helm/certmanager/config/cluster_issuer.yaml"]
-        end
+      def install_command
+        Gitlab::Kubernetes::Helm::InstallCommand.new(
+          name: 'certmanager',
+          version: VERSION,
+          rbac: cluster.platform_kubernetes_rbac?,
+          chart: chart,
+          files: files.merge(cluster_issuer_file),
+          postinstall: post_install_script
+        )
+      end
 
-        def cluster_issuer_file
-          {
-            'cluster_issuer.yaml': cluster_issuer_yaml_content
-          }
-        end
+      private
 
-        def cluster_issuer_yaml_content
-          data = YAML.load_file(cluster_issuer_file_path)
-          data["spec"]["acme"]["email"] = self.email
-          YAML.dump(data)
-        end  
+      def post_install_script
+        ["/usr/bin/kubectl create -f /data/helm/certmanager/config/cluster_issuer.yaml"]
+      end
 
-        def cluster_issuer_file_path
-          "#{Rails.root}/vendor/cert_manager/cluster_issuer.yaml"
-        end
+      def cluster_issuer_file
+        {
+          'cluster_issuer.yaml': cluster_issuer_yaml_content
+        }
+      end
+
+      def cluster_issuer_yaml_content
+        data = YAML.load_file(cluster_issuer_file_path)
+        data["spec"]["acme"]["email"] = self.email
+        YAML.dump(data)
+      end
+
+      def cluster_issuer_file_path
+        "#{Rails.root}/vendor/cert_manager/cluster_issuer.yaml"
       end
     end
   end
+end
