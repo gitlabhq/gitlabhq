0d1922dd7c08eb098390199af3bc7a3e92812a60
Jasper Maes <jaspermaes.jm@gmail.com>
2018-11-24 11:17:42 +0100

    Passing an argument to force an association to reload is now deprecated

--- app/models/merge_request.rb
+++ app/models/merge_request.rb
@@ -48,8 +48,8 @@ class MergeRequest < ActiveRecord::Base
   # is the inverse of MergeRequest#merge_request_diff, which means it may not be
   # the latest diff, because we could have loaded any diff from this particular
   # MR. If we haven't already loaded a diff, then it's fine to load the latest.
-  def merge_request_diff(*args)
-    fallback = latest_merge_request_diff if args.empty? && !association(:merge_request_diff).loaded?
+  def merge_request_diff
+    fallback = latest_merge_request_diff unless association(:merge_request_diff).loaded?
 
     fallback || super
   end
@@ -618,10 +618,6 @@ class MergeRequest < ActiveRecord::Base
       merge_request_diffs.create
       reload_merge_request_diff
     end
-  end
-
-  def reload_merge_request_diff
-    merge_request_diff(true)
   end
 
   def viewable_diffs
