c09fd53e54344bead8686a5c7fbb2da05bea0adc
Lin Jen-Shin <godfat@godfat.org>
2018-11-26 15:43:15 +0000

    Remove notes index on updated_at

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181126150622) do
+ActiveRecord::Schema.define(version: 20181126153547) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1392,7 +1392,6 @@ ActiveRecord::Schema.define(version: 20181126150622) do
     t.index ["noteable_id", "noteable_type"], name: "index_notes_on_noteable_id_and_noteable_type", using: :btree
     t.index ["noteable_type"], name: "index_notes_on_noteable_type", using: :btree
     t.index ["project_id", "noteable_type"], name: "index_notes_on_project_id_and_noteable_type", using: :btree
-    t.index ["updated_at"], name: "index_notes_on_updated_at", using: :btree
   end
 
   create_table "notification_settings", force: :cascade do |t|
