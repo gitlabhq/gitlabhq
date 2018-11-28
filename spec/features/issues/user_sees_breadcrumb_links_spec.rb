0e75378b03fc9df3a88965b4475ab420e98129b8
George Tsiolis <tsiolis.g@gmail.com>
2018-11-28 19:15:37 +0000

    Include new and edit links in breadcrumb for project milestones, group milestones, and labels

--- spec/features/issues/user_sees_breadcrumb_links_spec.rb
+++ spec/features/issues/user_sees_breadcrumb_links_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
-describe 'New issue breadcrumbs' do
+describe 'New issue breadcrumb' do
   let(:project) { create(:project) }
-  let(:user)    { project.creator }
+  let(:user) { project.creator }
 
   before do
     sign_in(user)
-    visit new_project_issue_path(project)
+    visit(new_project_issue_path(project))
   end
 
-  it 'display a link to project issues and new issue pages' do
+  it 'displays link to project issues and new issue' do
     page.within '.breadcrumbs' do
       expect(find_link('Issues')[:href]).to end_with(project_issues_path(project))
       expect(find_link('New')[:href]).to end_with(new_project_issue_path(project))
