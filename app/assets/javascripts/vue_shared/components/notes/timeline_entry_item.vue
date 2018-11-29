99862050de660128ca564573eab46616c389ea8c
Winnie Hellmann <winnie@gitlab.com>
2018-11-29 14:28:50 +0100

    Create shared timeline entry component

---
+++ app/assets/javascripts/vue_shared/components/notes/timeline_entry_item.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  name: 'TimelineEntry',
+};
+</script>
+
+<template>
+  <li class="timeline-entry">
+    <div class="timeline-entry-inner"><slot></slot></div>
+  </li>
+</template>
