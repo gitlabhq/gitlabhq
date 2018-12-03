058de0d40127e20b3068d159e053539f1e353fc3
jhampton <jhampton@gitlab.com>
2018-12-03 11:56:49 -0500

    Conditionally send variable values
    We only want to send trigger variable values if the user is a
    maintainer.

--- app/serializers/trigger_variable_entity.rb
+++ app/serializers/trigger_variable_entity.rb
@@ -3,5 +3,6 @@
 class TriggerVariableEntity < Grape::Entity
   include RequestAwareEntity
 
-  expose :key, :value, :public
+  expose :key, :public
+  expose :value, if: ->(_, _) { request.project.team.maintainer?(request.current_user) }
 end
