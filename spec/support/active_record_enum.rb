78dfbca6467a0025fdd99df63ff832f9d42d5790
Shinya Maeda <shinya@gitlab.com>
2018-12-03 21:41:24 +0900

    Use nice message

--- spec/support/active_record_enum.rb
+++ spec/support/active_record_enum.rb
@@ -3,7 +3,10 @@
 shared_examples 'Unique enum values' do
   described_class.defined_enums.each do |name, hash|
     it "has unique values in #{name}" do
-      expect(hash.values).to contain_exactly(*hash.values.uniq)
+      duplicated = hash.group_by(&:last).select { |key, value| value.size > 1 }
+
+      expect(duplicated).to be_empty,
+        "Duplicated values detected: #{duplicated.values.map(&Hash.method(:[]))}"
     end
   end
 end
