a648bcad7bb7fcf24c0be81abf273da8d0bbf410
Alexis Reigel <alexis.reigel.ext@siemens.com>
2018-12-04 14:40:47 +0100

    extract protected variable logic to helper

---
+++ app/helpers/ci_variables_helper.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module CiVariablesHelper
+  def ci_variable_protected_by_default?
+    Gitlab::CurrentSettings.current_application_settings.protected_ci_variables
+  end
+
+  def ci_variable_protected?(variable, only_key_value)
+    if variable && !only_key_value
+      variable.protected
+    else
+      ci_variable_protected_by_default?
+    end
+  end
+end
