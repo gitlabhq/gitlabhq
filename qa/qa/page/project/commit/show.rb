12c358f1b781fc6401eceee80237232bd0bb005a
Zeff Morgan <zeff.morgan@gmail.com>
2018-12-04 01:03:58 -0500

    Add tests for plain diff/email patch options
    Add spec file using before(:context) to reduce test time. With testing
    almost identical things, unnecessary to make them completely atomic.
    Includes two helper methods. Since the raw_content method is the only
    function needed on that page, created the method in the spec instead
    of adding another page object.
    
    Setup new project/commit page object and update project/show to add
    go_to_commit method. The go_to_commit method is near duplicate of
    go_to_file method, but decided to split them off to reduce overall
    refactoring and simplify language.
    
    Also add selectors to commit box partial and update qa.rb to load
    new page object.

---
+++ qa/qa/page/project/commit/show.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module QA
+  module Page
+    module Project
+      module Commit
+        class Show < Page::Base
+          view 'app/views/projects/commit/_commit_box.html.haml' do
+            element :options_button
+            element :email_patches
+            element :plain_diff
+          end
+
+          def select_email_patches
+            click_element :options_button
+            click_element :email_patches
+          end
+
+          def select_plain_diff
+            click_element :options_button
+            click_element :plain_diff
+          end
+        end
+      end
+    end
+  end
+end
