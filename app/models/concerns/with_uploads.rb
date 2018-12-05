4ded04119ce70f7a5dd8a49434675355563b591e
Jan Provaznik <jprovaznik@gitlab.com>
2018-12-05 15:01:12 +0100

    Allow pass additional options to use_fast_destroy
    These options are then used when setting before destroy callback.

--- app/models/concerns/with_uploads.rb
+++ app/models/concerns/with_uploads.rb
@@ -31,13 +31,9 @@ module WithUploads
 
     # TODO: when feature flag is removed, we can use just dependent: destroy
     # option on :file_uploads
-    before_destroy :remove_file_uploads
+    before_destroy :remove_file_uploads, unless: :fast_destroy_enabled?
 
-    use_fast_destroy :file_uploads
-  end
-
-  def perform_fast_destroy(subject)
-    super if fast_destroy_enabled?
+    use_fast_destroy :file_uploads, if: :fast_destroy_enabled?
   end
 
   def retrieve_upload(_identifier, paths)
