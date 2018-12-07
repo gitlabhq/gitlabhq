17634d72ee42bf5c22f557978e1d4d7530912691
Travis Miller <travis@travismiller.com>
2018-12-07 22:05:40 +0000

    Changed frontmatter filtering to support YAML, JSON, TOML, and arbitrary languages

---
+++ lib/banzai/filter/front_matter_filter.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module Banzai
+  module Filter
+    class FrontMatterFilter < HTML::Pipeline::Filter
+      DELIM_LANG = {
+        '---' => 'yaml',
+        '+++' => 'toml',
+        ';;;' => 'json'
+      }.freeze
+
+      DELIM = Regexp.union(DELIM_LANG.keys)
+
+      PATTERN = %r{
+        \A(?:[^\r\n]*coding:[^\r\n]*)?         # optional encoding line
+        \s*
+        ^(?<delim>#{DELIM})[ \t]*(?<lang>\S*)  # opening front matter marker (optional language specifier)
+        \s*
+        ^(?<front_matter>.*?)                  # front matter (not greedy)
+        \s*
+        ^\k<delim>                             # closing front matter marker
+        \s*
+      }mx
+
+      def call
+        html.sub(PATTERN) do |_match|
+          lang = $~[:lang].presence || DELIM_LANG[$~[:delim]]
+
+          ["```#{lang}", $~[:front_matter], "```", "\n"].join("\n")
+        end
+      end
+    end
+  end
+end
