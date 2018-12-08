c9b50acc8f64183a17afa3c85255c59b796f8af2
Matija Čupić <matteeyah@gmail.com>
2018-12-08 21:36:54 +0100

    Backport security fix for 11.6

--- lib/gitlab/git.rb
+++ lib/gitlab/git.rb
@@ -54,11 +54,11 @@ module Gitlab
       end
 
       def tag_ref?(ref)
-        ref.start_with?(TAG_REF_PREFIX)
+        ref =~ /^#{TAG_REF_PREFIX}.+/
       end
 
       def branch_ref?(ref)
-        ref.start_with?(BRANCH_REF_PREFIX)
+        ref =~ /^#{BRANCH_REF_PREFIX}.+/
       end
 
       def blank_ref?(ref)
