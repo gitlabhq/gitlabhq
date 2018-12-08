b87c364b0d3f96157d257dcb10e67c1c3b9f8ef8
Phil Hughes <me@iamphill.com>
2018-12-08 09:58:45 +0000

    Merge branch 'kp-issue_6086' into 'master'
    CE Backport: Epic issue list and related issue list re-design
    
    See merge request gitlab-org/gitlab-ce!23658

--- spec/javascripts/boards/mock_data.js
+++ spec/javascripts/boards/mock_data.js
@@ -1,5 +1,11 @@
 import BoardService from '~/boards/services/board_service';
 
+export const boardObj = {
+  id: 1,
+  name: 'test',
+  milestone_id: null,
+};
+
 export const listObj = {
   id: 300,
   position: 0,
@@ -40,6 +46,12 @@ export const BoardsMockData = {
         },
       ],
     },
+    '/test/issue-boards/milestones.json': [
+      {
+        id: 1,
+        title: 'test',
+      },
+    ],
   },
   POST: {
     '/test/-/boards/1/lists': listObj,
@@ -69,4 +81,61 @@ export const mockBoardService = (opts = {}) => {
     bulkUpdatePath,
     boardId,
   });
+};
+
+export const mockAssigneesList = [
+  {
+    id: 2,
+    name: 'Terrell Graham',
+    username: 'monserrate.gleichner',
+    state: 'active',
+    avatar_url: 'https://www.gravatar.com/avatar/598fd02741ac58b88854a99d16704309?s=80&d=identicon',
+    web_url: 'http://127.0.0.1:3001/monserrate.gleichner',
+    path: '/monserrate.gleichner',
+  },
+  {
+    id: 12,
+    name: 'Susy Johnson',
+    username: 'tana_harvey',
+    state: 'active',
+    avatar_url: 'https://www.gravatar.com/avatar/e021a7b0f3e4ae53b5068d487e68c031?s=80&d=identicon',
+    web_url: 'http://127.0.0.1:3001/tana_harvey',
+    path: '/tana_harvey',
+  },
+  {
+    id: 20,
+    name: 'Conchita Eichmann',
+    username: 'juliana_gulgowski',
+    state: 'active',
+    avatar_url: 'https://www.gravatar.com/avatar/c43c506cb6fd7b37017d3b54b94aa937?s=80&d=identicon',
+    web_url: 'http://127.0.0.1:3001/juliana_gulgowski',
+    path: '/juliana_gulgowski',
+  },
+  {
+    id: 6,
+    name: 'Bryce Turcotte',
+    username: 'melynda',
+    state: 'active',
+    avatar_url: 'https://www.gravatar.com/avatar/cc2518f2c6f19f8fac49e1a5ee092a9b?s=80&d=identicon',
+    web_url: 'http://127.0.0.1:3001/melynda',
+    path: '/melynda',
+  },
+  {
+    id: 1,
+    name: 'Administrator',
+    username: 'root',
+    state: 'active',
+    avatar_url: 'https://www.gravatar.com/avatar/e64c7d89f26bd1972efa854d13d7dd61?s=80&d=identicon',
+    web_url: 'http://127.0.0.1:3001/root',
+    path: '/root',
+  },
+];
+
+export const mockMilestone = {
+  id: 1,
+  state: 'active',
+  title: 'Milestone title',
+  description: 'Harum corporis aut consequatur quae dolorem error sequi quia.',
+  start_date: '2018-01-01',
+  due_date: '2019-12-31',
 };
