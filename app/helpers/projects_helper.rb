Persper/drepo-gitlab

Reduce N+1 from Activity Dashboard and Banzai
There is a combination of few strategies implemented here:

1. Few relations were eager loaded
2. Changed few polymorphic routes to specific ones so we don't have to
   use `#becomes(Namespace)` which doesn't preserve association cache

app/helpers/projects_helper.rb
@@ -2,7 +2,7 @@
 
 module ProjectsHelper
   def link_to_project(project)
-    link_to [project.namespace.becomes(Namespace), project], title: h(project.name) do
+    link_to admin_namespace_project_path(namespace_id: project.namespace_id, id: project), title: h(project.name) do
       title = content_tag(:span, project.name, class: 'project-name')
 
       if project.namespace

gitlabhq/gitlabhq

app/helpers/projects_helper.rb
@@ -2,7 +2,7 @@
 
 module ProjectsHelper
   def link_to_project(project)
-    link_to [project.namespace.becomes(Namespace), project], title: h(project.name) do
+    link_to namespace_project_path(namespace_id: project.namespace, id: project), title: h(project.name) do
       title = content_tag(:span, project.name, class: 'project-name')
 
       if project.namespace