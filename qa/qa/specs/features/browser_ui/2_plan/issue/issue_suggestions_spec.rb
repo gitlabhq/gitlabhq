17f1cc2067de034315a63d44b2e6a9d8bb2fa152
Ramya Authappan <rauthappan@gitlab.com>
2018-12-18 07:24:08 +0000

    Issue Suggestions E2E Tests

---
+++ qa/qa/specs/features/browser_ui/2_plan/issue/issue_suggestions_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module QA
+  context 'Plan' do
+    describe 'issue suggestions' do
+      let(:issue_title) { 'Issue Lists are awesome' }
+
+      it 'user sees issue suggestions when creating a new issue' do
+        Runtime::Browser.visit(:gitlab, Page::Main::Login)
+        Page::Main::Login.perform(&:sign_in_using_credentials)
+
+        project = Resource::Project.fabricate! do |resource|
+          resource.name = 'project-for-issue-suggestions'
+          resource.description = 'project for issue suggestions'
+        end
+
+        Resource::Issue.fabricate! do |issue|
+          issue.title = issue_title
+          issue.project = project
+        end
+
+        project.visit!
+
+        Page::Project::Show.perform(&:go_to_new_issue)
+        Page::Project::Issue::New.perform do |new_issue_page|
+          new_issue_page.add_title("issue")
+          expect(new_issue_page).to have_content(issue_title)
+
+          new_issue_page.add_title("Issue Board")
+          expect(new_issue_page).not_to have_content(issue_title)
+        end
+      end
+    end
+  end
+end
