3bb3f7ddd073e67fafbc210d0e18c76a87b96dcb
Frederic Van Espen <fes@escaux.com>
2018-12-27 12:37:43 +0100

    fix code style

--- lib/backup/manager.rb
+++ lib/backup/manager.rb
@@ -234,11 +234,11 @@ module Backup
     end
 
     def tar_file
-      if ENV['BACKUP']
-        @tar_file ||= ENV['BACKUP'] + "#{FILE_NAME_SUFFIX}"
-      else
-        @tar_file ||= "#{backup_information[:backup_created_at].strftime('%s_%Y_%m_%d_')}#{backup_information[:gitlab_version]}#{FILE_NAME_SUFFIX}"
-      end
+      @tar_file ||= if ENV['BACKUP']
+                      ENV['BACKUP'] + "#{FILE_NAME_SUFFIX}"
+                    else
+                      "#{backup_information[:backup_created_at].strftime('%s_%Y_%m_%d_')}#{backup_information[:gitlab_version]}#{FILE_NAME_SUFFIX}"
+                    end
     end
 
     def backup_information
