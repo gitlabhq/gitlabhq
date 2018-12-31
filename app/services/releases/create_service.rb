54df1a570186c6e1851b98f268121d9d92c2de8a
Shinya Maeda <shinya@gitlab.com>
2018-12-31 12:05:11 +0900

    Resolve a Cognitive Complexity of 12

--- app/services/releases/create_service.rb
+++ app/services/releases/create_service.rb
@@ -8,24 +8,30 @@ module Releases
       return error('Access Denied', 403) unless allowed?
       return error('Release already exists', 409) if release
 
-      new_tag = nil
+      tag = ensure_tag
 
-      unless tag_exist?
-        return error('Ref is not specified', 422) unless ref
+      return tag unless tag.is_a?(Gitlab::Git::Tag)
 
-        result = Tags::CreateService
-          .new(project, current_user)
-          .execute(tag_name, ref, nil)
-
-        return result unless result[:status] == :success
-
-        new_tag = result[:tag]
-      end
-
-      create_release(existing_tag || new_tag)
+      create_release(tag)
     end
 
     private
+
+    def ensure_tag
+      existing_tag || create_tag
+    end
+
+    def create_tag
+      return error('Ref is not specified', 422) unless ref
+
+      result = Tags::CreateService
+        .new(project, current_user)
+        .execute(tag_name, ref, nil)
+
+      return result unless result[:status] == :success
+
+      result[:tag]
+    end
 
     def allowed?
       Ability.allowed?(current_user, :create_release, project)
