124905e23b6d23253aee461f9d89d434a7b5b44b
Shinya Maeda <shinya@gitlab.com>
2019-01-03 20:37:02 +0900

    Add API for release asset links
    Authorize against release not project

--- lib/api/api.rb
+++ lib/api/api.rb
@@ -141,6 +141,7 @@ module API
     mount ::API::ProtectedBranches
     mount ::API::ProtectedTags
     mount ::API::Releases
+    mount ::API::Release::Links
     mount ::API::Repositories
     mount ::API::Runner
     mount ::API::Runners
