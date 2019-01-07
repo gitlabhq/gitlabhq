ngen-brett/gitlab-ce

Use parent instead of project
Add support for group entities to quick actions

app/services/quick_actions/interpret_service.rb
@@ -2,6 +2,7 @@
 
 module QuickActions
   class InterpretService < BaseService
+    include Gitlab::Utils::StrongMemoize
     include Gitlab::QuickActions::Dsl
 
     attr_reader :issuable
@@ -210,15 +211,9 @@ module QuickActions
     end
     params '~label1 ~"label 2"'
     condition do
-      if project
-        available_labels = LabelsFinder
-          .new(current_user, project_id: project.id, include_ancestor_groups: true)
-          .execute
-      end
-
-      project &&
-        current_user.can?(:"admin_#{issuable.to_ability_name}", project) &&
-        available_labels.any?
+      parent &&
+        current_user.can?(:"admin_#{issuable.to_ability_name}", parent) &&
+        find_labels.any?
     end
     command :label do |labels_param|
       label_ids = find_label_ids(labels_param)
@@ -245,7 +240,7 @@ module QuickActions
       issuable.is_a?(Issuable) &&
         issuable.persisted? &&
         issuable.labels.any? &&
-        current_user.can?(:"admin_#{issuable.to_ability_name}", project)
+        current_user.can?(:"admin_#{issuable.to_ability_name}", parent)
     end
     command :unlabel do |labels_param = nil|
       if labels_param.present?
@@ -674,9 +669,27 @@ module QuickActions
       MilestonesFinder.new(params.merge(project_ids: [project.id], group_ids: [project.group&.id])).execute
     end
 
-    def find_labels(labels_param)
-      extract_references(labels_param, :label) |
-        LabelsFinder.new(current_user, project_id: project.id, name: labels_param.split, include_ancestor_groups: true).execute
+    def parent
+      project || issuable_group
+    end
+
+    def issuable_group
+      strong_memoize(:issuable_group) do
+        issuable.group if issuable.respond_to?(:group)
+      end
+    end
+
+    def find_labels(label_references = nil)
+      labels_params = { include_ancestor_groups: true }
+      labels_params[:project_id] = project.id if project
+      labels_params[:group_id] = issuable_group.id if issuable_group
+      labels_params[:name] = label_references.split if label_references
+
+      result = LabelsFinder.new(current_user, labels_params).execute
+
+      return result unless label_references
+
+      extract_references(label_references, :label) | result
     end
 
     def find_label_references(labels_param)
@@ -709,7 +722,7 @@ module QuickActions
     def extract_references(arg, type)
       ext = Gitlab::ReferenceExtractor.new(project, current_user)
 
-      ext.analyze(arg, author: current_user)
+      ext.analyze(arg, author: current_user, group: issuable_group)
 
       ext.references(type)
     end

gitlabhq/gitlabhq

app/services/quick_actions/interpret_service.rb
@@ -2,6 +2,7 @@
 
 module QuickActions
   class InterpretService < BaseService
+    include Gitlab::Utils::StrongMemoize
     include Gitlab::QuickActions::Dsl
 
     attr_reader :issuable
@@ -210,15 +211,9 @@ module QuickActions
     end
     params '~label1 ~"label 2"'
     condition do
-      if project
-        available_labels = LabelsFinder
-          .new(current_user, project_id: project.id, include_ancestor_groups: true)
-          .execute
-      end
-
-      project &&
-        current_user.can?(:"admin_#{issuable.to_ability_name}", project) &&
-        available_labels.any?
+      parent &&
+        current_user.can?(:"admin_#{issuable.to_ability_name}", parent) &&
+        find_labels.any?
     end
     command :label do |labels_param|
       label_ids = find_label_ids(labels_param)
@@ -245,7 +240,7 @@ module QuickActions
       issuable.is_a?(Issuable) &&
         issuable.persisted? &&
         issuable.labels.any? &&
-        current_user.can?(:"admin_#{issuable.to_ability_name}", project)
+        current_user.can?(:"admin_#{issuable.to_ability_name}", parent)
     end
     command :unlabel do |labels_param = nil|
       if labels_param.present?
@@ -674,9 +669,25 @@ module QuickActions
       MilestonesFinder.new(params.merge(project_ids: [project.id], group_ids: [project.group&.id])).execute
     end
 
-    def find_labels(labels_param)
-      extract_references(labels_param, :label) |
-        LabelsFinder.new(current_user, project_id: project.id, name: labels_param.split, include_ancestor_groups: true).execute
+    def parent
+      project || group
+    end
+
+    def group
+      strong_memoize(:group) do
+        issuable.group if issuable.respond_to?(:group)
+      end
+    end
+
+    def find_labels(labels_params = nil)
+      finder_params = { include_ancestor_groups: true }
+      finder_params[:project_id] = project.id if project
+      finder_params[:group_id] = group.id if group
+      finder_params[:name] = labels_params.split if labels_params
+
+      result = LabelsFinder.new(current_user, finder_params).execute
+
+      extract_references(labels_params, :label) | result
     end
 
     def find_label_references(labels_param)
@@ -707,9 +718,11 @@ module QuickActions
 
     # rubocop: disable CodeReuse/ActiveRecord
     def extract_references(arg, type)
+      return [] unless arg
+
       ext = Gitlab::ReferenceExtractor.new(project, current_user)
 
-      ext.analyze(arg, author: current_user)
+      ext.analyze(arg, author: current_user, group: group)
 
       ext.references(type)
     end