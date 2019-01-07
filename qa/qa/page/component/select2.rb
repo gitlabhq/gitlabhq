a1ca9ad0f12a65009b309448d59dee3b255d8794
Sanad Liaquat <sliaquat@gitlab.com>
2019-01-07 16:57:03 +0500

    CE port of EE MR

--- qa/qa/page/component/select2.rb
+++ qa/qa/page/component/select2.rb
@@ -6,6 +6,12 @@ module QA
           find('.select2-result-label', text: item_text).click
         end
 
+        def clear_current_selection_if_present
+          if has_css?('a > abbr.select2-search-choice-close', wait: 1.0)
+            find('a > abbr.select2-search-choice-close').click
+          end
+        end
+
         def search_and_select(item_text)
           find('.select2-input').set(item_text)
           select_item(item_text)
