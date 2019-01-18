099613f41248f5c6d7ae5f6dfdb7a753e64c30cd
Sanad Liaquat <sliaquat@gitlab.com>
2019-01-18 19:12:00 +0500

    Add qa element

--- qa/qa/page/label/index.rb
+++ qa/qa/page/label/index.rb
@@ -10,12 +10,16 @@ module QA
           element :label_svg
         end
 
+        view 'app/assets/javascripts/lazy_loader.js' do
+          element :js_lazy_loaded
+        end
+
         def go_to_new_label
           # The 'labels.svg' takes a fraction of a second to load after which the "New label" button shifts up a bit
-          # This can cause webdriver to miss the hit so we wait for the svg to load (implicitly with has_css?)
+          # This can cause webdriver to miss the hit so we wait for the svg to load (implicitly with has_element?)
           # before clicking the button.
           within_element(:label_svg) do
-            has_css?('.js-lazy-loaded')
+            has_element?(:js_lazy_loaded)
           end
 
           click_element :label_create_new
