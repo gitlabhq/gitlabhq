10763a9d8845e6fb685418efa8c11bbf7964a87b
Sanad Liaquat <sliaquat@gitlab.com>
2019-01-18 18:10:49 +0500

    Wait for label svg to load

--- qa/qa/page/label/index.rb
+++ qa/qa/page/label/index.rb
@@ -6,7 +6,17 @@ module QA
           element :label_create_new
         end
 
+        view 'app/views/shared/empty_states/_labels.html.haml' do
+          element :label_svg
+        end
+
         def go_to_new_label
+          wait(reload: false) do
+            within_element(:label_svg) do
+              has_css?('.js-lazy-loaded')
+            end
+          end
+
           click_element :label_create_new
         end
       end
