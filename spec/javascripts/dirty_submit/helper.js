ngen-brett/gitlab-ce

Fix DirtySubmit handling of checkbox and radio inputs
Most browsers do not fire the "input" event for checkboxes or radios.
Adds a "change" listener to properly trigger these DirtySubmit updates.

spec/javascripts/dirty_submit/helper.js
@@ -1,25 +1,35 @@
 import DirtySubmitForm from '~/dirty_submit/dirty_submit_form';
 import setTimeoutPromiseHelper from '../helpers/set_timeout_promise_helper';
 
-export function setInput(element, value) {
-  element.value = value;
+export function setInput(input, value) {
+  const target = input;
+  const { type } = input;
+  let eventType;
 
-  element.dispatchEvent(
-    new Event('input', {
+  if (type === 'text') {
+    target.value = value;
+    eventType = 'input';
+  } else if (/(radio|checkbox)/.test(type)) {
+    target.checked = !target.checked;
+    eventType = 'change';
+  }
+
+  target.dispatchEvent(
+    new Event(eventType, {
       bubbles: true,
-      cancelable: true,
     }),
   );
 
   return setTimeoutPromiseHelper(DirtySubmitForm.THROTTLE_DURATION);
 }
 
-export function createForm() {
+export function createForm(type = 'text') {
   const form = document.createElement('form');
   form.innerHTML = `
-    <input type="text" value="original" class="js-input" name="input" />
+    <input type="${type}" name="${type}" class="js-input"/>
     <button type="submit" class="js-dirty-submit"></button>
   `;
+
   const input = form.querySelector('.js-input');
   const submit = form.querySelector('.js-dirty-submit');
 

spec/support/shared_examples/dirty_submit_form_shared_examples.rb
@@ -1,24 +1,36 @@
 shared_examples 'dirty submit form' do |selector_args|
   selectors = selector_args.is_a?(Array) ? selector_args : [selector_args]
 
+  def assert_disabled_state(form, submit, is_disabled = true)
+    disabled_selector = is_disabled == true ? '[disabled]' : ':not([disabled])'
+
+    form.find(".js-dirty-submit#{disabled_selector}", match: :first)
+
+    expect(submit.disabled?).to be is_disabled
+  end
+
   selectors.each do |selector|
-    it "disables #{selector[:form]} submit until there are changes", :js do
+    it "disables #{selector[:form]} submit until there are changes on #{selector[:input]}", :js do
       form = find(selector[:form])
       submit = form.first('.js-dirty-submit')
       input = form.first(selector[:input])
+      is_radio = input[:type] == 'radio'
+      is_checkbox = input[:type] == 'checkbox'
+      is_checkable = is_radio || is_checkbox
       original_value = input.value
+      original_checkable = form.find("input[name='#{input[:name]}'][checked]") if is_radio
+      original_checkable = input if is_checkbox
 
       expect(submit.disabled?).to be true
+      expect(input.checked?).to be false
 
-      input.set("#{original_value} changes")
+      is_checkable ? input.click : input.set("#{original_value} changes")
 
-      form.find('.js-dirty-submit:not([disabled])', match: :first)
-      expect(submit.disabled?).to be false
+      assert_disabled_state(form, submit, false)
 
-      input.set(original_value)
+      is_checkable ? original_checkable.click : input.set(original_value)
 
-      form.find('.js-dirty-submit[disabled]', match: :first)
-      expect(submit.disabled?).to be true
+      assert_disabled_state(form, submit)
     end
   end
 end

gitlabhq/gitlabhq

spec/javascripts/dirty_submit/helper.js
@@ -1,25 +1,42 @@
 import DirtySubmitForm from '~/dirty_submit/dirty_submit_form';
 import setTimeoutPromiseHelper from '../helpers/set_timeout_promise_helper';
 
-export function setInput(element, value) {
-  element.value = value;
+function isCheckableType(type) {
+  return /^(radio|checkbox)$/.test(type);
+}
+
+export function setInputValue(element, value) {
+  const { type } = element;
+  let eventType;
+
+  if (isCheckableType(type)) {
+    element.checked = !element.checked;
+    eventType = 'change';
+  } else {
+    element.value = value;
+    eventType = 'input';
+  }
 
   element.dispatchEvent(
-    new Event('input', {
+    new Event(eventType, {
       bubbles: true,
-      cancelable: true,
     }),
   );
 
   return setTimeoutPromiseHelper(DirtySubmitForm.THROTTLE_DURATION);
 }
 
-export function createForm() {
+export function getInputValue(input) {
+  return isCheckableType(input.type) ? input.checked : input.value;
+}
+
+export function createForm(type = 'text') {
   const form = document.createElement('form');
   form.innerHTML = `
-    <input type="text" value="original" class="js-input" name="input" />
+    <input type="${type}" name="${type}" class="js-input"/>
     <button type="submit" class="js-dirty-submit"></button>
   `;
+
   const input = form.querySelector('.js-input');
   const submit = form.querySelector('.js-dirty-submit');
 

spec/support/shared_examples/dirty_submit_form_shared_examples.rb
@@ -1,24 +1,36 @@
 shared_examples 'dirty submit form' do |selector_args|
   selectors = selector_args.is_a?(Array) ? selector_args : [selector_args]
 
+  def expect_disabled_state(form, submit, is_disabled = true)
+    disabled_selector = is_disabled == true ? '[disabled]' : ':not([disabled])'
+
+    form.find(".js-dirty-submit#{disabled_selector}", match: :first)
+
+    expect(submit.disabled?).to be is_disabled
+  end
+
   selectors.each do |selector|
-    it "disables #{selector[:form]} submit until there are changes", :js do
+    it "disables #{selector[:form]} submit until there are changes on #{selector[:input]}", :js do
       form = find(selector[:form])
       submit = form.first('.js-dirty-submit')
       input = form.first(selector[:input])
+      is_radio = input[:type] == 'radio'
+      is_checkbox = input[:type] == 'checkbox'
+      is_checkable = is_radio || is_checkbox
       original_value = input.value
+      original_checkable = form.find("input[name='#{input[:name]}'][checked]") if is_radio
+      original_checkable = input if is_checkbox
 
       expect(submit.disabled?).to be true
+      expect(input.checked?).to be false
 
-      input.set("#{original_value} changes")
+      is_checkable ? input.click : input.set("#{original_value} changes")
 
-      form.find('.js-dirty-submit:not([disabled])', match: :first)
-      expect(submit.disabled?).to be false
+      expect_disabled_state(form, submit, false)
 
-      input.set(original_value)
+      is_checkable ? original_checkable.click : input.set(original_value)
 
-      form.find('.js-dirty-submit[disabled]', match: :first)
-      expect(submit.disabled?).to be true
+      expect_disabled_state(form, submit)
     end
   end
 end