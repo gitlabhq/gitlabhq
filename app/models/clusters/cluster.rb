ngen-brett/gitlab-ce

Add domain field into Clusters::Cluster
This is the 1st step for moving Auto DevOps domain into cluster
settings, whether is project or group.

Related to https://gitlab.com/gitlab-org/gitlab-ce/issues/52363

app/models/clusters/cluster.rb
@@ -49,8 +49,9 @@ module Clusters
 
     validates :name, cluster_name: true
     validates :cluster_type, presence: true
-    validate :restrict_modification, on: :update
+    validates :domain, allow_blank: true, hostname: { allow_numeric_hostname: true, require_valid_tld: true }
 
+    validate :restrict_modification, on: :update
     validate :no_groups, unless: :group_type?
     validate :no_projects, unless: :project_type?
 

gitlabhq/gitlabhq

app/models/clusters/cluster.rb
@@ -49,8 +49,9 @@ module Clusters
 
     validates :name, cluster_name: true
     validates :cluster_type, presence: true
-    validate :restrict_modification, on: :update
+    validates :domain, allow_nil: true, hostname: { allow_numeric_hostname: true, require_valid_tld: true }
 
+    validate :restrict_modification, on: :update
     validate :no_groups, unless: :group_type?
     validate :no_projects, unless: :project_type?
 