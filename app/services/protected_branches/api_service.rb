4b868ba8e71be9aa5591378555122d76c27ac777
Francisco Javier López <fjlopez@gitlab.com>
2019-01-25 12:11:36 +0000

    [master] Check access rights when creating/updating ProtectedRefs

--- app/services/protected_branches/api_service.rb
+++ app/services/protected_branches/api_service.rb
@@ -6,8 +6,6 @@ module ProtectedBranches
       @push_params = AccessLevelParams.new(:push, params)
       @merge_params = AccessLevelParams.new(:merge, params)
 
-      verify_params!
-
       protected_branch_params = {
         name: params[:name],
         push_access_levels_attributes: @push_params.access_levels,
@@ -15,12 +13,6 @@ module ProtectedBranches
       }
 
       ::ProtectedBranches::CreateService.new(@project, @current_user, protected_branch_params).execute
-    end
-
-    private
-
-    def verify_params!
-      # EE-only
     end
   end
 end
