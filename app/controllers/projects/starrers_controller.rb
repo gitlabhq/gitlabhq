936d4e80e4abc2efcc5d88865d5d420c42a84370
Camil Staps <info@camilstaps.nl>
2019-01-27 10:33:51 +0100

    Add starring date to overview of starrers

--- app/controllers/projects/starrers_controller.rb
+++ app/controllers/projects/starrers_controller.rb
@@ -2,16 +2,19 @@
 
 class Projects::StarrersController < Projects::ApplicationController
   include SortingHelper
-  #
+
   # Authorize
   before_action :require_non_empty_project
 
+  # rubocop: disable CodeReuse/ActiveRecord
   def index
     @sort = params[:sort].presence || sort_value_name
 
     params[:has_starred] = @project
 
     @starrers = UsersFinder.new(current_user, params).execute
+    @starrers = @starrers.joins(:users_star_projects).select('"users".*, "users_star_projects"."created_at" as "starred_since"')
     @starrers = @starrers.sort_by_attribute(@sort)
   end
+  # rubocop: enable CodeReuse/ActiveRecord
 end
