b7b57b83cc294727736c2276bd72a7e47c313e68
Camil Staps <info@camilstaps.nl>
2019-01-28 19:43:31 +0100

    Avoid unnecessary includes in StarredProjectsFinder

--- app/finders/starred_projects_finder.rb
+++ app/finders/starred_projects_finder.rb
@@ -2,7 +2,7 @@
 
 class StarredProjectsFinder < ProjectsFinder
   def initialize(user, params: {}, current_user: nil)
-    project_ids = user.starred_projects.includes(:creator).select(:id)
+    project_ids = user.starred_projects.select(:id)
     super(params: params, current_user: current_user, project_ids_relation: project_ids)
   end
 end
