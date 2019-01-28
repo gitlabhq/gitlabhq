d022ce865502c051c9177fc0f5b2453eae7e7cc2
Andrew Newdigate <andrew@gitlab.com>
2019-01-28 15:47:48 +0200

    Add OpenTracing instrumentation for Action View Render events
    This change adds three new instrumentations, driven through rails
    notifications: render_template.action_view,
    render_collection.action_view and render_partial.action_view.
    
    These can help developers understand why renders are taking a long
    time which may in turn help them to improve their performance.

--- lib/gitlab/tracing/rails/active_record_subscriber.rb
+++ lib/gitlab/tracing/rails/active_record_subscriber.rb
@@ -4,24 +4,37 @@ module Gitlab
   module Tracing
     module Rails
       class ActiveRecordSubscriber
-        include Gitlab::Tracing::Common
+        include RailsCommon
 
         ACTIVE_RECORD_NOTIFICATION_TOPIC = 'sql.active_record'
-        DEFAULT_OPERATION_NAME = "sqlquery"
+        OPERATION_NAME_PREFIX = 'active_record:'
+        DEFAULT_OPERATION_NAME = 'sqlquery'
 
+        # Instruments Rails ActiveRecord events for opentracing.
+        # Returns a lambda, which, when called will unsubscribe from the notifications
         def self.instrument
           subscriber = new
 
-          ActiveSupport::Notifications.subscribe(ACTIVE_RECORD_NOTIFICATION_TOPIC) do |_, start, finish, _, payload|
+          subscription = ActiveSupport::Notifications.subscribe(ACTIVE_RECORD_NOTIFICATION_TOPIC) do |_, start, finish, _, payload|
             subscriber.notify(start, finish, payload)
           end
+
+          create_unsubscriber [subscription]
         end
 
         # For more information on the payloads: https://guides.rubyonrails.org/active_support_instrumentation.html
         def notify(start, finish, payload)
-          operation_name = payload[:name].presence || DEFAULT_OPERATION_NAME
-          exception = payload[:exception]
-          tags = {
+          generate_span_for_notification(notification_name(payload), start, finish, payload, tags_for_notification(payload))
+        end
+
+        private
+
+        def notification_name(payload)
+          OPERATION_NAME_PREFIX + (payload[:name].presence || DEFAULT_OPERATION_NAME)
+        end
+
+        def tags_for_notification(payload)
+          {
             'component' =>        'ActiveRecord',
             'span.kind' =>        'client',
             'db.type' =>          'sql',
@@ -29,8 +42,6 @@ module Gitlab
             'db.cached' =>        payload[:cached] || false,
             'db.statement' =>     payload[:sql]
           }
-
-          postnotify_span("active_record:#{operation_name}", start, finish, tags: tags, exception: exception)
         end
       end
     end
