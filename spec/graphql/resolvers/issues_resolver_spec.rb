ee60128cefae3122f21987b72c60a268ddc8f6b9
Lin Jen-Shin <godfat@godfat.org>
2019-01-31 01:18:18 +0800

    Add argument iids to IssuesResolver

--- spec/graphql/resolvers/issues_resolver_spec.rb
+++ spec/graphql/resolvers/issues_resolver_spec.rb
@@ -32,6 +32,26 @@ describe Resolvers::IssuesResolver do
 
       expect(resolve_issues).to contain_exactly(issue, issue2)
     end
+
+    it 'finds a specific issue with iids' do
+      expect(resolve_issues(iids: issue.iid)).to contain_exactly(issue)
+    end
+
+    it 'finds multiple issues with iids' do
+      expect(resolve_issues(iids: [issue.iid, issue2.iid]))
+        .to contain_exactly(issue, issue2)
+    end
+
+    it 'finds only the issues within the project we are looking at' do
+      another_project = create(:project)
+      iids = [issue, issue2].map(&:iid)
+
+      iids.each do |iid|
+        create(:issue, project: another_project, iid: iid)
+      end
+
+      expect(resolve_issues(iids: iids)).to contain_exactly(issue, issue2)
+    end
   end
 
   def resolve_issues(args = {}, context = { current_user: current_user })
