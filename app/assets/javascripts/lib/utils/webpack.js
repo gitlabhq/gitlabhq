6942aa5f1a497d84be8ebc82b7482911953a9b9c
Filipa Lacerda <filipa@gitlab.com>
2019-02-08 15:08:48 +0000

    Merge branch 'mr-web-worker-origin' into 'master'
    Fixes diff web worker not loading in staging
    
    See merge request gitlab-org/gitlab-ce!25039
    
    (cherry picked from commit 171d919c19693c12e36dc6485c40a8abaf594e88)
    
    37b860d7 Fixes diff web worker not loading in staging

---
+++ app/assets/javascripts/lib/utils/webpack.js
@@ -0,0 +1,10 @@
+// tell webpack to load assets from origin so that web workers don't break
+// eslint-disable-next-line import/prefer-default-export
+export function resetServiceWorkersPublicPath() {
+  // __webpack_public_path__ is a global variable that can be used to adjust
+  // the webpack publicPath setting at runtime.
+  // see: https://webpack.js.org/guides/public-path/
+  const relativeRootPath = (gon && gon.relative_url_root) || '';
+  const webpackAssetPath = `${relativeRootPath}/assets/webpack/`;
+  __webpack_public_path__ = webpackAssetPath; // eslint-disable-line camelcase
+}
