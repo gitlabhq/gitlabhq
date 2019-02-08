4dc05b8f44886642893b499734d5c4df25ab195d
Andrew Fontaine <afontaine@gitlab.com>
2019-02-08 16:09:24 +0000

    Update `chart.js` to 2.7.2

--- app/assets/javascripts/pages/projects/pipelines/charts/index.js
+++ app/assets/javascripts/pages/projects/pipelines/charts/index.js
@@ -1,29 +1,31 @@
 import $ from 'jquery';
 import Chart from 'chart.js';
 
-const options = {
-  scaleOverlay: true,
-  responsive: true,
-  maintainAspectRatio: false,
-};
+import { barChartOptions, lineChartOptions } from '~/lib/utils/chart_utils';
 
-const buildChart = chartScope => {
+const SUCCESS_LINE_COLOR = '#1aaa55';
+
+const TOTAL_LINE_COLOR = '#707070';
+
+const buildChart = (chartScope, shouldAdjustFontSize) => {
   const data = {
     labels: chartScope.labels,
     datasets: [
       {
-        fillColor: '#707070',
-        strokeColor: '#707070',
-        pointColor: '#707070',
-        pointStrokeColor: '#EEE',
-        data: chartScope.totalValues,
+        backgroundColor: SUCCESS_LINE_COLOR,
+        borderColor: SUCCESS_LINE_COLOR,
+        pointBackgroundColor: SUCCESS_LINE_COLOR,
+        pointBorderColor: '#fff',
+        data: chartScope.successValues,
+        fill: 'origin',
       },
       {
-        fillColor: '#1aaa55',
-        strokeColor: '#1aaa55',
-        pointColor: '#1aaa55',
-        pointStrokeColor: '#fff',
-        data: chartScope.successValues,
+        backgroundColor: TOTAL_LINE_COLOR,
+        borderColor: TOTAL_LINE_COLOR,
+        pointBackgroundColor: TOTAL_LINE_COLOR,
+        pointBorderColor: '#EEE',
+        data: chartScope.totalValues,
+        fill: '-1',
       },
     ],
   };
@@ -31,36 +33,51 @@ const buildChart = chartScope => {
     .get(0)
     .getContext('2d');
 
-  new Chart(ctx).Line(data, options);
+  return new Chart(ctx, {
+    type: 'line',
+    data,
+    options: lineChartOptions({
+      width: ctx.canvas.width,
+      numberOfPoints: chartScope.totalValues.length,
+      shouldAdjustFontSize,
+    }),
+  });
 };
 
-document.addEventListener('DOMContentLoaded', () => {
-  const chartTimesData = JSON.parse(document.getElementById('pipelinesTimesChartsData').innerHTML);
-  const chartsData = JSON.parse(document.getElementById('pipelinesChartsData').innerHTML);
+const buildBarChart = (chartTimesData, shouldAdjustFontSize) => {
   const data = {
     labels: chartTimesData.labels,
     datasets: [
       {
-        fillColor: 'rgba(220,220,220,0.5)',
-        strokeColor: 'rgba(220,220,220,1)',
-        barStrokeWidth: 1,
+        backgroundColor: 'rgba(220,220,220,0.5)',
+        borderColor: 'rgba(220,220,220,1)',
+        borderWidth: 1,
         barValueSpacing: 1,
         barDatasetSpacing: 1,
         data: chartTimesData.values,
       },
     ],
   };
-
-  if (window.innerWidth < 768) {
-    // Scale fonts if window width lower than 768px (iPad portrait)
-    options.scaleFontSize = 8;
-  }
-
-  new Chart(
+  return new Chart(
     $('#build_timesChart')
       .get(0)
       .getContext('2d'),
-  ).Bar(data, options);
+    {
+      type: 'bar',
+      data,
+      options: barChartOptions(shouldAdjustFontSize),
+    },
+  );
+};
 
-  chartsData.forEach(scope => buildChart(scope));
+document.addEventListener('DOMContentLoaded', () => {
+  const chartTimesData = JSON.parse(document.getElementById('pipelinesTimesChartsData').innerHTML);
+  const chartsData = JSON.parse(document.getElementById('pipelinesChartsData').innerHTML);
+
+  // Scale fonts if window width lower than 768px (iPad portrait)
+  const shouldAdjustFontSize = window.innerWidth < 768;
+
+  buildBarChart(chartTimesData, shouldAdjustFontSize);
+
+  chartsData.forEach(scope => buildChart(scope, shouldAdjustFontSize));
 });
