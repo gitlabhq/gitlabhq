7be1f0842f281305f2c36e2b6066842d8e87875c
Lin Jen-Shin <godfat@godfat.org>
2019-02-12 21:51:10 +0800

    Add tests for BaseResolver and update accordingly

--- spec/graphql/types/project_type_spec.rb
+++ spec/graphql/types/project_type_spec.rb
@@ -6,11 +6,16 @@ describe GitlabSchema.types['Project'] do
   it { expect(described_class.graphql_name).to eq('Project') }
 
   describe 'nested merge request' do
+    it { expect(described_class).to have_graphql_field(:merge_requests) }
     it { expect(described_class).to have_graphql_field(:merge_request) }
 
     it 'authorizes the merge request' do
       expect(described_class.fields['mergeRequest'])
         .to require_graphql_authorizations(:read_merge_request)
+    end
+
+    it 'authorizes the merge requests' do
+      skip
     end
   end
 
