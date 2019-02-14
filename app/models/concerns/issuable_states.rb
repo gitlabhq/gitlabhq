9671a67a4ce58683ca0188ff9e75b1d5dfcc5dec
Felipe Artur <felipefac@gmail.com>
2019-02-14 16:33:26 -0200

    Fix broken specs

--- app/models/concerns/issuable_states.rb
+++ app/models/concerns/issuable_states.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 module IssuableStates
   extend ActiveSupport::Concern
 
   # The state:string column is being migrated to state_id:integer column
   # This is a temporary hook to populate state_id column with new values
-  # and can be removed after the state column is removed.
+  # and should be removed after the state column is removed.
   # Check https://gitlab.com/gitlab-org/gitlab-ce/issues/51789 for more information
   included do
     before_save :set_state_id
@@ -11,6 +13,12 @@ module IssuableStates
 
   def set_state_id
     return if state.nil? || state.empty?
+
+    # Needed to prevent breaking some migration specs that
+    # rollback database to a point where state_id does not exist.
+    # We can use this guard clause for now since this file will should
+    # be removed in the next release.
+    return unless self.respond_to?(:state_id)
 
     states_hash = self.class.available_states
 
--- db/migrate/20190211131150_add_state_id_to_issuables.rb
+++ db/migrate/20190211131150_add_state_id_to_issuables.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddStateIdToIssuables < ActiveRecord::Migration[5.0]
   include Gitlab::Database::MigrationHelpers
 
