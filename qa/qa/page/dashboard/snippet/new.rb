e6c24bdbaede5e6f7fa6a50927841200e6a6a0ef
Roman Verevkin <verrom@yandex-team.ru>
2019-02-19 23:23:34 +0300

    Add a new test to create a snippet
    Add "smoke" tag to the test
    
    Fix links to views in Page Object files
    
    Review fixes
    
    Remove useless view block
    
    Implement Snippets::Show page object class
    
    Fix review suggestions
    
    Fix review suggestions 2
    
    Fix test with empty snippets state
    
    Remove useless css class
    
    Fix issues

---
+++ qa/qa/page/dashboard/snippet/new.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+module QA
+  module Page
+    module Dashboard
+      module Snippet
+        class New < Page::Base
+          view 'app/views/shared/form_elements/_description.html.haml' do
+            element :issuable_form_description
+          end
+
+          view 'app/views/shared/snippets/_form.html.haml' do
+            element :snippet_title
+            element :snippet_file_name
+            element :create_snippet_button
+          end
+
+          def fill_title(title)
+            fill_element :snippet_title, title
+          end
+
+          def fill_description(description)
+            fill_element :issuable_form_description, description
+          end
+
+          def set_visibility(visibility)
+            choose visibility
+          end
+
+          def fill_file_name(name)
+            finished_loading?
+            fill_element :snippet_file_name, name
+          end
+
+          def fill_file_content(content)
+            finished_loading?
+            text_area.set content
+          end
+
+          def create_snippet
+            click_element :create_snippet_button
+          end
+
+          private
+
+          def text_area
+            find('#editor>textarea', visible: false)
+          end
+        end
+      end
+    end
+  end
+end
