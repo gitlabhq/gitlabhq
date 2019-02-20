2d19b1adef1fd880c3d49f307ff8d5317d31d94a
James Fargher <proglottis@gmail.com>
2019-02-20 21:29:48 +0000

    Move ChatOps to Core
    ChatOps used to be in the Ultimate tier.

--- app/workers/build_finished_worker.rb
+++ app/workers/build_finished_worker.rb
@@ -30,5 +30,6 @@ class BuildFinishedWorker
     # We execute these async as these are independent operations.
     BuildHooksWorker.perform_async(build.id)
     ArchiveTraceWorker.perform_async(build.id)
+    ChatNotificationWorker.perform_async(build.id) if build.pipeline.chat?
   end
 end
--- spec/workers/build_finished_worker_spec.rb
+++ spec/workers/build_finished_worker_spec.rb
@@ -26,5 +26,24 @@ describe BuildFinishedWorker do
           .not_to raise_error
       end
     end
+
+    it 'schedules a ChatNotification job for a chat build' do
+      build = create(:ci_build, :success, pipeline: create(:ci_pipeline, source: :chat))
+
+      expect(ChatNotificationWorker)
+        .to receive(:perform_async)
+        .with(build.id)
+
+      described_class.new.perform(build.id)
+    end
+
+    it 'does not schedule a ChatNotification job for a regular build' do
+      build = create(:ci_build, :success, pipeline: create(:ci_pipeline))
+
+      expect(ChatNotificationWorker)
+        .not_to receive(:perform_async)
+
+      described_class.new.perform(build.id)
+    end
   end
 end
