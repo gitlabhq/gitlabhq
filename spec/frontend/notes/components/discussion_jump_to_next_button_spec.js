db735b66a0b19ab4f1f5048cf308bf588b64861e
Winnie Hellmann <winnie@gitlab.com>
2019-02-20 21:36:27 +0100

    Add snapshot test to discussion_jump_to_next_button_spec.js

--- spec/frontend/notes/components/discussion_jump_to_next_button_spec.js
+++ spec/frontend/notes/components/discussion_jump_to_next_button_spec.js
@@ -1,17 +1,21 @@
-import jumpToNextDiscussionButton from '~/notes/components/discussion_jump_to_next_button.vue';
+import JumpToNextDiscussionButton from '~/notes/components/discussion_jump_to_next_button.vue';
 import { shallowMount } from '@vue/test-utils';
 
-describe('jumpToNextDiscussionButton', () => {
+describe('JumpToNextDiscussionButton', () => {
   let wrapper;
 
   beforeEach(() => {
-    wrapper = shallowMount(jumpToNextDiscussionButton, {
+    wrapper = shallowMount(JumpToNextDiscussionButton, {
       sync: false,
     });
   });
 
   afterEach(() => {
     wrapper.destroy();
+  });
+
+  it('matches the snapshot', () => {
+    expect(wrapper.vm.$el).toMatchSnapshot();
   });
 
   it('emits onClick event on button click', () => {
