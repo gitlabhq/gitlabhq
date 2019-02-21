536463295c2598505135bc61570ea66e1bad6496
Reuben Pereira <rpereira@gitlab.com>
2019-02-21 11:19:51 +0000

    Catch exception in calculate reactive cache
    Return the exception as an error.

--- spec/services/error_tracking/list_issues_service_spec.rb
+++ spec/services/error_tracking/list_issues_service_spec.rb
@@ -45,7 +45,23 @@ describe ErrorTracking::ListIssuesService do
 
         it 'result is not ready' do
           expect(result).to eq(
-            status: :error, http_status: :no_content, message: 'not ready')
+            status: :error, http_status: :no_content, message: 'Not ready. Try again later')
+        end
+      end
+
+      context 'when list_sentry_issues returns error' do
+        before do
+          allow(error_tracking_setting)
+            .to receive(:list_sentry_issues)
+            .and_return(error: 'Sentry response status code: 401')
+        end
+
+        it 'returns the error' do
+          expect(result).to eq(
+            status: :error,
+            http_status: :bad_request,
+            message: 'Sentry response status code: 401'
+          )
         end
       end
     end
@@ -58,7 +74,11 @@ describe ErrorTracking::ListIssuesService do
       it 'returns error' do
         result = subject.execute
 
-        expect(result).to include(status: :error, message: 'access denied')
+        expect(result).to include(
+          status: :error,
+          message: 'Access denied',
+          http_status: :unauthorized
+        )
       end
     end
 
@@ -70,7 +90,7 @@ describe ErrorTracking::ListIssuesService do
       it 'raises error' do
         result = subject.execute
 
-        expect(result).to include(status: :error, message: 'not enabled')
+        expect(result).to include(status: :error, message: 'Error Tracking is not enabled')
       end
     end
   end
