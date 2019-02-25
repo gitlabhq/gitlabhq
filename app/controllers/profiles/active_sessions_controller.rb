8343a1ac1be60308c29e899b2f1d6beab1f981a0
Imre Farkas <ifarkas@gitlab.com>
2019-02-25 14:52:40 +0100

    Remove ability to revoke active session
    Session ID is used as a parameter for the revoke session endpoint but it
    should never be included in the HTML as an attacker could obtain it via
    XSS.

--- app/controllers/profiles/active_sessions_controller.rb
+++ app/controllers/profiles/active_sessions_controller.rb
@@ -4,13 +4,4 @@ class Profiles::ActiveSessionsController < Profiles::ApplicationController
   def index
     @sessions = ActiveSession.list(current_user).reject(&:is_impersonated)
   end
-
-  def destroy
-    ActiveSession.destroy(current_user, params[:id])
-
-    respond_to do |format|
-      format.html { redirect_to profile_active_sessions_url, status: :found }
-      format.js { head :ok }
-    end
-  end
 end
