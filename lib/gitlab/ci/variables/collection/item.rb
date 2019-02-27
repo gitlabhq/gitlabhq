316889cb4789e8a4a43bf0c79a4269643a97c336
Matija Čupić <matteeyah@gmail.com>
2019-02-27 01:22:51 +0100

    Revert "Merge branch 'revert-8baf9e5f' into 'master'"
    This reverts commit f5201a816f2eff9393e16f362403451e5d86ee6c, reversing
    changes made to 48e6db0dad6f256e8423e0bd6c9b254803f50ccf.

--- lib/gitlab/ci/variables/collection/item.rb
+++ lib/gitlab/ci/variables/collection/item.rb
@@ -5,12 +5,12 @@ module Gitlab
     module Variables
       class Collection
         class Item
-          def initialize(key:, value:, public: true, file: false)
+          def initialize(key:, value:, public: true, file: false, masked: false)
             raise ArgumentError, "`#{key}` must be of type String or nil value, while it was: #{value.class}" unless
               value.is_a?(String) || value.nil?
 
             @variable = {
-              key: key, value: value, public: public, file: file
+              key: key, value: value, public: public, file: file, masked: masked
             }
           end
 
@@ -27,9 +27,13 @@ module Gitlab
           # don't expose `file` attribute at all (stems from what the runner
           # expects).
           #
+          # If the `variable_masking` feature is enabled we expose the `masked`
+          # attribute, otherwise it's not exposed.
+          #
           def to_runner_variable
             @variable.reject do |hash_key, hash_value|
-              hash_key == :file && hash_value == false
+              (hash_key == :file && hash_value == false) ||
+                (hash_key == :masked && !Feature.enabled?(:variable_masking))
             end
           end
 
