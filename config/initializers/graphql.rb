58aaa766a44b8b2105f59bf7fcc5a26447bdf286
Bob Van Landuyt <bob@vanlanduyt.co>
2019-03-01 18:30:42 +0100

    Types::BaseField accepts authorize after reload
    When working on files in `app/graphql` the code correctly gets
    autoloaded.
    
    However, the redefined `Types::BaseField` would never receive the
    `.accepts_definition` call, causing all queries after a reload to
    fail. Calling `.accepts_definition` on its superclass makes sure the
    reloaded version also has the `authorize` definition specified.

--- config/initializers/graphql.rb
+++ config/initializers/graphql.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 
 GraphQL::Field.accepts_definitions(authorize: GraphQL::Define.assign_metadata_key(:authorize))
-Types::BaseField.accepts_definition(:authorize)
+GraphQL::Schema::Field.accepts_definition(:authorize)
