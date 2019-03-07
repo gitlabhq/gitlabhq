461e3979691ade5bac7b882ac2c14b4d3d229676
Patrick Bajao <ebajao@gitlab.com>
2019-03-07 16:51:58 +0800

    Move empty_repo check on its own

--- spec/lib/gitlab/checks/branch_check_spec.rb
+++ spec/lib/gitlab/checks/branch_check_spec.rb
@@ -48,10 +48,28 @@ describe Gitlab::Checks::BranchCheck do
       context 'when project repository is empty' do
         let(:project) { create(:project) }
 
-        it 'raises an error if the user is not allowed to push to protected branches' do
-          expect(user_access).to receive(:can_push_to_branch?).and_return(false)
+        context 'user is not allowed to push to protected branches' do
+          before do
+            allow(user_access)
+              .to receive(:can_push_to_branch?)
+              .and_return(false)
+          end
 
-          expect { subject.validate! }.to raise_error(Gitlab::GitAccess::UnauthorizedError, /Ask a project Owner or Maintainer to create a default branch/)
+          it 'raises an error' do
+            expect { subject.validate! }.to raise_error(Gitlab::GitAccess::UnauthorizedError, /Ask a project Owner or Maintainer to create a default branch/)
+          end
+        end
+
+        context 'user is allowed to push to protected branches' do
+          before do
+            allow(user_access)
+              .to receive(:can_push_to_branch?)
+              .and_return(true)
+          end
+
+          it 'allows branch creation' do
+            expect { subject.validate! }.not_to raise_error
+          end
         end
       end
 
@@ -114,21 +132,6 @@ describe Gitlab::Checks::BranchCheck do
                 .to receive(:branch_names_contains_sha)
                 .with(newrev)
                 .and_return(['branch'])
-            end
-
-            context "when repo is empty" do
-              let(:project) { create(:project, :empty_repo) }
-              let(:ref) { 'refs/heads/master' }
-
-              before do
-                allow(user_access)
-                  .to receive(:can_push_to_branch?)
-                  .and_return(true)
-              end
-
-              it 'allows branch creation' do
-                expect { subject.validate! }.not_to raise_error
-              end
             end
 
             context "newrev isn't in any protected branches" do
