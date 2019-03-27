f458c561070d754cd546b07caf60dfa7ffb06293
Brett Walker <bwalker@gitlab.com>
2019-03-27 15:02:25 -0500

    Initial field and query complexity limits
    It makes all Types::BaseField default to a complexity of 1.
    
    Queries themselves now have limited complexity, scaled
    to the type of user: no user, authenticated user, or an
    admin user.

--- app/graphql/types/base_field.rb
+++ app/graphql/types/base_field.rb
@@ -3,5 +3,14 @@
 module Types
   class BaseField < GraphQL::Schema::Field
     prepend Gitlab::Graphql::Authorize
+
+    DEFAULT_COMPLEXITY = 1
+
+    def initialize(*args, **kwargs, &block)
+      # complexity is already defaulted to 1, but let's make it explicit
+      kwargs[:complexity] ||= DEFAULT_COMPLEXITY
+
+      super(*args, **kwargs, &block)
+    end
   end
 end
