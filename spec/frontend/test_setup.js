346f7027e2285b3ff1280197045c4db541f4a700
Winnie Hellmann <winnie@gitlab.com>
2019-03-28 20:42:32 +0100

    Add workaround for innerText in Jest

--- spec/frontend/test_setup.js
+++ spec/frontend/test_setup.js
@@ -23,3 +23,12 @@ beforeEach(done => {
 });
 
 Vue.use(Translate);
+
+// workaround for JSDOM not supporting innerText
+// see https://github.com/jsdom/jsdom/issues/1245
+Object.defineProperty(global.Element.prototype, 'innerText', {
+  get() {
+    return this.textContent;
+  },
+  configurable: true, // make it so that it doesn't blow chunks on re-running tests with things like --watch
+});
