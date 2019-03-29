cedbb3366bf3dd9bafe95dde366c1e28ee70c615
Stan Hu <stanhu@gmail.com>
2019-03-29 15:16:31 -0700

    Fix API /project/:id/branches not returning correct merge status
    https://gitlab.com/gitlab-org/gitlab-ce/merge_requests/24034 introduced
    a regression where only the first 20 branches were used to determine
    whether a branch has been merged because the pagination was applied
    incorrectly. Requesting the second page of branches via the API would
    always have the wrong merge status. We fix this by properly paginating
    the branches before requesting their merge status.
    
    Closes https://gitlab.com/gitlab-org/gitlab-ce/issues/56250

--- spec/requests/api/branches_spec.rb
+++ spec/requests/api/branches_spec.rb
@@ -36,10 +36,30 @@ describe API::Branches do
         expect(branch_names).to match_array(project.repository.branch_names)
       end
 
+      def check_merge_status(json_response)
+        merged, unmerged = json_response.partition { |branch| branch['merged'] }
+        merged_branches = merged.map { |branch| branch['name'] }
+        unmerged_branches = unmerged.map { |branch| branch['name'] }
+        expect(Set.new(merged_branches)).to eq(project.repository.merged_branch_names(merged_branches + unmerged_branches))
+        expect(project.repository.merged_branch_names(unmerged_branches)).to be_empty
+      end
+
       it 'determines only a limited number of merged branch names' do
-        expect(API::Entities::Branch).to receive(:represent).with(anything, has_merged_branch_names_count(2))
+        expect(API::Entities::Branch).to receive(:represent).with(anything, has_merged_branch_names_count(1)).and_call_original
 
         get api(route, current_user), params: { per_page: 2 }
+
+        expect(response).to have_gitlab_http_status(200)
+
+        check_merge_status(json_response)
+      end
+
+      it 'merge status matches reality on paginated input' do
+        get api(route, current_user), params: { per_page: 20, page: 2 }
+
+        expect(response).to have_gitlab_http_status(200)
+
+        check_merge_status(json_response)
       end
 
       context 'when repository is disabled' do
