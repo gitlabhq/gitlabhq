mehulagg/gitlab_test

Extend quick actions dsl
* Extend quick actions dsl to allow specifying model types
to which given quick action applies
* Rename issuable attribute to noteable in interpret_service.rb
* Refactor and reorder quick action specs
* Create a shared example spec for each quick action to be used for
issue, merge request and commit specs.

https://gitlab.com/gitlab-org/gitlab-ce/issues/50199

spec/lib/gitlab/quick_actions/command_definition_spec.rb
@@ -69,6 +69,36 @@ describe Gitlab::QuickActions::CommandDefinition do
         expect(subject.available?(opts)).to be true
       end
     end
+
+    context "when the command has types" do
+      before do
+        subject.types = [Issue, Commit]
+      end
+
+      context "when the command targed type is allowed" do
+        it "returns true" do
+          opts[:quick_action_target] = Issue.new
+          expect(subject.available?(opts)).to be true
+        end
+      end
+
+      context "when the command targed type is not allowed" do
+        it "returns true" do
+          opts[:quick_action_target] = MergeRequest.new
+          expect(subject.available?(opts)).to be false
+        end
+      end
+    end
+
+    context "when the command has no types" do
+      it "any targed type is allowed" do
+        opts[:quick_action_target] = Issue.new
+        expect(subject.available?(opts)).to be true
+
+        opts[:quick_action_target] = MergeRequest.new
+        expect(subject.available?(opts)).to be true
+      end
+    end
   end
 
   describe "#execute" do

gitlabhq/gitlabhq

spec/lib/gitlab/quick_actions/command_definition_spec.rb
@@ -69,6 +69,36 @@ describe Gitlab::QuickActions::CommandDefinition do
         expect(subject.available?(opts)).to be true
       end
     end
+
+    context "when the command has types" do
+      before do
+        subject.types = [Issue, Commit]
+      end
+
+      context "when the command target type is allowed" do
+        it "returns true" do
+          opts[:quick_action_target] = Issue.new
+          expect(subject.available?(opts)).to be true
+        end
+      end
+
+      context "when the command target type is not allowed" do
+        it "returns true" do
+          opts[:quick_action_target] = MergeRequest.new
+          expect(subject.available?(opts)).to be false
+        end
+      end
+    end
+
+    context "when the command has no types" do
+      it "any target type is allowed" do
+        opts[:quick_action_target] = Issue.new
+        expect(subject.available?(opts)).to be true
+
+        opts[:quick_action_target] = MergeRequest.new
+        expect(subject.available?(opts)).to be true
+      end
+    end
   end
 
   describe "#execute" do