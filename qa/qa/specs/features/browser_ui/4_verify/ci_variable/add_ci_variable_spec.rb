fd8f0c5c752c3140a9326632fe26da2fd15b4301
Matija Čupić <matteeyah@gmail.com>
2019-04-05 11:50:28 +0200

    Remove spaces from variable name

--- qa/qa/specs/features/browser_ui/4_verify/ci_variable/add_ci_variable_spec.rb
+++ qa/qa/specs/features/browser_ui/4_verify/ci_variable/add_ci_variable_spec.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 module QA
-  # Failure issue: https://gitlab.com/gitlab-org/quality/nightly/issues/91
-  context 'Verify', :quarantine do
+  context 'Verify' do
     describe 'CI variable support' do
       it 'user adds a CI variable' do
         Runtime::Browser.visit(:gitlab, Page::Main::Login)
@@ -10,17 +9,17 @@ module QA
 
         Resource::CiVariable.fabricate! do |resource|
           resource.key = 'VARIABLE_KEY'
-          resource.value = 'some CI variable'
+          resource.value = 'some_CI_variable'
         end
 
         Page::Project::Settings::CICD.perform do |settings|
           settings.expand_ci_variables do |page|
             expect(page).to have_field(with: 'VARIABLE_KEY')
-            expect(page).not_to have_field(with: 'some CI variable')
+            expect(page).not_to have_field(with: 'some_CI_variable')
 
             page.reveal_variables
 
-            expect(page).to have_field(with: 'some CI variable')
+            expect(page).to have_field(with: 'some_CI_variable')
           end
         end
       end
