026896cca27ac63795bf1d0e606f6813500d9475
Sean McGivern <sean@gitlab.com>
2019-04-05 10:18:10 +0000

    Merge branch 'require-all-templates-to-include-default-stages' into 'master'
    Require all templates to use default stages
    
    Closes #59992
    
    See merge request gitlab-org/gitlab-ce!26954
    
    (cherry picked from commit 39eb16aab2dbac3347f61f83fb60f5448d44e965)
    
    e0df05cf Require all templates to use default stages

--- spec/lib/gitlab/ci/templates/templates_spec.rb
+++ spec/lib/gitlab/ci/templates/templates_spec.rb
@@ -3,46 +3,32 @@
 require 'spec_helper'
 
 describe "CI YML Templates" do
-  ABSTRACT_TEMPLATES = %w[Serverless].freeze
-  # These templates depend on the presence of the `project`
-  # param to enable processing of `include:` within CI config.
-  PROJECT_DEPENDENT_TEMPLATES = %w[Auto-DevOps DAST].freeze
+  using RSpec::Parameterized::TableSyntax
 
-  def self.concrete_templates
-    Gitlab::Template::GitlabCiYmlTemplate.all.reject do |template|
-      ABSTRACT_TEMPLATES.include?(template.name)
-    end
+  subject { Gitlab::Ci::YamlProcessor.new(content) }
+
+  where(:template_name) do
+    Gitlab::Template::GitlabCiYmlTemplate.all.map(&:full_name)
   end
 
-  def self.abstract_templates
-    Gitlab::Template::GitlabCiYmlTemplate.all.select do |template|
-      ABSTRACT_TEMPLATES.include?(template.name)
+  with_them do
+    let(:content) do
+      <<~EOS
+        include:
+          - template: #{template_name}
+
+        concrete_build_implemented_by_a_user:
+          stage: test
+          script: do something
+      EOS
     end
-  end
 
-  describe 'concrete templates with CI/CD jobs' do
-    concrete_templates.each do |template|
-      it "#{template.name} template should be valid" do
-        # Trigger processing of included files
-        project = create(:project, :test_repo) if PROJECT_DEPENDENT_TEMPLATES.include?(template.name)
-
-        expect { Gitlab::Ci::YamlProcessor.new(template.content, project: project) }
-          .not_to raise_error
-      end
+    it 'is valid' do
+      expect { subject }.not_to raise_error
     end
-  end
 
-  describe 'abstract templates without concrete jobs defined' do
-    abstract_templates.each do |template|
-      it "#{template.name} template should be valid after being implemented" do
-        content = template.content + <<~EOS
-          concrete_build_implemented_by_a_user:
-            stage: build
-            script: do something
-        EOS
-
-        expect { Gitlab::Ci::YamlProcessor.new(content) }.not_to raise_error
-      end
+    it 'require default stages to be included' do
+      expect(subject.stages).to include(*Gitlab::Ci::Config::Entry::Stages.default)
     end
   end
 end
