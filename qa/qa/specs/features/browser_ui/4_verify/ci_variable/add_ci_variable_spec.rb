02953c536d09c60069714e703e82c7f76e2daddb
Ramya Authappan <rauthappan@gitlab.com>
2019-04-05 07:52:07 +0000

    Merge branch 'qa-quarantine-failing-staging-nightly-tests' into 'master'
    Quarantine failing tests
    
    See merge request gitlab-org/gitlab-ce!27036
    
    (cherry picked from commit 88bf73a719933c136cc2bd9b791b6d5ca85cd330)
    
    fcc7b72b Quarantine failing tests

--- qa/qa/specs/features/browser_ui/4_verify/ci_variable/add_ci_variable_spec.rb
+++ qa/qa/specs/features/browser_ui/4_verify/ci_variable/add_ci_variable_spec.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 module QA
-  context 'Verify' do
+  # Failure issue: https://gitlab.com/gitlab-org/quality/nightly/issues/91
+  context 'Verify', :quarantine do
     describe 'CI variable support' do
       it 'user adds a CI variable' do
         Runtime::Browser.visit(:gitlab, Page::Main::Login)
