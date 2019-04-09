193e6602914a45974e5bd6e2ab12a63f91c62143
Sarah Yasonik <syasonik@gitlab.com>
2019-04-09 09:54:17 +0000

    Resolve Environments#additional_metrics TypeError, ensure unix format

--- app/assets/javascripts/monitoring/utils.js
+++ app/assets/javascripts/monitoring/utils.js
@@ -1,4 +1,4 @@
-import { timeWindows, msPerMinute } from './constants';
+import { timeWindows } from './constants';
 
 /**
  * method that converts a predetermined time window to minutes
@@ -6,27 +6,26 @@ import { timeWindows, msPerMinute } from './constants';
  * @param {String} timeWindow - The time window to convert to minutes
  * @returns {number} The time window in minutes
  */
-const getTimeDifferenceMinutes = timeWindow => {
+const getTimeDifferenceSeconds = timeWindow => {
   switch (timeWindow) {
     case timeWindows.thirtyMinutes:
-      return 30;
+      return 60 * 30;
     case timeWindows.threeHours:
-      return 60 * 3;
+      return 60 * 60 * 3;
     case timeWindows.oneDay:
-      return 60 * 24 * 1;
+      return 60 * 60 * 24 * 1;
     case timeWindows.threeDays:
-      return 60 * 24 * 3;
+      return 60 * 60 * 24 * 3;
     case timeWindows.oneWeek:
-      return 60 * 24 * 7 * 1;
+      return 60 * 60 * 24 * 7 * 1;
     default:
-      return 60 * 8;
+      return 60 * 60 * 8;
   }
 };
 
 export const getTimeDiff = selectedTimeWindow => {
-  const end = Date.now();
-  const timeDifferenceMinutes = getTimeDifferenceMinutes(selectedTimeWindow);
-  const start = new Date(end - timeDifferenceMinutes * msPerMinute).getTime();
+  const end = Date.now() / 1000; // convert milliseconds to seconds
+  const start = end - getTimeDifferenceSeconds(selectedTimeWindow);
 
   return { start, end };
 };
