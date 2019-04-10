81da9337e17d288366d4257e1bbe4ba62cb1b21c
Oswaldo Ferreira <oswaldo@gitlab.com>
2019-04-10 10:49:25 -0300

    Remove multi-line suggestions feature flag
    That's a straightforward feature flag code removal for 11.10

--- lib/banzai/filter/suggestion_filter.rb
+++ lib/banzai/filter/suggestion_filter.rb
@@ -6,15 +6,11 @@ module Banzai
     class SuggestionFilter < HTML::Pipeline::Filter
       # Class used for tagging elements that should be rendered
       TAG_CLASS = 'js-render-suggestion'.freeze
-      SUGGESTION_REGEX = Gitlab::Diff::SuggestionsParser::SUGGESTION_CONTEXT
 
       def call
         return doc unless suggestions_filter_enabled?
 
         doc.search('pre.suggestion > code').each do |node|
-          # TODO: Remove once multi-line suggestions FF get removed (#59178).
-          remove_multi_line_params(node.parent)
-
           node.add_class(TAG_CLASS)
         end
 
@@ -29,14 +25,6 @@ module Banzai
 
       def project
         context[:project]
-      end
-
-      def remove_multi_line_params(node)
-        return if Feature.enabled?(:multi_line_suggestions, project)
-
-        if node[SyntaxHighlightFilter::LANG_PARAMS_ATTR]&.match?(SUGGESTION_REGEX)
-          node.remove_attribute(SyntaxHighlightFilter::LANG_PARAMS_ATTR)
-        end
       end
     end
   end
