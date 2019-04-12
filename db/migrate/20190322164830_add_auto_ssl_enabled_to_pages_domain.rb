75194adeee020aca9646963807e7c257b5b2105b
Vladimir Shushlin <v.shushlin@gmail.com>
2019-04-12 18:41:31 +0300

    Add auto_ssl_enabled for pages domains

---
+++ db/migrate/20190322164830_add_auto_ssl_enabled_to_pages_domain.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class AddAutoSslEnabledToPagesDomain < ActiveRecord::Migration[5.0]
+  include Gitlab::Database::MigrationHelpers
+
+  DOWNTIME = false
+
+  disable_ddl_transaction!
+
+  def up
+    add_column_with_default :pages_domains, :auto_ssl_enabled, :boolean, default: false
+  end
+
+  def down
+    remove_column :pages_domains, :auto_ssl_enabled
+  end
+end
