b13ecdd6cc0d972a4563e1509d499969676f1488
Phil Hughes <me@iamphill.com>
2019-04-12 10:27:24 +0100

    CE backport of image-comment-reviews

---
+++ app/assets/javascripts/diffs/mixins/image_diff.js
@@ -0,0 +1,13 @@
+import { mapActions } from 'vuex';
+
+export default {
+  methods: {
+    ...mapActions(['toggleDiscussion']),
+    clickedToggle(discussion) {
+      this.toggleDiscussion({ discussionId: discussion.id });
+    },
+    toggleText(discussion, index) {
+      return index + 1;
+    },
+  },
+};
