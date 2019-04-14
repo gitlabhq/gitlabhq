91cf0cb2d53b6f48560862fc121f897bb7331046
Fernando <fernando@jabberfest.com>
2019-04-14 11:05:28 -0500

    Paginate license management and add license search
    First pass at license pagination
    
    * Paginate license management client side
    * Refactor license list into seperate component
    
    Add string filtering to license names
    
    * Add search input to query on license name
    
    Add add license button
    
    * Refactor add license button to be a slot
    
    Clean up styles and button state logic
    
    * Clean up alignment
    * Disable button when dorpdown is open
    
    Remove client side alphabetical sorting
    
    * Let the databse return order by date
    
    Refactor list to use row slot
    
    Further abstract pagination list compnent
    
    Finish refactor of paginated list
    
    * Refactor component into generic paginated list component
    * Add additional style tweaks + responsive classes
    
    Run prettier
    
    Update license_management_spec
    
    Run Prettier
    
    Add unit tests for paginated list component
    
    * Refactor template to be valid html (li in ul)
    * Add jest unit tests
    
    Add additional unit tests
    
    * Add unit tests around pagination and search states
    
    Add unit tests for filter props
    
    Pretty print, lint, and add changelog
    
    Update po files
    
    Regernate pot file
    
    Backport EE changes
    
    * Update paginated list component
    * Update specs and snapshot
    
    Add POT file
    
    Update default copy for pagianted list
    
    * update copy for empty and empty search result states
    
    Update pot file
    
    Backport changes from EE merge request
    
    * Paginaed list component and specs
    
    Backport EE changes
    
    Update paginated list snapshot
    
    Update license management docs and images
    
    Backport paginated-list component from EE
    
    Link to gitlab-ui artifacts job
    
    Match gitlab ui build to EE
    
    Update pot file
    
    Backport Paginated list changes
    
    Set gitlab-ui to temp artifact
    
    Add changelog

---
+++ app/assets/javascripts/vue_shared/components/paginated_list.vue
@@ -0,0 +1,35 @@
+<script>
+import { GlPaginatedList } from '@gitlab/ui';
+import { PREV, NEXT } from '~/vue_shared/components/pagination/constants';
+
+export default {
+  components: {
+    GlPaginatedList,
+  },
+  labels: {
+    prev: PREV,
+    next: NEXT,
+  },
+};
+</script>
+
+<template>
+  <gl-paginated-list
+    v-bind="$attrs"
+    :prev-text="$options.labels.prev"
+    :next-text="$options.labels.next"
+  >
+    <!-- proxy the slots -->
+    <template #header>
+      <slot name="header"></slot>
+    </template>
+
+    <template #subheader>
+      <slot name="subheader"></slot>
+    </template>
+
+    <template #default="{ listItem, query }">
+      <slot :listItem="listItem" :query="query"></slot>
+    </template>
+  </gl-paginated-list>
+</template>
