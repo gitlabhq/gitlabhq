6735f4c705ac6a08ac0b5e847c9f197b81c8502a
rpereira2 <rpereira@gitlab.com>
2019-04-15 14:59:59 +0530

    Make dashboard a top level key
    - Since each yml file can hold only one dashboard.

--- db/importers/common_metrics_importer.rb
+++ db/importers/common_metrics_importer.rb
@@ -38,7 +38,7 @@ module Importers
     attr_reader :content
 
     def initialize(filename = 'common_metrics.yml')
-      @content = YAML.load_file(Rails.root.join('config', 'prometheus', filename)).first
+      @content = YAML.load_file(Rails.root.join('config', 'prometheus', filename))
     end
 
     def execute
