c6456830b5f9a147807b9de32c437eb6dda4c53b
Sean McGivern <sean@gitlab.com>
2019-04-15 14:04:40 +0100

    Add QA tests for the performance bar
    These are very high-level. Currently, they test:
    
    1. That the performance bar appears.
    2. That it has detailed metrics for Postgres and Gitaly.
    3. That AJAX requests are included in the request selector.

---
+++ qa/qa/page/admin/settings/component/performance_bar.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module QA
+  module Page
+    module Admin
+      module Settings
+        module Component
+          class PerformanceBar < Page::Base
+            view 'app/views/admin/application_settings/_performance_bar.html.haml' do
+              element :enable_performance_bar_checkbox
+              element :save_changes_button
+            end
+
+            def enable_performance_bar
+              click_element :enable_performance_bar_checkbox
+              Capybara.current_session.driver.browser.manage.add_cookie(name: 'perf_bar_enabled', value: 'true')
+            end
+
+            def save_settings
+              click_element :save_changes_button
+            end
+          end
+        end
+      end
+    end
+  end
+end
