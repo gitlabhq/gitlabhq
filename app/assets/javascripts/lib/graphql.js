8e808bc221f1a7050629346df3748a6c608ff497
Kushal Pandya <kushalspandya@gmail.com>
2019-04-25 12:25:21 +0530

    Add support for baseUrl in ApolloClient instance
    Add support for providing baseUrl/rootUrl within `uri` while
    creating ApolloClient instance.

--- app/assets/javascripts/lib/graphql.js
+++ app/assets/javascripts/lib/graphql.js
@@ -3,10 +3,17 @@ import { InMemoryCache } from 'apollo-cache-inmemory';
 import { createUploadLink } from 'apollo-upload-client';
 import csrf from '~/lib/utils/csrf';
 
-export default (resolvers = {}) =>
-  new ApolloClient({
+export default (resolvers = {}, baseUrl = '') => {
+  let uri = `${gon.relative_url_root}/api/graphql`;
+
+  if (baseUrl) {
+    // Prepend baseUrl and ensure that `///` are replaced with `/`
+    uri = `${baseUrl}${uri}`.replace(/\/{3,}/g, '/');
+  }
+
+  return new ApolloClient({
     link: createUploadLink({
-      uri: `${gon.relative_url_root}/api/graphql`,
+      uri,
       headers: {
         [csrf.headerKey]: csrf.token,
       },
@@ -14,3 +21,4 @@ export default (resolvers = {}) =>
     cache: new InMemoryCache(),
     resolvers,
   });
+};
