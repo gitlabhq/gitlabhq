c948babf7088e26ae025c1476022699bfe9fb107
Małgorzata Ksionek <meksionek@gmail.com>
2019-04-26 06:11:47 +0200

    Add specs to group routing spec

--- spec/routing/group_routing_spec.rb
+++ spec/routing/group_routing_spec.rb
@@ -133,5 +133,19 @@ describe "Groups", "routing" do
         let(:resource) { create(:group, parent: parent, path: 'activity') }
       end
     end
+
+    describe 'subgroup "boards"' do
+      it 'shows group show page' do
+        allow(Group).to receive(:find_by_full_path).with('gitlabhq/boards', any_args).and_return(true)
+
+        expect(get('/groups/gitlabhq/boards')).to route_to('groups#show', id: 'gitlabhq/boards')
+      end
+
+      it 'shows boards index page' do
+        allow(Group).to receive(:find_by_full_path).with('gitlabhq', any_args).and_return(true)
+
+        expect(get('/groups/gitlabhq/-/boards')).to route_to('groups/boards#index', group_id: 'gitlabhq')
+      end
+    end
   end
 end
