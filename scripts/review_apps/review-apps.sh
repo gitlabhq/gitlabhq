84103e2d2f2668053c09e7fc225610005dfaa3f7
Rémy Coutable <remy@rymai.me>
2019-04-30 09:49:10 +0200

    Disable HTTP for the nginx-ingress Review App service
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- scripts/review_apps/review-apps.sh
+++ scripts/review_apps/review-apps.sh
@@ -216,6 +216,7 @@ HELM_CMD=$(cat << EOF
     --set global.ingress.configureCertmanager=false \
     --set global.ingress.tls.secretName=tls-cert \
     --set global.ingress.annotations."external-dns\.alpha\.kubernetes\.io/ttl"="10"
+    --set nginx-ingress.controller.service.enableHttp=false \
     --set nginx-ingress.defaultBackend.resources.requests.memory=7Mi \
     --set nginx-ingress.controller.resources.requests.memory=440M \
     --set nginx-ingress.controller.replicaCount=2 \
