d900ece1f7fbde93b903ba7b798646458cdee832
Ash McKenzie <amckenzie@gitlab.com>
2019-04-30 17:43:01 +1200

    Ensure Issue & MR note_html cannot be imported

--- spec/lib/gitlab/import_export/project_tree_restorer_spec.rb
+++ spec/lib/gitlab/import_export/project_tree_restorer_spec.rb
@@ -10,7 +10,6 @@ describe Gitlab::ImportExport::ProjectTreeRestorer do
         create(:user, username: 'bernard_willms'),
         create(:user, username: 'saul_will')
       ]
-      @markdown_classes = [AbuseReport, Appearance, ApplicationSetting, BroadcastMessage, Issue, Label, MergeRequest, Milestone, Namespace, Project, Release, ResourceLabelEvent, Snippet, UserStatus]
 
       RSpec::Mocks.with_temporary_scope do
         @project = create(:project, :builds_enabled, :issues_disabled, name: 'project', path: 'project')
@@ -22,7 +21,6 @@ describe Gitlab::ImportExport::ProjectTreeRestorer do
 
         expect_any_instance_of(Gitlab::Git::Repository).to receive(:create_branch).with('feature', 'DCBA')
         allow_any_instance_of(Gitlab::Git::Repository).to receive(:create_branch)
-        @markdown_classes.each {|klass| allow_any_instance_of(klass).to receive(:latest_cached_markdown_version).and_return(434343)}
 
         project_tree_restorer = described_class.new(user: @user, shared: @shared, project: @project)
 
@@ -61,20 +59,20 @@ describe Gitlab::ImportExport::ProjectTreeRestorer do
       end
 
       context 'when importing a project with cached_markdown_version and note_html' do
-        let!(:issue) { Issue.find_by(description: 'Aliquam enim illo et possimus.') }
-        let(:note1) { issue.notes.select {|n| n.note.match(/Quo reprehenderit aliquam qui dicta impedit cupiditate eligendi/)}.first }
-        let(:note2) { issue.notes.select {|n| n.note.match(/Est reprehenderit quas aut aspernatur autem recusandae voluptatem/)}.first }
-
-        it 'does not import the note_html' do
-          expect(note1.note_html).to match(/Quo reprehenderit aliquam qui dicta impedit cupiditate eligendi/)
+        context 'for an Issue' do
+          it 'does not import note_html' do
+            note_content = 'Quo reprehenderit aliquam qui dicta impedit cupiditate eligendi'
+            issue_note = Issue.find_by(description: 'Aliquam enim illo et possimus.').notes.select { |n| n.note.match(/#{note_content}/)}.first
+            expect(issue_note.note_html).to match(/#{note_content}/)
+          end
         end
 
-        it 'does not set the old cached_markdown_version' do
-          expect(note2.cached_markdown_version).not_to eq(121212)
-        end
-
-        it 'does not import the note_html' do
-          expect(note2.note_html).to match(/Est reprehenderit quas aut aspernatur autem recusandae voluptatem/)
+        context 'for a Merge Request' do
+          it 'does not import note_html' do
+            note_content = 'Sit voluptatibus eveniet architecto quidem'
+            merge_request_note = MergeRequest.find_by(title: 'MR1').notes.select { |n| n.note.match(/#{note_content}/)}.first
+            expect(merge_request_note.note_html).to match(/#{note_content}/)
+          end
         end
       end
 
