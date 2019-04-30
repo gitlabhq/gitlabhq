6f0d8ebc45ef1d0e5a03a0d2965169483bf7224d
Francisco Javier López <fjlopez@gitlab.com>
2019-04-30 08:21:21 +0000

    Refactored LfsImportService and ImportService
    In order to make `LfsImportService` more reusable, we
    need to extract the logic inside `ImportService` and
    encapsulate it into the service.

--- spec/services/projects/lfs_pointers/lfs_import_service_spec.rb
+++ spec/services/projects/lfs_pointers/lfs_import_service_spec.rb
@@ -1,148 +1,63 @@
 # frozen_string_literal: true
-
 require 'spec_helper'
 
 describe Projects::LfsPointers::LfsImportService do
+  let(:project) { create(:project) }
+  let(:user) { project.creator }
   let(:import_url) { 'http://www.gitlab.com/demo/repo.git' }
-  let(:default_endpoint) { "#{import_url}/info/lfs/objects/batch"}
-  let(:group) { create(:group, lfs_enabled: true)}
-  let!(:project) { create(:project, namespace: group, import_url: import_url, lfs_enabled: true) }
-  let!(:lfs_objects_project) { create_list(:lfs_objects_project, 2, project: project) }
-  let!(:existing_lfs_objects) { LfsObject.pluck(:oid, :size).to_h }
-  let(:oids) { { 'oid1' => 123, 'oid2' => 125 } }
   let(:oid_download_links) { { 'oid1' => "#{import_url}/gitlab-lfs/objects/oid1", 'oid2' => "#{import_url}/gitlab-lfs/objects/oid2" } }
-  let(:all_oids) { existing_lfs_objects.merge(oids) }
-  let(:remote_uri) { URI.parse(lfs_endpoint) }
 
-  subject { described_class.new(project) }
+  subject { described_class.new(project, user) }
 
-  before do
-    allow(project.repository).to receive(:lfsconfig_for).and_return(nil)
-    allow(Gitlab.config.lfs).to receive(:enabled).and_return(true)
-    allow_any_instance_of(Projects::LfsPointers::LfsListService).to receive(:execute).and_return(all_oids)
-  end
+  context 'when lfs is enabled for the project' do
+    before do
+      allow(project).to receive(:lfs_enabled?).and_return(true)
+    end
 
-  describe '#execute' do
-    context 'when no lfs pointer is linked' do
-      before do
-        allow_any_instance_of(Projects::LfsPointers::LfsLinkService).to receive(:execute).and_return([])
-        allow_any_instance_of(Projects::LfsPointers::LfsDownloadLinkListService).to receive(:execute).and_return(oid_download_links)
-        expect(Projects::LfsPointers::LfsDownloadLinkListService).to receive(:new).with(project, remote_uri: URI.parse(default_endpoint)).and_call_original
-      end
+    it 'downloads lfs objects' do
+      service = double
+      expect_any_instance_of(Projects::LfsPointers::LfsObjectDownloadListService).to receive(:execute).and_return(oid_download_links)
+      expect(Projects::LfsPointers::LfsDownloadService).to receive(:new).and_return(service).twice
+      expect(service).to receive(:execute).twice
 
-      it 'retrieves all lfs pointers in the project repository' do
-        expect_any_instance_of(Projects::LfsPointers::LfsListService).to receive(:execute)
+      result = subject.execute
 
-        subject.execute
-      end
+      expect(result[:status]).to eq :success
+    end
 
-      it 'links existent lfs objects to the project' do
-        expect_any_instance_of(Projects::LfsPointers::LfsLinkService).to receive(:execute)
+    context 'when no downloadable lfs object links' do
+      it 'does not call LfsDownloadService' do
+        expect_any_instance_of(Projects::LfsPointers::LfsObjectDownloadListService).to receive(:execute).and_return({})
+        expect(Projects::LfsPointers::LfsDownloadService).not_to receive(:new)
 
-        subject.execute
-      end
+        result = subject.execute
 
-      it 'retrieves the download links of non existent objects' do
-        expect_any_instance_of(Projects::LfsPointers::LfsDownloadLinkListService).to receive(:execute).with(all_oids)
-
-        subject.execute
+        expect(result[:status]).to eq :success
       end
     end
 
-    context 'when some lfs objects are linked' do
-      before do
-        allow_any_instance_of(Projects::LfsPointers::LfsLinkService).to receive(:execute).and_return(existing_lfs_objects.keys)
-        allow_any_instance_of(Projects::LfsPointers::LfsDownloadLinkListService).to receive(:execute).and_return(oid_download_links)
-      end
+    context 'when an exception is raised' do
+      it 'returns error' do
+        error_message = "error message"
+        expect_any_instance_of(Projects::LfsPointers::LfsObjectDownloadListService).to receive(:execute).and_raise(StandardError, error_message)
 
-      it 'retrieves the download links of non existent objects' do
-        expect_any_instance_of(Projects::LfsPointers::LfsDownloadLinkListService).to receive(:execute).with(oids)
+        result = subject.execute
 
-        subject.execute
-      end
-    end
-
-    context 'when all lfs objects are linked' do
-      before do
-        allow_any_instance_of(Projects::LfsPointers::LfsLinkService).to receive(:execute).and_return(all_oids.keys)
-        allow_any_instance_of(Projects::LfsPointers::LfsDownloadLinkListService).to receive(:execute)
-      end
-
-      it 'retrieves no download links' do
-        expect_any_instance_of(Projects::LfsPointers::LfsDownloadLinkListService).to receive(:execute).with({}).and_call_original
-
-        expect(subject.execute).to be_empty
-      end
-    end
-
-    context 'when lfsconfig file exists' do
-      before do
-        allow(project.repository).to receive(:lfsconfig_for).and_return("[lfs]\n\turl = #{lfs_endpoint}\n")
-      end
-
-      context 'when url points to the same import url host' do
-        let(:lfs_endpoint) { "#{import_url}/different_endpoint" }
-        let(:service) { double }
-
-        before do
-          allow(service).to receive(:execute)
-        end
-        it 'downloads lfs object using the new endpoint' do
-          expect(Projects::LfsPointers::LfsDownloadLinkListService).to receive(:new).with(project, remote_uri: remote_uri).and_return(service)
-
-          subject.execute
-        end
-
-        context 'when import url has credentials' do
-          let(:import_url) { 'http://user:password@www.gitlab.com/demo/repo.git'}
-
-          it 'adds the credentials to the new endpoint' do
-            expect(Projects::LfsPointers::LfsDownloadLinkListService)
-              .to receive(:new).with(project, remote_uri: URI.parse("http://user:password@www.gitlab.com/demo/repo.git/different_endpoint"))
-              .and_return(service)
-
-            subject.execute
-          end
-
-          context 'when url has its own credentials' do
-            let(:lfs_endpoint) { "http://user1:password1@www.gitlab.com/demo/repo.git/different_endpoint" }
-
-            it 'does not add the import url credentials' do
-              expect(Projects::LfsPointers::LfsDownloadLinkListService)
-                .to receive(:new).with(project, remote_uri: remote_uri)
-                .and_return(service)
-
-              subject.execute
-            end
-          end
-        end
-      end
-
-      context 'when url points to a third party service' do
-        let(:lfs_endpoint) { 'http://third_party_service.com/info/lfs/objects/' }
-
-        it 'disables lfs from the project' do
-          expect(project.lfs_enabled?).to be_truthy
-
-          subject.execute
-
-          expect(project.lfs_enabled?).to be_falsey
-        end
-
-        it 'does not download anything' do
-          expect_any_instance_of(Projects::LfsPointers::LfsListService).not_to receive(:execute)
-
-          subject.execute
-        end
+        expect(result[:status]).to eq :error
+        expect(result[:message]).to eq error_message
       end
     end
   end
 
-  describe '#default_endpoint_uri' do
-    let(:import_url) { 'http://www.gitlab.com/demo/repo' }
+  context 'when lfs is not enabled for the project' do
+    it 'does not download lfs objects' do
+      allow(project).to receive(:lfs_enabled?).and_return(false)
+      expect(Projects::LfsPointers::LfsObjectDownloadListService).not_to receive(:new)
+      expect(Projects::LfsPointers::LfsDownloadService).not_to receive(:new)
 
-    it 'adds suffix .git if the url does not have it' do
-      expect(subject.send(:default_endpoint_uri).path).to match(/repo.git/)
+      result = subject.execute
+
+      expect(result[:status]).to eq :success
     end
   end
 end
--- spec/services/projects/lfs_pointers/lfs_link_service_spec.rb
+++ spec/services/projects/lfs_pointers/lfs_link_service_spec.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-
 require 'spec_helper'
 
 describe Projects::LfsPointers::LfsLinkService do
