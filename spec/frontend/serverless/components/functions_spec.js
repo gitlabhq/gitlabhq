2aa024af61787186d881b772fcd04d190eb93aec
Winnie Hellmann <winnie@gitlab.com>
2019-05-19 19:55:04 +0000

    Use component selectors in serverless frontend tests

--- spec/frontend/serverless/components/functions_spec.js
+++ spec/frontend/serverless/components/functions_spec.js
@@ -1,9 +1,12 @@
 import Vuex from 'vuex';
+import { GlLoadingIcon } from '@gitlab/ui';
 import AxiosMockAdapter from 'axios-mock-adapter';
 import axios from '~/lib/utils/axios_utils';
 import functionsComponent from '~/serverless/components/functions.vue';
 import { createLocalVue, shallowMount } from '@vue/test-utils';
 import { createStore } from '~/serverless/store';
+import EmptyState from '~/serverless/components/empty_state.vue';
+import EnvironmentRow from '~/serverless/components/environment_row.vue';
 import { TEST_HOST } from 'helpers/test_constants';
 import { mockServerlessFunctions } from '../mock_data';
 
@@ -43,7 +46,7 @@ describe('functionsComponent', () => {
       sync: false,
     });
 
-    expect(component.vm.$el.querySelector('emptystate-stub')).not.toBe(null);
+    expect(component.find(EmptyState).exists()).toBe(true);
   });
 
   it('should render a loading component', () => {
@@ -60,7 +63,7 @@ describe('functionsComponent', () => {
       sync: false,
     });
 
-    expect(component.vm.$el.querySelector('glloadingicon-stub')).not.toBe(null);
+    expect(component.find(GlLoadingIcon).exists()).toBe(true);
   });
 
   it('should render empty state when there is no function data', () => {
@@ -104,7 +107,7 @@ describe('functionsComponent', () => {
     component.vm.$store.dispatch('receiveFunctionsSuccess', mockServerlessFunctions);
 
     return component.vm.$nextTick().then(() => {
-      expect(component.vm.$el.querySelector('environmentrow-stub')).not.toBe(null);
+      expect(component.find(EnvironmentRow).exists()).toBe(true);
     });
   });
 });
