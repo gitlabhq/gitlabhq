d02d3e34dd8a1d2da9106e41918bfdd7a988e904
Rémy Coutable <remy@rymai.me>
2019-05-22 13:10:19 +0200

    Fix MySQL CI jobs
    [run mysql]
    
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- scripts/prepare_build.sh
+++ scripts/prepare_build.sh
@@ -17,12 +17,10 @@ cp config/gitlab.yml.example config/gitlab.yml
 sed -i 's/bin_path: \/usr\/bin\/git/bin_path: \/usr\/local\/bin\/git/' config/gitlab.yml
 
 # Determine the database by looking at the job name.
-# For example, we'll get pg if the job is `rspec-pg 19 20`
-export GITLAB_DATABASE=$(echo $CI_JOB_NAME | cut -f1 -d' ' | cut -f2 -d-)
-
-# This would make the default database postgresql, and we could also use
-# pg to mean postgresql.
-if [ "$GITLAB_DATABASE" != 'mysql' ]; then
+# This would make the default database postgresql.
+if [[ "${CI_JOB_NAME#*mysql}" != "$CI_JOB_NAME" ]]; then
+  export GITLAB_DATABASE='mysql'
+else
   export GITLAB_DATABASE='postgresql'
 fi
 
