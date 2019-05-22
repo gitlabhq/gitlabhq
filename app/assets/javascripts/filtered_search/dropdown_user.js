d0bb69c02aa9bfb67308b367f105c85eacd47c78
Luke Bennett <lbennett@gitlab.com>
2019-05-22 04:08:30 +0100

    Resolve CE/EE diffs in dropdown_user
    Part of single codebase changes.

--- app/assets/javascripts/filtered_search/dropdown_user.js
+++ app/assets/javascripts/filtered_search/dropdown_user.js
@@ -18,6 +18,7 @@ export default class DropdownUser extends DropdownAjaxFilter {
         group_id: this.getGroupId(),
         project_id: this.getProjectId(),
         current_user: true,
+        ...this.projectOrGroupId(),
       },
       onLoadingFinished: () => {
         this.hideCurrentUser();
@@ -35,5 +36,18 @@ export default class DropdownUser extends DropdownAjaxFilter {
 
   getProjectId() {
     return this.input.getAttribute('data-project-id');
+  }
+
+  projectOrGroupId() {
+    const projectId = this.getProjectId();
+    const groupId = this.getGroupId();
+    if (groupId) {
+      return {
+        group_id: groupId,
+      };
+    }
+    return {
+      project_id: projectId,
+    };
   }
 }
