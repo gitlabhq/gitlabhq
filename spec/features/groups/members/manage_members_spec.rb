e8b4dbadc53b136d3a83f64c6862b5c884ea74bc
Stan Hu <stanhu@gmail.com>
2019-05-22 23:36:00 -0700

    Fix spec for inviting members

--- spec/features/groups/members/manage_members_spec.rb
+++ spec/features/groups/members/manage_members_spec.rb
@@ -98,6 +98,8 @@ describe 'Groups > Members > Manage members' do
 
     add_user('test@example.com', 'Reporter')
 
+    click_link('Pending')
+
     page.within('.content-list.invited-members-list') do
       expect(page).to have_content('test@example.com')
       expect(page).to have_content('Invited')
