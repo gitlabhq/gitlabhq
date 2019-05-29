a5adc6a024012f727ac32c440bb42f4634ae0605
Jan Provaznik <jprovaznik@gitlab.com>
2019-05-29 12:27:44 +0000

    Add Puma sampler
    This sampler gathers Puma-specific metrics which can be used by
    Prometheus then.

--- config/initializers/7_prometheus_metrics.rb
+++ config/initializers/7_prometheus_metrics.rb
@@ -29,11 +29,17 @@ if !Rails.env.test? && Gitlab::Metrics.prometheus_metrics_enabled?
   Gitlab::Cluster::LifecycleEvents.on_worker_start do
     defined?(::Prometheus::Client.reinitialize_on_pid_change) && Prometheus::Client.reinitialize_on_pid_change
 
-    unless Sidekiq.server?
+    if defined?(::Unicorn)
       Gitlab::Metrics::Samplers::UnicornSampler.initialize_instance(Settings.monitoring.unicorn_sampler_interval).start
     end
 
     Gitlab::Metrics::Samplers::RubySampler.initialize_instance(Settings.monitoring.ruby_sampler_interval).start
+  end
+
+  if defined?(::Puma)
+    Gitlab::Cluster::LifecycleEvents.on_master_start do
+      Gitlab::Metrics::Samplers::PumaSampler.initialize_instance(Settings.monitoring.puma_sampler_interval).start
+    end
   end
 end
 
