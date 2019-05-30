a1da4104a103050eb1f181e6f50a0a13101d9cbf
Stan Hu <stanhu@gmail.com>
2019-05-29 21:21:58 -0700

    Use Rails migration v5.0 for GitLab 11.9

--- db/migrate/20190529142545_add_dns_rebinding_protection_enabled_to_application_settings.rb
+++ db/migrate/20190529142545_add_dns_rebinding_protection_enabled_to_application_settings.rb
@@ -3,7 +3,7 @@
 # See http://doc.gitlab.com/ce/development/migration_style_guide.html
 # for more information on how to write migrations for GitLab.
 
-class AddDnsRebindingProtectionEnabledToApplicationSettings < ActiveRecord::Migration[5.1]
+class AddDnsRebindingProtectionEnabledToApplicationSettings < ActiveRecord::Migration[5.0]
   include Gitlab::Database::MigrationHelpers
 
   DOWNTIME = false
