e02cd451b2d6a81f4847f26b29ed1eb6c74f8dab
Jose Vargas <jvargas@gitlab.com>
2019-05-30 13:47:32 -0500

    Add single_stat chart component to the monitoring bundle
    This merge requests just adds the component without integrating it
    to the dashboard, it adds tests as well

---
+++ app/assets/javascripts/monitoring/components/charts/single_stat.vue
@@ -0,0 +1,37 @@
+<script>
+import { GlSingleStat } from '@gitlab/ui/dist/charts';
+
+export default {
+  components: {
+    GlSingleStat,
+  },
+  inheritAttrs: false,
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: Number,
+      required: true,
+    },
+    unit: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    valueWithUnit() {
+      return `${this.value}${this.unit}`;
+    },
+  },
+};
+</script>
+<template>
+  <div class="prometheus-graph col-12 col-lg-6">
+    <div class="prometheus-graph-header">
+      <h5 ref="graphTitle" class="prometheus-graph-title">{{ title }}</h5>
+    </div>
+    <gl-single-stat :value="valueWithUnit" :title="title" variant="success" />
+  </div>
+</template>
