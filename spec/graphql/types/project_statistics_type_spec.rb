4e251d01de8014a494049710ac27146a3f049d89
Alessio Caiazza <acaiazza@gitlab.com>
2019-06-04 12:11:41 +0000

    Expose wiki_size on GraphQL API

--- spec/graphql/types/project_statistics_type_spec.rb
+++ spec/graphql/types/project_statistics_type_spec.rb
@@ -5,6 +5,7 @@ require 'spec_helper'
 describe GitlabSchema.types['ProjectStatistics'] do
   it "has all the required fields" do
     is_expected.to have_graphql_fields(:storage_size, :repository_size, :lfs_objects_size,
-                                       :build_artifacts_size, :packages_size, :commit_count)
+                                       :build_artifacts_size, :packages_size, :commit_count,
+                                       :wiki_size)
   end
 end
