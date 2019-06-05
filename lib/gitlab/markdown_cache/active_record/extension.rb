ea12c5aae83dd3f2edefce765438e94ff7c4f870
Patrick Bajao <ebajao@gitlab.com>
2019-06-05 20:26:56 +0800

    Cleanup #attributes method
    Since we're prepending the ActiveRecord::Extension module, we
    can take advantage of it and avoid using an alias to extend the
    original #attributes method.

--- lib/gitlab/markdown_cache/active_record/extension.rb
+++ lib/gitlab/markdown_cache/active_record/extension.rb
@@ -7,29 +7,23 @@ module Gitlab
         extend ActiveSupport::Concern
 
         included do
-          # Always exclude _html fields from attributes (including serialization).
-          # They contain unredacted HTML, which would be a security issue
-          alias_method :attributes_before_markdown_cache, :attributes
-          def attributes
-            attrs = attributes_before_markdown_cache
-            html_fields = cached_markdown_fields.html_fields
-            whitelisted = cached_markdown_fields.html_fields_whitelisted
-            exclude_fields = html_fields - whitelisted
-
-            exclude_fields.each do |field|
-              attrs.delete(field)
-            end
-
-            if whitelisted.empty?
-              attrs.delete('cached_markdown_version')
-            end
-
-            attrs
-          end
-
           # Using before_update here conflicts with elasticsearch-model somehow
           before_create :refresh_markdown_cache, if: :invalidated_markdown_cache?
           before_update :refresh_markdown_cache, if: :invalidated_markdown_cache?
+        end
+
+        # Always exclude _html fields from attributes (including serialization).
+        # They contain unredacted HTML, which would be a security issue
+        def attributes
+          attrs = super
+          html_fields = cached_markdown_fields.html_fields
+          whitelisted = cached_markdown_fields.html_fields_whitelisted
+          exclude_fields = html_fields - whitelisted
+
+          attrs.except!(*exclude_fields)
+          attrs.delete('cached_markdown_version') if whitelisted.empty?
+
+          attrs
         end
 
         def changed_markdown_fields
