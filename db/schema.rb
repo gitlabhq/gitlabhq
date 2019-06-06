10dcfac1f17cffbbc542d47bdf27b682ab453dd2
Nick Thomas <nick@gitlab.com>
2019-06-06 13:57:50 +0000

    Revert "Merge branch 'pages-domain_ssl-valid-period' into 'master'"
    This reverts merge request !28743

--- db/schema.rb
+++ db/schema.rb
@@ -1583,8 +1583,6 @@ ActiveRecord::Schema.define(version: 20190530154715) do
     t.datetime_with_timezone "enabled_until"
     t.datetime_with_timezone "remove_at"
     t.boolean "auto_ssl_enabled", default: false, null: false
-    t.datetime_with_timezone "certificate_valid_not_before"
-    t.datetime_with_timezone "certificate_valid_not_after"
     t.index ["domain"], name: "index_pages_domains_on_domain", unique: true, using: :btree
     t.index ["project_id", "enabled_until"], name: "index_pages_domains_on_project_id_and_enabled_until", using: :btree
     t.index ["project_id"], name: "index_pages_domains_on_project_id", using: :btree
