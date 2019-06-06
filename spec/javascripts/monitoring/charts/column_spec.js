5665e91ade9e16ec39e3d7e82e31849ef3554ce4
Jose Vargas <jvargas@gitlab.com>
2019-06-06 13:35:29 -0500

    Add column chart component to the monitoring bundle
    This merge request just adds the column chart
    component without integrating it to the dashboard

---
+++ spec/javascripts/monitoring/charts/column_spec.js
@@ -0,0 +1,58 @@
+import { shallowMount } from '@vue/test-utils';
+import { GlColumnChart } from '@gitlab/ui/dist/charts';
+import ColumnChart from '~/monitoring/components/charts/column.vue';
+
+describe('Column component', () => {
+  let columnChart;
+
+  beforeEach(() => {
+    columnChart = shallowMount(ColumnChart, {
+      propsData: {
+        graphData: {
+          queries: [
+            {
+              x_label: 'Time',
+              y_label: 'Usage',
+              result: [
+                {
+                  metric: {},
+                  values: [
+                    [1495700554.925, '8.0390625'],
+                    [1495700614.925, '8.0390625'],
+                    [1495700674.925, '8.0390625'],
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+        containerWidth: 100,
+      },
+    });
+  });
+
+  afterEach(() => {
+    columnChart.destroy();
+  });
+
+  describe('wrapped components', () => {
+    describe('GitLab UI column chart', () => {
+      let glColumnChart;
+
+      beforeEach(() => {
+        glColumnChart = columnChart.find(GlColumnChart);
+      });
+
+      it('is a Vue instance', () => {
+        expect(glColumnChart.isVueInstance()).toBe(true);
+      });
+
+      it('receives data properties needed for proper chart render', () => {
+        const props = glColumnChart.props();
+
+        expect(props.data).toBe(columnChart.vm.chartData);
+        expect(props.option).toBe(columnChart.vm.chartOptions);
+      });
+    });
+  });
+});
