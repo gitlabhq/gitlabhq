88283d8b47af5f5c8bba12e2a3f1c98e91f5749f
sarahghp <sarah.groff.palermo@gmail.com>
2019-06-10 15:14:40 -0400

    Constrain parent so trunc works

--- app/assets/javascripts/pipelines/components/graph/job_name_component.vue
+++ app/assets/javascripts/pipelines/components/graph/job_name_component.vue
@@ -25,7 +25,7 @@ export default {
 };
 </script>
 <template>
-  <span class="ci-job-name-component">
+  <span class="ci-job-name-component mw-100">
     <ci-icon :status="status" />
     <span class="ci-status-text text-truncate mw-70p gl-pl-1 d-inline-block align-bottom">
       {{ name }}
