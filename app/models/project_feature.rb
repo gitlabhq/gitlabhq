06440b12d839c34ab4bf1dffdd0d4f5277901f8a
Nick Thomas <nick@gitlab.com>
2019-06-12 13:20:33 +0100

    Revert "Avoid loading objects from DB in ES results"
    This reverts commit d9cb907c3e987363065136bafb2156e86bc5de26.

--- app/models/project_feature.rb
+++ app/models/project_feature.rb
@@ -72,8 +72,6 @@ class ProjectFeature < ApplicationRecord
   default_value_for :wiki_access_level,           value: ENABLED, allows_nil: false
   default_value_for :repository_access_level,     value: ENABLED, allows_nil: false
 
-  scope :for_project_id, -> (project) { where(project: project) }
-
   def feature_available?(feature, user)
     # This feature might not be behind a feature flag at all, so default to true
     return false unless ::Feature.enabled?(feature, user, default_enabled: true)
--- app/services/search_service.rb
+++ app/services/search_service.rb
@@ -52,10 +52,6 @@ class SearchService
     @search_objects ||= search_results.objects(scope, params[:page])
   end
 
-  def display_options
-    @display_options ||= search_results.display_options(scope)
-  end
-
   private
 
   def search_service
