792a998bd4ea36687ddbbbdc0b991fae64476647
Peter Leitzen <pleitzen@gitlab.com>
2019-06-12 15:59:16 +0000

    Whitelist query limiting admin usage data endpoint

--- app/controllers/admin/application_settings_controller.rb
+++ app/controllers/admin/application_settings_controller.rb
@@ -2,7 +2,9 @@
 
 class Admin::ApplicationSettingsController < Admin::ApplicationController
   include InternalRedirect
+
   before_action :set_application_setting
+  before_action :whitelist_query_limiting, only: [:usage_data]
 
   def show
   end
@@ -100,6 +102,10 @@ class Admin::ApplicationSettingsController < Admin::ApplicationController
 
   def set_application_setting
     @application_setting = Gitlab::CurrentSettings.current_application_settings
+  end
+
+  def whitelist_query_limiting
+    Gitlab::QueryLimiting.whitelist('https://gitlab.com/gitlab-org/gitlab-ce/issues/63107')
   end
 
   def application_setting_params
