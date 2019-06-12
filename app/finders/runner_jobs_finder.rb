7edeedc700cdb825aa885b53ceca8ebc145c1b23
sujay patel <sujaypatel1996@gmail.com>
2019-06-13 01:33:21 +0530

    Adding order by to list runner jobs api.

--- app/finders/runner_jobs_finder.rb
+++ app/finders/runner_jobs_finder.rb
@@ -3,6 +3,8 @@
 class RunnerJobsFinder
   attr_reader :runner, :params
 
+  ALLOWED_INDEXED_COLUMNS = %w[id created_at].freeze
+
   def initialize(runner, params = {})
     @runner = runner
     @params = params
@@ -11,7 +13,7 @@ class RunnerJobsFinder
   def execute
     items = @runner.builds
     items = by_status(items)
-    items
+    sort_items(items)
   end
 
   private
@@ -21,6 +23,24 @@ class RunnerJobsFinder
     return items unless HasStatus::AVAILABLE_STATUSES.include?(params[:status])
 
     items.where(status: params[:status])
+  end
+  # rubocop: enable CodeReuse/ActiveRecord
+
+  # rubocop: disable CodeReuse/ActiveRecord
+  def sort_items(items)
+    order_by = if ALLOWED_INDEXED_COLUMNS.include?(params[:order_by])
+                 params[:order_by]
+               else
+                 :id
+               end
+
+    sort = if params[:sort] =~ /\A(ASC|DESC)\z/i
+             params[:sort]
+           else
+             :desc
+           end
+
+    items.order(order_by => sort)
   end
   # rubocop: enable CodeReuse/ActiveRecord
 end
