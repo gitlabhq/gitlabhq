4f817379401e8b1f6dd0be339929470dfa62ba47
Nick Thomas <nick@gitlab.com>
2019-06-14 18:29:25 +0100

    Renumber a new UserCallout enum for EE compatibility

--- app/models/user_callout_enums.rb
+++ app/models/user_callout_enums.rb
@@ -6,12 +6,15 @@ module UserCalloutEnums
   #
   # This method is separate from the `UserCallout` model so that it can be
   # extended by EE.
+  #
+  # If you are going to add new items to this hash, check that you're not going
+  # to conflict with EE-only values: https://gitlab.com/gitlab-org/gitlab-ee/blob/master/ee/app/models/ee/user_callout_enums.rb
   def self.feature_names
     {
       gke_cluster_integration: 1,
       gcp_signup_offer: 2,
       cluster_security_warning: 3,
-      suggest_popover_dismissed: 4
+      suggest_popover_dismissed: 9
     }
   end
 end
