87f66a5623cd90f142dc8e1a06a98a80fa7b0c7d
Sanad Liaquat <sliaquat@gitlab.com>
2019-06-17 20:59:45 +0500

    Unquarantine OAuth spec

--- qa/qa/specs/features/browser_ui/1_manage/login/login_via_oauth_spec.rb
+++ qa/qa/specs/features/browser_ui/1_manage/login/login_via_oauth_spec.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 module QA
-  # https://gitlab.com/gitlab-org/quality/nightly/issues/100
-  context 'Manage', :orchestrated, :oauth, :quarantine do
+  context 'Manage', :orchestrated, :oauth do
     describe 'OAuth login' do
       it 'User logs in to GitLab with GitHub OAuth' do
         Runtime::Browser.visit(:gitlab, Page::Main::Login)
