499bcda0f42a2560c32ff54dce940137a49162fe
Kamil Trzciński <ayufan@ayufan.eu>
2019-06-18 19:00:14 +0000

    Merge branch 'revert-concurrent-pipeline-schedule-creation' into 'master'
    Revert concurrent pipeline creation for pipeline schedules
    
    See merge request gitlab-org/gitlab-ce!29794
    
    (cherry picked from commit ba952d53c5782e49b59ba3e5dd89c2c1eca02c80)
    
    36b30cf1 Revert concurrent pipeline schedule creation

--- app/services/ci/pipeline_schedule_service.rb
+++ app/services/ci/pipeline_schedule_service.rb
@@ -7,7 +7,18 @@ module Ci
       # Otherwise, multiple pipelines could be created in a short interval.
       schedule.schedule_next_run!
 
-      RunPipelineScheduleWorker.perform_async(schedule.id, schedule.owner&.id)
+      if Feature.enabled?(:ci_pipeline_schedule_async)
+        RunPipelineScheduleWorker.perform_async(schedule.id, schedule.owner&.id)
+      else
+        begin
+          RunPipelineScheduleWorker.new.perform(schedule.id, schedule.owner&.id)
+        ensure
+          ##
+          # This is the temporary solution for avoiding the memory bloat.
+          # See more https://gitlab.com/gitlab-org/gitlab-ce/issues/61955
+          GC.start if Feature.enabled?(:ci_pipeline_schedule_force_gc, default_enabled: true)
+        end
+      end
     end
   end
 end
