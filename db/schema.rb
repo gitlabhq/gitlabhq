d9abaab0c8fcc2f09c525a75093f7bb8858764ad
Arun Kumar Mohan <arunmohandm@gmail.com>
2019-06-18 17:14:33 -0500

    Disallow `NULL` values for `geo_nodes.primary` column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190613030606) do
+ActiveRecord::Schema.define(version: 20190618171120) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1391,7 +1391,7 @@ ActiveRecord::Schema.define(version: 20190613030606) do
   end
 
   create_table "geo_nodes", id: :serial, force: :cascade do |t|
-    t.boolean "primary"
+    t.boolean "primary", default: false, null: false
     t.integer "oauth_application_id"
     t.boolean "enabled", default: true, null: false
     t.string "access_key"
