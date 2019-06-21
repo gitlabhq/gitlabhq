a26916d18cab6bc3ed2e6846d130fcd5e36fb313
Fabio Pitino <fpitino@gitlab.com>
2019-06-21 09:46:58 +0100

    Require pipeline if "Pipeline must succeed" is set
    When a user sets only_allow_merge_if_pipeline_succeeds, also named
    as "Pipeline must succeed" project setting, we require the pipeline
    to be present.
    
    This solves race condition issues especially with external CI
    providers when a build is triggered externally but no pipelines are
    created in GitLab yet.
    
    Document that a head pipeline is expected when using "Pipeline
    must succeed" setting. Also explain limitations with the use of
    only/except where there may not be any jobs created and the merge
    request will not be allowed to be merged.

--- app/models/merge_request.rb
+++ app/models/merge_request.rb
@@ -1030,7 +1030,7 @@ class MergeRequest < ApplicationRecord
 
   def mergeable_ci_state?
     return true unless project.only_allow_merge_if_pipeline_succeeds?
-    return true unless head_pipeline
+    return false unless head_pipeline
 
     actual_head_pipeline&.success? || actual_head_pipeline&.skipped?
   end
