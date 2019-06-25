364733ce51219c0594c7c5a9a354cc7bf81747a4
Rémy Coutable <remy@rymai.me>
2019-06-25 14:03:54 +0000

    Merge branch 'sh-recover-ee-schema-backport-migration-failure' into 'master'
    Prevent EE backport migrations from running if CE is not migrated
    
    Closes #63612
    
    See merge request gitlab-org/gitlab-ce!30002
    
    (cherry picked from commit 34df0b303eed0cee83d8c0ec6178d3c575a0b555)
    
    1b063778 Prevent EE backport migrations from running if CE is not migrated

---
+++ spec/migrations/backport_enterprise_schema_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+require Rails.root.join('db', 'migrate', '20190402150158_backport_enterprise_schema.rb')
+
+describe BackportEnterpriseSchema, :migration, schema: 20190329085614 do
+  include MigrationsHelpers
+
+  def drop_if_exists(table)
+    active_record_base.connection.drop_table(table) if active_record_base.connection.table_exists?(table)
+  end
+
+  describe '#up' do
+    it 'creates new EE tables' do
+      migrate!
+
+      expect(active_record_base.connection.table_exists?(:epics)).to be true
+      expect(active_record_base.connection.table_exists?(:geo_nodes)).to be true
+    end
+
+    context 'missing EE columns' do
+      before do
+        drop_if_exists(:epics)
+
+        active_record_base.connection.create_table "epics" do |t|
+          t.integer :group_id, null: false, index: true
+          t.integer :author_id, null: false, index: true
+        end
+      end
+
+      after do
+        drop_if_exists(:epics)
+      end
+
+      it 'flags an error' do
+        expect { migrate! }.to raise_error(/Your database is missing.*that is present for GitLab EE/)
+      end
+    end
+  end
+end
