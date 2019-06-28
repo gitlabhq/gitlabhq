7c38c0b4b2e43ae86ca32ea3363707e89544152f
Rémy Coutable <remy@rymai.me>
2019-06-28 15:55:54 +0000

    Revert "Merge branch '62722-boot-app-in-ci-and-check-memory' into 'master'"
    This reverts merge request !30031

--- config/boot.rb
+++ config/boot.rb
@@ -3,7 +3,7 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 # Set up gems listed in the Gemfile.
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
 begin
-  require 'bootsnap/setup' unless ENV['DISABLE_BOOTSNAP']
+  require 'bootsnap/setup'
 rescue LoadError
   # bootsnap is an optional dependency, so if we don't have it, it's fine
 end
