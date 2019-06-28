f0f3246a2b06a4e0edd7d6e309a21aa559118921
Eugenia Grieff <egrieff@gitlab.com>
2019-06-28 09:47:54 +0000

    Do not change updated_at on an issue when reordering on an issue board

--- spec/support/shared_examples/services/boards/issues_move_service.rb
+++ spec/support/shared_examples/services/boards/issues_move_service.rb
@@ -1,7 +1,16 @@
 shared_examples 'issues move service' do |group|
+  shared_examples 'updating timestamps' do
+    it 'updates updated_at' do
+      expect {described_class.new(parent, user, params).execute(issue)}
+        .to change {issue.reload.updated_at}
+    end
+  end
+
   context 'when moving an issue between lists' do
     let(:issue)  { create(:labeled_issue, project: project, labels: [bug, development]) }
     let(:params) { { board_id: board1.id, from_list_id: list1.id, to_list_id: list2.id } }
+
+    it_behaves_like 'updating timestamps'
 
     it 'delegates the label changes to Issues::UpdateService' do
       service = double(:service)
@@ -23,6 +32,8 @@ shared_examples 'issues move service' do |group|
 
     let(:issue)  { create(:labeled_issue, project: project, labels: [bug, development, testing, regression]) }
     let(:params) { { board_id: board1.id, from_list_id: list2.id, to_list_id: closed.id } }
+
+    it_behaves_like 'updating timestamps'
 
     it 'delegates the close proceedings to Issues::CloseService' do
       expect_any_instance_of(Issues::CloseService).to receive(:execute).with(issue).once
@@ -46,6 +57,8 @@ shared_examples 'issues move service' do |group|
     let(:issue)  { create(:labeled_issue, project: project, labels: [bug, development, testing, regression], milestone: milestone) }
     let(:params) { { board_id: board1.id, from_list_id: list2.id, to_list_id: backlog.id } }
 
+    it_behaves_like 'updating timestamps'
+
     it 'keeps labels and milestone' do
       described_class.new(parent, user, params).execute(issue)
       issue.reload
@@ -58,6 +71,8 @@ shared_examples 'issues move service' do |group|
   context 'when moving from closed' do
     let(:issue)  { create(:labeled_issue, :closed, project: project, labels: [bug]) }
     let(:params) { { board_id: board1.id, from_list_id: closed.id, to_list_id: list2.id } }
+
+    it_behaves_like 'updating timestamps'
 
     it 'delegates the re-open proceedings to Issues::ReopenService' do
       expect_any_instance_of(Issues::ReopenService).to receive(:execute).with(issue).once
@@ -75,10 +90,13 @@ shared_examples 'issues move service' do |group|
   end
 
   context 'when moving to same list' do
-    let(:issue)   { create(:labeled_issue, project: project, labels: [bug, development]) }
-    let(:issue1)  { create(:labeled_issue, project: project, labels: [bug, development]) }
-    let(:issue2)  { create(:labeled_issue, project: project, labels: [bug, development]) }
-    let(:params)  { { board_id: board1.id, from_list_id: list1.id, to_list_id: list1.id } }
+    let(:assignee) { create(:user) }
+    let(:params)   { { board_id: board1.id, from_list_id: list1.id, to_list_id: list1.id } }
+    let(:issue1)   { create(:labeled_issue, project: project, labels: [bug, development]) }
+    let(:issue2)   { create(:labeled_issue, project: project, labels: [bug, development]) }
+    let(:issue) do
+      create(:labeled_issue, project: project, labels: [bug, development], assignees: [assignee])
+    end
 
     it 'returns false' do
       expect(described_class.new(parent, user, params).execute(issue)).to eq false
@@ -90,16 +108,34 @@ shared_examples 'issues move service' do |group|
       expect(issue.reload.labels).to contain_exactly(bug, development)
     end
 
-    it 'sorts issues' do
-      [issue, issue1, issue2].each do |issue|
-        issue.move_to_end && issue.save!
-      end
+    it 'keeps issues assignees' do
+      described_class.new(parent, user, params).execute(issue)
 
-      params.merge!(move_after_id: issue1.id, move_before_id: issue2.id)
+      expect(issue.reload.assignees).to contain_exactly(assignee)
+    end
+
+    it 'sorts issues' do
+      reorder_issues(params, issues: [issue, issue1, issue2])
 
       described_class.new(parent, user, params).execute(issue)
 
       expect(issue.relative_position).to be_between(issue1.relative_position, issue2.relative_position)
+    end
+
+    it 'does not update updated_at' do
+      reorder_issues(params, issues: [issue, issue1, issue2])
+
+      updated_at = issue.updated_at
+      updated_at1 = issue1.updated_at
+      updated_at2 = issue2.updated_at
+
+      Timecop.travel(1.minute.from_now) do
+        described_class.new(parent, user, params).execute(issue)
+      end
+
+      expect(issue.reload.updated_at.change(usec: 0)).to eq updated_at.change(usec: 0)
+      expect(issue1.reload.updated_at.change(usec: 0)).to eq updated_at1.change(usec: 0)
+      expect(issue2.reload.updated_at.change(usec: 0)).to eq updated_at2.change(usec: 0)
     end
 
     if group
@@ -113,6 +149,14 @@ shared_examples 'issues move service' do |group|
           described_class.new(parent, user, params).execute(issue)
         end
       end
+    end
+
+    def reorder_issues(params, issues: [])
+      issues.each do |issue|
+        issue.move_to_end && issue.save!
+      end
+
+      params.merge!(move_after_id: issues[1].id, move_before_id: issues[2].id)
     end
   end
 end
