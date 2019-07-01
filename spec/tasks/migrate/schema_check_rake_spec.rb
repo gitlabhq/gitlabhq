de0f41b4d12af62c97e1715bdad14fa8fca6e482
Lin Jen-Shin <godfat@godfat.org>
2019-07-01 00:47:57 +0000

    Quarantine schema_version_check because tests after it failed

--- spec/tasks/migrate/schema_check_rake_spec.rb
+++ spec/tasks/migrate/schema_check_rake_spec.rb
@@ -3,7 +3,7 @@
 require 'spec_helper'
 require 'rake'
 
-describe 'schema_version_check rake task' do
+describe 'schema_version_check rake task', :quarantine do
   include StubENV
 
   before :all do
