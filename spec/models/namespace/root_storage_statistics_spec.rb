dfdfa913ba9cb74beb7adad0352c5efadec84494
Mayra Cabrera <mcabrera@gitlab.com>
2019-07-02 14:44:39 +0000

    Includes logic to persist namespace statistics
    - Add two new ActiveRecord models:
      - RootNamespaceStoragestatistics will persist root namespace statistics
      - NamespaceAggregationSchedule will save information when a new update
    to the namespace statistics needs to be scheduled
    - Inject into UpdateProjectStatistics concern a new callback that will
    call an async job to insert a new row onto NamespaceAggregationSchedule
    table
    - When a new row is inserted a new job is scheduled. This job will
    update call an specific service to update the statistics and after that
    it will delete thee aggregated scheduled row
    - The RefresherServices makes heavy use of arel to build composable
    queries to update Namespace::RootStorageStatistics attributes.
    - Add an extra worker to traverse pending rows on
    NAmespace::AggregationSchedule table and schedule a worker for each one
    of this rows.
    - Add an extra worker to traverse pending rows on
    NAmespace::AggregationSchedule table and schedule a worker for each one
    of this rows

--- spec/models/namespace/root_storage_statistics_spec.rb
+++ spec/models/namespace/root_storage_statistics_spec.rb
@@ -7,4 +7,69 @@ RSpec.describe Namespace::RootStorageStatistics, type: :model do
   it { is_expected.to have_one(:route).through(:namespace) }
 
   it { is_expected.to delegate_method(:all_projects).to(:namespace) }
+
+  describe '#recalculate!' do
+    let(:namespace) { create(:group) }
+    let(:root_storage_statistics) { create(:namespace_root_storage_statistics, namespace: namespace) }
+
+    let(:project1) { create(:project, namespace: namespace) }
+    let(:project2) { create(:project, namespace: namespace) }
+
+    let!(:stat1) { create(:project_statistics, project: project1, with_data: true, size_multiplier: 100) }
+    let!(:stat2) { create(:project_statistics, project: project2, with_data: true, size_multiplier: 200) }
+
+    shared_examples 'data refresh' do
+      it 'aggregates project statistics' do
+        root_storage_statistics.recalculate!
+
+        root_storage_statistics.reload
+
+        total_repository_size = stat1.repository_size + stat2.repository_size
+        total_wiki_size = stat1.wiki_size + stat2.wiki_size
+        total_lfs_objects_size = stat1.lfs_objects_size + stat2.lfs_objects_size
+        total_build_artifacts_size = stat1.build_artifacts_size + stat2.build_artifacts_size
+        total_packages_size = stat1.packages_size + stat2.packages_size
+        total_storage_size = stat1.storage_size + stat2.storage_size
+
+        expect(root_storage_statistics.repository_size).to eq(total_repository_size)
+        expect(root_storage_statistics.wiki_size).to eq(total_wiki_size)
+        expect(root_storage_statistics.lfs_objects_size).to eq(total_lfs_objects_size)
+        expect(root_storage_statistics.build_artifacts_size).to eq(total_build_artifacts_size)
+        expect(root_storage_statistics.packages_size).to eq(total_packages_size)
+        expect(root_storage_statistics.storage_size).to eq(total_storage_size)
+      end
+
+      it 'works when there are no projects' do
+        Project.delete_all
+
+        root_storage_statistics.recalculate!
+
+        root_storage_statistics.reload
+        expect(root_storage_statistics.repository_size).to eq(0)
+        expect(root_storage_statistics.wiki_size).to eq(0)
+        expect(root_storage_statistics.lfs_objects_size).to eq(0)
+        expect(root_storage_statistics.build_artifacts_size).to eq(0)
+        expect(root_storage_statistics.packages_size).to eq(0)
+        expect(root_storage_statistics.storage_size).to eq(0)
+      end
+    end
+
+    it_behaves_like 'data refresh'
+
+    context 'with subgroups', :nested_groups do
+      let(:subgroup1) { create(:group, parent: namespace)}
+      let(:subgroup2) { create(:group, parent: subgroup1)}
+
+      let(:project1) { create(:project, namespace: subgroup1) }
+      let(:project2) { create(:project, namespace: subgroup2) }
+
+      it_behaves_like 'data refresh'
+    end
+
+    context 'with a personal namespace' do
+      let(:namespace) { create(:user).namespace }
+
+      it_behaves_like 'data refresh'
+    end
+  end
 end
---
+++ spec/workers/namespaces/root_statistics_worker_spec.rb
@@ -0,0 +1,88 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Namespaces::RootStatisticsWorker, '#perform' do
+  let(:group) { create(:group, :with_aggregation_schedule) }
+
+  subject(:worker) { described_class.new }
+
+  context 'with a namespace' do
+    it 'executes refresher service' do
+      expect_any_instance_of(Namespaces::StatisticsRefresherService)
+        .to receive(:execute)
+
+      worker.perform(group.id)
+    end
+
+    it 'deletes namespace aggregated schedule row' do
+      worker.perform(group.id)
+
+      expect(group.reload.aggregation_schedule).to be_nil
+    end
+
+    context 'when something goes wrong when updating' do
+      before do
+        allow_any_instance_of(Namespaces::StatisticsRefresherService)
+          .to receive(:execute)
+          .and_raise(Namespaces::StatisticsRefresherService::RefresherError, 'error')
+      end
+
+      it 'does not delete the aggregation schedule' do
+        worker.perform(group.id)
+
+        expect(group.reload.aggregation_schedule).to be_present
+      end
+
+      it 'logs the error' do
+        # A Namespace::RootStatisticsWorker is scheduled when
+        # a Namespace::AggregationSchedule is created, so having
+        # create(:group, :with_aggregation_schedule), will execute
+        # another worker
+        allow_any_instance_of(Namespace::AggregationSchedule)
+          .to receive(:schedule_root_storage_statistics).and_return(nil)
+
+        expect(Gitlab::SidekiqLogger).to receive(:error).once
+
+        worker.perform(group.id)
+      end
+    end
+  end
+
+  context 'with no namespace' do
+    before do
+      group.destroy
+    end
+
+    it 'does not execute the refresher service' do
+      expect_any_instance_of(Namespaces::StatisticsRefresherService)
+        .not_to receive(:execute)
+
+      worker.perform(group.id)
+    end
+  end
+
+  context 'with a namespace with no aggregation scheduled' do
+    before do
+      group.aggregation_schedule.destroy
+    end
+
+    it 'does not execute the refresher service' do
+      expect_any_instance_of(Namespaces::StatisticsRefresherService)
+        .not_to receive(:execute)
+
+      worker.perform(group.id)
+    end
+  end
+
+  context 'when update_statistics_namespace is off' do
+    it 'does not create a new one' do
+      stub_feature_flags(update_statistics_namespace: false, namespace: group)
+
+      expect_any_instance_of(Namespaces::StatisticsRefresherService)
+        .not_to receive(:execute)
+
+      worker.perform(group.id)
+    end
+  end
+end
