ff1a4a4241f1628491a1aec05b440ff650ba66ad
Denys Mishunov <dmishunov@gitlab.com>
2019-07-03 14:14:44 +0200

    Initiate editor for any file without content
    Important to get file data for any file that has no content set yet: we
    need this information in order to build a previe for changed/staged
    files if they were not yet opened in the editor

--- spec/javascripts/ide/components/repo_editor_spec.js
+++ spec/javascripts/ide/components/repo_editor_spec.js
@@ -30,6 +30,7 @@ describe('RepoEditor', () => {
     Vue.set(vm.$store.state.entries, f.path, f);
 
     spyOn(vm, 'getFileData').and.returnValue(Promise.resolve());
+    spyOn(vm, 'getRawFileData').and.returnValue(Promise.resolve());
 
     vm.$mount();
 
@@ -407,6 +408,44 @@ describe('RepoEditor', () => {
     });
   });
 
+  describe('initEditor', () => {
+    beforeEach(() => {
+      spyOn(vm.editor, 'createInstance');
+      spyOnProperty(vm, 'shouldHideEditor').and.returnValue(true);
+    });
+
+    it('is being initialised for files without content even if shouldHideEditor is `true`', done => {
+      vm.file.content = '';
+      vm.file.raw = '';
+
+      vm.initEditor();
+      vm.$nextTick()
+        .then(() => {
+          expect(vm.getFileData).toHaveBeenCalled();
+          expect(vm.getRawFileData).toHaveBeenCalled();
+        })
+        .then(done)
+        .catch(done.fail);
+    });
+
+    it('does not initialize editor for files already with content', done => {
+      expect(vm.getFileData.calls.count()).toEqual(1);
+      expect(vm.getRawFileData.calls.count()).toEqual(1);
+
+      vm.file.content = 'foo';
+
+      vm.initEditor();
+      vm.$nextTick()
+        .then(() => {
+          expect(vm.getFileData.calls.count()).toEqual(1);
+          expect(vm.getRawFileData.calls.count()).toEqual(1);
+          expect(vm.editor.createInstance).not.toHaveBeenCalled();
+        })
+        .then(done)
+        .catch(done.fail);
+    });
+  });
+
   it('calls removePendingTab when old file is pending', done => {
     spyOnProperty(vm, 'shouldHideEditor').and.returnValue(true);
     spyOn(vm, 'removePendingTab');
@@ -416,6 +455,7 @@ describe('RepoEditor', () => {
     vm.$nextTick()
       .then(() => {
         vm.file = file('testing');
+        vm.file.content = 'foo'; // need to prevent full cycle of initEditor
 
         return vm.$nextTick();
       })
