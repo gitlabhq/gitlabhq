a01c10ac4ad3087c54e43d171b2c65bc9e7328d5
Stan Hu <stanhu@gmail.com>
2019-07-03 21:47:32 +0000

    Merge branch 'limit-amount-of-tests-returned' into 'master'
    Limit amount of JUnit tests returned
    
    Closes #64035
    
    See merge request gitlab-org/gitlab-ce!30274
    
    (cherry picked from commit a1d113c9ff74d973575db3ff9738939aaf1c59c1)
    
    a08209ff Limit amount of JUnit tests returned

--- spec/serializers/test_reports_comparer_serializer_spec.rb
+++ spec/serializers/test_reports_comparer_serializer_spec.rb
@@ -44,13 +44,7 @@ describe TestReportsComparerSerializer do
           base_reports.get_suite('rspec').add_test_case(create_test_case_rspec_success)
           base_reports.get_suite('junit').add_test_case(create_test_case_java_failed)
           head_reports.get_suite('rspec').add_test_case(create_test_case_rspec_success)
-          head_reports.get_suite('junit').add_test_case(create_test_case_java_resolved)
-        end
-
-        let(:create_test_case_java_resolved) do
-          create_test_case_java_failed.tap do |test_case|
-            test_case.instance_variable_set("@status", Gitlab::Ci::Reports::TestCase::STATUS_SUCCESS)
-          end
+          head_reports.get_suite('junit').add_test_case(create_test_case_java_success)
         end
 
         it 'matches the schema' do
