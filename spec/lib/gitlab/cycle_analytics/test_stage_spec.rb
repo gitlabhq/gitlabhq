9d2747a6a18561c3186567908323db1ea2d43ff7
Małgorzata Ksionek <mksionek@gitlab.com>
2019-07-04 11:13:29 +0200

    Modify specs according to review

--- spec/lib/gitlab/cycle_analytics/test_stage_spec.rb
+++ spec/lib/gitlab/cycle_analytics/test_stage_spec.rb
@@ -9,16 +9,15 @@ describe Gitlab::CycleAnalytics::TestStage do
   it_behaves_like 'base stage'
 
   describe '#median' do
-    let!(:issue_1) { create(:issue, project: project, created_at: 90.minutes.ago) }
-    let!(:issue_2) { create(:issue, project: project, created_at: 60.minutes.ago) }
-    let!(:issue_3) { create(:issue, project: project, created_at: 60.minutes.ago) }
-    let!(:mr_1) { create(:merge_request, :closed, source_project: project, created_at: 60.minutes.ago) }
-    let!(:mr_2) { create(:merge_request, :closed, source_project: project, created_at: 40.minutes.ago, source_branch: 'A') }
-    let!(:mr_3) { create(:merge_request, source_project: project, created_at: 10.minutes.ago, source_branch: 'B') }
-    let!(:mr_4) { create(:merge_request, source_project: project, created_at: 10.minutes.ago, source_branch: 'C') }
-    let!(:mr_5) { create(:merge_request, source_project: project, created_at: 10.minutes.ago, source_branch: 'D') }
-
     before do
+      issue_1 = create(:issue, project: project, created_at: 90.minutes.ago)
+      issue_2 = create(:issue, project: project, created_at: 60.minutes.ago)
+      issue_3 = create(:issue, project: project, created_at: 60.minutes.ago)
+      mr_1 = create(:merge_request, :closed, source_project: project, created_at: 60.minutes.ago)
+      mr_2 = create(:merge_request, :closed, source_project: project, created_at: 40.minutes.ago, source_branch: 'A')
+      mr_3 = create(:merge_request, source_project: project, created_at: 10.minutes.ago, source_branch: 'B')
+      mr_4 = create(:merge_request, source_project: project, created_at: 10.minutes.ago, source_branch: 'C')
+      mr_5 = create(:merge_request, source_project: project, created_at: 10.minutes.ago, source_branch: 'D')
       mr_1.metrics.update!(latest_build_started_at: 32.minutes.ago, latest_build_finished_at: 2.minutes.ago)
       mr_2.metrics.update!(latest_build_started_at: 62.minutes.ago, latest_build_finished_at: 32.minutes.ago)
       mr_3.metrics.update!(latest_build_started_at: nil, latest_build_finished_at: nil)
