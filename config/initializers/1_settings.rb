3d12f07331f8031748bdd281993c490d3edfd6da
Robert Speicher <rspeicher@gmail.com>
2019-07-08 14:47:16 -0500

    CE-EE parity for files in config/initializers

--- config/initializers/1_settings.rb
+++ config/initializers/1_settings.rb
@@ -76,6 +76,7 @@ Gitlab.ee do
   Settings['smartcard'] ||= Settingslogic.new({})
   Settings.smartcard['enabled'] = false if Settings.smartcard['enabled'].nil?
   Settings.smartcard['client_certificate_required_port'] = 3444 if Settings.smartcard['client_certificate_required_port'].nil?
+  Settings.smartcard['required_for_git_access'] = false if Settings.smartcard['required_for_git_access'].nil?
 end
 
 Settings['omniauth'] ||= Settingslogic.new({})
