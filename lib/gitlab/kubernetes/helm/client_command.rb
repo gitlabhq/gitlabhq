c9b0ac5fc45c6e42db193b967cbeb98ed4168d62
Dylan Griffith <dyl.griffith@gmail.com>
2019-07-12 15:45:43 +1000

    GitLab Managed App ensure helm version uses tls opts
    The TLS opts were missing from helm version command which meant that it
    was just perpetually failing and hence wasting 30s of time waiting for a
    command to be successful that was never going to be successful. This
    never actually caused any errors because this loop will happily just
    fail 30 times without breaking the overall script but it was just a
    waste of installation time so now installing apps should be ~30s faster.

--- lib/gitlab/kubernetes/helm/client_command.rb
+++ lib/gitlab/kubernetes/helm/client_command.rb
@@ -13,14 +13,26 @@ module Gitlab
         end
 
         def wait_for_tiller_command
+          helm_check = ['helm', 'version', *optional_tls_flags].shelljoin
           # This is necessary to give Tiller time to restart after upgrade.
           # Ideally we'd be able to use --wait but cannot because of
           # https://github.com/helm/helm/issues/4855
-          'for i in $(seq 1 30); do helm version && break; sleep 1s; echo "Retrying ($i)..."; done'
+          "for i in $(seq 1 30); do #{helm_check} && break; sleep 1s; echo \"Retrying ($i)...\"; done"
         end
 
         def repository_command
           ['helm', 'repo', 'add', name, repository].shelljoin if repository
+        end
+
+        def optional_tls_flags
+          return [] unless files.key?(:'ca.pem')
+
+          [
+            '--tls',
+            '--tls-ca-cert', "#{files_dir}/ca.pem",
+            '--tls-cert', "#{files_dir}/cert.pem",
+            '--tls-key', "#{files_dir}/key.pem"
+          ]
         end
       end
     end
