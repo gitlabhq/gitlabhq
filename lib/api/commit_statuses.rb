4e814c257b74ac78a50f54ec57b1e1c7f78d43b7
Gaetan Semet <gaetan.semet@renault.com>
2019-07-15 14:47:47 +0200

    Multiple pipeline support for Build status
    This allows user to specify the pipeline ID
    when several pipelines has been triggered
    on the same branch and commit.
    
    Signed-off-by: Gaetan Semet <gaetan.semet@renault.com>

--- lib/api/commit_statuses.rb
+++ lib/api/commit_statuses.rb
@@ -52,6 +52,7 @@ module API
         optional :name,        type: String,  desc: 'A string label to differentiate this status from the status of other systems. Default: "default"'
         optional :context,     type: String,  desc: 'A string label to differentiate this status from the status of other systems. Default: "default"'
         optional :coverage,    type: Float,   desc: 'The total code coverage'
+        optional :pipeline_id, type: Integer, desc: 'An existing pipeline id, when multiple pipelines on the same commit sha have been triggered'
       end
       # rubocop: disable CodeReuse/ActiveRecord
       post ':id/statuses/:sha' do
@@ -72,8 +73,12 @@ module API
         not_found! 'References for commit' unless ref
 
         name = params[:name] || params[:context] || 'default'
+        pipeline = if params[:pipeline_id]
+                     @project.ci_pipelines.find_by(id: params[:pipeline_id])
+                   else
+                     @project.pipeline_for(ref, commit.sha)
+                   end
 
-        pipeline = @project.pipeline_for(ref, commit.sha)
         unless pipeline
           pipeline = @project.ci_pipelines.create!(
             source: :external,
