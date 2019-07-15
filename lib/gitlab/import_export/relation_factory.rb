2397f171a35485c422a48e5dba50fb7fee4c76b4
Fabio Pitino <fpitino@gitlab.com>
2019-07-15 10:28:01 +0200

    Ensure ImportExport maintains trigger ownership

--- lib/gitlab/import_export/relation_factory.rb
+++ lib/gitlab/import_export/relation_factory.rb
@@ -28,7 +28,7 @@ module Gitlab
                     links: 'Releases::Link',
                     metrics_setting: 'ProjectMetricsSetting' }.freeze
 
-      USER_REFERENCES = %w[author_id assignee_id updated_by_id merged_by_id latest_closed_by_id user_id created_by_id last_edited_by_id merge_user_id resolved_by_id closed_by_id].freeze
+      USER_REFERENCES = %w[author_id assignee_id updated_by_id merged_by_id latest_closed_by_id user_id created_by_id last_edited_by_id merge_user_id resolved_by_id closed_by_id owner_id].freeze
 
       PROJECT_REFERENCES = %w[project_id source_project_id target_project_id].freeze
 
