90aafe78aeddf77a82ddf5efce8139cc29c7e52f
Thong Kuah <tkuah@gitlab.com>
2019-07-17 11:37:34 +1200

    Removes clusters_cte feature flag
    This has been enabled on production without issue
    
    https://gitlab.com/gitlab-org/gitlab-ce/issues/64259

--- app/models/concerns/deployment_platform.rb
+++ app/models/concerns/deployment_platform.rb
@@ -12,35 +12,14 @@ module DeploymentPlatform
   private
 
   def find_deployment_platform(environment)
-    find_platform_kubernetes(environment) ||
+    find_platform_kubernetes_with_cte(environment) ||
       find_instance_cluster_platform_kubernetes(environment: environment)
-  end
-
-  def find_platform_kubernetes(environment)
-    if Feature.enabled?(:clusters_cte)
-      find_platform_kubernetes_with_cte(environment)
-    else
-      find_cluster_platform_kubernetes(environment: environment) ||
-        find_group_cluster_platform_kubernetes(environment: environment)
-    end
   end
 
   # EE would override this and utilize environment argument
   def find_platform_kubernetes_with_cte(_environment)
     Clusters::ClustersHierarchy.new(self).base_and_ancestors
       .enabled.default_environment
-      .first&.platform_kubernetes
-  end
-
-  # EE would override this and utilize environment argument
-  def find_cluster_platform_kubernetes(environment: nil)
-    clusters.enabled.default_environment
-      .last&.platform_kubernetes
-  end
-
-  # EE would override this and utilize environment argument
-  def find_group_cluster_platform_kubernetes(environment: nil)
-    Clusters::Cluster.enabled.default_environment.ancestor_clusters_for_clusterable(self)
       .first&.platform_kubernetes
   end
 
