1136c0c8e98d4f0d3fb4f50219657cabe0d45c99
Stan Hu <stanhu@gmail.com>
2019-07-17 16:34:27 -0700

    Add Rugged calls and duration to API and Rails logs
    This adds `rugged_duration_ms` and `rugged_calls` fields to
    `api_json.log` and `production_json.log`. This will make it easier to
    identify performance issues caused by excessive I/O.
    
    Closes https://gitlab.com/gitlab-org/gitlab-ce/issues/64676

--- lib/gitlab/grape_logging/loggers/perf_logger.rb
+++ lib/gitlab/grape_logging/loggers/perf_logger.rb
@@ -6,9 +6,28 @@ module Gitlab
     module Loggers
       class PerfLogger < ::GrapeLogging::Loggers::Base
         def parameters(_, _)
+          gitaly_data.merge(rugged_data)
+        end
+
+        def gitaly_data
+          gitaly_calls = Gitlab::GitalyClient.get_request_count
+
+          return {} if gitaly_calls.zero?
+
           {
             gitaly_calls: Gitlab::GitalyClient.get_request_count,
             gitaly_duration: Gitlab::GitalyClient.query_time_ms
+          }
+        end
+
+        def rugged_data
+          rugged_calls = Gitlab::RuggedInstrumentation.query_count
+
+          return {} if rugged_calls.zero?
+
+          {
+            rugged_calls: rugged_calls,
+            rugged_duration_ms: Gitlab::RuggedInstrumentation.query_time_ms
           }
         end
       end
