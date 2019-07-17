a6b60fee67f6ea4d22ab5cb34901d0707829eec0
Etienne Baqué <ebaque@gitlab.com>
2019-07-17 09:38:28 -0400

    Iterating through token to encrypt with find_each

--- db/post_migrate/20190711201818_encrypt_deploy_tokens_tokens.rb
+++ db/post_migrate/20190711201818_encrypt_deploy_tokens_tokens.rb
@@ -9,7 +9,7 @@ class EncryptDeployTokensTokens < ActiveRecord::Migration[5.1]
 
   def up
     say_with_time("Encrypting tokens from deploy_tokens") do
-      DeploymentTokens.where('token_encrypted is NULL AND token IS NOT NULL').find_each do |deploy_token|
+      DeploymentTokens.where('token_encrypted is NULL AND token IS NOT NULL').find_each(batch_size: 10000) do |deploy_token|
         token_encrypted = Gitlab::CryptoHelper.aes256_gcm_encrypt(deploy_token.token)
         deploy_token.update!(token_encrypted: token_encrypted)
       end
@@ -18,7 +18,7 @@ class EncryptDeployTokensTokens < ActiveRecord::Migration[5.1]
 
   def down
     say_with_time("Decrypting tokens from deploy_tokens") do
-      DeploymentTokens.where('token_encrypted IS NOT NULL AND token IS NULL').find_each do |deploy_token|
+      DeploymentTokens.where('token_encrypted IS NOT NULL AND token IS NULL').find_each(batch_size: 10000) do |deploy_token|
         token = Gitlab::CryptoHelper.aes256_gcm_decrypt(deploy_token.token_encrypted)
         deploy_token.update!(token: token)
       end
