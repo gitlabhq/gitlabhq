d375e834c91659416d5644eb4c008b00d4914ccb
Walmyr Lima <walmyr@gitlab.com>
2019-07-18 17:13:02 +0200

    Refactor collapse comments end-to-end test
    To:
    - Better separate scopes (pre-conditions on before, tests on it)
    - Remove unnecessary expectation
    - Replace other unnecessary expectations by page.assert_text
    - Move variables closer to where they're used

--- qa/qa/specs/features/browser_ui/2_plan/issue/collapse_comments_in_discussions_spec.rb
+++ qa/qa/specs/features/browser_ui/2_plan/issue/collapse_comments_in_discussions_spec.rb
@@ -3,31 +3,32 @@
 module QA
   context 'Plan' do
     describe 'collapse comments in issue discussions' do
-      let(:issue_title) { 'issue title' }
+      let(:my_first_reply) { 'My first reply' }
 
-      it 'user collapses reply for comments in an issue' do
+      before do
         Runtime::Browser.visit(:gitlab, Page::Main::Login)
         Page::Main::Login.perform(&:sign_in_using_credentials)
 
         issue = Resource::Issue.fabricate_via_api! do |issue|
-          issue.title = issue_title
+          issue.title = 'issue title'
         end
 
         issue.visit!
 
-        expect(page).to have_content(issue_title)
-
         Page::Project::Issue::Show.perform do |show_page|
-          my_first_discussion = "My first discussion"
-          my_first_reply = "My First Reply"
-          one_reply = "1 reply"
+          my_first_discussion = 'My first discussion'
 
           show_page.select_all_activities_filter
           show_page.start_discussion(my_first_discussion)
-          expect(show_page).to have_content(my_first_discussion)
-
+          page.assert_text(my_first_discussion)
           show_page.reply_to_discussion(my_first_reply)
-          expect(show_page).to have_content(my_first_reply)
+          page.assert_text(my_first_reply)
+        end
+      end
+
+      it 'user collapses and expands reply for comments in an issue' do
+        Page::Project::Issue::Show.perform do |show_page|
+          one_reply = "1 reply"
 
           show_page.collapse_replies
           expect(show_page).to have_content(one_reply)
