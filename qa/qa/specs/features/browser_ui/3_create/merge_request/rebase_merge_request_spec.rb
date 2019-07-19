5ead2f7abac08d71d12199386cad1eea8fb8725a
Sanad Liaquat <sliaquat@gitlab.com>
2019-07-19 11:21:31 +0000

    Merge branch 'sl-qa-staging-66-quarantine-failing-spec' into 'master'
    Quarantine failing "rebase_merge_request_spec.rb"
    
    See merge request gitlab-org/gitlab-ce!30933
    
    (cherry picked from commit aab38a8c149f10eddfc03ce3bf6cb923fa7d8169)
    
    468daf5a Quarantine failing spec

--- qa/qa/specs/features/browser_ui/3_create/merge_request/rebase_merge_request_spec.rb
+++ qa/qa/specs/features/browser_ui/3_create/merge_request/rebase_merge_request_spec.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 module QA
-  context 'Create' do
+  # Failure issue: https://gitlab.com/gitlab-org/quality/staging/issues/66
+  context 'Create', :quarantine do
     describe 'Merge request rebasing' do
       it 'user rebases source branch of merge request' do
         Runtime::Browser.visit(:gitlab, Page::Main::Login)
