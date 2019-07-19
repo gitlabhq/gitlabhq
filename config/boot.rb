eedf735574b9f6c7c6c843943dd946ce84238236
Stan Hu <stanhu@gmail.com>
2019-07-19 13:00:46 -0700

    Make Bootsnap available via ENABLE_BOOTSNAP=1
    Bootsnap speeds up Rails loading and now ships by default with Rails 5.2
    apps. We should promote this to a default gem and test it out in
    production. This will also make it possible for the Helm Charts to take
    advantage of this.
    
    It appears that Bootsnap with Rails 5.2.3 and all the GitLab CE gems
    loads fine on an ARM platform now, so it's possible that
    https://gitlab.com/gitlab-org/gitlab-ce/issues/34799 has been resolved.
    
    Closes https://gitlab.com/gitlab-org/gitlab-ce/issues/45230

--- config/boot.rb
+++ config/boot.rb
@@ -3,7 +3,7 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 # Set up gems listed in the Gemfile.
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
 begin
-  require 'bootsnap/setup' unless ENV['DISABLE_BOOTSNAP']
+  require 'bootsnap/setup' if ENV['RAILS_ENV'] != 'production' || %w(1 yes true).include?(ENV['ENABLE_BOOTSNAP'])
 rescue LoadError
   # bootsnap is an optional dependency, so if we don't have it, it's fine
 end
