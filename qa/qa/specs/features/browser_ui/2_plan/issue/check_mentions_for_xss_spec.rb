033c1c0c3c8e15c120612c5e1671c253f37fec73
Sanad Liaquat <sliaquat@gitlab.com>
2019-07-19 19:23:43 +0500

    Create user as admin

--- qa/qa/specs/features/browser_ui/2_plan/issue/check_mentions_for_xss_spec.rb
+++ qa/qa/specs/features/browser_ui/2_plan/issue/check_mentions_for_xss_spec.rb
@@ -4,13 +4,23 @@ module QA
   context 'Plan' do
     describe 'check xss occurence in @mentions in issues' do
       before do
-        Runtime::Browser.visit(:gitlab, Page::Main::Login)
-        Page::Main::Login.perform(&:sign_in_using_credentials)
+        QA::Runtime::Env.personal_access_token = QA::Runtime::Env.admin_personal_access_token
+
+        unless QA::Runtime::Env.personal_access_token
+          Runtime::Browser.visit(:gitlab, Page::Main::Login)
+          Page::Main::Login.perform(&:sign_in_using_admin_credentials)
+        end
 
         user = Resource::User.fabricate_via_api! do |user|
           user.name = "eve <img src=x onerror=alert(2)&lt;img src=x onerror=alert(1)&gt;"
           user.password = "test1234"
         end
+
+        QA::Runtime::Env.personal_access_token = nil
+
+        Page::Main::Menu.perform(&:sign_out) if Page::Main::Menu.perform { |p| p.has_personal_area?(wait: 0) }
+
+        Page::Main::Login.perform(&:sign_in_using_credentials)
 
         project = Resource::Project.fabricate_via_api! do |resource|
           resource.name = 'xss-test-for-mentions-project'
