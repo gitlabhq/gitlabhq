f30b1d61d9adeaeb39febfd99ec0461b9a2251ea
Sergey Alekseev <sergey@asoft.co>
2019-07-23 20:17:59 +0000

    Speed up boot time
    by removing an unused `begin ... rescue ... end` block. The block is no longer necessary after moving the bootsnap gem to the :default group in eedf735574b9f6c7c6c843943dd946ce84238236.

--- config/boot.rb
+++ config/boot.rb
@@ -2,8 +2,4 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 # Set up gems listed in the Gemfile.
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
-begin
-  require 'bootsnap/setup' if ENV['RAILS_ENV'] != 'production' || %w(1 yes true).include?(ENV['ENABLE_BOOTSNAP'])
-rescue LoadError
-  # bootsnap is an optional dependency, so if we don't have it, it's fine
-end
+require 'bootsnap/setup' if ENV['RAILS_ENV'] != 'production' || %w(1 yes true).include?(ENV['ENABLE_BOOTSNAP'])
