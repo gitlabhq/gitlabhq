f0b2ef0b5fc4600c2a941a9b506873bc9dd80594
Jacques Erasmus <jerasmus@gitlab.com>
2019-07-25 17:38:06 +0000

    Update namespace label text for managed clusters
    Added the ability to update the namespace label text for gitlab-managed

--- app/assets/javascripts/pages/projects/clusters/show/index.js
+++ app/assets/javascripts/pages/projects/clusters/show/index.js
@@ -1,5 +1,7 @@
 import ClustersBundle from '~/clusters/clusters_bundle';
+import initGkeNamespace from '~/projects/gke_cluster_namespace';
 
 document.addEventListener('DOMContentLoaded', () => {
   new ClustersBundle(); // eslint-disable-line no-new
+  initGkeNamespace();
 });
