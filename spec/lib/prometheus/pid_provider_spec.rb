13676e021cddba7a801386a183dba696200312bf
Aleksei Lipniagov <alipniagov@gitlab.com>
2019-07-26 15:02:21 +0000

    Fix pid discovery for Unicorn in PidProvider

--- spec/lib/prometheus/pid_provider_spec.rb
+++ spec/lib/prometheus/pid_provider_spec.rb
@@ -25,22 +25,60 @@ describe Prometheus::PidProvider do
       before do
         stub_const('Unicorn::Worker', Class.new)
         hide_const('Puma')
+
+        expect(described_class).to receive(:process_name)
+          .at_least(:once)
+          .and_return(process_name)
       end
 
-      context 'when `Prometheus::Client::Support::Unicorn` provides worker_id' do
-        before do
-          expect(::Prometheus::Client::Support::Unicorn).to receive(:worker_id).and_return(1)
+      context 'when unicorn master is specified in process name' do
+        context 'when running in Omnibus' do
+          context 'before the process was renamed' do
+            let(:process_name) { "/opt/gitlab/embedded/bin/unicorn"}
+
+            it { is_expected.to eq 'unicorn_master' }
+          end
+
+          context 'after the process was renamed' do
+            let(:process_name) { "unicorn master -D -E production -c /var/opt/gitlab/gitlab-rails/etc/unicorn.rb /opt/gitlab/embedded/service/gitlab-rails/config.ru" }
+
+            it { is_expected.to eq 'unicorn_master' }
+          end
         end
 
-        it { is_expected.to eq 'unicorn_1' }
+        context 'when in development env' do
+          context 'before the process was renamed' do
+            let(:process_name) { "path_to_bindir/bin/unicorn_rails"}
+
+            it { is_expected.to eq 'unicorn_master' }
+          end
+
+          context 'after the process was renamed' do
+            let(:process_name) { "unicorn_rails master -c /gitlab_dir/config/unicorn.rb -E development" }
+
+            it { is_expected.to eq 'unicorn_master' }
+          end
+        end
       end
 
-      context 'when no worker_id is provided from `Prometheus::Client::Support::Unicorn`' do
-        before do
-          expect(::Prometheus::Client::Support::Unicorn).to receive(:worker_id).and_return(nil)
+      context 'when unicorn worker id is specified in process name' do
+        context 'when running in Omnibus' do
+          let(:process_name) { "unicorn worker[1] -D -E production -c /var/opt/gitlab/gitlab-rails/etc/unicorn.rb /opt/gitlab/embedded/service/gitlab-rails/config.ru" }
+
+          it { is_expected.to eq 'unicorn_1' }
         end
 
-        it { is_expected.to eq 'unicorn_master' }
+        context 'when in development env' do
+          let(:process_name) { "unicorn_rails worker[1] -c gitlab_dir/config/unicorn.rb -E development" }
+
+          it { is_expected.to eq 'unicorn_1' }
+        end
+      end
+
+      context 'when no specified unicorn master or worker id in process name' do
+        let(:process_name) { "bin/unknown_process"}
+
+        it { is_expected.to eq "process_#{Process.pid}" }
       end
     end
 
@@ -48,20 +86,20 @@ describe Prometheus::PidProvider do
       before do
         stub_const('Puma', Module.new)
         hide_const('Unicorn::Worker')
+
+        expect(described_class).to receive(:process_name)
+          .at_least(:once)
+          .and_return(process_name)
       end
 
       context 'when cluster worker id is specified in process name' do
-        before do
-          expect(described_class).to receive(:process_name).and_return('puma: cluster worker 1: 17483 [gitlab-puma-worker]')
-        end
+        let(:process_name) { 'puma: cluster worker 1: 17483 [gitlab-puma-worker]' }
 
         it { is_expected.to eq 'puma_1' }
       end
 
       context 'when no worker id is specified in process name' do
-        before do
-          expect(described_class).to receive(:process_name).and_return('bin/puma')
-        end
+        let(:process_name) { 'bin/puma' }
 
         it { is_expected.to eq 'puma_master' }
       end
