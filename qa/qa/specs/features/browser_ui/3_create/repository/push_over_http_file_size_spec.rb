6dbf634e93837e45a3cdc5c7beb8641cf1f9bbd1
Mark Lapierre <mlapierre@gitlab.com>
2019-07-31 14:55:57 +1000

    Fix push file size test
    And remove it from quarantine

--- qa/qa/specs/features/browser_ui/3_create/repository/push_over_http_file_size_spec.rb
+++ qa/qa/specs/features/browser_ui/3_create/repository/push_over_http_file_size_spec.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 module QA
-  # Failure issue: https://gitlab.com/gitlab-org/quality/nightly/issues/113
-  context 'Create', :requires_admin, :quarantine do
+  context 'Create', :requires_admin do
     describe 'push after setting the file size limit via admin/application_settings' do
       before(:context) do
         @project = Resource::Project.fabricate_via_api! do |p|
@@ -21,14 +20,21 @@ module QA
 
       it 'push successful when the file size is under the limit' do
         set_file_size_limit(5)
-        push = push_new_file('oversize_file_1.bin', wait_for_push: true)
-        expect(push.output).not_to have_content 'remote: fatal: pack exceeds maximum allowed size'
+
+        retry_on_fail do
+          push = push_new_file('oversize_file_1.bin', wait_for_push: true)
+
+          expect(push.output).not_to have_content 'remote: fatal: pack exceeds maximum allowed size'
+        end
       end
 
       it 'push fails when the file size is above the limit' do
         set_file_size_limit(1)
-        expect { push_new_file('oversize_file_2.bin', wait_for_push: false) }
-          .to raise_error(QA::Git::Repository::RepositoryCommandError, /remote: fatal: pack exceeds maximum allowed size/)
+
+        retry_on_fail do
+          expect { push_new_file('oversize_file_2.bin', wait_for_push: false) }
+            .to raise_error(QA::Git::Repository::RepositoryCommandError, /remote: fatal: pack exceeds maximum allowed size/)
+        end
       end
 
       def set_file_size_limit(limit)
@@ -53,6 +59,22 @@ module QA
         @project.wait_for_push commit_message
 
         output
+      end
+
+      # Application settings are cached for up to a minute. So when we change
+      # the `receive_max_input_size` setting, the setting might not be applied
+      # for minute. This caused the tests to intermittently fail.
+      # See https://gitlab.com/gitlab-org/quality/nightly/issues/113
+      #
+      # Instead of waiting a minute after changing the setting, we retry the
+      # attempt to push if it fails. Most of the time the setting is updated in
+      # under a minute, i.e., in fewer than 6 attempts with a 10 second sleep
+      # between attempts.
+      # See https://gitlab.com/gitlab-org/gitlab-ce/merge_requests/30233#note_188616863
+      def retry_on_fail
+        Support::Retrier.retry_on_exception(max_attempts: 6, reload_page: nil, sleep_interval: 10) do
+          yield
+        end
       end
     end
   end
