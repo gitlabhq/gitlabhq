ngen-brett/gitlab-ce

Add commit_id to draft_notes table
CE port of https://gitlab.com/gitlab-org/gitlab-ee/merge_requests/14520

db/migrate/20190703043358_add_commit_id_to_draft_notes.rb
@@ -1,0 +1,11 @@
+# frozen_string_literal: true
+
+class AddCommitIdToDraftNotes < ActiveRecord::Migration[5.1]
+  include Gitlab::Database::MigrationHelpers
+
+  DOWNTIME = false
+
+  def change
+    add_column :draft_notes, :commit_id, :string
+  end
+end

gitlabhq/gitlabhq

db/migrate/20190703043358_add_commit_id_to_draft_notes.rb
@@ -1,0 +1,11 @@
+# frozen_string_literal: true
+
+class AddCommitIdToDraftNotes < ActiveRecord::Migration[5.1]
+  include Gitlab::Database::MigrationHelpers
+
+  DOWNTIME = false
+
+  def change
+    add_column :draft_notes, :commit_id, :binary
+  end
+end