f74b33cf6e5ee415417d9f2ee255f97ab6fcf7ad
Igor <idrozdov@gitlab.com>
2019-08-01 16:51:14 +0000

    Add can_merge option to autocomplete response
    It's used only if merge_request_iid param is provided

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,4 +2,21 @@
 
 class UserSerializer < BaseSerializer
   entity UserEntity
+
+  def represent(resource, opts = {}, entity = nil)
+    if params[:merge_request_iid]
+      merge_request = opts[:project].merge_requests.find_by_iid!(params[:merge_request_iid])
+      preload_max_member_access(merge_request.project, Array(resource))
+
+      super(resource, opts.merge(merge_request: merge_request), MergeRequestAssigneeEntity)
+    else
+      super
+    end
+  end
+
+  private
+
+  def preload_max_member_access(project, users)
+    project.team.max_member_access_for_user_ids(users.map(&:id))
+  end
 end
