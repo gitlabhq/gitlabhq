87235d009c8d5f40ec0f29575c7af6e91cb0a926
Jason Colyer <jcolyer@Jasons-MacBook-Pro.local>
2019-08-02 14:35:10 -0500

    Make issue boards importable
    - Added Importable to models/list.rb
    - Did unless: :importable? on board validation
    - Created changelog
    - Modified haml to show issue boards are importable
    - Added needed spec tests
    - Modified project.json to include board information
    - Added relevant models to all_models
    - Added relevant models to import_export
    - Added relevant models to safe_model_attributes

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,10 +3,11 @@
 class List < ApplicationRecord
   belongs_to :board
   belongs_to :label
+  include Importable
 
   enum list_type: { backlog: 0, label: 1, closed: 2, assignee: 3, milestone: 4 }
 
-  validates :board, :list_type, presence: true
+  validates :board, :list_type, presence: true, unless: :importing?
   validates :label, :position, presence: true, if: :label?
   validates :label_id, uniqueness: { scope: :board_id }, if: :label?
   validates :position, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, if: :movable?
