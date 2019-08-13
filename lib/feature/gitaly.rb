3c5647bd57b3d651daade57abf0b9f266407a222
GitalyBot <gitalybot@zjvandeweg.nl>
2019-08-13 05:54:12 +0000

    Upgrade Gitaly to v1.59.0

--- lib/feature/gitaly.rb
+++ lib/feature/gitaly.rb
@@ -7,7 +7,7 @@ class Feature
     # Server feature flags should use '_' to separate words.
     SERVER_FEATURE_FLAGS =
       [
-        'get_commit_signatures'.freeze
+        # 'get_commit_signatures'.freeze
       ].freeze
 
     DEFAULT_ON_FLAGS = Set.new([]).freeze
