0c526db57e40fa440f331d6dff1f67172e3e1cad
Alex Buijs <abuijs@gitlab.com>
2019-08-14 21:33:39 +0000

    Add link to resend confirmation email
    This link is shown when a user tries to login with an unconfirmed
    email address and the grace period has expired

---
+++ spec/helpers/sessions_helper_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe SessionsHelper do
+  describe '#unconfirmed_email?' do
+    it 'returns true when the flash alert contains a devise failure unconfirmed message' do
+      flash[:alert] = t(:unconfirmed, scope: [:devise, :failure])
+      expect(helper.unconfirmed_email?).to be_truthy
+    end
+
+    it 'returns false when the flash alert does not contain a devise failure unconfirmed message' do
+      flash[:alert] = 'something else'
+      expect(helper.unconfirmed_email?).to be_falsey
+    end
+  end
+end
