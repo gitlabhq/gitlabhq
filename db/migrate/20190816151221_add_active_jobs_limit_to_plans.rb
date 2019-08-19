4e39edd18b6d2f416134c348afca628cf8497f4d
Fabio Pitino <fpitino@gitlab.com>
2019-08-19 16:29:53 +0200

    Add active_jobs_limit to plans table
    This is a port from EE changes where
    we introduce a new limit for Plan model.
    
    https://dev.gitlab.org/gitlab/gitlab-ee/merge_requests/1182

---
+++ db/migrate/20190816151221_add_active_jobs_limit_to_plans.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class AddActiveJobsLimitToPlans < ActiveRecord::Migration[5.1]
+  include Gitlab::Database::MigrationHelpers
+
+  DOWNTIME = false
+
+  disable_ddl_transaction!
+
+  def up
+    add_column_with_default :plans, :active_jobs_limit, :integer, default: 0
+  end
+
+  def down
+    remove_column :plans, :active_jobs_limit
+  end
+end
