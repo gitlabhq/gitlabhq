96b994bd05db072655bcd28204770013feeaa779
Igor Drozdov <idrozdov@gitlab.com>
2019-08-19 11:34:19 +0300

    Fix comments related to BE part
    - Edit commit messge body to fix "danger-review"

--- app/serializers/merge_request_sidebar_basic_entity.rb
+++ app/serializers/merge_request_sidebar_basic_entity.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class MergeRequestSidebarBasicEntity < IssuableSidebarBasicEntity
+  expose :current_user, if: lambda { |_issuable| current_user } do
+    expose :can_merge do |merge_request|
+      merge_request.can_be_merged_by?(current_user)
+    end
+  end
 end
-
-MergeRequestSidebarBasicEntity.prepend_if_ee('EE::MergeRequestSidebarBasicEntity')
