d7ae3efb87873425bce19dfa3ff1cce9e6acc8c0
Paul Okstad <pokstad@gitlab.com>
2019-08-22 06:53:14 +0000

    Add Gitaly info-ref cache feature flags

--- lib/feature/gitaly.rb
+++ lib/feature/gitaly.rb
@@ -6,8 +6,10 @@ class Feature
   class Gitaly
     # Server feature flags should use '_' to separate words.
     SERVER_FEATURE_FLAGS =
-      [
-        'get_commit_signatures'.freeze
+      %w[
+        get_commit_signatures
+        cache_invalidator
+        inforef_uploadpack_cache
       ].freeze
 
     DEFAULT_ON_FLAGS = Set.new([]).freeze
