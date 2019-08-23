99327de207965fe85e6c7759ebb2ef014b3574e8
Mark Lapierre <mlapierre@gitlab.com>
2019-08-23 14:06:22 +0000

    Fail E2E tests early if template not found via API

--- qa/qa/runtime/fixtures.rb
+++ qa/qa/runtime/fixtures.rb
@@ -3,10 +3,19 @@
 module QA
   module Runtime
     module Fixtures
+      include Support::Api
+
+      TemplateNotFoundError = Class.new(RuntimeError)
+
       def fetch_template_from_api(api_path, key)
         request = Runtime::API::Request.new(api_client, "/templates/#{api_path}/#{key}")
-        get request.url
-        json_body[:content]
+        response = get(request.url)
+
+        unless response.code == HTTP_STATUS_OK
+          raise TemplateNotFoundError, "Template at #{request.mask_url} could not be found (#{response.code}): `#{response}`."
+        end
+
+        parse_body(response)[:content]
       end
 
       private
