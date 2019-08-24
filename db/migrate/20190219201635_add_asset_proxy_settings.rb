ce07df77cc4d96d06dacc4319e4e42c23afe850e
Brett Walker <bwalker@gitlab.com>
2019-08-23 20:17:00 -0500

    disable cop Migration/AddLimitToStringColumns

--- db/migrate/20190219201635_add_asset_proxy_settings.rb
+++ db/migrate/20190219201635_add_asset_proxy_settings.rb
@@ -8,9 +8,9 @@ class AddAssetProxySettings < ActiveRecord::Migration[5.0]
 
   def change
     add_column :application_settings, :asset_proxy_enabled, :boolean, default: false, null: false
-    add_column :application_settings, :asset_proxy_url, :string
+    add_column :application_settings, :asset_proxy_url, :string # rubocop:disable Migration/AddLimitToStringColumns
     add_column :application_settings, :asset_proxy_whitelist, :text
     add_column :application_settings, :encrypted_asset_proxy_secret_key, :text
-    add_column :application_settings, :encrypted_asset_proxy_secret_key_iv, :string
+    add_column :application_settings, :encrypted_asset_proxy_secret_key_iv, :string # rubocop:disable Migration/AddLimitToStringColumns
   end
 end
