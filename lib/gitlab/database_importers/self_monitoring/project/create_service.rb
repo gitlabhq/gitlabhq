b99cf2024d1b518d876bc4f1f4aac8549b4dd993
rpereira2 <rpereira@gitlab.com>
2019-08-26 12:41:25 +0530

    Add nil check for Gitlab.config.prometheus

--- lib/gitlab/database_importers/self_monitoring/project/create_service.rb
+++ lib/gitlab/database_importers/self_monitoring/project/create_service.rb
@@ -175,7 +175,7 @@ module Gitlab
           end
 
           def prometheus_enabled?
-            Gitlab.config.prometheus.enable
+            Gitlab.config.prometheus.enable if Gitlab.config.prometheus
           rescue Settingslogic::MissingSetting
             log_error(_('prometheus.enable is not present in gitlab.yml'))
 
@@ -183,7 +183,7 @@ module Gitlab
           end
 
           def prometheus_listen_address
-            Gitlab.config.prometheus.listen_address
+            Gitlab.config.prometheus.listen_address if Gitlab.config.prometheus
           rescue Settingslogic::MissingSetting
             log_error(_('prometheus.listen_address is not present in gitlab.yml'))
 
