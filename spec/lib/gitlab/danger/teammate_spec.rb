e17ba8d9e32c9d547ff9ca31056bbf7abd864488
Rémy Coutable <remy@rymai.me>
2019-08-27 19:13:55 +0200

    Fix a nil error in Gitlab::Danger::Teammate
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- spec/lib/gitlab/danger/teammate_spec.rb
+++ spec/lib/gitlab/danger/teammate_spec.rb
@@ -50,6 +50,14 @@ describe Gitlab::Danger::Teammate do
         end
       end
 
+      context 'when role is Test Automation Engineer' do
+        let(:role) { 'Test Automation Engineer' }
+
+        it '#reviewer? returns false' do
+          expect(subject.reviewer?(project, :test, labels)).to be_falsey
+        end
+      end
+
       context 'when role is Test Automation Engineer, Manage' do
         let(:role) { 'Test Automation Engineer, Manage' }
 
