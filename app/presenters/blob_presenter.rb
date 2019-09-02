bf230da05d8a3a6feeeb80d81fa0370cc295478e
Patrick Bajao <ebajao@gitlab.com>
2019-09-02 11:19:32 +0800

    Support selective highlighting of lines
    Instead of highlighting all lines when not all of them
    are needed, only highlight from the beginning up to
    the specified limit.
    
    The `BlobPresenter#highlight` method has been updated
    to support `to` param. This param will be used to limit
    the content to be highlighted.

--- app/presenters/blob_presenter.rb
+++ app/presenters/blob_presenter.rb
@@ -3,12 +3,12 @@
 class BlobPresenter < Gitlab::View::Presenter::Delegated
   presents :blob
 
-  def highlight(plain: nil)
+  def highlight(to: nil, plain: nil)
     load_all_blob_data
 
     Gitlab::Highlight.highlight(
       blob.path,
-      blob.data,
+      limited_blob_data(to: to),
       language: blob.language_from_gitattributes,
       plain: plain
     )
@@ -22,5 +22,19 @@ class BlobPresenter < Gitlab::View::Presenter::Delegated
 
   def load_all_blob_data
     blob.load_all_data! if blob.respond_to?(:load_all_data!)
+  end
+
+  def limited_blob_data(to: nil)
+    return blob.data if to.blank?
+
+    # Even though we don't need all the lines at the start of the file (e.g
+    # viewing the middle part of a file), they still need to be highlighted
+    # to ensure that the succeeding lines can be formatted correctly (e.g.
+    # multi-line comments).
+    all_lines[0..to - 1].join
+  end
+
+  def all_lines
+    @all_lines ||= blob.data.lines
   end
 end
