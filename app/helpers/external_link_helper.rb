e6090e713b53ed748dc1a8fef962732bdf37ddfe
jakeburden <jburden@gitlab.com>
2019-09-03 17:26:39 -0400

    Allow for the passing of options to external_link

--- app/helpers/external_link_helper.rb
+++ app/helpers/external_link_helper.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 module ExternalLinkHelper
-  def external_link(body, url)
-    link_to url, target: '_blank', rel: 'noopener noreferrer' do
+  def external_link(body, url, options = {})
+    link_to url, { target: '_blank', rel: 'noopener noreferrer' }.merge(options) do
       "#{body} #{icon('external-link')}".html_safe
     end
   end
--- spec/helpers/external_link_helper_spec.rb
+++ spec/helpers/external_link_helper_spec.rb
@@ -9,4 +9,9 @@ describe ExternalLinkHelper do
     expect(external_link('https://gitlab.com', 'https://gitlab.com').to_s)
       .to eq('<a target="_blank" rel="noopener noreferrer" href="https://gitlab.com">https://gitlab.com <i aria-hidden="true" data-hidden="true" class="fa fa-external-link"></i></a>')
   end
+
+  it 'allows options when creating external link with icon' do
+    expect(external_link('https://gitlab.com', 'https://gitlab.com', { "data-foo": "bar", class: "externalLink" }).to_s)
+      .to eq('<a target="_blank" rel="noopener noreferrer" data-foo="bar" class="externalLink" href="https://gitlab.com">https://gitlab.com <i aria-hidden="true" data-hidden="true" class="fa fa-external-link"></i></a>')
+  end
 end
