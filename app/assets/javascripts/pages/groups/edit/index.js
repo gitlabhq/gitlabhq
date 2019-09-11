c27f619c2a7609af3a7478ae6a6e2d1f210b5be3
Jan Beckmann <king-jan1999@hotmail.de>
2019-09-11 14:28:34 +0000

    Disable "transfer group" button when no parent group is selected
    Refactor project_edit.js to be reusable for this purpose
    
    Closes #64799

--- app/assets/javascripts/pages/groups/edit/index.js
+++ app/assets/javascripts/pages/groups/edit/index.js
@@ -2,6 +2,7 @@ import initAvatarPicker from '~/avatar_picker';
 import TransferDropdown from '~/groups/transfer_dropdown';
 import initConfirmDangerModal from '~/confirm_danger_modal';
 import initSettingsPanels from '~/settings_panels';
+import setupTransferEdit from '~/transfer_edit';
 import dirtySubmitFactory from '~/dirty_submit/dirty_submit_factory';
 import mountBadgeSettings from '~/pages/shared/mount_badge_settings';
 import { GROUP_BADGE } from '~/badges/constants';
@@ -17,6 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('.js-general-settings-form, .js-general-permissions-form'),
   );
   mountBadgeSettings(GROUP_BADGE);
+  setupTransferEdit('.js-group-transfer-form', '#new_parent_group_id');
 
   // Initialize Subgroups selector
   groupsSelect();
---
+++ spec/frontend/transfer_edit_spec.js
@@ -0,0 +1,53 @@
+import $ from 'jquery';
+
+import setupTransferEdit from '~/transfer_edit';
+import { loadHTMLFixture } from 'helpers/fixtures';
+
+describe('setupTransferEdit', () => {
+  const formSelector = '.js-project-transfer-form';
+  const targetSelector = 'select.select2';
+
+  beforeEach(() => {
+    loadHTMLFixture('projects/edit.html');
+    setupTransferEdit(formSelector, targetSelector);
+  });
+
+  it('disables submit button on load', () => {
+    expect(
+      $(formSelector)
+        .find(':submit')
+        .prop('disabled'),
+    ).toBe(true);
+  });
+
+  it('enables submit button when selection changes to non-empty value', () => {
+    const nonEmptyValue = $(formSelector)
+      .find(targetSelector)
+      .find('option')
+      .not(':empty')
+      .val();
+    $(formSelector)
+      .find(targetSelector)
+      .val(nonEmptyValue)
+      .trigger('change');
+
+    expect(
+      $(formSelector)
+        .find(':submit')
+        .prop('disabled'),
+    ).toBeFalsy();
+  });
+
+  it('disables submit button when selection changes to empty value', () => {
+    $(formSelector)
+      .find(targetSelector)
+      .val('')
+      .trigger('change');
+
+    expect(
+      $(formSelector)
+        .find(':submit')
+        .prop('disabled'),
+    ).toBe(true);
+  });
+});
