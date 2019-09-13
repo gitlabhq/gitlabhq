9ce103af0b974b71321d4e0e9937ee82e10c740f
Fabio Pitino <fpitino@gitlab.com>
2019-09-13 07:40:00 +0100

    Cancel all running CI jobs when user is blocked
    This prevents a MITM attack where attacker could
    still access Git repository if any jobs were
    running long enough.

---
+++ spec/services/ci/cancel_user_pipelines_service_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Ci::CancelUserPipelinesService do
+  describe '#execute' do
+    let(:user) { create(:user) }
+
+    subject { described_class.new.execute(user) }
+
+    context 'when user has running CI pipelines' do
+      let(:pipeline) { create(:ci_pipeline, :running, user: user) }
+      let!(:build) { create(:ci_build, :running, pipeline: pipeline) }
+
+      it 'cancels all running pipelines and related jobs' do
+        subject
+
+        expect(pipeline.reload).to be_canceled
+        expect(build.reload).to be_canceled
+      end
+    end
+  end
+end
