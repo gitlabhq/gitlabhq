8395032721f6d6cb26126a5bffcb42984a240c07
Kerri Miller <kerrizor@kerrizor.com>
2019-09-23 10:55:32 -0700

    Avoid #authenticate_user! in #route_not_found
    This method, #route_not_found, is executed as the final fallback for
    unrecognized routes (as the name might imply.) We want to avoid
    `#authenticate_user!` when calling `#route_not_found`;
    `#authenticate_user!` can, depending on the request format, return a 401
    instead of redirecting to a login page. This opens a subtle security
    exploit where anonymous users will receive a 401 response when
    attempting to access a private repo, while a recognized user will
    receive a 404, exposing the existence of the private, hidden repo.

--- spec/controllers/projects/issues_controller_spec.rb
+++ spec/controllers/projects/issues_controller_spec.rb
@@ -1440,7 +1440,7 @@ describe Projects::IssuesController do
   context 'private project with token authentication' do
     let(:private_project) { create(:project, :private) }
 
-    it_behaves_like 'authenticates sessionless user', :index, :atom do
+    it_behaves_like 'authenticates sessionless user', :index, :atom, ignore_incrementing: true do
       before do
         default_params.merge!(project_id: private_project, namespace_id: private_project.namespace)
 
@@ -1448,7 +1448,7 @@ describe Projects::IssuesController do
       end
     end
 
-    it_behaves_like 'authenticates sessionless user', :calendar, :ics do
+    it_behaves_like 'authenticates sessionless user', :calendar, :ics, ignore_incrementing: true do
       before do
         default_params.merge!(project_id: private_project, namespace_id: private_project.namespace)
 
