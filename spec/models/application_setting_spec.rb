da6a3067ec16715a580bd914f55c9d638d96c73b
David Wilkins <dwilkins@gitlab.com>
2019-10-24 18:53:34 +0000

    Handle Stored XSS for Grafana URL in settings
    - Extend Gitlab::UrlBlocker to allow relative urls (require_absolute
      setting).  The new `require_absolute` setting defaults to true,
      which is the existing behavior.
    
    - Extend AddressableUrlValidator to accept `require_abosolute` and
      default to the existing behavior
    
    - Add validation for ApplicationSetting#grafana_url to validate that
      the URL does not contain XSS but can be a valid relative or absolute
      url.
    
    - In the case of existing stored URLs, validate the stored URL does
      not contain XSS. If the stored URL contains stored XSS or is an
      otherwise invalid URL, return the default database column value.
    
    - Add tests for Gitlab::UrlBlocker to test require_absolute setting
    
    - Add tests for AddressableUrlValidator
    
    - Add tests for ApplicationSetting#grafana_url

--- spec/models/application_setting_spec.rb
+++ spec/models/application_setting_spec.rb
@@ -17,6 +17,7 @@ describe ApplicationSetting do
     let(:http)  { 'http://example.com' }
     let(:https) { 'https://example.com' }
     let(:ftp)   { 'ftp://example.com' }
+    let(:javascript) { 'javascript:alert(window.opener.document.location)' }
 
     it { is_expected.to allow_value(nil).for(:home_page_url) }
     it { is_expected.to allow_value(http).for(:home_page_url) }
@@ -47,6 +48,11 @@ describe ApplicationSetting do
     it { is_expected.to allow_value(['gitlab.com']).for(:outbound_local_requests_whitelist) }
     it { is_expected.not_to allow_value(nil).for(:outbound_local_requests_whitelist) }
     it { is_expected.to allow_value([]).for(:outbound_local_requests_whitelist) }
+
+    it { is_expected.to allow_value(http).for(:grafana_url) }
+    it { is_expected.to allow_value(https).for(:grafana_url) }
+    it { is_expected.not_to allow_value(ftp).for(:grafana_url) }
+    it { is_expected.not_to allow_value(javascript).for(:grafana_url) }
 
     context "when user accepted let's encrypt terms of service" do
       before do
