879ca8ab319a60f114e99a57848467d252fc65c7
Aakriti Gupta <agupta@gitlab.com>
2019-11-06 17:07:11 +0100

    Prevent guests from seeing commits for cycle analytics - if the user has access level lower than REPORTER, don't include commit count in summary

--- lib/gitlab/cycle_analytics/stage_summary.rb
+++ lib/gitlab/cycle_analytics/stage_summary.rb
@@ -10,12 +10,28 @@ module Gitlab
       end
 
       def data
-        [serialize(Summary::Issue.new(project: @project, from: @from, current_user: @current_user)),
-         serialize(Summary::Commit.new(project: @project, from: @from)),
-         serialize(Summary::Deploy.new(project: @project, from: @from))]
+        summary = [issue_stats]
+        summary << commit_stats if user_has_sufficient_access?
+        summary << deploy_stats
       end
 
       private
+
+      def issue_stats
+        serialize(Summary::Issue.new(project: @project, from: @from, current_user: @current_user))
+      end
+
+      def commit_stats
+        serialize(Summary::Commit.new(project: @project, from: @from))
+      end
+
+      def deploy_stats
+        serialize(Summary::Deploy.new(project: @project, from: @from))
+      end
+
+      def user_has_sufficient_access?
+        @project.team.member?(@current_user, Gitlab::Access::REPORTER)
+      end
 
       def serialize(summary_object)
         AnalyticsSummarySerializer.new.represent(summary_object)
--- spec/lib/gitlab/cycle_analytics/stage_summary_spec.rb
+++ spec/lib/gitlab/cycle_analytics/stage_summary_spec.rb
@@ -8,6 +8,10 @@ describe Gitlab::CycleAnalytics::StageSummary do
   let(:user) { create(:user, :admin) }
   subject { described_class.new(project, from: Time.now, current_user: user).data }
 
+  before do
+    project.add_maintainer(user)
+  end
+
   describe "#new_issues" do
     it "finds the number of issues created after the 'from date'" do
       Timecop.freeze(5.days.ago) { create(:issue, project: project) }
@@ -41,6 +45,23 @@ describe Gitlab::CycleAnalytics::StageSummary do
       Timecop.freeze(5.days.from_now) { create_commit("Test message", project, user, 'master', count: 100) }
 
       expect(subject.second[:value]).to eq(100)
+    end
+
+    context 'when a guest user is signed in' do
+      let(:guest_user) { create(:user) }
+
+      before do
+        project.add_guest(guest_user)
+      end
+
+      it 'does not include commit stats' do
+        data = described_class.new(project, from: from, current_user: guest_user).data
+        expect(includes_commits?(data)).to be_falsy
+      end
+
+      def includes_commits?(data)
+        data.any? { |h| h["title"] == 'Commits' }
+      end
     end
   end
 
